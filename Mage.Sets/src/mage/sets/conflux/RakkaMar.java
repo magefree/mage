2cf247d68b0ecc3fa3581750f173c3ba2c62c6ce
Loki <loki@magefree>
2010-10-05 10:30:10 +0000

    some cards

---
+++ Mage.Sets/src/mage/sets/conflux/RakkaMar.java
@@ -0,0 +1,94 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+
+package mage.sets.conflux;
+
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Rarity;
+import mage.Constants.Zone;
+import mage.MageInt;
+import mage.ObjectColor;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.CreateTokenEffect;
+import mage.abilities.keyword.HasteAbility;
+import mage.cards.CardImpl;
+import mage.game.permanent.token.Token;
+
+/**
+ *
+ * @author Loki
+ */
+public class RakkaMar extends CardImpl<RakkaMar> {
+    private static ElementalToken token = new ElementalToken();
+
+    public RakkaMar(UUID ownerId) {
+        super(ownerId, "Rakka Mar", Rarity.RARE, new CardType[]{CardType.CREATURE}, "{2}{R}{R}");
+        this.expansionSetCode = "CON";
+        this.supertype.add("Legendary");
+        this.subtype.add("Human");
+        this.subtype.add("Shaman");
+        this.color.setRed(true);
+        this.power = new MageInt(2);
+	this.toughness = new MageInt(2);
+        this.abilities.add(HasteAbility.getInstance());
+        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new CreateTokenEffect(token), new ManaCostsImpl("{1}{R}"));
+        ability.addCost(new TapSourceCost());
+        this.addAbility(ability);
+    }
+
+    public RakkaMar(final RakkaMar card) {
+        super(card);
+    }
+
+    @Override
+    public RakkaMar copy() {
+        return new RakkaMar(this);
+    }
+
+    @Override
+    public String getArt() {
+        return "118775_typ_reg_sty_010.jpg";
+    }
+
+    private static class ElementalToken extends Token {
+        public ElementalToken() {
+            super("Elemental", "3/1 red Elemental creature with Haste");
+            cardType.add(CardType.CREATURE);
+            color = ObjectColor.RED;
+            subtype.add("Elemental");
+            power = new MageInt(3);
+            toughness = new MageInt(1);
+            abilities.add(HasteAbility.getInstance());
+        }
+    }
+
+}
---
+++ Mage.Sets/src/mage/sets/conflux/SoulsMajesty.java
@@ -0,0 +1,102 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+
+package mage.sets.conflux;
+
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Outcome;
+import mage.Constants.Rarity;
+import mage.abilities.Ability;
+import mage.abilities.effects.OneShotEffect;
+import mage.cards.CardImpl;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.players.Player;
+import mage.target.common.TargetCrearurePermanentYourControl;
+
+/**
+ *
+ * @author Loki
+ */
+public class SoulsMajesty extends CardImpl<SoulsMajesty> {
+
+    public SoulsMajesty(UUID ownerId) {
+        super(ownerId, "Soul's Majesty", Rarity.RARE, new CardType[]{CardType.SORCERY}, "{4}{G}");
+        this.expansionSetCode = "CON";
+        this.color.setGreen(true);
+        this.getSpellAbility().addTarget(new TargetCrearurePermanentYourControl());
+        this.getSpellAbility().addEffect(new SoulsMajestyEffect());
+    }
+
+    public SoulsMajesty(final SoulsMajesty card) {
+        super(card);
+    }
+
+    @Override
+    public SoulsMajesty copy() {
+        return new SoulsMajesty(this);
+    }
+
+    @Override
+    public String getArt() {
+        return "118768_typ_reg_sty_010.jpg";
+    }
+
+    private class SoulsMajestyEffect extends OneShotEffect<SoulsMajestyEffect> {
+
+        public SoulsMajestyEffect() {
+            super(Outcome.DrawCard);
+        }
+
+        public SoulsMajestyEffect(final SoulsMajestyEffect effect) {
+            super(effect);
+        }
+
+        @Override
+        public boolean apply(Game game, Ability source) {
+            Permanent target = game.getPermanent(source.getFirstTarget());
+            Player player = game.getPlayer(source.getControllerId());
+		if (player != null && target != null) {
+			player.drawCards(target.getPower().getValue(), game);
+			return true;
+		}
+		return false;
+        }
+
+        @Override
+        public SoulsMajestyEffect copy() {
+            return new SoulsMajestyEffect(this);
+        }
+
+        @Override
+        public String getText(Ability source) {
+            return "Draw cards equal to the power of target creature you control" + super.getText(source);
+        }
+    }
+}
