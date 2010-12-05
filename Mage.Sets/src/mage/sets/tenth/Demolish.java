24d14e87596bd9ec98553d65caf96c1dae92a6b7
BetaSteward <betasteward@gmail>
2010-12-05 21:59:51 +0000

    fixed issue 45

--- Mage.Sets/src/mage/sets/tenth/Demolish.java
+++ Mage.Sets/src/mage/sets/tenth/Demolish.java
@@ -52,9 +52,9 @@ public class Demolish extends CardImpl<Demolish> {
 	}
 
 	public Demolish(UUID ownerId) {
-		super(ownerId, 196, "Demolish", Rarity.COMMON, new CardType[]{CardType.SORCERY}, "{B}");
+		super(ownerId, 196, "Demolish", Rarity.COMMON, new CardType[]{CardType.SORCERY}, "{3}{R}");
 		this.expansionSetCode = "10E";
-		this.color.setBlack(true);
+		this.color.setRed(true);
 		this.getSpellAbility().addTarget(new TargetPermanent(filter));
 		this.getSpellAbility().addEffect(new DestroyTargetEffect());
 	}
