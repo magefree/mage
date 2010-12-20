67b393223f8918db124a46230aa44e431274eb98
Loki <loki@magefree>
2010-12-20 21:55:03 +0200

    special for nantuko - ravnika block lands :)

--- Mage.Sets/src/mage/sets/Sets.java
+++ Mage.Sets/src/mage/sets/Sets.java
@@ -60,6 +60,8 @@ public class Sets extends HashMap<String, ExpansionSet> {
 		names = new TreeSet<String>();
 		this.addSet(AlaraReborn.getInstance());
 		this.addSet(Conflux.getInstance());
+        this.addSet(Dissension.getInstance());
+        this.addSet(Guildpact.getInstance());
 		this.addSet(Magic2010.getInstance());
 		this.addSet(Magic2011.getInstance());
 		this.addSet(Planechase.getInstance());
