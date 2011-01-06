9384cc36e465e25d1b67ff6e86c05724a8600cc8
maurer.it <maurer.it@gmail>
2011-01-06 10:28:31 -0500

    Consuming Vapors

---
+++ Mage.Sets/src/mage/sets/riseoftheeldrazi/ConsumingVapors.java
@@ -0,0 +1,121 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.riseoftheeldrazi;
+
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Outcome;
+import mage.Constants.Rarity;
+import mage.Constants.TargetController;
+import mage.abilities.Ability;
+import mage.abilities.effects.OneShotEffect;
+import mage.abilities.keyword.ReboundAbility;
+import mage.cards.CardImpl;
+import mage.filter.common.FilterControlledPermanent;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.players.Player;
+import mage.target.TargetPermanent;
+import mage.target.TargetPlayer;
+
+/**
+ *
+ * @author maurer.it_at_gmail.com
+ */
+public class ConsumingVapors extends CardImpl<ConsumingVapors> {
+
+	
+
+	public ConsumingVapors(UUID ownerId) {
+		super(ownerId, 101, "Consuming Vapors", Rarity.RARE, new CardType[]{CardType.SORCERY}, "{3}{B}");
+		this.expansionSetCode = "ROE";
+
+		this.color.setBlack(true);
+		this.getSpellAbility().addEffect(new ConsumingVaporsEffect());
+		this.getSpellAbility().addTarget(new TargetPlayer());
+		this.addAbility(new ReboundAbility());
+	}
+
+	public ConsumingVapors(final ConsumingVapors card) {
+		super(card);
+	}
+
+	@Override
+	public ConsumingVapors copy() {
+		return new ConsumingVapors(this);
+	}
+}
+
+class ConsumingVaporsEffect extends OneShotEffect<ConsumingVaporsEffect> {
+
+	protected static final FilterControlledPermanent filter;
+
+	static {
+		filter = new FilterControlledPermanent();
+		filter.getCardType().add(CardType.CREATURE);
+		filter.setMessage(" a creature");
+	}
+
+	ConsumingVaporsEffect ( ) {
+		super(Outcome.Sacrifice);
+	}
+
+	ConsumingVaporsEffect ( ConsumingVaporsEffect effect ) {
+		super(effect);
+	}
+
+	@Override
+	public boolean apply(Game game, Ability source) {
+		Player player = game.getPlayer(source.getTargets().getFirstTarget());
+		Player controller = game.getPlayer(source.getControllerId());
+		filter.setTargetController(TargetController.YOU);
+		TargetPermanent target = new TargetPermanent(1, 1, filter, false);
+
+		//A spell or ability could have removed the only legal target this player
+		//had, if thats the case this ability should fizzle.
+		if (target.canChoose(player.getId(), game)) {
+			while (!target.isChosen()) {
+				player.choose(Outcome.Sacrifice, target, game);
+			}
+
+			Permanent permanent = game.getPermanent(target.getFirstTarget());
+
+			if ( permanent != null ) {
+				controller.gainLife(permanent.getToughness().getValue(), game);
+				return permanent.sacrifice(source.getId(), game);
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public ConsumingVaporsEffect copy() {
+		return new ConsumingVaporsEffect(this);
+	}
+
+}
