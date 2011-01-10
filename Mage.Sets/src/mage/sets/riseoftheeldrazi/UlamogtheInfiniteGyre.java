1d521779689368ad2114acb54c5819fce74267aa
maurer.it <maurer.it@gmail>
2011-01-10 14:24:52 -0500

    ROE cards and AnnihilatorAbility

---
+++ Mage.Sets/src/mage/sets/riseoftheeldrazi/UlamogtheInfiniteGyre.java
@@ -0,0 +1,148 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.riseoftheeldrazi;
+
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Outcome;
+import mage.Constants.Rarity;
+import mage.Constants.Zone;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.TriggeredAbilityImpl;
+import mage.abilities.common.ZoneChangeTriggeredAbility;
+import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.DestroyTargetEffect;
+import mage.abilities.keyword.AnnihilatorAbility;
+import mage.abilities.keyword.IndestructibleAbility;
+import mage.cards.Card;
+import mage.cards.CardImpl;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.events.GameEvent.EventType;
+import mage.game.stack.Spell;
+import mage.players.Player;
+import mage.target.TargetPermanent;
+
+/**
+ *
+ * @author maurer.it_at_gmail.com
+ */
+public class UlamogtheInfiniteGyre extends CardImpl<UlamogtheInfiniteGyre> {
+
+	private static final String effectText = "When Ulamog is put into a graveyard from anywhere, its owner shuffles his or her graveyard into his or her library";
+
+	public UlamogtheInfiniteGyre(UUID ownerId) {
+		super(ownerId, 12, "Ulamog, the Infinite Gyre", Rarity.MYTHIC, new CardType[]{CardType.CREATURE}, "{11}");
+		this.expansionSetCode = "ROE";
+		this.supertype.add("Legendary");
+		this.subtype.add("Eldrazi");
+
+		this.power = new MageInt(10);
+		this.toughness = new MageInt(10);
+
+		this.addAbility(new UlamogtheInfiniteGyreDestroyOnCastAbility());
+		this.addAbility(new AnnihilatorAbility(4));
+		this.addAbility(IndestructibleAbility.getInstance());
+		this.addAbility(new ZoneChangeTriggeredAbility(Zone.GRAVEYARD, new UlamogtheInfiniteGyreEnterGraveyardEffect(), effectText, false));
+	}
+
+	public UlamogtheInfiniteGyre(final UlamogtheInfiniteGyre card) {
+		super(card);
+	}
+
+	@Override
+	public UlamogtheInfiniteGyre copy() {
+		return new UlamogtheInfiniteGyre(this);
+	}
+}
+
+class UlamogtheInfiniteGyreDestroyOnCastAbility extends TriggeredAbilityImpl<UlamogtheInfiniteGyreDestroyOnCastAbility> {
+
+	private static final String abilityText = "When you cast Ulamog, the Infinite Gyre, destroy target permanent";
+
+	UlamogtheInfiniteGyreDestroyOnCastAbility ( ) {
+		super(Zone.STACK, new DestroyTargetEffect());
+		this.addTarget(new TargetPermanent());
+	}
+
+	UlamogtheInfiniteGyreDestroyOnCastAbility(UlamogtheInfiniteGyreDestroyOnCastAbility ability) {
+		super(ability);
+	}
+
+	@Override
+	public boolean checkTrigger(GameEvent event, Game game) {
+		if ( event.getType() == EventType.SPELL_CAST ) {
+			Spell spell = (Spell)game.getObject(event.getTargetId());
+			if ( this.getSourceId().equals(spell.getSourceId())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public UlamogtheInfiniteGyreDestroyOnCastAbility copy() {
+		return new UlamogtheInfiniteGyreDestroyOnCastAbility(this);
+	}
+
+	@Override
+	public String getRule() {
+		return abilityText;
+	}
+}
+
+class UlamogtheInfiniteGyreEnterGraveyardEffect extends OneShotEffect<UlamogtheInfiniteGyreEnterGraveyardEffect> {
+
+	UlamogtheInfiniteGyreEnterGraveyardEffect ( ) {
+		super(Outcome.Benefit);
+	}
+
+	UlamogtheInfiniteGyreEnterGraveyardEffect(UlamogtheInfiniteGyreEnterGraveyardEffect effect) {
+		super(effect);
+	}
+
+	@Override
+	public boolean apply(Game game, Ability source) {
+		Player player = game.getPlayer(source.getControllerId());
+		/*Card permanent = (Card)game.getObject(source.getSourceId());*/
+		if (player != null /* && permanent != null */) {
+			/*permanent.moveToZone(Zone.LIBRARY, source.getId(), game, true);*/
+			player.getLibrary().addAll(player.getGraveyard().getCards(game), game);
+			player.getGraveyard().clear();
+			player.getLibrary().shuffle();
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public UlamogtheInfiniteGyreEnterGraveyardEffect copy() {
+		return new UlamogtheInfiniteGyreEnterGraveyardEffect(this);
+	}
+}
---
+++ Mage.Sets/src/mage/sets/worldwake/EyeofUgin.java
@@ -0,0 +1,127 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+
+package mage.sets.worldwake;
+
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Rarity;
+import mage.Constants.Zone;
+import mage.abilities.Ability;
+import mage.abilities.TriggeredAbilityImpl;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.costs.mana.ManaCosts;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.SearchLibraryRevealPutInHandEffect;
+import mage.cards.Card;
+import mage.cards.CardImpl;
+import mage.filter.common.FilterCreatureCard;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.events.GameEvent.EventType;
+import mage.game.events.ZoneChangeEvent;
+import mage.game.stack.Spell;
+import mage.target.common.TargetCardInLibrary;
+
+/**
+ * TODO: Implement this better.
+ *
+ * @author maurer.it_at_gmail.com
+ */
+public class EyeofUgin extends CardImpl<EyeofUgin> {
+
+	private static final FilterCreatureCard filter;
+
+	static {
+		filter = new FilterCreatureCard();
+		filter.setColorless(true);
+		filter.setUseColorless(true);
+	}
+
+    public EyeofUgin (UUID ownerId) {
+        super(ownerId, 136, "Eye of Ugin", Rarity.MYTHIC, new CardType[]{CardType.LAND}, null);
+        this.expansionSetCode = "WWK";
+        this.supertype.add("Legendary");
+        this.subtype.add("Land");
+
+		this.addAbility(new EyeofUginCostReductionAbility());
+		Ability searchAbility = new SimpleActivatedAbility(Zone.BATTLEFIELD, new SearchLibraryRevealPutInHandEffect(new TargetCardInLibrary(filter)), new TapSourceCost());
+		searchAbility.addCost(new ManaCostsImpl("{7}"));
+        this.addAbility(searchAbility);
+    }
+
+    public EyeofUgin (final EyeofUgin card) {
+        super(card);
+    }
+
+    @Override
+    public EyeofUgin copy() {
+        return new EyeofUgin(this);
+    }
+}
+
+/**
+ * Implemented as a {@link TriggeredAbilityImpl} for now as there currently is
+ * no other way of interacting with a cards casting cost from an external object.
+ * This ability has no effect as of right now until a better way of implementing
+ * this form of object interaction is designed and implemented.
+ * 
+ * @author maurer.it_at_gmail.com
+ */
+class EyeofUginCostReductionAbility extends TriggeredAbilityImpl<EyeofUginCostReductionAbility> {
+
+	EyeofUginCostReductionAbility() {
+		super(Zone.BATTLEFIELD, null);
+	}
+
+	EyeofUginCostReductionAbility ( EyeofUginCostReductionAbility effect ) {
+		super(effect);
+	}
+
+	@Override
+	public boolean checkTrigger(GameEvent event, Game game) {
+		if ( event.getType() == EventType.ZONE_CHANGE &&
+			 event.getPlayerId().equals(this.getControllerId()) &&
+			 ((ZoneChangeEvent)event).getToZone() == Zone.STACK )
+		{
+			Card card = (Card)game.getObject(event.getTargetId());
+			Spell spell = (Spell)game.getObject(event.getSourceId());
+			if ( card.getSubtype().contains("Eldrazi") ) {
+				ManaCosts costs = spell.getSpellAbility().getManaCosts();
+				costs.load("{" + (card.getManaCost().convertedManaCost() - 2) + "}");
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public EyeofUginCostReductionAbility copy() {
+		return new EyeofUginCostReductionAbility(this);
+	}
+}
