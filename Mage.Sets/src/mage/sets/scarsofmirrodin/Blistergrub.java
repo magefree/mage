1da5458196df30730f844b34afc4202903368e11
Loki <loki@magefree>
2011-01-13 22:54:10 +0200

    two cards

---
+++ Mage.Sets/src/mage/sets/scarsofmirrodin/Blistergrub.java
@@ -0,0 +1,101 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+
+package mage.sets.scarsofmirrodin;
+
+import java.util.UUID;
+
+import mage.Constants;
+import mage.Constants.CardType;
+import mage.Constants.Duration;
+import mage.Constants.Rarity;
+import mage.Constants.Zone;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.PutIntoGraveFromBattlefieldTriggeredAbility;
+import mage.abilities.effects.OneShotEffect;
+import mage.cards.CardImpl;
+import mage.game.Game;
+import mage.players.Player;
+
+/**
+ *
+ * @author Loki
+ */
+public class Blistergrub extends CardImpl<Blistergrub> {
+
+    public Blistergrub (UUID ownerId) {
+        super(ownerId, 56, "Blistergrub", Rarity.COMMON, new CardType[]{CardType.CREATURE}, "{2}{B}");
+        this.expansionSetCode = "SOM";
+        this.subtype.add("Horror");
+		this.color.setBlack(true);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(2);
+        this.addAbility(new PutIntoGraveFromBattlefieldTriggeredAbility(new BlistergrubEffect(), false));
+    }
+
+    public Blistergrub (final Blistergrub card) {
+        super(card);
+    }
+
+    @Override
+    public Blistergrub copy() {
+        return new Blistergrub(this);
+    }
+}
+
+class BlistergrubEffect extends OneShotEffect<BlistergrubEffect> {
+
+    public BlistergrubEffect() {
+        super(Constants.Outcome.Damage);
+    }
+
+    public BlistergrubEffect(final BlistergrubEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        for (UUID opp : game.getOpponents(source.getControllerId())) {
+            Player opponent = game.getPlayer(opp);
+            if (opponent != null)
+                opponent.loseLife(2, game);
+        }
+        return true;
+    }
+
+    @Override
+    public String getText(Ability source) {
+        return "each opponent loses 2 life";
+    }
+
+    @Override
+    public BlistergrubEffect copy() {
+        return new BlistergrubEffect(this);
+    }
+}
