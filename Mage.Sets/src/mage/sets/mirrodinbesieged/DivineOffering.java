67a9108a944123151d8273c81bc3ad8497797d1b
maurer.it <maurer.it@gmail>
2011-03-07 10:23:30 -0500

    Now it actually takes aryat's implementation.

--- Mage.Sets/src/mage/sets/mirrodinbesieged/DivineOffering.java
+++ Mage.Sets/src/mage/sets/mirrodinbesieged/DivineOffering.java
@@ -1,16 +1,16 @@
 /*
  *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
- * 
+ *
  *  Redistribution and use in source and binary forms, with or without modification, are
  *  permitted provided that the following conditions are met:
- * 
+ *
  *     1. Redistributions of source code must retain the above copyright notice, this list of
  *        conditions and the following disclaimer.
- * 
+ *
  *     2. Redistributions in binary form must reproduce the above copyright notice, this list
  *        of conditions and the following disclaimer in the documentation and/or other materials
  *        provided with the distribution.
- * 
+ *
  *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
  *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
@@ -20,55 +20,54 @@
  *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  *  The views and conclusions contained in the software and documentation are those of the
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
  */
+
 package mage.sets.mirrodinbesieged;
 
 import java.util.UUID;
+
+import mage.Constants;
 import mage.Constants.CardType;
-import mage.Constants.Outcome;
 import mage.Constants.Rarity;
-import mage.Constants.Zone;
 import mage.abilities.Ability;
 import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.DestroyTargetEffect;
 import mage.cards.Card;
 import mage.cards.CardImpl;
+import mage.filter.Filter;
 import mage.filter.FilterPermanent;
 import mage.game.Game;
+import mage.game.permanent.Permanent;
 import mage.players.Player;
-import mage.target.Target;
 import mage.target.TargetPermanent;
 
 /**
  *
- * @author matt.maurer
+ * @author ayratn
  */
 public class DivineOffering extends CardImpl<DivineOffering> {
 
-    private static final FilterPermanent filter = new FilterPermanent();
+        private static FilterPermanent filter = new FilterPermanent("artifact");
 
     static {
         filter.getCardType().add(CardType.ARTIFACT);
+        filter.setScopeCardType(Filter.ComparisonScope.Any);
     }
 
-    public DivineOffering(UUID ownerId) {
+    public DivineOffering (UUID ownerId) {
         super(ownerId, 5, "Divine Offering", Rarity.COMMON, new CardType[]{CardType.INSTANT}, "{1}{W}");
         this.expansionSetCode = "MBS";
-        this.getColor().setWhite(true);
-
-        Target target = new TargetPermanent(filter);
-        target.setTargetName("artifact");
-        target.setRequired(true);
-        this.getSpellAbility().addTarget(target);
-        this.getSpellAbility().addEffect(new DestroyTargetEffect());
-        this.getSpellAbility().addEffect(new DivineOfferingGainLifeEffect());
+        this.color.setWhite(true);
+                this.getSpellAbility().addTarget(new TargetPermanent(filter));
+                this.getSpellAbility().addEffect(new DestroyTargetEffect());
+                this.getSpellAbility().addEffect(new DivineOfferingEffect());
     }
 
-    public DivineOffering(final DivineOffering card) {
+    public DivineOffering (final DivineOffering card) {
         super(card);
     }
 
@@ -76,40 +75,38 @@ public class DivineOffering extends CardImpl<DivineOffering> {
     public DivineOffering copy() {
         return new DivineOffering(this);
     }
-}
 
-class DivineOfferingGainLifeEffect extends OneShotEffect<DivineOfferingGainLifeEffect> {
+        private class DivineOfferingEffect extends OneShotEffect<DivineOfferingEffect> {
 
-    DivineOfferingGainLifeEffect() {
-        super(Outcome.DestroyPermanent);
-    }
+                public DivineOfferingEffect() {
+                        super(Constants.Outcome.DestroyPermanent);
+                }
 
-    DivineOfferingGainLifeEffect ( final DivineOfferingGainLifeEffect effect ) {
-        super(effect);
-    }
+                public DivineOfferingEffect(DivineOfferingEffect effect) {
+                        super(effect);
+                }
 
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Player player = game.getPlayer(source.getControllerId());
-        if (player != null) {
-            Card card = player.getGraveyard().get(source.getFirstTarget(), game);
-            if (card == null) {
-                card = game.getLastKnownInformation(source.getFirstTarget(), Zone.GRAVEYARD);
-            }
+                @Override
+                public boolean apply(Game game, Ability source) {
+                        Card card = game.getLastKnownInformation(source.getFirstTarget(), Constants.Zone.BATTLEFIELD);
             if (card != null) {
-                player.gainLife(card.getManaCost().convertedManaCost(), game);
+                            int cost = card.getManaCost().get(0).convertedManaCost();
+                                Player player = game.getPlayer(source.getControllerId());
+                                if (player != null) {
+                                        player.gainLife(cost, game);
+                                }
             }
+                        return true;
+                }
+
+                @Override
+                public DivineOfferingEffect copy() {
+                        return new DivineOfferingEffect(this);
+                }
+
+                @Override
+                public String getText(Ability source) {
+                        return "You gain life equal to its converted mana cost";
+                }
         }
-        return true;
-    }
-
-    @Override
-    public String getText(Ability source) {
-        return "You gain life equal to it's converted mana cost.";
-    }
-
-    @Override
-    public DivineOfferingGainLifeEffect copy() {
-        return new DivineOfferingGainLifeEffect(this);
-    }
 }
