667993a536e95d445911f8ea9e895cc707f208e8
magenoxx <magenoxx@gmail>
2011-03-08 11:22:01 +0300

    SacrificeTargetEffect.

---
+++ Mage/src/mage/abilities/effects/common/SacrificeTargetEffect.java
@@ -0,0 +1,79 @@
+/*
+* Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification, are
+* permitted provided that the following conditions are met:
+*
+*    1. Redistributions of source code must retain the above copyright notice, this list of
+*       conditions and the following disclaimer.
+*
+*    2. Redistributions in binary form must reproduce the above copyright notice, this list
+*       of conditions and the following disclaimer in the documentation and/or other materials
+*       provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+* FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+* ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+* ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* The views and conclusions contained in the software and documentation are those of the
+* authors and should not be interpreted as representing official policies, either expressed
+* or implied, of BetaSteward_at_googlemail.com.
+*/
+
+package mage.abilities.effects.common;
+
+import mage.Constants.Outcome;
+import mage.abilities.Ability;
+import mage.abilities.effects.OneShotEffect;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+
+import java.util.UUID;
+
+/**
+ *
+ * @author ayratn
+ */
+public class SacrificeTargetEffect extends OneShotEffect<SacrificeTargetEffect> {
+
+	public SacrificeTargetEffect() {
+		super(Outcome.Sacrifice);
+	}
+
+	public SacrificeTargetEffect(final SacrificeTargetEffect effect) {
+		super(effect);
+	}
+
+	@Override
+	public SacrificeTargetEffect copy() {
+		return new SacrificeTargetEffect(this);
+	}
+
+	@Override
+	public boolean apply(Game game, Ability source) {
+        int affectedTargets = 0;
+        for (UUID permanentId : source.getTargets().get(0).getTargets()) {
+            Permanent permanent = game.getPermanent(permanentId);
+            if (permanent != null) {
+			    permanent.sacrifice(source.getSourceId(), game);
+                affectedTargets++;
+            }
+        }
+		return affectedTargets > 0;
+	}
+
+	@Override
+	public String getText(Ability source) {
+        if (source.getTargets().get(0).getNumberOfTargets() == 1)
+		    return "The controller of target " + source.getTargets().get(0).getTargetName() + " sacrifices it";
+        else
+            return "The controller of " + source.getTargets().get(0).getNumberOfTargets() + " target " + source.getTargets().get(0).getTargetName() + " sacrifices it";
+	}
+
+}
