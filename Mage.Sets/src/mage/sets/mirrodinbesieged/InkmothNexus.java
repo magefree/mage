98bf4fa000293ec725b9c3729754a28652818998
Loki <loki@magefree>
2011-03-30 15:22:25 +0300

    Inkmoth Nexus

---
+++ Mage.Sets/src/mage/sets/mirrodinbesieged/InkmothNexus.java
@@ -0,0 +1,82 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+
+package mage.sets.mirrodinbesieged;
+
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Duration;
+import mage.Constants.Rarity;
+import mage.Constants.Zone;
+import mage.MageInt;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.mana.GenericManaCost;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.continious.BecomesCreatureSourceEOTEffect;
+import mage.abilities.keyword.FlyingAbility;
+import mage.abilities.keyword.InfectAbility;
+import mage.abilities.keyword.UnblockableAbility;
+import mage.abilities.mana.ColorlessManaAbility;
+import mage.cards.CardImpl;
+import mage.game.permanent.token.Token;
+
+/**
+ *
+ * @author Loki
+ */
+public class InkmothNexus extends CardImpl<InkmothNexus> {
+
+    public InkmothNexus (UUID ownerId) {
+        super(ownerId, 145, "Inkmoth Nexus", Rarity.RARE, new CardType[]{}, null);
+        this.expansionSetCode = "MBS";
+        this.subtype.add("Land");
+        this.addAbility(new ColorlessManaAbility());
+        this.addAbility(new SimpleActivatedAbility(Zone.BATTLEFIELD, new BecomesCreatureSourceEOTEffect(new InkmothNexusToken(), "land"), new GenericManaCost(1)));
+    }
+
+    public InkmothNexus (final InkmothNexus card) {
+        super(card);
+    }
+
+    @Override
+    public InkmothNexus copy() {
+        return new InkmothNexus(this);
+    }
+}
+
+class InkmothNexusToken extends Token {
+    public InkmothNexusToken() {
+        super("Blinkmoth", "a 1/1 Blinkmoth artifact creature with flying and infect");
+        cardType.add(CardType.ARTIFACT);
+        cardType.add(CardType.CREATURE);
+        power = new MageInt(1);
+		toughness = new MageInt(1);
+        this.addAbility(FlyingAbility.getInstance());
+        this.addAbility(InfectAbility.getInstance());
+    }
+}
