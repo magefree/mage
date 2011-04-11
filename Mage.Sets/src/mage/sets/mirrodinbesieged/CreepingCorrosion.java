888039f32260afb70130f0ddcbb255fdb87eab43
Viserion <viserion@magefree>
2011-04-10 22:06:27 -0500

    Hand of the Praetors, couple of Green MBS cards

--- Mage.Sets/src/mage/sets/mirrodinbesieged/CreepingCorrosion.java
+++ Mage.Sets/src/mage/sets/mirrodinbesieged/CreepingCorrosion.java
@@ -1,16 +1,16 @@
 /*
  *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
- *
+ * 
  *  Redistribution and use in source and binary forms, with or without modification, are
  *  permitted provided that the following conditions are met:
- *
+ * 
  *     1. Redistributions of source code must retain the above copyright notice, this list of
  *        conditions and the following disclaimer.
- *
+ * 
  *     2. Redistributions in binary form must reproduce the above copyright notice, this list
  *        of conditions and the following disclaimer in the documentation and/or other materials
  *        provided with the distribution.
- *
+ * 
  *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
  *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
@@ -20,7 +20,7 @@
  *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ * 
  *  The views and conclusions contained in the software and documentation are those of the
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
@@ -30,52 +30,35 @@ package mage.sets.mirrodinbesieged;
 
 import java.util.UUID;
 import mage.Constants.CardType;
-import mage.Constants.Duration;
 import mage.Constants.Rarity;
-import mage.Constants.Zone;
-import mage.MageInt;
-import mage.abilities.common.SimpleActivatedAbility;
-import mage.abilities.costs.mana.GenericManaCost;
-import mage.abilities.costs.mana.ManaCostsImpl;
-import mage.abilities.effects.common.continious.BecomesCreatureSourceEOTEffect;
-import mage.abilities.keyword.FlyingAbility;
-import mage.abilities.keyword.InfectAbility;
-import mage.abilities.keyword.UnblockableAbility;
-import mage.abilities.mana.ColorlessManaAbility;
+import mage.abilities.Ability;
+import mage.abilities.effects.common.DestroyAllEffect;
 import mage.cards.CardImpl;
-import mage.game.permanent.token.Token;
+import mage.filter.common.FilterArtifactPermanent;
 
 /**
  *
- * @author Loki
+ * @author Viserion
  */
-public class InkmothNexus extends CardImpl<InkmothNexus> {
+public class CreepingCorrosion extends CardImpl<CreepingCorrosion> {
 
-    public InkmothNexus (UUID ownerId) {
-        super(ownerId, 145, "Inkmoth Nexus", Rarity.RARE, new CardType[]{CardType.LAND}, null);
-        this.expansionSetCode = "MBS";
-        this.addAbility(new ColorlessManaAbility());
-        this.addAbility(new SimpleActivatedAbility(Zone.BATTLEFIELD, new BecomesCreatureSourceEOTEffect(new InkmothNexusToken(), "land"), new GenericManaCost(1)));
-    }
+	public CreepingCorrosion(UUID ownerId) {
+		super(ownerId, 78, "Creeping Corrosion", Rarity.RARE, new CardType[]{CardType.SORCERY}, "{2}{G}{G}");
+		this.expansionSetCode = "MBS";
+		this.color.setGreen(true);
+		this.getSpellAbility().addEffect(new DestroyAllEffect(FilterArtifactPermanent.getDefault()));
+	}
 
-    public InkmothNexus (final InkmothNexus card) {
-        super(card);
-    }
+	public CreepingCorrosion(final CreepingCorrosion card) {
+		super(card);
+	}
 
-    @Override
-    public InkmothNexus copy() {
-        return new InkmothNexus(this);
-    }
-}
+	@Override
+	public CreepingCorrosion copy() {
+		return new CreepingCorrosion(this);
+	}
 
-class InkmothNexusToken extends Token {
-    public InkmothNexusToken() {
-        super("Blinkmoth", "a 1/1 Blinkmoth artifact creature with flying and infect");
-        cardType.add(CardType.ARTIFACT);
-        cardType.add(CardType.CREATURE);
-        power = new MageInt(1);
-		toughness = new MageInt(1);
-        this.addAbility(FlyingAbility.getInstance());
-        this.addAbility(InfectAbility.getInstance());
-    }
+	public String getText(Ability source) {
+		return "Destroy all artifacts";
+	}
 }
---
+++ Mage.Sets/src/mage/sets/mirrodinbesieged/PistonSledge.java
@@ -0,0 +1,109 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+
+package mage.sets.mirrodinbesieged;
+
+import java.util.UUID;
+
+import mage.Constants;
+import mage.Constants.CardType;
+import mage.Constants.Rarity;
+import mage.Constants.Zone;
+import mage.abilities.Ability;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.costs.AlternativeCost;
+import mage.abilities.costs.common.SacrificeTargetCost;
+import mage.abilities.effects.common.AttachEffect;
+import mage.abilities.effects.common.continious.BoostEquippedEffect;
+import mage.abilities.keyword.EquipAbility;
+import mage.cards.CardImpl;
+import mage.filter.common.FilterControlledPermanent;
+import mage.target.common.TargetControlledCreaturePermanent;
+import mage.target.common.TargetControlledPermanent;
+
+/**
+ *
+ * @author Viserion
+ */
+public class PistonSledge extends CardImpl<PistonSledge> {
+
+
+    public PistonSledge (UUID ownerId) {
+        super(ownerId, 124, "Piston Sledge", Rarity.UNCOMMON, new CardType[]{CardType.ARTIFACT}, "{3}");
+        this.expansionSetCode = "MBS";
+        this.subtype.add("Equipment");
+        this.addAbility(new EquipAbility(Constants.Outcome.AddAbility, new PistonSledgeEquipCost()));
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new BoostEquippedEffect(3, 1)));
+
+		Ability ability = new EntersBattlefieldTriggeredAbility(new AttachEffect(Constants.Outcome.AddAbility), false);
+		ability.addTarget(new TargetControlledCreaturePermanent());
+		this.addAbility(ability);
+    }
+
+    public PistonSledge (final PistonSledge card) {
+        super(card);
+    }
+
+    @Override
+    public PistonSledge copy() {
+        return new PistonSledge(this);
+    }
+    
+	public String getText(Ability source) {
+		return "When Piston Sledge enters the battlefield, attach it to target creature you control.\nEquipped creature gets +3/+1.\nEquip-Sacrifice an artifact";
+	}
+}
+
+class PistonSledgeEquipCost extends AlternativeCost<PistonSledgeEquipCost> {
+	private static FilterControlledPermanent filter = new FilterControlledPermanent("artifact");
+
+	static {
+		filter.getCardType().add(CardType.ARTIFACT);
+	}
+
+	public PistonSledgeEquipCost() {
+		super("sacrifice an artifact");
+		this.add(new SacrificeTargetCost(new TargetControlledPermanent(1, 1, filter, false)));
+	}
+
+	public PistonSledgeEquipCost(final PistonSledgeEquipCost cost) {
+		super(cost);
+	}
+
+	@Override
+	public PistonSledgeEquipCost copy() {
+		return new PistonSledgeEquipCost(this);
+	}
+
+	@Override
+	public String getText() {
+		return " sacrifice an artifact";
+	}
+
+}
