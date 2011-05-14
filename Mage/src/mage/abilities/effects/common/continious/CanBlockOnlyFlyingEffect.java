a30782fbb165c636685ae01279e6589aa29b6ea7
North <robyter@gmail>
2011-05-14 21:09:19 +0300

    Card added Welkin Tern Moved and renamed Cloud Elemental effect.

--- Mage/src/mage/abilities/effects/common/continious/CanBlockOnlyFlyingEffect.java
+++ Mage/src/mage/abilities/effects/common/continious/CanBlockOnlyFlyingEffect.java
@@ -1,16 +1,16 @@
 /*
  *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
- *
+ * 
  *  Redistribution and use in source and binary forms, with or without modification, are
  *  permitted provided that the following conditions are met:
- *
+ * 
  *     1. Redistributions of source code must retain the above copyright notice, this list of
  *        conditions and the following disclaimer.
- *
+ * 
  *     2. Redistributions in binary form must reproduce the above copyright notice, this list
  *        of conditions and the following disclaimer in the documentation and/or other materials
  *        provided with the distribution.
- *
+ * 
  *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
  *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
@@ -20,53 +20,65 @@
  *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ * 
  *  The views and conclusions contained in the software and documentation are those of the
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
  */
-package mage.sets.tenth;
+package mage.abilities.effects.common.continious;
 
-import java.util.UUID;
-import mage.Constants.CardType;
-import mage.Constants.Rarity;
-import mage.Constants.Zone;
-import mage.MageInt;
-import mage.abilities.common.SimpleStaticAbility;
-import mage.abilities.effects.common.continious.CanBlockOnlyFlyingEffect;
+import mage.Constants.Duration;
+import mage.Constants.Outcome;
+import mage.abilities.Ability;
+import mage.abilities.effects.ReplacementEffectImpl;
 import mage.abilities.keyword.FlyingAbility;
-import mage.cards.CardImpl;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.events.GameEvent.EventType;
+import mage.game.permanent.Permanent;
 
 /**
  *
- * @author BetaSteward_at_googlemail.com
+ * @author North
  */
-public class CloudElemental extends CardImpl<CloudElemental> {
+public class CanBlockOnlyFlyingEffect extends ReplacementEffectImpl<CanBlockOnlyFlyingEffect> {
 
-    public CloudElemental(UUID ownerId) {
-        super(ownerId, 74, "Cloud Elemental", Rarity.COMMON, new CardType[]{CardType.CREATURE}, "{2}{U}");
-        this.expansionSetCode = "10E";
-        this.subtype.add("Elemental");
-        
-        this.color.setBlue(true);
-        this.power = new MageInt(2);
-        this.toughness = new MageInt(3);
-
-        this.addAbility(FlyingAbility.getInstance());
-        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new CanBlockOnlyFlyingEffect()));
+    public CanBlockOnlyFlyingEffect() {
+        super(Duration.WhileOnBattlefield, Outcome.Detriment);
     }
 
-    public CloudElemental(final CloudElemental card) {
-        super(card);
+    public CanBlockOnlyFlyingEffect(final CanBlockOnlyFlyingEffect effect) {
+        super(effect);
     }
 
     @Override
-    public CloudElemental copy() {
-        return new CloudElemental(this);
+    public CanBlockOnlyFlyingEffect copy() {
+        return new CanBlockOnlyFlyingEffect(this);
     }
 
     @Override
-    public String getArt() {
-        return "104536_typ_reg_sty_010.jpg";
+    public boolean apply(Game game, Ability source) {
+        return true;
+    }
+
+    @Override
+    public boolean replaceEvent(GameEvent event, Ability source, Game game) {
+        return true;
+    }
+
+    @Override
+    public boolean applies(GameEvent event, Ability source, Game game) {
+        if (event.getType() == EventType.DECLARE_BLOCKER && source.getSourceId().equals(event.getSourceId())) {
+            Permanent permanent = game.getPermanent(event.getTargetId());
+            if (permanent != null && !permanent.getAbilities().containsKey(FlyingAbility.getInstance().getId())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String getText(Ability source) {
+        return "{this} can block only creatures with flying";
     }
 }
