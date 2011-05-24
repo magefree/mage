a17acb1f514afd5287ebed5581286fec49f73de6
Loki <loki@magefree>
2011-05-25 00:01:34 +0300

    Linvala, Keeper of Silence

---
+++ Mage.Sets/src/mage/sets/riseoftheeldrazi/LinvalaKeeperofSilence.java
@@ -0,0 +1,115 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+
+package mage.sets.riseoftheeldrazi;
+
+import java.util.UUID;
+
+import mage.Constants;
+import mage.Constants.CardType;
+import mage.Constants.Rarity;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.ReplacementEffect;
+import mage.abilities.effects.ReplacementEffectImpl;
+import mage.abilities.keyword.FlyingAbility;
+import mage.cards.CardImpl;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.permanent.Permanent;
+
+import javax.xml.datatype.Duration;
+
+/**
+ *
+ * @author Loki
+ */
+public class LinvalaKeeperofSilence extends CardImpl<LinvalaKeeperofSilence> {
+
+    public LinvalaKeeperofSilence (UUID ownerId) {
+        super(ownerId, 33, "Linvala, Keeper of Silence", Rarity.MYTHIC, new CardType[]{CardType.CREATURE}, "{2}{W}{W}");
+        this.expansionSetCode = "ROE";
+        this.supertype.add("Legendary");
+        this.subtype.add("Angel");
+		this.color.setWhite(true);
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(4);
+        this.addAbility(FlyingAbility.getInstance());
+        this.addAbility(new SimpleStaticAbility(Constants.Zone.BATTLEFIELD, new LinvalaKeeperofSilenceEffect()));
+    }
+
+    public LinvalaKeeperofSilence (final LinvalaKeeperofSilence card) {
+        super(card);
+    }
+
+    @Override
+    public LinvalaKeeperofSilence copy() {
+        return new LinvalaKeeperofSilence(this);
+    }
+}
+
+class LinvalaKeeperofSilenceEffect extends ReplacementEffectImpl<LinvalaKeeperofSilenceEffect> {
+    LinvalaKeeperofSilenceEffect() {
+        super(Constants.Duration.WhileOnBattlefield, Constants.Outcome.Detriment);
+    }
+
+    LinvalaKeeperofSilenceEffect(final LinvalaKeeperofSilenceEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public boolean replaceEvent(GameEvent event, Ability source, Game game) {
+        return true;
+    }
+
+    @Override
+    public boolean applies(GameEvent event, Ability source, Game game) {
+        Permanent p = game.getPermanent(event.getTargetId());
+        if ( event.getType() == GameEvent.EventType.ACTIVATE_ABILITY && game.getOpponents(source.getControllerId()).contains(event.getPlayerId())
+                && p != null && p.getCardType().contains(CardType.CREATURE)) {
+			return true;
+		}
+        return false;
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public LinvalaKeeperofSilenceEffect copy() {
+        return new LinvalaKeeperofSilenceEffect(this);
+    }
+
+    @Override
+    public String getText(Ability source) {
+        return "Activated abilities of creatures your opponents control can't be activated";
+    }
+}
