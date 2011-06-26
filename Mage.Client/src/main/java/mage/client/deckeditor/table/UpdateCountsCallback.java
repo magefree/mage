d52ff8e22139f37e1c1ad8df3522c7b64c87ca8f
magenoxx <magenoxx@gmail>
2011-06-26 14:45:38 +0400

    Forgot to commit class.

---
+++ Mage.Client/src/main/java/mage/client/deckeditor/table/UpdateCountsCallback.java
@@ -0,0 +1,26 @@
+package mage.client.deckeditor.table;
+
+import javax.swing.*;
+
+/**
+ * Updates counts in deck editor.
+ * @author nantuko
+ */
+public class UpdateCountsCallback {
+
+	private javax.swing.JLabel lblCount;
+    private javax.swing.JLabel lblCreatureCount;
+    private javax.swing.JLabel lblLandCount;
+
+	public UpdateCountsCallback(JLabel count, JLabel creatures, JLabel lands) {
+		this.lblCount = count;
+		this.lblCreatureCount = creatures;
+		this.lblLandCount = lands;
+	}
+
+	public void update(int count, int creatures, int lands) {
+		this.lblCount.setText("Count: " + Integer.toString(count));
+		this.lblCreatureCount.setText("Creatures: " + Integer.toString(creatures));
+		this.lblLandCount.setText("Lands: " + Integer.toString(lands));
+	}
+}
