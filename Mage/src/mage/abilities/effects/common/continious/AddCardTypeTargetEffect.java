4600890c9330c7765c21f628f488f7e6962700c5
Loki <loki@magefree>
2011-06-28 13:56:13 +0300

    Liquimetal Coating

---
+++ Mage/src/mage/abilities/effects/common/continious/AddCardTypeTargetEffect.java
@@ -0,0 +1,43 @@
+package mage.abilities.effects.common.continious;
+
+import mage.Constants;
+import mage.abilities.Ability;
+import mage.abilities.effects.ContinuousEffectImpl;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+
+public class AddCardTypeTargetEffect extends ContinuousEffectImpl<AddCardTypeTargetEffect> {
+    private Constants.CardType addedCardType;
+
+    public AddCardTypeTargetEffect(Constants.CardType addedCardType, Constants.Duration duration) {
+        super(duration, Constants.Layer.TypeChangingEffects_4, Constants.SubLayer.NA, Constants.Outcome.Benefit);
+        this.addedCardType = addedCardType;
+    }
+
+    public AddCardTypeTargetEffect(final AddCardTypeTargetEffect effect) {
+        super(effect);
+        this.addedCardType = effect.addedCardType;
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Permanent target = game.getPermanent(targetPointer.getFirst(source));
+        if (target != null) {
+            if (!target.getCardType().contains(addedCardType))
+                target.getCardType().add(addedCardType);
+        }
+        return false;
+    }
+
+    @Override
+    public AddCardTypeTargetEffect copy() {
+        return new AddCardTypeTargetEffect(this);
+    }
+
+    @Override
+    public String getText(Ability source) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Target ").append(source.getTargets().get(0).getTargetName()).append(" becomes ").append(addedCardType.toString()).append(" in addition to its other types until end of turn");
+        return sb.toString();
+    }
+}
