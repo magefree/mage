a0ed8dbe01b1864a120504ac0b03788aacb7cea2
Loki <loki@magefree>
2011-07-02 00:24:29 +0300

    2 MBS

---
+++ Mage.Sets/src/mage/sets/mirrodinbesieged/NestedGhoul.java
@@ -0,0 +1,98 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+
+package mage.sets.mirrodinbesieged;
+
+import java.util.UUID;
+
+import mage.Constants;
+import mage.Constants.CardType;
+import mage.Constants.Rarity;
+import mage.MageInt;
+import mage.abilities.TriggeredAbilityImpl;
+import mage.abilities.effects.common.CreateTokenEffect;
+import mage.cards.CardImpl;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.permanent.token.ZombieToken;
+
+/**
+ *
+ * @author Loki
+ */
+public class NestedGhoul extends CardImpl<NestedGhoul> {
+
+    public NestedGhoul (UUID ownerId) {
+        super(ownerId, 48, "Nested Ghoul", Rarity.UNCOMMON, new CardType[]{CardType.CREATURE}, "{3}{B}{B}");
+        this.expansionSetCode = "MBS";
+        this.subtype.add("Zombie");
+        this.subtype.add("Warrior");
+		this.color.setBlack(true);
+        this.power = new MageInt(4);
+        this.toughness = new MageInt(2);
+        this.addAbility(new NestedGhoulTriggeredAbility());
+    }
+
+    public NestedGhoul (final NestedGhoul card) {
+        super(card);
+    }
+
+    @Override
+    public NestedGhoul copy() {
+        return new NestedGhoul(this);
+    }
+
+}
+
+class NestedGhoulTriggeredAbility extends TriggeredAbilityImpl<NestedGhoulTriggeredAbility> {
+    NestedGhoulTriggeredAbility() {
+        super(Constants.Zone.BATTLEFIELD, new CreateTokenEffect(new ZombieToken()));
+    }
+
+    NestedGhoulTriggeredAbility(final NestedGhoulTriggeredAbility ability) {
+        super(ability);
+    }
+
+    @Override
+    public NestedGhoulTriggeredAbility copy() {
+        return new NestedGhoulTriggeredAbility(this);
+    }
+
+    @Override
+    public boolean checkTrigger(GameEvent event, Game game) {
+        if (event.getType() == GameEvent.EventType.DAMAGED_CREATURE && event.getTargetId().equals(this.sourceId)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String getRule() {
+        return "Whenever a source deals damage to Nested Ghoul, put a 2/2 black Zombie creature token onto the battlefield.";
+    }
+}
