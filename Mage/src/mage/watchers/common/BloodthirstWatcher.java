360fa28f1b9f280a41e62ee4dcab1c0e6a27bd81
Loki <loki@magefree>
2011-07-17 14:12:03 +0300

    Bloodthirst keyword

---
+++ Mage/src/mage/watchers/common/BloodthirstWatcher.java
@@ -0,0 +1,37 @@
+package mage.watchers.common;
+
+import mage.game.Game;
+import mage.game.events.DamagedPlayerEvent;
+import mage.game.events.GameEvent;
+import mage.watchers.WatcherImpl;
+
+import java.util.UUID;
+
+/**
+ * Must be installed to card for proper Bloodthirst work
+ * @author Loki
+ */
+public class BloodthirstWatcher extends WatcherImpl<BloodthirstWatcher> {
+    public BloodthirstWatcher(UUID controllerId) {
+        super("DamagedOpponents", controllerId);
+    }
+
+    public BloodthirstWatcher(final BloodthirstWatcher watcher) {
+        super(watcher);
+    }
+
+    @Override
+    public void watch(GameEvent event, Game game) {
+        if (event.getType() == GameEvent.EventType.DAMAGED_PLAYER) {
+            DamagedPlayerEvent damageEvent = (DamagedPlayerEvent)event;
+            if (game.getOpponents(this.controllerId).contains(damageEvent.getPlayerId())) {
+                condition = true;
+            }
+        }
+    }
+
+    @Override
+    public BloodthirstWatcher copy() {
+        return new BloodthirstWatcher(this);
+    }
+}
