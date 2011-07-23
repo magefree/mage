f13c81398d481caef3cfb77d91ae7b887baaa4a5
Loki <loki@magefree>
2011-07-24 00:29:44 +0300

    2 WWK

--- Mage/src/mage/abilities/effects/common/UntapTargetEffect.java
+++ Mage/src/mage/abilities/effects/common/UntapTargetEffect.java
@@ -58,7 +58,7 @@ public class UntapTargetEffect extends OneShotEffect<UntapTargetEffect> {
 
 	@Override
 	public boolean apply(Game game, Ability source) {
-		for (UUID target: source.getTargets().get(0).getTargets()) {
+		for (UUID target: targetPointer.getTargets(source)) {
 			Permanent permanent = game.getPermanent(target);
 			if (permanent != null) {
 				permanent.untap(game);
