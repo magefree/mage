6bdc19182553351eb128453f584d283a66596604
magenoxx <magenoxx@gmail>
2011-08-03 19:17:01 +0400

    [SOM] Venser, the Sojourner

---
+++ Mage.Sets/src/mage/sets/scarsofmirrodin/VenserTheSojourner.java
@@ -0,0 +1,170 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+
+package mage.sets.scarsofmirrodin;
+
+import mage.Constants;
+import mage.Constants.CardType;
+import mage.Constants.Rarity;
+import mage.abilities.Ability;
+import mage.abilities.DelayedTriggeredAbility;
+import mage.abilities.LoyaltyAbility;
+import mage.abilities.common.EntersBattlefieldAbility;
+import mage.abilities.common.SpellCastTriggeredAbility;
+import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.ExileTargetEffect;
+import mage.abilities.effects.common.GetEmblemEffect;
+import mage.abilities.effects.common.ReturnFromExileEffect;
+import mage.abilities.effects.common.continious.GainAbilityAllEffect;
+import mage.abilities.effects.common.counter.AddCountersSourceEffect;
+import mage.abilities.keyword.UnblockableAbility;
+import mage.cards.CardImpl;
+import mage.counters.CounterType;
+import mage.filter.common.FilterCreaturePermanent;
+import mage.game.Game;
+import mage.game.command.Emblem;
+import mage.game.events.GameEvent;
+import mage.game.permanent.Permanent;
+import mage.target.Target;
+import mage.target.TargetPermanent;
+import mage.target.common.TargetControlledPermanent;
+
+import java.util.UUID;
+
+/**
+ * @author nantuko
+ */
+public class VenserTheSojourner extends CardImpl<VenserTheSojourner> {
+
+	public VenserTheSojourner(UUID ownerId) {
+		super(ownerId, 135, "Venser, the Sojourner", Rarity.MYTHIC, new CardType[]{CardType.PLANESWALKER}, "{3}{W}{U}");
+		this.expansionSetCode = "SOM";
+		this.subtype.add("Venser");
+		this.color.setWhite(true);
+		this.color.setBlue(true);
+		this.addAbility(new EntersBattlefieldAbility(new AddCountersSourceEffect(CounterType.LOYALTY.createInstance(3)), ""));
+
+		// +2: Exile target permanent you own. Return it to the battlefield under your control at the beginning of the next end step.
+		LoyaltyAbility ability1 = new LoyaltyAbility(new VenserTheSojournerEffect(), 2);
+		ability1.addTarget(new TargetControlledPermanent());
+		this.addAbility(ability1);
+
+		// -1: Creatures are unblockable this turn.
+		this.addAbility(new LoyaltyAbility(new GainAbilityAllEffect(UnblockableAbility.getInstance(), Constants.Duration.EndOfTurn, FilterCreaturePermanent.getDefault()), -1));
+
+		// -8: You get an emblem with "Whenever you cast a spell, exile target permanent."
+		LoyaltyAbility ability2 = new LoyaltyAbility(new GetEmblemEffect(new VenserTheSojournerEmblem()), -8);
+		this.addAbility(ability2);
+	}
+
+	public VenserTheSojourner(final VenserTheSojourner card) {
+		super(card);
+	}
+
+	@Override
+	public VenserTheSojourner copy() {
+		return new VenserTheSojourner(this);
+	}
+
+}
+
+class VenserTheSojournerEffect extends OneShotEffect<VenserTheSojournerEffect> {
+
+	private static final String effectText = "Exile target permanent you own. Return it to the battlefield under your control at the beginning of the next end step";
+
+	VenserTheSojournerEffect() {
+		super(Constants.Outcome.Benefit);
+		staticText = effectText;
+	}
+
+	VenserTheSojournerEffect(VenserTheSojournerEffect effect) {
+		super(effect);
+	}
+
+	@Override
+	public boolean apply(Game game, Ability source) {
+		if (getTargetPointer().getFirst(source) != null) {
+			Permanent permanent = game.getPermanent(getTargetPointer().getFirst(source));
+			if (permanent != null) {
+				if (permanent.moveToExile(permanent.getId(), "Venser, the Sojourner", source.getSourceId(), game)) {
+					//create delayed triggered ability
+					VenserTheSojournerDelayedTriggeredAbility delayedAbility = new VenserTheSojournerDelayedTriggeredAbility(permanent.getId());
+					delayedAbility.setSourceId(source.getSourceId());
+					delayedAbility.setControllerId(source.getControllerId());
+					game.addDelayedTriggeredAbility(delayedAbility);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public VenserTheSojournerEffect copy() {
+		return new VenserTheSojournerEffect(this);
+	}
+
+}
+
+class VenserTheSojournerDelayedTriggeredAbility extends DelayedTriggeredAbility<VenserTheSojournerDelayedTriggeredAbility> {
+
+	VenserTheSojournerDelayedTriggeredAbility(UUID exileId) {
+		super(new ReturnFromExileEffect(exileId, Constants.Zone.BATTLEFIELD));
+	}
+
+	VenserTheSojournerDelayedTriggeredAbility(VenserTheSojournerDelayedTriggeredAbility ability) {
+		super(ability);
+	}
+
+	@Override
+	public boolean checkTrigger(GameEvent event, Game game) {
+		if (event.getType() == GameEvent.EventType.END_TURN_STEP_PRE) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public VenserTheSojournerDelayedTriggeredAbility copy() {
+		return new VenserTheSojournerDelayedTriggeredAbility(this);
+	}
+}
+
+class VenserTheSojournerEmblem extends Emblem {
+
+	private static final String emblemText = "Whenever you cast a spell, exile target permanent";
+
+	public VenserTheSojournerEmblem() {
+		Ability ability = new SpellCastTriggeredAbility(new ExileTargetEffect(), false);
+		Target target = new TargetPermanent();
+		target.setRequired(true);
+		ability.addTarget(target);
+		this.getAbilities().add(ability);
+	}
+}
+
