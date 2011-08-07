c366c662dec1ad51323d51030d8a0b0dd6f3c86e
magenoxx <magenoxx@gmail>
2011-08-07 16:40:41 +0400

    [M12] 3 cards

---
+++ Mage.Sets/src/mage/sets/magic2012/GriffinRider.java
@@ -0,0 +1,93 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.magic2012;
+
+import java.util.UUID;
+
+import mage.Constants;
+import mage.Constants.CardType;
+import mage.Constants.Rarity;
+import mage.MageInt;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.condition.common.ControlsPermanent;
+import mage.abilities.decorator.ConditionalContinousEffect;
+import mage.abilities.effects.ContinuousEffect;
+import mage.abilities.effects.Effect;
+import mage.abilities.effects.common.continious.BoostSourceEffect;
+import mage.abilities.effects.common.continious.GainAbilitySourceEffect;
+import mage.abilities.keyword.FlyingAbility;
+import mage.cards.CardImpl;
+import mage.filter.Filter;
+import mage.filter.FilterPermanent;
+import mage.filter.common.FilterCreaturePermanent;
+
+/**
+ *
+ * @author nantuko
+ */
+public class GriffinRider extends CardImpl<GriffinRider> {
+
+	private static final FilterPermanent filterGriffinCard = new FilterCreaturePermanent();
+
+	static {
+		filterGriffinCard.getSubtype().add("Griffin");
+		filterGriffinCard.setScopeSubtype(Filter.ComparisonScope.Any);
+	}
+
+    public GriffinRider(UUID ownerId) {
+        super(ownerId, 20, "Griffin Rider", Rarity.COMMON, new CardType[]{CardType.CREATURE}, "{1}{W}");
+        this.expansionSetCode = "M12";
+        this.subtype.add("Human");
+        this.subtype.add("Knight");
+
+        this.color.setWhite(true);
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(1);
+
+        // As long as you control a Griffin creature, Griffin Rider gets +3/+3 and has flying.
+		this.addAbility(new SimpleStaticAbility(Constants.Zone.BATTLEFIELD,
+				new ConditionalContinousEffect(
+					new BoostSourceEffect(3, 3, Constants.Duration.WhileOnBattlefield),
+					new ControlsPermanent(filterGriffinCard),
+					"As long as you control a Griffin creature, Griffin Rider gets +3/+3")));
+		this.addAbility(new SimpleStaticAbility(Constants.Zone.BATTLEFIELD,
+				new ConditionalContinousEffect(
+					new GainAbilitySourceEffect(FlyingAbility.getInstance(), Constants.Duration.WhileOnBattlefield),
+					new ControlsPermanent(filterGriffinCard),
+					"As long as you control a Griffin creature, Griffin Rider gets flying")));
+    }
+
+    public GriffinRider(final GriffinRider card) {
+        super(card);
+    }
+
+    @Override
+    public GriffinRider copy() {
+        return new GriffinRider(this);
+    }
+}
---
+++ Mage.Sets/src/mage/sets/magic2012/TimelyReinforcements.java
@@ -0,0 +1,120 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.magic2012;
+
+import java.util.UUID;
+
+import mage.Constants;
+import mage.Constants.CardType;
+import mage.Constants.Rarity;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.Effect;
+import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.CreateTokenEffect;
+import mage.cards.CardImpl;
+import mage.filter.FilterPermanent;
+import mage.filter.common.FilterCreaturePermanent;
+import mage.game.Game;
+import mage.game.permanent.token.SoldierToken;
+import mage.players.Player;
+
+/**
+ * @author nantuko
+ */
+public class TimelyReinforcements extends CardImpl<TimelyReinforcements> {
+
+	public TimelyReinforcements(UUID ownerId) {
+		super(ownerId, 40, "Timely Reinforcements", Rarity.UNCOMMON, new CardType[]{CardType.SORCERY}, "{2}{W}");
+		this.expansionSetCode = "M12";
+		this.color.setWhite(true);
+
+		// If you have less life than an opponent, you gain 6 life. If you control fewer creatures than an opponent, put three 1/1 white Soldier creature tokens onto the battlefield.
+		this.getSpellAbility().addEffect(new TimelyReinforcementsEffect());
+	}
+
+	public TimelyReinforcements(final TimelyReinforcements card) {
+		super(card);
+	}
+
+	@Override
+	public TimelyReinforcements copy() {
+		return new TimelyReinforcements(this);
+	}
+}
+
+class TimelyReinforcementsEffect extends OneShotEffect<TimelyReinforcementsEffect> {
+
+	public TimelyReinforcementsEffect() {
+		super(Constants.Outcome.Benefit);
+		staticText = "If you have less life than an opponent, you gain 6 life. If you control fewer creatures than an opponent, put three 1/1 white Soldier creature tokens onto the battlefield";
+	}
+
+	public TimelyReinforcementsEffect(TimelyReinforcementsEffect effect) {
+		super(effect);
+	}
+
+	@Override
+	public boolean apply(Game game, Ability source) {
+		Player controller = game.getPlayer(source.getControllerId());
+		if (controller != null) {
+			boolean lessCreatures = false;
+			boolean lessLife = false;
+			FilterPermanent filter= new FilterCreaturePermanent();
+			int count = game.getBattlefield().countAll(filter, controller.getId());
+			for (UUID uuid : game.getOpponents(controller.getId())) {
+				Player opponent = game.getPlayer(uuid);
+				if (opponent != null) {
+					if (opponent.getLife() > controller.getLife()) {
+						 lessLife = true;
+					}
+					if (game.getBattlefield().countAll(filter, uuid) > count) {
+						lessCreatures = true;
+					}
+				}
+				if ( lessLife && lessCreatures) { // no need to search further
+					break;
+				}
+			}
+			if (lessLife) {
+				controller.gainLife(6, game);
+			}
+			if (lessCreatures) {
+				Effect effect = new CreateTokenEffect(new SoldierToken(), 3);
+				effect.apply(game, source);
+			}
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public TimelyReinforcementsEffect copy() {
+		return new TimelyReinforcementsEffect(this);
+	}
+}
