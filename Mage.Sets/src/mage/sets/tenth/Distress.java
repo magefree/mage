0624383888d60eac6cf05dd54b8883dd6a5ff7d1
magenoxx <magenoxx@gmail>
2011-08-09 16:42:51 +0400

    [M12, 10E, CHK] Distress

---
+++ Mage.Sets/src/mage/sets/tenth/Distress.java
@@ -0,0 +1,114 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.tenth;
+
+import mage.Constants;
+import mage.Constants.CardType;
+import mage.Constants.Rarity;
+import mage.abilities.Ability;
+import mage.abilities.effects.OneShotEffect;
+import mage.cards.Card;
+import mage.cards.CardImpl;
+import mage.filter.FilterCard;
+import mage.game.Game;
+import mage.players.Player;
+import mage.target.TargetCard;
+import mage.target.TargetPlayer;
+
+import java.util.UUID;
+
+/**
+ * @author nantuko
+ */
+public class Distress extends CardImpl<Distress> {
+
+	public Distress(UUID ownerId) {
+		super(ownerId, 136, "Distress", Rarity.COMMON, new CardType[]{CardType.SORCERY}, "{B}{B}");
+		this.expansionSetCode = "10E";
+
+		this.color.setBlack(true);
+
+		// Target player reveals his or her hand. You choose a nonland card from it. That player discards that card.
+		this.getSpellAbility().addTarget(new TargetPlayer());
+		this.getSpellAbility().addEffect(new DistressEffect());
+	}
+
+	public Distress(final Distress card) {
+		super(card);
+	}
+
+	@Override
+	public Distress copy() {
+		return new Distress(this);
+	}
+}
+
+class DistressEffect extends OneShotEffect<DistressEffect> {
+
+	private static final FilterCard filter = new FilterCard("nonland card");
+
+	static {
+		filter.getNotCardType().add(CardType.LAND);
+	}
+
+	public DistressEffect() {
+		super(Constants.Outcome.Discard);
+		staticText = "Target player reveals his or her hand. You choose a nonland card from it. That player discards that card";
+	}
+
+	public DistressEffect(final DistressEffect effect) {
+		super(effect);
+	}
+
+	@Override
+	public boolean apply(Game game, Ability source) {
+		Player player = game.getPlayer(source.getFirstTarget());
+		if (player != null) {
+			player.revealCards("Distress", player.getHand(), game);
+			Player you = game.getPlayer(source.getControllerId());
+			if (you != null) {
+				TargetCard target = new TargetCard(Constants.Zone.PICK, filter);
+				target.setRequired(true);
+				if (you.choose(Constants.Outcome.Benefit, player.getHand(), target, game)) {
+					Card card = player.getHand().get(target.getFirstTarget(), game);
+					if (card != null) {
+						return player.discard(card, source, game);
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public DistressEffect copy() {
+		return new DistressEffect(this);
+	}
+
+}
+
