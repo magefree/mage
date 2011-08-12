31747f8227b2fde8f2b499f342a75859ac084a80
BetaSteward <betasteward@gmail>
2011-08-12 10:10:04 -0400

    MBS - Phyrexian Hydra

---
+++ Mage.Sets/src/mage/sets/mirrodinbesieged/PhyrexianHydra.java
@@ -0,0 +1,124 @@
+/*
+ *  Copyright 2011 BetaSteward_at_googlemail.com. All rights reserved.
+ * 
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ * 
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ * 
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ * 
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+
+package mage.sets.mirrodinbesieged;
+
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Duration;
+import mage.Constants.Rarity;
+import mage.Constants.Zone;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.PreventionEffectImpl;
+import mage.abilities.keyword.InfectAbility;
+import mage.cards.CardImpl;
+import mage.counters.CounterType;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.permanent.Permanent;
+
+/**
+ *
+ * @author BetaSteward_at_googlemail.com
+ */
+public class PhyrexianHydra extends CardImpl<PhyrexianHydra> {
+
+	public PhyrexianHydra(UUID ownerId) {
+		super(ownerId, 85, "Phyrexian Hydra", Rarity.RARE, new CardType[]{CardType.CREATURE}, "{3}{G}{G}");
+		this.expansionSetCode = "MBS";
+		this.subtype.add("Hydra");
+		this.color.setGreen(true);
+		this.power = new MageInt(7);
+		this.toughness = new MageInt(7);
+
+		this.addAbility(InfectAbility.getInstance());
+		this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new PhyrexianHydraEffect()));
+	}
+
+	public PhyrexianHydra(final PhyrexianHydra card) {
+		super(card);
+	}
+
+	@Override
+	public PhyrexianHydra copy() {
+		return new PhyrexianHydra(this);
+	}
+
+}
+
+class PhyrexianHydraEffect extends PreventionEffectImpl<PhyrexianHydraEffect> {
+
+    public PhyrexianHydraEffect() {
+        super(Duration.WhileOnBattlefield);
+    }
+
+    public PhyrexianHydraEffect(final PhyrexianHydraEffect effect) {
+        super(effect);
+        staticText = "If damage would be dealt to {this}, prevent that damage. Put a -1/-1 counter on {this} for each 1 damage prevented this way";
+    }
+
+    @Override
+    public PhyrexianHydraEffect copy() {
+        return new PhyrexianHydraEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        return true;
+    }
+
+    @Override
+    public boolean replaceEvent(GameEvent event, Ability source, Game game) {
+        boolean retValue = false;
+        GameEvent preventEvent = new GameEvent(GameEvent.EventType.PREVENT_DAMAGE, source.getFirstTarget(), source.getId(), source.getControllerId(), event.getAmount(), false);
+        int damage = event.getAmount();
+        if (!game.replaceEvent(preventEvent)) {
+            event.setAmount(0);
+            game.fireEvent(GameEvent.getEvent(GameEvent.EventType.PREVENTED_DAMAGE, source.getFirstTarget(), source.getId(), source.getControllerId(), damage));
+            retValue = true;
+        }
+        Permanent permanent = game.getPermanent(source.getSourceId());
+        if (permanent != null) {
+            permanent.addCounters(CounterType.M1M1.createInstance(damage), game);
+        }
+        return retValue;
+    }
+
+    @Override
+    public boolean applies(GameEvent event, Ability source, Game game) {
+        if (super.applies(event, source, game)) {
+            if (event.getTargetId().equals(source.getSourceId())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
