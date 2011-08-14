a472f53036804129843232bf95ee90ece32e1d01
North <robyter@gmail>
2011-08-14 22:55:17 +0300

    [MBS] Brass Squire, [ZEN] Kor Outfitter

---
+++ Mage.Sets/src/mage/sets/mirrodinbesieged/BrassSquire.java
@@ -0,0 +1,109 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.mirrodinbesieged;
+
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Outcome;
+import mage.Constants.Rarity;
+import mage.Constants.Zone;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.effects.OneShotEffect;
+import mage.cards.CardImpl;
+import mage.filter.common.FilterControlledPermanent;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.target.common.TargetControlledCreaturePermanent;
+import mage.target.common.TargetControlledPermanent;
+
+/**
+ *
+ * @author North
+ */
+public class BrassSquire extends CardImpl<BrassSquire> {
+
+    private static final FilterControlledPermanent filter = new FilterControlledPermanent("Equipment you control");
+
+    static {
+        filter.getSubtype().add("Equipment");
+    }
+
+    public BrassSquire(UUID ownerId) {
+        super(ownerId, 101, "Brass Squire", Rarity.UNCOMMON, new CardType[]{CardType.ARTIFACT, CardType.CREATURE}, "{3}");
+        this.expansionSetCode = "MBS";
+        this.subtype.add("Myr");
+
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(3);
+
+        // {tap}: Attach target Equipment you control to target creature you control.
+        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new EquipEffect(), new TapSourceCost());
+        ability.addTarget(new TargetControlledPermanent(filter));
+        ability.addTarget(new TargetControlledCreaturePermanent());
+        this.addAbility(ability);
+    }
+
+    public BrassSquire(final BrassSquire card) {
+        super(card);
+    }
+
+    @Override
+    public BrassSquire copy() {
+        return new BrassSquire(this);
+    }
+}
+
+class EquipEffect extends OneShotEffect<EquipEffect> {
+
+    public EquipEffect() {
+        super(Outcome.BoostCreature);
+        staticText = "Attach target Equipment you control to target creature you control";
+    }
+
+    public EquipEffect(final EquipEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public EquipEffect copy() {
+        return new EquipEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Permanent equipment = game.getPermanent(source.getFirstTarget());
+        Permanent creature = game.getPermanent(source.getTargets().get(1).getFirstTarget());
+        if (creature != null && equipment != null) {
+            return creature.addAttachment(equipment.getId(), game);
+        }
+        return false;
+    }
+}
---
+++ Mage.Sets/src/mage/sets/zendikar/KorOutfitter.java
@@ -0,0 +1,109 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.zendikar;
+
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Outcome;
+import mage.Constants.Rarity;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.effects.OneShotEffect;
+import mage.cards.CardImpl;
+import mage.filter.common.FilterControlledPermanent;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.target.common.TargetControlledCreaturePermanent;
+import mage.target.common.TargetControlledPermanent;
+
+/**
+ *
+ * @author North
+ */
+public class KorOutfitter extends CardImpl<KorOutfitter> {
+
+    private static final FilterControlledPermanent filter = new FilterControlledPermanent("Equipment you control");
+
+    static {
+        filter.getSubtype().add("Equipment");
+    }
+
+    public KorOutfitter(UUID ownerId) {
+        super(ownerId, 21, "Kor Outfitter", Rarity.COMMON, new CardType[]{CardType.CREATURE}, "{W}{W}");
+        this.expansionSetCode = "ZEN";
+        this.subtype.add("Kor");
+        this.subtype.add("Soldier");
+
+        this.color.setWhite(true);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(2);
+
+        // When Kor Outfitter enters the battlefield, you may attach target Equipment you control to target creature you control.
+        Ability ability = new EntersBattlefieldTriggeredAbility(new EquipEffect(), true);
+        ability.addTarget(new TargetControlledPermanent(filter));
+        ability.addTarget(new TargetControlledCreaturePermanent());
+        this.addAbility(ability);
+    }
+
+    public KorOutfitter(final KorOutfitter card) {
+        super(card);
+    }
+
+    @Override
+    public KorOutfitter copy() {
+        return new KorOutfitter(this);
+    }
+}
+
+class EquipEffect extends OneShotEffect<EquipEffect> {
+
+    public EquipEffect() {
+        super(Outcome.BoostCreature);
+        staticText = "attach target Equipment you control to target creature you control";
+    }
+
+    public EquipEffect(final EquipEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public EquipEffect copy() {
+        return new EquipEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Permanent equipment = game.getPermanent(source.getFirstTarget());
+        Permanent creature = game.getPermanent(source.getTargets().get(1).getFirstTarget());
+        if (creature != null && equipment != null) {
+            return creature.addAttachment(equipment.getId(), game);
+        }
+        return false;
+    }
+}
