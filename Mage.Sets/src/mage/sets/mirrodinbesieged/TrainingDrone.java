bcc66390d8e8f027d921418a50c3fd88c53c77aa
North <robyter@gmail>
2011-08-20 20:41:20 +0300

    [MBS] Training Drone

---
+++ Mage.Sets/src/mage/sets/mirrodinbesieged/TrainingDrone.java
@@ -0,0 +1,109 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.mirrodinbesieged;
+
+import java.util.List;
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Duration;
+import mage.Constants.Rarity;
+import mage.Constants.Zone;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.RestrictionEffect;
+import mage.cards.CardImpl;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+
+/**
+ *
+ * @author North
+ */
+public class TrainingDrone extends CardImpl<TrainingDrone> {
+
+    public TrainingDrone(UUID ownerId) {
+        super(ownerId, 142, "Training Drone", Rarity.COMMON, new CardType[]{CardType.ARTIFACT, CardType.CREATURE}, "{3}");
+        this.expansionSetCode = "MBS";
+        this.subtype.add("Drone");
+
+        this.power = new MageInt(4);
+        this.toughness = new MageInt(4);
+
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new TrainingDroneEffect()));
+
+    }
+
+    public TrainingDrone(final TrainingDrone card) {
+        super(card);
+    }
+
+    @Override
+    public TrainingDrone copy() {
+        return new TrainingDrone(this);
+    }
+}
+
+class TrainingDroneEffect extends RestrictionEffect<TrainingDroneEffect> {
+
+    public TrainingDroneEffect() {
+        super(Duration.WhileOnBattlefield);
+        staticText = "{this} can't attack or block unless it's equipped";
+    }
+
+    public TrainingDroneEffect(final TrainingDroneEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public boolean applies(Permanent permanent, Ability source, Game game) {
+        List<UUID> attachments = permanent.getAttachments();
+        for (UUID uuid : attachments) {
+            Permanent attached = game.getBattlefield().getPermanent(uuid);
+            if (attached.getSubtype().contains("Equipment")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean canBlock(Permanent attacker, Permanent blocker, Game game) {
+        return false;
+    }
+
+    @Override
+    public boolean canAttack(Game game) {
+        return false;
+    }
+
+    @Override
+    public TrainingDroneEffect copy() {
+        return new TrainingDroneEffect(this);
+    }
+}
