567dee8ab640fe726efcdc27b70accb639741336
magenoxx <magenoxx@gmail>
2011-08-21 09:12:43 +0400

    [M12] Monomania, Rites of Flourishing

---
+++ Mage.Sets/src/mage/sets/magic2012/Monomania.java
@@ -0,0 +1,115 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.magic2012;
+
+import mage.Constants;
+import mage.Constants.CardType;
+import mage.Constants.Rarity;
+import mage.abilities.Ability;
+import mage.abilities.effects.OneShotEffect;
+import mage.cards.Card;
+import mage.cards.CardImpl;
+import mage.filter.FilterCard;
+import mage.game.Game;
+import mage.players.Player;
+import mage.target.TargetCard;
+import mage.target.TargetPlayer;
+
+import java.util.Iterator;
+import java.util.UUID;
+
+/**
+ *
+ * @author nantuko
+ */
+public class Monomania extends CardImpl<Monomania> {
+
+    public Monomania(UUID ownerId) {
+        super(ownerId, 102, "Monomania", Rarity.RARE, new CardType[]{CardType.SORCERY}, "{3}{B}{B}");
+        this.expansionSetCode = "M12";
+
+        this.color.setBlack(true);
+
+        // Target player chooses a card in his or her hand and discards the rest.
+		this.getSpellAbility().addEffect(new MonomaniaEffect());
+		this.getSpellAbility().addTarget(new TargetPlayer());
+    }
+
+    public Monomania(final Monomania card) {
+        super(card);
+    }
+
+    @Override
+    public Monomania copy() {
+        return new Monomania(this);
+    }
+}
+
+class MonomaniaEffect extends OneShotEffect<MonomaniaEffect> {
+
+	private static final FilterCard filter = new FilterCard("a card");
+
+	public MonomaniaEffect() {
+		super(Constants.Outcome.Discard);
+		staticText = "Target player chooses a card in his or her hand and discards the rest";
+	}
+
+	public MonomaniaEffect(final MonomaniaEffect effect) {
+		super(effect);
+	}
+
+	@Override
+	public boolean apply(Game game, Ability source) {
+		Player player = game.getPlayer(source.getFirstTarget());
+		if (player != null) {
+			TargetCard target = new TargetCard(Constants.Zone.PICK, filter);
+			target.setRequired(true);
+			if (player.choose(Constants.Outcome.Detriment, player.getHand(), target, game)) {
+				while (player.getHand().size() > 1) {
+					for (UUID uuid : player.getHand()) {
+						if (!uuid.equals(target.getFirstTarget())) {
+							Card card = player.getHand().get(uuid, game);
+							if (card != null) {
+								player.discard(card, source, game);
+								break;
+							}
+						}
+					}
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public MonomaniaEffect copy() {
+		return new MonomaniaEffect(this);
+	}
+
+}
