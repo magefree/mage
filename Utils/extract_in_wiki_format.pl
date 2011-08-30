e02b6980c76cf7efc1de3ee5cd07b8e8559a018d
North <robyter@gmail>
2011-08-30 18:10:44 +0300

    Updated extract_in_wiki_format.pl. Now the name of the cars are used for output.

--- Utils/extract_in_wiki_format.pl
+++ Utils/extract_in_wiki_format.pl
@@ -2,22 +2,47 @@
 
 use strict;
 
+my $dataFile = 'mtg-cards-data.txt';
+my $setsFile = 'mtg-sets-data.txt';
 my $knownSetsFile = 'known-sets.txt';
 
 my $cards_count = 0;
 
 my %knownSets;
+my %sets;
+my %cardsBySet;
 my %cards;
+
+sub toCamelCase {
+    my $string = $_[0];
+    $string =~ s/\b([\w']+)\b/ucfirst($1)/ge;
+    $string =~ s/[-,\s\']//g;
+    $string;
+}
+
+open (DATA, $dataFile) || die "can't open $dataFile";
+while(my $line = <DATA>) {
+    my @data = split('\\|', $line);
+    $cardsBySet{$data[1]}{toCamelCase($data[0])} = \@data;
+}
+close(DATA);
+
+open (DATA, $setsFile) || die "can't open $setsFile";
+while(my $line = <DATA>) {
+    my @data = split('\\|', $line);
+    $sets{$data[0]}= $data[1];
+}
+close(DATA);
 
 open (DATA, $knownSetsFile) || die "can't open $knownSetsFile";
 while(my $line = <DATA>) {
     my @data = split('\\|', $line);
-    $knownSets{$data[1]}= $data[2];
+    $knownSets{$data[1]}= $data[0];
 }
 close(DATA);
 
-open CARDS, "< added_cards.txt" or die;
 
+open CARDS, "< added_cards.txt" or die;
 while (<CARDS>) {
 	my $line = $_;
 	if ( $line =~/A Mage.Sets\\src\\mage\\sets\\(\w.*)\\(\w.*)\.java/ ) {
@@ -28,7 +53,7 @@ while (<CARDS>) {
 			$cards{$set} = [];
 		}
 		
-		push $cards{$set}, $card;
+		push @{$cards{$set}}, $card;
 	}
 }
 
@@ -37,17 +62,24 @@ print REPORT " * Added cards ($cards_count):\n";
 foreach my $set (keys(%cards)) {
     if ($set ne "tokens") {
 		if (exists $knownSets{$set}) {
-			print REPORT "   * $knownSets{$set}: ";
+			print REPORT "   * $sets{$knownSets{$set}}: ";
 		} else {
 			print REPORT " $set: ";
 		}
 		my $first = 1;
 		foreach my $card (@{$cards{$set}}) {
-			if ($first == 0) { print REPORT ","; }
-			$card =~ s/([A-Z]{1}[a-z]+)/ $1/g;
-			$card =~ s/A Ether/ AEther/g;
-			print REPORT $card;
-			$first = 0;
+			if ($first == 0) {
+				print REPORT "; ";
+			} else {
+				$first = 0;
+			}
+			if ($cardsBySet{$knownSets{$set}}{$card}) {
+				print REPORT $cardsBySet{$knownSets{$set}}{$card}[0];
+			} else {
+				#$card =~ s/([A-Z]{1}[a-z]+)/ $1/g;
+				#$card =~ s/A Ether/ AEther/g;
+				print "not processed: $sets{$knownSets{$set}} $card\n";
+			}
 		}
 		print REPORT "\n";
 	}
