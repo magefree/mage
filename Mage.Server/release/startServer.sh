19f0a6b7b0ef188fe76c1a66042f6b60f0369f30
Loki <loki@magefree>
2011-09-19 20:58:03 +0300

    move Mage-Server and Mage-Client jars to lib

--- Mage.Server/release/startServer.sh
+++ Mage.Server/release/startServer.sh
@@ -1,1 +1,1 @@
-java -Djava.security.policy=./config/security.policy -Djava.util.logging.config.file=./config/logging.config -Dlog4j.configuration=file:./config/log4j.properties -jar ./Mage-Server-${project.version}.jar
+java -Djava.security.policy=./config/security.policy -Djava.util.logging.config.file=./config/logging.config -Dlog4j.configuration=file:./config/log4j.properties -jar ./lib/Mage-Server-${project.version}.jar
