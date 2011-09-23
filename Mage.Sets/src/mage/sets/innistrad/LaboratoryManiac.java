df5e4abd05547380e5be2bc4f1b0dd3b38435bc6
BetaSteward <betasteward@gmail>
2011-09-23 14:54:27 -0400

    ISD - Laboratory Maniac

---
+++ Mage.Sets/src/mage/sets/innistrad/LaboratoryManiac.java
@@ -0,0 +1,119 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.innistrad;
+
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Duration;
+import mage.Constants.Outcome;
+import mage.Constants.Rarity;
+import mage.Constants.Zone;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.ReplacementEffectImpl;
+import mage.cards.CardImpl;
+import mage.counters.CounterType;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.events.GameEvent.EventType;
+import mage.players.Player;
+
+/**
+ *
+ * @author BetaSteward
+ */
+public class LaboratoryManiac extends CardImpl<LaboratoryManiac> {
+
+    public LaboratoryManiac(UUID ownerId) {
+        super(ownerId, 61, "Laboratory Maniac", Rarity.RARE, new CardType[]{CardType.CREATURE}, "{2}{U}");
+        this.expansionSetCode = "ISD";
+        this.subtype.add("Human");
+        this.subtype.add("Wizard");
+
+        this.color.setBlue(true);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(2);
+
+        // If you would draw a card while your library has no cards in it, you win the game instead.
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new LaboratoryManiacEffect()));
+
+    }
+
+    public LaboratoryManiac(final LaboratoryManiac card) {
+        super(card);
+    }
+
+    @Override
+    public LaboratoryManiac copy() {
+        return new LaboratoryManiac(this);
+    }
+}
+
+class LaboratoryManiacEffect extends ReplacementEffectImpl<LaboratoryManiacEffect> {
+
+    public LaboratoryManiacEffect() {
+        super(Duration.WhileOnBattlefield, Outcome.Benefit);
+        staticText = "If you would draw a card while your library has no cards in it, you win the game instead";
+    }
+
+    public LaboratoryManiacEffect(final LaboratoryManiacEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public LaboratoryManiacEffect copy() {
+        return new LaboratoryManiacEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        return true;
+    }
+
+    @Override
+    public boolean replaceEvent(GameEvent event, Ability source, Game game) {
+        Player player = game.getPlayer(event.getPlayerId());
+        if (player != null) {
+            player.won(game);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean applies(GameEvent event, Ability source, Game game) {
+        if (event.getType() == EventType.LOSES && event.getPlayerId().equals(source.getControllerId())) {
+            Player player = game.getPlayer(event.getPlayerId());
+  			if (!player.hasLost() && (player.getLife() > 0 && player.isEmptyDraw() && player.getCounters().getCount(CounterType.POISON) < 10)) {
+				return true;
+			}
+        }
+        return false;
+    }
+
+}
