f1140eb2fbab7c043f09b1c3cf8fa3ed26a60627
Loki <loki@magefree>
2011-09-30 02:01:53 +0300

    6 10E

---
+++ Mage.Sets/src/mage/sets/tenth/LordOfTheUndead.java
@@ -0,0 +1,90 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.tenth;
+
+import java.util.UUID;
+
+import mage.Constants;
+import mage.Constants.CardType;
+import mage.Constants.Rarity;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.ReturnToHandTargetEffect;
+import mage.abilities.effects.common.continious.BoostControlledEffect;
+import mage.cards.CardImpl;
+import mage.filter.Filter;
+import mage.filter.FilterCard;
+import mage.filter.common.FilterCreaturePermanent;
+import mage.target.common.TargetCardInYourGraveyard;
+
+/**
+ *
+ * @author Loki
+ */
+public class LordOfTheUndead extends CardImpl<LordOfTheUndead> {
+    private final static FilterCreaturePermanent filter = new FilterCreaturePermanent("Zombie");
+    private final static FilterCard filterCard = new FilterCard("Zombie card from your graveyard");
+
+    static {
+        filter.getSubtype().add("Zombie");
+        filter.setScopeSubtype(Filter.ComparisonScope.Any);
+        filterCard.getSubtype().add("Zombie");
+        filterCard.setScopeSubtype(Filter.ComparisonScope.Any);
+    }
+
+    public LordOfTheUndead(UUID ownerId) {
+        super(ownerId, 155, "Lord of the Undead", Rarity.RARE, new CardType[]{CardType.CREATURE}, "{1}{B}{B}");
+        this.expansionSetCode = "10E";
+        this.subtype.add("Zombie");
+
+        this.color.setBlack(true);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(2);
+
+        // Other Zombie creatures get +1/+1.
+        this.addAbility(new SimpleStaticAbility(Constants.Zone.BATTLEFIELD, new BoostControlledEffect(1, 1, Constants.Duration.WhileOnBattlefield, filter, true)));
+        // {1}{B}, {tap}: Return target Zombie card from your graveyard to your hand.
+        Ability ability = new SimpleActivatedAbility(Constants.Zone.BATTLEFIELD, new ReturnToHandTargetEffect(), new ManaCostsImpl("{1}{B}"));
+        ability.addCost(new TapSourceCost());
+        ability.addTarget(new TargetCardInYourGraveyard(filterCard));
+        this.addAbility(ability);
+    }
+
+    public LordOfTheUndead(final LordOfTheUndead card) {
+        super(card);
+    }
+
+    @Override
+    public LordOfTheUndead copy() {
+        return new LordOfTheUndead(this);
+    }
+}
