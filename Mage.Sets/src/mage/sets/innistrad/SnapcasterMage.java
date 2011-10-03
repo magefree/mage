9a937d9623687679be91390e6eecda1d8876fdb4
BetaSteward <betasteward@gmail>
2011-10-02 21:24:11 -0400

    ISD - Snapcaster Mage, Past In Flames

---
+++ Mage.Sets/src/mage/sets/innistrad/SnapcasterMage.java
@@ -0,0 +1,125 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.innistrad;
+
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Duration;
+import mage.Constants.Layer;
+import mage.Constants.Outcome;
+import mage.Constants.Rarity;
+import mage.Constants.SubLayer;
+import mage.Constants.TimingRule;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.effects.ContinuousEffectImpl;
+import mage.abilities.keyword.FlashAbility;
+import mage.abilities.keyword.FlashbackAbility;
+import mage.cards.Card;
+import mage.cards.CardImpl;
+import mage.filter.Filter.ComparisonScope;
+import mage.filter.FilterCard;
+import mage.game.Game;
+import mage.target.common.TargetCardInYourGraveyard;
+
+/**
+ *
+ * @author BetaSteward
+ */
+public class SnapcasterMage extends CardImpl<SnapcasterMage> {
+
+	private static final FilterCard filter = new FilterCard("instant or sorcery card in your graveyard");
+
+	static {
+		filter.getCardType().add(CardType.INSTANT);
+		filter.getCardType().add(CardType.SORCERY);
+		filter.setScopeCardType(ComparisonScope.Any);
+	}
+
+    public SnapcasterMage(UUID ownerId) {
+        super(ownerId, 78, "Snapcaster Mage", Rarity.RARE, new CardType[]{CardType.CREATURE}, "{1}{U}");
+        this.expansionSetCode = "ISD";
+        this.subtype.add("Human");
+        this.subtype.add("Wizard");
+
+        this.color.setBlue(true);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(1);
+
+        this.addAbility(FlashAbility.getInstance());
+        // When Snapcaster Mage enters the battlefield, target instant or sorcery card in your graveyard gains flashback until end of turn. The flashback cost is equal to its mana cost.
+        Ability ability = new EntersBattlefieldTriggeredAbility(new SnapcasterMageEffect());
+        ability.addTarget(new TargetCardInYourGraveyard(filter));
+        this.addAbility(ability);
+        
+    }
+
+    public SnapcasterMage(final SnapcasterMage card) {
+        super(card);
+    }
+
+    @Override
+    public SnapcasterMage copy() {
+        return new SnapcasterMage(this);
+    }
+}
+
+class SnapcasterMageEffect extends ContinuousEffectImpl<SnapcasterMageEffect> {
+
+	public SnapcasterMageEffect() {
+		super(Duration.EndOfTurn, Layer.AbilityAddingRemovingEffects_6, SubLayer.NA, Outcome.AddAbility);
+        this.staticText = "target instant or sorcery card in your graveyard gains flashback until end of turn. The flashback cost is equal to its mana cost";
+	}
+
+	public SnapcasterMageEffect(final SnapcasterMageEffect effect) {
+		super(effect);
+	}
+
+	@Override
+	public SnapcasterMageEffect copy() {
+		return new SnapcasterMageEffect(this);
+	}
+
+	@Override
+	public boolean apply(Game game, Ability source) {
+		Card card = game.getCard(targetPointer.getFirst(source));
+		if (card != null) {
+            FlashbackAbility ability;
+            if (card.getCardType().contains(CardType.INSTANT))
+                ability = new FlashbackAbility(card.getManaCost(), TimingRule.INSTANT);
+            else
+                ability = new FlashbackAbility(card.getManaCost(), TimingRule.SORCERY);
+            ability.setSourceId(card.getId());
+            ability.setControllerId(card.getOwnerId());
+            game.getState().addOtherAbility(card.getId(), ability);
+            return true;
+		}
+		return false;
+	}
+}
