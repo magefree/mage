82712c869f0eb0654e245ec98317919c8bd7fe8b
Alvin <alvin@magefree>
2011-10-05 23:36:58 +0900

    [ISD]Stromkirk Noble

---
+++ Mage.Sets/src/mage/sets/innistrad/StromkirkNoble.java
@@ -0,0 +1,100 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.innistrad;
+
+import java.util.UUID;
+
+import mage.Constants.CardType;
+import mage.Constants.Duration;
+import mage.Constants.Rarity;
+import mage.Constants.Zone;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.DealsCombatDamageToAPlayerTriggeredAbility;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.common.CantBlockSourceEffect;
+import mage.abilities.effects.common.continious.CantBeBlockedByOneEffect;
+import mage.abilities.effects.common.counter.AddCountersSourceEffect;
+import mage.cards.CardImpl;
+import mage.counters.CounterType;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+
+/**
+ *
+ * @author Alvin
+ */
+public class StromkirkNoble extends CardImpl<StromkirkNoble> {
+
+    public StromkirkNoble(UUID ownerId) {
+        super(ownerId, 164, "Stromkirk Noble", Rarity.RARE, new CardType[]{CardType.CREATURE}, "{R}");
+        this.expansionSetCode = "ISD";
+        this.subtype.add("Vampire");
+
+        this.color.setRed(true);
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(1);
+
+        // Stromkirk Noble can't be blocked by Humans.
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new CantBeBlockedByOneEffect(2)));
+        // Whenever Stromkirk Noble deals combat damage to a player, put a +1/+1 counter on it.
+        this.addAbility(new DealsCombatDamageToAPlayerTriggeredAbility(new AddCountersSourceEffect(CounterType.P1P1.createInstance()), false));
+        
+    }
+
+    public StromkirkNoble(final StromkirkNoble card) {
+        super(card);
+    }
+
+    @Override
+    public StromkirkNoble copy() {
+        return new StromkirkNoble(this);
+    }
+}
+
+class StromkirkEffect extends CantBlockSourceEffect {
+
+	public StromkirkEffect() {
+		super(Duration.WhileOnBattlefield);
+	}
+
+	public StromkirkEffect(final StromkirkEffect effect) {
+		super(effect);
+	}
+
+	@Override
+	public boolean canBeBlocked(Permanent attacker, Permanent blocker, Ability source, Game game) {
+		return !blocker.hasSubtype("Human");
+	}
+
+	@Override
+	public StromkirkEffect copy() {
+		return new StromkirkEffect(this);
+	}
+
+}
