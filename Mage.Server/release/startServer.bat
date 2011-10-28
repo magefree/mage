416e8cd30b0015f8c72f141500b2a2413473e2be
magenoxx <magenoxx@gmail>
2011-10-28 14:38:25 +0400

    added memory settings to scripts. fixed .command script for server.

--- Mage.Server/release/startServer.bat
+++ Mage.Server/release/startServer.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-java -Djava.security.policy=./config/security.policy -Djava.util.logging.config.file=./config/logging.config -Dlog4j.configuration=file:./config/log4j.properties -jar ./lib/Mage-Server-${project.version}.jar
+java -Djava.security.policy=./config/security.policy -Djava.util.logging.config.file=./config/logging.config -Dlog4j.configuration=file:./config/log4j.properties -jar ./lib/Mage-Server-${project.version}.jar -Xms256M -Xmx512M
 pause
--- Mage.Server/release/startServer.command
+++ Mage.Server/release/startServer.command
@@ -2,4 +2,4 @@
 
 cd "`dirname "$0"`"
 
-java -Djava.security.policy=./config/security.policy -Djava.util.logging.config.file=./config/logging.config -Dlog4j.configuration=file:./config/log4j.properties -jar ./lib/Mage-Server-0.8.jar
+java -Djava.security.policy=./config/security.policy -Djava.util.logging.config.file=./config/logging.config -Dlog4j.configuration=file:./config/log4j.properties -jar ./lib/Mage-Server-${project.version}.jar -Xms256M -Xmx512M
