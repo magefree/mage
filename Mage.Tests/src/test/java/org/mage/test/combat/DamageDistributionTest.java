9fe16d3518c3f75805a7b2447b1195e740571043
magenoxx <magenoxx@gmail>
2011-12-12 14:28:33 +0400

    Damage distribution test. Reproduces Issue 375.

---
+++ Mage.Tests/src/test/java/org/mage/test/combat/DamageDistributionTest.java
@@ -0,0 +1,34 @@
+package org.mage.test.combat;
+
+import junit.framework.Assert;
+import mage.Constants;
+import mage.game.permanent.Permanent;
+import org.junit.Test;
+import org.mage.test.serverside.base.CardTestBase;
+
+/**
+ *
+ * @author ayrat
+ */
+public class DamageDistributionTest extends CardTestBase {
+
+    @Test
+    public void testDoubleStrike() {
+        addCard(Constants.Zone.BATTLEFIELD, playerA, "Warren Instigator");
+        addCard(Constants.Zone.BATTLEFIELD, playerB, "Merfolk Looter");
+        setLife(playerB, 4);
+
+        execute();
+
+        Permanent instigator = getPermanent("Warren Instigator", playerA.getId());
+        Assert.assertNotNull(instigator);
+
+        // should block and die
+        assertPermanentCount(playerB, "Merfolk Looter", 0);
+
+        // creature is blocked
+        // blocker dies and second strike does nothing
+        assertLife(playerB, 4);
+    }
+
+}
