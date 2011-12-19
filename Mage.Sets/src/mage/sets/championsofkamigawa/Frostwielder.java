4e402d97f90a40be22a1c0db5ddaa5c13503b758
LevelX <ludwig.hirth@gmail>
2011-12-19 07:40:05 +0100

    [CHK] Frostwielder, Hold the Line, Reito Latern, Swamps

---
+++ Mage.Sets/src/mage/sets/championsofkamigawa/Frostwielder.java
@@ -0,0 +1,139 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+
+package mage.sets.championsofkamigawa;
+
+import java.util.UUID;
+
+import mage.Constants.CardType;
+import mage.Constants.Duration;
+import mage.Constants.Outcome;
+import mage.Constants.Rarity;
+import mage.Constants.Zone;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.ReplacementEffectImpl;
+import mage.abilities.effects.common.DamageTargetEffect;
+import mage.cards.CardImpl;
+import mage.filter.Filter;
+import mage.filter.common.FilterControlledPermanent;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.events.GameEvent.EventType;
+import mage.game.events.ZoneChangeEvent;
+import mage.game.permanent.Permanent;
+import mage.target.common.TargetCreatureOrPlayer;
+import mage.watchers.common.DamagedByWatcher;
+
+/**
+ * @author LevelX
+ */
+public class Frostwielder extends CardImpl<Frostwielder> {
+
+    private final static FilterControlledPermanent filter = new FilterControlledPermanent("a land");
+
+    static {
+        filter.getCardType().add(CardType.LAND);
+        filter.setScopeCardType(Filter.ComparisonScope.Any);
+    }
+
+    public Frostwielder(UUID ownerId) {
+        super(ownerId, 167, "Frostwielder", Rarity.COMMON, new CardType[]{CardType.CREATURE}, "{2}{R}{R}");
+        this.expansionSetCode = "CHK";
+        this.subtype.add("Human");
+        this.subtype.add("Shaman");
+        this.color.setRed(true);
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(2);
+        
+        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new DamageTargetEffect(1), new TapSourceCost());
+        ability.addTarget(new TargetCreatureOrPlayer());
+        this.addAbility(ability);
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new FrostwielderEffect()));
+        
+        this.addWatcher(new DamagedByWatcher());
+    }
+
+    public Frostwielder(final Frostwielder card) {
+        super(card);
+    }
+
+    @Override
+    public Frostwielder copy() {
+        return new Frostwielder(this);
+    }
+
+}
+
+
+class FrostwielderEffect extends ReplacementEffectImpl<FrostwielderEffect> {
+
+	public FrostwielderEffect() {
+		super(Duration.EndOfTurn, Outcome.Exile);
+		staticText = "If a creature dealt damage by {this} this turn would die, exile it instead";
+	}
+
+	public FrostwielderEffect(final FrostwielderEffect effect) {
+		super(effect);
+	}
+
+	@Override
+	public FrostwielderEffect copy() {
+		return new FrostwielderEffect(this);
+	}
+
+	@Override
+	public boolean apply(Game game, Ability source) {
+		return true;
+	}
+
+	@Override
+	public boolean replaceEvent(GameEvent event, Ability source, Game game) {
+        Permanent permanent = ((ZoneChangeEvent)event).getTarget();
+        if (permanent != null) {
+            return permanent.moveToExile(null, "", source.getId(), game);
+        }
+		return false;
+	}
+
+	@Override
+	public boolean applies(GameEvent event, Ability source, Game game) {
+		if (event.getType() == EventType.ZONE_CHANGE && ((ZoneChangeEvent)event).isDiesEvent()) {
+                        DamagedByWatcher watcher = 
+                                (DamagedByWatcher) game.getState().getWatchers().get("DamagedByWatcher", source.getSourceId());
+                        if (watcher != null)
+                                return watcher.damagedCreatures.contains(event.getTargetId());
+                        }
+		return false;
+	}
+
+}
