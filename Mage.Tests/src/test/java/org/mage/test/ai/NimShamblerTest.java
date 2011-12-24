f134a79203e81b1cf8bedf81d7ee6fc2d7fdeacd
magenoxx <magenoxx@gmail>
2011-12-24 19:00:10 +0400

    Added test for additional damage from AI after combat. Fixed NimShamblerTest.

--- Mage.Tests/src/test/java/org/mage/test/ai/NimShamblerTest.java
+++ Mage.Tests/src/test/java/org/mage/test/ai/NimShamblerTest.java
@@ -2,6 +2,7 @@ package org.mage.test.ai;
 
 import junit.framework.Assert;
 import mage.Constants;
+import mage.counters.CounterType;
 import mage.game.permanent.Permanent;
 import org.junit.Test;
 import org.mage.test.serverside.base.CardTestBase;
@@ -18,7 +19,7 @@ public class NimShamblerTest extends CardTestBase {
     public void testNoCreatureWasSacrificed() {
         addCard(Constants.Zone.BATTLEFIELD, playerA, "Nim Shambler");
         addCard(Constants.Zone.BATTLEFIELD, playerA, "Blood Cultist");
-        addCard(Constants.Zone.BATTLEFIELD, playerB, "Ob Nixilis, the Fallen");
+        addCard(Constants.Zone.BATTLEFIELD, playerB, "Craw Wurm");
 
         execute();
 
@@ -27,6 +28,26 @@ public class NimShamblerTest extends CardTestBase {
 
         Permanent bloodCultist = getPermanent("Blood Cultist", playerA.getId());
         Assert.assertNotNull(bloodCultist);
+        Assert.assertFalse(bloodCultist.isTapped()); // shouldn't be tapped
+    }
 
+    @Test
+    public void testAttackAndKillBlockerWithAdditionalDamage() {
+        addCard(Constants.Zone.BATTLEFIELD, playerA, "Nim Shambler");
+        addCard(Constants.Zone.BATTLEFIELD, playerA, "Blood Cultist");
+        addCard(Constants.Zone.BATTLEFIELD, playerB, "Ob Nixilis, the Fallen");
+
+        execute();
+
+        // should die in attack
+        assertPermanentCount(playerA, "Nim Shambler", 0);
+        // should die because of attack + 1 damage from Blood Cultist
+        assertPermanentCount(playerA, "Ob Nixilis, the Fallen", 0);
+
+        // Blood Cultist should kill Ob Nixilis, the Fallen and get +1\+1
+        Permanent bloodCultist = getPermanent("Blood Cultist", playerA.getId());
+        Assert.assertNotNull(bloodCultist);
+        Assert.assertEquals(1, bloodCultist.getCounters().size());
+        Assert.assertEquals(1, bloodCultist.getCounters().getCount(CounterType.P1P1));
     }
 }
