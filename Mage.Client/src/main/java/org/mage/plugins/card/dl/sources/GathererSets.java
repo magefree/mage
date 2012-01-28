3b1169e9d30ab29ad047659eb8181893229b4dc4
Loki <loki@magefree>
2012-01-28 14:29:54 +0400

    add USG set icon to download list

--- Mage.Client/src/main/java/org/mage/plugins/card/dl/sources/GathererSets.java
+++ Mage.Client/src/main/java/org/mage/plugins/card/dl/sources/GathererSets.java
@@ -16,7 +16,7 @@ public class GathererSets implements Iterable<DownloadJob> {
     private final static File DEFAULT_OUT_DIR = new File("plugins" + File.separator + "images" + SETS_PATH);
     private static File outDir = DEFAULT_OUT_DIR;
 
-    private static final String[] symbols = {"10E", "HOP", "TMP", "INV", "PLS", "APC", "MRD", "DST", "5DN", "CHK", "BOK", "SOK", "RAV", "GPT", "DIS", "TSP", "TSB", "PLC", "FUT", "LRW", "MOR", "SHM", "EVE"};
+    private static final String[] symbols = {"10E", "HOP", "TMP", "INV", "PLS", "APC", "MRD", "DST", "5DN", "CHK", "BOK", "SOK", "RAV", "GPT", "DIS", "TSP", "TSB", "PLC", "FUT", "LRW", "MOR", "SHM", "EVE", "USG"};
     private static final String[] withMythics = {"M10", "M11", "M12", "DDF", "ALA", "CFX", "ARB", "ZEN", "WWK", "ROE", "SOM", "MBS", "NPH", "ISD"};
     private static final HashMap<String, String> symbolsReplacements = new HashMap<String, String>();
 
@@ -26,6 +26,7 @@ public class GathererSets implements Iterable<DownloadJob> {
         symbolsReplacements.put("TMP", "TE");
         symbolsReplacements.put("INV", "IN");
         symbolsReplacements.put("PLS", "PS");
+        symbolsReplacements.put("USG", "UZ");
     }
 
     public GathererSets(String path) {
