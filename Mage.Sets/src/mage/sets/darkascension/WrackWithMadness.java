11a64a1c6add89141ab7d3793ac9d9308238028b
North <robyter@gmail>
2012-03-04 22:41:35 +0200

    [DKA] 5 cards

---
+++ Mage.Sets/src/mage/sets/darkascension/WrackWithMadness.java
@@ -0,0 +1,93 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.darkascension;
+
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Outcome;
+import mage.Constants.Rarity;
+import mage.abilities.Ability;
+import mage.abilities.effects.OneShotEffect;
+import mage.cards.CardImpl;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.target.common.TargetCreaturePermanent;
+
+/**
+ *
+ * @author North
+ */
+public class WrackWithMadness extends CardImpl<WrackWithMadness> {
+
+    public WrackWithMadness(UUID ownerId) {
+        super(ownerId, 107, "Wrack with Madness", Rarity.COMMON, new CardType[]{CardType.SORCERY}, "{3}{R}");
+        this.expansionSetCode = "DKA";
+
+        this.color.setRed(true);
+
+        // Target creature deals damage to itself equal to its power.
+        this.getSpellAbility().addEffect(new WrackWithMadnessEffect());
+        this.getSpellAbility().addTarget(new TargetCreaturePermanent());
+    }
+
+    public WrackWithMadness(final WrackWithMadness card) {
+        super(card);
+    }
+
+    @Override
+    public WrackWithMadness copy() {
+        return new WrackWithMadness(this);
+    }
+}
+
+class WrackWithMadnessEffect extends OneShotEffect<WrackWithMadnessEffect> {
+
+    public WrackWithMadnessEffect() {
+        super(Outcome.Damage);
+        this.staticText = "Target creature deals damage to itself equal to its power";
+    }
+
+    public WrackWithMadnessEffect(final WrackWithMadnessEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public WrackWithMadnessEffect copy() {
+        return new WrackWithMadnessEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Permanent permanent = game.getPermanent(targetPointer.getFirst(source));
+        if (permanent != null) {
+            permanent.damage(permanent.getPower().getValue(), permanent.getId(), game, true, false);
+            return true;
+        }
+        return false;
+    }
+}
