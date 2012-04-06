8d6766b0118e50823a1c41a98c5a5681102cabed
jeffwadsworth <jeffwadsworth@magefree>
2012-04-05 21:04:33 -0500

    - Added Champion's Drake <ROE>

---
+++ Mage.Sets/src/mage/sets/riseoftheeldrazi/ChampionsDrake.java
@@ -0,0 +1,80 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.riseoftheeldrazi;
+
+import java.util.UUID;
+import mage.Constants;
+import mage.Constants.CardType;
+import mage.Constants.Rarity;
+import mage.MageInt;
+import mage.Constants.Duration;
+import mage.abilities.condition.common.PermanentHasCounterCondition;
+import mage.abilities.keyword.FlyingAbility;
+import mage.cards.CardImpl;
+import mage.counters.CounterType;
+import mage.filter.common.FilterCreaturePermanent;
+import mage.abilities.effects.common.continious.BoostSourceEffect;
+import mage.abilities.decorator.ConditionalContinousEffect;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.Constants.Zone;
+
+
+/**
+ *
+ * @author jeffwadsworth
+ */
+public class ChampionsDrake extends CardImpl<ChampionsDrake> {
+    
+    private static final String rule = "{this} gets +3/+3 as long as you control a creature with three or more level counters on it.";
+
+    public ChampionsDrake(UUID ownerId) {
+
+        super(ownerId, 56, "Champion's Drake", Rarity.COMMON, new CardType[]{CardType.CREATURE}, "{1}{U}");
+        this.expansionSetCode = "ROE";
+        this.subtype.add("Drake");
+
+        this.color.setBlue(true);
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(1);
+
+        this.addAbility(FlyingAbility.getInstance());
+        
+        // Champion's Drake gets +3/+3 as long as you control a creature with three or more level counters on it.
+        ConditionalContinousEffect effect = new ConditionalContinousEffect(new BoostSourceEffect(3, 3, Duration.WhileOnBattlefield), new PermanentHasCounterCondition(CounterType.LEVEL, 2, new FilterCreaturePermanent(), PermanentHasCounterCondition.CountType.MORE_THAN), rule);
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, effect));
+    }
+
+    public ChampionsDrake(final ChampionsDrake card) {
+        super(card);
+    }
+
+    @Override
+    public ChampionsDrake copy() {
+        return new ChampionsDrake(this);
+    }
+}
---
+++ Mage/src/mage/abilities/condition/common/PermanentHasCounterCondition.java
@@ -0,0 +1,89 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.abilities.condition.common;
+
+import mage.abilities.Ability;
+import mage.abilities.condition.Condition;
+import mage.counters.CounterType;
+import mage.game.Game;
+import mage.filter.FilterPermanent;
+import mage.game.permanent.Permanent;
+import java.util.List;
+
+/**
+ * 
+ * @author jeffwadsworth
+ */
+public class PermanentHasCounterCondition implements Condition {
+    
+        public static enum CountType { MORE_THAN, FEWER_THAN, EQUAL_TO };
+	private CounterType counterType;
+	private int amount;
+        private FilterPermanent filter;
+        private CountType type;
+        
+        public PermanentHasCounterCondition(CounterType counterType, int amount, FilterPermanent filter) {
+                this(counterType, amount, filter, CountType.EQUAL_TO);
+        }
+        
+        public PermanentHasCounterCondition(CounterType counterType, int amount, FilterPermanent filter, CountType type) {
+                this.counterType = counterType;
+                this.amount = amount;
+                this.filter = filter;
+                this.type = type;
+        }
+
+	@Override
+	public boolean apply(Game game, Ability source) {
+                boolean conditionApplies = false;
+                List<Permanent> permanents = game.getBattlefield().getAllActivePermanents(this.filter, source.getControllerId());
+                for (Permanent permanent : permanents) {
+                    switch ( this.type ) {
+			case FEWER_THAN:
+                                    if (permanent.getCounters().getCount(this.counterType) < this.amount) {
+                                        conditionApplies = true;
+                                        break;
+                                    }
+                                    break;
+			case MORE_THAN:
+                                    if (permanent.getCounters().getCount(this.counterType) > this.amount) {
+                                        conditionApplies = true;
+                                        break;
+                                    }
+                                    break;
+			case EQUAL_TO:
+                                    if (permanent.getCounters().getCount(this.counterType) == this.amount) {
+                                        conditionApplies = true;
+                                        break;
+                                    }
+                                    break;
+                    }
+                }
+                return conditionApplies;
+	}
+}
