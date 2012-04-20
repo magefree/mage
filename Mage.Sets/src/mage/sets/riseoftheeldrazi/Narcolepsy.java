c95b7ae41c767560014550ac29db2106e1484f0e
jeffwadsworth <jeffwadsworth@magefree>
2012-04-20 11:16:04 -0500

    - Added Narcolepsy <ROE>

---
+++ Mage.Sets/src/mage/sets/riseoftheeldrazi/Narcolepsy.java
@@ -0,0 +1,115 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.riseoftheeldrazi;
+
+import java.util.UUID;
+import mage.Constants;
+import mage.Constants.CardType;
+import mage.Constants.Rarity;
+import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.AttachEffect;
+import mage.abilities.keyword.EnchantAbility;
+import mage.cards.CardImpl;
+import mage.filter.Filter;
+import mage.filter.common.FilterCreaturePermanent;
+import mage.target.TargetPermanent;
+import mage.Constants.Zone;
+import mage.abilities.common.BeginningOfUpkeepTriggeredAbility;
+import mage.game.permanent.Permanent;
+import mage.abilities.Ability;
+import mage.game.Game;
+
+/**
+ *
+ * @author jeffwadsworth
+ */
+public class Narcolepsy extends CardImpl<Narcolepsy> {
+    
+    private static final FilterCreaturePermanent filter = new FilterCreaturePermanent("creature");
+
+    static {
+        filter.setScopeCardType(Filter.ComparisonScope.Any);
+    }
+
+    public Narcolepsy(UUID ownerId) {
+        super(ownerId, 79, "Narcolepsy", Rarity.COMMON, new CardType[]{CardType.ENCHANTMENT}, "{1}{U}");
+        this.expansionSetCode = "ROE";
+        this.subtype.add("Aura");
+
+        this.color.setBlue(true);
+
+        // Enchant creature
+        TargetPermanent auraTarget = new TargetPermanent(filter);
+        this.getSpellAbility().addTarget(auraTarget);
+        this.getSpellAbility().addEffect(new AttachEffect(Constants.Outcome.Detriment));
+        EnchantAbility ability = new EnchantAbility(auraTarget.getTargetName());
+        this.addAbility(ability);
+        
+        // At the beginning of each upkeep, if enchanted creature is untapped, tap it.
+        this.addAbility(new BeginningOfUpkeepTriggeredAbility(Zone.BATTLEFIELD, new NarcolepsyEffect(), Constants.TargetController.ANY, false));
+    }
+
+    public Narcolepsy(final Narcolepsy card) {
+        super(card);
+    }
+
+    @Override
+    public Narcolepsy copy() {
+        return new Narcolepsy(this);
+    }
+}
+
+class NarcolepsyEffect extends OneShotEffect<NarcolepsyEffect> {
+    
+    public NarcolepsyEffect() {
+        super(Constants.Outcome.Detriment);
+        this.staticText = "if enchanted creature is untapped, tap it";
+    }
+    
+    public NarcolepsyEffect(final NarcolepsyEffect effect) {
+        super(effect);
+    }
+    
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Permanent narcolepsy = game.getPermanent(source.getSourceId());
+        if (narcolepsy != null) {
+            Permanent enchanted = game.getPermanent(narcolepsy.getAttachedTo());
+            if (enchanted != null && !enchanted.isTapped()) {
+                enchanted.tap(game);
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    @Override
+    public NarcolepsyEffect copy() {
+        return new NarcolepsyEffect(this);
+    }
+}
