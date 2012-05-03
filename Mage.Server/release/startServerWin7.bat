8a0117359f1e2f2d77e84e370337670f66408137
magenoxx <magenoxx@gmail>
2012-05-03 18:24:50 +0400

    added startServerWin7.bat like it was for client

---
+++ Mage.Server/release/startServerWin7.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+IF NOT EXIST "C:\Program Files (x86)\Java\jre6\bin\" GOTO NOJAVADIR
+set JAVA_HOME="C:\Program Files (x86)\Java\jre6\"
+set CLASSPATH=%JAVA_HOME%/bin;%CLASSPATH%
+set PATH=%JAVA_HOME%/bin;%PATH%
+:NOJAVADIR
+java -Djava.security.policy=./config/security.policy -Djava.util.logging.config.file=./config/logging.config -Dlog4j.configuration=file:./config/log4j.properties -jar ./lib/mage-server-${project.version}.jar -Xms256M -Xmx512M
