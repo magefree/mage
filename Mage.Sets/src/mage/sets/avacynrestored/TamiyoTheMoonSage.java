e9489b01f0e1cffb08a966e3d30ee088b260b051
North <robyter@gmail>
2012-05-09 20:52:25 +0300

    [AVR] comitted Planeswalkers Triggered abilitty of Tamayo emblem doesn't work yet.

---
+++ Mage.Sets/src/mage/sets/avacynrestored/TamiyoTheMoonSage.java
@@ -0,0 +1,211 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.avacynrestored;
+
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Duration;
+import mage.Constants.Outcome;
+import mage.Constants.Rarity;
+import mage.Constants.Zone;
+import mage.abilities.Ability;
+import mage.abilities.LoyaltyAbility;
+import mage.abilities.TriggeredAbilityImpl;
+import mage.abilities.common.EntersBattlefieldAbility;
+import mage.abilities.dynamicvalue.DynamicValue;
+import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.DrawCardControllerEffect;
+import mage.abilities.effects.common.SkipNextUntapTargetEffect;
+import mage.abilities.effects.common.TapTargetEffect;
+import mage.abilities.effects.common.continious.MaximumHandSizeControllerEffect;
+import mage.abilities.effects.common.counter.AddCountersSourceEffect;
+import mage.cards.Card;
+import mage.cards.CardImpl;
+import mage.counters.CounterType;
+import mage.filter.common.FilterCreaturePermanent;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.events.ZoneChangeEvent;
+import mage.target.TargetPermanent;
+import mage.target.TargetPlayer;
+import mage.target.targetpointer.FixedTarget;
+
+/**
+ *
+ * @author North
+ */
+public class TamiyoTheMoonSage extends CardImpl<TamiyoTheMoonSage> {
+
+    public TamiyoTheMoonSage(UUID ownerId) {
+        super(ownerId, 79, "Tamiyo, the Moon Sage", Rarity.MYTHIC, new CardType[]{CardType.PLANESWALKER}, "{3}{U}{U}");
+        this.expansionSetCode = "AVR";
+        this.subtype.add("Tamiyo");
+
+        this.color.setBlue(true);
+
+        this.addAbility(new EntersBattlefieldAbility(new AddCountersSourceEffect(CounterType.LOYALTY.createInstance(4)), ""));
+
+        // +1: Tap target permanent. It doesn't untap during its controller's next untap step.
+        LoyaltyAbility ability = new LoyaltyAbility(new TapTargetEffect(), 1);
+        ability.addEffect(new SkipNextUntapTargetEffect());
+        ability.addTarget(new TargetPermanent());
+        this.addAbility(ability);
+        // -2: Draw a card for each tapped creature target player controls.
+        ability = new LoyaltyAbility(new DrawCardControllerEffect(new TapedCreaturesControlledByTargetCount()), -2);
+        ability.addTarget(new TargetPlayer());
+        this.addAbility(ability);
+        // -8: You get an emblem with "You have no maximum hand size" and "Whenever a card is put into your graveyard from anywhere, you may return it to your hand."
+        this.addAbility(new LoyaltyAbility(new TamiyoTheMoonSageThirdEffect(), -8));
+    }
+
+    public TamiyoTheMoonSage(final TamiyoTheMoonSage card) {
+        super(card);
+    }
+
+    @Override
+    public TamiyoTheMoonSage copy() {
+        return new TamiyoTheMoonSage(this);
+    }
+}
+
+class TapedCreaturesControlledByTargetCount implements DynamicValue {
+
+    private static final FilterCreaturePermanent filter = new FilterCreaturePermanent();
+
+    static {
+        filter.setTapped(true);
+        filter.setUseTapped(true);
+    }
+
+    @Override
+    public int calculate(Game game, Ability sourceAbility) {
+        return game.getBattlefield().count(filter, sourceAbility.getFirstTarget(), game);
+    }
+
+    @Override
+    public DynamicValue clone() {
+        return new TapedCreaturesControlledByTargetCount();
+    }
+
+    @Override
+    public String toString() {
+        return "a";
+    }
+
+    @Override
+    public String getMessage() {
+        return "tapped creature target player controls";
+    }
+}
+
+class TamiyoTheMoonSageThirdEffect extends OneShotEffect<TamiyoTheMoonSageThirdEffect> {
+
+    public TamiyoTheMoonSageThirdEffect() {
+        super(Outcome.Benefit);
+        this.staticText = "You get an emblem with \"You have no maximum hand size\" and \"Whenever a card is put into your graveyard from anywhere, you may return it to your hand.\"";
+    }
+
+    public TamiyoTheMoonSageThirdEffect(final TamiyoTheMoonSageThirdEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public TamiyoTheMoonSageThirdEffect copy() {
+        return new TamiyoTheMoonSageThirdEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        // TODO: move this to emblem
+        game.addEffect(new MaximumHandSizeControllerEffect(Integer.MAX_VALUE, Duration.WhileOnBattlefield, false), source);
+        TamiyoTheMoonSageTriggeredAbility ability = new TamiyoTheMoonSageTriggeredAbility();
+        ability.setControllerId(source.getControllerId());
+        ability.setSourceId(source.getSourceId());
+        game.getState().addAbility(ability);
+        return true;
+    }
+}
+
+class TamiyoTheMoonSageTriggeredAbility extends TriggeredAbilityImpl<TamiyoTheMoonSageTriggeredAbility> {
+
+    public TamiyoTheMoonSageTriggeredAbility() {
+        super(Zone.COMMAND, new TamiyoTheMoonSageEffect(), true);
+    }
+
+    public TamiyoTheMoonSageTriggeredAbility(final TamiyoTheMoonSageTriggeredAbility ability) {
+        super(ability);
+    }
+
+    @Override
+    public TamiyoTheMoonSageTriggeredAbility copy() {
+        return new TamiyoTheMoonSageTriggeredAbility(this);
+    }
+
+    @Override
+    public boolean checkTrigger(GameEvent event, Game game) {
+        if (event.getType() == GameEvent.EventType.ZONE_CHANGE && ((ZoneChangeEvent) event).getToZone() == Zone.GRAVEYARD) {
+            Card card = game.getCard(event.getTargetId());
+            if (card != null && card.getOwnerId().equals(this.getControllerId())) {
+                this.getEffects().get(0).setTargetPointer(new FixedTarget(card.getId()));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String getRule() {
+        return "Whenever a card is put into your graveyard from anywhere, you may return it to your hand.";
+    }
+}
+
+class TamiyoTheMoonSageEffect extends OneShotEffect<TamiyoTheMoonSageEffect> {
+
+    public TamiyoTheMoonSageEffect() {
+        super(Outcome.ReturnToHand);
+        this.staticText = "return it to your hand";
+    }
+
+    public TamiyoTheMoonSageEffect(final TamiyoTheMoonSageEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public TamiyoTheMoonSageEffect copy() {
+        return new TamiyoTheMoonSageEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Card card = game.getCard(this.targetPointer.getFirst(source));
+        if (card != null) {
+            return card.moveToZone(Zone.HAND, source.getId(), game, true);
+        }
+        return false;
+    }
+}
---
+++ Mage.Sets/src/mage/sets/avacynrestored/TibaltTheFiendBlooded.java
@@ -0,0 +1,215 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.avacynrestored;
+
+import java.util.List;
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Duration;
+import mage.Constants.Layer;
+import mage.Constants.Outcome;
+import mage.Constants.Rarity;
+import mage.Constants.SubLayer;
+import mage.abilities.Ability;
+import mage.abilities.LoyaltyAbility;
+import mage.abilities.common.EntersBattlefieldAbility;
+import mage.abilities.dynamicvalue.DynamicValue;
+import mage.abilities.effects.ContinuousEffect;
+import mage.abilities.effects.ContinuousEffectImpl;
+import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.DamageTargetEffect;
+import mage.abilities.effects.common.continious.GainAbilityTargetEffect;
+import mage.abilities.effects.common.counter.AddCountersSourceEffect;
+import mage.abilities.keyword.HasteAbility;
+import mage.cards.Card;
+import mage.cards.CardImpl;
+import mage.counters.CounterType;
+import mage.filter.common.FilterCreaturePermanent;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.players.Player;
+import mage.target.TargetPlayer;
+import mage.target.targetpointer.FixedTarget;
+
+/**
+ *
+ * @author North
+ */
+public class TibaltTheFiendBlooded extends CardImpl<TibaltTheFiendBlooded> {
+
+    public TibaltTheFiendBlooded(UUID ownerId) {
+        super(ownerId, 161, "Tibalt, the Fiend-Blooded", Rarity.MYTHIC, new CardType[]{CardType.PLANESWALKER}, "{R}{R}");
+        this.expansionSetCode = "AVR";
+        this.subtype.add("Tibalt");
+
+        this.color.setRed(true);
+
+        this.addAbility(new EntersBattlefieldAbility(new AddCountersSourceEffect(CounterType.LOYALTY.createInstance(2)), ""));
+
+        // +1: Draw a card, then discard a card at random.
+        this.addAbility(new LoyaltyAbility(new TibaltTheFiendBloodedFirstEffect(), 1));
+        // -4: Tibalt, the Fiend-Blooded deals damage equal to the number of cards in target player's hand to that player.
+        LoyaltyAbility ability = new LoyaltyAbility(new DamageTargetEffect(new CardsInTargetHandCount()), -4);
+        ability.addTarget(new TargetPlayer());
+        this.addAbility(ability);
+        // -6: Gain control of all creatures until end of turn. Untap them. They gain haste until end of turn.
+        this.addAbility(new LoyaltyAbility(new TibaltTheFiendBloodedThirdEffect(), -6));
+    }
+
+    public TibaltTheFiendBlooded(final TibaltTheFiendBlooded card) {
+        super(card);
+    }
+
+    @Override
+    public TibaltTheFiendBlooded copy() {
+        return new TibaltTheFiendBlooded(this);
+    }
+}
+
+class TibaltTheFiendBloodedFirstEffect extends OneShotEffect<TibaltTheFiendBloodedFirstEffect> {
+
+    public TibaltTheFiendBloodedFirstEffect() {
+        super(Outcome.Benefit);
+        this.staticText = "Draw a card, then discard a card at random";
+    }
+
+    public TibaltTheFiendBloodedFirstEffect(final TibaltTheFiendBloodedFirstEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public TibaltTheFiendBloodedFirstEffect copy() {
+        return new TibaltTheFiendBloodedFirstEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Player player = game.getPlayer(source.getControllerId());
+        if (player != null) {
+            player.drawCards(1, game);
+            Card card = player.getHand().getRandom(game);
+            if (card != null) {
+                player.discard(card, source, game);
+            }
+            return true;
+        }
+        return false;
+    }
+}
+
+class TibaltTheFiendBloodedThirdEffect extends OneShotEffect<TibaltTheFiendBloodedThirdEffect> {
+
+    public TibaltTheFiendBloodedThirdEffect() {
+        super(Outcome.GainControl);
+        this.staticText = "Gain control of all creatures until end of turn. Untap them. They gain haste until end of turn";
+    }
+
+    public TibaltTheFiendBloodedThirdEffect(final TibaltTheFiendBloodedThirdEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public TibaltTheFiendBloodedThirdEffect copy() {
+        return new TibaltTheFiendBloodedThirdEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        List<Permanent> permanents = game.getBattlefield().getActivePermanents(new FilterCreaturePermanent(), source.getControllerId(), source.getId(), game);
+        for (Permanent permanent : permanents) {
+            permanent.untap(game);
+
+            ContinuousEffect effect = new TibaltTheFiendBloodedControlEffect(source.getControllerId());
+            effect.setTargetPointer(new FixedTarget(permanent.getId()));
+            game.addEffect(effect, source);
+
+            effect = new GainAbilityTargetEffect(HasteAbility.getInstance(), Duration.EndOfTurn);
+            effect.setTargetPointer(new FixedTarget(permanent.getId()));
+            game.addEffect(effect, source);
+        }
+        return true;
+    }
+}
+
+class TibaltTheFiendBloodedControlEffect extends ContinuousEffectImpl<TibaltTheFiendBloodedControlEffect> {
+
+    private UUID controllerId;
+
+    public TibaltTheFiendBloodedControlEffect(UUID controllerId) {
+        super(Duration.EndOfTurn, Layer.ControlChangingEffects_2, SubLayer.NA, Outcome.GainControl);
+        this.controllerId = controllerId;
+    }
+
+    public TibaltTheFiendBloodedControlEffect(final TibaltTheFiendBloodedControlEffect effect) {
+        super(effect);
+        this.controllerId = effect.controllerId;
+    }
+
+    @Override
+    public TibaltTheFiendBloodedControlEffect copy() {
+        return new TibaltTheFiendBloodedControlEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Permanent permanent = game.getPermanent(targetPointer.getFirst(source));
+        if (permanent != null && controllerId != null) {
+            return permanent.changeControllerId(controllerId, game);
+        }
+        return false;
+    }
+}
+
+class CardsInTargetHandCount implements DynamicValue {
+
+    @Override
+    public int calculate(Game game, Ability sourceAbility) {
+        if (sourceAbility != null) {
+            Player player = game.getPlayer(sourceAbility.getFirstTarget());
+            if (player != null) {
+                return player.getHand().size();
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public DynamicValue clone() {
+        return new CardsInTargetHandCount();
+    }
+
+    @Override
+    public String getMessage() {
+        return "cards in that player's hand";
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
