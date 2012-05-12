9e45c0a91152462584ba9c4e91efea4a1cb71886
magenoxx <magenoxx@gmail>
2012-05-12 15:46:07 +0400

    [AVR] Necrobite + Regenerate effect test

---
+++ Mage.Tests/src/test/java/org/mage/test/cards/abilities/oneshot/regenerate/NecrobiteRegenerateTest.java
@@ -0,0 +1,43 @@
+package org.mage.test.cards.abilities.oneshot.regenerate;
+
+import mage.Constants;
+import mage.game.permanent.Permanent;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mage.test.serverside.base.CardTestPlayerBase;
+
+/**
+ *
+ * @author noxx
+ */
+public class NecrobiteRegenerateTest extends CardTestPlayerBase {
+
+    @Test
+    public void testRegenerateAndDeathtouch() {
+        addCard(Constants.Zone.BATTLEFIELD, playerA, "Craw Wurm");
+
+        addCard(Constants.Zone.BATTLEFIELD, playerB, "Elite Vanguard");
+        addCard(Constants.Zone.BATTLEFIELD, playerB, "Swamp", 3);
+        addCard(Constants.Zone.HAND, playerB, "Necrobite");
+
+        attack(2, playerB, "Elite Vanguard");
+        block(2, playerA, "Craw Wurm", "Elite Vanguard");
+        castSpell(2, Constants.PhaseStep.DECLARE_BLOCKERS, playerB, "Necrobite", "Elite Vanguard");
+        
+        setStopAt(2, Constants.PhaseStep.POSTCOMBAT_MAIN);
+        execute();
+        
+        assertLife(playerA, 20);
+        assertLife(playerB, 20);
+
+        // blocker dies because of deathtouch
+        assertPermanentCount(playerA, "Craw Wurm", 0);
+        assertGraveyardCount(playerA, "Craw Wurm", 1);
+
+        Permanent eliteVanguard = getPermanent("Elite Vanguard", playerB.getId());
+        Assert.assertNotNull(eliteVanguard);
+
+        // regenerate causes to tap
+        Assert.assertTrue(eliteVanguard.isTapped());
+    }
+}
