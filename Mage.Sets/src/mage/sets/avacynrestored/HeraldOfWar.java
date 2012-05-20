ab6e5f400418616974a958cc90a9f3af10ee5600
magenoxx <magenoxx@gmail>
2012-05-20 15:56:50 +0400

    [AVR] 2 cards

---
+++ Mage.Sets/src/mage/sets/avacynrestored/HeraldOfWar.java
@@ -0,0 +1,124 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.avacynrestored;
+
+import mage.Constants;
+import mage.Constants.CardType;
+import mage.Constants.Rarity;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.SpellAbility;
+import mage.abilities.common.AttacksTriggeredAbility;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.CostModificationEffectImpl;
+import mage.abilities.effects.common.counter.AddCountersSourceEffect;
+import mage.abilities.keyword.FlyingAbility;
+import mage.cards.Card;
+import mage.cards.CardImpl;
+import mage.counters.CounterType;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.util.CardUtil;
+
+import java.util.UUID;
+
+/**
+ * @author noxx
+ */
+public class HeraldOfWar extends CardImpl<HeraldOfWar> {
+
+    public HeraldOfWar(UUID ownerId) {
+        super(ownerId, 24, "Herald of War", Rarity.RARE, new CardType[]{CardType.CREATURE}, "{3}{W}{W}");
+        this.expansionSetCode = "AVR";
+        this.subtype.add("Angel");
+
+        this.color.setWhite(true);
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(3);
+
+        this.addAbility(FlyingAbility.getInstance());
+
+        // Whenever Herald of War attacks, put a +1/+1 counter on it.
+        this.addAbility(new AttacksTriggeredAbility(new AddCountersSourceEffect(CounterType.P1P1.createInstance()), false));
+
+        // Angel spells and Human spells you cast cost {1} less to cast for each +1/+1 counter on Herald of War.
+        this.addAbility(new SimpleStaticAbility(Constants.Zone.BATTLEFIELD, new HeraldOfWarCostReductionEffect()));
+    }
+
+    public HeraldOfWar(final HeraldOfWar card) {
+        super(card);
+    }
+
+    @Override
+    public HeraldOfWar copy() {
+        return new HeraldOfWar(this);
+    }
+}
+
+class HeraldOfWarCostReductionEffect extends CostModificationEffectImpl<HeraldOfWarCostReductionEffect> {
+
+    HeraldOfWarCostReductionEffect() {
+        super(Constants.Duration.WhileOnBattlefield, Constants.Outcome.Benefit);
+        staticText = "Angel spells and Human spells you cast cost {1} less to cast for each +1/+1 counter on Herald of War";
+    }
+
+    HeraldOfWarCostReductionEffect(HeraldOfWarCostReductionEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source, Ability abilityToModify) {
+        SpellAbility spellAbility = (SpellAbility) abilityToModify;
+        Permanent sourcePermanent = game.getPermanent(source.getSourceId());
+        if (sourcePermanent != null) {
+            int amount = sourcePermanent.getCounters().getCount(CounterType.P1P1);
+            if (amount > 0) {
+                CardUtil.adjustCost(spellAbility, amount);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean applies(Ability abilityToModify, Ability source, Game game) {
+        if (abilityToModify instanceof SpellAbility) {
+            Card sourceCard = game.getCard(((SpellAbility) abilityToModify).getSourceId());
+            if (sourceCard != null && abilityToModify.getControllerId().equals(source.getControllerId()) && (sourceCard.getSubtype().contains("Angel") || sourceCard.getSubtype().contains("Human"))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public HeraldOfWarCostReductionEffect copy() {
+        return new HeraldOfWarCostReductionEffect(this);
+    }
+
+}
