8ff22a673cdf08a514a4a8a2c2a4f90760d925b9
jeffwadsworth <jeffwadsworth@magefree>
2012-05-21 13:55:04 -0500

    - Added Moonlight Geist.  <AVR>

---
+++ Mage/src/mage/abilities/effects/common/PreventCombatDamageFromSourceEffect.java
@@ -0,0 +1,86 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ * 
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ * 
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ * 
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ * 
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+
+package mage.abilities.effects.common;
+
+import mage.Constants.Duration;
+import mage.abilities.Ability;
+import mage.abilities.effects.PreventionEffectImpl;
+import mage.game.Game;
+import mage.game.events.DamageEvent;
+import mage.game.events.GameEvent;
+
+/**
+ *
+ * @author jeffwadsworth
+ */
+public class PreventCombatDamageFromSourceEffect extends PreventionEffectImpl<PreventCombatDamageFromSourceEffect> {
+
+	public PreventCombatDamageFromSourceEffect(Duration duration) {
+            super(duration);
+            staticText = "Prevent all combat damage that would be dealt by {this} " + duration.toString();
+	}
+
+	public PreventCombatDamageFromSourceEffect(final PreventCombatDamageFromSourceEffect effect) {
+            super(effect);
+	}
+
+	@Override
+	public PreventCombatDamageFromSourceEffect copy() {
+            return new PreventCombatDamageFromSourceEffect(this);
+	}
+
+	@Override
+	public boolean apply(Game game, Ability source) {
+            return true;
+	}
+
+	@Override
+	public boolean replaceEvent(GameEvent event, Ability source, Game game) {
+            GameEvent preventEvent = new GameEvent(GameEvent.EventType.PREVENT_DAMAGE, source.getSourceId(), source.getFirstTarget(), source.getControllerId(), event.getAmount(), false);
+            if (!game.replaceEvent(preventEvent)) {
+                int damage = event.getAmount();
+                event.setAmount(0);
+                game.fireEvent(GameEvent.getEvent(GameEvent.EventType.PREVENTED_DAMAGE, source.getSourceId(), source.getFirstTarget(), source.getControllerId(), damage));
+                return true;
+            }
+            return false;
+        }
+
+	@Override
+	public boolean applies(GameEvent event, Ability source, Game game) {
+            if (super.applies(event, source, game)) {
+                DamageEvent damageEvent = (DamageEvent) event;
+                if (event.getSourceId().equals(source.getSourceId()) && damageEvent.isCombatDamage()) {
+                    return true;
+                }
+            }
+            return false;
+	}
+
+}
