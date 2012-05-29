64677ed4f6c12c9cd7d5fa546bf3e3621a5bb264
magenoxx <magenoxx@gmail>
2012-05-29 08:42:58 +0400

    Fixed cascade test

--- Mage.Tests/src/test/java/org/mage/test/cards/triggers/BloodbraidElfTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/triggers/BloodbraidElfTest.java
@@ -4,17 +4,20 @@ import mage.Constants;
 import org.junit.Test;
 import org.mage.test.serverside.base.CardTestPlayerBase;
 
+/**
+ * @author noxx, Loki
+ */
 public class BloodbraidElfTest extends CardTestPlayerBase {
     @Test
     public void testCascade() {
         addCard(Constants.Zone.HAND, playerA, "Bloodbraid Elf");
-        addCard(Constants.Zone.HAND, playerA, "Unsummon");
+        addCard(Constants.Zone.HAND, playerA, "Terminus");
         addCard(Constants.Zone.BATTLEFIELD, playerA, "Goblin Wardriver", 1);
-        addCard(Constants.Zone.BATTLEFIELD, playerA, "Mountain", 2);
-        addCard(Constants.Zone.BATTLEFIELD, playerA, "Forest", 2);
-        addCard(Constants.Zone.BATTLEFIELD, playerA, "Island", 1);
+        addCard(Constants.Zone.BATTLEFIELD, playerA, "Mountain", 6);
+        addCard(Constants.Zone.BATTLEFIELD, playerA, "Forest", 6);
+        addCard(Constants.Zone.BATTLEFIELD, playerA, "Plains", 6);
 
-        castSpell(1, Constants.PhaseStep.PRECOMBAT_MAIN, playerA, "Unsummon", "Goblin Wardriver");
+        castSpell(1, Constants.PhaseStep.PRECOMBAT_MAIN, playerA, "Terminus");
 
         castSpell(1, Constants.PhaseStep.POSTCOMBAT_MAIN, playerA, "Bloodbraid Elf");
 
