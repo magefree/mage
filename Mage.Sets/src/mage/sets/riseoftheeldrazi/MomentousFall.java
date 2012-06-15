1abe05dfd4aaa7a6d70a23d0423a394fb84a8dbb
jeffwadsworth <jeffwadsworth@magefree>
2012-06-15 10:03:23 -0500

    - Added Momentous Fall.  <ROE>

---
+++ Mage.Sets/src/mage/sets/riseoftheeldrazi/MomentousFall.java
@@ -0,0 +1,109 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.riseoftheeldrazi;
+
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Outcome;
+import mage.Constants.Rarity;
+import mage.abilities.Ability;
+import mage.abilities.costs.Cost;
+import mage.abilities.costs.common.SacrificeTargetCost;
+import mage.abilities.effects.OneShotEffect;
+import mage.cards.CardImpl;
+import mage.game.Game;
+import mage.players.Player;
+import mage.target.common.TargetControlledCreaturePermanent;
+
+/**
+ *
+ * @author jeffwadsworth
+ */
+public class MomentousFall extends CardImpl<MomentousFall> {
+
+    public MomentousFall(UUID ownerId) {
+        super(ownerId, 197, "Momentous Fall", Rarity.RARE, new CardType[]{CardType.INSTANT}, "{2}{G}{G}");
+        this.expansionSetCode = "ROE";
+
+        this.color.setGreen(true);
+
+        // As an additional cost to cast Momentous Fall, sacrifice a creature.
+        this.getSpellAbility().addCost(new SacrificeTargetCost(new TargetControlledCreaturePermanent()));
+        
+        // You draw cards equal to the sacrificed creature's power, then you gain life equal to its toughness.
+        this.getSpellAbility().addEffect(new MomentousFallEffect());
+    }
+
+    public MomentousFall(final MomentousFall card) {
+        super(card);
+    }
+
+    @Override
+    public MomentousFall copy() {
+        return new MomentousFall(this);
+    }
+}
+
+class MomentousFallEffect extends OneShotEffect<MomentousFallEffect> {
+    
+    public MomentousFallEffect() {
+        super(Outcome.GainLife);
+        staticText = "You draw cards equal to the sacrificed creature's power, then you gain life equal to its toughness";
+    }
+    
+    public MomentousFallEffect(final MomentousFallEffect effect) {
+        super(effect);
+    }
+    
+    @Override
+    public boolean apply(Game game, Ability source) {
+        int power = 0;
+        int toughness = 0;
+        Player player = game.getPlayer(source.getControllerId());
+        
+        for (Cost cost: source.getCosts()) {
+            if (cost instanceof SacrificeTargetCost && ((SacrificeTargetCost)cost).getPermanents().size() > 0) {
+                power = ((SacrificeTargetCost)cost).getPermanents().get(0).getPower().getValue();
+                toughness = ((SacrificeTargetCost)cost).getPermanents().get(0).getToughness().getValue();
+                break;
+            }
+        }
+        if (power > 0) {
+            player.drawCards(power, game);
+        }
+        if (toughness > 0) {
+            player.gainLife(toughness, game);
+        }
+        return true;
+    }
+    
+    @Override
+    public MomentousFallEffect copy() {
+        return new MomentousFallEffect(this);
+    }
+}
