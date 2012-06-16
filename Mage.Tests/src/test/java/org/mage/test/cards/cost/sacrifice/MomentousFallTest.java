972a26a6bc4e22041e26aae02e6fdb23415ed00c
magenoxx <magenoxx@gmail>
2012-06-16 10:37:25 +0400

    Meomentous Fall tests. Passed.

---
+++ Mage.Tests/src/test/java/org/mage/test/cards/cost/sacrifice/MomentousFallTest.java
@@ -0,0 +1,48 @@
+package org.mage.test.cards.cost.sacrifice;
+
+import mage.Constants;
+import org.junit.Test;
+import org.mage.test.serverside.base.CardTestPlayerBase;
+
+/**
+ *
+ * @author noxx
+ */
+public class MomentousFallTest extends CardTestPlayerBase {
+
+    @Test
+    public void testSacrificeCostAndLKI() {
+        addCard(Constants.Zone.BATTLEFIELD, playerA, "Forest", 4);
+        addCard(Constants.Zone.HAND, playerA, "Momentous Fall");
+        addCard(Constants.Zone.BATTLEFIELD, playerA, "Geralf's Messenger", 1);
+        addCard(Constants.Zone.BATTLEFIELD, playerA, "Glorious Anthem", 1);
+
+        castSpell(1, Constants.PhaseStep.PRECOMBAT_MAIN, playerA, "Momentous Fall");
+
+        setStopAt(1, Constants.PhaseStep.BEGIN_COMBAT);
+        execute();
+        
+        assertPermanentCount(playerA, "Geralf's Messenger", 1);
+        assertPowerToughness(playerA, "Geralf's Messenger", 5, 4); // +1/+1 counter + Anthem effect
+        assertHandCount(playerA, 4); // +4 cards
+        assertLife(playerA, 23); // +3 life
+    }
+
+    @Test
+    public void testSacrificeCostForProGreen() {
+        addCard(Constants.Zone.BATTLEFIELD, playerA, "Forest", 4);
+        addCard(Constants.Zone.HAND, playerA, "Momentous Fall");
+        addCard(Constants.Zone.BATTLEFIELD, playerA, "Mirran Crusader");
+
+        castSpell(1, Constants.PhaseStep.PRECOMBAT_MAIN, playerA, "Momentous Fall");
+
+        setStopAt(1, Constants.PhaseStep.BEGIN_COMBAT);
+        execute();
+
+        assertPermanentCount(playerA, "Mirran Crusader", 0);
+        assertHandCount(playerA, 2); // +2 cards
+        assertLife(playerA, 22); // +2 life
+    }
+
+    
+}
