043fdc20b848c4b6b88d73b7b4a7c56883675715
jeffwadsworth <jeffwadsworth@magefree>
2012-06-23 20:45:34 -0500

    - Added Shared Discovery.  <ROE>

---
+++ Mage.Sets/src/mage/sets/riseoftheeldrazi/SharedDiscovery.java
@@ -0,0 +1,72 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.riseoftheeldrazi;
+
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Rarity;
+import mage.abilities.costs.common.TapTargetCost;
+import mage.abilities.effects.common.DrawCardControllerEffect;
+import mage.cards.CardImpl;
+import mage.filter.common.FilterControlledCreaturePermanent;
+import mage.target.common.TargetControlledCreaturePermanent;
+
+/**
+ *
+ * @author jeffwadsworth
+ */
+public class SharedDiscovery extends CardImpl<SharedDiscovery> {
+    
+    private static final FilterControlledCreaturePermanent filter = new FilterControlledCreaturePermanent("untapped creatures you control");
+    
+    static {
+        filter.setTapped(false);
+        filter.setUseTapped(true);
+    }
+
+    public SharedDiscovery(UUID ownerId) {
+        super(ownerId, 87, "Shared Discovery", Rarity.COMMON, new CardType[]{CardType.SORCERY}, "{U}");
+        this.expansionSetCode = "ROE";
+
+        this.color.setBlue(true);
+
+        // As an additional cost to cast Shared Discovery, tap four untapped creatures you control.
+        // Draw three cards.
+        this.getSpellAbility().addEffect(new DrawCardControllerEffect(3));
+        this.getSpellAbility().addCost(new TapTargetCost(new TargetControlledCreaturePermanent(4, 4, filter, true)));
+    }
+
+    public SharedDiscovery(final SharedDiscovery card) {
+        super(card);
+    }
+
+    @Override
+    public SharedDiscovery copy() {
+        return new SharedDiscovery(this);
+    }
+}
