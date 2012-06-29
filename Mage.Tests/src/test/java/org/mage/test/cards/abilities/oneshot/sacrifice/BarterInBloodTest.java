10eeea50bc19480e8fdf86174381c819b6473d4a
magenoxx <magenoxx@gmail>
2012-06-29 20:30:26 +0400

    Failing test for Barter In Blood cast against AI

---
+++ Mage.Tests/src/test/java/org/mage/test/cards/abilities/oneshot/sacrifice/BarterInBloodTest.java
@@ -0,0 +1,30 @@
+package org.mage.test.cards.abilities.oneshot.sacrifice;
+
+import mage.Constants;
+import org.junit.Test;
+import org.mage.test.serverside.base.CardTestPlayerBase;
+
+/**
+ * @author noxx
+ */
+public class BarterInBloodTest extends CardTestPlayerBase {
+
+    /**
+     * Checks that both creatures will be sacrificed
+     */
+    @Test
+    public void testSacrifice() {
+        addCard(Constants.Zone.BATTLEFIELD, playerA, "Swamp", 4);
+        addCard(Constants.Zone.HAND, playerA, "Barter in Blood");
+
+        addCard(Constants.Zone.BATTLEFIELD, playerB, "Flowering Lumberknot");
+        addCard(Constants.Zone.BATTLEFIELD, playerB, "Moorland Inquisitor");
+
+        castSpell(1, Constants.PhaseStep.PRECOMBAT_MAIN, playerA, "Barter in Blood");
+
+        setStopAt(1, Constants.PhaseStep.BEGIN_COMBAT);
+        execute();
+
+        assertPermanentCount(playerB, 0);
+    }
+}
