3ddff4a113cb3c3311577871967f38a7ef723dae
magenoxx <magenoxx@gmail>
2012-07-08 07:21:26 +0400

    Added M13 option for deck generator

--- Mage.Client/src/main/java/mage/client/util/sets/ConstructedFormats.java
+++ Mage.Client/src/main/java/mage/client/util/sets/ConstructedFormats.java
@@ -16,7 +16,7 @@ import java.util.List;
  */
 public class ConstructedFormats {
 
-    private static final String[] constructedFormats = {"ISD\\DKA\\AVR", "Standard", "Extended", "Modern", "All"};
+    private static final String[] constructedFormats = {"M13", "ISD\\DKA\\AVR", "Standard", "Extended", "Modern", "All"};
 
     private ConstructedFormats() {
     }
@@ -30,6 +30,9 @@ public class ConstructedFormats {
     }
 
     public static List<String> getSetsByFormat(String format) {
+        if (format.equals("M13")) {
+            return innistradBlock;
+        }
         if (format.equals("ISD\\DKA\\AVR")) {
             return innistradBlock;
         }
@@ -48,6 +51,9 @@ public class ConstructedFormats {
     private static void buildLists() {
         for (String setCode : CardsStorage.getSetCodes()) {
             ExpansionSet set = Sets.findSet(setCode);
+            if (set.getReleaseDate().after(m13Date)) {
+                m13.add(set.getCode());
+            }
             if (set.getReleaseDate().after(innistradBlockDate)) {
                 innistradBlock.add(set.getCode());
             }
@@ -62,6 +68,9 @@ public class ConstructedFormats {
             }
         }
     }
+
+    private static final List<String> m13= new ArrayList<String>();
+    private static final Date m13Date = new GregorianCalendar(2012, 6, 6).getTime();
 
     private static final List<String> innistradBlock = new ArrayList<String>();
     private static final Date innistradBlockDate = new GregorianCalendar(2011, 9, 29).getTime();
