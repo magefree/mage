2e25865d7ab31c8f8ac4c10964b4601d2f7421a6
jeffwadsworth <jeffwadsworth@magefree>
2012-07-10 12:06:43 -0500

    - Added Ground Seal.  <M13>

---
+++ Mage.Sets/src/mage/sets/magic2013/GroundSeal.java
@@ -0,0 +1,114 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.magic2013;
+
+import java.util.UUID;
+import mage.Constants;
+import mage.Constants.CardType;
+import mage.Constants.Rarity;
+import mage.abilities.Ability;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.ReplacementEffectImpl;
+import mage.abilities.effects.common.DrawCardControllerEffect;
+import mage.cards.Card;
+import mage.cards.CardImpl;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.stack.StackObject;
+
+/**
+ *
+ * @author jeffwadsworth
+ */
+public class GroundSeal extends CardImpl<GroundSeal> {
+
+    public GroundSeal(UUID ownerId) {
+        super(ownerId, 176, "Ground Seal", Rarity.RARE, new CardType[]{CardType.ENCHANTMENT}, "{1}{G}");
+        this.expansionSetCode = "M13";
+
+        this.color.setGreen(true);
+
+        // When Ground Seal enters the battlefield, draw a card.
+        this.addAbility(new EntersBattlefieldTriggeredAbility(new DrawCardControllerEffect(1)));
+        
+        // Cards in graveyards can't be the targets of spells or abilities.
+        this.addAbility(new SimpleStaticAbility(Constants.Zone.BATTLEFIELD, new GroundSealEffect()));
+    }
+
+    public GroundSeal(final GroundSeal card) {
+        super(card);
+    }
+
+    @Override
+    public GroundSeal copy() {
+        return new GroundSeal(this);
+    }
+}
+
+class GroundSealEffect extends ReplacementEffectImpl<GroundSealEffect> {
+
+    public GroundSealEffect() {
+        super(Constants.Duration.WhileOnBattlefield, Constants.Outcome.Benefit);
+        staticText = "Cards in graveyards can't be the targets of spells or abilities";
+    }
+
+    public GroundSealEffect(final GroundSealEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public GroundSealEffect copy() {
+        return new GroundSealEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        return true;
+    }
+
+    @Override
+    public boolean replaceEvent(GameEvent event, Ability source, Game game) {
+        return true;
+    }
+
+    @Override
+    public boolean applies(GameEvent event, Ability source, Game game) {
+        if (event.getType() == GameEvent.EventType.TARGET) {
+            Card targetCard = game.getCard(event.getTargetId());
+            StackObject stackObject = (StackObject) game.getStack().getStackObject(event.getSourceId());
+            if (targetCard != null && stackObject != null) {
+                Constants.Zone zone = game.getState().getZone(targetCard.getId());
+                if (zone != null && (zone == Constants.Zone.GRAVEYARD)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
