01f57aa06e27b9ea2fd3140d6615834ea9b6ab14
Loki <loki@magefree>
2012-07-13 20:05:27 +1200

    [M13] Jace's Phantasm

---
+++ Mage/src/mage/abilities/condition/common/CardsInOpponentGraveCondition.java
@@ -0,0 +1,34 @@
+package mage.abilities.condition.common;
+
+import mage.abilities.Ability;
+import mage.abilities.condition.Condition;
+import mage.game.Game;
+import mage.players.Player;
+
+import java.util.UUID;
+
+/**
+ * Condition for -
+ *  Any opponent has X or more cards in his or her graveyard
+ *  @author Loki
+ */
+public class CardsInOpponentGraveCondition implements Condition {
+    private int value;
+
+    public CardsInOpponentGraveCondition(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Player p = game.getPlayer(source.getControllerId());
+        if (p != null) {
+            for (UUID oppId : p.getInRange()) {
+                Player opponent = game.getPlayer(oppId);
+                if (opponent != null && opponent.getGraveyard().size() >= value)
+                    return true;
+            }
+        }
+        return false;
+    }
+}
