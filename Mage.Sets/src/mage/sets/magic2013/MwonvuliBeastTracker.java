9504004b8075027d75721d20f4e82a71af81fa54
jeffwadsworth <jeffwadsworth@magefree>
2012-07-19 16:36:26 -0500

    - Added Mwonvuli Beast Tracker.  <M13>

---
+++ Mage.Sets/src/mage/sets/magic2013/MwonvuliBeastTracker.java
@@ -0,0 +1,85 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.magic2013;
+
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Rarity;
+import mage.MageInt;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.effects.common.search.SearchLibraryPutOnLibraryEffect;
+import mage.abilities.keyword.DeathtouchAbility;
+import mage.abilities.keyword.HexproofAbility;
+import mage.abilities.keyword.ReachAbility;
+import mage.abilities.keyword.TrampleAbility;
+import mage.cards.CardImpl;
+import mage.filter.FilterCard;
+import mage.filter.predicate.Predicates;
+import mage.filter.predicate.mageobject.AbilityPredicate;
+import mage.target.common.TargetCardInLibrary;
+
+/**
+ *
+ * @author jeffwadsworth
+ */
+public class MwonvuliBeastTracker extends CardImpl<MwonvuliBeastTracker> {
+    
+    private final static FilterCard filter = new FilterCard("creature card with deathtouch, hexproof, reach, or trample in your library");
+
+    static {
+        filter.add(Predicates.or(
+        new AbilityPredicate(DeathtouchAbility.class),
+        new AbilityPredicate(HexproofAbility.class),
+        new AbilityPredicate(ReachAbility.class),
+        new AbilityPredicate(TrampleAbility.class)));
+    }
+
+    public MwonvuliBeastTracker(UUID ownerId) {
+        super(ownerId, 177, "Mwonvuli Beast Tracker", Rarity.UNCOMMON, new CardType[]{CardType.CREATURE}, "{1}{G}{G}");
+        this.expansionSetCode = "M13";
+        this.subtype.add("Human");
+        this.subtype.add("Scout");
+
+        this.color.setGreen(true);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(1);
+
+        // When Mwonvuli Beast Tracker enters the battlefield, search your library for a creature card with deathtouch, hexproof, reach, or trample and reveal it. Shuffle your library and put that card on top of it.
+        this.addAbility(new EntersBattlefieldTriggeredAbility(new SearchLibraryPutOnLibraryEffect(new TargetCardInLibrary(filter))));
+    }
+
+    public MwonvuliBeastTracker(final MwonvuliBeastTracker card) {
+        super(card);
+    }
+
+    @Override
+    public MwonvuliBeastTracker copy() {
+        return new MwonvuliBeastTracker(this);
+    }
+}
+
