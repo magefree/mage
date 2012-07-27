a5e6fd632293cdbf8408fdbb2b17dedf418fc048
jeffwadsworth <jeffwadsworth@magefree>
2012-07-27 11:59:11 -0500

    - Added Time of Heroes.  <ROE>  Love those predicates.

---
+++ Mage.Sets/src/mage/sets/riseoftheeldrazi/TimeOfHeroes.java
@@ -0,0 +1,76 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.riseoftheeldrazi;
+
+import java.util.UUID;
+import mage.Constants;
+import mage.Constants.CardType;
+import mage.Constants.Duration;
+import mage.Constants.Rarity;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.ContinuousEffect;
+import mage.cards.CardImpl;
+import mage.counters.CounterType;
+import mage.abilities.effects.common.continious.BoostAllEffect;
+import mage.filter.common.FilterCreaturePermanent;
+import mage.filter.predicate.mageobject.CounterPredicate;
+import mage.filter.predicate.permanent.ControllerPredicate;
+
+/**
+ *
+ * @author jeffwadsworth
+ */
+public class TimeOfHeroes extends CardImpl<TimeOfHeroes> {
+    
+    private static final FilterCreaturePermanent filter = new FilterCreaturePermanent("Each creature you control with a level counter");
+    
+    static {
+        filter.add(new ControllerPredicate(Constants.TargetController.YOU));
+        filter.add(new CounterPredicate(CounterType.LEVEL));
+    }
+
+    public TimeOfHeroes(UUID ownerId) {
+        super(ownerId, 49, "Time of Heroes", Rarity.UNCOMMON, new CardType[]{CardType.ENCHANTMENT}, "{1}{W}");
+        this.expansionSetCode = "ROE";
+
+        this.color.setWhite(true);
+
+        // Each creature you control with a level counter on it gets +2/+2.
+        ContinuousEffect boost = new BoostAllEffect(2, 2, Duration.WhileOnBattlefield, filter, false);
+        this.addAbility(new SimpleStaticAbility(Constants.Zone.BATTLEFIELD, boost));
+    }
+
+    public TimeOfHeroes(final TimeOfHeroes card) {
+        super(card);
+    }
+
+    @Override
+    public TimeOfHeroes copy() {
+        return new TimeOfHeroes(this);
+    }
+}
