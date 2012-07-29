df303cbd16b48d82e211e0cba028df355e12d40f
Loki <loki@magefree>
2012-07-30 11:44:18 +1200

    some more cards

--- Mage.Sets/src/mage/sets/urzaslegacy/MotherOfRunes.java
+++ Mage.Sets/src/mage/sets/urzaslegacy/MotherOfRunes.java
@@ -1,16 +1,16 @@
 /*
  *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
- * 
+ *
  *  Redistribution and use in source and binary forms, with or without modification, are
  *  permitted provided that the following conditions are met:
- * 
+ *
  *     1. Redistributions of source code must retain the above copyright notice, this list of
  *        conditions and the following disclaimer.
- * 
+ *
  *     2. Redistributions in binary form must reproduce the above copyright notice, this list
  *        of conditions and the following disclaimer in the documentation and/or other materials
  *        provided with the distribution.
- * 
+ *
  *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
  *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
@@ -20,63 +20,56 @@
  *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  *  The views and conclusions contained in the software and documentation are those of the
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
  */
+package mage.sets.urzaslegacy;
 
-package mage.abilities.effects.common.continious;
+import java.util.UUID;
 
-import mage.Constants.Duration;
+import mage.Constants;
+import mage.Constants.CardType;
+import mage.Constants.Rarity;
+import mage.MageInt;
 import mage.abilities.Ability;
-import mage.abilities.Mode;
-import mage.abilities.keyword.ProtectionAbility;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.effects.common.continious.GainProtectionFromColorTargetEffect;
+import mage.cards.CardImpl;
 import mage.choices.ChoiceColor;
-import mage.filter.FilterCard;
-import mage.filter.predicate.mageobject.ColorPredicate;
-import mage.game.Game;
-import mage.game.permanent.Permanent;
+import mage.target.common.TargetControlledCreaturePermanent;
 
 /**
  *
- * @author BetaSteward_at_googlemail.com
+ * @author Loki
  */
-public class GainProtectionFromColorTargetEffect extends GainAbilityTargetEffect {
+public class MotherOfRunes extends CardImpl<MotherOfRunes> {
 
-    FilterCard protectionFilter;
+    public MotherOfRunes(UUID ownerId) {
+        super(ownerId, 14, "Mother of Runes", Rarity.UNCOMMON, new CardType[]{CardType.CREATURE}, "{W}");
+        this.expansionSetCode = "ULG";
+        this.subtype.add("Human");
+        this.subtype.add("Cleric");
 
-    public GainProtectionFromColorTargetEffect(Duration duration) {
-        super(new ProtectionAbility(new FilterCard()), duration);
-        protectionFilter = (FilterCard)((ProtectionAbility)ability).getFilter();
+        this.color.setWhite(true);
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(1);
+
+        // {tap}: Target creature you control gains protection from the color of your choice until end of turn.
+        Ability ability = new SimpleActivatedAbility(Constants.Zone.BATTLEFIELD, new GainProtectionFromColorTargetEffect(Constants.Duration.EndOfTurn), new TapSourceCost());
+        ability.addChoice(new ChoiceColor());
+        ability.addTarget(new TargetControlledCreaturePermanent());
+        this.addAbility(ability);
     }
 
-    public GainProtectionFromColorTargetEffect(final GainProtectionFromColorTargetEffect effect) {
-        super(effect);
-        this.protectionFilter = effect.protectionFilter.copy();
-    }
-
-    @Override
-    public GainProtectionFromColorTargetEffect copy() {
-        return new GainProtectionFromColorTargetEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Permanent creature = game.getPermanent(source.getFirstTarget());
-        if (creature != null) {
-            ChoiceColor choice = (ChoiceColor) source.getChoices().get(0);
-            protectionFilter.add(new ColorPredicate(choice.getColor()));
-            protectionFilter.setMessage(choice.getChoice());
-            ((ProtectionAbility)ability).setFilter(protectionFilter);
-            creature.addAbility(ability, game);
-            return true;
-        }
-        return false;
+    public MotherOfRunes(final MotherOfRunes card) {
+        super(card);
     }
 
     @Override
-    public String getText(Mode mode) {
-        return "target creature you control gains protection from the color of your choice " + duration.toString();
+    public MotherOfRunes copy() {
+        return new MotherOfRunes(this);
     }
 }
