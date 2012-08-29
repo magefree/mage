47329e9ef933d5650d521e3a83295dc0bfdd136f
magenoxx <magenoxx@gmail.com>
2012-08-29 20:34:35 +0400

    Divine Verdict test

---
+++ Mage.Tests/src/test/java/org/mage/test/cards/targets/attacking/DivineVerdictTest.java
@@ -0,0 +1,31 @@
+package org.mage.test.cards.targets.attacking;
+
+import mage.Constants;
+import mage.Constants.PhaseStep;
+import org.junit.Test;
+import org.mage.test.serverside.base.CardTestPlayerBase;
+
+/**
+ * @author magenoxx_at_gmail.com
+ */
+public class DivineVerdictTest extends CardTestPlayerBase {
+
+    @Test
+    public void testAfterAttack() {
+        addCard(Constants.Zone.BATTLEFIELD, playerA, "Plains");
+        addCard(Constants.Zone.HAND, playerA, "Divine Verdict");
+        addCard(Constants.Zone.BATTLEFIELD, playerB, "Sejiri Merfolk");
+
+        attack(2, playerB, "Sejiri Merfolk");
+        castSpell(2, PhaseStep.POSTCOMBAT_MAIN, playerA, "Divine Verdict", "Sejiri Merfolk");
+
+        setStopAt(2, PhaseStep.END_TURN);
+        execute();
+
+        assertPermanentCount(playerB, "Sejiri Merfolk", 1);
+        assertLife(playerA, 18);
+        assertLife(playerB, 22);
+    }
+
+
+}
