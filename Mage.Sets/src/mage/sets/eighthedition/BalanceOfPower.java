d4f91d7bdfbb3445d288780ee24ec2aae08bf226
North <robyter@gmail>
2012-09-03 16:05:43 +0300

    [8ED] 5 sorcery cards

---
+++ Mage.Sets/src/mage/sets/eighthedition/BalanceOfPower.java
@@ -0,0 +1,96 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.eighthedition;
+
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Outcome;
+import mage.Constants.Rarity;
+import mage.abilities.Ability;
+import mage.abilities.effects.OneShotEffect;
+import mage.cards.CardImpl;
+import mage.game.Game;
+import mage.players.Player;
+import mage.target.common.TargetOpponent;
+
+/**
+ *
+ * @author North
+ */
+public class BalanceOfPower extends CardImpl<BalanceOfPower> {
+
+    public BalanceOfPower(UUID ownerId) {
+        super(ownerId, 62, "Balance of Power", Rarity.RARE, new CardType[]{CardType.SORCERY}, "{3}{U}{U}");
+        this.expansionSetCode = "8ED";
+
+        this.color.setBlue(true);
+
+        // If target opponent has more cards in hand than you, draw cards equal to the difference.
+        this.getSpellAbility().addEffect(new BalanceOfPowerEffect());
+        this.getSpellAbility().addTarget(new TargetOpponent());
+    }
+
+    public BalanceOfPower(final BalanceOfPower card) {
+        super(card);
+    }
+
+    @Override
+    public BalanceOfPower copy() {
+        return new BalanceOfPower(this);
+    }
+}
+
+class BalanceOfPowerEffect extends OneShotEffect<BalanceOfPowerEffect> {
+
+    public BalanceOfPowerEffect() {
+        super(Outcome.DrawCard);
+        this.staticText = "If target opponent has more cards in hand than you, draw cards equal to the difference";
+    }
+
+    public BalanceOfPowerEffect(final BalanceOfPowerEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public BalanceOfPowerEffect copy() {
+        return new BalanceOfPowerEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Player player = game.getPlayer(source.getControllerId());
+        Player opponent = game.getPlayer(source.getFirstTarget());
+
+        if (opponent != null || player != null && opponent.getHand().size() > player.getHand().size()) {
+            player.drawCards(opponent.getHand().size() - player.getHand().size(), game);
+            return true;
+        }
+
+        return false;
+    }
+}
