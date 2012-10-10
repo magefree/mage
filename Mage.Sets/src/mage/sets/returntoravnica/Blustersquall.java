28e4185965fbc5c848f29aa9d55899fda6cad447
LevelX2 <ludwig.hirth@online.de>
2012-10-10 20:47:00 +0200

    [RTR] 2 cards, OverloadAbility

---
+++ Mage.Sets/src/mage/sets/returntoravnica/Blustersquall.java
@@ -0,0 +1,112 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.returntoravnica;
+
+import java.util.List;
+import java.util.UUID;
+import mage.Constants;
+import mage.Constants.CardType;
+import mage.Constants.Rarity;
+import mage.abilities.Ability;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.TapTargetEffect;
+import mage.abilities.keyword.OverloadAbility;
+import mage.cards.CardImpl;
+import mage.filter.common.FilterCreaturePermanent;
+import mage.filter.predicate.permanent.ControllerPredicate;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.target.common.TargetCreaturePermanent;
+
+
+/**
+ *
+ * @author LevelX2
+ */
+public class Blustersquall extends CardImpl<Blustersquall> {
+
+    private static final FilterCreaturePermanent filter = new FilterCreaturePermanent("creature you don't control");
+
+    static {
+        filter.add(new ControllerPredicate(Constants.TargetController.NOT_YOU));
+    }
+
+    public Blustersquall(UUID ownerId) {
+        super(ownerId, 30, "Blustersquall", Rarity.UNCOMMON, new CardType[]{CardType.INSTANT}, "{U}");
+        this.expansionSetCode = "RTR";
+
+        this.color.setBlue(true);
+
+        // Tap target creature you don't control.
+        this.getSpellAbility().addTarget(new TargetCreaturePermanent(filter));
+        this.getSpellAbility().addEffect(new TapTargetEffect());
+
+        // Overload {3}{U} (You may cast this spell for its overload cost. If you do, change its text by replacing all instances of "target" with "each.")
+        this.addAbility(new OverloadAbility(this, new BlustersqallTapAllEffect(filter), new ManaCostsImpl("{3}{U}")));
+
+    }
+
+    public Blustersquall(final Blustersquall card) {
+        super(card);
+    }
+
+    @Override
+    public Blustersquall copy() {
+        return new Blustersquall(this);
+    }
+}
+
+class BlustersqallTapAllEffect extends OneShotEffect<BlustersqallTapAllEffect> {
+
+    protected FilterCreaturePermanent filter;
+
+    public BlustersqallTapAllEffect(FilterCreaturePermanent filter) {
+        super(Constants.Outcome.Tap);
+        this.filter = filter;
+        staticText = "Tap each creature you don't control";
+    }
+
+    public BlustersqallTapAllEffect(final BlustersqallTapAllEffect effect) {
+        super(effect);
+        this.filter = effect.filter;
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        for (Permanent creature : game.getBattlefield().getActivePermanents(filter, source.getControllerId(), source.getSourceId(), game)) {
+            creature.tap(game);
+        }
+        return true;
+    }
+
+    @Override
+    public BlustersqallTapAllEffect copy() {
+        return new BlustersqallTapAllEffect(this);
+    }
+}
---
+++ Mage/src/mage/abilities/keyword/OverloadAbility.java
@@ -0,0 +1,85 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+
+package mage.abilities.keyword;
+
+import mage.abilities.SpellAbility;
+import mage.abilities.costs.mana.ManaCosts;
+import mage.abilities.effects.Effect;
+import mage.cards.Card;
+
+
+/**
+ *
+ *
+ */
+
+/**
+ * 702.94. Overload
+ *
+ * 702.94a. Overload is a keyword that represents two static abilities: one that
+ * functions from any zone in which the spell with overload can be cast and
+ * another that functions while the card is on the stack. Overload [cost] means
+ * "You may choose to pay [cost] rather than pay this spell's mana cost" and
+ * "If you chose to pay this spell's overload cost, change its text by replacing
+ * all instances of the word 'target' with the word 'each.'" Using the overload
+ * ability follows the rules for paying alternative costs in rules 601.2b and 601.2e-g.
+ *
+ * 702.94b. If a player chooses to pay the overload cost of a spell, that spell
+ * won't require any targets. It may affect objects that couldn't be chosen as
+ * legal targets if the spell were cast without its overload cost being paid.
+ *
+ * 702.94c. Overload's second ability creates a text-changing effect. See rule
+ *          612, "Text-Changing Effects."
+ *
+ * @author LevelX2
+ *
+ */
+
+public class OverloadAbility extends SpellAbility {
+
+    public OverloadAbility(Card card,Effect effect, ManaCosts costs) {
+        super(costs, card.getName() + " with overload");
+        this.addEffect(effect);
+    }
+
+    public OverloadAbility(final OverloadAbility ability) {
+        super(ability);
+    }
+
+    @Override
+    public OverloadAbility copy() {
+        return new OverloadAbility(this);
+    }
+
+    @Override
+    public String getRule() {
+        return "Overload " + getManaCostsToPay().getText()+ " <i>(You may cast this spell for its overload cost. If you do, change its text by replacing all instances of \"target\" with \"each.\")</i>";
+    }
+
+}
