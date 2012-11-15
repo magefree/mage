f017ab903c7d0251fc784c313190bee619f3d652
Loki <loki@magefree>
2012-11-15 22:47:15 +1300

    version bump to 0.10.0

--- Mage.Client/src/main/java/mage/client/MageFrame.java
+++ Mage.Client/src/main/java/mage/client/MageFrame.java
@@ -124,7 +124,7 @@ public class MageFrame extends javax.swing.JFrame implements MageClient {
     private static Preferences prefs = Preferences.userNodeForPackage(MageFrame.class);
     private JLabel title;
     private Rectangle titleRectangle;
-    private final static MageVersion version = new MageVersion(0, 9, 2, "dev");
+    private final static MageVersion version = new MageVersion(0, 10, 0, "");
     private UUID clientId;
     private static MagePane activeFrame;
     private static boolean liteMode = false;
--- Mage.Server/src/main/java/mage/server/Main.java
+++ Mage.Server/src/main/java/mage/server/Main.java
@@ -72,7 +72,7 @@ public class Main {
     private final static String testModeArg = "-testMode=";
     private final static String adminPasswordArg = "-adminPassword=";
     private final static String pluginFolder = "plugins";
-    private static MageVersion version = new MageVersion(0, 9, 2, "dev");
+    private static MageVersion version = new MageVersion(0, 10, 0, "");
 
     public static PluginClassLoader classLoader = new PluginClassLoader();
     public static TransporterServer server;
