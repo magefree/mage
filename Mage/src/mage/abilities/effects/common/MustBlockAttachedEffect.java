8af27d10b5ad5744da97f40c1cd4879baa594643
LevelX2 <ludwig.hirth@online.de>
2012-11-23 00:49:46 +0100

    New MustBlockAttachedEffect.

---
+++ Mage/src/mage/abilities/effects/common/MustBlockAttachedEffect.java
@@ -0,0 +1,90 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+
+package mage.abilities.effects.common;
+
+import java.util.UUID;
+import mage.Constants.AttachmentType;
+import mage.Constants.Duration;
+import mage.abilities.Ability;
+import mage.abilities.effects.RequirementEffect;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+
+/**
+ *
+ * @author LevelX2
+ */
+public class MustBlockAttachedEffect extends RequirementEffect<MustBlockAttachedEffect> {
+
+    protected AttachmentType attachmentType;
+
+    public MustBlockAttachedEffect(AttachmentType attachmentType) {
+        this(Duration.WhileOnBattlefield, attachmentType);
+    }
+
+    public MustBlockAttachedEffect(Duration duration, AttachmentType attachmentType) {
+        super(duration);
+        this.attachmentType = attachmentType;
+        staticText = "All creatures able to block " + (attachmentType.equals(AttachmentType.AURA) ? "enchanted":"equipped") + " creature do so";
+    }
+
+    public MustBlockAttachedEffect(final MustBlockAttachedEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public boolean applies(Permanent permanent, Ability source, Game game) {
+        return true;
+    }
+
+    @Override
+    public boolean mustAttack(Game game) {
+        return false;
+    }
+
+    @Override
+    public boolean mustBlock(Game game) {
+        return true;
+    }
+
+    @Override
+    public UUID mustBlockAttacker(Ability source, Game game) {
+        Permanent attachment = game.getPermanent(source.getSourceId());
+        if (attachment != null && attachment.getAttachedTo() != null) {
+            return attachment.getAttachedTo() ;
+        }
+        return null;
+    }
+
+    @Override
+    public MustBlockAttachedEffect copy() {
+        return new MustBlockAttachedEffect(this);
+    }
+
+}
