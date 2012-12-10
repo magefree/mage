2fb81289bfa023e580034eb6a9c9d64d97fdbb6d
Plopman <Plopman@hotmail.ch>
2012-12-10 13:48:47 +0100

    Add 7 cards with Retrace Ability

---
+++ Mage.Sets/src/mage/sets/eventide/SyphonLife.java
@@ -0,0 +1,70 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.eventide;
+
+import java.util.UUID;
+import mage.Constants;
+import mage.Constants.CardType;
+import mage.Constants.Rarity;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.DamageTargetEffect;
+import mage.abilities.effects.common.GainLifeEffect;
+import mage.abilities.effects.common.LoseLifeTargetEffect;
+import mage.abilities.keyword.RetraceAbility;
+import mage.cards.CardImpl;
+import mage.target.TargetPlayer;
+
+/**
+ *
+ * @author Plopman
+ */
+public class SyphonLife extends CardImpl<SyphonLife> {
+
+    public SyphonLife(UUID ownerId) {
+        super(ownerId, 46, "Syphon Life", Rarity.UNCOMMON, new CardType[]{CardType.SORCERY}, "{1}{B}{B}");
+        this.expansionSetCode = "EVE";
+
+        this.color.setBlack(true);
+
+        // Target player loses 2 life and you gain 2 life.
+        this.getSpellAbility().addEffect(new LoseLifeTargetEffect(2));
+        this.getSpellAbility().addTarget(new TargetPlayer());
+        this.getSpellAbility().addEffect(new GainLifeEffect(2));
+        // Retrace
+        this.addAbility(new RetraceAbility(new ManaCostsImpl("{1}{B}{B}"), Constants.TimingRule.SORCERY));
+    }
+
+    public SyphonLife(final SyphonLife card) {
+        super(card);
+    }
+
+    @Override
+    public SyphonLife copy() {
+        return new SyphonLife(this);
+    }
+}
