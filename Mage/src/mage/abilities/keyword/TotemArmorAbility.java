6e054671b3b2ecaa569d0b048fb5964637b00311
LevelX2 <ludwig.hirth@online.de>
2012-12-20 08:48:56 +0100

    Minor changes to TotemArmorAbility.

--- Mage/src/mage/abilities/keyword/TotemArmorAbility.java
+++ Mage/src/mage/abilities/keyword/TotemArmorAbility.java
@@ -1,3 +1,31 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+
 package mage.abilities.keyword;
 
 import mage.Constants;
@@ -7,6 +35,17 @@ import mage.abilities.effects.ReplacementEffectImpl;
 import mage.game.Game;
 import mage.game.events.GameEvent;
 import mage.game.permanent.Permanent;
+
+/*
+ * 702.87. Totem Armor
+ *
+ * 702.87a Totem armor is a static ability that appears on some Auras. "Totem armor" means "
+ * If enchanted permanent would be destroyed, instead remove all damage marked on it and
+ * destroy this Aura."
+ *
+ * @author Loki
+ *
+ */
 
 public class TotemArmorAbility extends SimpleStaticAbility {
     public TotemArmorAbility() {
@@ -24,7 +63,7 @@ public class TotemArmorAbility extends SimpleStaticAbility {
 
     @Override
     public String getRule() {
-        return "Totem Armor";
+        return "Totem Armor <i>(If enchanted creature would be destroyed, instead remove all damage from it and destroy this Aura.)</i>";
     }
 }
 
