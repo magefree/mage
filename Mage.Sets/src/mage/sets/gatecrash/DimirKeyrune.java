94fd2429d1cc6f165e7f5f9da56081033c4a6a92
LevelX2 <ludwig.hirth@online.de>
2013-01-02 13:33:35 +0100

    [GTC] Gateway Shadow, Dimir Keyrune, Gruul Keyrune

---
+++ Mage.Sets/src/mage/sets/gatecrash/DimirKeyrune.java
@@ -0,0 +1,85 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.gatecrash;
+
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Duration;
+import mage.Constants.Rarity;
+import mage.Constants.Zone;
+import mage.MageInt;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.continious.BecomesCreatureSourceEffect;
+import mage.abilities.keyword.UnblockableAbility;
+import mage.abilities.mana.BlackManaAbility;
+import mage.abilities.mana.BlueManaAbility;
+import mage.cards.CardImpl;
+import mage.game.permanent.token.Token;
+
+/**
+ *
+ * @author LevelX2
+ */
+public class DimirKeyrune extends CardImpl<DimirKeyrune> {
+
+    public DimirKeyrune(UUID ownerId) {
+        super(ownerId, 228, "Dimir Keyrune", Rarity.UNCOMMON, new CardType[]{CardType.ARTIFACT}, "{3}");
+        this.expansionSetCode = "GTC";
+
+        // {T}: Add {U} or {B} to your mana pool.
+        this.addAbility(new BlueManaAbility());
+        this.addAbility(new BlackManaAbility());
+
+        // {U}{B}: Dimir Keyrune becomes a 2/2 blue and black Horror and is unblockable this turn
+        this.addAbility(new SimpleActivatedAbility(Zone.BATTLEFIELD, new BecomesCreatureSourceEffect(new DimirKeyruneToken(), "", Duration.EndOfTurn), new ManaCostsImpl("{U}{B}")));
+    }
+
+    public DimirKeyrune(final DimirKeyrune card) {
+        super(card);
+    }
+
+    @Override
+    public DimirKeyrune copy() {
+        return new DimirKeyrune(this);
+    }
+
+    private class DimirKeyruneToken extends Token {
+        DimirKeyruneToken() {
+            super("", "a 2/2 blue and black Horror until end of turn and is unblockable this turn");
+            cardType.add(CardType.ARTIFACT);
+            cardType.add(CardType.CREATURE);
+            color.setBlue(true);
+            color.setBlack(true);
+            subtype.add("Horror");
+            power = new MageInt(2);
+            toughness = new MageInt(2);
+            this.addAbility(new UnblockableAbility());
+        }
+    }
+}
---
+++ Mage.Sets/src/mage/sets/gatecrash/GruulKeyrune.java
@@ -0,0 +1,85 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.gatecrash;
+
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Duration;
+import mage.Constants.Rarity;
+import mage.Constants.Zone;
+import mage.MageInt;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.continious.BecomesCreatureSourceEffect;
+import mage.abilities.keyword.TrampleAbility;
+import mage.abilities.mana.GreenManaAbility;
+import mage.abilities.mana.RedManaAbility;
+import mage.cards.CardImpl;
+import mage.game.permanent.token.Token;
+
+/**
+ *
+ * @author LevelX2
+ */
+public class GruulKeyrune extends CardImpl<GruulKeyrune> {
+
+    public GruulKeyrune(UUID ownerId) {
+        super(ownerId, 230, "Gruul Keyrune", Rarity.UNCOMMON, new CardType[]{CardType.ARTIFACT}, "{3}");
+        this.expansionSetCode = "GTC";
+
+        // {T}: Add {R} or {G} to your mana pool.
+        this.addAbility(new RedManaAbility());
+        this.addAbility(new GreenManaAbility());
+
+        // {R}{G}: Gruul Keyrune becomes a 3/2 red and green Beast artifact creature with trample until end of turn.
+        this.addAbility(new SimpleActivatedAbility(Zone.BATTLEFIELD, new BecomesCreatureSourceEffect(new GruulKeyruneToken(), "", Duration.EndOfTurn), new ManaCostsImpl("{R}{G}")));
+    }
+
+    public GruulKeyrune(final GruulKeyrune card) {
+        super(card);
+    }
+
+    @Override
+    public GruulKeyrune copy() {
+        return new GruulKeyrune(this);
+    }
+
+    private class GruulKeyruneToken extends Token {
+        GruulKeyruneToken() {
+            super("", "a 3/2 red and green Beast artifact creature with trample");
+            cardType.add(CardType.ARTIFACT);
+            cardType.add(CardType.CREATURE);
+            color.setRed(true);
+            color.setGreen(true);
+            subtype.add("Beast");
+            power = new MageInt(3);
+            toughness = new MageInt(2);
+            this.addAbility(TrampleAbility.getInstance());
+        }
+    }
+}
