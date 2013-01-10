bbd3526780a147e55615bc470cbaa2f9872ae3fc
magenoxx <magenoxx@gmail.com>
2013-01-10 21:40:48 +0400

    Pumped version for Mage.Console

--- Mage.Server.Console/src/main/java/mage/server/console/ConsoleFrame.java
+++ Mage.Server.Console/src/main/java/mage/server/console/ConsoleFrame.java
@@ -57,7 +57,7 @@ public class ConsoleFrame extends javax.swing.JFrame implements MageClient {
     private static Session session;
     private ConnectDialog connectDialog;
     private static Preferences prefs = Preferences.userNodeForPackage(ConsoleFrame.class);
-    private final static MageVersion version = new MageVersion(0, 10, 0, "dev");
+    private final static MageVersion version = new MageVersion(0, 10, 1, "");
 
     /**
      * @return the session
