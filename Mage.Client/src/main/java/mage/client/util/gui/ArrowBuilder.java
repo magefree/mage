9f241a7041bff7c0b0640576de087e5a6d97367f
magenoxx <magenoxx@gmail.com>
2013-01-15 17:30:36 +0400

    Fixed Issue#109: Unwanted blue rectangle in UI

--- Mage.Client/src/main/java/mage/client/util/gui/ArrowBuilder.java
+++ Mage.Client/src/main/java/mage/client/util/gui/ArrowBuilder.java
@@ -65,7 +65,6 @@ public class ArrowBuilder {
             arrowPanel.setVisible(true);
             arrowPanel.setOpaque(false);
             arrowPanel.setLayout(null);
-            arrowPanel.setBorder(BorderFactory.createLineBorder(Color.blue));
             arrowPanel.setSize(currentWidth, currentHeight);
             arrowPanels.put(gameId, arrowPanel);
             getArrowsManagerPanel().add(arrowPanel);
