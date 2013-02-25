a5598c3c4b0fdae25d0ebdf9fac8db7c4871f369
magenoxx <magenoxx@gmail>
2013-02-25 19:47:41 +0400

    JVM tuning for scripts

--- Mage.Client/release/startClient.sh
+++ Mage.Client/release/startClient.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -jar ./lib/mage-client-${project.version}.jar -Xms256M -Xmx1024M -XX:MaxPermSize=256M &
+java -jar ./lib/mage-client-${project.version}.jar -Xms256M -Xmx1024M -XX:MaxPermSize=256M -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled &
