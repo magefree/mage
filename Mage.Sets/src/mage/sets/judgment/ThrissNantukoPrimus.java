f50dcdb8da2f3b5ef6deb039c8247468e596a473
North <robyter@gmail>
2013-03-02 15:34:24 +0200

    [minor] formatting

--- Mage.Sets/src/mage/sets/judgment/ThrissNantukoPrimus.java
+++ Mage.Sets/src/mage/sets/judgment/ThrissNantukoPrimus.java
@@ -1,1 +1,75 @@
-/* *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved. * *  Redistribution and use in source and binary forms, with or without modification, are *  permitted provided that the following conditions are met: * *     1. Redistributions of source code must retain the above copyright notice, this list of *        conditions and the following disclaimer. * *     2. Redistributions in binary form must reproduce the above copyright notice, this list *        of conditions and the following disclaimer in the documentation and/or other materials *        provided with the distribution. * *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. * *  The views and conclusions contained in the software and documentation are those of the *  authors and should not be interpreted as representing official policies, either expressed *  or implied, of BetaSteward_at_googlemail.com. */package mage.sets.judgment;import java.util.UUID;import mage.Constants.CardType;import mage.Constants.Duration;import mage.Constants.Rarity;import mage.Constants.Zone;import mage.MageInt;import mage.abilities.common.SimpleActivatedAbility;import mage.abilities.costs.common.TapSourceCost;import mage.abilities.costs.mana.ManaCostsImpl;import mage.abilities.effects.common.continious.BoostTargetEffect;import mage.cards.CardImpl;import mage.target.common.TargetCreaturePermanent;/** * * @author Backfir3 */public class ThrissNantukoPrimus extends CardImpl<ThrissNantukoPrimus> {    public ThrissNantukoPrimus(UUID ownerId) {        super(ownerId, 134, "Thriss, Nantuko Primus", Rarity.RARE, new CardType[]{CardType.CREATURE}, "{5}{G}{G}");        this.expansionSetCode = "JUD";        this.supertype.add("Legendary");        this.subtype.add("Insect");        this.subtype.add("Druid");        this.color.setGreen(true);        this.power = new MageInt(5);        this.toughness = new MageInt(5);        //G}, {T}: Target creature gets +5/+5 until end of turn.        SimpleActivatedAbility ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new BoostTargetEffect(5, 5, Duration.EndOfTurn), new ManaCostsImpl("{G}"));        ability.addCost(new TapSourceCost());        ability.addTarget(new TargetCreaturePermanent());        this.addAbility(ability);    }    public ThrissNantukoPrimus(final ThrissNantukoPrimus card) {        super(card);    }    @Override    public ThrissNantukoPrimus copy() {        return new ThrissNantukoPrimus(this);    }}
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.judgment;
+
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Duration;
+import mage.Constants.Rarity;
+import mage.Constants.Zone;
+import mage.MageInt;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.continious.BoostTargetEffect;
+import mage.cards.CardImpl;
+import mage.target.common.TargetCreaturePermanent;
+
+/**
+ *
+ * @author Backfir3
+ */
+public class ThrissNantukoPrimus extends CardImpl<ThrissNantukoPrimus> {
+
+    public ThrissNantukoPrimus(UUID ownerId) {
+        super(ownerId, 134, "Thriss, Nantuko Primus", Rarity.RARE, new CardType[]{CardType.CREATURE}, "{5}{G}{G}");
+        this.expansionSetCode = "JUD";
+        this.supertype.add("Legendary");
+        this.subtype.add("Insect");
+        this.subtype.add("Druid");
+
+        this.color.setGreen(true);
+        this.power = new MageInt(5);
+        this.toughness = new MageInt(5);
+
+        //G}, {T}: Target creature gets +5/+5 until end of turn.
+        SimpleActivatedAbility ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new BoostTargetEffect(5, 5, Duration.EndOfTurn), new ManaCostsImpl("{G}"));
+        ability.addCost(new TapSourceCost());
+        ability.addTarget(new TargetCreaturePermanent());
+        this.addAbility(ability);
+    }
+
+    public ThrissNantukoPrimus(final ThrissNantukoPrimus card) {
+        super(card);
+    }
+
+    @Override
+    public ThrissNantukoPrimus copy() {
+        return new ThrissNantukoPrimus(this);
+    }
+}
--- Mage.Sets/src/mage/sets/urzasdestiny/BloodshotCyclops.java
+++ Mage.Sets/src/mage/sets/urzasdestiny/BloodshotCyclops.java
@@ -1,1 +1,119 @@
-/* *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved. *  *  Redistribution and use in source and binary forms, with or without modification, are *  permitted provided that the following conditions are met: *  *     1. Redistributions of source code must retain the above copyright notice, this list of *        conditions and the following disclaimer. *  *     2. Redistributions in binary form must reproduce the above copyright notice, this list *        of conditions and the following disclaimer in the documentation and/or other materials *        provided with the distribution. *  *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. *  *  The views and conclusions contained in the software and documentation are those of the *  authors and should not be interpreted as representing official policies, either expressed *  or implied, of BetaSteward_at_googlemail.com. */package mage.sets.urzasdestiny;import java.util.UUID;import mage.Constants.CardType;import mage.Constants.Outcome;import mage.Constants.Rarity;import mage.Constants.Zone;import mage.MageInt;import mage.abilities.Ability;import mage.abilities.common.SimpleActivatedAbility;import mage.abilities.costs.Cost;import mage.abilities.costs.common.SacrificeTargetCost;import mage.abilities.effects.OneShotEffect;import mage.cards.CardImpl;import mage.game.Game;import mage.game.permanent.Permanent;import mage.players.Player;import mage.target.common.TargetControlledCreaturePermanent;import mage.target.common.TargetCreatureOrPlayer;/** * * @author Backfir3 */public class BloodshotCyclops extends CardImpl<BloodshotCyclops> {    public BloodshotCyclops(UUID ownerId) {        super(ownerId, 77, "Bloodshot Cyclops", Rarity.RARE, new CardType[]{CardType.CREATURE}, "{5}{R}");        this.expansionSetCode = "UDS";        this.subtype.add("Cyclops");        this.subtype.add("Giant");        this.color.setRed(true);        this.power = new MageInt(4);        this.toughness = new MageInt(4);        // {T}, Sacrifice a creature: Bloodshot Cyclops deals damage equal to the sacrificed        // creature's power to target creature or player.        SimpleActivatedAbility ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new BloodshotCyclopsEffect(), new SacrificeTargetCost(new TargetControlledCreaturePermanent()));        ability.addTarget(new TargetCreatureOrPlayer());        this.addAbility(ability);    }    public BloodshotCyclops(final BloodshotCyclops card) {        super(card);    }    @Override    public BloodshotCyclops copy() {        return new BloodshotCyclops(this);    }}class BloodshotCyclopsEffect extends OneShotEffect<BloodshotCyclopsEffect> {    public BloodshotCyclopsEffect() {        super(Outcome.Damage);        staticText = "{this} deals damage equal to the sacrificed creature's power to target creature or player";    }    public BloodshotCyclopsEffect(final BloodshotCyclopsEffect effect) {        super(effect);    }    @Override    public boolean apply(Game game, Ability source) {        int amount = 0;        for (Cost cost : source.getCosts()) {            if (cost instanceof SacrificeTargetCost && ((SacrificeTargetCost) cost).getPermanents().size() > 0) {                amount = ((SacrificeTargetCost) cost).getPermanents().get(0).getPower().getValue();                break;            }        }        if (amount > 0) {            Permanent permanent = game.getPermanent(source.getFirstTarget());            if (permanent != null) {                permanent.damage(amount, source.getSourceId(), game, true, false);                return true;            }            Player player = game.getPlayer(source.getFirstTarget());            if (player != null) {                player.damage(amount, source.getSourceId(), game, false, true);                return true;            }        }        return false;    }    @Override    public BloodshotCyclopsEffect copy() {        return new BloodshotCyclopsEffect(this);    }}
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.urzasdestiny;
+
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Outcome;
+import mage.Constants.Rarity;
+import mage.Constants.Zone;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.Cost;
+import mage.abilities.costs.common.SacrificeTargetCost;
+import mage.abilities.effects.OneShotEffect;
+import mage.cards.CardImpl;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.players.Player;
+import mage.target.common.TargetControlledCreaturePermanent;
+import mage.target.common.TargetCreatureOrPlayer;
+
+/**
+ *
+ * @author Backfir3
+ */
+public class BloodshotCyclops extends CardImpl<BloodshotCyclops> {
+
+    public BloodshotCyclops(UUID ownerId) {
+        super(ownerId, 77, "Bloodshot Cyclops", Rarity.RARE, new CardType[]{CardType.CREATURE}, "{5}{R}");
+        this.expansionSetCode = "UDS";
+        this.subtype.add("Cyclops");
+        this.subtype.add("Giant");
+        this.color.setRed(true);
+        this.power = new MageInt(4);
+        this.toughness = new MageInt(4);
+
+        // {T}, Sacrifice a creature: Bloodshot Cyclops deals damage equal to the sacrificed
+        // creature's power to target creature or player.
+        SimpleActivatedAbility ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new BloodshotCyclopsEffect(), new SacrificeTargetCost(new TargetControlledCreaturePermanent()));
+        ability.addTarget(new TargetCreatureOrPlayer());
+        this.addAbility(ability);
+    }
+
+    public BloodshotCyclops(final BloodshotCyclops card) {
+        super(card);
+    }
+
+    @Override
+    public BloodshotCyclops copy() {
+        return new BloodshotCyclops(this);
+    }
+}
+
+class BloodshotCyclopsEffect extends OneShotEffect<BloodshotCyclopsEffect> {
+
+    public BloodshotCyclopsEffect() {
+        super(Outcome.Damage);
+        staticText = "{this} deals damage equal to the sacrificed creature's power to target creature or player";
+    }
+
+    public BloodshotCyclopsEffect(final BloodshotCyclopsEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        int amount = 0;
+        for (Cost cost : source.getCosts()) {
+            if (cost instanceof SacrificeTargetCost && ((SacrificeTargetCost) cost).getPermanents().size() > 0) {
+                amount = ((SacrificeTargetCost) cost).getPermanents().get(0).getPower().getValue();
+                break;
+            }
+        }
+        if (amount > 0) {
+            Permanent permanent = game.getPermanent(source.getFirstTarget());
+            if (permanent != null) {
+                permanent.damage(amount, source.getSourceId(), game, true, false);
+                return true;
+            }
+            Player player = game.getPlayer(source.getFirstTarget());
+            if (player != null) {
+                player.damage(amount, source.getSourceId(), game, false, true);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public BloodshotCyclopsEffect copy() {
+        return new BloodshotCyclopsEffect(this);
+    }
+}
