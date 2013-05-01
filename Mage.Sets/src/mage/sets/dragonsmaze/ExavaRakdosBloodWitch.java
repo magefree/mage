a250e9a18335069baec2c5a3f7c06f7303894007
LevelX2 <ludwig.hirth@online.de>
2013-05-01 09:01:48 +0200

    [DGM] Emmara Tandris / Drown in Filth / Evava, Rakdos Blood Witch / Gaze of Granite / Gleam of Battle

---
+++ Mage.Sets/src/mage/sets/dragonsmaze/ExavaRakdosBloodWitch.java
@@ -0,0 +1,96 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.dragonsmaze;
+
+import java.util.UUID;
+import mage.Constants;
+import mage.Constants.CardType;
+import mage.Constants.Duration;
+import mage.Constants.Rarity;
+import mage.Constants.Zone;
+import mage.MageInt;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.common.continious.GainAbilityAllEffect;
+import mage.abilities.keyword.FirstStrikeAbility;
+import mage.abilities.keyword.HasteAbility;
+import mage.abilities.keyword.UnleashAbility;
+import mage.cards.CardImpl;
+import mage.counters.CounterType;
+import mage.filter.FilterPermanent;
+import mage.filter.predicate.mageobject.CardTypePredicate;
+import mage.filter.predicate.permanent.AnotherPredicate;
+import mage.filter.predicate.permanent.ControllerPredicate;
+import mage.filter.predicate.permanent.CounterPredicate;
+
+/**
+ *
+ * @author LevelX2
+ */
+public class ExavaRakdosBloodWitch extends CardImpl<ExavaRakdosBloodWitch> {
+
+    private static final FilterPermanent filter = new FilterPermanent();
+    static {
+        filter.add(new CardTypePredicate(CardType.CREATURE));
+        filter.add(new ControllerPredicate(Constants.TargetController.YOU));
+        filter.add(new CounterPredicate(CounterType.P1P1));
+        filter.add(new AnotherPredicate());
+    }
+
+    final String rule = "Each other creature you control with a +1/+1 counter on it has haste";
+    public ExavaRakdosBloodWitch(UUID ownerId) {
+        super(ownerId, 69, "Exava, Rakdos Blood Witch", Rarity.RARE, new CardType[]{CardType.CREATURE}, "{2}{B}{R}");
+        this.expansionSetCode = "DGM";
+        this.supertype.add("Legendary");
+        this.subtype.add("Human");
+        this.subtype.add("Cleric");
+
+        this.color.setRed(true);
+        this.color.setBlack(true);
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(3);
+
+        // First strike
+        this.addAbility(FirstStrikeAbility.getInstance());
+        // Haste
+        this.addAbility(HasteAbility.getInstance());
+        // Unleash
+        this.addAbility(new UnleashAbility());
+        // Each other creature you control with a +1/+1 counter on it has haste.
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new GainAbilityAllEffect(HasteAbility.getInstance(), Duration.WhileOnBattlefield, filter, rule)));
+
+    }
+
+    public ExavaRakdosBloodWitch(final ExavaRakdosBloodWitch card) {
+        super(card);
+    }
+
+    @Override
+    public ExavaRakdosBloodWitch copy() {
+        return new ExavaRakdosBloodWitch(this);
+    }
+}
