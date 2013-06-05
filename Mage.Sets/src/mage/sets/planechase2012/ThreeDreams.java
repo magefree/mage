a3651c3856bb42624d6a4df07d9192a9b0899972
LevelX2 <ludwig.hirth@online.de>
2013-06-05 17:26:37 +0200

    Added Krark's Thumb and Three Dreams.

---
+++ Mage.Sets/src/mage/sets/planechase2012/ThreeDreams.java
@@ -0,0 +1,103 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.planechase2012;
+
+import java.util.UUID;
+import mage.Constants.CardType;
+import mage.Constants.Rarity;
+import mage.abilities.effects.common.search.SearchLibraryPutInHandEffect;
+import mage.cards.Card;
+import mage.cards.CardImpl;
+import mage.cards.Cards;
+import mage.filter.FilterCard;
+import mage.filter.predicate.mageobject.SubtypePredicate;
+import mage.game.Game;
+import mage.target.common.TargetCardInLibrary;
+
+/**
+ *
+ * @author LevelX2
+ */
+public class ThreeDreams extends CardImpl<ThreeDreams> {
+
+    public ThreeDreams(UUID ownerId) {
+        super(ownerId, 13, "Three Dreams", Rarity.RARE, new CardType[]{CardType.SORCERY}, "{4}{W}");
+        this.expansionSetCode = "PC2";
+
+        this.color.setWhite(true);
+
+        // Search your library for up to three Aura cards with different names, reveal them, and put them into your hand. Then shuffle your library.
+        this.getSpellAbility().addEffect(new SearchLibraryPutInHandEffect(new ThreeDreamsTarget(), true, true));
+    }
+
+    public ThreeDreams(final ThreeDreams card) {
+        super(card);
+    }
+
+    @Override
+    public ThreeDreams copy() {
+        return new ThreeDreams(this);
+    }
+}
+
+class ThreeDreamsTarget extends TargetCardInLibrary {
+
+    private static final FilterCard aurafilter = new FilterCard("Aura cards with different names");
+    static {
+        aurafilter.add(new SubtypePredicate("Aura"));
+    }
+
+    public ThreeDreamsTarget() {
+        super(0, 3, aurafilter.copy());
+    }
+
+    public ThreeDreamsTarget(final ThreeDreamsTarget target) {
+        super(target);
+    }
+
+    @Override
+    public ThreeDreamsTarget copy() {
+        return new ThreeDreamsTarget(this);
+    }
+
+    @Override
+    public boolean canTarget(UUID id, Cards cards, Game game) {
+        Card card = cards.get(id, game);
+        if (card != null) {
+            // check if card with that name was selected before
+            for (UUID targetId : this.getTargets()) {
+                Card iCard = game.getCard(targetId);
+                if (iCard != null && iCard.getName().equals(card.getName())) {
+                    return false;
+                }
+            }
+            return filter.match(card, game);
+        }
+        return false;
+    }
+}
