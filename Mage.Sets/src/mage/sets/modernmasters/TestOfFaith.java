9c57f27dfd91a0231a3a89422375d2cc67e7e15f
LevelX2 <ludwig.hirth@online.de>
2013-06-16 17:34:03 +0200

    [minor] fixed some import errors.

---
+++ Mage.Sets/src/mage/sets/modernmasters/TestOfFaith.java
@@ -0,0 +1,137 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.modernmasters;
+
+import java.util.UUID;
+import mage.abilities.Ability;
+import mage.abilities.effects.PreventionEffectImpl;
+import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Rarity;
+import mage.counters.CounterType;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.permanent.Permanent;
+import mage.target.common.TargetCreaturePermanent;
+
+/**
+ *
+ * @author LevelX2
+ */
+public class TestOfFaith extends CardImpl<TestOfFaith> {
+
+    public TestOfFaith(UUID ownerId) {
+        super(ownerId, 33, "Test of Faith", Rarity.COMMON, new CardType[]{CardType.INSTANT}, "{1}{W}");
+        this.expansionSetCode = "MMA";
+
+        this.color.setWhite(true);
+
+        // Prevent the next 3 damage that would be dealt to target creature this turn, and put a +1/+1 counter on that creature for each 1 damage prevented this way.
+        this.getSpellAbility().addEffect(new TestOfFaithPreventDamageTargetEffect(Duration.EndOfTurn));
+        this.getSpellAbility().addTarget(new TargetCreaturePermanent(true));
+
+    }
+
+    public TestOfFaith(final TestOfFaith card) {
+        super(card);
+    }
+
+    @Override
+    public TestOfFaith copy() {
+        return new TestOfFaith(this);
+    }
+}
+
+class TestOfFaithPreventDamageTargetEffect extends PreventionEffectImpl<TestOfFaithPreventDamageTargetEffect> {
+
+    private int amount = 3;
+
+    public TestOfFaithPreventDamageTargetEffect(Duration duration) {
+        super(duration);
+        staticText = "Prevent the next 3 damage that would be dealt to target creature this turn, and put a +1/+1 counter on that creature for each 1 damage prevented this way";
+    }
+
+    public TestOfFaithPreventDamageTargetEffect(final TestOfFaithPreventDamageTargetEffect effect) {
+        super(effect);
+        this.amount = effect.amount;
+    }
+
+    @Override
+    public TestOfFaithPreventDamageTargetEffect copy() {
+        return new TestOfFaithPreventDamageTargetEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        return true;
+    }
+
+    @Override
+    public boolean replaceEvent(GameEvent event, Ability source, Game game) {
+        GameEvent preventEvent = new GameEvent(GameEvent.EventType.PREVENT_DAMAGE, source.getFirstTarget(), source.getId(), source.getControllerId(), event.getAmount(), false);
+        if (!game.replaceEvent(preventEvent)) {
+            int prevented = 0;
+            if (event.getAmount() >= this.amount) {
+                int damage = amount;
+                event.setAmount(event.getAmount() - amount);
+                this.used = true;
+                game.fireEvent(GameEvent.getEvent(GameEvent.EventType.PREVENTED_DAMAGE, source.getFirstTarget(), source.getId(), source.getControllerId(), damage));
+                prevented = damage;
+            } else {
+                int damage = event.getAmount();
+                event.setAmount(0);
+                amount -= damage;
+                game.fireEvent(GameEvent.getEvent(GameEvent.EventType.PREVENTED_DAMAGE, source.getFirstTarget(), source.getId(), source.getControllerId(), damage));
+                prevented = damage;
+            }
+
+            // add counters now
+            if (prevented > 0) {
+                Permanent targetPermanent = game.getPermanent(source.getTargets().getFirstTarget());
+                if (targetPermanent != null) {
+                    targetPermanent.addCounters(CounterType.P1P1.createInstance(prevented), game);
+                    game.informPlayers("Adding " + prevented + " +1/+1 counters to " + targetPermanent.getName());
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean applies(GameEvent event, Ability source, Game game) {
+        if (!this.used && super.applies(event, source, game)) {
+            if (source.getTargets().getFirstTarget().equals(event.getTargetId())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
