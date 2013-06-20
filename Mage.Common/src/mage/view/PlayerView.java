7c187a1d2aba9c662e473e6d06e5140c8718645d
magenoxx <magenoxx@gmail>
2013-06-20 15:50:06 +0400

    Fixes for timers.

--- Mage.Common/src/mage/view/PlayerView.java
+++ Mage.Common/src/mage/view/PlayerView.java
@@ -54,6 +54,7 @@ public class PlayerView implements Serializable {
     private int libraryCount;
     private int handCount;
     private boolean isActive;
+    private boolean hasPriority;
     private boolean hasLeft;
     private ManaPoolView manaPool;
     private SimpleCardsView graveyard = new SimpleCardsView();
@@ -74,6 +75,7 @@ public class PlayerView implements Serializable {
         this.handCount = player.getHand().size();
         this.manaPool = new ManaPoolView(player.getManaPool());
         this.isActive = (player.getId().equals(state.getActivePlayerId()));
+        this.hasPriority = player.getId().equals(state.getPriorityPlayerId());
         this.hasLeft = player.hasLeft();
         for (Card card: player.getGraveyard().getCards(game)) {
             graveyard.put(card.getId(), new SimpleCardView(card.getId(), card.getExpansionSetCode(), card.getCardNumber(), card.getUsesVariousArt(), card.isFaceDown()));
@@ -196,5 +198,9 @@ public class PlayerView implements Serializable {
 
     public int getPriorityTimeLeft() {
         return priorityTimeLeft;
+    }
+
+    public boolean hasPriority() {
+        return hasPriority;
     }
 }
