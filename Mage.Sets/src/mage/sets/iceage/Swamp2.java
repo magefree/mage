ea7bfdfc211994c1c734e5a9efb1d9211970b719
LevelX2 <ludwig.hirth@online.de>
2013-06-28 22:53:56 +0200

    * Swamps of Ice Age set - Corrected wrong collector numbers (were shown as Sulforous Springs).

--- Mage.Sets/src/mage/sets/iceage/Swamp2.java
+++ Mage.Sets/src/mage/sets/iceage/Swamp2.java
@@ -36,7 +36,7 @@ import java.util.UUID;
 public class Swamp2 extends mage.cards.basiclands.Swamp {
 
     public Swamp2(UUID ownerId) {
-        super(ownerId, 352);
+        super(ownerId, 354);
         this.expansionSetCode = "ICE";
     }
 
--- Mage.Sets/src/mage/sets/iceage/Swamp3.java
+++ Mage.Sets/src/mage/sets/iceage/Swamp3.java
@@ -36,7 +36,7 @@ import java.util.UUID;
 public class Swamp3 extends mage.cards.basiclands.Swamp {
 
     public Swamp3(UUID ownerId) {
-        super(ownerId, 353);
+        super(ownerId, 355);
         this.expansionSetCode = "ICE";
     }
 
