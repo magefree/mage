9d4746c31829fc92f7a4bad919d5b4953b6725c1
LevelX2 <ludwig.hirth@online.de>
2013-07-07 11:17:16 +0200

    * Fixed bugs in display of subtype and supertype in card editor (workaround) should be better fixed directly in card repository handling.

--- Mage/src/mage/cards/mock/MockCard.java
+++ Mage/src/mage/cards/mock/MockCard.java
@@ -20,8 +20,15 @@ public class MockCard extends CardImpl<MockCard> {
         this.toughness = mageIntFromString(card.getToughness());
         this.rarity = card.getRarity();
         this.cardType = card.getTypes();
-        this.subtype = card.getSubTypes();
-        this.supertype = card.getSupertypes();
+        // dont copy list with empty element (would be better to eliminate earlier but I don't know how (LevelX2))
+        if (card.getSubTypes().get(0).length() >0) {
+            // empty element leads to added "-" after cardtype.
+            this.subtype = card.getSubTypes();
+        }
+        if (card.getSupertypes().get(0).length() >0) {
+            // empty element leads to blank before Card Type.
+            this.supertype = card.getSupertypes();
+        }
 
         this.usesVariousArt = card.usesVariousArt();
 
