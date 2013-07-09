3e8765e1579f8a12aa207c2e0f3bbaf3c7d47350
LevelX2 <ludwig.hirth@online.de>
2013-07-09 08:33:42 +0200

    [M14] Artificer's Hex (There are some general problems to show Enchantment enchanting Equipment equips creature)

---
+++ Mage.Sets/src/mage/sets/magic2014/ArtificersHex.java
@@ -0,0 +1,119 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.magic2014;
+
+import java.util.UUID;
+import mage.abilities.Ability;
+import mage.abilities.common.BeginningOfUpkeepTriggeredAbility;
+import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.AttachEffect;
+import mage.abilities.keyword.EnchantAbility;
+import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Outcome;
+import mage.constants.Rarity;
+import mage.constants.TargetController;
+import mage.constants.Zone;
+import mage.filter.FilterPermanent;
+import mage.filter.predicate.mageobject.CardTypePredicate;
+import mage.filter.predicate.mageobject.SubtypePredicate;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.target.TargetPermanent;
+
+/**
+ *
+ * @author LevelX2
+ */
+public class ArtificersHex extends CardImpl<ArtificersHex> {
+    
+    private static final FilterPermanent filter = new FilterPermanent("Equipment");
+    static {
+        filter.add(new CardTypePredicate(CardType.ARTIFACT));
+        filter.add(new SubtypePredicate("Equipment"));
+    }
+
+    public ArtificersHex(UUID ownerId) {
+        super(ownerId, 85, "Artificer's Hex", Rarity.UNCOMMON, new CardType[]{CardType.ENCHANTMENT}, "{B}");
+        this.expansionSetCode = "M14";
+        this.subtype.add("Aura");
+
+        this.color.setBlack(true);
+
+        // Enchant Equipment
+        TargetPermanent auraTarget = new TargetPermanent(filter);
+        this.getSpellAbility().addTarget(auraTarget);
+        this.getSpellAbility().addEffect(new AttachEffect(Outcome.BoostCreature));
+        Ability ability = new EnchantAbility(auraTarget.getTargetName());
+        this.addAbility(ability);
+        // At the beginning of your upkeep, if enchanted Equipment is attached to a creature, destroy that creature.
+        this.addAbility(new BeginningOfUpkeepTriggeredAbility(Zone.BATTLEFIELD, new ArtificersHexEffect(), TargetController.YOU, false, true));
+    }
+
+    public ArtificersHex(final ArtificersHex card) {
+        super(card);
+    }
+
+    @Override
+    public ArtificersHex copy() {
+        return new ArtificersHex(this);
+    }
+}
+
+
+class ArtificersHexEffect extends OneShotEffect<ArtificersHexEffect> {
+
+    public ArtificersHexEffect() {
+        super(Outcome.Benefit);
+        this.staticText = "if enchanted Equipment is attached to a creature, destroy that creature";
+    }
+
+    public ArtificersHexEffect(final ArtificersHexEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public ArtificersHexEffect copy() {
+        return new ArtificersHexEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Permanent enchantment = game.getPermanent(source.getSourceId());
+        if (enchantment != null && enchantment.getAttachedTo() != null) {
+            Permanent equipment = game.getPermanent(enchantment.getAttachedTo());
+            if (equipment != null && equipment.getAttachedTo() != null) {
+                Permanent creature = game.getPermanent(equipment.getAttachedTo());
+                if (creature != null && creature.getCardType().contains(CardType.CREATURE)) {
+                    return creature.destroy(source.getSourceId(), game, false);
+                }
+            }
+        }
+        return false;
+    }
+}
