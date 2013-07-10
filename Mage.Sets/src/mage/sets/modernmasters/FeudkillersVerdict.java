98627c82df4b3fa9c818be101c2548b608be0ced
LevelX2 <ludwig.hirth@online.de>
2013-07-10 07:58:16 +0200

    * Feudkiller's Verdict - Fixed missing Giant subtype of the created token.

--- Mage.Sets/src/mage/sets/modernmasters/FeudkillersVerdict.java
+++ Mage.Sets/src/mage/sets/modernmasters/FeudkillersVerdict.java
@@ -28,14 +28,14 @@
 package mage.sets.modernmasters;
 
 import java.util.UUID;
-import mage.constants.CardType;
-import mage.constants.Outcome;
-import mage.constants.Rarity;
 import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.CreateTokenEffect;
 import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Outcome;
+import mage.constants.Rarity;
 import mage.game.Game;
 import mage.game.permanent.token.Token;
 import mage.players.Player;
@@ -114,6 +114,7 @@ class GiantWarriorToken extends Token {
         super("Warrior", "5/5 white Giant Warrior creature token");
         cardType.add(CardType.CREATURE);
         color.setWhite(true);
+        subtype.add("Giant");
         subtype.add("Warrior");
         power = new MageInt(5);
         toughness = new MageInt(5);
