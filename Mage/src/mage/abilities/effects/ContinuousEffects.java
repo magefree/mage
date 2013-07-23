8b6cfc952996df8c40fcc998a5eea3f69c6a851d
Plopman <Plopman@hotmail.ch>
2013-07-23 23:20:16 +0200

    Fixed a bug with multiple replacement effect.  The bug appear with the fix of #198 and #196

--- Mage/src/mage/abilities/effects/ContinuousEffects.java
+++ Mage/src/mage/abilities/effects/ContinuousEffects.java
@@ -563,6 +563,7 @@ public class ContinuousEffects implements Serializable {
                                 checked++;
                             }
                         }
+                        break;
                     }
                 }                
             }
