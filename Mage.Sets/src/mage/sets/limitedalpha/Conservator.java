002b74763d4dd1c56da1aa26e6fa034f115db7eb
KholdFuzion <kholdfuzion@T61p.(none)>
2013-07-23 20:06:48 -0500

    Add Missed files from 65d15aab906a49abe325758fb966f1844c667b49

---
+++ Mage.Sets/src/mage/sets/limitedalpha/Conservator.java
@@ -0,0 +1,125 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.limitedalpha;
+
+import java.util.UUID;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.costs.mana.GenericManaCost;
+import mage.abilities.effects.PreventionEffectImpl;
+import mage.abilities.effects.common.PreventDamageTargetEffect;
+import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Rarity;
+import mage.constants.Zone;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.target.TargetPlayer;
+
+/**
+ *
+ * @author KholdFuzion
+
+ */
+public class Conservator extends CardImpl<Conservator> {
+
+    public Conservator(UUID ownerId) {
+        super(ownerId, 237, "Conservator", Rarity.UNCOMMON, new CardType[]{CardType.ARTIFACT}, "{4}");
+        this.expansionSetCode = "LEA";
+
+        // {3}, {tap}: Prevent the next 2 damage that would be dealt to you this turn.
+        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new ConservatorEffect(), new GenericManaCost(3));
+        ability.addTarget(new TargetPlayer());
+        ability.addCost(new TapSourceCost());
+        this.addAbility(ability);
+    }
+
+    public Conservator(final Conservator card) {
+        super(card);
+    }
+
+    @Override
+    public Conservator copy() {
+        return new Conservator(this);
+    }
+}
+
+class ConservatorEffect extends PreventionEffectImpl<ConservatorEffect> {
+
+    private int amount = 2;
+
+    public ConservatorEffect() {
+        super(Duration.EndOfTurn);
+        this.staticText = "Prevent the next 2 damage that would be dealt to you this turn";
+    }
+
+    public ConservatorEffect(final ConservatorEffect effect) {
+        super(effect);
+        this.amount = effect.amount;
+    }
+
+    @Override
+    public ConservatorEffect copy() {
+        return new ConservatorEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        return true;
+    }
+
+    @Override
+    public boolean replaceEvent(GameEvent event, Ability source, Game game) {
+        GameEvent preventEvent = new GameEvent(GameEvent.EventType.PREVENT_DAMAGE,
+                source.getControllerId(), source.getId(), source.getControllerId(), event.getAmount(), false);
+        if (!game.replaceEvent(preventEvent)) {
+            int damage = event.getAmount();
+            if (damage >= this.amount) {
+                event.setAmount(damage - this.amount);
+                damage = this.amount;
+                this.used = true;
+            } else {
+                event.setAmount(0);
+                this.amount -= damage;
+            }
+            game.fireEvent(GameEvent.getEvent(GameEvent.EventType.PREVENTED_DAMAGE,
+                    source.getControllerId(), source.getId(), source.getControllerId(), damage));
+        }
+        return false;
+    }
+
+    @Override
+    public boolean applies(GameEvent event, Ability source, Game game) {
+        if (!this.used && super.applies(event, source, game) && event.getTargetId().equals(source.getControllerId())) {
+            return true;
+        }
+        return false;
+    }
+}
