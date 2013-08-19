207467d4787e417d94784ad8dd364a7a62d076ad
Plopman <Plopman@hotmail.ch>
2013-08-19 22:21:56 +0200

    Fixed Reach of Branches wrong color and subtype

--- Mage.Sets/src/mage/sets/modernmasters/ReachOfBranches.java
+++ Mage.Sets/src/mage/sets/modernmasters/ReachOfBranches.java
@@ -28,15 +28,15 @@
 package mage.sets.modernmasters;
 
 import java.util.UUID;
-import mage.constants.CardType;
-import mage.constants.Rarity;
-import mage.constants.Zone;
 import mage.MageInt;
 import mage.abilities.common.EntersBattlefieldAllTriggeredAbility;
 import mage.abilities.effects.common.CreateTokenEffect;
 import mage.abilities.effects.common.ReturnSourceFromGraveyardToHandEffect;
 import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Rarity;
 import mage.constants.TargetController;
+import mage.constants.Zone;
 import mage.filter.common.FilterLandPermanent;
 import mage.filter.predicate.mageobject.SubtypePredicate;
 import mage.filter.predicate.permanent.ControllerPredicate;
@@ -82,9 +82,9 @@ class TreefolkShamanToken extends Token {
     TreefolkShamanToken() {
         super("Treefolk Shaman", "2/5 green Treefolk Shaman creature token");
         cardType.add(CardType.CREATURE);
-        color.setBlack(true);
+        color.setGreen(true);
+        subtype.add("Treefolk");
         subtype.add("Shaman");
-        subtype.add("Rogue");
         power = new MageInt(2);
         toughness = new MageInt(5);
     }
