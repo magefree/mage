6a2df3d935a1de60b33ac6f1939a92812487716a
Backfir3 <backfir3_rage@yahoo.com>
2013-08-22 14:06:20 +0200

    Added [USG] Titania's Boon; Titania's Chosen [UDS] Rofellos, Llanowar Emissary

---
+++ Mage.Sets/src/mage/sets/urzasdestiny/RofellosLlanowarEmissary.java
@@ -0,0 +1,77 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ * 
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ * 
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ * 
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ * 
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+
+package mage.sets.urzasdestiny;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.Mana;
+import mage.abilities.dynamicvalue.common.PermanentsOnBattlefieldCount;
+import mage.abilities.mana.DynamicManaAbility;
+import mage.constants.CardType;
+import mage.constants.Rarity;
+import mage.cards.CardImpl;
+import mage.filter.common.FilterControlledPermanent;
+import mage.filter.predicate.mageobject.SubtypePredicate;
+
+/**
+ *
+ * @author Backfir3
+ */
+public class RofellosLlanowarEmissary extends CardImpl<RofellosLlanowarEmissary> {
+
+    private static final FilterControlledPermanent filter = new FilterControlledPermanent("for each Forest you control");
+
+    static {
+        filter.add(new SubtypePredicate("Forest"));
+    }
+
+    public RofellosLlanowarEmissary(UUID ownerId) {
+        super(ownerId, 118, "Rofellos, Llanowar Emissary", Rarity.RARE, new CardType[]{CardType.CREATURE}, "{G}{G}");
+        this.expansionSetCode = "UDS";
+        this.supertype.add("Legendary");
+        this.subtype.add("Elf");
+        this.subtype.add("Druid");
+        this.color.setGreen(true);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(1);
+		
+        // {tap}: Add {G} to your mana pool for each Forest you control.
+		this.addAbility(new DynamicManaAbility(Mana.GreenMana, new PermanentsOnBattlefieldCount(filter)));
+    }
+
+    public RofellosLlanowarEmissary(final RofellosLlanowarEmissary card) {
+        super(card);
+    }
+
+    @Override
+    public RofellosLlanowarEmissary copy() {
+        return new RofellosLlanowarEmissary(this);
+    }
+
+}
