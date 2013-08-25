6bcda61c4f1796567ec056ebd089a4b850a10c61
cbt33 <cbt33@users.noreply.github.com>
2013-08-25 00:52:37 -0400

    Create AncestralTribute.java

---
+++ Mage.Sets/src/mage/sets/odyssey/AncestralTribute.java
@@ -0,0 +1,65 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+
+
+package mage.sets.odyssey;
+
+import mage.constants.CardType;
+import mage.constants.Rarity;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.keyword.FlashbackAbility;
+import mage.cards.CardImpl;
+import mage.constants.TimingRule;
+import mage.abilities.dynamicvalue.common.CardsInControllerGraveyardCount;
+import mage.abilities.dynamicvalue.DynamicValue;
+import mage.abilities.effects.common.GainLifeEffect;
+
+import java.util.UUID;
+
+public class AncestralTribute extends CardImpl<AncestralTribute> {
+	
+	public AncestralTribute(ownerID UUID) {
+		super(ownerID, 2, "Ancestral Tribute", Rarity.RARE, CardType[](CardType.SORCERY), "{5}{W}{W}");
+		this.expansionCode = "ODY";
+	
+	this.Color.setWhite(true);
+	DynamicValue value = new CardsInControllerGraveyardCount();
+	this.getSpellAbility().addEffect(new GainLifeEffect(value.calculate(game, source) * 2));
+	this.addAbility(FlashbackAbility("{9}{W}{W}{W}", TimingRule.SORCERY));
+}
+
+public AncestralTribute(final AncestralTribute card){
+	super(card);
+}
+
+@Override
+public AncestralTribute copy() {
+	return new AncestralTribute(this);
+}
+
+}
