0f31cd9f1249563f04ae8e0f103057e36f76a766
LevelX2 <ludwig.hirth@online.de>
2013-08-28 08:35:47 +0200

    Added Aegis of Honor and Helm of Awakening.

---
+++ Mage/src/mage/abilities/effects/common/cost/SpellsCostReductionAllEffect.java
@@ -0,0 +1,86 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.abilities.effects.common.cost;
+
+import mage.abilities.Ability;
+import mage.abilities.SpellAbility;
+import mage.abilities.effects.CostModificationEffectImpl;
+import mage.abilities.keyword.FlashbackAbility;
+import mage.cards.Card;
+import mage.constants.Duration;
+import mage.constants.Outcome;
+import mage.filter.FilterCard;
+import mage.game.Game;
+import mage.util.CardUtil;
+
+/**
+ *
+ * @author LevelX2
+ */
+public class SpellsCostReductionAllEffect extends CostModificationEffectImpl<SpellsCostReductionAllEffect> {
+
+    private FilterCard filter;
+    private int amount;
+
+    public SpellsCostReductionAllEffect(int amount) {
+        this(new FilterCard("All Spells "), amount);
+    }
+
+    public SpellsCostReductionAllEffect(FilterCard filter, int amount) {
+        super(Duration.WhileOnBattlefield, Outcome.Benefit);
+        this.filter = filter;
+        this.amount = amount;
+        this.staticText = new StringBuilder(filter.getMessage()).append(" cost {").append(amount).append("} less to cast").toString();
+    }
+
+    protected SpellsCostReductionAllEffect(SpellsCostReductionAllEffect effect) {
+        super(effect);
+        this.filter = effect.filter;
+        this.amount = effect.amount;
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source, Ability abilityToModify) {
+        CardUtil.reduceCost(abilityToModify, this.amount);
+        return true;
+    }
+
+    @Override
+    public boolean applies(Ability abilityToModify, Ability source, Game game) {
+        if ((abilityToModify instanceof SpellAbility || abilityToModify instanceof FlashbackAbility)) {
+            Card sourceCard = game.getCard(abilityToModify.getSourceId());
+            return sourceCard != null && this.filter.match(sourceCard, game);
+        }
+        return false;
+    }
+
+    @Override
+    public SpellsCostReductionAllEffect copy() {
+        return new SpellsCostReductionAllEffect(this);
+    }
+}
