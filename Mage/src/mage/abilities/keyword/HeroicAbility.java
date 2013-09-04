7569056804395ba427d5fc8408926c1eb99488b7
LevelX2 <ludwig.hirth@online.de>
2013-09-04 11:55:24 +0200

    Added HeroicAbility for Theros.

---
+++ Mage/src/mage/abilities/keyword/HeroicAbility.java
@@ -0,0 +1,91 @@
+ /*
+* Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification, are
+* permitted provided that the following conditions are met:
+*
+*    1. Redistributions of source code must retain the above copyright notice, this list of
+*       conditions and the following disclaimer.
+*
+*    2. Redistributions in binary form must reproduce the above copyright notice, this list
+*       of conditions and the following disclaimer in the documentation and/or other materials
+*       provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+* FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+* ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+* ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* The views and conclusions contained in the software and documentation are those of the
+* authors and should not be interpreted as representing official policies, either expressed
+* or implied, of BetaSteward_at_googlemail.com.
+*/
+
+package mage.abilities.keyword;
+
+import java.util.UUID;
+import mage.abilities.SpellAbility;
+import mage.abilities.TriggeredAbilityImpl;
+import mage.abilities.effects.Effect;
+import mage.constants.Zone;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.stack.Spell;
+
+/**
+ * Heroic
+ *
+ *
+ * @author LevelX2
+ */
+
+public class HeroicAbility extends TriggeredAbilityImpl<HeroicAbility> {
+
+    public HeroicAbility(Effect effect) {
+        super(Zone.BATTLEFIELD, effect, false);
+    }
+
+    public HeroicAbility(final HeroicAbility ability) {
+        super(ability);
+    }
+
+    @Override
+    public HeroicAbility copy() {
+        return new HeroicAbility(this);
+    }
+
+    @Override
+    public boolean checkTrigger(GameEvent event, Game game) {
+        if (event.getType() == GameEvent.EventType.SPELL_CAST && event.getPlayerId().equals(this.getControllerId())) {
+            Spell spell = game.getStack().getSpell(event.getTargetId());
+            if (checkSpell(spell, game)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkSpell(Spell spell, Game game) {
+        if (spell != null) {
+            SpellAbility sa = spell.getSpellAbility();
+            for (Effect effect : sa.getEffects()) {
+                for (UUID targetId : effect.getTargetPointer().getTargets(game, sa)) {
+                    if (targetId.equals(this.getSourceId())) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String getRule() {
+        return new StringBuilder("<i>Heroic</i> - Whenever you cast a spell that targets {this}, ").append(super.getRule()).toString();
+    }
+}
