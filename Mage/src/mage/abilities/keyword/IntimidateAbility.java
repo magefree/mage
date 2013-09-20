949e861efdb509aa0357d5ed2105977fceb7edd2
LevelX2 <ludwig.hirth@online.de>
2013-09-20 19:35:51 +0200

    Updated Card Repository version.

--- Mage/src/mage/abilities/keyword/IntimidateAbility.java
+++ Mage/src/mage/abilities/keyword/IntimidateAbility.java
@@ -9,6 +9,19 @@ import mage.constants.Duration;
 import mage.game.Game;
 import mage.game.permanent.Permanent;
 
+/**
+ * 702.13. Intimidate #
+ *
+ *  702.13a Intimidate is an evasion ability.
+ *
+ *  702.13b A creature with intimidate can't be blocked except by artifact creatures
+ *          and/or creatures that share a color with it. (See rule 509, "Declare Blockers Step.") #
+ *
+ *  702.13c Multiple instances of intimidate on the same creature are redundant.
+ *
+ *
+ *
+ */
 public class IntimidateAbility extends EvasionAbility<IntimidateAbility> implements MageSingleton  {
     private static final IntimidateAbility fInstance = new IntimidateAbility();
 
