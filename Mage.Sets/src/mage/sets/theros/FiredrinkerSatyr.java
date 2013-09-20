44fd58999c5698e60bbe48fca05bc81023e6a5e3
LevelX2 <ludwig.hirth@online.de>
2013-09-20 08:22:00 +0200

    * Firedrinker Satyr - Added missing activated ability.

--- Mage.Sets/src/mage/sets/theros/FiredrinkerSatyr.java
+++ Mage.Sets/src/mage/sets/theros/FiredrinkerSatyr.java
@@ -31,9 +31,15 @@ import java.util.UUID;
 import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.DealtDamageToSourceTriggeredAbility;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.Effect;
 import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.DamageControllerEffect;
+import mage.abilities.effects.common.continious.BoostSourceEffect;
 import mage.cards.CardImpl;
 import mage.constants.CardType;
+import mage.constants.Duration;
 import mage.constants.Outcome;
 import mage.constants.Rarity;
 import mage.constants.Zone;
@@ -59,6 +65,11 @@ public class FiredrinkerSatyr extends CardImpl<FiredrinkerSatyr> {
         // Whenever Firedrinker Satyr is dealt damage, it deals that much damage to you.
         this.addAbility(new DealtDamageToSourceTriggeredAbility(Zone.BATTLEFIELD, new FiredrinkerSatyrDealDamageEffect(), false));
         // {1}{R}: Firedrinker Satyr gets +1/+0 until end of turn and deals 1 damage to you.
+        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new BoostSourceEffect(1,0, Duration.EndOfTurn), new ManaCostsImpl("{1}{R}"));
+        Effect effect = new DamageControllerEffect(1);
+        effect.setText("and deals 1 damage to you");
+        ability.addEffect(effect);
+        this.addAbility(ability);
     }
 
     public FiredrinkerSatyr(final FiredrinkerSatyr card) {
