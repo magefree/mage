38b00cbf0943beb199642e20445fe9831b8b216d
cbt33 <cbt33@users.noreply.github.com>
2013-10-10 21:10:12 -0400

    Create EngulfingFlames.java

---
+++ Mage.Sets/src/mage/sets/odyssey/EngulfingFlames.java
@@ -0,0 +1,115 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.odyssey;
+
+import java.util.UUID;
+import mage.abilities.Ability;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.ReplacementEffectImpl;
+import mage.abilities.effects.common.DamageTargetEffect;
+import mage.abilities.keyword.FlashbackAbility;
+import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Outcome;
+import mage.constants.Rarity;
+import mage.constants.TimingRule;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.target.common.TargetCreaturePermanent;
+import mage.watchers.common.DamagedByWatcher;
+
+/**
+ *
+ * @author cbt33, North (Incinerate)
+ */
+public class EngulfingFlames extends CardImpl<EngulfingFlames> {
+
+    public EngulfingFlames(UUID ownerId) {
+        super(ownerId, 191, "Engulfing Flames", Rarity.UNCOMMON, new CardType[]{CardType.INSTANT}, "{R}");
+        this.expansionSetCode = "ODY";
+
+        this.color.setRed(true);
+
+        // Engulfing Flames deals 1 damage to target creature. It can't be regenerated this turn.
+        this.getSpellAbility().addEffect(new DamageTargetEffect(1));
+        this.getSpellAbility().addTarget(new TargetCreaturePermanent(true));
+        this.getSpellAbility().addEffect(new EngulfingFlamesEffect());
+        this.addWatcher(new DamagedByWatcher());
+        // Flashback {3}{R}
+        this.addAbility(new FlashbackAbility(new ManaCostsImpl("{3}{R}"), TimingRule.SORCERY));
+    }
+
+    public EngulfingFlames(final EngulfingFlames card) {
+        super(card);
+    }
+
+    @Override
+    public EngulfingFlames copy() {
+        return new EngulfingFlames(this);
+    }
+}
+
+
+class EngulfingFlamesEffect extends ReplacementEffectImpl<EngulfingFlamesEffect> {
+
+    public EngulfingFlamesEffect() {
+        super(Duration.EndOfTurn, Outcome.Detriment);
+        staticText = "A creature dealt damage this way can't be regenerated this turn";
+    }
+
+    public EngulfingFlamesEffect(final EngulfingFlamesEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public EngulfingFlamesEffect copy() {
+        return new EngulfingFlamesEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        return true;
+    }
+
+    @Override
+    public boolean replaceEvent(GameEvent event, Ability source, Game game) {
+        return true;
+    }
+
+    @Override
+    public boolean applies(GameEvent event, Ability source, Game game) {
+        if (event.getType() == GameEvent.EventType.REGENERATE) {
+            DamagedByWatcher watcher = (DamagedByWatcher) game.getState().getWatchers().get("DamagedByWatcher", source.getSourceId());
+            if (watcher != null)
+                return watcher.damagedCreatures.contains(event.getTargetId());
+        }
+        return false;
+    }
+
+}
