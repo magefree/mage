bc2ff2a835168b97df139469f3aaf2a5c71e69d7
cbt33 <cbt33@users.noreply.github.com>
2013-10-15 21:46:15 -0400

    Create QuickSliver.java

---
+++ Mage.Sets/src/mage/sets/legions/QuickSliver.java
@@ -0,0 +1,109 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.legions;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.AsThoughEffectImpl;
+import mage.abilities.keyword.FlashAbility;
+import mage.cards.Card;
+import mage.cards.CardImpl;
+import mage.constants.AsThoughEffectType;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Outcome;
+import mage.constants.Rarity;
+import mage.constants.Zone;
+import mage.game.Game;
+
+/**
+ *
+ * @author cbt33, Loki (Shimmer Myr)
+ */
+public class QuickSliver extends CardImpl<QuickSliver> {
+
+    public QuickSliver(UUID ownerId) {
+        super(ownerId, 136, "Quick Sliver", Rarity.COMMON, new CardType[]{CardType.CREATURE}, "{1}{G}");
+        this.expansionSetCode = "LGN";
+        this.subtype.add("Sliver");
+
+        this.color.setGreen(true);
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(1);
+
+        // Flash
+        this.addAbility(FlashAbility.getInstance());
+        // Any player may play Sliver cards as though they had flash.
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new QuickSliverEffect()));
+    }
+
+    public QuickSliver(final QuickSliver card) {
+        super(card);
+    }
+
+    @Override
+    public QuickSliver copy() {
+        return new QuickSliver(this);
+    }
+}
+
+class QuickSliverEffect extends AsThoughEffectImpl<QuickSliverEffect> {
+
+    public QuickSliverEffect() {
+        super(AsThoughEffectType.CAST, Duration.WhileOnBattlefield, Outcome.Benefit);
+        staticText = "Any player may play Sliver cards as though they had flash";
+    }
+
+    public QuickSliverEffect(final QuickSliverEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        return true;
+    }
+
+    @Override
+    public QuickSliverEffect copy() {
+        return new QuickSliverEffect(this);
+    }
+
+    @Override
+    public boolean applies(UUID sourceId, Ability source, Game game) {
+        Card card = game.getCard(sourceId);
+        if (card != null) {
+            if (card.getSubtype().contains("Sliver")) {   
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
