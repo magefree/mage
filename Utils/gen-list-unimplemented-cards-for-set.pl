acd0b2e5eadb7f6c7552fa828663c88490057cba
Quercitron <oldquercitron@gmail.com>
2013-12-05 22:53:35 +0400

    Add script that generates list of unimplemented cards for a set

---
+++ Utils/gen-list-unimplemented-cards-for-set.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/perl -w
+
+#author: North
+
+use strict;
+
+
+my $dataFile = "mtg-cards-data.txt";
+my $setsFile = "mtg-sets-data.txt";
+my $knownSetsFile = "known-sets.txt";
+
+my %sets;
+my %knownSets;
+
+my @setCards;
+
+# gets the set name
+my $setName = $ARGV[0];
+if(!$setName) {
+    print 'Enter a set name: ';
+    $setName = <STDIN>;
+    chomp $setName;
+}
+
+
+open (DATA, $dataFile) || die "can't open $dataFile";
+while(my $line = <DATA>) {
+    my @data = split('\\|', $line);
+    if ($data[1] eq $setName) {
+        push(@setCards, \@data);
+    }
+}
+close(DATA);
+
+open (DATA, $knownSetsFile) || die "can't open $knownSetsFile";
+while(my $line = <DATA>) {
+    my @data = split('\\|', $line);
+    $knownSets{$data[0]}= $data[1];
+}
+close(DATA);
+
+open (DATA, $setsFile) || die "can't open $setsFile";
+while(my $line = <DATA>) {
+    my @data = split('\\|', $line);
+    $sets{$data[0]}= $data[1];
+}
+close(DATA);
+
+
+sub cardSort {
+  if (@{$a}[2] < @{$b}[2]) { return -1; }
+  elsif (@{$a}[2] == @{$b}[2]) { return 0;}
+  elsif (@{$a}[2] > @{$b}[2]) { return 1; }
+}
+
+sub toCamelCase {
+    my $string = $_[0];
+    $string =~ s/\b([\w']+)\b/ucfirst($1)/ge;
+    $string =~ s/[-,\s\']//g;
+    $string;
+}
+
+my $toPrint = '';
+foreach my $card (sort cardSort @setCards) {
+	my $className = toCamelCase(@{$card}[0]);
+    my $currentFileName = "../Mage.Sets/src/mage/sets/" . $knownSets{$setName} . "/" . $className . ".java";
+	if(! -e $currentFileName) {
+		if ($toPrint) {
+			$toPrint .= "\n";
+		}   
+		$toPrint .= "@{$card}[2]|@{$card}[0]"; 
+	}
+}
+open CARD, "> " . lc($sets{$setName}) . ".txt";
+print CARD $toPrint;
+close CARD;
+
