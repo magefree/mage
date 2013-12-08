7b584e17bf07251c4f35c29d726f8449e87b1e4e
LevelX2 <ludwig.hirth@online.de>
2013-12-08 10:56:58 +0100

    * Kruin Outlaw - Fixed that the werewolve blocking restriction will only be applied if Kurin Outlaw is transformed.

--- Mage.Sets/src/mage/sets/innistrad/TerrorOfKruinPass.java
+++ Mage.Sets/src/mage/sets/innistrad/TerrorOfKruinPass.java
@@ -28,21 +28,24 @@
 package mage.sets.innistrad;
 
 import java.util.UUID;
-import mage.constants.CardType;
-import mage.constants.Rarity;
-import mage.constants.TargetController;
-import mage.constants.Zone;
 import mage.MageInt;
 import mage.abilities.TriggeredAbility;
 import mage.abilities.common.BeginningOfUpkeepTriggeredAbility;
 import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.condition.common.TransformedCondition;
 import mage.abilities.condition.common.TwoOrMoreSpellsWereCastLastTurnCondition;
+import mage.abilities.decorator.ConditionalContinousEffect;
 import mage.abilities.decorator.ConditionalTriggeredAbility;
+import mage.abilities.effects.Effect;
 import mage.abilities.effects.common.TransformSourceEffect;
 import mage.abilities.effects.common.combat.CantBeBlockedByOneAllEffect;
 import mage.abilities.keyword.DoubleStrikeAbility;
 import mage.abilities.keyword.TransformAbility;
 import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Rarity;
+import mage.constants.TargetController;
+import mage.constants.Zone;
 import mage.filter.common.FilterCreaturePermanent;
 import mage.filter.predicate.mageobject.SubtypePredicate;
 import mage.filter.predicate.permanent.ControllerPredicate;
@@ -53,8 +56,8 @@ import mage.filter.predicate.permanent.ControllerPredicate;
  */
 public class TerrorOfKruinPass extends CardImpl<TerrorOfKruinPass> {
 
+    private static final String ruleText = "Each Werewolf you control can't be blocked except by two or more creatures";
     private static final FilterCreaturePermanent filter = new FilterCreaturePermanent("Werewolf you control");
-
     static {
         filter.add(new ControllerPredicate(TargetController.YOU));
         filter.add(new SubtypePredicate("Werewolf"));
@@ -75,7 +78,8 @@ public class TerrorOfKruinPass extends CardImpl<TerrorOfKruinPass> {
 
         this.addAbility(DoubleStrikeAbility.getInstance());
         // Each Werewolf you control can't be blocked except by two or more creatures.
-        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new CantBeBlockedByOneAllEffect(2, filter)));
+        Effect effect = new ConditionalContinousEffect(new CantBeBlockedByOneAllEffect(2, filter), new TransformedCondition(), ruleText);
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, effect));
         // At the beginning of each upkeep, if a player cast two or more spells last turn, transform Terror of Kruin Pass.
         TriggeredAbility ability = new BeginningOfUpkeepTriggeredAbility(new TransformSourceEffect(false), TargetController.ANY, false);
         this.addAbility(new ConditionalTriggeredAbility(ability, TwoOrMoreSpellsWereCastLastTurnCondition.getInstance(), TransformAbility.TWO_OR_MORE_SPELLS_TRANSFORM_RULE));
