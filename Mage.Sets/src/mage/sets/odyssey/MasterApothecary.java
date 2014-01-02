a25338b2ad4642d7271a302ec7a5ff1f4cab7023
Cowboy <Cowboy@Cowboy-PC.home>
2014-01-02 16:21:26 -0500

    Additional Odyssey cards

---
+++ Mage.Sets/src/mage/sets/odyssey/MasterApothecary.java
@@ -0,0 +1,84 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.odyssey;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.TapTargetCost;
+import mage.abilities.effects.common.PreventDamageTargetEffect;
+import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Rarity;
+import mage.constants.Zone;
+import mage.filter.common.FilterControlledCreaturePermanent;
+import mage.filter.predicate.mageobject.SubtypePredicate;
+import mage.target.common.TargetControlledCreaturePermanent;
+import mage.target.common.TargetCreatureOrPlayer;
+
+/**
+ *
+ * @author cbt33
+ */
+public class MasterApothecary extends CardImpl<MasterApothecary> {
+    
+    public static final FilterControlledCreaturePermanent filter = new FilterControlledCreaturePermanent("Cleric");
+    
+    static {
+        filter.add(new SubtypePredicate("Cleric"));
+    }
+
+    public MasterApothecary(UUID ownerId) {
+        super(ownerId, 32, "Master Apothecary", Rarity.RARE, new CardType[]{CardType.CREATURE}, "{W}{W}{W}");
+        this.expansionSetCode = "ODY";
+        this.subtype.add("Human");
+        this.subtype.add("Cleric");
+
+        this.color.setWhite(true);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(2);
+
+        // Tap an untapped Cleric you control: Prevent the next 2 damage that would be dealt to target creature or player this turn.
+        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, 
+                                                        new PreventDamageTargetEffect(Duration.EndOfTurn, 2), 
+                                                        new TapTargetCost(new TargetControlledCreaturePermanent(1, 1, filter, true, true)));
+        ability.addTarget(new TargetCreatureOrPlayer(true));
+        this.addAbility(ability);
+    }
+
+    public MasterApothecary(final MasterApothecary card) {
+        super(card);
+    }
+
+    @Override
+    public MasterApothecary copy() {
+        return new MasterApothecary(this);
+    }
+}
