4ca981265ccb69e3c274a76afd57f12ea5ba3ca6
LevelX2 <ludwig.hirth@online.de>
2014-01-23 00:29:21 +0100

    [BNG] Added 2 cards.

---
+++ Mage.Sets/src/mage/sets/bornofthegods/DawnToDusk.java
@@ -0,0 +1,88 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.bornofthegods;
+
+import java.util.UUID;
+import mage.abilities.Mode;
+import mage.abilities.effects.common.DestroyTargetEffect;
+import mage.abilities.effects.common.ReturnFromGraveyardToHandTargetEffect;
+import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Rarity;
+import mage.constants.TargetController;
+import mage.filter.FilterCard;
+import mage.filter.FilterPermanent;
+import mage.filter.predicate.mageobject.CardTypePredicate;
+import mage.filter.predicate.permanent.ControllerPredicate;
+import mage.target.TargetPermanent;
+import mage.target.common.TargetCardInYourGraveyard;
+
+/**
+ *
+ * @author LevelX2
+ */
+public class DawnToDusk extends CardImpl<DawnToDusk> {
+
+    private static final FilterCard filterCard = new FilterCard("enchantment card");
+    private static final FilterPermanent filter = new FilterPermanent("enchantment");
+    static {
+        filterCard.add(new CardTypePredicate(CardType.ENCHANTMENT));
+        filter.add(new CardTypePredicate(CardType.ENCHANTMENT));
+    }
+
+    static {
+        filter.add(new ControllerPredicate(TargetController.OPPONENT));
+    }
+    public DawnToDusk(UUID ownerId) {
+        super(ownerId, 6, "Dawn to Dusk", Rarity.UNCOMMON, new CardType[]{CardType.SORCERY}, "{2}{W}{W}");
+        this.expansionSetCode = "BNG";
+
+        this.color.setWhite(true);
+
+        // Choose one or both -
+        this.getSpellAbility().getModes().setMinModes(1);
+        this.getSpellAbility().getModes().setMaxModes(2);
+        // Return target enchantment card from your graveyard to your hand;
+        this.getSpellAbility().addEffect(new ReturnFromGraveyardToHandTargetEffect(true));
+        this.getSpellAbility().addTarget(new TargetCardInYourGraveyard(filterCard));
+        // and/or destroy target enchantment.
+        Mode mode = new Mode();
+        mode.getEffects().add(new DestroyTargetEffect());
+        mode.getTargets().add(new TargetPermanent(filter, true));
+        this.getSpellAbility().addMode(mode);
+    }
+
+    public DawnToDusk(final DawnToDusk card) {
+        super(card);
+    }
+
+    @Override
+    public DawnToDusk copy() {
+        return new DawnToDusk(this);
+    }
+}
