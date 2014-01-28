89ac171b24980aac6eb72baae2ecc78eebbc9ea3
LevelX2 <ludwig.hirth@online.de>
2014-01-28 17:04:24 +0100

    * Hunter's Prowess - Added missing trample ability.

--- Mage.Sets/src/mage/sets/bornofthegods/HuntersProwess.java
+++ Mage.Sets/src/mage/sets/bornofthegods/HuntersProwess.java
@@ -34,6 +34,7 @@ import mage.abilities.effects.Effect;
 import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.continious.BoostTargetEffect;
 import mage.abilities.effects.common.continious.GainAbilityTargetEffect;
+import mage.abilities.keyword.TrampleAbility;
 import mage.cards.CardImpl;
 import mage.constants.CardType;
 import mage.constants.Duration;
@@ -57,11 +58,14 @@ public class HuntersProwess extends CardImpl<HuntersProwess> {
 
         // Until end of turn, target creature gets +3/+3 and gains trample and "Whenever this creature deals combat damage to a player, draw that many cards."
         Effect effect = new BoostTargetEffect(3,3, Duration.EndOfTurn);
-        effect.setText("Until end of turn, target creature gets +3/+3");
+        effect.setText("Until end of turn, target creature gets +3/+3");        
         this.getSpellAbility().addEffect(effect);
+        effect = new GainAbilityTargetEffect(TrampleAbility.getInstance(), Duration.EndOfTurn);
+        effect.setText("and gains trample");        
+        this.getSpellAbility().addEffect(effect);        
         Ability grantedAbility = new DealsCombatDamageToAPlayerTriggeredAbility(new HuntersProwessDrawEffect(), false, true);
         this.getSpellAbility().addEffect(new GainAbilityTargetEffect(grantedAbility, Duration.EndOfTurn,
-                "and gains trample and \"Whenever this creature deals combat damage to a player, draw that many cards.\""));
+                "and \"Whenever this creature deals combat damage to a player, draw that many cards.\""));
         this.getSpellAbility().addTarget(new TargetCreaturePermanent(true));
     }
 
