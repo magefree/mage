b273074e2f3324cb530c9cd8abbaa13cfd332739
LevelX2 <ludwig.hirth@online.de>
2014-02-07 13:54:16 +0100

    * Removed dev version string accidently committed.

--- Mage.Server/src/main/java/mage/server/Main.java
+++ Mage.Server/src/main/java/mage/server/Main.java
@@ -78,7 +78,7 @@ import org.w3c.dom.Element;
 public class Main {
 
     private static final Logger logger = Logger.getLogger(Main.class);
-    private static final MageVersion version = new MageVersion(1, 3, 0, "dev2014-02-03");
+    private static final MageVersion version = new MageVersion(1, 3, 0, "");
 
     private static final String testModeArg = "-testMode=";
     private static final String adminPasswordArg = "-adminPassword=";
