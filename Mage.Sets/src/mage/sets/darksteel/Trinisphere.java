99e321f64abea85391ec87ab615f0f12731d87f2
LevelX2 <ludwig.hirth@online.de>
2014-04-30 07:15:21 +0200

    Minor formatting.

--- Mage.Sets/src/mage/sets/darksteel/Trinisphere.java
+++ Mage.Sets/src/mage/sets/darksteel/Trinisphere.java
@@ -70,10 +70,9 @@ public class Trinisphere extends CardImpl<Trinisphere> {
 
 class TrinisphereEffect extends CostModificationEffectImpl<TrinisphereEffect> {
 
-
     public TrinisphereEffect() {
         super(Duration.WhileOnBattlefield, Outcome.Neutral, CostModificationType.SET_COST);
-        this.staticText = "As long as Trinisphere is untapped, each spell that would cost less than three mana to cast costs three mana to cast";
+        this.staticText = "As long as {this} is untapped, each spell that would cost less than three mana to cast costs three mana to cast";
     }
 
     protected TrinisphereEffect(TrinisphereEffect effect) {
