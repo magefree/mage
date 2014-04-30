367dd0b228dbd9c7993d33a2c2ce6c1bda6f40ab
LevelX2 <ludwig.hirth@online.de>
2014-04-30 17:15:34 +0200

    Fixed a bug that spells with alternative costs are written to the game log as if the alternative cost was always used.

--- Mage/src/mage/abilities/AbilityImpl.java
+++ Mage/src/mage/abilities/AbilityImpl.java
@@ -918,7 +918,7 @@ public abstract class AbilityImpl<T extends AbilityImpl<T>> implements Ability {
             if (ability instanceof OptionalAdditionalSourceCosts) {
                 sb.append(((OptionalAdditionalSourceCosts) ability).getCastMessageSuffix());
             }
-            if (ability instanceof AlternativeSourceCosts) {
+            if (ability instanceof AlternativeSourceCosts && ((AlternativeSourceCosts) ability).isActivated()) {
                 sb.append(((AlternativeSourceCosts) ability).getCastMessageSuffix());
             }
         }
