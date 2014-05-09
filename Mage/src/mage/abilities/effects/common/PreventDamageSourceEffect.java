ef571dc00fe2d08f47561a651dbe28620eb060ed
Quercitron <oldquercitron@gmail.com>
2014-05-10 03:29:08 +0400

    Add PreventDamageSourceEffect

---
+++ Mage/src/mage/abilities/effects/common/PreventDamageSourceEffect.java
@@ -0,0 +1,106 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.abilities.effects.common;
+
+import mage.constants.Duration;
+import mage.abilities.Ability;
+import mage.abilities.Mode;
+import mage.abilities.effects.PreventionEffectImpl;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+
+/**
+ *
+ * @author Quercitron
+ */
+public class PreventDamageSourceEffect extends PreventionEffectImpl<PreventDamageSourceEffect> {
+
+    private int amount;
+
+    public PreventDamageSourceEffect(Duration duration, int amount) {
+        super(duration);
+        this.amount = amount;
+    }
+
+    public PreventDamageSourceEffect(final PreventDamageSourceEffect effect) {
+        super(effect);
+        this.amount = effect.amount;
+    }
+
+    @Override
+    public PreventDamageSourceEffect copy() {
+        return new PreventDamageSourceEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        return true;
+    }
+
+    @Override
+    public boolean replaceEvent(GameEvent event, Ability source, Game game) {
+        GameEvent preventEvent = new GameEvent(GameEvent.EventType.PREVENT_DAMAGE, source.getSourceId(), source.getId(), source.getControllerId(), event.getAmount(), false);
+        if (!game.replaceEvent(preventEvent)) {
+            if (event.getAmount() >= this.amount) {
+                int damage = amount;
+                event.setAmount(event.getAmount() - amount);
+                this.used = true;
+                game.fireEvent(GameEvent.getEvent(GameEvent.EventType.PREVENTED_DAMAGE, source.getSourceId(), source.getId(), source.getControllerId(), damage));
+            } else {
+                int damage = event.getAmount();
+                event.setAmount(0);
+                amount -= damage;
+                game.fireEvent(GameEvent.getEvent(GameEvent.EventType.PREVENTED_DAMAGE, source.getSourceId(), source.getId(), source.getControllerId(), damage));
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean applies(GameEvent event, Ability source, Game game) {
+        if (!this.used && super.applies(event, source, game) && event.getTargetId().equals(source.getSourceId())) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String getText(Mode mode) {
+        if (staticText != null && !staticText.isEmpty()) {
+            return staticText;
+        }
+        StringBuilder sb = new StringBuilder();
+        if (amount == Integer.MAX_VALUE) {
+            sb.append("Prevent all damage that would be dealt to ");
+        } else {
+            sb.append("Prevent the next ").append(amount).append(" damage that would be dealt to ");
+        }
+        sb.append("{source} ").append(duration.toString());
+        return sb.toString();
+    }
+}
