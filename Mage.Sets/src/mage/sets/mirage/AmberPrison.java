259a84109054537d8a5176d66f5b85f32fb33157
Quercitron <oldquercitron@gmail.com>
2014-05-23 23:53:27 +0400

    [6ED] Add 19 cards

---
+++ Mage.Sets/src/mage/sets/mirage/AmberPrison.java
@@ -0,0 +1,215 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.mirage;
+
+import java.util.UUID;
+import mage.abilities.Ability;
+import mage.abilities.TriggeredAbilityImpl;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.common.SkipUntapOptionalAbility;
+import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.costs.mana.GenericManaCost;
+import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.RestrictionEffect;
+import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Outcome;
+import mage.constants.Rarity;
+import mage.constants.Zone;
+import mage.filter.FilterPermanent;
+import mage.filter.predicate.Predicates;
+import mage.filter.predicate.mageobject.CardTypePredicate;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.permanent.Permanent;
+import mage.target.TargetPermanent;
+
+/**
+ *
+ * @author Quercitron
+ */
+public class AmberPrison extends CardImpl<AmberPrison> {
+
+    private static final FilterPermanent filter = new FilterPermanent("artifact, creature, or land");
+    
+    static {
+        filter.add(Predicates.or(
+                new CardTypePredicate(CardType.ARTIFACT),
+                new CardTypePredicate(CardType.CREATURE),
+                new CardTypePredicate(CardType.LAND)));
+    }
+    
+    public AmberPrison(UUID ownerId) {
+        super(ownerId, 257, "Amber Prison", Rarity.RARE, new CardType[]{CardType.ARTIFACT}, "{4}");
+        this.expansionSetCode = "MIR";
+
+        // You may choose not to untap Amber Prison during your untap step.
+        this.addAbility(new SkipUntapOptionalAbility());
+        
+        // {4}, {tap}: Tap target artifact, creature, or land. That permanent doesn't untap during its controller's untap step for as long as Amber Prison remains tapped.
+        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new AmberPrisonTapTargetEffect(), new GenericManaCost(4));
+        ability.addCost(new TapSourceCost());
+        ability.addTarget(new TargetPermanent(filter, true));
+        this.addAbility(ability);
+        this.addAbility(new AmberPrisonUntapTriggeredAbility());
+
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new AmberPrisonRestrictionEffect()));
+    }
+
+    public AmberPrison(final AmberPrison card) {
+        super(card);
+    }
+
+    @Override
+    public AmberPrison copy() {
+        return new AmberPrison(this);
+    }
+}
+
+class AmberPrisonTapTargetEffect extends OneShotEffect<AmberPrisonTapTargetEffect> {
+
+    public AmberPrisonTapTargetEffect() {
+        super(Outcome.Tap);
+        this.staticText = "Tap target artifact, creature, or land. That permanent doesn't untap during its controller's untap step for as long as {source} remains tapped.";
+    }
+    
+    public AmberPrisonTapTargetEffect(final AmberPrisonTapTargetEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public AmberPrisonTapTargetEffect copy() {
+        return new AmberPrisonTapTargetEffect(this);
+    }
+    
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Permanent sourcePermanent = game.getPermanent(source.getSourceId());
+        for (UUID target : targetPointer.getTargets(game, source)) {
+            Permanent permanent = game.getPermanent(target);
+            if (permanent != null) {
+                if (sourcePermanent != null) {
+                    sourcePermanent.addConnectedCard("AmberPrison", permanent.getId());
+                }
+                permanent.tap(game);
+            }
+        }
+        return true;
+    }
+    
+}
+
+class AmberPrisonRestrictionEffect extends RestrictionEffect<AmberPrisonRestrictionEffect> {
+
+    public AmberPrisonRestrictionEffect() {
+        super(Duration.WhileOnBattlefield);
+    }
+    
+    public AmberPrisonRestrictionEffect(final AmberPrisonRestrictionEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public AmberPrisonRestrictionEffect copy() {
+        return new AmberPrisonRestrictionEffect(this);
+    }
+    
+    @Override
+    public boolean applies(Permanent permanent, Ability source, Game game) {
+        Permanent sourcePermanent = game.getPermanent(source.getSourceId());
+        if (sourcePermanent != null && sourcePermanent.isTapped()) {
+            if (sourcePermanent.getConnectedCards("AmberPrison") != null) {
+                return sourcePermanent.getConnectedCards("AmberPrison").contains(permanent.getId());
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean canBeUntapped(Permanent permanent, Game game) {
+        return false;
+    }
+    
+}
+
+
+class AmberPrisonUntapTriggeredAbility extends TriggeredAbilityImpl<AmberPrisonUntapTriggeredAbility> {
+
+    public AmberPrisonUntapTriggeredAbility() {
+        super(Zone.BATTLEFIELD, new AmberPrisonReleaseOnUntapEffect(), false);
+        this.usesStack = false;
+        this.ruleVisible = false;
+    }
+    
+    public AmberPrisonUntapTriggeredAbility(final AmberPrisonUntapTriggeredAbility ability) {
+        super(ability);
+    }
+    
+    @Override
+    public AmberPrisonUntapTriggeredAbility copy() {
+        return new AmberPrisonUntapTriggeredAbility(this);
+    }
+
+    @Override
+    public boolean checkTrigger(GameEvent event, Game game) {
+        if (event.getType().equals(GameEvent.EventType.UNTAPPED) && event.getTargetId().equals(this.getSourceId())) {
+            return true;
+        }
+        return false;
+    }
+    
+}
+
+class AmberPrisonReleaseOnUntapEffect extends OneShotEffect<AmberPrisonReleaseOnUntapEffect> {
+
+    public AmberPrisonReleaseOnUntapEffect() {
+        super(Outcome.Detriment);
+    }
+    
+    public AmberPrisonReleaseOnUntapEffect(final AmberPrisonReleaseOnUntapEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public AmberPrisonReleaseOnUntapEffect copy() {
+        return new AmberPrisonReleaseOnUntapEffect(this);
+    }
+    
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Permanent sourcePermanent = game.getPermanent(source.getSourceId());
+        if (sourcePermanent != null) {
+            sourcePermanent.clearConnectedCards("AmberPrison");
+            return true;
+        }
+        return false;
+    }
+    
+}
---
+++ Mage.Sets/src/mage/sets/seventhedition/FinalFortune.java
@@ -0,0 +1,168 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.seventhedition;
+
+import java.util.UUID;
+import mage.abilities.Ability;
+import mage.abilities.DelayedTriggeredAbility;
+import mage.abilities.effects.OneShotEffect;
+import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Outcome;
+import mage.constants.Rarity;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.turn.TurnMod;
+import mage.players.Player;
+
+/**
+ *
+ * @author Quercitron
+ */
+public class FinalFortune extends CardImpl<FinalFortune> {
+
+    public FinalFortune(UUID ownerId) {
+        super(ownerId, 182, "Final Fortune", Rarity.RARE, new CardType[]{CardType.INSTANT}, "{R}{R}");
+        this.expansionSetCode = "7ED";
+
+        this.color.setRed(true);
+
+        // Take an extra turn after this one. At the beginning of that turn's end step, you lose the game.
+        this.getSpellAbility().addEffect(new FinalFortuneEffect());
+    }
+
+    public FinalFortune(final FinalFortune card) {
+        super(card);
+    }
+
+    @Override
+    public FinalFortune copy() {
+        return new FinalFortune(this);
+    }
+}
+
+class FinalFortuneEffect extends OneShotEffect<FinalFortuneEffect> {
+
+    public FinalFortuneEffect() {
+        super(Outcome.AIDontUseIt);
+        this.staticText = "Take an extra turn after this one. At the beginning of that turn's end step, you lose the game.";
+    }
+    
+    public FinalFortuneEffect(final FinalFortuneEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public FinalFortuneEffect copy() {
+        return new FinalFortuneEffect(this);
+    }
+    
+    @Override
+    public boolean apply(Game game, Ability source) {
+        // Take an extra turn after this one
+        TurnMod extraTurn = new TurnMod(source.getControllerId(), false);
+        game.getState().getTurnMods().add(extraTurn);
+        
+        FinalFortuneLoseDelayedTriggeredAbility delayedTriggeredAbility = new FinalFortuneLoseDelayedTriggeredAbility();
+        delayedTriggeredAbility.setSourceId(source.getSourceId());
+        delayedTriggeredAbility.setControllerId(source.getControllerId());
+        delayedTriggeredAbility.setConnectedTurnMod(extraTurn.getId());
+        game.addDelayedTriggeredAbility(delayedTriggeredAbility);
+        
+        return true;
+    }
+    
+}
+
+class FinalFortuneLoseDelayedTriggeredAbility extends DelayedTriggeredAbility<FinalFortuneLoseDelayedTriggeredAbility> {
+
+    private UUID connectedTurnMod;
+
+    public FinalFortuneLoseDelayedTriggeredAbility() {
+        super(new FinalFortuneLoseEffect(), Duration.EndOfGame);
+    }
+
+    public FinalFortuneLoseDelayedTriggeredAbility(final FinalFortuneLoseDelayedTriggeredAbility ability) {
+        super(ability);
+        this.connectedTurnMod = ability.connectedTurnMod;
+    }
+
+    @Override
+    public FinalFortuneLoseDelayedTriggeredAbility copy() {
+        return new FinalFortuneLoseDelayedTriggeredAbility(this);
+    }
+
+    @Override
+    public boolean checkTrigger(GameEvent event, Game game) {
+        if (event.getType() == GameEvent.EventType.END_PHASE_PRE) {
+            if (connectedTurnMod != null && connectedTurnMod.equals(game.getState().getTurnId())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void setConnectedTurnMod(UUID connectedTurnMod) {
+        this.connectedTurnMod = connectedTurnMod;
+    }
+
+    @Override
+    public String getRule() {
+        return "At the beginning of that turn's end step, you lose the game";
+    }
+    
+}
+
+class FinalFortuneLoseEffect extends OneShotEffect<FinalFortuneLoseEffect> {
+
+    public FinalFortuneLoseEffect() {
+        super(Outcome.Detriment);
+        this.staticText = "You lose the game";
+    }
+    
+    public FinalFortuneLoseEffect(final FinalFortuneLoseEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public FinalFortuneLoseEffect copy() {
+        return new FinalFortuneLoseEffect(this);
+    }
+    
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Player player = game.getPlayer(source.getControllerId());
+        if (player != null) {
+            player.lost(game);
+            return true;
+        }
+        return false;
+    }
+    
+}
