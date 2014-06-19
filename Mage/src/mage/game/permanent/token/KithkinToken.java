c08d266fce167a5af6f3a57837888c11bff64c41
LevelX2 <ludwig.hirth@online.de>
2014-06-20 00:47:01 +0200

    * Added reference for Kithkin Token.

--- Mage/src/mage/game/permanent/token/KithkinToken.java
+++ Mage/src/mage/game/permanent/token/KithkinToken.java
@@ -10,7 +10,12 @@ import mage.constants.CardType;
 public class KithkinToken extends Token{
 
     public KithkinToken() {
+        this("LRW");
+    }
+
+    public KithkinToken(String expansionSetCode) {
         super("Kithkin", "1/1 white Kithkin Soldier creature token");
+        setOriginalExpansionSetCode(expansionSetCode);
         cardType.add(CardType.CREATURE);
         color.setWhite(true);
         subtype.add("Kithkin");
