12851653510228a6011b7b098300c833fe610610
emerald000 <emerald000@hotmail.com>
2014-07-08 23:51:28 -0400

    [M15] Added 1 artifact and 4 black cards.

---
+++ Mage.Sets/src/mage/sets/magic2015/BloodHost.java
@@ -0,0 +1,117 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.magic2015;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.SacrificeTargetCost;
+import mage.abilities.costs.mana.ColoredManaCost;
+import mage.abilities.costs.mana.GenericManaCost;
+import mage.abilities.effects.Effect;
+import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.GainLifeEffect;
+import mage.abilities.effects.common.counter.AddCountersSourceEffect;
+import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.ColoredManaSymbol;
+import mage.constants.Outcome;
+import mage.constants.Rarity;
+import mage.constants.Zone;
+import mage.counters.CounterType;
+import mage.filter.common.FilterControlledCreaturePermanent;
+import mage.filter.common.FilterControlledPermanent;
+import mage.filter.predicate.permanent.AnotherPredicate;
+import mage.game.Game;
+import mage.target.common.TargetControlledPermanent;
+
+/**
+ *
+ * @author emerald000
+ */
+public class BloodHost extends CardImpl {
+    
+    private static final FilterControlledPermanent filter = new FilterControlledCreaturePermanent("another creature");
+    static {
+        filter.add(new AnotherPredicate());
+    }
+
+    public BloodHost(UUID ownerId) {
+        super(ownerId, 87, "Blood Host", Rarity.UNCOMMON, new CardType[]{CardType.CREATURE}, "{3}{B}{B}");
+        this.expansionSetCode = "M15";
+        this.subtype.add("Vampire");
+
+        this.color.setBlack(true);
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(3);
+
+        // {1}{B}, Sacrifice another creature: Put a +1/+1 counter on Blood Host
+        Effect effect = new AddCountersSourceEffect(CounterType.P1P1.createInstance());
+        effect.setText("Put a +1/+1 counter on {this}");
+        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, effect, new SacrificeTargetCost(new TargetControlledPermanent(filter)));
+        // and you gain 2 life.
+        effect = new GainLifeEffect(2);
+        effect.setText("and you gain 2 life");
+        ability.addEffect(effect);
+        ability.addCost(new GenericManaCost(1));
+        ability.addCost(new ColoredManaCost(ColoredManaSymbol.B));
+        this.addAbility(ability);
+    }
+
+    public BloodHost(final BloodHost card) {
+        super(card);
+    }
+
+    @Override
+    public BloodHost copy() {
+        return new BloodHost(this);
+    }
+}
+
+class BloodHostEffect extends OneShotEffect {
+    
+    BloodHostEffect() {
+        super(Outcome.Benefit);
+        this.staticText = "Put a +1/+1 counter on {this} and you gain 2 life.";
+    }
+    
+    BloodHostEffect(final BloodHostEffect effect) {
+        super(effect);
+    }
+    
+    @Override
+    public BloodHostEffect copy() {
+        return new BloodHostEffect(this);
+    }
+    
+    @Override
+    public boolean apply(Game game, Ability source) {
+        return false;
+    }
+}
