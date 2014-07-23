b4867ca7dcbe09b02b566db3ba8c6d7aa50e508a
LevelX2 <ludwig.hirth@online.de>
2014-07-23 23:01:25 +0200

    * Stab Wound - Fixed wrong rarity.

--- Mage.Sets/src/mage/sets/magic2015/StabWound.java
+++ Mage.Sets/src/mage/sets/magic2015/StabWound.java
@@ -28,6 +28,7 @@
 package mage.sets.magic2015;
 
 import java.util.UUID;
+import mage.constants.Rarity;
 
 /**
  *
@@ -37,6 +38,7 @@ public class StabWound extends mage.sets.returntoravnica.StabWound {
 
     public StabWound(UUID ownerId) {
         super(ownerId);
+        this.rarity = Rarity.UNCOMMON;
         this.cardNumber = 116;
         this.expansionSetCode = "M15";
     }
