82ba4a83e846f012d4be6ac6289f0a8a9e4ef6b3
LevelX2 <ludwig.hirth@online.de>
2014-10-25 22:49:40 +0200

    * Outlast - Fixed typo of tooltip text.

--- Mage/src/mage/abilities/keyword/OutlastAbility.java
+++ Mage/src/mage/abilities/keyword/OutlastAbility.java
@@ -59,7 +59,7 @@ public class OutlastAbility extends ActivatedAbilityImpl {
 
     @Override
     public String getRule() {
-        StringBuilder sb = new StringBuilder("Outcast ").append(manaCosts.getText());
+        StringBuilder sb = new StringBuilder("Outlast ").append(manaCosts.getText());
         sb.append(" <i>(").append(getManaCosts().getText()).append(", ").append(getCosts().getText()).append(":  Put a +1/+1 counter on this creature. Outlast only as a sorcery.)</i>").toString();
         return sb.toString();
     }
