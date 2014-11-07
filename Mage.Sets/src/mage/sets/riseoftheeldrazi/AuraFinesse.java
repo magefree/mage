387538154676f19138df85857065dd0be83222c9
LevelX2 <ludwig.hirth@online.de>
2014-11-07 13:46:34 +0100

    * Aura Finesse - Fixed that the aura was wrongly also moved if the target creature was no valid target for the moved aura.

--- Mage.Sets/src/mage/sets/riseoftheeldrazi/AuraFinesse.java
+++ Mage.Sets/src/mage/sets/riseoftheeldrazi/AuraFinesse.java
@@ -41,6 +41,8 @@ import mage.filter.predicate.mageobject.SubtypePredicate;
 import mage.filter.predicate.permanent.ControllerPredicate;
 import mage.game.Game;
 import mage.game.permanent.Permanent;
+import mage.players.Player;
+import mage.target.Target;
 import mage.target.TargetPermanent;
 import mage.target.common.TargetCreaturePermanent;
 
@@ -99,16 +101,23 @@ class AuraFinesseEffect extends OneShotEffect {
 
     @Override
     public boolean apply(Game game, Ability source) {
-        Permanent aura = game.getPermanent(source.getFirstTarget());
-        Permanent creature = game.getPermanent(source.getTargets().get(1).getFirstTarget());
-        if (aura != null && creature != null) {
-            Permanent oldCreature = game.getPermanent(aura.getAttachedTo());
-            if (oldCreature == null || oldCreature.equals(creature)) {
-                return false;
-            }
-            if (oldCreature.removeAttachment(aura.getId(), game)) {
-                return creature.addAttachment(aura.getId(), game);
-            }
+        Player controller = game.getPlayer(source.getControllerId());
+        if (controller != null) {
+            Permanent aura = game.getPermanent(source.getFirstTarget());
+            Permanent creature = game.getPermanent(source.getTargets().get(1).getFirstTarget());
+            if (aura != null && creature != null) {
+                Permanent oldCreature = game.getPermanent(aura.getAttachedTo());
+                if (oldCreature != null && !oldCreature.equals(creature)) {
+                    Target auraTarget = aura.getSpellAbility().getTargets().get(0);
+                    if (!auraTarget.canTarget(creature.getId(), game))  {
+                        game.informPlayers(aura.getLogName() + " was not attched to " +creature.getLogName() + " because it's no legal target for the aura" );
+                    } else if (oldCreature.removeAttachment(aura.getId(), game)) {
+                        game.informPlayers(aura.getLogName() + " was unattached from " + oldCreature.getLogName() + " and attached to " + creature.getLogName());
+                        creature.addAttachment(aura.getId(), game);
+                    }
+                }
+            }            
+            return true;
         }
         return false;
     }
