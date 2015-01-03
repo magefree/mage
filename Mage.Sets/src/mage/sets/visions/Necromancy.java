09a099facd4984000383e9784d154aaa42bf7553
LevelX2 <ludwig.hirth@online.de>
2015-01-03 23:47:30 +0100

    Minor changes to some cards.

--- Mage.Sets/src/mage/sets/visions/Necromancy.java
+++ Mage.Sets/src/mage/sets/visions/Necromancy.java
@@ -248,41 +248,41 @@ class NecromancyLeavesBattlefieldTriggeredEffect extends OneShotEffect {
     }
 }
 
-class NecromancyAttachEffect extends OneShotEffect {
-
-    public NecromancyAttachEffect(Outcome outcome) {
-        super(outcome);
-    }
-
-    public NecromancyAttachEffect(Outcome outcome, String rule) {
-        super(outcome);
-        staticText = rule;
-    }
-
-    public NecromancyAttachEffect(final NecromancyAttachEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public NecromancyAttachEffect copy() {
-        return new NecromancyAttachEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Card card = game.getCard(source.getFirstTarget());
-        if (card != null && game.getState().getZone(source.getFirstTarget()).equals(Zone.GRAVEYARD)) {
-            // Card have no attachedTo attribute yet so write ref only to enchantment now
-            Permanent enchantment = game.getPermanent(source.getSourceId());
-            if (enchantment != null) {
-                enchantment.attachTo(card.getId(), game);
-            }
-            return true;
-        }
-        return false;
-    }
-
-}
+//class NecromancyAttachEffect extends OneShotEffect {
+//
+//    public NecromancyAttachEffect(Outcome outcome) {
+//        super(outcome);
+//    }
+//
+//    public NecromancyAttachEffect(Outcome outcome, String rule) {
+//        super(outcome);
+//        staticText = rule;
+//    }
+//
+//    public NecromancyAttachEffect(final NecromancyAttachEffect effect) {
+//        super(effect);
+//    }
+//
+//    @Override
+//    public NecromancyAttachEffect copy() {
+//        return new NecromancyAttachEffect(this);
+//    }
+//
+//    @Override
+//    public boolean apply(Game game, Ability source) {
+//        Card card = game.getCard(source.getFirstTarget());
+//        if (card != null && game.getState().getZone(source.getFirstTarget()).equals(Zone.GRAVEYARD)) {
+//            // Card have no attachedTo attribute yet so write ref only to enchantment now
+//            Permanent enchantment = game.getPermanent(source.getSourceId());
+//            if (enchantment != null) {
+//                enchantment.attachTo(card.getId(), game);
+//            }
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//}
 
 class NecromancyChangeAbilityEffect extends ContinuousEffectImpl implements SourceEffect {
 
@@ -319,7 +319,7 @@ class NecromancyChangeAbilityEffect extends ContinuousEffectImpl implements Sour
 
     @Override
     public boolean apply(Layer layer, SubLayer sublayer, Ability source, Game game) {
-        Permanent permanent = affectedObjectList.get(0).getPermanent(game);;
+        Permanent permanent = affectedObjectList.get(0).getPermanent(game);
         if (permanent != null) {
             switch (layer) {
                 case TypeChangingEffects_4:
