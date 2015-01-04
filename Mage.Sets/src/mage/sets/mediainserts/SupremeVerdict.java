6ba2f067867f600db2f735a8e44ae2c84ed69492
Alchus <lordalch@gmail.com>
2015-01-04 16:45:47 -0600

    Implemented some cards from the Media Inserts set. This includes Buy-a-Box promos and other unique printings. These cards are not listed in Gatherer.

---
+++ Mage.Sets/src/mage/sets/mediainserts/SupremeVerdict.java
@@ -0,0 +1,21 @@
+package mage.sets.mediainserts;
+
+import java.util.UUID;
+
+public class SupremeVerdict extends mage.sets.returntoravnica.SupremeVerdict {
+    
+    public SupremeVerdict(UUID ownerId) {
+        super(ownerId);
+        this.cardNumber = 56;
+        this.expansionSetCode = "MBP";
+    }
+    
+    public SupremeVerdict(final SupremeVerdict card) {
+        super(card);
+    }
+    
+    @Override
+    public SupremeVerdict copy() {
+        return new SupremeVerdict(this);
+    }
+}
