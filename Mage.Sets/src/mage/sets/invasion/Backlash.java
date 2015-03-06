767c0de7fe771dc60506606b327078b90b5fd7bc
mnapoleon <michael.napoleon@gmail.com>
2015-03-06 10:29:25 -0500

    Implemented Invasion:Backlash and fixed issue with Explosive Growth after class name change in other module

--- Mage.Sets/src/mage/sets/invasion/Backlash.java
+++ Mage.Sets/src/mage/sets/invasion/Backlash.java
@@ -28,42 +28,70 @@
 package mage.sets.invasion;
 
 import java.util.UUID;
-import mage.abilities.condition.common.KickedCondition;
-import mage.abilities.decorator.ConditionalContinuousEffect;
-import mage.abilities.effects.common.continuous.BoostTargetEffect;
-import mage.abilities.keyword.KickerAbility;
+import mage.abilities.Ability;
+import mage.abilities.effects.OneShotEffect;
 import mage.cards.CardImpl;
 import mage.constants.CardType;
-import mage.constants.Duration;
+import mage.constants.Outcome;
 import mage.constants.Rarity;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.players.Player;
 import mage.target.common.TargetCreaturePermanent;
 
 /**
  *
- * @author michael.napoleon@gmail.com
+ * @author anonymous
  */
-public class ExplosiveGrowth extends CardImpl {
+public class Backlash extends CardImpl {
 
-    public ExplosiveGrowth(UUID ownerId) {
-        super(ownerId, 187, "Explosive Growth", Rarity.COMMON, new CardType[]{CardType.INSTANT}, "{G}");
+    public Backlash(UUID ownerId) {
+        super(ownerId, 234, "Backlash", Rarity.UNCOMMON, new CardType[]{CardType.INSTANT}, "{1}{B}{R}");
         this.expansionSetCode = "INV";
 
-        // Kicker {5}
-        this.addAbility(new KickerAbility("{5}"));
-        
-        // Target creature gets +2/+2 until end of turn. If Explosive Growth was kicked, that creature gets +5/+5 until end of turn instead.
+        // Tap target untapped creature. That creature deals damage equal to its power to its controller.
         this.getSpellAbility().addTarget(new TargetCreaturePermanent());
-        this.getSpellAbility().addEffect(new ConditionalContinuousEffect(new BoostTargetEffect(5, 5, Duration.EndOfTurn),
-          new BoostTargetEffect(2, 2, Duration.EndOfTurn), KickedCondition.getInstance(), 
-          "Target creature gets +2/+2 until end of turn. If Explosive Growth was kicked, that creature gets +5/+5 until end of turn instead."));
+        this.getSpellAbility().addEffect(new BacklashEffect());
     }
 
-    public ExplosiveGrowth(final ExplosiveGrowth card) {
+    public Backlash(final Backlash card) {
         super(card);
     }
 
     @Override
-    public ExplosiveGrowth copy() {
-        return new ExplosiveGrowth(this);
+    public Backlash copy() {
+        return new Backlash(this);
     }
+}
+
+class BacklashEffect extends OneShotEffect {
+  
+  public BacklashEffect() {
+    super(Outcome.Detriment);
+    this.staticText = "Tap target untapped creature. That creature deals damage equal to its power to its controller.";
+  }
+  
+  public BacklashEffect(final BacklashEffect effect) {
+    super(effect);
+  }
+  
+  @Override
+  public BacklashEffect copy () {
+    return new BacklashEffect(this);
+  }
+  
+  @Override
+  public boolean apply(Game game, Ability source) {
+    boolean applied = false;
+    Permanent targetCreature = game.getPermanent(targetPointer.getFirst(game, source));
+    if (targetCreature != null) {
+      applied = targetCreature.tap(game);
+      Player controller = game.getPlayer(targetCreature.getControllerId());
+      if (controller != null) {
+        controller.damage(targetCreature.getPower().getValue(), source.getSourceId(), game, false, true);
+        applied = true;
+      }
+    }
+    return applied;
+  }
 }
