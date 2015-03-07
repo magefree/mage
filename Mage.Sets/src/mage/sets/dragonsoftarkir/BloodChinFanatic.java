e9b89c73edf99a7ca66eccdeed596c2ca48ddeb0
Jeff <Jeff@AJAX>
2015-03-07 16:57:46 -0600

    - Added DTK cards.  Blood-Chin Fanatic, Den Protector, and Descent of the Dragons.

---
+++ Mage.Sets/src/mage/sets/dragonsoftarkir/BloodChinFanatic.java
@@ -0,0 +1,92 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.dragonsoftarkir;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.SacrificeTargetCost;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.dynamicvalue.common.SacrificeCostCreaturesPower;
+import mage.abilities.effects.Effect;
+import mage.abilities.effects.common.GainLifeEffect;
+import mage.abilities.effects.common.LoseLifeTargetEffect;
+import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Rarity;
+import mage.constants.Zone;
+import mage.filter.common.FilterControlledCreaturePermanent;
+import mage.filter.predicate.mageobject.SubtypePredicate;
+import mage.filter.predicate.permanent.AnotherPredicate;
+import mage.target.TargetPlayer;
+import mage.target.common.TargetControlledPermanent;
+
+/**
+ *
+ * @author jeffwadsworth
+ */
+public class BloodChinFanatic extends CardImpl {
+    
+    private static final FilterControlledCreaturePermanent filter = new FilterControlledCreaturePermanent("another Warrior creature");
+    
+    static {
+        filter.add(new AnotherPredicate());
+        filter.add(new SubtypePredicate("Warrior"));
+    }
+
+    public BloodChinFanatic(UUID ownerId) {
+        super(ownerId, 88, "Blood-Chin Fanatic", Rarity.RARE, new CardType[]{CardType.CREATURE}, "{1}{B}{B}");
+        this.expansionSetCode = "DTK";
+        this.subtype.add("Orc");
+        this.subtype.add("Warrior");
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(3);
+
+        // {1}{B}, Sacrifice another Warrior creature: Target player loses X life and you gain X life, where X is the sacrificed creature's power.
+        Effect effect = new LoseLifeTargetEffect(new SacrificeCostCreaturesPower());
+        effect.setText("Target player loses X life");
+        Effect effect2 = new GainLifeEffect(new SacrificeCostCreaturesPower());
+        effect2.setText("and you gain X life, where X is the sacrificed creature's power");
+        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, effect, new ManaCostsImpl("{1}{B}"));
+        ability.addEffect(effect2);
+        ability.addCost(new SacrificeTargetCost(new TargetControlledPermanent(filter)));
+        ability.addTarget(new TargetPlayer());
+        this.addAbility(ability);
+        
+    }
+
+    public BloodChinFanatic(final BloodChinFanatic card) {
+        super(card);
+    }
+
+    @Override
+    public BloodChinFanatic copy() {
+        return new BloodChinFanatic(this);
+    }
+}
---
+++ Mage.Sets/src/mage/sets/dragonsoftarkir/DescentOfTheDragons.java
@@ -0,0 +1,106 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.dragonsoftarkir;
+
+import java.util.UUID;
+import mage.abilities.Ability;
+import mage.abilities.effects.OneShotEffect;
+import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Outcome;
+import mage.constants.Rarity;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.game.permanent.token.DragonToken;
+import mage.players.Player;
+import mage.target.Target;
+import mage.target.common.TargetCreaturePermanent;
+
+/**
+ *
+ * @author jeffwadsworth
+ */
+public class DescentOfTheDragons extends CardImpl {
+
+    public DescentOfTheDragons(UUID ownerId) {
+        super(ownerId, 133, "Descent of the Dragons", Rarity.MYTHIC, new CardType[]{CardType.SORCERY}, "{4}{R}{R}");
+        this.expansionSetCode = "DTK";
+
+        // Destroy any number of target creatures.  For each creature destroyed this way, its controller puts a 4/4 red Dragon creature token with flying onto the battlefield.
+        this.getSpellAbility().addEffect(new DescentOfTheDragonsEffect());
+        this.getSpellAbility().addTarget(new TargetCreaturePermanent(0, Integer.MAX_VALUE));
+
+    }
+
+    public DescentOfTheDragons(final DescentOfTheDragons card) {
+        super(card);
+    }
+
+    @Override
+    public DescentOfTheDragons copy() {
+        return new DescentOfTheDragons(this);
+    }
+}
+
+class DescentOfTheDragonsEffect extends OneShotEffect {
+
+    public DescentOfTheDragonsEffect() {
+        super(Outcome.Benefit);
+        staticText = "Destroy any number of target creatures.  For each creature destroyed this way, its controller puts a 4/4 red Dragon creature token with flying onto the battlefield";
+    }
+
+    public DescentOfTheDragonsEffect(final DescentOfTheDragonsEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public DescentOfTheDragonsEffect copy() {
+        return new DescentOfTheDragonsEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Player controller = game.getPlayer(source.getControllerId());
+        if (controller != null) {
+            for (Target target : source.getTargets()) {
+                for (UUID permanentId : target.getTargets()) {
+                    Permanent permanent = game.getPermanent(permanentId);
+                    if (permanent != null) {
+                        UUID controllerOfTargetId = permanent.getControllerId();
+                        if (permanent.destroy(source.getSourceId(), game, false)) {
+                            DragonToken dragonToken = new DragonToken();
+                            dragonToken.putOntoBattlefield(1, game, source.getSourceId(), controllerOfTargetId);
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+}
