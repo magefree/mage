d9096643fb9890aba7243304632f741d156a7fec
Jeff <Jeff@AJAX>
2015-03-07 22:44:24 -0600

    - Fixed Savage Ventmaw.

--- Mage.Sets/src/mage/sets/dragonsoftarkir/SavageVentmaw.java
+++ Mage.Sets/src/mage/sets/dragonsoftarkir/SavageVentmaw.java
@@ -30,13 +30,16 @@ package mage.sets.dragonsoftarkir;
 import java.util.UUID;
 import mage.MageInt;
 import mage.Mana;
+import mage.abilities.Ability;
 import mage.abilities.common.AttacksTriggeredAbility;
 import mage.abilities.effects.Effect;
-import mage.abilities.effects.common.AddManaToManaPoolTargetControllerEffect;
+import mage.abilities.effects.common.ManaEffect;
 import mage.abilities.keyword.FlyingAbility;
 import mage.cards.CardImpl;
 import mage.constants.CardType;
 import mage.constants.Rarity;
+import mage.game.Game;
+import mage.players.Player;
 
 /**
  *
@@ -55,7 +58,7 @@ public class SavageVentmaw extends CardImpl {
         this.addAbility(FlyingAbility.getInstance());
 
         // Whenever Savage Ventmaw attacks, add {R}{R}{R}{G}{G}{G} to your mana pool. Until end of turn, this mana doesn't empty from your mana pool as steps and phases end.
-        Effect effect = new AddManaToManaPoolTargetControllerEffect(new Mana(3, 0, 3, 0, 0, 0, 0), "your", true);
+        Effect effect = new SavageVentmawManaEffect(new Mana(3, 0, 3, 0, 0, 0, 0), "your", true);
         effect.setText("add {R}{R}{R}{G}{G}{G} to your mana pool. Until end of turn, this mana doesn't empty from your mana pool as steps and phases end");
         this.addAbility(new AttacksTriggeredAbility(effect, false));
 
@@ -69,4 +72,44 @@ public class SavageVentmaw extends CardImpl {
     public SavageVentmaw copy() {
         return new SavageVentmaw(this);
     }
+}
+
+class SavageVentmawManaEffect extends ManaEffect {
+    
+    protected Mana mana;
+    protected boolean emptyOnlyOnTurnsEnd;
+    
+    public SavageVentmawManaEffect(Mana mana, String textManaPoolOwner, boolean emptyOnTurnsEnd) {
+        super();
+        this.mana = mana;
+        this.emptyOnlyOnTurnsEnd = emptyOnTurnsEnd;
+        this.staticText = (textManaPoolOwner.equals("his or her")?"that player adds ":"add ") + mana.toString() + " to " + textManaPoolOwner + " mana pool";
+    }
+
+    public SavageVentmawManaEffect(final SavageVentmawManaEffect effect) {
+        super(effect);
+        this.mana = effect.mana;
+        this.emptyOnlyOnTurnsEnd = effect.emptyOnlyOnTurnsEnd;
+    }
+
+    @Override
+    public SavageVentmawManaEffect copy() {
+        return new SavageVentmawManaEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Player controller = game.getPlayer(source.getControllerId());
+        if (controller != null) {
+            controller.getManaPool().addMana(mana, game, source, emptyOnlyOnTurnsEnd);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Mana getMana(Game game, Ability source) {
+        return mana;
+    }
+
 }
