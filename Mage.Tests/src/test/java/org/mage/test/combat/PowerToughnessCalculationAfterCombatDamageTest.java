e2734814995ed5b5c859c76158b76e83fb6fd31b
LevelX2 <ludwig.hirth@online.de>
2015-03-11 15:02:52 +0100

    Some minor changes.

--- Mage.Tests/src/test/java/org/mage/test/combat/PowerToughnessCalculationAfterCombatDamageTest.java
+++ Mage.Tests/src/test/java/org/mage/test/combat/PowerToughnessCalculationAfterCombatDamageTest.java
@@ -14,13 +14,17 @@ import org.mage.test.serverside.base.CardTestPlayerBase;
 /**
  * Test that calculated p/t is applied after combat damage resolution, so a 2/2 Nighthowler
  * dies if blocked from a 2/2 creature.
- *
+ * All combat damage happens simultaneously. Then any abilities that trigger from combat 
+ * damage go on the stack. In normal circumstances, the combo you're trying to construct doesn't work.
  * @author LevelX2
  */
 public class PowerToughnessCalculationAfterCombatDamageTest extends CardTestPlayerBase {
 
     @Test
     public void powerToughnessCalculation() {
+        // Pain Seer   Creature - Human Wizard 2/2
+        // Inspired - Whenever Pain Seer becomes untapped, reveal the top card of your library and put that card 
+        // into your hand. You lose life equal to that card's converted mana cost.
         addCard(Zone.BATTLEFIELD, playerA, "Pain Seer");
         addCard(Zone.GRAVEYARD, playerA, "Silvercoat Lion");
         // Nighthowler and enchanted creature each get +X/+X, where X is the number of creature cards in all graveyards.
