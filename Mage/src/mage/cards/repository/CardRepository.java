e4cfd917e7302fe583649c31af79cdc3bd33e3b1
LevelX2 <ludwig.hirth@online.de>
2015-03-20 18:34:20 +0100

    * Some minor updates.

--- Mage/src/mage/cards/repository/CardRepository.java
+++ Mage/src/mage/cards/repository/CardRepository.java
@@ -58,7 +58,7 @@ public enum CardRepository {
     private static final String JDBC_URL = "jdbc:h2:file:./db/cards.h2;AUTO_SERVER=TRUE";
     private static final String VERSION_ENTITY_NAME = "card";
     // raise this if db structure was changed
-    private static final long CARD_DB_VERSION = 36;
+    private static final long CARD_DB_VERSION = 37;
     // raise this if new cards were added to the server
     private static final long CARD_CONTENT_VERSION = 9;
 
