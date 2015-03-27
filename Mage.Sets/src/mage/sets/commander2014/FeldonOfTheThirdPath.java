d7ffc19737f54d569d542dbd9e9d87e0c92ad007
LevelX2 <ludwig.hirth@online.de>
2015-03-27 09:32:05 +0100

    * Feldon of the Third Path - Fixed that to the token was no Artifact card type added.

--- Mage.Sets/src/mage/sets/commander2014/FeldonOfTheThirdPath.java
+++ Mage.Sets/src/mage/sets/commander2014/FeldonOfTheThirdPath.java
@@ -106,9 +106,12 @@ class FeldonOfTheThirdPathEffect extends OneShotEffect {
         Card card = game.getCard(getTargetPointer().getFirst(game, source));
         if (card != null) {
             EmptyToken token = new EmptyToken();
-            // This fails if a card will be copied, that uses adjustTargets() method.
+            // TODO: This fails if a card will be copied, that uses adjustTargets() method.
             CardUtil.copyTo(token).from(card);
 
+            if (!token.getCardType().contains(CardType.ARTIFACT)) {
+                token.getCardType().add(CardType.ARTIFACT);
+            }
             token.addAbility(HasteAbility.getInstance());
             token.putOntoBattlefield(1, game, source.getSourceId(), source.getControllerId());
 
