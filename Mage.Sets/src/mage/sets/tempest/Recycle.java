061b600dd2b9f1d1919a5106924dfc3aa0554d14
emerald000 <emerald000@hotmail.com>
2015-04-01 15:15:00 -0400

    [TPR] Added Crashing Boars, Endangered Armodon and Recycle.

---
+++ Mage.Sets/src/mage/sets/tempest/Recycle.java
@@ -0,0 +1,105 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.tempest;
+
+import java.util.UUID;
+import mage.abilities.TriggeredAbilityImpl;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.common.DrawCardSourceControllerEffect;
+import mage.abilities.effects.common.SkipDrawStepEffect;
+import mage.abilities.effects.common.continuous.MaximumHandSizeControllerEffect;
+import mage.abilities.effects.common.continuous.MaximumHandSizeControllerEffect.HandSizeModification;
+import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Rarity;
+import mage.constants.Zone;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.events.GameEvent.EventType;
+
+/**
+ *
+ * @author emerald000
+ */
+public class Recycle extends CardImpl {
+
+    public Recycle(UUID ownerId) {
+        super(ownerId, 142, "Recycle", Rarity.RARE, new CardType[]{CardType.ENCHANTMENT}, "{4}{G}{G}");
+        this.expansionSetCode = "TMP";
+
+        // Skip your draw step.
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new SkipDrawStepEffect())); 
+        
+        // Whenever you play a card, draw a card.
+        this.addAbility(new RecycleTriggeredAbility());
+        
+        // Your maximum hand size is two.
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new MaximumHandSizeControllerEffect(2, Duration.WhileOnBattlefield, HandSizeModification.SET)));
+    }
+
+    public Recycle(final Recycle card) {
+        super(card);
+    }
+
+    @Override
+    public Recycle copy() {
+        return new Recycle(this);
+    }
+}
+
+class RecycleTriggeredAbility extends TriggeredAbilityImpl {
+    
+    RecycleTriggeredAbility() {
+        super(Zone.BATTLEFIELD, new DrawCardSourceControllerEffect(1), false);
+    }
+    
+    RecycleTriggeredAbility(final RecycleTriggeredAbility ability) {
+        super(ability);
+    }
+    
+    @Override
+    public RecycleTriggeredAbility copy() {
+        return new RecycleTriggeredAbility(this);
+    }
+
+    @Override
+    public boolean checkEventType(GameEvent event, Game game) {
+        return event.getType() == EventType.SPELL_CAST || event.getType() == EventType.LAND_PLAYED;
+    }
+    
+    @Override
+    public boolean checkTrigger(GameEvent event, Game game) {
+        return event.getPlayerId().equals(this.getControllerId());
+    }
+    
+    @Override
+    public String getRule() {
+        return "Whenever you play a card, draw a card.";
+    }
+}
