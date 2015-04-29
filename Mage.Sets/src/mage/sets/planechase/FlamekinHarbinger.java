d39dad518d58a0ef7366b7f47511f8e4b2ed3268
LevelX2 <ludwig.hirth@online.de>
2015-04-29 17:41:46 +0200

    * Flamekin Harbinger - The enters the battlefield effect is now optional.

--- Mage.Sets/src/mage/sets/planechase/FlamekinHarbinger.java
+++ Mage.Sets/src/mage/sets/planechase/FlamekinHarbinger.java
@@ -63,7 +63,7 @@ public class FlamekinHarbinger extends CardImpl {
         // When Flamekin Harbinger enters the battlefield, you may search your library for an Elemental card,
         // reveal it, then shuffle your library and put that card on top of it.
         TargetCardInLibrary target = new TargetCardInLibrary(filter);
-        this.addAbility(new EntersBattlefieldTriggeredAbility(new SearchLibraryPutOnLibraryEffect(target, true, true)));
+        this.addAbility(new EntersBattlefieldTriggeredAbility(new SearchLibraryPutOnLibraryEffect(target, true, true), true));
     }
 
     public FlamekinHarbinger(final FlamekinHarbinger card) {
