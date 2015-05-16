f81a4534dc626e4678d3d1d14826bf796ad50039
Luna Skyrise <lunaskyrise@users.noreply.github.com>
2015-05-16 19:26:24 -0300

    [TSP] Add Ghostflame Sliver

---
+++ Mage.Sets/src/mage/sets/timespiral/GhostflameSliver.java
@@ -0,0 +1,104 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.timespiral;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.ObjectColor;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.ContinuousEffectImpl;
+import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Layer;
+import mage.constants.Outcome;
+import mage.constants.Rarity;
+import mage.constants.SubLayer;
+import mage.constants.Zone;
+import mage.filter.FilterPermanent;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+
+/**
+ *
+ * @author anonymous
+ */
+public class GhostflameSliver extends CardImpl {
+
+    public GhostflameSliver(UUID ownerId) {
+        super(ownerId, 239, "Ghostflame Sliver", Rarity.UNCOMMON, new CardType[]{CardType.CREATURE}, "{B}{R}");
+        this.expansionSetCode = "TSP";
+        this.subtype.add("Sliver");
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(2);
+
+        // All Slivers are colorless.
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new GhostflameSliverEffect()));
+    }
+
+    public GhostflameSliver(final GhostflameSliver card) {
+        super(card);
+    }
+
+    @Override
+    public GhostflameSliver copy() {
+        return new GhostflameSliver(this);
+    }
+}
+
+class GhostflameSliverEffect extends ContinuousEffectImpl {
+    
+    private static final FilterPermanent filter = new FilterPermanent("Sliver", "All Slivers");
+
+    private static ObjectColor colorless = new ObjectColor();
+
+    public GhostflameSliverEffect() {
+        super(Duration.WhileOnBattlefield, Layer.ColorChangingEffects_5, SubLayer.NA, Outcome.Benefit);
+        staticText = "All Slivers are colorless";
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        for (Permanent perm : game.getBattlefield().getActivePermanents(source.getControllerId(), game)) {
+            if (filter.match(perm, game)) {
+                perm.getColor().setColor(colorless);
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public GhostflameSliverEffect copy() {
+        return new GhostflameSliverEffect(this);
+    }
+
+    private GhostflameSliverEffect(final GhostflameSliverEffect effect) {
+        super(effect);
+    }
+}
