e324d53d976c969caca4ed24d7912998282aeb01
zweistecken <zweistecken@cpd>
2015-05-19 18:33:10 +0200

    added SetTargetPointer Enum to AttackedByCreatureTriggeredAbility PERMANENT targeting attacking creature PLAYER targeting attacking player modified Garruk Apex Predator (only card using this) to use SetTargetPointer

--- Mage.Sets/src/mage/sets/magic2015/GarrukApexPredator.java
+++ Mage.Sets/src/mage/sets/magic2015/GarrukApexPredator.java
@@ -48,6 +48,7 @@ import mage.constants.CardType;
 import mage.constants.Duration;
 import mage.constants.Outcome;
 import mage.constants.Rarity;
+import mage.constants.SetTargetPointer;
 import mage.constants.Zone;
 import mage.counters.CounterType;
 import mage.filter.FilterPermanent;
@@ -171,7 +172,7 @@ class GarrukApexPredatorEmblem extends Emblem {
         setName("EMBLEM: Garruk, Apex Predator");
         Effect effect = new BoostTargetEffect(5,5,Duration.EndOfTurn);
         effect.setText("it gets +5/+5");
-        Ability ability = new AttackedByCreatureTriggeredAbility(Zone.COMMAND, effect, false, true);
+        Ability ability = new AttackedByCreatureTriggeredAbility(Zone.COMMAND, effect, false, SetTargetPointer.PERMANENT);
         effect = new GainAbilityTargetEffect(TrampleAbility.getInstance(), Duration.EndOfTurn,
                 "and gains trample until end of turn");
         ability.addEffect(effect);
