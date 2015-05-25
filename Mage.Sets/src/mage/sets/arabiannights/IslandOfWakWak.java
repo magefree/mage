0c43184d0a1c4cb40db2603864c2c96f988f1baf
Luna Skyrise <lunaskyrise@users.noreply.github.com>
2015-05-25 13:22:05 -0300

    [ARN, MED] Add Island of Wak-Wak

---
+++ Mage.Sets/src/mage/sets/arabiannights/IslandOfWakWak.java
@@ -0,0 +1,110 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.arabiannights;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.effects.Effect;
+import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.continuous.SetPowerToughnessTargetEffect;
+import mage.abilities.keyword.FlyingAbility;
+import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Outcome;
+import mage.constants.Rarity;
+import mage.constants.Zone;
+import mage.filter.common.FilterCreaturePermanent;
+import mage.filter.predicate.mageobject.AbilityPredicate;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.target.common.TargetCreaturePermanent;
+
+/**
+ *
+ * @author anonymous
+ */
+public class IslandOfWakWak extends CardImpl {
+    
+    private static final FilterCreaturePermanent filterWithFlying = new FilterCreaturePermanent("creature with flying");
+    
+    static {
+        filterWithFlying.add(new AbilityPredicate(FlyingAbility.class));
+    }
+    
+    public IslandOfWakWak(UUID ownerId) {
+        super(ownerId, 89, "Island of Wak-Wak", Rarity.RARE, new CardType[]{CardType.LAND}, "");
+        this.expansionSetCode = "ARN";
+
+        // {tap}: The power of target creature with flying becomes 0 until end of turn.
+        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new IslandOfWakWakEffect(), new TapSourceCost());
+        ability.addTarget(new TargetCreaturePermanent(filterWithFlying));
+        this.addAbility(ability);
+    }
+
+    public IslandOfWakWak(final IslandOfWakWak card) {
+        super(card);
+    }
+
+    @Override
+    public IslandOfWakWak copy() {
+        return new IslandOfWakWak(this);
+    }
+}
+
+
+class IslandOfWakWakEffect extends OneShotEffect {
+    
+    public IslandOfWakWakEffect() {
+        super(Outcome.Detriment);
+        staticText = "The power of target creature with flying becomes 0 until end of turn";
+    }
+    
+    public IslandOfWakWakEffect(final IslandOfWakWakEffect effect) {
+       super(effect);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Permanent targetCreature = game.getPermanent(source.getFirstTarget());
+        if (targetCreature != null) {
+            MageInt toughness = targetCreature.getToughness();
+            game.addEffect(new SetPowerToughnessTargetEffect(0, toughness.getValue(), Duration.EndOfTurn), source);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Effect copy() {
+        return new IslandOfWakWakEffect(this);
+    }
+}
