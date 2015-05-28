6a0988ef66f81222b7e3976636f5bdc1087a29d5
LevelX2 <ludwig.hirth@online.de>
2015-05-28 16:13:54 +0200

    * Banefire - Fixed Banefire's switched condition check.

--- Mage.Tests/src/test/java/org/mage/test/cards/prevention/SwansOfBrynArgollTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/prevention/SwansOfBrynArgollTest.java
@@ -64,4 +64,30 @@ public class SwansOfBrynArgollTest extends CardTestPlayerBase{
         assertHandCount(playerA, 0);
 
     }
+    /**
+     * Since you can't prevent damage that Combust deals, it should be able to kill Swans of Bryn Argoll. 
+     */
+    @Test
+    public void testAgainstBanefire() {
+        // 4/3 Flying
+        // If a source would deal damage to Swans of Bryn Argoll, prevent that damage. The source's controller draws cards equal to the damage prevented this way.
+        addCard(Zone.BATTLEFIELD, playerA, "Swans of Bryn Argoll");
+        
+        // Banefire deals X damage to target creature or player.
+        // If X is 5 or more, Banefire can't be countered by spells or abilities and the damage can't be prevented.
+        addCard(Zone.HAND, playerB, "Banefire", 1);        
+        addCard(Zone.BATTLEFIELD, playerB, "Mountain", 8);
+        
+        castSpell(2, PhaseStep.PRECOMBAT_MAIN, playerB, "Banefire", "Swans of Bryn Argoll");
+        setChoice(playerB, "X=7");
+        
+        setStopAt(2, PhaseStep.BEGIN_COMBAT);
+        execute();
+
+        assertGraveyardCount(playerB, "Banefire", 1);
+        assertPermanentCount(playerA, "Swans of Bryn Argoll", 0);
+        assertGraveyardCount(playerA, "Swans of Bryn Argoll", 1);
+        assertHandCount(playerA, 0);
+
+    }    
 }
