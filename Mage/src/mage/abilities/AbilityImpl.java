b25c1ded7bf009de9c33ef80e6112e7db3559b7e
LevelX2 <ludwig.hirth@online.de>
2015-05-29 00:26:51 +0200

    Fixed text with html that couldn't display html.

--- Mage/src/mage/abilities/AbilityImpl.java
+++ Mage/src/mage/abilities/AbilityImpl.java
@@ -334,7 +334,7 @@ public abstract class AbilityImpl implements Ability {
             }
             if (getTargets().size() > 0 && getTargets().chooseTargets(getEffects().get(0).getOutcome(), this.controllerId, this, game) == false) {
                 if ((variableManaCost != null || announceString != null) && !game.isSimulation()) {
-                    game.informPlayer(controller, new StringBuilder(sourceObject != null ? sourceObject.getLogName(): "").append(": no valid targets with this value of X").toString());
+                    game.informPlayer(controller, (sourceObject != null ? sourceObject.getIdName(): "") + ": no valid targets with this value of X");
                 } 
                 return false; // when activation of ability is canceled during target selection
             }
