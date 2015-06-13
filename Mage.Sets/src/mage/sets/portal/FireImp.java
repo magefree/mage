f287156d9ad5c200162aa6398c367a29a737977e
LoneFox <lonefox@kapsi.fi>
2015-06-13 16:58:31 +0300

    Remove duplicate import

--- Mage.Sets/src/mage/sets/portal/FireImp.java
+++ Mage.Sets/src/mage/sets/portal/FireImp.java
@@ -33,7 +33,6 @@ import mage.abilities.Ability;
 import mage.abilities.common.EntersBattlefieldTriggeredAbility;
 import mage.abilities.effects.common.DamageTargetEffect;
 import mage.cards.CardImpl;
-import mage.cards.CardImpl;
 import mage.constants.CardType;
 import mage.constants.Rarity;
 import mage.target.common.TargetCreaturePermanent;
