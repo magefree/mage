f36460137f9c33330d3d3d98c7876cce1969afd9
LoneFox <lonefox@kapsi.fi>
2015-06-18 10:27:54 +0300

    Fix Sengir Nosferatu tooltip text

--- Mage.Sets/src/mage/sets/timespiral/SengirNosferatu.java
+++ Mage.Sets/src/mage/sets/timespiral/SengirNosferatu.java
@@ -34,6 +34,7 @@ import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.mana.ManaCostsImpl;
 import mage.abilities.costs.common.ExileSourceCost;
 import mage.abilities.costs.common.SacrificeSourceCost;
+import mage.abilities.effects.Effect;
 import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.CreateTokenEffect;
 import mage.abilities.keyword.FlyingAbility;
@@ -67,8 +68,9 @@ public class SengirNosferatu extends CardImpl {
         // Flying
         this.addAbility(FlyingAbility.getInstance());
         // {1}{B}, Exile Sengir Nosferatu: Put a 1/2 black Bat creature token with flying onto the battlefield. It has "{1}{B}, Sacrifice this creature: Return an exiled card named Sengir Nosferatu to the battlefield under its owner's control."
-        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD,
-            new CreateTokenEffect(new SengirNosferatuBatToken(), 1), new ManaCostsImpl("{1}{B}"));
+        Effect effect = new CreateTokenEffect(new SengirNosferatuBatToken(), 1);
+        effect.setText("Put a 1/2 black Bat creature token with flying onto the battlefield. It has \"{1}{B}, Sacrifice this creature: Return an exiled card named Sengir Nosferatu to the battlefield under its owner's control.\"");
+        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, effect, new ManaCostsImpl("{1}{B}"));
         ability.addCost(new ExileSourceCost());
         this.addAbility(ability);
     }
