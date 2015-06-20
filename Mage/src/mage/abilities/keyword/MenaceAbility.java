03dcc4a9f60d003514ae7960ccf4b220de909c5f
LevelX2 <ludwig.hirth@online.de>
2015-06-20 19:53:11 +0200

    Added Menace ability.

---
+++ Mage/src/mage/abilities/keyword/MenaceAbility.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mage.abilities.keyword;
+
+import mage.abilities.Ability;
+import mage.abilities.StaticAbility;
+import mage.abilities.effects.common.combat.CantBeBlockedByMoreThanOneSourceEffect;
+import mage.abilities.effects.common.combat.CantBeBlockedByOneEffect;
+import mage.constants.Zone;
+
+/**
+ *
+ * @author LevelX2
+ */
+public class MenaceAbility extends StaticAbility {
+
+    public MenaceAbility() {
+        super(Zone.BATTLEFIELD, new CantBeBlockedByOneEffect(2));
+    }
+
+    public MenaceAbility(MenaceAbility ability) {
+        super(ability);
+    }
+
+    @Override
+    public Ability copy() {
+        return new MenaceAbility(this);
+    }    
+
+    @Override
+    public String getRule() {
+        return "Menace <i>(This creature can't be blocked except by two or more creatures.)</i>";
+    }    
+    
+}
