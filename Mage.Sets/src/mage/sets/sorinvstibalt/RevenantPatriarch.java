5c65b278abff00f546092aea007e209a88ec91fb
Marshall <ilcartographer90@gmail.com>
2015-06-20 21:49:05 -0400

    Revenant Patriarch added

---
+++ Mage.Sets/src/mage/sets/sorinvstibalt/RevenantPatriarch.java
@@ -0,0 +1,107 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.sorinvstibalt;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.CantBlockAbility;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.condition.Condition;
+import mage.abilities.condition.common.ManaWasSpentCondition;
+import mage.abilities.effects.OneShotEffect;
+import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.ColoredManaSymbol;
+import mage.constants.Outcome;
+import mage.constants.Rarity;
+import mage.constants.TurnPhase;
+import mage.game.Game;
+import mage.game.turn.TurnMod;
+import mage.players.Player;
+import mage.watchers.common.ManaSpentToCastWatcher;
+
+/**
+ *
+ * @author ilcartographer
+ */
+public class RevenantPatriarch extends CardImpl {
+
+    public RevenantPatriarch(UUID ownerId) {
+        super(ownerId, 16, "Revenant Patriarch", Rarity.UNCOMMON, new CardType[]{CardType.CREATURE}, "{4}{B}");
+        this.expansionSetCode = "DDK";
+        this.subtype.add("Spirit");
+        this.power = new MageInt(4);
+        this.toughness = new MageInt(3);
+
+        // When Revenant Patriarch enters the battlefield, if {W} was spent to cast it, target player skips his or her next combat phase.
+        this.addAbility(new EntersBattlefieldTriggeredAbility(new SkipNextCombatIfConditionEffect(new ManaWasSpentCondition(ColoredManaSymbol.W)), false), new ManaSpentToCastWatcher());   
+        // Revenant Patriarch can't block.
+        this.addAbility(new CantBlockAbility());
+    }
+
+    public RevenantPatriarch(final RevenantPatriarch card) {
+        super(card);
+    }
+
+    @Override
+    public RevenantPatriarch copy() {
+        return new RevenantPatriarch(this);
+    }
+}
+
+class SkipNextCombatIfConditionEffect extends OneShotEffect {
+    
+    protected Condition condition;
+    
+    public SkipNextCombatIfConditionEffect(Condition condition) {
+        super(Outcome.Detriment);
+        staticText = "that player skips his or her next combat phase";
+        this.condition = condition;
+    }
+
+    public SkipNextCombatIfConditionEffect(final SkipNextCombatIfConditionEffect effect) {
+        super(effect);
+        this.condition = effect.condition;
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Player player = game.getPlayer(getTargetPointer().getFirst(game, source));
+        if (player != null && !condition.apply(game, source)) {
+            game.getState().getTurnMods().add(new TurnMod(player.getId(), TurnPhase.COMBAT, null, true));
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public SkipNextCombatIfConditionEffect copy() {
+        return new SkipNextCombatIfConditionEffect(this);
+    }
+}
