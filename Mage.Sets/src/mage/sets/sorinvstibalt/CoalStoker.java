91edab2ae8b32a1db0f6b1cd9d6c0830ce1c0e11
Marshall <ilcartographer90@gmail.com>
2015-06-20 21:20:48 -0400

    Saved Coal Stoker file...

--- Mage.Sets/src/mage/sets/sorinvstibalt/CoalStoker.java
+++ Mage.Sets/src/mage/sets/sorinvstibalt/CoalStoker.java
@@ -29,10 +29,12 @@ package mage.sets.sorinvstibalt;
 
 import java.util.UUID;
 import mage.MageInt;
+import mage.Mana;
 import mage.abilities.Ability;
 import mage.abilities.common.EntersBattlefieldTriggeredAbility;
 import mage.abilities.condition.common.CastFromHandCondition;
 import mage.abilities.decorator.ConditionalOneShotEffect;
+import mage.abilities.effects.common.BasicManaEffect;
 import mage.abilities.effects.common.ExileAllEffect;
 import mage.cards.CardImpl;
 import mage.constants.CardType;
@@ -55,7 +57,7 @@ public class CoalStoker extends CardImpl {
 
         // When Coal Stoker enters the battlefield, if you cast it from your hand, add {R}{R}{R} to your mana pool.
         Ability ability = new EntersBattlefieldTriggeredAbility(
-                new ConditionalOneShotEffect(, new CastFromHandCondition(),
+                new ConditionalOneShotEffect(new BasicManaEffect(new Mana(3, 0, 0, 0, 0, 0, 0)), new CastFromHandCondition(),
                 " if you cast it from your hand, add {R}{R}{R} to your mana pool."));
         this.addAbility(ability, new CastFromHandWatcher());
     }
