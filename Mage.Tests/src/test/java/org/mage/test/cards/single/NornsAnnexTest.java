0a293063cf88e41e647060c0bfe261f0856d2db5
glerman <gal.lerman1@gmail.com>
2015-06-24 19:48:28 +0300

    added test for NornsAnnex still in progress, dur to friends bug

---
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/NornsAnnexTest.java
@@ -0,0 +1,27 @@
+package org.mage.test.cards.single;
+
+import mage.constants.PhaseStep;
+import mage.constants.Zone;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.mage.test.serverside.base.CardTestPlayerBase;
+
+/**
+ * Created by glerman on 23/6/15.
+ */
+@Ignore
+public class NornsAnnexTest extends CardTestPlayerBase{
+  @Test
+  @Ignore
+  public void testNornsAnnex() {
+    addCard(Zone.BATTLEFIELD, playerA, "Norn's Annex");
+    addCard(Zone.BATTLEFIELD, playerB, "Brindle Boar");
+    attack(2, playerB, "Brindle Boar", playerA);
+    setStopAt(2, PhaseStep.END_TURN);
+    execute();
+
+
+
+  }
+}
