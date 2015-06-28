9e4a7aad8ac1506a277d0b69503e13f81a695741
glerman <gal.lerman1@gmail.com>
2015-06-29 01:24:21 +0300

    added empty test for DustOfMoments

---
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/DustOfMomentsTest.java
@@ -0,0 +1,9 @@
+package org.mage.test.cards.single;
+
+import org.mage.test.serverside.base.CardTestPlayerBase;
+
+/**
+ * Created by glerman on 29/6/15.
+ */
+public class DustOfMomentsTest extends CardTestPlayerBase{
+}
