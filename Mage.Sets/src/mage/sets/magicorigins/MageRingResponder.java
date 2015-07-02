af02865bcad810122be48c27c4d2d235e97b41e2
fireshoes <fireshoes@fireshoes-PC>
2015-07-02 09:01:45 -0500

    [ORI] Updated overnight spoilers. Added Mage-Ring Responder and Stalwart Aven and reprint of Sigil of the Empty Throne.

---
+++ Mage.Sets/src/mage/sets/magicorigins/MageRingResponder.java
@@ -0,0 +1,122 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.magicorigins;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.abilities.TriggeredAbilityImpl;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.DamageTargetEffect;
+import mage.abilities.effects.common.DontUntapInControllersUntapStepSourceEffect;
+import mage.abilities.effects.common.UntapSourceEffect;
+import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Rarity;
+import mage.constants.Zone;
+import mage.filter.common.FilterCreaturePermanent;
+import mage.filter.predicate.permanent.ControllerIdPredicate;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.events.GameEvent.EventType;
+import mage.target.common.TargetCreaturePermanent;
+
+/**
+ *
+ * @author fireshoes
+ */
+public class MageRingResponder extends CardImpl {
+
+    public MageRingResponder(UUID ownerId) {
+        super(ownerId, 232, "Mage-Ring Responder", Rarity.RARE, new CardType[]{CardType.ARTIFACT, CardType.CREATURE}, "{7}");
+        this.expansionSetCode = "ORI";
+        this.subtype.add("GOlem");
+        this.power = new MageInt(7);
+        this.toughness = new MageInt(7);
+
+        // Mage-Ring Responder doesn't untap during your untap step.
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new DontUntapInControllersUntapStepSourceEffect()));
+        
+        // {7}: Untap Mage-Ring Responder.
+        this.addAbility(new SimpleActivatedAbility(Zone.BATTLEFIELD, new UntapSourceEffect(), new ManaCostsImpl("{7}")));
+        
+        // Whenever Mage-Ring Responder attacks, it deals 7 damage to target creature defending player controls.
+        this.addAbility(new MageRingResponderAbility());
+    }
+
+    public MageRingResponder(final MageRingResponder card) {
+        super(card);
+    }
+
+    @Override
+    public MageRingResponder copy() {
+        return new MageRingResponder(this);
+    }
+}
+
+class MageRingResponderAbility extends TriggeredAbilityImpl {
+
+    public MageRingResponderAbility() {
+        super(Zone.BATTLEFIELD, new DamageTargetEffect(7));
+    }
+
+    public MageRingResponderAbility(final MageRingResponderAbility ability) {
+        super(ability);
+    }
+
+    @Override
+    public boolean checkEventType(GameEvent event, Game game) {
+        return event.getType() == EventType.ATTACKER_DECLARED;
+    }
+
+    @Override
+    public boolean checkTrigger(GameEvent event, Game game) {
+        if (event.getSourceId().equals(this.getSourceId())) {
+            FilterCreaturePermanent filter = new FilterCreaturePermanent("creature defending player controls");
+            UUID defenderId = game.getCombat().getDefenderId(sourceId);
+            filter.add(new ControllerIdPredicate(defenderId));
+
+            this.getTargets().clear();
+            TargetCreaturePermanent target = new TargetCreaturePermanent(filter);
+            this.addTarget(target);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String getRule() {
+        return "Whenever {this} attacks, it deals 7 damage to target creature defending player controls.";
+    }
+
+    @Override
+    public MageRingResponderAbility copy() {
+        return new MageRingResponderAbility(this);
+    }
+}
