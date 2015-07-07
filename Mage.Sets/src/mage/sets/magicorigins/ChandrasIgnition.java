859bfd9bf376d91fd9c01ff697c51e609f23c007
LoneFox <lonefox@kapsi.fi>
2015-07-07 12:34:50 +0300

    Fix wrong source of damage for Chandra's Ignition

--- Mage.Sets/src/mage/sets/magicorigins/ChandrasIgnition.java
+++ Mage.Sets/src/mage/sets/magicorigins/ChandrasIgnition.java
@@ -66,34 +66,34 @@ public class ChandrasIgnition extends CardImpl {
 }
 
 class ChandrasIgnitionEffect extends OneShotEffect {
-    
+
     public ChandrasIgnitionEffect() {
         super(Outcome.Benefit);
         this.staticText = "Target creature you control deals damage equal to its power to each other creature and each opponent";
     }
-    
+
     public ChandrasIgnitionEffect(final ChandrasIgnitionEffect effect) {
         super(effect);
     }
-    
+
     @Override
     public ChandrasIgnitionEffect copy() {
         return new ChandrasIgnitionEffect(this);
     }
-    
+
     @Override
     public boolean apply(Game game, Ability source) {
         Permanent targetCreature = game.getPermanent(getTargetPointer().getFirst(game, source));
         if (targetCreature != null && targetCreature.getPower().getValue() > 0) {
             for (Permanent creature : game.getState().getBattlefield().getActivePermanents(new FilterCreaturePermanent(), source.getControllerId(), source.getSourceId(), game)) {
                 if (!creature.getId().equals(targetCreature.getId())) {
-                    creature.damage(targetCreature.getPower().getValue(), source.getSourceId(), game, false, true);
+                    creature.damage(targetCreature.getPower().getValue(), targetCreature.getId(), game, false, true);
                 }
             }
             for(UUID opponentId : game.getOpponents(source.getControllerId())) {
                 Player opponent = game.getPlayer(opponentId);
                 if (opponent != null) {
-                    opponent.damage(targetCreature.getPower().getValue(), source.getSourceId(), game, false, true);
+                    opponent.damage(targetCreature.getPower().getValue(), targetCreature.getId(), game, false, true);
                 }
             }
         }
