0df798393788c6b5bb48760d0f12506b12012444
LevelX2 <ludwig.hirth@online.de>
2015-07-19 19:27:49 +0200

    Added some test.

--- Mage.Tests/src/test/java/org/mage/test/AI/basic/TriggeredAbilityTest.java
+++ Mage.Tests/src/test/java/org/mage/test/AI/basic/TriggeredAbilityTest.java
@@ -33,19 +33,15 @@ import org.mage.test.serverside.base.CardTestPlayerBaseAI;
  *
  * @author LevelX2
  */
-public class CombatTest extends CardTestPlayerBaseAI {
+public class TriggeredAbilityTest extends CardTestPlayerBaseAI {
 
     /*
-     * Combat is not integrated yet in the CardTestPlayerBaseAI
      *
-     * TODO: Modify CardTestPlayerBaseAI to use the AI combat acting
+     * TODO: Create tests and fix AI to be able to handle tested situations
      *
      * Tests to create:
-     * AI is not attacking if opponent has a creature that can't block
-     * AI is not blocking also if able if the damage the attacker will do will kill the AI
-     * AI is not able to block with two or more creatures one attacking creature to kill it. Even if none of the AI creatures will die
-     * AI attacks with a flyer even if opponent has a bigger flyer that kills AI
-     * Opponent of AI has only 1 life. AI has more creatures that can attack taht do at least 1 damage than opponent has blockers. Ai should attack with all needed creatures
+     * AI has the only one creature in play and plays a Gilt-Leaf Winnower. AI should select No if asked to use the destray effect becuase it targets the AI creature
+     *
      *
      */
 }
