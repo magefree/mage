00e4e3be6c0316291761960552dd3e513d0b1d45
LevelX2 <ludwig.hirth@online.de>
2015-07-21 11:11:54 +0200

    * Coastal Drake - Fixed that the activated ability did only return the Coastal Drake itself to owners hand.

--- Mage.Sets/src/mage/sets/apocalypse/CoastalDrake.java
+++ Mage.Sets/src/mage/sets/apocalypse/CoastalDrake.java
@@ -28,18 +28,19 @@
 package mage.sets.apocalypse;
 
 import java.util.UUID;
-
-import mage.constants.CardType;
-import mage.constants.Rarity;
 import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.common.TapSourceCost;
 import mage.abilities.costs.mana.ManaCostsImpl;
-import mage.abilities.effects.common.ReturnToHandSourceEffect;
+import mage.abilities.effects.common.ReturnToHandTargetEffect;
 import mage.abilities.keyword.FlyingAbility;
 import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Rarity;
 import mage.constants.Zone;
+import mage.filter.common.FilterCreaturePermanent;
+import mage.target.common.TargetCreaturePermanent;
 
 /**
  *
@@ -54,12 +55,13 @@ public class CoastalDrake extends CardImpl {
 
         this.power = new MageInt(2);
         this.toughness = new MageInt(1);
-        
-        // Flying        
+
+        // Flying
         this.addAbility(FlyingAbility.getInstance());
-        
+
         // {1}{U}, {T} : Return target Kavu to its owner's hand.
-        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new ReturnToHandSourceEffect(true), new ManaCostsImpl("{1}{U}"));
+        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new ReturnToHandTargetEffect(true), new ManaCostsImpl("{1}{U}"));
+        ability.addTarget(new TargetCreaturePermanent(new FilterCreaturePermanent("Kavu", "Kavu")));
         ability.addCost(new TapSourceCost());
         this.addAbility(ability);
     }
