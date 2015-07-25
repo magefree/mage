e4005c0a3528880e3fd7f305e3044735af2585e2
LevelX2 <ludwig.hirth@online.de>
2015-07-25 12:40:59 +0200

    * Simic Manipulator - Fixed that also 0 counters could be selected (fixes #1140).

--- Mage.Sets/src/mage/sets/gatecrash/SimicManipulator.java
+++ Mage.Sets/src/mage/sets/gatecrash/SimicManipulator.java
@@ -1,40 +1,33 @@
 /*
-*  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
-*
-*  Redistribution and use in source and binary forms, with or without modification, are
-*  permitted provided that the following conditions are met:
-*
-*     1. Redistributions of source code must retain the above copyright notice, this list of
-*        conditions and the following disclaimer.
-*
-*     2. Redistributions in binary form must reproduce the above copyright notice, this list
-*        of conditions and the following disclaimer in the documentation and/or other materials
-*        provided with the distribution.
-*
-*  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
-*  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-*  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
-*  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-*  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-*  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-*  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-*  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-*  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-*  The views and conclusions contained in the software and documentation are those of the
-*  authors and should not be interpreted as representing official policies, either expressed
-*  or implied, of BetaSteward_at_googlemail.com.
-*/
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
 package mage.sets.gatecrash;
 
 import java.util.UUID;
-import mage.constants.CardType;
-import mage.constants.Duration;
-import mage.constants.Layer;
-import mage.constants.Outcome;
-import mage.constants.Rarity;
-import mage.constants.SubLayer;
-import mage.constants.Zone;
 import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.Mode;
@@ -45,6 +38,13 @@ import mage.abilities.costs.common.TapSourceCost;
 import mage.abilities.effects.ContinuousEffectImpl;
 import mage.abilities.keyword.EvolveAbility;
 import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Layer;
+import mage.constants.Outcome;
+import mage.constants.Rarity;
+import mage.constants.SubLayer;
+import mage.constants.Zone;
 import mage.counters.CounterType;
 import mage.filter.common.FilterCreaturePermanent;
 import mage.game.Game;
@@ -52,16 +52,17 @@ import mage.game.permanent.Permanent;
 import mage.target.common.TargetCreaturePermanent;
 
 /**
-* Gatecrash FAQ (01.2013)
-*
-* The power of the target creature is checked both as you target it and as the ability
-* resolves. If the power of the target creature when the ability resolves is greater
-* than the number of +1/+1 counters removed from Simic Manipulator, the ability will
-* be countered and none of its effects will happen. You won't gain control of any
-* creature, but the counters removed as a cost remain removed.
-*
-* @author LevelX2
-*/
+ * Gatecrash FAQ (01.2013)
+ *
+ * The power of the target creature is checked both as you target it and as the
+ * ability resolves. If the power of the target creature when the ability
+ * resolves is greater than the number of +1/+1 counters removed from Simic
+ * Manipulator, the ability will be countered and none of its effects will
+ * happen. You won't gain control of any creature, but the counters removed as a
+ * cost remain removed.
+ *
+ * @author LevelX2
+ */
 public class SimicManipulator extends CardImpl {
 
     private static final FilterCreaturePermanent filter = new FilterCreaturePermanent("creature with power less than or equal to the number of +1/+1 counters removed this way");
@@ -78,12 +79,12 @@ public class SimicManipulator extends CardImpl {
         // Evolve
         this.addAbility(new EvolveAbility());
 
-        // {tap}, Remove one or more +1/+1 counters from Simic Manipulator: Gain control of target creature with power less than or equal to the number of +1/+1 counters removed this way.
+        // {T}, Remove one or more +1/+1 counters from Simic Manipulator: Gain control of target creature with power less than or equal to the number of +1/+1 counters removed this way.
         // TODO: Improve targeting, that only valid targets (power <= removed counters) can be choosen
         //       Disadvantage now is, that a creature can be targeted that couldn't be targeted by rules.
         Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new SimicManipulatorGainControlTargetEffect(Duration.Custom), new TapSourceCost());
         ability.addTarget(new TargetCreaturePermanent(filter));
-        ability.addCost(new RemoveVariableCountersSourceCost(CounterType.P1P1.createInstance(),1));
+        ability.addCost(new RemoveVariableCountersSourceCost(CounterType.P1P1.createInstance(), 1, "Remove one or more +1/+1 counters from {this}"));
         this.addAbility(ability);
     }
 
