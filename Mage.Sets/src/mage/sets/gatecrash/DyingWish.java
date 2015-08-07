21cfe0611c15604e19bf26baf7dfbb6344c1594c
LevelX2 <ludwig.hirth@online.de>
2015-08-07 21:31:22 +0200

    * Some fixes to Liting Refrain, Dying Wish and Violet Pall.

--- Mage.Sets/src/mage/sets/gatecrash/DyingWish.java
+++ Mage.Sets/src/mage/sets/gatecrash/DyingWish.java
@@ -28,17 +28,21 @@
 package mage.sets.gatecrash;
 
 import java.util.UUID;
-import mage.constants.CardType;
-import mage.constants.Outcome;
-import mage.constants.Rarity;
 import mage.abilities.Ability;
 import mage.abilities.common.DiesAttachedTriggeredAbility;
 import mage.abilities.dynamicvalue.DynamicValue;
+import mage.abilities.effects.Effect;
 import mage.abilities.effects.common.AttachEffect;
 import mage.abilities.effects.common.GainLifeEffect;
 import mage.abilities.effects.common.LoseLifeTargetEffect;
 import mage.abilities.keyword.EnchantAbility;
 import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Outcome;
+import mage.constants.Rarity;
+import mage.constants.Zone;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
 import mage.target.TargetPermanent;
 import mage.target.TargetPlayer;
 import mage.target.common.TargetControlledCreaturePermanent;
@@ -62,9 +66,8 @@ public class DyingWish extends CardImpl {
         Ability ability = new EnchantAbility(auraTarget.getTargetName());
         this.addAbility(ability);
 
-
         // When enchanted creature dies, target player loses X life and you gain X life, where X is its power.
-        DynamicValue attachedPower = new AttachedPermanentPowerCount();
+        DynamicValue attachedPower = new DyingWishAttachedPermanentPowerCount();
         ability = new DiesAttachedTriggeredAbility(new LoseLifeTargetEffect(attachedPower), "enchanted creature");
         ability.addEffect(new GainLifeEffect(attachedPower));
         ability.addTarget(new TargetPlayer());
@@ -78,5 +81,40 @@ public class DyingWish extends CardImpl {
     @Override
     public DyingWish copy() {
         return new DyingWish(this);
+    }
+}
+
+class DyingWishAttachedPermanentPowerCount implements DynamicValue {
+
+    @Override
+    public int calculate(Game game, Ability sourceAbility, Effect effect) {
+        Permanent attachmentPermanent = game.getPermanent(sourceAbility.getSourceId());
+        if (attachmentPermanent == null) {
+            attachmentPermanent = (Permanent) game.getLastKnownInformation(sourceAbility.getSourceId(), Zone.BATTLEFIELD, sourceAbility.getSourceObjectZoneChangeCounter());
+        }
+        if (attachmentPermanent != null && attachmentPermanent.getAttachedTo() != null) {
+            if (effect.getValue("attachedTo") != null) {
+                Permanent attached = (Permanent) effect.getValue("attachedTo");
+                if (attached != null) {
+                    return attached.getPower().getValue();
+                }
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public DynamicValue copy() {
+        return new DyingWishAttachedPermanentPowerCount();
+    }
+
+    @Override
+    public String toString() {
+        return "X";
+    }
+
+    @Override
+    public String getMessage() {
+        return "its power";
     }
 }
