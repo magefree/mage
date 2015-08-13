30342fe5a03c0840336e046c4b67fd725760c90e
Edward Gao <edward.jl.gao@berkeley.edu>
2015-08-13 00:58:31 -0700

    Make hydromorphguardian only counter spells that target creatures you control

--- Mage.Sets/src/mage/sets/torment/HydromorphGuardian.java
+++ Mage.Sets/src/mage/sets/torment/HydromorphGuardian.java
@@ -27,6 +27,8 @@
  */
 package mage.sets.torment;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 import mage.MageInt;
 import mage.abilities.Ability;
@@ -39,13 +41,22 @@ import mage.constants.CardType;
 import mage.constants.ColoredManaSymbol;
 import mage.constants.Rarity;
 import mage.constants.Zone;
-import mage.target.TargetSpell;
+import mage.filter.Filter;
+import mage.filter.FilterSpell;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.game.stack.Spell;
+import mage.game.stack.StackObject;
+import mage.target.Target;
+import mage.target.TargetObject;
 
 /**
  *
  * @author TaVSt
  */
 public class HydromorphGuardian extends CardImpl {
+
+    private static FilterSpell filter = new FilterSpell("spell that targets a creature you control");
 
     public HydromorphGuardian(UUID ownerId) {
         super(ownerId, 39, "Hydromorph Guardian", Rarity.COMMON, new CardType[]{CardType.CREATURE}, "{2}{U}");
@@ -57,7 +68,7 @@ public class HydromorphGuardian extends CardImpl {
         // {U}, Sacrifice Hydromorph Guardian: Counter target spell that targets one or more creatures you control.
         Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new CounterTargetEffect(), new ColoredManaCost(ColoredManaSymbol.U));
         ability.addCost(new SacrificeSourceCost());
-        ability.addTarget(new TargetSpell());
+        ability.addTarget(new CustomTargetSpell(filter));
         this.addAbility(ability);
     }
 
@@ -68,5 +79,109 @@ public class HydromorphGuardian extends CardImpl {
     @Override
     public HydromorphGuardian copy() {
         return new HydromorphGuardian(this);
+    }
+    
+    private class CustomTargetSpell extends TargetObject {
+
+        protected FilterSpell filter;
+
+        public CustomTargetSpell() {
+            this(1, 1, new FilterSpell());
+        }
+
+        public CustomTargetSpell(FilterSpell filter) {
+            this(1, 1, filter);
+        }
+
+        public CustomTargetSpell(int numTargets, FilterSpell filter) {
+            this(numTargets, numTargets, filter);
+        }
+
+        public CustomTargetSpell(int minNumTargets, int maxNumTargets, FilterSpell filter) {
+            this.minNumberOfTargets = minNumTargets;
+            this.maxNumberOfTargets = maxNumTargets;
+            this.zone = Zone.STACK;
+            this.filter = filter;
+            this.targetName = filter.getMessage();
+        }
+
+        public CustomTargetSpell(final CustomTargetSpell target) {
+            super(target);
+            this.filter = target.filter.copy();
+        }
+
+        @Override
+        public boolean canChoose(UUID sourceId, UUID sourceControllerId, Game game) {
+            return canChoose(sourceControllerId, game);
+        }
+
+        @Override
+        public Set<UUID> possibleTargets(UUID sourceId, UUID sourceControllerId, Game game) {
+            return possibleTargets(sourceControllerId, game);
+        }
+
+        @Override
+        public boolean canTarget(UUID id, Ability source, Game game) {
+            if (super.canTarget(id, source, game)) {
+                if (targetsMyCreature(id, source.getControllerId(), game)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public boolean canChoose(UUID sourceControllerId, Game game) {
+            int count = 0;
+            for (StackObject stackObject : game.getStack()) {
+                if (stackObject instanceof Spell && game.getPlayer(sourceControllerId).getInRange().contains(stackObject.getControllerId()) && filter.match((Spell) stackObject, game)) {
+                    if (targetsMyCreature(stackObject.getId(), sourceControllerId, game)) {
+                        count++;
+                        if (count >= this.minNumberOfTargets)
+                            return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public Set<UUID> possibleTargets(UUID sourceControllerId, Game game) {
+            Set<UUID> possibleTargets = new HashSet<UUID>();
+            for (StackObject stackObject : game.getStack()) {
+                if (stackObject instanceof Spell && game.getPlayer(sourceControllerId).getInRange().contains(stackObject.getControllerId()) && filter.match((Spell) stackObject, game)) {
+                    if (targetsMyCreature(stackObject.getId(), sourceControllerId, game)) {
+                        possibleTargets.add(stackObject.getId());
+                    }
+                }
+            }
+            return possibleTargets;
+        }
+
+        @Override
+        public Filter getFilter() {
+            return filter;
+        }
+
+        private boolean targetsMyCreature(UUID id, UUID controllerId, Game game) {
+            StackObject spell = game.getStack().getStackObject(id);
+            if (spell != null) {
+                Ability ability = spell.getStackAbility();
+                for (Target target : ability.getTargets()) {
+                    for (UUID permanentId : target.getTargets()) {
+                        Permanent permanent = game.getPermanent(permanentId);
+                        if (permanent != null && permanent.getControllerId().equals(controllerId) && permanent.getCardType().contains(CardType.CREATURE)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public CustomTargetSpell copy() {
+            return new CustomTargetSpell(this);
+        }
     }
 }
