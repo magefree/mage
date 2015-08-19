8324d9df8faa191891a15b41aa811c164d88625a
LoneFox <lonefox@kapsi.fi>
2015-08-19 14:20:34 +0300

    Implement some creatures with horsemanship

---
+++ Mage.Sets/src/mage/sets/masterseditioniii/ZhangFeiFierceWarrior.java
@@ -0,0 +1,67 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.masterseditioniii;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.abilities.keyword.HorsemanshipAbility;
+import mage.abilities.keyword.VigilanceAbility;
+import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Rarity;
+
+/**
+ *
+ * @author LoneFox
+ */
+public class ZhangFeiFierceWarrior extends CardImpl {
+
+    public ZhangFeiFierceWarrior(UUID ownerId) {
+        super(ownerId, 28, "Zhang Fei, Fierce Warrior", Rarity.UNCOMMON, new CardType[]{CardType.CREATURE}, "{4}{W}{W}");
+        this.expansionSetCode = "ME3";
+        this.supertype.add("Legendary");
+        this.subtype.add("Human");
+        this.subtype.add("Soldier");
+        this.subtype.add("Warrior");
+        this.power = new MageInt(4);
+        this.toughness = new MageInt(4);
+
+        // Vigilance; horsemanship
+        this.addAbility(VigilanceAbility.getInstance());
+        this.addAbility(HorsemanshipAbility.getInstance());
+    }
+
+    public ZhangFeiFierceWarrior(final ZhangFeiFierceWarrior card) {
+        super(card);
+    }
+
+    @Override
+    public ZhangFeiFierceWarrior copy() {
+        return new ZhangFeiFierceWarrior(this);
+    }
+}
