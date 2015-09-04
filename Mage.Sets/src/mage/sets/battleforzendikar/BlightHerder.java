5d51615eabc782c8442c6cf657e5955ecbc1f07d
LevelX2 <ludwig.hirth@online.de>
2015-09-04 19:02:50 +0200

    [BFZ] Added Blight Herder.

--- Mage.Sets/src/mage/sets/battleforzendikar/BlightHerder.java
+++ Mage.Sets/src/mage/sets/battleforzendikar/BlightHerder.java
@@ -25,89 +25,91 @@
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
  */
-package mage.sets.shadowmoor;
+package mage.sets.battleforzendikar;
 
 import java.util.UUID;
 import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.CastSourceTriggeredAbility;
-import mage.abilities.keyword.FlyingAbility;
-import mage.abilities.keyword.HasteAbility;
-import mage.cards.Card;
+import mage.abilities.effects.common.CreateTokenEffect;
 import mage.cards.CardImpl;
+import mage.cards.Cards;
+import mage.cards.CardsImpl;
 import mage.constants.CardType;
 import mage.constants.Outcome;
 import mage.constants.Rarity;
+import mage.constants.TargetController;
 import mage.constants.Zone;
 import mage.filter.FilterCard;
-import mage.filter.predicate.mageobject.NamePredicate;
+import mage.filter.predicate.other.OwnerPredicate;
 import mage.game.Game;
+import mage.game.permanent.token.EldraziScionToken;
 import mage.players.Player;
+import mage.target.Target;
+import mage.target.common.TargetCardInExile;
 
 /**
  *
  * @author LevelX2
  */
-public class DemigodOfRevenge extends CardImpl {
+public class BlightHerder extends CardImpl {
 
-    public DemigodOfRevenge(UUID ownerId) {
-        super(ownerId, 183, "Demigod of Revenge", Rarity.RARE, new CardType[]{CardType.CREATURE}, "{B/R}{B/R}{B/R}{B/R}{B/R}");
-        this.expansionSetCode = "SHM";
-        this.subtype.add("Spirit");
-        this.subtype.add("Avatar");
+    public BlightHerder(UUID ownerId) {
+        super(ownerId, 2, "Blight Herder", Rarity.RARE, new CardType[]{CardType.CREATURE}, "{5}");
+        this.expansionSetCode = "BFZ";
+        this.subtype.add("Eldrazi");
+        this.subtype.add("Processor");
+        this.power = new MageInt(4);
+        this.toughness = new MageInt(5);
 
-        this.power = new MageInt(5);
-        this.toughness = new MageInt(4);
-
-        // Flying
-        this.addAbility(FlyingAbility.getInstance());
-
-        // Haste
-        this.addAbility(HasteAbility.getInstance());
-
-        // When you cast Demigod of Revenge, return all cards named Demigod of Revenge from your graveyard to the battlefield.
-        this.addAbility(new CastSourceTriggeredAbility(new DemigodOfRevengeReturnEffect()));
+        // When you cast Blight Herder, you may put two cards your opponents own from exile into their owners' graveyards. If you do, put three 1/1 colorless Eldrazi Scion creature tokens onto the battlefield. They have "Sacrifice this creature: Add {1} to your mana pool."
+        this.addAbility(new CastSourceTriggeredAbility(new BlightHerderEffect(), true));
     }
 
-    public DemigodOfRevenge(final DemigodOfRevenge card) {
+    public BlightHerder(final BlightHerder card) {
         super(card);
     }
 
     @Override
-    public DemigodOfRevenge copy() {
-        return new DemigodOfRevenge(this);
+    public BlightHerder copy() {
+        return new BlightHerder(this);
     }
 }
 
-class DemigodOfRevengeReturnEffect extends OneShotEffect {
+class BlightHerderEffect extends OneShotEffect {
 
-    private static final FilterCard filter = new FilterCard();
+    private final static FilterCard filter = new FilterCard("cards your opponents own from exile");
 
     static {
-        filter.add(new NamePredicate("Demigod of Revenge"));
+        filter.add(new OwnerPredicate(TargetController.OPPONENT));
     }
 
-    public DemigodOfRevengeReturnEffect() {
+    public BlightHerderEffect() {
         super(Outcome.PutCreatureInPlay);
-        staticText = "return all cards named Demigod of Revenge from your graveyard to the battlefield";
+        this.staticText = "you may put two cards your opponents own from exile into their owners' graveyards. If you do, put three 1/1 colorless Eldrazi Scion creature tokens onto the battlefield. They have \"Sacrifice this creature: Add {1} to your mana pool.";
     }
 
-    public DemigodOfRevengeReturnEffect(final DemigodOfRevengeReturnEffect effect) {
+    public BlightHerderEffect(final BlightHerderEffect effect) {
         super(effect);
     }
 
     @Override
-    public DemigodOfRevengeReturnEffect copy() {
-        return new DemigodOfRevengeReturnEffect(this);
+    public BlightHerderEffect copy() {
+        return new BlightHerderEffect(this);
     }
 
     @Override
     public boolean apply(Game game, Ability source) {
         Player controller = game.getPlayer(source.getControllerId());
         if (controller != null) {
-            for (Card creature : controller.getGraveyard().getCards(filter, game)) {
-                creature.putOntoBattlefield(game, Zone.GRAVEYARD, source.getSourceId(), source.getControllerId());
+            Target target = new TargetCardInExile(2, 2, filter, null);
+            if (target.canChoose(source.getSourceId(), source.getControllerId(), game)) {
+                if (controller.chooseTarget(outcome, target, source, game)) {
+                    Cards cardsToGraveyard = new CardsImpl(target.getTargets());
+                    controller.moveCards(cardsToGraveyard, null, Zone.GRAVEYARD, source, game);
+                    return new CreateTokenEffect(new EldraziScionToken(), 3).apply(game, source);
+                }
             }
             return true;
         }
