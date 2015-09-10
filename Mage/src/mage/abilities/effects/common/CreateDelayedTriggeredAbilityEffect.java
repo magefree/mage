5494f191b90851b9a9ac1723d62369ee826832b7
LevelX2 <ludwig.hirth@online.de>
2015-09-10 22:47:26 +0200

    * Arcbond - Fixed that an exception was thrown if the ability triggered.

--- Mage/src/mage/abilities/effects/common/CreateDelayedTriggeredAbilityEffect.java
+++ Mage/src/mage/abilities/effects/common/CreateDelayedTriggeredAbilityEffect.java
@@ -1,16 +1,16 @@
 /*
  *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
- * 
+ *
  *  Redistribution and use in source and binary forms, with or without modification, are
  *  permitted provided that the following conditions are met:
- * 
+ *
  *     1. Redistributions of source code must retain the above copyright notice, this list of
  *        conditions and the following disclaimer.
- * 
+ *
  *     2. Redistributions in binary form must reproduce the above copyright notice, this list
  *        of conditions and the following disclaimer in the documentation and/or other materials
  *        provided with the distribution.
- * 
+ *
  *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
  *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
@@ -20,12 +20,11 @@
  *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  *  The views and conclusions contained in the software and documentation are those of the
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
  */
-
 package mage.abilities.effects.common;
 
 import mage.abilities.Ability;
@@ -43,21 +42,28 @@ public class CreateDelayedTriggeredAbilityEffect extends OneShotEffect {
 
     protected DelayedTriggeredAbility ability;
     protected boolean copyTargets;
+    protected boolean initAbility;
 
     public CreateDelayedTriggeredAbilityEffect(DelayedTriggeredAbility ability) {
         this(ability, true);
     }
 
     public CreateDelayedTriggeredAbilityEffect(DelayedTriggeredAbility ability, boolean copyTargets) {
+        this(ability, copyTargets, false);
+    }
+
+    public CreateDelayedTriggeredAbilityEffect(DelayedTriggeredAbility ability, boolean copyTargets, boolean initAbility) {
         super(ability.getEffects().get(0).getOutcome());
         this.ability = ability;
         this.copyTargets = copyTargets;
+        this.initAbility = initAbility;
     }
 
     public CreateDelayedTriggeredAbilityEffect(final CreateDelayedTriggeredAbilityEffect effect) {
         super(effect);
         this.ability = effect.ability.copy();
         this.copyTargets = effect.copyTargets;
+        this.initAbility = effect.initAbility;
     }
 
     @Override
@@ -73,15 +79,18 @@ public class CreateDelayedTriggeredAbilityEffect extends OneShotEffect {
         delayedAbility.setSourceObject(source.getSourceObject(game), game);
         if (this.copyTargets) {
             if (source.getTargets().isEmpty()) {
-                for(Effect effect : delayedAbility.getEffects()) {
+                for (Effect effect : delayedAbility.getEffects()) {
                     effect.setTargetPointer(targetPointer);
                 }
             } else {
                 delayedAbility.getTargets().addAll(source.getTargets());
-                for(Effect effect : delayedAbility.getEffects()) {
+                for (Effect effect : delayedAbility.getEffects()) {
                     effect.getTargetPointer().init(game, source);
                 }
             }
+        }
+        if (initAbility) {
+            delayedAbility.init(game);
         }
         game.addDelayedTriggeredAbility(delayedAbility);
         return true;
@@ -89,7 +98,7 @@ public class CreateDelayedTriggeredAbilityEffect extends OneShotEffect {
 
     @Override
     public String getText(Mode mode) {
-        if (staticText != null && !staticText.isEmpty()){
+        if (staticText != null && !staticText.isEmpty()) {
             return staticText;
         }
         if (ability.getRuleVisible()) {
