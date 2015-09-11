de639cc1d60b336f2a65d32f2180d71fc3f2d996
LevelX2 <ludwig.hirth@online.de>
2015-09-11 23:06:26 +0200

    * Suspend, added the card name to the rule text of the ability triggering, so different suspended cards were not auto put on the stack by the activated same rule text option.

--- Mage.Server.Plugins/Mage.Player.Human/src/mage/player/human/HumanPlayer.java
+++ Mage.Server.Plugins/Mage.Player.Human/src/mage/player/human/HumanPlayer.java
@@ -383,7 +383,7 @@ public class HumanPlayer extends PlayerImpl {
         }
         while (!abort) {
             Set<UUID> possibleTargets = target.possibleTargets(source == null ? null : source.getSourceId(), abilityControllerId, game);
-            boolean required = target.isRequired(source);
+            boolean required = target.isRequired(source != null ? source.getSourceId() : null, game);
             if (possibleTargets.isEmpty() || target.getTargets().size() >= target.getNumberOfTargets()) {
                 required = false;
             }
@@ -715,21 +715,23 @@ public class HumanPlayer extends PlayerImpl {
                 if (triggerAutoOrderAbilityFirst.contains(ability.getOriginalId())) {
                     return ability;
                 }
-                if (triggerAutoOrderNameFirst.contains(ability.getRule())) {
+                MageObject object = game.getObject(ability.getSourceId());
+                String rule = ability.getRule(object != null ? object.getName() : null);
+                if (triggerAutoOrderNameFirst.contains(rule)) {
                     return ability;
                 }
                 if (triggerAutoOrderAbilityLast.contains(ability.getOriginalId())) {
                     abilityOrderLast = ability;
                     continue;
                 }
-                if (triggerAutoOrderNameLast.contains(ability.getRule())) {
+                if (triggerAutoOrderNameLast.contains(rule)) {
                     abilityOrderLast = ability;
                     continue;
                 }
                 if (autoOrderUse) {
                     if (autoOrderRuleText == null) {
-                        autoOrderRuleText = ability.getRule();
-                    } else if (!ability.getRule().equals(autoOrderRuleText)) {
+                        autoOrderRuleText = rule;
+                    } else if (!rule.equals(autoOrderRuleText)) {
                         autoOrderUse = false;
                     }
                 }
