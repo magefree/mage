9e5a8df1c940ed0dda832418fb8e1e3a7e115941
LevelX2 <ludwig.hirth@online.de>
2015-09-13 23:42:25 +0200

    * Hex Parasite - Fixed a bug that the boost was not always applied if only one counter was removed.

--- Mage.Sets/src/mage/sets/newphyrexia/HexParasite.java
+++ Mage.Sets/src/mage/sets/newphyrexia/HexParasite.java
@@ -28,11 +28,6 @@
 package mage.sets.newphyrexia;
 
 import java.util.UUID;
-import mage.constants.CardType;
-import mage.constants.Duration;
-import mage.constants.Outcome;
-import mage.constants.Rarity;
-import mage.constants.Zone;
 import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.SimpleActivatedAbility;
@@ -40,6 +35,11 @@ import mage.abilities.costs.mana.ManaCostsImpl;
 import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.continuous.BoostSourceEffect;
 import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Outcome;
+import mage.constants.Rarity;
+import mage.constants.Zone;
 import mage.game.Game;
 import mage.game.permanent.Permanent;
 import mage.players.Player;
@@ -77,12 +77,12 @@ public class HexParasite extends CardImpl {
 
 class HexParasiteEffect extends OneShotEffect {
 
-    HexParasiteEffect ( ) {
+    HexParasiteEffect() {
         super(Outcome.Benefit);
         staticText = "Remove up to X counters from target permanent. For each counter removed this way, {this} gets +1/+0 until end of turn";
     }
 
-    HexParasiteEffect ( HexParasiteEffect effect ) {
+    HexParasiteEffect(HexParasiteEffect effect) {
         super(effect);
     }
 
@@ -93,7 +93,7 @@ class HexParasiteEffect extends OneShotEffect {
 
     @Override
     public boolean apply(Game game, Ability source) {
-        TargetPermanent target = (TargetPermanent)source.getTargets().get(0);
+        TargetPermanent target = (TargetPermanent) source.getTargets().get(0);
         Permanent permanent = game.getPermanent(target.getFirstTarget());
         Player player = game.getPlayer(source.getControllerId());
         if (permanent != null && player != null) {
@@ -104,8 +104,8 @@ class HexParasiteEffect extends OneShotEffect {
                 if (player.chooseUse(Outcome.Neutral, "Do you want to remove " + counterName + " counters?", source, game)) {
                     if (permanent.getCounters().get(counterName).getCount() == 1 || toRemove == 1) {
                         permanent.getCounters().removeCounter(counterName, 1);
-                    }
-                    else {
+                        removed++;
+                    } else {
                         int amount = player.getAmount(1, Math.min(permanent.getCounters().get(counterName).getCount(), toRemove - removed), "How many?", game);
                         if (amount > 0) {
                             removed += amount;
@@ -113,8 +113,9 @@ class HexParasiteEffect extends OneShotEffect {
                         }
                     }
                 }
-                if (removed >= toRemove)
+                if (removed >= toRemove) {
                     break;
+                }
             }
             game.addEffect(new BoostSourceEffect(removed, 0, Duration.EndOfTurn), source);
             return true;
