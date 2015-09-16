40815d8560aad52c397fb5ed99020606756c17b2
LoneFox <lonefox@kapsi.fi>
2015-09-16 09:38:08 +0300

    Implement cards: Benalish Missionary, Boiling Blood, Chimeric Sphere, and Mwonvuli Ooze

--- Mage/src/mage/abilities/effects/common/PreventDamageByTargetEffect.java
+++ Mage/src/mage/abilities/effects/common/PreventDamageByTargetEffect.java
@@ -1,16 +1,16 @@
 /*
  *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
- * 
+ *
  *  Redistribution and use in source and binary forms, with or without modification, are
  *  permitted provided that the following conditions are met:
- * 
+ *
  *     1. Redistributions of source code must retain the above copyright notice, this list of
  *        conditions and the following disclaimer.
- * 
+ *
  *     2. Redistributions in binary form must reproduce the above copyright notice, this list
  *        of conditions and the following disclaimer in the documentation and/or other materials
  *        provided with the distribution.
- * 
+ *
  *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
  *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
@@ -20,7 +20,7 @@
  *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  *  The views and conclusions contained in the software and documentation are those of the
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
@@ -45,7 +45,7 @@ public class PreventDamageByTargetEffect extends PreventionEffectImpl {
     public PreventDamageByTargetEffect(Duration duration) {
         this(duration, false);
     }
-    
+
     public PreventDamageByTargetEffect(Duration duration, int amount) {
         this(duration, amount, false);
     }
@@ -53,11 +53,11 @@ public class PreventDamageByTargetEffect extends PreventionEffectImpl {
     public PreventDamageByTargetEffect(Duration duration, boolean onlyCombat) {
         this(duration, Integer.MAX_VALUE, onlyCombat);
     }
-    
+
     public PreventDamageByTargetEffect(Duration duration, int amount, boolean onlyCombat) {
         super(duration, amount, onlyCombat);
     }
-    
+
     public PreventDamageByTargetEffect(final PreventDamageByTargetEffect effect) {
         super(effect);
     }
@@ -88,7 +88,7 @@ public class PreventDamageByTargetEffect extends PreventionEffectImpl {
             StringBuilder sb = new StringBuilder();
             sb.append("Prevent all");
             if (onlyCombat) {
-                sb.append("combat ");
+                sb.append(" combat ");
             }
             sb.append(" damage target ");
             sb.append(mode.getTargets().get(0).getTargetName()).append(" would deal ").append(duration.toString());
