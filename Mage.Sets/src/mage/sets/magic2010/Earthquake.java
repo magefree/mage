8ed312e7e94f3bd6baad81d536b918f419e384ec
LoneFox <lonefox@kapsi.fi>
2015-09-17 11:37:17 +0300

    Remove some unnecessary custom effects.

--- Mage.Sets/src/mage/sets/magic2010/Earthquake.java
+++ Mage.Sets/src/mage/sets/magic2010/Earthquake.java
@@ -1,16 +1,16 @@
 /*
  *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
- * 
+ *
  *  Redistribution and use in source and binary forms, with or without modification, are
  *  permitted provided that the following conditions are met:
- * 
+ *
  *     1. Redistributions of source code must retain the above copyright notice, this list of
  *        conditions and the following disclaimer.
- * 
+ *
  *     2. Redistributions in binary form must reproduce the above copyright notice, this list
  *        of conditions and the following disclaimer in the documentation and/or other materials
  *        provided with the distribution.
- * 
+ *
  *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
  *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
@@ -20,7 +20,7 @@
  *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  *  The views and conclusions contained in the software and documentation are those of the
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
@@ -29,19 +29,15 @@
 package mage.sets.magic2010;
 
 import java.util.UUID;
-import mage.constants.CardType;
-import mage.constants.Outcome;
-import mage.constants.Rarity;
-import mage.abilities.Ability;
-import mage.abilities.effects.OneShotEffect;
+import mage.abilities.dynamicvalue.common.ManacostVariableValue;
+import mage.abilities.effects.common.DamageEverythingEffect;
 import mage.abilities.keyword.FlyingAbility;
 import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Rarity;
 import mage.filter.common.FilterCreaturePermanent;
 import mage.filter.predicate.Predicates;
 import mage.filter.predicate.mageobject.AbilityPredicate;
-import mage.game.Game;
-import mage.game.permanent.Permanent;
-import mage.players.Player;
 
 /**
  *
@@ -49,11 +45,18 @@ import mage.players.Player;
  */
 public class Earthquake extends CardImpl {
 
+    private static final FilterCreaturePermanent filter = new FilterCreaturePermanent("creature without flying");
+
+    static {
+        filter.add(Predicates.not(new AbilityPredicate(FlyingAbility.class)));
+    }
+
     public Earthquake(UUID ownerId) {
         super(ownerId, 134, "Earthquake", Rarity.RARE, new CardType[]{CardType.SORCERY}, "{X}{R}");
         this.expansionSetCode = "M10";
 
-        this.getSpellAbility().addEffect(new EarthquakeEffect());
+        // Hurricane deals X damage to each creature with flying and each player.
+        this.getSpellAbility().addEffect(new DamageEverythingEffect(new ManacostVariableValue(), filter));
     }
 
     public Earthquake(final Earthquake card) {
@@ -64,42 +67,4 @@ public class Earthquake extends CardImpl {
     public Earthquake copy() {
         return new Earthquake(this);
     }
-}
-
-class EarthquakeEffect extends OneShotEffect {
-
-    private static final FilterCreaturePermanent filter = new FilterCreaturePermanent();
-
-    static {
-        filter.add(Predicates.not(new AbilityPredicate(FlyingAbility.class)));
-    }
-
-    public EarthquakeEffect() {
-        super(Outcome.Damage);
-        staticText = "{this} deals X damage to each creature without flying and each player";
-    }
-
-    public EarthquakeEffect(final EarthquakeEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public EarthquakeEffect copy() {
-        return new EarthquakeEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        int amount = source.getManaCostsToPay().getX();
-        for (Permanent permanent: game.getBattlefield().getActivePermanents(filter, source.getControllerId(), game)) {
-            permanent.damage(amount, source.getSourceId(), game, false, true);
-        }
-        for (UUID playerId: game.getPlayer(source.getControllerId()).getInRange()) {
-            Player player = game.getPlayer(playerId);
-            if (player != null)
-                player.damage(amount, source.getSourceId(), game, false, true);
-        }
-        return true;
-    }
-
 }
