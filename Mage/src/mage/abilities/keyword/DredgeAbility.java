899e2921f0a0ed8cc0fcff7b5fc0f75413579c28
unknown <odowdb@USITISUSEL2293C.corp.emc.com>
2015-09-18 15:39:43 -0400

    Revert "Fixed issue with Dredge reminder text always indicating three cards"
    This reverts commit 85e71fa162e6248ebd79ec800e9f66d6cd68de70.
    Username incorrectly specified.

--- Mage/src/mage/abilities/keyword/DredgeAbility.java
+++ Mage/src/mage/abilities/keyword/DredgeAbility.java
@@ -71,7 +71,7 @@ class DredgeEffect extends ReplacementEffectImpl {
         super(Duration.WhileInGraveyard, Outcome.ReturnToHand);
         this.amount = value;
         this.staticText = new StringBuilder("Dredge ").append(Integer.toString(value))
-                .append(" <i>(If you would draw a card, instead you may put exactly " + value + " card(s) from the top of your library into your graveyard. If you do, return this card from your graveyard to your hand. Otherwise, draw a card.)</i>").toString();
+                .append(" <i>(If you would draw a card, instead you may put exactly three cards from the top of your library into your graveyard. If you do, return this card from your graveyard to your hand. Otherwise, draw a card.)</i>").toString();
     }
 
     public DredgeEffect(final DredgeEffect effect) {
