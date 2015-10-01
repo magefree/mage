73766a21eb2dfd9c6a0d222e6a4b4190b0c3f128
LevelX2 <ludwig.hirth@online.de>
2015-10-02 01:37:15 +0200

    * Fixed that the token image of Kithkin Solder was not shown (token was only named Kithkin instead of Kithkin Solder).

--- Mage.Sets/src/mage/sets/eventide/CennsEnlistment.java
+++ Mage.Sets/src/mage/sets/eventide/CennsEnlistment.java
@@ -28,14 +28,11 @@
 package mage.sets.eventide;
 
 import java.util.UUID;
-
-import mage.constants.CardType;
-import mage.constants.Rarity;
-import mage.abilities.costs.mana.ManaCostsImpl;
 import mage.abilities.effects.common.CreateTokenEffect;
 import mage.abilities.keyword.RetraceAbility;
 import mage.cards.CardImpl;
-import mage.constants.TimingRule;
+import mage.constants.CardType;
+import mage.constants.Rarity;
 import mage.game.permanent.token.KithkinToken;
 
 /**
@@ -50,7 +47,7 @@ public class CennsEnlistment extends CardImpl {
 
         // Put two 1/1 white Kithkin Soldier creature tokens onto the battlefield.
         this.getSpellAbility().addEffect(new CreateTokenEffect(new KithkinToken(), 2));
-        
+
         // Retrace
         this.addAbility(new RetraceAbility(this));
     }
