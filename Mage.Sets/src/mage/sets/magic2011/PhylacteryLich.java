328f7dd7a356b28e20710f4f3f20c350fbc31bf5
LevelX2 <ludwig.hirth@online.de>
2015-10-15 22:02:04 +0200

    * Phylactery Lich - Fixed that the replacement effect to select an artifact was handled targeted.

--- Mage.Sets/src/mage/sets/magic2011/PhylacteryLich.java
+++ Mage.Sets/src/mage/sets/magic2011/PhylacteryLich.java
@@ -1,16 +1,16 @@
 /*
  *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
- * 
+ *
  *  Redistribution and use in source and binary forms, with or without modification, are
  *  permitted provided that the following conditions are met:
- * 
+ *
  *     1. Redistributions of source code must retain the above copyright notice, this list of
  *        conditions and the following disclaimer.
- * 
+ *
  *     2. Redistributions in binary form must reproduce the above copyright notice, this list
  *        of conditions and the following disclaimer in the documentation and/or other materials
  *        provided with the distribution.
- * 
+ *
  *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
  *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
@@ -20,19 +20,14 @@
  *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  *  The views and conclusions contained in the software and documentation are those of the
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
  */
-
 package mage.sets.magic2011;
 
 import java.util.UUID;
-import mage.constants.CardType;
-import mage.constants.Outcome;
-import mage.constants.Rarity;
-import mage.constants.Zone;
 import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.StateTriggeredAbility;
@@ -41,6 +36,10 @@ import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.SacrificeSourceEffect;
 import mage.abilities.keyword.IndestructibleAbility;
 import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Outcome;
+import mage.constants.Rarity;
+import mage.constants.Zone;
 import mage.counters.Counter;
 import mage.filter.common.FilterControlledPermanent;
 import mage.filter.predicate.mageobject.CardTypePredicate;
@@ -64,8 +63,13 @@ public class PhylacteryLich extends CardImpl {
         this.power = new MageInt(5);
         this.toughness = new MageInt(5);
 
-        this.addAbility(new AsEntersBattlefieldAbility(new PhylacteryLichEffect(), "put a phylactery counter on an artifact you control"));
+        // Indestructible
         this.addAbility(IndestructibleAbility.getInstance());
+
+        // As Phylactery Lich enters the battlefield, put a phylactery counter on an artifact you control.
+        this.addAbility(new AsEntersBattlefieldAbility(new PhylacteryLichEffect(), "put a phylactery counter on an artifact you control"));
+
+        // When you control no permanents with phylactery counters on them, sacrifice Phylactery Lich.
         this.addAbility(new PhylacteryLichAbility());
     }
 
@@ -95,7 +99,7 @@ public class PhylacteryLich extends CardImpl {
 
         @Override
         public boolean checkTrigger(GameEvent event, Game game) {
-            for (Permanent perm: game.getBattlefield().getAllActivePermanents(controllerId)) {
+            for (Permanent perm : game.getBattlefield().getAllActivePermanents(controllerId)) {
                 if (perm.getCounters().getCount("phylactery") > 0) {
                     return false;
                 }
@@ -133,7 +137,7 @@ class PhylacteryLichEffect extends OneShotEffect {
     public boolean apply(Game game, Ability source) {
         Player player = game.getPlayer(source.getControllerId());
         if (player != null) {
-            TargetControlledPermanent target = new TargetControlledPermanent(filter);
+            TargetControlledPermanent target = new TargetControlledPermanent(1, 1, filter, true);
             if (target.canChoose(source.getControllerId(), game)) {
                 if (player.choose(Outcome.Neutral, target, source.getSourceId(), game)) {
                     Permanent permanent = game.getPermanent(target.getFirstTarget());
