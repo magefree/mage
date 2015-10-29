b23bd0cf1ffa02e3bc349d96015d1272d56f9b42
Neil Gentleman <ngentleman@gmail.com>
2015-10-28 22:51:24 -0700

    some Fallen Empires cards had the wrong package

--- Mage.Sets/src/mage/sets/fallenempires/IcatianJavelineers3.java
+++ Mage.Sets/src/mage/sets/fallenempires/IcatianJavelineers3.java
@@ -25,7 +25,7 @@
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
  */
-package mage.sets.timeshifted;
+package mage.sets.fallenempires;
 
 import java.util.UUID;
 import mage.constants.Rarity;
