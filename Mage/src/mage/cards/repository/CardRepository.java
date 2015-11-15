576a9ea74f33c2ae4a6952f8a3ae462a249f1573
LevelX2 <ludwig.hirth@online.de>
2015-11-15 10:25:02 +0100

    Updated mtg-cards-data.txt. Added a land deck for Mormir Basic format.

--- Mage/src/mage/cards/repository/CardRepository.java
+++ Mage/src/mage/cards/repository/CardRepository.java
@@ -61,9 +61,9 @@ public enum CardRepository {
     private static final String JDBC_URL = "jdbc:h2:file:./db/cards.h2;AUTO_SERVER=TRUE";
     private static final String VERSION_ENTITY_NAME = "card";
     // raise this if db structure was changed
-    private static final long CARD_DB_VERSION = 42;
+    private static final long CARD_DB_VERSION = 43;
     // raise this if new cards were added to the server
-    private static final long CARD_CONTENT_VERSION = 42;
+    private static final long CARD_CONTENT_VERSION = 43;
 
     private final Random random = new Random();
     private Dao<CardInfo, Object> cardDao;
