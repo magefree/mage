0d86e8e063685c8f66f2b32fd0f6f264a43047ba
LevelX2 <ludwig.hirth@online.de>
2015-11-21 10:33:43 +0100

    Xmage 1.4.5v0

--- Mage.Common/src/mage/utils/MageVersion.java
+++ Mage.Common/src/mage/utils/MageVersion.java
@@ -40,8 +40,8 @@ public class MageVersion implements Serializable, Comparable<MageVersion> {
      */
     public final static int MAGE_VERSION_MAJOR = 1;
     public final static int MAGE_VERSION_MINOR = 4;
-    public final static int MAGE_VERSION_PATCH = 4;
-    public final static String MAGE_VERSION_MINOR_PATCH = "v12";
+    public final static int MAGE_VERSION_PATCH = 5;
+    public final static String MAGE_VERSION_MINOR_PATCH = "v0";
     public final static String MAGE_VERSION_INFO = "";
 
     private final int major;
