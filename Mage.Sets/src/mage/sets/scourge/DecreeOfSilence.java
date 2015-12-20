55dcf800128029a5e7812ffc7f54503ecb869fdc
LoneFox <lonefox@kapsi.fi>
2015-12-20 11:24:31 +0200

    Implement cards: Consumptive Goo, Decree of Silence, Misguided Rage, and Recuperate

---
+++ Mage.Sets/src/mage/sets/scourge/DecreeOfSilence.java
@@ -0,0 +1,89 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.scourge;
+
+import java.util.UUID;
+import mage.abilities.Ability;
+import mage.abilities.common.CycleTriggeredAbility;
+import mage.abilities.common.SpellCastOpponentTriggeredAbility;
+import mage.abilities.condition.common.SourceHasCounterCondition;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.decorator.ConditionalOneShotEffect;
+import mage.abilities.effects.Effect;
+import mage.abilities.effects.common.CounterTargetEffect;
+import mage.abilities.effects.common.SacrificeSourceEffect;
+import mage.abilities.effects.common.counter.AddCountersSourceEffect;
+import mage.abilities.keyword.CyclingAbility;
+import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Rarity;
+import mage.constants.SetTargetPointer;
+import mage.constants.Zone;
+import mage.counters.CounterType;
+import mage.filter.FilterSpell;
+import mage.target.TargetSpell;
+
+/**
+ *
+ * @author LoneFox
+ */
+public class DecreeOfSilence extends CardImpl {
+
+    public DecreeOfSilence(UUID ownerId) {
+        super(ownerId, 32, "Decree of Silence", Rarity.RARE, new CardType[]{CardType.ENCHANTMENT}, "{6}{U}{U}");
+        this.expansionSetCode = "SCG";
+
+        // Whenever an opponent casts a spell, counter that spell and put a depletion counter on Decree of Silence. If there are three or more depletion counters on Decree of Silence, sacrifice it.
+        Effect effect = new CounterTargetEffect();
+        effect.setText("counter that spell");
+        Ability ability = new SpellCastOpponentTriggeredAbility(Zone.BATTLEFIELD, effect, new FilterSpell(),
+            false, SetTargetPointer.SPELL);
+        effect = new AddCountersSourceEffect(CounterType.DEPLETION.createInstance());
+        effect.setText("and put a depletion counter on {this}.");
+        ability.addEffect(effect);
+        ability.addEffect(new ConditionalOneShotEffect(new SacrificeSourceEffect(),
+            new SourceHasCounterCondition(CounterType.DEPLETION, 3, Integer.MAX_VALUE),
+            " If there are three or more depletion counters on {this}, sacrifice it"));
+        this.addAbility(ability);
+        // Cycling {4}{U}{U}
+        this.addAbility(new CyclingAbility(new ManaCostsImpl("{4}{U}{U}")));
+        // When you cycle Decree of Silence, you may counter target spell.
+        ability = new CycleTriggeredAbility(new CounterTargetEffect(), true);
+        ability.addTarget(new TargetSpell());
+        this.addAbility(ability);
+    }
+
+    public DecreeOfSilence(final DecreeOfSilence card) {
+        super(card);
+    }
+
+    @Override
+    public DecreeOfSilence copy() {
+        return new DecreeOfSilence(this);
+    }
+}
