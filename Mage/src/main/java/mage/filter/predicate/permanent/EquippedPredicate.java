7676c5e89490c11b9afb86ec893ef22c159b3090
LevelX2 <ludwig.hirth@online.de>
2016-01-01 14:10:40 +0100

    [OGW] Added General Tazri and Stone Haven Outfitter.

---
+++ Mage/src/main/java/mage/filter/predicate/permanent/EquippedPredicate.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mage.filter.predicate.permanent;
+
+import java.util.UUID;
+import mage.filter.predicate.Predicate;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+
+/**
+ *
+ * @author LevelX2
+ */
+public class EquippedPredicate implements Predicate<Permanent> {
+
+    @Override
+    public boolean apply(Permanent input, Game game) {
+        for (UUID attachmentId : input.getAttachments()) {
+            Permanent attachment = game.getPermanent(attachmentId);
+            if (attachment != null && attachment.getSubtype().contains("Equipment")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "equipped";
+    }
+}
