787f4f010ac6f77432cd383f26f4372ff696c69f
Me Car <menoc4r@gmail.com>
2016-01-07 23:43:28 +0900

    Keep registerClient for older clients. Rename the new version to registerClientWithPassword. Add an error message on NoSuchMethodException so that this kind of version incompatibility can be handled and informed to user.

--- Mage.Common/src/mage/interfaces/MageServer.java
+++ Mage.Common/src/mage/interfaces/MageServer.java
@@ -56,7 +56,11 @@ import mage.view.UserView;
 public interface MageServer {
 
     // connection methods
-    boolean registerClient(String userName, String password, String sessionId, MageVersion version) throws MageException;
+    // DEPRECATED - Use registerClientWithPassword instead. This is kept for older clients.
+    // This can be deleted once users transitioned to newer clients (1.4.6v1 and later).
+    boolean registerClient(String userName, String sessionId, MageVersion version) throws MageException;
+
+    boolean registerClientWithPassword(String userName, String password, String sessionId, MageVersion version) throws MageException;
 
     boolean registerAdmin(String password, String sessionId, MageVersion version) throws MageException;
 // Not used
