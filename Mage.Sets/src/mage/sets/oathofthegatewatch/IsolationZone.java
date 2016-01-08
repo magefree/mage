7a3c0bb8849e18c18e1b3c1c0c529e00f82be2e6
fireshoes <fireshoes@fireshoes-PC>
2016-01-08 15:06:25 -0600

    [OGW] Added most white cards.

--- Mage.Sets/src/mage/sets/oathofthegatewatch/IsolationZone.java
+++ Mage.Sets/src/mage/sets/oathofthegatewatch/IsolationZone.java
@@ -25,7 +25,7 @@
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
  */
-package mage.sets.battleforzendikar;
+package mage.sets.oathofthegatewatch;
 
 import java.util.UUID;
 import mage.abilities.Ability;
@@ -34,69 +34,69 @@ import mage.abilities.common.delayed.OnLeaveReturnExiledToBattlefieldAbility;
 import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.CreateDelayedTriggeredAbilityEffect;
 import mage.abilities.effects.common.ExileTargetEffect;
-import mage.abilities.keyword.FlashAbility;
 import mage.cards.CardImpl;
 import mage.constants.CardType;
 import mage.constants.Outcome;
 import mage.constants.Rarity;
 import mage.constants.TargetController;
-import mage.filter.common.FilterCreaturePermanent;
+import mage.filter.FilterPermanent;
+import mage.filter.predicate.Predicates;
+import mage.filter.predicate.mageobject.CardTypePredicate;
 import mage.filter.predicate.permanent.ControllerPredicate;
 import mage.game.Game;
 import mage.game.permanent.Permanent;
-import mage.target.common.TargetCreaturePermanent;
+import mage.target.TargetPermanent;
 import mage.util.CardUtil;
 
 /**
  *
  * @author fireshoes
  */
-public class StasisSnare extends CardImpl {
+public class IsolationZone extends CardImpl {
 
-    private final static FilterCreaturePermanent filter = new FilterCreaturePermanent("creature an opponent controls");
+    private final static FilterPermanent filter = new FilterPermanent("creature or enchantment an opponent controls");
 
     static {
+        filter.add(Predicates.or(new CardTypePredicate(CardType.CREATURE),
+                new CardTypePredicate(CardType.ENCHANTMENT)));
         filter.add(new ControllerPredicate(TargetController.OPPONENT));
     }
 
-    public StasisSnare(UUID ownerId) {
-        super(ownerId, 50, "Stasis Snare", Rarity.UNCOMMON, new CardType[]{CardType.ENCHANTMENT}, "{1}{W}{W}");
-        this.expansionSetCode = "BFZ";
+    public IsolationZone(UUID ownerId) {
+        super(ownerId, 22, "Isolation Zone", Rarity.COMMON, new CardType[]{CardType.ENCHANTMENT}, "{2}{W}{W}");
+        this.expansionSetCode = "OGW";
 
-        // Flash
-        this.addAbility(FlashAbility.getInstance());
-
-        // When Stasis Snare enters the battlefield, exile target creature an opponent controls until Stasis Snare leaves the battlefield.
-        Ability ability = new EntersBattlefieldTriggeredAbility(new StasisSnareExileEffect());
-        ability.addTarget(new TargetCreaturePermanent(filter));
+        // When Isolation Zone enters the battlefield, exile target creature or enchantment an opponent controls until Isolation Zone leaves the battlefield.
+        Ability ability = new EntersBattlefieldTriggeredAbility(new IsolationZoneExileEffect());
+        ability.addTarget(new TargetPermanent(filter));
         ability.addEffect(new CreateDelayedTriggeredAbilityEffect(new OnLeaveReturnExiledToBattlefieldAbility()));
         this.addAbility(ability);
     }
 
-    public StasisSnare(final StasisSnare card) {
+    public IsolationZone(final IsolationZone card) {
         super(card);
     }
 
     @Override
-    public StasisSnare copy() {
-        return new StasisSnare(this);
+    public IsolationZone copy() {
+        return new IsolationZone(this);
     }
 }
 
-class StasisSnareExileEffect extends OneShotEffect {
+class IsolationZoneExileEffect extends OneShotEffect {
 
-    public StasisSnareExileEffect() {
+    public IsolationZoneExileEffect() {
         super(Outcome.Benefit);
-        this.staticText = "exile target creature an opponent controls until {this} leaves the battlefield";
+        this.staticText = "exile target creature or enchantment an opponent controls until {this} leaves the battlefield";
     }
 
-    public StasisSnareExileEffect(final StasisSnareExileEffect effect) {
+    public IsolationZoneExileEffect(final IsolationZoneExileEffect effect) {
         super(effect);
     }
 
     @Override
-    public StasisSnareExileEffect copy() {
-        return new StasisSnareExileEffect(this);
+    public IsolationZoneExileEffect copy() {
+        return new IsolationZoneExileEffect(this);
     }
 
     @Override
---
+++ Mage.Sets/src/mage/sets/oathofthegatewatch/MakeAStand.java
@@ -0,0 +1,67 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.oathofthegatewatch;
+
+import java.util.UUID;
+import mage.abilities.effects.Effect;
+import mage.abilities.effects.common.continuous.BoostControlledEffect;
+import mage.abilities.effects.common.continuous.GainAbilityControlledEffect;
+import mage.abilities.keyword.IndestructibleAbility;
+import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Rarity;
+
+/**
+ *
+ * @author fireshoes
+ */
+public class MakeAStand extends CardImpl {
+
+    public MakeAStand(UUID ownerId) {
+        super(ownerId, 26, "Make a Stand", Rarity.UNCOMMON, new CardType[]{CardType.INSTANT}, "{2}{W}");
+        this.expansionSetCode = "OGW";
+
+        // Creature you control get +1/+0 and gain indestructible until end of turn.
+        Effect effect1 = new BoostControlledEffect(1, 0, Duration.EndOfTurn);
+        effect1.setText("Creature you control get +1/+0");
+        this.getSpellAbility().addEffect(effect1);
+        Effect effect2 = new GainAbilityControlledEffect(IndestructibleAbility.getInstance(), Duration.EndOfTurn);
+        effect2.setText("and gain indestructible until end of turn");
+        this.getSpellAbility().addEffect(effect2);
+    }
+
+    public MakeAStand(final MakeAStand card) {
+        super(card);
+    }
+
+    @Override
+    public MakeAStand copy() {
+        return new MakeAStand(this);
+    }
+}
