cdbda6a7e685621a6f12d5d541a3d7ae30c7693b
LevelX2 <ludwig.hirth@online.de>
2016-01-15 21:03:39 +0100

    * Fixed ArrayIndexOutOfBoundsException by trying to do actions on tables removed meanwhile.

--- Mage.Client/src/main/java/mage/client/util/ButtonColumn.java
+++ Mage.Client/src/main/java/mage/client/util/ButtonColumn.java
@@ -1,31 +1,30 @@
 /*
-* Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without modification, are
-* permitted provided that the following conditions are met:
-*
-*    1. Redistributions of source code must retain the above copyright notice, this list of
-*       conditions and the following disclaimer.
-*
-*    2. Redistributions in binary form must reproduce the above copyright notice, this list
-*       of conditions and the following disclaimer in the documentation and/or other materials
-*       provided with the distribution.
-*
-* THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
-* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-* FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
-* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-* ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-* ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-* The views and conclusions contained in the software and documentation are those of the
-* authors and should not be interpreted as representing official policies, either expressed
-* or implied, of BetaSteward_at_googlemail.com.
-*/
-
+ * Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice, this list of
+ *       conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *       of conditions and the following disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and should not be interpreted as representing official policies, either expressed
+ * or implied, of BetaSteward_at_googlemail.com.
+ */
 package mage.client.util;
 
 import java.awt.Component;
@@ -78,64 +77,64 @@ public class ButtonColumn extends AbstractCellEditor implements TableCellRendere
 
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-        if (hasFocus)
-        {
+        if (hasFocus) {
             renderButton.setForeground(table.getForeground());
             renderButton.setBackground(UIManager.getColor("Button.background"));
-        }
-        else if (isSelected)
-        {
+        } else if (isSelected) {
             renderButton.setForeground(table.getSelectionForeground());
-             renderButton.setBackground(table.getSelectionBackground());
-        }
-        else
-        {
+            renderButton.setBackground(table.getSelectionBackground());
+        } else {
             renderButton.setForeground(table.getForeground());
             renderButton.setBackground(UIManager.getColor("Button.background"));
         }
 
-        renderButton.setText( (value == null) ? "" : value.toString() );
+        renderButton.setText((value == null) ? "" : value.toString());
         return renderButton;
     }
 
     @Override
     public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
         text = (value == null) ? "" : value.toString();
-        editButton.setText( text );
+        editButton.setText(text);
         return editButton;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        int row = table.convertRowIndexToModel( table.getEditingRow() );
-        fireEditingStopped();
-        ActionEvent event = new ActionEvent(table, ActionEvent.ACTION_PERFORMED, "" + row);
-        action.actionPerformed(event);
-
+        if (table.getRowCount() >= table.getEditingRow()) {
+            int row = table.convertRowIndexToModel(table.getEditingRow());
+            fireEditingStopped();
+            ActionEvent event = new ActionEvent(table, ActionEvent.ACTION_PERFORMED, "" + row);
+            action.actionPerformed(event);
+        }
     }
 
     @Override
     public void mousePressed(MouseEvent arg0) {
-        if (table.isEditing() && table.getCellEditor() == this)
+        if (table.isEditing() && table.getCellEditor() == this) {
             isButtonColumnEditor = true;
+        }
     }
 
     @Override
     public void mouseReleased(MouseEvent arg0) {
-        if (isButtonColumnEditor && table.isEditing())
+        if (isButtonColumnEditor && table.isEditing()) {
             table.getCellEditor().stopCellEditing();
+        }
 
         isButtonColumnEditor = false;
     }
 
     @Override
-    public void mouseClicked(MouseEvent arg0) {}
+    public void mouseClicked(MouseEvent arg0) {
+    }
 
     @Override
-    public void mouseEntered(MouseEvent arg0) {}
+    public void mouseEntered(MouseEvent arg0) {
+    }
 
     @Override
-    public void mouseExited(MouseEvent arg0) {}
-
+    public void mouseExited(MouseEvent arg0) {
+    }
 
 }
