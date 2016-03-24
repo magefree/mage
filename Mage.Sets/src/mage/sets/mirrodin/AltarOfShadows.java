34150c2e40f18a440b4d3da8057139c150a32074
drmDev <dmontur1@gmail.com>
2016-03-24 17:32:00 -0400

    Altar of Shadows card implementation

---
+++ Mage.Sets/src/mage/sets/mirrodin/AltarOfShadows.java
@@ -0,0 +1,112 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.mirrodin;
+
+import java.util.UUID;
+import mage.Mana;
+import mage.abilities.Ability;
+import mage.abilities.common.BeginningOfPreCombatMainTriggeredAbility;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.costs.mana.GenericManaCost;
+import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.DestroyTargetEffect;
+import mage.abilities.effects.common.counter.AddCountersSourceEffect;
+import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Outcome;
+import mage.constants.Rarity;
+import mage.constants.TargetController;
+import mage.constants.Zone;
+import mage.counters.CounterType;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.players.Player;
+import mage.target.common.TargetCreaturePermanent;
+
+/**
+ *
+ * @author escplan9 (Derek Monturo - dmontur1 at gmail dot com)
+ */
+public class AltarOfShadows extends CardImpl {
+
+    public AltarOfShadows(UUID ownerId) {
+        super(ownerId, 143, "Altar of Shadows", Rarity.RARE, new CardType[]{CardType.ARTIFACT}, "{7}");
+        this.expansionSetCode = "MRD";
+
+        // At the beginning of your precombat main phase, add {B} to your mana pool for each charge counter on Altar of Shadows.
+        this.addAbility(new BeginningOfPreCombatMainTriggeredAbility(new AltarOfShadowsEffect(), TargetController.YOU, false));
+        
+        // {7}, {tap}: Destroy target creature. Then put a charge counter on Altar of Shadows.
+        Ability destroyAbility = new SimpleActivatedAbility(Zone.BATTLEFIELD, new DestroyTargetEffect(), new GenericManaCost(7));
+        destroyAbility.addCost(new TapSourceCost());
+        destroyAbility.addEffect(new AddCountersSourceEffect(CounterType.CHARGE.createInstance(), true));
+        destroyAbility.addTarget(new TargetCreaturePermanent());
+        this.addAbility(destroyAbility);
+    }
+
+    public AltarOfShadows(final AltarOfShadows card) {
+        super(card);
+    }
+
+    @java.lang.Override
+    public AltarOfShadows copy() {
+        return new AltarOfShadows(this);
+    }
+}
+
+class AltarOfShadowsEffect extends OneShotEffect {
+
+    public AltarOfShadowsEffect() {
+        super(Outcome.PutManaInPool);
+        this.staticText = "add {B} to your mana pool for each charge counter on Altar of Shadows";
+    }
+
+    public AltarOfShadowsEffect(final AltarOfShadowsEffect effect) {
+        super(effect);
+    }
+
+    @java.lang.Override
+    public AltarOfShadowsEffect copy() {
+        return new AltarOfShadowsEffect(this);
+    }
+
+    @java.lang.Override
+    public boolean apply(Game game, Ability source) {
+        Permanent sourcePermanent = game.getPermanent(source.getSourceId());
+        Player player = game.getPlayer(source.getControllerId());
+        if (sourcePermanent != null && player != null) {
+            int chargeCounters = sourcePermanent.getCounters().getCount(CounterType.CHARGE);
+            if (chargeCounters > 0){
+                player.getManaPool().addMana(Mana.BlackMana(chargeCounters), game, source);
+                return true;
+            }
+        }
+        return false;
+    }
+}
