d6c12d3577226fba965f93ba7fcc0455323809e5
fireshoes <fireshoes@fireshoes-PC>
2016-03-24 20:37:10 -0500

    [SOI] Updated mtg-cards-data.txt with additional 3/24 spoilers. Added several cards.

--- Mage.Sets/src/mage/sets/alarareborn/SigilOfTheNayanGods.java
+++ Mage.Sets/src/mage/sets/alarareborn/SigilOfTheNayanGods.java
@@ -28,11 +28,6 @@
 package mage.sets.alarareborn;
 
 import java.util.UUID;
-import mage.constants.CardType;
-import mage.constants.Duration;
-import mage.constants.Outcome;
-import mage.constants.Rarity;
-import mage.constants.Zone;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.costs.mana.ManaCostsImpl;
 import mage.abilities.dynamicvalue.common.PermanentsOnBattlefieldCount;
@@ -41,6 +36,11 @@ import mage.abilities.effects.common.continuous.BoostEnchantedEffect;
 import mage.abilities.keyword.CyclingAbility;
 import mage.abilities.keyword.EnchantAbility;
 import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Outcome;
+import mage.constants.Rarity;
+import mage.constants.Zone;
 import mage.filter.common.FilterControlledCreaturePermanent;
 import mage.target.TargetPermanent;
 import mage.target.common.TargetCreaturePermanent;
@@ -58,17 +58,17 @@ public class SigilOfTheNayanGods extends CardImpl {
         this.expansionSetCode = "ARB";
         this.subtype.add("Aura");
 
-
-
-
+        // Enchant creature
         TargetPermanent auraTarget = new TargetCreaturePermanent();
         this.getSpellAbility().addTarget(auraTarget);
         this.getSpellAbility().addEffect(new AttachEffect(Outcome.BoostCreature));
         this.addAbility(new EnchantAbility(auraTarget.getTargetName()));
 
+        // Enchanted creature gets +1/+1 for each creature you control.
         PermanentsOnBattlefieldCount amount = new PermanentsOnBattlefieldCount(filter, 1);
         this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new BoostEnchantedEffect(amount, amount, Duration.WhileOnBattlefield)));
 
+        // Cycling {G/W}
         this.addAbility(new CyclingAbility(new ManaCostsImpl("{G/W}")));
     }
 
