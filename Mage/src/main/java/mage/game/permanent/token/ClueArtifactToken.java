f88fb6fcaed5e9239a8cb6dc13d4b1218abb63b1
fireshoes <fireshoes@fireshoes-PC>
2016-04-20 09:59:58 -0500

    Randomized which Clue tokens appear for SOI.

--- Mage/src/main/java/mage/game/permanent/token/ClueArtifactToken.java
+++ Mage/src/main/java/mage/game/permanent/token/ClueArtifactToken.java
@@ -1,16 +1,16 @@
 /*
  *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
- * 
+ *
  *  Redistribution and use in source and binary forms, with or without modification, are
  *  permitted provided that the following conditions are met:
- * 
+ *
  *     1. Redistributions of source code must retain the above copyright notice, this list of
  *        conditions and the following disclaimer.
- * 
+ *
  *     2. Redistributions in binary form must reproduce the above copyright notice, this list
  *        of conditions and the following disclaimer in the documentation and/or other materials
  *        provided with the distribution.
- * 
+ *
  *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
  *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
@@ -20,13 +20,17 @@
  *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  *  The views and conclusions contained in the software and documentation are those of the
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
  */
 package mage.game.permanent.token;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import mage.abilities.Ability;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.common.SacrificeSourceCost;
@@ -36,13 +40,19 @@ import mage.constants.CardType;
 import mage.constants.Zone;
 
 /**
- * 
+ *
  */
 public class ClueArtifactToken extends Token {
 
+    final static private List<String> tokenImageSets = new ArrayList<>();
+
+    static {
+        tokenImageSets.addAll(Arrays.asList("SOI", "EDM"));
+    }
+
     public ClueArtifactToken() {
         super("Clue", "colorless Clue artifact token onto the battlefield with \"{2}, Sacrifice this artifact: Draw a card.\"");
-        this.setOriginalExpansionSetCode("SOI");
+        availableImageSetCodes = tokenImageSets;
         cardType.add(CardType.ARTIFACT);
         subtype.add("Clue");
 
@@ -52,5 +62,26 @@ public class ClueArtifactToken extends Token {
         cost.setText("Sacrifice this artifact");
         ability.addCost(cost);
         this.addAbility(ability);
+    }
+
+
+    @Override
+    public void setExpansionSetCodeForImage(String code) {
+        super.setExpansionSetCodeForImage(code);
+        if (getOriginalExpansionSetCode().equals("SOI")) {
+            this.setTokenType(new Random().nextInt(6) + 1); // 6 different images
+        }
+        if (getOriginalExpansionSetCode().equals("EDM")) {
+            this.setTokenType(new Random().nextInt(6) + 1); // 6 different images
+        }
+    }
+
+    public ClueArtifactToken(final ClueArtifactToken token) {
+        super(token);
+    }
+
+    @Override
+    public ClueArtifactToken copy() {
+        return new ClueArtifactToken(this);
     }
 }
