661c9c985f199315d747508c70d8d306d7d55cc4
drmDev <dmontur1@gmail.com>
2016-05-07 17:18:23 -0400

    fix twisted exp

--- Mage.Sets/src/mage/sets/urzasdestiny/TwistedExperiment.java
+++ Mage.Sets/src/mage/sets/urzasdestiny/TwistedExperiment.java
@@ -60,7 +60,7 @@ public class TwistedExperiment extends CardImpl {
         this.getSpellAbility().addEffect(new AttachEffect(Outcome.BoostCreature));
         Ability ability = new EnchantAbility(auraTarget.getTargetName());
         this.addAbility(ability);
-        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new BoostEnchantedEffect(2, -1, Duration.WhileOnBattlefield)));
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new BoostEnchantedEffect(3, -1, Duration.WhileOnBattlefield)));
     }
 
     public TwistedExperiment(final TwistedExperiment card) {
