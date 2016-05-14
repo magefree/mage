90e38ad0311c1ad852ce1eac0e3542f240583b38
Nathaniel Brandes <ndbrandes@gmail.com>
2016-05-13 23:23:42 -0700

    Rollback WIP file checked in by mistake.

--- Mage.Sets/src/mage/sets/shadowsoverinnistrad/HarnessTheStorm.java
+++ Mage.Sets/src/mage/sets/shadowsoverinnistrad/HarnessTheStorm.java
@@ -27,10 +27,8 @@
  */
 package mage.sets.shadowsoverinnistrad;
 
-import java.util.LinkedHashMap;
 import java.util.UUID;
 import mage.abilities.Ability;
-import mage.abilities.ActivatedAbility;
 import mage.abilities.common.SpellCastControllerTriggeredAbility;
 import mage.abilities.effects.Effect;
 import mage.abilities.effects.OneShotEffect;
@@ -135,10 +133,7 @@ class HarnessTheStormEffect extends OneShotEffect {
             Card card = controller.getGraveyard().get(getTargetPointer().getFirst(game, source), game);
             if (card != null) {
                 if (controller.chooseUse(outcome, "Cast " + card.getIdName() + " from your graveyard?", source, game)) {
-                    //LinkedHashMap<UUID, ActivatedAbility> useableAbilities = controller.getUseableActivatedAbilities(object, zone, game);
-                    //if (useableAbilities != null && useableAbilities.size() > 0) {
-                    	//controller.activateAbility(useableAbilities, card, game);
-                    //}
+                    controller.cast(card.getSpellAbility(), game, false);
                 }
             }
             return true;
