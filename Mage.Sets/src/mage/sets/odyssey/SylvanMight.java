1045f357053154e0642f04495960228e6747a1a5
LevelX2 <ludwig.hirth@online.de>
2016-05-29 17:38:08 +0200

    * Sylvan Might - Fixed that Trample was not given to target creature.

--- Mage.Sets/src/mage/sets/odyssey/SylvanMight.java
+++ Mage.Sets/src/mage/sets/odyssey/SylvanMight.java
@@ -29,8 +29,11 @@ package mage.sets.odyssey;
 
 import java.util.UUID;
 import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.Effect;
 import mage.abilities.effects.common.continuous.BoostTargetEffect;
+import mage.abilities.effects.common.continuous.GainAbilityTargetEffect;
 import mage.abilities.keyword.FlashbackAbility;
+import mage.abilities.keyword.TrampleAbility;
 import mage.cards.CardImpl;
 import mage.constants.CardType;
 import mage.constants.Duration;
@@ -49,7 +52,12 @@ public class SylvanMight extends CardImpl {
         this.expansionSetCode = "ODY";
 
         // Target creature gets +2/+2 and gains trample until end of turn.
-        this.getSpellAbility().addEffect(new BoostTargetEffect(2, 2, Duration.EndOfTurn));
+        Effect effect = new BoostTargetEffect(2, 2, Duration.EndOfTurn);
+        effect.setText("Target creature gets +2/+2");
+        this.getSpellAbility().addEffect(effect);
+        effect = new GainAbilityTargetEffect(TrampleAbility.getInstance(), Duration.EndOfTurn);
+        effect.setText("and gains trample until end of turn");
+        this.getSpellAbility().addEffect(effect);
         this.getSpellAbility().addTarget(new TargetCreaturePermanent());
         // Flashback {2}{G}{G}
         this.addAbility(new FlashbackAbility(new ManaCostsImpl("{2}{G}{G}"), TimingRule.INSTANT));
