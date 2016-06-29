4ed2e408411e8203ac98e1c36de681e131c84809
fireshoes <fireshoes@fireshoes-PC>
2016-06-29 11:59:05 -0500

    [EMN] Added more 6/29 spoilers to mtg-cards-data.txt.

---
+++ Mage.Sets/src/mage/sets/eldritchmoon/LoneRider.java
@@ -0,0 +1,109 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.eldritchmoon;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.TriggeredAbility;
+import mage.abilities.common.BeginningOfEndStepTriggeredAbility;
+import mage.abilities.condition.Condition;
+import mage.abilities.condition.IntCompareCondition;
+import mage.abilities.decorator.ConditionalTriggeredAbility;
+import mage.abilities.effects.common.TransformSourceEffect;
+import mage.abilities.keyword.FirstStrikeAbility;
+import mage.abilities.keyword.LifelinkAbility;
+import mage.abilities.keyword.TransformAbility;
+import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Rarity;
+import mage.constants.TargetController;
+import mage.game.Game;
+import mage.watchers.common.PlayerGainedLifeWatcher;
+
+/**
+ *
+ * @author fireshoes
+ */
+public class LoneRider extends CardImpl {
+
+    public LoneRider(UUID ownerId) {
+        super(ownerId, 33, "Lone Rider", Rarity.UNCOMMON, new CardType[]{CardType.CREATURE}, "{1}{W}");
+        this.expansionSetCode = "EMN";
+        this.subtype.add("Human");
+        this.subtype.add("Knight");
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(1);
+
+        this.canTransform = true;
+        this.secondSideCard = new ItThatRidesAsOne(ownerId);
+
+        // First strike
+        this.addAbility(FirstStrikeAbility.getInstance());
+
+        // Lifelink
+        this.addAbility(LifelinkAbility.getInstance());
+
+        // At the beginning of the end step, if you gained 3 or more life this turn, transform Lone Rider.
+        this.addAbility(new TransformAbility());
+        TriggeredAbility ability = new BeginningOfEndStepTriggeredAbility(new TransformSourceEffect(true), TargetController.ANY, false);
+        this.addAbility(new ConditionalTriggeredAbility(ability, new YouGainedLifeCondition(Condition.ComparisonType.GreaterThan, 2),
+                "At the beginning of the end step, if you gained 3 or more life this turn, transform {this}"));
+    }
+
+    public LoneRider(final LoneRider card) {
+        super(card);
+    }
+
+    @Override
+    public LoneRider copy() {
+        return new LoneRider(this);
+    }
+}
+
+class YouGainedLifeCondition extends IntCompareCondition {
+
+    public YouGainedLifeCondition(Condition.ComparisonType type, int value) {
+        super(type, value);
+    }
+
+    @Override
+    protected int getInputValue(Game game, Ability source) {
+        int gainedLife = 0;
+        PlayerGainedLifeWatcher watcher = (PlayerGainedLifeWatcher) game.getState().getWatchers().get("PlayerGainedLifeWatcher");
+        if (watcher != null) {
+            gainedLife = watcher.getLiveGained(source.getControllerId());
+        }
+        return gainedLife;
+    }
+
+    @Override
+    public String toString() {
+        return "if you gained 3 or more life this turn ";
+    }
+}
