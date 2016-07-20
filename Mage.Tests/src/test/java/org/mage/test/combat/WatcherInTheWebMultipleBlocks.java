68aa9d7c13776da1481ee6a15f009827d52f88b4
drmDev <dmontur1@gmail.com>
2016-07-20 08:39:10 -0400

    added ignore flag to test until framework supports blocking multiple creatures

--- Mage.Tests/src/test/java/org/mage/test/combat/WatcherInTheWebMultipleBlocks.java
+++ Mage.Tests/src/test/java/org/mage/test/combat/WatcherInTheWebMultipleBlocks.java
@@ -29,6 +29,7 @@ package org.mage.test.combat;
 
 import mage.constants.PhaseStep;
 import mage.constants.Zone;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mage.test.serverside.base.CardTestPlayerBase;
 
@@ -38,6 +39,8 @@ import org.mage.test.serverside.base.CardTestPlayerBase;
  */
 public class WatcherInTheWebMultipleBlocks extends CardTestPlayerBase {
 
+    // test must be ignored until creature blocking multiple supported by test framework
+    @Ignore
     @Test
     public void testCombat() {
         addCard(Zone.BATTLEFIELD, playerA, "Watcher in the Web", 1);
