bd70f923b7fd5b49f928ff01e7cda78c04de8da5
LevelX2 <ludwig.hirth@online.de>
2016-07-24 17:13:53 +0200

    * Permeating Mass - Fixed that the copy effect did only work, if Permeating Mass did not die from combat damge.

--- Mage.Sets/src/mage/sets/eldritchmoon/PermeatingMass.java
+++ Mage.Sets/src/mage/sets/eldritchmoon/PermeatingMass.java
@@ -88,9 +88,9 @@ class PermeatingMassEffect extends OneShotEffect {
     public boolean apply(Game game, Ability ability) {
         Permanent copyTo = game.getPermanentOrLKIBattlefield(getTargetPointer().getFirst(game, ability));
         if (copyTo != null) {
-            Permanent copyFrom = game.getPermanent(ability.getSourceId());
+            Permanent copyFrom = (Permanent) ability.getSourceObject(game);
             if (copyFrom != null) {
-                game.copyPermanent(Duration.WhileOnBattlefield, copyFrom, copyTo.getId(), ability, new EmptyApplyToPermanent());
+                game.copyPermanent(Duration.Custom, copyFrom, copyTo.getId(), ability, new EmptyApplyToPermanent());
             }
         }
         return true;
