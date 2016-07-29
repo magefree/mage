b7e0702b6278b59b5396551f3073be0e40577abd
drmDev <dmontur1@gmail.com>
2016-07-29 11:25:24 -0400

    Fixed Genesis bug - activation cost is 2G not 3 colorless

--- Mage.Sets/src/mage/sets/judgment/Genesis.java
+++ Mage.Sets/src/mage/sets/judgment/Genesis.java
@@ -58,7 +58,7 @@ public class Genesis extends CardImpl {
 
         // At the beginning of your upkeep, if Genesis is in your graveyard, you may pay {2}{G}. If you do, return target creature card from your graveyard to your hand.
         Ability ability = new BeginningOfUpkeepTriggeredAbility(
-                Zone.GRAVEYARD, new DoIfCostPaid(new ReturnFromGraveyardToHandTargetEffect(), new ManaCostsImpl("{3}")),TargetController.YOU, false, false);
+                Zone.GRAVEYARD, new DoIfCostPaid(new ReturnFromGraveyardToHandTargetEffect(), new ManaCostsImpl("{2}{G}")),TargetController.YOU, false, false);
         ability.addTarget(new TargetCardInYourGraveyard(new FilterCreatureCard("creature card from your graveyard")));
         this.addAbility(ability);
     }
