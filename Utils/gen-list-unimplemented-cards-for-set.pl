3636b84b0999c97f8655ade1ef366d3488957a83
spjspj <spjspj4@gmail.com>
2016-09-17 12:39:40 +1000

    spjspj - Generate a 'tracking issue' similar to Fenhl's for kaladesh.   Prompt if you've not typed in a correct set name

--- Utils/gen-list-unimplemented-cards-for-set.pl
+++ Utils/gen-list-unimplemented-cards-for-set.pl
@@ -14,11 +14,42 @@ my %knownSets;
 
 my @setCards;
 
+open (DATA, $knownSetsFile) || die "can't open $knownSetsFile";
+while(my $line = <DATA>) {
+    my @data = split('\\|', $line);
+    $knownSets{$data[0]} = $data[1];
+    #print ("$data[0] ===> $data[1]\n");
+}
+close(DATA);
+
+
 # gets the set name
 my $setName = $ARGV[0];
 if(!$setName) {
     print 'Enter a set name: ';
     $setName = <STDIN>;
+    chomp $setName;
+    $setName = $setName;
+}
+
+while (!defined ($knownSets{$setName}))
+{
+    print ("Invalid set - '$setName'\n");
+    print ("  Possible sets you meant:\n");
+    my $origSetName = $setName;
+    $setName =~ s/^(.).*/$1/;
+    my $key;
+    foreach $key (sort keys (%knownSets))
+    {
+        if ($key =~ m/^$setName/img)
+        {
+            print ("   '$key'\n");
+        }
+    }
+
+    print 'Enter a set name: ';
+    $setName = <STDIN>;
+    $setName = $setName;
     chomp $setName;
 }
 
@@ -29,13 +60,6 @@ while(my $line = <DATA>) {
     if ($data[1] eq $setName) {
         push(@setCards, \@data);
     }
-}
-close(DATA);
-
-open (DATA, $knownSetsFile) || die "can't open $knownSetsFile";
-while(my $line = <DATA>) {
-    my @data = split('\\|', $line);
-    $knownSets{$data[0]}= $data[1];
 }
 close(DATA);
 
@@ -63,11 +87,16 @@ sub toCamelCase {
 }
 
 # TODO: check for basic lands with ending 1,2,3,4,5 ...
+my %cardNames;
 my $toPrint = '';
 foreach my $card (sort cardSort @setCards) {
 	my $className = toCamelCase(@{$card}[0]);
+
+    $cardNames {@{$card}[0]} = 1;
+
     my $currentFileName = "../Mage.Sets/src/mage/sets/" . $knownSets{$setName} . "/" . $className . ".java";
 	if(! -e $currentFileName) {
+        $cardNames {@{$card}[0]} = 0;
 		if ($toPrint) {
 			$toPrint .= "\n";
 		}   
@@ -79,3 +108,24 @@ open CARD, "> " . lc($sets{$setName}) ."_unimplemented.txt";
 print CARD $toPrint;
 close CARD;
 
+
+print ("Unimplemented cards are here: " . lc($sets{$setName}) ."_unimplemented.txt\n");
+
+open ISSUE_TRACKER, "> " . lc($sets{$setName}) ."_issue_tracker.txt";
+print ISSUE_TRACKER "# Cards in set:\n- [x] [Example Card Name] (example URL here)\n";
+
+
+my $cn;
+foreach $cn (sort keys (%cardNames))
+{
+    my $x_or_not = "[ ]";
+    if ($cardNames {$cn} == 1)
+    {
+        $x_or_not = "[x]";
+    }
+    my $cn2 = $cn;
+    $cn2 =~ s/ /+/g;
+    print ISSUE_TRACKER "- $x_or_not [$cn] (https://www.google.com.au/search?q=$cn2+MTG&tbm=isch)\n";
+}
+close ISSUE_TRACKER;
+print ("Tracking Issue text for a new Github issue (similar to https://github.com/magefree/mage/issues/2215): " . lc($sets{$setName}) ."_issue_tracker.txt\n");
