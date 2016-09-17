4b768812f676402924b68d597503a4f923a3b81c
LevelX2 <ludwig.hirth@online.de>
2016-09-17 15:56:18 +0200

    [KLD] Added 6 artifacts and 1 multicolor card.

---
+++ Mage.Sets/src/mage/sets/kaladesh/EagerConstruct.java
@@ -0,0 +1,101 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.sets.kaladesh;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.effects.OneShotEffect;
+import mage.cards.CardImpl;
+import mage.constants.CardType;
+import mage.constants.Outcome;
+import mage.constants.Rarity;
+import mage.game.Game;
+import mage.players.Player;
+
+/**
+ *
+ * @author LevelX2
+ */
+public class EagerConstruct extends CardImpl {
+
+    public EagerConstruct(UUID ownerId) {
+        super(ownerId, 209, "Eager Construct", Rarity.COMMON, new CardType[]{CardType.ARTIFACT, CardType.CREATURE}, "{2}");
+        this.expansionSetCode = "KLD";
+        this.subtype.add("Construct");
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(2);
+
+        // When Eager Construct enters the battlefield, each player may scry 1.
+        this.addAbility(new EntersBattlefieldTriggeredAbility(new EagerConstructEffect()));
+    }
+
+    public EagerConstruct(final EagerConstruct card) {
+        super(card);
+    }
+
+    @Override
+    public EagerConstruct copy() {
+        return new EagerConstruct(this);
+    }
+}
+
+class EagerConstructEffect extends OneShotEffect {
+
+    public EagerConstructEffect() {
+        super(Outcome.Benefit);
+        this.staticText = "each player may scry 1";
+    }
+
+    public EagerConstructEffect(final EagerConstructEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public EagerConstructEffect copy() {
+        return new EagerConstructEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Player controller = game.getPlayer(source.getControllerId());
+        if (controller != null) {
+            for (UUID playerId : game.getState().getPlayersInRange(source.getControllerId(), game)) {
+                Player player = game.getPlayer(playerId);
+                if (player != null) {
+                    if (player.chooseUse(outcome, "Scry 1? <i>(Look at the top card of your library. You may put that card on the bottom of your library.)</i>", source, game)) {
+                        player.scry(1, source, game);
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+}
