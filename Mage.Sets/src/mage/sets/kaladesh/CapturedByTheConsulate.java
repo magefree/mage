d01e8303e84a04e99733cfbcf60b6c937f42240e
brodee <brodylodmell@gmail.com>
2016-09-18 15:35:56 -0700

    Captured by the Consulate incorrectly prevented creature from blocking

--- Mage.Sets/src/mage/sets/kaladesh/CapturedByTheConsulate.java
+++ Mage.Sets/src/mage/sets/kaladesh/CapturedByTheConsulate.java
@@ -81,8 +81,8 @@ public class CapturedByTheConsulate extends CardImpl {
         Ability ability = new EnchantAbility(auraTarget.getTargetName());
         this.addAbility(ability);
 
-        // Enchanted creature can't attack or block.
-        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new CantAttackAttachedEffect (AttachmentType.AURA)));
+        // Enchanted creature can't attack.
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new CantAttackAttachedEffect(AttachmentType.AURA)));
 
         // Whenever an opponent casts a spell, if it has a single target, change the target to enchanted creature if able.
         this.addAbility(new CapturedByTheConsulateTriggeredAbility(Zone.BATTLEFIELD, new CapturedByTheConsulateEffect()));
