769a37958d33f5899d7da6b6f085d3b150558852
LevelX2 <ludwig.hirth@online.de>
2016-10-08 11:31:17 +0200

    * Cauldron of Souls - Fixed tooltip text.

--- Mage.Sets/src/mage/cards/c/CauldronOfSouls.java
+++ Mage.Sets/src/mage/cards/c/CauldronOfSouls.java
@@ -31,6 +31,7 @@ import java.util.UUID;
 import mage.abilities.Ability;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.effects.Effect;
 import mage.abilities.effects.common.continuous.GainAbilityTargetEffect;
 import mage.abilities.keyword.PersistAbility;
 import mage.cards.CardImpl;
@@ -48,10 +49,12 @@ import mage.target.common.TargetCreaturePermanent;
 public class CauldronOfSouls extends CardImpl {
 
     public CauldronOfSouls(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.ARTIFACT},"{5}");
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT}, "{5}");
 
         // {tap}: Choose any number of target creatures. Each of those creatures gains persist until end of turn.
-        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new GainAbilityTargetEffect(new PersistAbility(), Duration.EndOfTurn), new TapSourceCost());
+        Effect effect = new GainAbilityTargetEffect(new PersistAbility(), Duration.EndOfTurn);
+        effect.setText("choose any number of target creatures. Each of those creatures gains persist until end of turn");
+        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, effect, new TapSourceCost());
         ability.addTarget(new TargetCreaturePermanent(0, Integer.MAX_VALUE, new FilterCreaturePermanent(), false));
         this.addAbility(ability);
 
