a6747590b336329404d34f3b1780b5fe9aab7340
LevelX2 <ludwig.hirth@online.de>
2016-10-08 08:55:31 +0200

    * Sachi, Daughter of Seshiro - Fixed that the mana abaility was only given to creatures instead to all Shaman permanents (e.g. Thornbite Staff) fixes #2439.

--- Mage.Sets/src/mage/cards/s/SachiDaughterOfSeshiro.java
+++ Mage.Sets/src/mage/cards/s/SachiDaughterOfSeshiro.java
@@ -1,4 +1,4 @@
-    /*
+/*
  *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without modification, are
@@ -25,13 +25,9 @@
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
  */
-
 package mage.cards.s;
 
 import java.util.UUID;
-import mage.constants.CardType;
-import mage.constants.Duration;
-import mage.constants.Zone;
 import mage.MageInt;
 import mage.Mana;
 import mage.abilities.common.SimpleStaticAbility;
@@ -42,6 +38,10 @@ import mage.abilities.effects.common.continuous.GainAbilityControlledEffect;
 import mage.abilities.mana.SimpleManaAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Zone;
+import mage.filter.FilterPermanent;
 import mage.filter.common.FilterCreaturePermanent;
 import mage.filter.predicate.mageobject.SubtypePredicate;
 
@@ -50,9 +50,8 @@ import mage.filter.predicate.mageobject.SubtypePredicate;
  */
 public class SachiDaughterOfSeshiro extends CardImpl {
 
-
     private static final FilterCreaturePermanent snakeFilter = new FilterCreaturePermanent("Snakes");
-    private static final FilterCreaturePermanent shamanFilter = new FilterCreaturePermanent("Shamans");
+    private static final FilterPermanent shamanFilter = new FilterPermanent("Shamans");
 
     static {
         snakeFilter.add(new SubtypePredicate("Snake"));
@@ -60,14 +59,14 @@ public class SachiDaughterOfSeshiro extends CardImpl {
     }
 
     public SachiDaughterOfSeshiro(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.CREATURE},"{2}{G}{G}");
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{2}{G}{G}");
         this.supertype.add("Legendary");
         this.subtype.add("Snake");
         this.subtype.add("Shaman");
 
         this.power = new MageInt(1);
         this.toughness = new MageInt(3);
-        
+
         // Other Snake creatures you control get +0/+1.
         this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new BoostControlledEffect(0, 1, Duration.WhileOnBattlefield, snakeFilter, true)));
         // Shamans you control have "{T}: Add {G}{G} to your mana pool."
