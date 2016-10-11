55af432a45ffb1919eb09fbac8397ea5ab1533ec
LevelX2 <ludwig.hirth@online.de>
2016-10-12 00:50:16 +0200

    Added Tamanoa.

--- Mage.Sets/src/mage/sets/Coldsnap.java
+++ Mage.Sets/src/mage/sets/Coldsnap.java
@@ -165,6 +165,7 @@ public class Coldsnap extends ExpansionSet {
         cards.add(new SetCardInfo("Surging Might", 125, Rarity.COMMON, mage.cards.s.SurgingMight.class));
         cards.add(new SetCardInfo("Surging Sentinels", 20, Rarity.COMMON, mage.cards.s.SurgingSentinels.class));
         cards.add(new SetCardInfo("Swift Maneuver", 21, Rarity.COMMON, mage.cards.s.SwiftManeuver.class));
+	cards.add(new SetCardInfo("Tamanoa", 132, Rarity.RARE, mage.cards.t.Tamanoa.class));
         cards.add(new SetCardInfo("Thermopod", 100, Rarity.COMMON, mage.cards.t.Thermopod.class));
         cards.add(new SetCardInfo("Thrumming Stone", 142, Rarity.RARE, mage.cards.t.ThrummingStone.class));
         cards.add(new SetCardInfo("Tresserhorn Sinks", 150, Rarity.UNCOMMON, mage.cards.t.TresserhornSinks.class));
