b5bb6dd0dbbf1019cc4fd8afb46bb0456d190ee4
Neil Gentleman <ngentleman@gmail.com>
2016-10-15 22:20:21 -0700

    prefer ObjectColor.isRed() to ObjectColor.contains(ObjectColor.RED)

--- Mage.Sets/src/mage/cards/c/CrystalRod.java
+++ Mage.Sets/src/mage/cards/c/CrystalRod.java
@@ -28,7 +28,6 @@
 package mage.cards.c;
 
 import java.util.UUID;
-import mage.ObjectColor;
 import mage.abilities.TriggeredAbilityImpl;
 import mage.abilities.costs.mana.GenericManaCost;
 import mage.abilities.effects.common.DoIfCostPaid;
@@ -89,7 +88,7 @@ class CrystalRodAbility extends TriggeredAbilityImpl {
     @Override
     public boolean checkTrigger(GameEvent event, Game game) {
         Spell spell = game.getStack().getSpell(event.getTargetId());
-        return spell != null && spell.getColor(game).contains(ObjectColor.BLUE);
+        return spell != null && spell.getColor(game).isBlue();
     }
 
     @Override
