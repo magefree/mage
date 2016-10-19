cfb4eea2676a070b052f14d936f08b0423769a0b
LevelX2 <ludwig.hirth@online.de>
2016-10-19 23:35:51 +0200

    [SWS] Fixed wrong p/t of Trade Federation Battleship.

--- Mage.Sets/src/mage/cards/t/TradeFederationBattleship.java
+++ Mage.Sets/src/mage/cards/t/TradeFederationBattleship.java
@@ -62,10 +62,10 @@ public class TradeFederationBattleship extends CardImpl {
     }
 
     public TradeFederationBattleship(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.ARTIFACT,CardType.CREATURE},"{3}{W}{U}{B}");
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT, CardType.CREATURE}, "{3}{W}{U}{B}");
         this.subtype.add("Starship");
-        this.power = new MageInt(2);
-        this.toughness = new MageInt(2);
+        this.power = new MageInt(0);
+        this.toughness = new MageInt(6);
 
         // Spaceflight
         this.addAbility(SpaceflightAbility.getInstance());
