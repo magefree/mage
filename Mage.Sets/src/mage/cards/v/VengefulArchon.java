magefree/mage

commit db4644cbc12db0645be6238de4947acd3643f605

Reworked PreventAllDamageToControllerEffect to be more generic.

Mage.Sets/src/mage/cards/v/VengefulArchon.java
@@ -1,16 +1,16 @@
 /*
  *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
- * 
+ *
  *  Redistribution and use in source and binary forms, with or without modification, are
  *  permitted provided that the following conditions are met:
- * 
+ *
  *     1. Redistributions of source code must retain the above copyright notice, this list of
  *        conditions and the following disclaimer.
- * 
+ *
  *     2. Redistributions in binary form must reproduce the above copyright notice, this list
  *        of conditions and the following disclaimer in the documentation and/or other materials
  *        provided with the distribution.
- * 
+ *
  *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
  *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
@@ -20,26 +20,26 @@
  *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  *  The views and conclusions contained in the software and documentation are those of the
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
  */
-
 package mage.cards.v;
 
 import java.util.UUID;
-import mage.constants.CardType;
-import mage.constants.Duration;
-import mage.constants.Zone;
 import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.mana.ManaCostsImpl;
-import mage.abilities.effects.PreventionEffectImpl;
+import mage.abilities.effects.PreventionEffectData;
+import mage.abilities.effects.common.PreventDamageToControllerEffect;
 import mage.abilities.keyword.FlyingAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Zone;
 import mage.game.Game;
 import mage.game.events.GameEvent;
 import mage.players.Player;
@@ -52,13 +52,16 @@ import mage.target.TargetPlayer;
 public class VengefulArchon extends CardImpl {
 
     public VengefulArchon(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.CREATURE},"{4}{W}{W}{W}");
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{4}{W}{W}{W}");
         this.subtype.add("Archon");
 
         this.power = new MageInt(7);
         this.toughness = new MageInt(7);
 
+        // Flying
         this.addAbility(FlyingAbility.getInstance());
+
+        // {X}: Prevent the next X damage that would be dealt to you this turn. If damage is prevented this way, Vengeful Archon deals that much damage to target player.
         Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new VengefulArchonEffect(), new ManaCostsImpl("{X}"));
         ability.addTarget(new TargetPlayer());
         this.addAbility(ability);
@@ -75,18 +78,15 @@ public class VengefulArchon extends CardImpl {
 
 }
 
-class VengefulArchonEffect extends PreventionEffectImpl {
-
-    protected int amount = 0;
+class VengefulArchonEffect extends PreventDamageToControllerEffect {
 
     public VengefulArchonEffect() {
-        super(Duration.EndOfTurn);
+        super(Duration.EndOfTurn, 0, false, true, new ManacostVariableValue());
         staticText = "Prevent the next X damage that would be dealt to you this turn. If damage is prevented this way, {this} deals that much damage to target player";
     }
 
     public VengefulArchonEffect(final VengefulArchonEffect effect) {
         super(effect);
-        this.amount = effect.amount;
     }
 
     @Override
@@ -95,46 +95,16 @@ class VengefulArchonEffect extends PreventionEffectImpl {
     }
 
     @Override
-    public void init(Ability source, Game game) {
-        super.init(source, game);
-        amount = source.getManaCostsToPay().getX();
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        return true;
-    }
-
-    @Override
-    public boolean replaceEvent(GameEvent event, Ability source, Game game) {
-        GameEvent preventEvent = new GameEvent(GameEvent.EventType.PREVENT_DAMAGE, source.getControllerId(), source.getSourceId(), source.getControllerId(), event.getAmount(), false);
-        if (!game.replaceEvent(preventEvent)) {
+    protected PreventionEffectData preventDamageAction(GameEvent event, Ability source, Game game) {
+        PreventionEffectData preventionEffectData = super.preventDamageAction(event, source, game);
+        int damage = preventionEffectData.getPreventedDamage();
+        if (damage > 0) {
             Player player = game.getPlayer(source.getFirstTarget());
             if (player != null) {
-                int damage = event.getAmount();
-                if (event.getAmount() >= this.amount) {
-                    event.setAmount(damage - this.amount);
-                    damage = this.amount;
-                    this.used = true;
-                } else {
-                    event.setAmount(0);
-                    this.amount -= damage;
-                }
-
                 player.damage(damage, source.getSourceId(), game, false, true);
-                game.fireEvent(GameEvent.getEvent(GameEvent.EventType.PREVENTED_DAMAGE,
-                        source.getControllerId(), source.getSourceId(), source.getControllerId(), damage));
             }
         }
-        return false;
-    }
-
-    @Override
-    public boolean applies(GameEvent event, Ability source, Game game) {
-        if (!this.used && super.applies(event, source, game) && event.getTargetId().equals(source.getControllerId())) {
-            return true;
-        }
-        return false;
+        return preventionEffectData;
     }
 
 }

Mage.Sets/src/mage/cards/c/Conservator.java
@@ -32,28 +32,26 @@ import mage.abilities.Ability;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.common.TapSourceCost;
 import mage.abilities.costs.mana.GenericManaCost;
-import mage.abilities.effects.PreventionEffectImpl;
+import mage.abilities.effects.common.PreventDamageToControllerEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.Duration;
 import mage.constants.Zone;
-import mage.game.Game;
-import mage.game.events.GameEvent;
 import mage.target.TargetPlayer;
 
 /**
  *
  * @author KholdFuzion
-
+ *
  */
 public class Conservator extends CardImpl {
 
     public Conservator(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.ARTIFACT},"{4}");
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT}, "{4}");
 
         // {3}, {tap}: Prevent the next 2 damage that would be dealt to you this turn.
-        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new ConservatorEffect(), new GenericManaCost(3));
+        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new PreventDamageToControllerEffect(Duration.EndOfTurn, 2), new GenericManaCost(3));
         ability.addTarget(new TargetPlayer());
         ability.addCost(new TapSourceCost());
         this.addAbility(ability);
@@ -66,58 +64,5 @@ public class Conservator extends CardImpl {
     @Override
     public Conservator copy() {
         return new Conservator(this);
-    }
-}
-
-class ConservatorEffect extends PreventionEffectImpl {
-
-    private int amount = 2;
-
-    public ConservatorEffect() {
-        super(Duration.EndOfTurn);
-        this.staticText = "Prevent the next 2 damage that would be dealt to you this turn";
-    }
-
-    public ConservatorEffect(final ConservatorEffect effect) {
-        super(effect);
-        this.amount = effect.amount;
-    }
-
-    @Override
-    public ConservatorEffect copy() {
-        return new ConservatorEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        return true;
-    }
-
-    @Override
-    public boolean replaceEvent(GameEvent event, Ability source, Game game) {
-        GameEvent preventEvent = new GameEvent(GameEvent.EventType.PREVENT_DAMAGE,
-                source.getControllerId(), source.getSourceId(), source.getControllerId(), event.getAmount(), false);
-        if (!game.replaceEvent(preventEvent)) {
-            int damage = event.getAmount();
-            if (damage >= this.amount) {
-                event.setAmount(damage - this.amount);
-                damage = this.amount;
-                this.used = true;
-            } else {
-                event.setAmount(0);
-                this.amount -= damage;
-            }
-            game.fireEvent(GameEvent.getEvent(GameEvent.EventType.PREVENTED_DAMAGE,
-                    source.getControllerId(), source.getSourceId(), source.getControllerId(), damage));
-        }
-        return false;
-    }
-
-    @Override
-    public boolean applies(GameEvent event, Ability source, Game game) {
-        if (!this.used && super.applies(event, source, game) && event.getTargetId().equals(source.getControllerId())) {
-            return true;
-        }
-        return false;
     }
 }

Mage.Sets/src/mage/cards/d/DecoratedGriffin.java
@@ -29,18 +29,15 @@ package mage.cards.d;
 
 import java.util.UUID;
 import mage.MageInt;
-import mage.abilities.Ability;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.mana.ManaCostsImpl;
-import mage.abilities.effects.PreventionEffectImpl;
+import mage.abilities.effects.common.PreventDamageToControllerEffect;
 import mage.abilities.keyword.FlyingAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.Duration;
 import mage.constants.Zone;
-import mage.game.Game;
-import mage.game.events.GameEvent;
 
 /**
  *
@@ -49,7 +46,7 @@ import mage.game.events.GameEvent;
 public class DecoratedGriffin extends CardImpl {
 
     public DecoratedGriffin(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.CREATURE},"{4}{W}");
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{4}{W}");
         this.subtype.add("Griffin");
 
         this.power = new MageInt(2);
@@ -57,8 +54,8 @@ public class DecoratedGriffin extends CardImpl {
 
         // Flying
         this.addAbility(FlyingAbility.getInstance());
-        // {1}{W}: Prevent the next 1 damage that would be dealt to you this turn.
-        this.addAbility(new SimpleActivatedAbility(Zone.BATTLEFIELD, new DecoratedGriffinPreventEffect(), new ManaCostsImpl("{1}{W}")));
+        // {1}{W}: Prevent the next 1 combat damage that would be dealt to you this turn.
+        this.addAbility(new SimpleActivatedAbility(Zone.BATTLEFIELD, new PreventDamageToControllerEffect(Duration.EndOfTurn, true, true, 1), new ManaCostsImpl("{1}{W}")));
     }
 
     public DecoratedGriffin(final DecoratedGriffin card) {
@@ -68,35 +65,5 @@ public class DecoratedGriffin extends CardImpl {
     @Override
     public DecoratedGriffin copy() {
         return new DecoratedGriffin(this);
-    }
-}
-
-class DecoratedGriffinPreventEffect extends PreventionEffectImpl {
-
-    public DecoratedGriffinPreventEffect() {
-        super(Duration.EndOfTurn, 1, false, true);
-        this.staticText = "Prevent the next 1 damage that would be dealt to you this turn";
-    }
-
-    public DecoratedGriffinPreventEffect(final DecoratedGriffinPreventEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public DecoratedGriffinPreventEffect copy() {
-        return new DecoratedGriffinPreventEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        return true;
-    }
-
-    @Override
-    public boolean applies(GameEvent event, Ability source, Game game) {
-        if (super.applies(event, source, game) && event.getTargetId().equals(source.getControllerId())) {
-            return true;
-        }
-        return false;
     }
 }

Mage.Sets/src/mage/cards/d/DruidsDeliverance.java
@@ -28,16 +28,12 @@
 package mage.cards.d;
 
 import java.util.UUID;
-import mage.constants.CardType;
-import mage.constants.Duration;
-import mage.abilities.Ability;
-import mage.abilities.effects.PreventionEffectImpl;
 import mage.abilities.effects.common.PopulateEffect;
+import mage.abilities.effects.common.PreventDamageToControllerEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.game.Game;
-import mage.game.events.DamageEvent;
-import mage.game.events.GameEvent;
+import mage.constants.CardType;
+import mage.constants.Duration;
 
 /**
  *
@@ -46,12 +42,11 @@ import mage.game.events.GameEvent;
 public class DruidsDeliverance extends CardImpl {
 
     public DruidsDeliverance(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.INSTANT},"{1}{G}");
-
+        super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{1}{G}");
 
         // Prevent all combat damage that would be dealt to you this turn. Populate.
         // (Create a token that's a copy of a creature token you control.)
-        this.getSpellAbility().addEffect(new DruidsDeliverancePreventCombatDamageEffect());
+        this.getSpellAbility().addEffect(new PreventDamageToControllerEffect(Duration.EndOfTurn, true, false, Integer.MAX_VALUE));
         this.getSpellAbility().addEffect(new PopulateEffect());
     }
 
@@ -63,50 +58,4 @@ public class DruidsDeliverance extends CardImpl {
     public DruidsDeliverance copy() {
         return new DruidsDeliverance(this);
     }
-}
-
-class DruidsDeliverancePreventCombatDamageEffect extends PreventionEffectImpl {
-
-    public DruidsDeliverancePreventCombatDamageEffect() {
-            super(Duration.EndOfTurn);
-            staticText = "Prevent all combat damage that would be dealt to you this turn";
-    }
-
-    public DruidsDeliverancePreventCombatDamageEffect(final DruidsDeliverancePreventCombatDamageEffect effect) {
-            super(effect);
-    }
-
-    @Override
-    public DruidsDeliverancePreventCombatDamageEffect copy() {
-            return new DruidsDeliverancePreventCombatDamageEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-            return true;
-    }
-
-    @Override
-    public boolean replaceEvent(GameEvent event, Ability source, Game game) {
-            GameEvent preventEvent = new GameEvent(GameEvent.EventType.PREVENT_DAMAGE, source.getFirstTarget(), source.getSourceId(), source.getControllerId(), event.getAmount(), false);
-            if (!game.replaceEvent(preventEvent)) {
-                int damage = event.getAmount();
-                event.setAmount(0);
-                game.fireEvent(GameEvent.getEvent(GameEvent.EventType.PREVENTED_DAMAGE, source.getFirstTarget(), source.getSourceId(), source.getControllerId(), damage));
-                return true;
-            }
-            return false;
-        }
-
-    @Override
-    public boolean applies(GameEvent event, Ability source, Game game) {
-            if (super.applies(event, source, game)) {
-                DamageEvent damageEvent = (DamageEvent) event;
-                if (event.getTargetId().equals(source.getControllerId()) && damageEvent.isCombatDamage()) {
-                    return true;
-                }
-            }
-            return false;
-    }
-
 }

Mage.Sets/src/mage/cards/e/EsperBattlemage.java
@@ -28,21 +28,18 @@
 package mage.cards.e;
 
 import java.util.UUID;
+import mage.MageInt;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.costs.mana.ColoredManaCost;
+import mage.abilities.effects.common.PreventDamageToControllerEffect;
+import mage.abilities.effects.common.continuous.BoostTargetEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.ColoredManaSymbol;
 import mage.constants.Duration;
 import mage.constants.Zone;
-import mage.MageInt;
-import mage.abilities.Ability;
-import mage.abilities.common.SimpleActivatedAbility;
-import mage.abilities.costs.common.TapSourceCost;
-import mage.abilities.costs.mana.ColoredManaCost;
-import mage.abilities.effects.PreventionEffectImpl;
-import mage.abilities.effects.common.continuous.BoostTargetEffect;
-import mage.cards.CardImpl;
-import mage.cards.CardSetInfo;
-import mage.game.Game;
-import mage.game.events.GameEvent;
 import mage.target.common.TargetCreaturePermanent;
 
 /**
@@ -52,7 +49,7 @@ import mage.target.common.TargetCreaturePermanent;
 public class EsperBattlemage extends CardImpl {
 
     public EsperBattlemage(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.ARTIFACT,CardType.CREATURE},"{2}{U}");
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT, CardType.CREATURE}, "{2}{U}");
         this.subtype.add("Human");
         this.subtype.add("Wizard");
 
@@ -60,7 +57,7 @@ public class EsperBattlemage extends CardImpl {
         this.toughness = new MageInt(2);
         // {W}, {tap}: Prevent the next 2 damage that would be dealt to you this turn.
         SimpleActivatedAbility ability = new SimpleActivatedAbility(Zone.BATTLEFIELD,
-                new EsperBattlemageEffect(),
+                new PreventDamageToControllerEffect(Duration.EndOfTurn, 2),
                 new ColoredManaCost(ColoredManaSymbol.W));
         ability.addCost(new TapSourceCost());
         this.addAbility(ability);
@@ -80,58 +77,5 @@ public class EsperBattlemage extends CardImpl {
     @Override
     public EsperBattlemage copy() {
         return new EsperBattlemage(this);
-    }
-}
-
-class EsperBattlemageEffect extends PreventionEffectImpl {
-
-    private int amount = 2;
-
-    public EsperBattlemageEffect() {
-        super(Duration.EndOfTurn);
-        this.staticText = "Prevent the next 2 damage that would be dealt to you this turn";
-    }
-
-    public EsperBattlemageEffect(final EsperBattlemageEffect effect) {
-        super(effect);
-        this.amount = effect.amount;
-    }
-
-    @Override
-    public EsperBattlemageEffect copy() {
-        return new EsperBattlemageEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        return true;
-    }
-
-    @Override
-    public boolean replaceEvent(GameEvent event, Ability source, Game game) {
-        GameEvent preventEvent = new GameEvent(GameEvent.EventType.PREVENT_DAMAGE,
-                source.getControllerId(), source.getSourceId(), source.getControllerId(), event.getAmount(), false);
-        if (!game.replaceEvent(preventEvent)) {
-            int damage = event.getAmount();
-            if (damage >= this.amount) {
-                event.setAmount(damage - this.amount);
-                damage = this.amount;
-                this.used = true;
-            } else {
-                event.setAmount(0);
-                this.amount -= damage;
-            }
-            game.fireEvent(GameEvent.getEvent(GameEvent.EventType.PREVENTED_DAMAGE,
-                    source.getControllerId(), source.getSourceId(), source.getControllerId(), damage));
-        }
-        return false;
-    }
-
-    @Override
-    public boolean applies(GameEvent event, Ability source, Game game) {
-        if (!this.used && super.applies(event, source, game) && event.getTargetId().equals(source.getControllerId())) {
-            return true;
-        }
-        return false;
     }
 }

Mage.Sets/src/mage/cards/g/GlacialChasm.java
@@ -31,7 +31,7 @@ import java.util.UUID;
 import mage.abilities.common.EntersBattlefieldTriggeredAbility;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.costs.common.PayLifeCost;
-import mage.abilities.effects.common.PreventAllDamageToControllerEffect;
+import mage.abilities.effects.common.PreventDamageToControllerEffect;
 import mage.abilities.effects.common.SacrificeControllerEffect;
 import mage.abilities.effects.common.combat.CantAttackAnyPlayerAllEffect;
 import mage.abilities.keyword.CumulativeUpkeepAbility;
@@ -63,7 +63,7 @@ public class GlacialChasm extends CardImpl {
         filter.add(new ControllerPredicate(TargetController.YOU));
         this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new CantAttackAnyPlayerAllEffect(Duration.WhileOnBattlefield, filter)));
         // Prevent all damage that would be dealt to you.
-        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new PreventAllDamageToControllerEffect(Duration.WhileOnBattlefield)));
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new PreventDamageToControllerEffect(Duration.WhileOnBattlefield)));
     }
 
     public GlacialChasm(final GlacialChasm card) {

Mage.Sets/src/mage/cards/p/PalliationAccord.java
@@ -28,11 +28,10 @@
 package mage.cards.p;
 
 import java.util.UUID;
-import mage.abilities.Ability;
 import mage.abilities.common.BecomesTappedTriggeredAbility;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.common.RemoveCountersSourceCost;
-import mage.abilities.effects.PreventionEffectImpl;
+import mage.abilities.effects.common.PreventDamageToControllerEffect;
 import mage.abilities.effects.common.counter.AddCountersSourceEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
@@ -43,8 +42,6 @@ import mage.constants.Zone;
 import mage.counters.CounterType;
 import mage.filter.common.FilterCreaturePermanent;
 import mage.filter.predicate.permanent.ControllerPredicate;
-import mage.game.Game;
-import mage.game.events.GameEvent;
 
 /**
  *
@@ -59,13 +56,15 @@ public class PalliationAccord extends CardImpl {
     }
 
     public PalliationAccord(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.ENCHANTMENT},"{3}{W}{U}");
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{3}{W}{U}");
 
         // Whenever a creature an opponent controls becomes tapped, put a shield counter on Palliation Accord.
         this.addAbility(new BecomesTappedTriggeredAbility(new AddCountersSourceEffect(CounterType.SHIELD.createInstance()), false, filter));
 
         // Remove a shield counter from Palliation Accord: Prevent the next 1 damage that would be dealt to you this turn.
-        this.addAbility(new SimpleActivatedAbility(Zone.BATTLEFIELD, new PalliationAccordPreventionEffect(), new RemoveCountersSourceCost(CounterType.SHIELD.createInstance())));
+        this.addAbility(new SimpleActivatedAbility(Zone.BATTLEFIELD,
+                new PreventDamageToControllerEffect(Duration.EndOfTurn, 1),
+                new RemoveCountersSourceCost(CounterType.SHIELD.createInstance())));
     }
 
     public PalliationAccord(final PalliationAccord card) {
@@ -75,51 +74,5 @@ public class PalliationAccord extends CardImpl {
     @Override
     public PalliationAccord copy() {
         return new PalliationAccord(this);
-    }
-}
-
-class PalliationAccordPreventionEffect extends PreventionEffectImpl {
-
-    public PalliationAccordPreventionEffect() {
-        super(Duration.EndOfTurn);
-        this.staticText = "Prevent the next 1 damage that would be dealt to you this turn";
-    }
-
-    public PalliationAccordPreventionEffect(final PalliationAccordPreventionEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public PalliationAccordPreventionEffect copy() {
-        return new PalliationAccordPreventionEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        return true;
-    }
-
-    @Override
-    public boolean replaceEvent(GameEvent event, Ability source, Game game) {
-        GameEvent preventEvent = new GameEvent(GameEvent.EventType.PREVENT_DAMAGE,
-                source.getControllerId(), source.getSourceId(), source.getControllerId(), event.getAmount(), false);
-        if (!game.replaceEvent(preventEvent)) {
-            int damage = event.getAmount();
-            if (damage > 0) {
-                event.setAmount(damage - 1);
-                this.used = true;
-                game.fireEvent(GameEvent.getEvent(GameEvent.EventType.PREVENTED_DAMAGE,
-                        source.getControllerId(), source.getSourceId(), source.getControllerId(), 1));
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public boolean applies(GameEvent event, Ability source, Game game) {
-        if (!this.used && super.applies(event, source, game) && event.getTargetId().equals(source.getControllerId())) {
-            return true;
-        }
-        return false;
     }
 }

Mage.Sets/src/mage/cards/r/ReinforcedBulwark.java
@@ -28,19 +28,16 @@
 package mage.cards.r;
 
 import java.util.UUID;
-import mage.constants.CardType;
-import mage.constants.Duration;
-import mage.constants.Zone;
 import mage.MageInt;
-import mage.abilities.Ability;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.common.TapSourceCost;
-import mage.abilities.effects.PreventionEffectImpl;
+import mage.abilities.effects.common.PreventDamageToControllerEffect;
 import mage.abilities.keyword.DefenderAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.game.Game;
-import mage.game.events.GameEvent;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Zone;
 
 /**
  *
@@ -49,7 +46,7 @@ import mage.game.events.GameEvent;
 public class ReinforcedBulwark extends CardImpl {
 
     public ReinforcedBulwark(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.ARTIFACT,CardType.CREATURE},"{3}");
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT, CardType.CREATURE}, "{3}");
         this.subtype.add("Wall");
         this.power = new MageInt(0);
         this.toughness = new MageInt(4);
@@ -57,7 +54,7 @@ public class ReinforcedBulwark extends CardImpl {
         // Defender
         this.addAbility(DefenderAbility.getInstance());
         // {tap}: Prevent the next 1 damage that would be dealt to you this turn.
-        this.addAbility(new SimpleActivatedAbility(Zone.BATTLEFIELD, new ReinforcedBulwarkEffect(), new TapSourceCost()));
+        this.addAbility(new SimpleActivatedAbility(Zone.BATTLEFIELD, new PreventDamageToControllerEffect(Duration.EndOfTurn, 1), new TapSourceCost()));
     }
 
     public ReinforcedBulwark(final ReinforcedBulwark card) {
@@ -67,51 +64,5 @@ public class ReinforcedBulwark extends CardImpl {
     @Override
     public ReinforcedBulwark copy() {
         return new ReinforcedBulwark(this);
-    }
-}
-
-class ReinforcedBulwarkEffect extends PreventionEffectImpl {
-
-    public ReinforcedBulwarkEffect() {
-        super(Duration.EndOfTurn);
-        this.staticText = "Prevent the next 1 damage that would be dealt to you this turn";
-    }
-
-    public ReinforcedBulwarkEffect(final ReinforcedBulwarkEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public ReinforcedBulwarkEffect copy() {
-        return new ReinforcedBulwarkEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        return true;
-    }
-
-    @Override
-    public boolean replaceEvent(GameEvent event, Ability source, Game game) {
-        GameEvent preventEvent = new GameEvent(GameEvent.EventType.PREVENT_DAMAGE,
-                source.getControllerId(), source.getSourceId(), source.getControllerId(), event.getAmount(), false);
-        if (!game.replaceEvent(preventEvent)) {
-            int damage = event.getAmount();
-            if (damage > 0) {
-                event.setAmount(damage - 1);
-                this.used = true;
-                game.fireEvent(GameEvent.getEvent(GameEvent.EventType.PREVENTED_DAMAGE,
-                        source.getControllerId(), source.getSourceId(), source.getControllerId(), 1));
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public boolean applies(GameEvent event, Ability source, Game game) {
-        if (!this.used && super.applies(event, source, game) && event.getTargetId().equals(source.getControllerId())) {
-            return true;
-        }
-        return false;
     }
 }

commit 126d78f34983c32afcd4bfa012d32d4c82e2420b

fix compilation
broken in db4644cbc12db0645be6238de4947acd3643f605

Mage.Sets/src/mage/cards/v/VengefulArchon.java
@@ -32,6 +32,7 @@ import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.dynamicvalue.common.ManacostVariableValue;
 import mage.abilities.effects.PreventionEffectData;
 import mage.abilities.effects.common.PreventDamageToControllerEffect;
 import mage.abilities.keyword.FlyingAbility;
@@ -81,7 +82,7 @@ public class VengefulArchon extends CardImpl {
 class VengefulArchonEffect extends PreventDamageToControllerEffect {
 
     public VengefulArchonEffect() {
-        super(Duration.EndOfTurn, 0, false, true, new ManacostVariableValue());
+        super(Duration.EndOfTurn, false, true, new ManacostVariableValue());
         staticText = "Prevent the next X damage that would be dealt to you this turn. If damage is prevented this way, {this} deals that much damage to target player";
     }
 