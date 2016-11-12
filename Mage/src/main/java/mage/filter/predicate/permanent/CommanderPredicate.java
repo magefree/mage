67d4ecafbe2443396d72d9c09d0529013f6088b3
LevelX2 <ludwig.hirth@online.de>
2016-11-12 09:05:22 +0100

    Fixed CommanderPredicate (fixed e.g. problem with Bastion Protector).

--- Mage/src/main/java/mage/filter/predicate/permanent/CommanderPredicate.java
+++ Mage/src/main/java/mage/filter/predicate/permanent/CommanderPredicate.java
@@ -22,7 +22,7 @@ public class CommanderPredicate implements Predicate<Permanent> {
         Player owner = game.getPlayer(input.getOwnerId());
         return input.getCardType().contains(CardType.CREATURE)
                 && owner != null
-                && input.getId().equals(owner.getCommandersIds());
+                && owner.getCommandersIds().contains(input.getId());
     }
 
     @Override
