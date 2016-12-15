ce8befffdd180a5e773b5b02170c39f0e0e925ee
spjspj <spjspj4@gmail.com>
2016-12-15 16:51:05 +1100

    spjspj - Evolutionary Escalation fix (C16)

--- Mage.Sets/src/mage/cards/e/EvolutionaryEscalation.java
+++ Mage.Sets/src/mage/cards/e/EvolutionaryEscalation.java
@@ -39,6 +39,7 @@ import mage.constants.TargetController;
 import mage.counters.Counter;
 import mage.counters.CounterType;
 import mage.filter.common.FilterCreaturePermanent;
+import mage.filter.predicate.permanent.ControllerPredicate;
 import mage.game.Game;
 import mage.game.permanent.Permanent;
 import mage.target.Target;
@@ -51,6 +52,10 @@ import mage.target.common.TargetCreaturePermanent;
  */
 public class EvolutionaryEscalation extends CardImpl {
     private static final FilterCreaturePermanent filterOpponentCreature = new FilterCreaturePermanent("creature an opponent controls");
+    
+    static {
+        filterOpponentCreature.add(new ControllerPredicate(TargetController.OPPONENT));
+    }    
 
     public EvolutionaryEscalation(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{1}{G}");
