40d8a32695fb92469c51c7039029b4fd6d6b28f9
LevelX2 <ludwig.hirth@online.de>
2016-12-17 10:35:45 +0100

    XMage 1.4.19V1

--- Mage/src/main/java/mage/cards/repository/CardRepository.java
+++ Mage/src/main/java/mage/cards/repository/CardRepository.java
@@ -37,16 +37,15 @@ import com.j256.ormlite.stmt.Where;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.support.DatabaseConnection;
 import com.j256.ormlite.table.TableUtils;
+import java.io.File;
+import java.sql.SQLException;
+import java.util.*;
+import java.util.concurrent.Callable;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.SetType;
 import mage.util.RandomUtil;
 import org.apache.log4j.Logger;
-
-import java.io.File;
-import java.sql.SQLException;
-import java.util.*;
-import java.util.concurrent.Callable;
 
 /**
  *
@@ -59,7 +58,7 @@ public enum CardRepository {
     private static final String JDBC_URL = "jdbc:h2:file:./db/cards.h2;AUTO_SERVER=TRUE";
     private static final String VERSION_ENTITY_NAME = "card";
     // raise this if db structure was changed
-    private static final long CARD_DB_VERSION = 48;
+    private static final long CARD_DB_VERSION = 49;
     // raise this if new cards were added to the server
     private static final long CARD_CONTENT_VERSION = 65;
     private final TreeSet<String> landTypes = new TreeSet();
