642b14db17fdd7564db64c4a41e1d94c698abdde
Brik Royster <elkemore@gmail.com>
2016-12-27 20:41:20 -0700

    [PLS] Dralnu's Crusade

---
+++ Mage.Sets/src/mage/cards/d/DralnusCrusade.java
@@ -0,0 +1,126 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.cards.d;
+
+import java.util.UUID;
+
+import mage.ObjectColor;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.ContinuousEffectImpl;
+import mage.abilities.effects.common.continuous.BoostAllEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.*;
+import mage.filter.common.FilterCreaturePermanent;
+import mage.filter.predicate.mageobject.CardTypePredicate;
+import mage.filter.predicate.mageobject.SubtypePredicate;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+
+/**
+ *
+ * @author brikr
+ */
+public class DralnusCrusade extends CardImpl {
+
+    private static final FilterCreaturePermanent filter = new FilterCreaturePermanent("Goblin creatures");
+
+    static {
+        filter.add(new SubtypePredicate("Goblin"));
+    }
+
+    public DralnusCrusade(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{1}{B}{R}");
+
+        // Goblin creatures get +1/+1.
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new BoostAllEffect(1, 1, Duration.WhileOnBattlefield, filter, false)));
+
+        // All Goblins are black and are Zombies in addition to their other creature types.
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new DralnusCrusadeEffect()));
+    }
+
+    public DralnusCrusade(final DralnusCrusade card) {
+        super(card);
+    }
+
+    @Override
+    public DralnusCrusade copy() {
+        return new DralnusCrusade(this);
+    }
+}
+
+class DralnusCrusadeEffect extends ContinuousEffectImpl {
+
+    private static final FilterCreaturePermanent filter = new FilterCreaturePermanent("Goblin creatures");
+
+    static {
+        filter.add(new SubtypePredicate("Goblin"));
+    }
+
+    public DralnusCrusadeEffect() {
+        super(Duration.WhileOnBattlefield, Outcome.Neutral);
+        staticText = "All Goblins are black and are Zombies in addition to their other creature types";
+    }
+
+    @Override
+    public boolean apply(Layer layer, SubLayer sublayer, Ability source, Game game) {
+        for(Permanent permanent : game.getState().getBattlefield().getActivePermanents(filter, source.getControllerId(), source.getSourceId(), game)) {
+            switch (layer) {
+                case TypeChangingEffects_4:
+                    if(!permanent.getSubtype(game).contains("Zombie")) {
+                        permanent.getSubtype(game).add("Zombie");
+                    }
+                    break;
+                case ColorChangingEffects_5:
+                    permanent.getColor(game).setColor(ObjectColor.BLACK);
+                    break;
+            }
+        }
+        return true;
+    }
+
+    public DralnusCrusadeEffect(final DralnusCrusadeEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public DralnusCrusadeEffect copy() {
+        return new DralnusCrusadeEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        return false;
+    }
+
+    @Override
+    public boolean hasLayer(Layer layer) {
+        return layer == Layer.TypeChangingEffects_4 || layer == Layer.ColorChangingEffects_5;
+    }
+}
