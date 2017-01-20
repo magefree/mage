c3f087246af2c8fec544cce3150c53f9cc7ad073
LevelX2 <ludwig.hirth@online.de>
2017-01-20 19:16:19 +0100

    * See Beyond - Fixed wrong card moving from hand to library.

--- Mage.Sets/src/mage/cards/s/SeeBeyond.java
+++ Mage.Sets/src/mage/cards/s/SeeBeyond.java
@@ -1,16 +1,16 @@
 /*
  *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
- * 
+ *
  *  Redistribution and use in source and binary forms, with or without modification, are
  *  permitted provided that the following conditions are met:
- * 
+ *
  *     1. Redistributions of source code must retain the above copyright notice, this list of
  *        conditions and the following disclaimer.
- * 
+ *
  *     2. Redistributions in binary form must reproduce the above copyright notice, this list
  *        of conditions and the following disclaimer in the documentation and/or other materials
  *        provided with the distribution.
- * 
+ *
  *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
  *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
@@ -20,23 +20,22 @@
  *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  *  The views and conclusions contained in the software and documentation are those of the
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
  */
-
 package mage.cards.s;
 
 import java.util.UUID;
-import mage.constants.CardType;
-import mage.constants.Outcome;
-import mage.constants.Zone;
 import mage.abilities.Ability;
 import mage.abilities.effects.OneShotEffect;
 import mage.cards.Card;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Outcome;
+import mage.constants.Zone;
 import mage.filter.FilterCard;
 import mage.game.Game;
 import mage.players.Player;
@@ -49,7 +48,7 @@ import mage.target.TargetCard;
 public class SeeBeyond extends CardImpl {
 
     public SeeBeyond(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.SORCERY},"{1}{U}");
+        super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{1}{U}");
 
         this.getSpellAbility().addEffect(new SeeBeyondEffect());
     }
@@ -67,8 +66,6 @@ public class SeeBeyond extends CardImpl {
 
 class SeeBeyondEffect extends OneShotEffect {
 
-    private static FilterCard filter = new FilterCard("card to shuffle into your library");
-
     public SeeBeyondEffect() {
         super(Outcome.DrawCard);
         staticText = "Draw two cards, then shuffle a card from your hand into your library";
@@ -80,18 +77,18 @@ class SeeBeyondEffect extends OneShotEffect {
 
     @Override
     public boolean apply(Game game, Ability source) {
-        Player player = game.getPlayer(source.getControllerId());
-        player.drawCards(2, game);
-        if (player.getHand().size() > 0) {
-            TargetCard target = new TargetCard(Zone.HAND, filter);
-            player.choose(Outcome.Detriment, player.getHand(), target, game);
-            Card card = player.getHand().get(target.getFirstTarget(), game);
+        Player controller = game.getPlayer(source.getControllerId());
+        controller.drawCards(2, game);
+        if (controller.getHand().size() > 0) {
+            TargetCard target = new TargetCard(Zone.HAND, new FilterCard("card to shuffle into your library"));
+            controller.choose(Outcome.Detriment, controller.getHand(), target, game);
+            Card card = controller.getHand().get(target.getFirstTarget(), game);
             if (card != null) {
-                player.removeFromHand(card, game);
-                card.moveToZone(Zone.LIBRARY, source.getSourceId(), game, true);
-                player.shuffleLibrary(source, game);
-                return true;
+                controller.moveCards(card, Zone.LIBRARY, source, game);
+                controller.shuffleLibrary(source, game);
             }
+            return true;
+
         }
         return true;
     }
