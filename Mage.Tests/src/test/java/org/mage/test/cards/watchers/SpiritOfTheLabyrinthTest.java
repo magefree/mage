682940eca696a4c21b1f2be25a6d4f86b4888ff0
LevelX2 <ludwig.hirth@online.de>
2017-01-22 23:00:25 +0100

    Added test.

--- Mage.Tests/src/test/java/org/mage/test/cards/watchers/SpiritOfTheLabyrinthTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/watchers/SpiritOfTheLabyrinthTest.java
@@ -10,28 +10,50 @@ import org.mage.test.serverside.base.CardTestPlayerBase;
  * @author BetaSteward
  */
 public class SpiritOfTheLabyrinthTest extends CardTestPlayerBase {
+
     /*
      * Spirit of the Labyrinth
      * Enchantment Creature — Spirit 3/1, 1W (2)
      * Each player can't draw more than one card each turn.
      *
-    */
-    
+     */
+
     // test that only 1 card is drawn
     @Test
     public void testDrawCard() {
         addCard(Zone.BATTLEFIELD, playerA, "Island", 5);
         addCard(Zone.BATTLEFIELD, playerA, "Spirit of the Labyrinth");
         addCard(Zone.HAND, playerA, "Brilliant Plan");
-        
+
         castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Brilliant Plan");
-        
+
         setStopAt(1, PhaseStep.BEGIN_COMBAT);
         execute();
-                
-        this.assertHandCount(playerA, 1);
-        
-    }
-    
 
+        this.assertHandCount(playerA, 1);
+
+    }
+
+    // test that only 1 card is drawn
+    @Test
+    public void testDrawCardHondenOfSeeingWinds() {
+        addCard(Zone.BATTLEFIELD, playerA, "Island", 5);
+        addCard(Zone.BATTLEFIELD, playerA, "Spirit of the Labyrinth");
+        addCard(Zone.HAND, playerA, "Brilliant Plan");
+
+        // At the beginning of your upkeep, draw a card for each Shrine you control.
+        addCard(Zone.BATTLEFIELD, playerB, "Honden of Seeing Winds");
+        // At the beginning of your upkeep, put a 1/1 colorless Spirit creature token onto the battlefield for each Shrine you control.
+        addCard(Zone.BATTLEFIELD, playerB, "Honden of Life's Web");
+
+        castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Brilliant Plan");
+
+        setStopAt(2, PhaseStep.PRECOMBAT_MAIN);
+        execute();
+
+        assertPermanentCount(playerB, "Spirit", 2);
+        this.assertHandCount(playerA, 1);
+        this.assertHandCount(playerB, 1);
+
+    }
 }
