ae8e121e90a72851f717fabd322d2ea76b8d3971
goblin <goblin@example.com>
2017-01-27 00:09:46 +0100

    fix up whitespace
    XMage people seem to be using spaces...

--- Mage.Tests/src/test/java/org/mage/test/cards/abilities/oneshot/damage/FlameheartWerewolfTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/abilities/oneshot/damage/FlameheartWerewolfTest.java
@@ -12,18 +12,18 @@ import org.mage.test.serverside.base.CardTestPlayerBase;
 public class FlameheartWerewolfTest extends CardTestPlayerBase {
 
     /**
-	 * https://github.com/magefree/mage/issues/2816
+     * https://github.com/magefree/mage/issues/2816
      */
     @Test
     public void testBlockingKalitas() {
-		// this card is the second face of double-faced card
+        // this card is the second face of double-faced card
 
-		// Flameheart Werewolf is a 3/2 with:
-		// Whenever Flameheart Werewolf blocks or becomes blocked by a creature, Flameheart Werewolf deals 2 damage to that creature.
+        // Flameheart Werewolf is a 3/2 with:
+        // Whenever Flameheart Werewolf blocks or becomes blocked by a creature, Flameheart Werewolf deals 2 damage to that creature.
 
-		// Kalitas, Traitor of Ghet is a 3/4 with:
-		// Lifelink
-		// If a nontoken creature an opponent controls would die, instead exile that card and put a 2/2 black Zombie creature token onto the battlefield
+        // Kalitas, Traitor of Ghet is a 3/4 with:
+        // Lifelink
+        // If a nontoken creature an opponent controls would die, instead exile that card and put a 2/2 black Zombie creature token onto the battlefield
 
         addCard(Zone.BATTLEFIELD, playerA, "Flameheart Werewolf");
         addCard(Zone.BATTLEFIELD, playerB, "Kalitas, Traitor of Ghet");
@@ -37,17 +37,17 @@ public class FlameheartWerewolfTest extends CardTestPlayerBase {
         assertLife(playerA, 20);
         assertLife(playerB, 23); // lifelinked
 
-		// both should die
+        // both should die
         assertPermanentCount(playerA, "Flameheart Werewolf", 0);
         assertExileCount("Flameheart Werewolf", 1); // exiled by Kalitas
         assertPermanentCount(playerB, "Kalitas, Traitor of Ghet", 0);
         assertGraveyardCount(playerB, "Kalitas, Traitor of Ghet", 1);
     }
     
-	@Test
+    @Test
     public void testBlockedByTwo22s() {
         addCard(Zone.BATTLEFIELD, playerA, "Flameheart Werewolf");
-		// Both 2/2 creatures should die before the combat starts
+        // Both 2/2 creatures should die before the combat starts
         addCard(Zone.BATTLEFIELD, playerB, "Falkenrath Reaver");
         addCard(Zone.BATTLEFIELD, playerB, "Wind Drake");
 
@@ -58,12 +58,12 @@ public class FlameheartWerewolfTest extends CardTestPlayerBase {
         setStopAt(3, PhaseStep.POSTCOMBAT_MAIN);
         execute();
 
-		// Flameheart Werewolf was blocked, no trample
+        // Flameheart Werewolf was blocked, no trample
         assertLife(playerA, 20);
         assertLife(playerB, 20);
 
-		// both 2/2s should die before they had a chance to deal damage
-		// to Flameheart Werewolf
+        // both 2/2s should die before they had a chance to deal damage
+        // to Flameheart Werewolf
         assertPermanentCount(playerA, "Flameheart Werewolf", 1);
         assertGraveyardCount(playerA, "Flameheart Werewolf", 0);
         assertPermanentCount(playerB, "Falkenrath Reaver", 0);
