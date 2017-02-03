6beb23dbb34f498b0a25e795306d6433f1b7bdff
LevelX2 <ludwig.hirth@online.de>
2017-02-03 21:58:10 +0100

    * Overwhelming Stampede - Fixed that given Trample ability did not only last until end of turn.

--- Mage.Sets/src/mage/cards/o/OverwhelmingStampede.java
+++ Mage.Sets/src/mage/cards/o/OverwhelmingStampede.java
@@ -1,16 +1,16 @@
 /*
  *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
- * 
+ *
  *  Redistribution and use in source and binary forms, with or without modification, are
  *  permitted provided that the following conditions are met:
- * 
+ *
  *     1. Redistributions of source code must retain the above copyright notice, this list of
  *        conditions and the following disclaimer.
- * 
+ *
  *     2. Redistributions in binary form must reproduce the above copyright notice, this list
  *        of conditions and the following disclaimer in the documentation and/or other materials
  *        provided with the distribution.
- * 
+ *
  *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
  *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
@@ -20,12 +20,11 @@
  *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  *  The views and conclusions contained in the software and documentation are those of the
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
  */
-
 package mage.cards.o;
 
 import java.util.UUID;
@@ -51,8 +50,8 @@ import mage.game.permanent.Permanent;
 public class OverwhelmingStampede extends CardImpl {
 
     public OverwhelmingStampede(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.SORCERY},"{3}{G}{G}");
-        
+        super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{3}{G}{G}");
+
         // Until end of turn, creatures you control gain trample and get +X/+X, where X is the greatest power among creatures you control.
         this.getSpellAbility().addEffect(new OverwhelmingStampedeInitEffect());
     }
@@ -68,30 +67,30 @@ public class OverwhelmingStampede extends CardImpl {
 }
 
 class OverwhelmingStampedeInitEffect extends OneShotEffect {
-    
+
     public OverwhelmingStampedeInitEffect() {
         super(Outcome.BoostCreature);
         this.staticText = "Until end of turn, creatures you control gain trample and get +X/+X, where X is the greatest power among creatures you control";
     }
-    
+
     public OverwhelmingStampedeInitEffect(final OverwhelmingStampedeInitEffect effect) {
         super(effect);
     }
-    
+
     @Override
     public OverwhelmingStampedeInitEffect copy() {
         return new OverwhelmingStampedeInitEffect(this);
     }
-    
+
     @Override
     public boolean apply(Game game, Ability source) {
         int maxPower = 0;
-        for (Permanent perm: game.getBattlefield().getAllActivePermanents(new FilterCreaturePermanent(), source.getControllerId(), game)) {
+        for (Permanent perm : game.getBattlefield().getAllActivePermanents(new FilterCreaturePermanent(), source.getControllerId(), game)) {
             if (perm.getPower().getValue() > maxPower) {
                 maxPower = perm.getPower().getValue();
             }
         }
-        ContinuousEffect effect = new GainAbilityControlledEffect(TrampleAbility.getInstance(), Duration.EndOfStep, new FilterCreaturePermanent());
+        ContinuousEffect effect = new GainAbilityControlledEffect(TrampleAbility.getInstance(), Duration.EndOfTurn, new FilterCreaturePermanent());
         game.addEffect(effect, source);
         if (maxPower != 0) {
             effect = new BoostControlledEffect(maxPower, maxPower, Duration.EndOfTurn);
