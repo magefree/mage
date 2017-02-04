0dacf2a6fa625693ffd2cfd67bcbb93ce18f7521
LevelX2 <ludwig.hirth@online.de>
2017-02-04 14:26:17 +0100

    * Fixed that the cascade converted mana costs comparison did not work correctly with split spells.

--- Mage.Sets/src/mage/cards/a/ArdentPlea.java
+++ Mage.Sets/src/mage/cards/a/ArdentPlea.java
@@ -25,15 +25,14 @@
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
  */
-
 package mage.cards.a;
 
 import java.util.UUID;
-import mage.constants.CardType;
 import mage.abilities.keyword.CascadeAbility;
 import mage.abilities.keyword.ExaltedAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
+import mage.constants.CardType;
 
 /**
  *
@@ -41,17 +40,17 @@ import mage.cards.CardSetInfo;
  */
 public class ArdentPlea extends CardImpl {
 
-    public ArdentPlea (UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.ENCHANTMENT},"{1}{W}{U}");
+    public ArdentPlea(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{1}{W}{U}");
 
-
-
-
+        // Exalted (Whenever a creature you control attacks alone, that creature gets +1/+1 until end of turn.)
         this.addAbility(new ExaltedAbility());
+
+        // Cascade (When you cast this spell, exile cards from the top of your library until you exile a nonland card that costs less. You may cast it without paying its mana cost. Put the exiled cards on the bottom in a random order.)
         this.addAbility(new CascadeAbility());
     }
 
-    public ArdentPlea (final ArdentPlea card) {
+    public ArdentPlea(final ArdentPlea card) {
         super(card);
     }
 
