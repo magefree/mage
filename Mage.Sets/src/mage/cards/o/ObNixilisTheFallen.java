899046cf16784ed2215b9a9b22f53595e66f5abc
LevelX2 <ludwig.hirth@online.de>
2017-02-18 01:26:02 +0100

    * Gratuitous Violence - FIxed that if the damage of a creature with lifelink was doubeled only the non doubled amount was added to life.

--- Mage.Sets/src/mage/cards/o/ObNixilisTheFallen.java
+++ Mage.Sets/src/mage/cards/o/ObNixilisTheFallen.java
@@ -25,19 +25,17 @@
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
  */
-
 package mage.cards.o;
 
 import java.util.UUID;
-
-import mage.constants.CardType;
 import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.LandfallAbility;
-import mage.abilities.effects.common.counter.AddCountersSourceEffect;
 import mage.abilities.effects.common.LoseLifeTargetEffect;
+import mage.abilities.effects.common.counter.AddCountersSourceEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
+import mage.constants.CardType;
 import mage.counters.CounterType;
 import mage.target.TargetPlayer;
 
@@ -47,21 +45,23 @@ import mage.target.TargetPlayer;
  */
 public class ObNixilisTheFallen extends CardImpl {
 
-    public ObNixilisTheFallen (UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.CREATURE},"{3}{B}{B}");
+    public ObNixilisTheFallen(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{3}{B}{B}");
         this.supertype.add("Legendary");
         this.subtype.add("Demon");
 
         this.power = new MageInt(3);
         this.toughness = new MageInt(3);
 
+        // Landfall - Whenever a land enters the battlefield under your control, you may have target player lose 3 life.
+        // If you do, put three +1/+1 counters on Ob Nixilis, the Fallen.
         Ability ability = new LandfallAbility(new LoseLifeTargetEffect(3), true);
         ability.addEffect(new AddCountersSourceEffect(CounterType.P1P1.createInstance(3)));
         ability.addTarget(new TargetPlayer());
         this.addAbility(ability);
     }
 
-    public ObNixilisTheFallen (final ObNixilisTheFallen card) {
+    public ObNixilisTheFallen(final ObNixilisTheFallen card) {
         super(card);
     }
 
