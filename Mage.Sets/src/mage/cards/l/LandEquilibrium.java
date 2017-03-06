2e4d5e2bba94c744af261dbfaf285167b2cdbc46
spjspj <spjspj4@gmail.com>
2017-03-07 00:57:04 +1100

    spjspj - Implement Land Equilibrium (LEG, ME3)

---
+++ Mage.Sets/src/mage/cards/l/LandEquilibrium.java
@@ -0,0 +1,102 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.cards.l;
+
+import java.util.UUID;
+import static jdk.nashorn.internal.objects.NativeRegExp.source;
+import mage.abilities.TriggeredAbilityImpl;
+import mage.abilities.effects.common.SacrificeEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import static mage.cards.m.ManaVortex.filter;
+import mage.constants.CardType;
+import mage.constants.Zone;
+import mage.filter.common.FilterLandPermanent;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.events.GameEvent.EventType;
+import mage.target.targetpointer.FixedTarget;
+
+/**
+ *
+ * @author spjspj
+ */
+public class LandEquilibrium extends CardImpl {
+
+    public LandEquilibrium(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{2}{U}{U}");
+        
+        // If an opponent who controls at least as many lands as you do would put a land onto the battlefield, that player instead puts that land onto the battlefield then sacrifices a land.
+        this.addAbility(new LandEquilibriumAbility());
+    }
+
+    public LandEquilibrium(final LandEquilibrium card) {
+        super(card);
+    }
+
+    @Override
+    public LandEquilibrium copy() {
+        return new LandEquilibrium(this);
+    }
+}
+
+class LandEquilibriumAbility extends TriggeredAbilityImpl {
+
+    public LandEquilibriumAbility() {
+        super(Zone.BATTLEFIELD, new SacrificeEffect(new FilterLandPermanent(), 1, ""), false);
+    }
+
+    public LandEquilibriumAbility(final LandEquilibriumAbility ability) {
+        super(ability);
+    }
+
+    @Override
+    public LandEquilibriumAbility copy() {
+        return new LandEquilibriumAbility(this);
+    }
+
+    @Override
+    public boolean checkEventType(GameEvent event, Game game) {
+        return event.getType() == EventType.LAND_PLAYED;
+    }
+
+    @Override
+    public boolean checkTrigger(GameEvent event, Game game) {
+        int numMyLands = game.getBattlefield().countAll(filter, this.getControllerId(), game);
+        if (numMyLands < game.getBattlefield().countAll(filter, event.getPlayerId(), game)) {
+            this.getEffects().get(0).setTargetPointer(new FixedTarget(event.getPlayerId()));
+            return true;
+        }
+        return false;        
+    }
+
+    @Override
+    public String getRule() {
+        return "If an opponent who controls at least as many lands as you do would put a land onto the battlefield, that player instead puts that land onto the battlefield then sacrifices a land";
+    }
+}
