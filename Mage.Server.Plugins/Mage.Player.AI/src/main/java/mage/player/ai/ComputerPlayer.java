2ecb415b4db19b9918604e0525d52ef343d9533c
Nathaniel Brandes <ndbrandes@gmail.com>
2017-03-08 21:41:57 -0800

    Removed potential integer overflow.
    If the number passed into max is Integer.MAX_VALUE, like it is with
    ChoiceOfDamnation, the random number will overflow. So let's not do
    that.

--- Mage.Server.Plugins/Mage.Player.AI/src/main/java/mage/player/ai/ComputerPlayer.java
+++ Mage.Server.Plugins/Mage.Player.AI/src/main/java/mage/player/ai/ComputerPlayer.java
@@ -1543,7 +1543,7 @@ public class ComputerPlayer extends PlayerImpl implements Player {
         }
         //TODO: improve this
         if (min < max && min == 0) {
-            return RandomUtil.nextInt(max + 1);
+            return RandomUtil.nextInt(max);
         }
         return min;
     }
