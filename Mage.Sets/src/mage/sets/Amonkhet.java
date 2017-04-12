ea98f96a9209a00ef2be66bb02782b34bebdc68f
spjspj <spjspj4@gmail.com>
2017-04-12 14:16:04 +1000

    spjspj - Implement Harsh Mentor (AKH)

--- Mage.Sets/src/mage/sets/Amonkhet.java
+++ Mage.Sets/src/mage/sets/Amonkhet.java
@@ -126,6 +126,7 @@ public class Amonkhet extends ExpansionSet {
         cards.add(new SetCardInfo("Gravedigger", 93, Rarity.UNCOMMON, mage.cards.g.Gravedigger.class));
         cards.add(new SetCardInfo("Grim Strider", 94, Rarity.UNCOMMON, mage.cards.g.GrimStrider.class));
         cards.add(new SetCardInfo("Hapatra, Vizier of Poisons", 199, Rarity.RARE, mage.cards.h.HapatraVizierOfPoisons.class));
+        cards.add(new SetCardInfo("Harsh Mentor", 135, Rarity.RARE, mage.cards.h.HarshMentor.class));
         cards.add(new SetCardInfo("Hazoret the Fervent", 136, Rarity.MYTHIC, mage.cards.h.HazoretTheFervent.class));
         cards.add(new SetCardInfo("Hazoret's Favor", 137, Rarity.RARE, mage.cards.h.HazoretsFavor.class));
         cards.add(new SetCardInfo("Hazoret's Monument", 229, Rarity.UNCOMMON, mage.cards.h.HazoretsMonument.class));
