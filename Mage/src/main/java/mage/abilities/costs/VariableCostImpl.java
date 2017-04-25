7e9d4ab515bbdcc67871fc23e834d4b661747d21
LevelX2 <ludwig.hirth@online.de>
2017-04-25 23:27:24 +0200

    * DreadshipReef - Fixed that the 3rd ability did not create mana (fixes #3223).

--- Mage/src/main/java/mage/abilities/costs/VariableCostImpl.java
+++ Mage/src/main/java/mage/abilities/costs/VariableCostImpl.java
@@ -30,6 +30,7 @@ package mage.abilities.costs;
 import java.util.UUID;
 import mage.abilities.Ability;
 import mage.abilities.keyword.FlashbackAbility;
+import mage.abilities.mana.ManaAbility;
 import mage.game.Game;
 import mage.game.stack.StackObject;
 import mage.players.Player;
@@ -171,7 +172,9 @@ public abstract class VariableCostImpl implements Cost, VariableCost {
         Player controller = game.getPlayer(source.getControllerId());
         StackObject stackObject = game.getStack().getStackObject(source.getId());
         if (controller != null
-                && (source instanceof FlashbackAbility || stackObject != null)) {
+                && (source instanceof ManaAbility
+                || source instanceof FlashbackAbility
+                || stackObject != null)) {
             xValue = controller.announceXCost(getMinValue(source, game), getMaxValue(source, game),
                     "Announce the number of " + actionText, game, source, this);
         }
