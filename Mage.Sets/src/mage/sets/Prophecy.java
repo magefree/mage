a1dba938a48055cf44a4ed63c49817620435bd21
bugfi5h <rene.kling1992@gmail.com>
2017-05-01 18:24:10 +0200

    Added Card: Entangler

--- Mage.Sets/src/mage/sets/Prophecy.java
+++ Mage.Sets/src/mage/sets/Prophecy.java
@@ -79,6 +79,7 @@ public class Prophecy extends ExpansionSet {
         cards.add(new SetCardInfo("Despoil", 62, Rarity.COMMON, mage.cards.d.Despoil.class));
         cards.add(new SetCardInfo("Devastate", 87, Rarity.COMMON, mage.cards.d.Devastate.class));
         cards.add(new SetCardInfo("Diving Griffin", 6, Rarity.COMMON, mage.cards.d.DivingGriffin.class));
+        cards.add(new SetCardInfo("Entangler", 7, Rarity.UNCOMMON, mage.cards.e.Entangler.class));
         cards.add(new SetCardInfo("Excavation", 33, Rarity.UNCOMMON, mage.cards.e.Excavation.class));
         cards.add(new SetCardInfo("Fault Riders", 88, Rarity.COMMON, mage.cards.f.FaultRiders.class));
         cards.add(new SetCardInfo("Fen Stalker", 64, Rarity.COMMON, mage.cards.f.FenStalker.class));
