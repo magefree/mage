7ee2c23addf107f4170fb6843f89bd1cb9b818e1
LevelX2 <ludwig.hirth@online.de>
2017-05-20 09:55:54 +0200

    * Crescendo of War - Fixed that all blocking creatures instead of only controlled creatures got the power boost (fixes #3399).

--- Mage.Sets/src/mage/cards/c/CrescendoOfWar.java
+++ Mage.Sets/src/mage/cards/c/CrescendoOfWar.java
@@ -27,11 +27,13 @@
  */
 package mage.cards.c;
 
+import java.util.UUID;
 import mage.abilities.common.BeginningOfUpkeepTriggeredAbility;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.dynamicvalue.common.CountersSourceCount;
 import mage.abilities.dynamicvalue.common.StaticValue;
 import mage.abilities.effects.common.continuous.BoostAllEffect;
+import mage.abilities.effects.common.continuous.BoostControlledEffect;
 import mage.abilities.effects.common.counter.AddCountersSourceEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
@@ -43,8 +45,6 @@ import mage.counters.CounterType;
 import mage.filter.common.FilterAttackingCreature;
 import mage.filter.common.FilterBlockingCreature;
 
-import java.util.UUID;
-
 /**
  *
  * @author fireshoes
@@ -52,18 +52,18 @@ import java.util.UUID;
 public class CrescendoOfWar extends CardImpl {
 
     public CrescendoOfWar(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.ENCHANTMENT},"{3}{W}");
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{3}{W}");
 
         // At the beginning of each upkeep, put a strife counter on Crescendo of War.
         this.addAbility(new BeginningOfUpkeepTriggeredAbility(new AddCountersSourceEffect(CounterType.STRIFE.createInstance(1), true), TargetController.ANY, false));
-        
+
         // Attacking creatures get +1/+0 for each strife counter on Crescendo of War.
         this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new BoostAllEffect(new CountersSourceCount(CounterType.STRIFE), new StaticValue(0),
-            Duration.WhileOnBattlefield, new FilterAttackingCreature(), false)));
-        
+                Duration.WhileOnBattlefield, new FilterAttackingCreature(), false)));
+
         // Blocking creatures you control get +1/+0 for each strife counter on Crescendo of War.
-        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new BoostAllEffect(new CountersSourceCount(CounterType.STRIFE), new StaticValue(0),
-            Duration.WhileOnBattlefield, new FilterBlockingCreature(), false)));
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new BoostControlledEffect(new CountersSourceCount(CounterType.STRIFE), new StaticValue(0),
+                Duration.WhileOnBattlefield, new FilterBlockingCreature(), false)));
     }
 
     public CrescendoOfWar(final CrescendoOfWar card) {
