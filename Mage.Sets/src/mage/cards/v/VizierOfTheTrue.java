b80aab0cfde51e295bbcbab4b3ac13e8134110f0
spjspj <spjspj4@gmail.com>
2017-06-25 20:06:51 +1000

    Implement Vizier of the True (HOU)

---
+++ Mage.Sets/src/mage/cards/v/VizierOfTheTrue.java
@@ -0,0 +1,123 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.cards.v;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.abilities.TriggeredAbilityImpl;
+import mage.abilities.effects.common.TapTargetEffect;
+import mage.abilities.keyword.ExertAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.TargetController;
+import mage.constants.Zone;
+import mage.filter.common.FilterCreaturePermanent;
+import mage.filter.predicate.permanent.ControllerPredicate;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.events.GameEvent.EventType;
+import mage.players.Player;
+import mage.target.Target;
+import mage.target.common.TargetCreaturePermanent;
+
+/**
+ *
+ * @author spjspj
+ */
+public class VizierOfTheTrue extends CardImpl {
+
+    public VizierOfTheTrue(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{3}{W}");
+
+        this.subtype.add("Human");
+        this.subtype.add("Cleric");
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(2);
+
+        // You may exert Vizier of the True as it attacks.
+        this.addAbility(new ExertAbility(null, false));
+
+        // Whenever you exert a creature, tap target creature an opponent controls.
+        this.addAbility(new VizierOfTheTrueAbility());
+    }
+
+    public VizierOfTheTrue(final VizierOfTheTrue card) {
+        super(card);
+    }
+
+    @Override
+    public VizierOfTheTrue copy() {
+        return new VizierOfTheTrue(this);
+    }
+}
+
+class VizierOfTheTrueAbility extends TriggeredAbilityImpl {
+
+    private static final FilterCreaturePermanent filter = new FilterCreaturePermanent("creature an opponent controls");
+
+    static {
+        filter.add(new ControllerPredicate(TargetController.OPPONENT));
+    }
+
+    public VizierOfTheTrueAbility() {
+        super(Zone.BATTLEFIELD, new TapTargetEffect());
+    }
+
+    public VizierOfTheTrueAbility(final VizierOfTheTrueAbility ability) {
+        super(ability);
+    }
+
+    @Override
+    public VizierOfTheTrueAbility copy() {
+        return new VizierOfTheTrueAbility(this);
+    }
+
+    @Override
+    public boolean checkEventType(GameEvent event, Game game) {
+        return event.getType() == EventType.BECOMES_EXERTED;
+    }
+
+    @Override
+    public boolean checkTrigger(GameEvent event, Game game) {
+        Player player = game.getPlayer(event.getPlayerId());
+        if (player != null) {
+            Target target = new TargetCreaturePermanent(filter);
+            target.setAbilityController(getControllerId());
+            this.getTargets().clear();
+            this.getTargets().add(target);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String getRule() {
+        return "Whenever you exert a creature, tap target creature an opponent controls.";
+    }
+}
