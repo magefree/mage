9fb04dc33cae5d3fee8e17d023b4782ab2370cfe
igoudt <igoudt@bol.com>
2017-07-04 21:54:24 +0200

    manticore elemental

--- Mage.Sets/src/mage/cards/m/ManticoreElemental.java
+++ Mage.Sets/src/mage/cards/m/ManticoreElemental.java
@@ -1,4 +1,35 @@
 package mage.cards.m;
 
-public class ManticoreElemental {
+import mage.MageInt;
+import mage.abilities.common.AttacksEachCombatStaticAbility;
+import mage.abilities.keyword.AfflictAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+
+import java.util.UUID;
+
+public class ManticoreElemental extends CardImpl {
+
+    public ManticoreElemental(UUID ownerId, CardSetInfo cardSetInfo) {
+        super(ownerId, cardSetInfo, new CardType[]{CardType.CREATURE}, "{3}{R}{R}");
+        subtype.add("Zombie");
+        subtype.add("Manticore");
+        power = new MageInt(5);
+        toughness = new MageInt(4);
+
+        // Afflict 3
+        addAbility(new AfflictAbility(3));
+
+        // Manticore Elemental attacks each combat if able
+        addAbility(new AttacksEachCombatStaticAbility());
+    }
+
+    public ManticoreElemental(final ManticoreElemental manticoreElemental) {
+        super(manticoreElemental);
+    }
+
+    public ManticoreElemental copy() {
+        return new ManticoreElemental(this);
+    }
 }
