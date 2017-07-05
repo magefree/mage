d60572ae052db6e459e92c690f022b7852ebd7e0
Lazar <lazar.gosic@gmail.com>
2017-07-05 22:16:04 +0200

    moved effect to own class

--- Mage.Sets/src/mage/cards/e/EndlessSands.java
+++ Mage.Sets/src/mage/cards/e/EndlessSands.java
@@ -1,23 +1,18 @@
 package mage.cards.e;
 
+import mage.abilities.effects.common.ReturnCreaturesFromExileEffect;
 import java.util.UUID;
 import mage.abilities.Ability;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.common.SacrificeSourceCost;
 import mage.abilities.costs.common.TapSourceCost;
 import mage.abilities.costs.mana.ManaCostsImpl;
-import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.ExileTargetEffect;
 import mage.abilities.mana.ColorlessManaAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.Outcome;
 import mage.constants.Zone;
-import mage.filter.common.FilterCreatureCard;
-import mage.game.ExileZone;
-import mage.game.Game;
-import mage.players.Player;
 import mage.target.common.TargetControlledCreaturePermanent;
 
 public class EndlessSands extends CardImpl {
@@ -37,7 +32,8 @@ public class EndlessSands extends CardImpl {
         this.addAbility(exileAbility);
         
         // {4}, {T}, Sacrifice Endless Sands: Return each creature card exiled with Endless Sands to the battlefield under its owner’s control.
-        Ability returnAbility = new SimpleActivatedAbility(Zone.BATTLEFIELD, new ReturnFromExileToOwnersControlEffect(this.getId()),  new ManaCostsImpl("{4}"));
+        ReturnCreaturesFromExileEffect returnFromExileEffect = new ReturnCreaturesFromExileEffect(this.getId(), true, "Return each creature card exiled with {this} to the battlefield under its owner’s control.");
+        Ability returnAbility = new SimpleActivatedAbility(Zone.BATTLEFIELD, returnFromExileEffect,  new ManaCostsImpl("{4}"));
         returnAbility.addCost(new TapSourceCost());
         returnAbility.addCost(new SacrificeSourceCost());
         this.addAbility(returnAbility);
@@ -50,37 +46,5 @@ public class EndlessSands extends CardImpl {
     @Override
     public EndlessSands copy() {
         return new EndlessSands(this);
-    }
-}
-
-class ReturnFromExileToOwnersControlEffect extends OneShotEffect {
-
-    private UUID exileId;
-
-    public ReturnFromExileToOwnersControlEffect(UUID exileId) {
-        super(Outcome.PutCardInPlay);
-        this.exileId = exileId;
-        this.setText("Return each creature card exiled with {this} to the battlefield under its owner’s control.");
-    }
-
-    public ReturnFromExileToOwnersControlEffect(final ReturnFromExileToOwnersControlEffect effect) {
-        super(effect);
-        this.exileId = effect.exileId;
-    }
-
-    @Override
-    public ReturnFromExileToOwnersControlEffect copy() {
-        return new ReturnFromExileToOwnersControlEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        ExileZone exile = game.getExile().getExileZone(exileId);
-        Player controller = game.getPlayer(source.getControllerId());
-        if (controller != null && exile != null) {
-            controller.moveCards(exile.getCards(new FilterCreatureCard(), game), Zone.BATTLEFIELD, source, game, false, false, true, null);
-            return true;
-        }
-        return false;
     }
 }
