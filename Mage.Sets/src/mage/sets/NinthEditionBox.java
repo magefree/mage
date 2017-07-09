98b2fe1063210bdc4c00c66ba05eda34c37b4b0e
LevelX2 <ludwig.hirth@online.de>
2017-07-09 10:50:29 +0200

    * Ninth Edition Box - Fixed error in card spelling of Coral Eel (was Corel Eel).

--- Mage.Sets/src/mage/sets/NinthEditionBox.java
+++ Mage.Sets/src/mage/sets/NinthEditionBox.java
@@ -1,6 +1,5 @@
 package mage.sets;
 
-import mage.cards.CardGraphicInfo;
 import mage.cards.ExpansionSet;
 import mage.constants.Rarity;
 import mage.constants.SetType;
@@ -8,16 +7,15 @@ import mage.constants.SetType;
 public class NinthEditionBox extends ExpansionSet {
 
     private static final NinthEditionBox instance = new NinthEditionBox();
-    
+
     public static NinthEditionBox getInstance() {
         return instance;
-    }    
-        
-    
+    }
+
     private NinthEditionBox() {
-        super ("Ninth Edition Box", "9EB", ExpansionSet.buildDate(2005, 7, 29), SetType.CORE);
+        super("Ninth Edition Box", "9EB", ExpansionSet.buildDate(2005, 7, 29), SetType.CORE);
         this.hasBoosters = false;
-        cards.add(new SetCardInfo("Corel Eel", 3, Rarity.COMMON, mage.cards.c.CoralEel.class));
+        cards.add(new SetCardInfo("Coral Eel", 3, Rarity.COMMON, mage.cards.c.CoralEel.class));
         cards.add(new SetCardInfo("Eager Cadet", 1, Rarity.COMMON, mage.cards.e.EagerCadet.class));
         cards.add(new SetCardInfo("Enormous Baloth", 9, Rarity.UNCOMMON, mage.cards.e.EnormousBaloth.class));
         cards.add(new SetCardInfo("Giant Octopus", 4, Rarity.COMMON, mage.cards.g.GiantOctopus.class));
