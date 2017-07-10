ba5c2161462f35036d11cdd18f624ed42a88baa8
Jeff <jeff@delmarus.com>
2017-07-10 11:48:11 -0500

    - Fixed issue #3646

--- Mage.Sets/src/mage/cards/e/ElvishHarbinger.java
+++ Mage.Sets/src/mage/cards/e/ElvishHarbinger.java
@@ -35,12 +35,12 @@ import mage.abilities.mana.AnyColorManaAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.SubType;
 import mage.filter.FilterCard;
 import mage.filter.predicate.mageobject.SubtypePredicate;
 import mage.target.common.TargetCardInLibrary;
 
 import java.util.UUID;
+import mage.constants.SubType;
 
 /**
  *
@@ -50,7 +50,7 @@ public class ElvishHarbinger extends CardImpl {
 
     static final FilterCard filter = new FilterCard("Elf card");
     static {
-        filter.add(new SubtypePredicate(SubType.ELEMENTAL));
+        filter.add(new SubtypePredicate(SubType.ELF));
     }
     public ElvishHarbinger(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId,setInfo,new CardType[]{CardType.CREATURE},"{2}{G}");
