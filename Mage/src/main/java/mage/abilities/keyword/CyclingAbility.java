11e3c9f729bc39f94b8fd64b213745a92d0ebef2
Achilles <jeff@delmarus.com>
2017-07-15 09:20:00 -0500

    - Fixed bug #3677

--- Mage/src/main/java/mage/abilities/keyword/CyclingAbility.java
+++ Mage/src/main/java/mage/abilities/keyword/CyclingAbility.java
@@ -27,20 +27,14 @@
  */
 package mage.abilities.keyword;
 
-import java.util.UUID;
-import mage.abilities.Ability;
 import mage.abilities.ActivatedAbilityImpl;
 import mage.abilities.costs.Cost;
-import mage.abilities.costs.CostImpl;
+import mage.abilities.costs.common.CyclingDiscardCost;
 import mage.abilities.costs.mana.ManaCost;
 import mage.abilities.effects.common.DrawCardSourceControllerEffect;
 import mage.abilities.effects.common.search.SearchLibraryPutInHandEffect;
-import mage.cards.Card;
 import mage.constants.Zone;
 import mage.filter.FilterCard;
-import mage.game.Game;
-import mage.game.events.GameEvent;
-import mage.players.Player;
 import mage.target.common.TargetCardInLibrary;
 
 /**
@@ -89,45 +83,4 @@ public class CyclingAbility extends ActivatedAbilityImpl {
         return rule.toString();
     }
 
-}
-
-class CyclingDiscardCost extends CostImpl {
-
-    public CyclingDiscardCost() {
-    }
-
-    public CyclingDiscardCost(CyclingDiscardCost cost) {
-        super(cost);
-    }
-
-    @Override
-    public boolean canPay(Ability ability, UUID sourceId, UUID controllerId, Game game) {
-        return game.getPlayer(controllerId).getHand().contains(sourceId);
-    }
-
-    @Override
-    public boolean pay(Ability ability, Game game, UUID sourceId, UUID controllerId, boolean noMana, Cost costToPay) {
-        Player player = game.getPlayer(controllerId);
-        if (player != null) {
-            Card card = player.getHand().get(sourceId, game);
-            if (card != null) {
-                game.fireEvent(GameEvent.getEvent(GameEvent.EventType.CYCLE_CARD, card.getId(), card.getId(), card.getOwnerId()));
-                paid = player.discard(card, null, game);
-                if (paid) {
-                    game.fireEvent(GameEvent.getEvent(GameEvent.EventType.CYCLED_CARD, card.getId(), card.getId(), card.getOwnerId()));
-                }
-            }
-        }
-        return paid;
-    }
-
-    @Override
-    public String getText() {
-        return "Discard this card";
-    }
-
-    @Override
-    public CyclingDiscardCost copy() {
-        return new CyclingDiscardCost(this);
-    }
 }
