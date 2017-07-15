d10f547879b2e51fdab44b2e208c112f26ac9ff2
Achilles <jeff@delmarus.com>
2017-07-15 15:38:06 -0500

    - Fixed bug #3678

--- Mage.Sets/src/mage/cards/m/MogisGodOfSlaughter.java
+++ Mage.Sets/src/mage/cards/m/MogisGodOfSlaughter.java
@@ -73,7 +73,7 @@ public class MogisGodOfSlaughter extends CardImpl {
         this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, effect));
 
         // At the beginning of each opponent's upkeep, Mogis deals 2 damage to that player unless he or she sacrifices a creature.
-        effect = new DoUnlessTargetPaysCost(new DamageTargetEffect(2, false, "that player"), new SacrificeTargetCost(new TargetControlledCreaturePermanent()),
+        effect = new DoUnlessTargetPaysCost(new DamageTargetEffect(2, true, "that player"), new SacrificeTargetCost(new TargetControlledCreaturePermanent()),
                 "Sacrifice a creature? (otherwise you get 2 damage)");
         effect.setText("Mogis deals 2 damage to that player unless he or she sacrifices a creature");
         Ability ability = new BeginningOfUpkeepTriggeredAbility(Zone.BATTLEFIELD, effect, TargetController.OPPONENT, false, true);
