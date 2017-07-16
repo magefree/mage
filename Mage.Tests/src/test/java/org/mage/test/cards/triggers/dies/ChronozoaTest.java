8c2388644cd21f354a11a48ccff8193f29340758
LevelX2 <ludwig.hirth@online.de>
2017-07-16 14:58:39 +0200

    * Added a test.

--- Mage.Tests/src/test/java/org/mage/test/cards/triggers/dies/ChronozoaTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/triggers/dies/ChronozoaTest.java
@@ -1,3 +1,10 @@
+package org.mage.test.cards.triggers.dies;
+
+import mage.constants.PhaseStep;
+import mage.constants.Zone;
+import org.junit.Test;
+import org.mage.test.serverside.base.CardTestPlayerBase;
+
 /*
  *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
  *
@@ -25,43 +32,42 @@
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
  */
-package mage.cards.v;
-
-import java.util.UUID;
-import mage.MageInt;
-import mage.abilities.common.SimpleActivatedAbility;
-import mage.abilities.costs.common.SacrificeTargetCost;
-import mage.abilities.effects.keyword.ScryEffect;
-import mage.cards.CardImpl;
-import mage.cards.CardSetInfo;
-import mage.constants.CardType;
-import mage.constants.Zone;
-import mage.target.common.TargetControlledCreaturePermanent;
-
 /**
  *
- * @author BetaSteward_at_googlemail.com
+ * @author LevelX2
  */
-public class VisceraSeer extends CardImpl {
+public class ChronozoaTest extends CardTestPlayerBase {
 
-    public VisceraSeer(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{B}");
-        this.subtype.add("Vampire");
-        this.subtype.add("Wizard");
-
-        this.power = new MageInt(1);
-        this.toughness = new MageInt(1);
+    /**
+     * Chronozoa's duplicating ability is triggering whenever any card is put
+     * into a graveyard from play, not just Chronozoa itself. Here's an excerpt
+     * from the log: As you can see, I sacrificed Viscera Seer and for some
+     * reason that triggered Chronozoa's ability.
+     */
+    @Test
+    public void testTriggerOtherCreature() {
+        addCard(Zone.BATTLEFIELD, playerA, "Island", 4);
+        // Flying
+        // Vanishing 3 (This permanent enters the battlefield with three time counters on it. At the beginning of your upkeep, remove a time counter from it. When the last is removed, sacrifice it.)
+        // When Chronozoa is put into a graveyard from play, if it had no time counters on it, create two tokens that are copies of it.
+        addCard(Zone.HAND, playerA, "Chronozoa"); // {3}{U}
+        addCard(Zone.GRAVEYARD, playerA, "Chronozoa");
         // Sacrifice a creature: Scry 1. (To scry 1, look at the top card of your library, then you may put that card on the bottom of your library.)
-        this.addAbility(new SimpleActivatedAbility(Zone.BATTLEFIELD, new ScryEffect(1), new SacrificeTargetCost(new TargetControlledCreaturePermanent())));
-    }
+        addCard(Zone.BATTLEFIELD, playerA, "Viscera Seer", 1);
 
-    public VisceraSeer(final VisceraSeer card) {
-        super(card);
-    }
+        castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Chronozoa");
+        activateAbility(1, PhaseStep.POSTCOMBAT_MAIN, playerA, "Sacrifice a creature");
+        addTarget(playerA, "Viscera Seer");
 
-    @Override
-    public VisceraSeer copy() {
-        return new VisceraSeer(this);
+        setStopAt(1, PhaseStep.END_TURN);
+        execute();
+
+        assertGraveyardCount(playerA, "Viscera Seer", 1);
+        assertGraveyardCount(playerA, "Chronozoa", 1);
+        assertPermanentCount(playerA, "Chronozoa", 1);
+        assertPermanentCount(playerA, 5);
+
+        assertHandCount(playerA, 0);
     }
 
 }
