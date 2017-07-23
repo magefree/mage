2edb0632161b01d5ef5b9b4d715c65347e4a1d82
Zzooouhh <Zzooouhh@users.noreply.github.com>
2017-07-23 23:49:31 +0200

    Update DaruSpiritualist.java

--- Mage.Sets/src/mage/cards/d/DaruSpiritualist.java
+++ Mage.Sets/src/mage/cards/d/DaruSpiritualist.java
@@ -28,6 +28,7 @@
  
 package mage.cards.d;
 
+import java.util.UUID;
 import mage.MageInt;
 import mage.abilities.TriggeredAbilityImpl;
 import mage.abilities.effects.Effect;
@@ -45,8 +46,6 @@ import mage.game.events.GameEvent;
 import mage.game.events.GameEvent.EventType;
 import mage.game.permanent.Permanent;
 import mage.target.targetpointer.FixedTarget;
- 
-import java.util.UUID;
 
 
 /**
