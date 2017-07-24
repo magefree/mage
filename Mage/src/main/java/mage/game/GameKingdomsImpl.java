f9173354da6e29eafddec4febb9548d21285972c
Andrew Bueide <abueide@protonmail.com>
2017-07-24 12:35:25 -0600

    Broken Build Save Point

--- Mage/src/main/java/mage/game/GameKingdomsImpl.java
+++ Mage/src/main/java/mage/game/GameKingdomsImpl.java
@@ -16,16 +16,16 @@ import java.util.UUID;
 public abstract class GameKingdomsImpl extends GameCommanderImpl {
 
     protected static final KingdomRole[] roles = {
-            new KingdomRole("King", "You are the king, Mighty ruler of the land! Your objective is to reclaim " +
+            new KingdomRole("King", "You are the king, mighty ruler of the land! Your objective is to reclaim " +
                     "control over your empire at any cost and eliminate any pesky barbarians or spies. Many will " +
                     "claim to be your knight in shining armor, but alas, there is no way of telling who is the true knight." +
                     "The king wins when either the king or the king and the knight are the only players left standing."),
             new KingdomRole("Knight", "You are the knight. Your mission in life is to protect your Majesty! " +
                     "Unfortunately, it appears that they are filled with imperialist rage and can’t recognize who you are! " +
                     "The knight wins when the king and the knight are the last players that remain."),
-            new KingdomRole("Barbarian", "You are 1 of 2 barbarian who lives on the outermost fringes of the kingdom. " +
+            new KingdomRole("Barbarian", "You are 1 of 2 barbarian leaders who live on the outermost fringes of the kingdom. " +
                     "Mustering up a crew of irritated guardsmen in the countryside you set out to challenge the king and his throne. " +
-                    "The barbarians wins when the king dies."),
+                    "The barbarians win when the king dies."),
             new KingdomRole("Assassin", "Your goal is to kill the king, but you know that if you kill the king, " +
                     "the gruesome barbarians will push you aside and take the throne for themselves. The assassin wins " +
                     "when the everyone but the king dies, and then the king dies. The king has to die last in order for the assassin to win.")
@@ -50,11 +50,30 @@ public abstract class GameKingdomsImpl extends GameCommanderImpl {
     }
 
     protected void initializeRoles() {
+        for (UUID player : randomizedPlayerList){
+            //Check if player is the king, if so increase their life total by 1/4.
+            if(getRole(player) == roles[0]){
+                state.getPlayer(player).gainLife(10);
+            }
+        }
+    }
 
+    protected KingdomRole getRole(UUID player){
+        int index = 0;
+        for(UUID id : randomizedPlayerList){
+            if(id == player) return roles[index];
+            index++;
+        }
+        return new KingdomRole("None", "None");
     }
 
     protected KingdomRole getRole(Player player){
-
+        int index = 0;
+        for(UUID id : randomizedPlayerList){
+            if(id == player.getId()) return roles[index];
+            index++;
+        }
+        return new KingdomRole("None", "None");
     }
 
 //    @Override
