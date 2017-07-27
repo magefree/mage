b55316f29e9b9b78384966768f1d206b33e18a0e
Jeff <jeff@delmarus.com>
2017-07-27 16:07:32 -0500

    - Added a requested card Hivis of the Scale.

---
+++ Mage.Sets/src/mage/cards/h/HivisOfTheScale.java
@@ -0,0 +1,100 @@
+/*
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
+ */
+package mage.cards.h;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.common.SkipUntapOptionalAbility;
+import mage.abilities.condition.Condition;
+import mage.abilities.condition.common.SourceMatchesFilterCondition;
+import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.decorator.ConditionalContinuousEffect;
+import mage.abilities.effects.Effect;
+import mage.abilities.effects.common.continuous.GainControlTargetEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.SubType;
+import mage.constants.SuperType;
+import mage.constants.TargetController;
+import mage.constants.Zone;
+import mage.filter.FilterPermanent;
+import mage.filter.predicate.mageobject.SubtypePredicate;
+import mage.filter.predicate.permanent.ControllerPredicate;
+import mage.filter.predicate.permanent.TappedPredicate;
+import mage.target.TargetPermanent;
+
+/**
+ *
+ * @author jeffwadsworth
+ */
+public class HivisOfTheScale extends CardImpl {
+    
+    private static final FilterPermanent filter = new FilterPermanent();
+    private static final FilterPermanent filterDragon = new FilterPermanent();
+    private static final String rule = "Gain control of target Dragon for as long as you control Hivis and Hivis remains tapped.";
+    
+    static {
+        filter.add(new TappedPredicate());
+        filter.add(new ControllerPredicate(TargetController.YOU));
+        filterDragon.add(new SubtypePredicate(SubType.DRAGON));
+    }
+
+    public HivisOfTheScale(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{3}{R}{R}");
+        
+        addSuperType(SuperType.LEGENDARY);
+        this.subtype.add("Viashino");
+        this.subtype.add("Shaman");
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(4);
+
+        // You may choose not to untap Hivis of the Scale during your untap step.
+        this.addAbility(new SkipUntapOptionalAbility());
+        
+        // {tap}: Gain control of target Dragon for as long as you control Hivis and Hivis remains tapped.
+        Condition condition = new SourceMatchesFilterCondition(filter);
+        Effect effect = new ConditionalContinuousEffect(new GainControlTargetEffect(Duration.Custom), condition, rule);
+        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, effect, new TapSourceCost());
+        ability.addTarget(new TargetPermanent(filterDragon));
+        this.addAbility(ability);
+        
+    }
+
+    public HivisOfTheScale(final HivisOfTheScale card) {
+        super(card);
+    }
+
+    @Override
+    public HivisOfTheScale copy() {
+        return new HivisOfTheScale(this);
+    }
+}
