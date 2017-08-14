41eff642baadecc24d4a4bca1ac66d17ea906b85
spjspj <spjspj4@gmail.com>
2017-08-15 01:04:25 +1000

    Implement Fractured Identity (C17)

--- Mage.Sets/src/mage/sets/Commander2017.java
+++ Mage.Sets/src/mage/sets/Commander2017.java
@@ -49,6 +49,7 @@ public class Commander2017 extends ExpansionSet {
 
         cards.add(new SetCardInfo("Balan, Wandering Knight", 2, Rarity.RARE, mage.cards.b.BalanWanderingKnight.class));
         cards.add(new SetCardInfo("Crimson Honor Guard", 23, Rarity.RARE, mage.cards.c.CrimsonHonorGuard.class));
+        cards.add(new SetCardInfo("Fractured Identity", 37, Rarity.RARE, mage.cards.f.FracturedIdentity.class));
         cards.add(new SetCardInfo("Nazahn, Revered Bladesmith", 44, Rarity.MYTHIC, mage.cards.n.NazahnReveredBladesmith.class));
         cards.add(new SetCardInfo("O-Kagachi, Vengeful Kami", 45, Rarity.MYTHIC, mage.cards.o.OKagachiVengefulKami.class));
         cards.add(new SetCardInfo("Patron of the Vein", 20, Rarity.RARE, mage.cards.p.PatronOfTheVein.class));
