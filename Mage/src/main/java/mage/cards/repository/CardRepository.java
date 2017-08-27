f35eee935cd222dfbfeaf0ad09d380ef5103dccc
LevelX2 <ludwig.hirth@online.de>
2017-08-27 08:04:18 +0200

    * XMage 1.4.26V2

--- Mage/src/main/java/mage/cards/repository/CardRepository.java
+++ Mage/src/main/java/mage/cards/repository/CardRepository.java
@@ -58,8 +58,7 @@ public enum CardRepository {
     // raise this if db structure was changed
     private static final long CARD_DB_VERSION = 51;
     // raise this if new cards were added to the server
-    private static final long CARD_CONTENT_VERSION = 87;
-    private final TreeSet<String> landTypes = new TreeSet<>();
+    private static final long CARD_CONTENT_VERSION = 89;
     private Dao<CardInfo, Object> cardDao;
     private Set<String> classNames;
 
