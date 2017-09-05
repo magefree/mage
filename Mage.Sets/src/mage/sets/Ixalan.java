601e7cba9b4fbf534e44cf3a0871c8f79ca28940
Evan Kranzler <theelk801@gmail.com>
2017-09-05 18:59:49 -0400

    Implemented Wanted Scoundrels

--- Mage.Sets/src/mage/sets/Ixalan.java
+++ Mage.Sets/src/mage/sets/Ixalan.java
@@ -112,6 +112,7 @@ public class Ixalan extends ExpansionSet {
         cards.add(new SetCardInfo("Vraska's Contempt", 129, Rarity.RARE, mage.cards.v.VraskasContempt.class));
         cards.add(new SetCardInfo("Waker of the Wilds", 215, Rarity.RARE, mage.cards.w.WakerOfTheWilds.class));
         cards.add(new SetCardInfo("Walk the Plank", 130, Rarity.UNCOMMON, mage.cards.w.WalkThePlank.class));
+        cards.add(new SetCardInfo("Wanted Scoundrels", 131, Rarity.UNCOMMON, mage.cards.w.WantedScoundrels.class));
         cards.add(new SetCardInfo("Woodland Stream", 284, Rarity.COMMON, mage.cards.w.WoodlandStream.class));
     }
 }
