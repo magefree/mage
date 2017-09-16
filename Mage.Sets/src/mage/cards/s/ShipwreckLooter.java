0f8a799115d494dab9e3447a21c68f671ec14978
Evan Kranzler <theelk801@gmail.com>
2017-09-15 20:52:13 -0400

    Implemented Shipwreck Looter

--- Mage.Sets/src/mage/cards/s/ShipwreckLooter.java
+++ Mage.Sets/src/mage/cards/s/ShipwreckLooter.java
@@ -25,18 +25,19 @@
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
  */
-package mage.cards.n;
+package mage.cards.s;
 
 import java.util.UUID;
+import mage.MageInt;
 import mage.abilities.Ability;
-import mage.abilities.common.BeginningOfEndStepTriggeredAbility;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
 import mage.abilities.condition.common.RaidCondition;
 import mage.abilities.decorator.ConditionalTriggeredAbility;
-import mage.abilities.effects.common.PutLibraryIntoGraveTargetEffect;
+import mage.abilities.effects.common.DrawDiscardControllerEffect;
+import mage.constants.SubType;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.TargetController;
 import mage.target.common.TargetOpponent;
 import mage.watchers.common.PlayerAttackedWatcher;
 
@@ -44,26 +45,31 @@ import mage.watchers.common.PlayerAttackedWatcher;
  *
  * @author TheElk801
  */
-public class NavigatorsRuin extends CardImpl {
+public class ShipwreckLooter extends CardImpl {
 
-    public NavigatorsRuin(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{2}{U}");
+    public ShipwreckLooter(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{1}{U}");
 
-        // Raid - At the beginning of your end step, if you attacked with a creature this turm, target opponent puts the top four cards of his or her library into his or her graveyard.
+        this.subtype.add(SubType.HUMAN);
+        this.subtype.add(SubType.PIRATE);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(1);
+
+        // Raid - When Shipwreck Looter enters the battlefield,if you attacked with a creature this turn, you may draw a card. If you do, discard a card.
         Ability ability = new ConditionalTriggeredAbility(
-                new BeginningOfEndStepTriggeredAbility(new PutLibraryIntoGraveTargetEffect(4), TargetController.YOU, false),
+                new EntersBattlefieldTriggeredAbility(new DrawDiscardControllerEffect(1, 1, true)),
                 RaidCondition.instance,
-                "<i>Raid</i> - At the beginning of your end step, if you attacked with a creature this turn, target opponent puts the top four cards of his or her library into his or her graveyard.");
+                "<i>Raid</i> - When {this} enters the battlefield,if you attacked with a creature this turn, you may draw a card. If you do, discard a card.");
         ability.addTarget(new TargetOpponent());
         this.addAbility(ability, new PlayerAttackedWatcher());
     }
 
-    public NavigatorsRuin(final NavigatorsRuin card) {
+    public ShipwreckLooter(final ShipwreckLooter card) {
         super(card);
     }
 
     @Override
-    public NavigatorsRuin copy() {
-        return new NavigatorsRuin(this);
+    public ShipwreckLooter copy() {
+        return new ShipwreckLooter(this);
     }
 }
