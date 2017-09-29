c6b9f8ded60c8be3ad724e89951dd94e2de87f3e
Evan Kranzler <theelk801@gmail.com>
2017-09-29 13:33:00 -0400

    Implemented Purraj of Urborg

--- Mage.Sets/src/mage/cards/p/PurrajOfUrborg.java
+++ Mage.Sets/src/mage/cards/p/PurrajOfUrborg.java
@@ -25,54 +25,77 @@
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
  */
-package mage.cards.a;
+package mage.cards.p;
 
 import java.util.UUID;
 import mage.MageInt;
 import mage.ObjectColor;
+import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.common.SpellCastAllTriggeredAbility;
+import mage.abilities.condition.common.SourceAttackingCondition;
 import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.decorator.ConditionalContinuousEffect;
 import mage.abilities.effects.common.DoIfCostPaid;
-import mage.abilities.effects.common.GainLifeEffect;
+import mage.abilities.effects.common.continuous.GainAbilitySourceEffect;
+import mage.abilities.effects.common.counter.AddCountersSourceEffect;
+import mage.abilities.keyword.FirstStrikeAbility;
+import mage.constants.SubType;
+import mage.constants.SuperType;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.SubType;
+import mage.constants.Zone;
+import mage.counters.CounterType;
 import mage.filter.FilterSpell;
 import mage.filter.predicate.mageobject.ColorPredicate;
 
 /**
  *
- * @author jeffwadsworth
- *
+ * @author TheElk801
  */
-public class ApothecaryInitiate extends CardImpl {
+public class PurrajOfUrborg extends CardImpl {
 
-    private static final FilterSpell filter = new FilterSpell("a white spell");
+    private static final FilterSpell filter = new FilterSpell("a black spell");
 
     static {
-        filter.add(new ColorPredicate(ObjectColor.WHITE));
+        filter.add(new ColorPredicate(ObjectColor.BLACK));
     }
 
-    public ApothecaryInitiate(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{W}");
-        this.subtype.add(SubType.KITHKIN);
-        this.subtype.add(SubType.CLERIC);
+    public PurrajOfUrborg(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{3}{B}{B}");
 
-        this.power = new MageInt(1);
-        this.toughness = new MageInt(1);
+        this.addSuperType(SuperType.LEGENDARY);
+        this.subtype.add(SubType.CAT);
+        this.subtype.add(SubType.WARRIOR);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(3);
 
-        // Whenever a player casts a white spell, you may pay {1}. If you do, you gain 1 life.
-        this.addAbility(new SpellCastAllTriggeredAbility(new DoIfCostPaid(new GainLifeEffect(1), new ManaCostsImpl("{1}")), filter, false));
+        // Purraj of Urborg has first strike as long as it's attacking.
+        this.addAbility(new SimpleStaticAbility(
+                Zone.BATTLEFIELD,
+                new ConditionalContinuousEffect(
+                        new GainAbilitySourceEffect(FirstStrikeAbility.getInstance()),
+                        SourceAttackingCondition.instance,
+                        "{this} has first strike as long as it's attacking"
+                )
+        ));
 
+        // Whenever a player casts a black spell, you may pay {B}. If you do, put a +1/+1 counter on Purraj of Urborg.
+        this.addAbility(new SpellCastAllTriggeredAbility(
+                new DoIfCostPaid(
+                        new AddCountersSourceEffect(CounterType.P1P1.createInstance()),
+                        new ManaCostsImpl("{B}")
+                ),
+                filter, false
+        ));
     }
 
-    public ApothecaryInitiate(final ApothecaryInitiate card) {
+    public PurrajOfUrborg(final PurrajOfUrborg card) {
         super(card);
     }
 
     @Override
-    public ApothecaryInitiate copy() {
-        return new ApothecaryInitiate(this);
+    public PurrajOfUrborg copy() {
+        return new PurrajOfUrborg(this);
     }
 }
