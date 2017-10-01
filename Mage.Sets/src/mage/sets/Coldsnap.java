5b9127da4ecfa4fd5938b2080886c2b2e308e654
Zzooouhh <Zzooouhh@users.noreply.github.com>
2017-10-01 22:27:45 +0200

    Error fix

--- Mage.Sets/src/mage/sets/Coldsnap.java
+++ Mage.Sets/src/mage/sets/Coldsnap.java
@@ -7,7 +7,6 @@
  *     1. Redistributions of source code must retain the above copyright notice, this list of
  *        conditions and the following disclaimer.
  *
-        cards.add(new SetCardInfo("Krovikan Whispers", 39, Rarity.UNCOMMON, mage.cards.k.KrovikanWhispers.class));
  *     2. Redistributions in binary form must reproduce the above copyright notice, this list
  *        of conditions and the following disclaimer in the documentation and/or other materials
  *        provided with the distribution.
