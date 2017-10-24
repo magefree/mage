90f2a017abb8f4d1364e537184bb1fca61af9a85
Zzooouhh <Zzooouhh@users.noreply.github.com>
2017-10-24 06:19:45 +0200

    Update MuragandaPetroglyphs.java

--- Mage.Sets/src/mage/cards/m/MuragandaPetroglyphs.java
+++ Mage.Sets/src/mage/cards/m/MuragandaPetroglyphs.java
@@ -35,6 +35,7 @@ import mage.abilities.Ability;
 import mage.abilities.SpellAbility;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.effects.common.continuous.BoostAllEffect;
+import mage.abilities.keyword.JohanVigilanceAbility;
 import mage.cards.Card;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
@@ -92,7 +93,9 @@ class NoAbilityPredicate implements Predicate<MageObject> {
         if (isFaceDown) {
             for (Ability ability : abilities) {
                 if (!ability.getSourceId().equals(input.getId())) {
-                    return false;
+                    if (!(ability instanceof JohanVigilanceAbility)) {
+                        return false;
+                    }
                 }
             }
             return true;
@@ -100,8 +103,9 @@ class NoAbilityPredicate implements Predicate<MageObject> {
 
         for (Ability ability : abilities) {
             if (!Objects.equals(ability.getClass(), SpellAbility.class)) {
-
-                return false;
+                if (!(ability instanceof JohanVigilanceAbility)) {
+                    return false;
+                }
             }
         }
         return true;
