a19e8790215df9eab7969f3c396923b1565c10e7
Oleg Agafonov <jaydi85@gmail.com>
2017-11-18 05:40:24 +0400

    "Can attack" draw fix

--- Mage.Client/src/main/java/org/mage/card/arcane/CardPanelComponentImpl.java
+++ Mage.Client/src/main/java/org/mage/card/arcane/CardPanelComponentImpl.java
@@ -432,7 +432,7 @@ public class CardPanelComponentImpl extends CardPanel {
         // draw attack border (inner part of border, not selection -- must always show attack)
         if (key.canAttack) {
             g2d.setColor(new Color(0, 0, 255, 230));
-            g2d.fillRoundRect(sizes.rectBorder.x + 1, sizes.rectBorder.y + 1, sizes.rectBorder.width - 2, sizes.rectBorder.height - 2, cornerSizeBorder, cornerSizeBorder);
+            g2d.fillRoundRect(sizes.rectBorder.x - 1, sizes.rectBorder.y - 1, sizes.rectBorder.width + 2, sizes.rectBorder.height + 2, cornerSizeBorder, cornerSizeBorder);
         }
 
         // draw real card by component (see imagePanel and other layout's items)
