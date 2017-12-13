2103bfb74a569dc1bd9cc6458581d2710484de71
Alexsandro <alexsandro2134@gmail.com>
2017-12-13 00:28:00 -0200

    Fix merge old stuff

--- Mage.Sets/src/mage/cards/a/AccursedWitch.java
+++ Mage.Sets/src/mage/cards/a/AccursedWitch.java
@@ -41,11 +41,7 @@ import mage.cards.i.InfectiousCurse;
 import mage.cards.Card;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.constants.CardType;
-import mage.constants.CostModificationType;
-import mage.constants.Duration;
-import mage.constants.Outcome;
-import mage.constants.Zone;
+import mage.constants.*;
 import mage.game.Game;
 import mage.game.permanent.Permanent;
 import mage.players.Player;
@@ -60,8 +56,8 @@ public class AccursedWitch extends CardImpl {
 
     public AccursedWitch(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId,setInfo,new CardType[]{CardType.CREATURE},"{3}{B}");
-        this.subtype.add("Human");
-        this.subtype.add("Shaman");
+        this.subtype.add(SubType.SHAMAN);
+        this.subtype.add(SubType.HUMAN);
         this.power = new MageInt(4);
         this.toughness = new MageInt(2);
 
