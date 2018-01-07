cd437ce8b8b5a416c5cc1342ec7d9aee3cd960c1
Oleg Agafonov <jaydi85@gmail.com>
2018-01-08 01:51:49 +0400

    * Raging Regisaur - Fixed wrong stats and text (see #4358)

--- Mage.Sets/src/mage/cards/r/RagingRegisaur.java
+++ Mage.Sets/src/mage/cards/r/RagingRegisaur.java
@@ -47,11 +47,11 @@ public class RagingRegisaur extends CardImpl {
     public RagingRegisaur(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{2}{R}{G}");
         this.subtype.add(SubType.DINOSAUR);
-        this.power = new MageInt(2);
-        this.toughness = new MageInt(2);
+        this.power = new MageInt(4);
+        this.toughness = new MageInt(4);
 
         // Whenever Raging Regisaur attacks, it deals 1 damage to target creature or player.
-        Ability ability = new AttacksTriggeredAbility(new DamageTargetEffect(1), false, "it deals 1 damage to target creature or player");
+        Ability ability = new AttacksTriggeredAbility(new DamageTargetEffect(1, "it"), false);
         ability.addTarget(new TargetCreatureOrPlayer());
         this.addAbility(ability);
     }
