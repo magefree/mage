6db1426e488b17d68b1bd07204872c859bf9c39e
Zzooouhh <Zzooouhh@users.noreply.github.com>
2018-01-12 16:47:12 +0000

    Text fix

--- Mage.Sets/src/mage/cards/w/WarpArtifact.java
+++ Mage.Sets/src/mage/cards/w/WarpArtifact.java
@@ -62,7 +62,7 @@ public class WarpArtifact extends CardImpl {
         Effect effect = new DamageTargetEffect(1);
         effect.setText("{this} deals 1 damage to that player");
         this.addAbility(new BeginningOfUpkeepTriggeredAbility(Zone.BATTLEFIELD, effect,
-            TargetController.CONTROLLER_ATTACHED_TO, false, true));
+            TargetController.CONTROLLER_ATTACHED_TO, false, true, "At the beginning of the upkeep of enchanted artifact's controller, "));
     }
 
     public WarpArtifact(final WarpArtifact card) {
