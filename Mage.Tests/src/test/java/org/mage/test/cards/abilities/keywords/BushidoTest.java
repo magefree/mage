88d4e2b96564dc6041ba66522dedc69e57f040af
LevelX2 <ludwig.hirth@online.de>
2018-02-04 21:13:07 +0100

    * Bushido - Fixed that it triggerd wrongly for each blocker instead of only once if blocked.

--- Mage.Tests/src/test/java/org/mage/test/cards/abilities/keywords/BushidoTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/abilities/keywords/BushidoTest.java
@@ -46,4 +46,24 @@ public class BushidoTest extends CardTestPlayerBase {
         assertPermanentCount(playerB, "Elite Vanguard", 0);
     }
 
+    /**
+     * Tests boosting on double block, it may only trigger once
+     */
+    @Test
+    public void testMultipleBlocker() {
+        addCard(Zone.BATTLEFIELD, playerA, "Llanowar Elves", 1);
+        addCard(Zone.BATTLEFIELD, playerA, "Quirion Elves", 1);
+
+        addCard(Zone.BATTLEFIELD, playerB, "Isao, Enlightened Bushi"); // 2/1  Bushido 2
+        attack(2, playerB, "Isao, Enlightened Bushi");
+        block(2, playerA, "Llanowar Elves", "Isao, Enlightened Bushi");
+        block(2, playerA, "Quirion Elves", "Isao, Enlightened Bushi");
+
+        setStopAt(2, PhaseStep.END_COMBAT);
+        execute();
+
+        assertPowerToughness(playerB, "Isao, Enlightened Bushi", 4, 3);
+        assertPermanentCount(playerA, "Llanowar Elves", 0);
+        assertPermanentCount(playerA, "Quirion Elves", 0);
+    }
 }
