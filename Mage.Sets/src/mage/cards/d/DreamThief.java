f0ee60eedb3bd027492e3112cbccca93323e04cb
LevelX2 <ludwig.hirth@online.de>
2018-02-18 18:31:48 +0100

    * Fixed some possible null pointer exceptions seen in xmage.de server log.

--- Mage.Sets/src/mage/cards/d/DreamThief.java
+++ Mage.Sets/src/mage/cards/d/DreamThief.java
@@ -27,6 +27,7 @@
  */
 package mage.cards.d;
 
+import java.util.List;
 import java.util.UUID;
 import mage.MageInt;
 import mage.abilities.Ability;
@@ -52,7 +53,7 @@ public class DreamThief extends CardImpl {
     private static final String rule = "draw a card if you've cast another blue spell this turn";
 
     public DreamThief(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.CREATURE},"{2}{U}");
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{2}{U}");
         this.subtype.add(SubType.FAERIE);
         this.subtype.add(SubType.ROGUE);
 
@@ -84,9 +85,12 @@ class CastBlueSpellThisTurnCondition implements Condition {
     public boolean apply(Game game, Ability source) {
         SpellsCastWatcher watcher = (SpellsCastWatcher) game.getState().getWatchers().get(SpellsCastWatcher.class.getSimpleName());
         if (watcher != null) {
-            for (Spell spell : watcher.getSpellsCastThisTurn(source.getControllerId())) {
-                if (!spell.getSourceId().equals(source.getSourceId()) && spell.getColor(game).isBlue()) {
-                    return true;
+            List<Spell> spells = watcher.getSpellsCastThisTurn(source.getControllerId());
+            if (spells != null) {
+                for (Spell spell : spells) {
+                    if (!spell.getSourceId().equals(source.getSourceId()) && spell.getColor(game).isBlue()) {
+                        return true;
+                    }
                 }
             }
         }
