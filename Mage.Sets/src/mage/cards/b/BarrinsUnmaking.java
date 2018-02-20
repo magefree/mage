c5cbdcf68fcc52c6b9068d52b9ee33d71b57cc0d
LevelX2 <ludwig.hirth@online.de>
2018-02-20 17:57:23 +0100

    * Fixed some target pointer handling (fixes #4540).

--- Mage.Sets/src/mage/cards/b/BarrinsUnmaking.java
+++ Mage.Sets/src/mage/cards/b/BarrinsUnmaking.java
@@ -52,7 +52,7 @@ public class BarrinsUnmaking extends CardImpl {
     public BarrinsUnmaking(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{1}{U}");
 
-        // Return target permanent to its owner's hand if that permanent shares a color with the most common color among all permanents or a color tied for most common.      
+        // Return target permanent to its owner's hand if that permanent shares a color with the most common color among all permanents or a color tied for most common.
         this.getSpellAbility().addEffect(new BarrinsUnmakingEffect());
         this.getSpellAbility().addTarget(new TargetPermanent());
     }
@@ -85,12 +85,12 @@ class BarrinsUnmakingEffect extends OneShotEffect {
 
     @Override
     public boolean apply(Game game, Ability source) {
-        Permanent permanent = game.getPermanent(source.getFirstTarget());
+        Permanent permanent = game.getPermanent(getTargetPointer().getFirst(game, source));
         if (permanent != null) {
             Condition condition = new MostCommonColorCondition(permanent.getColor(game));
             if (condition.apply(game, source)) {
                 Effect effect = new ReturnToHandTargetEffect();
-                effect.setTargetPointer(new FixedTarget(permanent.getId()));
+                effect.setTargetPointer(new FixedTarget(permanent, game));
                 return effect.apply(game, source);
             }
         }
