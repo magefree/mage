d0bde94ffe1fc06ace16a0e9cd4ce6eade6a8b80
Danny Plenge <dannyplenge@gmail.com>
2018-03-20 11:57:15 +0100

    Refactored method in EmpyrialArchAngel to not always return same value.

--- Mage.Sets/src/mage/cards/e/EmpyrialArchangel.java
+++ Mage.Sets/src/mage/cards/e/EmpyrialArchangel.java
@@ -90,8 +90,7 @@ class EmpyrialArchangelEffect extends ReplacementEffectImpl {
         DamagePlayerEvent damageEvent = (DamagePlayerEvent) event;
         Permanent p = game.getPermanent(source.getSourceId());
         if (p != null) {
-            p.damage(damageEvent.getAmount(), event.getSourceId(), game, damageEvent.isCombatDamage(), damageEvent.isPreventable());
-            return true;
+            p.damage(damageEvent.getAmount(), event.getSourceId(), game, damageEvent.isCombatDamage(), damageEvent.isPreventable());l
         }
         return true;
     }
