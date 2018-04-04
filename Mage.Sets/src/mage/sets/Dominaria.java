18249f870b189bffe8afd6734a9f5f0667f68f07
Michael Simons <rystan@gmail.com>
2018-04-03 23:21:52 -0400

    Add Goblin Warchief to Dominaria set
    https://scryfall.com/card/dom/130

--- Mage.Sets/src/mage/sets/Dominaria.java
+++ Mage.Sets/src/mage/sets/Dominaria.java
@@ -69,5 +69,6 @@ public class Dominaria extends ExpansionSet {
         cards.add(new SetCardInfo("Llanowar Elves", 168, Rarity.COMMON, mage.cards.l.LlanowarElves.class));
         cards.add(new SetCardInfo("Icy Manipulator", 219, Rarity.UNCOMMON, mage.cards.i.IcyManipulator.class));
         cards.add(new SetCardInfo("Meandering River", 274, Rarity.COMMON, mage.cards.m.MeanderingRiver.class));
+        cards.add(new SetCardInfo("Goblin Warchief", 130, Rarity.UNCOMMON, mage.cards.g.GoblinWarchief.class));
     }
 }
