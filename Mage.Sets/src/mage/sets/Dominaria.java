19f57937a5c30193606faada6588dfa32de84945
Michael Simons <rystan@gmail.com>
2018-04-03 22:56:21 -0400

    Add Icy Manipulator to Dominaria set
    https://scryfall.com/card/dom/219

--- Mage.Sets/src/mage/sets/Dominaria.java
+++ Mage.Sets/src/mage/sets/Dominaria.java
@@ -67,5 +67,6 @@ public class Dominaria extends ExpansionSet {
         cards.add(new SetCardInfo("Academy Drake", 40, Rarity.UNCOMMON, mage.cards.a.AcademyDrake.class));
         cards.add(new SetCardInfo("Cast Down", 81, Rarity.UNCOMMON, mage.cards.c.CastDown.class));
         cards.add(new SetCardInfo("Llanowar Elves", 168, Rarity.COMMON, mage.cards.l.LlanowarElves.class));
+        cards.add(new SetCardInfo("Icy Manipulator", 219, Rarity.UNCOMMON, mage.cards.i.IcyManipulator.class));
     }
 }
