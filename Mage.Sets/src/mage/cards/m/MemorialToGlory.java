c59f6d292eb13daf7cf546769cfc58c83cf5fedc
Michael Simons <rystan@gmail.com>
2018-04-08 22:14:53 -0400

    Fixed imports

--- Mage.Sets/src/mage/cards/m/MemorialToGlory.java
+++ Mage.Sets/src/mage/cards/m/MemorialToGlory.java
@@ -30,16 +30,19 @@ package mage.cards.m;
 
 import java.util.UUID;
 import mage.abilities.Ability;
+import mage.abilities.common.EntersBattlefieldTappedAbility;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.common.SacrificeSourceCost;
 import mage.abilities.costs.common.TapSourceCost;
-import mage.abilities.costs.mana.ManaCostImpl;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.CreateTokenEffect;
 import mage.abilities.mana.WhiteManaAbility;
 import mage.abilities.effects.common.DrawCardSourceControllerEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.Zone;
+import mage.game.permanent.token.SoldierToken;
 
 /**
  * @author Rystan
