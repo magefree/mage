ebc6362279bc91119e64ae4580310393466ee589
Evan Kranzler <theelk801@gmail.com>
2018-04-16 19:21:30 -0400

    fixed some tests

--- Mage.Tests/src/test/java/org/mage/test/cards/single/ChandraPyromasterTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/ChandraPyromasterTest.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.mage.test.cards.single;
 
 import mage.constants.PhaseStep;
@@ -15,30 +14,29 @@ import org.mage.test.serverside.base.CardTestPlayerBase;
  *
  * @author LevelX2
  */
-
 public class ChandraPyromasterTest extends CardTestPlayerBase {
 
     @Test
     public void testAbility2CastCardFromExile() {
         addCard(Zone.BATTLEFIELD, playerA, "Chandra, Pyromaster");
         addCard(Zone.BATTLEFIELD, playerA, "Mountain", 2);
-        
+
         skipInitShuffling();
         addCard(Zone.LIBRARY, playerA, "Mizzium Mortars");
-        
+
         addCard(Zone.BATTLEFIELD, playerB, "Silvercoat Lion", 1);
-        
-        activateAbility(1, PhaseStep.PRECOMBAT_MAIN, playerA, "+0: Exile the top card of your library. You may play it this turn.");
+
+        activateAbility(1, PhaseStep.PRECOMBAT_MAIN, playerA, "0: Exile the top card of your library. You may play it this turn.");
         castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Mizzium Mortars", "Silvercoat Lion");
         setStopAt(1, PhaseStep.BEGIN_COMBAT);
         execute();
 
         assertLife(playerA, 20);
         assertLife(playerB, 20);
-        
+
         assertPermanentCount(playerA, "Chandra, Pyromaster", 1);
         assertGraveyardCount(playerA, "Mizzium Mortars", 1);
-        
+
         assertPermanentCount(playerB, "Silvercoat Lion", 0);
         assertGraveyardCount(playerB, "Silvercoat Lion", 1);
 
@@ -54,7 +52,7 @@ public class ChandraPyromasterTest extends CardTestPlayerBase {
 
         addCard(Zone.BATTLEFIELD, playerB, "Silvercoat Lion", 1);
 
-        activateAbility(1, PhaseStep.PRECOMBAT_MAIN, playerA, "+0: Exile the top card of your library. You may play it this turn.");
+        activateAbility(1, PhaseStep.PRECOMBAT_MAIN, playerA, "0: Exile the top card of your library. You may play it this turn.");
         setStopAt(1, PhaseStep.BEGIN_COMBAT);
         execute();
 
@@ -66,29 +64,29 @@ public class ChandraPyromasterTest extends CardTestPlayerBase {
 
         assertExileCount(playerA, "Ancestral Vision", 1);
     }
-    
+
     @Test
     public void testAbility2CastCardFromExileWithOverlaod() {
-        
+
         addCard(Zone.BATTLEFIELD, playerA, "Chandra, Pyromaster");
         addCard(Zone.BATTLEFIELD, playerA, "Mountain", 6);
-        
+
         skipInitShuffling();
         addCard(Zone.LIBRARY, playerA, "Mizzium Mortars");
-        
+
         addCard(Zone.BATTLEFIELD, playerB, "Silvercoat Lion", 2);
-        
-        activateAbility(1, PhaseStep.PRECOMBAT_MAIN, playerA, "+0: Exile the top card of your library. You may play it this turn.");
+
+        activateAbility(1, PhaseStep.PRECOMBAT_MAIN, playerA, "0: Exile the top card of your library. You may play it this turn.");
         castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Mizzium Mortars with overload");
         setStopAt(1, PhaseStep.BEGIN_COMBAT);
         execute();
 
         assertLife(playerA, 20);
         assertLife(playerB, 20);
-        
+
         assertPermanentCount(playerA, "Chandra, Pyromaster", 1);
         assertGraveyardCount(playerA, "Mizzium Mortars", 1);
-        
+
         assertPermanentCount(playerB, "Silvercoat Lion", 0);
         assertGraveyardCount(playerB, "Silvercoat Lion", 2);
 
