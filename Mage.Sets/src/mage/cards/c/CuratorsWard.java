526196511f0aa08488d5850af354bf59abd30b00
LevelX2 <ludwig.hirth@online.de>
2018-04-18 11:10:25 +0200

    [DOM] Updated Curator's Ward.

--- Mage.Sets/src/mage/cards/c/CuratorsWard.java
+++ Mage.Sets/src/mage/cards/c/CuratorsWard.java
@@ -28,26 +28,25 @@
 package mage.cards.c;
 
 import java.util.UUID;
-import mage.constants.SubType;
 import mage.abilities.Ability;
 import mage.abilities.TriggeredAbilityImpl;
-import mage.abilities.effects.Effect;
 import mage.abilities.effects.common.AttachEffect;
 import mage.abilities.effects.common.DrawCardSourceControllerEffect;
 import mage.abilities.effects.common.continuous.GainAbilityAttachedEffect;
-import mage.constants.Outcome;
-import mage.target.TargetPermanent;
 import mage.abilities.keyword.EnchantAbility;
 import mage.abilities.keyword.HexproofAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.AttachmentType;
 import mage.constants.CardType;
+import mage.constants.Outcome;
+import mage.constants.SubType;
 import mage.constants.Zone;
 import mage.game.Game;
 import mage.game.events.GameEvent;
 import mage.game.events.ZoneChangeEvent;
 import mage.game.permanent.Permanent;
+import mage.target.TargetPermanent;
 
 /**
  *
@@ -57,7 +56,7 @@ public class CuratorsWard extends CardImpl {
 
     public CuratorsWard(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{2}{U}");
-        
+
         this.subtype.add(SubType.AURA);
 
         // Enchant permanent
@@ -69,7 +68,7 @@ public class CuratorsWard extends CardImpl {
 
         // Enchanted permanent has hexproof.
         ability.addEffect(new GainAbilityAttachedEffect(HexproofAbility.getInstance(), AttachmentType.AURA).setText("Enchanted permanent has hexproof"));
-        
+
         // When enchanted permanent leaves the battlefield, if it was historic, draw two cards.
         this.addAbility(new CuratorsWardTriggeredAbility());
     }
@@ -87,7 +86,7 @@ public class CuratorsWard extends CardImpl {
 class CuratorsWardTriggeredAbility extends TriggeredAbilityImpl {
 
     public CuratorsWardTriggeredAbility() {
-        super(Zone.BATTLEFIELD, new DrawCardSourceControllerEffect(2).setText("When enchanted permanent leaves the battlefield, if it was historic, draw two cards."), false);
+        super(Zone.BATTLEFIELD, new DrawCardSourceControllerEffect(2), false);
     }
 
     public CuratorsWardTriggeredAbility(CuratorsWardTriggeredAbility ability) {
@@ -104,16 +103,16 @@ class CuratorsWardTriggeredAbility extends TriggeredAbilityImpl {
         ZoneChangeEvent zEvent = (ZoneChangeEvent) event;
         if (zEvent.getFromZone() == Zone.BATTLEFIELD) {
             Permanent enchanted = game.getPermanentOrLKIBattlefield(event.getTargetId());
-            if (enchanted != null && enchanted.getAttachments().contains(sourceId) && enchanted.isHistoric()) {
+            if (enchanted != null && enchanted.getAttachments().contains(getSourceId()) && enchanted.isHistoric()) {
                 return true;
             }
         }
         return false;
     }
 
-    @Override 
+    @Override
     public String getRule() {
-        return "When enchanted permanent leaves the battlefield, if it was historic, draw two cards.";
+        return "When enchanted permanent leaves the battlefield, if it was historic," + super.getRule();
     }
 
     @Override
