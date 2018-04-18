26ce05b42d1517f8a9fb4424a44945065abdbe0a
LevelX2 <ludwig.hirth@online.de>
2018-04-18 15:18:23 +0200

    [DOM] Added Shield of the Realm.

--- Mage.Sets/src/mage/cards/g/GeneralsKabuto.java
+++ Mage.Sets/src/mage/cards/g/GeneralsKabuto.java
@@ -25,13 +25,12 @@
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
  */
-
 package mage.cards.g;
 
 import java.util.UUID;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.costs.mana.GenericManaCost;
-import mage.abilities.effects.common.PreventAllDamageToAttachedEffect;
+import mage.abilities.effects.common.PreventDamageToAttachedEffect;
 import mage.abilities.effects.common.continuous.GainAbilityAttachedEffect;
 import mage.abilities.keyword.EquipAbility;
 import mage.abilities.keyword.ShroudAbility;
@@ -45,21 +44,21 @@ import mage.constants.*;
  */
 public class GeneralsKabuto extends CardImpl {
 
-    public GeneralsKabuto (UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.ARTIFACT},"{4}");
+    public GeneralsKabuto(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT}, "{4}");
         this.subtype.add(SubType.EQUIPMENT);
 
         // Equipped creature has shroud. (It can't be the target of spells or abilities.)
         this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new GainAbilityAttachedEffect(ShroudAbility.getInstance(), AttachmentType.EQUIPMENT)));
 
         // Prevent all combat damage that would be dealt to equipped creature
-        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new PreventAllDamageToAttachedEffect(Duration.WhileOnBattlefield, "equipped creature", true)));
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new PreventDamageToAttachedEffect(Duration.WhileOnBattlefield, AttachmentType.EQUIPMENT, true)));
 
         // Equip {2}
         this.addAbility(new EquipAbility(Outcome.PreventDamage, new GenericManaCost(2)));
     }
 
-    public GeneralsKabuto (final GeneralsKabuto card) {
+    public GeneralsKabuto(final GeneralsKabuto card) {
         super(card);
     }
 
