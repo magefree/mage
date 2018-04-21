8f45bb71f4994f8b57a75b95c41a1a92309eb9f2
LevelX2 <ludwig.hirth@online.de>
2018-04-21 11:17:07 +0200

    * Navigator's Compass - Fixed tooltip rule text.

--- Mage.Sets/src/mage/cards/n/NavigatorsCompass.java
+++ Mage.Sets/src/mage/cards/n/NavigatorsCompass.java
@@ -27,7 +27,7 @@
  */
 package mage.cards.n;
 
-import mage.MageInt;
+import java.util.UUID;
 import mage.abilities.Ability;
 import mage.abilities.common.EntersBattlefieldTriggeredAbility;
 import mage.abilities.common.SimpleActivatedAbility;
@@ -39,11 +39,8 @@ import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.Duration;
 import mage.constants.Zone;
-import mage.filter.common.FilterControlledLandPermanent;
+import mage.filter.StaticFilters;
 import mage.target.common.TargetControlledPermanent;
-import mage.target.common.TargetLandPermanent;
-
-import java.util.UUID;
 
 /**
  *
@@ -52,14 +49,16 @@ import java.util.UUID;
 public class NavigatorsCompass extends CardImpl {
 
     public NavigatorsCompass(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.ARTIFACT},"{1}");
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT}, "{1}");
 
         // When Navigator’s Compass enters the battlefield, you gain 3 life.
         Ability etbAbility = new EntersBattlefieldTriggeredAbility(new GainLifeEffect(3));
         this.addAbility(etbAbility);
         // {tap}: Until end of turn, target land you control becomes the basic land type of your choice in addition to its other types.
-        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new BecomesBasicLandTargetEffect(Duration.EndOfTurn, true, false), new TapSourceCost());
-        ability.addTarget(new TargetControlledPermanent(new FilterControlledLandPermanent()));
+        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new BecomesBasicLandTargetEffect(Duration.EndOfTurn, true, false)
+                .setText("Until end of turn, target land you control becomes the basic land type of your choice in addition to its other types"),
+                 new TapSourceCost());
+        ability.addTarget(new TargetControlledPermanent(StaticFilters.FILTER_CONTROLLED_PERMANENT_LAND));
         this.addAbility(ability);
     }
 
