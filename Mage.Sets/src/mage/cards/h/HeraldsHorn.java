940fe603c6f16f111e5e7fed58c9868e87ebf290
LevelX2 <ludwig.hirth@online.de>
2018-04-30 23:15:48 +0200

    * Herald's Horn - Fixed that cast cost reduction was also applied to other players.

--- Mage.Sets/src/mage/cards/h/HeraldsHorn.java
+++ Mage.Sets/src/mage/cards/h/HeraldsHorn.java
@@ -27,6 +27,7 @@
  */
 package mage.cards.h;
 
+import java.util.UUID;
 import mage.MageObject;
 import mage.abilities.Ability;
 import mage.abilities.common.AsEntersBattlefieldAbility;
@@ -45,8 +46,6 @@ import mage.filter.predicate.mageobject.ChosenSubtypePredicate;
 import mage.game.Game;
 import mage.players.Player;
 
-import java.util.UUID;
-
 /**
  *
  * @author Saga
@@ -54,15 +53,15 @@ import java.util.UUID;
 public class HeraldsHorn extends CardImpl {
 
     public HeraldsHorn(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.ARTIFACT},"{3}");
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT}, "{3}");
 
         // As Herald's Horn enters the battlefield, choose a creature type.
         this.addAbility(new AsEntersBattlefieldAbility(new ChooseCreatureTypeEffect(Outcome.BoostCreature)));
-        
+
         // Creature spells you cast of the chosen type cost {1} less to cast.
         this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD,
-                new SpellsCostReductionAllOfChosenSubtypeEffect(new FilterCreatureCard("Creature spells you cast of the chosen type"), 1)));
-        
+                new SpellsCostReductionAllOfChosenSubtypeEffect(new FilterCreatureCard("Creature spells you cast of the chosen type"), 1, true)));
+
         // At the beginning of your upkeep, look at the top card of your library. If it's a creature card of the chosen type, you may reveal it and put it into your hand.
         this.addAbility(new BeginningOfUpkeepTriggeredAbility(Zone.BATTLEFIELD, new HeraldsHornEffect(), TargetController.YOU, false));
     }
@@ -97,21 +96,21 @@ class HeraldsHornEffect extends OneShotEffect {
     public boolean apply(Game game, Ability source) {
         Player controller = game.getPlayer(source.getControllerId());
         MageObject sourceObject = game.getObject(source.getSourceId());
-        
+
         // Look at the top card of your library.
         if (controller.getLibrary().hasCards()) {
             Card card = controller.getLibrary().getFromTop(game);
             Cards cards = new CardsImpl(card);
             controller.lookAtCards(sourceObject.getIdName(), cards, game);
-        
+
             // If it's a creature card of the chosen type, you may reveal it and put it into your hand.
             FilterCreatureCard filter = new FilterCreatureCard("creature card of the chosen type");
             filter.add(new ChosenSubtypePredicate(source.getSourceId()));
             String message = "Reveal the top card of your library and put that card into your hand?";
             if (card != null) {
                 if (filter.match(card, game) && controller.chooseUse(Outcome.Benefit, message, source, game)) {
-                        controller.moveCards(card, Zone.HAND, source, game);
-                        controller.revealCards(sourceObject.getIdName() + " put into hand", cards, game);
+                    controller.moveCards(card, Zone.HAND, source, game);
+                    controller.revealCards(sourceObject.getIdName() + " put into hand", cards, game);
                 }
             }
         }
