849fb5a14a979c5ae72d5288ea6db2fc94dcd32d
Michael Simons <rystan@gmail.com>
2018-05-09 22:11:50 -0400

    Token changes for C17

--- Mage/src/main/java/mage/game/permanent/token/EdgarMarkovToken.java
+++ Mage/src/main/java/mage/game/permanent/token/EdgarMarkovToken.java
@@ -27,6 +27,9 @@
  */
 package mage.game.permanent.token;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import mage.constants.CardType;
 import mage.constants.SubType;
 import mage.MageInt;
@@ -37,8 +40,15 @@ import mage.MageInt;
  */
 public class EdgarMarkovToken extends TokenImpl {
 
+    final static private List<String> tokenImageSets = new ArrayList<>();
+    static {
+        tokenImageSets.addAll(Arrays.asList("C17"));
+    }
+
     public EdgarMarkovToken() {
         super("Vampire", "1/1 black Vampire creature token");
+        availableImageSetCodes = tokenImageSets;
+        setExpansionSetCodeForImage("C17");
         cardType.add(CardType.CREATURE);
         color.setBlack(true);
         subtype.add(SubType.VAMPIRE);
