4a2e9143687f5f186ada5bf0b4078ce47611c7a5
Michael Simons <rystan@gmail.com>
2018-05-14 01:24:31 -0400

    Update Electryte.java

--- Mage.Sets/src/mage/cards/e/Electryte.java
+++ Mage.Sets/src/mage/cards/e/Electryte.java
@@ -1,7 +1,29 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and should not be interpreted as representing official policies, either expressed
+ *  or implied, of BetaSteward_at_googlemail.com.
  */
 package mage.cards.e;
 
@@ -22,12 +44,6 @@ import mage.filter.predicate.permanent.BlockingPredicate;
  */
 public class Electryte extends CardImpl {
 
-    private static final FilterBlockingCreature filter = new FilterBlockingCreature();
-
-    static {
-        filter.add(new BlockingPredicate());
-    }
-    
     public Electryte(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId,setInfo,new CardType[]{CardType.CREATURE},"{3}{R}{R}");
         this.subtype.add(SubType.BEAST);
@@ -37,7 +53,7 @@ public class Electryte extends CardImpl {
 
         // Whenever Electryte successfully deals combat damage to defending player, 
         // Electryte deals damage equal to its power to each blocking creature.
-        this.addAbility(new DealsCombatDamageToAPlayerTriggeredAbility(new DamageAllEffect(this.power.getValue(), filter), false, false));
+        this.addAbility(new DealsCombatDamageToAPlayerTriggeredAbility(new DamageAllEffect(this.power.getValue(), new FilterBlockingCreature()), false));
     }
 
     public Electryte(final Electryte card) {
