ea0a677e46bd8bed9608f214043d9890a26ceb1d
Evan Kranzler <theelk801@gmail.com>
2018-05-27 22:06:01 -0400

    Fixed Angel of Glory's Rise not bringing back Humans

--- Mage.Sets/src/mage/cards/a/AngelOfGlorysRise.java
+++ Mage.Sets/src/mage/cards/a/AngelOfGlorysRise.java
@@ -103,7 +103,7 @@ class AngelOfGlorysRiseEffect extends OneShotEffect {
             controller.moveCards(toExile, Zone.EXILED, source, game);
             FilterCreatureCard filterHuman = new FilterCreatureCard();
             filterHuman.add(new SubtypePredicate(SubType.HUMAN));
-            controller.moveCards(controller.getGraveyard().getCards(filterHuman, game), Zone.GRAVEYARD, source, game);
+            controller.moveCards(controller.getGraveyard().getCards(filterHuman, game), Zone.BATTLEFIELD, source, game);
         }
         return true;
     }
