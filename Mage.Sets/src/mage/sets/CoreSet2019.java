636d07c42d09f585222c29b0278957d5fc523fe1
Evan Kranzler <theelk801@gmail.com>
2018-05-30 10:56:13 -0400

    Implemented Goblin Motivator

--- Mage.Sets/src/mage/sets/CoreSet2019.java
+++ Mage.Sets/src/mage/sets/CoreSet2019.java
@@ -59,6 +59,7 @@ public final class CoreSet2019 extends ExpansionSet {
         cards.add(new SetCardInfo("Disperse", 50, Rarity.COMMON, mage.cards.d.Disperse.class));
         cards.add(new SetCardInfo("Fire Elemental", 141, Rarity.COMMON, mage.cards.f.FireElemental.class));
         cards.add(new SetCardInfo("Goblin Instigator", 142, Rarity.COMMON, mage.cards.g.GoblinInstigator.class));
+        cards.add(new SetCardInfo("Goblin Motivator", 143, Rarity.COMMON, mage.cards.g.GoblinMotivator.class));
         cards.add(new SetCardInfo("Inspired Charge", 15, Rarity.COMMON, mage.cards.i.InspiredCharge.class));
         cards.add(new SetCardInfo("Lava Axe", 150, Rarity.COMMON, mage.cards.l.LavaAxe.class));
         cards.add(new SetCardInfo("Mighty Leap", 28, Rarity.COMMON, mage.cards.m.MightyLeap.class));
