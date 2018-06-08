243aa7c8731b5cbbce42ae82b99ae4c615638ce3
Evan Kranzler <theelk801@gmail.com>
2018-06-07 22:55:56 -0400

    Implemented Ferocious Zheng

---
+++ Mage.Sets/src/mage/cards/f/FerociousZheng.java
@@ -0,0 +1,33 @@
+package mage.cards.f;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.constants.SubType;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+
+/**
+ *
+ * @author TheElk801
+ */
+public final class FerociousZheng extends CardImpl {
+
+    public FerociousZheng(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{2}{G}{G}");
+
+        this.subtype.add(SubType.CAT);
+        this.subtype.add(SubType.BEAST);
+        this.power = new MageInt(4);
+        this.toughness = new MageInt(4);
+    }
+
+    public FerociousZheng(final FerociousZheng card) {
+        super(card);
+    }
+
+    @Override
+    public FerociousZheng copy() {
+        return new FerociousZheng(this);
+    }
+}
