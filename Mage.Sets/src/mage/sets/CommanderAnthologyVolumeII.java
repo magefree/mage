8f668a859cb1d9e0c912e3993fdf68ae3aecb81c
Michael Simons <rystan@gmail.com>
2018-06-10 22:28:37 -0400

    Update and rename CommanderAnthologyVolII.java to CommanderAnthologyVolumeII.java
    * removed copyright
    * renamed file & references to spell out full set name

--- Mage.Sets/src/mage/sets/CommanderAnthologyVolumeII.java
+++ Mage.Sets/src/mage/sets/CommanderAnthologyVolumeII.java
@@ -1,30 +1,4 @@
-/*
- * Copyright 2011 BetaSteward_at_googlemail.com. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification, are
- * permitted provided that the following conditions are met:
- *
- *    1. Redistributions of source code must retain the above copyright notice, this list of
- *       conditions and the following disclaimer.
- *
- *    2. Redistributions in binary form must reproduce the above copyright notice, this list
- *       of conditions and the following disclaimer in the documentation and/or other materials
- *       provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * The views and conclusions contained in the software and documentation are those of the
- * authors and should not be interpreted as representing official policies, either expressed
- * or implied, of BetaSteward_at_googlemail.com.
- */
+
 package mage.sets;
 
 import mage.cards.ExpansionSet;
@@ -35,17 +9,17 @@ import mage.constants.SetType;
  *
  * @author rystan
  */
-public class CommanderAnthologyVolII extends ExpansionSet {
+public class CommanderAnthologyVolumeII extends ExpansionSet {
 
-    private static final CommanderAnthologyVolII instance = new CommanderAnthologyVolII();
+    private static final CommanderAnthologyVolumeII instance = new CommanderAnthologyVolumeII();
 
-    public static CommanderAnthologyVolII getInstance() {
+    public static CommanderAnthologyVolumeII getInstance() {
         return instance;
     }
     
-    private CommanderAnthologyVolII() {
-        super("Commander Anthology Vol. II", "CM2", ExpansionSet.buildDate(2018, 6, 8), SetType.SUPPLEMENTAL);
-        this.blockName = "Commander Anthology Vol. II";
+    private CommanderAnthologyVolumeII() {
+        super("Commander Anthology Volume II", "CM2", ExpansionSet.buildDate(2018, 6, 8), SetType.SUPPLEMENTAL);
+        this.blockName = "Commander Anthology Volume II";
         this.hasBasicLands = false;
 
         cards.add(new SetCardInfo("Abzan Falconer", 15, Rarity.UNCOMMON, mage.cards.a.AbzanFalconer.class));
