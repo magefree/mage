500e3734f3da84260731d1579df2485a42723bc8
Evan Kranzler <theelk801@gmail.com>
2018-06-11 20:44:16 -0400

    Implemented Strangling Spores

---
+++ Mage.Sets/src/mage/cards/s/StranglingSpores.java
@@ -0,0 +1,33 @@
+package mage.cards.s;
+
+import java.util.UUID;
+import mage.abilities.effects.common.continuous.BoostTargetEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.target.common.TargetCreaturePermanent;
+
+/**
+ *
+ * @author TheElk801
+ */
+public final class StranglingSpores extends CardImpl {
+
+    public StranglingSpores(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{3}{B}");
+
+        // Target creature gets -3/-3 until end of turn.
+        this.getSpellAbility().addEffect(new BoostTargetEffect(-3, -3, Duration.EndOfTurn));
+        this.getSpellAbility().addTarget(new TargetCreaturePermanent());
+    }
+
+    public StranglingSpores(final StranglingSpores card) {
+        super(card);
+    }
+
+    @Override
+    public StranglingSpores copy() {
+        return new StranglingSpores(this);
+    }
+}
