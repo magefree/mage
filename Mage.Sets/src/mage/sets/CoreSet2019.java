6073139e45c29c456c8de61e04e34bbf0fc3d1c2
Evan Kranzler <theelk801@gmail.com>
2018-06-12 19:41:33 -0400

    Implemented Sparktongue Dragon

--- Mage.Sets/src/mage/sets/CoreSet2019.java
+++ Mage.Sets/src/mage/sets/CoreSet2019.java
@@ -106,6 +106,7 @@ public final class CoreSet2019 extends ExpansionSet {
         cards.add(new SetCardInfo("Sleep", 74, Rarity.UNCOMMON, mage.cards.s.Sleep.class));
         cards.add(new SetCardInfo("Snapping Drake", 75, Rarity.COMMON, mage.cards.s.SnappingDrake.class));
         cards.add(new SetCardInfo("Sovereign's Bite", 120, Rarity.COMMON, mage.cards.s.SovereignsBite.class));
+        cards.add(new SetCardInfo("Sparktongue Dragon", 159, Rarity.COMMON, mage.cards.s.SparktongueDragon.class));
         cards.add(new SetCardInfo("Star-Crowned Stag", 38, Rarity.COMMON, mage.cards.s.StarCrownedStag.class));
         cards.add(new SetCardInfo("Strangling Spores", 122, Rarity.COMMON, mage.cards.s.StranglingSpores.class));
         cards.add(new SetCardInfo("Sun Sentinel", 307, Rarity.COMMON, mage.cards.s.SunSentinel.class));
