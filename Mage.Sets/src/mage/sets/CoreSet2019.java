977d4bdc50599c32b8efe82c9433c0e0b8adc4a1
Evan Kranzler <theelk801@gmail.com>
2018-06-13 08:14:22 -0400

    Updated Core 2019 spoiler/reprints

--- Mage.Sets/src/mage/sets/CoreSet2019.java
+++ Mage.Sets/src/mage/sets/CoreSet2019.java
@@ -77,6 +77,7 @@ public final class CoreSet2019 extends ExpansionSet {
         cards.add(new SetCardInfo("Loxodon Line Breaker", 24, Rarity.COMMON, mage.cards.l.LoxodonLineBreaker.class));
         cards.add(new SetCardInfo("Luminous Bonds", 25, Rarity.COMMON, mage.cards.l.LuminousBonds.class));
         cards.add(new SetCardInfo("Marauder's Axe", 240, Rarity.COMMON, mage.cards.m.MaraudersAxe.class));
+        cards.add(new SetCardInfo("Mentor of the Meek", 27, Rarity.RARE, mage.cards.m.MentorOfTheMeek.class));
         cards.add(new SetCardInfo("Mighty Leap", 28, Rarity.COMMON, mage.cards.m.MightyLeap.class));
         cards.add(new SetCardInfo("Militia Bugler", 29, Rarity.UNCOMMON, mage.cards.m.MilitiaBugler.class));
         cards.add(new SetCardInfo("Mist-Cloaked Herald", 310, Rarity.COMMON, mage.cards.m.MistCloakedHerald.class));
