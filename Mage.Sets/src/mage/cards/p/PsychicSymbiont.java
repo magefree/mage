20664e0d1a5c0c314c176a464669379a707cb5e6
Evan Kranzler <theelk801@gmail.com>
2018-06-18 20:53:09 -0400

    Implemented Psychic Symbiont

---
+++ Mage.Sets/src/mage/cards/p/PsychicSymbiont.java
@@ -0,0 +1,48 @@
+package mage.cards.p;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.effects.common.DrawCardSourceControllerEffect;
+import mage.abilities.effects.common.discard.DiscardTargetEffect;
+import mage.constants.SubType;
+import mage.abilities.keyword.FlyingAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.target.common.TargetOpponent;
+
+/**
+ *
+ * @author TheElk801
+ */
+public final class PsychicSymbiont extends CardImpl {
+
+    public PsychicSymbiont(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{4}{U}{B}");
+
+        this.subtype.add(SubType.NIGHTMARE);
+        this.subtype.add(SubType.HORROR);
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(3);
+
+        // Flying
+        this.addAbility(FlyingAbility.getInstance());
+
+        // When Psychic Symbiont enters the battlefield, target opponent discards a card and you draw a card.
+        Ability ability = new EntersBattlefieldTriggeredAbility(new DiscardTargetEffect(1));
+        ability.addEffect(new DrawCardSourceControllerEffect(1).setText("and you draw a card"));
+        ability.addTarget(new TargetOpponent());
+        this.addAbility(ability);
+    }
+
+    public PsychicSymbiont(final PsychicSymbiont card) {
+        super(card);
+    }
+
+    @Override
+    public PsychicSymbiont copy() {
+        return new PsychicSymbiont(this);
+    }
+}
--- Mage.Sets/src/mage/sets/CoreSet2019.java
+++ Mage.Sets/src/mage/sets/CoreSet2019.java
@@ -123,6 +123,7 @@ public final class CoreSet2019 extends ExpansionSet {
         cards.add(new SetCardInfo("Patient Rebuilding", 67, Rarity.RARE, mage.cards.p.PatientRebuilding.class));
         cards.add(new SetCardInfo("Pegasus Courser", 32, Rarity.COMMON, mage.cards.p.PegasusCourser.class));
         cards.add(new SetCardInfo("Plummet", 193, Rarity.COMMON, mage.cards.p.Plummet.class));
+        cards.add(new SetCardInfo("Psychic Symbiont", 221, Rarity.UNCOMMON, mage.cards.p.PsychicSymbiont.class));
         cards.add(new SetCardInfo("Rabid Bite", 195, Rarity.COMMON, mage.cards.r.RabidBite.class));
         cards.add(new SetCardInfo("Radiating Lightning", 313, Rarity.COMMON, mage.cards.r.RadiatingLightning.class));
         cards.add(new SetCardInfo("Reassembling Skeleton", 116, Rarity.UNCOMMON, mage.cards.r.ReassemblingSkeleton.class));
