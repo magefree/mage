9863a0316467433de1332318aea831c2b05ee42e
Evan Kranzler <theelk801@gmail.com>
2018-06-20 08:50:48 -0400

    Implemented Root Snare

---
+++ Mage.Sets/src/mage/cards/r/RootSnare.java
@@ -0,0 +1,31 @@
+package mage.cards.r;
+
+import java.util.UUID;
+import mage.abilities.effects.common.PreventAllDamageByAllPermanentsEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+
+/**
+ *
+ * @author TheElk801
+ */
+public final class RootSnare extends CardImpl {
+
+    public RootSnare(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{1}{G}");
+
+        // Prevent all combat damage that would be dealt this turn.
+        this.getSpellAbility().addEffect(new PreventAllDamageByAllPermanentsEffect(Duration.EndOfTurn, true));
+    }
+
+    public RootSnare(final RootSnare card) {
+        super(card);
+    }
+
+    @Override
+    public RootSnare copy() {
+        return new RootSnare(this);
+    }
+}
--- Mage.Sets/src/mage/sets/CoreSet2019.java
+++ Mage.Sets/src/mage/sets/CoreSet2019.java
@@ -160,6 +160,7 @@ public final class CoreSet2019 extends ExpansionSet {
         cards.add(new SetCardInfo("Revitalize", 35, Rarity.COMMON, mage.cards.r.Revitalize.class));
         cards.add(new SetCardInfo("Riddlemaster Sphinx", 287, Rarity.RARE, mage.cards.r.RiddlemasterSphinx.class));
         cards.add(new SetCardInfo("Rogue's Gloves", 243, Rarity.UNCOMMON, mage.cards.r.RoguesGloves.class));
+        cards.add(new SetCardInfo("Root Snare", 199, Rarity.COMMON, mage.cards.r.RootSnare.class));
         cards.add(new SetCardInfo("Rustwing Falcon", 36, Rarity.COMMON, mage.cards.r.RustwingFalcon.class));
         cards.add(new SetCardInfo("Salvager of Secrets", 70, Rarity.COMMON, mage.cards.s.SalvagerOfSecrets.class));
         cards.add(new SetCardInfo("Sarkhan's Whelp", 299, Rarity.UNCOMMON, mage.cards.s.SarkhansWhelp.class));
