9ce0d15acd5275fed01c8b144279b8bb90a668a3
Evan Kranzler <theelk801@gmail.com>
2018-06-21 14:50:39 -0400

    Implemented Isolate

---
+++ Mage.Sets/src/mage/cards/i/Isolate.java
@@ -0,0 +1,41 @@
+package mage.cards.i;
+
+import java.util.UUID;
+import mage.abilities.effects.common.ExileTargetEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.ComparisonType;
+import mage.filter.FilterPermanent;
+import mage.filter.predicate.mageobject.ConvertedManaCostPredicate;
+import mage.target.TargetPermanent;
+
+/**
+ *
+ * @author TheElk801
+ */
+public final class Isolate extends CardImpl {
+
+    private static final FilterPermanent filter = new FilterPermanent("permanent with converted mana cost 1");
+
+    static {
+        filter.add(new ConvertedManaCostPredicate(ComparisonType.EQUAL_TO, 1));
+    }
+
+    public Isolate(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{W}");
+
+        // Exile target permanent with converted mana cost 1.
+        this.getSpellAbility().addEffect(new ExileTargetEffect());
+        this.getSpellAbility().addTarget(new TargetPermanent(filter));
+    }
+
+    public Isolate(final Isolate card) {
+        super(card);
+    }
+
+    @Override
+    public Isolate copy() {
+        return new Isolate(this);
+    }
+}
