f3d4194adda940a5fc9bfef9ed68a1ff30c7c487
LevelX2 <ludwig.hirth@online.de>
2018-06-24 02:01:20 +0200

    * Palladia-Mors, the Ruiner - Fixed possible exception.

--- Mage.Sets/src/mage/cards/p/PalladiaMorsTheRuiner.java
+++ Mage.Sets/src/mage/cards/p/PalladiaMorsTheRuiner.java
@@ -79,7 +79,7 @@ enum PalladiaMorsTheRuinerCondition implements Condition {
     public boolean apply(Game game, Ability source) {
         Permanent permanent = game.getBattlefield().getPermanent(source.getSourceId());
         PalladiaMorsTheRuinerWatcher watcher = (PalladiaMorsTheRuinerWatcher) game.getState().getWatchers().get(PalladiaMorsTheRuinerWatcher.class.getSimpleName());
-        return !watcher.getDamagers().contains(new MageObjectReference(permanent, game));
+        return permanent != null && !watcher.getDamagers().contains(new MageObjectReference(permanent, game));
     }
 
     @Override
@@ -91,7 +91,7 @@ enum PalladiaMorsTheRuinerCondition implements Condition {
 
 class PalladiaMorsTheRuinerWatcher extends Watcher {
 
-    private Set<MageObjectReference> damagers = new HashSet();
+    private final Set<MageObjectReference> damagers = new HashSet();
 
     public PalladiaMorsTheRuinerWatcher() {
         super(PalladiaMorsTheRuinerWatcher.class.getSimpleName(), WatcherScope.GAME);
