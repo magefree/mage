b25d7256c5b7d8df16ca45d3f3c0ddca3f3d8e6f
EikePeace <eike.pubantz@gmail.com>
2018-07-25 00:57:35 +0200

    Adding Lorado Custom Set with first two Cards

---
+++ Mage.Sets/src/mage/sets/Lorado.java
@@ -0,0 +1,36 @@
+package mage.sets;
+
+
+import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
+import mage.constants.SetType;
+
+/**
+ *
+ * @author EikePace
+ */
+public final class Lorado extends ExpansionSet {
+
+    private static final Lorado instance = new Lorado();
+
+    public static Lorado getInstance() {
+        return instance;
+    }
+
+    private Lorado() {
+        super("Lorado", "LDO", ExpansionSet.buildDate(2018, 4, 16), SetType.CUSTOM_SET);
+        this.blockName = "Lorado";
+        this.hasBoosters = true;
+        this.hasBasicLands = true;
+        this.numBoosterLands = 1;
+        this.numBoosterCommon = 10;
+        this.numBoosterUncommon = 3;
+        this.numBoosterRare = 1;
+        this.ratioBoosterMythic = 8;
+        cards.add(new SetCardInfo("Angelic Regulator", 1, Rarity.RARE, mage.cards.a.AngelicRegulator.class));
+        cards.add(new SetCardInfo("Arrest", 2, Rarity.UNCOMMON, mage.cards.a.Arrest.class));
+        cards.add(new SetCardInfo("Back in the Saddle", 3, Rarity.UNCOMMON, mage.cards.b.BackInTheSaddle.class));
+    }
+
+
+}
