b3786358c7be04de354be22f6ba1034f2ec16be4
Evan Kranzler <theelk801@gmail.com>
2018-07-25 12:18:37 -0400

    Implemented Empyrial Storm

---
+++ Mage.Sets/src/mage/cards/e/EmpyrialStorm.java
@@ -0,0 +1,35 @@
+package mage.cards.e;
+
+import java.util.UUID;
+import mage.abilities.effects.common.CreateTokenEffect;
+import mage.abilities.keyword.CommanderStormAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.game.permanent.token.AngelToken;
+
+/**
+ *
+ * @author TheElk801
+ */
+public final class EmpyrialStorm extends CardImpl {
+
+    public EmpyrialStorm(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{4}{W}{W}");
+
+        // When you cast this spell, copy it for each time you've cast your commander from the command zone this game.
+        this.addAbility(new CommanderStormAbility());
+
+        // Create a 4/4 white Angel creature token with flying.
+        this.getSpellAbility().addEffect(new CreateTokenEffect(new AngelToken()));
+    }
+
+    public EmpyrialStorm(final EmpyrialStorm card) {
+        super(card);
+    }
+
+    @Override
+    public EmpyrialStorm copy() {
+        return new EmpyrialStorm(this);
+    }
+}
--- Mage.Sets/src/mage/sets/Commander2018.java
+++ Mage.Sets/src/mage/sets/Commander2018.java
@@ -34,6 +34,7 @@ public final class Commander2018 extends ExpansionSet {
         cards.add(new SetCardInfo("Echo Storm", 7, Rarity.RARE, mage.cards.e.EchoStorm.class));
         cards.add(new SetCardInfo("Eel Umbra", 89, Rarity.COMMON, mage.cards.e.EelUmbra.class));
         cards.add(new SetCardInfo("Eidolon of Blossoms", 140, Rarity.RARE, mage.cards.e.EidolonOfBlossoms.class));
+        cards.add(new SetCardInfo("Empyrial Storm", 2, Rarity.RARE, mage.cards.e.EmpyrialStorm.class));
         cards.add(new SetCardInfo("Enchanter's Bane", 21, Rarity.RARE, mage.cards.e.EnchantersBane.class));
         cards.add(new SetCardInfo("Enchantress's Presence", 141, Rarity.RARE, mage.cards.e.EnchantresssPresence.class));
         cards.add(new SetCardInfo("Explosive Vegetation", 144, Rarity.UNCOMMON, mage.cards.e.ExplosiveVegetation.class));
