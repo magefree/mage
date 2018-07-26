9b7c0da3187d0d68923548b3e5f3f5d12ed70712
Evan Kranzler <theelk801@gmail.com>
2018-07-25 23:37:24 -0400

    Implemented Turntimber Sower

---
+++ Mage.Sets/src/mage/cards/t/TurntimberSower.java
@@ -0,0 +1,120 @@
+package mage.cards.t;
+
+import java.util.Set;
+import java.util.UUID;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.TriggeredAbilityImpl;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.SacrificeTargetCost;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.CreateTokenEffect;
+import mage.abilities.effects.common.ReturnToHandTargetEffect;
+import mage.cards.Card;
+import mage.constants.SubType;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Zone;
+import mage.filter.StaticFilters;
+import mage.filter.common.FilterControlledCreaturePermanent;
+import mage.filter.common.FilterControlledPermanent;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.events.ZoneChangeGroupEvent;
+import mage.game.permanent.token.PlantToken;
+import mage.target.common.TargetCardInYourGraveyard;
+import mage.target.common.TargetControlledPermanent;
+
+/**
+ *
+ * @author TheElk801
+ */
+public final class TurntimberSower extends CardImpl {
+
+    private static final FilterControlledPermanent filter
+            = new FilterControlledCreaturePermanent("three creatures");
+
+    public TurntimberSower(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{2}{G}");
+
+        this.subtype.add(SubType.ELF);
+        this.subtype.add(SubType.DRUID);
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(3);
+
+        // Whenever one or more land cards are put into your graveyard from anywhere, create a 0/1 green Plant creature token.
+        this.addAbility(new TurntimberSowerTriggeredAbility());
+
+        // {G}, Sacrifice three creatures: Return target land card from your graveyard to your hand.
+        Ability ability = new SimpleActivatedAbility(
+                new ReturnToHandTargetEffect()
+                        .setText("Return target land card "
+                                + "from your graveyard to your hand"),
+                new ManaCostsImpl("{G}")
+        );
+        ability.addCost(new SacrificeTargetCost(
+                new TargetControlledPermanent(3, 3, filter, true)
+        ));
+        ability.addTarget(new TargetCardInYourGraveyard(StaticFilters.FILTER_CARD_LAND));
+        this.addAbility(ability);
+    }
+
+    public TurntimberSower(final TurntimberSower card) {
+        super(card);
+    }
+
+    @Override
+    public TurntimberSower copy() {
+        return new TurntimberSower(this);
+    }
+}
+
+class TurntimberSowerTriggeredAbility extends TriggeredAbilityImpl {
+
+    public TurntimberSowerTriggeredAbility() {
+        super(Zone.BATTLEFIELD, new CreateTokenEffect(new PlantToken()), false);
+    }
+
+    public TurntimberSowerTriggeredAbility(final TurntimberSowerTriggeredAbility ability) {
+        super(ability);
+    }
+
+    @Override
+    public boolean checkEventType(GameEvent event, Game game) {
+        return event.getType() == GameEvent.EventType.ZONE_CHANGE_GROUP;
+    }
+
+    @Override
+    public boolean checkTrigger(GameEvent event, Game game) {
+        ZoneChangeGroupEvent zEvent = (ZoneChangeGroupEvent) event;
+        if (zEvent != null
+                && Zone.GRAVEYARD == zEvent.getToZone()
+                && zEvent.getCards() != null) {
+            for (Card card : zEvent.getCards()) {
+                if (card != null) {
+                    UUID cardOwnerId = card.getOwnerId();
+                    Set<CardType> cardType = card.getCardType();
+                    if (cardOwnerId != null
+                            && card.isOwnedBy(getControllerId())
+                            && cardType != null
+                            && card.isLand()) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public TurntimberSowerTriggeredAbility copy() {
+        return new TurntimberSowerTriggeredAbility(this);
+    }
+
+    @Override
+    public String getRule() {
+        return "Whenever one or more land cards are put into your graveyard "
+                + "from anywhere, create a 0/1 green Plant creature token.";
+    }
+}
--- Mage.Sets/src/mage/sets/Commander2018.java
+++ Mage.Sets/src/mage/sets/Commander2018.java
@@ -62,6 +62,7 @@ public final class Commander2018 extends ExpansionSet {
         cards.add(new SetCardInfo("Tawnos, Urza's Apprentice", 45, Rarity.MYTHIC, mage.cards.t.TawnosUrzasApprentice.class));
         cards.add(new SetCardInfo("Thantis the Warweaver", 46, Rarity.MYTHIC, mage.cards.t.ThantisTheWarweaver.class));
         cards.add(new SetCardInfo("Thopter Spy Network", 107, Rarity.RARE, mage.cards.t.ThopterSpyNetwork.class));
+        cards.add(new SetCardInfo("Turntimber Sower", 35, Rarity.RARE, mage.cards.t.TurntimberSower.class));
         cards.add(new SetCardInfo("Tuvasa the Sunlit", 47, Rarity.MYTHIC, mage.cards.t.TuvasaTheSunlit.class));
         cards.add(new SetCardInfo("Varchild, Betrayer of Kjeldor", 28, Rarity.RARE, mage.cards.v.VarchildBetrayerOfKjeldor.class));
         cards.add(new SetCardInfo("Whiptongue Hydra", 36, Rarity.RARE, mage.cards.w.WhiptongueHydra.class));
