ec7454d888d51c254d58aade3eed449ed3b45f3b
Piotr Szczepanski <piotr.szczepanski@avast.com>
2018-08-20 12:37:53 +0200

    Commanders can be other types than a creature.
    Commanders do not have to be creatures. They can also be Planeswalkers (Commander 2015, Commander 2018), but also can be other types (e.g. lands when enchanted by Song of the Dryads or Imprisoned in the Moon).

--- Mage/src/main/java/mage/filter/predicate/permanent/CommanderPredicate.java
+++ Mage/src/main/java/mage/filter/predicate/permanent/CommanderPredicate.java
@@ -19,13 +19,12 @@ public class CommanderPredicate implements Predicate<Permanent> {
     @Override
     public boolean apply(Permanent input, Game game) {
         Player owner = game.getPlayer(input.getOwnerId());
-        return input.isCreature()
-                && owner != null
+        return owner != null
                 && owner.getCommandersIds().contains(input.getId());
     }
 
     @Override
     public String toString() {
-        return "Commander creature";
+        return "Commander";
     }
 }
