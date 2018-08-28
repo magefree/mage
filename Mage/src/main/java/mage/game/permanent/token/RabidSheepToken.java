bce19d31af9bf0b3b74a61322f9e498c34f92aa1
L_J <Zzooouhh@users.noreply.github.com>
2018-08-28 23:35:53 +0000

    Rabid Sheep token fix

--- Mage/src/main/java/mage/game/permanent/token/RabidSheepToken.java
+++ Mage/src/main/java/mage/game/permanent/token/RabidSheepToken.java
@@ -9,7 +9,7 @@ import mage.MageInt;
  *
  * @author L_J
  */
-public class RabidSheepToken extends Token {
+public final class RabidSheepToken extends TokenImpl {
 
     public RabidSheepToken() {
         super("Rabid Sheep", "2/2 green Sheep creature token named Rabid Sheep");
@@ -18,5 +18,13 @@ public class RabidSheepToken extends Token {
         color.setGreen(true);
         power = new MageInt(2);
         toughness = new MageInt(2);
+    }
+
+    public RabidSheepToken(final RabidSheepToken token) {
+        super(token);
+    }
+
+    public RabidSheepToken copy() {
+        return new RabidSheepToken(this);
     }
 }
