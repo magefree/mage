087b6126a0deffda3565ca792454c3a939f06581
Evan Kranzler <theelk801@gmail.com>
2018-09-02 22:18:16 -0400

    Implemented Boros Challenger

---
+++ Mage.Sets/src/mage/cards/b/BorosChallenger.java
@@ -0,0 +1,49 @@
+package mage.cards.b;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.continuous.BoostSourceEffect;
+import mage.constants.SubType;
+import mage.abilities.keyword.MentorAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Zone;
+
+/**
+ *
+ * @author TheElk801
+ */
+public final class BorosChallenger extends CardImpl {
+
+    public BorosChallenger(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{R}{W}");
+
+        this.subtype.add(SubType.HUMAN);
+        this.subtype.add(SubType.SOLDIER);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(3);
+
+        // Mentor
+        this.addAbility(new MentorAbility());
+
+        // {2}{R}{W}: Boros Challenger gets +1/+1 until end of turn.
+        this.addAbility(new SimpleActivatedAbility(
+                Zone.BATTLEFIELD,
+                new BoostSourceEffect(1, 1, Duration.EndOfTurn),
+                new ManaCostsImpl("{2}{R}{W}")
+        ));
+    }
+
+    public BorosChallenger(final BorosChallenger card) {
+        super(card);
+    }
+
+    @Override
+    public BorosChallenger copy() {
+        return new BorosChallenger(this);
+    }
+}
