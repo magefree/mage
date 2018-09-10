b3558b53942c4bf23cd0e986508ca859ff30877a
Evan Kranzler <theelk801@gmail.com>
2018-09-10 14:05:08 -0400

    Implemented Sunhome Stalwart

---
+++ Mage.Sets/src/mage/cards/s/SunhomeStalwart.java
@@ -0,0 +1,41 @@
+package mage.cards.s;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.constants.SubType;
+import mage.abilities.keyword.FirstStrikeAbility;
+import mage.abilities.keyword.MentorAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+
+/**
+ *
+ * @author TheElk801
+ */
+public final class SunhomeStalwart extends CardImpl {
+
+    public SunhomeStalwart(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{1}{W}");
+
+        this.subtype.add(SubType.HUMAN);
+        this.subtype.add(SubType.SOLDIER);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(2);
+
+        // First strike
+        this.addAbility(FirstStrikeAbility.getInstance());
+
+        // Mentor
+        this.addAbility(new MentorAbility());
+    }
+
+    public SunhomeStalwart(final SunhomeStalwart card) {
+        super(card);
+    }
+
+    @Override
+    public SunhomeStalwart copy() {
+        return new SunhomeStalwart(this);
+    }
+}
