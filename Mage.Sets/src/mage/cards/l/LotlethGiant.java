2b09eb034edfa027527e0bd1b5d4dcbd698934d0
Evan Kranzler <theelk801@gmail.com>
2018-09-14 15:56:51 -0400

    Implemented Lotleth Giant

---
+++ Mage.Sets/src/mage/cards/l/LotlethGiant.java
@@ -0,0 +1,48 @@
+package mage.cards.l;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.dynamicvalue.common.CardsInControllerGraveyardCount;
+import mage.abilities.effects.common.DamageTargetEffect;
+import mage.constants.SubType;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.filter.StaticFilters;
+import mage.target.common.TargetOpponent;
+
+/**
+ *
+ * @author TheElk801
+ */
+public final class LotlethGiant extends CardImpl {
+
+    public LotlethGiant(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{6}{B}");
+
+        this.subtype.add(SubType.ZOMBIE);
+        this.subtype.add(SubType.GIANT);
+        this.power = new MageInt(6);
+        this.toughness = new MageInt(5);
+
+        // Undergrowth — When Lotleth Giant enters the battlefield, it deals 1 damage to target opponent for each creature card in your graveyard.
+        Ability ability = new EntersBattlefieldTriggeredAbility(
+                new DamageTargetEffect(new CardsInControllerGraveyardCount(
+                        StaticFilters.FILTER_CARD_CREATURE
+                ), "it"), false, "<i>Undergrowth</i> &mdash; "
+        );
+        ability.addTarget(new TargetOpponent());
+        this.addAbility(ability);
+    }
+
+    public LotlethGiant(final LotlethGiant card) {
+        super(card);
+    }
+
+    @Override
+    public LotlethGiant copy() {
+        return new LotlethGiant(this);
+    }
+}
