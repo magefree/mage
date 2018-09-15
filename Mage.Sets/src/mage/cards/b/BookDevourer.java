117d2588216035dfaa7c72771bb9d1f9629268a4
Evan Kranzler <theelk801@gmail.com>
2018-09-15 14:20:03 -0400

    Implemented Book Devourer

---
+++ Mage.Sets/src/mage/cards/b/BookDevourer.java
@@ -0,0 +1,45 @@
+package mage.cards.b;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.abilities.common.DealsCombatDamageToAPlayerTriggeredAbility;
+import mage.abilities.effects.common.discard.DiscardHandDrawSameNumberSourceEffect;
+import mage.constants.SubType;
+import mage.abilities.keyword.TrampleAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+
+/**
+ *
+ * @author TheElk801
+ */
+public final class BookDevourer extends CardImpl {
+
+    public BookDevourer(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{5}{R}");
+
+        this.subtype.add(SubType.BEAST);
+        this.power = new MageInt(4);
+        this.toughness = new MageInt(5);
+
+        // Trample
+        this.addAbility(TrampleAbility.getInstance());
+
+        // Whenever Book Devourer deals combat damage to a player, you may discard all the cards in your hand. If you do, draw that many cards.
+        this.addAbility(new DealsCombatDamageToAPlayerTriggeredAbility(
+                new DiscardHandDrawSameNumberSourceEffect()
+                        .setText("discard all the cards in your hand. "
+                                + "If you do, draw that many cards"), true
+        ));
+    }
+
+    public BookDevourer(final BookDevourer card) {
+        super(card);
+    }
+
+    @Override
+    public BookDevourer copy() {
+        return new BookDevourer(this);
+    }
+}
--- Mage.Sets/src/mage/sets/GuildsOfRavnica.java
+++ Mage.Sets/src/mage/sets/GuildsOfRavnica.java
@@ -33,6 +33,7 @@ public final class GuildsOfRavnica extends ExpansionSet {
         cards.add(new SetCardInfo("Beast Whisperer", 123, Rarity.RARE, mage.cards.b.BeastWhisperer.class));
         cards.add(new SetCardInfo("Blade Instructor", 1, Rarity.COMMON, mage.cards.b.BladeInstructor.class));
         cards.add(new SetCardInfo("Blood Operative", 63, Rarity.RARE, mage.cards.b.BloodOperative.class));
+        cards.add(new SetCardInfo("Book Devourer", 93, Rarity.UNCOMMON, mage.cards.b.BookDevourer.class));
         cards.add(new SetCardInfo("Boros Challenger", 156, Rarity.UNCOMMON, mage.cards.b.BorosChallenger.class));
         cards.add(new SetCardInfo("Boros Guildgate", 243, Rarity.COMMON, mage.cards.b.BorosGuildgate.class));
         cards.add(new SetCardInfo("Boros Guildgate", 244, Rarity.COMMON, mage.cards.b.BorosGuildgate.class));
