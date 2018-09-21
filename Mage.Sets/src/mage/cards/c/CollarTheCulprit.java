878a61bde2e22a85d367c0f9a93d6cc1adb24a07
Ryan-Saklad <43081507+Ryan-Saklad@users.noreply.github.com>
2018-09-21 16:49:07 -0400

    Add files via upload

---
+++ Mage.Sets/src/mage/cards/c/CollarTheCulprit.java
@@ -0,0 +1,42 @@
+
+package mage.cards.c;
+
+import java.util.UUID;
+import mage.abilities.effects.common.DestroyTargetEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.ComparisonType;
+import mage.filter.common.FilterCreaturePermanent;
+import mage.filter.predicate.mageobject.ToughnessPredicate;
+import mage.target.common.TargetCreaturePermanent;
+
+/**
+ * @author Ryan-Saklad
+ */
+
+public final class CollarTheCulprit extends CardImpl {
+
+    private static final FilterCreaturePermanent filter = new FilterCreaturePermanent("creature with toughness 4 or greater");
+
+    static {
+        filter.add(new ToughnessPredicate(ComparisonType.MORE_THAN, 3));
+    }
+
+    public CollarTheCulprit(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{3}{W}");
+
+        // Destroy target creature with toughness 4 or greater.
+        this.getSpellAbility().addEffect(new DestroyTargetEffect());
+        this.getSpellAbility().addTarget(new TargetCreaturePermanent(filter));
+    }
+
+    public CollarTheCulprit(final CollarTheCulprit card) {
+        super(card);
+    }
+
+    @Override
+    public CollarTheCulprit copy() {
+        return new CollarTheCulprit(this);
+    }
+}
