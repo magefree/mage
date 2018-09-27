7b4acbe38611f7ed8415b62c663da3be230eae73
Ingmar Goudt <ingmargoudt@gmail.com>
2018-09-27 22:16:32 +0200

    clean up some null checks

--- Mage.Server/src/main/java/mage/server/TableController.java
+++ Mage.Server/src/main/java/mage/server/TableController.java
@@ -942,7 +942,7 @@ public class TableController {
             if (!(table.getState() == TableState.WAITING || table.getState() == TableState.STARTING || table.getState() == TableState.READY_TO_START)) {
                 if (match == null) {
                     logger.warn("- Match table with no match:");
-                    logger.warn("-- matchId:" + match.getId() + " [" + match.getName() + ']');
+                    logger.warn("-- matchId:" + match.getId() + " , table : " + table.getId());
                     // return false;
                 } else if (match.isDoneSideboarding() && match.getGame() == null) {
                     // no sideboarding and not active game -> match seems to hang (maybe the Draw bug)
