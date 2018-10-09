bebc0cbfccfc66712cc9bc39af012212377b7d70
Oleg Agafonov <jaydi85@gmail.com>
2018-10-09 18:37:25 +0400

    [ANA] - implemented River's Favor

---
+++ Mage.Sets/src/mage/cards/r/RiversFavor.java
@@ -0,0 +1,47 @@
+package mage.cards.r;
+
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.common.AttachEffect;
+import mage.abilities.effects.common.continuous.BoostEnchantedEffect;
+import mage.abilities.keyword.EnchantAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Outcome;
+import mage.constants.SubType;
+import mage.constants.Zone;
+import mage.target.TargetPermanent;
+import mage.target.common.TargetCreaturePermanent;
+
+import java.util.UUID;
+
+/**
+ * @author JayDi85
+ */
+public final class RiversFavor extends CardImpl {
+
+    public RiversFavor(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{U}");
+        this.subtype.add(SubType.AURA);
+
+        // Enchant creature
+        TargetPermanent auraTarget = new TargetCreaturePermanent();
+        this.getSpellAbility().addTarget(auraTarget);
+        this.getSpellAbility().addEffect(new AttachEffect(Outcome.BoostCreature));
+        Ability ability = new EnchantAbility(auraTarget.getTargetName());
+        this.addAbility(ability);
+
+        // Enchanted creature gets +1/+1.
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new BoostEnchantedEffect(1, 1)));
+    }
+
+    public RiversFavor(final RiversFavor card) {
+        super(card);
+    }
+
+    @Override
+    public RiversFavor copy() {
+        return new RiversFavor(this);
+    }
+}
