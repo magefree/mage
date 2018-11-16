92d1b5c51af676a089d44b1d11d9cd1e6d4449d8
Oleg Agafonov <jaydi85@gmail.com>
2018-11-17 02:48:50 +0400

    * UI: added new skill level column with star icons instead text;

--- Mage.Client/src/main/java/org/mage/card/arcane/ManaSymbolsCellRenderer.java
+++ Mage.Client/src/main/java/org/mage/card/arcane/ManaSymbolsCellRenderer.java
@@ -9,10 +9,13 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.StringTokenizer;
 
+/**
+ * @author JayDi85
+ */
 public final class ManaSymbolsCellRenderer extends DefaultTableCellRenderer {
 
     // base panel to render
-    private JPanel manaPanel = new JPanel();
+    private JPanel renderPanel = new JPanel();
 
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
@@ -20,47 +23,47 @@ public final class ManaSymbolsCellRenderer extends DefaultTableCellRenderer {
 
         // get table text cell settings
         DefaultTableCellRenderer baseRenderer = (DefaultTableCellRenderer) table.getDefaultRenderer(String.class);
-        JLabel baseLabel = (JLabel)baseRenderer.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        JLabel baseComp = (JLabel) baseRenderer.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
 
         // apply settings to mana panel from parent
-        manaPanel.setOpaque(baseLabel.isOpaque());
-        manaPanel.setForeground(baseLabel.getForeground());
-        manaPanel.setBackground(baseLabel.getBackground());
+        renderPanel.setOpaque(baseComp.isOpaque());
+        renderPanel.setForeground(CardRendererUtils.copyColor(baseComp.getForeground()));
+        renderPanel.setBackground(CardRendererUtils.copyColor(baseComp.getBackground()));
+        renderPanel.setBorder(baseComp.getBorder());
 
         // icons size with margin
         int symbolWidth = GUISizeHelper.symbolTableSize;
         int symbolHorizontalMargin = 2;
 
         // create each mana symbol as child label
-        String manaCost = (String)value;
-        manaPanel.removeAll();
-        manaPanel.setLayout(new BoxLayout(manaPanel, BoxLayout.X_AXIS));
-        if(manaCost != null){
+        String manaCost = (String) value;
+        renderPanel.removeAll();
+        renderPanel.setLayout(new BoxLayout(renderPanel, BoxLayout.X_AXIS));
+        if (manaCost != null) {
             StringTokenizer tok = new StringTokenizer(manaCost, " ");
             while (tok.hasMoreTokens()) {
                 String symbol = tok.nextToken();
 
                 JLabel symbolLabel = new JLabel();
                 //symbolLabel.setBorder(new LineBorder(new Color(150, 150, 150))); // debug
-                symbolLabel.setBorder(new EmptyBorder(0, symbolHorizontalMargin,0, 0));
+                symbolLabel.setBorder(new EmptyBorder(0, symbolHorizontalMargin, 0, 0));
 
                 BufferedImage image = ManaSymbols.getSizedManaSymbol(symbol, symbolWidth);
-                if (image != null){
+                if (image != null) {
                     // icon
                     symbolLabel.setIcon(new ImageIcon(image));
-                }else
-                {
+                } else {
                     // text
                     symbolLabel.setText("{" + symbol + "}");
-                    symbolLabel.setOpaque(baseLabel.isOpaque());
-                    symbolLabel.setForeground(baseLabel.getForeground());
-                    symbolLabel.setBackground(baseLabel.getBackground());
+                    symbolLabel.setOpaque(baseComp.isOpaque());
+                    symbolLabel.setForeground(baseComp.getForeground());
+                    symbolLabel.setBackground(baseComp.getBackground());
                 }
 
-                manaPanel.add(symbolLabel);
+                renderPanel.add(symbolLabel);
             }
         }
 
-        return manaPanel;
+        return renderPanel;
     }
 }
