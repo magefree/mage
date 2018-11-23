2e17cb334e445b6f2dd2df33338a925036af6c3c
jeffwadsworth <jeff@delmarus.com>
2018-11-23 11:36:29 -0600

    - Fixed #5100.  Added a replacement effect for skipping the combat phase.

--- Mage.Sets/src/mage/cards/f/FalsePeace.java
+++ Mage.Sets/src/mage/cards/f/FalsePeace.java
@@ -1,11 +1,11 @@
-
 package mage.cards.f;
 
 import java.util.UUID;
-import mage.abilities.effects.common.SkipNextCombatEffect;
+import mage.abilities.effects.common.SkipCombatStepEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.constants.Duration;
 import mage.target.TargetPlayer;
 
 /**
@@ -13,19 +13,19 @@ import mage.target.TargetPlayer;
  * @author escplan9 (Derek Monturo - dmontur1 at gmail dot com)
  */
 public final class FalsePeace extends CardImpl {
-
+    
     public FalsePeace(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.SORCERY},"{W}");
+        super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{W}");
 
         // Target player skips all combat phases of their next turn.
-        this.getSpellAbility().addEffect(new SkipNextCombatEffect());
+        this.getSpellAbility().addEffect(new SkipCombatStepEffect(Duration.UntilYourNextTurn).setText("Target player skips all combat phases of their next turn."));
         this.getSpellAbility().addTarget(new TargetPlayer());
     }
-
+    
     public FalsePeace(final FalsePeace card) {
         super(card);
     }
-
+    
     @Override
     public FalsePeace copy() {
         return new FalsePeace(this);
