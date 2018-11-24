1e40e6984d2bc1b1b26e3c6eef5c02c37df73cc2
Oleg Agafonov <jaydi85@gmail.com>
2018-11-24 04:52:05 +0400

    Tests: fixed SweepTest;

--- Mage.Tests/src/test/java/org/mage/test/cards/dynamicvalue/SweepTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/dynamicvalue/SweepTest.java
@@ -1,5 +1,3 @@
-
-
 package org.mage.test.cards.dynamicvalue;
 
 import mage.constants.PhaseStep;
@@ -8,7 +6,6 @@ import org.junit.Test;
 import org.mage.test.serverside.base.CardTestPlayerBase;
 
 /**
- *
  * @author BetaSteward
  */
 public class SweepTest extends CardTestPlayerBase {
@@ -18,10 +15,9 @@ public class SweepTest extends CardTestPlayerBase {
      * Plow Through Reito
      * 1W
      * Instant -- Arcane
-     * Sweep -- Return any number of Plains you control to their owner's hand. 
+     * Sweep -- Return any number of Plains you control to their owner's hand.
      * Target creature gets +1/+1 until end of turn for each Plains returned this way.
-     * 
-    */
+     */
     @Test
     public void testSweep1x() {
         addCard(Zone.BATTLEFIELD, playerA, "Plains", 5);
@@ -29,17 +25,17 @@ public class SweepTest extends CardTestPlayerBase {
         addCard(Zone.HAND, playerA, "Plow Through Reito");
 
         castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Plow Through Reito");
-        addTarget(playerA, "Plains");
+        addTarget(playerA, "Raging Goblin"); // target to boost
+        addTarget(playerA, "Plains"); // targets to sweep
 
         setStopAt(1, PhaseStep.BEGIN_COMBAT);
         execute();
 
         assertPermanentCount(playerA, "Raging Goblin", 1);
         assertPermanentCount(playerA, "Plains", 4);
-        assertPowerToughness(playerA,  "Raging Goblin", 2, 2);
-        
+        assertPowerToughness(playerA, "Raging Goblin", 2, 2);
     }
-    
+
     @Test
     public void testSweep2x() {
         addCard(Zone.BATTLEFIELD, playerA, "Plains", 5);
@@ -47,15 +43,15 @@ public class SweepTest extends CardTestPlayerBase {
         addCard(Zone.HAND, playerA, "Plow Through Reito");
 
         castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Plow Through Reito");
-        addTarget(playerA, "Plains^Plains");
+        addTarget(playerA, "Raging Goblin"); // target to boost
+        addTarget(playerA, "Plains^Plains"); // targets to sweep
 
         setStopAt(1, PhaseStep.BEGIN_COMBAT);
         execute();
 
         assertPermanentCount(playerA, "Raging Goblin", 1);
         assertPermanentCount(playerA, "Plains", 3);
-        assertPowerToughness(playerA,  "Raging Goblin", 3, 3);
-        
+        assertPowerToughness(playerA, "Raging Goblin", 3, 3);
     }
 
     @Test
@@ -65,15 +61,15 @@ public class SweepTest extends CardTestPlayerBase {
         addCard(Zone.HAND, playerA, "Plow Through Reito");
 
         castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Plow Through Reito");
-        addTarget(playerA, "Plains^Plains^Plains");
+        addTarget(playerA, "Raging Goblin"); // target to boost
+        addTarget(playerA, "Plains^Plains^Plains"); // targets to sweep
 
         setStopAt(1, PhaseStep.BEGIN_COMBAT);
         execute();
 
         assertPermanentCount(playerA, "Raging Goblin", 1);
         assertPermanentCount(playerA, "Plains", 2);
-        assertPowerToughness(playerA,  "Raging Goblin", 4, 4);
-        
+        assertPowerToughness(playerA, "Raging Goblin", 4, 4);
     }
 
     @Test
@@ -83,14 +79,15 @@ public class SweepTest extends CardTestPlayerBase {
         addCard(Zone.HAND, playerA, "Plow Through Reito");
 
         castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Plow Through Reito");
+        addTarget(playerA, "Raging Goblin"); // target to boost
+        addTarget(playerA, ""); // targets to sweep (zero)
 
         setStopAt(1, PhaseStep.BEGIN_COMBAT);
         execute();
 
         assertPermanentCount(playerA, "Raging Goblin", 1);
         assertPermanentCount(playerA, "Plains", 5);
-        assertPowerToughness(playerA,  "Raging Goblin", 1, 1);
-        
+        assertPowerToughness(playerA, "Raging Goblin", 1, 1);
     }
-    
+
 }
