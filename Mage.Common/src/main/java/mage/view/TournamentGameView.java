2e3fabb1614bb640b3c4d6d360d029fa87f24935
Oleg Agafonov <jaydi85@gmail.com>
2018-12-22 20:41:42 +0400

    Fixed NPE error in lobby

--- Mage.Common/src/main/java/mage/view/TournamentGameView.java
+++ Mage.Common/src/main/java/mage/view/TournamentGameView.java
@@ -43,7 +43,7 @@ public class TournamentGameView implements Serializable {
         String duelingTime = "";
         
         if (game.hasEnded()) {
-            if (game.getEndTime() != null) {
+            if (game.getEndTime() != null && game.getStartTime() != null) {
                 duelingTime = " (" + DateFormat.getDuration((game.getEndTime().getTime() - game.getStartTime().getTime())/1000) + ')';
             }
             this.state = "Finished" + duelingTime;
