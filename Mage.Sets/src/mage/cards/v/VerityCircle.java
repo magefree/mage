2e6b94982e7cc46a812fbea05bfe96eeb4e97950
Evan Kranzler <theelk801@gmail.com>
2019-01-04 22:46:29 -0500

    fixed a few errors

--- Mage.Sets/src/mage/cards/v/VerityCircle.java
+++ Mage.Sets/src/mage/cards/v/VerityCircle.java
@@ -1,15 +1,24 @@
 package mage.cards.v;
 
+import mage.abilities.Ability;
 import mage.abilities.TriggeredAbilityImpl;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.mana.ManaCostsImpl;
 import mage.abilities.effects.common.DrawCardSourceControllerEffect;
+import mage.abilities.effects.common.TapTargetEffect;
+import mage.abilities.keyword.FlyingAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.Zone;
+import mage.filter.common.FilterCreaturePermanent;
+import mage.filter.predicate.Predicates;
+import mage.filter.predicate.mageobject.AbilityPredicate;
 import mage.game.Game;
 import mage.game.events.GameEvent;
 import mage.game.permanent.Permanent;
 import mage.players.Player;
+import mage.target.common.TargetCreaturePermanent;
 
 import java.util.UUID;
 
@@ -18,11 +27,22 @@ import java.util.UUID;
  */
 public final class VerityCircle extends CardImpl {
 
+    private static final FilterCreaturePermanent filter = new FilterCreaturePermanent("creature without flying");
+
+    static {
+        filter.add(Predicates.not(new AbilityPredicate(FlyingAbility.class)));
+    }
+
     public VerityCircle(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{2}{U}");
 
         // Whenever a creature an opponent controls becomes tapped, if it isn't being declared as an attacker, you may draw a card.
         this.addAbility(new VerityCircleTriggeredAbility());
+
+        // {4}{U}: Tap target creature without flying.
+        Ability ability = new SimpleActivatedAbility(new TapTargetEffect(), new ManaCostsImpl("{4}{U}"));
+        ability.addTarget(new TargetCreaturePermanent(filter));
+        this.addAbility(ability);
     }
 
     private VerityCircle(final VerityCircle card) {
