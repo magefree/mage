0293b91c25ffb87810bce2b3253eb0569562f179
Oleg Agafonov <jaydi85@gmail.com>
2019-01-05 13:12:59 +0400

    Tests: added random results image generators (random, shuffle, dice, planar dice);

--- Mage/src/main/java/mage/players/Library.java
+++ Mage/src/main/java/mage/players/Library.java
@@ -1,14 +1,14 @@
-
 package mage.players;
 
-import java.io.Serializable;
-import java.util.*;
-import java.util.stream.Collectors;
 import mage.cards.Card;
 import mage.constants.Zone;
 import mage.filter.FilterCard;
 import mage.game.Game;
 import mage.util.RandomUtil;
+
+import java.io.Serializable;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author BetaSteward_at_googlemail.com
@@ -233,5 +233,18 @@ public class Library implements Serializable {
 
     public void reset() {
         this.emptyDraw = false;
+    }
+
+    /**
+     * Tests only -- find card position in library
+     */
+    public int getCardPosition(UUID cardId) {
+        UUID[] list = library.toArray(new UUID[0]);
+        for (int i = 0; i < list.length; i++) {
+            if (list[i].equals(cardId)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
