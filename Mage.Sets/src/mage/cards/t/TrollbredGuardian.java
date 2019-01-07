82c137c5e01752cb66eb72fb868a741b8ce97529
Evan Kranzler <theelk801@gmail.com>
2019-01-07 16:41:22 -0500

    Implemented Trollbred Guardian

---
+++ Mage.Sets/src/mage/cards/t/TrollbredGuardian.java
@@ -0,0 +1,64 @@
+package mage.cards.t;
+
+import mage.MageInt;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.common.continuous.GainAbilityAllEffect;
+import mage.abilities.keyword.AdaptAbility;
+import mage.abilities.keyword.TrampleAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.SubType;
+import mage.constants.Zone;
+import mage.counters.CounterType;
+import mage.filter.FilterPermanent;
+import mage.filter.common.FilterControlledCreaturePermanent;
+import mage.filter.predicate.permanent.CounterPredicate;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class TrollbredGuardian extends CardImpl {
+
+    private static final FilterPermanent filter = new FilterControlledCreaturePermanent();
+
+    static {
+        filter.add(new CounterPredicate(CounterType.P1P1));
+    }
+
+    public TrollbredGuardian(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{4}{G}");
+
+        this.subtype.add(SubType.TROLL);
+        this.subtype.add(SubType.FROG);
+        this.subtype.add(SubType.WARRIOR);
+        this.power = new MageInt(5);
+        this.toughness = new MageInt(5);
+
+        // {2}{G}: Adapt 2.
+        this.addAbility(new AdaptAbility(2, "{2}{G}"));
+
+        // Each creature you control with a +1/+1 counter on it has trample.
+        this.addAbility(new SimpleStaticAbility(
+                Zone.BATTLEFIELD,
+                new GainAbilityAllEffect(
+                        TrampleAbility.getInstance(),
+                        Duration.WhileOnBattlefield,
+                        filter, "Each creature you control " +
+                        "with a +1/+1 counter on it has trample"
+                )
+        ));
+    }
+
+    private TrollbredGuardian(final TrollbredGuardian card) {
+        super(card);
+    }
+
+    @Override
+    public TrollbredGuardian copy() {
+        return new TrollbredGuardian(this);
+    }
+}
--- Mage.Sets/src/mage/sets/RavnicaAllegiance.java
+++ Mage.Sets/src/mage/sets/RavnicaAllegiance.java
@@ -116,6 +116,7 @@ public final class RavnicaAllegiance extends ExpansionSet {
         cards.add(new SetCardInfo("The Haunt of Hightower", 273, Rarity.MYTHIC, mage.cards.t.TheHauntOfHightower.class));
         cards.add(new SetCardInfo("Titanic Brawl", 146, Rarity.COMMON, mage.cards.t.TitanicBrawl.class));
         cards.add(new SetCardInfo("Tithe Taker", 27, Rarity.RARE, mage.cards.t.TitheTaker.class));
+        cards.add(new SetCardInfo("Trollbred Guardian", 148, Rarity.UNCOMMON, mage.cards.t.TrollbredGuardian.class));
         cards.add(new SetCardInfo("Verity Circle", 58, Rarity.RARE, mage.cards.v.VerityCircle.class));
         cards.add(new SetCardInfo("Wilderness Reclamation", 149, Rarity.UNCOMMON, mage.cards.w.WildernessReclamation.class));
         cards.add(new SetCardInfo("Zegana, Utopian Speaker", 214, Rarity.RARE, mage.cards.z.ZeganaUtopianSpeaker.class));
