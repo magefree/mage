0a9adededeacb7285e6e32b5bf1277e245af91d5
Evan Kranzler <theelk801@gmail.com>
2019-01-12 12:50:10 -0500

    fixed Arrester's Admonition

--- Mage.Sets/src/mage/cards/a/ArrestersAdmonition.java
+++ Mage.Sets/src/mage/cards/a/ArrestersAdmonition.java
@@ -1,6 +1,9 @@
 package mage.cards.a;
 
+import mage.abilities.condition.common.AddendumCondition;
+import mage.abilities.decorator.ConditionalOneShotEffect;
 import mage.abilities.effects.common.DrawCardSourceControllerEffect;
+import mage.abilities.effects.common.ReturnToHandTargetEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
@@ -16,15 +19,15 @@ public final class ArrestersAdmonition extends CardImpl {
     public ArrestersAdmonition(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{2}{U}");
 
-
         // Return target creature to its owner's hand.
-        this.getSpellAbility().addEffect(new ReturnToHandTargetPermanentEffect());
+        this.getSpellAbility().addEffect(new ReturnToHandTargetEffect());
         this.getSpellAbility().addTarget(new TargetCreaturePermanent());
 
         // Addendum — If you cast this spell during your main phase, draw a card.
-        this.getSpellAbility().addEffect(new DrawCardSourceControllerEffect(1)
-                .setText("<br><i>Addendum</i> &mdash; If you cast this spell during your main phase, draw a card.")
-        );
+        this.getSpellAbility().addEffect(new ConditionalOneShotEffect(
+                new DrawCardSourceControllerEffect(1), AddendumCondition.instance,
+                "<br><i>Addendum</i> &mdash; If you cast this spell during your main phase, draw a card."
+        ));
     }
 
     private ArrestersAdmonition(final ArrestersAdmonition card) {
