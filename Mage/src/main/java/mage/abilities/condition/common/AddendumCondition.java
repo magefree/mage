0ca71a26167f0c1bc2c97dfe32031148d8dbf9b4
Evan Kranzler <theelk801@gmail.com>
2019-01-12 17:25:34 -0500

    fixed Sentinel's Mark trigger

--- Mage/src/main/java/mage/abilities/condition/common/AddendumCondition.java
+++ Mage/src/main/java/mage/abilities/condition/common/AddendumCondition.java
@@ -17,13 +17,18 @@ import java.util.Set;
 public enum AddendumCondition implements Condition {
 
     instance;
-    private static final Set<TurnPhase> turnPhases = EnumSet.of(TurnPhase.PRECOMBAT_MAIN, TurnPhase.POSTCOMBAT_MAIN);
+    private static final Set<TurnPhase> turnPhases = EnumSet.of(
+            TurnPhase.PRECOMBAT_MAIN, TurnPhase.POSTCOMBAT_MAIN
+    );
 
     @Override
     public boolean apply(Game game, Ability source) {
         if (!game.isActivePlayer(source.getControllerId()) ||
                 !turnPhases.contains(game.getTurn().getPhase().getType())) {
             return false;
+        }
+        if (CastFromEverywhereSourceCondition.instance.apply(game, source)) {
+            return true;
         }
         Spell spell = game.getSpell(source.getSourceId());
         return spell != null && !spell.isCopy();
