24b8d0c6ed12b5e6d11eb8dd5072d11bf8984dc7
Evan Kranzler <theelk801@gmail.com>
2019-01-11 19:00:12 -0500

    Implemented Gravel-Hide Goblin

---
+++ Mage.Sets/src/mage/cards/g/GravelHideGoblin.java
@@ -0,0 +1,42 @@
+package mage.cards.g;
+
+import mage.MageInt;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.continuous.BoostSourceEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.SubType;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class GravelHideGoblin extends CardImpl {
+
+    public GravelHideGoblin(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{1}{R}");
+
+        this.subtype.add(SubType.GOBLIN);
+        this.subtype.add(SubType.SHAMAN);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(1);
+
+        // {3}{G}: Gravel-Hide Goblin gets +2/+2 until end of turn.
+        this.addAbility(new SimpleActivatedAbility(
+                new BoostSourceEffect(2, 2, Duration.EndOfTurn), new ManaCostsImpl("{3}{G}")
+        ));
+    }
+
+    private GravelHideGoblin(final GravelHideGoblin card) {
+        super(card);
+    }
+
+    @Override
+    public GravelHideGoblin copy() {
+        return new GravelHideGoblin(this);
+    }
+}
