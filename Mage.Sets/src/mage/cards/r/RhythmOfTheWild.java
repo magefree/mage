c2472c0f9b6b80fc37ebda722181dbb3615c02eb
LevelX2 <ludwig.hirth@online.de>
2019-01-12 16:35:28 +0100

    * Rhythm of the Wild - Fixed that the riot trigger did not work (fixes #5514).

--- Mage.Sets/src/mage/cards/r/RhythmOfTheWild.java
+++ Mage.Sets/src/mage/cards/r/RhythmOfTheWild.java
@@ -1,21 +1,24 @@
 package mage.cards.r;
 
+import java.util.UUID;
+import mage.abilities.Ability;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.effects.common.CantBeCounteredControlledEffect;
 import mage.abilities.effects.common.continuous.GainAbilityControlledEffect;
+import mage.abilities.effects.common.continuous.GainAbilityControlledSpellsEffect;
 import mage.abilities.keyword.RiotAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.Duration;
+import mage.constants.TargetController;
 import mage.filter.FilterPermanent;
 import mage.filter.FilterSpell;
 import mage.filter.common.FilterControlledCreaturePermanent;
+import mage.filter.common.FilterCreatureSpell;
 import mage.filter.predicate.Predicates;
-import mage.filter.predicate.mageobject.CardTypePredicate;
+import mage.filter.predicate.permanent.ControllerPredicate;
 import mage.filter.predicate.permanent.TokenPredicate;
-
-import java.util.UUID;
 
 /**
  * @author TheElk801
@@ -23,12 +26,12 @@ import java.util.UUID;
 public final class RhythmOfTheWild extends CardImpl {
 
     private static final FilterSpell filter
-            = new FilterSpell("Creature spells you control");
+            = new FilterCreatureSpell("Creature spells you control");
     private static final FilterPermanent filter2
-            = new FilterControlledCreaturePermanent("Nontoken creatures");
+            = new FilterControlledCreaturePermanent();
 
     static {
-        filter.add(new CardTypePredicate(CardType.CREATURE));
+        filter.add(new ControllerPredicate(TargetController.YOU));
         filter2.add(Predicates.not(new TokenPredicate()));
     }
 
@@ -41,9 +44,12 @@ public final class RhythmOfTheWild extends CardImpl {
         )));
 
         // Nontoken creatures you control have riot.
-        this.addAbility(new SimpleStaticAbility(new GainAbilityControlledEffect(
+        Ability ability = new SimpleStaticAbility(new GainAbilityControlledSpellsEffect(
+                new RiotAbility(), new FilterCreatureSpell()).setText("Nontoken creatures you control have riot. <i>(They enter the battlefield with your choice of a +1/+1 counter or haste.)</i>"));
+        ability.addEffect(new GainAbilityControlledEffect(
                 new RiotAbility(), Duration.WhileOnBattlefield, filter2
-        )));
+        ).setText(""));
+        this.addAbility(ability);
     }
 
     private RhythmOfTheWild(final RhythmOfTheWild card) {
