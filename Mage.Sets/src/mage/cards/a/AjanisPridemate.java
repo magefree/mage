fa0e295932a1400369e33026b13cb4033d145786
Evan Kranzler <theelk801@gmail.com>
2019-01-17 17:40:15 -0500

    updated Ajani's Pridemate to reflect recent errata

--- Mage.Sets/src/mage/cards/a/AjanisPridemate.java
+++ Mage.Sets/src/mage/cards/a/AjanisPridemate.java
@@ -2,76 +2,43 @@
 
 package mage.cards.a;
 
-import java.util.UUID;
 import mage.MageInt;
-import mage.abilities.TriggeredAbilityImpl;
+import mage.abilities.common.GainLifeControllerTriggeredAbility;
 import mage.abilities.effects.common.counter.AddCountersSourceEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.SubType;
-import mage.constants.Zone;
 import mage.counters.CounterType;
-import mage.game.Game;
-import mage.game.events.GameEvent;
-import mage.game.events.GameEvent.EventType;
+
+import java.util.UUID;
 
 /**
- *
  * @author BetaSteward_at_googlemail.com
  */
 public final class AjanisPridemate extends CardImpl {
 
     public AjanisPridemate(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.CREATURE},"{1}{W}");
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{1}{W}");
         this.subtype.add(SubType.CAT);
         this.subtype.add(SubType.SOLDIER);
 
         this.power = new MageInt(2);
         this.toughness = new MageInt(2);
 
-        this.addAbility(new AjanisPridemateAbility());
+        // Whenever you gain life, put a +1/+1 counter on Ajani's Pridemate.
+        this.addAbility(new GainLifeControllerTriggeredAbility(
+                new AddCountersSourceEffect(CounterType.P1P1.createInstance()), false
+        ));
     }
 
-    public AjanisPridemate(final AjanisPridemate card) {
+    private AjanisPridemate(final AjanisPridemate card) {
         super(card);
     }
 
     @Override
     public AjanisPridemate copy() {
         return new AjanisPridemate(this);
-    }
-
-}
-
-class AjanisPridemateAbility extends TriggeredAbilityImpl {
-
-    public AjanisPridemateAbility() {
-        super(Zone.BATTLEFIELD, new AddCountersSourceEffect(CounterType.P1P1.createInstance()), true);
-    }
-
-    public AjanisPridemateAbility(final AjanisPridemateAbility ability) {
-        super(ability);
-    }
-
-    @Override
-    public AjanisPridemateAbility copy() {
-        return new AjanisPridemateAbility(this);
-    }
-
-    @Override
-    public boolean checkEventType(GameEvent event, Game game) {
-        return event.getType() == EventType.GAINED_LIFE;
-    }
-
-    @Override
-    public boolean checkTrigger(GameEvent event, Game game) {
-        return event.getPlayerId().equals(controllerId);
-    }
-
-    @Override
-    public String getRule() {
-        return "Whenever you gain life, you may put a +1/+1 counter on {this}.";
     }
 
 }
