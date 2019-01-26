e5f6980b2d917d1a392d78da9a77b8b3fa39205c
Evan Kranzler <theelk801@gmail.com>
2019-01-26 09:41:48 -0500

    updated implementation of Minion of the Wastes and Nameless Race

--- Mage.Sets/src/mage/cards/m/MinionOfTheWastes.java
+++ Mage.Sets/src/mage/cards/m/MinionOfTheWastes.java
@@ -4,6 +4,8 @@ import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.AsEntersBattlefieldAbility;
 import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.costs.Cost;
+import mage.abilities.costs.common.PayLifeCost;
 import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.InfoEffect;
 import mage.abilities.effects.common.continuous.SetPowerToughnessSourceEffect;
@@ -13,7 +15,9 @@ import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.*;
 import mage.game.Game;
+import mage.game.permanent.Permanent;
 import mage.players.Player;
+import mage.util.CardUtil;
 
 import java.util.UUID;
 
@@ -38,7 +42,7 @@ public final class MinionOfTheWastes extends CardImpl {
         this.addAbility(new SimpleStaticAbility(Zone.ALL, new InfoEffect("{this}'s power and toughness are each equal to the life paid as it entered the battlefield")));
     }
 
-    public MinionOfTheWastes(final MinionOfTheWastes card) {
+    private MinionOfTheWastes(final MinionOfTheWastes card) {
         super(card);
     }
 
@@ -50,12 +54,12 @@ public final class MinionOfTheWastes extends CardImpl {
 
 class MinionOfTheWastesEffect extends OneShotEffect {
 
-    public MinionOfTheWastesEffect() {
+    MinionOfTheWastesEffect() {
         super(Outcome.LoseLife);
         staticText = "pay any amount of life";
     }
 
-    public MinionOfTheWastesEffect(final MinionOfTheWastesEffect effect) {
+    private MinionOfTheWastesEffect(final MinionOfTheWastesEffect effect) {
         super(effect);
     }
 
@@ -67,15 +71,22 @@ class MinionOfTheWastesEffect extends OneShotEffect {
     @Override
     public boolean apply(Game game, Ability source) {
         Player controller = game.getPlayer(source.getControllerId());
-        if (controller != null) {
-            int payAmount = controller.getAmount(0, controller.getLife(), "Pay any amount of life", game);
-            controller.loseLife(payAmount, game, false);
-            Card sourceCard = game.getCard(source.getSourceId());
-            game.informPlayers((sourceCard != null ? sourceCard.getLogName() : "") + ": " + controller.getLogName() +
-                    " pays " + payAmount + " life");
-            game.addEffect(new SetPowerToughnessSourceEffect(payAmount, payAmount, Duration.Custom, SubLayer.SetPT_7b), source);
-            return true;
+        Permanent permanent = game.getPermanentEntering(source.getSourceId());
+        if (controller == null || permanent == null) {
+            return false;
         }
-        return false;
+        int payAmount = controller.getAmount(0, controller.getLife(), "Pay any amount of life", game);
+        Cost cost = new PayLifeCost(payAmount);
+        if (!cost.pay(source, game, source.getSourceId(), source.getControllerId(), true)) {
+            return false;
+        }
+        Card sourceCard = game.getCard(source.getSourceId());
+        game.informPlayers((sourceCard != null ? sourceCard.getLogName() : "") + ": " + controller.getLogName() +
+                " pays " + payAmount + " life");
+        game.addEffect(new SetPowerToughnessSourceEffect(
+                payAmount, payAmount, Duration.Custom, SubLayer.CharacteristicDefining_7a
+        ), source);
+        permanent.addInfo("life paid", CardUtil.addToolTipMarkTags("Life paid: " + payAmount), game);
+        return true;
     }
 }
