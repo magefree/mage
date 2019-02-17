9ef888c63454dbef1f754d2fd189f694aa11f7aa
Oleg Agafonov <jaydi85@gmail.com>
2019-02-17 15:54:52 +0400

    UI: symbols download fixes:  * fixed that mana symbols don't refresh after download finisged (#5592, no more xmage restarts);  * fixed that window don't close after download finished;

--- Mage.Client/src/main/java/org/mage/plugins/card/dl/DownloadJob.java
+++ Mage.Client/src/main/java/org/mage/plugins/card/dl/DownloadJob.java
@@ -1,28 +1,18 @@
-/**
- * DownloadJob.java
- *
- * Created on 25.08.2010
- */
 package org.mage.plugins.card.dl;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Proxy;
-import java.net.URL;
-import java.net.URLConnection;
-import javax.swing.BoundedRangeModel;
-import javax.swing.DefaultBoundedRangeModel;
 import org.mage.plugins.card.dl.beans.properties.Property;
 import org.mage.plugins.card.dl.lm.AbstractLaternaBean;
 import org.mage.plugins.card.utils.CardImageUtils;
 
+import javax.swing.*;
+import java.io.*;
+import java.net.Proxy;
+import java.net.URL;
+import java.net.URLConnection;
+
 /**
- * The class DownloadJob.
+ * Downloader job to download one resource
  *
- * @version V0.0 25.08.2010
  * @author Clemens Koza, JayDi85
  */
 public class DownloadJob extends AbstractLaternaBean {
@@ -88,11 +78,8 @@ public class DownloadJob extends AbstractLaternaBean {
      */
     public void setError(String message, Exception error) {
         if (message == null) {
-
-            message = "Download of " + name + "from " + source.toString() + " caused error: " + error.toString();
+            message = "Download of " + name + " from " + source.toString() + " caused error: " + error.toString();
         }
-//        log.warn(message, error);
-        log.warn(message);
         this.state.setValue(State.ABORTED);
         this.error.setValue(error);
         this.message.setValue(message);
@@ -116,7 +103,7 @@ public class DownloadJob extends AbstractLaternaBean {
             return;
         }
 
-        // change to working state on good prepare call
+        // can continue
         this.state.setValue(State.WORKING);
     }
 
