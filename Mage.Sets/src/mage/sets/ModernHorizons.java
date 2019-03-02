f1b283769735ee2455174e1e76bbca3e65dd1fe7
Evan Kranzler <theelk801@gmail.com>
2019-03-01 19:53:16 -0500

    Implemented Cabal Therapist

--- Mage.Sets/src/mage/sets/ModernHorizons.java
+++ Mage.Sets/src/mage/sets/ModernHorizons.java
@@ -27,6 +27,7 @@ public final class ModernHorizons extends ExpansionSet {
         this.numBoosterRare = 1;
         this.ratioBoosterMythic = 8;
 
+        cards.add(new SetCardInfo("Cabal Therapist", 80, Rarity.RARE, mage.cards.c.CabalTherapist.class));
         cards.add(new SetCardInfo("Serra the Benevolent", 26, Rarity.MYTHIC, mage.cards.s.SerraTheBenevolent.class));
     }
 
