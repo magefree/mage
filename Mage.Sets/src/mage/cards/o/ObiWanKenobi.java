e4feb2932212133beeebeb08fcb8f03d1dabcd23
Richard Coates <prefecttophat@gmail.com>
2019-03-18 22:57:28 +0100

    Update ObiWanKenobi.java
    Added import supertype

--- Mage.Sets/src/mage/cards/o/ObiWanKenobi.java
+++ Mage.Sets/src/mage/cards/o/ObiWanKenobi.java
@@ -15,6 +15,7 @@ import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.SubType;
+import mage.constants.SuperType;
 import mage.constants.Duration;
 import mage.game.command.emblems.ObiWanKenobiEmblem;
 import mage.target.common.TargetCreaturePermanent;
