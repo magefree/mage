2a0d2d125bf8d0cbebdcfdd6bcd24a8bc21fedb9
Oleg Agafonov <jaydi85@gmail.com>
2019-03-18 09:00:27 +0400

    * UI: fixed error message window for long text (auto-size), improved wrong version message;

--- Mage.Common/src/main/java/mage/remote/MageVersionException.java
+++ Mage.Common/src/main/java/mage/remote/MageVersionException.java
@@ -1,11 +1,9 @@
-
 package mage.remote;
 
 import mage.MageException;
 import mage.utils.MageVersion;
 
 /**
- *
  * @author BetaSteward_at_googlemail.com
  */
 public class MageVersionException extends MageException {
@@ -13,7 +11,11 @@ public class MageVersionException extends MageException {
     private final MageVersion serverVersion;
 
     public MageVersionException(MageVersion clientVersion, MageVersion serverVersion) {
-        super("Wrong client version " + clientVersion + ", expecting version " + serverVersion + ". \r\n\r\nPlease download needed version from http://XMage.de or http://www.slightlymagic.net/forum/viewforum.php?f=70");
+        super("Wrong client version."
+                + "<br/>Your version: " + clientVersion
+                + "<br/>Server version: " + serverVersion
+                + "<br/>Release app download: http://xmage.de"
+                + "<br/>BETA app download: http://xmage.today");
         this.serverVersion = serverVersion;
     }
 
