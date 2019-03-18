72a792f82906450863e1c709805a8fe099279752
Richard Coates <prefecttophat@gmail.com>
2019-03-18 22:57:57 +0100

    Update DarthSidiousSithLord.java
    Added import supertype.

--- Mage.Sets/src/mage/cards/d/DarthSidiousSithLord.java
+++ Mage.Sets/src/mage/cards/d/DarthSidiousSithLord.java
@@ -14,6 +14,7 @@ import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.SubType;
+import mage.constants.SuperType;
 import mage.constants.Duration;
 import mage.filter.FilterPermanent;
 import mage.filter.predicate.Predicates;
