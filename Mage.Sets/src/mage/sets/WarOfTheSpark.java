0a1e7cf6544b8a2ac42f050b2c556cb0ecd215ae
Evan Kranzler <theelk801@gmail.com>
2019-03-28 19:30:45 -0400

    added WAR set file and first confirmed reprint

---
+++ Mage.Sets/src/mage/sets/WarOfTheSpark.java
@@ -0,0 +1,29 @@
+package mage.sets;
+
+import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
+import mage.constants.SetType;
+
+public final class WarOfTheSpark extends ExpansionSet {
+
+    private static final WarOfTheSpark instance = new WarOfTheSpark();
+
+    public static WarOfTheSpark getInstance() {
+        return instance;
+    }
+
+    private WarOfTheSpark() {
+        super("War of the Spark", "WAR", ExpansionSet.buildDate(2019, 5, 3), SetType.EXPANSION);
+        this.blockName = "Guilds of Ravnica";
+        this.hasBoosters = true;
+        this.numBoosterSpecial = 1;
+        this.numBoosterLands = 0;
+        this.numBoosterCommon = 10;
+        this.numBoosterUncommon = 3;
+        this.numBoosterRare = 1;
+        this.ratioBoosterMythic = 8;
+        this.maxCardNumberInBooster = 264;
+
+        cards.add(new SetCardInfo("Ajani's Pridemate", 4, Rarity.UNCOMMON, mage.cards.a.AjanisPridemate.class));
+    }
+}
