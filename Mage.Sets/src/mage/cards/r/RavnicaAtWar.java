837e83f911a323a9063a8568105db1bf90105673
Evan Kranzler <theelk801@gmail.com>
2019-03-31 13:04:39 -0400

    Implemented Ravnica at War

---
+++ Mage.Sets/src/mage/cards/r/RavnicaAtWar.java
@@ -0,0 +1,39 @@
+package mage.cards.r;
+
+import mage.abilities.effects.common.ExileAllEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.filter.FilterPermanent;
+import mage.filter.StaticFilters;
+import mage.filter.predicate.mageobject.MulticoloredPredicate;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class RavnicaAtWar extends CardImpl {
+
+    private static final FilterPermanent filter = new FilterPermanent("multicolored permanents");
+
+    static {
+        filter.add(MulticoloredPredicate.instance);
+    }
+
+    public RavnicaAtWar(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{3}{W}");
+
+        // Exile all multicolored permanents.
+        this.getSpellAbility().addEffect(new ExileAllEffect(StaticFilters.FILTER_PERMANENT));
+    }
+
+    private RavnicaAtWar(final RavnicaAtWar card) {
+        super(card);
+    }
+
+    @Override
+    public RavnicaAtWar copy() {
+        return new RavnicaAtWar(this);
+    }
+}
