aea78d18846cb63f835894d12d029eda26d7178a
Evan Kranzler <theelk801@gmail.com>
2019-04-05 08:14:52 -0400

    Implemented Mizzium Tank

---
+++ Mage.Sets/src/mage/cards/m/MizziumTank.java
@@ -0,0 +1,55 @@
+package mage.cards.m;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SpellCastControllerTriggeredAbility;
+import mage.abilities.effects.common.continuous.AddCardTypeSourceEffect;
+import mage.abilities.effects.common.continuous.BoostSourceEffect;
+import mage.abilities.keyword.CrewAbility;
+import mage.abilities.keyword.TrampleAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.SubType;
+import mage.filter.StaticFilters;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class MizziumTank extends CardImpl {
+
+    public MizziumTank(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT}, "{1}{R}{R}");
+
+        this.subtype.add(SubType.VEHICLE);
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(2);
+
+        // Trample
+        this.addAbility(TrampleAbility.getInstance());
+
+        // Whenever you cast a noncreature spell, Mizzium Tank becomes an artifact creature and gets +1/+1 until end of turn.
+        Ability ability = new SpellCastControllerTriggeredAbility(new AddCardTypeSourceEffect(
+                Duration.EndOfTurn, CardType.ARTIFACT, CardType.CREATURE
+        ).setText("{this} becomes an artifact creature"), StaticFilters.FILTER_SPELL_NON_CREATURE, false);
+        ability.addEffect(new BoostSourceEffect(
+                1, 1, Duration.EndOfTurn
+        ).setText("and gets +1/+1 until end of turn"));
+        this.addAbility(ability);
+
+        // Crew 1
+        this.addAbility(new CrewAbility(1));
+    }
+
+    private MizziumTank(final MizziumTank card) {
+        super(card);
+    }
+
+    @Override
+    public MizziumTank copy() {
+        return new MizziumTank(this);
+    }
+}
