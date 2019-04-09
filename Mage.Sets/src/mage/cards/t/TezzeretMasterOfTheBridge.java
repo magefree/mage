6e3baada5d6a341d3650cc3c75e778c939212c95
Evan Kranzler <theelk801@gmail.com>
2019-04-09 13:28:17 -0400

    fixed Tezzeret, Master of the Bridge's ultimate not working

--- Mage.Sets/src/mage/cards/t/TezzeretMasterOfTheBridge.java
+++ Mage.Sets/src/mage/cards/t/TezzeretMasterOfTheBridge.java
@@ -125,7 +125,7 @@ class TezzeretMasterOfTheBridgeEffect2 extends OneShotEffect {
 
     @Override
     public boolean apply(Game game, Ability source) {
-        Player player = game.getPlayer(source.getSourceId());
+        Player player = game.getPlayer(source.getControllerId());
         if (player == null) {
             return false;
         }
