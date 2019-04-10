33b6e13d11f28b06ace32e37c34e897725dbd308
Evan Kranzler <theelk801@gmail.com>
2019-04-10 15:14:39 -0400

    Implemented Topple the Statue

---
+++ Mage.Sets/src/mage/cards/t/ToppleTheStatue.java
@@ -0,0 +1,68 @@
+package mage.cards.t;
+
+import mage.abilities.Ability;
+import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.DrawCardSourceControllerEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Outcome;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.target.TargetPermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class ToppleTheStatue extends CardImpl {
+
+    public ToppleTheStatue(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{2}{W}");
+
+        // Tap target permanent. If it's an artifact, destroy it.
+        // Draw a card.
+        this.getSpellAbility().addEffect(new ToppleTheStatueEffect());
+        this.getSpellAbility().addTarget(new TargetPermanent());
+    }
+
+    private ToppleTheStatue(final ToppleTheStatue card) {
+        super(card);
+    }
+
+    @Override
+    public ToppleTheStatue copy() {
+        return new ToppleTheStatue(this);
+    }
+}
+
+class ToppleTheStatueEffect extends OneShotEffect {
+
+    ToppleTheStatueEffect() {
+        super(Outcome.Benefit);
+        staticText = "Tap target permanent. If it's an artifact, destroy it.<br>Draw a card.";
+    }
+
+    private ToppleTheStatueEffect(final ToppleTheStatueEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public ToppleTheStatueEffect copy() {
+        return new ToppleTheStatueEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Permanent permanent = game.getPermanent(source.getFirstTarget());
+        if (permanent == null) {
+            return false;
+        }
+        permanent.tap(game);
+        if (permanent.isArtifact()) {
+            permanent.destroy(source.getSourceId(), game, false);
+        }
+        return new DrawCardSourceControllerEffect(1).apply(game, source);
+    }
+}
