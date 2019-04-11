f14f9f8fb2e76e948c32914e4860f1869f58042c
Evan Kranzler <theelk801@gmail.com>
2019-04-11 19:24:41 -0400

    Implemented Desperate Lunge

---
+++ Mage.Sets/src/mage/cards/d/DesperateLunge.java
@@ -0,0 +1,42 @@
+package mage.cards.d;
+
+import mage.abilities.effects.common.GainLifeEffect;
+import mage.abilities.effects.common.continuous.BoostTargetEffect;
+import mage.abilities.effects.common.continuous.GainAbilityTargetEffect;
+import mage.abilities.keyword.FlyingAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.target.common.TargetCreaturePermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class DesperateLunge extends CardImpl {
+
+    public DesperateLunge(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{1}{W}");
+
+        // Target creature gets +2/+2 and gains flying until end of turn. You gain 2 life.
+        this.getSpellAbility().addEffect(new BoostTargetEffect(
+                2, 2, Duration.EndOfTurn
+        ).setText("Target creature gets +2/+2"));
+        this.getSpellAbility().addEffect(new GainAbilityTargetEffect(
+                FlyingAbility.getInstance(), Duration.EndOfTurn
+        ).setText("and gains flying until end of turn."));
+        this.getSpellAbility().addEffect(new GainLifeEffect(2));
+        this.getSpellAbility().addTarget(new TargetCreaturePermanent());
+    }
+
+    private DesperateLunge(final DesperateLunge card) {
+        super(card);
+    }
+
+    @Override
+    public DesperateLunge copy() {
+        return new DesperateLunge(this);
+    }
+}
--- Mage.Sets/src/mage/sets/WarOfTheSpark.java
+++ Mage.Sets/src/mage/sets/WarOfTheSpark.java
@@ -55,6 +55,7 @@ public final class WarOfTheSpark extends ExpansionSet {
         cards.add(new SetCardInfo("Deathsprout", 189, Rarity.UNCOMMON, mage.cards.d.Deathsprout.class));
         cards.add(new SetCardInfo("Defiant Strike", 9, Rarity.COMMON, mage.cards.d.DefiantStrike.class));
         cards.add(new SetCardInfo("Demolish", 123, Rarity.COMMON, mage.cards.d.Demolish.class));
+        cards.add(new SetCardInfo("Desperate Lunge", 266, Rarity.COMMON, mage.cards.d.DesperateLunge.class));
         cards.add(new SetCardInfo("Devouring Hellion", 124, Rarity.UNCOMMON, mage.cards.d.DevouringHellion.class));
         cards.add(new SetCardInfo("Domri's Ambush", 192, Rarity.UNCOMMON, mage.cards.d.DomrisAmbush.class));
         cards.add(new SetCardInfo("Domri, Anarch of Bolas", 191, Rarity.RARE, mage.cards.d.DomriAnarchOfBolas.class));
