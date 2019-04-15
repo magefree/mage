17023e72f51c0a4ca106c2dca802405bad4be34b
Evan Kranzler <theelk801@gmail.com>
2019-04-15 19:35:24 -0400

    Implemented Spark Reaper

---
+++ Mage.Sets/src/mage/cards/s/SparkReaper.java
@@ -0,0 +1,58 @@
+package mage.cards.s;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.SacrificeTargetCost;
+import mage.abilities.costs.mana.GenericManaCost;
+import mage.abilities.effects.common.DrawCardSourceControllerEffect;
+import mage.abilities.effects.common.GainLifeEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.filter.common.FilterControlledPermanent;
+import mage.filter.predicate.Predicates;
+import mage.filter.predicate.mageobject.CardTypePredicate;
+import mage.target.common.TargetControlledPermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class SparkReaper extends CardImpl {
+
+    private static final FilterControlledPermanent filter
+            = new FilterControlledPermanent("a creature or planeswalker");
+
+    static {
+        filter.add(Predicates.or(
+                new CardTypePredicate(CardType.CREATURE),
+                new CardTypePredicate(CardType.PLANESWALKER)
+        ));
+    }
+
+    public SparkReaper(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{2}{B}");
+
+        this.subtype.add(SubType.ZOMBIE);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(3);
+
+        // {3}, Sacrifice a creature or planeswalker: You gain 1 life and draw a card.
+        Ability ability = new SimpleActivatedAbility(new GainLifeEffect(1), new GenericManaCost(3));
+        ability.addEffect(new DrawCardSourceControllerEffect(1).concatBy("and"));
+        ability.addCost(new SacrificeTargetCost(new TargetControlledPermanent(filter)));
+        this.addAbility(ability);
+    }
+
+    private SparkReaper(final SparkReaper card) {
+        super(card);
+    }
+
+    @Override
+    public SparkReaper copy() {
+        return new SparkReaper(this);
+    }
+}
