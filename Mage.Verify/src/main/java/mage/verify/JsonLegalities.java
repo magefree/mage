00633ce055342386ab5cbc810857aefef5a2d8e2
Oleg Agafonov <jaydi85@gmail.com>
2019-04-28 19:08:13 +0400

    Fixed mtgjson compatibility

--- Mage.Verify/src/main/java/mage/verify/JsonLegalities.java
+++ Mage.Verify/src/main/java/mage/verify/JsonLegalities.java
@@ -1,7 +1,11 @@
 package mage.verify;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import static mage.verify.MtgJson.MTGJSON_IGNORE_NEW_PROPERTIES;
+
+@JsonIgnoreProperties(ignoreUnknown = MTGJSON_IGNORE_NEW_PROPERTIES)
 public class JsonLegalities {
     @JsonProperty("1v1")
     public String oneVersusOne;
--- Mage.Verify/src/main/java/mage/verify/JsonMeta.java
+++ Mage.Verify/src/main/java/mage/verify/JsonMeta.java
@@ -1,5 +1,10 @@
 package mage.verify;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import static mage.verify.MtgJson.MTGJSON_IGNORE_NEW_PROPERTIES;
+
+@JsonIgnoreProperties(ignoreUnknown = MTGJSON_IGNORE_NEW_PROPERTIES)
 public class JsonMeta {
     public String date;
     public String version;
