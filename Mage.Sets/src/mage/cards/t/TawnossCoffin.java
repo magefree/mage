9273dd1ea033788605d032dc8238ea9598385f70
Oleg Agafonov <jaydi85@gmail.com>
2019-04-30 19:21:43 +0400

    Fixed that some cards doesn't trigger counter add events;

--- Mage.Sets/src/mage/cards/t/TawnossCoffin.java
+++ Mage.Sets/src/mage/cards/t/TawnossCoffin.java
@@ -1,8 +1,5 @@
 package mage.cards.t;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
 import mage.abilities.Ability;
 import mage.abilities.common.LeavesBattlefieldTriggeredAbility;
 import mage.abilities.common.SimpleActivatedAbility;
@@ -32,8 +29,11 @@ import mage.target.Target;
 import mage.target.common.TargetCreaturePermanent;
 import mage.util.CardUtil;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
 /**
- *
  * @author MarcoMarin
  */
 public final class TawnossCoffin extends CardImpl {
@@ -194,7 +194,7 @@ class TawnossCoffinReturnEffect extends OneShotEffect {
             if (notedCounters != null) {
                 for (Counter c : notedCounters.values()) { //would be nice if could just use that copy function to set the whole field
                     if (c != null) {
-                        newPermanent.getCounters(game).addCounter(c);
+                        newPermanent.getCounters(game).addCounter(c); // it's restore counters, not add (e.g. without add events)
                     }
                 }
             }
