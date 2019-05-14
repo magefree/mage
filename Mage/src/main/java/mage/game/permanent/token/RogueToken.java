a4d6fbfba2f4609c1b84408e818e626cac0163bc
jmharmon <37360760+jmharmon@users.noreply.github.com>
2019-05-13 22:27:34 -0700

    Implement Rogue token for Dungeon Master

---
+++ Mage/src/main/java/mage/game/permanent/token/RogueToken.java
@@ -0,0 +1,32 @@
+package mage.game.permanent.token;
+
+import mage.MageInt;
+import mage.abilities.keyword.HexproofAbility;
+import mage.constants.CardType;
+import mage.constants.SubType;
+
+/**
+ *
+ * @author jmharmon
+ */
+
+public final class RogueToken extends TokenImpl {
+
+    public RogueToken() {
+        super("Rogue","2/2 black Rogue creature token with hexproof");
+        cardType.add(CardType.CREATURE);
+        color.setBlack(true);
+        subtype.add(SubType.ROGUE);
+        power = new MageInt(2);
+        toughness = new MageInt(2);
+        addAbility(HexproofAbility.getInstance());
+    }
+
+    public RogueToken(final RogueToken token) {
+        super(token);
+    }
+
+    public RogueToken copy() {
+        return new RogueToken(this);
+    }
+}
