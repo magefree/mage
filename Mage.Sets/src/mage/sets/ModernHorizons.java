e0235d99980606f7b1841700458f6be1eda4a303
Evan Kranzler <theelk801@gmail.com>
2019-05-21 13:33:58 -0400

    Implemented Waterlogged Grove

--- Mage.Sets/src/mage/sets/ModernHorizons.java
+++ Mage.Sets/src/mage/sets/ModernHorizons.java
@@ -62,6 +62,7 @@ public final class ModernHorizons extends ExpansionSet {
         cards.add(new SetCardInfo("Undead Augur", 112, Rarity.UNCOMMON, mage.cards.u.UndeadAugur.class));
         cards.add(new SetCardInfo("Venomous Changeling", 114, Rarity.COMMON, mage.cards.v.VenomousChangeling.class));
         cards.add(new SetCardInfo("Wall of One Thousand Cuts", 36, Rarity.COMMON, mage.cards.w.WallOfOneThousandCuts.class));
+        cards.add(new SetCardInfo("Waterlogged Grove", 248, Rarity.RARE, mage.cards.w.WaterloggedGrove.class));
         cards.add(new SetCardInfo("Wing Shards", 38, Rarity.UNCOMMON, mage.cards.w.WingShards.class));
     }
 }
