e457aba61d94b5dd51c4780c60bb22766f209a69
Evan Kranzler <theelk801@gmail.com>
2019-05-23 18:10:15 -0400

    Implemented Generous Gift

---
+++ Mage.Sets/src/mage/cards/g/GenerousGift.java
@@ -0,0 +1,74 @@
+package mage.cards.g;
+
+import mage.abilities.Ability;
+import mage.abilities.effects.Effect;
+import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.CreateTokenTargetEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Outcome;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.game.permanent.token.ElephantToken;
+import mage.players.Player;
+import mage.target.TargetPermanent;
+import mage.target.targetpointer.FixedTarget;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class GenerousGift extends CardImpl {
+
+    public GenerousGift(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{2}{W}");
+
+        // Destroy target permanent. Its controller creates a 3/3 green Elephant creature token.
+        this.getSpellAbility().addEffect(new GenerousGiftEffect());
+        this.getSpellAbility().addTarget(new TargetPermanent());
+    }
+
+    private GenerousGift(final GenerousGift card) {
+        super(card);
+    }
+
+    @Override
+    public GenerousGift copy() {
+        return new GenerousGift(this);
+    }
+}
+
+class GenerousGiftEffect extends OneShotEffect {
+
+    GenerousGiftEffect() {
+        super(Outcome.Benefit);
+        staticText = "Destroy target permanent. Its controller creates a 3/3 green Elephant creature token.";
+    }
+
+    private GenerousGiftEffect(final GenerousGiftEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public GenerousGiftEffect copy() {
+        return new GenerousGiftEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Permanent permanent = game.getPermanent(source.getFirstTarget());
+        if (permanent == null) {
+            return false;
+        }
+        Player player = game.getPlayer(permanent.getControllerId());
+        permanent.destroy(source.getSourceId(), game, false);
+        if (player == null) {
+            return false;
+        }
+        Effect effect = new CreateTokenTargetEffect(new ElephantToken());
+        effect.setTargetPointer(new FixedTarget(player.getId(), game));
+        return effect.apply(game, source);
+    }
+}
