ced16df3158c0a78c2fcb63ee93e813758c756aa
Evan Kranzler <theelk801@gmail.com>
2019-05-30 16:59:42 -0400

    Implemented Marit Lage's Slumber

--- Mage.Sets/src/mage/sets/ModernHorizons.java
+++ Mage.Sets/src/mage/sets/ModernHorizons.java
@@ -115,6 +115,7 @@ public final class ModernHorizons extends ExpansionSet {
         cards.add(new SetCardInfo("Lonely Sandbar", 242, Rarity.UNCOMMON, mage.cards.l.LonelySandbar.class));
         cards.add(new SetCardInfo("Magmatic Sinkhole", 135, Rarity.COMMON, mage.cards.m.MagmaticSinkhole.class));
         cards.add(new SetCardInfo("Man-o'-War", 55, Rarity.COMMON, mage.cards.m.ManOWar.class));
+        cards.add(new SetCardInfo("Marit Lage's Slumber", 56, Rarity.RARE, mage.cards.m.MaritLagesSlumber.class));
         cards.add(new SetCardInfo("Martyr's Soul", 19, Rarity.COMMON, mage.cards.m.MartyrsSoul.class));
         cards.add(new SetCardInfo("Mind Rake", 96, Rarity.COMMON, mage.cards.m.MindRake.class));
         cards.add(new SetCardInfo("Mirrodin Besieged", 57, Rarity.RARE, mage.cards.m.MirrodinBesieged.class));
