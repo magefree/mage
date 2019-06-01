285e72dc9d04b55800630d8170d3bdaeec83a78f
Evan Kranzler <theelk801@gmail.com>
2019-05-31 21:47:41 -0400

    Implemented Excavating Anurid

--- Mage.Sets/src/mage/sets/ModernHorizons.java
+++ Mage.Sets/src/mage/sets/ModernHorizons.java
@@ -69,6 +69,7 @@ public final class ModernHorizons extends ExpansionSet {
         cards.add(new SetCardInfo("Ephemerate", 7, Rarity.COMMON, mage.cards.e.Ephemerate.class));
         cards.add(new SetCardInfo("Etchings of the Chosen", 198, Rarity.UNCOMMON, mage.cards.e.EtchingsOfTheChosen.class));
         cards.add(new SetCardInfo("Everdream", 47, Rarity.UNCOMMON, mage.cards.e.Everdream.class));
+        cards.add(new SetCardInfo("Excavating Anurid", 163, Rarity.COMMON, mage.cards.e.ExcavatingAnurid.class));
         cards.add(new SetCardInfo("Exclude", 48, Rarity.UNCOMMON, mage.cards.e.Exclude.class));
         cards.add(new SetCardInfo("Fact or Fiction", 50, Rarity.UNCOMMON, mage.cards.f.FactOrFiction.class));
         cards.add(new SetCardInfo("Faerie Seer", 51, Rarity.COMMON, mage.cards.f.FaerieSeer.class));
