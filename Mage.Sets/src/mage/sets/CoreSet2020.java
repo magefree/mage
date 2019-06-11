cea541e2ac599df00ee5e830de5a8b6d75d29885
Evan Kranzler <theelk801@gmail.com>
2019-06-11 13:40:31 -0400

    small fix to temporarily prevent test fails

--- Mage.Sets/src/mage/sets/CoreSet2020.java
+++ Mage.Sets/src/mage/sets/CoreSet2020.java
@@ -1,11 +1,7 @@
 package mage.sets;
 
 import mage.cards.ExpansionSet;
-import mage.cards.repository.CardInfo;
 import mage.constants.SetType;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author TheElk801
@@ -18,13 +14,10 @@ public final class CoreSet2020 extends ExpansionSet {
         return instance;
     }
 
-    List<CardInfo> savedSpecialCommon = new ArrayList<>();
-    protected final List<CardInfo> savedSpecialLand = new ArrayList<>();
-
     private CoreSet2020() {
         super("Core Set 2020", "M20", ExpansionSet.buildDate(2019, 7, 12), SetType.CORE);
         this.hasBoosters = true;
-        this.hasBasicLands = true;
+        this.hasBasicLands = false;
         this.numBoosterSpecial = 0;
         this.numBoosterLands = 1;
         this.numBoosterCommon = 10;
