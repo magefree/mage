c4fc04f89c6f7b91b3098bbeb23d30694539b2e2
Evan Kranzler <theelk801@gmail.com>
2019-06-17 18:10:53 -0400

    Implemented Corpse Knight

---
+++ Mage.Sets/src/mage/cards/c/CorpseKnight.java
@@ -0,0 +1,47 @@
+package mage.cards.c;
+
+import mage.MageInt;
+import mage.abilities.common.EntersBattlefieldControlledTriggeredAbility;
+import mage.abilities.effects.common.LoseLifeOpponentsEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.filter.FilterPermanent;
+import mage.filter.common.FilterCreaturePermanent;
+import mage.filter.predicate.permanent.AnotherPredicate;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class CorpseKnight extends CardImpl {
+
+    private static final FilterPermanent filter = new FilterCreaturePermanent("another creature");
+
+    static {
+        filter.add(AnotherPredicate.instance);
+    }
+
+    public CorpseKnight(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{W}{B}");
+
+        this.subtype.add(SubType.ZOMBIE);
+        this.subtype.add(SubType.KNIGHT);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(2);
+
+        // Whenever another creature enters the battlefield under your control, each opponent loses 1 life.
+        this.addAbility(new EntersBattlefieldControlledTriggeredAbility(new LoseLifeOpponentsEffect(1), filter));
+    }
+
+    private CorpseKnight(final CorpseKnight card) {
+        super(card);
+    }
+
+    @Override
+    public CorpseKnight copy() {
+        return new CorpseKnight(this);
+    }
+}
