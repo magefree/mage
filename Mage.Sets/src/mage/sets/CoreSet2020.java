7e9ec1fb4af65f875a0ccc14878318ca17ca65c8
Evan Kranzler <theelk801@gmail.com>
2019-06-17 17:50:32 -0400

    Implemented Octoprophet

--- Mage.Sets/src/mage/sets/CoreSet2020.java
+++ Mage.Sets/src/mage/sets/CoreSet2020.java
@@ -40,6 +40,7 @@ public final class CoreSet2020 extends ExpansionSet {
         cards.add(new SetCardInfo("Empyrean Eagle", 208, Rarity.UNCOMMON, mage.cards.e.EmpyreanEagle.class));
         cards.add(new SetCardInfo("Infuriate", 145, Rarity.COMMON, mage.cards.i.Infuriate.class));
         cards.add(new SetCardInfo("Leyline of Sanctity", 26, Rarity.RARE, mage.cards.l.LeylineOfSanctity.class));
+        cards.add(new SetCardInfo("Octoprophet", 70, Rarity.COMMON, mage.cards.o.Octoprophet.class));
         cards.add(new SetCardInfo("Unsummon", 78, Rarity.COMMON, mage.cards.u.Unsummon.class));
     }
 }
