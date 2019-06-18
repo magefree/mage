5b2bd556bd34844daa029f3211c02530acf008c2
Evan Kranzler <theelk801@gmail.com>
2019-06-18 19:32:17 -0400

    Implemented Captivating Gyre

---
+++ Mage.Sets/src/mage/cards/c/CaptivatingGyre.java
@@ -0,0 +1,32 @@
+package mage.cards.c;
+
+import mage.abilities.effects.common.ReturnToHandTargetEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.target.common.TargetCreaturePermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class CaptivatingGyre extends CardImpl {
+
+    public CaptivatingGyre(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{4}{U}{U}");
+
+        // Return up to three target creatures to their owners' hands.
+        this.getSpellAbility().addEffect(new ReturnToHandTargetEffect(true));
+        this.getSpellAbility().addTarget(new TargetCreaturePermanent(0, 3));
+    }
+
+    private CaptivatingGyre(final CaptivatingGyre card) {
+        super(card);
+    }
+
+    @Override
+    public CaptivatingGyre copy() {
+        return new CaptivatingGyre(this);
+    }
+}
