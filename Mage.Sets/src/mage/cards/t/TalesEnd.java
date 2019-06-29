e2030aead9c0882498eaed3eec88194b7958e1bd
Oleg Agafonov <jaydi85@gmail.com>
2019-06-29 18:04:15 +0400

    Merge fix

--- Mage.Sets/src/mage/cards/t/TalesEnd.java
+++ Mage.Sets/src/mage/cards/t/TalesEnd.java
@@ -1,25 +1,22 @@
 package mage.cards.t;
 
-import java.util.UUID;
-
 import mage.abilities.effects.common.CounterTargetEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.target.common.TargetActivatedOrTriggeredAbilityOrLegendarySpell;
 
+import java.util.UUID;
+
 /**
- *
  * @author rscoates
  */
 public final class TalesEnd extends CardImpl {
 
     public TalesEnd(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{1}{U}");
-        
 
         // Counter target activated ability, triggered ability, or legendary spell.
-        // Counter target activated or triggered ability.
         this.getSpellAbility().addEffect(new CounterTargetEffect());
         this.getSpellAbility().addTarget(new TargetActivatedOrTriggeredAbilityOrLegendarySpell());
     }
