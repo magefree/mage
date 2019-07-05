cdf7f6df6ce7cef2e1812efbce792c4739ee0e82
Oleg Agafonov <jaydi85@gmail.com>
2019-07-05 13:55:10 +0400

    * Goad abilities - fixed missing text;

--- Mage/src/main/java/mage/abilities/effects/common/combat/GoadTargetEffect.java
+++ Mage/src/main/java/mage/abilities/effects/common/combat/GoadTargetEffect.java
@@ -1,4 +1,3 @@
-
 package mage.abilities.effects.common.combat;
 
 import mage.abilities.Ability;
@@ -12,20 +11,20 @@ import mage.players.Player;
 import mage.target.targetpointer.FixedTarget;
 
 /**
- *
  * @author TheElk801
  */
 public class GoadTargetEffect extends OneShotEffect {
 
     /**
      * 701.36. Goad
-     *
+     * <p>
      * 701.36a Certain spells and abilities can goad a creature. Until the next
      * turn of the controller of that spell or ability, that creature attacks
      * each combat if able and attacks a player other than that player if able.
      */
     public GoadTargetEffect() {
-        super(Outcome.Benefit);
+        super(Outcome.Detriment);
+        staticText = "you may goad target creature. <i>(Until your next turn, that creature attacks each combat if able and attacks a player other than you if able.)</i>";
     }
 
     public GoadTargetEffect(final GoadTargetEffect effect) {
@@ -42,6 +41,14 @@ public class GoadTargetEffect extends OneShotEffect {
         Permanent targetCreature = game.getPermanent(getTargetPointer().getFirst(game, source));
         Player controller = game.getPlayer(source.getControllerId());
         if (targetCreature != null && controller != null) {
+            // TODO: impoves goad to allows to target controller, current AttacksIfAbleTargetEffect is not support it
+            // https://github.com/magefree/mage/issues/5283
+            /*
+            If the creature doesn’t meet any of the above exceptions and can attack, it must attack a player other than
+            the controller of the spell or ability that goaded it if able. It the creature can’t attack any of those
+            players but could otherwise attack, it must attack an opposing planeswalker (controlled by any opponent)
+            or the player that goaded it. (2016-08-23)
+             */
             ContinuousEffect effect = new AttacksIfAbleTargetEffect(Duration.UntilYourNextTurn);
             effect.setTargetPointer(new FixedTarget(getTargetPointer().getFirst(game, source)));
             game.addEffect(effect, source);
