b5e65ba34aed1b5849e95a5567bf90e0980f4b68
Evan Kranzler <theelk801@gmail.com>
2019-07-08 16:35:07 -0400

    updated Commander ban list

--- Mage.Server.Plugins/Mage.Deck.Constructed/src/mage/deck/Commander.java
+++ Mage.Server.Plugins/Mage.Deck.Constructed/src/mage/deck/Commander.java
@@ -43,6 +43,7 @@ public class Commander extends Constructed {
         banned.add("Fastbond");
         banned.add("Gifts Ungiven");
         banned.add("Griselbrand");
+        banned.add("Iona, Shield of Emeria");
         banned.add("Karakas");
         banned.add("Leovold, Emissary of Trest");
         banned.add("Library of Alexandria");
@@ -52,8 +53,8 @@ public class Commander extends Constructed {
         banned.add("Mox Pearl");
         banned.add("Mox Ruby");
         banned.add("Mox Sapphire");
-        banned.add("Painter's Servant");
         banned.add("Panoptic Mirror");
+        banned.add("Paradox Engine");
         banned.add("Primeval Titan");
         banned.add("Prophet of Kruphix");
         banned.add("Recurring Nightmare");
