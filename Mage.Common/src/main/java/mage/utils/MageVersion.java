683f0fd3fe243264342b3a9301dac96dca7368e2
Oleg Agafonov <jaydi85@gmail.com>
2019-07-13 18:47:57 +0400

    Prepare hotfix

--- Mage.Common/src/main/java/mage/utils/MageVersion.java
+++ Mage.Common/src/main/java/mage/utils/MageVersion.java
@@ -13,7 +13,7 @@ public class MageVersion implements Serializable, Comparable<MageVersion> {
     public static final int MAGE_VERSION_MINOR = 4;
     public static final int MAGE_VERSION_PATCH = 37;
     public static final String MAGE_EDITION_INFO = ""; // set "-beta" for 1.4.32-betaV0
-    public static final String MAGE_VERSION_MINOR_PATCH = "V3"; // default
+    public static final String MAGE_VERSION_MINOR_PATCH = "V4"; // default
     // strict mode
     private static final boolean MAGE_VERSION_MINOR_PATCH_MUST_BE_SAME = false; // set true on uncompatible github changes, set false after new major release (after MAGE_VERSION_PATCH changes)
 
