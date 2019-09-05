53d20d79098282f4cdba140436963071548690d7
jmharmon <37360760+jmharmon@users.noreply.github.com>
2019-09-05 12:47:28 -0700

    Fix text on Viridian Claw

--- Mage.Sets/src/mage/cards/v/ViridianClaw.java
+++ Mage.Sets/src/mage/cards/v/ViridianClaw.java
@@ -3,6 +3,8 @@
 package mage.cards.v;
 
 import java.util.UUID;
+
+import mage.abilities.Ability;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.costs.mana.GenericManaCost;
 import mage.abilities.effects.common.continuous.BoostEquippedEffect;
@@ -26,9 +28,16 @@ public final class ViridianClaw extends CardImpl {
     public ViridianClaw (UUID ownerId, CardSetInfo setInfo) {
         super(ownerId,setInfo,new CardType[]{CardType.ARTIFACT},"{2}");
         this.subtype.add(SubType.EQUIPMENT);
+
+        // Equipped creature gets +1/+0 and has first strike.
+        Ability ability = new SimpleStaticAbility(new BoostEquippedEffect(1, 0));
+        ability.addEffect(new GainAbilityAttachedEffect(
+                FirstStrikeAbility.getInstance(), AttachmentType.EQUIPMENT).setText("and has first strike")
+        );
+        this.addAbility(ability);
+
+        // Equip {1}
         this.addAbility(new EquipAbility(Outcome.AddAbility, new GenericManaCost(1)));
-        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new BoostEquippedEffect(1, 0)));
-        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new GainAbilityAttachedEffect(FirstStrikeAbility.getInstance(), AttachmentType.EQUIPMENT)));
     }
 
     public ViridianClaw (final ViridianClaw card) {
