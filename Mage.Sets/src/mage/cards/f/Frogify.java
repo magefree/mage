67ea74a11e35da52cece31fd880035f6f70b14b1
Evan Kranzler <theelk801@gmail.com>
2019-09-07 08:36:45 -0400

    Implemented Frogify

---
+++ Mage.Sets/src/mage/cards/f/Frogify.java
@@ -0,0 +1,53 @@
+package mage.cards.f;
+
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.common.AttachEffect;
+import mage.abilities.effects.common.continuous.BecomesCreatureAttachedEffect;
+import mage.abilities.keyword.EnchantAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Outcome;
+import mage.constants.SubType;
+import mage.game.permanent.token.custom.CreatureToken;
+import mage.target.TargetPermanent;
+import mage.target.common.TargetCreaturePermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class Frogify extends CardImpl {
+
+    public Frogify(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{1}{U}");
+
+        this.subtype.add(SubType.AURA);
+
+        // Enchant creature
+        TargetPermanent auraTarget = new TargetCreaturePermanent();
+        this.getSpellAbility().addTarget(auraTarget);
+        this.getSpellAbility().addEffect(new AttachEffect(Outcome.BoostCreature));
+        Ability ability = new EnchantAbility(auraTarget.getTargetName());
+        this.addAbility(ability);
+
+        // Enchanted creature loses all abilities and is a blue Frog creature with base power and toughness 1/1.
+        this.addAbility(new SimpleStaticAbility(new BecomesCreatureAttachedEffect(
+                new CreatureToken(1, 1, "", SubType.FROG).withColor("U"),
+                "Enchanted creature loses all abilities and is a blue Frog creature with base power and toughness 1/1",
+                Duration.WhileOnBattlefield, BecomesCreatureAttachedEffect.LoseType.ALL
+        )));
+    }
+
+    private Frogify(final Frogify card) {
+        super(card);
+    }
+
+    @Override
+    public Frogify copy() {
+        return new Frogify(this);
+    }
+}
