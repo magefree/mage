71fd51ec2b746d35486448e5b3967ab2281e1f10
Evan Kranzler <theelk801@gmail.com>
2019-09-10 17:20:17 -0400

    Implemented Weaselback Redcap

---
+++ Mage.Sets/src/mage/cards/w/WeaselbackRedcap.java
@@ -0,0 +1,42 @@
+package mage.cards.w;
+
+import mage.MageInt;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.continuous.BoostSourceEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.SubType;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class WeaselbackRedcap extends CardImpl {
+
+    public WeaselbackRedcap(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{R}");
+
+        this.subtype.add(SubType.GOBLIN);
+        this.subtype.add(SubType.KNIGHT);
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(1);
+
+        // {1}{R}: Weaselback Redcap gets +2/+0 until end of turn.
+        this.addAbility(new SimpleActivatedAbility(
+                new BoostSourceEffect(2, 0, Duration.EndOfTurn), new ManaCostsImpl("{1}{R}")
+        ));
+    }
+
+    private WeaselbackRedcap(final WeaselbackRedcap card) {
+        super(card);
+    }
+
+    @Override
+    public WeaselbackRedcap copy() {
+        return new WeaselbackRedcap(this);
+    }
+}
