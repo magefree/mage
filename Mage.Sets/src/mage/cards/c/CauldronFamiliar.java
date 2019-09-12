5c58b605a27fe905388812de69901b9aeb329b98
Evan Kranzler <theelk801@gmail.com>
2019-09-12 07:59:50 -0400

    Implemented Cauldron Familiar

---
+++ Mage.Sets/src/mage/cards/c/CauldronFamiliar.java
@@ -0,0 +1,55 @@
+package mage.cards.c;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.SacrificeTargetCost;
+import mage.abilities.effects.common.GainLifeEffect;
+import mage.abilities.effects.common.LoseLifeOpponentsEffect;
+import mage.abilities.effects.common.ReturnSourceFromGraveyardToBattlefieldEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.constants.Zone;
+import mage.filter.common.FilterControlledPermanent;
+import mage.target.common.TargetControlledPermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class CauldronFamiliar extends CardImpl {
+
+    private static final FilterControlledPermanent filter = new FilterControlledPermanent(SubType.FOOD, "a Food");
+
+    public CauldronFamiliar(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{B}");
+
+        this.subtype.add(SubType.CAT);
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(1);
+
+        // When Cauldron Familiar enters the battlefield, each opponent loses 1 life and you gain 1 life.
+        Ability ability = new EntersBattlefieldTriggeredAbility(new LoseLifeOpponentsEffect(1));
+        ability.addEffect(new GainLifeEffect(1).concatBy("and"));
+        this.addAbility(ability);
+
+        // Sacrifice a Food: Return Cauldron Familiar from your graveyard to the battlefield.
+        this.addAbility(new SimpleActivatedAbility(
+                Zone.GRAVEYARD, new ReturnSourceFromGraveyardToBattlefieldEffect(),
+                new SacrificeTargetCost(new TargetControlledPermanent(filter))
+        ));
+    }
+
+    private CauldronFamiliar(final CauldronFamiliar card) {
+        super(card);
+    }
+
+    @Override
+    public CauldronFamiliar copy() {
+        return new CauldronFamiliar(this);
+    }
+}
--- Mage.Sets/src/mage/sets/ThroneOfEldraine.java
+++ Mage.Sets/src/mage/sets/ThroneOfEldraine.java
@@ -43,6 +43,7 @@ public final class ThroneOfEldraine extends ExpansionSet {
         cards.add(new SetCardInfo("Bramblefort Fink", 311, Rarity.UNCOMMON, mage.cards.b.BramblefortFink.class));
         cards.add(new SetCardInfo("Brimstone Trebuchet", 116, Rarity.COMMON, mage.cards.b.BrimstoneTrebuchet.class));
         cards.add(new SetCardInfo("Burning-Yard Trainer", 117, Rarity.UNCOMMON, mage.cards.b.BurningYardTrainer.class));
+        cards.add(new SetCardInfo("Cauldron Familiar", 81, Rarity.UNCOMMON, mage.cards.c.CauldronFamiliar.class));
         cards.add(new SetCardInfo("Charming Prince", 8, Rarity.RARE, mage.cards.c.CharmingPrince.class));
         cards.add(new SetCardInfo("Chittering Witch", 319, Rarity.RARE, mage.cards.c.ChitteringWitch.class));
         cards.add(new SetCardInfo("Chulane, Teller of Tales", 326, Rarity.MYTHIC, mage.cards.c.ChulaneTellerOfTales.class));
