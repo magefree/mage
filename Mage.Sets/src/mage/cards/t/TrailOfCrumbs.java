99c884eca1b78b8ab394381ca05eb5b427e0213b
Evan Kranzler <theelk801@gmail.com>
2019-09-12 17:35:55 -0400

    Implemented Trail of Crumbs

---
+++ Mage.Sets/src/mage/cards/t/TrailOfCrumbs.java
@@ -0,0 +1,86 @@
+package mage.cards.t;
+
+import mage.abilities.TriggeredAbilityImpl;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.costs.mana.GenericManaCost;
+import mage.abilities.effects.common.CreateTokenEffect;
+import mage.abilities.effects.common.DoIfCostPaid;
+import mage.abilities.effects.common.LookLibraryAndPickControllerEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.constants.Zone;
+import mage.filter.FilterCard;
+import mage.filter.common.FilterPermanentCard;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.permanent.Permanent;
+import mage.game.permanent.token.FoodToken;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class TrailOfCrumbs extends CardImpl {
+
+    public TrailOfCrumbs(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{1}{G}");
+
+        // When Trail of Crumbs enters the battlefield, create a Food token.
+        this.addAbility(new EntersBattlefieldTriggeredAbility(new CreateTokenEffect(new FoodToken())));
+
+        // Whenever you sacrifice a Food, you may pay {1}. If you do, look at the top two cards of your library. You may reveal a permanent card from among them and put it into your hand. Put the rest on the bottom of your library in any order.
+        this.addAbility(new TrailOfCrumbsTriggeredAbility());
+    }
+
+    private TrailOfCrumbs(final TrailOfCrumbs card) {
+        super(card);
+    }
+
+    @Override
+    public TrailOfCrumbs copy() {
+        return new TrailOfCrumbs(this);
+    }
+}
+
+class TrailOfCrumbsTriggeredAbility extends TriggeredAbilityImpl {
+
+    private static final FilterCard filter = new FilterPermanentCard();
+
+    TrailOfCrumbsTriggeredAbility() {
+        super(Zone.BATTLEFIELD, new DoIfCostPaid(new LookLibraryAndPickControllerEffect(
+                2, 1, filter, true, true, Zone.HAND, true
+        ), new GenericManaCost(1)));
+    }
+
+    private TrailOfCrumbsTriggeredAbility(final TrailOfCrumbsTriggeredAbility ability) {
+        super(ability);
+    }
+
+    @Override
+    public TrailOfCrumbsTriggeredAbility copy() {
+        return new TrailOfCrumbsTriggeredAbility(this);
+    }
+
+    @Override
+    public boolean checkEventType(GameEvent event, Game game) {
+        return event.getType() == GameEvent.EventType.SACRIFICED_PERMANENT;
+    }
+
+    @Override
+    public boolean checkTrigger(GameEvent event, Game game) {
+        Permanent permanent = game.getPermanentOrLKIBattlefield(event.getTargetId());
+        return permanent != null
+                && event.getPlayerId().equals(this.getControllerId())
+                && permanent.hasSubtype(SubType.FOOD, game);
+    }
+
+    @Override
+    public String getRule() {
+        return "Whenever you sacrifice a Food, you may pay {1}. If you do, " +
+                "look at the top two cards of your library. You may reveal a permanent card from among them " +
+                "and put it into your hand. Put the rest on the bottom of your library in any order.";
+    }
+}
--- Mage.Sets/src/mage/sets/ThroneOfEldraine.java
+++ Mage.Sets/src/mage/sets/ThroneOfEldraine.java
@@ -160,6 +160,7 @@ public final class ThroneOfEldraine extends ExpansionSet {
         cards.add(new SetCardInfo("Tome Raider", 68, Rarity.COMMON, mage.cards.t.TomeRaider.class));
         cards.add(new SetCardInfo("Tome of Legends", 332, Rarity.RARE, mage.cards.t.TomeOfLegends.class));
         cards.add(new SetCardInfo("Tournament Grounds", 248, Rarity.UNCOMMON, mage.cards.t.TournamentGrounds.class));
+        cards.add(new SetCardInfo("Trail of Crumbs", 179, Rarity.UNCOMMON, mage.cards.t.TrailOfCrumbs.class));
         cards.add(new SetCardInfo("True Love's Kiss", 34, Rarity.COMMON, mage.cards.t.TrueLovesKiss.class));
         cards.add(new SetCardInfo("Turn into a Pumpkin", 69, Rarity.UNCOMMON, mage.cards.t.TurnIntoAPumpkin.class));
         cards.add(new SetCardInfo("Venerable Knight", 35, Rarity.UNCOMMON, mage.cards.v.VenerableKnight.class));
