9604aa9bef9964a32ccfe4f7b9a11630d09c4bdd
Evan Kranzler <theelk801@gmail.com>
2019-09-13 08:12:09 -0400

    Implemented Wolf's Quarry

---
+++ Mage/src/main/java/mage/game/permanent/token/WolfsQuarryToken.java
@@ -0,0 +1,32 @@
+package mage.game.permanent.token;
+
+import mage.MageInt;
+import mage.abilities.common.DiesTriggeredAbility;
+import mage.abilities.effects.common.CreateTokenEffect;
+import mage.constants.CardType;
+import mage.constants.SubType;
+
+/**
+ * @author TheElk801
+ */
+public final class WolfsQuarryToken extends TokenImpl {
+
+    public WolfsQuarryToken() {
+        super("Boar", "1/1 green Boar creature token with \"When this creature dies, create a Food token.\"");
+        cardType.add(CardType.CREATURE);
+        color.setGreen(true);
+        subtype.add(SubType.BOAR);
+        power = new MageInt(1);
+        toughness = new MageInt(1);
+
+        this.addAbility(new DiesTriggeredAbility(new CreateTokenEffect(new FoodToken())));
+    }
+
+    private WolfsQuarryToken(final WolfsQuarryToken token) {
+        super(token);
+    }
+
+    public WolfsQuarryToken copy() {
+        return new WolfsQuarryToken(this);
+    }
+}
