5f20674d09f5ee1317b69ef74400e51a2b02ee5b
Matthaios Tsiridis <newtone.3g@gmail.com>
2019-09-15 18:36:10 +0300

    Fixed Inspiring Veteran where there was a bug that he would Give +1/+1 to himself as well.

--- Mage.Sets/src/mage/cards/i/InspiringVeteran.java
+++ Mage.Sets/src/mage/cards/i/InspiringVeteran.java
@@ -2,13 +2,19 @@ package mage.cards.i;
 
 import mage.MageInt;
 import mage.abilities.common.SimpleStaticAbility;
-import mage.abilities.effects.common.continuous.BoostControlledEffect;
+import mage.abilities.effects.common.continuous.BoostAllEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.Duration;
 import mage.constants.SubType;
+import mage.constants.TargetController;
 import mage.filter.common.FilterCreaturePermanent;
+import mage.filter.predicate.permanent.ControllerPredicate;
+import mage.constants.Zone;
+import mage.filter.predicate.mageobject.SubtypePredicate;
+
+
 
 import java.util.UUID;
 
@@ -20,6 +26,12 @@ public final class InspiringVeteran extends CardImpl {
     private static final FilterCreaturePermanent filter
             = new FilterCreaturePermanent(SubType.KNIGHT, "Knights");
 
+            static
+            {
+                filter.add(new SubtypePredicate(SubType.KNIGHT));
+                filter.add(new ControllerPredicate(TargetController.YOU));
+            }
+
     public InspiringVeteran(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{R}{W}");
 
@@ -29,9 +41,7 @@ public final class InspiringVeteran extends CardImpl {
         this.toughness = new MageInt(2);
 
         // Other Knights you control get +1/+1.
-        this.addAbility(new SimpleStaticAbility(
-                new BoostControlledEffect(1, 1, Duration.WhileOnBattlefield, filter)
-        ));
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new BoostAllEffect(1, 1, Duration.WhileOnBattlefield, filter, true)));
     }
 
     private InspiringVeteran(final InspiringVeteran card) {
