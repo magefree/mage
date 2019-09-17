ca14b2fe507017e93d32e8ce26aa3ede1f1729ef
Evan Kranzler <theelk801@gmail.com>
2019-09-16 20:12:10 -0400

    Implemented Syr Alin, the Lion's Claw

---
+++ Mage.Sets/src/mage/cards/s/SyrAlinTheLionsClaw.java
@@ -0,0 +1,47 @@
+package mage.cards.s;
+
+import mage.MageInt;
+import mage.abilities.common.AttacksTriggeredAbility;
+import mage.abilities.effects.common.continuous.BoostControlledEffect;
+import mage.abilities.keyword.FirstStrikeAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.SubType;
+import mage.constants.SuperType;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class SyrAlinTheLionsClaw extends CardImpl {
+
+    public SyrAlinTheLionsClaw(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{3}{W}{W}");
+
+        this.addSuperType(SuperType.LEGENDARY);
+        this.subtype.add(SubType.HUMAN);
+        this.subtype.add(SubType.KNIGHT);
+        this.power = new MageInt(4);
+        this.toughness = new MageInt(4);
+
+        // First strike
+        this.addAbility(FirstStrikeAbility.getInstance());
+
+        // Whenever Syr Alin, the Lion's Claw attacks, other creatures you control get +1/+1 until end of turn.
+        this.addAbility(new AttacksTriggeredAbility(new BoostControlledEffect(
+                1, 1, Duration.EndOfTurn, true
+        ), false));
+    }
+
+    private SyrAlinTheLionsClaw(final SyrAlinTheLionsClaw card) {
+        super(card);
+    }
+
+    @Override
+    public SyrAlinTheLionsClaw copy() {
+        return new SyrAlinTheLionsClaw(this);
+    }
+}
