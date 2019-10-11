00fd8d18dae845e80e6bb0ab73004d0757a282e3
Adrian Petrescu <adrian@apetre.sc>
2019-10-11 09:59:54 -0400

    Remove unneeded import in an unrelated test, just for kicks

--- Mage.Tests/src/test/java/org/mage/test/cards/single/ArchfiendOfSpiteTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/ArchfiendOfSpiteTest.java
@@ -2,7 +2,6 @@ package org.mage.test.cards.single;
 
 import mage.constants.PhaseStep;
 import mage.constants.Zone;
-import org.junit.Assert;
 import org.junit.Test;
 import org.mage.test.serverside.base.CardTestPlayerBase;
 
