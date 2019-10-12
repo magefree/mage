811ab5d69ab8bfc52d238800ec72c2cf4c83da37
Evan Kranzler <theelk801@gmail.com>
2019-10-12 10:40:52 -0400

    Implemented Sphinx of Enlightenment

--- Mage.Sets/src/mage/sets/GameNight2019.java
+++ Mage.Sets/src/mage/sets/GameNight2019.java
@@ -1,6 +1,7 @@
 package mage.sets;
 
 import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
 import mage.constants.SetType;
 
 /**
@@ -17,5 +18,7 @@ public final class GameNight2019 extends ExpansionSet {
     private GameNight2019() {
         super("Game Night 2019", "GN2", ExpansionSet.buildDate(2019, 11, 15), SetType.SUPPLEMENTAL);
         this.hasBasicLands = false; // TODO: change when spoiled
+
+        cards.add(new SetCardInfo("Sphinx of Enlightenment", 2, Rarity.MYTHIC, mage.cards.s.SphinxOfEnlightenment.class));
     }
 }
