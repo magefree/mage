23324f21693d2a951dc98bb55cc3591c3a235e68
Evan Kranzler <theelk801@gmail.com>
2019-12-11 19:39:51 -0500

    Added Theros: Beyond Death

---
+++ Mage.Sets/src/mage/sets/TherosBeyondDeath.java
@@ -0,0 +1,35 @@
+package mage.sets;
+
+import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
+import mage.constants.SetType;
+
+/**
+ * @author TheElk801
+ */
+public final class TherosBeyondDeath extends ExpansionSet {
+
+    private static final TherosBeyondDeath instance = new TherosBeyondDeath();
+
+    public static TherosBeyondDeath getInstance() {
+        return instance;
+    }
+
+    private TherosBeyondDeath() {
+        super("Theros: Beyond Death", "THB", ExpansionSet.buildDate(2020, 1, 24), SetType.EXPANSION);
+        this.blockName = "Theros: Beyond Death";
+        this.hasBoosters = true;
+        this.numBoosterLands = 1;
+        this.numBoosterCommon = 10;
+        this.numBoosterUncommon = 3;
+        this.numBoosterRare = 1;
+        this.ratioBoosterMythic = 8;
+        this.maxCardNumberInBooster = 254;
+
+        cards.add(new SetCardInfo("Forest", 254, Rarity.LAND, mage.cards.basiclands.Forest.class, FULL_ART_BFZ_VARIOUS));
+        cards.add(new SetCardInfo("Island", 251, Rarity.LAND, mage.cards.basiclands.Island.class, FULL_ART_BFZ_VARIOUS));
+        cards.add(new SetCardInfo("Mountain", 253, Rarity.LAND, mage.cards.basiclands.Mountain.class, FULL_ART_BFZ_VARIOUS));
+        cards.add(new SetCardInfo("Plains", 250, Rarity.LAND, mage.cards.basiclands.Plains.class, FULL_ART_BFZ_VARIOUS));
+        cards.add(new SetCardInfo("Swamp", 252, Rarity.LAND, mage.cards.basiclands.Swamp.class, FULL_ART_BFZ_VARIOUS));
+    }
+}
