9f19866435f0975d7fb5f13f26c028c56244b934
Evan Kranzler <theelk801@gmail.com>
2019-12-16 21:13:15 -0500

    Implemented The Akroan War

--- Mage.Sets/src/mage/sets/TherosBeyondDeath.java
+++ Mage.Sets/src/mage/sets/TherosBeyondDeath.java
@@ -49,5 +49,6 @@ public final class TherosBeyondDeath extends ExpansionSet {
         cards.add(new SetCardInfo("Setessan Champion", 198, Rarity.RARE, mage.cards.s.SetessanChampion.class));
         cards.add(new SetCardInfo("Staggering Insight", 228, Rarity.UNCOMMON, mage.cards.s.StaggeringInsight.class));
         cards.add(new SetCardInfo("Swamp", 252, Rarity.LAND, mage.cards.basiclands.Swamp.class, FULL_ART_BFZ_VARIOUS));
+        cards.add(new SetCardInfo("The Akroan War", 124, Rarity.RARE, mage.cards.t.TheAkroanWar.class));
     }
 }
