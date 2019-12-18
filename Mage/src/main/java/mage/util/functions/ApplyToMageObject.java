4ef9a75fe777f499f7a1f9820286e551ac1eeee8
jeffwadsworth <jeff@delmarus.com>
2019-12-18 15:41:08 -0600

    - Fixed #6101

--- Mage/src/main/java/mage/util/functions/ApplyToMageObject.java
+++ Mage/src/main/java/mage/util/functions/ApplyToMageObject.java
@@ -3,7 +3,6 @@ package mage.util.functions;
 import mage.MageObject;
 import mage.abilities.Ability;
 import mage.game.Game;
-
 import java.util.Objects;
 import java.util.UUID;
 
@@ -14,11 +13,11 @@ public abstract class ApplyToMageObject {
 
     // WARNING:
     // 1. Applier uses for copy effects only;
-    // 2. It's applies to blueprint, not real object (real object is targetObjectId and can be card or token, even outside from game like EmptyToken);
-    // 3. "source" is current copy ability and can be different from original copy ability (copy of copy);
+    // 2. It applies to the blueprint, not the real object (the real object is targetObjectId and can be card or token, even from outside the game like EmptyToken);
+    // 3. "source" is the current copy ability and can be different from the original copy ability (copy of copy);
     // 4. Don't use "source" param at all;
-    // 5. Use isCopyOfCopy() to detect it (some effects can applies to copy of copy, but other can't -- ses Spark Double as example).
-    // TODO: check all aplliers implementations - remove source uses, add isCopyOfCopy processing
+    // 5. Use isCopyOfCopy() to detect it (some effects can apply to copy of copy, but others can't -- see Spark Double as an example).
+    // TODO: check all applier implementations - remove source uses, add isCopyOfCopy processing
     public abstract boolean apply(Game game, MageObject mageObject, Ability source, UUID targetObjectId);
 
     public boolean isCopyOfCopy(Ability source, UUID targetObjectId) {
