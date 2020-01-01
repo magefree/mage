a89619373dda4dc70e6bec5c72175dfce09ed036
Evan Kranzler <theelk801@gmail.com>
2020-01-01 11:05:29 -0500

    Implemented Treacherous Blessing

---
+++ Mage.Sets/src/mage/cards/t/TreacherousBlessing.java
@@ -0,0 +1,43 @@
+package mage.cards.t;
+
+import mage.abilities.common.BecomesTargetTriggeredAbility;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.common.SpellCastControllerTriggeredAbility;
+import mage.abilities.effects.common.DrawCardSourceControllerEffect;
+import mage.abilities.effects.common.LoseLifeSourceControllerEffect;
+import mage.abilities.effects.common.SacrificeSourceEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class TreacherousBlessing extends CardImpl {
+
+    public TreacherousBlessing(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{2}{B}");
+
+        // When Treacherous Blessing enters the battlefield, draw three cards.
+        this.addAbility(new EntersBattlefieldTriggeredAbility(new DrawCardSourceControllerEffect(3)));
+
+        // Whenever you cast a spell, you lose 1 life.
+        this.addAbility(new SpellCastControllerTriggeredAbility(
+                new LoseLifeSourceControllerEffect(1), false
+        ));
+
+        // When Treacherous Blessing becomes the target of a spell or ability, sacrifice it.
+        this.addAbility(new BecomesTargetTriggeredAbility(new SacrificeSourceEffect()));
+    }
+
+    private TreacherousBlessing(final TreacherousBlessing card) {
+        super(card);
+    }
+
+    @Override
+    public TreacherousBlessing copy() {
+        return new TreacherousBlessing(this);
+    }
+}
