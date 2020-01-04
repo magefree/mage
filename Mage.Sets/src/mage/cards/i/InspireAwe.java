cc2e42dc00aa3449182af8b486bac362d70c4a72
Evan Kranzler <theelk801@gmail.com>
2020-01-03 22:03:43 -0500

    fixed Inspire Awe implementation

--- Mage.Sets/src/mage/cards/i/InspireAwe.java
+++ Mage.Sets/src/mage/cards/i/InspireAwe.java
@@ -7,6 +7,7 @@ import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.Duration;
 import mage.filter.common.FilterCreaturePermanent;
+import mage.filter.predicate.Predicates;
 import mage.filter.predicate.mageobject.EnchantmentOrEnchantedPredicate;
 
 import java.util.UUID;
@@ -16,11 +17,10 @@ import java.util.UUID;
  */
 public final class InspireAwe extends CardImpl {
 
-    private static final FilterCreaturePermanent filter
-            = new FilterCreaturePermanent("enchanted creatures and enchantment creatures");
+    private static final FilterCreaturePermanent filter = new FilterCreaturePermanent();
 
     static {
-        filter.add(EnchantmentOrEnchantedPredicate.instance);
+        filter.add(Predicates.not(EnchantmentOrEnchantedPredicate.instance));
     }
 
     public InspireAwe(UUID ownerId, CardSetInfo setInfo) {
@@ -29,7 +29,8 @@ public final class InspireAwe extends CardImpl {
         // Prevent all combat damage that would be dealt this turn except by enchanted creatures and enchantment creatures. Scry 2.
         this.getSpellAbility().addEffect(new PreventAllDamageByAllPermanentsEffect(
                 filter, Duration.EndOfTurn, true
-        ));
+        ).setText("Prevent all combat damage that would be dealt this turn " +
+                "except by enchanted creatures and enchantment creatures."));
         this.getSpellAbility().addEffect(new ScryEffect(2));
     }
 
