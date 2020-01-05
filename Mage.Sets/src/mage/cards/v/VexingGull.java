8406a76de05e616e438053e8fdec459c9f4b0f05
Evan Kranzler <theelk801@gmail.com>
2020-01-05 07:38:45 -0500

    Implemented Vexing Gull

---
+++ Mage.Sets/src/mage/cards/v/VexingGull.java
@@ -0,0 +1,40 @@
+package mage.cards.v;
+
+import mage.MageInt;
+import mage.abilities.keyword.FlashAbility;
+import mage.abilities.keyword.FlyingAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class VexingGull extends CardImpl {
+
+    public VexingGull(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{2}{U}");
+
+        this.subtype.add(SubType.BIRD);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(2);
+
+        // Flash
+        this.addAbility(FlashAbility.getInstance());
+
+        // Flying
+        this.addAbility(FlyingAbility.getInstance());
+    }
+
+    private VexingGull(final VexingGull card) {
+        super(card);
+    }
+
+    @Override
+    public VexingGull copy() {
+        return new VexingGull(this);
+    }
+}
