2f9d1af709b4b086c3f8d03f29aa707cd9abf1ae
Evan Kranzler <theelk801@gmail.com>
2020-01-06 18:39:15 -0500

    Implemented  Careless Celebrant

---
+++ Mage.Sets/src/mage/cards/c/CarelessCelebrant.java
@@ -0,0 +1,57 @@
+package mage.cards.c;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.DiesTriggeredAbility;
+import mage.abilities.effects.common.DamageTargetEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.constants.TargetController;
+import mage.filter.FilterPermanent;
+import mage.filter.predicate.Predicates;
+import mage.filter.predicate.permanent.ControllerPredicate;
+import mage.target.TargetPermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class CarelessCelebrant extends CardImpl {
+
+    private static final FilterPermanent filter
+            = new FilterPermanent("creature or planeswalker an opponent controls");
+
+    static {
+        filter.add(new ControllerPredicate(TargetController.OPPONENT));
+        filter.add(Predicates.or(
+                CardType.CREATURE.getPredicate(),
+                CardType.PLANESWALKER.getPredicate()
+        ));
+    }
+
+    public CarelessCelebrant(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{1}{R}");
+
+        this.subtype.add(SubType.SATYR);
+        this.subtype.add(SubType.SHAMAN);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(1);
+
+        // When Careless Celebrant dies, it deals 2 damage to target creature or planeswalker an opponent controls.
+        Ability ability = new DiesTriggeredAbility(new DamageTargetEffect(2, "it"));
+        ability.addTarget(new TargetPermanent(filter));
+        this.addAbility(ability);
+    }
+
+    private CarelessCelebrant(final CarelessCelebrant card) {
+        super(card);
+    }
+
+    @Override
+    public CarelessCelebrant copy() {
+        return new CarelessCelebrant(this);
+    }
+}
--- Mage.Sets/src/mage/sets/TherosBeyondDeath.java
+++ Mage.Sets/src/mage/sets/TherosBeyondDeath.java
@@ -39,6 +39,7 @@ public final class TherosBeyondDeath extends ExpansionSet {
         cards.add(new SetCardInfo("Banishing Light", 4, Rarity.UNCOMMON, mage.cards.b.BanishingLight.class));
         cards.add(new SetCardInfo("Blood Aspirant", 128, Rarity.UNCOMMON, mage.cards.b.BloodAspirant.class));
         cards.add(new SetCardInfo("Brine Giant", 44, Rarity.COMMON, mage.cards.b.BrineGiant.class));
+        cards.add(new SetCardInfo("Careless Celebrant", 129, Rarity.UNCOMMON, mage.cards.c.CarelessCelebrant.class));
         cards.add(new SetCardInfo("Chainweb Aracnir", 167, Rarity.UNCOMMON, mage.cards.c.ChainwebAracnir.class));
         cards.add(new SetCardInfo("Cling to Dust", 87, Rarity.UNCOMMON, mage.cards.c.ClingToDust.class));
         cards.add(new SetCardInfo("Commanding Presence", 7, Rarity.UNCOMMON, mage.cards.c.CommandingPresence.class));
