cab8724a7ddd822ceb51ee183f7f084a3104d68a
Evan Kranzler <theelk801@gmail.com>
2020-01-06 18:40:52 -0500

    Implemented Mantle of the Wolf

---
+++ Mage.Sets/src/mage/cards/m/MantleOfTheWolf.java
@@ -0,0 +1,53 @@
+package mage.cards.m;
+
+import mage.abilities.Ability;
+import mage.abilities.common.PutIntoGraveFromBattlefieldSourceTriggeredAbility;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.common.AttachEffect;
+import mage.abilities.effects.common.CreateTokenEffect;
+import mage.abilities.effects.common.continuous.BoostEnchantedEffect;
+import mage.abilities.keyword.EnchantAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Outcome;
+import mage.constants.SubType;
+import mage.game.permanent.token.WolfToken;
+import mage.target.TargetPermanent;
+import mage.target.common.TargetCreaturePermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class MantleOfTheWolf extends CardImpl {
+
+    public MantleOfTheWolf(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{3}{G}");
+
+        this.subtype.add(SubType.AURA);
+
+        // Enchant creature
+        TargetPermanent auraTarget = new TargetCreaturePermanent();
+        this.getSpellAbility().addTarget(auraTarget);
+        this.getSpellAbility().addEffect(new AttachEffect(Outcome.BoostCreature));
+        Ability ability = new EnchantAbility(auraTarget.getTargetName());
+        this.addAbility(ability);
+
+        // Enchanted creature gets +4/+4.
+        this.addAbility(new SimpleStaticAbility(new BoostEnchantedEffect(4, 4)));
+
+        // When Mantle of the Wolf is put into a graveyard from the battlefield, create two 2/2 green Wolf creature tokens.
+        this.addAbility(new PutIntoGraveFromBattlefieldSourceTriggeredAbility(new CreateTokenEffect(new WolfToken(), 2)));
+    }
+
+    private MantleOfTheWolf(final MantleOfTheWolf card) {
+        super(card);
+    }
+
+    @Override
+    public MantleOfTheWolf copy() {
+        return new MantleOfTheWolf(this);
+    }
+}
