b1bb698fbd7dc4d775e39b158e2fe45f841d84e8
Evan Kranzler <theelk801@gmail.com>
2020-01-06 21:46:04 -0500

    Implemented Fateful End

---
+++ Mage.Sets/src/mage/cards/f/FatefulEnd.java
@@ -0,0 +1,34 @@
+package mage.cards.f;
+
+import mage.abilities.effects.common.DamageTargetEffect;
+import mage.abilities.effects.keyword.ScryEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.target.common.TargetAnyTarget;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class FatefulEnd extends CardImpl {
+
+    public FatefulEnd(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{2}{R}");
+
+        // Fateful End deals 3 damage to any target. Scry 1.
+        this.getSpellAbility().addEffect(new DamageTargetEffect(3, true, "any target."));
+        this.getSpellAbility().addTarget(new TargetAnyTarget());
+        this.getSpellAbility().addEffect(new ScryEffect(1));
+    }
+
+    private FatefulEnd(final FatefulEnd card) {
+        super(card);
+    }
+
+    @Override
+    public FatefulEnd copy() {
+        return new FatefulEnd(this);
+    }
+}
