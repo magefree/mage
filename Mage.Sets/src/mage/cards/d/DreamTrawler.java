c73a3c5885fe03c0e7b876f0044b115e6328160a
Evan Kranzler <theelk801@gmail.com>
2020-01-09 13:00:41 -0500

    Implemented Dream Trawler

---
+++ Mage.Sets/src/mage/cards/d/DreamTrawler.java
@@ -0,0 +1,65 @@
+package mage.cards.d;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.AttacksTriggeredAbility;
+import mage.abilities.common.DrawCardControllerTriggeredAbility;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.DiscardCardCost;
+import mage.abilities.effects.common.DrawCardSourceControllerEffect;
+import mage.abilities.effects.common.TapSourceEffect;
+import mage.abilities.effects.common.continuous.BoostSourceEffect;
+import mage.abilities.effects.common.continuous.GainAbilitySourceEffect;
+import mage.abilities.keyword.FlyingAbility;
+import mage.abilities.keyword.HexproofAbility;
+import mage.abilities.keyword.LifelinkAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.SubType;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class DreamTrawler extends CardImpl {
+
+    public DreamTrawler(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{2}{W}{W}{U}{U}");
+
+        this.subtype.add(SubType.SPHINX);
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(5);
+
+        // Flying
+        this.addAbility(FlyingAbility.getInstance());
+
+        // Lifelink
+        this.addAbility(LifelinkAbility.getInstance());
+
+        // Whenever you draw a card, Dream Trawler gets +1/+0 until end of turn.
+        this.addAbility(new DrawCardControllerTriggeredAbility(
+                new BoostSourceEffect(1, 0, Duration.EndOfTurn), false
+        ));
+
+        // Whenever Dream Trawler attacks, draw a card.
+        this.addAbility(new AttacksTriggeredAbility(new DrawCardSourceControllerEffect(1), false));
+
+        // Discard a card: Dream Trawler gains hexproof until end of turn. Tap it.
+        Ability ability = new SimpleActivatedAbility(new GainAbilitySourceEffect(
+                HexproofAbility.getInstance(), Duration.EndOfTurn
+        ), new DiscardCardCost());
+        ability.addEffect(new TapSourceEffect().setText("Tap it"));
+    }
+
+    private DreamTrawler(final DreamTrawler card) {
+        super(card);
+    }
+
+    @Override
+    public DreamTrawler copy() {
+        return new DreamTrawler(this);
+    }
+}
--- Mage.Sets/src/mage/sets/TherosBeyondDeath.java
+++ Mage.Sets/src/mage/sets/TherosBeyondDeath.java
@@ -57,6 +57,7 @@ public final class TherosBeyondDeath extends ExpansionSet {
         cards.add(new SetCardInfo("Devourer of Memory", 213, Rarity.UNCOMMON, mage.cards.d.DevourerOfMemory.class));
         cards.add(new SetCardInfo("Discordant Piper", 88, Rarity.COMMON, mage.cards.d.DiscordantPiper.class));
         cards.add(new SetCardInfo("Drag to the Underworld", 89, Rarity.UNCOMMON, mage.cards.d.DragToTheUnderworld.class));
+        cards.add(new SetCardInfo("Dream Trawler", 214, Rarity.RARE, mage.cards.d.DreamTrawler.class));
         cards.add(new SetCardInfo("Dreamshaper Shaman", 130, Rarity.UNCOMMON, mage.cards.d.DreamshaperShaman.class));
         cards.add(new SetCardInfo("Dryad of the Ilysian Grove", 169, Rarity.RARE, mage.cards.d.DryadOfTheIlysianGrove.class));
         cards.add(new SetCardInfo("Eat to Extinction", 90, Rarity.RARE, mage.cards.e.EatToExtinction.class));
