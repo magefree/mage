b0aa8a98ad280f198c59b96d164a8626066a4edd
Evan Kranzler <theelk801@gmail.com>
2020-01-09 15:50:29 -0500

    Implemented Mindwrack Harpy

---
+++ Mage.Sets/src/mage/cards/m/MindwrackHarpy.java
@@ -0,0 +1,45 @@
+package mage.cards.m;
+
+import mage.MageInt;
+import mage.abilities.common.BeginningOfCombatTriggeredAbility;
+import mage.abilities.effects.common.PutTopCardOfLibraryIntoGraveEachPlayerEffect;
+import mage.abilities.keyword.FlyingAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.constants.TargetController;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class MindwrackHarpy extends CardImpl {
+
+    public MindwrackHarpy(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT, CardType.CREATURE}, "{3}{B}");
+
+        this.subtype.add(SubType.HARPY);
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(2);
+
+        // Flying
+        this.addAbility(FlyingAbility.getInstance());
+
+        // At the beginning of combat on your turn, each player puts the top three cards of their library into their graveyard.
+        this.addAbility(new BeginningOfCombatTriggeredAbility(
+                new PutTopCardOfLibraryIntoGraveEachPlayerEffect(3, TargetController.ANY),
+                TargetController.YOU, false
+        ));
+    }
+
+    private MindwrackHarpy(final MindwrackHarpy card) {
+        super(card);
+    }
+
+    @Override
+    public MindwrackHarpy copy() {
+        return new MindwrackHarpy(this);
+    }
+}
