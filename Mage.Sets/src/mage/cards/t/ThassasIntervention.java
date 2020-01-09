b6fca3ac7d5a523f88c2b09e0db7d9581c2eff39
Evan Kranzler <theelk801@gmail.com>
2020-01-09 16:06:14 -0500

    Implemented Thassa's Intervention

---
+++ Mage.Sets/src/mage/cards/t/ThassasIntervention.java
@@ -0,0 +1,56 @@
+package mage.cards.t;
+
+import mage.abilities.Mode;
+import mage.abilities.dynamicvalue.DynamicValue;
+import mage.abilities.dynamicvalue.MultipliedValue;
+import mage.abilities.dynamicvalue.common.ManacostVariableValue;
+import mage.abilities.dynamicvalue.common.StaticValue;
+import mage.abilities.effects.common.CounterUnlessPaysEffect;
+import mage.abilities.effects.common.LookLibraryAndPickControllerEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Zone;
+import mage.filter.StaticFilters;
+import mage.target.TargetSpell;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class ThassasIntervention extends CardImpl {
+
+    private static final DynamicValue xValue = new MultipliedValue(ManacostVariableValue.instance, 2);
+
+    public ThassasIntervention(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{X}{U}{U}");
+
+        // Choose one-
+        // • Look at the top X cards of your library. Put up to two of them into your hand and the rest on the bottom of your library in a random order.
+        this.getSpellAbility().addEffect(new LookLibraryAndPickControllerEffect(
+                        ManacostVariableValue.instance, false, StaticValue.get(2),
+                        StaticFilters.FILTER_CARD, Zone.LIBRARY, false, false,
+                        true, Zone.HAND, true, false, false
+                ).setBackInRandomOrder(true).setText(
+                "Look at the top X cards of your library. Put up to two of them into your hand " +
+                        "and the rest on the bottom of your library in a random order."
+                )
+        );
+
+        // • Counter target spell unless its controller pays twice {X}.
+        Mode mode = new Mode(new CounterUnlessPaysEffect(xValue)
+                .setText("Counter target spell unless its controller pays twice {X}."));
+        mode.addTarget(new TargetSpell());
+        this.getSpellAbility().addMode(mode);
+    }
+
+    private ThassasIntervention(final ThassasIntervention card) {
+        super(card);
+    }
+
+    @Override
+    public ThassasIntervention copy() {
+        return new ThassasIntervention(this);
+    }
+}
