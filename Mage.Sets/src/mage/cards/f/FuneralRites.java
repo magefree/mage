3a4dcc76889c04ae9047c32d2e6ce86d861e28ce
Evan Kranzler <theelk801@gmail.com>
2020-01-10 18:08:55 -0500

    Implemented Funeral Rites

---
+++ Mage.Sets/src/mage/cards/f/FuneralRites.java
@@ -0,0 +1,37 @@
+package mage.cards.f;
+
+import mage.abilities.effects.common.DrawCardSourceControllerEffect;
+import mage.abilities.effects.common.LoseLifeSourceControllerEffect;
+import mage.abilities.effects.common.PutTopCardOfLibraryIntoGraveControllerEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class FuneralRites extends CardImpl {
+
+    public FuneralRites(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{2}{B}");
+
+        // You draw two cards, lose 2 life, and put the top two cards of your library into your graveyard,
+        this.getSpellAbility().addEffect(new DrawCardSourceControllerEffect(2)
+                .setText("you draw two cards,"));
+        this.getSpellAbility().addEffect(new LoseLifeSourceControllerEffect(2)
+                .setText("lose 2 life,"));
+        this.getSpellAbility().addEffect(new PutTopCardOfLibraryIntoGraveControllerEffect(2)
+                .concatBy("and"));
+    }
+
+    private FuneralRites(final FuneralRites card) {
+        super(card);
+    }
+
+    @Override
+    public FuneralRites copy() {
+        return new FuneralRites(this);
+    }
+}
