8f13ac962066718e42e616e0db866e1b4da53154
Evan Kranzler <theelk801@gmail.com>
2020-01-10 18:13:42 -0500

    Implemented Rumbling Sentry

---
+++ Mage.Sets/src/mage/cards/r/RumblingSentry.java
@@ -0,0 +1,37 @@
+package mage.cards.r;
+
+import mage.MageInt;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.effects.keyword.ScryEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class RumblingSentry extends CardImpl {
+
+    public RumblingSentry(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{3}{W}{W}");
+
+        this.subtype.add(SubType.GIANT);
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(6);
+
+        // When Rumbling Sentry enters the battlefield, scry 1.
+        this.addAbility(new EntersBattlefieldTriggeredAbility(new ScryEffect(1)));
+    }
+
+    private RumblingSentry(final RumblingSentry card) {
+        super(card);
+    }
+
+    @Override
+    public RumblingSentry copy() {
+        return new RumblingSentry(this);
+    }
+}
