f50594444035175bdae5a7242a581ff983873780
Evan Kranzler <theelk801@gmail.com>
2020-01-10 18:48:11 -0500

    Implemented Mogis's Favor

---
+++ Mage.Sets/src/mage/cards/m/MogissFavor.java
@@ -0,0 +1,51 @@
+package mage.cards.m;
+
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.common.AttachEffect;
+import mage.abilities.effects.common.continuous.BoostEnchantedEffect;
+import mage.abilities.keyword.EnchantAbility;
+import mage.abilities.keyword.EscapeAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Outcome;
+import mage.constants.SubType;
+import mage.target.TargetPermanent;
+import mage.target.common.TargetCreaturePermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class MogissFavor extends CardImpl {
+
+    public MogissFavor(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{B}");
+
+        this.subtype.add(SubType.AURA);
+
+        // Enchant creature
+        TargetPermanent auraTarget = new TargetCreaturePermanent();
+        this.getSpellAbility().addTarget(auraTarget);
+        this.getSpellAbility().addEffect(new AttachEffect(Outcome.BoostCreature));
+        Ability ability = new EnchantAbility(auraTarget.getTargetName());
+        this.addAbility(ability);
+
+        // Enchanted creature gets +2/-1.
+        this.addAbility(new SimpleStaticAbility(new BoostEnchantedEffect(2, -1)));
+
+        // Escape—{2}{B}, Exile two other cards from your graveyard.
+        this.addAbility(new EscapeAbility(this, "{2}{B}", 2));
+    }
+
+    private MogissFavor(final MogissFavor card) {
+        super(card);
+    }
+
+    @Override
+    public MogissFavor copy() {
+        return new MogissFavor(this);
+    }
+}
