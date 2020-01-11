8defedc72ea21fb457b90629fa2f568dd4f7dc86
Evan Kranzler <theelk801@gmail.com>
2020-01-10 20:25:45 -0500

    Implemented Flicker of Fate

---
+++ Mage.Sets/src/mage/cards/f/FlickerOfFate.java
@@ -0,0 +1,45 @@
+package mage.cards.f;
+
+import mage.abilities.effects.common.ExileTargetForSourceEffect;
+import mage.abilities.effects.common.ReturnToBattlefieldUnderOwnerControlTargetEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.filter.FilterPermanent;
+import mage.filter.predicate.Predicates;
+import mage.target.TargetPermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class FlickerOfFate extends CardImpl {
+
+    private static final FilterPermanent filter = new FilterPermanent("creature or enchantment");
+
+    static {
+        filter.add(Predicates.or(
+                CardType.CREATURE.getPredicate(),
+                CardType.ENCHANTMENT.getPredicate()
+        ));
+    }
+
+    public FlickerOfFate(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{1}{W}");
+
+        // Exile target creature or enchantment, then return it to the battlefield under its owner's control.
+        this.getSpellAbility().addEffect(new ExileTargetForSourceEffect());
+        this.getSpellAbility().addEffect(new ReturnToBattlefieldUnderOwnerControlTargetEffect(true));
+        this.getSpellAbility().addTarget(new TargetPermanent(filter));
+    }
+
+    private FlickerOfFate(final FlickerOfFate card) {
+        super(card);
+    }
+
+    @Override
+    public FlickerOfFate copy() {
+        return new FlickerOfFate(this);
+    }
+}
--- Mage.Sets/src/mage/sets/TherosBeyondDeath.java
+++ Mage.Sets/src/mage/sets/TherosBeyondDeath.java
@@ -89,6 +89,7 @@ public final class TherosBeyondDeath extends ExpansionSet {
         cards.add(new SetCardInfo("Favored of Iroas", 15, Rarity.UNCOMMON, mage.cards.f.FavoredOfIroas.class));
         cards.add(new SetCardInfo("Field of Ruin", 242, Rarity.UNCOMMON, mage.cards.f.FieldOfRuin.class));
         cards.add(new SetCardInfo("Final Death", 95, Rarity.COMMON, mage.cards.f.FinalDeath.class));
+        cards.add(new SetCardInfo("Flicker of Fate", 16, Rarity.COMMON, mage.cards.f.FlickerOfFate.class));
         cards.add(new SetCardInfo("Forest", 254, Rarity.LAND, mage.cards.basiclands.Forest.class, FULL_ART_BFZ_VARIOUS));
         cards.add(new SetCardInfo("Fruit of Tizerus", 96, Rarity.COMMON, mage.cards.f.FruitOfTizerus.class));
         cards.add(new SetCardInfo("Funeral Rites", 97, Rarity.COMMON, mage.cards.f.FuneralRites.class));
