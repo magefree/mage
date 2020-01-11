f18fd8582814c4c0d9d90cc5194a03f9629365d6
Evan Kranzler <theelk801@gmail.com>
2020-01-10 20:22:29 -0500

    Implemented Sunmane Pegasus

---
+++ Mage.Sets/src/mage/cards/s/SunmanePegasus.java
@@ -0,0 +1,52 @@
+package mage.cards.s;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.continuous.GainAbilitySourceEffect;
+import mage.abilities.keyword.FlyingAbility;
+import mage.abilities.keyword.LifelinkAbility;
+import mage.abilities.keyword.VigilanceAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.SubType;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class SunmanePegasus extends CardImpl {
+
+    public SunmanePegasus(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{3}{W}");
+
+        this.subtype.add(SubType.PEGASUS);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(3);
+
+        // Flying
+        this.addAbility(FlyingAbility.getInstance());
+
+        // {1}{W}: Sunmane Pegasus gains vigilance and lifelink until end of turn.
+        Ability ability = new SimpleActivatedAbility(new GainAbilitySourceEffect(
+                VigilanceAbility.getInstance(), Duration.EndOfTurn
+        ).setText("{this} gains vigilance"), new ManaCostsImpl("{1}{W}"));
+        ability.addEffect(new GainAbilitySourceEffect(
+                LifelinkAbility.getInstance(), Duration.EndOfTurn
+        ).setText("and lifelink until end of turn"));
+        this.addAbility(ability);
+    }
+
+    private SunmanePegasus(final SunmanePegasus card) {
+        super(card);
+    }
+
+    @Override
+    public SunmanePegasus copy() {
+        return new SunmanePegasus(this);
+    }
+}
