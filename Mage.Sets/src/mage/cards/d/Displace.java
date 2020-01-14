3a681d565aed10140a52d74d320d9017595384c5
Oleg Agafonov <jaydi85@gmail.com>
2020-01-14 19:15:42 +0400

    * Displace - fixed that it return cards under your control instead owner;

--- Mage.Sets/src/mage/cards/d/Displace.java
+++ Mage.Sets/src/mage/cards/d/Displace.java
@@ -1,32 +1,30 @@
-
 package mage.cards.d;
 
-import java.util.UUID;
 import mage.abilities.effects.Effect;
 import mage.abilities.effects.common.ExileTargetForSourceEffect;
-import mage.abilities.effects.common.ReturnToBattlefieldUnderYourControlTargetEffect;
+import mage.abilities.effects.common.ReturnToBattlefieldUnderOwnerControlTargetEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.filter.common.FilterControlledCreaturePermanent;
 import mage.target.common.TargetControlledCreaturePermanent;
 
+import java.util.UUID;
+
 /**
- *
  * @author LevelX2
  */
 public final class Displace extends CardImpl {
 
     public Displace(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.INSTANT},"{2}{U}");
+        super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{2}{U}");
 
         // Exile up to two target creatures you control, then return those cards to the battlefield under their owner's control.
         this.getSpellAbility().addTarget(new TargetControlledCreaturePermanent(0, 2, new FilterControlledCreaturePermanent("creatures you control"), false));
         Effect effect = new ExileTargetForSourceEffect();
-        effect.setText("Exile up to two target creatures you control");
         this.getSpellAbility().addEffect(effect);
-        effect = new ReturnToBattlefieldUnderYourControlTargetEffect(true);
-        effect.setText(", then return those cards to the battlefield under their owner's control");
+        effect = new ReturnToBattlefieldUnderOwnerControlTargetEffect()
+                .withReturnNames("those cards", "their owner's").concatBy(", then");
         this.getSpellAbility().addEffect(effect);
     }
 
