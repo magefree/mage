ab10ee10c9a6cc76bd4b33d0fb631f4b979e459d
Oleg Agafonov <jaydi85@gmail.com>
2020-01-29 07:18:23 +0400

    Server: logs cleanup

--- Mage.Server.Plugins/Mage.Player.Human/src/mage/player/human/HumanPlayer.java
+++ Mage.Server.Plugins/Mage.Player.Human/src/mage/player/human/HumanPlayer.java
@@ -135,9 +135,8 @@ public class HumanPlayer extends PlayerImpl {
             numTimesWaiting++;
             if (numTimesWaiting >= 300) {
                 // game freezed -- need to report about error and continue to execute
-                String s = "ERROR - game freezed in waitResponseOpen for user " + getName() + " (connection problem)";
-                //Throwable th = new IllegalStateException(s); stack info
-                logger.error(s);
+                String s = "Game freezed in waitResponseOpen for user " + getName() + " (connection problem)";
+                logger.warn(s);
                 break;
             }
 
