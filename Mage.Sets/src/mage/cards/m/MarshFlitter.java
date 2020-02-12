7923564f7c1b2570e020a377b573804f0da3e991
Oleg Agafonov <jaydi85@gmail.com>
2020-02-12 18:24:01 +0400

    * Marsh Flitter - fixed that it can't sacrifice goblin;

--- Mage.Sets/src/mage/cards/m/MarshFlitter.java
+++ Mage.Sets/src/mage/cards/m/MarshFlitter.java
@@ -1,7 +1,5 @@
-
 package mage.cards.m;
 
-import java.util.UUID;
 import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.EntersBattlefieldTriggeredAbility;
@@ -18,8 +16,9 @@ import mage.filter.common.FilterControlledPermanent;
 import mage.game.permanent.token.GoblinRogueToken;
 import mage.target.common.TargetControlledPermanent;
 
+import java.util.UUID;
+
 /**
- *
  * @author LevelX2
  */
 public final class MarshFlitter extends CardImpl {
@@ -27,11 +26,11 @@ public final class MarshFlitter extends CardImpl {
     private static final FilterControlledPermanent filter = new FilterControlledPermanent("a Goblin");
 
     static {
-        filter.add(SubType.EQUIPMENT.getPredicate());
+        filter.add(SubType.GOBLIN.getPredicate());
     }
 
     public MarshFlitter(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.CREATURE},"{3}{B}");
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{3}{B}");
         this.subtype.add(SubType.FAERIE);
         this.subtype.add(SubType.ROGUE);
 
@@ -40,8 +39,10 @@ public final class MarshFlitter extends CardImpl {
 
         // Flying
         this.addAbility(FlyingAbility.getInstance());
+
         // When Marsh Flitter enters the battlefield, create two 1/1 black Goblin Rogue creature tokens.
         this.addAbility(new EntersBattlefieldTriggeredAbility(new CreateTokenEffect(new GoblinRogueToken(), 2), false));
+
         // Sacrifice a Goblin: Marsh Flitter has base power and toughness 3/3 until end of turn.
         Effect effect = new SetPowerToughnessSourceEffect(3, 3, Duration.EndOfTurn, SubLayer.SetPT_7b);
         effect.setText("{this} has base power and toughness 3/3 until end of turn");
