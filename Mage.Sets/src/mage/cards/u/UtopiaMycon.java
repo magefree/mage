f6079c4fd6f6d1ce3529f59345042064210407c6
Oleg Agafonov <jaydi85@gmail.com>
2020-02-17 20:57:18 +0400

    * Utopia Mycon - fixed unnecessary target choose;

--- Mage.Sets/src/mage/cards/u/UtopiaMycon.java
+++ Mage.Sets/src/mage/cards/u/UtopiaMycon.java
@@ -1,7 +1,5 @@
-
 package mage.cards.u;
 
-import java.util.UUID;
 import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.BeginningOfUpkeepTriggeredAbility;
@@ -20,36 +18,35 @@ import mage.constants.Zone;
 import mage.counters.CounterType;
 import mage.filter.common.FilterControlledCreaturePermanent;
 import mage.game.permanent.token.SaprolingToken;
-import mage.target.TargetPermanent;
 import mage.target.common.TargetControlledCreaturePermanent;
 
+import java.util.UUID;
+
 /**
- *
  * @author fireshoes
  */
 public final class UtopiaMycon extends CardImpl {
-    
+
     private static final FilterControlledCreaturePermanent filter = new FilterControlledCreaturePermanent("Saproling");
-    
+
     static {
         filter.add(SubType.SAPROLING.getPredicate());
     }
 
     public UtopiaMycon(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.CREATURE},"{G}");
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{G}");
         this.subtype.add(SubType.FUNGUS);
         this.power = new MageInt(0);
         this.toughness = new MageInt(2);
 
         // At the beginning of your upkeep, put a spore counter on Utopia Mycon.
         this.addAbility(new BeginningOfUpkeepTriggeredAbility(new AddCountersSourceEffect(CounterType.SPORE.createInstance()), TargetController.YOU, false));
-        
+
         // Remove three spore counters from Utopia Mycon: Create a 1/1 green Saproling creature token.
         this.addAbility(new SimpleActivatedAbility(Zone.BATTLEFIELD, new CreateTokenEffect(new SaprolingToken()), new RemoveCountersSourceCost(CounterType.SPORE.createInstance(3))));
-        
+
         // Sacrifice a Saproling: Add one mana of any color.
-        Ability ability = new AnyColorManaAbility(new SacrificeTargetCost(new TargetControlledCreaturePermanent(1,1, filter, false)));
-        ability.addTarget(new TargetPermanent(filter));
+        Ability ability = new AnyColorManaAbility(new SacrificeTargetCost(new TargetControlledCreaturePermanent(1, 1, filter, false)));
         this.addAbility(ability);
     }
 
