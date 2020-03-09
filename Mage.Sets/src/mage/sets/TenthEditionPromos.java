763a9367207a5fca856071585e5f8279327c0465
Thomas Hess <thomas.hess@udo.edu>
2020-03-09 12:31:52 +0100

    Added the Tenth Edition Promos (P10E) set.

---
+++ Mage.Sets/src/mage/sets/TenthEditionPromos.java
@@ -0,0 +1,25 @@
+package mage.sets;
+
+import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
+import mage.constants.SetType;
+
+/**
+ * https://scryfall.com/sets/p10e
+ */
+public class TenthEditionPromos extends ExpansionSet {
+
+    private static final TenthEditionPromos instance = new TenthEditionPromos();
+
+    public static TenthEditionPromos getInstance() {
+        return instance;
+    }
+
+    private TenthEditionPromos() {
+        super("Tenth Edition Promos", "P10E", ExpansionSet.buildDate(2007, 7, 13), SetType.PROMOTIONAL);
+        this.hasBoosters = false;
+        this.hasBasicLands = false;
+
+        cards.add(new SetCardInfo("Reya Dawnbringer", 35, Rarity.RARE, mage.cards.r.ReyaDawnbringer.class));
+     }
+}
