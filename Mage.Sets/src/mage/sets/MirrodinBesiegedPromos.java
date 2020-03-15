8d10ba37cf77186b5bcc9b175c7c66b5bfcc2b1a
Thomas Hess <thomas.hess@udo.edu>
2020-03-15 18:28:43 +0100

    Added the Mirrodin Besieged Promos (PMBS) set.

---
+++ Mage.Sets/src/mage/sets/MirrodinBesiegedPromos.java
@@ -0,0 +1,30 @@
+package mage.sets;
+
+import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
+import mage.constants.SetType;
+
+/**
+ * https://scryfall.com/sets/pmbs
+ */
+public class MirrodinBesiegedPromos extends ExpansionSet {
+
+    private static final MirrodinBesiegedPromos instance = new MirrodinBesiegedPromos();
+
+    public static MirrodinBesiegedPromos getInstance() {
+        return instance;
+    }
+
+    private MirrodinBesiegedPromos() {
+        super("Mirrodin Besieged Promos", "PMBS", ExpansionSet.buildDate(2011, 2, 3), SetType.PROMOTIONAL);
+        this.hasBoosters = false;
+        this.hasBasicLands = false;
+
+        cards.add(new SetCardInfo("Black Sun's Zenith", 39, Rarity.RARE, mage.cards.b.BlackSunsZenith.class));
+        cards.add(new SetCardInfo("Glissa, the Traitor", 96, Rarity.MYTHIC, mage.cards.g.GlissaTheTraitor.class));
+        cards.add(new SetCardInfo("Hero of Bladehold", 8, Rarity.MYTHIC, mage.cards.h.HeroOfBladehold.class));
+        cards.add(new SetCardInfo("Mirran Crusader", "*14", Rarity.RARE, mage.cards.m.MirranCrusader.class));
+        cards.add(new SetCardInfo("Thopter Assembly", 140, Rarity.RARE, mage.cards.t.ThopterAssembly.class));
+        cards.add(new SetCardInfo("Treasure Mage", 34, Rarity.UNCOMMON, mage.cards.t.TreasureMage.class));
+     }
+}
