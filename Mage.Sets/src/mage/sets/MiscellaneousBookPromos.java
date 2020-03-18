07bd00c6e4593fb152084061979ce497edfbf7ab
Thomas Hess <thomas.hess@udo.edu>
2020-03-18 17:14:29 +0100

    Added the Miscellaneous Book Promos (PBOK) set.

---
+++ Mage.Sets/src/mage/sets/MiscellaneousBookPromos.java
@@ -0,0 +1,25 @@
+package mage.sets;
+
+import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
+import mage.constants.SetType;
+
+/**
+ * https://scryfall.com/sets/pbok
+ */
+public class MiscellaneousBookPromos extends ExpansionSet {
+
+    private static final MiscellaneousBookPromos instance = new MiscellaneousBookPromos();
+
+    public static MiscellaneousBookPromos getInstance() {
+        return instance;
+    }
+
+    private MiscellaneousBookPromos() {
+        super("Miscellaneous Book Promos", "PBOK", ExpansionSet.buildDate(2009, 1, 27), SetType.PROMOTIONAL);
+        this.hasBoosters = false;
+        this.hasBasicLands = false;
+
+        cards.add(new SetCardInfo("Jace Beleren", 1, Rarity.MYTHIC, mage.cards.j.JaceBeleren.class));
+     }
+}
