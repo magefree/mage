6d8e78f07fb8300813c3a5e1ba94b4b92ddb7351
Evan Kranzler <theelk801@gmail.com>
2020-04-02 15:16:09 -0400

    Implemented Sprite Dragon

---
+++ Mage.Sets/src/mage/cards/s/SpriteDragon.java
@@ -0,0 +1,57 @@
+package mage.cards.s;
+
+import mage.MageInt;
+import mage.abilities.common.SpellCastControllerTriggeredAbility;
+import mage.abilities.effects.common.counter.AddCountersSourceEffect;
+import mage.abilities.keyword.FlyingAbility;
+import mage.abilities.keyword.HasteAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.counters.CounterType;
+import mage.filter.FilterSpell;
+import mage.filter.predicate.Predicates;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class SpriteDragon extends CardImpl {
+
+    private static final FilterSpell filter = new FilterSpell("a noncreature spell");
+
+    static {
+        filter.add(Predicates.not(CardType.CREATURE.getPredicate()));
+    }
+
+    public SpriteDragon(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{U}{R}");
+
+        this.subtype.add(SubType.FAERIE);
+        this.subtype.add(SubType.DRAGON);
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(1);
+
+        // Flying
+        this.addAbility(FlyingAbility.getInstance());
+
+        // Haste
+        this.addAbility(HasteAbility.getInstance());
+
+        // Whenever you cast a noncreature spell, put a +1/+1 counter on Sprite Dragon.
+        this.addAbility(new SpellCastControllerTriggeredAbility(
+                new AddCountersSourceEffect(CounterType.P1P1.createInstance()), filter, false
+        ));
+    }
+
+    private SpriteDragon(final SpriteDragon card) {
+        super(card);
+    }
+
+    @Override
+    public SpriteDragon copy() {
+        return new SpriteDragon(this);
+    }
+}
