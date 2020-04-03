da721e23424b555c448d36b291e109e8b404f3d4
Evan Kranzler <theelk801@gmail.com>
2020-04-03 14:07:04 -0400

    Implemented Shredded Sails

---
+++ Mage.Sets/src/mage/cards/s/ShreddedSails.java
@@ -0,0 +1,56 @@
+package mage.cards.s;
+
+import mage.abilities.Mode;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.DamageTargetEffect;
+import mage.abilities.effects.common.DestroyTargetEffect;
+import mage.abilities.keyword.CyclingAbility;
+import mage.abilities.keyword.FlyingAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.filter.FilterPermanent;
+import mage.filter.common.FilterCreaturePermanent;
+import mage.filter.predicate.mageobject.AbilityPredicate;
+import mage.target.TargetPermanent;
+import mage.target.common.TargetArtifactPermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class ShreddedSails extends CardImpl {
+
+    private static final FilterPermanent filter = new FilterCreaturePermanent("creature with flying");
+
+    static {
+        filter.add(new AbilityPredicate(FlyingAbility.class));
+    }
+
+    public ShreddedSails(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{1}{R}");
+
+        // Choose one —
+        // • Destroy target artifact.
+        this.getSpellAbility().addEffect(new DestroyTargetEffect());
+        this.getSpellAbility().addTarget(new TargetArtifactPermanent());
+
+        // • Shredded Sails deals 4 damage to target creature with flying.
+        Mode mode = new Mode(new DamageTargetEffect(4));
+        mode.addTarget(new TargetPermanent(filter));
+        this.getSpellAbility().addMode(mode);
+
+        // Cycling {2}
+        this.addAbility(new CyclingAbility(new ManaCostsImpl("{2}")));
+    }
+
+    private ShreddedSails(final ShreddedSails card) {
+        super(card);
+    }
+
+    @Override
+    public ShreddedSails copy() {
+        return new ShreddedSails(this);
+    }
+}
