b84c01af87e5bcb5d7b2394c5f01542d062a3d0c
Evan Kranzler <theelk801@gmail.com>
2020-04-04 11:45:24 -0400

    updated C20 spoiler and reprints

--- Mage.Sets/src/mage/sets/Commander2020Edition.java
+++ Mage.Sets/src/mage/sets/Commander2020Edition.java
@@ -1,6 +1,7 @@
 package mage.sets;
 
 import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
 import mage.constants.SetType;
 
 /**
@@ -17,5 +18,13 @@ public final class Commander2020Edition extends ExpansionSet {
     private Commander2020Edition() {
         super("Commander 2020 Edition", "C20", ExpansionSet.buildDate(2020, 4, 24), SetType.SUPPLEMENTAL);
         this.blockName = "Command Zone";
+
+        cards.add(new SetCardInfo("Ajani Unyielding", 201, Rarity.MYTHIC, mage.cards.a.AjaniUnyielding.class));
+        cards.add(new SetCardInfo("Alesha, Who Smiles at Death", 143, Rarity.RARE, mage.cards.a.AleshaWhoSmilesAtDeath.class));
+        cards.add(new SetCardInfo("Arcane Signet", 217, Rarity.COMMON, mage.cards.a.ArcaneSignet.class));
+        cards.add(new SetCardInfo("Crop Rotation", 169, Rarity.COMMON, mage.cards.c.CropRotation.class));
+        cards.add(new SetCardInfo("Lifecrafter's Bestiary", 244, Rarity.RARE, mage.cards.l.LifecraftersBestiary.class));
+        cards.add(new SetCardInfo("Rashmi, Eternities Crafter", 229, Rarity.RARE, mage.cards.r.RashmiEternitiesCrafter.class));
+        cards.add(new SetCardInfo("Shared Animosity", 158, Rarity.RARE, mage.cards.s.SharedAnimosity.class));
     }
 }
