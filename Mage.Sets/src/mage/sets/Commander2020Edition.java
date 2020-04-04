44228bbea8e5a9dc00e2727a8ec46d0aca9bb20f
Evan Kranzler <theelk801@gmail.com>
2020-04-04 18:43:45 -0400

    Implemented Brallin, Skyshark Rider

--- Mage.Sets/src/mage/sets/Commander2020Edition.java
+++ Mage.Sets/src/mage/sets/Commander2020Edition.java
@@ -22,6 +22,7 @@ public final class Commander2020Edition extends ExpansionSet {
         cards.add(new SetCardInfo("Ajani Unyielding", 201, Rarity.MYTHIC, mage.cards.a.AjaniUnyielding.class));
         cards.add(new SetCardInfo("Alesha, Who Smiles at Death", 143, Rarity.RARE, mage.cards.a.AleshaWhoSmilesAtDeath.class));
         cards.add(new SetCardInfo("Arcane Signet", 237, Rarity.COMMON, mage.cards.a.ArcaneSignet.class));
+        cards.add(new SetCardInfo("Brallin, Skyshark Rider", 4, Rarity.MYTHIC, mage.cards.b.BrallinSkysharkRider.class));
         cards.add(new SetCardInfo("Crop Rotation", 169, Rarity.COMMON, mage.cards.c.CropRotation.class));
         cards.add(new SetCardInfo("Curious Herd", 59, Rarity.RARE, mage.cards.c.CuriousHerd.class));
         cards.add(new SetCardInfo("Lifecrafter's Bestiary", 244, Rarity.RARE, mage.cards.l.LifecraftersBestiary.class));
