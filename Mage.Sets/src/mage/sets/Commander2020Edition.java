5fc1f96babc5a41e58a50374ada5e6f9da01f7cf
Evan Kranzler <theelk801@gmail.com>
2020-04-04 17:49:05 -0400

    Implemented Curious Herd

--- Mage.Sets/src/mage/sets/Commander2020Edition.java
+++ Mage.Sets/src/mage/sets/Commander2020Edition.java
@@ -23,6 +23,7 @@ public final class Commander2020Edition extends ExpansionSet {
         cards.add(new SetCardInfo("Alesha, Who Smiles at Death", 143, Rarity.RARE, mage.cards.a.AleshaWhoSmilesAtDeath.class));
         cards.add(new SetCardInfo("Arcane Signet", 237, Rarity.COMMON, mage.cards.a.ArcaneSignet.class));
         cards.add(new SetCardInfo("Crop Rotation", 169, Rarity.COMMON, mage.cards.c.CropRotation.class));
+        cards.add(new SetCardInfo("Curious Herd", 59, Rarity.RARE, mage.cards.c.CuriousHerd.class));
         cards.add(new SetCardInfo("Lifecrafter's Bestiary", 244, Rarity.RARE, mage.cards.l.LifecraftersBestiary.class));
         cards.add(new SetCardInfo("Rashmi, Eternities Crafter", 229, Rarity.RARE, mage.cards.r.RashmiEternitiesCrafter.class));
         cards.add(new SetCardInfo("Shared Animosity", 158, Rarity.RARE, mage.cards.s.SharedAnimosity.class));
