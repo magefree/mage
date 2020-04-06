484094b0f5adfc34930501dd8051165d09be25d4
Evan Kranzler <theelk801@gmail.com>
2020-04-05 21:58:03 -0400

    Implemented Daring Fiendbonder

---
+++ Mage.Sets/src/mage/cards/d/DaringFiendbonder.java
@@ -0,0 +1,59 @@
+package mage.cards.d;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.ActivateAsSorceryActivatedAbility;
+import mage.abilities.common.AttacksEachCombatStaticAbility;
+import mage.abilities.costs.common.ExileSourceFromGraveCost;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.counter.AddCountersTargetEffect;
+import mage.abilities.keyword.HasteAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.constants.Zone;
+import mage.counters.CounterType;
+import mage.target.common.TargetCreaturePermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class DaringFiendbonder extends CardImpl {
+
+    public DaringFiendbonder(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{3}{B}");
+
+        this.subtype.add(SubType.HUMAN);
+        this.subtype.add(SubType.WARLOCK);
+        this.power = new MageInt(5);
+        this.toughness = new MageInt(1);
+
+        // Haste
+        this.addAbility(HasteAbility.getInstance());
+
+        // Daring Fiendbonder attacks each combat if able.
+        this.addAbility(new AttacksEachCombatStaticAbility());
+
+        // {1}{B}, Exile Daring Fiendbonder from your graveyard: Put an indestructible counter on target creature. Activate this ability only any time you could cast a sorcery.
+        Ability ability = new ActivateAsSorceryActivatedAbility(
+                Zone.GRAVEYARD,
+                new AddCountersTargetEffect(CounterType.INDESTRUCTIBLE.createInstance()),
+                new ManaCostsImpl("{1}{B}")
+        );
+        ability.addCost(new ExileSourceFromGraveCost());
+        ability.addTarget(new TargetCreaturePermanent());
+        this.addAbility(ability);
+    }
+
+    private DaringFiendbonder(final DaringFiendbonder card) {
+        super(card);
+    }
+
+    @Override
+    public DaringFiendbonder copy() {
+        return new DaringFiendbonder(this);
+    }
+}
