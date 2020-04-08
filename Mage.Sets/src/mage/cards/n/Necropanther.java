c632f48ba6cd716fde5e39586075a0e036277752
Evan Kranzler <theelk801@gmail.com>
2020-04-08 17:51:11 -0400

    Implemented Necropanther

---
+++ Mage.Sets/src/mage/cards/n/Necropanther.java
@@ -0,0 +1,57 @@
+package mage.cards.n;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.MutatesSourceTriggeredAbility;
+import mage.abilities.effects.common.ReturnFromGraveyardToBattlefieldTargetEffect;
+import mage.abilities.keyword.MutateAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.ComparisonType;
+import mage.constants.SubType;
+import mage.filter.FilterCard;
+import mage.filter.common.FilterCreatureCard;
+import mage.filter.predicate.mageobject.ConvertedManaCostPredicate;
+import mage.target.common.TargetCardInYourGraveyard;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class Necropanther extends CardImpl {
+
+    private static final FilterCard filter
+            = new FilterCreatureCard("creature card with converted mana cost 3 or less from your graveyard");
+
+    static {
+        filter.add(new ConvertedManaCostPredicate(ComparisonType.FEWER_THAN, 4));
+    }
+
+    public Necropanther(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{1}{W}{B}");
+
+        this.subtype.add(SubType.CAT);
+        this.subtype.add(SubType.NIGHTMARE);
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(3);
+
+        // Mutate {2}{W/B}{W/B}
+        this.addAbility(new MutateAbility(this, "{2}{W/B}{W/B}"));
+
+        // Whenever this creature mutates, return target creature card with converted mana cost 3 or less from your graveyard to the battlefield.
+        Ability ability = new MutatesSourceTriggeredAbility(new ReturnFromGraveyardToBattlefieldTargetEffect());
+        ability.addTarget(new TargetCardInYourGraveyard(filter));
+        this.addAbility(ability);
+    }
+
+    private Necropanther(final Necropanther card) {
+        super(card);
+    }
+
+    @Override
+    public Necropanther copy() {
+        return new Necropanther(this);
+    }
+}
