e8a390e2060e7769607decb9a7d77aed658155fc
Evan Kranzler <theelk801@gmail.com>
2020-04-09 21:05:30 -0400

    Implemented Checkpoint Officer

---
+++ Mage.Sets/src/mage/cards/c/CheckpointOfficer.java
@@ -0,0 +1,45 @@
+package mage.cards.c;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.TapTargetEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.target.common.TargetCreaturePermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class CheckpointOfficer extends CardImpl {
+
+    public CheckpointOfficer(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{1}{W}");
+
+        this.subtype.add(SubType.HUMAN);
+        this.subtype.add(SubType.SOLDIER);
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(2);
+
+        // {1}{W}, {T}: Tap target creature.
+        Ability ability = new SimpleActivatedAbility(new TapTargetEffect(), new ManaCostsImpl("{1}{W}"));
+        ability.addCost(new TapSourceCost());
+        ability.addTarget(new TargetCreaturePermanent());
+        this.addAbility(ability);
+    }
+
+    private CheckpointOfficer(final CheckpointOfficer card) {
+        super(card);
+    }
+
+    @Override
+    public CheckpointOfficer copy() {
+        return new CheckpointOfficer(this);
+    }
+}
