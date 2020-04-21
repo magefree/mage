062410caca9bb89fd1157d82496cccd275a03122
Evan Kranzler <theelk801@gmail.com>
2020-04-20 21:39:52 -0400

    fixed Rumbling Rockslide text

--- Mage.Sets/src/mage/cards/r/RumblingRockslide.java
+++ Mage.Sets/src/mage/cards/r/RumblingRockslide.java
@@ -6,8 +6,7 @@ import mage.abilities.effects.common.DamageTargetEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.filter.FilterPermanent;
-import mage.filter.common.FilterControlledLandPermanent;
+import mage.filter.StaticFilters;
 import mage.target.common.TargetCreaturePermanent;
 
 import java.util.UUID;
@@ -17,14 +16,15 @@ import java.util.UUID;
  */
 public final class RumblingRockslide extends CardImpl {
 
-    private static final FilterPermanent filter = new FilterControlledLandPermanent("lands you control");
-    private static final DynamicValue xValue = new PermanentsOnBattlefieldCount(filter);
+    private static final DynamicValue xValue
+            = new PermanentsOnBattlefieldCount(StaticFilters.FILTER_CONTROLLED_LAND_SHORT_TEXT);
 
     public RumblingRockslide(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{3}{R}");
 
         // Rumbling Rockslide deals damage to target creature equal to the number of lands you control.
-        this.getSpellAbility().addEffect(new DamageTargetEffect(xValue));
+        this.getSpellAbility().addEffect(new DamageTargetEffect(xValue)
+                .setText("{this} deals damage to target creature equal to the number of lands you control"));
         this.getSpellAbility().addTarget(new TargetCreaturePermanent());
     }
 
