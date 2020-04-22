be966453fd9f75b91a99b8d4ac92147188443940
Benjamin Maitland <marthinwurer@gmail.com>
2020-04-21 23:15:25 -0400

    Added a Classic Modern format

---
+++ Mage.Server.Plugins/Mage.Deck.Constructed/src/mage/deck/ClassicModern.java
@@ -0,0 +1,66 @@
+package mage.deck;
+
+import mage.cards.ExpansionSet;
+import mage.cards.Sets;
+import mage.cards.decks.Constructed;
+import mage.sets.EighthEdition;
+import mage.sets.WarOfTheSpark;
+
+import java.util.Date;
+
+/**
+ * Deck validation for modern before WAR was released.
+ * @author marthinwurer@gmail.com
+ */
+public class ClassicModern extends Constructed {
+
+    public ClassicModern() {
+        super("Constructed - Modern");
+
+        Date cutoff = EighthEdition.getInstance().getReleaseDate(); // Eighth edition release date
+        Date finalCutoff = WarOfTheSpark.getInstance().getReleaseDate(); // WAR release date
+        for (ExpansionSet set : Sets.getInstance().values()) {
+            if (
+                set.getSetType().isModernLegal() &&
+                (set.getReleaseDate().after(cutoff) || set.getReleaseDate().equals(cutoff)) &&
+                        set.getReleaseDate().before(finalCutoff)
+            ) {
+                setCodes.add(set.getCode());
+            }
+        }
+
+        banned.add("Ancient Den");
+        banned.add("Birthing Pod");
+        banned.add("Blazing Shoal");
+        banned.add("Chrome Mox");
+        banned.add("Cloudpost");
+        banned.add("Dark Depths");
+        banned.add("Deathrite Shaman");
+        banned.add("Dig Through Time");
+        banned.add("Dread Return");
+        banned.add("Eye of Ugin");
+        banned.add("Gitaxian Probe");
+        banned.add("Glimpse of Nature");
+        banned.add("Golgari Grave-Troll");
+        banned.add("Great Furnace");
+        banned.add("Green Sun's Zenith");
+        banned.add("Hypergenesis");
+        banned.add("Krark-Clan Ironworks");
+        banned.add("Mental Misstep");
+        banned.add("Ponder");
+        banned.add("Preordain");
+        banned.add("Punishing Fire");
+        banned.add("Rite of Flame");
+        banned.add("Seat of the Synod");
+        banned.add("Second Sunrise");
+        banned.add("Seething Song");
+        banned.add("Sensei's Divining Top");
+        banned.add("Skullclamp");
+        banned.add("Splinter Twin");
+        banned.add("Summer Bloom");
+        banned.add("Treasure Cruise");
+        banned.add("Tree of Tales");
+        banned.add("Umezawa's Jitte");
+        banned.add("Vault of Whispers");
+    }
+}
