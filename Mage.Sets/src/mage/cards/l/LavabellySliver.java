02730a194c4c3a79d6a7f5832ca52be1f6bcb70e
Evan Kranzler <theelk801@gmail.com>
2020-04-23 20:54:09 -0400

    fixed various sliver texts

--- Mage.Sets/src/mage/cards/l/LavabellySliver.java
+++ Mage.Sets/src/mage/cards/l/LavabellySliver.java
@@ -30,7 +30,7 @@ public final class LavabellySliver extends CardImpl {
         this.toughness = new MageInt(2);
 
         // Sliver creatures you control have "When this creature enters the battlefield, it deals 1 damage to target player or planeswalker and you gain 1 life."
-        Ability ability = new EntersBattlefieldTriggeredAbility(new DamageTargetEffect(1, "it"));
+        Ability ability = new EntersBattlefieldTriggeredAbility(new DamageTargetEffect(1, "When this creature enters the battlefield, it"),false,true);
         ability.addEffect(new GainLifeEffect(1).concatBy("and"));
         ability.addTarget(new TargetPlayerOrPlaneswalker());
         this.addAbility(new SimpleStaticAbility(new GainAbilityControlledEffect(
