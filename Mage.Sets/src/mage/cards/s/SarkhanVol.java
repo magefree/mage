29dfb1a40446a2a3239b0bfa8c47963add9924f3
Evan Kranzler <theelk801@gmail.com>
2020-04-24 22:14:13 -0400

    one day all the text will be fixed, today is not that day

--- Mage.Sets/src/mage/cards/s/SarkhanVol.java
+++ Mage.Sets/src/mage/cards/s/SarkhanVol.java
@@ -1,7 +1,5 @@
-
 package mage.cards.s;
 
-import java.util.UUID;
 import mage.abilities.LoyaltyAbility;
 import mage.abilities.common.PlaneswalkerEntersWithLoyaltyCountersAbility;
 import mage.abilities.effects.Effects;
@@ -15,15 +13,16 @@ import mage.abilities.keyword.HasteAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.SubType;
 import mage.constants.Duration;
+import mage.constants.SubType;
 import mage.constants.SuperType;
 import mage.filter.StaticFilters;
 import mage.game.permanent.token.DragonToken;
 import mage.target.common.TargetCreaturePermanent;
 
+import java.util.UUID;
+
 /**
- *
  * @author BetaSteward_at_googlemail.com
  */
 public final class SarkhanVol extends CardImpl {
@@ -31,7 +30,7 @@ public final class SarkhanVol extends CardImpl {
     private static DragonToken dragonToken = new DragonToken();
 
     public SarkhanVol(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.PLANESWALKER},"{2}{R}{G}");
+        super(ownerId, setInfo, new CardType[]{CardType.PLANESWALKER}, "{2}{R}{G}");
         this.addSuperType(SuperType.LEGENDARY);
         this.subtype.add(SubType.SARKHAN);
 
@@ -44,11 +43,9 @@ public final class SarkhanVol extends CardImpl {
         this.addAbility(new LoyaltyAbility(effects1, 1));
 
         // -2: Gain control of target creature until end of turn. Untap that creature. It gains haste until end of turn.
-        Effects effects2 = new Effects();
-        effects2.add(new GainControlTargetEffect(Duration.EndOfTurn));
-        effects2.add(new UntapTargetEffect());
-        effects2.add(new GainAbilityTargetEffect(HasteAbility.getInstance(), Duration.EndOfTurn));
-        LoyaltyAbility ability = new LoyaltyAbility(effects2, -2);
+        LoyaltyAbility ability = new LoyaltyAbility(new GainControlTargetEffect(Duration.EndOfTurn), -2);
+        ability.addEffect(new UntapTargetEffect().setText("Untap that creature"));
+        ability.addEffect(new GainAbilityTargetEffect(HasteAbility.getInstance(), Duration.EndOfTurn));
         ability.addTarget(new TargetCreaturePermanent());
         this.addAbility(ability);
 
@@ -56,7 +53,7 @@ public final class SarkhanVol extends CardImpl {
         this.addAbility(new LoyaltyAbility(new CreateTokenEffect(dragonToken, 5), -6));
     }
 
-    public SarkhanVol(final SarkhanVol card) {
+    private SarkhanVol(final SarkhanVol card) {
         super(card);
     }
 
