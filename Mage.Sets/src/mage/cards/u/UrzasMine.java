40d3e14416926866b9a71a283addc8e946b80070
Evan Kranzler <theelk801@gmail.com>
2020-04-30 20:47:52 -0400

    fixed implementation of tron lands

--- Mage.Sets/src/mage/cards/u/UrzasMine.java
+++ Mage.Sets/src/mage/cards/u/UrzasMine.java
@@ -1,8 +1,6 @@
-
 package mage.cards.u;
 
 import mage.Mana;
-import mage.abilities.Ability;
 import mage.abilities.dynamicvalue.common.UrzaTerrainValue;
 import mage.abilities.mana.DynamicManaAbility;
 import mage.cards.CardImpl;
@@ -13,21 +11,22 @@ import mage.constants.SubType;
 import java.util.UUID;
 
 /**
- *
  * @author Melkhior
  */
 public final class UrzasMine extends CardImpl {
+
     public UrzasMine(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.LAND},"");
+        super(ownerId, setInfo, new CardType[]{CardType.LAND}, "");
         this.subtype.add(SubType.URZAS, SubType.MINE);
 
         // {T}: Add {C}. If you control an Urza's Power-Plant and an Urza's Tower, add {C}{C} instead.
-        Ability urzaManaAbility = new DynamicManaAbility(Mana.ColorlessMana(1), new UrzaTerrainValue(2),
-                "Add {C}. If you control an Urza's Power-Plant and an Urza's Tower, add {C}{C} instead");
-        this.addAbility(urzaManaAbility);
+        this.addAbility(new DynamicManaAbility(
+                Mana.ColorlessMana(1), UrzaTerrainValue.MINE,
+                "Add {C}. If you control an Urza's Power-Plant and an Urza's Tower, add {C}{C} instead"
+        ));
     }
 
-    public UrzasMine(final UrzasMine card) {
+    private UrzasMine(final UrzasMine card) {
         super(card);
     }
 
