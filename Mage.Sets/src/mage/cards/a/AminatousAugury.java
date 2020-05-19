28013dd1eb589d4fd2f0dcc20962671300aa8c49
etpalmer63 <etpalmer@math.sc.edu>
2020-05-19 12:04:13 -0700

    allowing casting of anything like with narset

--- Mage.Sets/src/mage/cards/a/AminatousAugury.java
+++ Mage.Sets/src/mage/cards/a/AminatousAugury.java
@@ -1,36 +1,29 @@
 package mage.cards.a;
 
-import java.util.EnumSet;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.UUID;
 import mage.MageObject;
 import mage.abilities.Ability;
 import mage.abilities.effects.AsThoughEffectImpl;
+import mage.abilities.effects.ContinuousEffect;
 import mage.abilities.effects.OneShotEffect;
 import mage.cards.Card;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.cards.Cards;
-import mage.cards.CardsImpl;
-import mage.choices.Choice;
-import mage.choices.ChoiceImpl;
-import mage.constants.AsThoughEffectType;
-import mage.constants.CardType;
-import mage.constants.Duration;
-import mage.constants.Outcome;
-import mage.constants.Zone;
-import mage.filter.StaticFilters;
-import mage.game.ExileZone;
+import mage.constants.*;
+import mage.filter.FilterCard;
+import mage.filter.common.FilterLandCard;
 import mage.game.Game;
 import mage.players.Player;
-import mage.target.TargetCard;
 import mage.target.targetpointer.FixedTarget;
-import mage.util.CardUtil;
+
+import java.util.Set;
+import java.util.UUID;
 
 /**
  *
  * @author credman0
+ *
+ * Comparing to cards like Narset and Emrakul, the Promised End.
+ *
  */
 public class AminatousAugury extends CardImpl {
 
@@ -56,6 +49,8 @@ public class AminatousAugury extends CardImpl {
 
 class AminatousAuguryEffect extends OneShotEffect {
 
+    private static final FilterCard filter = new FilterLandCard("land card to put on the battlefield");
+
     public AminatousAuguryEffect() {
         super(Outcome.PlayForFree);
         staticText = "Exile the top eight cards of your library. You may put a land card from among them onto the"
@@ -74,7 +69,118 @@ class AminatousAuguryEffect extends OneShotEffect {
 
     @Override
     public boolean apply(Game game, Ability source) {
+        Player player = game.getPlayer(source.getControllerId());
+        MageObject sourceObject = game.getObject(source.getSourceId());
+        if (player != null && sourceObject != null) {
+            //Cards cards = new CardsImpl(controller.getLibrary().getTopCards(game, 5));
+            Set<Card> cards = player.getLibrary().getTopCards(game, 8);
+            //Cards cards = new CardsImpl(player.getLibrary().getTopCards(game, 8));
 
+
+            player.moveCards(cards, Zone.EXILED, source, game);
+
+            /*
+            if (player.chooseUse(Outcome.PutCardInPlay, "Put a land from among the exiled cards into play?", source, game)) {
+                TargetCardInExile target = new TargetCardInExile(StaticFilters.FILTER_CARD_LAND_A);
+                if (player.choose(Outcome.PutCardInPlay, target, source.getSourceId(), game)) {
+                    Card card = game.getCard(target.getFirstTarget());
+                    if (card != null) {
+                        return player.moveCards(card, Zone.BATTLEFIELD, source, game, false, false, false, null);
+                    }
+                }
+            }
+
+             */
+
+
+            for (Card card : cards) {
+                if (game.getState().getZone(card.getId()) == Zone.EXILED && !card.isLand()) {
+                    ContinuousEffect effect = new AminatousAuguryCastFromExileEffect();
+                    effect.setTargetPointer(new FixedTarget(card, game));
+                    game.addEffect(effect, source);
+                }
+            }
+            return true;
+
+            //ContinuousEffect effect = new AminatousAuguryCastFromExileEffect();
+            //effect.setTargetPointer(new FixedTargets(cards.getCards(StaticFilters.FILTER_CARD_NON_LAND, game), game));
+            //game.addEffect(effect, source);
+
+            //return true;
+
+        }
+        return false;
+
+        /*
+        Player player = game.getPlayer(source.getControllerId());
+        if (player == null) {
+            return false;
+        }
+        MageObject sourceObject = game.getObject(source.getSourceId());
+        if (player != null && sourceObject != null) {
+            Set<Card> cards = player.getLibrary().getTopCards(game, 8);
+
+            Cards aminatouExileSet = new CardsImpl();
+            aminatouExileSet.addAll(cards);
+
+            player.moveCards(cards, Zone.EXILED, source, game);
+
+            TargetCard target = new TargetCard(Zone.EXILED, StaticFilters.FILTER_CARD_LAND);
+
+
+            //check for land card
+
+            //for (Card xCard : cards){
+           //    System.out.println("Card name -- " + xCard.toString());
+            //}
+
+
+            if (aminatouExileSet.count(StaticFilters.FILTER_CARD_LAND, game) > 0) {
+                //System.out.println("Exile set contains land!");
+                if (player.chooseUse(Outcome.PutCardInPlay, "Put a land from among the exiled cards into play?", source, game)) {
+                    if (player.choose(Outcome.PutCardInPlay, target, source.getSourceId(), game)) {
+                        Card card = game.getCard(target.getFirstTarget());
+                        if (card != null) {
+                                return player.moveCards(card, Zone.BATTLEFIELD, source, game, false, false, false, null);
+                        }
+                    }
+                }
+            }
+            Cards canBeCast = new CardsImpl();
+            for (Card card : cards) {
+                if (!card.isLand()) {
+                    canBeCast.add(card);
+                }
+            }
+
+            ContinuousEffect effect = new AminatousAuguryCastFromExileEffect();
+            effect.setTargetPointer(new FixedTargets(canBeCast, game));
+            game.addEffect(effect, source);
+
+            for (Card card : cards) {
+                if (game.getState().getZone(card.getId()) == Zone.EXILED) {
+
+                }
+            }
+
+
+            ExileZone auguryExileZone = game.getExile().getExileZone(source.getSourceId());
+            Cards cardsToCast = new CardsImpl();
+            cardsToCast.addAll(auguryExileZone.getCards(game));
+
+
+
+        }
+
+        return false;
+
+         */
+    }
+
+    /*
+    @Override
+    public boolean apply(Game game, Ability source) {
+        System.out.println("Aminatou Apply Override");
         Player controller = game.getPlayer(source.getControllerId());
         MageObject sourceObject = source.getSourceObject(game);
         if (controller != null && sourceObject != null) {
@@ -91,6 +197,7 @@ class AminatousAuguryEffect extends OneShotEffect {
                     Zone.EXILED,
                     StaticFilters.FILTER_CARD_LAND_A
             );
+
             if (cardsToCast.count(StaticFilters.FILTER_CARD_LAND, game) > 0) {
                 if (controller.chooseUse(Outcome.PutLandInPlay, "Put a land from among the exiled cards into play?", source, game)) {
                     if (controller.choose(Outcome.PutLandInPlay, cardsToCast, target, game)) {
@@ -103,6 +210,7 @@ class AminatousAuguryEffect extends OneShotEffect {
                 }
             }
             for (Card card : cardsToCast.getCards(StaticFilters.FILTER_CARD_NON_LAND, game)) {
+                System.out.println("Card -- " + card.toString());
                 AminatousAuguryCastFromExileEffect effect = new AminatousAuguryCastFromExileEffect();
                 effect.setTargetPointer(new FixedTarget(card.getId(), card.getZoneChangeCounter(game)));
                 game.addEffect(effect, source);
@@ -110,6 +218,8 @@ class AminatousAuguryEffect extends OneShotEffect {
         }
         return false;
     }
+    */
+
 }
 
 class AminatousAuguryCastFromExileEffect extends AsThoughEffectImpl {
@@ -133,41 +243,144 @@ class AminatousAuguryCastFromExileEffect extends AsThoughEffectImpl {
         return new AminatousAuguryCastFromExileEffect(this);
     }
 
+    //track each card type -- 5 types
+    //allow casting if not already cast from the 8 cards moved to exile
+
+    @Override
+    public boolean applies(UUID objectId, Ability source, UUID affectedControllerId, Game game) {
+
+        if (objectId.equals(getTargetPointer().getFirst(game, source))
+                && affectedControllerId.equals(source.getControllerId())) {
+            Card card = game.getCard(objectId);
+            if (card != null) {
+                Player player = game.getPlayer(affectedControllerId);
+                if (player != null) {
+                    player.setCastSourceIdWithAlternateMana(objectId, null, card.getSpellAbility().getCosts());
+                    return true;
+                }
+            }
+        }
+        return false;
+
+        /*
+
+        //EnumSet<CardType> usedCardTypesOutside = EnumSet.noneOf(CardType.class);
+        //EnumSet<CardType> unusedCardTypesOutside = EnumSet.noneOf(CardType.class);
+
+
+
+
+        if (objectId.equals(getTargetPointer().getFirst(game, source))
+                && affectedControllerId.equals(source.getControllerId())) {
+            Card card = game.getCard(objectId);
+            //System.out.println("card type -- " + card.getSuperType().toString());
+            if (card != null) {
+
+
+
+                Thread t = Thread.currentThread();
+                System.out.println("Thread ID --  " + t.getId());
+
+
+                EnumSet<CardType> usedCardTypes = EnumSet.noneOf(CardType.class);
+                EnumSet<CardType> unusedCardTypes = EnumSet.noneOf(CardType.class);
+
+                for (CardType cardT : card.getCardType()) {
+                    //System.out.println("Card Types -- " +  card.getCardType().toString());
+                    if (!usedCardTypes.contains(cardT)) {
+                        unusedCardTypes.add(cardT);
+                    }
+                    if (!usedCardTypesOutside.contains(cardT)) {
+                        unusedCardTypesOutside.add(cardT);
+                    }
+                }
+
+                System.out.print("Used Card Types: ");
+                for ( CardType pCard : usedCardTypes){
+                    System.out.print(pCard.toString() + " ");
+                }
+                System.out.println();
+                System.out.print("Unused Card Types: ");
+                for ( CardType pCard : unusedCardTypes){
+                    System.out.print(pCard.toString() + " ");
+                }
+                System.out.println();
+                System.out.print("UOed Card Types: ");
+                for ( CardType pCard : usedCardTypesOutside){
+                    System.out.print(pCard.toString() + " ");
+                }
+                System.out.println();
+                System.out.print("UOused Card Types: ");
+                for ( CardType pCard : unusedCardTypesOutside){
+                    System.out.print(pCard.toString() + " ");
+                }
+                System.out.println();
+
+
+                Player player = game.getPlayer(affectedControllerId);
+                if (player != null) {
+                    player.setCastSourceIdWithAlternateMana(objectId, null, card.getSpellAbility().getCosts());
+                    return true;
+                }
+            }
+        }
+        return false;
+
+         */
+    }
+
+    /*
     @Override
     public boolean applies(UUID sourceId, Ability source, UUID affectedControllerId, Game game) {
+
+        //System.out.println("Aminatou Applies Override");
+
         Player player = game.getPlayer(affectedControllerId);
         EnumSet<CardType> usedCardTypes = EnumSet.noneOf(CardType.class);
 
         if (game.getState().getValue(source.getSourceId().toString() + "cardTypes") != null) {
             usedCardTypes = (EnumSet<CardType>) game.getState().getValue(source.getSourceId().toString() + "cardTypes");
         }
+
+        for ( CardType xCard : usedCardTypes){
+            System.out.println("Used card types -xx- " + xCard.toString());
+        }
+
+
         //TODO add code for adding additional costs to the card
         if (player != null
                 && sourceId != null
                 && sourceId.equals(getTargetPointer().getFirst(game, source))
                 && affectedControllerId.equals(source.getControllerId())) {
             Card card = game.getCard(sourceId);
-            if (card != null
-                    && game.getState().getZone(sourceId) == Zone.EXILED) {
+            if (card != null  && game.getState().getZone(sourceId) == Zone.EXILED) {
                 EnumSet<CardType> unusedCardTypes = EnumSet.noneOf(CardType.class);
                 for (CardType cardT : card.getCardType()) {
+                    System.out.println("Card Types -- " +  card.getCardType().toString());
                     if (!usedCardTypes.contains(cardT)) {
                         unusedCardTypes.add(cardT);
                     }
                 }
+
+                for ( CardType xCard : unusedCardTypes){
+                    System.out.println("Unused card types -- " + xCard.toString());
+                }
+
                 if (!unusedCardTypes.isEmpty()) {
                     if (!game.inCheckPlayableState()) { // some actions may not be done while the game only checks if a card can be cast
-                        // Select the card type to consume and remove all not seleczted card types
+                        // Select the card type to consume and remove all not selected card types
                         if (unusedCardTypes.size() > 1) {
                             Choice choice = new ChoiceImpl(true);
                             choice.setMessage("Which card type do you want to consume?");
                             Set<String> choices = choice.getChoices();
                             for (CardType cardType : unusedCardTypes) {
                                 choices.add(cardType.toString());
+                                System.out.println("Aminatou adds type: ");
                             }
                             player.choose(Outcome.Detriment, choice, game);
                             for (Iterator<CardType> iterator = unusedCardTypes.iterator(); iterator.hasNext();) {
                                 CardType next = iterator.next();
+                                System.out.println("Aminatou selects: ");
                                 if (!next.toString().equals(choice.getChoice())) {
                                     iterator.remove();
                                 }
@@ -184,4 +397,6 @@ class AminatousAuguryCastFromExileEffect extends AsThoughEffectImpl {
         }
         return false;
     }
+    */
+
 }
