d4dbf33d192d3ab7190d8bce4c633374dc707072
LevelX2 <ludwig.hirth@online.de>
2020-06-04 10:57:36 +0200

    * Some minor code changes.

--- Mage.Tests/src/test/java/org/mage/test/cards/mana/SylvokExplorerTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/mana/SylvokExplorerTest.java
@@ -4,7 +4,6 @@ import mage.abilities.mana.ManaOptions;
 import mage.constants.PhaseStep;
 import mage.constants.Zone;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mage.test.serverside.base.CardTestPlayerBase;
 
@@ -14,25 +13,7 @@ import org.mage.test.serverside.base.CardTestPlayerBase;
  */
 public class SylvokExplorerTest extends CardTestPlayerBase {
 
-    /**
-     * java.lang.StackOverflowError at
-     * mage.filter.predicate.Predicates.and(Predicates.java:68) at
-     * mage.filter.FilterImpl.match(FilterImpl.java:62) at
-     * mage.filter.FilterPermanent.match(FilterPermanent.java:74) at
-     * mage.game.permanent.Battlefield.getActivePermanents(Battlefield.java:362)
-     * at
-     * mage.abilities.mana.AnyColorLandsProduceManaEffect.getManaTypes(AnyColorLandsProduceManaAbility.java:164)
-     * at
-     * mage.abilities.mana.AnyColorLandsProduceManaEffect.getNetMana(AnyColorLandsProduceManaAbility.java:181)
-     * at
-     * mage.abilities.mana.AnyColorLandsProduceManaAbility.getNetMana(AnyColorLandsProduceManaAbility.java:70)
-     * at
-     * mage.abilities.mana.AnyColorLandsProduceManaEffect.getManaTypes(AnyColorLandsProduceManaAbility.java:170)
-     * at
-     * mage.abilities.mana.AnyColorLandsProduceManaEffect.getNetMana(AnyColorLandsProduceManaAbility.java:181)
-     */
     @Test
-    @Ignore
     public void testOneInstance() {
         addCard(Zone.BATTLEFIELD, playerB, "Island", 1);
         addCard(Zone.BATTLEFIELD, playerB, "Mountain", 1);
@@ -42,7 +23,9 @@ public class SylvokExplorerTest extends CardTestPlayerBase {
         addCard(Zone.BATTLEFIELD, playerA, "Plains", 1);
 
         setStopAt(1, PhaseStep.PRECOMBAT_MAIN);
+        setStrictChooseMode(true);
         execute();
+        assertAllCommandsUsed();
 
         ManaOptions options = playerA.getAvailableManaTest(currentGame);
         Assert.assertEquals("Player should be able to create 1 red and 1 white mana", "{W}{R}", options.get(0).toString());
@@ -51,6 +34,7 @@ public class SylvokExplorerTest extends CardTestPlayerBase {
 
     @Test
     public void testTwoInstances() {
+        // {T}: Add one mana of any color that a land an opponent controls could produce.        
         addCard(Zone.BATTLEFIELD, playerB, "Exotic Orchard", 2);
 
         // {T}: Add one mana of any color that a land an opponent controls could produce.
@@ -58,8 +42,11 @@ public class SylvokExplorerTest extends CardTestPlayerBase {
         addCard(Zone.BATTLEFIELD, playerA, "Plains", 1);
 
         setStopAt(1, PhaseStep.PRECOMBAT_MAIN);
+        
+        setStrictChooseMode(true);
         execute();
-
+        assertAllCommandsUsed();
+        
         ManaOptions options = playerA.getAvailableManaTest(currentGame);
         Assert.assertEquals("Player should be able to create 3 white mana", "{W}{W}{W}", options.get(0).toString());
     }
