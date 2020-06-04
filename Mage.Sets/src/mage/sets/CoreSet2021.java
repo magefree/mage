d34b73bd789482e21db08e1ce6cb39a34bca99c8
Evan Kranzler <theelk801@gmail.com>
2020-06-04 18:40:23 -0400

    added M21 set file

---
+++ Mage.Sets/src/mage/sets/CoreSet2021.java
@@ -0,0 +1,36 @@
+package mage.sets;
+
+import mage.cards.ExpansionSet;
+import mage.cards.repository.CardInfo;
+import mage.constants.Rarity;
+import mage.constants.SetType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author TheElk801
+ */
+public final class CoreSet2021 extends ExpansionSet {
+
+    private static final CoreSet2021 instance = new CoreSet2021();
+
+    public static CoreSet2021 getInstance() {
+        return instance;
+    }
+
+    private final List<CardInfo> savedSpecialLand = new ArrayList<>();
+
+    private CoreSet2021() {
+        super("Core Set 2021", "M21", ExpansionSet.buildDate(2020, 7, 3), SetType.CORE);
+        this.hasBoosters = true;
+        this.hasBasicLands = false; // change when basics are available
+        this.numBoosterSpecial = 0;
+        this.numBoosterLands = 1;
+        this.numBoosterCommon = 10;
+        this.numBoosterUncommon = 3;
+        this.numBoosterRare = 1;
+        this.ratioBoosterMythic = 8;
+        this.maxCardNumberInBooster = 274;
+    }
+}
