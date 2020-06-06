fad462bfecbc499a06b4f725e4ce391a020cead1
Evan Kranzler <theelk801@gmail.com>
2020-06-05 20:20:43 -0400

    Implemented Teferi's Protege

--- Mage.Sets/src/mage/sets/CoreSet2021.java
+++ Mage.Sets/src/mage/sets/CoreSet2021.java
@@ -44,6 +44,7 @@ public final class CoreSet2021 extends ExpansionSet {
         cards.add(new SetCardInfo("Quirion Dryad", 198, Rarity.UNCOMMON, mage.cards.q.QuirionDryad.class));
         cards.add(new SetCardInfo("Rain of Revelation", 61, Rarity.UNCOMMON, mage.cards.r.RainOfRevelation.class));
         cards.add(new SetCardInfo("Runed Halo", 32, Rarity.RARE, mage.cards.r.RunedHalo.class));
+        cards.add(new SetCardInfo("Teferi's Protege", 77, Rarity.COMMON, mage.cards.t.TeferisProtege.class));
         cards.add(new SetCardInfo("Tormod's Crypt", 241, Rarity.UNCOMMON, mage.cards.t.TormodsCrypt.class));
         cards.add(new SetCardInfo("Ugin, the Spirit Dragon", 1, Rarity.MYTHIC, mage.cards.u.UginTheSpiritDragon.class));
     }
