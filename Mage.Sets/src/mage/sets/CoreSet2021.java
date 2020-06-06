2c4ffeb5abbe4b5c08d1444c19355e35e55051e0
Evan Kranzler <theelk801@gmail.com>
2020-06-06 16:15:35 -0400

    Implemented Sparkhunter Masticore

--- Mage.Sets/src/mage/sets/CoreSet2021.java
+++ Mage.Sets/src/mage/sets/CoreSet2021.java
@@ -53,6 +53,7 @@ public final class CoreSet2021 extends ExpansionSet {
         cards.add(new SetCardInfo("Runed Halo", 32, Rarity.RARE, mage.cards.r.RunedHalo.class));
         cards.add(new SetCardInfo("Shipwreck Dowser", 71, Rarity.UNCOMMON, mage.cards.s.ShipwreckDowser.class));
         cards.add(new SetCardInfo("Sigiled Contender", 323, Rarity.UNCOMMON, mage.cards.s.SigiledContender.class));
+        cards.add(new SetCardInfo("Sparkhunter Masticore", 240, Rarity.RARE, mage.cards.s.SparkhunterMasticore.class));
         cards.add(new SetCardInfo("Spirit of Malevolence", 331, Rarity.COMMON, mage.cards.s.SpiritOfMalevolence.class));
         cards.add(new SetCardInfo("Storm Caller", 335, Rarity.COMMON, mage.cards.s.StormCaller.class));
         cards.add(new SetCardInfo("Teferi's Protege", 77, Rarity.COMMON, mage.cards.t.TeferisProtege.class));
