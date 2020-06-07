0d49fb1a3b96573c97437a51af5df789c706721a
htrajan <htrajan2002@yahoo.com>
2020-06-06 20:02:21 -0700

    AdherentOfHope init commit

--- Mage.Sets/src/mage/cards/a/AdherentOfHope.java
+++ Mage.Sets/src/mage/cards/a/AdherentOfHope.java
@@ -2,16 +2,30 @@ package mage.cards.a;
 
 import java.util.UUID;
 import mage.MageInt;
+import mage.abilities.common.BeginningOfCombatTriggeredAbility;
+import mage.abilities.condition.common.PermanentsOnTheBattlefieldCondition;
+import mage.abilities.decorator.ConditionalInterveningIfTriggeredAbility;
+import mage.abilities.effects.common.counter.AddCountersSourceEffect;
 import mage.constants.SubType;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.constants.TargetController;
+import mage.counters.CounterType;
+import mage.filter.common.FilterControlledPermanent;
 
 /**
  *
  * @author htrajan
  */
 public final class AdherentOfHope extends CardImpl {
+
+    private static final FilterControlledPermanent filter = new FilterControlledPermanent();
+
+    static {
+        filter.add(CardType.PLANESWALKER.getPredicate());
+        filter.add(SubType.BASRI.getPredicate());
+    }
 
     public AdherentOfHope(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{1}{W}");
@@ -22,6 +36,10 @@ public final class AdherentOfHope extends CardImpl {
         this.toughness = new MageInt(1);
 
         // At the beginning of combat on your turn, if you control a Basri planeswalker, put a +1/+1 counter on Adherent of Hope.
+        this.addAbility(new ConditionalInterveningIfTriggeredAbility(
+                new BeginningOfCombatTriggeredAbility(new AddCountersSourceEffect(CounterType.P1P1.createInstance()), TargetController.YOU, false),
+                new PermanentsOnTheBattlefieldCondition(filter),
+                "At the beginning of combat on your turn, if you control a Basri planeswalker, put a +1/+1 counter on {this}."));
     }
 
     private AdherentOfHope(final AdherentOfHope card) {
