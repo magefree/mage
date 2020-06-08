43ca97a24d37943da1476524d2eb9b81d46634da
johnm <johnm@WINDOWS-QR5QIIL.lan>
2020-06-08 20:47:29 +0100

    review amends

--- Mage.Tests/src/test/java/org/mage/test/cards/abilities/oneshot/destroy/UnlicensedDisintegrationTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/abilities/oneshot/destroy/UnlicensedDisintegrationTest.java
@@ -13,18 +13,31 @@ The creature dies but the last piece of text does not work (teM, 2020-02-24 15:1
 */
 public class UnlicensedDisintegrationTest extends CardTestPlayerBase{
     
+    /*
+    Unlicensed Disintegration {1}{B}{R}
+    
+    Destroy target creature. If you control an artifact, 
+    Unlicensed Disintegration deals 3 damage to that creature's controller.
+    
+    
+    Avacyn, Angel of Hope {5}{W}{W}
+    
+    Flying, vigilance, indestructible
+    Other permanents you control have indestructible.
+    */
+    
     @Test
     public void testDestroyCreatureLifeLoss(){
-        addCard(Zone.BATTLEFIELD, playerA, "Mountain");
-        addCard(Zone.BATTLEFIELD, playerA, "Swamp");
-        addCard(Zone.BATTLEFIELD, playerA, "Swamp");
         addCard(Zone.HAND, playerA, "Unlicensed Disintegration");
-        addCard(Zone.BATTLEFIELD, playerB, "Volrath, the Shapestealer");
+        addCard(Zone.BATTLEFIELD, playerB, "Balduvian Bears");
+        addCard(Zone.BATTLEFIELD, playerA, "Mountain");
+        addCard(Zone.BATTLEFIELD, playerA, "Swamp",2);
         
         // Need an artifact to trigger the damage
         addCard(Zone.BATTLEFIELD, playerA, "Sol Ring");
 
-        castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Unlicensed Disintegration", "Volrath, the Shapestealer");
+        // Play Unlicensed Disintegration, targeting Balduvian Bears
+        castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Unlicensed Disintegration", "Balduvian Bears");
         
         setStopAt(1, PhaseStep.BEGIN_COMBAT);
         
@@ -35,22 +48,22 @@ public class UnlicensedDisintegrationTest extends CardTestPlayerBase{
         assertLife(playerA, 20);
         assertGraveyardCount(playerA, "Unlicensed Disintegration", 1);
         assertLife(playerB, 17);
-        assertGraveyardCount(playerB, "Volrath, the Shapestealer", 1);
+        assertGraveyardCount(playerB, "Balduvian Bears", 1);
     }
   
     @Test
     public void testDestroyCreatureLifeLossIndestructible(){
-        addCard(Zone.BATTLEFIELD, playerA, "Mountain");
-        addCard(Zone.BATTLEFIELD, playerA, "Swamp");
-        addCard(Zone.BATTLEFIELD, playerA, "Swamp");
         addCard(Zone.HAND, playerA, "Unlicensed Disintegration");
-        addCard(Zone.BATTLEFIELD, playerB, "Volrath, the Shapestealer");
+        addCard(Zone.BATTLEFIELD, playerB, "Balduvian Bears");
         addCard(Zone.BATTLEFIELD, playerB, "Avacyn, Angel of Hope");
+        addCard(Zone.BATTLEFIELD, playerA, "Mountain");
+        addCard(Zone.BATTLEFIELD, playerA, "Swamp",2);
         
         // Need an artifact to trigger the damage
         addCard(Zone.BATTLEFIELD, playerA, "Sol Ring");
 
-        castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Unlicensed Disintegration", "Volrath, the Shapestealer");
+        // Play Unlicensed Disintegration, targeting Balduvian Bears
+        castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Unlicensed Disintegration", "Balduvian Bears");
         
         setStopAt(1, PhaseStep.BEGIN_COMBAT);
         
@@ -62,19 +75,19 @@ public class UnlicensedDisintegrationTest extends CardTestPlayerBase{
         assertGraveyardCount(playerA, "Unlicensed Disintegration", 1);
         
         assertLife(playerB, 17);
-        assertPermanentCount(playerB, "Volrath, the Shapestealer", 1);
+        assertPermanentCount(playerB, "Balduvian Bears", 1);
         assertPermanentCount(playerB, "Avacyn, Angel of Hope", 1);
     }
 
     @Test
     public void testDestroyCreatureNoLifeLossNoArtifact(){
-        addCard(Zone.BATTLEFIELD, playerA, "Mountain");
-        addCard(Zone.BATTLEFIELD, playerA, "Swamp");
-        addCard(Zone.BATTLEFIELD, playerA, "Swamp");
         addCard(Zone.HAND, playerA, "Unlicensed Disintegration");
-        addCard(Zone.BATTLEFIELD, playerB, "Volrath, the Shapestealer");
-
-        castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Unlicensed Disintegration", "Volrath, the Shapestealer");
+        addCard(Zone.BATTLEFIELD, playerB, "Balduvian Bears");
+        addCard(Zone.BATTLEFIELD, playerA, "Mountain");
+        addCard(Zone.BATTLEFIELD, playerA, "Swamp",2);
+        
+        // Play Unlicensed Disintegration, targeting Balduvian Bears
+        castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Unlicensed Disintegration", "Balduvian Bears");
         
         setStopAt(1, PhaseStep.BEGIN_COMBAT);
         
@@ -85,19 +98,19 @@ public class UnlicensedDisintegrationTest extends CardTestPlayerBase{
         assertLife(playerA, 20);
         assertGraveyardCount(playerA, "Unlicensed Disintegration", 1);
         assertLife(playerB, 20);
-        assertGraveyardCount(playerB, "Volrath, the Shapestealer", 1);
+        assertGraveyardCount(playerB, "Balduvian Bears", 1);
     }
 
     @Test
     public void testDestroyCreatureNoLifeLossNoArtifactIndestructible(){
         addCard(Zone.BATTLEFIELD, playerA, "Mountain");
-        addCard(Zone.BATTLEFIELD, playerA, "Swamp");
-        addCard(Zone.BATTLEFIELD, playerA, "Swamp");
+        addCard(Zone.BATTLEFIELD, playerA, "Swamp",2);  
         addCard(Zone.HAND, playerA, "Unlicensed Disintegration");
-        addCard(Zone.BATTLEFIELD, playerB, "Volrath, the Shapestealer");
+        addCard(Zone.BATTLEFIELD, playerB, "Balduvian Bears");
         addCard(Zone.BATTLEFIELD, playerB, "Avacyn, Angel of Hope");
         
-        castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Unlicensed Disintegration", "Volrath, the Shapestealer");
+        // Play Unlicensed Disintegration, targeting Balduvian Bears
+        castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Unlicensed Disintegration", "Balduvian Bears");
         
         setStopAt(1, PhaseStep.BEGIN_COMBAT);
         
@@ -109,7 +122,7 @@ public class UnlicensedDisintegrationTest extends CardTestPlayerBase{
         assertGraveyardCount(playerA, "Unlicensed Disintegration", 1);
         
         assertLife(playerB, 20);
-        assertPermanentCount(playerB, "Volrath, the Shapestealer", 1);
+        assertPermanentCount(playerB, "Balduvian Bears", 1);
         assertPermanentCount(playerB, "Avacyn, Angel of Hope", 1);
     }
 }
