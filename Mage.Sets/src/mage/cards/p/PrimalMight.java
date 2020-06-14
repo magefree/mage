7f511275adc563bad191107d56dde9a4c1175207
Oleg Agafonov <jaydi85@gmail.com>
2020-06-14 05:11:11 +0400

    [M21] Primal Might - fixed wrong targets and text;

--- Mage.Sets/src/mage/cards/p/PrimalMight.java
+++ Mage.Sets/src/mage/cards/p/PrimalMight.java
@@ -1,20 +1,17 @@
-
-
 package mage.cards.p;
 
 import mage.abilities.dynamicvalue.common.ManacostVariableValue;
+import mage.abilities.effects.common.FightTargetsEffect;
 import mage.abilities.effects.common.continuous.BoostTargetEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.Duration;
-import mage.filter.common.FilterCreaturePermanent;
-import mage.target.common.TargetCreaturePermanent;
-import mage.abilities.effects.common.FightTargetsEffect;
-import java.util.UUID;
-import mage.filter.FilterPermanent;
+import mage.filter.StaticFilters;
 import mage.target.TargetPermanent;
-import mage.target.common.TargetAnyTarget;
+import mage.target.common.TargetControlledCreaturePermanent;
+
+import java.util.UUID;
 
 /**
  * @author mikalinn777
@@ -23,16 +20,17 @@ import mage.target.common.TargetAnyTarget;
 
 public final class PrimalMight extends CardImpl {
 
-    private static final FilterPermanent filter = new FilterCreaturePermanent("creature you don't control");
-
     public PrimalMight(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.SORCERY},"{X}{G}");
-        // Target creature gets +X/+X until end of turn.
-        this.getSpellAbility().addTarget(new TargetCreaturePermanent());
+        super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{X}{G}");
+
+        // Target creature you control gets +X/+X until end of turn. Then it fights up to one target creature you don’t control.
+        this.getSpellAbility().addTarget(new TargetControlledCreaturePermanent());
         this.getSpellAbility().addEffect(new BoostTargetEffect(ManacostVariableValue.instance, ManacostVariableValue.instance, Duration.EndOfTurn));
-        // Then, it fights up to one target creature.
-        this.getSpellAbility().addEffect(new FightTargetsEffect());
-        this.getSpellAbility().addTarget(new TargetPermanent(0, 1, filter, false));
+        //
+        this.getSpellAbility().addEffect(new FightTargetsEffect()
+                .concatBy("Then")
+                .setText("it fights up to one target creature you don’t control"));
+        this.getSpellAbility().addTarget(new TargetPermanent(0, 1, StaticFilters.FILTER_CREATURE_YOU_DONT_CONTROL, false));
 
     }
 
