844fad2872850f1d34173dd4125cf066d845f86a
jmharmon <37360760+jmharmon@users.noreply.github.com>
2020-06-17 13:06:46 -0700

    Update SanctumOfCalmWaters.java

--- Mage.Sets/src/mage/cards/s/SanctumOfCalmWaters.java
+++ Mage.Sets/src/mage/cards/s/SanctumOfCalmWaters.java
@@ -3,7 +3,6 @@ package mage.cards.s;
 import mage.abilities.Ability;
 import mage.abilities.common.BeginningOfPreCombatMainTriggeredAbility;
 import mage.abilities.dynamicvalue.common.PermanentsOnBattlefieldCount;
-import mage.abilities.effects.Effect;
 import mage.abilities.effects.common.DrawCardSourceControllerEffect;
 import mage.abilities.effects.common.discard.DiscardControllerEffect;
 import mage.cards.CardImpl;
@@ -22,12 +21,6 @@ import java.util.UUID;
 
 public final class SanctumOfCalmWaters extends CardImpl {
 
-    private static final FilterControlledPermanent filter = new FilterControlledPermanent("Shrine");
-
-    static {
-        filter.add(SubType.SHRINE.getPredicate());
-    }
-
     public SanctumOfCalmWaters(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{3}{U}");
 
@@ -35,7 +28,7 @@ public final class SanctumOfCalmWaters extends CardImpl {
         this.subtype.add(SubType.SHRINE);
 
         // At the beginning of your precombat main phase, you may draw X cards, where X is the number of Shrines you control. If you do, discard a card.
-        Ability ability = new BeginningOfPreCombatMainTriggeredAbility(new DrawCardSourceControllerEffect(new PermanentsOnBattlefieldCount(filter))
+        Ability ability = new BeginningOfPreCombatMainTriggeredAbility(new DrawCardSourceControllerEffect(new PermanentsOnBattlefieldCount(new FilterControlledPermanent(SubType.SHRINE)))
                 .setText("At the beginning of your precombat main phase, you may draw X cards, where X is the number of Shrines you control"),
                 TargetController.YOU, true);
         ability.addEffect(new DiscardControllerEffect(1).setText("If you do, discard a card"));
