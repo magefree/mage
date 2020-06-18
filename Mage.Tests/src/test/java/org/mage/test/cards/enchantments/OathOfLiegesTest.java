b84e0c03f83436dca84014208058e88f62d00f5c
jeffwadsworth <jeff@delmarus.com>
2020-06-18 17:14:32 -0500

    - Fix test

--- Mage.Tests/src/test/java/org/mage/test/cards/enchantments/OathOfLiegesTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/enchantments/OathOfLiegesTest.java
@@ -151,14 +151,14 @@ public class OathOfLiegesTest extends CardTestPlayerBase {
         // turn 2 - B
         // oath A triggers for B and do nothing
         // copy oath A triggers for B and do nothing
-        setChoice(playerA, "At the beginning of each upkeep"); // two triggers on upkeep
+        setChoice(playerA, "At the beginning of each player's upkeep"); // two triggers on upkeep
         checkPermanentCount("A have 10 plains", 1, PhaseStep.END_TURN, playerA, "Plains", 10);
         checkPermanentCount("B have 12 plains", 1, PhaseStep.END_TURN, playerB, "Plains", 12);
 
         // turn 3 - A
         // oath A triggers for A and activates
         // copy oath A triggers for A and activates
-        setChoice(playerA, "At the beginning of each upkeep"); // two triggers on upkeep
+        setChoice(playerA, "At the beginning of each player's upkeep"); // two triggers on upkeep
         // 1
         addTarget(playerA, playerB); // who control more lands
         setChoice(playerA, "Yes"); // search library
