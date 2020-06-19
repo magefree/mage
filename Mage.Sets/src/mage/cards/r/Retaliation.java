f2c09a7193b35c0f1b7cb746fde8b7eb1dc32b6f
jeffwadsworth <jeff@delmarus.com>
2020-06-19 14:36:01 -0500

    - Text fix

--- Mage.Sets/src/mage/cards/r/Retaliation.java
+++ Mage.Sets/src/mage/cards/r/Retaliation.java
@@ -1,9 +1,9 @@
-
 package mage.cards.r;
 
 import java.util.UUID;
 import mage.abilities.common.BecomesBlockedByCreatureTriggeredAbility;
 import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.Effect;
 import mage.abilities.effects.common.continuous.BoostSourceEffect;
 import mage.abilities.effects.common.continuous.GainAbilityControlledEffect;
 import mage.cards.CardImpl;
@@ -20,12 +20,20 @@ import mage.filter.StaticFilters;
 public final class Retaliation extends CardImpl {
 
     public Retaliation(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.ENCHANTMENT},"{2}{G}");
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{2}{G}");
 
-        // Creatures you control have "Whenever this creature becomes blocked by a creature, this creature gets +1/+1 until end of turn."
-        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, 
-                new GainAbilityControlledEffect(new BecomesBlockedByCreatureTriggeredAbility(new BoostSourceEffect(1, 1, Duration.EndOfTurn), false), 
-                        Duration.WhileOnBattlefield, StaticFilters.FILTER_PERMANENT_CREATURES)));
+        // Creatures you control have "Whenever this creature becomes blocked by a creature, 
+        // this creature gets +1/+1 until end of turn."
+        Effect effect = new GainAbilityControlledEffect(
+                new BecomesBlockedByCreatureTriggeredAbility(
+                        new BoostSourceEffect(
+                                1, 1,
+                                Duration.EndOfTurn), false),
+                Duration.WhileOnBattlefield, StaticFilters.FILTER_PERMANENT_CREATURES);
+        effect.setText("Creatures you control have \"Whenever this creature becomes blocked by a creature, "
+                + "this creature gets +1/+1 until end of turn.");
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD,
+                effect));
     }
 
     public Retaliation(final Retaliation card) {
