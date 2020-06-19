7bf84357f5b27fead8148b9b8576e8a4fa18c0ae
arcox <10953229+arcox@users.noreply.github.com>
2020-06-19 02:24:36 -0400

    Implement Track Down from Core 2021

--- Mage.Sets/src/mage/sets/CoreSet2021.java
+++ Mage.Sets/src/mage/sets/CoreSet2021.java
@@ -273,6 +273,7 @@ public final class CoreSet2021 extends ExpansionSet {
         cards.add(new SetCardInfo("Titanic Growth", 210, Rarity.COMMON, mage.cards.t.TitanicGrowth.class));
         cards.add(new SetCardInfo("Tolarian Kraken", 80, Rarity.UNCOMMON, mage.cards.t.TolarianKraken.class));
         cards.add(new SetCardInfo("Tormod's Crypt", 241, Rarity.UNCOMMON, mage.cards.t.TormodsCrypt.class));
+        cards.add(new SetCardInfo("Track Down", 211, Rarity.COMMON, mage.cards.t.TrackDown.class));
         cards.add(new SetCardInfo("Traitorous Greed", 166, Rarity.UNCOMMON, mage.cards.t.TraitorousGreed.class));
         cards.add(new SetCardInfo("Tranquil Cove", 258, Rarity.COMMON, mage.cards.t.TranquilCove.class));
         cards.add(new SetCardInfo("Transmogrify", 167, Rarity.RARE, mage.cards.t.Transmogrify.class));
