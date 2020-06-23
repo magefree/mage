5aebb8696299a984bc134ff673d2710829a5be22
18ths <13023067+18ths@users.noreply.github.com>
2020-06-23 21:57:25 +0200

    added allosaurus shepherd and blessed sanctuary

---
+++ Mage/src/main/java/mage/game/permanent/token/UnicornToken.java
@@ -0,0 +1,27 @@
+package mage.game.permanent.token;
+
+import mage.MageInt;
+import mage.constants.CardType;
+import mage.constants.SubType;
+
+public final class UnicornToken extends TokenImpl {
+
+    public UnicornToken() {
+        super("Unicorn", "2/2 white Unicorn creature token");
+        setExpansionSetCodeForImage("JMP");
+        cardType.add(CardType.CREATURE);
+        subtype.add(SubType.UNICORN);
+        color.setWhite(true);
+        power = new MageInt(2);
+        toughness = new MageInt(2);
+    }
+
+    private UnicornToken(final UnicornToken token) {
+        super(token);
+    }
+
+    @Override
+    public UnicornToken copy() {
+        return new UnicornToken(this);
+    }
+}
