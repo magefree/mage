4b12e16ed36fe2814357bafbb996b2a5cc3e170d
htrajan <htrajan2002@yahoo.com>
2020-06-29 01:30:50 -0700

    add comments

--- Mage.Tests/src/test/java/org/mage/test/cards/single/m21/EnthrallingHoldTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/m21/EnthrallingHoldTest.java
@@ -9,10 +9,21 @@ public class EnthrallingHoldTest extends CardTestPlayerBase {
 
     @Test
     public void testTappedTarget_untapped_doesNotFizzle() {
+        // Traxos, Scourge of Kroog enters the battlefield tapped and doesn't untap during your untap step.
         addCard(Zone.BATTLEFIELD, playerB, "Traxos, Scourge of Kroog");
 
         addCard(Zone.BATTLEFIELD, playerA, "Island", 6);
+        /*
+         * {3}{U}{U}
+         * Enchant creature
+         * You can't choose an untapped creature as this spell's target as you cast it.
+         * You control enchanted creature.
+         */
         addCard(Zone.HAND, playerA, "Enthralling Hold");
+        /*
+         * {U}
+         * You may tap or untap target artifact, creature, or land.
+         */
         addCard(Zone.HAND, playerA, "Twiddle");
 
         castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Enthralling Hold", "Traxos, Scourge of Kroog");
@@ -32,11 +43,22 @@ public class EnthrallingHoldTest extends CardTestPlayerBase {
 
     @Test
     public void testTappedTarget_becomesIllegal_fizzles() {
+        // Traxos, Scourge of Kroog enters the battlefield tapped and doesn't untap during your untap step.
         addCard(Zone.BATTLEFIELD, playerB, "Traxos, Scourge of Kroog");
 
         addCard(Zone.BATTLEFIELD, playerA, "Swamp", 5);
         addCard(Zone.BATTLEFIELD, playerA, "Island", 2);
+        /*
+         * {3}{U}{U}
+         * Enchant creature
+         * You can't choose an untapped creature as this spell's target as you cast it.
+         * You control enchanted creature.
+         */
         addCard(Zone.HAND, playerA, "Enthralling Hold");
+        /*
+         * {1}{B}
+         * Destroy target nonblack creature
+         */
         addCard(Zone.HAND, playerA, "Doom Blade");
 
         castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Enthralling Hold", "Traxos, Scourge of Kroog");
