f517406b1d5abbe143d6654b7026394ba247294a
Dragas <mgridinas@gmail.com>
2020-07-10 20:01:15 +0300

    Fixes #6805

--- Mage.Server/release/startServer.sh
+++ Mage.Server/release/startServer.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
+#!/bin/sh -x
 
-java -Xms256M -Xmx512M -XX:MaxPermSize=256m -Djava.security.policy=./config/security.policy -Dlog4j.configuration=file:./config/log4j.properties -jar ./lib/mage-server-${project.version}.jar
+script_dir="$(readlink -f -- "$BASH_SOURCE")"
+script_dir="$(dirname -- $script_dir)"
+cd $script_dir
+
+java -Xms256M -Xmx512M -XX:MaxPermSize=256m "-Djava.security.policy=${script_dir}/config/security.policy" "-Dlog4j.configuration=file:${script_dir}/config/log4j.properties" -jar "${script_dir}/lib/mage-server-${project.version}.jar"
--- Utils/release/startMage.sh
+++ Utils/release/startMage.sh
@@ -1,5 +1,13 @@
-#!/bin/sh
-cd ./mage-client
-./startClient.sh
-cd ../mage-server
-./startServer.sh
+#!/bin/sh -x
+
+script_dir="$(readlink -f -- "$BASH_SOURCE")"
+script_dir="$(dirname -- $script_dir)"
+cd $script_dir
+
+# i dont really recommend using this script
+# because both "child" scripts will output
+# to the same terminal
+# instead execute each individually
+
+"${script_dir}/startClient.sh" &
+"${script_dir}/startServer.sh" &
