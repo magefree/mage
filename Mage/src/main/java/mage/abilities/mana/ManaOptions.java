magefree/mage

commit 67dd45c1c7b52465ddd3586d2b260974b896e349

* Added getProducableManaTypes method for mana effects to handle Squandered Resources or Reflecting Pool like abilities. * Fixes to Soldevi Adnate, Skirge Familiar, Mana Web.

Mage/src/main/java/mage/abilities/mana/ManaOptions.java
@@ -94,7 +94,7 @@ public class ManaOptions extends ArrayList<Mana> {
         this.clear();
         for (Mana netMana : netManas) {
             for (Mana mana : copy) {
-                if (!hasTapCost(ability) || checkManaReplacementAndTriggeredMana(ability, game, netMana)) {
+                if (ability.hasTapCost() || checkManaReplacementAndTriggeredMana(ability, game, netMana)) {
                     Mana newMana = new Mana();
                     newMana.add(mana);
                     newMana.add(netMana);
@@ -104,7 +104,7 @@ public class ManaOptions extends ArrayList<Mana> {
         }
     }
 
-    private List<List<Mana>> getSimulatedTriggeredManaFromPlayer(Game game, Ability ability) {
+    private static List<List<Mana>> getSimulatedTriggeredManaFromPlayer(Game game, Ability ability) {
         Player player = game.getPlayer(ability.getControllerId());
         List<List<Mana>> newList = new ArrayList<>();
         if (player != null) {
@@ -124,7 +124,7 @@ public class ManaOptions extends ArrayList<Mana> {
      * @return false if mana production was completely replaced
      */
     private boolean checkManaReplacementAndTriggeredMana(Ability ability, Game game, Mana mana) {
-        if (hasTapCost(ability)) {
+        if (ability.hasTapCost()) {
             ManaEvent event = new ManaEvent(GameEvent.EventType.TAPPED_FOR_MANA, ability.getSourceId(), ability.getSourceId(), ability.getControllerId(), mana);
             if (game.replaceEvent(event)) {
                 return false;
@@ -135,15 +135,6 @@ public class ManaOptions extends ArrayList<Mana> {
         manaEvent.setData(mana.toString());
         game.fireEvent(manaEvent);
         return true;
-    }
-
-    public boolean hasTapCost(Ability ability) {
-        for (Cost cost : ability.getCosts()) {
-            if (cost instanceof TapSourceCost) {
-                return true;
-            }
-        }
-        return false;
     }
 
     /**
@@ -267,10 +258,10 @@ public class ManaOptions extends ArrayList<Mana> {
         return wasUsable;
     }
 
-    private List<Mana> getTriggeredManaVariations(Game game, Ability ability, Mana baseMana) {
+    public static List<Mana> getTriggeredManaVariations(Game game, Ability ability, Mana baseMana) {
         List<Mana> baseManaPlusTriggeredMana = new ArrayList<>();
         baseManaPlusTriggeredMana.add(baseMana);
-        List<List<Mana>> availableTriggeredManaList = getSimulatedTriggeredManaFromPlayer(game, ability);
+        List<List<Mana>> availableTriggeredManaList = ManaOptions.getSimulatedTriggeredManaFromPlayer(game, ability);
         for (List<Mana> availableTriggeredMana : availableTriggeredManaList) {
             if (availableTriggeredMana.size() == 1) {
                 for (Mana prevMana : baseManaPlusTriggeredMana) {
@@ -372,7 +363,7 @@ public class ManaOptions extends ArrayList<Mana> {
         Mana prevMana = currentMana.copy();
         // generic mana costs can be paid with different colored mana, can lead to different color combinations
         if (cost.getGeneric() > 0 && cost.getGeneric() > (currentMana.getGeneric() + currentMana.getColorless())) {
-            for (Mana payCombination : getPossiblePayCombinations(cost.getGeneric(), currentMana)) {
+            for (Mana payCombination : ManaOptions.getPossiblePayCombinations(cost.getGeneric(), currentMana)) {
                 Mana currentManaCopy = currentMana.copy();
                 while (currentManaCopy.includesMana(payCombination)) { // loop for multiple usage if possible
                     boolean newCombinations = false;
@@ -420,7 +411,13 @@ public class ManaOptions extends ArrayList<Mana> {
         return oldManaWasReplaced;
     }
 
-    private List<Mana> getPossiblePayCombinations(int number, Mana manaAvailable) {
+    /**
+     * 
+     * @param number of generic mana 
+     * @param manaAvailable
+     * @return 
+     */
+    public static List<Mana> getPossiblePayCombinations(int number, Mana manaAvailable) {
         List<Mana> payCombinations = new ArrayList<>();
         List<String> payCombinationsStrings = new ArrayList<>();
         if (manaAvailable.countColored() > 0) {
@@ -439,28 +436,28 @@ public class ManaOptions extends ArrayList<Mana> {
                     manaToPayFrom.subtract(existingMana);
                     if (manaToPayFrom.getBlack() > 0 && !payCombinationsStrings.contains(existingMana.toString() + Mana.BlackMana(1).toString())) {
                         manaToPayFrom.subtract(Mana.BlackMana(1));
-                        addManaCombination(Mana.BlackMana(1), existingMana, payCombinations, payCombinationsStrings);
+                        ManaOptions.addManaCombination(Mana.BlackMana(1), existingMana, payCombinations, payCombinationsStrings);
                     }
                     if (manaToPayFrom.getBlue() > 0 && !payCombinationsStrings.contains(existingMana.toString() + Mana.BlueMana(1).toString())) {
                         manaToPayFrom.subtract(Mana.BlueMana(1));
-                        addManaCombination(Mana.BlueMana(1), existingMana, payCombinations, payCombinationsStrings);
+                        ManaOptions.addManaCombination(Mana.BlueMana(1), existingMana, payCombinations, payCombinationsStrings);
                     }
                     if (manaToPayFrom.getGreen() > 0 && !payCombinationsStrings.contains(existingMana.toString() + Mana.GreenMana(1).toString())) {
                         manaToPayFrom.subtract(Mana.GreenMana(1));
-                        addManaCombination(Mana.GreenMana(1), existingMana, payCombinations, payCombinationsStrings);
+                        ManaOptions.addManaCombination(Mana.GreenMana(1), existingMana, payCombinations, payCombinationsStrings);
                     }
                     if (manaToPayFrom.getRed() > 0 && !payCombinationsStrings.contains(existingMana.toString() + Mana.RedMana(1).toString())) {
                         manaToPayFrom.subtract(Mana.RedMana(1));
-                        addManaCombination(Mana.RedMana(1), existingMana, payCombinations, payCombinationsStrings);
+                        ManaOptions.addManaCombination(Mana.RedMana(1), existingMana, payCombinations, payCombinationsStrings);
                     }
                     if (manaToPayFrom.getWhite() > 0 && !payCombinationsStrings.contains(existingMana.toString() + Mana.WhiteMana(1).toString())) {
                         manaToPayFrom.subtract(Mana.WhiteMana(1));
-                        addManaCombination(Mana.WhiteMana(1), existingMana, payCombinations, payCombinationsStrings);
+                        ManaOptions.addManaCombination(Mana.WhiteMana(1), existingMana, payCombinations, payCombinationsStrings);
                     }
                     // Pay with any only needed if colored payment was not possible
                     if (payCombinations.isEmpty() && manaToPayFrom.getAny() > 0 && !payCombinationsStrings.contains(existingMana.toString() + Mana.AnyMana(1).toString())) {
                         manaToPayFrom.subtract(Mana.AnyMana(1));
-                        addManaCombination(Mana.AnyMana(1), existingMana, payCombinations, payCombinationsStrings);
+                        ManaOptions.addManaCombination(Mana.AnyMana(1), existingMana, payCombinations, payCombinationsStrings);
                     }
                 }
             }
@@ -480,7 +477,7 @@ public class ManaOptions extends ArrayList<Mana> {
         return false;
     }
 
-    private void addManaCombination(Mana mana, Mana existingMana, List<Mana> payCombinations, List<String> payCombinationsStrings) {
+    public static void addManaCombination(Mana mana, Mana existingMana, List<Mana> payCombinations, List<String> payCombinationsStrings) {
         Mana newMana = existingMana.copy();
         newMana.add(mana);
         payCombinations.add(newMana);

Mage.Sets/src/mage/cards/k/KatabaticWinds.java
@@ -3,7 +3,6 @@ package mage.cards.k;
 import mage.MageObject;
 import mage.abilities.Ability;
 import mage.abilities.common.SimpleStaticAbility;
-import mage.abilities.costs.common.TapSourceCost;
 import mage.abilities.effects.ContinuousRuleModifyingEffectImpl;
 import mage.abilities.effects.RestrictionEffect;
 import mage.abilities.keyword.FlyingAbility;
@@ -119,14 +118,12 @@ class KatabaticWindsRuleModifyingEffect extends ContinuousRuleModifyingEffectImp
     public boolean applies(GameEvent event, Ability source, Game game) {
         MageObject object = game.getObject(event.getSourceId());
         Optional<Ability> ability = game.getAbility(event.getTargetId(), event.getSourceId());
-        if (ability.isPresent()
+        return ability.isPresent()
                 && object != null
                 && object.isCreature()
                 && object.getAbilities().contains(FlyingAbility.getInstance())
-                && game.getState().getPlayersInRange(source.getControllerId(), game).contains(event.getPlayerId())) {
-            return ability.get().getCosts().stream().anyMatch((cost) -> (cost instanceof TapSourceCost));
-        }
-        return false;
+                && game.getState().getPlayersInRange(source.getControllerId(), game).contains(event.getPlayerId())
+                && ability.get().hasTapCost();
     }
 
     @Override

Mage.Sets/src/mage/cards/m/MagewrightsStone.java
@@ -66,10 +66,8 @@ class HasAbilityWithTapSymbolPredicate implements Predicate<MageObject> {
 
         for (Ability ability : abilities) {
             if ((ability.getAbilityType() == AbilityType.ACTIVATED || ability.getAbilityType() == AbilityType.MANA) && !ability.getCosts().isEmpty()) {
-                for (Cost cost : ability.getCosts()) {
-                    if (cost instanceof TapSourceCost) {
+                if (ability.hasTapCost()) {
                         return true;
-                    }
                 }
             }
         }

Mage.Sets/src/mage/cards/m/ManaWeb.java
@@ -2,15 +2,18 @@
 package mage.cards.m;
 
 import java.util.Objects;
+import java.util.Set;
 import java.util.UUID;
 import mage.Mana;
 import mage.abilities.Ability;
 import mage.abilities.TriggeredAbilityImpl;
 import mage.abilities.effects.OneShotEffect;
 import mage.abilities.mana.ActivatedManaAbilityImpl;
+import mage.abilities.mana.AnyColorLandsProduceManaAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.constants.ManaType;
 import mage.constants.Outcome;
 import mage.constants.Zone;
 import mage.filter.common.FilterLandPermanent;
@@ -61,6 +64,9 @@ class ManaWebTriggeredAbility extends TriggeredAbilityImpl {
 
     @Override
     public boolean checkTrigger(GameEvent event, Game game) {
+        if (game.inCheckPlayableState()) {
+            return false;
+        }
         if (game.getOpponents(controllerId).contains(event.getPlayerId())) {
             Permanent permanent = game.getPermanent(event.getSourceId());
 
@@ -103,34 +109,18 @@ class ManaWebeffect extends OneShotEffect {
 
     @Override
     public boolean apply(Game game, Ability source) {
-        Permanent permanent = null;
-
-        if (game != null && source != null) {
-            permanent = game.getPermanent(getTargetPointer().getFirst(game, source));
-        }
-
-        if (permanent != null && game != null) {
-            Mana mana = new Mana();
-
-            for (ActivatedManaAbilityImpl ability : permanent.getAbilities().getActivatedManaAbilities(Zone.BATTLEFIELD)) {
-                for (Mana netMana : ability.getNetMana(game)) {
-                    mana.add(netMana);
-                }
-            }
-
+        Permanent permanent = game.getPermanent(getTargetPointer().getFirst(game, source));
+        if (permanent != null) {
+            Set<ManaType> manaTypesSource = AnyColorLandsProduceManaAbility.getManaTypesFromPermanent(permanent, game);
             boolean tappedLands = false;
             for (Permanent opponentPermanent : game.getBattlefield().getActivePermanents(filter, permanent.getControllerId(), game)) {
                 if (Objects.equals(opponentPermanent.getControllerId(), permanent.getControllerId())) {
-                    Mana opponentLandMana = new Mana();
-
-                    for (ActivatedManaAbilityImpl ability : opponentPermanent.getAbilities().getAvailableActivatedManaAbilities(Zone.BATTLEFIELD, game)) {
-                        for (Mana netMana : ability.getNetMana(game)) {
-                            opponentLandMana.add(netMana);
+                    Set<ManaType> manaTypes = AnyColorLandsProduceManaAbility.getManaTypesFromPermanent(opponentPermanent, game);
+                    for (ManaType manaType : manaTypes) {
+                        if (manaTypesSource.contains(manaType)) {
+                           tappedLands = opponentPermanent.tap(game) || tappedLands;
+                           break;
                         }
-                    }
-
-                    if (mana.containsAny(opponentLandMana, true)) {
-                        tappedLands = opponentPermanent.tap(game) || tappedLands;
                     }
                 }
             }

Mage.Sets/src/mage/cards/p/Powerleech.java
@@ -3,8 +3,6 @@ package mage.cards.p;
 
 import java.util.UUID;
 import mage.abilities.TriggeredAbilityImpl;
-import mage.abilities.costs.Cost;
-import mage.abilities.costs.common.TapSourceCost;
 import mage.abilities.effects.common.GainLifeEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
@@ -25,7 +23,7 @@ public final class Powerleech extends CardImpl {
     public Powerleech(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{G}{G}");
 
-        // Whenever an artifact an opponent controls becomes tapped or an opponent activates an artifact's ability without {tap} in its activation cost, you gain 1 life.
+        // Whenever an artifact an opponent controls becomes tapped or an opponent activates an artifact's ability without {T} in its activation cost, you gain 1 life.
         this.addAbility(new PowerleechTriggeredAbility());
     }
 
@@ -75,17 +73,8 @@ class PowerleechTriggeredAbility extends TriggeredAbilityImpl {
             if (stackAbility == null) {
                 return false;
             }
-            boolean triggerable = true;
-            for (Cost cost : stackAbility.getCosts()) {
-                if (cost instanceof TapSourceCost) {
-                    triggerable = false;
-                    break;
-                }
-            }
-            if (!triggerable) {
-                return false;
-            }
-            return player.hasOpponent(permanent.getControllerId(), game);
+            return !stackAbility.hasTapCost() 
+                    && player.hasOpponent(permanent.getControllerId(), game);
         }
         if (event.getType() == GameEvent.EventType.TAPPED) {
             Permanent permanent = game.getPermanentOrLKIBattlefield(event.getTargetId());

Mage.Sets/src/mage/cards/s/SerraBestiary.java
@@ -7,7 +7,6 @@ import mage.MageObject;
 import mage.abilities.Ability;
 import mage.abilities.common.BeginningOfUpkeepTriggeredAbility;
 import mage.abilities.common.SimpleStaticAbility;
-import mage.abilities.costs.common.TapSourceCost;
 import mage.abilities.costs.mana.ManaCostsImpl;
 import mage.abilities.effects.ContinuousRuleModifyingEffectImpl;
 import mage.abilities.effects.common.AttachEffect;
@@ -104,16 +103,12 @@ class SerraBestiaryRuleModifyingEffect extends ContinuousRuleModifyingEffectImpl
         }
         MageObject object = game.getObject(event.getSourceId());
         Optional<Ability> ability = game.getAbility(event.getTargetId(), event.getSourceId());
-        if (ability.isPresent()
+        return ability.isPresent()
                 && object != null
                 && object.isCreature()
                 && object.getId().equals(enchantedCreature.getId())
-                && game.getState().getPlayersInRange(source.getControllerId(), game).contains(event.getPlayerId())) {
-            if (ability.get().getCosts().stream().anyMatch((cost) -> (cost instanceof TapSourceCost))) {
-                return true;
-            }
-        }
-        return false;
+                && game.getState().getPlayersInRange(source.getControllerId(), game).contains(event.getPlayerId())
+                && ability.get().hasTapCost();
     }
 
     @Override

Mage.Sets/src/mage/cards/s/SkirgeFamiliar.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 import mage.MageInt;
 import mage.Mana;
 import mage.abilities.costs.common.DiscardCardCost;
+import mage.abilities.dynamicvalue.IntPlusDynamicValue;
+import mage.abilities.dynamicvalue.common.CardsInControllerHandCount;
 import mage.abilities.keyword.FlyingAbility;
 import mage.abilities.mana.SimpleManaAbility;
 import mage.cards.CardImpl;
@@ -29,7 +31,13 @@ public final class SkirgeFamiliar extends CardImpl {
         this.addAbility(FlyingAbility.getInstance());
         
         // Discard a card: Add {B}.
-        this.addAbility(new SimpleManaAbility(Zone.BATTLEFIELD, Mana.BlackMana(1), new DiscardCardCost(false)));
+        // public SimpleManaAbility(Zone zone, Mana mana, Cost cost, DynamicValue netAmount) {
+        this.addAbility(new SimpleManaAbility(Zone.BATTLEFIELD, Mana.BlackMana(1), 
+                new DiscardCardCost(false),
+                // not perfect but for hand cards correct, activated abilities on Battlefield will miss one possible available mana
+                // to solve this we have to do possible mana calculation per pell/ability to use.
+                new IntPlusDynamicValue(-1, CardsInControllerHandCount.instance)
+                ));
     }
 
     public SkirgeFamiliar(final SkirgeFamiliar card) {

Mage.Sets/src/mage/cards/s/SoldeviAdnate.java
@@ -7,6 +7,8 @@ import mage.Mana;
 import mage.ObjectColor;
 import mage.abilities.Ability;
 import mage.abilities.costs.common.SacrificeTargetCost;
+import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.dynamicvalue.common.HighestCMCOfPermanentValue;
 import mage.abilities.dynamicvalue.common.SacrificeCostConvertedMana;
 import mage.abilities.mana.DynamicManaAbility;
 import mage.cards.CardImpl;
@@ -38,8 +40,9 @@ public final class SoldeviAdnate extends CardImpl {
         this.toughness = new MageInt(2);
 
         // {T}, Sacrifice a black or artifact creature: Add an amount of {B} equal to the sacrificed creature's converted mana cost.
-        Ability ability = new DynamicManaAbility(Mana.BlackMana(1), new SacrificeCostConvertedMana("creature"),
-                "add an amount of {B} equal to the sacrificed creature's converted mana cost");
+        Ability ability = new DynamicManaAbility(Mana.BlackMana(1),  new SacrificeCostConvertedMana("creature"), new TapSourceCost(),
+                "add an amount of {B} equal to the sacrificed creature's converted mana cost" , false, 
+                new HighestCMCOfPermanentValue(filter, true));
         ability.addCost(new SacrificeTargetCost(new TargetControlledPermanent(filter)));
         this.addAbility(ability);
     }

commit cbc1c02cde6632db847c29f3a7afb301c981a8cb

* Fixed bug that caused some tests to fail introduced with 67dd45c1c7b52465ddd3586d2b260974b896e349.

Mage/src/main/java/mage/abilities/mana/ManaOptions.java
@@ -94,7 +94,7 @@ public class ManaOptions extends ArrayList<Mana> {
         this.clear();
         for (Mana netMana : netManas) {
             for (Mana mana : copy) {
-                if (ability.hasTapCost() || checkManaReplacementAndTriggeredMana(ability, game, netMana)) {
+                if (!ability.hasTapCost() || checkManaReplacementAndTriggeredMana(ability, game, netMana)) {
                     Mana newMana = new Mana();
                     newMana.add(mana);
                     newMana.add(netMana);