6a369c3ae076a73ea14d8f41f9083ccfb3ef5cdd
Evan Kranzler <theelk801@gmail.com>
2020-09-01 13:39:44 -0400

    Updated ZNR spoiler and reprints

--- Mage.Sets/src/mage/sets/ZendikarRising.java
+++ Mage.Sets/src/mage/sets/ZendikarRising.java
@@ -28,6 +28,7 @@ public final class ZendikarRising extends ExpansionSet {
         this.maxCardNumberInBooster = 280;
 
         cards.add(new SetCardInfo("Jace, Mirror Mage", 63, Rarity.MYTHIC, mage.cards.j.JaceMirrorMage.class));
+        cards.add(new SetCardInfo("Lotus Cobra", 193, Rarity.RARE, mage.cards.l.LotusCobra.class));
         cards.add(new SetCardInfo("Nahiri, Heir of the Ancients", 230, Rarity.MYTHIC, mage.cards.n.NahiriHeirOfTheAncients.class));
     }
 }
