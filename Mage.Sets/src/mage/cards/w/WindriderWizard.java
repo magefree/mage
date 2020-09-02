1220e65d62638956662b6e20534eaedcc98a7e34
Evan Kranzler <theelk801@gmail.com>
2020-09-01 21:41:50 -0400

    [ZNR] Implemented Windrider Wizard

---
+++ Mage.Sets/src/mage/cards/w/WindriderWizard.java
@@ -0,0 +1,56 @@
+package mage.cards.w;
+
+import mage.MageInt;
+import mage.abilities.common.SpellCastControllerTriggeredAbility;
+import mage.abilities.effects.common.DrawDiscardControllerEffect;
+import mage.abilities.keyword.FlyingAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.filter.FilterSpell;
+import mage.filter.predicate.Predicates;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class WindriderWizard extends CardImpl {
+
+    private static final FilterSpell filter = new FilterSpell("an instant, sorcery, or Wizard spell");
+
+    static {
+        filter.add(Predicates.or(
+                CardType.INSTANT.getPredicate(),
+                CardType.SORCERY.getPredicate(),
+                SubType.WIZARD.getPredicate()
+        ));
+    }
+
+    public WindriderWizard(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{2}{U}");
+
+        this.subtype.add(SubType.HUMAN);
+        this.subtype.add(SubType.WIZARD);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(2);
+
+        // Flying
+        this.addAbility(FlyingAbility.getInstance());
+
+        // Whenever you cast an instant, sorcery, or Wizard spell, you may draw a card. If you do, discard a card.
+        this.addAbility(new SpellCastControllerTriggeredAbility(
+                new DrawDiscardControllerEffect(1, 1, true), filter, false
+        ));
+    }
+
+    private WindriderWizard(final WindriderWizard card) {
+        super(card);
+    }
+
+    @Override
+    public WindriderWizard copy() {
+        return new WindriderWizard(this);
+    }
+}
