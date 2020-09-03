b9094b92cb7cf8591f92ef3e55afa7b624f1231b
Evan Kranzler <theelk801@gmail.com>
2020-09-03 15:07:41 -0400

    [ZNR] Implemented Maul of the Skyclaves

---
+++ Mage.Sets/src/mage/cards/m/MaulOfTheSkyclaves.java
@@ -0,0 +1,62 @@
+package mage.cards.m;
+
+import mage.abilities.Ability;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.AttachEffect;
+import mage.abilities.effects.common.continuous.BoostEquippedEffect;
+import mage.abilities.effects.common.continuous.GainAbilityAttachedEffect;
+import mage.abilities.keyword.EquipAbility;
+import mage.abilities.keyword.FirstStrikeAbility;
+import mage.abilities.keyword.FlyingAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.AttachmentType;
+import mage.constants.CardType;
+import mage.constants.Outcome;
+import mage.constants.SubType;
+import mage.target.common.TargetControlledCreaturePermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class MaulOfTheSkyclaves extends CardImpl {
+
+    public MaulOfTheSkyclaves(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT}, "{2}{W}");
+
+        this.subtype.add(SubType.EQUIPMENT);
+
+        // When Maul of the Skyclaves enters the battlefield, attach it to target creature you control.
+        Ability ability = new EntersBattlefieldTriggeredAbility(new AttachEffect(
+                Outcome.BoostCreature, "attach it to target creature you control"
+        ), false);
+        ability.addTarget(new TargetControlledCreaturePermanent());
+        this.addAbility(ability);
+
+        // Equipped creature gets +2/+2 and has flying and first strike.
+        ability = new SimpleStaticAbility(new BoostEquippedEffect(2, 2));
+        ability.addEffect(new GainAbilityAttachedEffect(
+                FlyingAbility.getInstance(), AttachmentType.EQUIPMENT
+        ).setText("and has flying"));
+        ability.addEffect(new GainAbilityAttachedEffect(
+                FirstStrikeAbility.getInstance(), AttachmentType.EQUIPMENT
+        ).setText("and first strike"));
+        this.addAbility(ability);
+
+        // Equip {2}{W}{W}
+        this.addAbility(new EquipAbility(Outcome.BoostCreature, new ManaCostsImpl<>("{2}{W}{W}")));
+    }
+
+    private MaulOfTheSkyclaves(final MaulOfTheSkyclaves card) {
+        super(card);
+    }
+
+    @Override
+    public MaulOfTheSkyclaves copy() {
+        return new MaulOfTheSkyclaves(this);
+    }
+}
