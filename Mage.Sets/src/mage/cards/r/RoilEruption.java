5a38e8f1fca3d28901bb98c41ffda0682b727f78
Evan Kranzler <theelk801@gmail.com>
2020-09-04 17:41:42 -0400

    [ZNR] Implemented Roil Eruption

---
+++ Mage.Sets/src/mage/cards/r/RoilEruption.java
@@ -0,0 +1,42 @@
+package mage.cards.r;
+
+import mage.abilities.condition.common.KickedCondition;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.decorator.ConditionalOneShotEffect;
+import mage.abilities.effects.common.DamageTargetEffect;
+import mage.abilities.keyword.KickerAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.target.common.TargetAnyTarget;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class RoilEruption extends CardImpl {
+
+    public RoilEruption(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{1}{R}");
+
+        // Kicker {5}
+        this.addAbility(new KickerAbility(new ManaCostsImpl<>("{5}")));
+
+        // Roil Eruption deals 3 damage to any target. If this spell was kicked, it deals 5 damage instead.
+        this.getSpellAbility().addEffect(new ConditionalOneShotEffect(
+                new DamageTargetEffect(5), new DamageTargetEffect(3), KickedCondition.instance,
+                "{this} deals 3 damage to any target. If this spell was kicked, it deals 5 damage instead"
+        ));
+        this.getSpellAbility().addTarget(new TargetAnyTarget());
+    }
+
+    private RoilEruption(final RoilEruption card) {
+        super(card);
+    }
+
+    @Override
+    public RoilEruption copy() {
+        return new RoilEruption(this);
+    }
+}
