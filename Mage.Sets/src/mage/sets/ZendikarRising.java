159e420e9b1271a45b707a35fd47ba7b9054ccce
Evan Kranzler <theelk801@gmail.com>
2020-09-05 17:14:38 -0400

    [ZNR] Implemented Scorch Rider

--- Mage.Sets/src/mage/sets/ZendikarRising.java
+++ Mage.Sets/src/mage/sets/ZendikarRising.java
@@ -182,6 +182,7 @@ public final class ZendikarRising extends ExpansionSet {
         cards.add(new SetCardInfo("Roil Eruption", 155, Rarity.COMMON, mage.cards.r.RoilEruption.class));
         cards.add(new SetCardInfo("Roiling Regrowth", 201, Rarity.UNCOMMON, mage.cards.r.RoilingRegrowth.class));
         cards.add(new SetCardInfo("Ruin Crab", 75, Rarity.UNCOMMON, mage.cards.r.RuinCrab.class));
+        cards.add(new SetCardInfo("Scorch Rider", 158, Rarity.COMMON, mage.cards.s.ScorchRider.class));
         cards.add(new SetCardInfo("Sea Gate Banneret", 36, Rarity.COMMON, mage.cards.s.SeaGateBanneret.class));
         cards.add(new SetCardInfo("Sea Gate Colossus", 251, Rarity.COMMON, mage.cards.s.SeaGateColossus.class));
         cards.add(new SetCardInfo("Sejiri Glacier", 37, Rarity.UNCOMMON, mage.cards.s.SejiriGlacier.class));
