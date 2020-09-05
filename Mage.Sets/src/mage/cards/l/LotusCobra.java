9c6ac4d58e8cb9340fc24eae645aa48b61750c60
Evan Kranzler <theelk801@gmail.com>
2020-09-05 17:04:41 -0400

    updated Lotus Cobra ability to match errata

--- Mage.Sets/src/mage/cards/l/LotusCobra.java
+++ Mage.Sets/src/mage/cards/l/LotusCobra.java
@@ -1,8 +1,5 @@
-
-
 package mage.cards.l;
 
-import java.util.UUID;
 import mage.MageInt;
 import mage.abilities.common.LandfallAbility;
 import mage.abilities.effects.mana.AddManaOfAnyColorEffect;
@@ -11,21 +8,22 @@ import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.SubType;
 
+import java.util.UUID;
+
 /**
- *
  * @author BetaSteward_at_googlemail.com
  */
 public final class LotusCobra extends CardImpl {
 
     public LotusCobra(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.CREATURE},"{1}{G}");
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{1}{G}");
         this.subtype.add(SubType.SNAKE);
 
         this.power = new MageInt(2);
         this.toughness = new MageInt(1);
 
         // Landfall — Whenever a land enters the battlefield under your control, you may add one mana of any color.
-        this.addAbility(new LandfallAbility(new AddManaOfAnyColorEffect(), true));
+        this.addAbility(new LandfallAbility(new AddManaOfAnyColorEffect(), false));
     }
 
     public LotusCobra(final LotusCobra card) {
@@ -36,5 +34,4 @@ public final class LotusCobra extends CardImpl {
     public LotusCobra copy() {
         return new LotusCobra(this);
     }
-
 }
