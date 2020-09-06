ce9cf54a85b5a2a6a5890c667301400d2863061a
Evan Kranzler <theelk801@gmail.com>
2020-09-05 20:32:20 -0400

    [ZNR] Implemented Veteran Adventurer

--- Mage.Sets/src/mage/sets/ZendikarRising.java
+++ Mage.Sets/src/mage/sets/ZendikarRising.java
@@ -213,6 +213,7 @@ public final class ZendikarRising extends ExpansionSet {
         cards.add(new SetCardInfo("Umara Skyfalls", 86, Rarity.UNCOMMON, mage.cards.u.UmaraSkyfalls.class));
         cards.add(new SetCardInfo("Umara Wizard", 86, Rarity.UNCOMMON, mage.cards.u.UmaraWizard.class));
         cards.add(new SetCardInfo("Utility Knife", 256, Rarity.COMMON, mage.cards.u.UtilityKnife.class));
+        cards.add(new SetCardInfo("Veteran Adventurer", 218, Rarity.UNCOMMON, mage.cards.v.VeteranAdventurer.class));
         cards.add(new SetCardInfo("Windrider Wizard", 87, Rarity.UNCOMMON, mage.cards.w.WindriderWizard.class));
         cards.add(new SetCardInfo("Zulaport Duelist", 88, Rarity.COMMON, mage.cards.z.ZulaportDuelist.class));
 
