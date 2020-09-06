a72fa79007c69cc5611839babb9de6309eafc93f
Evan Kranzler <theelk801@gmail.com>
2020-09-06 14:50:59 -0400

    [ZNR] Implemented Magmatic Channeler

--- Mage.Sets/src/mage/sets/ZendikarRising.java
+++ Mage.Sets/src/mage/sets/ZendikarRising.java
@@ -162,6 +162,7 @@ public final class ZendikarRising extends ExpansionSet {
         cards.add(new SetCardInfo("Lotus Cobra", 193, Rarity.RARE, mage.cards.l.LotusCobra.class));
         cards.add(new SetCardInfo("Lullmage's Familiar", 227, Rarity.UNCOMMON, mage.cards.l.LullmagesFamiliar.class));
         cards.add(new SetCardInfo("Luminarch Aspirant", 24, Rarity.RARE, mage.cards.l.LuminarchAspirant.class));
+        cards.add(new SetCardInfo("Magmatic Channeler", 148, Rarity.RARE, mage.cards.m.MagmaticChanneler.class));
         cards.add(new SetCardInfo("Makindi Mesas", 26, Rarity.UNCOMMON, mage.cards.m.MakindiMesas.class));
         cards.add(new SetCardInfo("Makindi Ox", 25, Rarity.COMMON, mage.cards.m.MakindiOx.class));
         cards.add(new SetCardInfo("Makindi Stampede", 26, Rarity.UNCOMMON, mage.cards.m.MakindiStampede.class));
