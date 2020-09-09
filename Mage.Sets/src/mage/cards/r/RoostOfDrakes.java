f0750d016b5f2cc0cb66c70a044f055dc9803b7a
Evan Kranzler <theelk801@gmail.com>
2020-09-09 11:18:14 -0400

    [ZNR] Implemented Roost of Drakes

---
+++ Mage.Sets/src/mage/cards/r/RoostOfDrakes.java
@@ -0,0 +1,57 @@
+package mage.cards.r;
+
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.common.SpellCastControllerTriggeredAbility;
+import mage.abilities.condition.common.KickedCondition;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.decorator.ConditionalInterveningIfTriggeredAbility;
+import mage.abilities.effects.common.CreateTokenEffect;
+import mage.abilities.keyword.KickerAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.filter.FilterSpell;
+import mage.filter.predicate.mageobject.KickedPredicate;
+import mage.game.permanent.token.DrakeToken;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class RoostOfDrakes extends CardImpl {
+
+    private static final FilterSpell filter = new FilterSpell("a kicked spell");
+
+    static {
+        filter.add(KickedPredicate.instance);
+    }
+
+    public RoostOfDrakes(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{U}");
+
+        // Kicker {2}{U}
+        this.addAbility(new KickerAbility(new ManaCostsImpl<>("{2}{U}")));
+
+        // When Roost of Drakes enters the battlefield, if it was kicked, create a 2/2 blue Drake creature token with flying.
+        this.addAbility(new ConditionalInterveningIfTriggeredAbility(
+                new EntersBattlefieldTriggeredAbility(new CreateTokenEffect(new DrakeToken())),
+                KickedCondition.instance, "When {this} enters the battlefield, if it was kicked, " +
+                "create a 2/2 blue Drake creature token with flying."
+        ));
+
+        // Whenever you cast a kicked spell, create a 2/2 blue Drake creature token with flying.
+        this.addAbility(new SpellCastControllerTriggeredAbility(
+                new CreateTokenEffect(new DrakeToken()), filter, false
+        ));
+    }
+
+    private RoostOfDrakes(final RoostOfDrakes card) {
+        super(card);
+    }
+
+    @Override
+    public RoostOfDrakes copy() {
+        return new RoostOfDrakes(this);
+    }
+}
