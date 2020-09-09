233e12873ec4ea26367212f24743425ffe13d052
Oleg Agafonov <jaydi85@gmail.com>
2020-09-09 16:42:19 +0400

    Fixed verify test

--- Mage.Verify/src/test/java/mage/verify/VerifyCardDataTest.java
+++ Mage.Verify/src/test/java/mage/verify/VerifyCardDataTest.java
@@ -104,6 +104,7 @@ public class VerifyCardDataTest {
         skipListCreate(SKIP_LIST_SUBTYPE);
         skipListAddName(SKIP_LIST_SUBTYPE, "UGL", "Miss Demeanor");
         skipListAddName(SKIP_LIST_SUBTYPE, "ZNR", "Veteran Adventurer"); // TODO: additional types must be added by effect, not direct?
+        skipListAddName(SKIP_LIST_SUBTYPE, "ZNR", "Stonework Packbeast"); // TODO: additional types must be added by effect, not direct?
 
         // number
         skipListCreate(SKIP_LIST_NUMBER);
