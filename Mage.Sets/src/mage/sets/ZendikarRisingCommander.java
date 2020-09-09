aa74c80eda42095291f59927529ee0b0a627a410
Evan Kranzler <theelk801@gmail.com>
2020-09-09 16:34:17 -0400

    [ZNC] Implemented Enigma Thief

--- Mage.Sets/src/mage/sets/ZendikarRisingCommander.java
+++ Mage.Sets/src/mage/sets/ZendikarRisingCommander.java
@@ -1,6 +1,7 @@
 package mage.sets;
 
 import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
 import mage.constants.SetType;
 
 /**
@@ -16,5 +17,7 @@ public final class ZendikarRisingCommander extends ExpansionSet {
 
     private ZendikarRisingCommander() {
         super("Zendikar Rising Commander", "ZNC", ExpansionSet.buildDate(2020, 9, 25), SetType.SUPPLEMENTAL);
+
+        cards.add(new SetCardInfo("Enigma Thief", 4, Rarity.RARE, mage.cards.e.EnigmaThief.class));
     }
 }
