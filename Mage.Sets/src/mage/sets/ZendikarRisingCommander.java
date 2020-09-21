18ba6d23e8557ebc8db4e551c55e72d7b7c475b1
Evan Kranzler <theelk801@gmail.com>
2020-09-21 18:26:21 -0400

    [ZNC] small rarity fix

--- Mage.Sets/src/mage/sets/ZendikarRisingCommander.java
+++ Mage.Sets/src/mage/sets/ZendikarRisingCommander.java
@@ -23,7 +23,7 @@ public final class ZendikarRisingCommander extends ExpansionSet {
         cards.add(new SetCardInfo("Enigma Thief", 4, Rarity.RARE, mage.cards.e.EnigmaThief.class));
         cards.add(new SetCardInfo("Geode Rager", 6, Rarity.RARE, mage.cards.g.GeodeRager.class));
         cards.add(new SetCardInfo("Obuun, Mul Daya Ancestor", 2, Rarity.MYTHIC, mage.cards.o.ObuunMulDayaAncestor.class));
-        cards.add(new SetCardInfo("Trove Warden", 3, Rarity.MYTHIC, mage.cards.t.TroveWarden.class));
+        cards.add(new SetCardInfo("Trove Warden", 3, Rarity.RARE, mage.cards.t.TroveWarden.class));
         cards.add(new SetCardInfo("Whispersteel Dagger", 5, Rarity.RARE, mage.cards.w.WhispersteelDagger.class));
     }
 }
