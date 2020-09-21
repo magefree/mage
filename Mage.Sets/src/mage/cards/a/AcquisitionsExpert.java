120570df79b91b1bfa0f5fcb639d87c4b3f1c634
Evan Kranzler <theelk801@gmail.com>
2020-09-21 17:17:24 -0400

    [ZNR] fixed Acquisitions Expert text (#7044)

--- Mage.Sets/src/mage/cards/a/AcquisitionsExpert.java
+++ Mage.Sets/src/mage/cards/a/AcquisitionsExpert.java
@@ -1,20 +1,19 @@
 package mage.cards.a;
 
-import java.util.UUID;
-
 import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.EntersBattlefieldTriggeredAbility;
 import mage.abilities.dynamicvalue.common.PartyCount;
-import mage.abilities.dynamicvalue.common.PermanentsOnBattlefieldCount;
 import mage.abilities.effects.common.discard.DiscardCardYouChooseTargetEffect;
 import mage.abilities.hint.common.PartyCountHint;
-import mage.constants.SubType;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.constants.SubType;
 import mage.constants.TargetController;
 import mage.target.common.TargetOpponent;
+
+import java.util.UUID;
 
 /**
  * @author TheElk801
@@ -32,6 +31,9 @@ public final class AcquisitionsExpert extends CardImpl {
         // When Acquisitions Expert enters the battlefield, target opponent reveals a number of cards from their hand equal to the number of creatures in your party. You choose one of those cards. That player discards that card.
         Ability ability = new EntersBattlefieldTriggeredAbility(
                 new DiscardCardYouChooseTargetEffect(TargetController.ANY, PartyCount.instance)
+                        .setText("target opponent reveals a number of cards from their hand " +
+                                "equal to the number of creatures in your party. You choose one of those cards. " +
+                                "That player discards that card." + PartyCount.getReminder())
         );
         ability.addTarget(new TargetOpponent());
         this.addAbility(ability.addHint(PartyCountHint.instance));
