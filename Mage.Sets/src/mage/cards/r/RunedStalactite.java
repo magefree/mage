1c87551916977cdcda1a85031d1fa52ba4383aac
Evan Kranzler <theelk801@gmail.com>
2020-09-30 17:36:49 -0400

    updated effects which give all creature types to attached creatures

--- Mage.Sets/src/mage/cards/r/RunedStalactite.java
+++ Mage.Sets/src/mage/cards/r/RunedStalactite.java
@@ -1,38 +1,33 @@
-
 package mage.cards.r;
 
-import java.util.UUID;
+import mage.abilities.Ability;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.costs.mana.ManaCostsImpl;
-import mage.abilities.effects.Effect;
 import mage.abilities.effects.common.continuous.BoostEquippedEffect;
-import mage.abilities.effects.common.continuous.GainAbilityAttachedEffect;
-import mage.abilities.keyword.ChangelingAbility;
+import mage.abilities.effects.common.continuous.GainAllCreatureTypesAttachedEffect;
 import mage.abilities.keyword.EquipAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.constants.AttachmentType;
 import mage.constants.CardType;
-import mage.constants.SubType;
-import mage.constants.Duration;
 import mage.constants.Outcome;
-import mage.constants.Zone;
+import mage.constants.SubType;
+
+import java.util.UUID;
 
 /**
- *
  * @author LevelX2
  */
 public final class RunedStalactite extends CardImpl {
 
     public RunedStalactite(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.ARTIFACT},"{1}");
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT}, "{1}");
         this.subtype.add(SubType.EQUIPMENT);
 
         // Equipped creature gets +1/+1 and is every creature type.
-        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new BoostEquippedEffect(1,1)));
-        Effect effect = new GainAbilityAttachedEffect(ChangelingAbility.getInstance(), AttachmentType.EQUIPMENT, Duration.WhileOnBattlefield);
-        effect.setText("Equipped creature is every creature type (Changeling)");
-        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, effect));
+        Ability ability = new SimpleStaticAbility(new BoostEquippedEffect(1, 1));
+        ability.addEffect(new GainAllCreatureTypesAttachedEffect());
+        this.addAbility(ability);
+
         // Equip {2}
         this.addAbility(new EquipAbility(Outcome.BoostCreature, new ManaCostsImpl("{2}")));
     }
