c2e0a5e8e609db261c89ee8ec7840695e5e51608
Evan Kranzler <theelk801@gmail.com>
2020-10-30 19:28:19 -0400

    [CMR] Implemented Numa, Joraga Chieftain

--- Mage.Sets/src/mage/sets/CommanderLegends.java
+++ Mage.Sets/src/mage/sets/CommanderLegends.java
@@ -103,6 +103,7 @@ public final class CommanderLegends extends ExpansionSet {
         cards.add(new SetCardInfo("Nekusar, the Mindrazer", 529, Rarity.MYTHIC, mage.cards.n.NekusarTheMindrazer.class));
         cards.add(new SetCardInfo("Ninth Bridge Patrol", 33, Rarity.COMMON, mage.cards.n.NinthBridgePatrol.class));
         cards.add(new SetCardInfo("Null Caller", 140, Rarity.UNCOMMON, mage.cards.n.NullCaller.class));
+        cards.add(new SetCardInfo("Numa, Joraga Chieftain", 246, Rarity.UNCOMMON, mage.cards.n.NumaJoragaChieftain.class));
         cards.add(new SetCardInfo("Nymris, Oona's Trickster", 288, Rarity.RARE, mage.cards.n.NymrisOonasTrickster.class));
         cards.add(new SetCardInfo("Omenspeaker", 83, Rarity.COMMON, mage.cards.o.Omenspeaker.class));
         cards.add(new SetCardInfo("Path of Ancestry", 353, Rarity.COMMON, mage.cards.p.PathOfAncestry.class));
