04837ff0c4df678e60d7f10d7ade80431648b1b9
Evan Kranzler <theelk801@gmail.com>
2020-10-31 19:01:19 -0400

    [CMR] Implemented Keskit, the Flesh Sculptor

---
+++ Mage.Sets/src/mage/cards/k/KeskitTheFleshSculptor.java
@@ -0,0 +1,70 @@
+package mage.cards.k;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.SacrificeTargetCost;
+import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.dynamicvalue.common.StaticValue;
+import mage.abilities.effects.common.LookLibraryAndPickControllerEffect;
+import mage.abilities.keyword.PartnerAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.constants.SuperType;
+import mage.constants.Zone;
+import mage.filter.StaticFilters;
+import mage.filter.common.FilterControlledPermanent;
+import mage.filter.predicate.Predicates;
+import mage.filter.predicate.permanent.AnotherPredicate;
+import mage.target.common.TargetControlledPermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class KeskitTheFleshSculptor extends CardImpl {
+
+    private static final FilterControlledPermanent filter
+            = new FilterControlledPermanent("three other artifacts and/or creatures");
+
+    static {
+        filter.add(AnotherPredicate.instance);
+        filter.add(Predicates.or(
+                CardType.ARTIFACT.getPredicate(),
+                CardType.CREATURE.getPredicate()
+        ));
+    }
+
+    public KeskitTheFleshSculptor(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{2}{B}");
+
+        this.addSuperType(SuperType.LEGENDARY);
+        this.subtype.add(SubType.HUMAN);
+        this.subtype.add(SubType.ARTIFICER);
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(3);
+
+        // {T}, Sacrifice three other artifacts and/or creatures: Look at the top three cards of your library. Put two of them into your hand and the other into your graveyard.
+        Ability ability = new SimpleActivatedAbility(new LookLibraryAndPickControllerEffect(
+                StaticValue.get(3), false, StaticValue.get(2), StaticFilters.FILTER_CARD,
+                Zone.GRAVEYARD, false, false
+        ), new TapSourceCost());
+        ability.addCost(new SacrificeTargetCost(new TargetControlledPermanent(3, filter)));
+        this.addAbility(ability);
+
+        // Partner
+        this.addAbility(PartnerAbility.getInstance());
+    }
+
+    private KeskitTheFleshSculptor(final KeskitTheFleshSculptor card) {
+        super(card);
+    }
+
+    @Override
+    public KeskitTheFleshSculptor copy() {
+        return new KeskitTheFleshSculptor(this);
+    }
+}
--- Mage.Sets/src/mage/sets/CommanderLegends.java
+++ Mage.Sets/src/mage/sets/CommanderLegends.java
@@ -98,6 +98,7 @@ public final class CommanderLegends extends ExpansionSet {
         cards.add(new SetCardInfo("Kediss, Emberclaw Familiar", 188, Rarity.UNCOMMON, mage.cards.k.KedissEmberclawFamiliar.class));
         cards.add(new SetCardInfo("Keeper of the Accord", 27, Rarity.RARE, mage.cards.k.KeeperOfTheAccord.class));
         cards.add(new SetCardInfo("Keleth, Sunmane Familiar", 28, Rarity.UNCOMMON, mage.cards.k.KelethSunmaneFamiliar.class));
+        cards.add(new SetCardInfo("Keskit, the Flesh Sculptor", 131, Rarity.UNCOMMON, mage.cards.k.KeskitTheFleshSculptor.class));
         cards.add(new SetCardInfo("Kinsbaile Courier", 29, Rarity.COMMON, mage.cards.k.KinsbaileCourier.class));
         cards.add(new SetCardInfo("Kitesail Corsair", 76, Rarity.COMMON, mage.cards.k.KitesailCorsair.class));
         cards.add(new SetCardInfo("Kitesail Skirmisher", 77, Rarity.COMMON, mage.cards.k.KitesailSkirmisher.class));
