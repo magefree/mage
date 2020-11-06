e7fbfe0d8e1c1a9fd8fbaa791188db45d6411b56
Evan Kranzler <theelk801@gmail.com>
2020-11-06 07:05:16 -0500

    [CMR] updated spoiler and reprints

--- Mage.Sets/src/mage/cards/b/BoardingParty.java
+++ Mage.Sets/src/mage/cards/b/BoardingParty.java
@@ -1,5 +1,6 @@
 package mage.cards.b;
 
+import mage.MageInt;
 import mage.abilities.keyword.CascadeAbility;
 import mage.abilities.keyword.HasteAbility;
 import mage.cards.CardImpl;
@@ -19,6 +20,8 @@ public final class BoardingParty extends CardImpl {
 
         this.subtype.add(SubType.HUMAN);
         this.subtype.add(SubType.PIRATE);
+        this.power = new MageInt(6);
+        this.toughness = new MageInt(3);
 
         // Haste
         this.addAbility(HasteAbility.getInstance());
