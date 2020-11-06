6c5339c737e2b94d112001ccc35d2b74c4569af9
Evan Kranzler <theelk801@gmail.com>
2020-11-06 08:05:18 -0500

    [CMR] Implemented Feast of Succession

---
+++ Mage.Sets/src/mage/cards/f/FeastOfSuccession.java
@@ -0,0 +1,33 @@
+package mage.cards.f;
+
+import mage.abilities.effects.common.BecomesMonarchSourceEffect;
+import mage.abilities.effects.common.continuous.BoostAllEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class FeastOfSuccession extends CardImpl {
+
+    public FeastOfSuccession(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{4}{B}{B}");
+
+        // All creatures get -4/-4 until end of turn. You become the monarch.
+        this.getSpellAbility().addEffect(new BoostAllEffect(-4, -4, Duration.EndOfTurn));
+        this.getSpellAbility().addEffect(new BecomesMonarchSourceEffect().setText("You become the monarch"));
+    }
+
+    private FeastOfSuccession(final FeastOfSuccession card) {
+        super(card);
+    }
+
+    @Override
+    public FeastOfSuccession copy() {
+        return new FeastOfSuccession(this);
+    }
+}
