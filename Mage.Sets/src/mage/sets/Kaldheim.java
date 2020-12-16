6e80d11d75fb550277a920b0d7a7a8a66f9bded3
Evan Kranzler <theelk801@gmail.com>
2020-12-15 20:47:19 -0500

    [KHM] added set and spoiler

---
+++ Mage.Sets/src/mage/sets/Kaldheim.java
@@ -0,0 +1,32 @@
+package mage.sets;
+
+import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
+import mage.constants.SetType;
+
+/**
+ * @author TheElk801
+ */
+public final class Kaldheim extends ExpansionSet {
+
+    private static final Kaldheim instance = new Kaldheim();
+
+    public static Kaldheim getInstance() {
+        return instance;
+    }
+
+    private Kaldheim() {
+        super("Kaldheim", "KHM", ExpansionSet.buildDate(2021, 2, 5), SetType.EXPANSION);
+        this.blockName = "Kaldheim";
+        this.hasBasicLands = true;
+        this.hasBoosters = true;
+        this.numBoosterLands = 1;
+        this.numBoosterCommon = 9;
+        this.numBoosterUncommon = 3;
+        this.numBoosterRare = 1;
+        this.ratioBoosterMythic = 8;
+        this.numBoosterDoubleFaced = 1;
+        this.maxCardNumberInBooster = 285;
+
+    }
+}
