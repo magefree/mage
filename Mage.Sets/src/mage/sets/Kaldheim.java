b379035be4323d97cb3eed9627551a4e27114dee
Daniel Bomar <dbdaniel42@gmail.com>
2020-12-19 12:33:28 -0600

    [KHM] Implemented Armed and Armored

--- Mage.Sets/src/mage/sets/Kaldheim.java
+++ Mage.Sets/src/mage/sets/Kaldheim.java
@@ -29,6 +29,7 @@ public final class Kaldheim extends ExpansionSet {
         this.maxCardNumberInBooster = 285;
 
         cards.add(new SetCardInfo("Absorb Identity", 383, Rarity.UNCOMMON, mage.cards.a.AbsorbIdentity.class));
+        cards.add(new SetCardInfo("Armed and Armored", 379, Rarity.UNCOMMON, mage.cards.a.ArmedAndArmored.class));
         cards.add(new SetCardInfo("Barkchannel Pathway", 251, Rarity.RARE, mage.cards.b.BarkchannelPathway.class));
         cards.add(new SetCardInfo("Bearded Axe", 388, Rarity.UNCOMMON, mage.cards.b.BeardedAxe.class));
         cards.add(new SetCardInfo("Blightstep Pathway", 252, Rarity.RARE, mage.cards.b.BlightstepPathway.class));
