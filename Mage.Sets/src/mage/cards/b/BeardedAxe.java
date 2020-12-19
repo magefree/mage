6d3c682b1b56532eb3c6b90ceab0cbda90724026
Evan Kranzler <theelk801@gmail.com>
2020-12-18 20:30:33 -0500

    [KHM] Implemented Bearded Axe

---
+++ Mage.Sets/src/mage/cards/b/BeardedAxe.java
@@ -0,0 +1,60 @@
+package mage.cards.b;
+
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.dynamicvalue.DynamicValue;
+import mage.abilities.dynamicvalue.common.PermanentsOnBattlefieldCount;
+import mage.abilities.effects.common.continuous.BoostEquippedEffect;
+import mage.abilities.hint.Hint;
+import mage.abilities.hint.ValueHint;
+import mage.abilities.keyword.EquipAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.filter.FilterPermanent;
+import mage.filter.common.FilterControlledPermanent;
+import mage.filter.predicate.Predicates;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class BeardedAxe extends CardImpl {
+
+    private static final FilterPermanent filter = new FilterControlledPermanent();
+
+    static {
+        filter.add(Predicates.or(
+                SubType.DWARF.getPredicate(),
+                SubType.EQUIPMENT.getPredicate(),
+                SubType.VEHICLE.getPredicate()
+        ));
+    }
+
+    private static final DynamicValue xValue = new PermanentsOnBattlefieldCount(filter);
+    private static final Hint hint = new ValueHint("Dwarves, Equipment, and/or Vehicles you control", xValue);
+
+    public BeardedAxe(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT}, "{2}{R}");
+
+        this.subtype.add(SubType.EQUIPMENT);
+
+        // Equipped creature gets +1/+1 for each Dwarf, Equipment, and/or Vehicle you control.
+        this.addAbility(new SimpleStaticAbility(new BoostEquippedEffect(xValue, xValue)
+                .setText("equipped creature gets +1/+1 for each Dwarf, Equipment, and/or Vehicle you control")
+        ).addHint(hint));
+
+        // Equip {2}
+        this.addAbility(new EquipAbility(2));
+    }
+
+    private BeardedAxe(final BeardedAxe card) {
+        super(card);
+    }
+
+    @Override
+    public BeardedAxe copy() {
+        return new BeardedAxe(this);
+    }
+}
--- Mage.Sets/src/mage/sets/Kaldheim.java
+++ Mage.Sets/src/mage/sets/Kaldheim.java
@@ -29,6 +29,7 @@ public final class Kaldheim extends ExpansionSet {
         this.maxCardNumberInBooster = 285;
 
         cards.add(new SetCardInfo("Barkchannel Pathway", 251, Rarity.RARE, mage.cards.b.BarkchannelPathway.class));
+        cards.add(new SetCardInfo("Bearded Axe", 388, Rarity.UNCOMMON, mage.cards.b.BeardedAxe.class));
         cards.add(new SetCardInfo("Blightstep Pathway", 252, Rarity.RARE, mage.cards.b.BlightstepPathway.class));
         cards.add(new SetCardInfo("Canopy Tactician", 378, Rarity.RARE, mage.cards.c.CanopyTactician.class));
         cards.add(new SetCardInfo("Darkbore Pathway", 254, Rarity.RARE, mage.cards.d.DarkborePathway.class));
