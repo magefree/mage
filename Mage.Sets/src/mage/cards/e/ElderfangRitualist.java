7cc8c8322aec447617f2fd8427a892586d8e1abd
Daniel Bomar <dbdaniel42@gmail.com>
2020-12-20 11:43:40 -0600

    [KHM] Implemented Elderfang Ritualist

---
+++ Mage.Sets/src/mage/cards/e/ElderfangRitualist.java
@@ -0,0 +1,51 @@
+package mage.cards.e;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.DiesSourceTriggeredAbility;
+import mage.abilities.effects.common.ReturnFromGraveyardToHandTargetEffect;
+import mage.constants.SubType;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.filter.FilterCard;
+import mage.filter.common.FilterBySubtypeCard;
+import mage.filter.predicate.mageobject.AnotherCardPredicate;
+import mage.target.common.TargetCardInYourGraveyard;
+
+/**
+ *
+ * @author weirddan455
+ */
+public final class ElderfangRitualist extends CardImpl {
+
+    private static final FilterCard filter = new FilterBySubtypeCard(SubType.ELF);
+    static {
+        filter.add(new AnotherCardPredicate());
+        filter.setMessage("another target Elf card from your graveyard");
+    }
+
+    public ElderfangRitualist(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{2}{B}");
+
+        this.subtype.add(SubType.ELF);
+        this.subtype.add(SubType.CLERIC);
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(1);
+
+        // When Elderfang Ritualist dies, return another target Elf card from your graveyard to your hand.
+        Ability ability = new DiesSourceTriggeredAbility(new ReturnFromGraveyardToHandTargetEffect());
+        ability.addTarget(new TargetCardInYourGraveyard(filter));
+        this.addAbility(ability);
+    }
+
+    private ElderfangRitualist(final ElderfangRitualist card) {
+        super(card);
+    }
+
+    @Override
+    public ElderfangRitualist copy() {
+        return new ElderfangRitualist(this);
+    }
+}
