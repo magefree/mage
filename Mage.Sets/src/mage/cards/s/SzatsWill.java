7753ca305e2c96f9acc85204cb03a2d119d8ce6a
Ingmar Goudt <ingmargoudt@gmail.com>
2020-12-26 15:29:54 +0100

    fix for #7295

--- Mage.Sets/src/mage/cards/s/SzatsWill.java
+++ Mage.Sets/src/mage/cards/s/SzatsWill.java
@@ -134,7 +134,7 @@ class SzatsWillEffect extends OneShotEffect {
                 .max()
                 .orElse(0);
         if (maxPower > 0) {
-            new BreedingPitThrullToken().putOntoBattlefield(1, game, source, source.getControllerId()
+            new BreedingPitThrullToken().putOntoBattlefield(maxPower, game, source, source.getControllerId()
             );
         }
         return true;
