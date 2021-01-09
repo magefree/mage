d3cefca7331059e8ea25a97bf7cb58b4bed72926
ciaccona007 <ciaccona007@users.noreply.github.com>
2021-01-09 01:11:16 -0500

    Implement Bretagard Stronghold (#7351)

--- Mage.Sets/src/mage/sets/Kaldheim.java
+++ Mage.Sets/src/mage/sets/Kaldheim.java
@@ -50,6 +50,7 @@ public final class Kaldheim extends ExpansionSet {
         cards.add(new SetCardInfo("Behold the Multiverse", 46, Rarity.COMMON, mage.cards.b.BeholdTheMultiverse.class));
         cards.add(new SetCardInfo("Binding the Old Gods", 206, Rarity.UNCOMMON, mage.cards.b.BindingTheOldGods.class));
         cards.add(new SetCardInfo("Blightstep Pathway", 252, Rarity.RARE, mage.cards.b.BlightstepPathway.class));
+        cards.add(new SetCardInfo("Bretagard Stronghold", 253, Rarity.UNCOMMON, mage.cards.b.BretagardStronghold.class));
         cards.add(new SetCardInfo("Canopy Tactician", 378, Rarity.RARE, mage.cards.c.CanopyTactician.class));
         cards.add(new SetCardInfo("Cleaving Reaper", 376, Rarity.RARE, mage.cards.c.CleavingReaper.class));
         cards.add(new SetCardInfo("Darkbore Pathway", 254, Rarity.RARE, mage.cards.d.DarkborePathway.class));
