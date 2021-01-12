599cc2673c6eb2cfe1032dbcd1bdafbc0429388a
Evan Kranzler <theelk801@gmail.com>
2021-01-12 08:41:11 -0500

    [KHM] Implemented Harald, King of Skemfar

---
+++ Mage.Sets/src/mage/cards/h/HaraldKingOfSkemfar.java
@@ -0,0 +1,63 @@
+package mage.cards.h;
+
+import mage.MageInt;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.dynamicvalue.common.StaticValue;
+import mage.abilities.effects.common.LookLibraryAndPickControllerEffect;
+import mage.abilities.keyword.MenaceAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.constants.SuperType;
+import mage.constants.Zone;
+import mage.filter.FilterCard;
+import mage.filter.predicate.Predicates;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class HaraldKingOfSkemfar extends CardImpl {
+
+    private static final FilterCard filter = new FilterCard("an Elf, Warrior, or Tyvar card");
+
+    static {
+        filter.add(Predicates.or(
+                SubType.ELF.getPredicate(),
+                SubType.WARRIOR.getPredicate(),
+                SubType.TYVAR.getPredicate()
+        ));
+    }
+
+    public HaraldKingOfSkemfar(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{1}{B}{G}");
+
+        this.addSuperType(SuperType.LEGENDARY);
+        this.subtype.add(SubType.ELF);
+        this.subtype.add(SubType.WARRIOR);
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(2);
+
+        // Menace
+        this.addAbility(new MenaceAbility());
+
+        // When Harald, King of Skemfar enters the battlefield, look at the top five cards of your library. You may reveal an Elf, Warrior, or Tyvar card from among them and put it into your hand. Put the rest on the bottom of your library in a random order.
+        this.addAbility(new EntersBattlefieldTriggeredAbility(new LookLibraryAndPickControllerEffect(
+                StaticValue.get(5), false, StaticValue.get(1), filter, Zone.LIBRARY, false,
+                true, false, Zone.HAND, true, false, false
+        ).setBackInRandomOrder(true).setText("look at the top five cards of your library." +
+                " You may reveal an Elf, Warrior, or Tyvar card from among them and put it into your hand. " +
+                "Put the rest on the bottom of your library in a random order")));
+    }
+
+    private HaraldKingOfSkemfar(final HaraldKingOfSkemfar card) {
+        super(card);
+    }
+
+    @Override
+    public HaraldKingOfSkemfar copy() {
+        return new HaraldKingOfSkemfar(this);
+    }
+}
