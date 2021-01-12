d515496b6e0cc925af9c4402caaabd7dfb9d854a
Evan Kranzler <theelk801@gmail.com>
2021-01-12 17:28:52 -0500

    [KHM] Implemented Skemfar Shadowsage

--- Mage.Sets/src/mage/cards/l/LittjaraKinseekers.java
+++ Mage.Sets/src/mage/cards/l/LittjaraKinseekers.java
@@ -1,11 +1,11 @@
 package mage.cards.l;
 
 import mage.MageInt;
-import mage.MageObject;
 import mage.abilities.Ability;
 import mage.abilities.common.EntersBattlefieldTriggeredAbility;
 import mage.abilities.condition.Condition;
 import mage.abilities.decorator.ConditionalInterveningIfTriggeredAbility;
+import mage.abilities.dynamicvalue.common.GreatestSharedCreatureTypeCount;
 import mage.abilities.effects.common.counter.AddCountersSourceEffect;
 import mage.abilities.effects.keyword.ScryEffect;
 import mage.abilities.keyword.ChangelingAbility;
@@ -14,11 +14,9 @@ import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.SubType;
 import mage.counters.CounterType;
-import mage.filter.StaticFilters;
 import mage.game.Game;
-import mage.game.permanent.Permanent;
 
-import java.util.*;
+import java.util.UUID;
 
 /**
  * @author TheElk801
@@ -45,7 +43,7 @@ public final class LittjaraKinseekers extends CardImpl {
                 "put a +1/+1 counter on {this}, then scry 1."
         );
         ability.addEffect(new ScryEffect(1));
-        this.addAbility(ability);
+        this.addAbility(ability.addHint(GreatestSharedCreatureTypeCount.getHint()));
     }
 
     private LittjaraKinseekers(final LittjaraKinseekers card) {
@@ -63,26 +61,6 @@ enum LittjaraKinseekersCondition implements Condition {
 
     @Override
     public boolean apply(Game game, Ability source) {
-        List<Permanent> permanentList = game.getBattlefield().getActivePermanents(
-                StaticFilters.FILTER_CONTROLLED_CREATURE,
-                source.getControllerId(), source.getSourceId(), game
-        );
-        permanentList.removeIf(Objects::isNull);
-        long changelings = permanentList
-                .stream()
-                .filter(Objects::nonNull)
-                .filter(MageObject::isAllCreatureTypes)
-                .count();
-        if (changelings > 2) {
-            return true;
-        }
-        permanentList.removeIf(MageObject::isAllCreatureTypes);
-        Map<SubType, Integer> typeMap = new HashMap<>();
-        return permanentList
-                .stream()
-                .map(permanent -> permanent.getSubtype(game))
-                .flatMap(Collection::stream)
-                .mapToInt(subType -> typeMap.compute(subType, (s, i) -> i == null ? 1 : Integer.sum(i, 1)))
-                .anyMatch(x -> x + changelings > 2);
+        return GreatestSharedCreatureTypeCount.instance.calculate(game, source, null) >= 3;
     }
 }
---
+++ Mage/src/main/java/mage/abilities/dynamicvalue/common/GreatestSharedCreatureTypeCount.java
@@ -0,0 +1,75 @@
+package mage.abilities.dynamicvalue.common;
+
+import mage.MageObject;
+import mage.abilities.Ability;
+import mage.abilities.dynamicvalue.DynamicValue;
+import mage.abilities.effects.Effect;
+import mage.abilities.hint.Hint;
+import mage.abilities.hint.ValueHint;
+import mage.constants.SubType;
+import mage.constants.SubTypeSet;
+import mage.filter.StaticFilters;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+
+import java.util.*;
+
+/**
+ * @author TheElk801
+ */
+public enum GreatestSharedCreatureTypeCount implements DynamicValue {
+    instance;
+
+    private static final Hint hint = new ValueHint(
+            "Greatest number of creatures you control that share a creature type", instance
+    );
+
+    @Override
+    public int calculate(Game game, Ability sourceAbility, Effect effect) {
+        List<Permanent> permanentList = game.getBattlefield().getActivePermanents(
+                StaticFilters.FILTER_CONTROLLED_CREATURE,
+                sourceAbility.getControllerId(), sourceAbility.getSourceId(), game
+        );
+        permanentList.removeIf(Objects::isNull);
+        int changelings = permanentList
+                .stream()
+                .filter(Objects::nonNull)
+                .filter(MageObject::isAllCreatureTypes)
+                .mapToInt(x -> 1)
+                .sum();
+        permanentList.removeIf(MageObject::isAllCreatureTypes);
+        Map<SubType, Integer> typeMap = new HashMap<>();
+        permanentList
+                .stream()
+                .map(permanent -> permanent.getSubtype(game))
+                .flatMap(Collection::stream)
+                .filter(subType -> subType.getSubTypeSet() == SubTypeSet.CreatureType)
+                .forEach(subType -> typeMap.compute(subType, (s, i) -> i == null ? 1 : Integer.sum(i, 1)));
+        return changelings
+                + typeMap
+                .values()
+                .stream()
+                .mapToInt(x -> x)
+                .max()
+                .getAsInt();
+    }
+
+    @Override
+    public GreatestSharedCreatureTypeCount copy() {
+        return instance;
+    }
+
+    @Override
+    public String toString() {
+        return "X";
+    }
+
+    @Override
+    public String getMessage() {
+        return "greatest number of creatures you control that have a creature type in common";
+    }
+
+    public static Hint getHint() {
+        return hint;
+    }
+}
