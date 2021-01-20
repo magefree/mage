02df4c480af145cee69620007ee2aa30e1d5a404
Daniel Bomar <dbdaniel42@gmail.com>
2021-01-19 18:26:34 -0600

    Fixed Release to the Wind not allowing to cast the exiled permanent

--- Mage/src/main/java/mage/abilities/effects/common/asthought/PlayFromNotOwnHandZoneTargetEffect.java
+++ Mage/src/main/java/mage/abilities/effects/common/asthought/PlayFromNotOwnHandZoneTargetEffect.java
@@ -159,7 +159,7 @@ public class PlayFromNotOwnHandZoneTargetEffect extends AsThoughEffectImpl {
             return false;
         }
         ContinuousEffect effect = new PlayFromNotOwnHandZoneTargetEffect(Zone.EXILED, allowedCaster, duration, withoutMana);
-        effect.setTargetPointer(new FixedTargets(cards, game));
+        effect.setTargetPointer(new FixedTargets(game.getExile().getExileZone(exileId), game));
         game.addEffect(effect, source);
         return true;
     }
