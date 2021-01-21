68807ff34fde4f65455b6993449e3c68477f8cc7
Evan Kranzler <theelk801@gmail.com>
2021-01-21 12:48:08 -0500

    [KHM] fixed Battershield Warrior missing P/T (fixes #7423)

--- Mage.Sets/src/mage/cards/b/BattershieldWarrior.java
+++ Mage.Sets/src/mage/cards/b/BattershieldWarrior.java
@@ -1,5 +1,6 @@
 package mage.cards.b;
 
+import mage.MageInt;
 import mage.abilities.effects.common.continuous.BoostControlledEffect;
 import mage.abilities.keyword.BoastAbility;
 import mage.cards.CardImpl;
@@ -20,6 +21,8 @@ public final class BattershieldWarrior extends CardImpl {
 
         this.subtype.add(SubType.HUMAN);
         this.subtype.add(SubType.WARRIOR);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(2);
 
         // Boast — {1}{W}: Creatures you control get +1/+1 until end of turn.
         this.addAbility(new BoastAbility(new BoostControlledEffect(
