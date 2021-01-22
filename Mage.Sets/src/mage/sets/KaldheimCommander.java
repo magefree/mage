92bddc1565ff498651084eb0273c5f6871b8678c
Evan Kranzler <theelk801@gmail.com>
2021-01-22 13:17:48 -0500

    [KHC] Implemented Ruthless Winnower

--- Mage.Sets/src/mage/sets/KaldheimCommander.java
+++ Mage.Sets/src/mage/sets/KaldheimCommander.java
@@ -36,6 +36,7 @@ public final class KaldheimCommander extends ExpansionSet {
         cards.add(new SetCardInfo("Elderfang Venom", 15, Rarity.RARE, mage.cards.e.ElderfangVenom.class));
         cards.add(new SetCardInfo("Inspired Sphinx", 40, Rarity.MYTHIC, mage.cards.i.InspiredSphinx.class));
         cards.add(new SetCardInfo("Lathril, Blade of the Elves", 1, Rarity.MYTHIC, mage.cards.l.LathrilBladeOfTheElves.class));
+        cards.add(new SetCardInfo("Ruthless Winnower", 10, Rarity.RARE, mage.cards.r.RuthlessWinnower.class));
         cards.add(new SetCardInfo("Spectral Deluge", 7, Rarity.RARE, mage.cards.s.SpectralDeluge.class));
 
         cards.removeIf(setCardInfo -> unfinished.contains(setCardInfo.getName())); // remove when mechanic is fully implemented
