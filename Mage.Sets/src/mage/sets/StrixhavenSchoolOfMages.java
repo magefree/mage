98c6b3955064faf8e9c3042900873b61efeaa5a9
Evan Kranzler <theelk801@gmail.com>
2021-02-18 19:37:47 -0500

    [STX] added set

---
+++ Mage.Sets/src/mage/sets/StrixhavenSchoolOfMages.java
@@ -0,0 +1,28 @@
+package mage.sets;
+
+import mage.cards.ExpansionSet;
+import mage.constants.SetType;
+
+/**
+ * @author TheElk801
+ */
+public final class StrixhavenSchoolOfMages extends ExpansionSet {
+
+    private static final StrixhavenSchoolOfMages instance = new StrixhavenSchoolOfMages();
+
+    public static StrixhavenSchoolOfMages getInstance() {
+        return instance;
+    }
+
+    private StrixhavenSchoolOfMages() {
+        super("Strixhaven: School of Mages", "STX", ExpansionSet.buildDate(2021, 4, 23), SetType.EXPANSION);
+        this.blockName = "Strixhaven: School of Mages";
+        this.hasBoosters = false;
+        this.numBoosterLands = 1;
+        this.numBoosterCommon = 10;
+        this.numBoosterUncommon = 3;
+        this.numBoosterRare = 1;
+        this.ratioBoosterMythic = 7.4;
+        this.maxCardNumberInBooster = 275;
+    }
+}
