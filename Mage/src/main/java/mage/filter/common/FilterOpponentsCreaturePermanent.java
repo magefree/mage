b6cb5cef9c88cc83146a1fb0bf750ea987fe53ca
Oleg Agafonov <jaydi85@gmail.com>
2021-02-21 08:15:16 +0400

    Refactor: removed default comment headers

--- Mage/src/main/java/mage/filter/common/FilterOpponentsCreaturePermanent.java
+++ Mage/src/main/java/mage/filter/common/FilterOpponentsCreaturePermanent.java
@@ -3,11 +3,6 @@ package mage.filter.common;
 import mage.constants.SubType;
 import mage.constants.TargetController;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author Styxo
