b55e3d52c6fa07783f72d9bf3210de7a45b5c03e
Evan Kranzler <theelk801@gmail.com>
2021-03-05 17:26:40 -0500

    [AKH] fixed Throne of the God-Pharaoh text (fixes #7647)

--- Mage.Sets/src/mage/cards/t/ThroneOfTheGodPharaoh.java
+++ Mage.Sets/src/mage/cards/t/ThroneOfTheGodPharaoh.java
@@ -1,8 +1,7 @@
-
 package mage.cards.t;
 
-import java.util.UUID;
 import mage.abilities.common.BeginningOfEndStepTriggeredAbility;
+import mage.abilities.dynamicvalue.DynamicValue;
 import mage.abilities.dynamicvalue.common.PermanentsOnBattlefieldCount;
 import mage.abilities.effects.common.LoseLifeOpponentsEffect;
 import mage.cards.CardImpl;
@@ -14,17 +13,20 @@ import mage.filter.FilterPermanent;
 import mage.filter.common.FilterControlledCreaturePermanent;
 import mage.filter.predicate.permanent.TappedPredicate;
 
+import java.util.UUID;
+
 /**
- *
  * @author Styxo
  */
 public final class ThroneOfTheGodPharaoh extends CardImpl {
 
-    private static final FilterPermanent filter = new FilterControlledCreaturePermanent("tapped creature you control");
+    private static final FilterPermanent filter = new FilterControlledCreaturePermanent();
 
     static {
         filter.add(TappedPredicate.instance);
     }
+
+    private static final DynamicValue xValue = new PermanentsOnBattlefieldCount(filter);
 
     public ThroneOfTheGodPharaoh(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT}, "{2}");
@@ -32,7 +34,11 @@ public final class ThroneOfTheGodPharaoh extends CardImpl {
         addSuperType(SuperType.LEGENDARY);
 
         // At the beginning of your end step, each opponent loses life equal to the number of tapped creatures you control.
-        this.addAbility(new BeginningOfEndStepTriggeredAbility(new LoseLifeOpponentsEffect(new PermanentsOnBattlefieldCount(filter)), TargetController.YOU, false));
+        this.addAbility(new BeginningOfEndStepTriggeredAbility(
+                new LoseLifeOpponentsEffect(xValue).setText(
+                        "each opponent loses life equal to the number of tapped creatures you control"
+                ), TargetController.YOU, false
+        ));
     }
 
     private ThroneOfTheGodPharaoh(final ThroneOfTheGodPharaoh card) {
