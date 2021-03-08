ebe6f5967b117531265fa2e0f2e39357a3153143
Evan Kranzler <theelk801@gmail.com>
2021-03-08 18:19:53 -0500

    update implementation of Fork to use new applier

--- Mage/src/main/java/mage/util/functions/SpellCopyApplier.java
+++ Mage/src/main/java/mage/util/functions/SpellCopyApplier.java
@@ -1,9 +1,14 @@
 package mage.util.functions;
 
+import mage.game.Game;
+import mage.game.stack.Spell;
+
 import java.io.Serializable;
 
 /**
  * @author TheElk801
  */
 public interface SpellCopyApplier extends Serializable {
+
+    void modifySpell(Spell spell, Game game);
 }
