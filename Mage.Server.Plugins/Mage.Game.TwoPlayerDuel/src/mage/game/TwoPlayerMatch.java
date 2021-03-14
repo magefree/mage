4874ad31c199ea573187ea2790268be3a4d4c95a
Evan Kranzler <theelk801@gmail.com>
2021-03-14 19:05:25 -0400

    [IKO] added workaround for Yorion, Sky Nomad not being usable in a two player match with limited set as deck type

--- Mage.Server.Plugins/Mage.Game.TwoPlayerDuel/src/mage/game/TwoPlayerMatch.java
+++ Mage.Server.Plugins/Mage.Game.TwoPlayerDuel/src/mage/game/TwoPlayerMatch.java
@@ -17,7 +17,7 @@ public class TwoPlayerMatch extends MatchImpl {
     @Override
     public void startGame() throws GameException {
         Mulligan mulligan = options.getMulliganType().getMulligan(options.getFreeMulligans());
-        TwoPlayerDuel game = new TwoPlayerDuel(options.getAttackOption(), options.getRange(), mulligan, 20, options.isLimited() ? 40 : 60);
+        TwoPlayerDuel game = new TwoPlayerDuel(options.getAttackOption(), options.getRange(), mulligan, 20, options.isLimited() || "Limited".equals(options.getDeckType()) ? 40 : 60);
         // Sets a start message about the match score
         game.setStartMessage(this.createGameStartMessage());
         initGame(game);
