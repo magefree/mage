8400127de1ce6208fedf329b8700545a27eab7ec
Evan Kranzler <theelk801@gmail.com>
2021-03-15 09:35:33 -0400

    fixed Twincast targeting any spell (fixes #7674)

--- Mage.Sets/src/mage/cards/t/Twincast.java
+++ Mage.Sets/src/mage/cards/t/Twincast.java
@@ -1,7 +1,5 @@
-
 package mage.cards.t;
 
-import java.util.UUID;
 import mage.abilities.effects.common.CopyTargetSpellEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
@@ -9,18 +7,19 @@ import mage.constants.CardType;
 import mage.filter.StaticFilters;
 import mage.target.TargetSpell;
 
+import java.util.UUID;
+
 /**
- *
  * @author Loki
  */
 public final class Twincast extends CardImpl {
 
     public Twincast(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.INSTANT},"{U}{U}");
+        super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{U}{U}");
 
         // Copy target instant or sorcery spell. You may choose new targets for the copy.
         this.getSpellAbility().addEffect(new CopyTargetSpellEffect());
-        this.getSpellAbility().addTarget(new TargetSpell());
+        this.getSpellAbility().addTarget(new TargetSpell(StaticFilters.FILTER_SPELL_INSTANT_OR_SORCERY));
     }
 
     private Twincast(final Twincast card) {
