754a5b38ebba104b83494629c1f3881020168b1e
Evan Kranzler <theelk801@gmail.com>
2021-04-02 08:12:06 -0400

    [STX] Implemented Access Tunnel

--- Mage.Sets/src/mage/sets/StrixhavenSchoolOfMages.java
+++ Mage.Sets/src/mage/sets/StrixhavenSchoolOfMages.java
@@ -32,6 +32,7 @@ public final class StrixhavenSchoolOfMages extends ExpansionSet {
         this.ratioBoosterMythic = 7.4;
         this.maxCardNumberInBooster = 275;
 
+        cards.add(new SetCardInfo("Access Tunnel", 262, Rarity.UNCOMMON, mage.cards.a.AccessTunnel.class));
         cards.add(new SetCardInfo("Accomplished Alchemist", 119, Rarity.RARE, mage.cards.a.AccomplishedAlchemist.class));
         cards.add(new SetCardInfo("Aether Helix", 162, Rarity.UNCOMMON, mage.cards.a.AetherHelix.class));
         cards.add(new SetCardInfo("Ageless Guardian", 8, Rarity.COMMON, mage.cards.a.AgelessGuardian.class));
