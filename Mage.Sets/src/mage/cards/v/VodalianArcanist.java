d9adbd1627168da3e8f7d0196f7e1d4ae7a6da8d
Evan Kranzler <theelk801@gmail.com>
2021-04-05 20:16:54 -0400

    [STX] Implemented Galazeth Prismari

--- Mage.Sets/src/mage/cards/v/VodalianArcanist.java
+++ Mage.Sets/src/mage/cards/v/VodalianArcanist.java
@@ -1,16 +1,16 @@
-
 package mage.cards.v;
 
-import java.util.UUID;
 import mage.MageInt;
-import mage.abilities.costs.common.TapSourceCost;
 import mage.abilities.mana.ConditionalColorlessManaAbility;
-import mage.constants.SubType;
+import mage.abilities.mana.builder.common.InstantOrSorcerySpellManaBuilder;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.constants.SubType;
+
+import java.util.UUID;
+
 /**
- *
  * @author TheElk801
  */
 public final class VodalianArcanist extends CardImpl {
@@ -24,8 +24,7 @@ public final class VodalianArcanist extends CardImpl {
         this.toughness = new MageInt(3);
 
         // {T}: Add {C}. Spend this mana only to cast an instant or sorcery spell.
-        this.addAbility(new ConditionalColorlessManaAbility(new TapSourceCost(), 1, 
-                new mage.abilities.mana.builder.common.InstantOrSorcerySpellManaBuilder()));
+        this.addAbility(new ConditionalColorlessManaAbility(1, new InstantOrSorcerySpellManaBuilder()));
     }
 
     private VodalianArcanist(final VodalianArcanist card) {
--- Mage.Sets/src/mage/sets/StrixhavenSchoolOfMages.java
+++ Mage.Sets/src/mage/sets/StrixhavenSchoolOfMages.java
@@ -101,6 +101,7 @@ public final class StrixhavenSchoolOfMages extends ExpansionSet {
         cards.add(new SetCardInfo("Frostboil Snarl", 265, Rarity.RARE, mage.cards.f.FrostboilSnarl.class));
         cards.add(new SetCardInfo("Fuming Effigy", 103, Rarity.COMMON, mage.cards.f.FumingEffigy.class));
         cards.add(new SetCardInfo("Furycalm Snarl", 266, Rarity.RARE, mage.cards.f.FurycalmSnarl.class));
+        cards.add(new SetCardInfo("Galazeth Prismari", 189, Rarity.MYTHIC, mage.cards.g.GalazethPrismari.class));
         cards.add(new SetCardInfo("Gnarled Professor", 133, Rarity.RARE, mage.cards.g.GnarledProfessor.class));
         cards.add(new SetCardInfo("Go Blank", 72, Rarity.UNCOMMON, mage.cards.g.GoBlank.class));
         cards.add(new SetCardInfo("Golden Ratio", 190, Rarity.UNCOMMON, mage.cards.g.GoldenRatio.class));
