b7c5bcdbdd089281c95ba5fe5163b75c04eee42e
Evan Kranzler <theelk801@gmail.com>
2021-04-08 20:51:11 -0400

    [C21] Implemented Incarnation Technique

--- Mage.Sets/src/mage/sets/Commander2021Edition.java
+++ Mage.Sets/src/mage/sets/Commander2021Edition.java
@@ -75,6 +75,7 @@ public final class Commander2021Edition extends ExpansionSet {
         cards.add(new SetCardInfo("Hoard-Smelter Dragon", 173, Rarity.RARE, mage.cards.h.HoardSmelterDragon.class));
         cards.add(new SetCardInfo("Humble Defector", 174, Rarity.UNCOMMON, mage.cards.h.HumbleDefector.class));
         cards.add(new SetCardInfo("Ichor Wellspring", 245, Rarity.COMMON, mage.cards.i.IchorWellspring.class));
+        cards.add(new SetCardInfo("Incarnation Technique", 41, Rarity.RARE, mage.cards.i.IncarnationTechnique.class));
         cards.add(new SetCardInfo("Inferno Project", 52, Rarity.RARE, mage.cards.i.InfernoProject.class));
         cards.add(new SetCardInfo("Inspiring Refrain", 27, Rarity.RARE, mage.cards.i.InspiringRefrain.class));
         cards.add(new SetCardInfo("Izzet Boilerworks", 294, Rarity.UNCOMMON, mage.cards.i.IzzetBoilerworks.class));
