d718ebc97f58cb895eeeb4fd8c26e1586302fa43
HT Rajan <htrajan2002@yahoo.com>
2021-04-10 18:16:31 -0700

    [STX] Implement Devouring Tendrils

--- Mage.Sets/src/mage/cards/d/DevouringTendrils.java
+++ Mage.Sets/src/mage/cards/d/DevouringTendrils.java
@@ -1,24 +1,20 @@
 package mage.cards.d;
 
-import mage.MageObjectReference;
-import mage.abilities.Ability;
+import mage.abilities.DelayedTriggeredAbility;
 import mage.abilities.effects.Effect;
-import mage.abilities.effects.OneShotEffect;
-import mage.abilities.effects.ReplacementEffectImpl;
+import mage.abilities.effects.common.CreateDelayedTriggeredAbilityEffect;
 import mage.abilities.effects.common.DamageWithPowerFromOneToAnotherTargetEffect;
+import mage.abilities.effects.common.GainLifeEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.Duration;
-import mage.constants.Outcome;
 import mage.constants.TargetController;
 import mage.filter.FilterPermanent;
 import mage.filter.common.FilterCreatureOrPlaneswalkerPermanent;
 import mage.game.Game;
 import mage.game.events.GameEvent;
 import mage.game.events.ZoneChangeEvent;
-import mage.game.permanent.Permanent;
-import mage.players.Player;
 import mage.target.Target;
 import mage.target.TargetPermanent;
 import mage.target.common.TargetControlledCreaturePermanent;
@@ -48,7 +44,7 @@ public final class DevouringTendrils extends CardImpl {
         target.setTargetTag(2);
         this.getSpellAbility().addTarget(target);
 
-        this.getSpellAbility().addEffect(new DevouringTendrilsEffect());
+        this.getSpellAbility().addEffect(new CreateDelayedTriggeredAbilityEffect(new DevouringTendrilsDelayedTriggeredAbility(), true));
     }
 
     private DevouringTendrils(final DevouringTendrils card) {
@@ -61,74 +57,41 @@ public final class DevouringTendrils extends CardImpl {
     }
 }
 
-class DevouringTendrilsEffect extends OneShotEffect {
+class DevouringTendrilsDelayedTriggeredAbility extends DelayedTriggeredAbility {
 
-    DevouringTendrilsEffect() {
-        super(Outcome.Benefit);
-        staticText = "when the permanent you don't control dies this turn, you gain 2 life";
+    DevouringTendrilsDelayedTriggeredAbility(){
+        super(new GainLifeEffect(2), Duration.EndOfTurn, false);
     }
 
-    DevouringTendrilsEffect(DevouringTendrilsEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Target target = source.getTargets().stream()
-            .filter(t -> t.getTargetTag() == 2)
-            .findFirst()
-            .orElseThrow(() -> new IllegalStateException("Was expecting to find a target with tag 2 but found none"));
-        Permanent permanent = game.getPermanent(target.getFirstTarget());
-        if (permanent != null) {
-            game.addEffect(new DevouringTendrilsReplacementEffect(new MageObjectReference(permanent, game)), source);
-        }
-        return true;
+    DevouringTendrilsDelayedTriggeredAbility(DevouringTendrilsDelayedTriggeredAbility ability) {
+        super(ability);
     }
 
     @Override
-    public DevouringTendrilsEffect copy() {
-        return new DevouringTendrilsEffect(this);
-    }
-}
-
-class DevouringTendrilsReplacementEffect extends ReplacementEffectImpl {
-
-    private final MageObjectReference objectRef;
-
-    DevouringTendrilsReplacementEffect(MageObjectReference objectRef) {
-        super(Duration.EndOfTurn, Outcome.Benefit);
-        this.objectRef = objectRef;
-    }
-
-    DevouringTendrilsReplacementEffect(DevouringTendrilsReplacementEffect effect) {
-        super(effect);
-        this.objectRef = effect.objectRef;
+    public boolean checkEventType(GameEvent event, Game game) {
+        return event.getType() == GameEvent.EventType.ZONE_CHANGE;
     }
 
     @Override
-    public boolean replaceEvent(GameEvent event, Ability source, Game game) {
-        Permanent permanent = ((ZoneChangeEvent) event).getTarget();
-        Player controller = game.getPlayer(source.getControllerId());
-        if (controller != null && permanent != null) {
-            controller.gainLife(2, game, source);
-            return true;
+    public boolean checkTrigger(GameEvent event, Game game) {
+        ZoneChangeEvent zEvent = (ZoneChangeEvent) event;
+        if (zEvent.isDiesEvent()) {
+            Target target = getTargets().stream()
+                .filter(t -> t.getTargetTag() == 2)
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Expected to find target with tag 2 but found none"));
+            return zEvent.getTarget() != null && zEvent.getTargetId().equals(target.getFirstTarget());
         }
         return false;
     }
 
     @Override
-    public boolean checksEventType(GameEvent event, Game game) {
-        return event.getType() == GameEvent.EventType.ZONE_CHANGE;
+    public DevouringTendrilsDelayedTriggeredAbility copy() {
+        return new DevouringTendrilsDelayedTriggeredAbility(this);
     }
 
     @Override
-    public boolean applies(GameEvent event, Ability source, Game game) {
-        ZoneChangeEvent zce = (ZoneChangeEvent) event;
-        return zce.isDiesEvent() && objectRef.equals(new MageObjectReference(zce.getTarget(), game));
-    }
-
-    @Override
-    public DevouringTendrilsReplacementEffect copy() {
-        return new DevouringTendrilsReplacementEffect(this);
+    public String getRule() {
+        return "When the permanent you don't control dies this turn, you gain 2 life.";
     }
 }
