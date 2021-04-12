2fe78560c8fd67fba8ba65af46dd5bf4f0e6a842
Evan Kranzler <theelk801@gmail.com>
2021-04-12 18:20:30 -0400

    [C21] Implemented Fain, the Broker

--- Mage.Sets/src/mage/sets/Commander2021Edition.java
+++ Mage.Sets/src/mage/sets/Commander2021Edition.java
@@ -94,6 +94,7 @@ public final class Commander2021Edition extends ExpansionSet {
         cards.add(new SetCardInfo("Excavation Technique", 16, Rarity.RARE, mage.cards.e.ExcavationTechnique.class));
         cards.add(new SetCardInfo("Ezuri's Predation", 188, Rarity.RARE, mage.cards.e.EzurisPredation.class));
         cards.add(new SetCardInfo("Ezzaroot Channeler", 60, Rarity.RARE, mage.cards.e.EzzarootChanneler.class));
+        cards.add(new SetCardInfo("Fain, the Broker", 40, Rarity.RARE, mage.cards.f.FainTheBroker.class));
         cards.add(new SetCardInfo("Faithless Looting", 168, Rarity.COMMON, mage.cards.f.FaithlessLooting.class));
         cards.add(new SetCardInfo("Feldon of the Third Path", 169, Rarity.MYTHIC, mage.cards.f.FeldonOfTheThirdPath.class));
         cards.add(new SetCardInfo("Felisa, Fang of Silverquill", 2, Rarity.MYTHIC, mage.cards.f.FelisaFangOfSilverquill.class));
