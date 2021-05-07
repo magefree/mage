91676e8fbec0a03cf25200852fc5a9c325cfd16f
Evan Kranzler <theelk801@gmail.com>
2021-05-07 06:44:19 -0400

    [AFR] added set

---
+++ Mage.Sets/src/mage/sets/AdventuresInTheForgottenRealms.java
@@ -0,0 +1,35 @@
+package mage.sets;
+
+import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
+import mage.constants.SetType;
+
+/**
+ * @author TheElk801
+ */
+public final class AdventuresInTheForgottenRealms extends ExpansionSet {
+
+    private static final AdventuresInTheForgottenRealms instance = new AdventuresInTheForgottenRealms();
+
+    public static AdventuresInTheForgottenRealms getInstance() {
+        return instance;
+    }
+
+    private AdventuresInTheForgottenRealms() {
+        super("Adventures in the Forgotten Realms", "AFR", ExpansionSet.buildDate(2021, 7, 23), SetType.EXPANSION);
+        this.blockName = "Adventures in the Forgotten Realms";
+        this.hasBoosters = true;
+        this.hasBasicLands = true;
+        this.numBoosterCommon = 10;
+        this.numBoosterUncommon = 3;
+        this.numBoosterRare = 1;
+        this.maxCardNumberInBooster = 275;
+
+        cards.add(new SetCardInfo("Forest", 281, Rarity.LAND, mage.cards.basiclands.Forest.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Island", 269, Rarity.LAND, mage.cards.basiclands.Island.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Mountain", 277, Rarity.LAND, mage.cards.basiclands.Mountain.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Plains", 265, Rarity.LAND, mage.cards.basiclands.Plains.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Swamp", 273, Rarity.LAND, mage.cards.basiclands.Swamp.class, NON_FULL_USE_VARIOUS));
+    }
+}
+
