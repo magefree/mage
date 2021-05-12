3fc28b3c03a51554f0bc4bbf25269ce6e696ca59
Ingmar Goudt <ingmargoudt@gmail.com>
2021-05-12 23:52:23 +0200

    add afr tests

---
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/afr/ProsperousInnkeeperTest.java
@@ -0,0 +1,37 @@
+package org.mage.test.cards.single.afr;
+
+import mage.constants.PhaseStep;
+import mage.constants.Zone;
+import org.junit.Test;
+import org.mage.test.serverside.base.CardTestPlayerBase;
+
+public class ProsperousInnkeeperTest extends CardTestPlayerBase {
+
+    // When Prosperous Innkeeper enters the battlefield, create a Treasure token.
+    // Whenever another creature enters the battlefield under your control, you gain 1 life.
+    private final String innkeeper = "Prosperous Innkeeper";
+
+    @Test
+    public void createTreasureToken(){
+        addCard(Zone.HAND, playerA, innkeeper);
+        addCard(Zone.BATTLEFIELD, playerA, "Forest", 2);
+        castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, innkeeper);
+        setStopAt(1, PhaseStep.POSTCOMBAT_MAIN);
+        execute();
+        assertAllCommandsUsed();
+        assertPermanentCount(playerA, "Treasure", 1);
+    }
+
+    @Test
+    public void gainLife(){
+        addCard(Zone.HAND, playerA, innkeeper, 2);
+        addCard(Zone.BATTLEFIELD, playerA, "Forest", 2);
+        castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, innkeeper);
+        castSpell(3, PhaseStep.PRECOMBAT_MAIN, playerA, innkeeper);
+        setStopAt(3, PhaseStep.POSTCOMBAT_MAIN);
+        execute();
+        assertAllCommandsUsed();
+        assertLife(playerA, 21);
+    }
+
+}
