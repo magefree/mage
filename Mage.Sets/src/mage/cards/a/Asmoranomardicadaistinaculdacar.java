acb8070d6b1343e5dd3a1205d72f267eee390814
Evan Kranzler <theelk801@gmail.com>
2021-06-03 07:48:38 -0400

    [MH2] Implemented Recalibrate

--- Mage.Sets/src/mage/cards/a/Asmoranomardicadaistinaculdacar.java
+++ Mage.Sets/src/mage/cards/a/Asmoranomardicadaistinaculdacar.java
@@ -4,28 +4,29 @@ import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.EntersBattlefieldTriggeredAbility;
 import mage.abilities.common.SimpleActivatedAbility;
-import mage.abilities.condition.Condition;
+import mage.abilities.condition.common.ControllerDiscardedThisTurnCondition;
 import mage.abilities.costs.AlternativeCostSourceAbility;
 import mage.abilities.costs.common.SacrificeTargetCost;
 import mage.abilities.costs.mana.ManaCostsImpl;
 import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.search.SearchLibraryPutInHandEffect;
+import mage.abilities.hint.common.ControllerDiscardedHint;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.constants.*;
+import mage.constants.CardType;
+import mage.constants.Outcome;
+import mage.constants.SubType;
+import mage.constants.SuperType;
 import mage.filter.FilterCard;
 import mage.filter.common.FilterControlledPermanent;
 import mage.filter.predicate.mageobject.NamePredicate;
 import mage.game.Game;
-import mage.game.events.GameEvent;
 import mage.game.permanent.Permanent;
 import mage.target.common.TargetCardInLibrary;
 import mage.target.common.TargetControlledPermanent;
 import mage.target.common.TargetCreaturePermanent;
-import mage.watchers.Watcher;
+import mage.watchers.common.DiscardedCardWatcher;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.UUID;
 
 /**
@@ -53,9 +54,9 @@ public final class Asmoranomardicadaistinaculdacar extends CardImpl {
 
         // As long as you've discarded a card this turn, you may pay {B/R} to cast this spell.
         this.addAbility(new AlternativeCostSourceAbility(
-                new ManaCostsImpl<>("{B/R}"), AsmoranomardicadaistinaculdacarCondition.instance,
+                new ManaCostsImpl<>("{B/R}"), ControllerDiscardedThisTurnCondition.instance,
                 "as long as you've discarded a card this turn, you may pay {B/R} to cast this spell"
-        ), new AsmoranomardicadaistinaculdacarWatcher());
+        ).addHint(ControllerDiscardedHint.instance), new DiscardedCardWatcher());
 
         // When Asmoranomardicadaistinaculdacar enters the battlefield, you may search your library for a card named The Underworld Cookbook, reveal it, put it into your hand, then shuffle.
         this.addAbility(new EntersBattlefieldTriggeredAbility(
@@ -78,15 +79,6 @@ public final class Asmoranomardicadaistinaculdacar extends CardImpl {
     @Override
     public Asmoranomardicadaistinaculdacar copy() {
         return new Asmoranomardicadaistinaculdacar(this);
-    }
-}
-
-enum AsmoranomardicadaistinaculdacarCondition implements Condition {
-    instance;
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        return AsmoranomardicadaistinaculdacarWatcher.checkPlayer(source.getControllerId(), game);
     }
 }
 
@@ -113,34 +105,6 @@ class AsmoranomardicadaistinaculdacarEffect extends OneShotEffect {
             return false;
         }
         return permanent.damage(6, permanent.getId(), source, game) > 0;
-    }
-}
-
-class AsmoranomardicadaistinaculdacarWatcher extends Watcher {
-
-    private final Set<UUID> playerSet = new HashSet<>();
-
-    AsmoranomardicadaistinaculdacarWatcher() {
-        super(WatcherScope.GAME);
-    }
-
-    @Override
-    public void watch(GameEvent event, Game game) {
-        if (event.getType() == GameEvent.EventType.DISCARDED_CARD) {
-            playerSet.add(event.getPlayerId());
-        }
-    }
-
-    @Override
-    public void reset() {
-        playerSet.clear();
-        super.reset();
-    }
-
-    static boolean checkPlayer(UUID playerId, Game game) {
-        AsmoranomardicadaistinaculdacarWatcher watcher
-                = game.getState().getWatcher(AsmoranomardicadaistinaculdacarWatcher.class);
-        return watcher != null && watcher.playerSet.contains(playerId);
     }
 }
 // it's easier to pronounce if you break it into separate words: asmorano mardica daistina culdacar
---
+++ Mage/src/main/java/mage/watchers/common/DiscardedCardWatcher.java
@@ -0,0 +1,40 @@
+package mage.watchers.common;
+
+import mage.constants.WatcherScope;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.watchers.Watcher;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public class DiscardedCardWatcher extends Watcher {
+
+    private final Map<UUID, Integer> playerMap = new HashMap<>();
+
+    public DiscardedCardWatcher() {
+        super(WatcherScope.GAME);
+    }
+
+    @Override
+    public void watch(GameEvent event, Game game) {
+        if (event.getType() == GameEvent.EventType.DISCARDED_CARD) {
+            playerMap.compute(event.getPlayerId(), (u, i) -> i == null ? 1 : Integer.sum(i, 1));
+        }
+    }
+
+    @Override
+    public void reset() {
+        playerMap.clear();
+        super.reset();
+    }
+
+    public static boolean checkPlayerDiscarded(UUID playerId, Game game) {
+        DiscardedCardWatcher watcher = game.getState().getWatcher(DiscardedCardWatcher.class);
+        return watcher != null && watcher.playerMap.getOrDefault(playerId, 0) > 0;
+    }
+}
