fef82520564b56892b5891a1d4b7351ed6103362
Daniel Bomar <dbdaniel42@gmail.com>
2021-06-04 20:49:00 -0500

    [MH2] Implemented Persist

--- Mage.Sets/src/mage/cards/u/UnbreakableBond.java
+++ Mage.Sets/src/mage/cards/u/UnbreakableBond.java
@@ -1,20 +1,11 @@
 package mage.cards.u;
 
-import mage.abilities.Ability;
-import mage.abilities.effects.ReplacementEffectImpl;
-import mage.abilities.effects.common.InfoEffect;
-import mage.abilities.effects.common.ReturnFromGraveyardToBattlefieldTargetEffect;
+import mage.abilities.effects.common.ReturnFromGraveyardToBattlefieldWithCounterTargetEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.Duration;
-import mage.constants.Outcome;
 import mage.counters.CounterType;
 import mage.filter.StaticFilters;
-import mage.game.Game;
-import mage.game.events.EntersTheBattlefieldEvent;
-import mage.game.events.GameEvent;
-import mage.game.permanent.Permanent;
 import mage.target.common.TargetCardInYourGraveyard;
 
 import java.util.UUID;
@@ -28,9 +19,7 @@ public final class UnbreakableBond extends CardImpl {
         super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{4}{B}");
 
         // Return target creature card from your graveyard to the battlefield with a lifelink counter on it.
-        this.getSpellAbility().addEffect(new UnbreakableBondReplacementEffect());
-        this.getSpellAbility().addEffect(new ReturnFromGraveyardToBattlefieldTargetEffect());
-        this.getSpellAbility().addEffect(new InfoEffect("with a lifelink counter on it"));
+        this.getSpellAbility().addEffect(new ReturnFromGraveyardToBattlefieldWithCounterTargetEffect(CounterType.LIFELINK.createInstance()));
         this.getSpellAbility().addTarget(new TargetCardInYourGraveyard(StaticFilters.FILTER_CARD_CREATURE_YOUR_GRAVEYARD));
     }
 
@@ -41,47 +30,5 @@ public final class UnbreakableBond extends CardImpl {
     @Override
     public UnbreakableBond copy() {
         return new UnbreakableBond(this);
-    }
-}
-
-class UnbreakableBondReplacementEffect extends ReplacementEffectImpl {
-
-    UnbreakableBondReplacementEffect() {
-        super(Duration.EndOfStep, Outcome.BoostCreature);
-    }
-
-    private UnbreakableBondReplacementEffect(UnbreakableBondReplacementEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public boolean checksEventType(GameEvent event, Game game) {
-        return event.getType() == GameEvent.EventType.ENTERS_THE_BATTLEFIELD;
-    }
-
-    @Override
-    public boolean applies(GameEvent event, Ability source, Game game) {
-        return event.getTargetId().equals(getTargetPointer().getFirst(game, source));
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        return false;
-    }
-
-    @Override
-    public boolean replaceEvent(GameEvent event, Ability source, Game game) {
-        Permanent creature = ((EntersTheBattlefieldEvent) event).getTarget();
-        if (creature == null) {
-            return false;
-        }
-        creature.addCounters(CounterType.LIFELINK.createInstance(), source.getControllerId(), source, game, event.getAppliedEffects());
-        discard();
-        return false;
-    }
-
-    @Override
-    public UnbreakableBondReplacementEffect copy() {
-        return new UnbreakableBondReplacementEffect(this);
     }
 }
