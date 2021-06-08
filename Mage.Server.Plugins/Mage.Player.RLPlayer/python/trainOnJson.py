f14011246d5fbc304469fcaaae25d8efb2d256d5
Elchanan Haas <haas2376@gmail.com>
2021-06-08 10:14:27 -0400

    PPO kind of works

--- Mage.Server.Plugins/Mage.Player.RLPlayer/python/trainOnJson.py
+++ Mage.Server.Plugins/Mage.Player.RLPlayer/python/trainOnJson.py
@@ -35,28 +35,39 @@ def collect_experience(min_actions,env,net,converter):
         game_counter+=1
     return (actions,observations,rewards,weights)
 for games in range(10000):
-    (actions,observations,rewards,weights)=collect_experience(300,env,net,converter)
-    optimizer.zero_grad()
-    (out,critic)=net(observations)
+    (actions,observations,rewards,weights)=collect_experience(hparams['exp_per_batch'],env,net,converter)
+    with torch.no_grad():
+        (base_log_prob,critic_base)=net(observations)
+        base_log_prob=base_log_prob.detach()
     max_action_len=max([len(obs['action_mask']) for obs in observations])
-    back_grad=torch.zeros((len(observations),max_action_len))
-    #Use neagive entropy becasue I want to increase it
-    entropy=torch.mean(out*torch.exp(out),dim=1,keepdim=True)*hparams["entropy_weight"]
-    scalar_loss=entropy
-    critic_pred=torch.tanh(critic)
-    for i in range(len(actions)):
-        back_grad[i,actions[i]]=-(rewards[i]-critic_pred[i,0].detach()) #negate end reward to IMPROVE agent!
-    critic_target=(torch.tensor(rewards).unsqueeze(dim=1)+1)/2
-    critic_error=F.binary_cross_entropy_with_logits(critic,critic_target,reduction="none")
-    scalar_loss=scalar_loss+critic_error*hparams['critic_weight']
-    out=torch.cat([out,scalar_loss],dim=1)
-    back_grad=torch.cat([back_grad,torch.ones_like(scalar_loss)],dim=1)
-    grad_weight=torch.tensor(weights).unsqueeze(dim=-1)
-    back_grad=back_grad*grad_weight
-    out.backward(back_grad)
-    torch.nn.utils.clip_grad_norm_(net.parameters(), hparams['grad_clip'])
-    #print(out.shape,back_grad.shape)
-    optimizer.step()
+    for i in range(hparams['steps_per_batch']):
+        optimizer.zero_grad()
+        (out,critic)=net(observations)
+        advantage=torch.zeros((len(observations),max_action_len))
+        #Use neagive entropy becasue I want to increase it
+        entropy=torch.mean(out*torch.exp(out),dim=1,keepdim=True)*hparams["entropy_weight"]
+        scalar_loss=entropy
+        critic_target=(torch.tensor(rewards).unsqueeze(dim=1)+1)/2
+        critic_error=F.binary_cross_entropy_with_logits(critic,critic_target,reduction="none")
+        scalar_loss=scalar_loss+critic_error*hparams['critic_weight']
+        critic_pred=torch.tanh(critic_base)
+        
+        for i in range(len(actions)):
+            advantage[i,actions[i]]=(rewards[i]-critic_pred[i,0].detach()) #negate end reward to IMPROVE agent!
+        action_ratio=torch.exp(out-base_log_prob)
+        unclipped=action_ratio*advantage
+        clipped=torch.clamp(action_ratio,1-hparams["PPOeps"],1+hparams["PPOeps"])*advantage
+        out=torch.min(unclipped,clipped)
+        out=-torch.sum(out,dim=1)
+        scalar_loss=torch.mean(scalar_loss)
+        print(torch.mean(out),torch.mean(scalar_loss))
+        out=out+scalar_loss
+        out=out*torch.tensor(weights).unsqueeze(dim=-1)
+        out=torch.mean(out)
+        out.backward()
+        torch.nn.utils.clip_grad_norm_(net.parameters(), hparams['grad_clip'])
+        #print(out.shape,back_grad.shape)
+        optimizer.step()
 from pathlib import Path
 import pickle 
 base_path=str(Path.home())+"/python/xmage/model_simple"
