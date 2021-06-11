f771dcdb24a1572729657b5b6f56b22dc0b8a521
Evan Kranzler <theelk801@gmail.com>
2021-06-11 06:48:53 -0400

    updated historic ban list

--- Mage.Server.Plugins/Mage.Deck.Constructed/src/mage/deck/Historic.java
+++ Mage.Server.Plugins/Mage.Deck.Constructed/src/mage/deck/Historic.java
@@ -43,6 +43,7 @@ public class Historic extends Constructed {
         banned.add("Swords to Plowshares");
         banned.add("Teferi, Time Raveler");
         banned.add("Thassa's Oracle");
+        banned.add("Time Warp");
         banned.add("Uro, Titan of Nature's Wrath");
         banned.add("Veil of Summer");
         banned.add("Wilderness Reclamation");
