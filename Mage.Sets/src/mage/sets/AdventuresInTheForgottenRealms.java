546173ee453f0d5a009408970291b8a04805ccb5
Evan Kranzler <theelk801@gmail.com>
2021-06-17 08:17:26 -0400

    [AFR] Implemented Drizzt Do'Urden

--- Mage.Sets/src/mage/sets/AdventuresInTheForgottenRealms.java
+++ Mage.Sets/src/mage/sets/AdventuresInTheForgottenRealms.java
@@ -25,6 +25,7 @@ public final class AdventuresInTheForgottenRealms extends ExpansionSet {
         this.numBoosterRare = 1;
         this.maxCardNumberInBooster = 275;
 
+        cards.add(new SetCardInfo("Drizzt Do'Urden", 220, Rarity.RARE, mage.cards.d.DrizztDoUrden.class));
         cards.add(new SetCardInfo("Flumph", 15, Rarity.RARE, mage.cards.f.Flumph.class));
         cards.add(new SetCardInfo("Forest", 281, Rarity.LAND, mage.cards.basiclands.Forest.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Island", 269, Rarity.LAND, mage.cards.basiclands.Island.class, NON_FULL_USE_VARIOUS));
---
+++ Mage/src/main/java/mage/game/permanent/token/GuenhwyvarToken.java
@@ -0,0 +1,32 @@
+package mage.game.permanent.token;
+
+import mage.MageInt;
+import mage.abilities.keyword.TrampleAbility;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.constants.SuperType;
+
+/**
+ * @author TheElk801
+ */
+public final class GuenhwyvarToken extends TokenImpl {
+
+    public GuenhwyvarToken() {
+        super("Guenhwyvar", "Guenhwyvar, a legendary 4/1 green Cat creature token with trample");
+        supertype.add(SuperType.LEGENDARY);
+        cardType.add(CardType.CREATURE);
+        color.setGreen(true);
+        subtype.add(SubType.CAT);
+        power = new MageInt(4);
+        toughness = new MageInt(1);
+        addAbility(TrampleAbility.getInstance());
+    }
+
+    private GuenhwyvarToken(final GuenhwyvarToken token) {
+        super(token);
+    }
+
+    public GuenhwyvarToken copy() {
+        return new GuenhwyvarToken(this);
+    }
+}
