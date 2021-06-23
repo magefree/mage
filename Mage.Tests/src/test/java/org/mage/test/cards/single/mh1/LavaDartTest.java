9a4489b47f2af46e38519515e3f3c3f9334b9929
Oleg Agafonov <jaydi85@gmail.com>
2021-06-23 05:05:51 +0400

    * Cast an instant or sorcery spell this turn - fixed rollback error when you cast graveyard spell as first in turn (#7918);

---
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/mh1/LavaDartTest.java
@@ -0,0 +1,30 @@
+package org.mage.test.cards.single.mh1;
+
+import mage.constants.PhaseStep;
+import mage.constants.Zone;
+import org.junit.Test;
+import org.mage.test.serverside.base.CardTestPlayerBase;
+
+/**
+ * @author JayDi85
+ */
+public class LavaDartTest extends CardTestPlayerBase {
+
+    @Test
+    public void test_Play() {
+        // Lava Dart deals 1 damage to any target.
+        // Flashback-Sacrifice a Mountain.
+        addCard(Zone.GRAVEYARD, playerA, "Lava Dart"); // {R}
+        addCard(Zone.BATTLEFIELD, playerA, "Mountain", 1);
+
+        activateAbility(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Flashback", playerB);
+        setChoice(playerA, "Mountain"); // sacrifice cost
+
+        setStrictChooseMode(true);
+        setStopAt(1, PhaseStep.END_TURN);
+        execute();
+        assertAllCommandsUsed();
+
+        assertLife(playerB, 20 - 1);
+    }
+}
