c4fd57fe175fc18f520f18944a7b9d2d102f3a8f
Evan Kranzler <theelk801@gmail.com>
2021-06-24 18:53:50 -0400

    [AFR] Implemented Tomb of Annihilation

---
+++ Mage/src/main/java/mage/game/permanent/token/TheAtropalToken.java
@@ -0,0 +1,33 @@
+package mage.game.permanent.token;
+
+import mage.MageInt;
+import mage.abilities.keyword.DeathtouchAbility;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.constants.SuperType;
+
+/**
+ * @author TheElk801
+ */
+public final class TheAtropalToken extends TokenImpl {
+
+    public TheAtropalToken() {
+        super("The Atropal", "The Atropal, a legendary 4/4 black God Horror creature token with deathtouch");
+        supertype.add(SuperType.LEGENDARY);
+        cardType.add(CardType.CREATURE);
+        color.setBlack(true);
+        subtype.add(SubType.GOD);
+        subtype.add(SubType.HORROR);
+        power = new MageInt(4);
+        toughness = new MageInt(4);
+        addAbility(DeathtouchAbility.getInstance());
+    }
+
+    public TheAtropalToken(final TheAtropalToken token) {
+        super(token);
+    }
+
+    public TheAtropalToken copy() {
+        return new TheAtropalToken(this);
+    }
+}
