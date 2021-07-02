e2059477ffec1d1068087bde1f8c9057eb25c0e5
Evan Kranzler <theelk801@gmail.com>
2021-07-02 08:35:00 -0400

    [AFR] Implemented Gretchen Titchwillow

---
+++ Mage.Sets/src/mage/cards/g/GretchenTitchwillow.java
@@ -0,0 +1,48 @@
+package mage.cards.g;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.DrawCardSourceControllerEffect;
+import mage.abilities.effects.common.PutCardFromHandOntoBattlefieldEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.constants.SuperType;
+import mage.filter.StaticFilters;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class GretchenTitchwillow extends CardImpl {
+
+    public GretchenTitchwillow(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{G}{U}");
+
+        this.addSuperType(SuperType.LEGENDARY);
+        this.subtype.add(SubType.HALFLING);
+        this.subtype.add(SubType.DRUID);
+        this.power = new MageInt(0);
+        this.toughness = new MageInt(4);
+
+        // {2}{G}{U}: Draw a card. You may put a land card from your hand onto the battlefield.
+        Ability ability = new SimpleActivatedAbility(
+                new DrawCardSourceControllerEffect(1), new ManaCostsImpl<>("{2}{G}{U}")
+        );
+        ability.addEffect(new PutCardFromHandOntoBattlefieldEffect(StaticFilters.FILTER_CARD_LAND_A));
+        this.addAbility(ability);
+    }
+
+    private GretchenTitchwillow(final GretchenTitchwillow card) {
+        super(card);
+    }
+
+    @Override
+    public GretchenTitchwillow copy() {
+        return new GretchenTitchwillow(this);
+    }
+}
