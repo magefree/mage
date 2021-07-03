62f571729cacc28bde3c04ac13255a22944bea49
Raphael-Schulz <schulz-raphael@gmx.de>
2021-07-03 16:16:23 +0200

    [AFR] Implemented Meteor Swarm

---
+++ Mage.Sets/src/mage/cards/m/MeteorSwarm.java
@@ -0,0 +1,51 @@
+package mage.cards.m;
+
+import mage.abilities.Ability;
+import mage.abilities.effects.common.DamageMultiEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.game.Game;
+import mage.target.common.TargetCreatureOrPlaneswalkerAmount;
+import mage.target.targetadjustment.TargetAdjuster;
+
+import java.util.UUID;
+
+/**
+ * @author Raphael-Schulz
+ */
+public final class MeteorSwarm extends CardImpl {
+
+    public MeteorSwarm(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{X}{R}{R}{R}");
+
+        // Meteor Swarm deals 8 damage divided as you choose among X target creatures and/or planeswalkers.
+        this.getSpellAbility().addEffect(
+                new DamageMultiEffect(8).
+                        setText("Meteor Swarm deals 8 damage divided as you choose among X target creatures and/or planeswalkers.")
+        );
+        this.getSpellAbility().addTarget(new TargetCreatureOrPlaneswalkerAmount(8));
+        this.getSpellAbility().setTargetAdjuster(MeteorSwarmAdjuster.instance);
+    }
+
+    private MeteorSwarm(final MeteorSwarm card) {
+        super(card);
+    }
+
+    @Override
+    public MeteorSwarm copy() {
+        return new MeteorSwarm(this);
+    }
+}
+
+enum MeteorSwarmAdjuster implements TargetAdjuster {
+    instance;
+
+    @Override
+    public void adjustTargets(Ability ability, Game game) {
+        ability.getTargets().clear();
+        TargetCreatureOrPlaneswalkerAmount targetCreatureOrPlaneswalkerAmount = new TargetCreatureOrPlaneswalkerAmount(8);
+        targetCreatureOrPlaneswalkerAmount.setMaxNumberOfTargets(ability.getManaCostsToPay().getX());
+        ability.addTarget(targetCreatureOrPlaneswalkerAmount);
+    }
+}
