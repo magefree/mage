744327fedf4bf6f795ae074de3641d6fcb267ce6
Evan Kranzler <theelk801@gmail.com>
2021-07-05 16:16:30 -0400

    [AFR] Implemented Precipitous Drop

---
+++ Mage.Sets/src/mage/cards/p/PrecipitousDrop.java
@@ -0,0 +1,59 @@
+package mage.cards.p;
+
+import mage.abilities.Ability;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.condition.common.CompletedDungeonCondition;
+import mage.abilities.decorator.ConditionalContinuousEffect;
+import mage.abilities.effects.common.AttachEffect;
+import mage.abilities.effects.common.continuous.BoostEnchantedEffect;
+import mage.abilities.effects.keyword.VentureIntoTheDungeonEffect;
+import mage.abilities.keyword.EnchantAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Outcome;
+import mage.constants.SubType;
+import mage.target.TargetPermanent;
+import mage.target.common.TargetCreaturePermanent;
+import mage.watchers.common.CompletedDungeonWatcher;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class PrecipitousDrop extends CardImpl {
+
+    public PrecipitousDrop(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{2}{B}");
+
+        this.subtype.add(SubType.AURA);
+
+        // Enchant creature
+        TargetPermanent auraTarget = new TargetCreaturePermanent();
+        this.getSpellAbility().addTarget(auraTarget);
+        this.getSpellAbility().addEffect(new AttachEffect(Outcome.BoostCreature));
+        Ability ability = new EnchantAbility(auraTarget.getTargetName());
+        this.addAbility(ability);
+
+        // When Precipitous Drop enters the battlefield, venture into the dungeon.
+        this.addAbility(new EntersBattlefieldTriggeredAbility(new VentureIntoTheDungeonEffect()));
+
+        // Enchanted creature gets -2/-2. It gets -5/-5 instead as long as you've completed a dungeon.
+        this.addAbility(new SimpleStaticAbility(new ConditionalContinuousEffect(
+                new BoostEnchantedEffect(-5, -5), new BoostEnchantedEffect(-2, -2),
+                CompletedDungeonCondition.instance, "enchanted creature gets -2/-2. " +
+                "It gets -5/-5 instead as long as you've completed a dungeon"
+        )).addHint(CompletedDungeonCondition.getHint()), new CompletedDungeonWatcher());
+    }
+
+    private PrecipitousDrop(final PrecipitousDrop card) {
+        super(card);
+    }
+
+    @Override
+    public PrecipitousDrop copy() {
+        return new PrecipitousDrop(this);
+    }
+}
