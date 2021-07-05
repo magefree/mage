5b1a38b426d11db54deb747d04936bbc7fd38f2c
Evan Kranzler <theelk801@gmail.com>
2021-07-05 16:36:36 -0400

    [AFR] Implemented Death-Priest of Myrkul

--- Mage/src/main/java/mage/game/permanent/token/SkeletonToken.java
+++ Mage/src/main/java/mage/game/permanent/token/SkeletonToken.java
@@ -1,24 +1,21 @@
 package mage.game.permanent.token;
 
 import mage.MageInt;
-import mage.abilities.common.SimpleActivatedAbility;
-import mage.abilities.costs.mana.ColoredManaCost;
-import mage.abilities.effects.common.RegenerateSourceEffect;
 import mage.constants.CardType;
 import mage.constants.SubType;
-import mage.constants.ColoredManaSymbol;
-import mage.constants.Zone;
 
+/**
+ * @author TheElk801
+ */
 public final class SkeletonToken extends TokenImpl {
 
     public SkeletonToken() {
-        super("Skeleton", "1/1 black Skeleton creature token with \"{B}: Regenerate this creature\"");
+        super("Skeleton", "1/1 black Skeleton creature token");
         cardType.add(CardType.CREATURE);
         this.subtype.add(SubType.SKELETON);
         color.setBlack(true);
         power = new MageInt(1);
         toughness = new MageInt(1);
-        this.addAbility(new SimpleActivatedAbility(Zone.BATTLEFIELD, new RegenerateSourceEffect(), new ColoredManaCost(ColoredManaSymbol.B)));
     }
 
     public SkeletonToken(final SkeletonToken token) {
