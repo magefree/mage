dff5b201d5b8859b2c8dff9122f7129208e86ecc
Evan Kranzler <theelk801@gmail.com>
2021-07-07 19:39:00 -0400

    [AFR] Implemented The Book of Exalted Deeds

---
+++ Mage/src/main/java/mage/game/permanent/token/Angel33Token.java
@@ -0,0 +1,36 @@
+package mage.game.permanent.token;
+
+import mage.MageInt;
+import mage.abilities.keyword.FlyingAbility;
+import mage.constants.CardType;
+import mage.constants.SubType;
+
+import java.util.Arrays;
+
+/**
+ * @author TheElk801
+ */
+public final class Angel33Token extends TokenImpl {
+
+    public Angel33Token() {
+        super("Angel", "3/3 white Angel creature token with flying");
+        cardType.add(CardType.CREATURE);
+        color.setWhite(true);
+        subtype.add(SubType.ANGEL);
+        power = new MageInt(3);
+        toughness = new MageInt(3);
+
+        // Flying
+        this.addAbility(FlyingAbility.getInstance());
+
+        availableImageSetCodes = Arrays.asList("AFR");
+    }
+
+    public Angel33Token(final Angel33Token token) {
+        super(token);
+    }
+
+    public Angel33Token copy() {
+        return new Angel33Token(this);
+    }
+}
