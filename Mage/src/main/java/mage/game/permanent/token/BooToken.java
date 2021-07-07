acd488dca334015216f33595341793a60b0386e0
Evan Kranzler <theelk801@gmail.com>
2021-07-07 17:26:29 -0400

    [AFR] Implemented Minsc, Beloved Ranger

---
+++ Mage/src/main/java/mage/game/permanent/token/BooToken.java
@@ -0,0 +1,34 @@
+package mage.game.permanent.token;
+
+import mage.MageInt;
+import mage.abilities.keyword.HasteAbility;
+import mage.abilities.keyword.TrampleAbility;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.constants.SuperType;
+
+/**
+ * @author TheElk801
+ */
+public final class BooToken extends TokenImpl {
+
+    public BooToken() {
+        super("Boo", "Boo, a legendary 1/1 red Hamster creature token with trample and haste");
+        supertype.add(SuperType.LEGENDARY);
+        cardType.add(CardType.CREATURE);
+        color.setRed(true);
+        subtype.add(SubType.HAMSTER);
+        power = new MageInt(1);
+        toughness = new MageInt(1);
+        addAbility(TrampleAbility.getInstance());
+        addAbility(HasteAbility.getInstance());
+    }
+
+    private BooToken(final BooToken token) {
+        super(token);
+    }
+
+    public BooToken copy() {
+        return new BooToken(this);
+    }
+}
