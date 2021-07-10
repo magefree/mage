2f7d22426ae667e6b53460a367c2035ac9657ce6
Evan Kranzler <theelk801@gmail.com>
2021-07-10 07:02:41 -0400

    [AFC] updated spoiler and reprints

--- Mage.Sets/src/mage/sets/ForgottenRealmsCommander.java
+++ Mage.Sets/src/mage/sets/ForgottenRealmsCommander.java
@@ -1,6 +1,7 @@
 package mage.sets;
 
 import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
 import mage.constants.SetType;
 
 /**
@@ -17,5 +18,9 @@ public final class ForgottenRealmsCommander extends ExpansionSet {
     private ForgottenRealmsCommander() {
         super("Forgotten Realms Commander", "AFC", ExpansionSet.buildDate(2021, 7, 23), SetType.SUPPLEMENTAL);
         this.hasBasicLands = false;
+
+        cards.add(new SetCardInfo("Heroic Intervention", 161, Rarity.RARE, mage.cards.h.HeroicIntervention.class));
+        cards.add(new SetCardInfo("Nature's Lore", 164, Rarity.COMMON, mage.cards.n.NaturesLore.class));
+        cards.add(new SetCardInfo("Swiftfoot Boots", 217, Rarity.UNCOMMON, mage.cards.s.SwiftfootBoots.class));
     }
 }
