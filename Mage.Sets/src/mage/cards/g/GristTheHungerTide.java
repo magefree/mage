2a367b7c138b9a998dc185dba06b93d66448e35c
Evan Kranzler <theelk801@gmail.com>
2021-07-14 20:50:14 -0400

    [MH2] fixed Grist, the Hunger Tide not creating tokens

--- Mage.Sets/src/mage/cards/g/GristTheHungerTide.java
+++ Mage.Sets/src/mage/cards/g/GristTheHungerTide.java
@@ -166,7 +166,7 @@ class GristTheHungerTideTokenEffect extends OneShotEffect {
 
     @Override
     public boolean apply(Game game, Ability source) {
-        Player player = game.getPlayer(source.getSourceId());
+        Player player = game.getPlayer(source.getControllerId());
         if (player == null) {
             return false;
         }
