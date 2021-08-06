5bfcfe0ff77d27ef9ab783bb9da4a516974a04c9
Evan Kranzler <theelk801@gmail.com>
2021-08-05 21:26:19 -0400

    [MID] added set

---
+++ Mage.Sets/src/mage/sets/InnistradMidnightHunt.java
@@ -0,0 +1,36 @@
+package mage.sets;
+
+import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
+import mage.constants.SetType;
+
+/**
+ * @author TheElk801
+ */
+public final class InnistradMidnightHunt extends ExpansionSet {
+
+    private static final InnistradMidnightHunt instance = new InnistradMidnightHunt();
+
+    public static InnistradMidnightHunt getInstance() {
+        return instance;
+    }
+
+    private InnistradMidnightHunt() {
+        super("Innistrad: Midnight Hunt", "MID", ExpansionSet.buildDate(2021, 9, 24), SetType.EXPANSION);
+        this.blockName = "Innistrad: Midnight Hunt";
+        this.hasBoosters = true;
+        this.hasBasicLands = true;
+        this.numBoosterLands = 1;
+        this.numBoosterCommon = 9;
+        this.numBoosterUncommon = 3;
+        this.numBoosterRare = 1;
+        this.ratioBoosterMythic = 8;
+        this.numBoosterDoubleFaced = 1;
+
+        cards.add(new SetCardInfo("Forest", 276, Rarity.LAND, mage.cards.basiclands.Forest.class, FULL_ART_BFZ_VARIOUS));
+        cards.add(new SetCardInfo("Island", 270, Rarity.LAND, mage.cards.basiclands.Island.class, FULL_ART_BFZ_VARIOUS));
+        cards.add(new SetCardInfo("Mountain", 274, Rarity.LAND, mage.cards.basiclands.Mountain.class, FULL_ART_BFZ_VARIOUS));
+        cards.add(new SetCardInfo("Plains", 268, Rarity.LAND, mage.cards.basiclands.Plains.class, FULL_ART_BFZ_VARIOUS));
+        cards.add(new SetCardInfo("Swamp", 272, Rarity.LAND, mage.cards.basiclands.Swamp.class, FULL_ART_BFZ_VARIOUS));
+    }
+}
