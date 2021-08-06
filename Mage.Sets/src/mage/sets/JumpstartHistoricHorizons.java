3bb432a037f6cc2eb150dae1fccf0c97ff6a4f9f
Oleg Agafonov <jaydi85@gmail.com>
2021-08-07 00:11:04 +0400

    Revert "[J21] fixed images download, added reprints"
    This reverts commit 99a561b136712ef5c3d0c142fabb3d98dd687a89.

--- Mage.Sets/src/mage/sets/JumpstartHistoricHorizons.java
+++ Mage.Sets/src/mage/sets/JumpstartHistoricHorizons.java
@@ -20,22 +20,9 @@ public final class JumpstartHistoricHorizons extends ExpansionSet {
         this.hasBoosters = false;
         this.hasBasicLands = false;
 
-        cards.add(new SetCardInfo("Archfiend of Sorrows", "999-AOS", Rarity.UNCOMMON, mage.cards.a.ArchfiendOfSorrows.class));
-        cards.add(new SetCardInfo("Blade Splicer", "999-BS", Rarity.RARE, mage.cards.b.BladeSplicer.class));
-        cards.add(new SetCardInfo("Bounty of the Deep", "999-BOTD", Rarity.UNCOMMON, mage.cards.b.BountyOfTheDeep.class));
-        cards.add(new SetCardInfo("Faceless Agent", "999-FA", Rarity.COMMON, mage.cards.f.FacelessAgent.class));
-        cards.add(new SetCardInfo("First-Sphere Gargantua", "999-FSG", Rarity.COMMON, mage.cards.f.FirstSphereGargantua.class));
-        cards.add(new SetCardInfo("Goblin Dark-Dwellers", "999-GDD", Rarity.RARE, mage.cards.g.GoblinDarkDwellers.class));
-        cards.add(new SetCardInfo("Hardened Scales", "999-HS", Rarity.RARE, mage.cards.h.HardenedScales.class));
-        cards.add(new SetCardInfo("Manor Guardian", "999-MG", Rarity.UNCOMMON, mage.cards.m.ManorGuardian.class));
-        cards.add(new SetCardInfo("Ponder", "999-P", Rarity.COMMON, mage.cards.p.Ponder.class));
-        cards.add(new SetCardInfo("Ranger-Captain of Eos", "999-RCOE", Rarity.MYTHIC, mage.cards.r.RangerCaptainOfEos.class));
-        cards.add(new SetCardInfo("Return to the Ranks", "999-RTTR", Rarity.RARE, mage.cards.r.ReturnToTheRanks.class));
-        cards.add(new SetCardInfo("Seasoned Pyromancer", "999-SP", Rarity.MYTHIC, mage.cards.s.SeasonedPyromancer.class));
-        cards.add(new SetCardInfo("Skyshroud Lookout", "999-SL", Rarity.UNCOMMON, mage.cards.s.SkyshroudLookout.class));
-        cards.add(new SetCardInfo("Stormfront Pegasus", "999-SFP", Rarity.UNCOMMON, mage.cards.s.StormfrontPegasus.class));
-        cards.add(new SetCardInfo("Thalia's Lieutenant", "999-TL", Rarity.RARE, mage.cards.t.ThaliasLieutenant.class));
-        cards.add(new SetCardInfo("Tropical Island", "999-TI", Rarity.RARE, mage.cards.t.TropicalIsland.class));
-        cards.add(new SetCardInfo("Yawgmoth, Thran Physician", "999-YTP", Rarity.MYTHIC, mage.cards.y.YawgmothThranPhysician.class));
+        cards.add(new SetCardInfo("Bounty of the Deep", 7, Rarity.UNCOMMON, mage.cards.b.BountyOfTheDeep.class));
+        cards.add(new SetCardInfo("Faceless Agent", 31, Rarity.COMMON, mage.cards.f.FacelessAgent.class));
+        cards.add(new SetCardInfo("Manor Guardian", 16, Rarity.UNCOMMON, mage.cards.m.ManorGuardian.class));
+        cards.add(new SetCardInfo("Skyshroud Lookout", 29, Rarity.UNCOMMON, mage.cards.s.SkyshroudLookout.class));
     }
 }
