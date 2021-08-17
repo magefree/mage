7f1504ff8c1977212f0bfc082ea82a8b5552f3ba
Evan Kranzler <theelk801@gmail.com>
2021-08-17 19:50:13 -0400

    refactored various cards to use new methods

--- Mage/src/main/java/mage/abilities/effects/common/cost/CastWithoutPayingManaCostEffect.java
+++ Mage/src/main/java/mage/abilities/effects/common/cost/CastWithoutPayingManaCostEffect.java
@@ -1,21 +1,16 @@
 package mage.abilities.effects.common.cost;
 
-import mage.ApprovingObject;
 import mage.abilities.Ability;
 import mage.abilities.dynamicvalue.DynamicValue;
 import mage.abilities.dynamicvalue.common.StaticValue;
 import mage.abilities.effects.OneShotEffect;
-import mage.cards.Card;
 import mage.constants.ComparisonType;
 import mage.constants.Outcome;
 import mage.filter.FilterCard;
-import mage.filter.common.FilterNonlandCard;
 import mage.filter.predicate.mageobject.ManaValuePredicate;
 import mage.game.Game;
 import mage.players.Player;
-import mage.target.Target;
-import mage.target.common.TargetCardInHand;
-import org.apache.log4j.Logger;
+import mage.util.CardUtil;
 
 /**
  * @author fireshoes - Original Code
@@ -51,47 +46,13 @@ public class CastWithoutPayingManaCostEffect extends OneShotEffect {
     @Override
     public boolean apply(Game game, Ability source) {
         Player controller = game.getPlayer(source.getControllerId());
-
         if (controller == null) {
             return false;
         }
         int cmc = manaCost.calculate(game, source, this);
-        FilterCard filter = new FilterNonlandCard("card with mana value "
-                + cmc + " or less from your hand");
+        FilterCard filter = new FilterCard();
         filter.add(new ManaValuePredicate(ComparisonType.FEWER_THAN, cmc + 1));
-
-        Target target = new TargetCardInHand(filter);
-        if (target.canChoose(source.getSourceId(), controller.getId(), game)
-                && controller.chooseUse(Outcome.PlayForFree, "Cast a card with mana value " + cmc
-                        + " or less from your hand without paying its mana cost?", source, game)) {
-            Card cardToCast = null;
-            boolean cancel = false;
-            while (controller.canRespond() 
-                    && !cancel) {
-                if (controller.chooseTarget(Outcome.PlayForFree, target, source, game)) {
-                    cardToCast = game.getCard(target.getFirstTarget());
-                    if (cardToCast != null) {
-                        if (cardToCast.getSpellAbility() == null) {
-                            Logger.getLogger(CastWithoutPayingManaCostEffect.class).fatal("Card: "
-                                    + cardToCast.getName() + " is no land and has no spell ability!");
-                            cancel = true;
-                        }
-                        if (cardToCast.getSpellAbility().canChooseTarget(game, controller.getId())) {
-                            cancel = true;
-                        }
-                    }
-                } else {
-                    cancel = true;
-                }
-            }
-            if (cardToCast != null) {
-                game.getState().setValue("PlayFromNotOwnHandZone" + cardToCast.getId(), Boolean.TRUE);
-                controller.cast(controller.chooseAbilityForCast(cardToCast, game, true),
-                        game, true, new ApprovingObject(source, game));
-                game.getState().setValue("PlayFromNotOwnHandZone" + cardToCast.getId(), null);
-            }
-        }
-        return true;
+        return CardUtil.castSpellWithAttributesForFree(controller, source, game, controller.getHand(), filter);
     }
 
     @Override
