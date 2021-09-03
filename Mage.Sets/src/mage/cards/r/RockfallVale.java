04efba1506732e3f80264bc3d2e9d7c5a24ee84f
Evan Kranzler <theelk801@gmail.com>
2021-09-02 20:05:04 -0400

    [MID] Implemented Rockfall Vale

--- Mage.Sets/src/mage/cards/r/RockfallVale.java
+++ Mage.Sets/src/mage/cards/r/RockfallVale.java
@@ -1,11 +1,11 @@
-package mage.cards.h;
+package mage.cards.r;
 
 import mage.abilities.common.EntersBattlefieldAbility;
 import mage.abilities.condition.Condition;
 import mage.abilities.condition.common.PermanentsOnTheBattlefieldCondition;
 import mage.abilities.decorator.ConditionalOneShotEffect;
 import mage.abilities.effects.common.TapSourceEffect;
-import mage.abilities.mana.BlackManaAbility;
+import mage.abilities.mana.GreenManaAbility;
 import mage.abilities.mana.RedManaAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
@@ -18,32 +18,33 @@ import java.util.UUID;
 /**
  * @author TheElk801
  */
-public final class HauntedRidge extends CardImpl {
+public final class RockfallVale extends CardImpl {
 
     private static final Condition condition = new PermanentsOnTheBattlefieldCondition(
             StaticFilters.FILTER_LANDS, ComparisonType.FEWER_THAN, 2
     );
 
-    public HauntedRidge(UUID ownerId, CardSetInfo setInfo) {
+    public RockfallVale(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.LAND}, "");
 
-        // Haunted Ridge enters the battlefield tapped unless you control two or more other lands.
+
+        // Rockfall Vale enters the battlefield tapped unless you control two or more other lands.
         this.addAbility(new EntersBattlefieldAbility(
                 new ConditionalOneShotEffect(new TapSourceEffect(), condition),
                 "tapped unless you control two or more other lands"
         ));
 
-        // {T}: Add {B} or {R}.
-        this.addAbility(new BlackManaAbility());
+        // {T}: Add {R} or {G}.
         this.addAbility(new RedManaAbility());
+        this.addAbility(new GreenManaAbility());
     }
 
-    private HauntedRidge(final HauntedRidge card) {
+    private RockfallVale(final RockfallVale card) {
         super(card);
     }
 
     @Override
-    public HauntedRidge copy() {
-        return new HauntedRidge(this);
+    public RockfallVale copy() {
+        return new RockfallVale(this);
     }
 }
--- Mage.Sets/src/mage/sets/InnistradMidnightHunt.java
+++ Mage.Sets/src/mage/sets/InnistradMidnightHunt.java
@@ -41,6 +41,7 @@ public final class InnistradMidnightHunt extends ExpansionSet {
         cards.add(new SetCardInfo("Mountain", 274, Rarity.LAND, mage.cards.basiclands.Mountain.class, FULL_ART_BFZ_VARIOUS));
         cards.add(new SetCardInfo("Plains", 268, Rarity.LAND, mage.cards.basiclands.Plains.class, FULL_ART_BFZ_VARIOUS));
         cards.add(new SetCardInfo("Play with Fire", 154, Rarity.UNCOMMON, mage.cards.p.PlayWithFire.class));
+        cards.add(new SetCardInfo("Rockfall Vale", 266, Rarity.RARE, mage.cards.r.RockfallVale.class));
         cards.add(new SetCardInfo("Shipwreck Marsh", 267, Rarity.RARE, mage.cards.s.ShipwreckMarsh.class));
         cards.add(new SetCardInfo("Stormrider Spirit", 79, Rarity.COMMON, mage.cards.s.StormriderSpirit.class));
         cards.add(new SetCardInfo("Swamp", 272, Rarity.LAND, mage.cards.basiclands.Swamp.class, FULL_ART_BFZ_VARIOUS));
