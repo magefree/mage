a6dad60d48994f35442ff485f56ba209890cf649
Evan Kranzler <theelk801@gmail.com>
2021-09-10 09:37:30 -0400

    [MID] added placeholder disturb mechanic

---
+++ Mage/src/main/java/mage/abilities/keyword/DisturbAbility.java
@@ -0,0 +1,18 @@
+package mage.abilities.keyword;
+
+import mage.abilities.SpellAbility;
+import mage.abilities.costs.Cost;
+import mage.constants.SpellAbilityType;
+import mage.constants.TimingRule;
+import mage.constants.Zone;
+
+/**
+ * @author weirddan455
+ * TODO: this is currently implemented in a pull request
+ */
+public class DisturbAbility extends SpellAbility {
+
+    public DisturbAbility(Cost cost, TimingRule timingRule) {
+        super(null, "", Zone.GRAVEYARD, SpellAbilityType.BASE_ALTERNATE);
+    }
+}
