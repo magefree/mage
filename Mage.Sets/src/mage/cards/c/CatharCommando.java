44c6c15c4f1d7ba9314d0c041fc9743560af7c92
ciaccona007 <ciaccona007@users.noreply.github.com>
2021-09-10 17:54:44 -0400

    [MID] Implement Cathar Commando (#8243)

---
+++ Mage.Sets/src/mage/cards/c/CatharCommando.java
@@ -0,0 +1,47 @@
+package mage.cards.c;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.SacrificeSourceCost;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.DestroyTargetEffect;
+import mage.abilities.keyword.FlashAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.constants.Zone;
+import mage.filter.StaticFilters;
+import mage.target.TargetPermanent;
+
+import java.util.UUID;
+
+public final class CatharCommando extends CardImpl {
+
+    public CatharCommando(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId,setInfo,new CardType[]{CardType.CREATURE},"{1}{W}");
+        this.subtype.add(SubType.HUMAN);
+        this.subtype.add(SubType.SOLDIER);
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(1);
+
+        // Flash
+        this.addAbility(FlashAbility.getInstance());
+
+        // {1}, Sacrifice Cathar Commando: Destroy target artifact or enchantment.
+        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new DestroyTargetEffect(), new ManaCostsImpl("{1}{G}"));
+        ability.addCost(new SacrificeSourceCost());
+        ability.addTarget(new TargetPermanent(StaticFilters.FILTER_PERMANENT_ARTIFACT_OR_ENCHANTMENT));
+        this.addAbility(ability);
+    }
+
+    private CatharCommando(final CatharCommando card) {
+        super(card);
+    }
+
+    @Override
+    public CatharCommando copy() {
+        return new CatharCommando(this);
+    }
+}
