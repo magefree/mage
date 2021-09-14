242c019ee239f0f6e27808bd1cf959085725ee13
who?! <LePwnerer@users.noreply.github.com>
2021-09-15 01:43:44 +0200

    [MID] Implemented Ominous Roost

---
+++ Mage/src/main/java/mage/game/permanent/token/OminousRoostToken.java
@@ -0,0 +1,32 @@
+package mage.game.permanent.token;
+
+import mage.MageInt;
+import mage.abilities.common.CanBlockOnlyFlyingAbility;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.keyword.FlyingAbility;
+import mage.constants.CardType;
+import mage.constants.SubType;
+
+public class OminousRoostToken extends TokenImpl {
+    public OminousRoostToken() {
+        super("Bird", "blue Bird creature token with flying and " +
+                "\"This creature can block only creatures with flying.\"");
+        cardType.add(CardType.CREATURE);
+        color.setBlue(true);
+        subtype.add(SubType.BIRD);
+        power = new MageInt(1);
+        toughness = new MageInt(1);
+        
+        this.addAbility(FlyingAbility.getInstance());
+        this.addAbility(new SimpleStaticAbility(new CanBlockOnlyFlyingAbility()));
+    }
+
+    private OminousRoostToken(final OminousRoostToken token) {
+        super(token);
+    }
+
+    @Override
+    public OminousRoostToken copy() {
+        return new OminousRoostToken(this);
+    }
+}
