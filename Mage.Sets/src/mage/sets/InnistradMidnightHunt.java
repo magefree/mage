cf93e5d572246ed6eab98ba7abe44daafd3568fa
Raphael "who?!" Kehldorfer <LePwnerer@users.noreply.github.com>
2021-09-17 14:07:54 +0200

    [MID] Implemented Wake to Slaughter (#8270)
    * [MID] Implemented Wake to Slaughter
    
    * [MID] Refactored Wake to Slaughter:
    + Reworked WakeToSlaughterEffect
    + Added Min / Max
    
    * [MID] Refactored Wake to Slaughter
    + Added missing ability and trigger

--- Mage.Sets/src/mage/sets/InnistradMidnightHunt.java
+++ Mage.Sets/src/mage/sets/InnistradMidnightHunt.java
@@ -322,6 +322,7 @@ public final class InnistradMidnightHunt extends ExpansionSet {
         cards.add(new SetCardInfo("Voldaren Ambusher", 166, Rarity.UNCOMMON, mage.cards.v.VoldarenAmbusher.class));
         cards.add(new SetCardInfo("Voldaren Stinger", 167, Rarity.COMMON, mage.cards.v.VoldarenStinger.class));
         cards.add(new SetCardInfo("Waildrifter", 55, Rarity.COMMON, mage.cards.w.Waildrifter.class));
+        cards.add(new SetCardInfo("Wake to Slaughter", 250, Rarity.RARE, mage.cards.w.WakeToSlaughter.class));
         cards.add(new SetCardInfo("Willow Geist", 207, Rarity.RARE, mage.cards.w.WillowGeist.class));
         cards.add(new SetCardInfo("Wing Shredder", 169, Rarity.COMMON, mage.cards.w.WingShredder.class));
         cards.add(new SetCardInfo("Winterthorn Blessing", 251, Rarity.UNCOMMON, mage.cards.w.WinterthornBlessing.class));
