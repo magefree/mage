56ad1c8a75640df17a17e1ba45f2e2f885c7a755
who?! <LePwnerer@users.noreply.github.com>
2021-10-28 21:34:40 +0200

    [VOW] Added Innistrad: Crimson Vow set.

---
+++ Mage.Sets/src/mage/sets/InnistradCrimsonVow.java
@@ -0,0 +1,29 @@
+package mage.sets;
+
+import mage.cards.ExpansionSet;
+import mage.constants.SetType;
+
+/**
+ * @author LePwnerer
+ */
+public final class InnistradCrimsonVow extends ExpansionSet {
+
+    private static final InnistradCrimsonVow instance = new InnistradCrimsonVow();
+
+    public static InnistradCrimsonVow getInstance() {
+        return instance;
+    }
+
+    private InnistradCrimsonVow() {
+        super("Innistrad: Crimson Vow", "VOW", ExpansionSet.buildDate(2021, 11, 19), SetType.EXPANSION);
+        this.blockName = "Innistrad: Crimson Vow";
+        this.hasBoosters = true;
+        this.hasBasicLands = true;
+        this.numBoosterLands = 1;
+        this.numBoosterCommon = 9;
+        this.numBoosterUncommon = 3;
+        this.numBoosterRare = 1;
+        this.ratioBoosterMythic = 8;
+        this.numBoosterDoubleFaced = 1;
+    }
+}
