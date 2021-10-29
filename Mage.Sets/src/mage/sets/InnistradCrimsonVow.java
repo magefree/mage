ef2a45227d4af71efd254244b3becee3d48de7b3
Daniel Bomar <dbdaniel42@gmail.com>
2021-10-29 11:01:32 -0500

    [VOW] Implemented Anje, Maid of Dishonor

--- Mage.Sets/src/mage/sets/InnistradCrimsonVow.java
+++ Mage.Sets/src/mage/sets/InnistradCrimsonVow.java
@@ -31,6 +31,7 @@ public final class InnistradCrimsonVow extends ExpansionSet {
         this.ratioBoosterMythic = 8;
         this.numBoosterDoubleFaced = 1;
 
+        cards.add(new SetCardInfo("Anje, Maid of Dishonor", 231, Rarity.RARE, mage.cards.a.AnjeMaidOfDishonor.class));
         cards.add(new SetCardInfo("Apprentice Sharpshooter", 185, Rarity.COMMON, mage.cards.a.ApprenticeSharpshooter.class));
         cards.add(new SetCardInfo("By Invitation Only", 5, Rarity.RARE, mage.cards.b.ByInvitationOnly.class));
         cards.add(new SetCardInfo("Deathcap Glade", 261, Rarity.RARE, mage.cards.d.DeathcapGlade.class));
