f15741828be70d68706cb2603e0dadf67162e961
Oleg Agafonov <jaydi85@gmail.com>
2021-11-05 10:09:38 +0400

    Merge fix

--- Mage.Sets/src/mage/cards/w/WearyPrisoner.java
+++ Mage.Sets/src/mage/cards/w/WearyPrisoner.java
@@ -3,6 +3,7 @@ package mage.cards.w;
 import mage.MageInt;
 import mage.abilities.keyword.DayboundAbility;
 import mage.abilities.keyword.DefenderAbility;
+import mage.abilities.keyword.TransformAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
@@ -29,6 +30,7 @@ public final class WearyPrisoner extends CardImpl {
         this.addAbility(DefenderAbility.getInstance());
 
         // Daybound
+        this.addAbility(new TransformAbility());
         this.addAbility(new DayboundAbility());
     }
 
