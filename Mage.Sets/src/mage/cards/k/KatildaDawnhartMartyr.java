d3d77af3f46a5272c3799215280f531d958f7a94
Evan Kranzler <theelk801@gmail.com>
2021-11-06 19:48:44 -0400

    [VOW] Implemented Katila, Dawnhart Martyr / Katilda's Rising Dawn

---
+++ Mage.Sets/src/mage/cards/k/KatildaDawnhartMartyr.java
@@ -0,0 +1,78 @@
+package mage.cards.k;
+
+import mage.MageInt;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.dynamicvalue.DynamicValue;
+import mage.abilities.dynamicvalue.common.PermanentsOnBattlefieldCount;
+import mage.abilities.effects.common.continuous.SetPowerToughnessSourceEffect;
+import mage.abilities.hint.Hint;
+import mage.abilities.hint.ValueHint;
+import mage.abilities.keyword.*;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.*;
+import mage.filter.FilterPermanent;
+import mage.filter.common.FilterControlledPermanent;
+import mage.filter.predicate.Predicates;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class KatildaDawnhartMartyr extends CardImpl {
+
+    private static final FilterPermanent filter
+            = new FilterPermanent(SubType.VAMPIRE, "Vampires");
+    private static final FilterPermanent filter2
+            = new FilterControlledPermanent("permanents you control that are Spirits and/or enchantments");
+
+    static {
+        filter2.add(Predicates.or(
+                SubType.SPIRIT.getPredicate(),
+                CardType.ENCHANTMENT.getPredicate()
+        ));
+    }
+
+    private static final DynamicValue xValue = new PermanentsOnBattlefieldCount(filter2);
+    private static final Hint hint = new ValueHint("Spirits and enchantments you control", xValue);
+
+    public KatildaDawnhartMartyr(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{1}{W}{W}");
+
+        this.addSuperType(SuperType.LEGENDARY);
+        this.subtype.add(SubType.SPIRIT);
+        this.subtype.add(SubType.WARLOCK);
+        this.power = new MageInt(0);
+        this.toughness = new MageInt(0);
+        this.secondSideCardClazz = mage.cards.k.KatildasRisingDawn.class;
+
+        // Flying
+        this.addAbility(FlyingAbility.getInstance());
+
+        // Lifelink
+        this.addAbility(LifelinkAbility.getInstance());
+
+        // Protection from Vampires
+        this.addAbility(new ProtectionAbility(filter));
+
+        // Katilda, Dawnhart Martyr's power and toughness are each equal to the number of permanents you control that are Spirits and/or enchantments.
+        this.addAbility(new SimpleStaticAbility(
+                Zone.ALL, new SetPowerToughnessSourceEffect(xValue, Duration.EndOfGame)
+        ).addHint(hint));
+
+        // Disturb {3}{W}{W}
+        this.addAbility(new TransformAbility());
+        this.addAbility(new DisturbAbility(new ManaCostsImpl<>("{3}{W}{W}")));
+    }
+
+    private KatildaDawnhartMartyr(final KatildaDawnhartMartyr card) {
+        super(card);
+    }
+
+    @Override
+    public KatildaDawnhartMartyr copy() {
+        return new KatildaDawnhartMartyr(this);
+    }
+}
--- Mage.Sets/src/mage/sets/InnistradCrimsonVow.java
+++ Mage.Sets/src/mage/sets/InnistradCrimsonVow.java
@@ -160,6 +160,8 @@ public final class InnistradCrimsonVow extends ExpansionSet {
         cards.add(new SetCardInfo("Into the Night", 163, Rarity.UNCOMMON, mage.cards.i.IntoTheNight.class));
         cards.add(new SetCardInfo("Investigator's Journal", 258, Rarity.RARE, mage.cards.i.InvestigatorsJournal.class));
         cards.add(new SetCardInfo("Island", 399, Rarity.LAND, mage.cards.basiclands.Island.class, FULL_ART_BFZ_VARIOUS));
+        cards.add(new SetCardInfo("Katilda's Rising Dawn", 21, Rarity.RARE, mage.cards.k.KatildasRisingDawn.class));
+        cards.add(new SetCardInfo("Katilda, Dawnhart Martyr", 21, Rarity.RARE, mage.cards.k.KatildaDawnhartMartyr.class));
         cards.add(new SetCardInfo("Kessig Flamebreather", 164, Rarity.COMMON, mage.cards.k.KessigFlamebreather.class));
         cards.add(new SetCardInfo("Kessig Wolfrider", 165, Rarity.RARE, mage.cards.k.KessigWolfrider.class));
         cards.add(new SetCardInfo("Kindly Ancestor", 22, Rarity.COMMON, mage.cards.k.KindlyAncestor.class));
