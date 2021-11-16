b38f302dba5010bcc76a90d61b59fedca2f0bcf7
Jeff Wadsworth <jeff@delmarus.com>
2021-11-16 08:41:53 -0600

    - outcome fixes for AI

--- Mage.Sets/src/mage/cards/s/SavageSurge.java
+++ Mage.Sets/src/mage/cards/s/SavageSurge.java
@@ -1,4 +1,3 @@
-
 package mage.cards.s;
 
 import java.util.UUID;
@@ -19,16 +18,17 @@ import mage.target.common.TargetCreaturePermanent;
 public final class SavageSurge extends CardImpl {
 
     public SavageSurge(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.INSTANT},"{1}{G}");
-
+        super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{1}{G}");
 
         // Target creature gets +2/+2 until end of turn. Untap that creature.
         this.getSpellAbility().addTarget(new TargetCreaturePermanent());
-        this.getSpellAbility().addEffect(new BoostTargetEffect(2, 2, Duration.EndOfTurn));
-        Effect effect = new UntapTargetEffect();
-        effect.setOutcome(Outcome.Benefit);
-        effect.setText("Untap that creature");
-        this.getSpellAbility().addEffect(effect);
+        Effect boostEffect = new BoostTargetEffect(2, 2, Duration.EndOfTurn);
+        boostEffect.setOutcome(Outcome.Benefit);
+        this.getSpellAbility().addEffect(boostEffect);
+        Effect untapEffect = new UntapTargetEffect();
+        untapEffect.setOutcome(Outcome.Benefit);
+        untapEffect.setText("Untap that creature");
+        this.getSpellAbility().addEffect(untapEffect);
     }
 
     private SavageSurge(final SavageSurge card) {
