f911a4290361fada6ccd6b1ab3503847ac4580f9
Oleg Agafonov <jaydi85@gmail.com>
2021-11-17 15:59:35 +0400

    Fixed outdated db version

--- Mage/src/main/java/mage/cards/repository/CardRepository.java
+++ Mage/src/main/java/mage/cards/repository/CardRepository.java
@@ -34,7 +34,7 @@ public enum CardRepository {
     private static final String JDBC_URL = "jdbc:h2:file:./db/cards.h2;AUTO_SERVER=TRUE";
     private static final String VERSION_ENTITY_NAME = "card";
     // raise this if db structure was changed
-    private static final long CARD_DB_VERSION = 53;
+    private static final long CARD_DB_VERSION = 54;
     // raise this if new cards were added to the server
     private static final long CARD_CONTENT_VERSION = 241;
     private Dao<CardInfo, Object> cardDao;
