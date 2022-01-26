0da2b8c248d3795e1bbc0acd6b01fae28f76f61e
Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>
2022-01-26 17:28:10 -0500

    Removed Kjeldoran Elite Guard and pushing it separately.

--- Mage.Sets/src/mage/sets/IceAge.java
+++ Mage.Sets/src/mage/sets/IceAge.java
@@ -196,7 +196,6 @@ public final class IceAge extends ExpansionSet {
         cards.add(new SetCardInfo("Karplusan Yeti", 197, Rarity.RARE, mage.cards.k.KarplusanYeti.class));
         cards.add(new SetCardInfo("Kelsinko Ranger", 33, Rarity.COMMON, mage.cards.k.KelsinkoRanger.class));
         cards.add(new SetCardInfo("Kjeldoran Dead", 137, Rarity.COMMON, mage.cards.k.KjeldoranDead.class));
-        cards.add(new SetCardInfo("Kjeldoran Elite Guard", 34, Rarity.UNCOMMON, mage.cards.k.KjeldoranEliteGuard.class));
         cards.add(new SetCardInfo("Kjeldoran Frostbeast", 296, Rarity.UNCOMMON, mage.cards.k.KjeldoranFrostbeast.class));
         cards.add(new SetCardInfo("Kjeldoran Guard", 35, Rarity.UNCOMMON, mage.cards.k.KjeldoranGuard.class));
         cards.add(new SetCardInfo("Kjeldoran Knight", 36, Rarity.RARE, mage.cards.k.KjeldoranKnight.class));
