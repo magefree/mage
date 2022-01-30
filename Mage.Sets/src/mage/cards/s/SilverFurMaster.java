136bb921f6b73183026b729c9a6fe0f440ab85c5
Craig <craig_brennan24@hotmail.com>
2022-01-30 21:45:53 +0000

    Changed SilverFurMaster cost reduction method to AbilitiesCostReductionControllerEffect()

--- Mage.Sets/src/mage/cards/s/SilverFurMaster.java
+++ Mage.Sets/src/mage/cards/s/SilverFurMaster.java
@@ -1,23 +1,18 @@
 package mage.cards.s;
 
 import mage.MageInt;
-import mage.abilities.Ability;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.effects.common.continuous.BoostControlledEffect;
-import mage.abilities.effects.common.cost.CostModificationEffectImpl;
+import mage.abilities.effects.common.cost.AbilitiesCostReductionControllerEffect;
 import mage.abilities.keyword.NinjutsuAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.CostModificationType;
 import mage.constants.Duration;
-import mage.constants.Outcome;
 import mage.constants.SubType;
 import mage.constants.Zone;
 import mage.filter.common.FilterCreaturePermanent;
 import mage.filter.predicate.Predicates;
-import mage.game.Game;
-import mage.util.CardUtil;
 
 import java.util.UUID;
 
@@ -47,7 +42,7 @@ public final class SilverFurMaster extends CardImpl {
         this.addAbility(new NinjutsuAbility("{U}{B}"));
 
         // Ninjutsu abilities you activate cost {1} less to activate.
-        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new SilverFurMasterCostModificationEffect()));
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new AbilitiesCostReductionControllerEffect(NinjutsuAbility.class, "Ninjutsu")));
 
         // Other Ninja and Rogue creatures you control get +1/+1
         this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new BoostControlledEffect(1, 1, Duration.WhileOnBattlefield, filter, true)));
@@ -62,34 +57,4 @@ public final class SilverFurMaster extends CardImpl {
     public SilverFurMaster copy() {
         return new SilverFurMaster(this);
     }
-}
-
-class SilverFurMasterCostModificationEffect extends CostModificationEffectImpl {
-
-    SilverFurMasterCostModificationEffect() {
-        super(Duration.WhileOnBattlefield, Outcome.Benefit, CostModificationType.REDUCE_COST);
-        staticText = "Ninjutsu abilities you activate cost {1} less to activate";
-    }
-
-    SilverFurMasterCostModificationEffect(SilverFurMasterCostModificationEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source, Ability abilityToModify) {
-        CardUtil.reduceCost(abilityToModify, 1);
-        return true;
-    }
-
-    @Override
-    public boolean applies(Ability abilityToModify, Ability source, Game game) {
-        return abilityToModify.isControlledBy(source.getControllerId())
-                && (abilityToModify instanceof NinjutsuAbility);
-    }
-
-    @Override
-    public SilverFurMasterCostModificationEffect copy() {
-        return new SilverFurMasterCostModificationEffect(this);
-    }
-
 }
