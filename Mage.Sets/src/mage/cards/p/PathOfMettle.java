9c85a8f7f34c4da3f4439a5d475680b6eb47c1a6
Alex W. Jackson <alex.w.jackson@gmail.com>
2022-01-30 12:36:18 -0500

    [RIX] Path of Mettle use AttacksWithCreaturesTriggeredAbility

--- Mage.Sets/src/mage/cards/p/PathOfMettle.java
+++ Mage.Sets/src/mage/cards/p/PathOfMettle.java
@@ -1,9 +1,8 @@
-
 package mage.cards.p;
 
 import java.util.UUID;
 
-import mage.abilities.TriggeredAbilityImpl;
+import mage.abilities.common.AttacksWithCreaturesTriggeredAbility;
 import mage.abilities.common.EntersBattlefieldTriggeredAbility;
 import mage.abilities.effects.common.DamageAllEffect;
 import mage.abilities.effects.common.TransformSourceEffect;
@@ -16,28 +15,32 @@ import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.SuperType;
-import mage.constants.Zone;
 import mage.filter.common.FilterCreaturePermanent;
 import mage.filter.predicate.Predicates;
 import mage.filter.predicate.mageobject.AbilityPredicate;
-import mage.game.Game;
-import mage.game.events.GameEvent;
-import mage.game.permanent.Permanent;
 
 /**
  * @author LevelX2
  */
 public final class PathOfMettle extends CardImpl {
 
-    private static final FilterCreaturePermanent filter = new FilterCreaturePermanent("creature that doesn't have first strike, double strike, vigilance, or haste");
+    private static final FilterCreaturePermanent filterDamage = new FilterCreaturePermanent("creature that doesn't have first strike, double strike, vigilance, or haste");
+    private static final FilterCreaturePermanent filterTransform = new FilterCreaturePermanent("creatures that have first strike, double strike, vigilance, and/or haste");
 
     static {
-        filter.add(Predicates.not(Predicates.or(
+        filterDamage.add(Predicates.not(Predicates.or(
                 new AbilityPredicate(FirstStrikeAbility.class),
                 new AbilityPredicate(DoubleStrikeAbility.class),
                 new AbilityPredicate(VigilanceAbility.class),
                 new AbilityPredicate(HasteAbility.class)
         )));
+
+        filterTransform.add(Predicates.or(
+                new AbilityPredicate(FirstStrikeAbility.class),
+                new AbilityPredicate(DoubleStrikeAbility.class),
+                new AbilityPredicate(VigilanceAbility.class),
+                new AbilityPredicate(HasteAbility.class)
+        ));
     }
 
     public PathOfMettle(UUID ownerId, CardSetInfo setInfo) {
@@ -48,11 +51,12 @@ public final class PathOfMettle extends CardImpl {
         this.secondSideCardClazz = mage.cards.m.MetzaliTowerOfTriumph.class;
 
         // When Path of Mettle enters the battlefield, it deals 1 damage to each creature that doesn't have first strike, double strike, vigilance, or haste.
-        this.addAbility(new EntersBattlefieldTriggeredAbility(new DamageAllEffect(1, filter)));
+        this.addAbility(new EntersBattlefieldTriggeredAbility(new DamageAllEffect(1, filterDamage)));
 
         // Whenever you attack with at least two creatures that have first strike, double strike, vigilance, and/or haste, transform Path of Mettle.
         this.addAbility(new TransformAbility());
-        this.addAbility(new PathOfMettleTriggeredAbility());
+        this.addAbility(new AttacksWithCreaturesTriggeredAbility(new TransformSourceEffect(), 2, filterTransform)
+                .setTriggerPhrase("Whenever you attack with at least two creatures that have first strike, double strike, vigilance, and/or haste, "));
     }
 
     private PathOfMettle(final PathOfMettle card) {
@@ -62,60 +66,5 @@ public final class PathOfMettle extends CardImpl {
     @Override
     public PathOfMettle copy() {
         return new PathOfMettle(this);
-    }
-}
-
-class PathOfMettleTriggeredAbility extends TriggeredAbilityImpl {
-
-    private static final FilterCreaturePermanent filter = new FilterCreaturePermanent("creature that doesn't have first strike, double strike, vigilance, or haste");
-
-    static {
-        filter.add(Predicates.or(
-                new AbilityPredicate(FirstStrikeAbility.class),
-                new AbilityPredicate(DoubleStrikeAbility.class),
-                new AbilityPredicate(VigilanceAbility.class),
-                new AbilityPredicate(HasteAbility.class)
-        ));
-    }
-
-    public PathOfMettleTriggeredAbility() {
-        super(Zone.BATTLEFIELD, new TransformSourceEffect());
-    }
-
-    public PathOfMettleTriggeredAbility(final PathOfMettleTriggeredAbility ability) {
-        super(ability);
-    }
-
-    @Override
-    public PathOfMettleTriggeredAbility copy() {
-        return new PathOfMettleTriggeredAbility(this);
-    }
-
-    @Override
-    public boolean checkEventType(GameEvent event, Game game) {
-        return event.getType() == GameEvent.EventType.DECLARED_ATTACKERS;
-    }
-
-    @Override
-    public boolean checkTrigger(GameEvent event, Game game) {
-        int attackerCount = 0;
-        if (game.getCombat() != null) {
-            if (isControlledBy(game.getCombat().getAttackingPlayerId())) {
-                for (UUID attackerId : game.getCombat().getAttackers()) {
-                    Permanent attacker = game.getPermanent(attackerId);
-                    if (attacker != null && filter.match(attacker, game)) {
-                        attackerCount++;
-                    }
-                }
-                return attackerCount >= 2;
-            }
-        }
-        return false;
-
-    }
-
-    @Override
-    public String getRule() {
-        return "Whenever you attack with at least two creatures that have first strike, double strike, vigilance, and/or haste, transform Path of Mettle";
     }
 }
