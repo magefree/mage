6d981e18456909451ab550e4b95e464ce9e92f42
Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>
2022-01-30 20:01:48 -0500

    Simplified several cards by replacing custom effects with ExileTopXMayPlayUntilEndOfTurnEffect

--- Mage.Sets/src/mage/cards/i/IreShaman.java
+++ Mage.Sets/src/mage/cards/i/IreShaman.java
@@ -3,26 +3,15 @@ package mage.cards.i;
 
 import java.util.UUID;
 import mage.MageInt;
-import mage.abilities.Ability;
 import mage.abilities.common.TurnedFaceUpSourceTriggeredAbility;
 import mage.abilities.costs.mana.ManaCostsImpl;
-import mage.abilities.effects.AsThoughEffectImpl;
-import mage.abilities.effects.ContinuousEffect;
-import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.ExileTopXMayPlayUntilEndOfTurnEffect;
 import mage.abilities.keyword.MenaceAbility;
 import mage.abilities.keyword.MorphAbility;
-import mage.cards.Card;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.constants.AsThoughEffectType;
 import mage.constants.CardType;
 import mage.constants.SubType;
-import mage.constants.Duration;
-import mage.constants.Outcome;
-import mage.game.Game;
-import mage.game.permanent.Permanent;
-import mage.players.Player;
-import mage.target.targetpointer.FixedTarget;
 
 /**
  *
@@ -44,7 +33,7 @@ public final class IreShaman extends CardImpl {
         this.addAbility(new MorphAbility(this, new ManaCostsImpl("{R}"), true));
 
         // When Ire Shaman is turned face up, exile the top card of your library. Until end of turn, you may play that card.
-        this.addAbility(new TurnedFaceUpSourceTriggeredAbility(new IreShamanExileEffect(), false));
+        this.addAbility(new TurnedFaceUpSourceTriggeredAbility(new ExileTopXMayPlayUntilEndOfTurnEffect(1), false));
     }
 
     private IreShaman(final IreShaman card) {
@@ -54,68 +43,5 @@ public final class IreShaman extends CardImpl {
     @Override
     public IreShaman copy() {
         return new IreShaman(this);
-    }
-}
-
-class IreShamanExileEffect extends OneShotEffect {
-
-    public IreShamanExileEffect() {
-        super(Outcome.Detriment);
-        this.staticText = "exile the top card of your library. Until end of turn, you may play that card";
-    }
-
-    public IreShamanExileEffect(final IreShamanExileEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public IreShamanExileEffect copy() {
-        return new IreShamanExileEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Player controller = game.getPlayer(source.getControllerId());
-        Permanent sourcePermanent = game.getPermanentOrLKIBattlefield(source.getSourceId());
-        if (sourcePermanent != null && controller != null && controller.getLibrary().hasCards()) {
-            Card card = controller.getLibrary().getFromTop(game);
-            if (card != null) {
-                String exileName = sourcePermanent.getIdName() + " <this card may be played the turn it was exiled>";
-                controller.moveCardsToExile(card, source, game, true, source.getSourceId(), exileName);
-                ContinuousEffect effect = new IreShamanCastFromExileEffect();
-                effect.setTargetPointer(new FixedTarget(card.getId()));
-                game.addEffect(effect, source);
-            }
-            return true;
-        }
-        return false;
-    }
-}
-
-class IreShamanCastFromExileEffect extends AsThoughEffectImpl {
-
-    public IreShamanCastFromExileEffect() {
-        super(AsThoughEffectType.PLAY_FROM_NOT_OWN_HAND_ZONE, Duration.EndOfTurn, Outcome.Benefit);
-        staticText = "You may play the card from exile";
-    }
-
-    public IreShamanCastFromExileEffect(final IreShamanCastFromExileEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        return true;
-    }
-
-    @Override
-    public IreShamanCastFromExileEffect copy() {
-        return new IreShamanCastFromExileEffect(this);
-    }
-
-    @Override
-    public boolean applies(UUID objectId, Ability source, UUID affectedControllerId, Game game) {
-        return source.isControlledBy(affectedControllerId)
-                && objectId.equals(getTargetPointer().getFirst(game, source));
     }
 }
--- Mage.Sets/src/mage/cards/o/OutpostSiege.java
+++ Mage.Sets/src/mage/cards/o/OutpostSiege.java
@@ -7,26 +7,16 @@ import mage.abilities.common.EntersBattlefieldAbility;
 import mage.abilities.common.ZoneChangeAllTriggeredAbility;
 import mage.abilities.condition.common.ModeChoiceSourceCondition;
 import mage.abilities.decorator.ConditionalTriggeredAbility;
-import mage.abilities.effects.AsThoughEffectImpl;
-import mage.abilities.effects.ContinuousEffect;
-import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.ChooseModeEffect;
 import mage.abilities.effects.common.DamageTargetEffect;
-import mage.cards.Card;
+import mage.abilities.effects.common.ExileTopXMayPlayUntilEndOfTurnEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.constants.AsThoughEffectType;
 import mage.constants.CardType;
-import mage.constants.Duration;
-import mage.constants.Outcome;
 import mage.constants.TargetController;
 import mage.constants.Zone;
 import mage.filter.common.FilterControlledCreaturePermanent;
-import mage.game.Game;
-import mage.game.permanent.Permanent;
-import mage.players.Player;
 import mage.target.common.TargetAnyTarget;
-import mage.target.targetpointer.FixedTarget;
 
 /**
  *
@@ -46,7 +36,7 @@ public final class OutpostSiege extends CardImpl {
 
         // * Khans - At the beginning of your upkeep, exile the top card of your library. Until end of turn, you may play that card.
         this.addAbility(new ConditionalTriggeredAbility(
-                new BeginningOfUpkeepTriggeredAbility(new OutpostSiegeExileEffect(), TargetController.YOU, false),
+                new BeginningOfUpkeepTriggeredAbility(new ExileTopXMayPlayUntilEndOfTurnEffect(1), TargetController.YOU, false),
                 new ModeChoiceSourceCondition("Khans"),
                 ruleTrigger1));
 
@@ -68,81 +58,5 @@ public final class OutpostSiege extends CardImpl {
     @Override
     public OutpostSiege copy() {
         return new OutpostSiege(this);
-    }
-}
-
-class OutpostSiegeExileEffect extends OneShotEffect {
-
-    public OutpostSiegeExileEffect() {
-        super(Outcome.Benefit);
-        this.staticText = "exile the top card of your library. Until end of turn, you may play that card";
-    }
-
-    public OutpostSiegeExileEffect(final OutpostSiegeExileEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public OutpostSiegeExileEffect copy() {
-        return new OutpostSiegeExileEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Player controller = game.getPlayer(source.getControllerId());
-        Permanent sourcePermanent = game.getPermanentOrLKIBattlefield(source.getSourceId());
-        if (controller != null && sourcePermanent != null) {
-            Card card = controller.getLibrary().getFromTop(game);
-            if (card != null) {
-                String exileName = sourcePermanent.getIdName() + " <this card may be played the turn it was exiled";
-                controller.moveCardsToExile(card, source, game, true, source.getSourceId(), exileName);
-                if (game.getState().getZone(card.getId()) == Zone.EXILED) {
-                    ContinuousEffect effect = new CastFromNonHandZoneTargetEffect(Duration.EndOfTurn);
-                    effect.setTargetPointer(new FixedTarget(card, game));
-                    game.addEffect(effect, source);
-                }
-            }
-            return true;
-        }
-        return false;
-    }
-}
-
-class CastFromNonHandZoneTargetEffect extends AsThoughEffectImpl {
-
-    public CastFromNonHandZoneTargetEffect(Duration duration) {
-        super(AsThoughEffectType.PLAY_FROM_NOT_OWN_HAND_ZONE, duration, Outcome.Benefit);
-        staticText = "until end of turn, you may play that card";
-    }
-
-    public CastFromNonHandZoneTargetEffect(final CastFromNonHandZoneTargetEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        return true;
-    }
-
-    @Override
-    public CastFromNonHandZoneTargetEffect copy() {
-        return new CastFromNonHandZoneTargetEffect(this);
-    }
-
-    @Override
-    public boolean applies(UUID objectId, Ability source, UUID affectedControllerId, Game game) {
-        Card theCard = game.getCard(objectId);
-        if (theCard == null) {
-            return false;
-        }
-        objectId = theCard.getMainCard().getId(); // for split cards and mdfc
-        if (getTargetPointer().getTargets(game, source).contains(objectId)
-                && source.isControlledBy(affectedControllerId)) {
-            Card card = game.getCard(objectId);
-            if (card != null) {
-                return true;
-            }
-        }
-        return false;
     }
 }
