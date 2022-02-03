27ea8760234751d4f7fdbb61b25acd8dcfd8f0f4
Evan Kranzler <theelk801@gmail.com>
2022-02-03 08:00:05 -0500

    [NEO] Implemented Akki Ronin

--- Mage.Sets/src/mage/sets/KamigawaNeonDynasty.java
+++ Mage.Sets/src/mage/sets/KamigawaNeonDynasty.java
@@ -27,6 +27,7 @@ public final class KamigawaNeonDynasty extends ExpansionSet {
         this.numBoosterDoubleFaced = 1; // temporary test fix
 
         cards.add(new SetCardInfo("Akki Ember-Keeper", 130, Rarity.COMMON, mage.cards.a.AkkiEmberKeeper.class));
+        cards.add(new SetCardInfo("Akki Ronin", 131, Rarity.COMMON, mage.cards.a.AkkiRonin.class));
         cards.add(new SetCardInfo("Akki War Paint", 132, Rarity.COMMON, mage.cards.a.AkkiWarPaint.class));
         cards.add(new SetCardInfo("Ancestral Katana", 1, Rarity.COMMON, mage.cards.a.AncestralKatana.class));
         cards.add(new SetCardInfo("Anchor to Reality", 45, Rarity.UNCOMMON, mage.cards.a.AnchorToReality.class));
