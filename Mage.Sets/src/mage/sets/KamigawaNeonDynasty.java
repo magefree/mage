626486d704e7a7b1a1d1811503c13ceb2b27b956
Evan Kranzler <theelk801@gmail.com>
2022-02-03 19:02:29 -0500

    [NEO] Implemented Go-Shintai of Boundless Vigor

--- Mage.Sets/src/mage/sets/KamigawaNeonDynasty.java
+++ Mage.Sets/src/mage/sets/KamigawaNeonDynasty.java
@@ -68,6 +68,7 @@ public final class KamigawaNeonDynasty extends ExpansionSet {
         cards.add(new SetCardInfo("Geothermal Kami", 186, Rarity.COMMON, mage.cards.g.GeothermalKami.class));
         cards.add(new SetCardInfo("Gloomshrieker", 219, Rarity.UNCOMMON, mage.cards.g.Gloomshrieker.class));
         cards.add(new SetCardInfo("Go-Shintai of Ancient Wars", 144, Rarity.UNCOMMON, mage.cards.g.GoShintaiOfAncientWars.class));
+        cards.add(new SetCardInfo("Go-Shintai of Boundless Vigor", 187, Rarity.COMMON, mage.cards.g.GoShintaiOfBoundlessVigor.class));
         cards.add(new SetCardInfo("Go-Shintai of Shared Purpose", 14, Rarity.UNCOMMON, mage.cards.g.GoShintaiOfSharedPurpose.class));
         cards.add(new SetCardInfo("Goro-Goro, Disciple of Ryusei", 145, Rarity.RARE, mage.cards.g.GoroGoroDiscipleOfRyusei.class));
         cards.add(new SetCardInfo("Gravelighter", 98, Rarity.UNCOMMON, mage.cards.g.Gravelighter.class));
