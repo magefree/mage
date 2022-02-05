fa3c862a79bb4191275cd108358003937e3b9058
Evan Kranzler <theelk801@gmail.com>
2022-02-05 17:30:04 -0500

    [NEO] Implemented Kami of Restless Shadows

---
+++ Mage.Sets/src/mage/cards/k/KamiOfRestlessShadows.java
@@ -0,0 +1,62 @@
+package mage.cards.k;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.Mode;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.effects.common.PutOnLibraryTargetEffect;
+import mage.abilities.effects.common.ReturnFromGraveyardToHandTargetEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.filter.FilterCard;
+import mage.filter.StaticFilters;
+import mage.filter.common.FilterCreatureCard;
+import mage.filter.predicate.Predicates;
+import mage.target.common.TargetCardInYourGraveyard;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class KamiOfRestlessShadows extends CardImpl {
+
+    private static final FilterCard filter
+            = new FilterCreatureCard("Ninja or Rogue creature card from your graveyard");
+
+    static {
+        filter.add(Predicates.or(
+                SubType.NINJA.getPredicate(),
+                SubType.ROGUE.getPredicate()
+        ));
+    }
+
+    public KamiOfRestlessShadows(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{4}{B}");
+
+        this.subtype.add(SubType.SPIRIT);
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(3);
+
+        // When Kami of Restless Shadows enters the battlefield, choose one —
+        // • Return up to one target Ninja or Rogue creature card from your graveyard to your hand.
+        Ability ability = new EntersBattlefieldTriggeredAbility(new ReturnFromGraveyardToHandTargetEffect());
+        ability.addTarget(new TargetCardInYourGraveyard(0, 1, filter));
+
+        // • Put target creature card from your graveyard on top of your library.
+        Mode mode = new Mode(new PutOnLibraryTargetEffect(true));
+        mode.addTarget(new TargetCardInYourGraveyard(StaticFilters.FILTER_CARD_CREATURE_YOUR_GRAVEYARD));
+        this.getSpellAbility().addMode(mode);
+    }
+
+    private KamiOfRestlessShadows(final KamiOfRestlessShadows card) {
+        super(card);
+    }
+
+    @Override
+    public KamiOfRestlessShadows copy() {
+        return new KamiOfRestlessShadows(this);
+    }
+}
--- Mage.Sets/src/mage/sets/KamigawaNeonDynasty.java
+++ Mage.Sets/src/mage/sets/KamigawaNeonDynasty.java
@@ -136,6 +136,7 @@ public final class KamigawaNeonDynasty extends ExpansionSet {
         cards.add(new SetCardInfo("Kairi, the Swirling Sky", 60, Rarity.MYTHIC, mage.cards.k.KairiTheSwirlingSky.class));
         cards.add(new SetCardInfo("Kaito Shizuki", 226, Rarity.MYTHIC, mage.cards.k.KaitoShizuki.class));
         cards.add(new SetCardInfo("Kaito's Pursuit", 103, Rarity.COMMON, mage.cards.k.KaitosPursuit.class));
+        cards.add(new SetCardInfo("Kami of Restless Shadows", 104, Rarity.COMMON, mage.cards.k.KamiOfRestlessShadows.class));
         cards.add(new SetCardInfo("Kami of Terrible Secrets", 105, Rarity.COMMON, mage.cards.k.KamiOfTerribleSecrets.class));
         cards.add(new SetCardInfo("Kami's Flare", 150, Rarity.COMMON, mage.cards.k.KamisFlare.class));
         cards.add(new SetCardInfo("Kappa Tech-Wrecker", 198, Rarity.UNCOMMON, mage.cards.k.KappaTechWrecker.class));
