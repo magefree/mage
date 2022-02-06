a978cf8851a1a8c970794a4fb9b8a277e53375b9
Evan Kranzler <theelk801@gmail.com>
2022-02-05 21:32:30 -0500

    removed commented out line

--- Mage.Verify/src/test/java/mage/verify/VerifyCardDataTest.java
+++ Mage.Verify/src/test/java/mage/verify/VerifyCardDataTest.java
@@ -90,7 +90,6 @@ public class VerifyCardDataTest {
 
         // power-toughness
         skipListCreate(SKIP_LIST_PT);
-//        skipListAddName(SKIP_LIST_PT, "NEO", "Futurist Sentinel"); // temporary
 
         // color
         skipListCreate(SKIP_LIST_COLOR);
