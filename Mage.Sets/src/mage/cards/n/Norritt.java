3cbfe4d623f2585cad6904c1c881c201b2057129
Evan Kranzler <theelk801@gmail.com>
2022-02-24 17:25:17 -0500

    [BOK] reworked implementation of Shirei, Shizo's Caretaker and added test

--- Mage.Sets/src/mage/cards/n/Norritt.java
+++ Mage.Sets/src/mage/cards/n/Norritt.java
@@ -104,7 +104,7 @@ class NorrittDelayedDestroyEffect extends OneShotEffect {
         DestroyTargetEffect effect = new DestroyTargetEffect();
         effect.setTargetPointer(new FixedTarget(source.getFirstTarget(), game));
         AtTheBeginOfNextEndStepDelayedTriggeredAbility delayedAbility
-                = new AtTheBeginOfNextEndStepDelayedTriggeredAbility(Zone.ALL, effect, TargetController.ANY, new InvertCondition(TargetAttackedThisTurnCondition.instance));
+                = new AtTheBeginOfNextEndStepDelayedTriggeredAbility(effect, TargetController.ANY, new InvertCondition(TargetAttackedThisTurnCondition.instance));
         delayedAbility.getDuration();
         delayedAbility.getTargets().addAll(source.getTargets());
         game.addDelayedTriggeredAbility(delayedAbility, source);
