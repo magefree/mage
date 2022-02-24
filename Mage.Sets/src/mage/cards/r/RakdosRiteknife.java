00c09631b9f38b0642d2d4c5a9cba77c6ca9f10e
Evan Kranzler <theelk801@gmail.com>
2022-02-23 22:30:09 -0500

    a few more reworked cards with getId in constructor

--- Mage.Sets/src/mage/cards/r/RakdosRiteknife.java
+++ Mage.Sets/src/mage/cards/r/RakdosRiteknife.java
@@ -1,7 +1,5 @@
 package mage.cards.r;
 
-import java.util.UUID;
-
 import mage.abilities.Ability;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.common.SimpleStaticAbility;
@@ -9,50 +7,51 @@ import mage.abilities.costs.common.SacrificeSourceCost;
 import mage.abilities.costs.common.SacrificeTargetCost;
 import mage.abilities.costs.common.TapSourceCost;
 import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.dynamicvalue.DynamicValue;
 import mage.abilities.dynamicvalue.common.CountersSourceCount;
-import mage.abilities.dynamicvalue.common.StaticValue;
-import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.ContinuousEffectImpl;
 import mage.abilities.effects.common.SacrificeEffect;
-import mage.abilities.effects.common.continuous.BoostEquippedEffect;
-import mage.abilities.effects.common.continuous.GainAbilityAttachedEffect;
+import mage.abilities.effects.common.counter.AddCountersTargetEffect;
 import mage.abilities.keyword.EquipAbility;
-import mage.constants.*;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
+import mage.constants.*;
 import mage.counters.CounterType;
 import mage.filter.StaticFilters;
 import mage.game.Game;
 import mage.game.permanent.Permanent;
 import mage.target.TargetPlayer;
 import mage.target.common.TargetControlledCreaturePermanent;
+import mage.target.targetpointer.FixedTarget;
+
+import java.util.UUID;
 
 import static mage.filter.StaticFilters.FILTER_CONTROLLED_CREATURE_SHORT_TEXT;
 
 /**
- *
- * @author noahg
+ * @author TheElk801
  */
 public final class RakdosRiteknife extends CardImpl {
 
+    private static final DynamicValue xValue = new CountersSourceCount(CounterType.BLOOD);
+
     public RakdosRiteknife(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT}, "{2}");
-        
+
         this.subtype.add(SubType.EQUIPMENT);
 
         // Equipped creature gets +1/+0 for each blood counter on Rakdos Riteknife and has "{T}, Sacrifice a creature: Put a blood counter on Rakdos Riteknife."
-        SimpleStaticAbility staticAbility = new SimpleStaticAbility(Zone.BATTLEFIELD, new BoostEquippedEffect(new CountersSourceCount(CounterType.BLOOD), StaticValue.get(0)).setText("Equipped creature gets +1/+0 for each blood counter on {this}"));
-        SimpleActivatedAbility grantedAbility = new SimpleActivatedAbility(new RakdosRiteKnifeEffect(this.getId()), new TapSourceCost());
-        grantedAbility.addCost(new SacrificeTargetCost(new TargetControlledCreaturePermanent(FILTER_CONTROLLED_CREATURE_SHORT_TEXT)));
-        staticAbility.addEffect(new GainAbilityAttachedEffect(grantedAbility, AttachmentType.EQUIPMENT).setText("and has \"{T}, Sacrifice a creature: Put a blood counter on {this}.\""));
-        this.addAbility(staticAbility);
+        this.addAbility(new SimpleStaticAbility(new RakdosRiteknifeEffect()));
 
         // {B}{R}, Sacrifice Rakdos Riteknife: Target player sacrifices a permanent for each blood counter on Rakdos Riteknife.
-        SimpleActivatedAbility activatedAbility = new SimpleActivatedAbility(
-                new SacrificeEffect(StaticFilters.FILTER_PERMANENT, new CountersSourceCount(CounterType.BLOOD), "Target player")
-                        .setText("target player sacrifices a permanent for each blood counter on {this}"), new ManaCostsImpl("{R}{B}"));
-        activatedAbility.addCost(new SacrificeSourceCost());
-        activatedAbility.addTarget(new TargetPlayer());
-        this.addAbility(activatedAbility);
+        Ability ability = new SimpleActivatedAbility(
+                new SacrificeEffect(StaticFilters.FILTER_PERMANENT, xValue, "Target player")
+                        .setText("target player sacrifices a permanent for each blood counter on {this}"),
+                new ManaCostsImpl<>("{R}{B}")
+        );
+        ability.addCost(new SacrificeSourceCost());
+        ability.addTarget(new TargetPlayer());
+        this.addAbility(ability);
 
         // Equip {2}
         this.addAbility(new EquipAbility(2));
@@ -68,34 +67,69 @@ public final class RakdosRiteknife extends CardImpl {
     }
 }
 
-class RakdosRiteKnifeEffect extends OneShotEffect {
+class RakdosRiteknifeEffect extends ContinuousEffectImpl {
 
-    private UUID effectGivingEquipmentId;
-
-    public RakdosRiteKnifeEffect(UUID effectGivingEquipmentId) {
-        super(Outcome.Benefit);
-        this.effectGivingEquipmentId = effectGivingEquipmentId;
-        staticText = "Put a blood counter on Rakdos Riteknife";
+    RakdosRiteknifeEffect() {
+        super(Duration.WhileOnBattlefield, Outcome.AddAbility);
+        staticText = "equipped creature gets +1/+0 for each blood counter on {this} " +
+                "and has \"{T}, Sacrifice a creature: Put a blood counter on {this}.\"";
     }
 
-    public RakdosRiteKnifeEffect(final RakdosRiteKnifeEffect effect) {
+    private RakdosRiteknifeEffect(final RakdosRiteknifeEffect effect) {
         super(effect);
-        this.effectGivingEquipmentId = effect.effectGivingEquipmentId;
     }
 
     @Override
     public boolean apply(Game game, Ability source) {
-        Permanent equipment = game.getPermanent(this.effectGivingEquipmentId);
-        if (equipment != null) {
-            equipment.addCounters(CounterType.BLOOD.createInstance(), source.getControllerId(), source, game);
-        }
-        return true;
+        return false;
     }
 
     @Override
-    public RakdosRiteKnifeEffect copy() {
-        return new RakdosRiteKnifeEffect(this);
+    public RakdosRiteknifeEffect copy() {
+        return new RakdosRiteknifeEffect(this);
     }
 
+    @Override
+    public boolean apply(Layer layer, SubLayer sublayer, Ability source, Game game) {
+        Permanent permanent = source.getSourcePermanentIfItStillExists(game);
+        if (permanent == null) {
+            return false;
+        }
+        Permanent creature = game.getPermanent(permanent.getAttachedTo());
+        if (creature == null) {
+            return false;
+        }
+        switch (layer) {
+            case AbilityAddingRemovingEffects_6:
+                creature.addAbility(makeAbility(permanent, game), source.getSourceId(), game);
+                return true;
+            case PTChangingEffects_7:
+                if (sublayer != SubLayer.ModifyPT_7c) {
+                    return false;
+                }
+                int count = permanent.getCounters(game).getCount(CounterType.BLOOD);
+                if (count < 1) {
+                    creature.getPower().boostValue(count);
+                    return true;
+                }
+        }
+        return false;
+    }
 
+    @Override
+    public boolean hasLayer(Layer layer) {
+        return layer == Layer.AbilityAddingRemovingEffects_6
+                || layer == Layer.PTChangingEffects_7;
+    }
+
+    private static Ability makeAbility(Permanent permanent, Game game) {
+        Ability ability = new SimpleActivatedAbility(
+                new AddCountersTargetEffect(CounterType.BLOOD.createInstance())
+                        .setText("put a blood counter on " + permanent.getName())
+                        .setTargetPointer(new FixedTarget(permanent, game)),
+                new TapSourceCost()
+        );
+        ability.addCost(new SacrificeTargetCost(new TargetControlledCreaturePermanent(FILTER_CONTROLLED_CREATURE_SHORT_TEXT)));
+        return ability;
+    }
 }
