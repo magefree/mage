eb4366013bbf4f4b770c5d2d4c4a6edec2d08223
Evan Kranzler <theelk801@gmail.com>
2022-02-24 20:14:43 -0500

    reworked auras which grant protection but don't remove themselves

--- Mage.Sets/src/mage/cards/r/RedWard.java
+++ Mage.Sets/src/mage/cards/r/RedWard.java
@@ -1,10 +1,7 @@
-
 package mage.cards.r;
 
-import java.util.UUID;
 import mage.ObjectColor;
 import mage.abilities.common.SimpleStaticAbility;
-import mage.abilities.effects.Effect;
 import mage.abilities.effects.common.AttachEffect;
 import mage.abilities.effects.common.continuous.GainAbilityAttachedEffect;
 import mage.abilities.keyword.EnchantAbility;
@@ -13,28 +10,20 @@ import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.AttachmentType;
 import mage.constants.CardType;
-import mage.constants.SubType;
 import mage.constants.Outcome;
-import mage.constants.Zone;
-import mage.filter.FilterCard;
-import mage.filter.predicate.mageobject.ColorPredicate;
+import mage.constants.SubType;
 import mage.target.TargetPermanent;
 import mage.target.common.TargetCreaturePermanent;
 
+import java.util.UUID;
+
 /**
- *
  * @author LoneFox
  */
 public final class RedWard extends CardImpl {
 
-    private static final FilterCard filter = new FilterCard("red");
-
-    static {
-        filter.add(new ColorPredicate(ObjectColor.RED));
-    }
-
     public RedWard(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.ENCHANTMENT},"{W}");
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{W}");
         this.subtype.add(SubType.AURA);
 
         // Enchant creature
@@ -42,12 +31,10 @@ public final class RedWard extends CardImpl {
         this.getSpellAbility().addTarget(auraTarget);
         this.getSpellAbility().addEffect(new AttachEffect(Outcome.Protect));
         this.addAbility(new EnchantAbility(auraTarget.getTargetName()));
-        // Enchanted creature has protection from red. This effect doesn't remove Red Ward.
-        ProtectionAbility gainedAbility = new ProtectionAbility(filter);
-        gainedAbility.setAuraIdNotToBeRemoved(this.getId());
-        Effect effect = new GainAbilityAttachedEffect(gainedAbility, AttachmentType.AURA);
-        effect.setText("Enchanted creature has protection from red. This effect doesn't remove {this}.");
-        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, effect));
+
+        this.addAbility(new SimpleStaticAbility(new GainAbilityAttachedEffect(
+                ProtectionAbility.from(ObjectColor.RED), AttachmentType.AURA
+        ).setDoesntRemoveItself(true)));
     }
 
     private RedWard(final RedWard card) {
--- Mage.Sets/src/mage/cards/t/TattooWard.java
+++ Mage.Sets/src/mage/cards/t/TattooWard.java
@@ -1,9 +1,5 @@
-
 package mage.cards.t;
 
-import java.util.UUID;
-import mage.constants.SubType;
-import mage.target.common.TargetCreaturePermanent;
 import mage.abilities.Ability;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.common.SimpleStaticAbility;
@@ -12,30 +8,21 @@ import mage.abilities.effects.common.AttachEffect;
 import mage.abilities.effects.common.DestroyTargetEffect;
 import mage.abilities.effects.common.continuous.BoostEnchantedEffect;
 import mage.abilities.effects.common.continuous.GainAbilityAttachedEffect;
-import mage.constants.Outcome;
-import mage.target.TargetPermanent;
 import mage.abilities.keyword.EnchantAbility;
 import mage.abilities.keyword.ProtectionAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.constants.AttachmentType;
-import mage.constants.CardType;
-import mage.constants.Duration;
-import mage.constants.Zone;
-import mage.filter.FilterCard;
+import mage.constants.*;
 import mage.filter.StaticFilters;
+import mage.target.TargetPermanent;
+import mage.target.common.TargetCreaturePermanent;
+
+import java.util.UUID;
 
 /**
- *
  * @author jeffwadsworth
  */
 public final class TattooWard extends CardImpl {
-
-    private static final FilterCard filter = new FilterCard("enchantments");
-
-    static {
-        filter.add(CardType.ENCHANTMENT.getPredicate());
-    }
 
     public TattooWard(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{2}{W}");
@@ -46,20 +33,22 @@ public final class TattooWard extends CardImpl {
         TargetPermanent auraTarget = new TargetCreaturePermanent();
         this.getSpellAbility().addTarget(auraTarget);
         this.getSpellAbility().addEffect(new AttachEffect(Outcome.BoostCreature));
-        Ability ability = new EnchantAbility(auraTarget.getTargetName());
-        this.addAbility(ability);
+        this.addAbility(new EnchantAbility(auraTarget.getTargetName()));
 
         // Enchanted creature gets +1/+1 and has protection from enchantments. This effect doesn't remove Tattoo Ward.
-        Ability ability2 = new SimpleStaticAbility(Zone.BATTLEFIELD, new BoostEnchantedEffect(1, 1, Duration.WhileOnBattlefield));
-        ProtectionAbility protectionAbility = new ProtectionAbility(filter);
-        protectionAbility.setAuraIdNotToBeRemoved(getId());
-        ability2.addEffect(new GainAbilityAttachedEffect(protectionAbility, AttachmentType.AURA, Duration.WhileOnBattlefield));
-        this.addAbility(ability2);
+        Ability ability = new SimpleStaticAbility(new BoostEnchantedEffect(
+                1, 1, Duration.WhileOnBattlefield
+        ));
+        ability.addEffect(new GainAbilityAttachedEffect(
+                new ProtectionAbility(StaticFilters.FILTER_PERMANENT_ENCHANTMENTS),
+                AttachmentType.AURA, Duration.WhileOnBattlefield
+        ).setDoesntRemoveItself(true));
+        this.addAbility(ability);
 
         // Sacrifice Tattoo Ward: Destroy target enchantment.
-        Ability ability3 = new SimpleActivatedAbility(new DestroyTargetEffect(), new SacrificeSourceCost());
-        ability3.addTarget(new TargetPermanent(StaticFilters.FILTER_PERMANENT_ENCHANTMENT));
-        this.addAbility(ability3);
+        ability = new SimpleActivatedAbility(new DestroyTargetEffect(), new SacrificeSourceCost());
+        ability.addTarget(new TargetPermanent(StaticFilters.FILTER_PERMANENT_ENCHANTMENT));
+        this.addAbility(ability);
 
     }
 
