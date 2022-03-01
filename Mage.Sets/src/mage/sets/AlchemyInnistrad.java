d21913c31685cba15393505e933cd747b131bfd7
Evan Kranzler <theelk801@gmail.com>
2022-03-01 17:46:52 -0500

    [Y22] added set

---
+++ Mage.Sets/src/mage/sets/AlchemyInnistrad.java
@@ -0,0 +1,23 @@
+package mage.sets;
+
+import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
+import mage.constants.SetType;
+
+/**
+ * @author TheElk801
+ */
+public final class AlchemyInnistrad extends ExpansionSet {
+
+    private static final AlchemyInnistrad instance = new AlchemyInnistrad();
+
+    public static AlchemyInnistrad getInstance() {
+        return instance;
+    }
+
+    private AlchemyInnistrad() {
+        super("Alchemy: Innistrad", "Y22", ExpansionSet.buildDate(2021, 12, 9), SetType.MAGIC_ARENA);
+        this.blockName = "Alchemy";
+        this.hasBoosters = false;
+    }
+}
