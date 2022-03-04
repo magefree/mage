7deb8de3c17aff2b5b0248e7ab03076b1cfa05f3
Evan Kranzler <theelk801@gmail.com>
2022-03-04 06:44:36 -0500

    [SNC] Implemented Xander's Lounge

--- Mage.Sets/src/mage/sets/StreetsOfNewCapenna.java
+++ Mage.Sets/src/mage/sets/StreetsOfNewCapenna.java
@@ -27,5 +27,6 @@ public final class StreetsOfNewCapenna extends ExpansionSet {
         cards.add(new SetCardInfo("Plains", 272, Rarity.LAND, mage.cards.basiclands.Plains.class, FULL_ART_BFZ_VARIOUS));
         cards.add(new SetCardInfo("Raffine's Tower", 254, Rarity.RARE, mage.cards.r.RaffinesTower.class));
         cards.add(new SetCardInfo("Swamp", 276, Rarity.LAND, mage.cards.basiclands.Swamp.class, FULL_ART_BFZ_VARIOUS));
+        cards.add(new SetCardInfo("Xander's Lounge", 260, Rarity.RARE, mage.cards.x.XandersLounge.class));
     }
 }
