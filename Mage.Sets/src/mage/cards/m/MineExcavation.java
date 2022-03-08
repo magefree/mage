8999a6e6473fc25d0679ea09bccd54e35cf337aa
Evan Kranzler <theelk801@gmail.com>
2022-03-07 22:53:52 -0500

    [SHM] various text fixes

--- Mage.Sets/src/mage/cards/m/MineExcavation.java
+++ Mage.Sets/src/mage/cards/m/MineExcavation.java
@@ -3,6 +3,7 @@ package mage.cards.m;
 
 import java.util.UUID;
 import mage.abilities.effects.common.ReturnFromGraveyardToHandTargetEffect;
+import mage.abilities.effects.common.ReturnToHandTargetEffect;
 import mage.abilities.keyword.ConspireAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
@@ -17,7 +18,7 @@ import mage.target.common.TargetCardInGraveyard;
  */
 public final class MineExcavation extends CardImpl {
 
-    private static final FilterCard filter = new FilterCard("artifact or enchantment card in a graveyard");
+    private static final FilterCard filter = new FilterCard("artifact or enchantment card from a graveyard");
 
     static {
         filter.add(Predicates.or(CardType.ARTIFACT.getPredicate(),
@@ -28,7 +29,7 @@ public final class MineExcavation extends CardImpl {
         super(ownerId,setInfo,new CardType[]{CardType.SORCERY},"{1}{W}");
 
         // Return target artifact or enchantment card from a graveyard to its owner's hand.
-        this.getSpellAbility().addEffect(new ReturnFromGraveyardToHandTargetEffect());
+        this.getSpellAbility().addEffect(new ReturnToHandTargetEffect());
         this.getSpellAbility().addTarget(new TargetCardInGraveyard(filter));
 
         // Conspire
