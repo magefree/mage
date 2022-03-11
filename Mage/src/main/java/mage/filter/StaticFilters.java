f45e90ad1bcb3c38b088f3e915c39bf5a2466aaa
Evan Kranzler <theelk801@gmail.com>
2022-03-10 23:29:07 -0500

    fixed a test failure

--- Mage/src/main/java/mage/filter/StaticFilters.java
+++ Mage/src/main/java/mage/filter/StaticFilters.java
@@ -124,7 +124,7 @@ public final class StaticFilters {
         FILTER_CARD_INSTANT_OR_SORCERY_FROM_YOUR_GRAVEYARD.setLockedFilter(true);
     }
 
-    public static final FilterCard FILTER_CARD_ARTIFACT_FROM_YOUR_GRAVEYARD = new FilterInstantOrSorceryCard("artifact card from your graveyard");
+    public static final FilterCard FILTER_CARD_ARTIFACT_FROM_YOUR_GRAVEYARD = new FilterArtifactCard("artifact card from your graveyard");
 
     static {
         FILTER_CARD_ARTIFACT_FROM_YOUR_GRAVEYARD.setLockedFilter(true);
