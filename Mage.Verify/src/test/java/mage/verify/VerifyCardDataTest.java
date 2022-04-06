a6821460d940c3f77e081c8b7947cf52f11eb24c
Evan Kranzler <theelk801@gmail.com>
2022-04-06 07:43:49 -0400

    added temporary verify skip

--- Mage.Verify/src/test/java/mage/verify/VerifyCardDataTest.java
+++ Mage.Verify/src/test/java/mage/verify/VerifyCardDataTest.java
@@ -100,6 +100,7 @@ public class VerifyCardDataTest {
 
         // cost
         skipListCreate(SKIP_LIST_COST);
+        skipListAddName(SKIP_LIST_COST, "SNC", "Brokers Charm"); // temporary
 
         // supertype
         skipListCreate(SKIP_LIST_SUPERTYPE);
