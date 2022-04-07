1c41e16e58453f3c9726d9bf0759e13d3c03d6e1
Evan Kranzler <theelk801@gmail.com>
2022-04-07 17:57:30 -0400

    reworked hideaway to match rule change

--- Mage.Sets/src/mage/cards/s/ShelldockIsle.java
+++ Mage.Sets/src/mage/cards/s/ShelldockIsle.java
@@ -1,6 +1,7 @@
 package mage.cards.s;
 
 import mage.abilities.Ability;
+import mage.abilities.common.EntersBattlefieldTappedAbility;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.condition.Condition;
 import mage.abilities.condition.common.CardsInAnyLibraryCondition;
@@ -28,7 +29,8 @@ public final class ShelldockIsle extends CardImpl {
         super(ownerId, setInfo, new CardType[]{CardType.LAND}, "");
 
         // Hideaway
-        this.addAbility(new HideawayAbility());
+        this.addAbility(new HideawayAbility(4));
+        this.addAbility(new EntersBattlefieldTappedAbility());
 
         // {tap}: Add {U}.
         this.addAbility(new BlueManaAbility());
