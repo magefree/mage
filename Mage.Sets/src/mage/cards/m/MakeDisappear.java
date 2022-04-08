92ef1af6113022835a1d8b8bc365974382ffe718
Evan Kranzler <theelk801@gmail.com>
2022-04-08 19:00:09 -0400

    [SNC] Implemented Make Disappear

---
+++ Mage.Sets/src/mage/cards/m/MakeDisappear.java
@@ -0,0 +1,37 @@
+package mage.cards.m;
+
+import mage.abilities.costs.mana.GenericManaCost;
+import mage.abilities.effects.common.CounterUnlessPaysEffect;
+import mage.abilities.keyword.CasualtyAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.target.TargetSpell;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class MakeDisappear extends CardImpl {
+
+    public MakeDisappear(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{1}{U}");
+
+        // Casualty 1
+        this.addAbility(new CasualtyAbility(this, 1));
+
+        // Counter target spell unless its controller pays {2}.
+        this.getSpellAbility().addEffect(new CounterUnlessPaysEffect(new GenericManaCost(2)));
+        this.getSpellAbility().addTarget(new TargetSpell());
+    }
+
+    private MakeDisappear(final MakeDisappear card) {
+        super(card);
+    }
+
+    @Override
+    public MakeDisappear copy() {
+        return new MakeDisappear(this);
+    }
+}
