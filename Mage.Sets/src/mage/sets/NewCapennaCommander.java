2d2763ebf6d899836991aaac703f5578fa4c420a
Evan Kranzler <theelk801@gmail.com>
2022-04-09 11:23:48 -0400

    [NCC] Implemented Kitt Kanto, Mayhem Diva

--- Mage.Sets/src/mage/sets/NewCapennaCommander.java
+++ Mage.Sets/src/mage/sets/NewCapennaCommander.java
@@ -1,6 +1,7 @@
 package mage.sets;
 
 import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
 import mage.constants.SetType;
 
 import java.util.Arrays;
@@ -21,6 +22,8 @@ public final class NewCapennaCommander extends ExpansionSet {
     private NewCapennaCommander() {
         super("New Capenna Commander", "NCC", ExpansionSet.buildDate(2022, 4, 29), SetType.SUPPLEMENTAL);
         this.hasBasicLands = false;
+
+        cards.add(new SetCardInfo("Kitt Kanto, Mayhem Diva", 4, Rarity.MYTHIC, mage.cards.k.KittKantoMayhemDiva.class));
 
         cards.removeIf(setCardInfo -> unfinished.contains(setCardInfo.getName())); // remove when shield counters are implemented
     }
