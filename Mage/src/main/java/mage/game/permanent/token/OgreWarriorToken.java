c5cb6eac8e6790e84d0731b76f5e8c5afa50b429
Evan Kranzler <theelk801@gmail.com>
2022-04-09 01:06:58 -0400

    [SNC] Implemented Join the Maestros

---
+++ Mage/src/main/java/mage/game/permanent/token/OgreWarriorToken.java
@@ -0,0 +1,29 @@
+package mage.game.permanent.token;
+
+import mage.MageInt;
+import mage.constants.CardType;
+import mage.constants.SubType;
+
+/**
+ * @author TheElk801
+ */
+public final class OgreWarriorToken extends TokenImpl {
+
+    public OgreWarriorToken() {
+        super("Ogre Warrior Token", "4/3 black Ogre Warrior creature token");
+        color.setBlack(true);
+        cardType.add(CardType.CREATURE);
+        subtype.add(SubType.OGRE);
+        subtype.add(SubType.WARRIOR);
+        power = new MageInt(4);
+        toughness = new MageInt(3);
+    }
+
+    public OgreWarriorToken(final OgreWarriorToken token) {
+        super(token);
+    }
+
+    public OgreWarriorToken copy() {
+        return new OgreWarriorToken(this);
+    }
+}
