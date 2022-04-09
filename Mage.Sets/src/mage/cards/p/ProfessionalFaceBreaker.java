4a6fa15b37fd14a250e3fba4295926442d031f94
Evan Kranzler <theelk801@gmail.com>
2022-04-09 11:11:15 -0400

    fixed verify failure

--- Mage.Sets/src/mage/cards/p/ProfessionalFaceBreaker.java
+++ Mage.Sets/src/mage/cards/p/ProfessionalFaceBreaker.java
@@ -33,7 +33,7 @@ public final class ProfessionalFaceBreaker extends CardImpl {
         this.toughness = new MageInt(3);
 
         // Menace
-        this.addAbility(new MenaceAbility());
+        this.addAbility(new MenaceAbility(false));
 
         // Whenever one or more creatures you control deal combat damage to a player, create a Treasure token.
         this.addAbility(new DealCombatDamageControlledTriggeredAbility(new CreateTokenEffect(new TreasureToken())));
