7eb8236efa517875d26a7f6f90dea06e741d5bd1
Evan Kranzler <theelk801@gmail.com>
2022-04-09 11:07:06 -0400

    [NCC] updated spoiler, added shield counter skip

--- Mage.Sets/src/mage/sets/NewCapennaCommander.java
+++ Mage.Sets/src/mage/sets/NewCapennaCommander.java
@@ -3,11 +3,15 @@ package mage.sets;
 import mage.cards.ExpansionSet;
 import mage.constants.SetType;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author TheElk801
  */
 public final class NewCapennaCommander extends ExpansionSet {
 
+    private static final List<String> unfinished = Arrays.asList("Kros, Defense Contractor", "Perrie, the Pulveriser");
     private static final NewCapennaCommander instance = new NewCapennaCommander();
 
     public static NewCapennaCommander getInstance() {
@@ -17,5 +21,7 @@ public final class NewCapennaCommander extends ExpansionSet {
     private NewCapennaCommander() {
         super("New Capenna Commander", "NCC", ExpansionSet.buildDate(2022, 4, 29), SetType.SUPPLEMENTAL);
         this.hasBasicLands = false;
+
+        cards.removeIf(setCardInfo -> unfinished.contains(setCardInfo.getName())); // remove when shield counters are implemented
     }
 }
