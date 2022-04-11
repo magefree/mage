477f6c1719558aaeb7bd1e6248e756992981c1e0
Evan Kranzler <theelk801@gmail.com>
2022-04-11 19:42:14 -0400

    [SNC] Implemented Black Market Tycoon

--- Mage.Sets/src/mage/sets/StreetsOfNewCapenna.java
+++ Mage.Sets/src/mage/sets/StreetsOfNewCapenna.java
@@ -28,6 +28,7 @@ public final class StreetsOfNewCapenna extends ExpansionSet {
 
         cards.add(new SetCardInfo("A Little Chat", 47, Rarity.UNCOMMON, mage.cards.a.ALittleChat.class));
         cards.add(new SetCardInfo("An Offer You Can't Refuse", 51, Rarity.UNCOMMON, mage.cards.a.AnOfferYouCantRefuse.class));
+        cards.add(new SetCardInfo("Black Market Tycoon", 167, Rarity.RARE, mage.cards.b.BlackMarketTycoon.class));
         cards.add(new SetCardInfo("Botanical Plaza", 247, Rarity.COMMON, mage.cards.b.BotanicalPlaza.class));
         cards.add(new SetCardInfo("Brokers Ascendancy", 170, Rarity.RARE, mage.cards.b.BrokersAscendancy.class));
         cards.add(new SetCardInfo("Brokers Charm", 171, Rarity.UNCOMMON, mage.cards.b.BrokersCharm.class));
