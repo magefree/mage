d2cb46029a60a1838aef49a03ae747a162348f91
Evan Kranzler <theelk801@gmail.com>
2022-04-13 22:41:22 -0400

    [SNC] Implemented Mayhem Patrol

---
+++ Mage.Sets/src/mage/cards/m/MayhemPatrol.java
@@ -0,0 +1,50 @@
+package mage.cards.m;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.AttacksTriggeredAbility;
+import mage.abilities.effects.common.continuous.BoostTargetEffect;
+import mage.abilities.keyword.BlitzAbility;
+import mage.abilities.keyword.MenaceAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.target.common.TargetCreaturePermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class MayhemPatrol extends CardImpl {
+
+    public MayhemPatrol(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{1}{R}");
+
+        this.subtype.add(SubType.DEVIL);
+        this.subtype.add(SubType.WARRIOR);
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(2);
+
+        // Menace
+        this.addAbility(new MenaceAbility());
+
+        // Whenever Mayhem Patrol attacks, target creature gets +1/+0 until end of turn.
+        Ability ability = new AttacksTriggeredAbility(new BoostTargetEffect(1, 0));
+        ability.addTarget(new TargetCreaturePermanent());
+        this.addAbility(ability);
+
+        // Blitz {1}{R}
+        this.addAbility(new BlitzAbility("{1}{R}"));
+    }
+
+    private MayhemPatrol(final MayhemPatrol card) {
+        super(card);
+    }
+
+    @Override
+    public MayhemPatrol copy() {
+        return new MayhemPatrol(this);
+    }
+}
