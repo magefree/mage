4ebd65e52f2350d4d407a97856cbf3d837976fde
Evan Kranzler <theelk801@gmail.com>
2022-04-16 09:40:02 -0400

    [SNC] Implemented Ready to Rumble

--- Mage.Sets/src/mage/sets/StreetsOfNewCapenna.java
+++ Mage.Sets/src/mage/sets/StreetsOfNewCapenna.java
@@ -176,6 +176,7 @@ public final class StreetsOfNewCapenna extends ExpansionSet {
         cards.add(new SetCardInfo("Raffine's Silencer", 90, Rarity.UNCOMMON, mage.cards.r.RaffinesSilencer.class));
         cards.add(new SetCardInfo("Raffine's Tower", 254, Rarity.RARE, mage.cards.r.RaffinesTower.class));
         cards.add(new SetCardInfo("Raffine, Scheming Seer", 213, Rarity.MYTHIC, mage.cards.r.RaffineSchemingSeer.class));
+        cards.add(new SetCardInfo("Ready to Rumble", 119, Rarity.COMMON, mage.cards.r.ReadyToRumble.class));
         cards.add(new SetCardInfo("Refuse to Yield", 27, Rarity.UNCOMMON, mage.cards.r.RefuseToYield.class));
         cards.add(new SetCardInfo("Reservoir Kraken", 56, Rarity.RARE, mage.cards.r.ReservoirKraken.class));
         cards.add(new SetCardInfo("Revel Ruiner", 91, Rarity.COMMON, mage.cards.r.RevelRuiner.class));
