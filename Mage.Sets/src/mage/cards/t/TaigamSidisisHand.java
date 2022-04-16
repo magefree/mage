f44230bc72a21cb2dfc25af3c7eec8b26d43e0f1
Alex W. Jackson <alex.w.jackson@gmail.com>
2022-04-15 22:03:47 -0400

    [C17] Reimplement Taigam, Sidisi's Hand

--- Mage.Sets/src/mage/cards/t/TaigamSidisisHand.java
+++ Mage.Sets/src/mage/cards/t/TaigamSidisisHand.java
@@ -6,12 +6,12 @@ import mage.abilities.Ability;
 import mage.abilities.common.BeginningOfUpkeepTriggeredAbility;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.common.SimpleStaticAbility;
-import mage.abilities.costs.Cost;
-import mage.abilities.costs.common.ExileFromGraveCost;
+import mage.abilities.costs.common.ExileXFromYourGraveCost;
 import mage.abilities.costs.common.TapSourceCost;
 import mage.abilities.costs.mana.ColoredManaCost;
-import mage.abilities.effects.ContinuousEffect;
-import mage.abilities.effects.OneShotEffect;
+import mage.abilities.dynamicvalue.DynamicValue;
+import mage.abilities.dynamicvalue.common.GetXValue;
+import mage.abilities.dynamicvalue.common.SignInversionDynamicValue;
 import mage.abilities.effects.common.LookLibraryAndPickControllerEffect;
 import mage.abilities.effects.common.LookLibraryControllerEffect.PutCards;
 import mage.abilities.effects.common.SkipDrawStepEffect;
@@ -20,18 +20,15 @@ import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.*;
 import mage.filter.StaticFilters;
-import mage.game.Game;
-import mage.game.permanent.Permanent;
-import mage.players.Player;
-import mage.target.common.TargetCardInYourGraveyard;
 import mage.target.common.TargetCreaturePermanent;
-import mage.target.targetpointer.FixedTarget;
 
 /**
  *
- * @author spjspj
+ * @author awjackson
  */
 public final class TaigamSidisisHand extends CardImpl {
+
+    private static final DynamicValue xValue = new SignInversionDynamicValue(GetXValue.instance);
 
     public TaigamSidisisHand(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{3}{U}{B}");
@@ -45,15 +42,18 @@ public final class TaigamSidisisHand extends CardImpl {
         // Skip your draw step.
         this.addAbility(new SimpleStaticAbility(new SkipDrawStepEffect()));
 
-        // At the beginning of your upkeep, look at the top three cards of your library. Put one of them into your hand and the rest into your graveyard.
+        // At the beginning of your upkeep, look at the top three cards of your library.
+        // Put one of them into your hand and the rest into your graveyard.
         this.addAbility(new BeginningOfUpkeepTriggeredAbility(
                 new LookLibraryAndPickControllerEffect(3, 1, PutCards.HAND, PutCards.GRAVEYARD),
                 TargetController.YOU, false));
 
         // {B}, {T}, Exile X cards from your graveyard: Target creature gets -X/-X until end of turn.
-        Ability ability = new SimpleActivatedAbility(new TaigamSidisisHandEffect(), new ColoredManaCost(ColoredManaSymbol.B));
+        Ability ability = new SimpleActivatedAbility(
+                new BoostTargetEffect(xValue, xValue, Duration.EndOfTurn),
+                new ColoredManaCost(ColoredManaSymbol.B));
         ability.addCost(new TapSourceCost());
-        ability.addCost(new ExileFromGraveCost(new TargetCardInYourGraveyard(0, Integer.MAX_VALUE, StaticFilters.FILTER_CARDS_FROM_YOUR_GRAVEYARD)));
+        ability.addCost(new ExileXFromYourGraveCost(StaticFilters.FILTER_CARDS_FROM_YOUR_GRAVEYARD));
         ability.addTarget(new TargetCreaturePermanent());
         this.addAbility(ability);
     }
@@ -65,43 +65,5 @@ public final class TaigamSidisisHand extends CardImpl {
     @Override
     public TaigamSidisisHand copy() {
         return new TaigamSidisisHand(this);
-    }
-}
-
-class TaigamSidisisHandEffect extends OneShotEffect {
-
-    public TaigamSidisisHandEffect() {
-        super(Outcome.Benefit);
-        this.staticText = "creature gets -X/-X until end of turn";
-    }
-
-    public TaigamSidisisHandEffect(final TaigamSidisisHandEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public TaigamSidisisHandEffect copy() {
-        return new TaigamSidisisHandEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Player controller = game.getPlayer(source.getControllerId());
-        if (controller != null) {
-            Permanent targetCreature = game.getPermanent(getTargetPointer().getFirst(game, source));
-
-            if (targetCreature != null) {
-                int amount = 0;
-                for (Cost cost : source.getCosts()) {
-                    if (cost instanceof ExileFromGraveCost) {
-                        amount = ((ExileFromGraveCost) cost).getExiledCards().size();
-                        ContinuousEffect effect = new BoostTargetEffect(-amount, -amount, Duration.EndOfTurn);
-                        effect.setTargetPointer(new FixedTarget(source.getTargets().getFirstTarget(), game));
-                        game.addEffect(effect, source);
-                    }
-                }
-            }
-        }
-        return false;
     }
 }
