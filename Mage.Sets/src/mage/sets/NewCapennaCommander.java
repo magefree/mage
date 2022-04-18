b97c0d40739a4e10fb3d5ff766c9ab37f29f4273
Evan Kranzler <theelk801@gmail.com>
2022-04-18 07:41:12 -0400

    removed all skips

--- Mage.Sets/src/mage/sets/NewCapennaCommander.java
+++ Mage.Sets/src/mage/sets/NewCapennaCommander.java
@@ -4,15 +4,10 @@ import mage.cards.ExpansionSet;
 import mage.constants.Rarity;
 import mage.constants.SetType;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * @author TheElk801
  */
 public final class NewCapennaCommander extends ExpansionSet {
-
-    private static final List<String> unfinished = Arrays.asList("Henzie \"Toolbox\" Torre");
 
     private static final NewCapennaCommander instance = new NewCapennaCommander();
 
@@ -25,7 +20,5 @@ public final class NewCapennaCommander extends ExpansionSet {
         this.hasBasicLands = false;
 
         cards.add(new SetCardInfo("Kitt Kanto, Mayhem Diva", 4, Rarity.MYTHIC, mage.cards.k.KittKantoMayhemDiva.class));
-
-        cards.removeIf(setCardInfo -> unfinished.contains(setCardInfo.getName())); // remove when shield counters are implemented
     }
 }
