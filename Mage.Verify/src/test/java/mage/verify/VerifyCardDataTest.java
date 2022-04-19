e24952a4a6c5250deb9b6cf07f929370d5f28229
Alex W. Jackson <alex.w.jackson@gmail.com>
2022-04-19 01:31:00 -0400

    Fix temporary verify skip

--- Mage.Verify/src/test/java/mage/verify/VerifyCardDataTest.java
+++ Mage.Verify/src/test/java/mage/verify/VerifyCardDataTest.java
@@ -109,7 +109,7 @@ public class VerifyCardDataTest {
         skipListAddName(SKIP_LIST_TYPE, "UNH", "Old Fogey"); // uses summon word as a joke card
         skipListAddName(SKIP_LIST_TYPE, "UND", "Old Fogey");
         skipListAddName(SKIP_LIST_TYPE, "UST", "capital offense"); // uses "instant" instead "Instant" as a joke card
-        skipListAddName(SKIP_LIST_TYPE, "SNC", "Spellbinding Soprano"); // temporary
+        skipListAddName(SKIP_LIST_TYPE, "NCC", "Spellbinding Soprano"); // temporary
 
         // subtype
         skipListCreate(SKIP_LIST_SUBTYPE);
