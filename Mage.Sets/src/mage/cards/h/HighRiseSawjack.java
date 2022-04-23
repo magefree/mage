53e5e2cd747e6e65de87f0e77969c4900c115d28
Hidde van Bavel <hiddevanbavel@hotmail.com>
2022-04-23 22:43:26 +0200

    [SNC] Implement High-Rise Sawjack.  (#8847)
    * [SNC] Implement High-Rise Sawjack. Add BlocksCreatureWithFlyingTriggeredAbility.
    
    * [SNC] Implement more generic BlocksCreatureTriggeredAbility
    
    Co-authored-by: Evan Kranzler <theelk801@gmail.com>

--- Mage.Sets/src/mage/cards/h/HighRiseSawjack.java
+++ Mage.Sets/src/mage/cards/h/HighRiseSawjack.java
@@ -1,7 +1,5 @@
+package mage.cards.h;
 
-package mage.cards.n;
-
-import java.util.UUID;
 import mage.MageInt;
 import mage.abilities.common.BlocksCreatureTriggeredAbility;
 import mage.abilities.effects.common.continuous.BoostSourceEffect;
@@ -10,16 +8,17 @@ import mage.abilities.keyword.ReachAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.SubType;
 import mage.constants.Duration;
+import mage.constants.SubType;
 import mage.filter.common.FilterCreaturePermanent;
 import mage.filter.predicate.mageobject.AbilityPredicate;
 
+import java.util.UUID;
+
 /**
- *
- * @author LevelX2
+ * @author Hiddevb
  */
-public final class NetcasterSpider extends CardImpl {
+public final class HighRiseSawjack extends CardImpl {
 
     private static final FilterCreaturePermanent filter = new FilterCreaturePermanent("creature with flying");
 
@@ -27,26 +26,27 @@ public final class NetcasterSpider extends CardImpl {
         filter.add(new AbilityPredicate(FlyingAbility.class));
     }
 
-    public NetcasterSpider(UUID ownerId, CardSetInfo setInfo) {
+    public HighRiseSawjack(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId,setInfo,new CardType[]{CardType.CREATURE},"{2}{G}");
-        this.subtype.add(SubType.SPIDER);
 
+        this.subtype.add(SubType.ELF);
+        this.subtype.add(SubType.CITIZEN);
         this.power = new MageInt(2);
         this.toughness = new MageInt(3);
 
         // Reach
         this.addAbility(ReachAbility.getInstance());
 
-        // Whenever Netcaster Spider blocks a creature with flying, Netcaster Spider gets +2/+0 until end of turn.
+        // Whenever High-Rise Sawjack blocks a creature with flying, High-Rise Sawjack gets +2/+0 until end of turn.
         this.addAbility(new BlocksCreatureTriggeredAbility(new BoostSourceEffect(2, 0, Duration.EndOfTurn), filter, false));
     }
 
-    private NetcasterSpider(final NetcasterSpider card) {
+    private HighRiseSawjack(final HighRiseSawjack card) {
         super(card);
     }
 
     @Override
-    public NetcasterSpider copy() {
-        return new NetcasterSpider(this);
+    public HighRiseSawjack copy() {
+        return new HighRiseSawjack(this);
     }
 }
