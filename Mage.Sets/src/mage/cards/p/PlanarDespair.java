ec607ae4c4b7ae01fc5470533ab922bc81375b3c
Jeff Wadsworth <jeff@delmarus.com>
2022-05-03 10:19:10 -0500

    - Fixed #8898

--- Mage.Sets/src/mage/cards/p/PlanarDespair.java
+++ Mage.Sets/src/mage/cards/p/PlanarDespair.java
@@ -1,8 +1,7 @@
-
 package mage.cards.p;
 
 import java.util.UUID;
-import mage.abilities.dynamicvalue.DynamicValue;
+import mage.abilities.dynamicvalue.LockedInDynamicValue;
 import mage.abilities.dynamicvalue.common.DomainValue;
 import mage.abilities.dynamicvalue.common.SignInversionDynamicValue;
 import mage.abilities.effects.Effect;
@@ -16,15 +15,17 @@ import mage.constants.Duration;
 /**
  *
  * @author LoneFox
-
+ *
  */
 public final class PlanarDespair extends CardImpl {
 
+    // rule 608.2h
+    private static final LockedInDynamicValue dv = new LockedInDynamicValue(new SignInversionDynamicValue(DomainValue.REGULAR));
+
     public PlanarDespair(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.SORCERY},"{3}{B}{B}");
+        super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{3}{B}{B}");
 
         // Domain - All creatures get -1/-1 until end of turn for each basic land type among lands you control.
-        DynamicValue dv = new SignInversionDynamicValue(DomainValue.REGULAR);
         Effect effect = new BoostAllEffect(dv, dv, Duration.EndOfTurn);
         effect.setText("<i>Domain</i> &mdash; All creatures get -1/-1 until end of turn for each basic land type among lands you control.");
         this.getSpellAbility().addEffect(effect);
