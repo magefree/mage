ffa486473d5906d13643c28b8653203cef57292a
Evan Kranzler <theelk801@gmail.com>
2022-05-18 19:00:21 -0400

    [CLB] updated spoiler

--- Mage.Sets/src/mage/sets/CommanderLegendsBattleForBaldursGate.java
+++ Mage.Sets/src/mage/sets/CommanderLegendsBattleForBaldursGate.java
@@ -69,7 +69,7 @@ public final class CommanderLegendsBattleForBaldursGate extends ExpansionSet {
         cards.add(new SetCardInfo("Wand of Wonder", 204, Rarity.RARE, mage.cards.w.WandOfWonder.class));
         cards.add(new SetCardInfo("Wayfarer's Bauble", 344, Rarity.COMMON, mage.cards.w.WayfarersBauble.class));
         cards.add(new SetCardInfo("White Plume Adventurer", 49, Rarity.RARE, mage.cards.w.WhitePlumeAdventurer.class));
-        cards.add(new SetCardInfo("Wilson, Refined Grizzly", 522, Rarity.UNCOMMON, mage.cards.w.WilsonRefinedGrizzly.class));
+        cards.add(new SetCardInfo("Wilson, Refined Grizzly", 261, Rarity.UNCOMMON, mage.cards.w.WilsonRefinedGrizzly.class));
         cards.add(new SetCardInfo("Zevlor, Elturel Exile", 296, Rarity.RARE, mage.cards.z.ZevlorElturelExile.class));
     }
 }
