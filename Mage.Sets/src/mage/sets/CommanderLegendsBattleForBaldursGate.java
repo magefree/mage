ca7de23eb57f3db7d36c78641774f5df5899d5d9
Evan Kranzler <theelk801@gmail.com>
2022-05-30 10:46:33 -0400

    [CLB] Implemented Nimblewright Schematic

--- Mage.Sets/src/mage/sets/CommanderLegendsBattleForBaldursGate.java
+++ Mage.Sets/src/mage/sets/CommanderLegendsBattleForBaldursGate.java
@@ -191,6 +191,7 @@ public final class CommanderLegendsBattleForBaldursGate extends ExpansionSet {
         cards.add(new SetCardInfo("Navigation Orb", 329, Rarity.COMMON, mage.cards.n.NavigationOrb.class));
         cards.add(new SetCardInfo("Nemesis Phoenix", 189, Rarity.UNCOMMON, mage.cards.n.NemesisPhoenix.class));
         cards.add(new SetCardInfo("Nimbleclaw Adept", 86, Rarity.COMMON, mage.cards.n.NimbleclawAdept.class));
+        cards.add(new SetCardInfo("Nimblewright Schematic", 330, Rarity.COMMON, mage.cards.n.NimblewrightSchematic.class));
         cards.add(new SetCardInfo("Nine-Fingers Keene", 289, Rarity.RARE, mage.cards.n.NineFingersKeene.class));
         cards.add(new SetCardInfo("Noble's Purse", 331, Rarity.UNCOMMON, mage.cards.n.NoblesPurse.class));
         cards.add(new SetCardInfo("Nothic", 138, Rarity.UNCOMMON, mage.cards.n.Nothic.class));
--- Mage/src/main/java/mage/abilities/common/EntersBattlefieldOrDiesSourceTriggeredAbility.java
+++ Mage/src/main/java/mage/abilities/common/EntersBattlefieldOrDiesSourceTriggeredAbility.java
@@ -1,5 +1,3 @@
-
-
 package mage.abilities.common;
 
 import mage.abilities.TriggeredAbilityImpl;
@@ -7,23 +5,28 @@ import mage.abilities.effects.Effect;
 import mage.constants.Zone;
 import mage.game.Game;
 import mage.game.events.GameEvent;
-import mage.game.events.GameEvent.EventType;
 import mage.game.events.ZoneChangeEvent;
 
 /**
- *
  * @author LevelX2
  */
-
 public class EntersBattlefieldOrDiesSourceTriggeredAbility extends TriggeredAbilityImpl {
 
+    private final boolean diesText;
+
     public EntersBattlefieldOrDiesSourceTriggeredAbility(Effect effect, boolean optional) {
+        this(effect, optional, true);
+    }
+
+    public EntersBattlefieldOrDiesSourceTriggeredAbility(Effect effect, boolean optional, boolean diesText) {
         super(Zone.BATTLEFIELD, effect, optional);
+        this.diesText = diesText;
         setLeavesTheBattlefieldTrigger(true);
     }
 
     public EntersBattlefieldOrDiesSourceTriggeredAbility(final EntersBattlefieldOrDiesSourceTriggeredAbility ability) {
         super(ability);
+        this.diesText = ability.diesText;
     }
 
     @Override
@@ -33,7 +36,8 @@ public class EntersBattlefieldOrDiesSourceTriggeredAbility extends TriggeredAbil
 
     @Override
     public boolean checkEventType(GameEvent event, Game game) {
-        return event.getType() == GameEvent.EventType.ENTERS_THE_BATTLEFIELD || event.getType() == GameEvent.EventType.ZONE_CHANGE ;
+        return event.getType() == GameEvent.EventType.ENTERS_THE_BATTLEFIELD
+                || event.getType() == GameEvent.EventType.ZONE_CHANGE;
     }
 
     @Override
@@ -53,6 +57,7 @@ public class EntersBattlefieldOrDiesSourceTriggeredAbility extends TriggeredAbil
 
     @Override
     public String getTriggerPhrase() {
-        return "When {this} enters the battlefield or dies, " ;
+        return "When {this} enters the battlefield or " +
+                (diesText ? "dies" : "is put into a graveyard from the battlefield") + ", ";
     }
 }
