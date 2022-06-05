3fc84d9ba471bf82e41a3a8b9f807d069d2412ba
PurpleCrowbar <loganreid47@hotmail.co.uk>
2022-06-05 13:48:53 +0100

    Removed duplicate dragon egg dragon token class

--- Mage/src/main/java/mage/game/permanent/token/DragonEggDragonToken.java
+++ Mage/src/main/java/mage/game/permanent/token/DragonEggDragonToken.java
@@ -28,7 +28,7 @@ public final class DragonEggDragonToken extends TokenImpl {
         this.addAbility(FlyingAbility.getInstance());
         this.addAbility(new SimpleActivatedAbility(Zone.BATTLEFIELD, new BoostSourceEffect(1, 0, Duration.EndOfTurn), new ManaCostsImpl("{R}")));
 
-        availableImageSetCodes = Arrays.asList("C18", "EMA", "M14", "M19", "CMR", "IMA");
+        availableImageSetCodes = Arrays.asList("C18", "EMA", "M14", "M15", "M19", "CMR", "IMA");
     }
 
     public DragonEggDragonToken(final DragonEggDragonToken token) {
