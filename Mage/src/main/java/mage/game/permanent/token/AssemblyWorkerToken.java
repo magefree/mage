c3e20a3f0cb7bb8a75fc8d01cf151d2eaa63cb76
PurpleCrowbar <26198472+PurpleCrowbar@users.noreply.github.com>
2022-06-09 00:05:45 +0100

    Removed 4ED as an available token image source (no token images)

--- Mage/src/main/java/mage/game/permanent/token/AssemblyWorkerToken.java
+++ Mage/src/main/java/mage/game/permanent/token/AssemblyWorkerToken.java
@@ -22,7 +22,7 @@ public final class AssemblyWorkerToken extends TokenImpl {
         power = new MageInt(2);
         toughness = new MageInt(2);
 
-        availableImageSetCodes = Arrays.asList("4ED", "ATQ", "TSR");
+        availableImageSetCodes = Arrays.asList("ATQ", "TSR");
     }
 
     public AssemblyWorkerToken(final AssemblyWorkerToken token) {
