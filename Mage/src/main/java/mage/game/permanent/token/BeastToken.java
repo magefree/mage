8316e6a81348e03deaac66273bcafaccbb028fd9
PurpleCrowbar <26198472+PurpleCrowbar@users.noreply.github.com>
2022-06-09 00:15:35 +0100

    Removed 5DN as an available token image source (no token images)

--- Mage/src/main/java/mage/game/permanent/token/BeastToken.java
+++ Mage/src/main/java/mage/game/permanent/token/BeastToken.java
@@ -20,7 +20,7 @@ public final class BeastToken extends TokenImpl {
         power = new MageInt(3);
         toughness = new MageInt(3);
 
-        availableImageSetCodes = Arrays.asList("5DN", "C14", "C16", "C19", "CMA", "CN2", "GVL",
+        availableImageSetCodes = Arrays.asList("C14", "C16", "C19", "CMA", "CN2", "GVL",
                 "DD3C", "DD3GVL", "DDD", "DDL", "DST", "E01", "EVE", "LRW", "M10", "M11", "M12",
                 "M13", "M14", "M15", "MM3", "NPH", "PC2", "USG", "M19", "IKO", "M21", "CMR", "C21", "AFC", "MIC", "NEC", "2XM", "NCC", "BBD", "PCA", "CLB");
     }
