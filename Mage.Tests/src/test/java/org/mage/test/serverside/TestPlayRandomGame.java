8f4a2404e9b85cc9a7948d72b77d0e68733d1ed6
DeepCrimson <deepcrimson@users.noreplyl.github.com>
2022-06-26 23:28:23 -0700

    Refactor: Remove redundant method generateRandomDeck

--- Mage.Tests/src/test/java/org/mage/test/serverside/TestPlayRandomGame.java
+++ Mage.Tests/src/test/java/org/mage/test/serverside/TestPlayRandomGame.java
@@ -1,9 +1,6 @@
 package org.mage.test.serverside;
 
-import mage.cards.Card;
-import mage.cards.Sets;
 import mage.cards.decks.Deck;
-import mage.constants.ColoredManaSymbol;
 import mage.constants.MultiplayerAttackOption;
 import mage.constants.RangeOfInfluence;
 import mage.game.Game;
@@ -11,12 +8,12 @@ import mage.game.GameException;
 import mage.game.GameOptions;
 import mage.game.TwoPlayerDuel;
 import mage.game.mulligan.MulliganType;
-import mage.player.ai.ComputerPlayer;
 import mage.players.Player;
 import mage.util.RandomUtil;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.mage.test.serverside.base.MageTestBase;
+import org.mage.test.utils.DeckTestUtils;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -30,7 +27,6 @@ import java.util.Locale;
 public class TestPlayRandomGame extends MageTestBase {
 
     private static final List<String> colorChoices = new ArrayList<>(Arrays.asList("bu", "bg", "br", "bw", "ug", "ur", "uw", "gr", "gw", "rw", "bur", "buw", "bug", "brg", "brw", "bgw", "wur", "wug", "wrg", "rgu"));
-    private static final int DECK_SIZE = 40;
 
     @Test
     @Ignore
@@ -45,21 +41,17 @@ public class TestPlayRandomGame extends MageTestBase {
         Game game = new TwoPlayerDuel(MultiplayerAttackOption.LEFT, RangeOfInfluence.ALL, MulliganType.GAME_DEFAULT.getMulligan(0), 20);
 
         Player computerA = createRandomPlayer("ComputerA");
-        Deck deck = generateRandomDeck();
+        String deckAColors = colorChoices.get(RandomUtil.nextInt(colorChoices.size())).toUpperCase(Locale.ENGLISH);
+        Deck deckA = DeckTestUtils.buildRandomDeck(deckAColors, false);
 
-        if (deck.getCards().size() < DECK_SIZE) {
-            throw new IllegalArgumentException("Couldn't load deck, deck size = " + deck.getCards().size() + ", but must be " + DECK_SIZE);
-        }
-        game.addPlayer(computerA, deck);
-        game.loadCards(deck.getCards(), computerA.getId());
+        game.addPlayer(computerA, deckA);
+        game.loadCards(deckA.getCards(), computerA.getId());
 
         Player computerB = createRandomPlayer("ComputerB");
-        Deck deck2 = generateRandomDeck();
-        if (deck2.getCards().size() < DECK_SIZE) {
-            throw new IllegalArgumentException("Couldn't load deck, deck size=" + deck2.getCards().size() + ", but must be " + DECK_SIZE);
-        }
-        game.addPlayer(computerB, deck2);
-        game.loadCards(deck2.getCards(), computerB.getId());
+        String deckBColors = colorChoices.get(RandomUtil.nextInt(colorChoices.size())).toUpperCase(Locale.ENGLISH);
+        Deck deckB = DeckTestUtils.buildRandomDeck(deckBColors, false);
+        game.addPlayer(computerB, deckB);
+        game.loadCards(deckB.getCards(), computerB.getId());
 
         long t1 = System.nanoTime();
         GameOptions options = new GameOptions();
@@ -70,17 +62,5 @@ public class TestPlayRandomGame extends MageTestBase {
 
         logger.info("Winner: " + game.getWinner());
         logger.info("Time: " + (t2 - t1) / 1000000 + " ms");
-    }
-
-    private Deck generateRandomDeck() {
-        String selectedColors = colorChoices.get(RandomUtil.nextInt(colorChoices.size())).toUpperCase(Locale.ENGLISH);
-        List<ColoredManaSymbol> allowedColors = new ArrayList<>();
-        logger.info("Building deck with colors: " + selectedColors);
-        for (int i = 0; i < selectedColors.length(); i++) {
-            char c = selectedColors.charAt(i);
-            allowedColors.add(ColoredManaSymbol.lookup(c));
-        }
-        List<Card> cardPool = Sets.generateRandomCardPool(45, allowedColors);
-        return ComputerPlayer.buildDeck(DECK_SIZE, cardPool, allowedColors);
     }
 }
