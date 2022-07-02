6dd898a83c1eff0db18020213a79445c404bfe58
Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>
2022-07-01 20:47:56 -0400

    Fix verify failure

--- Mage.Sets/src/mage/cards/c/CloakOfInvisibility.java
+++ Mage.Sets/src/mage/cards/c/CloakOfInvisibility.java
@@ -41,9 +41,9 @@ public final class CloakOfInvisibility extends CardImpl {
         // Enchanted creature has phasing and can't be blocked except by Walls.
         ability = new SimpleStaticAbility(Zone.BATTLEFIELD, new GainAbilityAttachedEffect(PhasingAbility.getInstance(), AttachmentType.AURA).setText("Enchanted creature has phasing "));
         ability.addEffect(new CantBeBlockedByCreaturesAttachedEffect(Duration.WhileOnBattlefield, filter, AttachmentType.AURA)
-                .setText("and can’t be blocked except by Walls. " +
+                .setText("and can't be blocked except by Walls. " +
                         "<i>(It phases in or out before its controller untaps during each of their untap steps. " +
-                        "While it’s phased out, it’s treated as though it doesn’t exist.)</i>"));
+                        "While it's phased out, it's treated as though it doesn't exist.)</i>"));
         this.addAbility(ability);
     }
 
