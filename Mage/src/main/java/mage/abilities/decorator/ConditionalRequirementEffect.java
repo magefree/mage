78f3547644f5bc9ad6db468ae77b244d2cf8cd83
Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>
2022-07-04 20:28:54 -0400

    Added common interface to conditional effects (#9208)

--- Mage/src/main/java/mage/abilities/decorator/ConditionalRequirementEffect.java
+++ Mage/src/main/java/mage/abilities/decorator/ConditionalRequirementEffect.java
@@ -150,4 +150,8 @@ public class ConditionalRequirementEffect extends RequirementEffect {
         return new ConditionalRequirementEffect(this);
     }
 
+    @Override
+    public Condition getCondition() {
+        return condition;
+    }
 }
