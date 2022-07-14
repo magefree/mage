7ede88f22035ddbc6b9b42017a5a63a628c7e849
Alex W. Jackson <alex.w.jackson@gmail.com>
2022-07-14 19:36:59 -0400

    Use ChooseHint for some cards with two targets

--- Mage.Sets/src/mage/cards/a/AgonyWarp.java
+++ Mage.Sets/src/mage/cards/a/AgonyWarp.java
@@ -1,42 +1,32 @@
-
 package mage.cards.a;
 
 import java.util.UUID;
-import mage.abilities.effects.Effect;
 import mage.abilities.effects.common.continuous.BoostTargetEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.Duration;
-import mage.filter.common.FilterCreaturePermanent;
-import mage.target.Target;
 import mage.target.common.TargetCreaturePermanent;
 import mage.target.targetpointer.SecondTargetPointer;
 
 /**
  *
- * @author North
+ * @author awjackson
  */
 public final class AgonyWarp extends CardImpl {
 
     public AgonyWarp(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId,setInfo,new CardType[]{CardType.INSTANT},"{U}{B}");
 
-
         // Target creature gets -3/-0 until end of turn.
-        Effect effect = new BoostTargetEffect(-3,0, Duration.EndOfTurn);
-        effect.setText("Target creature gets -3/-0 until end of turn");
-        this.getSpellAbility().addEffect(effect);
-        Target target = new TargetCreaturePermanent(new FilterCreaturePermanent("first creature"));
-        this.getSpellAbility().addTarget(target);
+        this.getSpellAbility().addTarget(new TargetCreaturePermanent().withChooseHint("-3/-0"));
+        this.getSpellAbility().addEffect(new BoostTargetEffect(-3, 0, Duration.EndOfTurn));
 
         // Target creature gets -0/-3 until end of turn.
-        Effect effect2 = new BoostTargetEffect(-0,-3, Duration.EndOfTurn);
-        effect2.setText("<br><br>Target creature gets -0/-3 until end of turn");
-        effect2.setTargetPointer(SecondTargetPointer.getInstance());
-        this.getSpellAbility().addEffect(effect2);
-        target = new TargetCreaturePermanent(new FilterCreaturePermanent("second creature (can be the same as the first)"));
-        this.getSpellAbility().addTarget(target);
+        this.getSpellAbility().addTarget(new TargetCreaturePermanent().withChooseHint("-0/-3"));
+        this.getSpellAbility().addEffect(new BoostTargetEffect(0, -3, Duration.EndOfTurn)
+                .setTargetPointer(SecondTargetPointer.getInstance())
+                .concatBy("<br>"));
     }
 
     private AgonyWarp(final AgonyWarp card) {
