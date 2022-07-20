9e41fc9156256ef6b1adcd2e2f9ebb01ddf51957
Spencer Lanman <slanman3755@gmail.com>
2022-07-20 00:18:30 -0700

    Add ability to Smuggler's Share

--- Mage.Sets/src/mage/cards/s/SmugglersShare.java
+++ Mage.Sets/src/mage/cards/s/SmugglersShare.java
@@ -1,21 +1,40 @@
 package mage.cards.s;
 
+import java.util.List;
 import java.util.UUID;
+
+import mage.abilities.Ability;
+import mage.abilities.common.BeginningOfEndStepTriggeredAbility;
+import mage.abilities.dynamicvalue.DynamicValue;
+import mage.abilities.effects.Effect;
+import mage.abilities.effects.common.CreateTokenEffect;
+import mage.abilities.effects.common.DrawCardAllEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.constants.TargetController;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.game.permanent.token.TreasureToken;
+import mage.players.Player;
+import mage.watchers.common.CardsAmountDrawnThisTurnWatcher;
+import mage.watchers.common.PermanentsEnteredBattlefieldWatcher;
 
 /**
+ * One way or another, everyone gets paid what they're owed.
+ * Based on Gadrak, the Crown Scourge, Hoard Hauler, Lavaball Trap, Walker Of The Grove, Widespread Thieving, and Wound Reflection
  *
- * @author anonymous
+ * @author Slanman3755
  */
 public final class SmugglersShare extends CardImpl {
 
     public SmugglersShare(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{2}{W}");
-        
 
         // At the beginning of each end step, draw a card for each opponent who drew two or more cards this turn, then create a Treasure token for each opponent who had two or more lands enter the battlefield under their control this turn.
+        Ability ability = new BeginningOfEndStepTriggeredAbility(new DrawCardAllEffect(SmugglersShareDrawValue.instance), TargetController.YOU, false);
+        ability.addEffect(new CreateTokenEffect(new TreasureToken(), SmugglersShareTreasureValue.instance).concatBy(", then"));
+        this.addAbility(ability);
     }
 
     private SmugglersShare(final SmugglersShare card) {
@@ -25,5 +44,88 @@ public final class SmugglersShare extends CardImpl {
     @Override
     public SmugglersShare copy() {
         return new SmugglersShare(this);
+    }
+}
+
+enum SmugglersShareDrawValue implements DynamicValue {
+    instance;
+
+    @Override
+    public int calculate(Game game, Ability source, Effect effect) {
+        Player controller = game.getPlayer(source.getControllerId());
+        CardsAmountDrawnThisTurnWatcher watcher = game.getState().getWatcher(CardsAmountDrawnThisTurnWatcher.class);
+        int drawCount = 0;
+        if (watcher != null) {
+            for (UUID playerId : game.getOpponents(controller.getId())) {
+                Player opponent = game.getPlayer(playerId);
+                if (opponent != null) {
+                    if (watcher.getAmountCardsDrawn(playerId) >= 2) {
+                        drawCount++;
+                    }
+                }
+            }
+        }
+        return drawCount;
+    }
+
+    @Override
+    public SmugglersShareDrawValue copy() {
+        return instance;
+    }
+
+    @Override
+    public String getMessage() {
+        return "opponent who drew two or more cards this turn";
+    }
+
+    @Override
+    public String toString() {
+        return "1";
+    }
+}
+
+enum SmugglersShareTreasureValue implements DynamicValue {
+    instance;
+
+    @Override
+    public int calculate(Game game, Ability source, Effect effect) {
+        Player controller = game.getPlayer(source.getControllerId());
+        PermanentsEnteredBattlefieldWatcher watcher = game.getState().getWatcher(PermanentsEnteredBattlefieldWatcher.class);
+        int treasureCount = 0;
+        if (watcher != null) {
+            for (UUID opponentId : game.getOpponents(controller.getId())) {
+                Player opponent = game.getPlayer(opponentId);
+                if (opponent != null) {
+                    List<Permanent> enteredPermanents = watcher.getThisTurnEnteringPermanents(opponentId);
+                    if (enteredPermanents != null) {
+                        int enteredLandCount = 0;
+                        for (Permanent permanent: enteredPermanents) {
+                            if (permanent.isLand(game)) {
+                                enteredLandCount++;
+                            }
+                        }
+                        if (enteredLandCount >= 2) {
+                            treasureCount++;
+                        }
+                    }
+                }
+            }
+        }
+        return treasureCount;
+    }
+
+    @Override
+    public SmugglersShareTreasureValue copy() {
+        return instance;
+    }
+
+    @Override
+    public String getMessage() {
+        return "opponent who had two or more lands enter the battlefield under their control this turn";
+    }
+
+    @Override
+    public String toString() {
+        return "1";
     }
 }
