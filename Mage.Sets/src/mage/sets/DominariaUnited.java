62b23c8a1c6108d69c5566b4f8589eebd1a39701
Evan Kranzler <theelk801@gmail.com>
2022-07-22 08:28:35 -0400

    [DMU] add set

---
+++ Mage.Sets/src/mage/sets/DominariaUnited.java
@@ -0,0 +1,41 @@
+package mage.sets;
+
+import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
+import mage.constants.SetType;
+
+/**
+ * @author TheElk801
+ */
+public final class DominariaUnited extends ExpansionSet {
+
+    private static final DominariaUnited instance = new DominariaUnited();
+
+    public static DominariaUnited getInstance() {
+        return instance;
+    }
+
+    private DominariaUnited() {
+        super("Dominaria United", "SNC", ExpansionSet.buildDate(2022, 11, 9), SetType.EXPANSION);
+        this.blockName = "Dominaria United";
+        this.hasBoosters = false; // temporary
+        this.hasBasicLands = true;
+        this.numBoosterLands = 1;
+        this.numBoosterCommon = 10;
+        this.numBoosterUncommon = 3;
+        this.numBoosterRare = 1;
+        this.ratioBoosterMythic = 7;
+        this.maxCardNumberInBooster = 281;
+
+        cards.add(new SetCardInfo("Forest", 281, Rarity.LAND, mage.cards.basiclands.Forest.class, FULL_ART_BFZ_VARIOUS));
+        cards.add(new SetCardInfo("Island", 278, Rarity.LAND, mage.cards.basiclands.Island.class, FULL_ART_BFZ_VARIOUS));
+        cards.add(new SetCardInfo("Mountain", 280, Rarity.LAND, mage.cards.basiclands.Mountain.class, FULL_ART_BFZ_VARIOUS));
+        cards.add(new SetCardInfo("Plains", 277, Rarity.LAND, mage.cards.basiclands.Plains.class, FULL_ART_BFZ_VARIOUS));
+        cards.add(new SetCardInfo("Swamp", 279, Rarity.LAND, mage.cards.basiclands.Swamp.class, FULL_ART_BFZ_VARIOUS));
+    }
+
+//    @Override
+//    public BoosterCollator createCollator() {
+//        return new DominariaUnitedCollator();
+//    }
+}
