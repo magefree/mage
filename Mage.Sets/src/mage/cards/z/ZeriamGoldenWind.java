406f10ab3fd13c4429f4e4093ec15ca0d663480b
FreaIsDead <edoardo1997123@gmail.com>
2022-08-20 15:58:32 +0200

    DealsDamageToAPlayerAllTriggeredAbility change

--- Mage.Sets/src/mage/cards/z/ZeriamGoldenWind.java
+++ Mage.Sets/src/mage/cards/z/ZeriamGoldenWind.java
@@ -2,14 +2,17 @@ package mage.cards.z;
 
 import java.util.UUID;
 import mage.MageInt;
-import mage.abilities.common.DealsCombatDamageToAPlayerTriggeredAbility;
+import mage.abilities.common.DealsDamageToAPlayerAllTriggeredAbility;
 import mage.abilities.effects.common.CreateTokenEffect;
+import mage.constants.SetTargetPointer;
 import mage.constants.SubType;
 import mage.constants.SuperType;
 import mage.abilities.keyword.FlyingAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.filter.common.FilterControlledPermanent;
+import mage.game.permanent.token.GoblinRogueToken;
 import mage.game.permanent.token.GriffinToken;
 
 /**
@@ -17,6 +20,11 @@ import mage.game.permanent.token.GriffinToken;
  * @author freaisdead
  */
 public final class ZeriamGoldenWind extends CardImpl {
+    private static final FilterControlledPermanent filter = new FilterControlledPermanent("a Griffin you control");
+
+    static {
+        filter.add(SubType.GRIFFIN.getPredicate());
+    }
 
     public ZeriamGoldenWind(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{3}{W}");
@@ -30,7 +38,9 @@ public final class ZeriamGoldenWind extends CardImpl {
         this.addAbility(FlyingAbility.getInstance());
 
         // Whenever a Griffin you control deals combat damage to a player, create a 2/2 white Griffin creature token with flying.
-        this.addAbility(new DealsCombatDamageToAPlayerTriggeredAbility(new CreateTokenEffect(new GriffinToken(), 1), true));
+        this.addAbility(new DealsDamageToAPlayerAllTriggeredAbility(
+                new CreateTokenEffect(new GriffinToken()),
+                filter, false, SetTargetPointer.NONE, true));
     }
 
     private ZeriamGoldenWind(final ZeriamGoldenWind card) {
