d24fcdd61cc982cad5d44aa96ef6da759bb0c83d
FreaIsDead <edoardo1997123@gmail.com>
2022-08-20 15:09:08 +0200

    implemented ZeriamGoldenWind

--- Mage.Sets/src/mage/cards/z/ZeriamGoldenWind.java
+++ Mage.Sets/src/mage/cards/z/ZeriamGoldenWind.java
@@ -2,12 +2,15 @@ package mage.cards.z;
 
 import java.util.UUID;
 import mage.MageInt;
+import mage.abilities.common.DealsCombatDamageToAPlayerTriggeredAbility;
+import mage.abilities.effects.common.CreateTokenEffect;
 import mage.constants.SubType;
 import mage.constants.SuperType;
 import mage.abilities.keyword.FlyingAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.game.permanent.token.GriffinToken;
 
 /**
  *
@@ -27,6 +30,7 @@ public final class ZeriamGoldenWind extends CardImpl {
         this.addAbility(FlyingAbility.getInstance());
 
         // Whenever a Griffin you control deals combat damage to a player, create a 2/2 white Griffin creature token with flying.
+        this.addAbility(new DealsCombatDamageToAPlayerTriggeredAbility(new CreateTokenEffect(new GriffinToken(), 1), true));
     }
 
     private ZeriamGoldenWind(final ZeriamGoldenWind card) {
