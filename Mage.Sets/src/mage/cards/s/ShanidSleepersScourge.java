2501cc10ceaaf4756f6688fa5694617f8e0737e9
FreaIsDead <edoardo1997123@gmail.com>
2022-08-22 16:31:58 +0200

    create DrawAndLoseLife triggered ability

--- Mage.Sets/src/mage/cards/s/ShanidSleepersScourge.java
+++ Mage.Sets/src/mage/cards/s/ShanidSleepersScourge.java
@@ -10,11 +10,11 @@ import mage.abilities.keyword.MenaceAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.*;
-import mage.filter.FilterPermanent;
-import mage.filter.FilterSpell;
 import mage.filter.common.FilterCreaturePermanent;
 import mage.game.Game;
 import mage.game.events.GameEvent;
+import mage.game.permanent.Permanent;
+import mage.game.stack.Spell;
 import mage.players.Player;
 
 import java.util.UUID;
@@ -23,17 +23,10 @@ import java.util.UUID;
  * @author freaisdead
  */
 public final class ShanidSleepersScourge extends CardImpl {
-    private static final FilterCreaturePermanent otherLegendaryCreaturesFilter = new FilterCreaturePermanent("other legendary creatures");
-    private static final FilterSpell legendarySpellFilter = new FilterSpell("a legendary spell");
-    private static final FilterPermanent legendaryLandFilter = new FilterPermanent("a legendary land");
+    private static final FilterCreaturePermanent otherLegendaryCreaturesFilter = new FilterCreaturePermanent();
 
     static {
         otherLegendaryCreaturesFilter.add(SuperType.LEGENDARY.getPredicate());
-
-        legendarySpellFilter.add(SuperType.LEGENDARY.getPredicate());
-
-        legendaryLandFilter.add(CardType.LAND.getPredicate());
-        legendaryLandFilter.add(SuperType.LEGENDARY.getPredicate());
     }
 
     public ShanidSleepersScourge(UUID ownerId, CardSetInfo setInfo) {
@@ -55,7 +48,7 @@ public final class ShanidSleepersScourge extends CardImpl {
                 otherLegendaryCreaturesFilter,
                 true)));
         // Whenever you play a legendary land or cast a legendary spell, you draw a card and you lose 1 life.
-        this.addAbility(new DrawAndLoseLife(1,1));
+        this.addAbility(new DrawAndLoseLife(1, 1));
     }
 
     private ShanidSleepersScourge(final ShanidSleepersScourge card) {
@@ -82,25 +75,29 @@ class DrawAndLoseLife extends TriggeredAbilityImpl {
 
     @Override
     public boolean checkEventType(GameEvent event, Game game) {
-        if(event.getType() == GameEvent.EventType.LAND_PLAYED || event.getType() == GameEvent.EventType.SPELL_CAST){
-            return true;
+        return event.getType() == GameEvent.EventType.LAND_PLAYED || event.getType() == GameEvent.EventType.SPELL_CAST;
+    }
+
+    @Override
+    public boolean checkTrigger(GameEvent event, Game game) {
+        if (event.getPlayerId().equals(this.getControllerId())) {
+            return false;
+        } else if (event.getType() == GameEvent.EventType.LAND_PLAYED) {
+            Permanent land = game.getPermanent(event.getTargetId());
+            return land != null && land.getControllerId().equals(this.getControllerId()) && land.getSuperType().contains(SuperType.LEGENDARY);
+        } else if (event.getType() == GameEvent.EventType.SPELL_CAST) {
+            Spell spell = game.getStack().getSpell(event.getTargetId());
+            return spell != null && spell.getControllerId().equals(this.getControllerId()) && spell.getSuperType().contains(SuperType.LEGENDARY);
         } else {
             return false;
         }
     }
 
     @Override
-    public boolean checkTrigger(GameEvent event, Game game) {
-        return event.getPlayerId().equals(this.getControllerId());
-    }
-
-    @Override
     public boolean checkInterveningIfClause(Game game) {
         Player player = game.getPlayer(this.getControllerId());
         if (player != null) {
-            if (player.getLandsPlayed() != 1) {
-                return true;
-            }
+            return player.getLandsPlayed() != 1;
         }
         return false;
     }
