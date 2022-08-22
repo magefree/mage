aad0090b4f5ad391a939e5d17f15eb39c5e955eb
FreaIsDead <edoardo1997123@gmail.com>
2022-08-22 17:38:27 +0200

    test works using CardTestCommander4Players

--- Mage.Tests/src/test/java/org/mage/test/cards/abilities/oneshot/exile/BanishmentTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/abilities/oneshot/exile/BanishmentTest.java
@@ -2,15 +2,13 @@ package org.mage.test.cards.abilities.oneshot.exile;
 
 import mage.constants.PhaseStep;
 import mage.constants.Zone;
-import mage.game.GameException;
 import org.junit.Test;
-import org.mage.test.serverside.base.CardTestMultiPlayerBase;
+import org.mage.test.serverside.base.CardTestCommander4Players;
 
-public class BanishmentTest extends CardTestMultiPlayerBase {
+public class BanishmentTest extends CardTestCommander4Players {
 
     @Test
-    public void testBanishment() throws GameException {
-        //addCard(Zone.BATTLEFIELD, playerA, "Memnite", 2);
+    public void testBanishment() {
         addCard(Zone.BATTLEFIELD, playerA, "Plains", 4);
         addCard(Zone.HAND, playerA, "Banishment", 1);
         addCard(Zone.BATTLEFIELD, playerB, "Memnite", 1);
@@ -29,6 +27,31 @@ public class BanishmentTest extends CardTestMultiPlayerBase {
         assertPermanentCount(playerB, "Memnite", 0);
         assertPermanentCount(playerB, "Steel Overseer", 4);
         assertPermanentCount(playerC, "Memnite", 0);
+        assertPermanentCount(playerC, "Steel Overseer", 1);
+    }
+
+    @Test
+    public void testDestroyBanishment() {
+        addCard(Zone.BATTLEFIELD, playerA, "Plains", 6);
+        addCard(Zone.HAND, playerA, "Banishment", 1);
+        addCard(Zone.HAND, playerA, "Disenchant", 1);
+        addCard(Zone.BATTLEFIELD, playerB, "Memnite", 1);
+        addCard(Zone.BATTLEFIELD, playerB, "Steel Overseer", 4);
+        addCard(Zone.BATTLEFIELD, playerC, "Memnite", 5);
+        addCard(Zone.BATTLEFIELD, playerC, "Steel Overseer", 1);
+
+        castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Banishment");
+        addTarget(playerA, "Memnite");
+        castSpell(1, PhaseStep.POSTCOMBAT_MAIN, playerA, "Disenchant");
+        addTarget(playerA, "Banishment");
+
+        setStopAt(1, PhaseStep.END_TURN);
+        execute();
+
+        assertPermanentCount(playerA, "Banishment", 0);
+        assertPermanentCount(playerB, "Memnite", 1);
+        assertPermanentCount(playerB, "Steel Overseer", 4);
+        assertPermanentCount(playerC, "Memnite", 5);
         assertPermanentCount(playerC, "Steel Overseer", 1);
     }
 }
