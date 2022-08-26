d04ea3a88bb4d04853fafa993f6ca9a5ee3837df
PurpleCrowbar <26198472+PurpleCrowbar@users.noreply.github.com>
2022-08-26 03:57:25 +0100

    [CC2] Added set

--- Mage.Client/src/main/java/org/mage/plugins/card/dl/sources/ScryfallImageSupportCards.java
+++ Mage.Client/src/main/java/org/mage/plugins/card/dl/sources/ScryfallImageSupportCards.java
@@ -505,6 +505,7 @@ public class ScryfallImageSupportCards {
             add("VOC"); // Crimson Vow Commander
             add("Y22"); // Alchemy: Innistrad
             add("DBL"); // Innistrad: Double Feature
+            add("CC2"); // Commander Collection: Black
             add("NEO"); // Kamigawa: Neon Dynasty
             add("NEC"); // Neon Dynasty Commander
             add("SNC"); // Streets of New Capenna
