d5b0daaf959768ee93238e9d97d24db3633a1235
Evan Kranzler <theelk801@gmail.com>
2022-08-26 20:05:12 -0400

    [DMU] Implemented Geothermal Bog

--- Mage.Sets/src/mage/sets/DominariaUnited.java
+++ Mage.Sets/src/mage/sets/DominariaUnited.java
@@ -81,6 +81,7 @@ public final class DominariaUnited extends ExpansionSet {
         cards.add(new SetCardInfo("Forest", 274, Rarity.LAND, mage.cards.basiclands.Forest.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Frostfist Strider", 51, Rarity.UNCOMMON, mage.cards.f.FrostfistStrider.class));
         cards.add(new SetCardInfo("Gaea's Might", 164, Rarity.COMMON, mage.cards.g.GaeasMight.class));
+        cards.add(new SetCardInfo("Geothermal Bog", 247, Rarity.COMMON, mage.cards.g.GeothermalBog.class));
         cards.add(new SetCardInfo("Gibbering Barricade", 95, Rarity.COMMON, mage.cards.g.GibberingBarricade.class));
         cards.add(new SetCardInfo("Goblin Picker", 128, Rarity.COMMON, mage.cards.g.GoblinPicker.class));
         cards.add(new SetCardInfo("Griffin Protector", 18, Rarity.COMMON, mage.cards.g.GriffinProtector.class));
