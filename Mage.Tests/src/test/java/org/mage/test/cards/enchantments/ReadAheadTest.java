6f0a6b93e54c070429bbd756bd8a159701f58726
Evan Kranzler <theelk801@gmail.com>
2022-08-27 12:41:37 -0400

    small change to test

--- Mage.Tests/src/test/java/org/mage/test/cards/enchantments/ReadAheadTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/enchantments/ReadAheadTest.java
@@ -11,6 +11,7 @@ import org.mage.test.serverside.base.CardTestPlayerBase;
 public class ReadAheadTest extends CardTestPlayerBase {
 
     private static final String war = "The Elder Dragon War";
+    private static final String recall = "Ancestral Recall";
 
     @Test
     public void testElderDragonWarChapter1() {
@@ -33,20 +34,20 @@ public class ReadAheadTest extends CardTestPlayerBase {
     @Test
     public void testElderDragonWarChapter2() {
         addCard(Zone.HAND, playerA, war);
-        addCard(Zone.HAND, playerA, "Island");
+        addCard(Zone.HAND, playerA, recall);
         addCard(Zone.BATTLEFIELD, playerA, "Mountain", 4);
 
         castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, war);
         setChoice(playerA, "X=2");
-        setChoice(playerA, "Island");
+        setChoice(playerA, recall);
 
         setStopAt(1, PhaseStep.END_TURN);
         setStrictChooseMode(true);
         execute();
 
         assertLife(playerB, 20);
-        assertGraveyardCount(playerA, "Island", 1);
-        assertHandCount(playerA, "Island", 0);
+        assertGraveyardCount(playerA, recall, 1);
+        assertHandCount(playerA, recall, 0);
         assertPermanentCount(playerA, war, 1);
         assertGraveyardCount(playerA, war, 0);
     }
