bb8d276282aeb4e204f5f1bdbe174d40854f66d1
Evan Kranzler <theelk801@gmail.com>
2022-08-27 17:11:36 -0400

    [DMU] Implemented Blight Pile

--- Mage.Sets/src/mage/sets/DominariaUnited.java
+++ Mage.Sets/src/mage/sets/DominariaUnited.java
@@ -52,6 +52,7 @@ public final class DominariaUnited extends ExpansionSet {
         cards.add(new SetCardInfo("Benalish Faithbonder", 7, Rarity.COMMON, mage.cards.b.BenalishFaithbonder.class));
         cards.add(new SetCardInfo("Benalish Sleeper", 8, Rarity.COMMON, mage.cards.b.BenalishSleeper.class));
         cards.add(new SetCardInfo("Bite Down", 155, Rarity.COMMON, mage.cards.b.BiteDown.class));
+        cards.add(new SetCardInfo("Blight Pile", 82, Rarity.UNCOMMON, mage.cards.b.BlightPile.class));
         cards.add(new SetCardInfo("Bog Badger", 156, Rarity.COMMON, mage.cards.b.BogBadger.class));
         cards.add(new SetCardInfo("Bone Splinters", 83, Rarity.COMMON, mage.cards.b.BoneSplinters.class));
         cards.add(new SetCardInfo("Bortuk Bonerattle", 197, Rarity.UNCOMMON, mage.cards.b.BortukBonerattle.class));
