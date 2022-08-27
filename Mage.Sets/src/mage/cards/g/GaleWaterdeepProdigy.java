db49105de82faf314dc43029ae1f7d4f64785500
Rjayz <rubenjacobs2006@gmail.com>
2022-08-27 12:05:03 +0200

    [CLB] Implemented Gale, Waterdeep Prodigy

---
+++ Mage.Sets/src/mage/cards/g/GaleWaterdeepProdigy.java
@@ -0,0 +1,98 @@
+package mage.cards.g;
+
+import mage.MageInt;
+import mage.abilities.common.ChooseABackgroundAbility;
+import mage.abilities.common.SpellCastControllerTriggeredAbility;
+import mage.abilities.effects.CastCardFromGraveyardThenExileItEffect;
+import mage.abilities.effects.Effect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.constants.SuperType;
+import mage.filter.FilterCard;
+import mage.filter.FilterSpell;
+import mage.filter.common.FilterInstantOrSorcerySpell;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.stack.Spell;
+import mage.target.common.TargetCardInYourGraveyard;
+import mage.watchers.common.CastFromHandWatcher;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class GaleWaterdeepProdigy extends CardImpl {
+
+    public GaleWaterdeepProdigy(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{2}{U}");
+
+        this.addSuperType(SuperType.LEGENDARY);
+        this.subtype.add(SubType.HUMAN);
+        this.subtype.add(SubType.WIZARD);
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(3);
+        
+        // Whenever you cast an instant or sorcery spell from your hand, you may cast up to one of the other type from your graveyard. 
+        // If a spell cast from your graveyard this way would be put into your graveyard, exile it instead.
+        CastCardFromGraveyardThenExileItEffect effect = new CastCardFromGraveyardThenExileItEffect();
+        effect.setText("you may cast up to one of the other type from your graveyard. If a spell cast from your graveyard this way would be put into your graveyard, exile it instead.");
+        this.addAbility(new GaleWaterdeepProdigyTriggeredAbility(effect,
+                new FilterInstantOrSorcerySpell("an instant or sorcery spell from your hand"),
+                false), new CastFromHandWatcher());
+
+        // Choose a Background
+        this.addAbility(ChooseABackgroundAbility.getInstance());
+    }
+
+    private GaleWaterdeepProdigy(final GaleWaterdeepProdigy card) {
+        super(card);
+    }
+
+    @Override
+    public GaleWaterdeepProdigy copy() {
+        return new GaleWaterdeepProdigy(this);
+    }
+}
+
+class GaleWaterdeepProdigyTriggeredAbility extends SpellCastControllerTriggeredAbility {
+
+    public GaleWaterdeepProdigyTriggeredAbility(Effect effect, FilterSpell filter, boolean optional) {
+        super(effect, filter, optional);
+    }
+
+    public GaleWaterdeepProdigyTriggeredAbility(mage.cards.g.GaleWaterdeepProdigyTriggeredAbility ability) {
+        super(ability);
+    }
+
+    @Override
+    public boolean checkTrigger(GameEvent event, Game game) {
+        if (super.checkTrigger(event, game)) {
+            CastFromHandWatcher watcher = game.getState().getWatcher(CastFromHandWatcher.class);
+            if (watcher != null && watcher.spellWasCastFromHand(event.getSourceId())) {
+                Spell spell = game.getState().getStack().getSpell(event.getSourceId());
+                if (spell != null) {
+                    FilterCard filterCard = new FilterCard();
+                    if(spell.getCardType(game).contains(CardType.SORCERY)) {
+                        filterCard.setMessage("an instant card");
+                        filterCard.add(CardType.INSTANT.getPredicate());
+                    } else {
+                        filterCard.setMessage("a sorcery card");
+                        filterCard.add(CardType.SORCERY.getPredicate());
+                    }
+                    this.getTargets().clear();
+                    this.getTargets().add(new TargetCardInYourGraveyard(filterCard));
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public mage.cards.g.GaleWaterdeepProdigyTriggeredAbility copy() {
+        return new mage.cards.g.GaleWaterdeepProdigyTriggeredAbility(this);
+    }
+}
---
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/clb/GaleWaterdeepProdigyTest.java
@@ -0,0 +1,48 @@
+package org.mage.test.cards.single.clb;
+
+import mage.constants.PhaseStep;
+import mage.constants.Zone;
+import org.junit.Test;
+import org.mage.test.serverside.base.CardTestPlayerBase;
+
+/**
+ *
+ * @author rjayz
+ */
+public class GaleWaterdeepProdigyTest extends CardTestPlayerBase {
+
+    @Test
+    public void TestGaleWaterDeepProdigy() {
+        addCard(Zone.BATTLEFIELD, playerA, "Island", 3);
+        addCard(Zone.BATTLEFIELD, playerA, "Mountain", 3);
+
+        // Whenever you cast an instant or sorcery spell from your hand, you may cast up to one of the other type from your graveyard.
+        // If a spell cast from your graveyard this way would be put into your graveyard, exile it instead.
+        addCard(Zone.BATTLEFIELD, playerA, "Gale, Waterdeep Prodigy", 1);
+
+        // Draw two cards on a sorcery
+        addCard(Zone.HAND, playerA, "Divination");
+        addCard(Zone.GRAVEYARD, playerA, "Lightning Bolt");
+
+        playerA.getLibrary().clear();
+        addCard(Zone.LIBRARY, playerA, "Island");
+        addCard(Zone.LIBRARY, playerA, "Island");
+        skipInitShuffling();
+
+        setStrictChooseMode(true);
+
+        castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Divination");
+        // Target Lightning Bolt in graveyard with Gale's ability
+        addTarget(playerA, "Lightning Bolt");
+
+        execute();
+
+        // Divination will have been cast, Lightning bolt should not have been cast
+        assertHandCount(playerA, 2);
+        assertHandCount(playerA, "Island", 2);
+        assertLibraryCount(playerA, 0);
+        assertGraveyardCount(playerA, 2);
+        assertGraveyardCount(playerA, "Divination", 1);
+        assertGraveyardCount(playerA, "Lightning Bolt", 1);
+    }
+}
