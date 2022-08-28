7c945fd72187cfd6af2cd99086b16eef26153477
Daniel Bomar <dbdaniel42@gmail.com>
2022-08-28 14:16:39 -0500

    [DMU] Implemented The Weatherseed Treaty

--- Mage.Sets/src/mage/sets/DominariaUnited.java
+++ Mage.Sets/src/mage/sets/DominariaUnited.java
@@ -210,6 +210,7 @@ public final class DominariaUnited extends ExpansionSet {
         cards.add(new SetCardInfo("The Cruelty of Gix", 87, Rarity.RARE, mage.cards.t.TheCrueltyOfGix.class));
         cards.add(new SetCardInfo("The Elder Dragon War", 121, Rarity.RARE, mage.cards.t.TheElderDragonWar.class));
         cards.add(new SetCardInfo("The Raven Man", 103, Rarity.RARE, mage.cards.t.TheRavenMan.class));
+        cards.add(new SetCardInfo("The Weatherseed Treaty", 188, Rarity.UNCOMMON, mage.cards.t.TheWeatherseedTreaty.class));
         cards.add(new SetCardInfo("The World Spell", 189, Rarity.MYTHIC, mage.cards.t.TheWorldSpell.class));
         cards.add(new SetCardInfo("Threats Undetected", 185, Rarity.RARE, mage.cards.t.ThreatsUndetected.class));
         cards.add(new SetCardInfo("Thrill of Possibility", 148, Rarity.COMMON, mage.cards.t.ThrillOfPossibility.class));
