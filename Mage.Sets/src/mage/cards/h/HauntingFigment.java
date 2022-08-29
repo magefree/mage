4deb9176f9736b4efe0d6de134de6f0c20b85bc4
Evan Kranzler <theelk801@gmail.com>
2022-08-29 18:50:52 -0400

    [DMU] Implemented Haunting Figment

---
+++ Mage.Sets/src/mage/cards/h/HauntingFigment.java
@@ -0,0 +1,70 @@
+package mage.cards.h;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.condition.Condition;
+import mage.abilities.decorator.ConditionalContinuousEffect;
+import mage.abilities.effects.common.combat.CantBeBlockedSourceEffect;
+import mage.abilities.keyword.VigilanceAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.SubType;
+import mage.game.Game;
+import mage.game.stack.Spell;
+import mage.watchers.common.SpellsCastWatcher;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class HauntingFigment extends CardImpl {
+
+    public HauntingFigment(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{1}{U}");
+
+        this.subtype.add(SubType.ILLUSION);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(1);
+
+        // Vigilance
+        this.addAbility(VigilanceAbility.getInstance());
+
+        // Haunting Figment can't be blocked as long as you've cast an instant or sorcery spell this turn.
+        this.addAbility(new SimpleStaticAbility(new ConditionalContinuousEffect(
+                new CantBeBlockedSourceEffect(Duration.WhileOnBattlefield), HauntingFigmentCondition.instance,
+                "can't be blocked as long as you've cast an instant or sorcery spell this turn"
+        )), new SpellsCastWatcher());
+    }
+
+    private HauntingFigment(final HauntingFigment card) {
+        super(card);
+    }
+
+    @Override
+    public HauntingFigment copy() {
+        return new HauntingFigment(this);
+    }
+}
+
+enum HauntingFigmentCondition implements Condition {
+    instance;
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        SpellsCastWatcher watcher = game.getState().getWatcher(SpellsCastWatcher.class);
+        if (watcher == null) {
+            return false;
+        }
+        List<Spell> spells = watcher.getSpellsCastThisTurn(source.getControllerId());
+        return spells != null && spells
+                .stream()
+                .filter(Objects::nonNull)
+                .anyMatch(spell -> spell.isInstantOrSorcery(game));
+    }
+}
