2a87790d5c03d3f8ea1b88a6ea4281d79b12c7ef
Evan Kranzler <theelk801@gmail.com>
2022-08-31 08:12:36 -0400

    [DMU] Implemented Leyline Binding

---
+++ Mage.Sets/src/mage/cards/l/LeylineBinding.java
@@ -0,0 +1,63 @@
+package mage.cards.l;
+
+import mage.abilities.Ability;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.common.delayed.OnLeaveReturnExiledToBattlefieldAbility;
+import mage.abilities.dynamicvalue.common.DomainValue;
+import mage.abilities.effects.common.CreateDelayedTriggeredAbilityEffect;
+import mage.abilities.effects.common.ExileUntilSourceLeavesEffect;
+import mage.abilities.effects.common.cost.SpellCostReductionSourceEffect;
+import mage.abilities.hint.common.DomainHint;
+import mage.abilities.keyword.FlashAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.AbilityWord;
+import mage.constants.CardType;
+import mage.constants.TargetController;
+import mage.constants.Zone;
+import mage.filter.FilterPermanent;
+import mage.filter.common.FilterNonlandPermanent;
+import mage.target.TargetPermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class LeylineBinding extends CardImpl {
+
+    private static final FilterPermanent filter
+            = new FilterNonlandPermanent("nonland permanent an opponent controls");
+
+    static {
+        filter.add(TargetController.OPPONENT.getControllerPredicate());
+    }
+
+    public LeylineBinding(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{5}{W}");
+
+        // Flash
+        this.addAbility(FlashAbility.getInstance());
+
+        // Domain -- This spell costs {1} less to cast for each basic land type among lands you control.
+        this.addAbility(new SimpleStaticAbility(
+                Zone.ALL, new SpellCostReductionSourceEffect(DomainValue.REGULAR)
+        ).setAbilityWord(AbilityWord.DOMAIN).addHint(DomainHint.instance));
+
+        // When Leyline Binding enters the battlefield, exile target nonland permanent an opponent controls until Leyline Binding leaves the battlefield.
+        Ability ability = new EntersBattlefieldTriggeredAbility(new ExileUntilSourceLeavesEffect(filter.getMessage()));
+        ability.addTarget(new TargetPermanent(filter));
+        ability.addEffect(new CreateDelayedTriggeredAbilityEffect(new OnLeaveReturnExiledToBattlefieldAbility()));
+        this.addAbility(ability);
+    }
+
+    private LeylineBinding(final LeylineBinding card) {
+        super(card);
+    }
+
+    @Override
+    public LeylineBinding copy() {
+        return new LeylineBinding(this);
+    }
+}
--- Mage.Sets/src/mage/sets/DominariaUnited.java
+++ Mage.Sets/src/mage/sets/DominariaUnited.java
@@ -135,6 +135,7 @@ public final class DominariaUnited extends ExpansionSet {
         cards.add(new SetCardInfo("Knight of Dusk's Shadow", 96, Rarity.UNCOMMON, mage.cards.k.KnightOfDusksShadow.class));
         cards.add(new SetCardInfo("Lagomos, Hand of Hatred", 205, Rarity.UNCOMMON, mage.cards.l.LagomosHandOfHatred.class));
         cards.add(new SetCardInfo("Leaf-Crowned Visionary", 167, Rarity.RARE, mage.cards.l.LeafCrownedVisionary.class));
+        cards.add(new SetCardInfo("Leyline Binding", 24, Rarity.RARE, mage.cards.l.LeylineBinding.class));
         cards.add(new SetCardInfo("Lightning Strike", 137, Rarity.COMMON, mage.cards.l.LightningStrike.class));
         cards.add(new SetCardInfo("Liliana of the Veil", 97, Rarity.MYTHIC, mage.cards.l.LilianaOfTheVeil.class));
         cards.add(new SetCardInfo("Linebreaker Baloth", 168, Rarity.UNCOMMON, mage.cards.l.LinebreakerBaloth.class));
