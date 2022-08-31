fcaa07e4c703b03e108d0e38e19a79f2de12ae05
Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>
2022-08-31 08:31:47 -0400

    Fix PrimalPlasma

--- Mage.Sets/src/mage/cards/p/PrimalPlasma.java
+++ Mage.Sets/src/mage/cards/p/PrimalPlasma.java
@@ -7,6 +7,7 @@ import mage.abilities.Ability;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.effects.ReplacementEffectImpl;
 import mage.abilities.effects.common.continuous.GainAbilitySourceEffect;
+import mage.abilities.effects.common.continuous.SetBasePowerToughnessSourceEffect;
 import mage.abilities.keyword.DefenderAbility;
 import mage.abilities.keyword.FlyingAbility;
 import mage.cards.CardImpl;
@@ -118,8 +119,7 @@ public final class PrimalPlasma extends CardImpl {
                         game.addEffect(new GainAbilitySourceEffect(DefenderAbility.getInstance(), Duration.Custom), source);
                         break;
                 }
-                permanent.getPower().setModifiedBaseValue(power);
-                permanent.getToughness().setModifiedBaseValue(toughness);
+                game.addEffect(new SetBasePowerToughnessSourceEffect(power, toughness, Duration.WhileOnBattlefield, true), source);
             }
             return false;
 
