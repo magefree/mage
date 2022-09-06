e310ede83710c853d4cc876c98b332cd2fecceb5
Alex W. Jackson <alex.w.jackson@gmail.com>
2022-09-06 06:50:01 -0400

    Replace many custom effects with ExileUntilSourceLeavesEffect

--- Mage.Sets/src/mage/cards/f/FairgroundsWarden.java
+++ Mage.Sets/src/mage/cards/f/FairgroundsWarden.java
@@ -1,4 +1,3 @@
-
 package mage.cards.f;
 
 import java.util.UUID;
@@ -6,19 +5,13 @@ import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.EntersBattlefieldTriggeredAbility;
 import mage.abilities.common.delayed.OnLeaveReturnExiledToBattlefieldAbility;
-import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.CreateDelayedTriggeredAbilityEffect;
-import mage.abilities.effects.common.ExileTargetEffect;
+import mage.abilities.effects.common.ExileUntilSourceLeavesEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.SubType;
-import mage.constants.Outcome;
-import mage.filter.StaticFilters;
-import mage.game.Game;
-import mage.game.permanent.Permanent;
-import mage.target.common.TargetCreaturePermanent;
-import mage.util.CardUtil;
+import mage.target.common.TargetOpponentsCreaturePermanent;
 
 /**
  *
@@ -34,8 +27,8 @@ public final class FairgroundsWarden extends CardImpl {
         this.toughness = new MageInt(3);
 
         // When Fairgrounds Warden enters the battlefield, exile target creature an opponent controls until Fairgrounds Warden leaves the battlefield.
-        Ability ability = new EntersBattlefieldTriggeredAbility(new FairsgroundsWardenExileEffect());
-        ability.addTarget(new TargetCreaturePermanent(StaticFilters.FILTER_OPPONENTS_PERMANENT_CREATURE));
+        Ability ability = new EntersBattlefieldTriggeredAbility(new ExileUntilSourceLeavesEffect());
+        ability.addTarget(new TargetOpponentsCreaturePermanent());
         ability.addEffect(new CreateDelayedTriggeredAbilityEffect(new OnLeaveReturnExiledToBattlefieldAbility()));
         this.addAbility(ability);
     }
@@ -47,33 +40,5 @@ public final class FairgroundsWarden extends CardImpl {
     @Override
     public FairgroundsWarden copy() {
         return new FairgroundsWarden(this);
-    }
-}
-
-class FairsgroundsWardenExileEffect extends OneShotEffect {
-
-    public FairsgroundsWardenExileEffect() {
-        super(Outcome.Benefit);
-        this.staticText = "exile target creature an opponent controls until {this} leaves the battlefield.";
-    }
-
-    public FairsgroundsWardenExileEffect(final FairsgroundsWardenExileEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public FairsgroundsWardenExileEffect copy() {
-        return new FairsgroundsWardenExileEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Permanent permanent = (Permanent) source.getSourceObjectIfItStillExists(game);
-        // If Fairsgrounds Warden leaves the battlefield before its triggered ability resolves,
-        // the target creature won't be exiled.
-        if (permanent != null) {
-            return new ExileTargetEffect(CardUtil.getExileZoneId(game, source.getSourceId(), source.getSourceObjectZoneChangeCounter()), permanent.getIdName()).apply(game, source);
-        }
-        return false;
     }
 }
--- Mage.Sets/src/mage/cards/s/SuspensionField.java
+++ Mage.Sets/src/mage/cards/s/SuspensionField.java
@@ -1,28 +1,22 @@
-
 package mage.cards.s;
 
 import java.util.UUID;
 import mage.abilities.Ability;
 import mage.abilities.common.EntersBattlefieldTriggeredAbility;
 import mage.abilities.common.delayed.OnLeaveReturnExiledToBattlefieldAbility;
-import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.CreateDelayedTriggeredAbilityEffect;
-import mage.abilities.effects.common.ExileTargetEffect;
+import mage.abilities.effects.common.ExileUntilSourceLeavesEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.ComparisonType;
-import mage.constants.Outcome;
 import mage.filter.common.FilterCreaturePermanent;
 import mage.filter.predicate.mageobject.ToughnessPredicate;
-import mage.game.Game;
-import mage.game.permanent.Permanent;
-import mage.target.common.TargetCreaturePermanent;
-import mage.util.CardUtil;
+import mage.target.TargetPermanent;
 
 /**
  *
- * @author emerald000
+ * @author awjackson
  */
 public final class SuspensionField extends CardImpl {
 
@@ -36,11 +30,10 @@ public final class SuspensionField extends CardImpl {
         super(ownerId,setInfo,new CardType[]{CardType.ENCHANTMENT},"{1}{W}");
 
         // When Suspension Field enters the battlefield, you may exile target creature with toughness 3 or greater until Suspension Field leaves the battlefield.
-        Ability ability = new EntersBattlefieldTriggeredAbility(new SuspensionFieldExileEffect(), true);
-        ability.addTarget(new TargetCreaturePermanent(filter));
+        Ability ability = new EntersBattlefieldTriggeredAbility(new ExileUntilSourceLeavesEffect(), true);
+        ability.addTarget(new TargetPermanent(filter));
         ability.addEffect(new CreateDelayedTriggeredAbilityEffect(new OnLeaveReturnExiledToBattlefieldAbility()));
         this.addAbility(ability);
-
     }
 
     private SuspensionField(final SuspensionField card) {
@@ -50,32 +43,5 @@ public final class SuspensionField extends CardImpl {
     @Override
     public SuspensionField copy() {
         return new SuspensionField(this);
-    }
-}
-
-class SuspensionFieldExileEffect extends OneShotEffect {
-
-    SuspensionFieldExileEffect() {
-        super(Outcome.Exile);
-        this.staticText = "you may exile target creature with toughness 3 or greater until {this} leaves the battlefield. <i>(That creature returns under its owner's control.)</i>";
-    }
-
-    SuspensionFieldExileEffect(final SuspensionFieldExileEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public SuspensionFieldExileEffect copy() {
-        return new SuspensionFieldExileEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Permanent sourcePermanent = game.getPermanent(source.getSourceId());
-        // If Suspension Field leaves the battlefield before its triggered ability resolves, the target won't be exiled.
-        if (sourcePermanent != null) {
-            return new ExileTargetEffect(CardUtil.getCardExileZoneId(game, source), sourcePermanent.getIdName()).apply(game, source);
-        }
-        return false;
     }
 }
