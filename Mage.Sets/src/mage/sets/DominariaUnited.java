a24cdb31fc8ef5948530c6c2f2a6c78b06ed48ee
Evan Kranzler <theelk801@gmail.com>
2022-09-05 20:14:13 -0400

    [DMU] Implemented Rona, Sheoldred's Faithful

--- Mage.Sets/src/mage/sets/DominariaUnited.java
+++ Mage.Sets/src/mage/sets/DominariaUnited.java
@@ -189,6 +189,7 @@ public final class DominariaUnited extends ExpansionSet {
         cards.add(new SetCardInfo("Resolute Reinforcements", 29, Rarity.UNCOMMON, mage.cards.r.ResoluteReinforcements.class));
         cards.add(new SetCardInfo("Rith, Liberated Primeval", 214, Rarity.MYTHIC, mage.cards.r.RithLiberatedPrimeval.class));
         cards.add(new SetCardInfo("Rona's Vortex", 63, Rarity.UNCOMMON, mage.cards.r.RonasVortex.class));
+        cards.add(new SetCardInfo("Rona, Sheoldred's Faithful", 216, Rarity.UNCOMMON, mage.cards.r.RonaSheoldredsFaithful.class));
         cards.add(new SetCardInfo("Rulik Mons, Warren Chief", 217, Rarity.UNCOMMON, mage.cards.r.RulikMonsWarrenChief.class));
         cards.add(new SetCardInfo("Rundvelt Hordemaster", 142, Rarity.RARE, mage.cards.r.RundveltHordemaster.class));
         cards.add(new SetCardInfo("Runic Shot", 30, Rarity.UNCOMMON, mage.cards.r.RunicShot.class));
