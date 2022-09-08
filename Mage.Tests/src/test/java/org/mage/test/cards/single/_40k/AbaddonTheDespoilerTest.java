ba7e9f86008199acf54f1f60c4fa17d1fd857e5c
Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>
2022-09-08 09:16:50 -0400

    [40K] Added currently failing and ignored test for #9490.

---
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/_40k/AbaddonTheDespoilerTest.java
@@ -0,0 +1,40 @@
+package org.mage.test.cards.single._40k;
+
+import mage.constants.PhaseStep;
+import mage.constants.Zone;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.mage.test.serverside.base.CardTestPlayerBase;
+
+/**
+ * During your turn, spells you cast from your hand with mana value X or less have cascade,
+ * where X is the total amount of life your opponents have lost this turn.
+ * @author Alex-Vasile
+ */
+public class AbaddonTheDespoilerTest extends CardTestPlayerBase {
+    private static final String abaddonTheDespoiler = "Abaddon the Despoiler";
+
+    /**
+     * Reported bug: https://github.com/magefree/mage/issues/9490
+     */
+    @Ignore
+    @Test
+    public void cascadeWorks() {
+        String lightningBolt = "Lightning Bolt";
+        addCard(Zone.HAND, playerA, lightningBolt, 2);
+        addCard(Zone.BATTLEFIELD, playerA, "Mountain", 2);
+        addCard(Zone.LIBRARY, playerA, "Crimson Kobolds"); // {0}
+
+        setStrictChooseMode(true);
+
+        castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, lightningBolt, playerB);
+        waitStackResolved(1, PhaseStep.PRECOMBAT_MAIN);
+
+        castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, lightningBolt, playerB);
+
+        setStopAt(1, PhaseStep.PRECOMBAT_MAIN);
+        execute();
+        assertLife(playerB, 20 - 2*3);
+        assertPermanentCount(playerA, "Crimson Kobolds", 1);
+    }
+}
