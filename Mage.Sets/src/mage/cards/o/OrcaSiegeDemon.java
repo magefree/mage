8f46d59847b26b5edb9cd3ccf5ff32c56d703a80
PurpleCrowbar <26198472+PurpleCrowbar@users.noreply.github.com>
2022-09-09 22:41:45 +0100

    Simplified Orca, Siege Demon

--- Mage.Sets/src/mage/cards/o/OrcaSiegeDemon.java
+++ Mage.Sets/src/mage/cards/o/OrcaSiegeDemon.java
@@ -4,22 +4,17 @@ import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.DiesCreatureTriggeredAbility;
 import mage.abilities.common.DiesSourceTriggeredAbility;
-import mage.abilities.effects.OneShotEffect;
+import mage.abilities.dynamicvalue.common.SourcePermanentPowerCount;
+import mage.abilities.effects.common.DamageMultiEffect;
 import mage.abilities.effects.common.counter.AddCountersSourceEffect;
 import mage.abilities.keyword.TrampleAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.Outcome;
 import mage.constants.SubType;
 import mage.constants.SuperType;
 import mage.counters.CounterType;
-import mage.game.Game;
-import mage.game.permanent.Permanent;
-import mage.players.Player;
-import mage.target.Target;
 import mage.target.common.TargetAnyTargetAmount;
-import mage.target.targetadjustment.TargetAdjuster;
 
 import java.util.UUID;
 
@@ -43,8 +38,9 @@ public final class OrcaSiegeDemon extends CardImpl {
         this.addAbility(new DiesCreatureTriggeredAbility(new AddCountersSourceEffect(CounterType.P1P1.createInstance()), false, true));
 
         // When Orca dies, it deals damage equal to its power divided as you choose among any number of targets.
-        Ability ability = new DiesSourceTriggeredAbility(new OrcaSiegeDemonEffect());
-        ability.setTargetAdjuster(OrcaSiegeDemonAdjuster.instance);
+        Ability ability = new DiesSourceTriggeredAbility(new DamageMultiEffect(new SourcePermanentPowerCount())
+                .setText("it deals damage equal to its power divided as you choose among any number of targets."));
+        ability.addTarget(new TargetAnyTargetAmount(new SourcePermanentPowerCount()));
         this.addAbility(ability);
     }
 
@@ -55,56 +51,5 @@ public final class OrcaSiegeDemon extends CardImpl {
     @Override
     public OrcaSiegeDemon copy() {
         return new OrcaSiegeDemon(this);
-    }
-}
-
-enum OrcaSiegeDemonAdjuster implements TargetAdjuster {
-    instance;
-
-    @Override
-    public void adjustTargets(Ability ability, Game game) {
-        Permanent sourcePermanent = game.getPermanentOrLKIBattlefield(ability.getSourceId());
-        if (sourcePermanent != null) {
-            ability.getTargets().clear();
-            ability.addTarget(new TargetAnyTargetAmount(sourcePermanent.getPower().getValue()));
-        }
-    }
-}
-
-class OrcaSiegeDemonEffect extends OneShotEffect {
-
-    public OrcaSiegeDemonEffect() {
-        super(Outcome.Damage);
-        this.staticText = "it deals damage equal to its power divided " +
-                "as you choose among any number of targets";
-    }
-
-    public OrcaSiegeDemonEffect(final OrcaSiegeDemonEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public OrcaSiegeDemonEffect copy() {
-        return new OrcaSiegeDemonEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        if (!source.getTargets().isEmpty()) {
-            Target multiTarget = source.getTargets().get(0);
-            for (UUID target : multiTarget.getTargets()) {
-                Permanent permanent = game.getPermanent(target);
-                if (permanent != null) {
-                    permanent.damage(multiTarget.getTargetAmount(target), source.getSourceId(), source, game, false, true);
-                } else {
-                    Player player = game.getPlayer(target);
-                    if (player != null) {
-                        player.damage(multiTarget.getTargetAmount(target), source.getSourceId(), source, game, false, true);
-                    }
-                }
-            }
-            return true;
-        }
-        return false;
     }
 }
