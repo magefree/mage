40d2019288aaf0dcfe3b0637b3c3a012b4d38084
Evan Kranzler <theelk801@gmail.com>
2022-09-13 05:38:02 -0400

    [40K] updated spoiler and reprints

--- Mage.Sets/src/mage/sets/Warhammer40000.java
+++ Mage.Sets/src/mage/sets/Warhammer40000.java
@@ -20,8 +20,12 @@ public final class Warhammer40000 extends ExpansionSet {
         this.hasBasicLands = false;
         this.hasBoosters = false;
 
-        cards.add(new SetCardInfo("Abaddon the Despoiler", 171, Rarity.MYTHIC, mage.cards.a.AbaddonTheDespoiler.class));
-        cards.add(new SetCardInfo("Blood for the Blood God!", 108, Rarity.COMMON, mage.cards.b.BloodForTheBloodGod.class));
+        cards.add(new SetCardInfo("Abaddon the Despoiler", 2, Rarity.MYTHIC, mage.cards.a.AbaddonTheDespoiler.class));
+        cards.add(new SetCardInfo("Abundance", 210, Rarity.RARE, mage.cards.a.Abundance.class));
+        cards.add(new SetCardInfo("Arcane Signet", 227, Rarity.COMMON, mage.cards.a.ArcaneSignet.class));
+        cards.add(new SetCardInfo("Command Tower", 270, Rarity.COMMON, mage.cards.c.CommandTower.class));
         cards.add(new SetCardInfo("Fabricate", 181, Rarity.RARE, mage.cards.f.Fabricate.class));
+        cards.add(new SetCardInfo("Hardened Scales", 215, Rarity.RARE, mage.cards.h.HardenedScales.class));
+        cards.add(new SetCardInfo("Sol Ring", 249, Rarity.UNCOMMON, mage.cards.s.SolRing.class));
     }
 }
