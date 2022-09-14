835d90f0da49d2ff76a2525dfed0911b15b09a9f
Evan Kranzler <theelk801@gmail.com>
2022-09-14 10:02:06 -0400

    another verify fix

--- Mage.Sets/src/mage/sets/Warhammer40000.java
+++ Mage.Sets/src/mage/sets/Warhammer40000.java
@@ -17,7 +17,7 @@ public final class Warhammer40000 extends ExpansionSet {
 
     private Warhammer40000() {
         super("Warhammer 40,000", "40K", ExpansionSet.buildDate(2022, 4, 29), SetType.SUPPLEMENTAL);
-        this.hasBasicLands = false;
+        this.hasBasicLands = true;
         this.hasBoosters = false;
 
         cards.add(new SetCardInfo("Abaddon the Despoiler", 2, Rarity.MYTHIC, mage.cards.a.AbaddonTheDespoiler.class));
