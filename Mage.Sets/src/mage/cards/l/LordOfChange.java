eef335b450960dfb94accd6ac920e553422daf3d
Evan Kranzler <theelk801@gmail.com>
2022-09-15 06:47:59 -0400

    [40K] Implemented Lord of Change

---
+++ Mage.Sets/src/mage/cards/l/LordOfChange.java
@@ -0,0 +1,46 @@
+package mage.cards.l;
+
+import mage.MageInt;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.DrawCardSourceControllerEffect;
+import mage.abilities.keyword.FlyingAbility;
+import mage.abilities.keyword.WardAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class LordOfChange extends CardImpl {
+
+    public LordOfChange(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{6}{U}");
+
+        this.subtype.add(SubType.DEMON);
+        this.power = new MageInt(6);
+        this.toughness = new MageInt(6);
+
+        // Flying
+        this.addAbility(FlyingAbility.getInstance());
+
+        // Ward {3}
+        this.addAbility(new WardAbility(new ManaCostsImpl<>("{3}")));
+
+        // Architect of Deception -- When Lord of Change enters the battlefield, draw three cards.
+        this.addAbility(new EntersBattlefieldTriggeredAbility(new DrawCardSourceControllerEffect(3)).withFlavorWord("Architect of Deception"));
+    }
+
+    private LordOfChange(final LordOfChange card) {
+        super(card);
+    }
+
+    @Override
+    public LordOfChange copy() {
+        return new LordOfChange(this);
+    }
+}
--- Mage.Sets/src/mage/sets/Warhammer40000.java
+++ Mage.Sets/src/mage/sets/Warhammer40000.java
@@ -72,6 +72,7 @@ public final class Warhammer40000 extends ExpansionSet {
         cards.add(new SetCardInfo("Island", 308, Rarity.LAND, mage.cards.basiclands.Island.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Kill! Maim! Burn!", 128, Rarity.RARE, mage.cards.k.KillMaimBurn.class));
         cards.add(new SetCardInfo("Let the Galaxy Burn", 81, Rarity.RARE, mage.cards.l.LetTheGalaxyBurn.class));
+        cards.add(new SetCardInfo("Lord of Change", 24, Rarity.RARE, mage.cards.l.LordOfChange.class));
         cards.add(new SetCardInfo("Molten Slagheap", 284, Rarity.UNCOMMON, mage.cards.m.MoltenSlagheap.class));
         cards.add(new SetCardInfo("Mountain", 315, Rarity.LAND, mage.cards.basiclands.Mountain.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("New Horizons", 218, Rarity.COMMON, mage.cards.n.NewHorizons.class));
