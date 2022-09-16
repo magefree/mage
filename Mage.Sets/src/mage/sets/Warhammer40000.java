191ea321f6b1c1f44b416db572c0bcbbf49404f1
Evan Kranzler <theelk801@gmail.com>
2022-09-15 21:50:05 -0400

    [40K] Implemented Termagant Swarm

--- Mage.Sets/src/mage/sets/Warhammer40000.java
+++ Mage.Sets/src/mage/sets/Warhammer40000.java
@@ -110,6 +110,7 @@ public final class Warhammer40000 extends ExpansionSet {
         cards.add(new SetCardInfo("Temple of Epiphany", 298, Rarity.RARE, mage.cards.t.TempleOfEpiphany.class));
         cards.add(new SetCardInfo("Temple of Mystery", 299, Rarity.RARE, mage.cards.t.TempleOfMystery.class));
         cards.add(new SetCardInfo("Temple of the False God", 300, Rarity.UNCOMMON, mage.cards.t.TempleOfTheFalseGod.class));
+        cards.add(new SetCardInfo("Termagant Swarm", 99, Rarity.RARE, mage.cards.t.TermagantSwarm.class));
         cards.add(new SetCardInfo("Terramorphic Expanse", 301, Rarity.COMMON, mage.cards.t.TerramorphicExpanse.class));
         cards.add(new SetCardInfo("Tervigon", 100, Rarity.RARE, mage.cards.t.Tervigon.class));
         cards.add(new SetCardInfo("The Swarmlord", 4, Rarity.MYTHIC, mage.cards.t.TheSwarmlord.class));
