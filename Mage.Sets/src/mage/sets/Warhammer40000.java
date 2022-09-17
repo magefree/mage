91b36d4106a60fece42aad61708e76b119e9096a
Evan Kranzler <theelk801@gmail.com>
2022-09-16 21:12:46 -0400

    [40K] Implemented Sautekh Immortal

--- Mage.Sets/src/mage/sets/Warhammer40000.java
+++ Mage.Sets/src/mage/sets/Warhammer40000.java
@@ -122,6 +122,7 @@ public final class Warhammer40000 extends ExpansionSet {
         cards.add(new SetCardInfo("Reverberate", 207, Rarity.RARE, mage.cards.r.Reverberate.class));
         cards.add(new SetCardInfo("Royal Warden", 52, Rarity.RARE, mage.cards.r.RoyalWarden.class));
         cards.add(new SetCardInfo("Rugged Highlands", 292, Rarity.COMMON, mage.cards.r.RuggedHighlands.class));
+        cards.add(new SetCardInfo("Sautekh Immortal", 54, Rarity.UNCOMMON, mage.cards.s.SautekhImmortal.class));
         cards.add(new SetCardInfo("Screamer-Killer", 84, Rarity.RARE, mage.cards.s.ScreamerKiller.class));
         cards.add(new SetCardInfo("Skorpekh Destroyer", 57, Rarity.UNCOMMON, mage.cards.s.SkorpekhDestroyer.class));
         cards.add(new SetCardInfo("Skorpekh Lord", 58, Rarity.RARE, mage.cards.s.SkorpekhLord.class));
