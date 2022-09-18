2323c088c02551631c95fddb0a2779bf327a914b
Evan Kranzler <theelk801@gmail.com>
2022-09-18 19:51:53 -0400

    [40K] updated spoiler

--- Mage.Sets/src/mage/cards/i/ImotekhTheStormlord.java
+++ Mage.Sets/src/mage/cards/i/ImotekhTheStormlord.java
@@ -14,7 +14,10 @@ import mage.constants.CardType;
 import mage.constants.SubType;
 import mage.constants.SuperType;
 import mage.constants.TargetController;
+import mage.filter.FilterPermanent;
 import mage.filter.StaticFilters;
+import mage.filter.common.FilterControlledCreaturePermanent;
+import mage.filter.predicate.mageobject.AnotherPredicate;
 import mage.game.permanent.token.NecronWarriorToken;
 import mage.target.TargetPermanent;
 
@@ -24,6 +27,14 @@ import java.util.UUID;
  * @author TheElk801
  */
 public final class ImotekhTheStormlord extends CardImpl {
+
+    private static final FilterPermanent filter
+            = new FilterControlledCreaturePermanent("another target artifact creature you control");
+
+    static {
+        filter.add(AnotherPredicate.instance);
+        filter.add(CardType.ARTIFACT.getPredicate());
+    }
 
     public ImotekhTheStormlord(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT, CardType.CREATURE}, "{2}{B}{B}");
@@ -39,15 +50,15 @@ public final class ImotekhTheStormlord extends CardImpl {
                 StaticFilters.FILTER_CARD_ARTIFACTS
         ).withFlavorWord("Phaeron"));
 
-        // Grand Strategist -- At the beginning of combat on your turn, another target creature you control gets +2/+2 and gains menace until end of turn.
+        // Grand Strategist -- At the beginning of combat on your turn, another target artifact creature you control gets +2/+2 and gains menace until end of turn.
         Ability ability = new BeginningOfCombatTriggeredAbility(
                 new BoostTargetEffect(2, 2)
-                        .setText("another target creature you control gets +2/+2"),
+                        .setText("another target artifact creature you control gets +2/+2"),
                 TargetController.YOU, false
         );
         ability.addEffect(new GainAbilityTargetEffect(new MenaceAbility(false))
                 .setText("and gains menace until end of turn"));
-        ability.addTarget(new TargetPermanent(StaticFilters.FILTER_CONTROLLED_ANOTHER_CREATURE));
+        ability.addTarget(new TargetPermanent(filter));
         this.addAbility(ability.withFlavorWord("Grand Strategist"));
     }
 
