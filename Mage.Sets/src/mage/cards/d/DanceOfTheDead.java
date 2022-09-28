b1b78d6db01b4b7c241776cc761e347177468c88
Alex W. Jackson <alex.w.jackson@gmail.com>
2022-09-27 20:08:50 -0400

    Reimplement Animate Dead and friends; fixes #5250 (#9581)

--- Mage.Sets/src/mage/cards/d/DanceOfTheDead.java
+++ Mage.Sets/src/mage/cards/d/DanceOfTheDead.java
@@ -1,79 +1,60 @@
-
 package mage.cards.d;
 
 import java.util.UUID;
-import mage.MageObjectReference;
 import mage.abilities.Ability;
 import mage.abilities.Mode;
+import mage.abilities.common.AnimateDeadTriggeredAbility;
 import mage.abilities.common.BeginningOfUpkeepTriggeredAbility;
-import mage.abilities.common.EntersBattlefieldTriggeredAbility;
-import mage.abilities.common.LeavesBattlefieldTriggeredAbility;
 import mage.abilities.common.SimpleStaticAbility;
-import mage.abilities.condition.common.SourceOnBattlefieldCondition;
 import mage.abilities.costs.mana.ManaCostsImpl;
-import mage.abilities.decorator.ConditionalInterveningIfTriggeredAbility;
-import mage.abilities.effects.ContinuousEffectImpl;
 import mage.abilities.effects.Effect;
-import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.AttachEffect;
 import mage.abilities.effects.common.DoIfCostPaid;
 import mage.abilities.effects.common.DontUntapInControllersUntapStepEnchantedEffect;
 import mage.abilities.effects.common.UntapEnchantedEffect;
 import mage.abilities.effects.common.continuous.BoostEnchantedEffect;
-import mage.abilities.effects.common.continuous.SourceEffect;
 import mage.abilities.keyword.EnchantAbility;
-import mage.cards.Card;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.constants.*;
+import mage.constants.CardType;
+import mage.constants.Outcome;
+import mage.constants.SubType;
+import mage.constants.TargetController;
 import mage.filter.common.FilterCreatureCard;
-import mage.filter.common.FilterCreaturePermanent;
-import mage.filter.predicate.permanent.PermanentIdPredicate;
 import mage.game.Game;
 import mage.game.permanent.Permanent;
 import mage.players.Player;
-import mage.target.Target;
 import mage.target.common.TargetCardInGraveyard;
-import mage.target.common.TargetCreaturePermanent;
 import mage.util.CardUtil;
 
 /**
- *
- * @author LevelX2
+ * @author LevelX2, awjackson
  */
 public final class DanceOfTheDead extends CardImpl {
+
+    private static final FilterCreatureCard filter = new FilterCreatureCard("creature card in a graveyard");
 
     public DanceOfTheDead(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{1}{B}");
         this.subtype.add(SubType.AURA);
 
         // Enchant creature card in a graveyard
-        TargetCardInGraveyard auraTarget = new TargetCardInGraveyard(new FilterCreatureCard("creature card in a graveyard"));
+        TargetCardInGraveyard auraTarget = new TargetCardInGraveyard(filter);
         this.getSpellAbility().addTarget(auraTarget);
-        this.getSpellAbility().addEffect(new DanceOfTheDeadAttachEffect(Outcome.PutCreatureInPlay));
-        Ability enchantAbility = new EnchantAbility(auraTarget.getTargetName());
-        this.addAbility(enchantAbility);
-        // When Dance of the Dead enters the battlefield, if it's on the battlefield, it loses "enchant creature card in a graveyard" and gains "enchant creature put onto the battlefield with Dance of the Dead." Put enchanted creature card to the battlefield tapped under your control and attach Dance of the Dead to it. When Dance of the Dead leaves the battlefield, that creature's controller sacrifices it.
-        Ability ability = new ConditionalInterveningIfTriggeredAbility(
-                new EntersBattlefieldTriggeredAbility(new DanceOfTheDeadReAttachEffect(), false),
-                SourceOnBattlefieldCondition.instance,
-                "When {this} enters the battlefield, if it's on the battlefield, "
-                + "it loses \"enchant creature card in a graveyard\" and gains "
-                + "\"enchant creature put onto the battlefield with {this}.\" "
-                + "Return enchanted creature card to the battlefield tapped under your control and attach {this} to it.");
-        ability.addEffect(new DanceOfTheDeadChangeAbilityEffect());
-        this.addAbility(ability);
-        this.addAbility(new LeavesBattlefieldTriggeredAbility(new DanceOfTheDeadLeavesBattlefieldTriggeredEffect(), false));
+        this.getSpellAbility().addEffect(new AttachEffect(Outcome.PutCreatureInPlay));
+        this.addAbility(new EnchantAbility(auraTarget.getTargetName()));
+
+        this.addAbility(new AnimateDeadTriggeredAbility(false, true));
 
         // Enchanted creature gets +1/+1 and doesn't untap during its controller's untap step.
-        ability = new SimpleStaticAbility(Zone.BATTLEFIELD, new BoostEnchantedEffect(1, 1, Duration.WhileOnBattlefield));
+        Ability ability = new SimpleStaticAbility(new BoostEnchantedEffect(1, 1));
         Effect effect = new DontUntapInControllersUntapStepEnchantedEffect();
         effect.setText("and doesn't untap during its controller's untap step");
         ability.addEffect(effect);
         this.addAbility(ability);
 
         // At the beginning of the upkeep of enchanted creature's controller, that player may pay {1}{B}. If they do, untap that creature.
-        this.addAbility(new BeginningOfUpkeepTriggeredAbility(Zone.BATTLEFIELD, new DanceOfTheDeadDoIfCostPaidEffect(), TargetController.CONTROLLER_ATTACHED_TO, false));
-
+        this.addAbility(new BeginningOfUpkeepTriggeredAbility(new DanceOfTheDeadDoIfCostPaidEffect(), TargetController.CONTROLLER_ATTACHED_TO, false));
     }
 
     private DanceOfTheDead(final DanceOfTheDead card) {
@@ -83,169 +64,6 @@ public final class DanceOfTheDead extends CardImpl {
     @Override
     public DanceOfTheDead copy() {
         return new DanceOfTheDead(this);
-    }
-}
-
-class DanceOfTheDeadReAttachEffect extends OneShotEffect {
-
-    public DanceOfTheDeadReAttachEffect() {
-        super(Outcome.Benefit);
-        this.staticText = "Return enchanted creature card to the battlefield under your control and attach {this} to it";
-    }
-
-    public DanceOfTheDeadReAttachEffect(final DanceOfTheDeadReAttachEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public DanceOfTheDeadReAttachEffect copy() {
-        return new DanceOfTheDeadReAttachEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Player controller = game.getPlayer(source.getControllerId());
-        Permanent enchantment = game.getPermanent(source.getSourceId());
-
-        if (controller != null && enchantment != null) {
-            Card cardInGraveyard = game.getCard(enchantment.getAttachedTo());
-            if (cardInGraveyard == null) {
-                return true;
-            }
-
-            // put card into play
-            controller.moveCards(cardInGraveyard, Zone.BATTLEFIELD, source, game, true, false, false, null);
-            Permanent enchantedCreature = game.getPermanent(cardInGraveyard.getId());
-
-            FilterCreaturePermanent filter = new FilterCreaturePermanent("enchant creature put onto the battlefield with Dance of the Dead");
-            filter.add(new PermanentIdPredicate(cardInGraveyard.getId()));
-            Target target = new TargetCreaturePermanent(filter);
-            //enchantAbility.setTargetName(target.getTargetName());
-            if (enchantedCreature != null) {
-                target.addTarget(enchantedCreature.getId(), source, game);
-                enchantment.getSpellAbility().getTargets().clear();
-                enchantment.getSpellAbility().getTargets().add(target);
-                enchantedCreature.addAttachment(enchantment.getId(), source, game);
-            }
-            return true;
-        }
-
-        return false;
-    }
-}
-
-class DanceOfTheDeadLeavesBattlefieldTriggeredEffect extends OneShotEffect {
-
-    public DanceOfTheDeadLeavesBattlefieldTriggeredEffect() {
-        super(Outcome.Benefit);
-        this.staticText = "enchanted creature's controller sacrifices it";
-    }
-
-    public DanceOfTheDeadLeavesBattlefieldTriggeredEffect(final DanceOfTheDeadLeavesBattlefieldTriggeredEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public DanceOfTheDeadLeavesBattlefieldTriggeredEffect copy() {
-        return new DanceOfTheDeadLeavesBattlefieldTriggeredEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Player controller = game.getPlayer(source.getControllerId());
-        Permanent sourcePermanent = game.getPermanentOrLKIBattlefield(source.getSourceId());
-        if (controller != null && sourcePermanent != null) {
-            if (sourcePermanent.getAttachedTo() != null) {
-                Permanent attachedTo = game.getPermanent(sourcePermanent.getAttachedTo());
-                if (attachedTo != null && attachedTo.getZoneChangeCounter(game) == sourcePermanent.getAttachedToZoneChangeCounter()) {
-                    attachedTo.sacrifice(source, game);
-                }
-            }
-            return true;
-        }
-        return false;
-    }
-}
-
-class DanceOfTheDeadAttachEffect extends OneShotEffect {
-
-    public DanceOfTheDeadAttachEffect(Outcome outcome) {
-        super(outcome);
-    }
-
-    public DanceOfTheDeadAttachEffect(Outcome outcome, String rule) {
-        super(outcome);
-        staticText = rule;
-    }
-
-    public DanceOfTheDeadAttachEffect(final DanceOfTheDeadAttachEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public DanceOfTheDeadAttachEffect copy() {
-        return new DanceOfTheDeadAttachEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Card card = game.getCard(source.getFirstTarget());
-        if (card != null && game.getState().getZone(source.getFirstTarget()) == Zone.GRAVEYARD) {
-            // Card have no attachedTo attribute yet so write ref only to enchantment now
-            Permanent enchantment = game.getPermanent(source.getSourceId());
-            if (enchantment != null) {
-                enchantment.attachTo(card.getId(), source, game);
-            }
-            return true;
-        }
-        return false;
-    }
-
-}
-
-class DanceOfTheDeadChangeAbilityEffect extends ContinuousEffectImpl implements SourceEffect {
-
-    private static final Ability newAbility = new EnchantAbility("creature put onto the battlefield with Dance of the Dead");
-
-    static {
-        newAbility.setRuleAtTheTop(true);
-    }
-
-    public DanceOfTheDeadChangeAbilityEffect() {
-        super(Duration.WhileOnBattlefield, Layer.AbilityAddingRemovingEffects_6, SubLayer.NA, Outcome.AddAbility);
-        staticText = "it loses \"enchant creature card in a graveyard\" and gains \"enchant creature put onto the battlefield with Dance of the Dead\"";
-    }
-
-    public DanceOfTheDeadChangeAbilityEffect(final DanceOfTheDeadChangeAbilityEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public DanceOfTheDeadChangeAbilityEffect copy() {
-        return new DanceOfTheDeadChangeAbilityEffect(this);
-    }
-
-    @Override
-    public void init(Ability source, Game game) {
-        super.init(source, game);
-        affectedObjectList.add(new MageObjectReference(source.getSourceId(), game));
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Permanent permanent = affectedObjectList.get(0).getPermanent(game);
-        if (permanent != null) {
-            Ability abilityToRemove = null;
-            for (Ability ability : permanent.getAbilities()) {
-                if (ability instanceof EnchantAbility) {
-                    abilityToRemove = ability;
-                }
-            }
-            permanent.removeAbility(abilityToRemove, source.getSourceId(), game);
-            permanent.addAbility(newAbility, source.getSourceId(), game);
-            return true;
-        }
-        return false;
     }
 }
 
--- Mage.Sets/src/mage/cards/n/Necromancy.java
+++ Mage.Sets/src/mage/cards/n/Necromancy.java
@@ -1,39 +1,26 @@
-
 package mage.cards.n;
 
-import mage.MageObjectReference;
 import mage.abilities.Ability;
-import mage.abilities.common.EntersBattlefieldTriggeredAbility;
-import mage.abilities.common.LeavesBattlefieldTriggeredAbility;
+import mage.abilities.common.AnimateDeadTriggeredAbility;
 import mage.abilities.common.SacrificeIfCastAtInstantTimeTriggeredAbility;
 import mage.abilities.common.SimpleStaticAbility;
-import mage.abilities.condition.common.SourceOnBattlefieldCondition;
-import mage.abilities.decorator.ConditionalInterveningIfTriggeredAbility;
-import mage.abilities.effects.ContinuousEffectImpl;
-import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.continuous.CastAsThoughItHadFlashSourceEffect;
-import mage.abilities.effects.common.continuous.SourceEffect;
-import mage.abilities.keyword.EnchantAbility;
-import mage.cards.Card;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.constants.*;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Zone;
 import mage.filter.common.FilterCreatureCard;
-import mage.filter.common.FilterCreaturePermanent;
-import mage.filter.predicate.permanent.PermanentIdPredicate;
-import mage.game.Game;
-import mage.game.permanent.Permanent;
-import mage.players.Player;
-import mage.target.Target;
 import mage.target.common.TargetCardInGraveyard;
-import mage.target.common.TargetCreaturePermanent;
 
 import java.util.UUID;
 
 /**
- * @author LevelX2
+ * @author LevelX2, awjackson
  */
 public final class Necromancy extends CardImpl {
+
+    private static final FilterCreatureCard filter = new FilterCreatureCard("creature card in a graveyard");
 
     public Necromancy(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{2}{B}");
@@ -42,16 +29,9 @@ public final class Necromancy extends CardImpl {
         this.addAbility(new SimpleStaticAbility(Zone.ALL, new CastAsThoughItHadFlashSourceEffect(Duration.EndOfGame)));
         this.addAbility(new SacrificeIfCastAtInstantTimeTriggeredAbility());
 
-        // When Necromancy enters the battlefield, if it's on the battlefield, it becomes an Aura with "enchant creature put onto the battlefield with Necromancy."
-        // Put target creature card from a graveyard onto the battlefield under your control and attach Necromancy to it.
-        // When Necromancy leaves the battlefield, that creature's controller sacrifices it.
-        Ability ability = new ConditionalInterveningIfTriggeredAbility(
-                new EntersBattlefieldTriggeredAbility(new NecromancyReAttachEffect(), false),
-                SourceOnBattlefieldCondition.instance,
-                "When {this} enters the battlefield, if it's on the battlefield,  it becomes an Aura with \"enchant creature put onto the battlefield with {this}.\" Put target creature card from a graveyard onto the battlefield under your control and attach {this} to it.");
-        ability.addTarget(new TargetCardInGraveyard(new FilterCreatureCard("creature card from a graveyard")));
+        Ability ability = new AnimateDeadTriggeredAbility(true);
+        ability.addTarget(new TargetCardInGraveyard(filter));
         this.addAbility(ability);
-        this.addAbility(new LeavesBattlefieldTriggeredAbility(new NecromancyLeavesBattlefieldTriggeredEffect(), false));
     }
 
     private Necromancy(final Necromancy card) {
@@ -62,140 +42,4 @@ public final class Necromancy extends CardImpl {
     public Necromancy copy() {
         return new Necromancy(this);
     }
-}
-
-class NecromancyReAttachEffect extends OneShotEffect {
-
-    public NecromancyReAttachEffect() {
-        super(Outcome.Benefit);
-        this.staticText = "it becomes an Aura with \"enchant creature put onto the battlefield with {this}\"";
-    }
-
-    public NecromancyReAttachEffect(final NecromancyReAttachEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public NecromancyReAttachEffect copy() {
-        return new NecromancyReAttachEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Player controller = game.getPlayer(source.getControllerId());
-        Permanent enchantment = game.getPermanent(source.getSourceId());
-        Card cardInGraveyard = game.getCard(getTargetPointer().getFirst(game, source));
-        if (controller != null && enchantment != null && cardInGraveyard != null) {
-            controller.moveCards(cardInGraveyard, Zone.BATTLEFIELD, source, game);
-            Permanent enchantedCreature = game.getPermanent(cardInGraveyard.getId());
-            if (enchantedCreature != null) {
-                enchantedCreature.addAttachment(enchantment.getId(), source, game);
-                FilterCreaturePermanent filter = new FilterCreaturePermanent("enchant creature put onto the battlefield with " + enchantment.getIdName());
-                filter.add(new PermanentIdPredicate(cardInGraveyard.getId()));
-                Target target = new TargetCreaturePermanent(filter);
-                target.addTarget(enchantedCreature.getId(), source, game);
-                game.addEffect(new NecromancyChangeAbilityEffect(target), source);
-            }
-            return true;
-        }
-
-        return false;
-    }
-}
-
-class NecromancyLeavesBattlefieldTriggeredEffect extends OneShotEffect {
-
-    public NecromancyLeavesBattlefieldTriggeredEffect() {
-        super(Outcome.Benefit);
-        this.staticText = "enchanted creature's controller sacrifices it";
-    }
-
-    public NecromancyLeavesBattlefieldTriggeredEffect(final NecromancyLeavesBattlefieldTriggeredEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public NecromancyLeavesBattlefieldTriggeredEffect copy() {
-        return new NecromancyLeavesBattlefieldTriggeredEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Player controller = game.getPlayer(source.getControllerId());
-        Permanent sourcePermanent = game.getPermanentOrLKIBattlefield(source.getSourceId());
-        if (controller != null && sourcePermanent != null) {
-            if (sourcePermanent.getAttachedTo() != null) {
-                Permanent attachedTo = game.getPermanent(sourcePermanent.getAttachedTo());
-                if (attachedTo != null && attachedTo.getZoneChangeCounter(game) == sourcePermanent.getAttachedToZoneChangeCounter()) {
-                    attachedTo.sacrifice(source, game);
-                }
-            }
-            return true;
-        }
-        return false;
-    }
-}
-
-class NecromancyChangeAbilityEffect extends ContinuousEffectImpl implements SourceEffect {
-
-    private static final Ability newAbility = new EnchantAbility("creature put onto the battlefield with Necromancy");
-
-    static {
-        newAbility.setRuleAtTheTop(true);
-    }
-
-    Target target;
-
-    public NecromancyChangeAbilityEffect(Target target) {
-        super(Duration.Custom, Outcome.AddAbility);
-        staticText = "it becomes an Aura with \"enchant creature put onto the battlefield with {this}\"";
-        this.target = target;
-        dependencyTypes.add(DependencyType.AuraAddingRemoving);
-    }
-
-    public NecromancyChangeAbilityEffect(final NecromancyChangeAbilityEffect effect) {
-        super(effect);
-        this.target = effect.target;
-    }
-
-    @Override
-    public NecromancyChangeAbilityEffect copy() {
-        return new NecromancyChangeAbilityEffect(this);
-    }
-
-    @Override
-    public void init(Ability source, Game game) {
-        super.init(source, game);
-        affectedObjectList.add(new MageObjectReference(source.getSourceId(), game));
-    }
-
-    @Override
-    public boolean apply(Layer layer, SubLayer sublayer, Ability source, Game game) {
-        Permanent permanent = affectedObjectList.get(0).getPermanent(game);
-        if (permanent != null) {
-            switch (layer) {
-                case TypeChangingEffects_4:
-                    permanent.addSubType(game, SubType.AURA);
-                    break;
-                case AbilityAddingRemovingEffects_6:
-                    permanent.addAbility(newAbility, source.getSourceId(), game);
-                    permanent.getSpellAbility().getTargets().clear();
-                    permanent.getSpellAbility().getTargets().add(target);
-            }
-            return true;
-        }
-        this.discard();
-        return false;
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        return false;
-    }
-
-    @Override
-    public boolean hasLayer(Layer layer) {
-        return layer == Layer.AbilityAddingRemovingEffects_6 || layer == Layer.TypeChangingEffects_4;
-    }
-
 }
