faccf12035bdf61968d41f83ba25408ba1d02dc3
Evan Kranzler <theelk801@gmail.com>
2022-09-28 08:27:20 -0400

    [UNF] Implemented Circuits Act

--- Mage.Sets/src/mage/sets/Unfinity.java
+++ Mage.Sets/src/mage/sets/Unfinity.java
@@ -24,6 +24,7 @@ public final class Unfinity extends ExpansionSet {
         cards.add(new SetCardInfo("Blood Crypt", 279, Rarity.RARE, mage.cards.b.BloodCrypt.class));
         cards.add(new SetCardInfo("Boing!", 40, Rarity.COMMON, mage.cards.b.Boing.class));
         cards.add(new SetCardInfo("Breeding Pool", 286, Rarity.RARE, mage.cards.b.BreedingPool.class));
+        cards.add(new SetCardInfo("Circuits Act", 103, Rarity.COMMON, mage.cards.c.CircuitsAct.class));
         cards.add(new SetCardInfo("Clown Car", 186, Rarity.RARE, mage.cards.c.ClownCar.class));
         cards.add(new SetCardInfo("Forest", 239, Rarity.LAND, mage.cards.basiclands.Forest.class, FULL_ART_BFZ_VARIOUS));
         cards.add(new SetCardInfo("Godless Shrine", 282, Rarity.RARE, mage.cards.g.GodlessShrine.class));
