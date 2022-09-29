7ec60c30771aabadf4c75048d6621948200cff77
Grath <1895280+Grath@users.noreply.github.com>
2022-09-29 10:25:38 -0400

    Switch from a new predicate to just negating NoAbilityPredicate once Alex pointed out that I can negate it.

--- Mage.Sets/src/mage/cards/j/JasmineBorealOfTheSeven.java
+++ Mage.Sets/src/mage/cards/j/JasmineBorealOfTheSeven.java
@@ -1,27 +1,19 @@
 package mage.cards.j;
 
 import mage.MageInt;
-import mage.MageObject;
 import mage.Mana;
-import mage.abilities.Abilities;
-import mage.abilities.Ability;
-import mage.abilities.SpellAbility;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.effects.common.combat.CantBeBlockedByCreaturesAllEffect;
-import mage.abilities.keyword.special.JohanVigilanceAbility;
 import mage.abilities.mana.ConditionalColoredManaAbility;
 import mage.abilities.mana.conditional.ConditionalSpellManaBuilder;
-import mage.cards.Card;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.*;
 import mage.filter.FilterSpell;
 import mage.filter.common.FilterCreaturePermanent;
-import mage.filter.predicate.Predicate;
+import mage.filter.predicate.Predicates;
 import mage.filter.predicate.mageobject.NoAbilityPredicate;
-import mage.game.Game;
 
-import java.util.Objects;
 import java.util.UUID;
 
 public final class JasmineBorealOfTheSeven extends CardImpl {
@@ -38,7 +30,7 @@ public final class JasmineBorealOfTheSeven extends CardImpl {
         spell_filter.add(CardType.CREATURE.getPredicate());
         your_creatures_filter.add(NoAbilityPredicate.instance);
         your_creatures_filter.add(TargetController.YOU.getControllerPredicate());
-        with_abilities_filter.add(new AnyAbilityPredicate());
+        with_abilities_filter.add(Predicates.not(NoAbilityPredicate.instance));
     }
 
     public JasmineBorealOfTheSeven(UUID ownerId, CardSetInfo setInfo) {
@@ -69,43 +61,5 @@ public final class JasmineBorealOfTheSeven extends CardImpl {
     @Override
     public JasmineBorealOfTheSeven copy() {
         return new JasmineBorealOfTheSeven(this);
-    }
-}
-
-class AnyAbilityPredicate implements Predicate<MageObject> {
-    @Override
-    public boolean apply(MageObject input, Game game) {
-        boolean isFaceDown = false;
-        Abilities<Ability> abilities;
-        if (input instanceof Card) {
-            abilities = ((Card) input).getAbilities(game);
-            isFaceDown = ((Card) input).isFaceDown(game);
-        } else {
-            abilities = input.getAbilities();
-        }
-        if (isFaceDown) {
-            for (Ability ability : abilities) {
-                if (ability.getWorksFaceDown()) {
-                    // inner face down abilities like turn up and becomes creature
-                    continue;
-                }
-                if (!Objects.equals(ability.getClass(), SpellAbility.class) && !ability.getClass().equals(JohanVigilanceAbility.class)) {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        for (Ability ability : abilities) {
-            if (!Objects.equals(ability.getClass(), SpellAbility.class) && !ability.getClass().equals(JohanVigilanceAbility.class)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public String toString() {
-        return "with abilities";
     }
 }
