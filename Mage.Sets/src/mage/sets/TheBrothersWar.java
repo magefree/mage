6e7e3957ab3d1a9c2510f1a569b39abff489cc68
Evan Kranzler <theelk801@gmail.com>
2022-09-29 22:07:24 -0400

    [BRO] add set and spoiler

---
+++ Mage.Sets/src/mage/sets/TheBrothersWar.java
@@ -0,0 +1,34 @@
+package mage.sets;
+
+import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
+import mage.constants.SetType;
+
+/**
+ * @author TheElk801
+ */
+public final class TheBrothersWar extends ExpansionSet {
+
+    private static final TheBrothersWar instance = new TheBrothersWar();
+
+    public static TheBrothersWar getInstance() {
+        return instance;
+    }
+
+    private TheBrothersWar() {
+        super("The Brothers' War", "BRO", ExpansionSet.buildDate(2022, 11, 9), SetType.EXPANSION);
+        this.blockName = "The Brothers' War";
+        this.hasBoosters = false; // temporary
+
+        cards.add(new SetCardInfo("Forest", 286, Rarity.LAND, mage.cards.basiclands.Forest.class, FULL_ART_BFZ_VARIOUS));
+        cards.add(new SetCardInfo("Island", 280, Rarity.LAND, mage.cards.basiclands.Island.class, FULL_ART_BFZ_VARIOUS));
+        cards.add(new SetCardInfo("Mountain", 284, Rarity.LAND, mage.cards.basiclands.Mountain.class, FULL_ART_BFZ_VARIOUS));
+        cards.add(new SetCardInfo("Plains", 278, Rarity.LAND, mage.cards.basiclands.Plains.class, FULL_ART_BFZ_VARIOUS));
+        cards.add(new SetCardInfo("Swamp", 282, Rarity.LAND, mage.cards.basiclands.Swamp.class, FULL_ART_BFZ_VARIOUS));
+    }
+
+//    @Override
+//    public BoosterCollator createCollator() {
+//        return new TheBrothersWarCollator();
+//    }
+}
