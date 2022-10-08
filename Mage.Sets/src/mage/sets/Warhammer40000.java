e82500fa26c98e455431a308a9d3a219be04f08a
PurpleCrowbar <26198472+PurpleCrowbar@users.noreply.github.com>
2022-10-08 19:36:10 +0100

    [40K] Implement Biophagus

--- Mage.Sets/src/mage/sets/Warhammer40000.java
+++ Mage.Sets/src/mage/sets/Warhammer40000.java
@@ -43,6 +43,7 @@ public final class Warhammer40000 extends ExpansionSet {
         cards.add(new SetCardInfo("Be'lakor, the Dark Master", 6, Rarity.MYTHIC, mage.cards.b.BelakorTheDarkMaster.class));
         cards.add(new SetCardInfo("Beacon of Unrest", 194, Rarity.RARE, mage.cards.b.BeaconOfUnrest.class));
         cards.add(new SetCardInfo("Bile Blight", 195, Rarity.UNCOMMON, mage.cards.b.BileBlight.class));
+        cards.add(new SetCardInfo("Biophagus", 87, Rarity.RARE, mage.cards.b.Biophagus.class));
         cards.add(new SetCardInfo("Biotransference", 30, Rarity.RARE, mage.cards.b.Biotransference.class));
         cards.add(new SetCardInfo("Birth of the Imperium", 107, Rarity.RARE, mage.cards.b.BirthOfTheImperium.class));
         cards.add(new SetCardInfo("Bituminous Blast", 221, Rarity.UNCOMMON, mage.cards.b.BituminousBlast.class));
