6fdaa1f3c5053c8e6ddd2cdc7129d99c46faca1a
Evan Kranzler <theelk801@gmail.com>
2022-10-13 20:30:46 -0400

    [BOT] Implemented Arcee, Sharpshooter / Arcee, Acrobatic Coupe

--- Mage.Sets/src/mage/sets/Transformers.java
+++ Mage.Sets/src/mage/sets/Transformers.java
@@ -19,6 +19,8 @@ public final class Transformers extends ExpansionSet {
         super("Transformers", "BOT", ExpansionSet.buildDate(2022, 11, 18), SetType.SUPPLEMENTAL);
         this.hasBasicLands = false;
 
+        cards.add(new SetCardInfo("Arcee, Acrobatic Coupe", 7, Rarity.MYTHIC, mage.cards.a.ArceeAcrobaticCoupe.class));
+        cards.add(new SetCardInfo("Arcee, Sharpshooter", 7, Rarity.MYTHIC, mage.cards.a.ArceeSharpshooter.class));
         cards.add(new SetCardInfo("Blitzwing, Adaptive Assailant", 4, Rarity.MYTHIC, mage.cards.b.BlitzwingAdaptiveAssailant.class));
         cards.add(new SetCardInfo("Blitzwing, Cruel Tormentor", 4, Rarity.MYTHIC, mage.cards.b.BlitzwingCruelTormentor.class));
         cards.add(new SetCardInfo("Flamewar, Brash Veteran", 10, Rarity.MYTHIC, mage.cards.f.FlamewarBrashVeteran.class));
