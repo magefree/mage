543c19bd2c37ab7b8508284a8beda284ea265ac9
Alex W. Jackson <alex.w.jackson@gmail.com>
2022-10-27 13:42:16 -0400

    [MID] Fix Unblinking Observer mana condition

--- Mage.Sets/src/mage/cards/u/UnblinkingObserver.java
+++ Mage.Sets/src/mage/cards/u/UnblinkingObserver.java
@@ -75,11 +75,14 @@ class UnblinkingObserverManaCondition extends ManaCondition implements Condition
 
     @Override
     public boolean apply(Game game, Ability source) {
+        if (source instanceof DisturbAbility) {
+            return true;
+        }
         if (source instanceof SpellAbility) {
             MageObject object = game.getObject(source);
             return object != null && object.isInstantOrSorcery(game);
         }
-        return source instanceof DisturbAbility;
+        return false;
     }
 
     @Override
