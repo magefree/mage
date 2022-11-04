646f2b6e004205e0afd59ac3a5bab627f143d55f
Evan Kranzler <theelk801@gmail.com>
2022-11-04 09:01:57 -0400

    [BRO] Implement Terror Ballista

---
+++ Mage.Sets/src/mage/cards/t/TerrorBallista.java
@@ -0,0 +1,57 @@
+package mage.cards.t;
+
+import mage.MageInt;
+import mage.abilities.common.AttacksTriggeredAbility;
+import mage.abilities.common.delayed.ReflexiveTriggeredAbility;
+import mage.abilities.costs.common.SacrificeTargetCost;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.DestroyTargetEffect;
+import mage.abilities.effects.common.DoWhenCostPaid;
+import mage.abilities.keyword.MenaceAbility;
+import mage.abilities.keyword.UnearthAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.filter.StaticFilters;
+import mage.target.common.TargetOpponentsCreaturePermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class TerrorBallista extends CardImpl {
+
+    public TerrorBallista(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT, CardType.CREATURE}, "{7}");
+
+        this.subtype.add(SubType.CONSTRUCT);
+        this.power = new MageInt(5);
+        this.toughness = new MageInt(3);
+
+        // Menace
+        this.addAbility(new MenaceAbility());
+
+        // When Terror Ballista attacks, you may sacrifice another creature. When you do, destroy target creature an opponent controls.
+        ReflexiveTriggeredAbility ability = new ReflexiveTriggeredAbility(new DestroyTargetEffect(), false);
+        ability.addTarget(new TargetOpponentsCreaturePermanent());
+        this.addAbility(new AttacksTriggeredAbility(
+                new DoWhenCostPaid(ability, new SacrificeTargetCost(
+                        StaticFilters.FILTER_CONTROLLED_ANOTHER_CREATURE
+                ), "Sacrifice a creature?")
+        ));
+
+        // Unearth {3}{B}{B}
+        this.addAbility(new UnearthAbility(new ManaCostsImpl<>("{3}{B}{B}")));
+    }
+
+    private TerrorBallista(final TerrorBallista card) {
+        super(card);
+    }
+
+    @Override
+    public TerrorBallista copy() {
+        return new TerrorBallista(this);
+    }
+}
--- Mage.Sets/src/mage/sets/TheBrothersWar.java
+++ Mage.Sets/src/mage/sets/TheBrothersWar.java
@@ -158,6 +158,7 @@ public final class TheBrothersWar extends ExpansionSet {
         cards.add(new SetCardInfo("Tawnos, the Toymaker", 222, Rarity.RARE, mage.cards.t.TawnosTheToymaker.class));
         cards.add(new SetCardInfo("Teferi, Temporal Pilgrim", 66, Rarity.MYTHIC, mage.cards.t.TeferiTemporalPilgrim.class));
         cards.add(new SetCardInfo("Terisian Mindbreaker", 83, Rarity.RARE, mage.cards.t.TerisianMindbreaker.class));
+        cards.add(new SetCardInfo("Terror Ballista", 290, Rarity.RARE, mage.cards.t.TerrorBallista.class));
         cards.add(new SetCardInfo("The Mightstone and Weakstone", "238a", Rarity.RARE, mage.cards.t.TheMightstoneAndWeakstone.class));
         cards.add(new SetCardInfo("The Stasis Coffin", 245, Rarity.RARE, mage.cards.t.TheStasisCoffin.class));
         cards.add(new SetCardInfo("Third Path Iconoclast", 223, Rarity.UNCOMMON, mage.cards.t.ThirdPathIconoclast.class));
