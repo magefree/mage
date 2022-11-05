405a95d5fe1fc9cf15c9020b7c9d68ed104ee9eb
Evan Kranzler <theelk801@gmail.com>
2022-11-04 20:53:09 -0400

    [BRO] Implement Desynchronize

--- Mage.Sets/src/mage/sets/TheBrothersWar.java
+++ Mage.Sets/src/mage/sets/TheBrothersWar.java
@@ -64,6 +64,7 @@ public final class TheBrothersWar extends ExpansionSet {
         cards.add(new SetCardInfo("Deadly Riposte", 5, Rarity.COMMON, mage.cards.d.DeadlyRiposte.class));
         cards.add(new SetCardInfo("Defabricate", 45, Rarity.UNCOMMON, mage.cards.d.Defabricate.class));
         cards.add(new SetCardInfo("Depth Charge Colossus", 78, Rarity.COMMON, mage.cards.d.DepthChargeColossus.class));
+        cards.add(new SetCardInfo("Desynchronize", 46, Rarity.COMMON, mage.cards.d.Desynchronize.class));
         cards.add(new SetCardInfo("Diabolic Intent", 89, Rarity.RARE, mage.cards.d.DiabolicIntent.class));
         cards.add(new SetCardInfo("Disciples of Gix", 90, Rarity.UNCOMMON, mage.cards.d.DisciplesOfGix.class));
         cards.add(new SetCardInfo("Disenchant", 6, Rarity.COMMON, mage.cards.d.Disenchant.class));
