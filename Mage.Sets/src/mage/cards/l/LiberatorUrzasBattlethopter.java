6cbae4ae8bfa3f0ac1a41102b6d3f1c5b0b0edec
PurpleCrowbar <26198472+PurpleCrowbar@users.noreply.github.com>
2022-11-05 00:13:13 +0000

    [BRO] Implement Liberator, Urza's Battlethopter

---
+++ Mage.Sets/src/mage/cards/l/LiberatorUrzasBattlethopter.java
@@ -0,0 +1,106 @@
+package mage.cards.l;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.common.SpellCastControllerTriggeredAbility;
+import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.continuous.CastAsThoughItHadFlashAllEffect;
+import mage.abilities.keyword.FlashAbility;
+import mage.abilities.keyword.FlyingAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.*;
+import mage.counters.CounterType;
+import mage.filter.FilterCard;
+import mage.filter.StaticFilters;
+import mage.filter.predicate.Predicates;
+import mage.filter.predicate.mageobject.ColorlessPredicate;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.game.stack.Spell;
+import mage.watchers.common.ManaPaidSourceWatcher;
+
+import java.util.UUID;
+
+/**
+ * @author PurpleCrowbar
+ */
+public final class LiberatorUrzasBattlethopter extends CardImpl {
+
+    private static final FilterCard filter = new FilterCard("colorless spells and artifact spells");
+
+    static {
+        filter.add(Predicates.or(
+                ColorlessPredicate.instance,
+                CardType.ARTIFACT.getPredicate()
+        ));
+    }
+
+    public LiberatorUrzasBattlethopter(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT, CardType.CREATURE}, "{3}");
+        this.addSuperType(SuperType.LEGENDARY);
+        this.subtype.add(SubType.THOPTER);
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(2);
+
+        // Flash
+        this.addAbility(FlashAbility.getInstance());
+
+        // Flying
+        this.addAbility(FlyingAbility.getInstance());
+
+        // You may cast colorless spells and artifact spells as though they had flash.
+        this.addAbility(new SimpleStaticAbility(
+                new CastAsThoughItHadFlashAllEffect(Duration.WhileOnBattlefield, filter)
+        ));
+
+        // Whenever you cast a spell, if the amount of mana spent to cast that spell is greater
+        // than Liberator, Urza's Battlethopter's power, put a +1/+1 counter on Liberator.
+        this.addAbility(new SpellCastControllerTriggeredAbility(
+                new LiberatorUrzasBattlethopterEffect(), StaticFilters.FILTER_SPELL_A, false, true
+        ));
+    }
+
+    private LiberatorUrzasBattlethopter(final LiberatorUrzasBattlethopter card) {
+        super(card);
+    }
+
+    @Override
+    public LiberatorUrzasBattlethopter copy() {
+        return new LiberatorUrzasBattlethopter(this);
+    }
+}
+
+class LiberatorUrzasBattlethopterEffect extends OneShotEffect {
+
+    LiberatorUrzasBattlethopterEffect() {
+        super(Outcome.BoostCreature);
+        staticText = "if the amount of mana spent to cast that spell is greater than {this}'s power, put a +1/+1 counter on {this}";
+    }
+
+    private LiberatorUrzasBattlethopterEffect(final LiberatorUrzasBattlethopterEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public LiberatorUrzasBattlethopterEffect copy() {
+        return new LiberatorUrzasBattlethopterEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Permanent permanent = source.getSourcePermanentIfItStillExists(game);
+        if (permanent == null) {
+            return false;
+        }
+        Spell spell = (Spell) getValue("spellCast");
+        if (spell != null) {
+            int totalPaid = ManaPaidSourceWatcher.getTotalPaid(spell.getId(), game);
+            if (totalPaid > permanent.getPower().getValue()) {
+                permanent.addCounters(CounterType.P1P1.createInstance(), source.getControllerId(), source, game);
+            }
+        }
+        return true;
+    }
+}
