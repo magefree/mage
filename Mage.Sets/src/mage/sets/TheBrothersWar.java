63c1325f374adc64caf9e7c79ee5b19e71868412
PurpleCrowbar <26198472+PurpleCrowbar@users.noreply.github.com>
2022-11-06 13:43:33 +0000

    [BRO] Implement Saheeli, Filigree Master (#9735)
    * [BRO] Implement Saheeli, Filigree Master
    
    * Add some comment text to Saheeli, Filigree Master's emblem

--- Mage.Sets/src/mage/sets/TheBrothersWar.java
+++ Mage.Sets/src/mage/sets/TheBrothersWar.java
@@ -196,6 +196,8 @@ public final class TheBrothersWar extends ExpansionSet {
         cards.add(new SetCardInfo("Retrieval Agent", 60, Rarity.COMMON, mage.cards.r.RetrievalAgent.class));
         cards.add(new SetCardInfo("Roc Hunter", 150, Rarity.COMMON, mage.cards.r.RocHunter.class));
         cards.add(new SetCardInfo("Rust Goliath", 204, Rarity.COMMON, mage.cards.r.RustGoliath.class));
+        cards.add(new SetCardInfo("Saheeli, Filigree Master", 219, Rarity.MYTHIC, mage.cards.s.SaheeliFiligreeMaster.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Saheeli, Filigree Master", 294, Rarity.MYTHIC, mage.cards.s.SaheeliFiligreeMaster.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Sardian Cliffstomper", 151, Rarity.UNCOMMON, mage.cards.s.SardianCliffstomper.class));
         cards.add(new SetCardInfo("Sarinth Greatwurm", 220, Rarity.MYTHIC, mage.cards.s.SarinthGreatwurm.class));
         cards.add(new SetCardInfo("Sarinth Steelseeker", 189, Rarity.UNCOMMON, mage.cards.s.SarinthSteelseeker.class));
---
+++ Mage/src/main/java/mage/game/command/emblems/SaheeliFiligreeMasterEmblem.java
@@ -0,0 +1,32 @@
+package mage.game.command.emblems;
+
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.common.continuous.BoostControlledEffect;
+import mage.abilities.effects.common.cost.SpellsCostReductionControllerEffect;
+import mage.constants.Duration;
+import mage.constants.Zone;
+import mage.filter.StaticFilters;
+import mage.game.command.Emblem;
+
+/**
+ * @author PurpleCrowbar
+ */
+public final class SaheeliFiligreeMasterEmblem extends Emblem {
+
+    // −4: You get an emblem with "Artifact creatures you control get +1/+1" and "Artifact spells you cast cost {1} less to cast."
+    public SaheeliFiligreeMasterEmblem() {
+        this.setName("Emblem Saheeli");
+        this.setExpansionSetCodeForImage("BRO");
+        Ability ability = new SimpleStaticAbility(
+                Zone.COMMAND,
+                new BoostControlledEffect(
+                        1, 1, Duration.EndOfGame,
+                        StaticFilters.FILTER_PERMANENT_ARTIFACT_CREATURE
+                ).setText("Artifact creatures you control get +1/+1")
+        );
+        ability.addEffect(new SpellsCostReductionControllerEffect(StaticFilters.FILTER_CARD_ARTIFACT, 1)
+                .setText("Artifact spells you cast cost {1} less to cast"));
+        this.getAbilities().add(ability);
+    }
+}
