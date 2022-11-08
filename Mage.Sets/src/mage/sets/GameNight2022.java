68540173704488aeb14e8edaa8cf03014371b558
PurpleCrowbar <26198472+PurpleCrowbar@users.noreply.github.com>
2022-11-08 01:13:06 +0000

    [GN3] Implement Vogar, Necropolis Tyrant

--- Mage.Sets/src/mage/sets/GameNight2022.java
+++ Mage.Sets/src/mage/sets/GameNight2022.java
@@ -22,6 +22,7 @@ public class GameNight2022 extends ExpansionSet {
         this.hasBoosters = false;
         this.hasBasicLands = false; //temporary
 
+        cards.add(new SetCardInfo("Vogar, Necropolis Tyrant", 3, Rarity.MYTHIC, mage.cards.v.VogarNecropolisTyrant.class));
         cards.add(new SetCardInfo("Zamriel, Seraph of Steel", 1, Rarity.MYTHIC, mage.cards.z.ZamrielSeraphOfSteel.class));
     }
 }
