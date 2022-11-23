1cb987d854bb31256f378e7d22314bf8b6c4a31b
Evan Kranzler <theelk801@gmail.com>
2022-11-22 20:44:27 -0500

    [J22] add watcher and hint to Rodolfo, Duskbringer

--- Mage.Sets/src/mage/cards/r/RodolfDuskbringer.java
+++ Mage.Sets/src/mage/cards/r/RodolfDuskbringer.java
@@ -5,6 +5,7 @@ import mage.abilities.common.BeginningOfEndStepTriggeredAbility;
 import mage.abilities.common.GainLifeControllerTriggeredAbility;
 import mage.abilities.common.delayed.ReflexiveTriggeredAbility;
 import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.dynamicvalue.common.ControllerGotLifeCount;
 import mage.abilities.effects.common.DoWhenCostPaid;
 import mage.abilities.effects.common.ReturnFromGraveyardToBattlefieldTargetEffect;
 import mage.abilities.effects.common.continuous.GainAbilitySourceEffect;
@@ -70,7 +71,7 @@ public final class RodolfDuskbringer extends CardImpl {
         ability.addTarget(new TargetCardInYourGraveyard(filter));
         this.addAbility(new BeginningOfEndStepTriggeredAbility(new DoWhenCostPaid(
                 ability, new ManaCostsImpl<>("{1}{W/B}"), "Pay {1}{W/B}?"
-        ), TargetController.YOU, false));
+        ), TargetController.YOU, false).addHint(ControllerGotLifeCount.getHint()), new PlayerGainedLifeWatcher());
     }
 
     private RodolfDuskbringer(final RodolfDuskbringer card) {
