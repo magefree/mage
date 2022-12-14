420de9ec213785088ac8ff91ec45d0b027e34749
Daniel Eberhard <daniel.h.e@gmx.de>
2022-12-14 14:16:41 +0100

    Added card "Festival of the Ancestors" and its abilities and effects

--- Mage.Sets/src/mage/cards/f/FestivalOfTheAncestors.java
+++ Mage.Sets/src/mage/cards/f/FestivalOfTheAncestors.java
@@ -1,4 +1,28 @@
 package mage.cards.f;
 
-public class FestivalOfTheAncestors {
+import mage.abilities.effects.common.GainLifeTargetEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.target.TargetPlayer;
+
+import java.util.UUID;
+
+public class FestivalOfTheAncestors extends CardImpl {
+    public FestivalOfTheAncestors(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{3}");
+
+        //Target player gains 5 life.
+        this.getSpellAbility().addEffect(new GainLifeTargetEffect(5));
+        this.getSpellAbility().addTarget(new TargetPlayer());
+    }
+
+    public FestivalOfTheAncestors(FestivalOfTheAncestors card) {
+        super(card);
+    }
+
+    @Override
+    public FestivalOfTheAncestors copy() {
+        return new FestivalOfTheAncestors(this);
+    }
 }
