cbed0a93921e4356cbc6e0f2245cae4d5d29869c
Daniel Eberhard <daniel.h.e@gmx.de>
2022-12-17 00:02:20 +0100

    Added card "Hold Captive" and its abilities and effects

--- Mage.Sets/src/mage/cards/h/HoldCaptive.java
+++ Mage.Sets/src/mage/cards/h/HoldCaptive.java
@@ -1,4 +1,43 @@
 package mage.cards.h;
 
-public class HoldCaptive {
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.common.AttachEffect;
+import mage.abilities.effects.common.combat.CantBlockAttackActivateAttachedEffect;
+import mage.abilities.keyword.EnchantAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.*;
+import mage.target.TargetPermanent;
+import mage.target.common.TargetCreaturePermanent;
+
+import java.util.UUID;
+
+/**
+ * @author Merlingilb
+ */
+public class HoldCaptive extends CardImpl {
+    public HoldCaptive(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{W}{B}");
+        this.addSubType(SubType.AURA);
+
+        //Enchant creature
+        TargetPermanent auraTarget = new TargetCreaturePermanent();
+        this.getSpellAbility().addTarget(auraTarget);
+        this.getSpellAbility().addEffect(new AttachEffect(Outcome.AddAbility));
+        Ability ability = new EnchantAbility(auraTarget);
+        this.addAbility(ability);
+
+        //Enchanted creature can't attack or block, and its activated abilities can't be activited.
+        this.addAbility(new SimpleStaticAbility(new CantBlockAttackActivateAttachedEffect()));
+    }
+
+    public HoldCaptive(HoldCaptive card) {
+        super(card);
+    }
+
+    @Override
+    public HoldCaptive copy() {
+        return new HoldCaptive(this);
+    }
 }
