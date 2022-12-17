b89ba1b229ac5a1f4b1621fc751dfc6328fd8f61
Daniel Eberhard <daniel.h.e@gmx.de>
2022-12-17 20:53:17 +0100

    Added card "Training Droid" and its abilities and effects

--- Mage.Sets/src/mage/cards/t/TrainingDroid.java
+++ Mage.Sets/src/mage/cards/t/TrainingDroid.java
@@ -1,4 +1,34 @@
 package mage.cards.t;
 
-public class TrainingDroid {
+import mage.MageInt;
+import mage.abilities.keyword.RepairAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+
+import java.util.UUID;
+
+/**
+ * @author Merlingilb
+ */
+public class TrainingDroid extends CardImpl {
+    public TrainingDroid(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT, CardType.CREATURE}, "{1}");
+        this.addSubType(SubType.DROID);
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(1);
+
+        //Repair 2
+        this.addAbility(new RepairAbility(2));
+    }
+
+    public TrainingDroid(final TrainingDroid card) {
+        super(card);
+    }
+
+    @Override
+    public TrainingDroid copy() {
+        return new TrainingDroid(this);
+    }
 }
