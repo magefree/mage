26f2c5feaa3cde5c25a86a7cc151dbe5f8a3d9f1
Daniel Eberhard <daniel.h.e@gmx.de>
2022-12-17 03:34:15 +0100

    Added card "Orbak" and its abilities and effects

--- Mage.Sets/src/mage/cards/o/Orbak.java
+++ Mage.Sets/src/mage/cards/o/Orbak.java
@@ -1,4 +1,34 @@
 package mage.cards.o;
 
-public class Orbak {
+import mage.MageInt;
+import mage.abilities.keyword.MonstrosityAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+
+import java.util.UUID;
+
+/**
+ * @author Merlingilb
+ */
+public class Orbak extends CardImpl {
+    public Orbak(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{1}{W}");
+        this.addSubType(SubType.BEAST);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(2);
+
+        //{4}{W}: Monstrosity 3
+        this.addAbility(new MonstrosityAbility("{4}{W}", 3));
+    }
+
+    public Orbak(final Orbak card) {
+        super(card);
+    }
+
+    @Override
+    public Orbak copy() {
+        return new Orbak(this);
+    }
 }
