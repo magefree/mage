9b4f06872f129f6c6d181a7388420b04e8443047
Daniel Eberhard <daniel.h.e@gmx.de>
2022-12-17 02:54:49 +0100

    Added card "Lightspeed Skipping" and its abilities and effects

--- Mage.Sets/src/mage/cards/l/LightspeedSkipping.java
+++ Mage.Sets/src/mage/cards/l/LightspeedSkipping.java
@@ -1,4 +1,42 @@
 package mage.cards.l;
 
-public class LightspeedSkipping {
+import mage.abilities.Mode;
+import mage.abilities.effects.common.RemoveFromCombatTargetEffect;
+import mage.abilities.effects.common.continuous.GainAbilityTargetEffect;
+import mage.abilities.keyword.HexproofAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.target.common.TargetAttackingOrBlockingCreature;
+import mage.target.common.TargetCreaturePermanent;
+
+import java.util.UUID;
+
+/**
+ * @author Merlingilb
+ */
+public class LightspeedSkipping extends CardImpl {
+    public LightspeedSkipping(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{W}");
+
+        //Choose one --
+        //* Target creature gains hexproof until end of turn.
+        this.getSpellAbility().addEffect(new GainAbilityTargetEffect(HexproofAbility.getInstance(), Duration.EndOfTurn));
+        this.getSpellAbility().addTarget(new TargetCreaturePermanent());
+
+        //* Remove target creature from combat.
+        Mode mode = new Mode(new RemoveFromCombatTargetEffect());
+        mode.addTarget(new TargetAttackingOrBlockingCreature());
+        this.getSpellAbility().addMode(mode);
+    }
+
+    public LightspeedSkipping(final LightspeedSkipping card) {
+        super(card);
+    }
+
+    @Override
+    public LightspeedSkipping copy() {
+        return new LightspeedSkipping(this);
+    }
 }
