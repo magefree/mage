755af0d54036c8e71ea15b09e2d0d50f55747597
Daniel Eberhard <daniel.h.e@gmx.de>
2022-12-18 01:16:05 +0100

    Added card "Xyston Star Destroyer" and its abilities and effects

--- Mage.Sets/src/mage/cards/x/XystonStarDestroyer.java
+++ Mage.Sets/src/mage/cards/x/XystonStarDestroyer.java
@@ -1,4 +1,43 @@
 package mage.cards.x;
 
-public class XystonStarDestroyer {
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.effects.common.DestroyTargetEffect;
+import mage.abilities.keyword.SpaceflightAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.target.common.TargetCreaturePermanent;
+
+import java.util.UUID;
+
+/**
+ * @author Merlingilb
+ */
+public class XystonStarDestroyer extends CardImpl {
+    public XystonStarDestroyer(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT, CardType.CREATURE}, "{6}{B}{B}");
+        this.subtype.add(SubType.STARSHIP);
+        this.power = new MageInt(9);
+        this.toughness = new MageInt(9);
+
+        //Spaceflight
+        this.addAbility(SpaceflightAbility.getInstance());
+
+        //When Xyston Star Destroyer enters the battlefield, destroy target creature.
+        Ability ability = new EntersBattlefieldTriggeredAbility(new DestroyTargetEffect());
+        ability.addTarget(new TargetCreaturePermanent());
+        this.addAbility(ability);
+    }
+
+    public XystonStarDestroyer(final XystonStarDestroyer card) {
+        super(card);
+    }
+
+    @Override
+    public XystonStarDestroyer copy() {
+        return new XystonStarDestroyer(this);
+    }
 }
