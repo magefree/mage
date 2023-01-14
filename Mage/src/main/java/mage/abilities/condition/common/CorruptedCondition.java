d244e52ede9088bbebc553a701997beb8a276c9c
theelk801 <theelk801@gmail.com>
2023-01-14 14:28:26 -0500

    [ONE] Implement Skrelv's Hive

---
+++ Mage/src/main/java/mage/abilities/condition/common/CorruptedCondition.java
@@ -0,0 +1,40 @@
+package mage.abilities.condition.common;
+
+import mage.abilities.Ability;
+import mage.abilities.condition.Condition;
+import mage.abilities.hint.ConditionHint;
+import mage.abilities.hint.Hint;
+import mage.counters.CounterType;
+import mage.game.Game;
+import mage.game.stack.Spell;
+import mage.players.Player;
+
+import java.util.Objects;
+
+/**
+ * @author TheElk801
+ */
+
+public enum CorruptedCondition implements Condition {
+    instance;
+    private static final Hint hint = new ConditionHint(instance, "An opponent has three or more poison counters");
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        return game.getOpponents(source.getControllerId())
+                .stream()
+                .map(game::getPlayer)
+                .filter(Objects::nonNull)
+                .map(Player::getCounters)
+                .anyMatch(counters -> counters.getCount(CounterType.POISON) >= 3);
+    }
+
+    @Override
+    public String toString() {
+        return "an opponent has three or more poison counters";
+    }
+
+    public static Hint getHint() {
+        return hint;
+    }
+}
