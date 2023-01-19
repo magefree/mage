f850a93277e24c79f40a56644ee063cf9a62422c
theelk801 <theelk801@gmail.com>
2023-01-18 21:54:59 -0500

    [ONE] Implement Apostle of Invasion

--- Mage.Sets/src/mage/sets/PhyrexiaAllWillBeOne.java
+++ Mage.Sets/src/mage/sets/PhyrexiaAllWillBeOne.java
@@ -20,6 +20,7 @@ public final class PhyrexiaAllWillBeOne extends ExpansionSet {
         this.blockName = "Phyrexia: All Will Be One";
         this.hasBoosters = false; // temporary
 
+        cards.add(new SetCardInfo("Apostle of Invasion", 3, Rarity.UNCOMMON, mage.cards.a.ApostleOfInvasion.class));
         cards.add(new SetCardInfo("Archfiend of the Dross", 82, Rarity.RARE, mage.cards.a.ArchfiendOfTheDross.class));
         cards.add(new SetCardInfo("Argentum Masticore", 222, Rarity.RARE, mage.cards.a.ArgentumMasticore.class));
         cards.add(new SetCardInfo("Black Sun's Twilight", 84, Rarity.RARE, mage.cards.b.BlackSunsTwilight.class));
