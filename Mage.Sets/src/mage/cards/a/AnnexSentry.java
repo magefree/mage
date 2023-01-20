2acf8c35225183f06f00920bb85f789e75b2bf58
AhmadYProjects <yousufa@kean.edu>
2023-01-20 18:57:05 -0500

    Annex Sentry

---
+++ Mage.Sets/src/mage/cards/a/AnnexSentry.java
@@ -0,0 +1,59 @@
+package mage.cards.a;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.common.delayed.OnLeaveReturnExiledToBattlefieldAbility;
+import mage.abilities.effects.common.CreateDelayedTriggeredAbilityEffect;
+import mage.abilities.effects.common.ExileUntilSourceLeavesEffect;
+import mage.abilities.keyword.ToxicAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.ComparisonType;
+import mage.constants.SubType;
+import mage.filter.common.FilterOpponentsArtifactCreaturePermanent;
+import mage.filter.predicate.mageobject.ManaValuePredicate;
+import mage.target.TargetPermanent;
+
+import java.util.UUID;
+
+/**
+ *
+ * @author AhmadYProjects
+ */
+public final class AnnexSentry extends CardImpl {
+
+    private static final FilterOpponentsArtifactCreaturePermanent filter = new FilterOpponentsArtifactCreaturePermanent("creature with cmc 3 or less");
+
+    static {
+        filter.add(new ManaValuePredicate(ComparisonType.FEWER_THAN,4));
+    }
+
+    public AnnexSentry(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT, CardType.CREATURE}, "{2}{W}");
+
+        this.subtype.add(SubType.PHYREXIAN);
+        this.subtype.add(SubType.CLERIC);
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(4);
+
+        // Toxic 1
+        this.addAbility(new ToxicAbility(1));
+
+        // When Annex Sentry enters the battlefield, exile target artifact or creature an opponent controls with mana value 3 or less until Annex Sentry leaves the battlefield.
+        Ability ability = new EntersBattlefieldTriggeredAbility(new ExileUntilSourceLeavesEffect(), true);
+        ability.addTarget(new TargetPermanent(filter));
+        ability.addEffect(new CreateDelayedTriggeredAbilityEffect(new OnLeaveReturnExiledToBattlefieldAbility()));
+        this.addAbility(ability);
+    }
+
+    private AnnexSentry(final AnnexSentry card) {
+        super(card);
+    }
+
+    @Override
+    public AnnexSentry copy() {
+        return new AnnexSentry(this);
+    }
+}
