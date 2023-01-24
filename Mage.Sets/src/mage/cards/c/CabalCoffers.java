67a46a6b81cf668b5dea33e16bb462225fb076ee
PurpleCrowbar <26198472+PurpleCrowbar@users.noreply.github.com>
2023-01-24 02:10:38 +0000

    Add dynamic value hint to Cabal Coffers

--- Mage.Sets/src/mage/cards/c/CabalCoffers.java
+++ Mage.Sets/src/mage/cards/c/CabalCoffers.java
@@ -6,6 +6,8 @@ import mage.abilities.Ability;
 import mage.abilities.costs.common.TapSourceCost;
 import mage.abilities.costs.mana.GenericManaCost;
 import mage.abilities.dynamicvalue.common.PermanentsOnBattlefieldCount;
+import mage.abilities.hint.Hint;
+import mage.abilities.hint.ValueHint;
 import mage.abilities.mana.DynamicManaAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
@@ -25,13 +27,17 @@ public final class CabalCoffers extends CardImpl {
         filter.add(SubType.SWAMP.getPredicate());
     }
 
+    private static final Hint hint = new ValueHint(
+            "Number of Swamps you control", new PermanentsOnBattlefieldCount(filter)
+    );
+
     public CabalCoffers(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.LAND}, "");
 
         // {2}, {T}: Add {B} for each Swamp you control.
         Ability ability = new DynamicManaAbility(Mana.BlackMana(1), new PermanentsOnBattlefieldCount(filter), new GenericManaCost(2));
         ability.addCost(new TapSourceCost());
-        this.addAbility(ability);
+        this.addAbility(ability.addHint(hint));
     }
 
     private CabalCoffers(final CabalCoffers card) {
