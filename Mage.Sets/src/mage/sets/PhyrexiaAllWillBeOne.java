34379e6680b2d6fbbeeacd0eeb1c8df199210d5e
theelk801 <theelk801@gmail.com>
2023-01-29 15:12:17 -0500

    [ONE] Implement Incubation Sac

--- Mage.Sets/src/mage/sets/PhyrexiaAllWillBeOne.java
+++ Mage.Sets/src/mage/sets/PhyrexiaAllWillBeOne.java
@@ -95,6 +95,7 @@ public final class PhyrexiaAllWillBeOne extends ExpansionSet {
         cards.add(new SetCardInfo("Ichor Synthesizer", 55, Rarity.COMMON, mage.cards.i.IchorSynthesizer.class));
         cards.add(new SetCardInfo("Ichorspit Basilisk", 170, Rarity.COMMON, mage.cards.i.IchorspitBasilisk.class));
         cards.add(new SetCardInfo("Incisor Glider", 15, Rarity.COMMON, mage.cards.i.IncisorGlider.class));
+        cards.add(new SetCardInfo("Incubation Sac", 171, Rarity.UNCOMMON, mage.cards.i.IncubationSac.class));
         cards.add(new SetCardInfo("Indoctrination Attendant", 16, Rarity.COMMON, mage.cards.i.IndoctrinationAttendant.class));
         cards.add(new SetCardInfo("Infectious Bite", 172, Rarity.UNCOMMON, mage.cards.i.InfectiousBite.class));
         cards.add(new SetCardInfo("Infectious Inquiry", 97, Rarity.COMMON, mage.cards.i.InfectiousInquiry.class));
