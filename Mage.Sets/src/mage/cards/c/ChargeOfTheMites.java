93853664e8a86f39a52d19ae4e3bdea00df9c2b6
AhmadYProjects <yousufa@kean.edu>
2023-02-02 10:35:12 -0500

    Modes added and card tested

--- Mage.Sets/src/mage/cards/c/ChargeOfTheMites.java
+++ Mage.Sets/src/mage/cards/c/ChargeOfTheMites.java
@@ -1,23 +1,34 @@
 package mage.cards.c;
 
-import java.util.UUID;
+import mage.abilities.Mode;
+import mage.abilities.dynamicvalue.common.PermanentsOnBattlefieldCount;
+import mage.abilities.effects.common.CreateTokenEffect;
+import mage.abilities.effects.common.DamageTargetEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.filter.common.FilterControlledCreaturePermanent;
+import mage.game.permanent.token.PhyrexianMiteToken;
+import mage.target.common.TargetCreatureOrPlaneswalker;
+
+import java.util.UUID;
 
 /**
  *
  * @author AhmadYProjects
  */
 public final class ChargeOfTheMites extends CardImpl {
-
     public ChargeOfTheMites(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{2}{W}");
         
 
         // Choose one--
         // * Charge of the Mites deals damage equal to the number of creatures you control to target creature or planeswalker.
+        this.getSpellAbility().addEffect(new DamageTargetEffect(new PermanentsOnBattlefieldCount(new FilterControlledCreaturePermanent())));
+        this.getSpellAbility().addTarget(new TargetCreatureOrPlaneswalker());
         // * Create two 1/1 colorless Phyrexian Mite artifact creature tokens with toxic 1 and "This creature can't block."
+        Mode mode2 = new Mode(new CreateTokenEffect(new PhyrexianMiteToken(),2));
+        this.getSpellAbility().addMode(mode2);
     }
 
     private ChargeOfTheMites(final ChargeOfTheMites card) {
