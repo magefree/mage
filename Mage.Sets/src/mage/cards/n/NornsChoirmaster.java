5edfd2276604d7c3c3a18fa3004f5d97300e1f27
PurpleCrowbar <26198472+PurpleCrowbar@users.noreply.github.com>
2023-02-14 18:22:19 +0000

    [ONC] Implement Norn's Choirmaster

---
+++ Mage.Sets/src/mage/cards/n/NornsChoirmaster.java
@@ -0,0 +1,52 @@
+package mage.cards.n;
+
+import mage.MageInt;
+import mage.abilities.common.EntersBattlefieldOrAttacksAllTriggeredAbility;
+import mage.abilities.effects.common.counter.ProliferateEffect;
+import mage.abilities.keyword.FirstStrikeAbility;
+import mage.abilities.keyword.FlyingAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.filter.common.FilterControlledCreaturePermanent;
+import mage.filter.predicate.mageobject.CommanderPredicate;
+
+import java.util.UUID;
+
+/**
+ * @author PurpleCrowbar
+ */
+public final class NornsChoirmaster extends CardImpl {
+
+    private static final FilterControlledCreaturePermanent filter = new FilterControlledCreaturePermanent("a commander you control");
+
+    static {
+        filter.add(CommanderPredicate.instance);
+    }
+
+    public NornsChoirmaster(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{3}{W}{W}");
+        this.subtype.add(SubType.PHYREXIAN, SubType.ANGEL);
+        this.power = new MageInt(5);
+        this.toughness = new MageInt(4);
+
+        // Flying
+        this.addAbility(FlyingAbility.getInstance());
+
+        // First strike
+        this.addAbility(FirstStrikeAbility.getInstance());
+
+        // Whenever a commander you control enters the battlefield or attacks, proliferate.
+        this.addAbility(new EntersBattlefieldOrAttacksAllTriggeredAbility(new ProliferateEffect(), filter));
+    }
+
+    private NornsChoirmaster(final NornsChoirmaster card) {
+        super(card);
+    }
+
+    @Override
+    public NornsChoirmaster copy() {
+        return new NornsChoirmaster(this);
+    }
+}
--- Mage.Sets/src/mage/sets/PhyrexiaAllWillBeOneCommander.java
+++ Mage.Sets/src/mage/sets/PhyrexiaAllWillBeOneCommander.java
@@ -118,6 +118,8 @@ public final class PhyrexiaAllWillBeOneCommander extends ExpansionSet {
         cards.add(new SetCardInfo("Necroblossom Snarl", 160, Rarity.RARE, mage.cards.n.NecroblossomSnarl.class));
         cards.add(new SetCardInfo("Night's Whisper", 94, Rarity.COMMON, mage.cards.n.NightsWhisper.class));
         cards.add(new SetCardInfo("Norn's Annex", 83, Rarity.RARE, mage.cards.n.NornsAnnex.class));
+        cards.add(new SetCardInfo("Norn's Choirmaster", 8, Rarity.RARE, mage.cards.n.NornsChoirmaster.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Norn's Choirmaster", 46, Rarity.RARE, mage.cards.n.NornsChoirmaster.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Noxious Revival", 110, Rarity.UNCOMMON, mage.cards.n.NoxiousRevival.class));
         cards.add(new SetCardInfo("Otharri, Suns' Glory", 3, Rarity.MYTHIC, mage.cards.o.OtharriSunsGlory.class));
         cards.add(new SetCardInfo("Painful Truths", 95, Rarity.RARE, mage.cards.p.PainfulTruths.class));
