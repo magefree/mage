84768fe8a5a1c4307b1f1a5aae4ed8efbe8aa74f
Merlingilb <daniel.h.e@outlook.de>
2023-02-20 20:53:56 +0100

    [ONE] Implement Phyrexian Vindicator (#10003)

--- Mage.Sets/src/mage/sets/PhyrexiaAllWillBeOne.java
+++ Mage.Sets/src/mage/sets/PhyrexiaAllWillBeOne.java
@@ -168,6 +168,7 @@ public final class PhyrexiaAllWillBeOne extends ExpansionSet {
         cards.add(new SetCardInfo("Phyrexian Arena", 104, Rarity.RARE, mage.cards.p.PhyrexianArena.class));
         cards.add(new SetCardInfo("Phyrexian Atlas", 237, Rarity.COMMON, mage.cards.p.PhyrexianAtlas.class));
         cards.add(new SetCardInfo("Phyrexian Obliterator", 105, Rarity.MYTHIC, mage.cards.p.PhyrexianObliterator.class));
+        cards.add(new SetCardInfo("Phyrexian Vindicator", 27, Rarity.MYTHIC, mage.cards.p.PhyrexianVindicator.class));
         cards.add(new SetCardInfo("Plague Nurse", 179, Rarity.COMMON, mage.cards.p.PlagueNurse.class));
         cards.add(new SetCardInfo("Plains", 272, Rarity.LAND, mage.cards.basiclands.Plains.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Planar Disruption", 28, Rarity.COMMON, mage.cards.p.PlanarDisruption.class));
