22480a01a6f793c712dcb0df8e107aa1384f6fd4
Daniel Eberhard <daniel.h.e@gmx.de>
2023-02-20 15:29:35 +0100

    added legendary type

--- Mage.Sets/src/mage/cards/y/YargleAndMultani.java
+++ Mage.Sets/src/mage/cards/y/YargleAndMultani.java
@@ -5,12 +5,14 @@ import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.SubType;
+import mage.constants.SuperType;
 
 import java.util.UUID;
 
 public class YargleAndMultani extends CardImpl {
     public YargleAndMultani(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{3}{B}{B}{G}");
+        this.addSuperType(SuperType.LEGENDARY);
         this.addSubType(SubType.FROG);
         this.addSubType(SubType.SPIRIT);
         this.addSubType(SubType.ELEMENTAL);
