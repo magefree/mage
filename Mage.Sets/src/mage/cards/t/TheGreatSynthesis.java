650100137c21b20f26856bd21b5d883001f55790
Daniel Eberhard <daniel.h.e@gmx.de>
2023-02-21 05:22:05 +0100

    rule text improvement

--- Mage.Sets/src/mage/cards/t/TheGreatSynthesis.java
+++ Mage.Sets/src/mage/cards/t/TheGreatSynthesis.java
@@ -35,7 +35,7 @@ public class TheGreatSynthesis extends CardImpl {
         this.color.setBlue(true);
 
         //(As this Saga enters and after your draw step, add a lore counter.)
-        SagaAbility sagaAbility = new SagaAbility(this);
+        SagaAbility sagaAbility = new SagaAbility(this, false);
 
         //I — Draw cards equal to the number of cards in your hand. You have no maximum hand size for as long as you
         //control The Great Synthesis.
