6b0d3ae2a871c03d846b41ed335c3bb36afe2d26
Susucre <34709007+Susucre@users.noreply.github.com>
2023-02-24 17:07:31 +0100

    fix Verify Error + cleaner Prototype GetRule()

--- Mage/src/main/java/mage/abilities/keyword/PrototypeAbility.java
+++ Mage/src/main/java/mage/abilities/keyword/PrototypeAbility.java
@@ -4,6 +4,7 @@ import mage.ObjectColor;
 import mage.abilities.SpellAbility;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.condition.common.PrototypedCondition;
+import mage.abilities.costs.mana.ManaCost;
 import mage.abilities.costs.mana.ManaCostsImpl;
 import mage.abilities.decorator.ConditionalContinuousEffect;
 import mage.abilities.effects.common.continuous.SetBasePowerToughnessSourceEffect;
@@ -12,6 +13,7 @@ import mage.abilities.effects.common.cost.SetCardCostSourceEffect;
 import mage.cards.Card;
 import mage.constants.*;
 import mage.game.Game;
+import mage.util.CardUtil;
 
 import java.util.Arrays;
 import java.util.UUID;
@@ -22,7 +24,8 @@ import java.util.stream.Collectors;
  */
 public class PrototypeAbility extends SpellAbility {
 
-    private String ruleText;
+    private int power;
+    private int toughness;
 
     public PrototypeAbility(Card card, String manaString, ObjectColor color, int power, int toughness) {
         super(new ManaCostsImpl<>(manaString), card.getName(), Zone.HAND, SpellAbilityType.PROTOTYPE);
@@ -56,7 +59,9 @@ public class PrototypeAbility extends SpellAbility {
                 "")
         ));
 
-        this.ruleText =  "Prototype " + this.manaCosts.getText() + " — " + power + "/" + toughness;
+        this.power = power;
+        this.toughness = toughness;
+
         setRuleAtTheTop(true);
     }
 
@@ -71,6 +76,10 @@ public class PrototypeAbility extends SpellAbility {
 
     @Override
     public String getRule() {
-        return ruleText;
+        StringBuilder sb = new StringBuilder("Prototype ");
+        sb.append(this.manaCosts.getText());
+        sb.append("&mdash; ").append(this.power).append("/").append(this.toughness);
+        sb.append(" <i>(You may cast this spell with different mana cost, color, and size. It keeps its abilities and types.)");
+        return sb.toString();
     }
 }
