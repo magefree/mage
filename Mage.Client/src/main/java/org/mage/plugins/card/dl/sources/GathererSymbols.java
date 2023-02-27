4274b75225306290e4e26a5aad60dd09ff0bc7e2
Oleg Agafonov <jaydi85@gmail.com>
2023-02-27 14:48:01 +0400

    * GUI: fixed hybrid mana symbols download and render (#10082);

--- Mage.Client/src/main/java/org/mage/plugins/card/dl/sources/GathererSymbols.java
+++ Mage.Client/src/main/java/org/mage/plugins/card/dl/sources/GathererSymbols.java
@@ -1,11 +1,13 @@
 package org.mage.plugins.card.dl.sources;
 
 import com.google.common.collect.AbstractIterator;
-import java.io.File;
-import static java.lang.String.format;
-import java.util.Iterator;
 import mage.client.constants.Constants;
 import org.mage.plugins.card.dl.DownloadJob;
+
+import java.io.File;
+import java.util.Iterator;
+
+import static java.lang.String.format;
 import static org.mage.plugins.card.dl.DownloadJob.fromURL;
 import static org.mage.plugins.card.dl.DownloadJob.toFile;
 import static org.mage.plugins.card.utils.CardImageUtils.getImagesDir;
@@ -13,10 +15,11 @@ import static org.mage.plugins.card.utils.CardImageUtils.getImagesDir;
 /**
  * The class GathererSymbols.
  *
- * @version V0.0 25.08.2010
  * @author Clemens Koza
+ * @version V0.0 25.08.2010
  */
 public class GathererSymbols implements Iterable<DownloadJob> {
+
     //TODO chaos and planeswalker symbol
     //chaos: https://gatherer.wizards.com/Images/Symbols/chaos.gif
 
@@ -27,11 +30,11 @@ public class GathererSymbols implements Iterable<DownloadJob> {
     private static final String[] sizes = {"small", "medium", "large"};
 
     private static final String[] symbols = {"W", "U", "B", "R", "G",
-        "W/U", "U/B", "B/R", "R/G", "G/W", "W/B", "U/R", "B/G", "R/W", "G/U",
-        "W/U/P", "U/B/P", "B/R/P", "R/G/P", "G/W/P", "W/B/P", "U/R/P", "B/G/P", "R/W/P", "G/U/P",
-        "2/W", "2/U", "2/B", "2/R", "2/G",
-        "WP", "UP", "BP", "RP", "GP",
-        "X", "S", "T", "Q", "C", "E"};
+            "W/U", "U/B", "B/R", "R/G", "G/W", "W/B", "U/R", "B/G", "R/W", "G/U",
+            "W/U/P", "U/B/P", "B/R/P", "R/G/P", "G/W/P", "W/B/P", "U/R/P", "B/G/P", "R/W/P", "G/U/P",
+            "2/W", "2/U", "2/B", "2/R", "2/G",
+            "WP", "UP", "BP", "RP", "GP",
+            "X", "S", "T", "Q", "C", "E"};
     private static final int minNumeric = 0, maxNumeric = 16;
 
     public GathererSymbols() {
@@ -53,10 +56,13 @@ public class GathererSymbols implements Iterable<DownloadJob> {
                 while (true) {
                     String sym;
                     if (symIndex < symbols.length) {
+                        // take next char symbol
                         sym = symbols[symIndex++];
                     } else if (numeric <= maxNumeric) {
+                        // take next numeric symbol
                         sym = String.valueOf(numeric++);
                     } else {
+                        // switch to next size
                         sizeIndex++;
                         if (sizeIndex == sizes.length) {
                             return endOfData();
@@ -70,26 +76,34 @@ public class GathererSymbols implements Iterable<DownloadJob> {
                     String symbol = sym.replaceAll("/", "");
                     File dst = new File(dir, symbol + ".gif");
 
-                    /**
-                     * Handle a bug on Gatherer where a few symbols are missing
-                     * at the large size. Fall back to using the medium symbol
-                     * for those cases.
-                     */
+                    // workaround for miss icons on Gatherer (no cards with it, so no icons)
+                    // TODO: comment and try download without workaround, keep fix for symbols with "Resource not found" error
+                    switch (symbol) {
+                        case "WUP":
+                        case "BRP":
+                        case "BGP":
+                        case "WBP":
+                        case "UBP":
+                        case "URP":
+                            // skip icon download
+                            continue;
+                    }
+
+                    // workaround for miss large size icons on Gatherer (replace it by medium size)
+                    // TODO: comment and try download without workaround, keep fix for symbols with "Resource not found" error
                     int modSizeIndex = sizeIndex;
                     if (sizeIndex == 2) {
-                        switch (sym) {
-                            case "WP":
-                            case "UP":
-                            case "BP":
-                            case "RP":
-                            case "GP":
-                            case "E":
-                            case "C":
+                        switch (symbol) {
+                            case "GUP":
+                            case "GWP":
+                            case "RGP":
+                            case "RWP":
+                                // need replace to medium size
                                 modSizeIndex = 1;
                                 break;
-
                             default:
-                            // Nothing to do, symbol is available in the large size
+                                // normal size
+                                break;
                         }
                     }
 
