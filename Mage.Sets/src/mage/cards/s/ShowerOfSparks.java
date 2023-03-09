4268a47d77154be8347040b9d77ff19e416b97fd
chesse20 <chesse20@gmail.com>
2023-03-09 13:18:11 -0800

    attempted fix

--- Mage.Sets/src/mage/cards/s/ShowerOfSparks.java
+++ Mage.Sets/src/mage/cards/s/ShowerOfSparks.java
@@ -21,14 +21,16 @@ public final class ShowerOfSparks extends CardImpl {
         super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{R}");
 
         // Shower of Sparks deals 1 damage to target creature and 1 damage to target player.
-        this.getSpellAbility().addEffect(new DamageTargetEffect(1));
+        this.getSpellAbility().addEffect(new DamageTargetEffect(1).setText("deals 1 damage to target creature and 1 damage to target player");
         this.getSpellAbility().addTarget(new TargetCreaturePermanent());
 
-        Effect effect = new DamageTargetEffect(1);
-        effect.setTargetPointer(new SecondTargetPointer());
-        effect.setText("and 1 damage to target player");
-        this.getSpellAbility().addEffect(effect);
+        //** doing it the way below this causes the ability to trigger twice making it do 2 damage to player and 2 damage to target creature                                    
+        //Effect effect = new DamageTargetEffect(1);
+        //effect.setTargetPointer(new SecondTargetPointer()); 
+        //effect.setText("and 1 damage to target player");
+        //this.getSpellAbility().addEffect(effect); **//
         this.getSpellAbility().addTarget(new TargetPlayerOrPlaneswalker());
+        
     }
 
     private ShowerOfSparks(final ShowerOfSparks card) {
