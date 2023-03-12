3843dbd045768fe9579c18b1bda01749cdfef8ed
sprangg <a@b.c>
2023-03-12 20:00:21 +0200

    FreeformUnlimitedCommander does not depend on FreeformCommanderFreeForAll anymore. FreeformUnlimitedCommander has sideboarding. In two-player matches of FreeformUnlimitedCommander, the player who goes first doesn't draw a card on their first turn anymore.

--- Mage.Server.Plugins/Mage.Game.FreeformUnlimitedCommander/src/mage/game/FreeformUnlimitedCommanderMatch.java
+++ Mage.Server.Plugins/Mage.Game.FreeformUnlimitedCommander/src/mage/game/FreeformUnlimitedCommanderMatch.java
@@ -1,10 +1,23 @@
 package mage.game;
 
+import mage.game.match.MatchImpl;
 import mage.game.match.MatchOptions;
+import mage.game.mulligan.Mulligan;
 
-public class FreeformUnlimitedCommanderMatch extends FreeformCommanderFreeForAllMatch {
+public class FreeformUnlimitedCommanderMatch extends MatchImpl {
 
     public FreeformUnlimitedCommanderMatch(MatchOptions options) {
         super(options);
     }
+
+    @Override
+    public void startGame() throws GameException {
+        int startLife = 40;
+        Mulligan mulligan = options.getMulliganType().getMulligan(options.getFreeMulligans());
+        FreeformUnlimitedCommander game = new FreeformUnlimitedCommander(options.getAttackOption(), options.getRange(), mulligan, startLife);
+        game.setStartMessage(this.createGameStartMessage());
+        initGame(game);
+        games.add(game);
+    }
+
 }
