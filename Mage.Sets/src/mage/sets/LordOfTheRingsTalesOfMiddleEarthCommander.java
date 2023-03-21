1812497c742278cbb4fc798862f9762c7603c400
Daniel Eberhard <daniel.h.e@gmx.de>
2023-03-21 02:34:00 +0100

    LTC: added set

---
+++ Mage.Sets/src/mage/sets/LordOfTheRingsTalesOfMiddleEarthCommander.java
@@ -0,0 +1,27 @@
+package mage.sets;
+
+import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
+import mage.constants.SetType;
+
+public final class LordOfTheRingsTalesOfMiddleEarthCommander extends ExpansionSet {
+
+    private static final LordOfTheRingsTalesOfMiddleEarthCommander instance = new LordOfTheRingsTalesOfMiddleEarthCommander();
+
+    public static LordOfTheRingsTalesOfMiddleEarthCommander getInstance() {
+        return instance;
+    }
+
+    private LordOfTheRingsTalesOfMiddleEarthCommander() {
+        super("Lord of the Rings: Tales of Middle-Earth Commander", "LTC", ExpansionSet.buildDate(2023, 6, 23), SetType.SUPPLEMENTAL);
+        this.hasBasicLands = false;
+
+        cards.add(new SetCardInfo("Bridge of Khazad-dûm", 350, Rarity.MYTHIC, mage.cards.e.EnsnaringBridge.class));
+        cards.add(new SetCardInfo("Sol Ring", 284, Rarity.UNCOMMON, mage.cards.s.SolRing.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Sol Ring", 408, Rarity.MYTHIC, mage.cards.s.SolRing.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Sol Ring", 409, Rarity.MYTHIC, mage.cards.s.SolRing.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Sol Ring", 410, Rarity.MYTHIC, mage.cards.s.SolRing.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("The Party Tree", 348, Rarity.MYTHIC, mage.cards.t.TheGreatHenge.class));
+        cards.add(new SetCardInfo("Valley of Gorgoroth", 376, Rarity.MYTHIC, mage.cards.w.Wasteland.class));
+    }
+}
