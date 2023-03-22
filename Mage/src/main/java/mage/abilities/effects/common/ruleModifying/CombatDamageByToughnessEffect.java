86a734e93393c26407fef9b4b41791e5fff3eaf0
chesse20 <chesse20@gmail.com>
2023-03-21 22:18:54 -0700

    super constructors work like this?

--- Mage/src/main/java/mage/abilities/effects/common/ruleModifying/CombatDamageByToughnessEffect.java
+++ Mage/src/main/java/mage/abilities/effects/common/ruleModifying/CombatDamageByToughnessEffect.java
@@ -19,19 +19,20 @@ public class CombatDamageByToughnessEffect extends ContinuousEffectImpl {
 
     private final FilterCreaturePermanent filter;
     private final boolean onlyControlled;
+    private final Duration duration;
 
     public CombatDamageByToughnessEffect(FilterCreaturePermanent filter, boolean onlyControlled) {
         super(Duration.WhileOnBattlefield, Layer.RulesEffects, SubLayer.NA, Outcome.Detriment);
         this.filter = filter;
         this.onlyControlled = onlyControlled;
     }
-    public CombatDamageByToughnessEffect(FilterCreaturePermanent filter, boolean onlyControlled, Duration durationOfEffect) {
-        super(Layer.RulesEffects, SubLayer.NA, Outcome.Detriment);
-        this.duration = durationOfEffect;
+     
+    public CombatDamageByToughnessEffect(FilterCreaturePermanent filter, boolean onlyControlled, Duration duration) {
+        super(this.duration, Layer.RulesEffects, SubLayer.NA, Outcome.Detriment);
         this.filter = filter;
         this.onlyControlled = onlyControlled;
     }
-
+    
 
     private CombatDamageByToughnessEffect(final CombatDamageByToughnessEffect effect) {
         super(effect);
