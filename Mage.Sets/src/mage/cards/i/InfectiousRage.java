26e07454e02c5b9ab3bb1fd0a780bdc15f52c3c8
xenohedron <xenohedron@users.noreply.github.com>
2023-04-07 18:08:53 -0400

    Implement [JUD] Infectious Rage

---
+++ Mage.Sets/src/mage/cards/i/InfectiousRage.java
@@ -0,0 +1,104 @@
+package mage.cards.i;
+
+import mage.abilities.Ability;
+import mage.abilities.common.DiesAttachedTriggeredAbility;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.AttachEffect;
+import mage.abilities.effects.common.continuous.BoostEnchantedEffect;
+import mage.abilities.keyword.EnchantAbility;
+import mage.cards.Card;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.*;
+import mage.filter.FilterPermanent;
+import mage.filter.predicate.permanent.PermanentCanBeAttachedToPredicate;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.players.Player;
+import mage.target.TargetPermanent;
+import mage.target.common.TargetCreaturePermanent;
+import mage.util.RandomUtil;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author xenohedron
+ */
+
+public final class InfectiousRage extends CardImpl {
+
+    public InfectiousRage(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{1}{R}");
+        this.subtype.add(SubType.AURA);
+
+        // Enchant creature
+        TargetPermanent auraTarget = new TargetCreaturePermanent();
+        this.getSpellAbility().addTarget(auraTarget);
+        this.getSpellAbility().addEffect(new AttachEffect(Outcome.BoostCreature));
+        Ability ability = new EnchantAbility(auraTarget);
+        this.addAbility(ability);
+
+        // Enchanted creature gets +2/-1.
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new BoostEnchantedEffect(2, -1)));
+        
+        // When enchanted creature dies, choose a creature at random Infectious Rage can enchant.
+        // Return Infectious Rage to the battlefield attached to that creature.
+        this.addAbility(new DiesAttachedTriggeredAbility(new InfectiousRageReattachEffect(), "enchanted creature"));
+        
+    }
+
+    private InfectiousRage(final InfectiousRage card) {
+        super(card);
+    }
+
+    @Override
+    public InfectiousRage copy() {
+        return new InfectiousRage(this);
+    }
+}
+
+class InfectiousRageReattachEffect extends OneShotEffect {
+
+    public InfectiousRageReattachEffect() {
+        super(Outcome.PutCardInPlay);
+        this.staticText = "choose a creature at random {this} can enchant. Return {this} to the battlefield attached to that creature.";
+    }
+
+    public InfectiousRageReattachEffect(final InfectiousRageReattachEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public InfectiousRageReattachEffect copy() {
+        return new InfectiousRageReattachEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+
+        Card aura = game.getCard(source.getSourceId());
+        Player controller = game.getPlayer(source.getControllerId());
+        if (controller == null || aura == null) {
+            return false;
+        }
+
+        FilterPermanent filter = new FilterPermanent();
+        filter.add(new PermanentCanBeAttachedToPredicate((Permanent) aura));
+        List<Permanent> permanents = game.getBattlefield().getActivePermanents(filter, source.getControllerId(), source, game);
+
+        if (!permanents.isEmpty()) {
+            Permanent creature = permanents.get(RandomUtil.nextInt(permanents.size()));
+            if (creature != null) {
+                game.getState().setValue("attachTo:" + aura.getId(), creature);
+                controller.moveCards(aura, Zone.BATTLEFIELD, source, game);
+                return creature.addAttachment(aura.getId(), source, game);
+            }
+        }
+
+        return false;
+
+    }
+
+}
---
+++ Mage/src/main/java/mage/filter/predicate/permanent/PermanentCanBeAttachedToPredicate.java
@@ -0,0 +1,34 @@
+package mage.filter.predicate.permanent;
+
+import mage.filter.Filter;
+import mage.filter.predicate.ObjectSourcePlayer;
+import mage.filter.predicate.ObjectSourcePlayerPredicate;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.target.Target;
+import mage.util.TargetAddress;
+
+/**
+ * @author duncant
+ */
+
+public class PermanentCanBeAttachedToPredicate implements ObjectSourcePlayerPredicate<Permanent> {
+
+    protected Permanent aura;
+
+    public PermanentCanBeAttachedToPredicate(Permanent aura) {
+        super();
+        this.aura = aura;
+    }
+
+    @Override
+    public boolean apply(ObjectSourcePlayer<Permanent> input, Game game) {
+        Permanent potentialAttachment = input.getObject();
+        for (TargetAddress addr : TargetAddress.walk(aura)) {
+            Target target = addr.getTarget(aura);
+            Filter filter = target.getFilter();
+            return filter.match(potentialAttachment, game);
+        }
+        return false;
+    }
+}
