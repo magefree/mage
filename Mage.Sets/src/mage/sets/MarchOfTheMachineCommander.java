402cea2697439345fc5412849ebd358a879f62a6
theelk801 <theelk801@gmail.com>
2023-04-08 21:18:36 -0400

    [MOC] Implement Herald of Hoofbeats

--- Mage.Sets/src/mage/sets/MarchOfTheMachineCommander.java
+++ Mage.Sets/src/mage/sets/MarchOfTheMachineCommander.java
@@ -130,6 +130,7 @@ public final class MarchOfTheMachineCommander extends ExpansionSet {
         cards.add(new SetCardInfo("Heaven // Earth", 328, Rarity.RARE, mage.cards.h.HeavenEarth.class));
         cards.add(new SetCardInfo("Hedron Archive", 359, Rarity.UNCOMMON, mage.cards.h.HedronArchive.class));
         cards.add(new SetCardInfo("Hellkite Igniter", 284, Rarity.RARE, mage.cards.h.HellkiteIgniter.class));
+        cards.add(new SetCardInfo("Herald of Hoofbeats", 22, Rarity.RARE, mage.cards.h.HeraldOfHoofbeats.class));
         cards.add(new SetCardInfo("Herald's Horn", 360, Rarity.UNCOMMON, mage.cards.h.HeraldsHorn.class));
         cards.add(new SetCardInfo("Hero of Bladehold", 188, Rarity.MYTHIC, mage.cards.h.HeroOfBladehold.class));
         cards.add(new SetCardInfo("High Sentinels of Arashin", 189, Rarity.RARE, mage.cards.h.HighSentinelsOfArashin.class));
