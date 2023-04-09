7abc099fd83f908077d78833ca7f1909f4adb048
theelk801 <theelk801@gmail.com>
2023-04-09 09:48:06 -0400

    add control change test

--- Mage.Tests/src/test/java/org/mage/test/cards/battle/BattleTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/battle/BattleTest.java
@@ -16,6 +16,7 @@ public class BattleTest extends CardTestPlayerBase {
 
     private static final String belenon = "Invasion of Belenon";
     private static final String bear = "Grizzly Bears";
+    private static final String confiscate = "Confiscate";
 
     private void assertBattle(Player controller, Player protector, String name) {
         assertPermanentCount(controller, name, 1);
@@ -78,5 +79,23 @@ public class BattleTest extends CardTestPlayerBase {
         assertTapped(bear, false);
         assertLife(playerB, 20);
         assertCounterCount(belenon, CounterType.DEFENSE, 5);
+    }
+
+    @Test
+    public void testChangeControl() {
+        addCard(Zone.BATTLEFIELD, playerA, "Plains", 3);
+        addCard(Zone.BATTLEFIELD, playerB, "Island", 6);
+        addCard(Zone.HAND, playerB, confiscate);
+        addCard(Zone.HAND, playerA, belenon);
+
+        castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, belenon);
+
+        castSpell(2, PhaseStep.PRECOMBAT_MAIN, playerB, confiscate, belenon);
+
+        setStopAt(2, PhaseStep.END_TURN);
+        execute();
+
+        assertBattle(playerB, playerA, belenon);
+        assertPermanentCount(playerA, "Knight Token", 1);
     }
 }
