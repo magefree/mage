3a6fd1d22c24c1ef931c6fc2f8ad3e7e3ac1593a
xenohedron <xenohedron@users.noreply.github.com>
2023-04-17 23:42:04 -0400

    Fix text: sacrifice multiple permanents (as cost)

--- Mage.Sets/src/mage/cards/p/PhyrexianTribute.java
+++ Mage.Sets/src/mage/cards/p/PhyrexianTribute.java
@@ -7,8 +7,10 @@ import mage.abilities.effects.common.DestroyTargetEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.filter.common.FilterControlledCreaturePermanent;
+import mage.filter.common.FilterControlledPermanent;
 import mage.target.common.TargetArtifactPermanent;
-import mage.target.common.TargetControlledCreaturePermanent;
+import mage.target.common.TargetControlledPermanent;
 
 /**
  *
@@ -16,11 +18,14 @@ import mage.target.common.TargetControlledCreaturePermanent;
  */
 public final class PhyrexianTribute extends CardImpl {
 
+    private static final FilterControlledPermanent filter
+            = new FilterControlledCreaturePermanent("creatures");
+
     public PhyrexianTribute(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId,setInfo,new CardType[]{CardType.SORCERY},"{2}{B}");
 
         // As an additional cost to cast Phyrexian Tribute, sacrifice two creatures.
-        this.getSpellAbility().addCost(new SacrificeTargetCost(new TargetControlledCreaturePermanent(2)));
+        this.getSpellAbility().addCost(new SacrificeTargetCost(new TargetControlledPermanent(2, filter)));
         // Destroy target artifact.
         this.getSpellAbility().addEffect(new DestroyTargetEffect());
         this.getSpellAbility().addTarget(new TargetArtifactPermanent());
