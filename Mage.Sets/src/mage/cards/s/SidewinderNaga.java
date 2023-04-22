8ca616ba62e77250de0a13a90bc53b96d27b7252
xenohedron <xenohedron@users.noreply.github.com>
2023-04-21 22:00:29 -0400

    Fix text: Solitary Camel, Sidewinder Naga

--- Mage.Sets/src/mage/cards/s/SidewinderNaga.java
+++ Mage.Sets/src/mage/cards/s/SidewinderNaga.java
@@ -1,28 +1,21 @@
 
 package mage.cards.s;
 
-import java.util.UUID;
 import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.SimpleStaticAbility;
-import mage.abilities.condition.Condition;
 import mage.abilities.condition.common.DesertControlledOrGraveyardCondition;
 import mage.abilities.decorator.ConditionalContinuousEffect;
-import mage.abilities.dynamicvalue.common.PermanentsOnBattlefieldCount;
 import mage.abilities.effects.common.continuous.BoostSourceEffect;
 import mage.abilities.effects.common.continuous.GainAbilitySourceEffect;
 import mage.abilities.keyword.TrampleAbility;
-import mage.cards.Card;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.Duration;
 import mage.constants.SubType;
-import mage.constants.Zone;
-import mage.filter.FilterCard;
-import mage.filter.common.FilterControlledLandPermanent;
-import mage.game.Game;
-import mage.players.Player;
+
+import java.util.UUID;
 
 /**
  *
@@ -41,8 +34,8 @@ public final class SidewinderNaga extends CardImpl {
         // As long as  you control a Desert or there is a Desert card in your graveyard, Sidewinder Naga gets +1/+0 and has trample.
         Ability ability = new SimpleStaticAbility(new ConditionalContinuousEffect(
                 new BoostSourceEffect(1, 0, Duration.WhileOnBattlefield),
-                DesertControlledOrGraveyardCondition.instance, "As long as  you control a Desert " +
-                "or there is a Desert card in your graveyard, {this} gets +1/+0 "
+                DesertControlledOrGraveyardCondition.instance, "As long as you control a Desert " +
+                "or there is a Desert card in your graveyard, {this} gets +1/+0"
         ));
         ability.addEffect(new ConditionalContinuousEffect(new GainAbilitySourceEffect(
                 TrampleAbility.getInstance(), Duration.WhileOnBattlefield
