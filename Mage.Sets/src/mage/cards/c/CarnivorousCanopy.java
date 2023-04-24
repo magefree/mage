2fe8e12f712a1c4d620fe7a4b378903c617ba702
theelk801 <theelk801@gmail.com>
2023-04-23 21:36:54 -0400

    [ONE] a few small changes

--- Mage.Sets/src/mage/cards/c/CarnivorousCanopy.java
+++ Mage.Sets/src/mage/cards/c/CarnivorousCanopy.java
@@ -19,38 +19,38 @@ import mage.target.TargetPermanent;
 import java.util.UUID;
 
 /**
- *
  * @author AhmadYProjects
  */
 public final class CarnivorousCanopy extends CardImpl {
+
     private static final FilterPermanent filter = new FilterPermanent("artifact, enchantment, or creature with flying");
     private static final FilterPermanent filter2 = new FilterPermanent();
 
 
     static {
         filter.add(Predicates.or(
-                CardType.ARTIFACT.getPredicate(),
-                CardType.ENCHANTMENT.getPredicate(),
-                Predicates.and(
-                        CardType.CREATURE.getPredicate(),
-                        new AbilityPredicate(FlyingAbility.class))
+                        CardType.ARTIFACT.getPredicate(),
+                        CardType.ENCHANTMENT.getPredicate(),
+                        Predicates.and(
+                                CardType.CREATURE.getPredicate(),
+                                new AbilityPredicate(FlyingAbility.class)
+                        )
                 )
         );
         filter2.add(new ManaValuePredicate(ComparisonType.FEWER_THAN, 4));
-
     }
 
     private static final Condition condition = new SourceTargetsPermanentCondition(filter2);
 
     public CarnivorousCanopy(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{2}{G}");
-        
 
         // Destroy target artifact, enchantment, or creature with flying. If that permanent's mana value was 3 or less, proliferate.
         this.getSpellAbility().addEffect(new DestroyTargetEffect());
         this.getSpellAbility().addTarget(new TargetPermanent(filter));
-        this.getSpellAbility().addEffect(new ConditionalOneShotEffect(new ProliferateEffect(), condition));
-
+        this.getSpellAbility().addEffect(new ConditionalOneShotEffect(
+                new ProliferateEffect(), condition, "If that permanent's mana value was 3 or less, proliferate"
+        ));
     }
 
     private CarnivorousCanopy(final CarnivorousCanopy card) {
--- Mage.Sets/src/mage/cards/c/CinderslashRavager.java
+++ Mage.Sets/src/mage/cards/c/CinderslashRavager.java
@@ -1,6 +1,5 @@
 package mage.cards.c;
 
-import java.util.UUID;
 import mage.MageInt;
 import mage.abilities.common.EntersBattlefieldTriggeredAbility;
 import mage.abilities.common.SimpleStaticAbility;
@@ -8,43 +7,49 @@ import mage.abilities.dynamicvalue.DynamicValue;
 import mage.abilities.dynamicvalue.common.PermanentsOnBattlefieldCount;
 import mage.abilities.effects.common.DamageAllEffect;
 import mage.abilities.effects.common.cost.SpellCostReductionForEachSourceEffect;
-import mage.constants.SubType;
 import mage.abilities.keyword.VigilanceAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.constants.SubType;
 import mage.constants.Zone;
 import mage.counters.CounterType;
+import mage.filter.StaticFilters;
 import mage.filter.common.FilterControlledPermanent;
-import mage.filter.common.FilterOpponentsCreaturePermanent;
+
+import java.util.UUID;
 
 /**
- *
  * @author AhmadYProjects
  */
 public final class CinderslashRavager extends CardImpl {
-    private static final FilterControlledPermanent filter = new FilterControlledPermanent("permanents you control with an oil counter on it");
+
+    private static final FilterControlledPermanent filter = new FilterControlledPermanent("permanent you control with an oil counter on it");
 
     static {
         filter.add(CounterType.OIL.getPredicate());
     }
+
     private static final DynamicValue xValue = new PermanentsOnBattlefieldCount(filter);
 
     public CinderslashRavager(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{4}{R}{G}");
-        
+
         this.subtype.add(SubType.PHYREXIAN);
         this.subtype.add(SubType.WARRIOR);
         this.power = new MageInt(5);
         this.toughness = new MageInt(5);
 
         // This spell costs {1} less to cast for each permanent you control with oil counters on it.
-        this.addAbility(new SimpleStaticAbility(Zone.ALL,new SpellCostReductionForEachSourceEffect(1, xValue)));
+        this.addAbility(new SimpleStaticAbility(Zone.ALL, new SpellCostReductionForEachSourceEffect(1, xValue)));
+
         // Vigilance
         this.addAbility(VigilanceAbility.getInstance());
 
         // When Cinderslash Ravager enters the battlefield, it deals 1 damage to each creature your opponents control.
-        this.addAbility(new EntersBattlefieldTriggeredAbility(new DamageAllEffect(1, new FilterOpponentsCreaturePermanent())));
+        this.addAbility(new EntersBattlefieldTriggeredAbility(new DamageAllEffect(
+                1, "it", StaticFilters.FILTER_OPPONENTS_PERMANENT_CREATURES
+        )));
     }
 
     private CinderslashRavager(final CinderslashRavager card) {
