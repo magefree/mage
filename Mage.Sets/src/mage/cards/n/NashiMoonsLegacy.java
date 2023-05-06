f91543411f655101ef0afca211830d6ee3a7b529
theelk801 <theelk801@gmail.com>
2023-05-05 20:45:27 -0400

    fix verify failure

--- Mage.Sets/src/mage/cards/n/NashiMoonsLegacy.java
+++ Mage.Sets/src/mage/cards/n/NashiMoonsLegacy.java
@@ -44,7 +44,7 @@ public final class NashiMoonsLegacy extends CardImpl {
         this.toughness = new MageInt(4);
 
         // Menace
-        this.addAbility(new MenaceAbility());
+        this.addAbility(new MenaceAbility(false));
 
         // Ward {1}
         this.addAbility(new WardAbility(new ManaCostsImpl<>("{1}"), false));
