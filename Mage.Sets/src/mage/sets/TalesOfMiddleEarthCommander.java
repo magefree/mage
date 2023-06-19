31b2bf5ebbd3f1febe60e6aef57f5becf05e4dec
theelk801 <theelk801@gmail.com>
2023-06-18 22:33:14 -0400

    [LTC] Implement Haldir, Lorien Lieutenant

--- Mage.Sets/src/mage/sets/TalesOfMiddleEarthCommander.java
+++ Mage.Sets/src/mage/sets/TalesOfMiddleEarthCommander.java
@@ -113,6 +113,7 @@ public final class TalesOfMiddleEarthCommander extends ExpansionSet {
         cards.add(new SetCardInfo("Grey Host Reinforcements", 98, Rarity.RARE, mage.cards.g.GreyHostReinforcements.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Growth Spiral", 267, Rarity.COMMON, mage.cards.g.GrowthSpiral.class));
         cards.add(new SetCardInfo("Guttersnipe", 220, Rarity.UNCOMMON, mage.cards.g.Guttersnipe.class));
+        cards.add(new SetCardInfo("Haldir, Lorien Lieutenant", 39, Rarity.RARE, mage.cards.h.HaldirLorienLieutenant.class));
         cards.add(new SetCardInfo("Harmonize", 248, Rarity.UNCOMMON, mage.cards.h.Harmonize.class));
         cards.add(new SetCardInfo("Harsh Mentor", 221, Rarity.RARE, mage.cards.h.HarshMentor.class));
         cards.add(new SetCardInfo("Heirloom Blade", 279, Rarity.UNCOMMON, mage.cards.h.HeirloomBlade.class));
