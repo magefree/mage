3f2d104cf80be35d135b4c23e8f0842687fe8650
theelk801 <theelk801@gmail.com>
2023-06-18 22:33:49 -0400

    [LTC] Implement Corsairs of Umbar

--- Mage.Sets/src/mage/sets/TalesOfMiddleEarthCommander.java
+++ Mage.Sets/src/mage/sets/TalesOfMiddleEarthCommander.java
@@ -52,6 +52,7 @@ public final class TalesOfMiddleEarthCommander extends ExpansionSet {
         cards.add(new SetCardInfo("Command Tower", 301, Rarity.COMMON, mage.cards.c.CommandTower.class));
         cards.add(new SetCardInfo("Commander's Sphere", 276, Rarity.COMMON, mage.cards.c.CommandersSphere.class));
         cards.add(new SetCardInfo("Consider", 187, Rarity.COMMON, mage.cards.c.Consider.class));
+        cards.add(new SetCardInfo("Corsairs of Umbar", 19, Rarity.RARE, mage.cards.c.CorsairsOfUmbar.class));
         cards.add(new SetCardInfo("Court of Ire", 213, Rarity.RARE, mage.cards.c.CourtOfIre.class));
         cards.add(new SetCardInfo("Crumbling Necropolis", 302, Rarity.UNCOMMON, mage.cards.c.CrumblingNecropolis.class));
         cards.add(new SetCardInfo("Crypt Incursion", 198, Rarity.COMMON, mage.cards.c.CryptIncursion.class));
