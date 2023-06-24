768524c7c694c8ac435629abe0a29809153e21d3
xenohedron <xenohedron@users.noreply.github.com>
2023-06-23 23:09:07 -0400

    adjust implementation and fix text

--- Mage.Sets/src/mage/cards/m/MasterChef.java
+++ Mage.Sets/src/mage/cards/m/MasterChef.java
@@ -8,11 +8,7 @@ import mage.abilities.effects.common.continuous.GainAbilityAllEffect;
 import mage.abilities.effects.common.counter.AddCountersSourceEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.constants.CardType;
-import mage.constants.Duration;
-import mage.constants.Outcome;
-import mage.constants.SubType;
-import mage.constants.SuperType;
+import mage.constants.*;
 import mage.counters.CounterType;
 import mage.filter.StaticFilters;
 import mage.game.Game;
@@ -22,25 +18,28 @@ import mage.game.permanent.Permanent;
 
 import java.util.UUID;
 
-public class MasterChef extends CardImpl{
+public class MasterChef extends CardImpl {
 
     public MasterChef(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{2}{G}");
-
-        this.addSuperType(SuperType.LEGENDARY);
+        this.supertype.add(SuperType.LEGENDARY);
         this.subtype.add(SubType.BACKGROUND);
 
-        // Commander creatures you own have “This creature enters the battlefield with an additional +1/+1 counter on it” 
-        // and “Other creatures you control enter the battlefield with an additional +1/+1 counter on them.”
-        this.addAbility(new SimpleStaticAbility(new GainAbilityAllEffect(
+        // Commander creatures you own have "This creature enters the battlefield with an additional +1/+1 counter on it"
+        // and "Other creatures you control enter the battlefield with an additional +1/+1 counter on them."
+        Ability ability = new SimpleStaticAbility(new GainAbilityAllEffect(
             new EntersBattlefieldAbility(
                 new AddCountersSourceEffect(CounterType.P1P1.createInstance()).setText("with an additional +1/+1 counter on it")
-            ), Duration.WhileOnBattlefield, StaticFilters.FILTER_CREATURES_OWNED_COMMANDER, "Commander creatures")));
-            
-        this.addAbility(new SimpleStaticAbility(new GainAbilityAllEffect(
+            ), Duration.WhileOnBattlefield, StaticFilters.FILTER_CREATURES_OWNED_COMMANDER)
+                .setText("commander creatures you own have \"This creature enters the battlefield with an additional +1/+1 counter on it\"")
+        );
+        ability.addEffect(new GainAbilityAllEffect(
             new SimpleStaticAbility(
                 new MasterChefReplacementEffect()
-            ), Duration.WhileOnBattlefield, StaticFilters.FILTER_CREATURES_OWNED_COMMANDER)));
+            ), Duration.WhileOnBattlefield, StaticFilters.FILTER_CREATURES_OWNED_COMMANDER)
+                .setText("and \"Other creatures you control enter the battlefield with an additional +1/+1 counter on them.\"")
+        );
+        this.addAbility(ability);
 
     }
 
@@ -59,8 +58,7 @@ class MasterChefReplacementEffect extends ReplacementEffectImpl {
 
     MasterChefReplacementEffect() {
         super(Duration.WhileOnBattlefield, Outcome.BoostCreature);
-        staticText = "Other Creatures you control enter the " +
-                "battlefield with an additional +1/+1 counter on it";
+        staticText = "other creatures you control enter the battlefield with an additional +1/+1 counter on them";
     }
 
     private MasterChefReplacementEffect(final MasterChefReplacementEffect effect) {
@@ -85,7 +83,7 @@ class MasterChefReplacementEffect extends ReplacementEffectImpl {
     public boolean applies(GameEvent event, Ability source, Game game) {
         Permanent creature = ((EntersTheBattlefieldEvent)event).getTarget();
         return creature != null
-                && creature.isCreature()
+                && creature.isCreature(game)
                 && creature.isControlledBy(source.getControllerId())
                 && !source.getSourceId().equals(creature.getId());
     }
@@ -95,5 +93,4 @@ class MasterChefReplacementEffect extends ReplacementEffectImpl {
         return new MasterChefReplacementEffect(this);
     }
 
-    
 }
