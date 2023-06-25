ab4f695340a651337c2b559dba12a1ee6a3653e5
Alexander Novotny <alexander_novo@mail.tmcc.edu>
2023-06-25 14:41:59 -0700

    More unhappy tests

--- Mage.Tests/src/test/java/org/mage/test/cards/single/ltr/GoldberryRiverDaughterTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/ltr/GoldberryRiverDaughterTest.java
@@ -20,8 +20,6 @@ public class GoldberryRiverDaughterTest extends CardTestPlayerBase {
         CounterType counter2 = CounterType.AEGIS;
         String island = "Island";
 
-        // Bolt will be our first spell - to make sure it doesn't trigger
-        // Isamaru will be our second spell - to make sure we get two, since it's legendary
         addCard(Zone.BATTLEFIELD, playerA, goldberry, 1);
         addCard(Zone.BATTLEFIELD, playerA, island, 1);
 
@@ -48,11 +46,77 @@ public class GoldberryRiverDaughterTest extends CardTestPlayerBase {
         assertCounterCount(island, counter2, 1);
         assertCounterCount(goldberry, counter1, 1);
         assertCounterCount(goldberry, counter2, 0);
+
+        // One from turn 2 draw, one from goldberry
+        assertHandCount(playerA, 2);
     }
 
+    @Test
+    // Author: alexander-novo
     // Unhappy path - Try to remove some counters from something when some of those counters are already on Goldberry
+    public void testCounterAlreadyOnGoldberry() {
+        CounterType counter = CounterType.ACORN;
+        String island = "Island";
 
-    // Unhappy path - Try to not move a counter from Goldberry even though she has a counter on her
+        addCard(Zone.BATTLEFIELD, playerA, goldberry, 1);
+        addCard(Zone.BATTLEFIELD, playerA, island, 1);
 
+        addCounters(1, PhaseStep.PRECOMBAT_MAIN, playerA, island, counter, 1);
+        addCounters(1, PhaseStep.PRECOMBAT_MAIN, playerA, goldberry, counter, 1);
+
+        activateAbility(1, PhaseStep.PRECOMBAT_MAIN, playerA, ability1, island);
+        waitStackResolved(1, PhaseStep.PRECOMBAT_MAIN, 1);
+
+        setStrictChooseMode(true);
+        setStopAt(1, PhaseStep.PRECOMBAT_MAIN);
+        execute();
+
+        assertCounterCount(island, counter, 1);
+        assertCounterCount(goldberry, counter, 1);
+    }
+
+    @Test(expected = AssertionError.class)
+    // Author: alexander-novo
+    // Unhappy path - Try to not move a counter from Goldberry even though she has a counter on her.
+    // Should fail since we are attempting to move 0 counters, even though we must move at least one if possible.
+    public void testNotMovingCounter() {
+        CounterType counter = CounterType.ACORN;
+        String island = "Island";
+
+        addCard(Zone.BATTLEFIELD, playerA, goldberry, 1);
+        addCard(Zone.BATTLEFIELD, playerA, island, 1);
+
+        addCounters(1, PhaseStep.PRECOMBAT_MAIN, playerA, goldberry, counter, 1);
+
+        activateAbility(1, PhaseStep.PRECOMBAT_MAIN, playerA, ability2, island);
+        waitStackResolved(1, PhaseStep.PRECOMBAT_MAIN, 1);
+        setChoiceAmount(playerA, 0); // Try to remove 0 counters from goldberry
+
+        setStrictChooseMode(true);
+        setStopAt(1, PhaseStep.PRECOMBAT_MAIN);
+        execute();
+
+        assertCounterCount(goldberry, counter, 0);
+        assertCounterCount(island, counter, 1);
+        assertHandCount(playerA, 1);
+    }
+
+    @Test
+    // Author: alexander-novo
     // Unhappy path - Activate second ability with no counters on Goldberry
+    public void testNoCounters() {
+        String island = "Island";
+
+        addCard(Zone.BATTLEFIELD, playerA, goldberry, 1);
+        addCard(Zone.BATTLEFIELD, playerA, island, 1);
+
+        activateAbility(1, PhaseStep.PRECOMBAT_MAIN, playerA, ability2, island);
+        waitStackResolved(1, PhaseStep.PRECOMBAT_MAIN, 1);
+
+        setStrictChooseMode(true);
+        setStopAt(1, PhaseStep.PRECOMBAT_MAIN);
+        execute();
+
+        assertHandCount(playerA, 0);
+    }
 }
