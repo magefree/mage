7566f24be7b671379a3edcdc0a21aea69c13d0de
Susucre <34709007+Susucre@users.noreply.github.com>
2023-07-01 18:31:44 +0200

    refactor with DoWhenCostPaid

--- Mage.Sets/src/mage/cards/s/ShireShirriff.java
+++ Mage.Sets/src/mage/cards/s/ShireShirriff.java
@@ -1,28 +1,22 @@
 package mage.cards.s;
 
 import mage.MageInt;
-import mage.abilities.Ability;
 import mage.abilities.common.EntersBattlefieldTriggeredAbility;
 import mage.abilities.common.delayed.OnLeaveReturnExiledToBattlefieldAbility;
 import mage.abilities.common.delayed.ReflexiveTriggeredAbility;
-import mage.abilities.effects.OneShotEffect;
+import mage.abilities.costs.common.SacrificeTargetCost;
 import mage.abilities.effects.common.CreateDelayedTriggeredAbilityEffect;
+import mage.abilities.effects.common.DoWhenCostPaid;
 import mage.abilities.effects.common.ExileUntilSourceLeavesEffect;
 import mage.abilities.keyword.VigilanceAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.Outcome;
 import mage.constants.SubType;
 import mage.filter.StaticFilters;
 import mage.filter.common.FilterControlledPermanent;
 import mage.filter.predicate.permanent.TokenPredicate;
-import mage.game.Game;
-import mage.game.permanent.Permanent;
-import mage.players.Player;
-import mage.target.Target;
 import mage.target.TargetPermanent;
-import mage.target.common.TargetControlledPermanent;
 
 import java.util.UUID;
 
@@ -31,6 +25,12 @@ import java.util.UUID;
  * @author Susucr
  */
 public final class ShireShirriff extends CardImpl {
+
+    public static final FilterControlledPermanent filterToken = new FilterControlledPermanent("a token");
+
+    static {
+        filterToken.add(TokenPredicate.TRUE);
+    }
 
     public ShireShirriff(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{1}{W}");
@@ -45,7 +45,18 @@ public final class ShireShirriff extends CardImpl {
 
         // When Shire Shirriff enters the battlefield, you may sacrifice a token.
         // When you do, exile target creature an opponent controls until Shire Shirriff leaves the battlefield.
-        this.addAbility(new EntersBattlefieldTriggeredAbility(new ShireShirriffEffect(), true));
+        ReflexiveTriggeredAbility reflexive = new ReflexiveTriggeredAbility(
+            new ExileUntilSourceLeavesEffect(), false,
+            "exile target creature an opponent controls until {this} leaves the battlefield."
+        );
+        reflexive.addTarget(new TargetPermanent(StaticFilters.FILTER_OPPONENTS_PERMANENT_CREATURE));
+        reflexive.addEffect(new CreateDelayedTriggeredAbilityEffect(new OnLeaveReturnExiledToBattlefieldAbility()));
+
+        this.addAbility(new EntersBattlefieldTriggeredAbility(new DoWhenCostPaid(
+            reflexive,
+            new SacrificeTargetCost(filterToken),
+            "Sacrifice a token?"
+        ), true));
     }
 
     private ShireShirriff(final ShireShirriff card) {
@@ -55,62 +66,5 @@ public final class ShireShirriff extends CardImpl {
     @Override
     public ShireShirriff copy() {
         return new ShireShirriff(this);
-    }
-}
-
-class ShireShirriffEffect extends OneShotEffect {
-
-    public static final FilterControlledPermanent filterToken = new FilterControlledPermanent("a token");
-
-    static {
-        filterToken.add(TokenPredicate.TRUE);
-    }
-
-    ShireShirriffEffect() {
-        super(Outcome.Benefit);
-        this.staticText = "sacrifice a token. When you do, "
-            + "exile target creature an opponent controls until {this} leaves the battlefield.";
-    }
-
-    private ShireShirriffEffect(final ShireShirriffEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public ShireShirriffEffect copy() {
-        return new ShireShirriffEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Player controller = game.getPlayer(source.getControllerId());
-        if (controller == null) {
-            return false;
-        }
-
-        Target target = new TargetControlledPermanent(
-            1, 1, filterToken, true
-        );
-        if (!controller.choose(outcome, target, source, game)) {
-            return false;
-        }
-
-        Permanent toSacrifice = game.getPermanent(target.getFirstTarget());
-        if (toSacrifice == null) {
-            return false;
-        }
-        if (!toSacrifice.sacrifice(source, game)) {
-            return false;
-        }
-
-        ReflexiveTriggeredAbility trigger = new ReflexiveTriggeredAbility(
-            new ExileUntilSourceLeavesEffect(), false,
-            "exile target creature an opponent controls until {this} leaves the battlefield."
-        );
-        trigger.addTarget(new TargetPermanent(StaticFilters.FILTER_OPPONENTS_PERMANENT_CREATURE));
-        trigger.addEffect(new CreateDelayedTriggeredAbilityEffect(new OnLeaveReturnExiledToBattlefieldAbility()));
-
-        game.fireReflexiveTriggeredAbility(trigger, source);
-        return true;
     }
 }
