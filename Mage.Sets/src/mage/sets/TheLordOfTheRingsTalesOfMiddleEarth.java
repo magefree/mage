c8564efbb7f02b81a94748631d3a0167462d91ab
Bobby McCann <88658699+bobby-mccann@users.noreply.github.com>
2023-07-06 00:22:21 +0100

    [LTR] Implement Witch-king of Angmar (#10563)
    * Add card
    
    * Add TapSourceEffect
    
    * De-duplicate watcher logic
    
    * Abstract and fix(?) logic
    
    * Fix sacrifice targets
    
    * Controller instead of Owner
    
    * Add tests, fix, and refactor
    
    * Throw if controller not supported
    
    * Fix test (not supposed to start tapped)

--- Mage.Sets/src/mage/sets/TheLordOfTheRingsTalesOfMiddleEarth.java
+++ Mage.Sets/src/mage/sets/TheLordOfTheRingsTalesOfMiddleEarth.java
@@ -280,6 +280,7 @@ public final class TheLordOfTheRingsTalesOfMiddleEarth extends ExpansionSet {
         cards.add(new SetCardInfo("Warbeast of Gorgoroth", 152, Rarity.COMMON, mage.cards.w.WarbeastOfGorgoroth.class));
         cards.add(new SetCardInfo("Westfold Rider", 37, Rarity.COMMON, mage.cards.w.WestfoldRider.class));
         cards.add(new SetCardInfo("Willow-Wind", 76, Rarity.COMMON, mage.cards.w.WillowWind.class));
+        cards.add(new SetCardInfo("Witch-king of Angmar", 114, Rarity.MYTHIC, mage.cards.w.WitchKingOfAngmar.class));
         cards.add(new SetCardInfo("Witch-king, Bringer of Ruin", 293, Rarity.RARE, mage.cards.w.WitchKingBringerOfRuin.class));
         cards.add(new SetCardInfo("Wizard's Rockets", 252, Rarity.COMMON, mage.cards.w.WizardsRockets.class));
         cards.add(new SetCardInfo("Wose Pathfinder", 190, Rarity.COMMON, mage.cards.w.WosePathfinder.class));
--- Mage/src/main/java/mage/constants/TargetController.java
+++ Mage/src/main/java/mage/constants/TargetController.java
@@ -27,7 +27,8 @@ public enum TargetController {
     EACH_PLAYER,
     ENCHANTED,
     SOURCE_TARGETS,
-    MONARCH;
+    MONARCH,
+    SOURCE_CONTROLLER;
 
     private final OwnerPredicate ownerPredicate;
     private final PlayerPredicate playerPredicate;
@@ -78,6 +79,8 @@ public enum TargetController {
                 case ENCHANTED:
                     Permanent permanent = input.getSource().getSourcePermanentIfItStillExists(game);
                     return permanent != null && input.getObject().isOwnedBy(permanent.getAttachedTo());
+                case SOURCE_CONTROLLER:
+                    return card.isOwnedBy(input.getSource().getControllerId());
                 case SOURCE_TARGETS:
                     return card.isOwnedBy(input.getSource().getFirstTarget());
                 case MONARCH:
@@ -119,8 +122,10 @@ public enum TargetController {
                             game.getPlayer(playerId).hasOpponent(player.getId(), game);
                 case NOT_YOU:
                     return !player.getId().equals(playerId);
+                case SOURCE_CONTROLLER:
+                    return player.getId().equals(input.getSource().getControllerId());
                 case SOURCE_TARGETS:
-                    return player.equals(input.getSource().getFirstTarget());
+                    return player.getId().equals(input.getSource().getFirstTarget());
                 case MONARCH:
                     return player.getId().equals(game.getMonarchId());
                 default:
@@ -162,6 +167,8 @@ public enum TargetController {
                 case ENCHANTED:
                     Permanent permanent = input.getSource().getSourcePermanentIfItStillExists(game);
                     return permanent != null && input.getObject().isControlledBy(permanent.getAttachedTo());
+                case SOURCE_CONTROLLER:
+                    return object.isControlledBy(input.getSource().getControllerId());
                 case SOURCE_TARGETS:
                     return object.isControlledBy(input.getSource().getFirstTarget());
                 case MONARCH:
