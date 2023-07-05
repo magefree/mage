0b8331ff6fdabb0ce040e4bb2022a84dffb0e8ae
Bobby McCann <bobby.mccann@spareroom.co.uk>
2023-07-05 21:26:39 +0100

    Implementation

--- Mage.Sets/src/mage/cards/r/RadagastTheBrown.java
+++ Mage.Sets/src/mage/cards/r/RadagastTheBrown.java
@@ -1,18 +1,37 @@
 package mage.cards.r;
 
-import java.util.UUID;
 import mage.MageInt;
-import mage.constants.SubType;
-import mage.constants.SuperType;
+import mage.abilities.Ability;
+import mage.abilities.common.EntersBattlefieldControlledTriggeredAbility;
+import mage.abilities.effects.Effect;
+import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.LookLibraryAndPickControllerEffect;
+import mage.cards.Card;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.constants.CardType;
+import mage.constants.*;
+import mage.filter.FilterPermanent;
+import mage.filter.common.FilterControlledCreaturePermanent;
+import mage.filter.common.FilterCreatureCard;
+import mage.filter.predicate.Predicate;
+import mage.filter.predicate.permanent.TokenPredicate;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+
+import java.util.List;
+import java.util.UUID;
 
 /**
  *
  * @author bobby-mccann
  */
 public final class RadagastTheBrown extends CardImpl {
+
+    static final FilterPermanent filter = new FilterControlledCreaturePermanent("{this} or another nontoken creature you control");
+
+    static {
+        filter.add(TokenPredicate.FALSE);
+    }
 
     public RadagastTheBrown(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{2}{G}{G}");
@@ -28,6 +47,11 @@ public final class RadagastTheBrown extends CardImpl {
         // You may reveal a creature card from among them that doesn't share a creature type with a creature you control
         // and put it into your hand.
         // Put the rest on the bottom of your library in a random order.
+        this.addAbility(new EntersBattlefieldControlledTriggeredAbility(
+                Zone.BATTLEFIELD,
+                new RadagastEffect(),
+                filter, false
+        ));
     }
 
     private RadagastTheBrown(final RadagastTheBrown card) {
@@ -37,5 +61,65 @@ public final class RadagastTheBrown extends CardImpl {
     @Override
     public RadagastTheBrown copy() {
         return new RadagastTheBrown(this);
+    }
+}
+
+class RadagastEffect extends OneShotEffect {
+
+    static final FilterCreatureCard filter = new FilterCreatureCard("creature card that doesn't share a creature type with a creature you control");
+
+    static {
+        filter.add(SharesACreatureTypeWithCreaturesYouControl.FALSE);
+    }
+
+    public RadagastEffect() {
+        super(Outcome.Benefit);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Permanent enteringBattlefield = (Permanent) getValue("permanentEnteringBattlefield");
+        if (enteringBattlefield == null) {
+            return false;
+        }
+        int manaValue = enteringBattlefield.getManaValue();
+        if (manaValue == 0) {
+            return false;
+        }
+        new LookLibraryAndPickControllerEffect(
+                manaValue, 1,
+                filter,
+                PutCards.HAND, PutCards.BOTTOM_RANDOM
+        );
+        return false;
+    }
+
+    @Override
+    public Effect copy() {
+        return null;
+    }
+}
+
+enum SharesACreatureTypeWithCreaturesYouControl implements Predicate<Card> {
+    TRUE(true),
+    FALSE(false);
+    private final boolean doesShare;
+
+    SharesACreatureTypeWithCreaturesYouControl(boolean doesShare) {
+        this.doesShare = doesShare;
+    }
+    @Override
+    public boolean apply(Card card, Game game) {
+        UUID playerId = card.getOwnerId();
+        List<Permanent> creaturesYouControl = game.getBattlefield().getActivePermanents(
+                new FilterControlledCreaturePermanent(),
+                playerId,
+                game
+        );
+        for (Permanent creature : creaturesYouControl) {
+            creature.shareCreatureTypes(game, card);
+            return doesShare;
+        }
+        return !doesShare;
     }
 }
