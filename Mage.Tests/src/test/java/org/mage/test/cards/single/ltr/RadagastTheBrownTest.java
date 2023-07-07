2393282abd82905bb6172c8491ac412c543b0c42
Bobby McCann <bobby.mccann@spareroom.co.uk>
2023-07-07 22:52:21 +0100

    Add test for when Radagast is a token

--- Mage.Tests/src/test/java/org/mage/test/cards/single/ltr/RadagastTheBrownTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/ltr/RadagastTheBrownTest.java
@@ -1,5 +1,6 @@
 package org.mage.test.cards.single.ltr;
 
+import mage.cards.f.FeldonOfTheThirdPath;
 import mage.constants.PhaseStep;
 import mage.constants.Zone;
 import org.junit.Test;
@@ -50,5 +51,37 @@ public class RadagastTheBrownTest extends CardTestPlayerBase {
 
         assertHandCount(playerA, 2);
         assertHandCount(playerA, "Goblin Guide", 1);
+    }
+
+    @Test
+    public void whenItsAToken() {
+        setStrictChooseMode(true);
+
+        skipInitShuffling();
+        removeAllCardsFromLibrary(playerA);
+        addCard(Zone.LIBRARY, playerA, "Goblin Guide"); // Goblin - 1 CMC
+        addCard(Zone.LIBRARY, playerA, "Amoeboid Changeling"); // Changeling - 2 CMC - should share creature types with anything
+        addCard(Zone.LIBRARY, playerA, "Heliod's Emissary"); // Enchantment Creature - Elk - 4 CMC
+        addCard(Zone.LIBRARY, playerA, "Overbeing of Myth"); // Spirit Avatar - 5 CMC
+        addCard(Zone.LIBRARY, playerA, "Boggart Shenanigans"); // Tribal Enchantment - Goblin
+        addCard(Zone.LIBRARY, playerA, "Stampeding Elk Herd"); // Elk - 5 CMC
+
+        addCard(Zone.BATTLEFIELD, playerA, "Mountain", 10);
+
+        addCard(Zone.GRAVEYARD, playerA, radagast);
+        addCard(Zone.BATTLEFIELD, playerA, "Feldon of the Third Path");
+
+        activateAbility(3, PhaseStep.PRECOMBAT_MAIN, playerA,
+                "{2}{R}, {T}: Create a token that's a copy of target creature card in your graveyard, except it's an artifact in addition to its other types. It gains haste. Sacrifice it at the beginning of the next end step.",
+                radagast);
+
+        // 4 cards revealed - choose from Stampeding Elk Herd and Heliod's Emissary:
+        setChoice(playerA, "Yes");
+        addTarget(playerA, "Heliod's Emissary");
+
+        setStopAt(3, PhaseStep.POSTCOMBAT_MAIN);
+        execute();
+
+        assertPermanentCount(playerA, 12);
     }
 }
