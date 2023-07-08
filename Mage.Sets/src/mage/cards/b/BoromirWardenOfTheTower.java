8acb5e914a72dac2d9d0b45b57f781fea1b4b5fc
Susucre <34709007+Susucre@users.noreply.github.com>
2023-07-09 00:02:51 +0200

    group trigger from Boromir and Lavinia

--- Mage.Sets/src/mage/cards/b/BoromirWardenOfTheTower.java
+++ Mage.Sets/src/mage/cards/b/BoromirWardenOfTheTower.java
@@ -2,10 +2,9 @@ package mage.cards.b;
 
 import mage.MageInt;
 import mage.abilities.Ability;
-import mage.abilities.TriggeredAbilityImpl;
 import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.common.SpellCastOpponentNoManaSpentTriggeredAbility;
 import mage.abilities.costs.common.SacrificeSourceCost;
-import mage.abilities.effects.Effect;
 import mage.abilities.effects.common.CounterTargetEffect;
 import mage.abilities.effects.common.continuous.GainAbilityAllEffect;
 import mage.abilities.effects.keyword.TheRingTemptsYouEffect;
@@ -13,12 +12,11 @@ import mage.abilities.keyword.IndestructibleAbility;
 import mage.abilities.keyword.VigilanceAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.constants.*;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.SubType;
+import mage.constants.SuperType;
 import mage.filter.StaticFilters;
-import mage.game.Game;
-import mage.game.events.GameEvent;
-import mage.game.stack.Spell;
-import mage.target.targetpointer.FixedTarget;
 
 import java.util.UUID;
 
@@ -41,7 +39,7 @@ public final class BoromirWardenOfTheTower extends CardImpl {
         this.addAbility(VigilanceAbility.getInstance());
 
         // Whenever an opponent casts a spell, if no mana was spent to cast it, counter that spell.
-        this.addAbility(new BoromirWardenOfTheTowerTriggeredAbility());
+        this.addAbility(new SpellCastOpponentNoManaSpentTriggeredAbility(new CounterTargetEffect()));
 
         // Sacrifice Boromir, Warden of the Tower: Creatures you control gain indestructible until end of turn. The Ring tempts you.
         Ability ability = new SimpleActivatedAbility(new GainAbilityAllEffect(
@@ -59,45 +57,5 @@ public final class BoromirWardenOfTheTower extends CardImpl {
     @Override
     public BoromirWardenOfTheTower copy() {
         return new BoromirWardenOfTheTower(this);
-    }
-}
-
-class BoromirWardenOfTheTowerTriggeredAbility extends TriggeredAbilityImpl {
-
-    public BoromirWardenOfTheTowerTriggeredAbility() {
-        super(Zone.BATTLEFIELD, new CounterTargetEffect(), false);
-    }
-
-    public BoromirWardenOfTheTowerTriggeredAbility(final BoromirWardenOfTheTowerTriggeredAbility ability) {
-        super(ability);
-    }
-
-    @Override
-    public BoromirWardenOfTheTowerTriggeredAbility copy() {
-        return new BoromirWardenOfTheTowerTriggeredAbility(this);
-    }
-
-    @Override
-    public boolean checkEventType(GameEvent event, Game game) {
-        return event.getType() == GameEvent.EventType.SPELL_CAST;
-    }
-
-    @Override
-    public boolean checkTrigger(GameEvent event, Game game) {
-        if (game.getPlayer(this.getControllerId()).hasOpponent(event.getPlayerId(), game)) {
-            Spell spell = game.getStack().getSpell(event.getTargetId());
-            if (spell != null && spell.getStackAbility().getManaCostsToPay().getUsedManaToPay().count() == 0) {
-                for (Effect effect : this.getEffects()) {
-                    effect.setTargetPointer(new FixedTarget(event.getTargetId()));
-                }
-                return true;
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public String getRule() {
-        return "Whenever an opponent casts a spell, if no mana was spent to cast it, counter that spell.";
     }
 }
