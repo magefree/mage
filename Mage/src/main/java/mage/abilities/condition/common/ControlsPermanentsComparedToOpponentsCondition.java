636c045ad2ebd7f2918a9628a47d7eff8505d818
Bobby McCann <bobby.mccann@spareroom.co.uk>
2023-07-11 11:25:06 +0100

    Add other enum values to ControlsPermanentsComparedToOpponentsCondition.toString()

--- Mage/src/main/java/mage/abilities/condition/common/ControlsPermanentsComparedToOpponentsCondition.java
+++ Mage/src/main/java/mage/abilities/condition/common/ControlsPermanentsComparedToOpponentsCondition.java
@@ -43,6 +43,8 @@ public class ControlsPermanentsComparedToOpponentsCondition implements Condition
             case EQUAL_TO: return String.format("equal %s to each opponent", filterPermanent.getMessage());
             case FEWER_THAN: return String.format("fewer %s than each opponent", filterPermanent.getMessage());
             case MORE_THAN: return String.format("more %s than each opponent", filterPermanent.getMessage());
+            case OR_LESS: return String.format("fewer or equal %s than each opponent", filterPermanent.getMessage());
+            case OR_GREATER: return String.format("more or equal %s than each opponent", filterPermanent.getMessage());
             default: throw new IllegalArgumentException("comparison rules for " + type + " missing");
         }
     }
