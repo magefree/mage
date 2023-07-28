85b345ea6bea0bf96cc1d87f18fa20b8cce5cd01
Susucre <34709007+Susucre@users.noreply.github.com>
2023-07-28 19:48:55 +0200

    [CMM] Implement Narci, Fable Singer

--- Mage.Sets/src/mage/cards/t/TomBombadil.java
+++ Mage.Sets/src/mage/cards/t/TomBombadil.java
@@ -1,34 +1,25 @@
 package mage.cards.t;
 
-import java.util.Optional;
-import java.util.UUID;
 import mage.MageInt;
 import mage.abilities.Ability;
-import mage.abilities.TriggeredAbility;
-import mage.abilities.TriggeredAbilityImpl;
-import mage.abilities.common.SagaAbility;
+import mage.abilities.common.FinalChapterAbilityResolvesTriggeredAbility;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.condition.common.CountersOnPermanentsCondition;
 import mage.abilities.decorator.ConditionalContinuousEffect;
 import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.continuous.GainAbilitySourceEffect;
+import mage.abilities.hint.common.CountersOnPermanentsHint;
 import mage.abilities.keyword.HexproofAbility;
 import mage.abilities.keyword.IndestructibleAbility;
-import mage.cards.Card;
-import mage.cards.CardImpl;
-import mage.cards.CardSetInfo;
-import mage.cards.Cards;
-import mage.cards.CardsImpl;
+import mage.cards.*;
 import mage.constants.*;
 import mage.counters.CounterType;
 import mage.filter.FilterPermanent;
 import mage.filter.common.FilterControlledPermanent;
 import mage.game.Game;
-import mage.game.events.GameEvent;
-import mage.game.permanent.Permanent;
 import mage.players.Player;
-import mage.util.CardUtil;
-import mage.abilities.hint.common.CountersOnPermanentsHint;
+
+import java.util.UUID;
 
 /**
  * @author alexander-novo
@@ -49,21 +40,17 @@ public final class TomBombadil extends CardImpl {
         this.power = new MageInt(4);
         this.toughness = new MageInt(4);
 
-        // As long as there are four or more lore counters among Sagas you control, Tom
-        // Bombadil has hexproof and indestructible.
+        // As long as there are four or more lore counters among Sagas you control, Tom Bombadil has hexproof and indestructible.
         Ability ability = new SimpleStaticAbility(new ConditionalContinuousEffect(
-                new GainAbilitySourceEffect(HexproofAbility.getInstance()),
-                condition,
-                "As long as there are four or more lore counters among Sagas you control, {this} has hexproof"));
+            new GainAbilitySourceEffect(HexproofAbility.getInstance()),
+            condition,
+            "As long as there are four or more lore counters among Sagas you control, {this} has hexproof"));
         ability.addEffect(new ConditionalContinuousEffect(
-                new GainAbilitySourceEffect(IndestructibleAbility.getInstance()), condition, "and indestructible"));
+            new GainAbilitySourceEffect(IndestructibleAbility.getInstance()), condition, "and indestructible"));
         this.addAbility(ability.addHint(hint));
 
-        // Whenever the final chapter ability of a Saga you control resolves, reveal
-        // cards from the top of your library until you reveal a Saga card. Put that
-        // card onto the battlefield and the rest on the bottom of your library in a
-        // random order. This ability triggers only once each turn.
-        this.addAbility(new TomBombadilTriggeredAbility().setTriggersOnceEachTurn(true));
+        // Whenever the final chapter ability of a Saga you control resolves, reveal cards from the top of your library until you reveal a Saga card. Put that card onto the battlefield and the rest on the bottom of your library in a random order. This ability triggers only once each turn.
+        this.addAbility(new FinalChapterAbilityResolvesTriggeredAbility(new TomBombadilEffect()).setTriggersOnceEachTurn(true));
     }
 
     private TomBombadil(final TomBombadil card) {
@@ -74,71 +61,6 @@ public final class TomBombadil extends CardImpl {
     public TomBombadil copy() {
         return new TomBombadil(this);
     }
-}
-
-class TomBombadilTriggeredAbility extends TriggeredAbilityImpl {
-
-    public TomBombadilTriggeredAbility() {
-        super(Zone.BATTLEFIELD, new TomBombadilEffect(), false);
-        setTriggerPhrase("Whenever the final chapter ability of a Saga you control resolves, ");
-    }
-
-    public TomBombadilTriggeredAbility(final TomBombadilTriggeredAbility ability) {
-        super(ability);
-    }
-
-    @Override
-    public TomBombadilTriggeredAbility copy() {
-        return new TomBombadilTriggeredAbility(this);
-    }
-
-    @Override
-    public boolean checkEventType(GameEvent event, Game game) {
-        return event.getType() == GameEvent.EventType.RESOLVING_ABILITY;
-    }
-
-    @Override
-    public boolean checkTrigger(GameEvent event, Game game) {
-        // At this point, the stack ability no longer exists, so we can only reference
-        // the ability that it came from. For EventType.RESOLVING_ABILITY, targetID is
-        // the ID of the original ability (on the permanent) that the resolving ability
-        // came from.
-        Optional<Ability> ability_opt = game.getAbility(event.getTargetId(), event.getSourceId());
-        if (!ability_opt.isPresent())
-            return false;
-
-        // Make sure it was a triggered ability (needed for checking if it's a chapter
-        // ability)
-        Ability ability = ability_opt.get();
-        if (!(ability instanceof TriggeredAbility))
-            return false;
-
-        // Make sure it was a chapter ability
-        TriggeredAbility triggeredAbility = (TriggeredAbility) ability;
-        if (!SagaAbility.isChapterAbility(triggeredAbility))
-            return false;
-
-        // There's a chance that the permanent that this abiltiy came from no longer
-        // exists, so try and find it on the battlefield or check last known
-        // information.
-        // This permanent is needed to check if the resolving ability was the final
-        // chapter ability on that permanent.
-        Permanent permanent = game.getPermanentOrLKIBattlefield(event.getSourceId());
-        if (permanent == null
-                || !permanent.isControlledBy(getControllerId())
-                || !permanent.hasSubtype(SubType.SAGA, game)) {
-            return false;
-        }
-        // Find the max chapter number from that permanent
-        int maxChapter = CardUtil
-                .castStream(permanent.getAbilities(game).stream(), SagaAbility.class)
-                .map(SagaAbility::getMaxChapter)
-                .mapToInt(SagaChapter::getNumber)
-                .sum();
-        // Check if the ability was the last one
-        return SagaAbility.isFinalAbility(triggeredAbility, maxChapter);
-    }
-
 }
 
 // From PrismaticBridgeEffect
