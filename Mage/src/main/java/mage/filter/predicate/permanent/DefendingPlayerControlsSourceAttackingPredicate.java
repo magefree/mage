967b9651d199c2c220ebc4071d3fe104454999be
Susucre <34709007+Susucre@users.noreply.github.com>
2023-08-02 17:36:32 +0200

    clean comment

--- Mage/src/main/java/mage/filter/predicate/permanent/DefendingPlayerControlsSourceAttackingPredicate.java
+++ Mage/src/main/java/mage/filter/predicate/permanent/DefendingPlayerControlsSourceAttackingPredicate.java
@@ -9,7 +9,7 @@ import mage.game.permanent.Permanent;
  * This predicate can only apply when source is an attacking creature.
  * <p>
  * It should be used when the source is an attacking creature during an effect
- * 'when {this} attacks, target creature defending player [...]'
+ * e.g. 'when {this} attacks, target creature defending player controls can't block this turn.'
  *
  * @author Susucr
  */
