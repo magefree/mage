f74d2e32ae0999fce30ee0cc44d47729ffcadad1
xenohedron <xenohedron@users.noreply.github.com>
2023-08-05 17:28:50 -0400

    followup fixes

--- Mage.Sets/src/mage/cards/t/TheHowlingAbomination.java
+++ Mage.Sets/src/mage/cards/t/TheHowlingAbomination.java
@@ -47,7 +47,7 @@ public final class TheHowlingAbomination extends CardImpl {
         // Electric Thunder—Whenever Blanka becomes the target of a spell, he gets +2/+2 until end of turn and deals 2 damage to each opponent.
         Ability ability = new SourceBecomesTargetTriggeredAbility(new BoostSourceEffect(
                 2, 2, Duration.EndOfTurn
-        ).setText("he gets +2/+2 until end of turn"), StaticFilters.FILTER_SPELL_A).setTriggerPhrase("Whenever {this} becomes the target of a spell, ");
+        ).setText("it gets +2/+2 until end of turn"), StaticFilters.FILTER_SPELL_A).setTriggerPhrase("Whenever {this} becomes the target of a spell, ");
         ability.addEffect(new DamagePlayersEffect(2, TargetController.OPPONENT)
                 .setText("and deals 2 damage to each opponent"));
         this.addAbility(ability);
