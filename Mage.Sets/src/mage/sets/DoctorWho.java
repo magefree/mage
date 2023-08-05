6ba4a9ee06bd53172183373a3e5c45aa120fe256
theelk801 <theelk801@gmail.com>
2023-08-05 13:14:41 -0400

    [WHO] update spoiler and reprints

--- Mage.Sets/src/mage/sets/DoctorWho.java
+++ Mage.Sets/src/mage/sets/DoctorWho.java
@@ -19,6 +19,7 @@ public final class DoctorWho extends ExpansionSet {
         super("Doctor Who", "WHO", ExpansionSet.buildDate(2023, 10, 13), SetType.SUPPLEMENTAL);
 
         cards.add(new SetCardInfo("Exterminate!", 68, Rarity.UNCOMMON, mage.cards.e.Exterminate.class));
+        cards.add(new SetCardInfo("Farewell", 207, Rarity.RARE, mage.cards.f.Farewell.class));
         cards.add(new SetCardInfo("Forest", 205, Rarity.LAND, mage.cards.basiclands.Forest.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Island", 199, Rarity.LAND, mage.cards.basiclands.Island.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Mountain", 203, Rarity.LAND, mage.cards.basiclands.Mountain.class, NON_FULL_USE_VARIOUS));
