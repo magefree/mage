35a5cc48a73f5dfd3787896843432c0c94cdcd95
xenohedron <xenohedron@users.noreply.github.com>
2023-08-06 23:04:53 -0400

    Improve DealsCombatDamageEquippedTriggeredAbility (#10767)
    * improve variable name
    
    * add test for Jitte trample damage
    
    * fix duplicated triggers when trampling over to player
    
    * bring corresponding ability into alignment
    
    * adjust authorship, comment

--- Mage/src/main/java/mage/abilities/common/DealsCombatDamageTriggeredAbility.java
+++ Mage/src/main/java/mage/abilities/common/DealsCombatDamageTriggeredAbility.java
@@ -4,29 +4,30 @@ import mage.abilities.TriggeredAbilityImpl;
 import mage.abilities.effects.Effect;
 import mage.constants.Zone;
 import mage.game.Game;
+import mage.game.events.DamagedBatchEvent;
 import mage.game.events.DamagedEvent;
 import mage.game.events.GameEvent;
 
 /**
- * This triggers only once for each phase the source creature deals damage.
+ * This triggers only once for each combat damage step the source creature deals damage.
  * So a creature blocked by two creatures and dealing damage to both blockers in the same
  * combat damage step triggers only once.
  *
- * @author LevelX
+ * @author LevelX, xenohedron
  */
 public class DealsCombatDamageTriggeredAbility extends TriggeredAbilityImpl {
 
-    private boolean usedInPhase;
+    private boolean usedThisStep;
 
     public DealsCombatDamageTriggeredAbility(Effect effect, boolean optional) {
         super(Zone.BATTLEFIELD, effect, optional);
-        this.usedInPhase = false;
+        this.usedThisStep = false;
         setTriggerPhrase("Whenever {this} deals combat damage, ");
     }
 
     protected DealsCombatDamageTriggeredAbility(final DealsCombatDamageTriggeredAbility ability) {
         super(ability);
-        this.usedInPhase = ability.usedInPhase;
+        this.usedThisStep = ability.usedThisStep;
     }
 
     @Override
@@ -36,22 +37,33 @@ public class DealsCombatDamageTriggeredAbility extends TriggeredAbilityImpl {
 
     @Override
     public boolean checkEventType(GameEvent event, Game game) {
-        return event instanceof DamagedEvent || event.getType() == GameEvent.EventType.COMBAT_DAMAGE_STEP_PRE;
+        return event instanceof DamagedBatchEvent || event.getType() == GameEvent.EventType.COMBAT_DAMAGE_STEP_PRE;
     }
 
     @Override
     public boolean checkTrigger(GameEvent event, Game game) {
-        if (event instanceof DamagedEvent
-                && !usedInPhase
-                && event.getSourceId().equals(this.sourceId)
-                && ((DamagedEvent) event).isCombatDamage()) {
-            usedInPhase = true;
-            getEffects().setValue("damage", event.getAmount());
-            return true;
-        }
         if (event.getType() == GameEvent.EventType.COMBAT_DAMAGE_STEP_PRE) {
-            usedInPhase = false;
+            usedThisStep = false; // clear before damage
+            return false;
         }
-        return false;
+        if (usedThisStep || !(event instanceof DamagedBatchEvent)) {
+            return false; // trigger only on DamagedEvent and if not yet triggered this step
+        }
+        int amount = ((DamagedBatchEvent) event)
+                .getEvents()
+                .stream()
+                .filter(DamagedEvent::isCombatDamage)
+                .filter(e -> e.getAttackerId().equals(this.sourceId))
+                .mapToInt(GameEvent::getAmount)
+                .sum();
+        if (amount < 1) {
+            return false;
+        }
+        usedThisStep = true;
+        this.getEffects().setValue("damage", amount);
+        // TODO: this value saved will not be correct if both permanent and player damaged by a single creature
+        // Need to rework engine logic to fire a single DamagedBatchEvent including both permanents and players
+        // Only Aisha of Sparks and Smoke is currently affected.
+        return true;
     }
 }
