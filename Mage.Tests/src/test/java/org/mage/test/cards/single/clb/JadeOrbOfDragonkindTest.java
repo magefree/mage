b8b297997b9e7003b93e374f1e0fd0d85919ea68
xenohedron <xenohedron@users.noreply.github.com>
2023-08-12 18:03:32 -0400

    adjust tests

--- Mage.Tests/src/test/java/org/mage/test/cards/single/clb/JadeOrbOfDragonkindTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/clb/JadeOrbOfDragonkindTest.java
@@ -17,88 +17,103 @@ import org.mage.test.serverside.base.CardTestPlayerBase;
 
 public class JadeOrbOfDragonkindTest extends CardTestPlayerBase {
   private static final String jadeOrb = "Jade Orb of Dragonkind";
+  private static final String arcades = "Arcades, the Strategist";
+  private static final String mowu = "Mowu, Loyal Companion";
 
   @Test
-  public void ManaCost() {
-    addCard(Zone.BATTLEFIELD, playerA, "Forest", 1);
-    addCard(Zone.BATTLEFIELD, playerA, "Mountain", 2);
-    addCard(Zone.HAND, playerA, jadeOrb);
-    castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, jadeOrb);
-    execute();
-    assertPermanentCount(playerA, 4);
-  }
-
-  @Test
-  public void ManaAbility() {
+  public void manaAbility() {
     addCard(Zone.HAND, playerA, "Ancient Bronze Dragon");
     addCard(Zone.BATTLEFIELD, playerA, "Forest", 1);
     addCard(Zone.BATTLEFIELD, playerA, "Mountain", 5);
     addCard(Zone.BATTLEFIELD, playerA, jadeOrb);
     castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Ancient Bronze Dragon");
+
+    setStrictChooseMode(true);
+    setStopAt(1, PhaseStep.BEGIN_COMBAT);
     execute();
+
     assertPermanentCount(playerA, 8);
     assertTapped(jadeOrb, true);
   }
 
   @Test
-  public void ManaUsedEffects() {
-    addCard(Zone.HAND, playerA, "Arcades, the Strategist");
+  public void manaUsedEffects() {
+    addCard(Zone.HAND, playerA, arcades);
     addCard(Zone.BATTLEFIELD, playerA, "Island", 2);
     addCard(Zone.BATTLEFIELD, playerA, "Plains", 1);
     addCard(Zone.BATTLEFIELD, playerA, jadeOrb);
-    castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Arcades, the Strategist");
-    execute();
+    addCard(Zone.BATTLEFIELD, playerA, "Kronch Wrangler");
+    // Whenever a creature with power 4 or greater enters the battlefield under your control, put a +1/+1 counter on Kronch Wrangler.
+
+    castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, arcades);
+
+    setStrictChooseMode(true);
     setStopAt(1, PhaseStep.END_COMBAT);
-    assertPermanentCount(playerA, 5);
+    execute();
+
+    assertPermanentCount(playerA, 6);
     assertTapped(jadeOrb, true);
-    assertCounterCount("Arcades, the Strategist", CounterType.P1P1, 1);
-    assertAbility(playerA, "Arcades, the Strategist", HexproofAbility.getInstance(), true);
+    assertCounterCount(arcades, CounterType.P1P1, 1);
+    assertPowerToughness(playerA, "Kronch Wrangler", 3, 2);
+    assertAbility(playerA, arcades, HexproofAbility.getInstance(), true);
   }
 
   @Test
-  public void HexproofDropOff() {
-    addCard(Zone.HAND, playerA, "Arcades, the Strategist");
+  public void hexproofDropOff() {
+    addCard(Zone.HAND, playerA, arcades);
     addCard(Zone.BATTLEFIELD, playerA, "Island", 2);
     addCard(Zone.BATTLEFIELD, playerA, "Plains", 1);
     addCard(Zone.BATTLEFIELD, playerA, jadeOrb);
-    castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Arcades, the Strategist");
-    execute();
+    castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, arcades);
+
+    setStrictChooseMode(true);
     setStopAt(1, PhaseStep.END_COMBAT);
+    execute();
+
     assertPermanentCount(playerA, 5);
     assertTapped(jadeOrb, true);
-    assertAbility(playerA, "Arcades, the Strategist", HexproofAbility.getInstance(), true);
+    assertAbility(playerA, arcades, HexproofAbility.getInstance(), true);
+
     setStopAt(4, PhaseStep.UPKEEP);
     execute();
-    assertAbility(playerA, "Arcades, the Strategist", HexproofAbility.getInstance(), false);
+
+    assertAbility(playerA, arcades, HexproofAbility.getInstance(), false);
   }
 
   @Test
-  public void TwoOrbs() {
-    addCard(Zone.HAND, playerA, "Arcades, the Strategist");
+  public void twoOrbs() {
+    addCard(Zone.HAND, playerA, arcades);
     addCard(Zone.BATTLEFIELD, playerA, "Island", 1);
     addCard(Zone.BATTLEFIELD, playerA, "Plains", 1);
     addCard(Zone.BATTLEFIELD, playerA, jadeOrb, 2);
-    castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Arcades, the Strategist");
-    execute();
+    castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, arcades);
+    //setChoice(playerA, "When you spend"); // order identical triggers
+
+    setStrictChooseMode(false); // trigger order is problematic
     setStopAt(1, PhaseStep.END_COMBAT);
+    execute();
+
     assertPermanentCount(playerA, 5);
     assertTapped(jadeOrb, true);
-    assertCounterCount("Arcades, the Strategist", CounterType.P1P1, 2);
-    assertAbility(playerA, "Arcades, the Strategist", HexproofAbility.getInstance(), true);
+    assertCounterCount(arcades, CounterType.P1P1, 2);
+    assertAbility(playerA, arcades, HexproofAbility.getInstance(), true);
   }
 
   @Test
-  public void MowuAndMaskwoodNexusCombo() {
-    addCard(Zone.HAND, playerA, "Mowu, Loyal Companion");
+  public void comboMowuAndMaskwoodNexus() {
+    addCard(Zone.HAND, playerA, mowu);
     addCard(Zone.BATTLEFIELD, playerA, "Maskwood Nexus", 1);
     addCard(Zone.BATTLEFIELD, playerA, "Forest", 2);
     addCard(Zone.BATTLEFIELD, playerA, "Plains", 1);
     addCard(Zone.BATTLEFIELD, playerA, jadeOrb);
-    castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Mowu, Loyal Companion");
-    execute();
+    castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, mowu);
+
+    setStrictChooseMode(true);
     setStopAt(1, PhaseStep.END_COMBAT);
+    execute();
+    
     assertTapped(jadeOrb, true);
-    assertCounterCount("Mowu, Loyal Companion", CounterType.P1P1, 2);
-    assertAbility(playerA, "Mowu, Loyal Companion", HexproofAbility.getInstance(), true);
+    assertCounterCount(mowu, CounterType.P1P1, 2);
+    assertAbility(playerA, mowu, HexproofAbility.getInstance(), true);
   }
 }
