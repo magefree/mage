ff65b261ccb95fa8dc68d9a19d11f83dac1da2bd
Artemis Kearney <artemis.wk@gmail.com>
2023-08-12 16:46:32 -0500

    look up cards in a way that preserves which art

--- Mage/src/main/java/mage/game/command/emblems/EmblemOfCard.java
+++ Mage/src/main/java/mage/game/command/emblems/EmblemOfCard.java
@@ -4,29 +4,58 @@ import mage.MageObject;
 import mage.abilities.AbilityImpl;
 import mage.cards.Card;
 import mage.cards.decks.DeckCardInfo;
-import mage.cards.decks.importer.CardLookup;
 import mage.cards.mock.MockCard;
+import mage.cards.repository.CardCriteria;
+import mage.cards.repository.CardInfo;
+import mage.cards.repository.CardRepository;
 import mage.constants.Zone;
 import mage.game.command.Emblem;
+import mage.util.CardUtil;
 import org.apache.log4j.Logger;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 public final class EmblemOfCard extends Emblem {
     private boolean usesVariousArt;
     private static final Logger logger = Logger.getLogger(EmblemOfCard.class);
-    public static Card cardFromDeckInfo(DeckCardInfo info) {
-        return CardLookup.instance.lookupCardInfo(info.getCardName(), info.getSetCode(), info.getCardNum())
-                .orElseThrow(() -> new IllegalArgumentException("No real card for DeckCardInfo" + info.getCardName()))
+    public static Card lookupCard(
+            String cardName,
+            String cardNumber,
+            String setCode,
+            String infoTypeForError
+    ) {
+        int cardNumberInt = CardUtil.parseCardNumberAsInt(cardNumber);
+        List<CardInfo> found = CardRepository.instance.findCards(new CardCriteria()
+                .name(cardName)
+                .minCardNumber(cardNumberInt)
+                .maxCardNumber(cardNumberInt)
+                .setCodes(setCode);
+        return found.stream()
+                .filter(ci -> ci.getCardNumber().equals(cardNumber))
+                .findFirst()
+                .orElseGet(() -> found.stream()
+                        .findFirst()
+                        .orElseThrow(() -> new IllegalArgumentException("No real card for " + infoTypeForError + " " + cardName)))
                 .getCard();
+    }
+    public static Card cardFromDeckInfo(DeckCardInfo info) {
+        return lookupCard(
+                info.getCardName(),
+                info.getCardNum(),
+                info.getSetCode(),
+                "DeckCardInfo"
+        )
     }
     public EmblemOfCard(Card card, Zone zone) {
         super(card.getName());
         if (card instanceof MockCard) {
-            MockCard mock = (MockCard)card;
-            card = CardLookup.instance.lookupCardInfo(card.getName(), card.getExpansionSetCode(), card.getCardNumber())
-                    .orElseThrow(() -> new IllegalArgumentException("No real card for mock card " + mock.getFullName(true)))
-                    .getCard();
+            card = lookupCard(
+                    card.getName(),
+                    card.getCardNumber()
+                    card.getExpansionSetCode(),
+                    "MockCard"
+            );
         }
         this.getAbilities().addAll(card.getAbilities().stream().filter(
                 ability -> zone.match(ability.getZone())
