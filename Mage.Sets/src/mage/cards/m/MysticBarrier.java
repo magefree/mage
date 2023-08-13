baef93d0ec9fc665bfb1f53e037dffa294902167
xenohedron <xenohedron@users.noreply.github.com>
2023-08-13 00:51:35 -0400

    text fix: Mystic Barrier

--- Mage.Sets/src/mage/cards/m/MysticBarrier.java
+++ Mage.Sets/src/mage/cards/m/MysticBarrier.java
@@ -3,7 +3,6 @@ package mage.cards.m;
 import mage.abilities.common.BeginningOfUpkeepTriggeredAbility;
 import mage.abilities.common.EntersBattlefieldTriggeredAbility;
 import mage.abilities.common.SimpleStaticAbility;
-import mage.abilities.effects.common.ChooseModeEffect;
 import mage.abilities.effects.common.continuous.PlayerCanOnlyAttackInDirectionRestrictionEffect;
 import mage.abilities.meta.OrTriggeredAbility;
 import mage.cards.CardImpl;
@@ -20,16 +19,11 @@ import java.util.UUID;
  */
 public final class MysticBarrier extends CardImpl {
 
-    static final String ALLOW_ATTACKING_LEFT = "Allow attacking left";
-    static final String ALLOW_ATTACKING_RIGHT = "Allow attacking right";
-
     public MysticBarrier(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{4}{W}");
 
         // When Mystic Barrier enters the battlefield or at the beginning of your upkeep, choose left or right.
-        this.addAbility(new OrTriggeredAbility(Zone.BATTLEFIELD, new ChooseModeEffect(
-                "Choose a direction to allow attacking in.",
-                ALLOW_ATTACKING_LEFT, ALLOW_ATTACKING_RIGHT),
+        this.addAbility(new OrTriggeredAbility(Zone.BATTLEFIELD, PlayerCanOnlyAttackInDirectionRestrictionEffect.choiceEffect(),
                 new EntersBattlefieldTriggeredAbility(null, false),
                 new BeginningOfUpkeepTriggeredAbility(null, TargetController.YOU, false)));
 
