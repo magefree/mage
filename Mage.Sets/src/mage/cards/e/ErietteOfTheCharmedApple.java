e6529910e856da1fbf919880b145907486d1e60b
Xanderhall <vivian.greenslade@gmail.com>
2023-08-19 18:17:18 -0230

    implemented can't attack effect

--- Mage.Sets/src/mage/cards/e/ErietteOfTheCharmedApple.java
+++ Mage.Sets/src/mage/cards/e/ErietteOfTheCharmedApple.java
@@ -1,26 +1,31 @@
 package mage.cards.e;
 
-import java.util.Set;
+import java.util.Objects;
 import java.util.UUID;
 import mage.MageInt;
 import mage.abilities.Ability;
-import mage.abilities.TriggeredAbilityImpl;
 import mage.abilities.common.BeginningOfEndStepTriggeredAbility;
+import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.dynamicvalue.DynamicValue;
+import mage.abilities.effects.ContinuousEffectImpl;
 import mage.abilities.effects.Effect;
-import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.GainLifeEffect;
 import mage.abilities.effects.common.LoseLifeOpponentsEffect;
+import mage.abilities.effects.common.combat.CantAttackYouAllEffect;
 import mage.constants.SubType;
 import mage.constants.SuperType;
 import mage.constants.TargetController;
+import mage.constants.Zone;
 import mage.filter.FilterPermanent;
 import mage.filter.StaticFilters;
+import mage.filter.predicate.permanent.ControllerIdPredicate;
 import mage.game.Game;
-import mage.watchers.common.PlayerLostLifeWatcher;
+import mage.game.permanent.Permanent;
+import mage.target.targetpointer.FixedTarget;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.constants.Duration;
 import mage.constants.Outcome;
 
 /**
@@ -39,11 +44,12 @@ public final class ErietteOfTheCharmedApple extends CardImpl {
         this.toughness = new MageInt(4);
 
         // Each creature that's enchanted by an Aura you control can't attack you or planeswalkers you control.
-        
+        this.addAbility(new SimpleStaticAbility(Zone.BATTLEFIELD, new ErietteOfTheCharmedAppleEffect()));
 
         // At the beginning of your end step, each opponent loses X life and you gain X life, where X is the number of Auras you control.
         Ability ability = new BeginningOfEndStepTriggeredAbility(new LoseLifeOpponentsEffect(ErietteOfTheCharmedAppleValue.instance), TargetController.YOU, false);
         ability.addEffect(new GainLifeEffect(ErietteOfTheCharmedAppleValue.instance));
+        this.addAbility(ability);
     }
 
     private ErietteOfTheCharmedApple(final ErietteOfTheCharmedApple card) {
@@ -56,19 +62,57 @@ public final class ErietteOfTheCharmedApple extends CardImpl {
     }
 }
 
+class ErietteOfTheCharmedAppleEffect extends ContinuousEffectImpl {
+
+    ErietteOfTheCharmedAppleEffect() {
+        super(Duration.WhileOnBattlefield, Outcome.Benefit);
+        this.staticText = "Each creature that's enchanted by an Aura you control can't attack you or planeswalkers you control.";
+    }
+
+    private ErietteOfTheCharmedAppleEffect(final ErietteOfTheCharmedAppleEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public ErietteOfTheCharmedAppleEffect copy() {
+        return new ErietteOfTheCharmedAppleEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        // Implementation taken from Kaima, The Fractured Calm
+        for (Permanent permanent : game.getBattlefield().getActivePermanents(StaticFilters.FILTER_PERMANENT_CREATURE, source.getControllerId(), source, game)) {
+            if (permanent.getAttachments()
+                    .stream()
+                    .map(game::getPermanent)
+                    .filter(Objects::nonNull)
+                    .noneMatch(p -> p.isControlledBy(source.getControllerId()) && p.hasSubtype(SubType.AURA, game))
+            ) {
+                continue;
+            }
+            game.addEffect(new CantAttackYouAllEffect(Duration.Custom).setTargetPointer(new FixedTarget(permanent, game)), source);
+        }
+
+        return true;
+    }
+
+}
+
 enum ErietteOfTheCharmedAppleValue implements DynamicValue {
     instance;
 
-    private FilterPermanent filter = new FilterPermanent("Auras you control");
+    private static FilterPermanent filter = new FilterPermanent("Auras you control");
 
-    {
+    static {
         filter.add(CardType.ENCHANTMENT.getPredicate());
         filter.add(SubType.AURA.getPredicate());
     }
 
     @Override
     public int calculate(Game game, Ability sourceAbility, Effect effect) {
-        return game.getBattlefield().countAll(filter, sourceAbility.getControllerId(), game);
+        FilterPermanent controllerFilter = filter.copy();
+        controllerFilter.add(new ControllerIdPredicate(sourceAbility.getControllerId()));
+        return game.getBattlefield().count(controllerFilter, sourceAbility.getControllerId(), sourceAbility, game);
     }
 
     @Override
@@ -78,11 +122,6 @@ enum ErietteOfTheCharmedAppleValue implements DynamicValue {
 
     @Override
     public String getMessage() {
-        return "the amount of life they lost this turn";
-    }
-
-    @Override
-    public String toString() {
-        return "1";
+        return "the amount of Auras you control";
     }
 }
