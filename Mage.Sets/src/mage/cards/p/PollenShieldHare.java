7c10b4ae7da72462c3074ea4074374b61d7464ff
Susucre <34709007+Susucre@users.noreply.github.com>
2023-08-22 06:51:41 +0200

    apply review

--- Mage.Sets/src/mage/cards/p/PollenShieldHare.java
+++ Mage.Sets/src/mage/cards/p/PollenShieldHare.java
@@ -3,20 +3,18 @@ package mage.cards.p;
 
 import mage.MageInt;
 import mage.abilities.common.SimpleStaticAbility;
-import mage.abilities.dynamicvalue.DynamicValue;
-import mage.abilities.dynamicvalue.common.PermanentsOnBattlefieldCount;
-import mage.abilities.effects.common.continuous.BoostAllEffect;
+import mage.abilities.dynamicvalue.common.CreaturesYouControlCount;
+import mage.abilities.effects.common.continuous.BoostControlledEffect;
 import mage.abilities.effects.common.continuous.BoostTargetEffect;
 import mage.abilities.effects.common.continuous.GainAbilityTargetEffect;
-import mage.abilities.hint.Hint;
-import mage.abilities.hint.ValueHint;
+import mage.abilities.hint.common.CreaturesYouControlHint;
 import mage.abilities.keyword.VigilanceAbility;
 import mage.cards.AdventureCard;
 import mage.cards.CardSetInfo;
-import mage.constants.*;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.SubType;
 import mage.filter.StaticFilters;
-import mage.filter.common.FilterCreaturePermanent;
-import mage.filter.predicate.permanent.TokenPredicate;
 import mage.target.common.TargetControlledCreaturePermanent;
 
 import java.util.UUID;
@@ -25,18 +23,7 @@ import java.util.UUID;
  * @author Susucr
  */
 public final class PollenShieldHare extends AdventureCard {
-
-    private static final DynamicValue xValue = new PermanentsOnBattlefieldCount(StaticFilters.FILTER_CONTROLLED_CREATURES);
-
-    private static final Hint hint = new ValueHint("Number of controlled creatures", xValue);
-
-    private static final FilterCreaturePermanent filter = new FilterCreaturePermanent("Creature tokens you control");
-
-    static {
-        filter.add(TokenPredicate.TRUE);
-        filter.add(TargetController.YOU.getControllerPredicate());
-    }
-
+    
     public PollenShieldHare(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, new CardType[]{CardType.SORCERY}, "{1}{W}", "Hare Raising", "{G}");
         this.subtype.add(SubType.RABBIT);
@@ -46,21 +33,21 @@ public final class PollenShieldHare extends AdventureCard {
 
         // Creature tokens you control get +1/+1.
         this.addAbility(new SimpleStaticAbility(
-                new BoostAllEffect(1, 1, Duration.WhileOnBattlefield, filter, false)
+                new BoostControlledEffect(1, 1, Duration.WhileOnBattlefield, StaticFilters.FILTER_CREATURE_TOKENS, false)
         ));
 
         // Hare Raising
         // Target creature you control gains vigilance and gets +X/+X until end of turn, where X is the number of creatures you control.
         this.getSpellCard().getSpellAbility().addEffect(
                 new GainAbilityTargetEffect(VigilanceAbility.getInstance())
-                        .setText("Target creature you control gains vigilance")
+                        .setText("target creature you control gains vigilance")
         );
         this.getSpellCard().getSpellAbility().addEffect(
-                new BoostTargetEffect(xValue, xValue)
-                        .setText(" and gets +X/+X until end of turn, where X is the number of creatures you control.")
+                new BoostTargetEffect(CreaturesYouControlCount.instance, CreaturesYouControlCount.instance)
+                        .setText("and gets +X/+X until end of turn, where X is the number of creatures you control")
         );
         this.getSpellCard().getSpellAbility().addTarget(new TargetControlledCreaturePermanent());
-        this.getSpellCard().getSpellAbility().addHint(hint);
+        this.getSpellCard().getSpellAbility().addHint(CreaturesYouControlHint.instance);
     }
 
     private PollenShieldHare(final PollenShieldHare card) {
