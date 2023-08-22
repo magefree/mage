86195038da3f92a5d1f8bf0b444102670604b9b6
Susucre <34709007+Susucre@users.noreply.github.com>
2023-08-22 15:29:06 +0200

    use OrTriggeredAbility

--- Mage.Sets/src/mage/cards/u/UpTheBeanstalk.java
+++ Mage.Sets/src/mage/cards/u/UpTheBeanstalk.java
@@ -1,7 +1,9 @@
 package mage.cards.u;
 
-import mage.abilities.TriggeredAbilityImpl;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.common.SpellCastControllerTriggeredAbility;
 import mage.abilities.effects.common.DrawCardSourceControllerEffect;
+import mage.abilities.meta.OrTriggeredAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
@@ -9,23 +11,30 @@ import mage.constants.ComparisonType;
 import mage.constants.Zone;
 import mage.filter.FilterSpell;
 import mage.filter.predicate.mageobject.ManaValuePredicate;
-import mage.game.Game;
-import mage.game.events.GameEvent;
-import mage.game.stack.Spell;
 
 import java.util.UUID;
 
 /**
- *
  * @author Susucr
  */
 public final class UpTheBeanstalk extends CardImpl {
+
+    private static FilterSpell filter = new FilterSpell("spell with mana value 5 or greater");
+
+    static {
+        filter.add(new ManaValuePredicate(ComparisonType.OR_GREATER, 5));
+    }
 
     public UpTheBeanstalk(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{1}{G}");
 
         // When Up the Beanstalk enters the battlefield and whenever you cast a spell with mana value 5 or greater, draw a card.
-        this.addAbility(new UpTheBeanstalkTriggeredAbility());
+        this.addAbility(new OrTriggeredAbility(
+                Zone.BATTLEFIELD, new DrawCardSourceControllerEffect(1), false,
+                "When {this} enters the battlefield and whenever you cast a spell with mana value 5 or greater, ",
+                new EntersBattlefieldTriggeredAbility(null),
+                new SpellCastControllerTriggeredAbility(null, filter, false)
+        ));
     }
 
     private UpTheBeanstalk(final UpTheBeanstalk card) {
@@ -35,50 +44,5 @@ public final class UpTheBeanstalk extends CardImpl {
     @Override
     public UpTheBeanstalk copy() {
         return new UpTheBeanstalk(this);
-    }
-}
-
-class UpTheBeanstalkTriggeredAbility extends TriggeredAbilityImpl {
-
-    private static FilterSpell filter = new FilterSpell("spell with mana value 5 or greater");
-
-    static {
-        filter.add(new ManaValuePredicate(ComparisonType.OR_GREATER, 5));
-    }
-
-    UpTheBeanstalkTriggeredAbility() {
-        super(Zone.BATTLEFIELD, new DrawCardSourceControllerEffect(1));
-        setTriggerPhrase("When {this} enters the battlefield and whenever you cast a spell with mana value 5 or greater, ");
-    }
-
-    private UpTheBeanstalkTriggeredAbility(final UpTheBeanstalkTriggeredAbility ability) {
-        super(ability);
-    }
-
-    @Override
-    public boolean checkEventType(GameEvent event, Game game) {
-        return event.getType() == GameEvent.EventType.SPELL_CAST
-                || event.getType() == GameEvent.EventType.ENTERS_THE_BATTLEFIELD;
-    }
-
-    @Override
-    public boolean checkTrigger(GameEvent event, Game game) {
-        switch (event.getType()) {
-            case SPELL_CAST:
-                if (event.getPlayerId().equals(controllerId)) {
-                    Spell spell = game.getStack().getSpell(event.getTargetId());
-                    return spell != null && filter.match(spell, controllerId, this, game);
-                }
-                return false;
-            case ENTERS_THE_BATTLEFIELD:
-                return event.getTargetId().equals(getSourceId());
-            default:
-                return false;
-        }
-    }
-
-    @Override
-    public UpTheBeanstalkTriggeredAbility copy() {
-        return new UpTheBeanstalkTriggeredAbility(this);
     }
 }
