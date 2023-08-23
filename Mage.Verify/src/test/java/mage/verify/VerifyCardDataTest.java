8e240518a987b113bf57ca36af6b5c2fb3932f39
theelk801 <theelk801@gmail.com>
2023-08-22 20:13:15 -0400

    fix verify failures

--- Mage.Verify/src/test/java/mage/verify/VerifyCardDataTest.java
+++ Mage.Verify/src/test/java/mage/verify/VerifyCardDataTest.java
@@ -150,6 +150,9 @@ public class VerifyCardDataTest {
         // rarity
         skipListCreate(SKIP_LIST_RARITY);
         skipListAddName(SKIP_LIST_RARITY, "CMR", "The Prismatic Piper"); // Collation is not yet set up for CMR https://www.lethe.xyz/mtg/collation/cmr.html
+        skipListAddName(SKIP_LIST_RARITY, "WOE", "Picklock Prankster"); // temporary
+        skipListAddName(SKIP_LIST_RARITY, "WOE", "Free the Fae"); // temporary
+        skipListAddName(SKIP_LIST_RARITY, "WOE", "Up the Beanstalk"); // temporary
 
         // missing abilities
         skipListCreate(SKIP_LIST_MISSING_ABILITIES);
