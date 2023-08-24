b3196da4fe04a80d2b88a939adef128d50dce988
theelk801 <theelk801@gmail.com>
2023-08-24 18:15:17 -0400

    [WOE] Implement Savior of the Sleeping

--- Mage.Sets/src/mage/sets/WildsOfEldraine.java
+++ Mage.Sets/src/mage/sets/WildsOfEldraine.java
@@ -173,6 +173,7 @@ public final class WildsOfEldraine extends ExpansionSet {
         cards.add(new SetCardInfo("Rowdy Research", 312, Rarity.UNCOMMON, mage.cards.r.RowdyResearch.class));
         cards.add(new SetCardInfo("Royal Treatment", 183, Rarity.UNCOMMON, mage.cards.r.RoyalTreatment.class));
         cards.add(new SetCardInfo("Ruby, Daring Tracker", 212, Rarity.UNCOMMON, mage.cards.r.RubyDaringTracker.class));
+        cards.add(new SetCardInfo("Savior of the Sleeping", 28, Rarity.COMMON, mage.cards.s.SaviorOfTheSleeping.class));
         cards.add(new SetCardInfo("Scalding Viper", 235, Rarity.RARE, mage.cards.s.ScaldingViper.class));
         cards.add(new SetCardInfo("Scream Puff", 105, Rarity.COMMON, mage.cards.s.ScreamPuff.class));
         cards.add(new SetCardInfo("Sentinel of Lost Lore", 184, Rarity.RARE, mage.cards.s.SentinelOfLostLore.class));
