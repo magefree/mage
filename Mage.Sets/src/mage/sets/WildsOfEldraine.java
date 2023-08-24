6fd9a2c359d7583299f84d392581796265e0a37e
theelk801 <theelk801@gmail.com>
2023-08-24 18:09:33 -0400

    [WOE] Implement Vantress Transmuter

--- Mage.Sets/src/mage/sets/WildsOfEldraine.java
+++ Mage.Sets/src/mage/sets/WildsOfEldraine.java
@@ -228,6 +228,7 @@ public final class WildsOfEldraine extends ExpansionSet {
         cards.add(new SetCardInfo("Unassuming Sage", 37, Rarity.COMMON, mage.cards.u.UnassumingSage.class));
         cards.add(new SetCardInfo("Unruly Catapult", 156, Rarity.COMMON, mage.cards.u.UnrulyCatapult.class));
         cards.add(new SetCardInfo("Up the Beanstalk", 195, Rarity.UNCOMMON, mage.cards.u.UpTheBeanstalk.class));
+        cards.add(new SetCardInfo("Vantress Transmuter", 75, Rarity.COMMON, mage.cards.v.VantressTransmuter.class));
         cards.add(new SetCardInfo("Virtue of Knowledge", 76, Rarity.MYTHIC, mage.cards.v.VirtueOfKnowledge.class));
         cards.add(new SetCardInfo("Virtue of Loyalty", 38, Rarity.MYTHIC, mage.cards.v.VirtueOfLoyalty.class));
         cards.add(new SetCardInfo("Virtue of Persistence", 115, Rarity.MYTHIC, mage.cards.v.VirtueOfPersistence.class));
