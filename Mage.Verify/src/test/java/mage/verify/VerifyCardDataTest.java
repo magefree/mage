1f17cc9d72e7ee74e451f86a01447a21377ea8ec
theelk801 <theelk801@gmail.com>
2023-08-24 18:47:14 -0400

    remove verify skips

--- Mage.Verify/src/test/java/mage/verify/VerifyCardDataTest.java
+++ Mage.Verify/src/test/java/mage/verify/VerifyCardDataTest.java
@@ -124,7 +124,6 @@ public class VerifyCardDataTest {
 
         // cost
         // skipListAddName(SKIP_LIST_COST, set, cardName);
-        skipListAddName(SKIP_LIST_COST, "WOE", "Scream Puff"); //temporary
 
         // supertype
         // skipListAddName(SKIP_LIST_SUPERTYPE, set, cardName);
@@ -134,16 +133,12 @@ public class VerifyCardDataTest {
         skipListAddName(SKIP_LIST_TYPE, "UNH", "Old Fogey"); // uses summon word as a joke card
         skipListAddName(SKIP_LIST_TYPE, "UND", "Old Fogey");
         skipListAddName(SKIP_LIST_TYPE, "UST", "capital offense"); // uses "instant" instead "Instant" as a joke card
-        skipListAddName(SKIP_LIST_TYPE, "WOE", "Scream Puff"); //temporary
-        skipListAddName(SKIP_LIST_TYPE, "WOE", "Redtooth Vanguard"); //temporary
 
         // subtype
         // skipListAddName(SKIP_LIST_SUBTYPE, set, cardName);
         skipListAddName(SKIP_LIST_SUBTYPE, "UGL", "Miss Demeanor"); // uses multiple types as a joke card: Lady, of, Proper, Etiquette
         skipListAddName(SKIP_LIST_SUBTYPE, "UGL", "Elvish Impersonators"); // subtype is "Elves" pun
         skipListAddName(SKIP_LIST_SUBTYPE, "UND", "Elvish Impersonators");
-        skipListAddName(SKIP_LIST_SUBTYPE, "WOE", "Scream Puff"); //temporary
-        skipListAddName(SKIP_LIST_SUBTYPE, "WOE", "Redtooth Vanguard"); //temporary
 
         // number
         // skipListAddName(SKIP_LIST_NUMBER, set, cardName);
@@ -151,7 +146,6 @@ public class VerifyCardDataTest {
         // rarity
         // skipListAddName(SKIP_LIST_RARITY, set, cardName);
         skipListAddName(SKIP_LIST_RARITY, "CMR", "The Prismatic Piper"); // Collation is not yet set up for CMR https://www.lethe.xyz/mtg/collation/cmr.html
-        skipListAddName(SKIP_LIST_RARITY, "WOE", "Eerie Interference"); // temporary
 
         // missing abilities
         // skipListAddName(SKIP_LIST_MISSING_ABILITIES, set, cardName);
