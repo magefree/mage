a6c606f4b563b7708f41de063c6d1aa5d21d1178
theelk801 <theelk801@gmail.com>
2023-08-24 21:37:11 -0400

    fix verify failure

--- Mage.Verify/src/test/java/mage/verify/VerifyCardDataTest.java
+++ Mage.Verify/src/test/java/mage/verify/VerifyCardDataTest.java
@@ -139,6 +139,7 @@ public class VerifyCardDataTest {
         skipListAddName(SKIP_LIST_SUBTYPE, "UGL", "Miss Demeanor"); // uses multiple types as a joke card: Lady, of, Proper, Etiquette
         skipListAddName(SKIP_LIST_SUBTYPE, "UGL", "Elvish Impersonators"); // subtype is "Elves" pun
         skipListAddName(SKIP_LIST_SUBTYPE, "UND", "Elvish Impersonators");
+        skipListAddName(SKIP_LIST_SUBTYPE, "UND", "Hare Raising");
 
         // number
         // skipListAddName(SKIP_LIST_NUMBER, set, cardName);
