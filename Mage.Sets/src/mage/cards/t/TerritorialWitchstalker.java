afc275ae642a8d658b5225ca0f619692ef0a7f06
Vivian Greenslade <vivian.greenslade@gmail.com>
2023-08-25 12:31:45 -0230

    Update TerritorialWitchstalker.java
    fixed text

--- Mage.Sets/src/mage/cards/t/TerritorialWitchstalker.java
+++ Mage.Sets/src/mage/cards/t/TerritorialWitchstalker.java
@@ -34,7 +34,7 @@ public final class TerritorialWitchstalker extends CardImpl {
         this.addAbility(DefenderAbility.getInstance());
 
         // At the beginning of combat on your turn, if you control a creature with power 4 or greater, Territorial Witchstalker gets +1/+0 until end of turn and can attack this turn as though it didn't have defender.
-        Ability ability = new BeginningOfCombatTriggeredAbility(new ConditionalContinuousEffect(new BoostSourceEffect(1, 0, Duration.EndOfTurn), FerociousCondition.instance, "if you control a creature with power 4 or greater, Territorial Witchstalker gets +1/+0 until end of turn"), TargetController.YOU, false);
+        Ability ability = new BeginningOfCombatTriggeredAbility(new ConditionalContinuousEffect(new BoostSourceEffect(1, 0, Duration.EndOfTurn), FerociousCondition.instance, "if you control a creature with power 4 or greater, {this} gets +1/+0 until end of turn"), TargetController.YOU, false);
         ability.addEffect(new ConditionalAsThoughEffect(new CanAttackAsThoughItDidntHaveDefenderSourceEffect(Duration.EndOfTurn), FerociousCondition.instance).setText("and can attack this turn as though it didn't have defender"));
         this.addAbility(ability);
 
