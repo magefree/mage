2b3cc274c9ca290773e7965fdff83285d89845d9
Steven Knipe <knipesteven@gmail.com>
2023-08-25 20:50:06 -0700

    Add Override

--- Mage.Tests/src/test/java/org/mage/test/mulligan/SmoothedLondonMulliganTest.java
+++ Mage.Tests/src/test/java/org/mage/test/mulligan/SmoothedLondonMulliganTest.java
@@ -2,9 +2,11 @@ package org.mage.test.mulligan;
 import mage.game.mulligan.MulliganType;
 
 public class SmoothedLondonMulliganTest extends LondonMulliganTest {
+    @Override
     protected MulliganType getMullType() {
         return MulliganType.SMOOTHED_LONDON;
     }
+    @Override
     protected int getCardsPerMull() {
         return 14;
     }
