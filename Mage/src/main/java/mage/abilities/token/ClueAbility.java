3aff7e56ab4dc749e066abc6ae45783bfd727b70
Xanderhall <vivian.greenslade@gmail.com>
2023-08-29 13:50:13 -0230

    fix clue

--- Mage/src/main/java/mage/abilities/token/ClueAbility.java
+++ Mage/src/main/java/mage/abilities/token/ClueAbility.java
@@ -2,7 +2,6 @@ package mage.abilities.token;
 
 import mage.abilities.ActivatedAbilityImpl;
 import mage.abilities.costs.common.SacrificeSourceCost;
-import mage.abilities.costs.common.TapSourceCost;
 import mage.abilities.costs.mana.GenericManaCost;
 import mage.abilities.effects.common.DrawCardSourceControllerEffect;
 import mage.constants.Zone;
@@ -11,8 +10,7 @@ public class ClueAbility extends ActivatedAbilityImpl {
     
     public ClueAbility(boolean named) {
         super(Zone.BATTLEFIELD, new DrawCardSourceControllerEffect(1), new GenericManaCost(2));
-        // {2}, {T}, Sacrifice this artifact: You draw a card.”
-        this.addCost(new TapSourceCost());
+        // {2}, Sacrifice this artifact: You draw a card.”
         this.addCost(new SacrificeSourceCost().setText("sacrifice " + (named ? "{this}" : "this artifact")));
     }
 
