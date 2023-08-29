2cd4db97d7d3f53c941376feab25a6058f9c6e82
Xanderhall <vivian.greenslade@gmail.com>
2023-08-29 12:25:29 -0230

    fixed text issues, added hint

--- Mage.Sets/src/mage/cards/t/TerritorialWitchstalker.java
+++ Mage.Sets/src/mage/cards/t/TerritorialWitchstalker.java
@@ -9,6 +9,7 @@ import mage.abilities.condition.common.FerociousCondition;
 import mage.abilities.decorator.ConditionalInterveningIfTriggeredAbility;
 import mage.abilities.effects.common.combat.CanAttackAsThoughItDidntHaveDefenderSourceEffect;
 import mage.abilities.effects.common.continuous.BoostSourceEffect;
+import mage.abilities.hint.common.FerociousHint;
 import mage.abilities.keyword.DefenderAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
@@ -36,12 +37,12 @@ public final class TerritorialWitchstalker extends CardImpl {
         // At the beginning of combat on your turn, if you control a creature with power 4 or greater, Territorial Witchstalker gets +1/+0 until end of turn and can attack this turn as though it didn't have defender.
         TriggeredAbility ability = new BeginningOfCombatTriggeredAbility(new BoostSourceEffect(1, 0, Duration.EndOfTurn), TargetController.YOU, false);
         ability.addEffect(new CanAttackAsThoughItDidntHaveDefenderSourceEffect(Duration.EndOfTurn));
-        
+        ability.addHint(FerociousHint.instance);
         this.addAbility(new ConditionalInterveningIfTriggeredAbility(
             ability, 
             FerociousCondition.instance, 
             "At the beginning of combat on your turn, if you control a creature with power 4 or greater, "
-            + "Territorial Witchstalker gets +1/+0 until end of turn and can attack this turn as though it didn't have defender"
+            + "{this} gets +1/+0 until end of turn and can attack this turn as though it didn't have defender"
         ));
 
     }
