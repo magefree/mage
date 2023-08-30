ef01f4ba8fcbb47591d65abde1f4c8744f566424
Xanderhall <vivian.greenslade@gmail.com>
2023-08-30 19:14:38 -0230

    adds unit test

---
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/woc/KorvoldGleefulGluttonTest.java
@@ -0,0 +1,54 @@
+package org.mage.test.cards.single.woc;
+
+import org.junit.Test;
+import org.mage.test.serverside.base.CardTestPlayerBase;
+
+import mage.constants.PhaseStep;
+import mage.constants.Zone;
+import mage.counters.CounterType;
+
+public class KorvoldGleefulGluttonTest extends CardTestPlayerBase {
+    
+    /** 
+        This spell costs {1} less to cast for each card type among permanents you’ve sacrificed this turn.
+        Flying, trample, haste
+        Whenever Korvold deals combat damage to a player, put X +1/+1 counters on Korvold and draw X cards, where X is the number of permanent types among cards in your graveyard.
+     */
+    private final String KORVOLD = "Korvold, Gleeful Glutton";
+    private final String OVEN = "Witch's Oven";
+    private final String CAT = "Cauldron Familiar";
+    private final String DOG = "Spirited Companion";
+    private final String LAND = "Archway Commons";
+
+    private void setup() {
+        addCard(Zone.HAND, playerA, KORVOLD);
+        addCard(Zone.BATTLEFIELD, playerA, OVEN);
+        addCard(Zone.BATTLEFIELD, playerA, DOG);
+        addCard(Zone.BATTLEFIELD, playerA, LAND, 5, false);
+        addCard(Zone.GRAVEYARD, playerA, CAT);
+    }
+
+    @Test
+    public void testEffects() {
+        setup();
+
+        // Sacrifice dog to oven
+        activateAbility(1, PhaseStep.PRECOMBAT_MAIN, playerA, "{T}, Sacrifice");
+
+        // Sacrifice food to cat
+        activateAbility(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Sacrifice a Food");
+
+        // Korvold should be reduced by 3 (enchantment, creature, artifact sacrificed)
+        castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, KORVOLD);
+
+        // Korvold attacks, effect should trigger for X=2 because of dog
+        attack(1, playerA, KORVOLD);
+
+        setStopAt(1, PhaseStep.POSTCOMBAT_MAIN);
+        setStrictChooseMode(true);
+
+        assertCounterCount(KORVOLD, CounterType.P1P1, 2);
+        // Account for initial draw plus Korvold
+        assertHandCount(playerA, 1 + 2);
+    }
+}
