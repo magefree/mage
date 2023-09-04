025ebee0261314f44a72854de36f32e9ef3aca40
ssk97 <knipesteven@gmail.com>
2023-09-03 21:33:50 -0700

    [ALA] Sacellum Godspeaker - fixed wrong reveal ability, refactor related cards (#11102)
    * Sacellum Godspeaker needs to reveal the cards
    * Use CardsImpl constructor rather than looping through all cards
    * Remove unneeded check from Arsenal Thresher
    * added source to Phosphorescent Feast's reveal
    * Add null check for controller

--- Mage.Sets/src/mage/cards/s/SacellumGodspeaker.java
+++ Mage.Sets/src/mage/cards/s/SacellumGodspeaker.java
@@ -8,6 +8,7 @@ import mage.abilities.effects.mana.ManaEffect;
 import mage.abilities.mana.SimpleManaAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
+import mage.cards.CardsImpl;
 import mage.constants.*;
 import mage.filter.common.FilterCreatureCard;
 import mage.filter.predicate.mageobject.PowerPredicate;
@@ -88,9 +89,13 @@ class SacellumGodspeakerEffect extends ManaEffect {
     @Override
     public Mana produceMana(Game game, Ability source) {
         if (game != null) {
-            TargetCardInHand target = new TargetCardInHand(0, Integer.MAX_VALUE, filter);
-            if (target.choose(Outcome.Benefit, source.getControllerId(), source.getSourceId(), source, game)) {
-                return Mana.GreenMana(target.getTargets().size());
+            Player controller = game.getPlayer(source.getControllerId());
+            if (controller != null) {
+                TargetCardInHand target = new TargetCardInHand(0, Integer.MAX_VALUE, filter);
+                if (target.choose(Outcome.Benefit, source.getControllerId(), source.getSourceId(), source, game)) {
+                    controller.revealCards(source, new CardsImpl(target.getTargets()), game);
+                    return Mana.GreenMana(target.getTargets().size());
+                }
             }
         }
         return new Mana();
