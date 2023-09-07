d82d58f389e209c1b3343191c956cfd7a433049f
Xanderhall <vivian.greenslade@gmail.com>
2023-09-07 18:22:04 -0230

    added unit test

---
+++ Mage.Tests/src/test/java/org/mage/test/cards/targets/attacking/RedirectAttackerTargetEffectTest.java
@@ -0,0 +1,46 @@
+package org.mage.test.cards.targets.attacking;
+
+import org.junit.Test;
+import org.mage.test.serverside.base.CardTestCommander4Players;
+
+import mage.constants.PhaseStep;
+import mage.constants.Zone;
+import mage.counters.CounterType;
+
+public class RedirectAttackerTargetEffectTest extends CardTestCommander4Players {
+    
+    private static final String SIGNPOST = "Misleading Signpost";
+    
+    private static final String LION = "Silvercoat Lion";
+    private static final String PLANESWALKER = "Teferi, Master of Time";
+    private static final String BATTLE = "Invasion of Segovia";
+
+    /**
+     * When Misleading Signpost enters the battlefield during the declare attackers step, you may reselect which player or permanent target attacking creature is attacking.
+     */
+    @Test
+    public void testSingleTarget() {
+        addCard(Zone.BATTLEFIELD, playerA, LION);
+
+        addCard(Zone.BATTLEFIELD, playerB, "Island", 3);
+        addCard(Zone.HAND, playerB, SIGNPOST);
+
+        addCard(Zone.BATTLEFIELD, playerC, PLANESWALKER);
+
+        // Player A declares an attack against Player B
+        attack(1, playerA, LION, playerB);
+
+        // Player B responds by casting Signpost, redirecting the attack to Player C's planeswalker
+        castSpell(1, PhaseStep.DECLARE_ATTACKERS, playerB, SIGNPOST);
+        setChoice(playerB, true);
+        addTarget(playerB, LION);
+        addTarget(playerB, PLANESWALKER);
+
+        setStrictChooseMode(true);
+        setStopAt(1, PhaseStep.POSTCOMBAT_MAIN);
+        execute();
+
+        assertCounterCount(PLANESWALKER, CounterType.LOYALTY, 1);
+        assertLife(playerB, 20);
+    }
+}
