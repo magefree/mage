aaef379a3e40fd0c615df6330a6ec6837a3ee0d6
Xanderhall <vivian.greenslade@gmail.com>
2023-09-07 22:40:24 -0230

    added battle to test

--- Mage.Tests/src/test/java/org/mage/test/cards/targets/attacking/RedirectAttackerTargetEffectTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/targets/attacking/RedirectAttackerTargetEffectTest.java
@@ -10,7 +10,7 @@ import mage.counters.CounterType;
 public class RedirectAttackerTargetEffectTest extends CardTestCommander4Players {
     
     private static final String SIGNPOST = "Misleading Signpost";
-    
+    private static final String MAGE = "Portal Mage";
     private static final String LION = "Silvercoat Lion";
     private static final String PLANESWALKER = "Teferi, Master of Time";
     private static final String BATTLE = "Invasion of Segovia";
@@ -41,6 +41,34 @@ public class RedirectAttackerTargetEffectTest extends CardTestCommander4Players 
         execute();
 
         assertCounterCount(PLANESWALKER, CounterType.LOYALTY, 1);
+        assertLife(playerB, 20);
+    }
+
+    @Test
+    public void testBattle() {
+        addCard(Zone.BATTLEFIELD, playerA, LION);
+        addCard(Zone.BATTLEFIELD, playerA, "Island", 3);
+        addCard(Zone.HAND, playerA, BATTLE);
+
+        addCard(Zone.BATTLEFIELD, playerB, "Island", 3);
+        addCard(Zone.HAND, playerB, SIGNPOST);
+        addCard(Zone.BATTLEFIELD, playerB, BATTLE);
+        setChoice(playerB, "PlayerC");
+
+        // Player A declares an attack against Player B
+        attack(1, playerA, LION, playerB);
+
+        // Player B responds by casting Signpost, redirecting the attack to Player C's planeswalker
+        castSpell(1, PhaseStep.DECLARE_ATTACKERS, playerB, SIGNPOST);
+        setChoice(playerB, true);
+        addTarget(playerB, LION);
+        addTarget(playerB, BATTLE);
+
+        setStrictChooseMode(true);
+        setStopAt(1, PhaseStep.POSTCOMBAT_MAIN);
+        execute();
+
+        assertCounterCount(BATTLE, CounterType.DEFENSE, 2);
         assertLife(playerB, 20);
     }
 }
