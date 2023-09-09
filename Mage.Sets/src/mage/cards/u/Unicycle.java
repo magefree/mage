7a0c62a30e5b44173afc5a3fc1c53ee463e1a030
Susucre <34709007+Susucre@users.noreply.github.com>
2023-09-09 21:06:00 +0200

    clean imports

--- Mage.Sets/src/mage/cards/u/Unicycle.java
+++ Mage.Sets/src/mage/cards/u/Unicycle.java
@@ -6,16 +6,16 @@ import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.effects.Effect;
-import mage.abilities.effects.common.ExileTargetEffect;
 import mage.abilities.effects.common.continuous.GainAbilityAttachedEffect;
-import mage.abilities.keyword.*;
+import mage.abilities.keyword.CrewAbility;
+import mage.abilities.keyword.EquipAbility;
+import mage.abilities.keyword.FirstStrikeAbility;
+import mage.abilities.keyword.HasteAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.AttachmentType;
 import mage.constants.CardType;
 import mage.constants.SubType;
-import mage.constants.Zone;
-import mage.target.common.TargetCreaturePermanent;
 
 import java.util.UUID;
 
