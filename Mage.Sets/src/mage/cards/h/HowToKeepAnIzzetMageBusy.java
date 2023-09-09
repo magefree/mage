35dd76b203ec22e4e9bdc1046d38302f507bf785
Susucre <34709007+Susucre@users.noreply.github.com>
2023-09-09 09:27:13 +0200

    [CMB1] Implement How to Keep an Izzet Mage Busy

---
+++ Mage.Sets/src/mage/cards/h/HowToKeepAnIzzetMageBusy.java
@@ -0,0 +1,35 @@
+
+package mage.cards.h;
+
+import mage.abilities.effects.common.ReturnToHandSourceEffect;
+import mage.abilities.effects.common.continuous.LoseAllAbilitiesTargetEffect;
+import mage.abilities.effects.common.continuous.SetBasePowerToughnessTargetEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.target.common.TargetCreaturePermanent;
+
+import java.util.UUID;
+
+/**
+ * @author Susucr
+ */
+public final class HowToKeepAnIzzetMageBusy extends CardImpl {
+
+    public HowToKeepAnIzzetMageBusy(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{U/R}");
+
+        // Return How to Keep an Izzet Mage Busy to its owner's hand.
+        this.getSpellAbility().addEffect(new ReturnToHandSourceEffect());
+    }
+
+    private HowToKeepAnIzzetMageBusy(final HowToKeepAnIzzetMageBusy card) {
+        super(card);
+    }
+
+    @Override
+    public HowToKeepAnIzzetMageBusy copy() {
+        return new HowToKeepAnIzzetMageBusy(this);
+    }
+}
