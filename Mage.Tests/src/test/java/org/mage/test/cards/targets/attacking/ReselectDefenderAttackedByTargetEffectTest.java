a7e6bd259d29222a8ce5348bf42b51d95f1f5f27
Xanderhall <vivian.greenslade@gmail.com>
2023-09-09 11:34:09 -0230

    added unit test for 508.7c

--- Mage.Tests/src/test/java/org/mage/test/cards/targets/attacking/ReselectDefenderAttackedByTargetEffectTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/targets/attacking/ReselectDefenderAttackedByTargetEffectTest.java
@@ -10,10 +10,10 @@ import mage.counters.CounterType;
 public class ReselectDefenderAttackedByTargetEffectTest extends CardTestCommander4Players {
     
     private static final String SIGNPOST = "Misleading Signpost";
-    private static final String MAGE = "Portal Mage";
     private static final String LION = "Silvercoat Lion";
     private static final String PLANESWALKER = "Teferi, Master of Time";
     private static final String BATTLE = "Invasion of Segovia";
+    private static final String ARCHON = "Blazing Archon";
 
     /**
      * When Misleading Signpost enters the battlefield during the declare attackers step, you may reselect which player or permanent target attacking creature is attacking.
@@ -70,5 +70,36 @@ public class ReselectDefenderAttackedByTargetEffectTest extends CardTestCommande
 
         assertCounterCount(BATTLE, CounterType.DEFENSE, 2);
         assertLife(playerB, 20);
+    }
+
+    /**
+     * Test of 508.7b
+     */
+    @Test
+    public void testAvoidRestrictions() {
+        addCard(Zone.BATTLEFIELD, playerA, LION);
+
+        addCard(Zone.BATTLEFIELD, playerB, "Island", 3);
+        addCard(Zone.HAND, playerB, SIGNPOST);
+
+        addCard(Zone.BATTLEFIELD, playerC, PLANESWALKER);
+        addCard(Zone.BATTLEFIELD, playerC, ARCHON);
+
+        // Player A attacks Player C's planeswalker, can't attack Player C because of Archon
+        attack(1, playerA, LION, PLANESWALKER);
+
+        // Player B should be able to redirect to Player C, ignoring Archon
+        castSpell(1, PhaseStep.DECLARE_ATTACKERS, playerB, SIGNPOST);
+        setChoice(playerB, true);
+        addTarget(playerB, LION);
+        addTarget(playerB, playerC);
+
+        setStrictChooseMode(true);
+        setStopAt(1, PhaseStep.POSTCOMBAT_MAIN);
+        execute();
+
+        // Player C should be damaged, not planeswalker
+        assertLife(playerC, 18);
+        assertCounterCount(PLANESWALKER, CounterType.LOYALTY, 3);
     }
 }
