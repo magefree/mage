48d7d07f939650dde59aefa8425306a037269e03
xenohedron <xenohedron@users.noreply.github.com>
2023-09-21 23:46:09 -0400

    reimplement Fractured Loyalty

--- Mage/src/main/java/mage/abilities/common/BecomesTargetAttachedTriggeredAbility.java
+++ Mage/src/main/java/mage/abilities/common/BecomesTargetAttachedTriggeredAbility.java
@@ -2,6 +2,7 @@ package mage.abilities.common;
 
 import mage.abilities.TriggeredAbilityImpl;
 import mage.abilities.effects.Effect;
+import mage.constants.SetTargetPointer;
 import mage.constants.Zone;
 import mage.filter.FilterStackObject;
 import mage.filter.StaticFilters;
@@ -9,6 +10,7 @@ import mage.game.Game;
 import mage.game.events.GameEvent;
 import mage.game.stack.StackObject;
 import mage.game.permanent.Permanent;
+import mage.target.targetpointer.FixedTarget;
 import mage.util.CardUtil;
 
 /**
@@ -17,20 +19,23 @@ import mage.util.CardUtil;
 public class BecomesTargetAttachedTriggeredAbility extends TriggeredAbilityImpl {
 
     private final FilterStackObject filter;
+    private final SetTargetPointer setTargetPointer;
 
     public BecomesTargetAttachedTriggeredAbility(Effect effect) {
-        this(effect, StaticFilters.FILTER_SPELL_OR_ABILITY_A, false);
+        this(effect, StaticFilters.FILTER_SPELL_OR_ABILITY_A, SetTargetPointer.NONE, false);
     }
 
-    public BecomesTargetAttachedTriggeredAbility(Effect effect, FilterStackObject filter, boolean optional) {
+    public BecomesTargetAttachedTriggeredAbility(Effect effect, FilterStackObject filter, SetTargetPointer setTargetPointer, boolean optional) {
         super(Zone.BATTLEFIELD, effect, optional);
         this.filter = filter;
+        this.setTargetPointer = setTargetPointer;
         setTriggerPhrase("When enchanted creature becomes the target of " + filter.getMessage() + ", ");
     }
 
     protected BecomesTargetAttachedTriggeredAbility(final BecomesTargetAttachedTriggeredAbility ability) {
         super(ability);
         this.filter = ability.filter;
+        this.setTargetPointer = ability.setTargetPointer;
     }
 
     @Override
@@ -53,6 +58,21 @@ public class BecomesTargetAttachedTriggeredAbility extends TriggeredAbilityImpl 
         if (targetingObject == null || !filter.match(targetingObject, getControllerId(), this, game)) {
             return false;
         }
-        return CardUtil.checkTargetMap(this.id, targetingObject, event, game);
+        if (!CardUtil.checkTargetMap(this.id, targetingObject, event, game)) {
+            return false;
+        }
+        switch (setTargetPointer) {
+            case PLAYER:
+                this.getAllEffects().setTargetPointer(new FixedTarget(targetingObject.getControllerId(), game));
+                break;
+            case SPELL:
+                this.getAllEffects().setTargetPointer(new FixedTarget(targetingObject.getId()));
+                break;
+            case NONE:
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported SetTargetPointer in BecomesTargetAttachedTriggeredAbility");
+        }
+        return true;
     }
 }
