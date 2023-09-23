60829a9208656e91c25f99dd16ddc55ca7afface
Codermann63 <andreas.j.jorgensen@gmail.com>
2023-09-23 22:54:35 +0200

    Liberated Livestock ready to merge into main

--- Mage.Sets/src/mage/cards/l/LiberatedLivestock.java
+++ Mage.Sets/src/mage/cards/l/LiberatedLivestock.java
@@ -1,6 +1,5 @@
 package mage.cards.l;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
@@ -8,20 +7,17 @@ import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.DiesSourceTriggeredAbility;
 import mage.abilities.effects.OneShotEffect;
-import mage.abilities.effects.common.CreateTokenEffect;
 import mage.cards.*;
 import mage.constants.Outcome;
 import mage.constants.SubType;
 import mage.constants.CardType;
 import mage.constants.Zone;
 import mage.filter.FilterCard;
-import mage.filter.predicate.Predicates;
 import mage.game.Game;
 import mage.game.permanent.Permanent;
 import mage.game.permanent.token.*;
 import mage.players.Player;
 import mage.target.TargetCard;
-import mage.target.Targets;
 import mage.target.common.TargetCardInHand;
 import mage.target.common.TargetCardInYourGraveyard;
 
@@ -43,11 +39,8 @@ public final class LiberatedLivestock extends CardImpl {
 
 
         // When Liberated Livestock dies, create a 1/1 white Cat creature token with lifelink,
-        //this.addAbility(new DiesSourceTriggeredAbility(new CreateTokenEffect(new CatToken2())));
         // a 1/1 white Bird creature token with flying,
-        //this.addAbility(new DiesSourceTriggeredAbility(new CreateTokenEffect(new BirdToken())));
         // and a 2/4 white Ox creature token.
-        //this.addAbility(new DiesSourceTriggeredAbility(new CreateTokenEffect(new OxToken())));
         // For each of those tokens, you may put an Aura card from your hand and/or graveyard onto the battlefield attached to it.
         this.addAbility(new DiesSourceTriggeredAbility(new LiberatedLivestockEffect()));
     }
@@ -62,7 +55,6 @@ public final class LiberatedLivestock extends CardImpl {
     }
 }
 
-//QUESTION: Unsure if oneshoteffect is correct here
 class LiberatedLivestockEffect extends OneShotEffect {
 
     LiberatedLivestockEffect() {
@@ -81,63 +73,44 @@ class LiberatedLivestockEffect extends OneShotEffect {
         Token cat = new CatToken2();
         Token bird = new BirdToken();
         Token ox = new OxToken();
+
+        // pause to get token onto the battlefield all the same time
+        game.getState().pause();
         cat.putOntoBattlefield(1, game, source, source.getControllerId());
         bird.putOntoBattlefield(1, game, source, source.getControllerId());
         ox.putOntoBattlefield(1, game, source, source.getControllerId());
+        game.getState().resume();
 
+        // Setup for choosing and attaching auras to tokens
         List<Token> tokens = Arrays.asList(cat, bird, ox);
         FilterCard filter = new FilterCard("Up to one Aura");
         filter.add(SubType.AURA.getPredicate());
-
         TargetCard target;
+
 
         for (Token token : tokens){
             for (UUID t : token.getLastAddedTokenIds()){
                 Permanent sourcePermanent = game.getPermanent(t);
                 if (sourcePermanent == null){continue;}
-                if (controller.chooseUse(outcome, "Look in Hand or Graveyard?", null, "Hand", "Graveyard", source, game)){
-                    target = new TargetCardInHand(0,1,filter);
-                }
-                else{
-                    target = new TargetCardInYourGraveyard(0,1,filter);
-                }
+                filter.setMessage("Aura from you hand or graveyard that can attach to " + sourcePermanent.getName());
 
+                // Choose from hand or graveyard?
+                if (controller.chooseUse(outcome, "Look in Hand or Graveyard for aura to attach to "+sourcePermanent.getName()+"?", null, "Hand", "Graveyard", source, game)){
+                    target = new TargetCardInHand(0,1,filter);}
+                else{
+                    target = new TargetCardInYourGraveyard(0,1,filter);}
                 target.withNotTarget(true);
+
+                // Handle putting aura onto the battlefield and attachment to token
                 controller.chooseTarget(outcome, target, source, game);
                 Card card = game.getCard(target.getFirstTarget());
                 if (card == null){continue;}
+                if (sourcePermanent.cantBeAttachedBy(card,source,game,true)){continue;}
                 game.getState().setValue("attachTo:" + card.getId(), sourcePermanent);
                 controller.moveCards(card, Zone.BATTLEFIELD, source, game);
                 sourcePermanent.addAttachment(card.getId(), source, game);
-
             }
-
         }
-
-
-
-
-
-        /*
-
-
-        controller.choose(outcome, target, source, game);
-        Cards cards = new CardsImpl(target.getTargets());
-        if (cards.isEmpty()) {
-            return false;
-        }
-        cards.getCards(game)
-                .stream()
-                .forEach(card -> game.getState().setValue("attachTo:" + card.getId(), permanent));
-        player.moveCards(cards, Zone.BATTLEFIELD, source, game);
-        for (UUID cardId : cards) {
-            permanent.addAttachment(cardId, source, game);
-        }
-        return true;
-        */
-
-
-
         return true;
     }
 
