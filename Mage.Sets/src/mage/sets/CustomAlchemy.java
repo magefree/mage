4e05f158ff559e5141faa6110bcee5314fb43349
Susucre <34709007+Susucre@users.noreply.github.com>
2023-09-24 00:44:24 +0200

    ensure custom set is properly set up

--- Mage.Sets/src/mage/sets/CustomAlchemy.java
+++ Mage.Sets/src/mage/sets/CustomAlchemy.java
@@ -20,6 +20,9 @@ public final class CustomAlchemy extends ExpansionSet {
 
     private CustomAlchemy() {
         super("Custom Alchemy", "CALC", ExpansionSet.buildDate(2023, 9, 24), SetType.CUSTOM_SET);
+        this.hasBoosters = false;
+        this.hasBasicLands = false;
+
         cards.add(new SetCardInfo("C-Pillar of the Paruns", 1, Rarity.SPECIAL, mage.cards.p.PillarOfTheParunsCustom.class));
     }
 
