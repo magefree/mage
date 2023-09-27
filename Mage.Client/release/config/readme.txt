ff7f46410cf542eed3f911a22c7ae092af788755
arcox <10953229+arcox@users.noreply.github.com>
2023-09-27 18:27:28 -0400

    remove log4j flags from scripts

--- Mage.Client/release/config/readme.txt
+++ Mage.Client/release/config/readme.txt
@@ -1,3 +1,2 @@
 It's a sample config files. If you want to use it then run app with that extra commands (add it in launcher settings or in run script):
--Dlog4j.configuration=file:./config/log4j.properties
 -Djava.security.policy=./config/security.policy
--- Mage.Server/release/startServer.sh
+++ Mage.Server/release/startServer.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Xms256M -Xmx512M -Dfile.encoding=UTF-8 -Djava.security.policy=./config/security.policy -Dlog4j.configuration=file:./config/log4j.properties -jar ./lib/mage-server-${project.version}.jar
+java -Xms256M -Xmx512M -Dfile.encoding=UTF-8 -Djava.security.policy=./config/security.policy -jar ./lib/mage-server-${project.version}.jar
