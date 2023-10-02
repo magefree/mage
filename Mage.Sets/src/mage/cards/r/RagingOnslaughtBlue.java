ef7e69582847896852bf101c6392cecb20139828
chesse20 <chesse20@gmail.com>
2023-10-02 15:39:49 -0700

    make the card an enchantment

--- Mage.Sets/src/mage/cards/r/RagingOnslaughtBlue.java
+++ Mage.Sets/src/mage/cards/r/RagingOnslaughtBlue.java
@@ -2,6 +2,7 @@ package mage.cards.r;
 
 import mage.abilities.effects.common.DamageTargetEffect;
 import mage.abilities.effects.common.PreventDamageToControllerEffect;
+import mage.abilities.effects.common.SacrificeSourceEffect;
 import mage.abilities.effects.common.counter.GetEnergyCountersControllerEffect;
 import mage.abilities.keyword.ChannelAbility;
 import mage.abilities.keyword.PitchAbility;
@@ -10,8 +11,10 @@ import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.Duration;
 import mage.constants.Zone;
+import mage.game.events.GameEvent.EventType;
 import mage.target.TargetPlayer;
 import mage.abilities.Ability;
+import mage.abilities.common.OnEventTriggeredAbility;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.common.PayEnergyCost;
 import mage.abilities.costs.common.PayResourceCost;
@@ -27,7 +30,7 @@ import javafx.scene.effect.Effect;
 public final class RagingOnslaughtBlue extends CardImpl {
 
     public RagingOnslaughtBlue(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{0}");
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{0}");
 
         // Ragefire deals 5 damage to target player.
         
@@ -37,6 +40,7 @@ public final class RagingOnslaughtBlue extends CardImpl {
         this.addAbility(new ChannelAbility ("{0}", new PreventDamageToControllerEffect(Duration.EndOfTurn, 3)));
         this.addAbility(new PitchAbility ("{0}", new GetEnergyCountersControllerEffect(3)));
 
+        this.addAbility(new OnEventTriggeredAbility(EventType.END_TURN_STEP_PRE, "beginning of the end step", true, new SacrificeSourceEffect()));
     }
 
     private RagingOnslaughtBlue(final RagingOnslaughtBlue card) {
