639aba45920fdd3c25419bc93a0c3303f12aaecf
Steven Knipe <knipesteven@gmail.com>
2023-10-01 22:50:22 -0700

    CastAsThoughItHadFlashAllEffect applies function variant throws error instead of returning false

--- Mage/src/main/java/mage/abilities/effects/common/continuous/CastAsThoughItHadFlashAllEffect.java
+++ Mage/src/main/java/mage/abilities/effects/common/continuous/CastAsThoughItHadFlashAllEffect.java
@@ -60,7 +60,7 @@ public class CastAsThoughItHadFlashAllEffect extends AsThoughEffectImpl {
     }
     @Override
     public boolean applies(UUID objectId, Ability source, UUID affectedControllerId, Game game) {
-        return false;
+        throw new IllegalArgumentException("Wrong code usage: can't call applies method on empty affectedAbility");
     }
 
     private String setText() {
