66e8cc04b3f017a6a5368b2a396a88cca0391949
chesse20 <chesse20@gmail.com>
2023-10-02 15:35:46 -0700

    this isnt brothers war

--- Mage.Sets/src/mage/sets/WelcomeToRathe.java
+++ Mage.Sets/src/mage/sets/WelcomeToRathe.java
@@ -4,17 +4,14 @@ import mage.cards.Card;
 import mage.cards.ExpansionSet;
 import mage.constants.Rarity;
 import mage.constants.SetType;
-import mage.util.RandomUtil;
 
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * @author chesse20
  */
 public final class WelcomeToRathe extends ExpansionSet {
 
-    private static final List<String> unfinished = Arrays.asList("Arcane Proxy", "Autonomous Assembler", "Blitz Automaton", "Boulderbranch Golem", "Combat Thresher", "Cradle Clearcutter", "Depth Charge Colossus", "Fallaji Dragon Engine", "Goring Warplow", "Hulking Metamorph", "Iron-Craw Crusher", "Phyrexian Fleshgorger", "Rootwire Amalgam", "Rust Goliath", "Skitterbeam Battalion", "Spotter Thopter", "Steel Seraph", "Woodcaller Automaton");
+    //private static final List<String> unfinished = Arrays.asList();
 
     private static final WelcomeToRathe instance = new WelcomeToRathe();
 
@@ -23,8 +20,8 @@ public final class WelcomeToRathe extends ExpansionSet {
     }
 
     private WelcomeToRathe() {
-        super("Welcome to Rathe", "BRO", ExpansionSet.buildDate(2022, 11, 18), SetType.EXPANSION);
-        this.blockName = "The Brothers' War";
+        super("Welcome to Rathe", "WTR", ExpansionSet.buildDate(2019, 10, 11), SetType.EXPANSION);
+        this.blockName = "Welcome to rathe";
         this.hasBoosters = true;
         this.numBoosterLands = 0;
         this.numBoosterCommon = 15; 
@@ -36,27 +33,11 @@ public final class WelcomeToRathe extends ExpansionSet {
 
         cards.add(new SetCardInfo("RagingOnslaught", 1, Rarity.COMMON, mage.cards.r.RagingOnslaughtBlue.class));
 
-        cards.removeIf(setCardInfo -> unfinished.contains(setCardInfo.getName())); // remove when mechanic is implemented
-    }
-
-    @Override
-    protected void addSpecialCards(List<Card> booster, int number) {
-        // number is here always 1
-        // Boosters have one card from BRR, odds are 2/3 for uncommon, 4/15 for rare, 1/15 for mythic
-        Rarity rarity;
-        int rarityKey = RandomUtil.nextInt(15);
-        if (rarityKey == 14) {
-            rarity = Rarity.MYTHIC;
-        } else if (rarityKey >= 10) {
-            rarity = Rarity.RARE;
-        } else {
-            rarity = Rarity.UNCOMMON;
-        }
-        addToBooster(booster, TheBrothersWarRetroArtifacts.getInstance().getCardsByRarity(rarity));
+        //cards.removeIf(setCardInfo -> unfinished.contains(setCardInfo.getName())); // remove when mechanic is implemented
     }
 
 //    @Override
 //    public BoosterCollator createCollator() {
-//        return new TheBrothersWarCollator();
+//        return new WelcomeToRatheCollator();
 //    }
 }
