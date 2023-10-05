a84a6a998f116022193b5029b2f83932707fba72
xenohedron <xenohedron@users.noreply.github.com>
2023-10-05 16:14:43 -0400

    remove boilerplate template comments

--- Mage/src/main/java/mage/abilities/common/delayed/AtTheBeginOfYourNextDrawStepDelayedTriggeredAbility.java
+++ Mage/src/main/java/mage/abilities/common/delayed/AtTheBeginOfYourNextDrawStepDelayedTriggeredAbility.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package mage.abilities.common.delayed;
 
 import mage.abilities.DelayedTriggeredAbility;
@@ -43,4 +39,3 @@ public class AtTheBeginOfYourNextDrawStepDelayedTriggeredAbility extends Delayed
         return event.getPlayerId().equals(this.controllerId);
     }
 }
-
