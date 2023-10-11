64321534c55314dfc1dbd99a0d98d232abb9a6dc
xenohedron <xenohedron@users.noreply.github.com>
2023-10-10 23:22:28 -0400

    fix Awaken the Maelstrom

--- Mage.Sets/src/mage/cards/a/AwakenTheMaelstrom.java
+++ Mage.Sets/src/mage/cards/a/AwakenTheMaelstrom.java
@@ -91,6 +91,7 @@ class AwakenTheMaelstromEffect extends OneShotEffect {
             return false;
         }
         makeToken(player, game, source);
+        game.getState().processAction(game);
         distributeCounters(player, game, source);
         return true;
     }
@@ -113,10 +114,10 @@ class AwakenTheMaelstromEffect extends OneShotEffect {
         if (game.getBattlefield().count(StaticFilters.FILTER_CONTROLLED_CREATURE, player.getId(), source, game) < 1) {
             return;
         }
-        TargetPermanentAmount target = new TargetCreaturePermanentAmount(3);
+        TargetPermanentAmount target = new TargetCreaturePermanentAmount(3, StaticFilters.FILTER_CONTROLLED_CREATURE);
         target.withNotTarget(true);
         target.withChooseHint("to distribute counters");
-        player.choose(outcome, target, source, game);
+        target.chooseTarget(outcome, player.getId(), source, game);
         for (UUID targetId : target.getTargets()) {
             Permanent permanent = game.getPermanent(targetId);
             if (permanent != null) {
