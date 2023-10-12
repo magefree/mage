03a505e994af9372e5a647bbbafcab1c94ea0688
Steven Knipe <knipesteven@gmail.com>
2023-10-12 14:59:17 -0700

    use new VanishingAbility in CrackInTime

--- Mage.Sets/src/mage/cards/c/CrackInTime.java
+++ Mage.Sets/src/mage/cards/c/CrackInTime.java
@@ -6,8 +6,7 @@ import mage.abilities.common.EntersBattlefieldAbility;
 import mage.abilities.common.EntersBattlefieldTriggeredAbility;
 import mage.abilities.effects.common.ExileUntilSourceLeavesEffect;
 import mage.abilities.effects.common.counter.AddCountersSourceEffect;
-import mage.abilities.keyword.VanishingSacrificeAbility;
-import mage.abilities.keyword.VanishingUpkeepAbility;
+import mage.abilities.keyword.VanishingAbility;
 import mage.abilities.meta.OrTriggeredAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
@@ -32,8 +31,7 @@ public final class CrackInTime extends CardImpl {
         Ability ability = new EntersBattlefieldAbility(new AddCountersSourceEffect(CounterType.TIME.createInstance(3)));
         ability.setRuleVisible(false);
         this.addAbility(ability);
-        this.addAbility(new VanishingUpkeepAbility(3));
-        this.addAbility(new VanishingSacrificeAbility());
+        this.addAbility(new VanishingAbility(3));
 
         // When Crack in Time enters the battlefield and at the beginning of your precombat main phase, exile target creature an opponent controls until Crack in Time leaves the battlefield.
         ability = new OrTriggeredAbility(Zone.BATTLEFIELD, new ExileUntilSourceLeavesEffect(), false,
