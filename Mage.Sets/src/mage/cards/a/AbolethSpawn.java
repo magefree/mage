93f44064edb48c15c1e00cca5196638e2dea29be
xenohedron <xenohedron@users.noreply.github.com>
2023-10-14 16:21:16 -0400

    reimplement

--- Mage.Sets/src/mage/cards/a/AbolethSpawn.java
+++ Mage.Sets/src/mage/cards/a/AbolethSpawn.java
@@ -1,6 +1,7 @@
 package mage.cards.a;
 
 import mage.MageInt;
+import mage.abilities.Ability;
 import mage.abilities.TriggeredAbility;
 import mage.abilities.TriggeredAbilityImpl;
 import mage.abilities.costs.mana.ManaCostsImpl;
@@ -16,12 +17,11 @@ import mage.game.Game;
 import mage.game.events.GameEvent;
 import mage.game.permanent.Permanent;
 import mage.game.stack.StackObject;
-import mage.target.targetpointer.FixedTarget;
 
 import java.util.UUID;
 
 /**
- * @author Rowan-Gudmundsson
+ * @author Rowan-Gudmundsson, xenohedron
  */
 public final class AbolethSpawn extends CardImpl {
 
@@ -36,12 +36,12 @@ public final class AbolethSpawn extends CardImpl {
 
         // Flash
         this.addAbility(FlashAbility.getInstance());
+
         // Ward {2}
         this.addAbility(new WardAbility(new ManaCostsImpl<>("{2}"), false));
 
-        // Whenever a creature entering the battlefield under an opponent’s control
-        // causes a triggered ability of that creature to trigger, you may copy that
-        // ability. You may choose new targets for the copy.
+        // Whenever a creature entering the battlefield under an opponent's control causes a triggered ability of that creature to trigger,
+        // you may copy that ability. You may choose new targets for the copy.
         this.addAbility(new AbolethSpawnTriggeredAbility());
     }
 
@@ -59,6 +59,7 @@ class AbolethSpawnTriggeredAbility extends TriggeredAbilityImpl {
 
     AbolethSpawnTriggeredAbility() {
         super(Zone.BATTLEFIELD, new CopyStackObjectEffect(), true);
+        setTriggerPhrase("Whenever a creature entering the battlefield under an opponent's control causes a triggered ability of that creature to trigger, ");
     }
 
     private AbolethSpawnTriggeredAbility(final AbolethSpawnTriggeredAbility ability) {
@@ -74,28 +75,25 @@ class AbolethSpawnTriggeredAbility extends TriggeredAbilityImpl {
     public boolean checkTrigger(GameEvent event, Game game) {
         StackObject stackObject = game.getStack().getStackObject(event.getTargetId());
         Permanent permanent = game.getPermanent(event.getSourceId());
-
-        if (stackObject == null || !(stackObject.getStackAbility() instanceof TriggeredAbility)) {
+        if (stackObject == null || permanent == null || !permanent.isCreature(game)) {
+            return false; // only creatures
+        }
+        if (!game.getOpponents(this.getControllerId()).contains(permanent.getControllerId())) {
+            return false; // only creatures entering under opponent's control
+        }
+        Ability stackAbility = stackObject.getStackAbility();
+        if (!(stackAbility instanceof TriggeredAbility)) {
             return false;
         }
-
-        TriggeredAbility ability = (TriggeredAbility) stackObject.getStackAbility();
-
-        GameEvent triggerEvent = ability.getTriggerEvent();
-
-        if (triggerEvent == null
-                || triggerEvent.getType() != GameEvent.EventType.ENTERS_THE_BATTLEFIELD) {
-            return false;
+        GameEvent triggerEvent = ((TriggeredAbility) stackAbility).getTriggerEvent();
+        if (triggerEvent == null || triggerEvent.getType() != GameEvent.EventType.ENTERS_THE_BATTLEFIELD) {
+            return false; // only ETB triggers
         }
-
-        if (permanent == null
-                || !permanent.isCreature(game)
-                || permanent.getControllerId().equals(controllerId)) {
-            return false;
+        if (triggerEvent.getSourceId() != permanent.getId()) {
+            return false; // only triggered abilities of that creature
         }
-
-        this.getEffects().setValue("stackObject", stackObject);
-        this.getEffects().setTargetPointer(new FixedTarget(event.getTargetId(), game));
+        // CopyStackObjectEffect needs value set
+        getEffects().setValue("stackObject", stackObject);
         return true;
     }
 
@@ -104,8 +102,4 @@ class AbolethSpawnTriggeredAbility extends TriggeredAbilityImpl {
         return new AbolethSpawnTriggeredAbility(this);
     }
 
-    @Override
-    public String getRule() {
-        return "Whenever a creature entering the battlefield under an opponent's control causes a triggered ability of that creature to trigger, you may copy that ability. You may choose new targets for the copy.";
-    }
 }
--- Mage.Tests/src/test/java/org/mage/test/cards/single/clb/AbolethSpawnTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/clb/AbolethSpawnTest.java
@@ -6,48 +6,43 @@ import org.junit.Test;
 import org.mage.test.serverside.base.CardTestPlayerBase;
 
 /**
- * @author Rowan-Gudmundsson
+ * @author xenohedron
  */
 public class AbolethSpawnTest extends CardTestPlayerBase {
 
+    private static final String aboleth = "Aboleth Spawn"; // 2/3
+    // Whenever a creature entering the battlefield under an opponent's control
+    // causes a triggered ability of that creature to trigger,
+    // you may copy that ability. You may choose new targets for the copy.
+    private static final String sparkmage = "Sparkmage Apprentice";
+    // When Sparkmage Apprentice enters the battlefield, it deals 1 damage to any target.
+    private static final String attendant = "Soul's Attendant";
+    // Whenever another creature enters the battlefield, you may gain 1 life.
+    private static final String hatchling = "Kraken Hatchling"; // 0/4
+
     @Test
-    public void TestAbolethSpawn() {
-        addCard(Zone.BATTLEFIELD, playerA, "Aboleth Spawn");
+    public void testTriggerCopies() {
+        addCard(Zone.BATTLEFIELD, playerA, aboleth);
+        addCard(Zone.BATTLEFIELD, playerA, hatchling);
+        addCard(Zone.BATTLEFIELD, playerB, attendant);
+        addCard(Zone.HAND, playerB, sparkmage);
+        addCard(Zone.BATTLEFIELD, playerB, "Mountain", 2);
 
-        addCard(Zone.HAND, playerB, "Wall of Omens");
-        addCard(Zone.BATTLEFIELD, playerB, "Plains", 5);
+        castSpell(2, PhaseStep.PRECOMBAT_MAIN, playerB, sparkmage);
+        setChoice(playerB, "Whenever another"); // order triggers
+        addTarget(playerB, hatchling); // to deal 1 damage
+        setChoice(playerA, true); // yes to copy sparkmage trigger
+        setChoice(playerA, true); // yes to change targets
+        addTarget(playerA, aboleth); // to deal 1 damage
+        setChoice(playerB, true); // yes to gain 1 life
 
-        addCard(Zone.LIBRARY, playerA, "Island", 200);
-        addCard(Zone.LIBRARY, playerB, "Island", 200);
-
-        castSpell(2, PhaseStep.PRECOMBAT_MAIN, playerB, "Wall of Omens");
-        waitStackResolved(2, PhaseStep.PRECOMBAT_MAIN, 1);
-
-        checkStackObject(
-                "Has Aboleth trigger",
-                2,
-                PhaseStep.PRECOMBAT_MAIN,
-                playerA,
-                "Whenever a creature entering the battlefield under an opponent's control causes a triggered ability of that creature to trigger",
-                1);
-
-        setChoice(playerA, true);
-
-        waitStackResolved(2, PhaseStep.PRECOMBAT_MAIN, 1);
-
-        checkStackObject(
-                "Has Wall of Omens trigger",
-                2,
-                PhaseStep.PRECOMBAT_MAIN,
-                playerA,
-                "When {this} enters the battlefield, draw a card.",
-                2);
-
-        setStopAt(2, PhaseStep.PRECOMBAT_MAIN);
-
+        setStopAt(2, PhaseStep.END_TURN);
+        setStrictChooseMode(true);
         execute();
 
-        assertHandCount(playerB, 2);
-        assertHandCount(playerA, 1);
+        assertDamageReceived(playerA, hatchling, 1);
+        assertDamageReceived(playerA, aboleth, 1);
+        assertLife(playerA, 20);
+        assertLife(playerB, 21);
     }
 }
