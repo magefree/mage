764bc096cdbbccf2447df90d4010b6336f066468
PurpleCrowbar <26198472+PurpleCrowbar@users.noreply.github.com>
2023-10-18 20:54:49 +0100

    Refactor self-suspend cards (#11317)

--- Mage.Sets/src/mage/cards/i/InspiringRefrain.java
+++ Mage.Sets/src/mage/cards/i/InspiringRefrain.java
@@ -1,15 +1,12 @@
 package mage.cards.i;
 
 import mage.abilities.costs.mana.ManaCostsImpl;
-import mage.abilities.dynamicvalue.common.StaticValue;
 import mage.abilities.effects.common.DrawCardSourceControllerEffect;
-import mage.abilities.effects.common.ExileSpellEffect;
-import mage.abilities.effects.common.counter.AddCountersSourceEffect;
+import mage.abilities.effects.common.ExileSpellWithTimeCountersEffect;
 import mage.abilities.keyword.SuspendAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.counters.CounterType;
 
 import java.util.UUID;
 
@@ -23,10 +20,7 @@ public final class InspiringRefrain extends CardImpl {
 
         // Draw two cards. Exile Inspiring Refrain with three time counters on it.
         this.getSpellAbility().addEffect(new DrawCardSourceControllerEffect(2));
-        this.getSpellAbility().addEffect(new ExileSpellEffect());
-        this.getSpellAbility().addEffect(new AddCountersSourceEffect(
-                CounterType.TIME.createInstance(), StaticValue.get(3), false, true
-        ).setText("with three time counters on it"));
+        this.getSpellAbility().addEffect(new ExileSpellWithTimeCountersEffect(3));
 
         // Suspend 3—{2}{U}
         this.addAbility(new SuspendAbility(3, new ManaCostsImpl<>("{2}{U}"), this));
