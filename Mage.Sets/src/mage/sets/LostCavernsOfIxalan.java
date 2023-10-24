894d16013077421b31ae8d7f9949d3d757ace541
Susucre <34709007+Susucre@users.noreply.github.com>
2023-10-24 19:42:36 +0200

    [LCI] Implement Cenote Scout

--- Mage.Sets/src/mage/sets/LostCavernsOfIxalan.java
+++ Mage.Sets/src/mage/sets/LostCavernsOfIxalan.java
@@ -23,6 +23,7 @@ public final class LostCavernsOfIxalan extends ExpansionSet {
 
         cards.add(new SetCardInfo("Breeches, Eager Pillager", 137, Rarity.RARE, mage.cards.b.BreechesEagerPillager.class));
         cards.add(new SetCardInfo("Cavern of Souls", 269, Rarity.MYTHIC, mage.cards.c.CavernOfSouls.class));
+        cards.add(new SetCardInfo("Cenote Scout", 178, Rarity.UNCOMMON, mage.cards.c.CenoteScout.class));
         cards.add(new SetCardInfo("Forest", 291, Rarity.LAND, mage.cards.basiclands.Forest.class, FULL_ART_BFZ_VARIOUS));
         cards.add(new SetCardInfo("Ghalta, Stampede Tyrant", 185, Rarity.MYTHIC, mage.cards.g.GhaltaStampedeTyrant.class));
         cards.add(new SetCardInfo("Huatli, Poet of Unity", 189, Rarity.MYTHIC, mage.cards.h.HuatliPoetOfUnity.class));
