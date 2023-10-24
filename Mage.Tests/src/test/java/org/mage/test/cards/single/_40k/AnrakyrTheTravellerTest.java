98a2d16b60cb3f32402fc78fa135c61d03cfa13b
gravitybone <7361357+gravitybone@users.noreply.github.com>
2023-10-24 20:47:15 +0200

    adds comments to tests and makes cardnames constants
    Co-authored-by: Aquid <Aquid117@users.noreply.github.com>

--- Mage.Tests/src/test/java/org/mage/test/cards/single/_40k/AnrakyrTheTravellerTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/_40k/AnrakyrTheTravellerTest.java
@@ -8,20 +8,30 @@ import org.mage.test.serverside.base.CardTestPlayerBase;
 
 public class AnrakyrTheTravellerTest extends CardTestPlayerBase {
 
+    // Lord of the Pyrrhian Legions — Whenever Anrakyr the Traveller attacks, you may cast an artifact spell from your
+    // hand or graveyard by paying life equal to its mana value rather than paying its mana cost.
     private static final String ANRAKYR = "Anrakyr the Traveller";
+
+    private static final String CRYPTEK = "Cryptek";
+
+    // Multikicker {2}
+    // Everflowing Chalice enters the battlefield with a charge counter on it for each time it was kicked.
+    private static final String EVERFLOWING_CHALICE = "Everflowing Chalice";
+
+    // As an additional cost to cast this spell, discard a card.
+    private static final String LESSER_MASTICORE = "Lesser Masticore";
 
     @Test
     public void testCastArtifactCardForLifeFromHand_ShouldCastForLife() {
         // prepare
-        final String cryptek = "Cryptek";
         addCard(Zone.BATTLEFIELD, playerA, ANRAKYR);
-        addCard(Zone.HAND, playerA, cryptek);
+        addCard(Zone.HAND, playerA, CRYPTEK);
 
         // execute
         attack(1, playerA, ANRAKYR);
-        setChoice(playerA, true);
-        setChoice(playerA, cryptek);
-        setChoice(playerA, true);
+        setChoice(playerA, true); // confirm use of Anrakyr's ability
+        setChoice(playerA, CRYPTEK); // select Cryptek to cast
+        setChoice(playerA, true); // confirm casting spell by paying life instead of mana
 
         setStrictChooseMode(true);
         setStopAt(1, PhaseStep.END_TURN);
@@ -29,22 +39,21 @@ public class AnrakyrTheTravellerTest extends CardTestPlayerBase {
 
         // assert
         assertLife(playerA, 20 - 4);
-        assertPermanentCount(playerA, cryptek, 1);
+        assertPermanentCount(playerA, CRYPTEK, 1);
     }
 
     @Test
     public void testNotEnoughLife_ShouldNotCast() {
         // prepare
-        final String cryptek = "Cryptek";
         addCard(Zone.BATTLEFIELD, playerA, ANRAKYR);
-        addCard(Zone.HAND, playerA, cryptek);
+        addCard(Zone.HAND, playerA, CRYPTEK);
         setLife(playerA, 3);
 
         // execute
         attack(1, playerA, ANRAKYR);
-        setChoice(playerA, true);
-        setChoice(playerA, cryptek);
-        setChoice(playerA, true);
+        setChoice(playerA, true); // confirm use of Anrakyr's ability
+        setChoice(playerA, CRYPTEK); // select Cryptek to cast
+        setChoice(playerA, true); // confirm casting spell by paying life instead of mana
 
         setStrictChooseMode(true);
         setStopAt(1, PhaseStep.END_TURN);
@@ -52,21 +61,20 @@ public class AnrakyrTheTravellerTest extends CardTestPlayerBase {
 
         // assert
         assertLife(playerA, 3);
-        assertPermanentCount(playerA, cryptek, 0);
+        assertPermanentCount(playerA, CRYPTEK, 0);
     }
 
     @Test
     public void testCastArtifactCardForLifeFromGraveyard_ShouldCastForLife() {
         // prepare
-        final String cryptek = "Cryptek";
         addCard(Zone.BATTLEFIELD, playerA, ANRAKYR);
-        addCard(Zone.GRAVEYARD, playerA, cryptek);
+        addCard(Zone.GRAVEYARD, playerA, CRYPTEK);
 
         // execute
         attack(1, playerA, ANRAKYR);
-        setChoice(playerA, true);
-        setChoice(playerA, cryptek);
-        setChoice(playerA, true);
+        setChoice(playerA, true); // confirm use of Anrakyr's ability
+        setChoice(playerA, CRYPTEK); // select Cryptek to cast
+        setChoice(playerA, true); // confirm casting spell by paying life instead of mana
 
         setStrictChooseMode(true);
         setStopAt(1, PhaseStep.END_TURN);
@@ -74,25 +82,23 @@ public class AnrakyrTheTravellerTest extends CardTestPlayerBase {
 
         // assert
         assertLife(playerA, 20 - 4);
-        assertPermanentCount(playerA, cryptek, 1);
-        assertGraveyardCount(playerA, cryptek, 0);
+        assertPermanentCount(playerA, CRYPTEK, 1);
+        assertGraveyardCount(playerA, CRYPTEK, 0);
     }
 
     @Test
     public void testLesserMasticore_ShouldCast() {
         // prepare
-        final String lesserMasticore = "Lesser Masticore";
-        final String cryptek = "Cryptek";
         addCard(Zone.BATTLEFIELD, playerA, ANRAKYR);
-        addCard(Zone.HAND, playerA, lesserMasticore);
-        addCard(Zone.HAND, playerA, cryptek);
+        addCard(Zone.HAND, playerA, LESSER_MASTICORE);
+        addCard(Zone.HAND, playerA, CRYPTEK);
 
         // execute
         attack(1, playerA, ANRAKYR);
-        setChoice(playerA, true);
-        setChoice(playerA, lesserMasticore);
-        setChoice(playerA, true);
-        setChoice(playerA, cryptek);
+        setChoice(playerA, true); // confirm use of Anrakyr's ability
+        setChoice(playerA, LESSER_MASTICORE); // select Lesser Masticore to cast
+        setChoice(playerA, true); // confirm casting spell by paying life instead of mana
+        setChoice(playerA, CRYPTEK); // select Cryptek to discard to pay for Lesser Masticore
 
         setStrictChooseMode(true);
         setStopAt(1, PhaseStep.END_TURN);
@@ -100,21 +106,20 @@ public class AnrakyrTheTravellerTest extends CardTestPlayerBase {
 
         // assert
         assertLife(playerA, 20 - 2);
-        assertPermanentCount(playerA, lesserMasticore, 1);
+        assertPermanentCount(playerA, LESSER_MASTICORE, 1);
     }
 
     @Test
     public void testLesserMasticoreNoCardInHand_ShouldNotCast() {
         // prepare
-        final String lesserMasticore = "Lesser Masticore";
         addCard(Zone.BATTLEFIELD, playerA, ANRAKYR);
-        addCard(Zone.HAND, playerA, lesserMasticore);
+        addCard(Zone.HAND, playerA, LESSER_MASTICORE);
 
         // execute
         attack(1, playerA, ANRAKYR);
-        setChoice(playerA, true);
-        setChoice(playerA, lesserMasticore);
-        setChoice(playerA, true);
+        setChoice(playerA, true); // confirm use of Anrakyr's ability
+        setChoice(playerA, LESSER_MASTICORE); // select Lesser Masticore to cast
+        setChoice(playerA, true); // confirm casting spell by paying life instead of mana
 
         setStrictChooseMode(true);
         setStopAt(1, PhaseStep.END_TURN);
@@ -122,26 +127,25 @@ public class AnrakyrTheTravellerTest extends CardTestPlayerBase {
 
         // assert
         assertLife(playerA, 20);
-        assertPermanentCount(playerA, lesserMasticore, 0);
+        assertPermanentCount(playerA, LESSER_MASTICORE, 0);
     }
 
     @Test
     public void testEverflowingChaliceMultikicker1_ShouldCast() {
         // prepare
-        final String everflowingChalice = "Everflowing Chalice";
         final String swamp = "Swamp";
         addCard(Zone.BATTLEFIELD, playerA, ANRAKYR);
         addCard(Zone.BATTLEFIELD, playerA, swamp);
         addCard(Zone.BATTLEFIELD, playerA, swamp);
-        addCard(Zone.HAND, playerA, everflowingChalice);
+        addCard(Zone.HAND, playerA, EVERFLOWING_CHALICE);
 
         // execute
         attack(1, playerA, ANRAKYR);
-        setChoice(playerA, true);
-        setChoice(playerA, everflowingChalice);
-        setChoice(playerA, true);
+        setChoice(playerA, true); // confirm use of Anrakyr's ability
+        setChoice(playerA, EVERFLOWING_CHALICE); // select Everflowing Chalice to cast
+        setChoice(playerA, true); // confirm casting spell by paying life instead of mana
 
-        // multikicker
+        // multikicker 1
         setChoice(playerA, true);
         setChoice(playerA, false);
 
@@ -151,24 +155,23 @@ public class AnrakyrTheTravellerTest extends CardTestPlayerBase {
 
         // assert
         assertLife(playerA, 20);
-        assertPermanentCount(playerA, everflowingChalice, 1);
-        assertCounterCount(playerA, everflowingChalice, CounterType.CHARGE, 1);
+        assertPermanentCount(playerA, EVERFLOWING_CHALICE, 1);
+        assertCounterCount(playerA, EVERFLOWING_CHALICE, CounterType.CHARGE, 1);
     }
 
     @Test
     public void testEverflowingChaliceMultikicker0_ShouldCast() {
         // prepare
-        final String everflowingChalice = "Everflowing Chalice";
         addCard(Zone.BATTLEFIELD, playerA, ANRAKYR);
-        addCard(Zone.HAND, playerA, everflowingChalice);
+        addCard(Zone.HAND, playerA, EVERFLOWING_CHALICE);
 
         // execute
         attack(1, playerA, ANRAKYR);
-        setChoice(playerA, true);
-        setChoice(playerA, everflowingChalice);
-        setChoice(playerA, true);
+        setChoice(playerA, true); // confirm use of Anrakyr's ability
+        setChoice(playerA, EVERFLOWING_CHALICE); // select Everflowing Chalice to cast
+        setChoice(playerA, true); // confirm casting spell by paying life instead of mana
 
-        // multikicker
+        // multikicker 0
         setChoice(playerA, false);
 
         setStrictChooseMode(true);
@@ -177,24 +180,23 @@ public class AnrakyrTheTravellerTest extends CardTestPlayerBase {
 
         // assert
         assertLife(playerA, 20);
-        assertPermanentCount(playerA, everflowingChalice, 1);
-        assertCounterCount(playerA, everflowingChalice, CounterType.CHARGE, 0);
+        assertPermanentCount(playerA, EVERFLOWING_CHALICE, 1);
+        assertCounterCount(playerA, EVERFLOWING_CHALICE, CounterType.CHARGE, 0);
     }
 
     @Test
     public void testEverflowingChaliceMultikicker1NoMana_ShouldNotCast() {
         // prepare
-        final String everflowingChalice = "Everflowing Chalice";
         addCard(Zone.BATTLEFIELD, playerA, ANRAKYR);
-        addCard(Zone.HAND, playerA, everflowingChalice);
+        addCard(Zone.HAND, playerA, EVERFLOWING_CHALICE);
 
         // execute
         attack(1, playerA, ANRAKYR);
-        setChoice(playerA, true);
-        setChoice(playerA, everflowingChalice);
-        setChoice(playerA, true);
+        setChoice(playerA, true); // confirm use of Anrakyr's ability
+        setChoice(playerA, EVERFLOWING_CHALICE); // select Everflowing Chalice to cast
+        setChoice(playerA, true); // confirm casting spell by paying life instead of mana
 
-        // multikicker
+        // multikicker 1
         setChoice(playerA, true);
         setChoice(playerA, false);
 
@@ -204,6 +206,6 @@ public class AnrakyrTheTravellerTest extends CardTestPlayerBase {
 
         // assert
         assertLife(playerA, 20);
-        assertPermanentCount(playerA, everflowingChalice, 0);
+        assertPermanentCount(playerA, EVERFLOWING_CHALICE, 0);
     }
 }
