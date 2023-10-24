576b334c2a87958bf67a8bd9e59f229745a5942c
theelk801 <theelk801@gmail.com>
2023-10-24 19:55:58 -0400

    [LCC] update spoiler and reprints

--- Mage.Sets/src/mage/sets/LostCavernsOfIxalanCommander.java
+++ Mage.Sets/src/mage/sets/LostCavernsOfIxalanCommander.java
@@ -21,6 +21,10 @@ public final class LostCavernsOfIxalanCommander extends ExpansionSet {
         this.hasBoosters = false;
         this.hasBasicLands = false;
 
+        cards.add(new SetCardInfo("Amulet of Vigor", 103, Rarity.RARE, mage.cards.a.AmuletOfVigor.class));
+        cards.add(new SetCardInfo("Arcane Signet", 104, Rarity.UNCOMMON, mage.cards.a.ArcaneSignet.class));
+        cards.add(new SetCardInfo("Chalice of the Void", 105, Rarity.MYTHIC, mage.cards.c.ChaliceOfTheVoid.class));
         cards.add(new SetCardInfo("Coercive Portal", 109, Rarity.MYTHIC, mage.cards.c.CoercivePortal.class));
+        cards.add(new SetCardInfo("Expedition Map", 112, Rarity.UNCOMMON, mage.cards.e.ExpeditionMap.class));
     }
 }
