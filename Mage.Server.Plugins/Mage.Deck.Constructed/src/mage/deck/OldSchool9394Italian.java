1cb484e03e7d9841d1d8af8bc2a4be3f82cee934
Susucre <34709007+Susucre@users.noreply.github.com>
2023-10-26 18:29:59 +0200

    unban Chaos Orb from OId School formats (#11344)

--- Mage.Server.Plugins/Mage.Deck.Constructed/src/mage/deck/OldSchool9394Italian.java
+++ Mage.Server.Plugins/Mage.Deck.Constructed/src/mage/deck/OldSchool9394Italian.java
@@ -30,6 +30,9 @@ public class OldSchool9394Italian extends Constructed {
         setCodes.add(mage.sets.TheDark.getInstance().getCode());
         setCodes.add(mage.sets.RevisedEdition.getInstance().getCode());
 
+        // unban chaos orb, we allow it (restricted) for the xmage-errated version in Old School Constructed.
+        banned.removeIf(name -> name.equals("Chaos Orb"));
+
         restricted.add("Ancestral Recall");
         restricted.add("Balance");
         restricted.add("Black Lotus");
