a55879575c623ac9114ea7cb58739cbf1a24d89a
theelk801 <theelk801@gmail.com>
2023-10-28 09:44:53 -0400

    [LCI] Implement Abyssal Gorestalker

--- Mage.Sets/src/mage/sets/TheLostCavernsOfIxalan.java
+++ Mage.Sets/src/mage/sets/TheLostCavernsOfIxalan.java
@@ -21,6 +21,7 @@ public final class TheLostCavernsOfIxalan extends ExpansionSet {
         this.hasBasicLands = true;
 
         cards.add(new SetCardInfo("Abuelo, Ancestral Echo", 219, Rarity.RARE, mage.cards.a.AbueloAncestralEcho.class));
+        cards.add(new SetCardInfo("Abyssal Gorestalker", 87, Rarity.UNCOMMON, mage.cards.a.AbyssalGorestalker.class));
         cards.add(new SetCardInfo("Amalia Benavides Aguirre", 221, Rarity.RARE, mage.cards.a.AmaliaBenavidesAguirre.class));
         cards.add(new SetCardInfo("Bartolome del Presidio", 224, Rarity.UNCOMMON, mage.cards.b.BartolomeDelPresidio.class));
         cards.add(new SetCardInfo("Bladewheel Chariot", 36, Rarity.UNCOMMON, mage.cards.b.BladewheelChariot.class));
