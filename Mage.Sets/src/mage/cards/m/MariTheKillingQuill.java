c461191da9c4cbc1a7c429d3bb041c98e78ac298
Shashakar <dextarm@gmail.com>
2023-10-28 20:42:55 -0600

    fix text: Mari, the Killing Quill (#11360)
    Updated the text so it shows correctly on Mari as well as any other creatures that have the effect.

--- Mage.Sets/src/mage/cards/m/MariTheKillingQuill.java
+++ Mage.Sets/src/mage/cards/m/MariTheKillingQuill.java
@@ -69,8 +69,8 @@ public class MariTheKillingQuill extends CardImpl {
         // NOTE: Optional part is handled inside the effect
         Ability dealsDamageAbility = new DealsCombatDamageToAPlayerTriggeredAbility(new MariTheKillingQuillDealsDamageEffect(), false, true);
         Effect drawAndTreasureEffect = new GainAbilityControlledEffect(dealsDamageAbility, Duration.WhileOnBattlefield, filter);
-        drawAndTreasureEffect.setText(
-                "\"Whenever this creature deals combat damage to a player, you may remove a hit counter from a card that player owns in exile. " +
+        drawAndTreasureEffect.setText("\"Whenever this creature deals combat damage to a player, " +
+                "you may remove a hit counter from a card that player owns in exile. " +
                 "If you do, draw a card and create two Treasure tokens.\"");
         drawAndTreasureEffect.concatBy("and");
 
@@ -93,6 +93,8 @@ class MariTheKillingQuillDealsDamageEffect extends OneShotEffect {
 
     MariTheKillingQuillDealsDamageEffect() {
         super(Outcome.Benefit);
+        staticText =  "you may remove a hit counter from a card that player owns in exile. " +
+                "If you do, draw a card and create two Treasure tokens.";
     }
 
     private MariTheKillingQuillDealsDamageEffect(final MariTheKillingQuillDealsDamageEffect effect) {
