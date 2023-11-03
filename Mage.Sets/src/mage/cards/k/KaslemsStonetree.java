d92981c1635d02a7f9b3dc046360657fb557f48c
theelk801 <theelk801@gmail.com>
2023-11-02 21:28:59 -0400

    [LCI] add missing filter to Kaslem's Stonetree

--- Mage.Sets/src/mage/cards/k/KaslemsStonetree.java
+++ Mage.Sets/src/mage/cards/k/KaslemsStonetree.java
@@ -8,6 +8,7 @@ import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.PutCards;
 import mage.constants.SubType;
+import mage.filter.StaticFilters;
 
 import java.util.UUID;
 
@@ -22,7 +23,8 @@ public final class KaslemsStonetree extends CardImpl {
 
         // When Kaslem's Stonetree enters the battlefield, look at the top six cards of your library. You may put a land card from among them onto the battlefield tapped. Put the rest on the bottom in a random order.
         this.addAbility(new EntersBattlefieldTriggeredAbility(new LookLibraryAndPickControllerEffect(
-                6, 1, PutCards.BATTLEFIELD_TAPPED, PutCards.BOTTOM_RANDOM
+                6, 1, StaticFilters.FILTER_CARD_LAND_A,
+                PutCards.BATTLEFIELD_TAPPED, PutCards.BOTTOM_RANDOM
         )));
 
         // Craft with Cave {5}{G}
