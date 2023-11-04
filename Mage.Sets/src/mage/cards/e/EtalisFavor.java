3fbab2aa3bdea4668d4d86505654faf62ab651fe
theelk801 <theelk801@gmail.com>
2023-11-03 21:14:46 -0400

    [LCI] Implement Etali's Favor

---
+++ Mage.Sets/src/mage/cards/e/EtalisFavor.java
@@ -0,0 +1,58 @@
+package mage.cards.e;
+
+import mage.abilities.Ability;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.common.AttachEffect;
+import mage.abilities.effects.common.continuous.BoostEnchantedEffect;
+import mage.abilities.effects.common.continuous.GainAbilityAttachedEffect;
+import mage.abilities.effects.keyword.DiscoverEffect;
+import mage.abilities.keyword.EnchantAbility;
+import mage.abilities.keyword.TrampleAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.AttachmentType;
+import mage.constants.CardType;
+import mage.constants.Outcome;
+import mage.constants.SubType;
+import mage.target.TargetPermanent;
+import mage.target.common.TargetControlledCreaturePermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class EtalisFavor extends CardImpl {
+
+    public EtalisFavor(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{2}{R}");
+
+        this.subtype.add(SubType.AURA);
+
+        // Enchant creature you control
+        TargetPermanent auraTarget = new TargetControlledCreaturePermanent();
+        this.getSpellAbility().addTarget(auraTarget);
+        this.getSpellAbility().addEffect(new AttachEffect(Outcome.BoostCreature));
+        this.addAbility(new EnchantAbility(auraTarget));
+
+        // When Etali's Favor enters the battlefield, discover 3.
+        this.addAbility(new EntersBattlefieldTriggeredAbility(new DiscoverEffect(3)));
+
+        // Enchanted creature gets +1/+1 and has trample.
+        Ability ability = new SimpleStaticAbility(new BoostEnchantedEffect(1, 1));
+        ability.addEffect(new GainAbilityAttachedEffect(
+                TrampleAbility.getInstance(), AttachmentType.AURA
+        ).setText("and has trample"));
+        this.addAbility(ability);
+    }
+
+    private EtalisFavor(final EtalisFavor card) {
+        super(card);
+    }
+
+    @Override
+    public EtalisFavor copy() {
+        return new EtalisFavor(this);
+    }
+}
