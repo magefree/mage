3c01d97ff2c7637f7e8153bd016932d5aa3f1ec4
Steven Knipe <knipesteven@gmail.com>
2023-11-11 15:18:04 -0800

    Find more addAbility that should be done without subabilities

--- Mage/src/main/java/mage/abilities/effects/common/CopyEffect.java
+++ Mage/src/main/java/mage/abilities/effects/common/CopyEffect.java
@@ -119,11 +119,11 @@ public class CopyEffect extends ContinuousEffectImpl {
         permanent.removeAllAbilities(source.getSourceId(), game);
         if (copyFromObject instanceof Permanent) {
             for (Ability ability : ((Permanent) copyFromObject).getAbilities(game)) {
-                permanent.addAbility(ability, getSourceId(), game);
+                permanent.addAbility(ability, getSourceId(), game, false);
             }
         } else {
             for (Ability ability : copyFromObject.getAbilities()) {
-                permanent.addAbility(ability, getSourceId(), game);
+                permanent.addAbility(ability, getSourceId(), game, false);
             }
         }
 
