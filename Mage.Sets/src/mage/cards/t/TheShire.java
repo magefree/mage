776ea3e77a90946976e1c820f7a1a583e5285aba
Daniel Cowman <32685899+permadeath@users.noreply.github.com>
2023-11-20 21:39:20 -0500

    Update TheShire.java
    Fixed "You control a legendary creature" hint.

--- Mage.Sets/src/mage/cards/t/TheShire.java
+++ Mage.Sets/src/mage/cards/t/TheShire.java
@@ -4,6 +4,7 @@ import mage.abilities.Ability;
 import mage.abilities.common.EntersBattlefieldAbility;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.condition.Condition;
+import mage.abilities.condition.InvertCondition;
 import mage.abilities.condition.common.PermanentsOnTheBattlefieldCondition;
 import mage.abilities.costs.common.TapSourceCost;
 import mage.abilities.costs.common.TapTargetCost;
@@ -40,7 +41,7 @@ public final class TheShire extends CardImpl {
 
     private static final Condition condition
             = new PermanentsOnTheBattlefieldCondition(filter, ComparisonType.EQUAL_TO, 0);
-    private static final Hint hint = new ConditionHint(condition, "You control a legendary creature");
+    private static final Hint hint = new ConditionHint( new InvertCondition(condition), "You control a legendary creature");
 
     public TheShire(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.LAND}, "");
