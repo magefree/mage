cb7801f26c779c759818be6e86129ccc4dc0373e
PurpleCrowbar <26198472+PurpleCrowbar@users.noreply.github.com>
2023-12-05 22:36:39 +0000

    Update Uba Mask exile zone to clear at end of turn

--- Mage.Sets/src/mage/cards/u/UbaMask.java
+++ Mage.Sets/src/mage/cards/u/UbaMask.java
@@ -66,7 +66,16 @@ class UbaMaskReplacementEffect extends ReplacementEffectImpl {
         if (player != null && sourceObject != null) {
             Card card = player.getLibrary().getFromTop(game);
             if (card != null) {
-                if (player.moveCardsToExile(card, source, game, true, source.getSourceId(), sourceObject.getIdName())) {
+                UUID exileId = CardUtil.getExileZoneId(
+                        player.getId().toString()
+                                + "-" + game.getState().getTurnNum()
+                                + "-" + sourceObject.getIdName(), game
+                );
+                String exileName = sourceObject.getIdName() + " play on turn " + game.getState().getTurnNum()
+                        + " for " + player.getName();
+                game.getExile().createZone(exileId, exileName).setCleanupOnEndTurn(true);
+
+                if (player.moveCardsToExile(card, source, game, true, exileId, exileName)) {
                     UbaMaskExiledCardsWatcher watcher = game.getState().getWatcher(UbaMaskExiledCardsWatcher.class);
                     if (watcher != null) {
                         watcher.addExiledCard(event.getPlayerId(), card, game);
@@ -91,7 +100,7 @@ class UbaMaskReplacementEffect extends ReplacementEffectImpl {
 class UbaMaskPlayEffect extends AsThoughEffectImpl {
 
     public UbaMaskPlayEffect() {
-        super(AsThoughEffectType.PLAY_FROM_NOT_OWN_HAND_ZONE, Duration.EndOfGame, Outcome.Benefit);
+        super(AsThoughEffectType.PLAY_FROM_NOT_OWN_HAND_ZONE, Duration.WhileOnBattlefield, Outcome.Benefit);
         staticText = "Each player may play lands and cast spells from among cards they exiled with {this} this turn";
     }
 
