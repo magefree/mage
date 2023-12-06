7340e8b67bee2ce95dc32e8521ecdf521882ac3d
theelk801 <theelk801@gmail.com>
2023-12-05 22:13:21 -0500

    [CLU] Implement Lead Pipe

---
+++ Mage.Sets/src/mage/cards/l/LeadPipe.java
@@ -0,0 +1,54 @@
+package mage.cards.l;
+
+import mage.abilities.Ability;
+import mage.abilities.common.DiesAttachedTriggeredAbility;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.costs.common.SacrificeSourceCost;
+import mage.abilities.costs.mana.GenericManaCost;
+import mage.abilities.effects.common.DrawCardSourceControllerEffect;
+import mage.abilities.effects.common.LoseLifeOpponentsEffect;
+import mage.abilities.effects.common.continuous.BoostEquippedEffect;
+import mage.abilities.keyword.EquipAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class LeadPipe extends CardImpl {
+
+    public LeadPipe(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT}, "{B}");
+
+        this.subtype.add(SubType.CLUE);
+        this.subtype.add(SubType.EQUIPMENT);
+
+        // Equipped creature gets +2/+0.
+        this.addAbility(new SimpleStaticAbility(new BoostEquippedEffect(2, 0)));
+
+        // Whenever equipped creature dies, each opponent loses 1 life.
+        this.addAbility(new DiesAttachedTriggeredAbility(new LoseLifeOpponentsEffect(1), "equipped creature"));
+
+        // {2}, Sacrifice Lead Pipe: Draw a card.
+        Ability ability = new SimpleActivatedAbility(new DrawCardSourceControllerEffect(1), new GenericManaCost(2));
+        ability.addCost(new SacrificeSourceCost());
+        this.addAbility(ability);
+
+        // Equip {2}
+        this.addAbility(new EquipAbility(2));
+    }
+
+    private LeadPipe(final LeadPipe card) {
+        super(card);
+    }
+
+    @Override
+    public LeadPipe copy() {
+        return new LeadPipe(this);
+    }
+}
--- Mage.Sets/src/mage/sets/RavnicaClueEdition.java
+++ Mage.Sets/src/mage/sets/RavnicaClueEdition.java
@@ -21,6 +21,7 @@ public final class RavnicaClueEdition extends ExpansionSet {
         this.hasBoosters = false;
 
         cards.add(new SetCardInfo("Hallowed Fountain", 277, Rarity.RARE, mage.cards.h.HallowedFountain.class));
+        cards.add(new SetCardInfo("Lead Pipe", 9, Rarity.UNCOMMON, mage.cards.l.LeadPipe.class));
         cards.add(new SetCardInfo("Steam Vents", 280, Rarity.RARE, mage.cards.s.SteamVents.class));
     }
 }
