8459babbcdb64d115867c97faf9c3e727f2a6204
jimga150 <jimga150@users.noreply.github.com>
2023-12-31 14:25:10 -0500

    [LCC] Implement From the Rubble (#11592)

---
+++ Mage.Sets/src/mage/cards/f/FromTheRubble.java
@@ -0,0 +1,63 @@
+package mage.cards.f;
+
+import java.util.UUID;
+
+import mage.abilities.Ability;
+import mage.abilities.Mode;
+import mage.abilities.common.AsEntersBattlefieldAbility;
+import mage.abilities.common.BeginningOfYourEndStepTriggeredAbility;
+import mage.abilities.effects.common.ChooseCreatureTypeEffect;
+import mage.abilities.effects.common.ReturnFromGraveyardToBattlefieldTargetEffect;
+import mage.abilities.effects.common.ReturnFromGraveyardToBattlefieldWithCounterTargetEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.ComparisonType;
+import mage.constants.Outcome;
+import mage.counters.CounterType;
+import mage.filter.FilterCard;
+import mage.filter.common.FilterCreatureCard;
+import mage.filter.predicate.mageobject.ChosenSubtypePredicate;
+import mage.filter.predicate.mageobject.ManaValuePredicate;
+import mage.target.common.TargetCardInYourGraveyard;
+
+/**
+ *
+ * @author jimga150
+ */
+public final class FromTheRubble extends CardImpl {
+
+    private static final FilterCreatureCard filter = new FilterCreatureCard(
+            "a creature card of the chosen type from your graveyard"
+    );
+
+    static {
+        filter.add(ChosenSubtypePredicate.TRUE);
+    }
+
+    public FromTheRubble(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{4}{W}{W}");
+        
+
+        // As From the Rubble enters the battlefield, choose a creature type.
+        this.addAbility(new AsEntersBattlefieldAbility(new ChooseCreatureTypeEffect(Outcome.PutCreatureInPlay)));
+
+        // At the beginning of your end step, return target creature card of the chosen type from your graveyard to
+        // the battlefield with a finality counter on it.
+        Ability ability = new BeginningOfYourEndStepTriggeredAbility(
+                new ReturnFromGraveyardToBattlefieldWithCounterTargetEffect(CounterType.FINALITY.createInstance()),
+                false
+        );
+        ability.addTarget(new TargetCardInYourGraveyard(filter));
+        this.addAbility(ability);
+    }
+
+    private FromTheRubble(final FromTheRubble card) {
+        super(card);
+    }
+
+    @Override
+    public FromTheRubble copy() {
+        return new FromTheRubble(this);
+    }
+}
