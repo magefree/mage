ee68f6a47d80411bfd179b555d68d446358514e6
Steven Knipe <knipesteven@gmail.com>
2024-01-10 19:43:35 -0800

    Ravenous Tyrannosaurus

--- Mage.Sets/src/mage/sets/JurassicWorldCollection.java
+++ Mage.Sets/src/mage/sets/JurassicWorldCollection.java
@@ -34,6 +34,7 @@ public final class JurassicWorldCollection extends ExpansionSet {
         cards.add(new SetCardInfo("Permission Denied", 17, Rarity.RARE, mage.cards.p.PermissionDenied.class));
         cards.add(new SetCardInfo("Plains", 21, Rarity.LAND, mage.cards.basiclands.Plains.class, FULL_ART_BFZ_VARIOUS));
         cards.add(new SetCardInfo("Plains", "21b", Rarity.LAND, mage.cards.basiclands.Plains.class, FULL_ART_BFZ_VARIOUS));
+        cards.add(new SetCardInfo("Ravenous Tyrannosaurus", 18, Rarity.RARE, mage.cards.r.RavenousTyrannosaurus.class));
         cards.add(new SetCardInfo("Savage Order", 6, Rarity.RARE, mage.cards.s.SavageOrder.class));
         cards.add(new SetCardInfo("Spitting Dilophosaurus", 3, Rarity.RARE, mage.cards.s.SpittingDilophosaurus.class));
         cards.add(new SetCardInfo("Swamp", 23, Rarity.LAND, mage.cards.basiclands.Swamp.class, FULL_ART_BFZ_VARIOUS));
