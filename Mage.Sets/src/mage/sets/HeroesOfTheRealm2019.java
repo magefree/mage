6439c97e82aace2c2d59f67ff5e03e0ee1a06cd3
PurpleCrowbar <26198472+PurpleCrowbar@users.noreply.github.com>
2024-01-15 03:34:37 +0000

    [PH19] Implement The Cinematic Phoenix (#11657)

---
+++ Mage.Sets/src/mage/sets/HeroesOfTheRealm2019.java
@@ -0,0 +1,26 @@
+package mage.sets;
+
+import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
+import mage.constants.SetType;
+
+/**
+ * https://scryfall.com/sets/ph19
+ * @author PurpleCrowbar
+ */
+public class HeroesOfTheRealm2019 extends ExpansionSet {
+
+    private static final HeroesOfTheRealm2019 instance = new HeroesOfTheRealm2019();
+
+    public static HeroesOfTheRealm2019 getInstance() {
+        return instance;
+    }
+
+    private HeroesOfTheRealm2019() {
+        super("Heroes of the Realm 2019", "PH19", ExpansionSet.buildDate(2020, 8, 1), SetType.JOKESET);
+        this.hasBoosters = false;
+        this.hasBasicLands = false;
+
+        cards.add(new SetCardInfo("The Cinematic Phoenix", 6, Rarity.MYTHIC, mage.cards.t.TheCinematicPhoenix.class));
+    }
+}
