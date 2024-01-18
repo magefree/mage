1f12beb212700c9198713e7ec635afc177990d63
theelk801 <theelk801@gmail.com>
2024-01-18 09:57:09 -0500

    [MKM] Implement Gearbane Orangutan

---
+++ Mage.Sets/src/mage/cards/g/GearbaneOrangutan.java
@@ -0,0 +1,59 @@
+package mage.cards.g;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.Mode;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.costs.common.SacrificeTargetCost;
+import mage.abilities.effects.common.DestroyTargetEffect;
+import mage.abilities.effects.common.DoIfCostPaid;
+import mage.abilities.effects.common.counter.AddCountersSourceEffect;
+import mage.abilities.keyword.ReachAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.counters.CounterType;
+import mage.filter.StaticFilters;
+import mage.target.common.TargetArtifactPermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class GearbaneOrangutan extends CardImpl {
+
+    public GearbaneOrangutan(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{2}{R}");
+
+        this.subtype.add(SubType.APE);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(2);
+
+        // Reach
+        this.addAbility(ReachAbility.getInstance());
+
+        // When Gearbane Orangutan enters the battlefield, choose one --
+        // * Destroy up to one target artifact.
+        Ability ability = new EntersBattlefieldTriggeredAbility(new DestroyTargetEffect());
+        ability.addTarget(new TargetArtifactPermanent(0, 1));
+
+        // * Sacrifice an artifact. If you do, put two +1/+1 counters on Gearbane Orangutan.
+        ability.addMode(new Mode(new DoIfCostPaid(
+                new AddCountersSourceEffect(CounterType.P1P1.createInstance()),
+                new SacrificeTargetCost(StaticFilters.FILTER_CONTROLLED_PERMANENT_ARTIFACT_AN),
+                null, false
+        )));
+        this.addAbility(ability);
+    }
+
+    private GearbaneOrangutan(final GearbaneOrangutan card) {
+        super(card);
+    }
+
+    @Override
+    public GearbaneOrangutan copy() {
+        return new GearbaneOrangutan(this);
+    }
+}
--- Mage.Sets/src/mage/sets/MurdersAtKarlovManor.java
+++ Mage.Sets/src/mage/sets/MurdersAtKarlovManor.java
@@ -46,6 +46,7 @@ public final class MurdersAtKarlovManor extends ExpansionSet {
         cards.add(new SetCardInfo("Forest", 276, Rarity.LAND, mage.cards.basiclands.Forest.class, FULL_ART_BFZ_VARIOUS));
         cards.add(new SetCardInfo("Gadget Technician", 204, Rarity.COMMON, mage.cards.g.GadgetTechnician.class));
         cards.add(new SetCardInfo("Galvanize", 128, Rarity.COMMON, mage.cards.g.Galvanize.class));
+        cards.add(new SetCardInfo("Gearbane Orangutan", 129, Rarity.COMMON, mage.cards.g.GearbaneOrangutan.class));
         cards.add(new SetCardInfo("Gleaming Geardrake", 205, Rarity.UNCOMMON, mage.cards.g.GleamingGeardrake.class));
         cards.add(new SetCardInfo("Hedge Maze", 262, Rarity.RARE, mage.cards.h.HedgeMaze.class));
         cards.add(new SetCardInfo("Homicide Investigator", 86, Rarity.RARE, mage.cards.h.HomicideInvestigator.class));
