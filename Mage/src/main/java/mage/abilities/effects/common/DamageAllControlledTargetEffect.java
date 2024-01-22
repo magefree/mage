5a5e3205427582768cf22003c6ad4aa70a1b0d8e
xenohedron <xenohedron@users.noreply.github.com>
2024-01-21 22:44:35 -0500

    refactor text gen for DamageAllControlledTargetEffect

--- Mage/src/main/java/mage/abilities/effects/common/DamageAllControlledTargetEffect.java
+++ Mage/src/main/java/mage/abilities/effects/common/DamageAllControlledTargetEffect.java
@@ -19,24 +19,9 @@ public class DamageAllControlledTargetEffect extends OneShotEffect {
 
     private final DynamicValue amount;
     private final FilterPermanent filter;
-    private String sourceName = "{this}";
 
     public DamageAllControlledTargetEffect(int amount) {
         this(amount, StaticFilters.FILTER_PERMANENT_CREATURE);
-    }
-
-    public DamageAllControlledTargetEffect(int amount, String whoDealDamageName) {
-        this(amount, StaticFilters.FILTER_PERMANENT_CREATURE);
-        this.sourceName = whoDealDamageName;
-    }
-
-    public DamageAllControlledTargetEffect(DynamicValue amount) {
-        this(amount, StaticFilters.FILTER_PERMANENT_CREATURE);
-    }
-
-    public DamageAllControlledTargetEffect(DynamicValue amount, String whoDealDamageName) {
-        this(amount, StaticFilters.FILTER_PERMANENT_CREATURE);
-        this.sourceName = whoDealDamageName;
     }
 
     public DamageAllControlledTargetEffect(int amount, FilterPermanent filter) {
@@ -53,7 +38,6 @@ public class DamageAllControlledTargetEffect extends OneShotEffect {
         super(effect);
         this.amount = effect.amount.copy();
         this.filter = effect.filter.copy();
-        this.sourceName = effect.sourceName;
     }
 
     @Override
@@ -78,14 +62,8 @@ public class DamageAllControlledTargetEffect extends OneShotEffect {
         if (staticText != null && !staticText.isEmpty()) {
             return staticText;
         }
-        StringBuilder sb = new StringBuilder(sourceName);
-        sb.append(" deals ").append(amount).append(" damage to each ").append(filter.getMessage());
-        if (mode.getTargets().isEmpty()) {
-            sb.append(" that player");
-        } else {
-            sb.append(" target ").append(mode.getTargets().get(0).getTargetName());
-        }
-        sb.append(" controls");
-        return sb.toString();
+        return "{this} deals " + amount + " damage to each " + filter.getMessage() + ' '
+                + getTargetPointer().describeTargets(mode.getTargets(), "that player")
+                + " controls";
     }
 }
