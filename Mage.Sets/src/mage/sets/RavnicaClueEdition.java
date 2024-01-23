7df86e91a021f5f02b89fa668466ab841362bc8d
Susucre <34709007+Susucre@users.noreply.github.com>
2024-01-23 19:51:13 +0100

    [CLU] Implement Sludge Titan
    Introduced TargetCardAndOrCardInGraveyard, derived from the Library one.
    Did not achieved everything I wanted in the tests, as the choice seems to be accepted.
    Tested it live, no particular issue, you can't select more than one per category.

--- Mage.Sets/src/mage/sets/RavnicaClueEdition.java
+++ Mage.Sets/src/mage/sets/RavnicaClueEdition.java
@@ -44,6 +44,7 @@ public final class RavnicaClueEdition extends ExpansionSet {
         cards.add(new SetCardInfo("Sacred Foundry", 279, Rarity.RARE, mage.cards.s.SacredFoundry.class));
         cards.add(new SetCardInfo("Secret Passage", 20, Rarity.UNCOMMON, mage.cards.s.SecretPassage.class));
         cards.add(new SetCardInfo("Senator Peacock", 2, Rarity.RARE, mage.cards.s.SenatorPeacock.class));
+        cards.add(new SetCardInfo("Sludge Titan", 43, Rarity.RARE, mage.cards.s.SludgeTitan.class));
         cards.add(new SetCardInfo("Steam Vents", 280, Rarity.RARE, mage.cards.s.SteamVents.class));
         cards.add(new SetCardInfo("Stomping Ground", 281, Rarity.RARE, mage.cards.s.StompingGround.class));
         cards.add(new SetCardInfo("Study", 21, Rarity.UNCOMMON, mage.cards.s.Study.class));
