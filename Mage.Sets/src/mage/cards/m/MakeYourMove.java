48b0a7fce4415d2f8d315338daea5948080aed17
theelk801 <theelk801@gmail.com>
2024-01-25 10:56:36 -0500

    [MKM] Implement Make Your Move

---
+++ Mage.Sets/src/mage/cards/m/MakeYourMove.java
@@ -0,0 +1,50 @@
+package mage.cards.m;
+
+import mage.abilities.effects.common.DestroyTargetEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.ComparisonType;
+import mage.filter.FilterPermanent;
+import mage.filter.predicate.Predicates;
+import mage.filter.predicate.mageobject.PowerPredicate;
+import mage.target.TargetPermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class MakeYourMove extends CardImpl {
+
+    private static final FilterPermanent filter
+            = new FilterPermanent("artifact, enchantment, or creature with power 4 or greater");
+
+    static {
+        filter.add(Predicates.or(
+                CardType.ARTIFACT.getPredicate(),
+                CardType.ENCHANTMENT.getPredicate(),
+                Predicates.and(
+                        CardType.CREATURE.getPredicate(),
+                        new PowerPredicate(ComparisonType.MORE_THAN, 4)
+                )
+        ));
+    }
+
+    public MakeYourMove(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{2}{W}");
+
+        // Destroy target artifact, enchantment, or creature with power 4 or greater.
+        this.getSpellAbility().addEffect(new DestroyTargetEffect());
+        this.getSpellAbility().addTarget(new TargetPermanent(filter));
+    }
+
+    private MakeYourMove(final MakeYourMove card) {
+        super(card);
+    }
+
+    @Override
+    public MakeYourMove copy() {
+        return new MakeYourMove(this);
+    }
+}
