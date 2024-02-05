9fca5d3f5f8412a0635dcdfb4f13137e30a9ae92
Cameron Merkel <44722506+cguy7777@users.noreply.github.com>
2024-02-05 06:33:37 -0600

    Replace training GameEvent with custom TriggeredAbility

--- Mage/src/main/java/mage/abilities/keyword/TrainingAbility.java
+++ Mage/src/main/java/mage/abilities/keyword/TrainingAbility.java
@@ -2,18 +2,13 @@ package mage.abilities.keyword;
 
 import mage.MageInt;
 import mage.MageObject;
-import mage.abilities.Ability;
 import mage.abilities.TriggeredAbilityImpl;
 import mage.abilities.effects.common.counter.AddCountersSourceEffect;
-import mage.constants.WatcherScope;
 import mage.constants.Zone;
 import mage.counters.CounterType;
 import mage.game.Game;
 import mage.game.events.GameEvent;
 import mage.game.permanent.Permanent;
-import mage.game.stack.StackAbility;
-import mage.game.stack.StackObject;
-import mage.watchers.Watcher;
 
 import java.util.Objects;
 
@@ -24,7 +19,6 @@ public class TrainingAbility extends TriggeredAbilityImpl {
 
     public TrainingAbility() {
         super(Zone.BATTLEFIELD, new AddCountersSourceEffect(CounterType.P1P1.createInstance()));
-        addWatcher(new TrainingWatcher());
     }
 
     private TrainingAbility(final TrainingAbility ability) {
@@ -62,35 +56,5 @@ public class TrainingAbility extends TriggeredAbilityImpl {
     public String getRule() {
         return "Training <i>(Whenever this creature attacks with another creature " +
                 "with greater power, put a +1/+1 counter on this creature.)</i>";
-    }
-}
-
-class TrainingWatcher extends Watcher {
-
-    public TrainingWatcher() {
-        super(WatcherScope.GAME);
-    }
-
-    @Override
-    public void watch(GameEvent event, Game game) {
-        // 20240202 - 702.149c
-        // Some creatures with training have abilities that trigger when they train.
-        // "When this creature trains" means "When a resolving training ability puts a +1/+1 counter on this creature."
-        if (event.getType() == GameEvent.EventType.COUNTER_ADDED && event.getData().equals(CounterType.P1P1.getName())) {
-            StackObject stackObject = game.getStack().getStackObject(event.getSourceId());
-            if (!(stackObject instanceof StackAbility)) {
-                return;
-            }
-
-            Ability ability = stackObject.getStackAbility();
-            if (ability instanceof TrainingAbility) {
-                game.fireEvent(GameEvent.getEvent(
-                        GameEvent.EventType.TRAINED_CREATURE,
-                        event.getTargetId(),
-                        ability,
-                        event.getPlayerId()
-                ));
-            }
-        }
     }
 }
