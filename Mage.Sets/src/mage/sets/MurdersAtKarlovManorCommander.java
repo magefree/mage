2e5d0b76e8afc1ac5c1748f470596b1bd62237be
PurpleCrowbar <26198472+PurpleCrowbar@users.noreply.github.com>
2024-02-21 03:19:24 +0000

    [MKC] Implement Copy Catchers

--- Mage.Sets/src/mage/sets/MurdersAtKarlovManorCommander.java
+++ Mage.Sets/src/mage/sets/MurdersAtKarlovManorCommander.java
@@ -60,6 +60,7 @@ public final class MurdersAtKarlovManorCommander extends ExpansionSet {
         cards.add(new SetCardInfo("Confirm Suspicions", 97, Rarity.RARE, mage.cards.c.ConfirmSuspicions.class));
         cards.add(new SetCardInfo("Connive // Concoct", 203, Rarity.RARE, mage.cards.c.ConniveConcoct.class));
         cards.add(new SetCardInfo("Consider", 98, Rarity.COMMON, mage.cards.c.Consider.class));
+        cards.add(new SetCardInfo("Copy Catchers", 20, Rarity.RARE, mage.cards.c.CopyCatchers.class));
         cards.add(new SetCardInfo("Curate", 99, Rarity.COMMON, mage.cards.c.Curate.class));
         cards.add(new SetCardInfo("Curse of Opulence", 150, Rarity.UNCOMMON, mage.cards.c.CurseOfOpulence.class));
         cards.add(new SetCardInfo("Darien, King of Kjeldor", 59, Rarity.RARE, mage.cards.d.DarienKingOfKjeldor.class));
