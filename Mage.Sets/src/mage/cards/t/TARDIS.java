fae67114b0c2ff731b2a4bb1e45c71c3b3a5434f
xenohedron <xenohedron@users.noreply.github.com>
2024-02-21 01:11:28 -0500

    adjust code format

--- Mage.Sets/src/mage/cards/t/TARDIS.java
+++ Mage.Sets/src/mage/cards/t/TARDIS.java
@@ -1,27 +1,27 @@
 package mage.cards.t;
 
-import java.util.UUID;
 import mage.MageInt;
 import mage.abilities.Ability;
+import mage.abilities.common.AttacksTriggeredAbility;
 import mage.abilities.condition.Condition;
 import mage.abilities.condition.common.PermanentsOnTheBattlefieldCondition;
+import mage.abilities.decorator.ConditionalInterveningIfTriggeredAbility;
 import mage.abilities.effects.common.PlaneswalkEffect;
 import mage.abilities.effects.common.continuous.NextSpellCastHasAbilityEffect;
 import mage.abilities.hint.ConditionHint;
 import mage.abilities.hint.Hint;
 import mage.abilities.keyword.CascadeAbility;
-import mage.constants.SubType;
-import mage.abilities.keyword.FlyingAbility;
 import mage.abilities.keyword.CrewAbility;
+import mage.abilities.keyword.FlyingAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.abilities.common.AttacksTriggeredAbility;
-import mage.abilities.decorator.ConditionalInterveningIfTriggeredAbility;
+import mage.constants.SubType;
 import mage.filter.common.FilterControlledPermanent;
 
+import java.util.UUID;
+
 /**
- *
  * @author Skiwkr
  */
 public final class TARDIS extends CardImpl {
@@ -33,7 +33,7 @@ public final class TARDIS extends CardImpl {
 
     public TARDIS(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT}, "{2}");
-        
+
         this.subtype.add(SubType.VEHICLE);
         this.power = new MageInt(2);
         this.toughness = new MageInt(4);
@@ -49,7 +49,7 @@ public final class TARDIS extends CardImpl {
 
         ability.addHint(hint);
 
-		this.addAbility(ability);
+        this.addAbility(ability);
         // Crew 2
         this.addAbility(new CrewAbility(2));
 
