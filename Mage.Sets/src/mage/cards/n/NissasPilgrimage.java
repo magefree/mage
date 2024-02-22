51c997a8ba08dadebde0fe87de6cfd31def3deae
Cameron Merkel <44722506+cguy7777@users.noreply.github.com>
2024-02-22 03:23:49 -0600

    Move Cultivate search effect to common class

--- Mage.Sets/src/mage/cards/n/NissasPilgrimage.java
+++ Mage.Sets/src/mage/cards/n/NissasPilgrimage.java
@@ -2,23 +2,15 @@
 package mage.cards.n;
 
 import java.util.UUID;
-import mage.MageObject;
-import mage.abilities.Ability;
 import mage.abilities.condition.common.SpellMasteryCondition;
-import mage.abilities.effects.OneShotEffect;
-import mage.cards.Card;
+import mage.abilities.decorator.ConditionalOneShotEffect;
+import mage.abilities.effects.common.search.SearchLibraryPutOneOntoBattlefieldTappedRestInHandEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.cards.Cards;
-import mage.cards.CardsImpl;
 import mage.constants.CardType;
-import mage.constants.Outcome;
 import mage.constants.SubType;
 import mage.constants.SuperType;
-import mage.constants.Zone;
 import mage.filter.FilterCard;
-import mage.game.Game;
-import mage.players.Player;
 import mage.target.common.TargetCardInLibrary;
 
 /**
@@ -27,12 +19,24 @@ import mage.target.common.TargetCardInLibrary;
  */
 public final class NissasPilgrimage extends CardImpl {
 
+    private static final FilterCard filter = new FilterCard("basic Forest cards");
+
+    static {
+        filter.add(SuperType.BASIC.getPredicate());
+        filter.add(SubType.FOREST.getPredicate());
+    }
+
     public NissasPilgrimage(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{2}{G}");
 
-        // Search your library for up to two basic Forest cards, reveal those cards, and put one onto the battlefield tapped and the rest into your hand.  Then shuffle your library.
+        // Search your library for up to two basic Forest cards, reveal those cards, and put one onto the battlefield tapped and the rest into your hand. Then shuffle.
         // <i>Spell Mastery</i> &mdash; If there are two or more instant and/or sorcery cards in your graveyard, search your library for up to three basic Forest cards instead of two.
-        this.getSpellAbility().addEffect(new NissasPilgrimageEffect());
+        this.getSpellAbility().addEffect(new ConditionalOneShotEffect(
+                new SearchLibraryPutOneOntoBattlefieldTappedRestInHandEffect(new TargetCardInLibrary(0, 3, filter)),
+                new SearchLibraryPutOneOntoBattlefieldTappedRestInHandEffect(new TargetCardInLibrary(0, 2, filter)),
+                SpellMasteryCondition.instance,
+                "Search your library for up to two basic Forest cards, reveal those cards, and put one onto the battlefield tapped and the rest into your hand. Then shuffle."
+                        + "<br><i>Spell mastery</i> &mdash; If there are two or more instant and/or sorcery cards in your graveyard, search your library for up to three basic Forest cards instead of two."));
     }
 
     private NissasPilgrimage(final NissasPilgrimage card) {
@@ -42,59 +46,5 @@ public final class NissasPilgrimage extends CardImpl {
     @Override
     public NissasPilgrimage copy() {
         return new NissasPilgrimage(this);
-    }
-}
-
-class NissasPilgrimageEffect extends OneShotEffect {
-
-    private static final FilterCard filter = new FilterCard("basic Forest card");
-
-    static {
-        filter.add(SuperType.BASIC.getPredicate());
-        filter.add(SubType.FOREST.getPredicate());
-    }
-
-    public NissasPilgrimageEffect() {
-        super(Outcome.Benefit);
-        this.staticText = "Search your library for up to two basic Forest cards, reveal those cards, and put one onto the battlefield tapped and the rest into your hand. Then shuffle."
-                + "<br><i>Spell mastery</i> &mdash; If there are two or more instant and/or sorcery cards in your graveyard, search your library for up to three basic Forest cards instead of two.";
-    }
-
-    private NissasPilgrimageEffect(final NissasPilgrimageEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public NissasPilgrimageEffect copy() {
-        return new NissasPilgrimageEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Player controller = game.getPlayer(source.getControllerId());
-        MageObject sourceObject = source.getSourceObject(game);
-        if (controller != null && sourceObject != null) {
-            int number = 2;
-            if (SpellMasteryCondition.instance.apply(game, source)) {
-                number++;
-            }
-            TargetCardInLibrary target = new TargetCardInLibrary(0, number, filter);
-            controller.searchLibrary(target, source, game);
-            if (!target.getTargets().isEmpty()) {
-                Cards cards = new CardsImpl(target.getTargets());
-                controller.revealCards(sourceObject.getIdName(), cards, game);
-                if (!cards.isEmpty()) {
-                    Card card = cards.getRandom(game);
-                    if (card != null) {
-                        cards.remove(card);
-                        controller.moveCards(card, Zone.BATTLEFIELD, source, game, true, false, false, null);
-                        controller.moveCards(cards, Zone.HAND, source, game);
-                    }
-                }
-            }
-            controller.shuffleLibrary(source, game);
-            return true;
-        }
-        return false;
     }
 }
---
+++ Mage/src/main/java/mage/abilities/effects/common/search/SearchLibraryPutOneOntoBattlefieldTappedRestInHandEffect.java
@@ -0,0 +1,79 @@
+package mage.abilities.effects.common.search;
+
+import mage.MageObject;
+import mage.abilities.Ability;
+import mage.abilities.effects.SearchEffect;
+import mage.cards.Card;
+import mage.cards.Cards;
+import mage.cards.CardsImpl;
+import mage.constants.Outcome;
+import mage.constants.Zone;
+import mage.filter.FilterCard;
+import mage.game.Game;
+import mage.players.Player;
+import mage.target.TargetCard;
+import mage.target.common.TargetCardInLibrary;
+
+import java.util.Set;
+
+/**
+ * @author BetaSteward_at_googlemail.com, edited by Cguy7777
+ */
+public class SearchLibraryPutOneOntoBattlefieldTappedRestInHandEffect extends SearchEffect {
+
+    private static final FilterCard filter = new FilterCard("card to put on the battlefield tapped");
+
+    public SearchLibraryPutOneOntoBattlefieldTappedRestInHandEffect(TargetCardInLibrary target) {
+        super(target, Outcome.PutLandInPlay);
+        staticText = "search your library for " + target.getDescription() +
+                ", reveal those cards, put one onto the battlefield tapped and the other into your hand, then shuffle";
+    }
+
+    protected SearchLibraryPutOneOntoBattlefieldTappedRestInHandEffect(final SearchLibraryPutOneOntoBattlefieldTappedRestInHandEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public SearchLibraryPutOneOntoBattlefieldTappedRestInHandEffect copy() {
+        return new SearchLibraryPutOneOntoBattlefieldTappedRestInHandEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Player controller = game.getPlayer(source.getControllerId());
+        MageObject sourceObject = game.getObject(source);
+        if (controller == null || sourceObject == null) {
+            return false;
+        }
+
+        if (controller.searchLibrary(target, source, game)) {
+            if (!target.getTargets().isEmpty()) {
+                Cards revealed = new CardsImpl(target.getTargets());
+                controller.revealCards(sourceObject.getIdName(), revealed, game);
+
+                if (target.getTargets().size() >= 2) {
+                    TargetCard targetCardToBattlefield = new TargetCard(Zone.LIBRARY, filter);
+                    controller.choose(Outcome.PutLandInPlay, revealed, targetCardToBattlefield, source, game);
+                    Card cardToBattlefield = revealed.get(targetCardToBattlefield.getFirstTarget(), game);
+                    if (cardToBattlefield != null) {
+                        controller.moveCards(cardToBattlefield, Zone.BATTLEFIELD, source, game, true, false, false, null);
+                        revealed.remove(cardToBattlefield);
+                    }
+
+                    Set<Card> cardsToHand = revealed.getCards(game);
+                    controller.moveCards(cardsToHand, Zone.HAND, source, game);
+                } else if (target.getTargets().size() == 1) {
+                    Set<Card> cards = revealed.getCards(game);
+                    Card card = cards.isEmpty() ? null : cards.iterator().next();
+                    if (card != null) {
+                        controller.moveCards(card, Zone.BATTLEFIELD, source, game, true, false, false, null);
+                    }
+                }
+            }
+            controller.shuffleLibrary(source, game);
+            return true;
+        }
+        controller.shuffleLibrary(source, game);
+        return false;
+    }
+}
