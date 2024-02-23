512b1bcd336d3547700cebc5c78bc24cfd93ed89
theelk801 <theelk801@gmail.com>
2024-02-23 09:58:03 -0500

    [PIP] Implement Boomer Scrapper

--- Mage.Sets/src/mage/sets/Fallout.java
+++ Mage.Sets/src/mage/sets/Fallout.java
@@ -37,6 +37,7 @@ public final class Fallout extends ExpansionSet {
         cards.add(new SetCardInfo("Black Market", 183, Rarity.RARE, mage.cards.b.BlackMarket.class));
         cards.add(new SetCardInfo("Blasphemous Act", 188, Rarity.RARE, mage.cards.b.BlasphemousAct.class));
         cards.add(new SetCardInfo("Bloodforged Battle-Axe", 226, Rarity.RARE, mage.cards.b.BloodforgedBattleAxe.class));
+        cards.add(new SetCardInfo("Boomer Scrapper", 95, Rarity.RARE, mage.cards.b.BoomerScrapper.class));
         cards.add(new SetCardInfo("Brass Knuckles", 227, Rarity.UNCOMMON, mage.cards.b.BrassKnuckles.class));
         cards.add(new SetCardInfo("Break Down", 74, Rarity.UNCOMMON, mage.cards.b.BreakDown.class));
         cards.add(new SetCardInfo("Buried Ruin", 254, Rarity.UNCOMMON, mage.cards.b.BuriedRuin.class));
