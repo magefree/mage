dca9787cb981f8bfde368f0ff72c15b301a1fe06
theelk801 <theelk801@gmail.com>
2024-02-24 16:04:33 -0500

    [ACR] Implement Ezio, Blade of Vengeance

--- Mage.Sets/src/mage/sets/AssassinsCreed.java
+++ Mage.Sets/src/mage/sets/AssassinsCreed.java
@@ -23,6 +23,7 @@ public final class AssassinsCreed extends ExpansionSet {
 
         cards.add(new SetCardInfo("Cover of Darkness", 89, Rarity.RARE, mage.cards.c.CoverOfDarkness.class));
         cards.add(new SetCardInfo("Eivor, Battle-Ready", 274, Rarity.MYTHIC, mage.cards.e.EivorBattleReady.class));
+        cards.add(new SetCardInfo("Ezio, Blade of Vengeance", 275, Rarity.MYTHIC, mage.cards.e.EzioBladeOfVengeance.class));
         cards.add(new SetCardInfo("Haystack", 175, Rarity.UNCOMMON, mage.cards.h.Haystack.class));
         cards.add(new SetCardInfo("Sword of Feast and Famine", 99, Rarity.MYTHIC, mage.cards.s.SwordOfFeastAndFamine.class));
         cards.add(new SetCardInfo("Temporal Trespass", 86, Rarity.MYTHIC, mage.cards.t.TemporalTrespass.class));
