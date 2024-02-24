71b524be1bec645a58521ff74ad4b2395e431e78
theelk801 <theelk801@gmail.com>
2024-02-24 09:42:24 -0500

    [BIG] add set

---
+++ Mage.Sets/src/mage/sets/TheBigScore.java
@@ -0,0 +1,24 @@
+package mage.sets;
+
+import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
+import mage.constants.SetType;
+
+/**
+ * @author TheElk801
+ */
+public final class TheBigScore extends ExpansionSet {
+
+    private static final TheBigScore instance = new TheBigScore();
+
+    public static TheBigScore getInstance() {
+        return instance;
+    }
+
+    private TheBigScore() {
+        super("The Big Score", "BIG", ExpansionSet.buildDate(2024, 4,19), SetType.SUPPLEMENTAL_STANDARD_LEGAL);
+        this.blockName = "Outlaws of Thunder Junction";
+        this.hasBasicLands = false;
+        this.hasBoosters = false;
+    }
+}
