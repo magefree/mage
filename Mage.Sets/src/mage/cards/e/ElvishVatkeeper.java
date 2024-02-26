ddcd54c0df89247336f83f0a20a71575d708b386
PurpleCrowbar <26198472+PurpleCrowbar@users.noreply.github.com>
2024-02-26 05:12:14 +0000

    [BLB] Implement Byrke, Long Ear of the Law (#11860)
    New common class DoubleCountersTargetEffect

--- Mage.Sets/src/mage/cards/e/ElvishVatkeeper.java
+++ Mage.Sets/src/mage/cards/e/ElvishVatkeeper.java
@@ -5,20 +5,17 @@ import mage.abilities.Ability;
 import mage.abilities.common.EntersBattlefieldTriggeredAbility;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.mana.GenericManaCost;
-import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.DoubleCountersTargetEffect;
 import mage.abilities.effects.common.TransformTargetEffect;
 import mage.abilities.effects.keyword.IncubateEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.Outcome;
 import mage.constants.SubType;
 import mage.counters.CounterType;
 import mage.filter.FilterPermanent;
 import mage.filter.common.FilterControlledPermanent;
 import mage.filter.predicate.permanent.TokenPredicate;
-import mage.game.Game;
-import mage.game.permanent.Permanent;
 import mage.target.TargetPermanent;
 
 import java.util.UUID;
@@ -49,7 +46,7 @@ public final class ElvishVatkeeper extends CardImpl {
 
         // {5}: Transform target Incubator token you control. Double the number of +1/+1 counters on it.
         Ability ability = new SimpleActivatedAbility(new TransformTargetEffect(), new GenericManaCost(5));
-        ability.addEffect(new ElvishVatkeeperEffect());
+        ability.addEffect(new DoubleCountersTargetEffect(CounterType.P1P1));
         ability.addTarget(new TargetPermanent(filter));
         this.addAbility(ability);
     }
@@ -61,30 +58,5 @@ public final class ElvishVatkeeper extends CardImpl {
     @Override
     public ElvishVatkeeper copy() {
         return new ElvishVatkeeper(this);
-    }
-}
-
-class ElvishVatkeeperEffect extends OneShotEffect {
-
-    ElvishVatkeeperEffect() {
-        super(Outcome.Benefit);
-        staticText = "double the number of +1/+1 counters on it";
-    }
-
-    private ElvishVatkeeperEffect(final ElvishVatkeeperEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public ElvishVatkeeperEffect copy() {
-        return new ElvishVatkeeperEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Permanent permanent = game.getPermanent(getTargetPointer().getFirst(game, source));
-        return permanent != null && permanent.addCounters(CounterType.P1P1.createInstance(
-                permanent.getCounters(game).getCount(CounterType.P1P1)
-        ), source.getControllerId(), source, game);
     }
 }
