910f6fec068e48ff9f1bfd0e07f96682bc6cbc5a
tiera3 <87589219+tiera3@users.noreply.github.com>
2024-03-05 12:21:09 +1000

    Update SecretLairShowdown.java
    Fixed Dragonlord Slumigar collector number (from 19 -> 9)
    
    Also changed all collector numbers to numeric instead of strings.

--- Mage.Sets/src/mage/sets/SecretLairShowdown.java
+++ Mage.Sets/src/mage/sets/SecretLairShowdown.java
@@ -20,24 +20,24 @@ public class SecretLairShowdown extends ExpansionSet {
         this.hasBoosters = false;
         this.hasBasicLands = false;
 		
-        cards.add(new SetCardInfo("An Offer You Can't Refuse", "7", Rarity.RARE, mage.cards.a.AnOfferYouCantRefuse.class));
-        cards.add(new SetCardInfo("Brainstorm", "1", Rarity.RARE, mage.cards.b.Brainstorm.class));
-        cards.add(new SetCardInfo("Dark Ritual", "16", Rarity.RARE, mage.cards.d.DarkRitual.class));
-        cards.add(new SetCardInfo("Death's Shadow", "8", Rarity.RARE, mage.cards.d.DeathsShadow.class));
-        cards.add(new SetCardInfo("Dragonlord Silumgar", "19", Rarity.MYTHIC, mage.cards.d.DragonlordSilumgar.class));
-        cards.add(new SetCardInfo("Eldritch Evolution", "5", Rarity.RARE, mage.cards.e.EldritchEvolution.class));
-        cards.add(new SetCardInfo("Explore", "12", Rarity.RARE, mage.cards.e.Explore.class));
-        cards.add(new SetCardInfo("Expressive Iteration", "13", Rarity.RARE, mage.cards.e.ExpressiveIteration.class));
-        cards.add(new SetCardInfo("Fatal Push", "3", Rarity.RARE, mage.cards.f.FatalPush.class));
-        cards.add(new SetCardInfo("Murktide Regent", "17", Rarity.MYTHIC, mage.cards.m.MurktideRegent.class));
-        cards.add(new SetCardInfo("Ponder", "19", Rarity.RARE, mage.cards.p.Ponder.class));
-        cards.add(new SetCardInfo("Ragavan, Nimble Pilferer", "2", Rarity.MYTHIC, mage.cards.r.RagavanNimblePilferer.class));
-        cards.add(new SetCardInfo("Relentless Rats", "10", Rarity.RARE, mage.cards.r.RelentlessRats.class, NON_FULL_USE_VARIOUS));
-        cards.add(new SetCardInfo("Relentless Rats", "11", Rarity.RARE, mage.cards.r.RelentlessRats.class, NON_FULL_USE_VARIOUS));
-        cards.add(new SetCardInfo("Spell Pierce", "18", Rarity.RARE, mage.cards.s.SpellPierce.class));
-        cards.add(new SetCardInfo("Ugin, the Spirit Dragon", "6", Rarity.MYTHIC, mage.cards.u.UginTheSpiritDragon.class));
-        cards.add(new SetCardInfo("Unholy Heat", "4", Rarity.RARE, mage.cards.u.UnholyHeat.class));
-        cards.add(new SetCardInfo("Valakut, the Molten Pinnacle", "14", Rarity.RARE, mage.cards.v.ValakutTheMoltenPinnacle.class));
-        cards.add(new SetCardInfo("Wrenn and Six", "15", Rarity.MYTHIC, mage.cards.w.WrennAndSix.class));
+        cards.add(new SetCardInfo("An Offer You Can't Refuse", 7, Rarity.RARE, mage.cards.a.AnOfferYouCantRefuse.class));
+        cards.add(new SetCardInfo("Brainstorm", 1, Rarity.RARE, mage.cards.b.Brainstorm.class));
+        cards.add(new SetCardInfo("Dark Ritual", 16, Rarity.RARE, mage.cards.d.DarkRitual.class));
+        cards.add(new SetCardInfo("Death's Shadow", 8, Rarity.RARE, mage.cards.d.DeathsShadow.class));
+        cards.add(new SetCardInfo("Dragonlord Silumgar", 9, Rarity.MYTHIC, mage.cards.d.DragonlordSilumgar.class));
+        cards.add(new SetCardInfo("Eldritch Evolution", 5, Rarity.RARE, mage.cards.e.EldritchEvolution.class));
+        cards.add(new SetCardInfo("Explore", 12, Rarity.RARE, mage.cards.e.Explore.class));
+        cards.add(new SetCardInfo("Expressive Iteration", 13, Rarity.RARE, mage.cards.e.ExpressiveIteration.class));
+        cards.add(new SetCardInfo("Fatal Push", 3, Rarity.RARE, mage.cards.f.FatalPush.class));
+        cards.add(new SetCardInfo("Murktide Regent", 17, Rarity.MYTHIC, mage.cards.m.MurktideRegent.class));
+        cards.add(new SetCardInfo("Ponder", 19, Rarity.RARE, mage.cards.p.Ponder.class));
+        cards.add(new SetCardInfo("Ragavan, Nimble Pilferer", 2, Rarity.MYTHIC, mage.cards.r.RagavanNimblePilferer.class));
+        cards.add(new SetCardInfo("Relentless Rats", 10, Rarity.RARE, mage.cards.r.RelentlessRats.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Relentless Rats", 11, Rarity.RARE, mage.cards.r.RelentlessRats.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Spell Pierce", 18, Rarity.RARE, mage.cards.s.SpellPierce.class));
+        cards.add(new SetCardInfo("Ugin, the Spirit Dragon", 6, Rarity.MYTHIC, mage.cards.u.UginTheSpiritDragon.class));
+        cards.add(new SetCardInfo("Unholy Heat", 4, Rarity.RARE, mage.cards.u.UnholyHeat.class));
+        cards.add(new SetCardInfo("Valakut, the Molten Pinnacle", 14, Rarity.RARE, mage.cards.v.ValakutTheMoltenPinnacle.class));
+        cards.add(new SetCardInfo("Wrenn and Six", 15, Rarity.MYTHIC, mage.cards.w.WrennAndSix.class));
     }
 }
