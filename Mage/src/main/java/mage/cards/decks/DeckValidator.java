b6d1e2a93bab77ed8078dfbd82842c488e6d7ae8
tiera3 <87589219+tiera3@users.noreply.github.com>
2024-03-07 09:48:54 +1000

    enforce deck validator limit for Once More with Feeling (#11913)

--- Mage/src/main/java/mage/cards/decks/DeckValidator.java
+++ Mage/src/main/java/mage/cards/decks/DeckValidator.java
@@ -34,6 +34,7 @@ public abstract class DeckValidator implements Serializable {
         maxCopiesMap.put("Persistent Petitioners", Integer.MAX_VALUE);
         maxCopiesMap.put("Dragon's Approach", Integer.MAX_VALUE);
         maxCopiesMap.put("Slime Against Humanity", Integer.MAX_VALUE);
+        maxCopiesMap.put("Once More with Feeling", 1);
         maxCopiesMap.put("Seven Dwarves", 7);
         maxCopiesMap.put("Nazgul", 9);
     }
