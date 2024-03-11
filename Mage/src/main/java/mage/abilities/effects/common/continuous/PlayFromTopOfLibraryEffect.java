93247a32a2d8bbc5f2b6e8c48c56b480c963ba0d
xenohedron <xenohedron@users.noreply.github.com>
2024-03-10 23:21:10 -0400

    simplify common classes which were bloated to support a single card

--- Mage/src/main/java/mage/abilities/effects/common/continuous/PlayFromTopOfLibraryEffect.java
+++ Mage/src/main/java/mage/abilities/effects/common/continuous/PlayFromTopOfLibraryEffect.java
@@ -7,23 +7,19 @@ import mage.cards.Card;
 import mage.constants.AsThoughEffectType;
 import mage.constants.Duration;
 import mage.constants.Outcome;
-import mage.constants.TargetController;
 import mage.filter.FilterCard;
 import mage.game.Game;
 import mage.players.Player;
-import mage.util.CardUtil;
 
 import java.util.Locale;
-import java.util.Objects;
 import java.util.UUID;
 
 /**
- * @author nantuko, JayDi85
+ * @author nantuko, JayDi85, xenohedron
  */
 public class PlayFromTopOfLibraryEffect extends AsThoughEffectImpl {
 
     private final FilterCard filter;
-    private final TargetController targetLibrary;
 
     private static final FilterCard defaultFilter = new FilterCard("play lands and cast spells");
 
@@ -38,33 +34,9 @@ public class PlayFromTopOfLibraryEffect extends AsThoughEffectImpl {
      * You may [play lands and/or cast spells, according to filter] from the top of your library
      */
     public PlayFromTopOfLibraryEffect(FilterCard filter) {
-        this(TargetController.YOU, filter);
-    }
-
-    public PlayFromTopOfLibraryEffect(TargetController targetLibrary) {
-        this(targetLibrary, defaultFilter);
-    }
-
-    public PlayFromTopOfLibraryEffect(TargetController targetLibrary, FilterCard filter) {
         super(AsThoughEffectType.PLAY_FROM_NOT_OWN_HAND_ZONE, Duration.WhileOnBattlefield, Outcome.Benefit);
         this.filter = filter;
-        this.targetLibrary = targetLibrary;
-
-        String libInfo;
-        switch (this.targetLibrary) {
-            case YOU:
-                libInfo = "your library";
-                break;
-            case OPPONENT:
-                libInfo = "opponents libraries";
-                break;
-            case SOURCE_TARGETS:
-                libInfo = "target player's library";
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown target library type: " + targetLibrary);
-        }
-        this.staticText = "you may " + filter.getMessage() + " from the top of " + libInfo;
+        this.staticText = "you may " + filter.getMessage() + " from the top of your library";
 
         // verify check: this ability is to allow playing lands or casting spells, not playing a "card"
         if (filter.getMessage().toLowerCase(Locale.ENGLISH).contains("card")) {
@@ -75,7 +47,6 @@ public class PlayFromTopOfLibraryEffect extends AsThoughEffectImpl {
     protected PlayFromTopOfLibraryEffect(final PlayFromTopOfLibraryEffect effect) {
         super(effect);
         this.filter = effect.filter;
-        this.targetLibrary = effect.targetLibrary;
     }
 
     @Override
@@ -112,53 +83,16 @@ public class PlayFromTopOfLibraryEffect extends AsThoughEffectImpl {
         if (!playerId.equals(source.getControllerId())) {
             return false;
         }
-
         Player cardOwner = game.getPlayer(cardToCheck.getOwnerId());
         Player controller = game.getPlayer(source.getControllerId());
         if (cardOwner == null || controller == null) {
             return false;
         }
-
-        // main card of spell must be on top of the relevant library
-        switch (this.targetLibrary) {
-            case YOU: {
-                Card topCard = controller.getLibrary().getFromTop(game);
-                if (topCard == null || !topCard.getId().equals(cardToCheck.getMainCard().getId())) {
-                    return false;
-                }
-                break;
-            }
-
-            case OPPONENT: { // no current usage
-                if (!game.getOpponents(controller.getId()).contains(cardOwner.getId())) {
-                    return false;
-                }
-                Card topCard = cardOwner.getLibrary().getFromTop(game);
-                if (topCard == null || !topCard.getId().equals(cardToCheck.getMainCard().getId())) {
-                    return false;
-                }
-                break;
-            }
-
-            case SOURCE_TARGETS: { // only used for Xanathar, Guild Kingpin
-                UUID needCardId = cardToCheck.getMainCard().getId();
-                if (CardUtil.getAllSelectedTargets(source, game).stream()
-                        .map(game::getPlayer)
-                        .filter(Objects::nonNull)
-                        .noneMatch(player -> {
-                            Card topCard = player.getLibrary().getFromTop(game);
-                            return topCard != null && topCard.getId().equals(needCardId);
-                        })) {
-                    return false;
-                }
-                break;
-            }
-
-            default: {
-                return false;
-            }
+        // main card of spell must be on top of your library
+        Card topCard = controller.getLibrary().getFromTop(game);
+        if (topCard == null || !topCard.getId().equals(cardToCheck.getMainCard().getId())) {
+            return false;
         }
-
         // spell characteristics must match filter
         return filter.match(cardToCheck, playerId, source, game);
     }
