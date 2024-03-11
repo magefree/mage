a7f808afb6bb4de6cdf582c53dd47f4266e2e8d6
Cameron Merkel <44722506+Cguy7777@users.noreply.github.com>
2024-03-10 23:16:42 -0500

    Implement [MKM] Connecting the Dots; [PIP] Expert-Level Safe; related refactoring (#11922)
    * Switch to common class for exiling top cards of your library
    
    * Replace some custom classes with ReturnFromExileForSourceEffect; text-gen additions to that Effect
    
    * Knowledge Vault: add tap cost to 1st ability, fix text on 3rd ability; Petradon: remove unused import
    
    * [MKM] Implement Connecting the Dots
    
    * [PIP] Implement Expert-Level Safe
    
    * Set staticText in constructor
    
    * Use {this} in Expert-Level Safe text
    
    * Use createObjectRealtedWindowTitle for exile names
    
    * Remove unnecessary field

--- Mage.Sets/src/mage/cards/k/KnowledgeVault.java
+++ Mage.Sets/src/mage/cards/k/KnowledgeVault.java
@@ -2,25 +2,23 @@
 package mage.cards.k;
 
 import java.util.UUID;
-import mage.MageObject;
 import mage.abilities.Ability;
 import mage.abilities.common.LeavesBattlefieldTriggeredAbility;
 import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.TapSourceCost;
 import mage.abilities.costs.mana.GenericManaCost;
 import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.ExileCardsFromTopOfLibraryControllerEffect;
 import mage.abilities.effects.common.ReturnFromExileForSourceEffect;
 import mage.abilities.effects.common.discard.DiscardHandControllerEffect;
-import mage.cards.Card;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.Outcome;
 import mage.constants.Zone;
-import mage.game.ExileZone;
 import mage.game.Game;
 import mage.game.permanent.Permanent;
 import mage.players.Player;
-import mage.util.CardUtil;
 
 /**
  *
@@ -32,13 +30,20 @@ public final class KnowledgeVault extends CardImpl {
         super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT}, "{4}");
 
         // {2}, {T}: Exile the top card of your library face down.
-        this.addAbility(new SimpleActivatedAbility(Zone.BATTLEFIELD, new KnowledgeVaultExileEffect(), new GenericManaCost(2)));
+        Ability ability = new SimpleActivatedAbility(
+                Zone.BATTLEFIELD,
+                new ExileCardsFromTopOfLibraryControllerEffect(1, true, true),
+                new GenericManaCost(2));
+        ability.addCost(new TapSourceCost());
+        this.addAbility(ability);
 
         // {0}: Sacrifice Knowledge Vault. If you do, discard your hand, then put all cards exiled with Knowledge Vault into their owner’s hand.
         this.addAbility(new SimpleActivatedAbility(Zone.BATTLEFIELD, new KnowledgeVaultReturnEffect(), new GenericManaCost(0)));
 
         // When Knowledge Vault leaves the battlefield, put all cards exiled with Knowledge Vault into their owner’s graveyard.
-        this.addAbility(new LeavesBattlefieldTriggeredAbility(new ReturnFromExileForSourceEffect(Zone.GRAVEYARD), false));
+        this.addAbility(new LeavesBattlefieldTriggeredAbility(
+                new ReturnFromExileForSourceEffect(Zone.GRAVEYARD).withText(true, false, true),
+                false));
     }
 
     private KnowledgeVault(final KnowledgeVault card) {
@@ -48,40 +53,6 @@ public final class KnowledgeVault extends CardImpl {
     @Override
     public KnowledgeVault copy() {
         return new KnowledgeVault(this);
-    }
-}
-
-class KnowledgeVaultExileEffect extends OneShotEffect {
-
-    KnowledgeVaultExileEffect() {
-        super(Outcome.Exile);
-        this.staticText = "exile the top card of your library face down";
-    }
-
-    private KnowledgeVaultExileEffect(final KnowledgeVaultExileEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public KnowledgeVaultExileEffect copy() {
-        return new KnowledgeVaultExileEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Player controller = game.getPlayer(source.getControllerId());
-        MageObject sourceObject = source.getSourceObject(game);
-        if (controller != null && sourceObject != null) {
-            Card card = controller.getLibrary().getFromTop(game);
-            if (card != null) {
-                UUID exileZoneId = CardUtil.getExileZoneId(game, source.getSourceId(), source.getSourceObjectZoneChangeCounter());
-                card.setFaceDown(true, game);
-                controller.moveCardsToExile(card, source, game, false, exileZoneId, sourceObject.getIdName());
-                card.setFaceDown(true, game);
-                return true;
-            }
-        }
-        return false;
     }
 }
 
@@ -108,10 +79,7 @@ class KnowledgeVaultReturnEffect extends OneShotEffect {
         if (sourcePermanent != null && controller != null) {
             if (sourcePermanent.sacrifice(source, game)) {
                 new DiscardHandControllerEffect().apply(game, source);
-                ExileZone exileZone = game.getExile().getExileZone(CardUtil.getExileZoneId(game, source.getSourceId(), source.getSourceObjectZoneChangeCounter()));
-                if (exileZone != null) {
-                    controller.moveCards(exileZone, Zone.HAND, source, game);
-                }
+                new ReturnFromExileForSourceEffect(Zone.HAND).apply(game, source);
             }
             return true;
         }
