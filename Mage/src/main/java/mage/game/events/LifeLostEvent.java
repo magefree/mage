196d232e8b47d3a9accae47f93a55b62a48a7ed9
jimga150 <jimga150@users.noreply.github.com>
2024-03-20 23:21:21 -0400

    add author to LifeLostEvent

--- Mage/src/main/java/mage/game/events/LifeLostEvent.java
+++ Mage/src/main/java/mage/game/events/LifeLostEvent.java
@@ -4,6 +4,9 @@ import mage.abilities.Ability;
 
 import java.util.UUID;
 
+/**
+ * @author jimga150
+ */
 public class LifeLostEvent extends GameEvent{
     public LifeLostEvent(UUID playerId, Ability source, int amount, boolean atCombat){
         super(GameEvent.EventType.LOST_LIFE,
