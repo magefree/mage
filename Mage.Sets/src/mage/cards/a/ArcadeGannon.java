2571d822b3ca09164f5acd8f4ca0dab505111ed3
justin <42110866+justinjohnson14@users.noreply.github.com>
2024-03-23 23:41:01 -0700

    Implement Arcade Gannon

--- Mage.Sets/src/mage/cards/a/ArcadeGannon.java
+++ Mage.Sets/src/mage/cards/a/ArcadeGannon.java
@@ -1,52 +1,22 @@
 package mage.cards.a;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.UUID;
 
-import mage.MageIdentifier;
 import mage.MageInt;
-import mage.MageObjectReference;
 import mage.abilities.Ability;
-import mage.abilities.SpellAbility;
 import mage.abilities.common.*;
-import mage.abilities.common.delayed.ReflexiveTriggeredAbility;
-import mage.abilities.condition.Condition;
 import mage.abilities.costs.common.TapSourceCost;
-import mage.abilities.decorator.ConditionalActivatedAbility;
-import mage.abilities.decorator.ConditionalInterveningIfTriggeredAbility;
 import mage.abilities.dynamicvalue.DynamicValue;
 import mage.abilities.dynamicvalue.common.CountersSourceCount;
-import mage.abilities.effects.AsThoughEffectImpl;
-import mage.abilities.effects.OneShotEffect;
-import mage.abilities.effects.common.DestroyTargetEffect;
 import mage.abilities.effects.common.DrawDiscardControllerEffect;
-import mage.abilities.effects.common.PreventDamageToTargetEffect;
 import mage.abilities.effects.common.counter.AddCountersSourceEffect;
-import mage.cards.Card;
-import mage.cards.CardsImpl;
 import mage.constants.*;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.counters.CounterType;
 import mage.filter.FilterCard;
-import mage.filter.FilterPermanent;
-import mage.filter.FilterSpell;
-import mage.filter.common.FilterNonlandPermanent;
 import mage.filter.predicate.Predicates;
-import mage.filter.predicate.card.ManaValueLessThanOrEqualToSourcePowerPredicate;
-import mage.filter.predicate.mageobject.ManaValuePredicate;
-import mage.game.Game;
-import mage.game.events.GameEvent;
-import mage.game.permanent.Permanent;
-import mage.game.stack.Spell;
-import mage.players.Player;
-import mage.target.TargetPermanent;
-import mage.util.CardUtil;
-import mage.watchers.Watcher;
-import mage.watchers.common.CardsAmountDrawnThisTurnWatcher;
-import mage.watchers.common.CastFromGraveyardWatcher;
-import mage.watchers.common.OnceEachTurnCastWatcher;
+import mage.filter.predicate.card.ManaValueLessThanOrEqualToSourceQuestCounterPredicate;
 
 /**
  *
@@ -54,7 +24,7 @@ import mage.watchers.common.OnceEachTurnCastWatcher;
  */
 public final class ArcadeGannon extends CardImpl {
 
-    private static final FilterCard filter = new FilterCard();
+    private static final FilterCard filter = new FilterCard("an artifact or Human spell from your graveyard with mana value less than or equal to the number of quest counters on Arcade Gannon");
 
     private static final DynamicValue xValue = new CountersSourceCount(CounterType.QUEST);
 
@@ -63,8 +33,7 @@ public final class ArcadeGannon extends CardImpl {
                 CardType.ARTIFACT.getPredicate(),
                 SubType.HUMAN.getPredicate()
         ));
-        filter.add(ManaValueLessThanOrEqualToSourcePowerPredicate.instance);
-        filter.withMessage("an artifact or Human spell from your graveyard with mana value less than or equal to the number of quest counters on Arcade Gannon.");
+        filter.add(ManaValueLessThanOrEqualToSourceQuestCounterPredicate.instance);
     }
 
     public ArcadeGannon(UUID ownerId, CardSetInfo setInfo) {
@@ -81,9 +50,7 @@ public final class ArcadeGannon extends CardImpl {
         ability.addEffect(new AddCountersSourceEffect(CounterType.QUEST.createInstance(1)));
         this.addAbility(ability);
         // For Auld Lang Syne -- Once during each of your turns, you may cast an artifact or Human spell from your graveyard with mana value less than or equal to the number of quest counters on Arcade Gannon.
-        Ability ability1 = new CastFromGraveyardOnceEachTurnAbility(filter);
-        ability1.withFlavorWord("For Auld Lang Syne");
-        this.addAbility(ability1);
+        this.addAbility(new CastFromGraveyardOnceEachTurnAbility(filter).withFlavorWord("For Auld Lang Syne"));
     }
 
     private ArcadeGannon(final ArcadeGannon card) {
