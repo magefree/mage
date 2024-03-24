c16a572971c3fbb455bc22288e047fcd2dcc1165
justin <42110866+justinjohnson14@users.noreply.github.com>
2024-03-23 18:55:47 -0700

    Implement Brotherhood Vertibird

---
+++ Mage.Sets/src/mage/cards/b/BrotherhoodVertibird.java
@@ -0,0 +1,54 @@
+package mage.cards.b;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.dynamicvalue.common.PermanentsOnBattlefieldCount;
+import mage.abilities.effects.common.continuous.SetBasePowerSourceEffect;
+import mage.constants.SubType;
+import mage.abilities.keyword.FlyingAbility;
+import mage.abilities.keyword.CrewAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Zone;
+import mage.filter.common.FilterControlledPermanent;
+
+/**
+ *
+ * @author justinjohnson14
+ */
+public final class BrotherhoodVertibird extends CardImpl {
+
+    private static final FilterControlledPermanent filter = new FilterControlledPermanent("artifacts you control");
+
+    static {
+        filter.add(CardType.ARTIFACT.getPredicate());
+    }
+
+    public BrotherhoodVertibird(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT}, "{3}");
+        
+        this.subtype.add(SubType.VEHICLE);
+        this.power = new MageInt(0);
+        this.toughness = new MageInt(4);
+
+        // Flying
+        this.addAbility(FlyingAbility.getInstance());
+
+        // Brotherhood Vertibird's power is equal to the number of artifacts you control.
+        this.addAbility(new SimpleStaticAbility(Zone.ALL, new SetBasePowerSourceEffect(new PermanentsOnBattlefieldCount(filter))));
+        // Crew 2
+        this.addAbility(new CrewAbility(2));
+
+    }
+
+    private BrotherhoodVertibird(final BrotherhoodVertibird card) {
+        super(card);
+    }
+
+    @Override
+    public BrotherhoodVertibird copy() {
+        return new BrotherhoodVertibird(this);
+    }
+}
