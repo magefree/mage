c4c768b8416c5bdb54a5bed9910fcc51e868bacf
theelk801 <theelk801@gmail.com>
2024-03-27 13:00:04 -0400

    [OTC] Implement Stella Lee, Wild Card

--- Mage.Sets/src/mage/sets/OutlawsOfThunderJunctionCommander.java
+++ Mage.Sets/src/mage/sets/OutlawsOfThunderJunctionCommander.java
@@ -1,6 +1,7 @@
 package mage.sets;
 
 import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
 import mage.constants.SetType;
 
 /**
@@ -17,5 +18,7 @@ public final class OutlawsOfThunderJunctionCommander extends ExpansionSet {
     private OutlawsOfThunderJunctionCommander() {
         super("Outlaws of Thunder Junction Commander", "OTC", ExpansionSet.buildDate(2024, 4, 19), SetType.SUPPLEMENTAL);
         this.hasBasicLands = false;
+
+        cards.add(new SetCardInfo("Stella Lee, Wild Card", 3, Rarity.MYTHIC, mage.cards.s.StellaLeeWildCard.class));
     }
 }
