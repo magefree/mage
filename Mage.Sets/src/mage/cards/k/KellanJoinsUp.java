ceaa526b79241e2962b364effc69f60f8c2a512d
Susucre <34709007+Susucre@users.noreply.github.com>
2024-03-29 18:46:56 +0100

    [OTJ] Implement Jace Reawakened

--- Mage.Sets/src/mage/cards/k/KellanJoinsUp.java
+++ Mage.Sets/src/mage/cards/k/KellanJoinsUp.java
@@ -1,17 +1,13 @@
 package mage.cards.k;
 
-import mage.abilities.Ability;
 import mage.abilities.common.EntersBattlefieldControlledTriggeredAbility;
 import mage.abilities.common.EntersBattlefieldTriggeredAbility;
-import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.MayExileCardFromHandPlottedEffect;
 import mage.abilities.effects.common.counter.AddCountersAllEffect;
-import mage.abilities.keyword.PlotAbility;
-import mage.cards.Card;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.ComparisonType;
-import mage.constants.Outcome;
 import mage.constants.SuperType;
 import mage.counters.CounterType;
 import mage.filter.FilterCard;
@@ -19,9 +15,6 @@ import mage.filter.StaticFilters;
 import mage.filter.common.FilterCreaturePermanent;
 import mage.filter.predicate.Predicates;
 import mage.filter.predicate.mageobject.ManaValuePredicate;
-import mage.game.Game;
-import mage.players.Player;
-import mage.target.common.TargetCardInHand;
 
 import java.util.UUID;
 
@@ -31,9 +24,12 @@ import java.util.UUID;
 public final class KellanJoinsUp extends CardImpl {
 
     private static final FilterCreaturePermanent filter = new FilterCreaturePermanent("legendary creature");
+    private static final FilterCard filterCard = new FilterCard("nonland card with mana value 3 or less");
 
     static {
         filter.add(SuperType.LEGENDARY.getPredicate());
+        filterCard.add(Predicates.not(CardType.LAND.getPredicate()));
+        filterCard.add(new ManaValuePredicate(ComparisonType.OR_LESS, 3));
     }
 
     public KellanJoinsUp(UUID ownerId, CardSetInfo setInfo) {
@@ -42,7 +38,7 @@ public final class KellanJoinsUp extends CardImpl {
         this.supertype.add(SuperType.LEGENDARY);
 
         // When Kellan Joins Up enters the battlefield, you may exile a nonland card with mana value 3 or less from your hand. If you do, it becomes plotted.
-        this.addAbility(new EntersBattlefieldTriggeredAbility(new KellanJoinsUpEffect()));
+        this.addAbility(new EntersBattlefieldTriggeredAbility(new MayExileCardFromHandPlottedEffect(filterCard)));
 
         // Whenever a legendary creature enters the battlefield under your control, put a +1/+1 counter on each creature you control.
         this.addAbility(new EntersBattlefieldControlledTriggeredAbility(
@@ -60,45 +56,5 @@ public final class KellanJoinsUp extends CardImpl {
     @Override
     public KellanJoinsUp copy() {
         return new KellanJoinsUp(this);
-    }
-}
-
-class KellanJoinsUpEffect extends OneShotEffect {
-
-    private static final FilterCard filter = new FilterCard("nonland card with mana value 3 or less");
-
-    static {
-        filter.add(Predicates.not(CardType.LAND.getPredicate()));
-        filter.add(new ManaValuePredicate(ComparisonType.OR_LESS, 3));
-    }
-
-    public KellanJoinsUpEffect() {
-        super(Outcome.PutCardInPlay);
-        this.staticText = "you may exile a nonland card with mana value 3 or less from your hand. If you do, it becomes plotted";
-    }
-
-    private KellanJoinsUpEffect(final KellanJoinsUpEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public KellanJoinsUpEffect copy() {
-        return new KellanJoinsUpEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Player player = game.getPlayer(source.getControllerId());
-        if (player == null) {
-            return false;
-        }
-        TargetCardInHand target = new TargetCardInHand(0, 1, filter);
-        if (player.chooseTarget(outcome, target, source, game)) {
-            Card card = game.getCard(target.getFirstTarget());
-            if (card != null) {
-                PlotAbility.doExileAndPlotCard(card, game, source);
-            }
-        }
-        return true;
     }
 }
