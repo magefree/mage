04e5f9980720fa6ccd4239d5e6d6b0f65237abdd
jimga150 <jimga150@users.noreply.github.com>
2024-03-30 21:54:45 -0400

    incorporate simple fixes - these cards use no methods that aren't re-implemented in DamagedBatchForOnePermanentEvent

--- Mage.Sets/src/mage/cards/p/PhyrexianNegator.java
+++ Mage.Sets/src/mage/cards/p/PhyrexianNegator.java
@@ -64,7 +64,7 @@ class PhyrexianNegatorTriggeredAbility extends TriggeredAbilityImpl {
 
     @Override
     public boolean checkEventType(GameEvent event, Game game) {
-        return event.getType() == GameEvent.EventType.DAMAGED_PERMANENT;
+        return event.getType() == GameEvent.EventType.DAMAGED_BATCH_FOR_ONE_PERMANENT;
     }
     
     @Override
