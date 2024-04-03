616ab58212daa83f91d0802b5392cd367693fef2
DarkNik <mitakidisnikos@gmail.com>
2024-04-03 15:28:34 +0300

    [PIP] Implement Vault 75: Middle School

--- Mage.Sets/src/mage/cards/v/Vault75MiddleSchool.java
+++ Mage.Sets/src/mage/cards/v/Vault75MiddleSchool.java
@@ -3,6 +3,7 @@ package mage.cards.v;
 import mage.abilities.Ability;
 import mage.abilities.common.SagaAbility;
 import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.ExileAllEffect;
 import mage.abilities.effects.common.counter.AddCountersAllEffect;
 import mage.cards.*;
 import mage.constants.*;
@@ -24,6 +25,11 @@ import java.util.UUID;
  */
 public final class Vault75MiddleSchool extends CardImpl {
 
+    private static final FilterCreaturePermanent filterCreatureWithPower4OrGreater = new FilterCreaturePermanent("creatures with power 4 or greater");
+
+    static {
+        filterCreatureWithPower4OrGreater.add(new PowerPredicate(ComparisonType.MORE_THAN, 3));
+    }
     public Vault75MiddleSchool(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{2}{W}{W}");
         
@@ -33,7 +39,7 @@ public final class Vault75MiddleSchool extends CardImpl {
         SagaAbility sagaAbility = new SagaAbility(this);
 
         // I -- Exile all creatures with power 4 or greater.
-        sagaAbility.addChapterEffect(this, SagaChapter.CHAPTER_I, new Vault75MiddleSchoolChapterIEffect());
+        sagaAbility.addChapterEffect(this, SagaChapter.CHAPTER_I, new ExileAllEffect(filterCreatureWithPower4OrGreater));
 
         // II, III -- Put a +1/+1 counter on each creature you control.
         sagaAbility.addChapterEffect(this, SagaChapter.CHAPTER_II, SagaChapter.CHAPTER_III,
@@ -50,49 +56,4 @@ public final class Vault75MiddleSchool extends CardImpl {
     @Override
     public Vault75MiddleSchool copy() { return new Vault75MiddleSchool(this); }
 
-}
-
-class Vault75MiddleSchoolChapterIEffect extends OneShotEffect {
-
-    private static final FilterCreaturePermanent filter = new FilterCreaturePermanent("creatures with power 4 or greater");
-
-    static {
-        filter.add(new PowerPredicate(ComparisonType.OR_GREATER, 4));
-    }
-
-    public Vault75MiddleSchoolChapterIEffect() {
-        super(Outcome.Benefit);
-        this.staticText = "exile all creatures with power 4 or greater";
-    }
-
-    private Vault75MiddleSchoolChapterIEffect(final mage.cards.v.Vault75MiddleSchoolChapterIEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public mage.cards.v.Vault75MiddleSchoolChapterIEffect copy() {
-        return new mage.cards.v.Vault75MiddleSchoolChapterIEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Player controller = game.getPlayer(source.getControllerId());
-        if (controller == null) {
-            return false;
-        }
-
-        Permanent permanent = source.getSourcePermanentIfItStillExists(game);
-        if (permanent == null) {
-            return false;
-        }
-
-        Set<Card> toExile = new LinkedHashSet<>(game.getBattlefield().getActivePermanents(filter, controller.getId(), source, game));
-        if (toExile.isEmpty()) {
-            return false;
-        }
-
-        controller.moveCardsToExile(toExile, source, game, true, CardUtil.getCardExileZoneId(game, source), permanent.getIdName());
-        return true;
-
-    }
 }
