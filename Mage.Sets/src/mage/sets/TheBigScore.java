ef7276824df90e7b237100f52d28d2b6fb856fbd
theelk801 <theelk801@gmail.com>
2024-04-03 13:42:57 -0400

    [BIG] Implement Nexus of Becoming

--- Mage.Sets/src/mage/sets/TheBigScore.java
+++ Mage.Sets/src/mage/sets/TheBigScore.java
@@ -31,6 +31,7 @@ public final class TheBigScore extends ExpansionSet {
         cards.add(new SetCardInfo("Lost Jitte", 53, Rarity.MYTHIC, mage.cards.l.LostJitte.class));
         cards.add(new SetCardInfo("Lotus Ring", 24, Rarity.MYTHIC, mage.cards.l.LotusRing.class));
         cards.add(new SetCardInfo("Molten Duplication", 14, Rarity.MYTHIC, mage.cards.m.MoltenDuplication.class));
+        cards.add(new SetCardInfo("Nexus of Becoming", 25, Rarity.MYTHIC, mage.cards.n.NexusOfBecoming.class));
         cards.add(new SetCardInfo("Oltec Matterweaver", 3, Rarity.MYTHIC, mage.cards.o.OltecMatterweaver.class));
         cards.add(new SetCardInfo("Omenpath Journey", 18, Rarity.MYTHIC, mage.cards.o.OmenpathJourney.class));
         cards.add(new SetCardInfo("Pest Control", 22, Rarity.MYTHIC, mage.cards.p.PestControl.class));
