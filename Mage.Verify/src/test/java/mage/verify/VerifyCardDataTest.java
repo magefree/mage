63320c1a54a4ad0c14999e86b6fa46421038dc42
Susucre <34709007+Susucre@users.noreply.github.com>
2024-04-04 12:02:52 +0200

    Temporary skip for Verify Test

--- Mage.Verify/src/test/java/mage/verify/VerifyCardDataTest.java
+++ Mage.Verify/src/test/java/mage/verify/VerifyCardDataTest.java
@@ -148,7 +148,8 @@ public class VerifyCardDataTest {
         skipListAddName(SKIP_LIST_SUBTYPE, "UGL", "Miss Demeanor"); // uses multiple types as a joke card: Lady, of, Proper, Etiquette
         skipListAddName(SKIP_LIST_SUBTYPE, "UGL", "Elvish Impersonators"); // subtype is "Elves" pun
         skipListAddName(SKIP_LIST_SUBTYPE, "UND", "Elvish Impersonators");
-        skipListAddName(SKIP_LIST_SUBTYPE, "OTJ", "Stop Cold");
+        skipListAddName(SKIP_LIST_SUBTYPE, "OTJ", "Stop Cold"); // tmp
+        skipListAddName(SKIP_LIST_SUBTYPE, "OTC", "Stella Lee, Wild Card"); // tmp
 
         // number
         // skipListAddName(SKIP_LIST_NUMBER, set, cardName);
