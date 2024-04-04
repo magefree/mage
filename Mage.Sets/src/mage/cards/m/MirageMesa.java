dd3057fa1058bef8934c94982f1d7e09a61974a8
Susucre <34709007+Susucre@users.noreply.github.com>
2024-04-04 12:47:35 +0200

    [BIG] Implement Tarnation Vista

--- Mage.Sets/src/mage/cards/m/MirageMesa.java
+++ Mage.Sets/src/mage/cards/m/MirageMesa.java
@@ -1,15 +1,12 @@
 package mage.cards.m;
 
-import mage.abilities.Ability;
-import mage.abilities.common.EntersBattlefieldTappedAbility;
+import mage.abilities.common.EntersBattlefieldTappedAsItEntersChooseColorAbility;
 import mage.abilities.costs.common.TapSourceCost;
-import mage.abilities.effects.common.ChooseColorEffect;
 import mage.abilities.effects.mana.AddManaChosenColorEffect;
 import mage.abilities.mana.SimpleManaAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.Outcome;
 import mage.constants.SubType;
 import mage.constants.Zone;
 
@@ -26,11 +23,7 @@ public final class MirageMesa extends CardImpl {
         this.subtype.add(SubType.DESERT);
 
         // Mirage Mesa enters the battlefield tapped. As it enters, choose a color.
-        Ability ability = new EntersBattlefieldTappedAbility(
-                "{this} enters the battlefield tapped. As it enters, choose a color."
-        );
-        ability.addEffect(new ChooseColorEffect(Outcome.Benefit));
-        this.addAbility(ability);
+        this.addAbility(new EntersBattlefieldTappedAsItEntersChooseColorAbility());
 
         // {T}: Add one mana of the chosen color.
         this.addAbility(new SimpleManaAbility(Zone.BATTLEFIELD, new AddManaChosenColorEffect(), new TapSourceCost()));
