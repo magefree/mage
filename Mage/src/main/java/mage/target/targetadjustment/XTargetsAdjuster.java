74213c375ad1912077c542fc518fa22c34843aa4
Steven Knipe <knipesteven@gmail.com>
2024-04-09 20:14:34 -0700

    Convert XTargetsAdjuster to use DynamicValueTargetsAdjuster

--- Mage/src/main/java/mage/target/targetadjustment/XTargetsAdjuster.java
+++ Mage/src/main/java/mage/target/targetadjustment/XTargetsAdjuster.java
@@ -1,30 +1,13 @@
 package mage.target.targetadjustment;
 
-import mage.abilities.Ability;
-import mage.game.Game;
-import mage.target.Target;
+import mage.abilities.dynamicvalue.common.ManacostVariableValue;
 
 /**
- *
- * @author TheElk801, notgreat
+ * @author notgreat
  */
-public class XTargetsAdjuster implements TargetAdjuster {
-    private Target blueprintTarget = null;
+public class XTargetsAdjuster extends DynamicValueTargetsAdjuster {
 
     public XTargetsAdjuster() {
-    }
-
-    @Override
-    public void adjustTargets(Ability ability, Game game) {
-        if (blueprintTarget == null) {
-            blueprintTarget = ability.getTargets().get(0).copy();
-        }
-        Target newTarget = blueprintTarget.copy();
-        newTarget.setMaxNumberOfTargets(ability.getManaCostsToPay().getX());
-        if (blueprintTarget.getMinNumberOfTargets() != 0) {
-            newTarget.setMinNumberOfTargets(ability.getManaCostsToPay().getX());
-        }
-        ability.getTargets().clear();
-        ability.addTarget(newTarget);
+        super(ManacostVariableValue.REGULAR);
     }
 }
