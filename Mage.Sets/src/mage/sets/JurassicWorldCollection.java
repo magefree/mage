444a15df5d8b0980f01cac0fe0f941d2f6c913d3
jimga150 <jimga150@users.noreply.github.com>
2024-04-10 23:43:06 -0400

    Implement [REX] Cresting Mosasaurus (#12100)

--- Mage.Sets/src/mage/sets/JurassicWorldCollection.java
+++ Mage.Sets/src/mage/sets/JurassicWorldCollection.java
@@ -22,6 +22,7 @@ public final class JurassicWorldCollection extends ExpansionSet {
 
         cards.add(new SetCardInfo("Command Tower", 26, Rarity.COMMON, mage.cards.c.CommandTower.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Command Tower", "26b", Rarity.COMMON, mage.cards.c.CommandTower.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Cresting Mosasaurus", 2, Rarity.RARE, mage.cards.c.CrestingMosasaurus.class));
         cards.add(new SetCardInfo("Dino DNA", 20, Rarity.RARE, mage.cards.d.DinoDNA.class));
         cards.add(new SetCardInfo("Don't Move", 1, Rarity.RARE, mage.cards.d.DontMove.class));
         cards.add(new SetCardInfo("Ellie and Alan, Paleontologists", 10, Rarity.RARE, mage.cards.e.EllieAndAlanPaleontologists.class));
