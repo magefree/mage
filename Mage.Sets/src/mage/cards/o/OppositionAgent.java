fc011ac7e0ff93955633ea1fcaef43b950cb60dd
Susucre <34709007+Susucre@users.noreply.github.com>
2024-04-13 12:59:52 +0200

    fix Opposition Agent (#10576)
    Fix #10576

--- Mage.Sets/src/mage/cards/o/OppositionAgent.java
+++ Mage.Sets/src/mage/cards/o/OppositionAgent.java
@@ -77,9 +77,9 @@ class OppositionAgentReplacementEffect extends ReplacementEffectImpl {
         if (!event.getTargetId().equals(event.getPlayerId())) {
             return false;
         }
-
+        Player controller = game.getPlayer(source.getControllerId());
         Player targetPlayer = game.getPlayer(event.getTargetId());
-        if (targetPlayer == null) {
+        if (targetPlayer == null || controller == null) {
             return false;
         }
 
@@ -103,8 +103,8 @@ class OppositionAgentReplacementEffect extends ReplacementEffectImpl {
 
         // You may play those cards for as long as they remain exiled, and you may spend mana as though it were mana of any color to cast them
         for (Card card : cardsToExile) {
-            // the source ability is tied to the effect so we need to keep it active to work correctly
-            CardUtil.makeCardPlayable(game, source, card, false, Duration.EndOfGame, true);
+            CardUtil.makeCardPlayable(game, source, card, false,
+                    Duration.EndOfGame, true, controller.getId(), null);
         }
 
         // return false all the time
