641d0b6855079bfc2b9517b982a458a69a88b7b7
PurpleCrowbar <26198472+PurpleCrowbar@users.noreply.github.com>
2024-04-14 23:29:08 +0100

    Remove filter check, clean up code

--- Mage/src/main/java/mage/game/permanent/PermanentImpl.java
+++ Mage/src/main/java/mage/game/permanent/PermanentImpl.java
@@ -1352,20 +1352,14 @@ public abstract class PermanentImpl extends CardImpl implements Permanent {
             }
         }
 
-        boolean canAttach = true;
+        boolean canAttach;
         Permanent attachmentPermanent = game.getPermanent(attachment.getId());
-        if (attachment.hasSubtype(SubType.AURA, game)
+        canAttach = (attachment.hasSubtype(SubType.AURA, game)
                 && attachmentPermanent != null
                 && attachmentPermanent.getSpellAbility() != null
-                && !attachmentPermanent.getSpellAbility().getTargets().isEmpty()) {
-            Filter filter = attachmentPermanent.getSpellAbility().getTargets().get(0).getFilter();
-            if (filter instanceof FilterPermanent) {
-                canAttach = attachmentPermanent.getSpellAbility().getTargets().get(0).canTarget(attachmentPermanent.getControllerId(), this.getId(), source, game);
-            }
-            else {
-                canAttach = false;
-            }
-        }
+                && !attachmentPermanent.getSpellAbility().getTargets().isEmpty()
+                && attachmentPermanent.getSpellAbility().getTargets().get(0).canTarget(attachmentPermanent.getControllerId(), this.getId(), source, game)
+        );
 
         return !canAttach || game.getContinuousEffects().preventedByRuleModification(new StayAttachedEvent(this.getId(), attachment.getId(), source), null, game, silentMode);
     }
