794604ab0c428acb22e8c70cfebbb40f4b10bc72
jimga150 <jimga150@users.noreply.github.com>
2024-04-14 21:05:15 -0400

    fix test and add blink test

--- Mage.Tests/src/test/java/org/mage/test/cards/watchers/IanMalcolmChaoticianTests.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/watchers/IanMalcolmChaoticianTests.java
@@ -14,6 +14,8 @@ public class IanMalcolmChaoticianTests extends CardTestCommander4Players {
     @Test
     public void testManaCostsandWatcher() {
 
+        skipInitShuffling();
+
         // Whenever a player draws their second card each turn, that player exiles the top card of their library.
         // During each player's turn, that player may cast a spell from among the cards they don't own exiled with
         // Ian Malcolm, Chaotician, and mana of any type can be spent to cast it.
@@ -32,9 +34,7 @@ public class IanMalcolmChaoticianTests extends CardTestCommander4Players {
         addCard(Zone.LIBRARY, playerC, "Chromanticore", 10);
         addCard(Zone.LIBRARY, playerD, "Garth One-Eye", 10);
 
-        addCard(Zone.BATTLEFIELD, playerA, "Swamp", 15);
-
-        //addCard(Zone.HAND, playerA, "Ephemerate");
+        addCard(Zone.BATTLEFIELD, playerA, "Plains", 15);
 
         // Should be able to cast all cards exiled from other players' decks
         checkPlayableAbility("Can't cast card exiled with this Ian Malcolm", 5, PhaseStep.PRECOMBAT_MAIN, playerA, "Cast Fusion Elemental", true);
@@ -54,8 +54,55 @@ public class IanMalcolmChaoticianTests extends CardTestCommander4Players {
         setStrictChooseMode(true);
         execute();
 
-        assertHandCount(playerA, "Horde of Notions", 0);
-        
+    }
+
+    @Test
+    public void testBlink() {
+
+        skipInitShuffling();
+
+        // Whenever a player draws their second card each turn, that player exiles the top card of their library.
+        // During each player's turn, that player may cast a spell from among the cards they don't own exiled with
+        // Ian Malcolm, Chaotician, and mana of any type can be spent to cast it.
+        addCard(Zone.BATTLEFIELD, playerA, "Ian Malcolm, Chaotician");
+
+        // Flying
+        // At the beginning of your draw step, draw an additional card.
+        // At the beginning of your end step, discard your hand.
+        addCard(Zone.BATTLEFIELD, playerA, "Heightened Awareness");
+        addCard(Zone.BATTLEFIELD, playerB, "Heightened Awareness");
+        addCard(Zone.BATTLEFIELD, playerC, "Heightened Awareness");
+        addCard(Zone.BATTLEFIELD, playerD, "Heightened Awareness");
+
+        addCard(Zone.LIBRARY, playerA, "Horde of Notions", 3);
+        addCard(Zone.LIBRARY, playerA, "Ephemerate", 1);
+        addCard(Zone.LIBRARY, playerB, "Fusion Elemental", 10);
+        addCard(Zone.LIBRARY, playerC, "Chromanticore", 10);
+        addCard(Zone.LIBRARY, playerD, "Garth One-Eye", 10);
+
+        addCard(Zone.BATTLEFIELD, playerA, "Plains", 15);
+
+        // Should be able to cast all cards exiled from other players' decks
+        checkPlayableAbility("Can't cast card exiled with this Ian Malcolm", 5, PhaseStep.PRECOMBAT_MAIN, playerA, "Cast Fusion Elemental", true);
+        checkPlayableAbility("Can't cast card exiled with this Ian Malcolm", 5, PhaseStep.PRECOMBAT_MAIN, playerA, "Cast Chromanticore", true);
+        checkPlayableAbility("Can't cast card exiled with this Ian Malcolm", 5, PhaseStep.PRECOMBAT_MAIN, playerA, "Cast Garth One-Eye", true);
+
+        // Should NOT be able to cast own card exiled in same way
+        checkPlayableAbility("Able to cast Horde of Notions, but should not be.", 5, PhaseStep.PRECOMBAT_MAIN, playerA, "Cast Horde of Notions", false);
+
+        // Blink Ian Malcolm, causing all cards in his current exile zone to become uncastable
+        castSpell(5, PhaseStep.PRECOMBAT_MAIN, playerA, "Ephemerate", true);
+        addTarget(playerA, "Ian Malcolm, Chaotician");
+
+        // Should no longer be able to cast all cards exiled from other players' decks
+        checkPlayableAbility("Can't cast card exiled with this Ian Malcolm", 5, PhaseStep.PRECOMBAT_MAIN, playerA, "Cast Fusion Elemental", false);
+        checkPlayableAbility("Can't cast card exiled with this Ian Malcolm", 5, PhaseStep.PRECOMBAT_MAIN, playerA, "Cast Chromanticore", false);
+        checkPlayableAbility("Can't cast card exiled with this Ian Malcolm", 5, PhaseStep.PRECOMBAT_MAIN, playerA, "Cast Garth One-Eye", false);
+
+        setStopAt(5, PhaseStep.END_TURN);
+        setStrictChooseMode(true);
+        execute();
+
     }
 
 }
