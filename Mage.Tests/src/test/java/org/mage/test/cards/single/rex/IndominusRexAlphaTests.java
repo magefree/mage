09c5d570a91e84015bc647765550241648c3c0e5
jimga150 <jimga150@users.noreply.github.com>
2024-04-29 17:09:18 -0400

    add more tests (Nullhide Ferox, Madness)

--- Mage.Tests/src/test/java/org/mage/test/cards/single/rex/IndominusRexAlphaTests.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/rex/IndominusRexAlphaTests.java
@@ -102,4 +102,79 @@ public class IndominusRexAlphaTests extends CardTestPlayerBase {
 
     }
 
+    @Test
+    public void testIndominusRexAlphaDiscardReplacement() {
+
+        addCard(Zone.HAND, playerA, "Indominus Rex, Alpha", 1);
+
+        // If a spell or ability an opponent controls causes you to discard Nullhide Ferox,
+        // put it onto the battlefield instead of putting it into your graveyard.
+        addCard(Zone.HAND, playerA, "Nullhide Ferox"); // Hexproof
+
+        addCard(Zone.LIBRARY, playerA, "Swamp", 20);
+
+        addCard(Zone.BATTLEFIELD, playerA, "Swamp", 2);
+        addCard(Zone.BATTLEFIELD, playerA, "Forest", 3);
+
+        castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Indominus Rex, Alpha", true);
+
+        // Cards to discard
+        setChoice(playerA, "Nullhide Ferox");
+
+        setStopAt(1, PhaseStep.END_TURN);
+        setStrictChooseMode(true);
+        execute();
+
+        assertCounterCount(playerA, "Indominus Rex, Alpha", CounterType.HEXPROOF, 1);
+
+        // Since the ability causing the discard was also owned by player A, Nullhide Ferox should not trigger,
+        // and it will be in the graveyard.
+        assertPermanentCount(playerA, "Nullhide Ferox", 0);
+        assertGraveyardCount(playerA, "Nullhide Ferox", 1);
+
+        assertHandCount(playerA, 1);
+
+    }
+
+    @Test
+    public void testIndominusRexAlphaMadness() {
+
+        addCard(Zone.HAND, playerA, "Indominus Rex, Alpha", 1);
+
+        // Flying, haste
+        // Madness {B} (If you discard this card, discard it into exile. When you do, cast it for its madness cost
+        // or put it into your graveyard.)
+        addCard(Zone.HAND, playerA, "Kitchen Imp");
+
+        addCard(Zone.LIBRARY, playerA, "Swamp", 20);
+
+        addCard(Zone.BATTLEFIELD, playerA, "Swamp", 5);
+        addCard(Zone.BATTLEFIELD, playerA, "Forest", 3);
+
+        castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Indominus Rex, Alpha", true);
+
+        // Cards to discard
+        setChoice(playerA, "Kitchen Imp");
+
+        // Pick madness cast to happen first
+        setChoice(playerA, "When this card");
+
+        // Cast for madness
+        setChoice(playerA, "Yes");
+
+        setStopAt(1, PhaseStep.END_TURN);
+        setStrictChooseMode(true);
+        execute();
+
+        assertCounterCount(playerA, "Indominus Rex, Alpha", CounterType.FLYING, 1);
+        assertCounterCount(playerA, "Indominus Rex, Alpha", CounterType.HASTE, 1);
+
+        // Check that madness resulted in cast
+        assertPermanentCount(playerA, "Kitchen Imp", 1);
+        assertGraveyardCount(playerA, "Kitchen Imp", 0);
+
+        assertHandCount(playerA, 2);
+
+    }
+
 }
