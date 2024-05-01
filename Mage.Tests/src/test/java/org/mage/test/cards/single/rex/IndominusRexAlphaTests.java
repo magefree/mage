ea863cefad5fedf10d6130a8e267c37adc7eb04c
jimga150 <jimga150@users.noreply.github.com>
2024-04-30 21:36:58 -0400

    test for graveyard movement

--- Mage.Tests/src/test/java/org/mage/test/cards/single/rex/IndominusRexAlphaTests.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/rex/IndominusRexAlphaTests.java
@@ -60,6 +60,60 @@ public class IndominusRexAlphaTests extends CardTestPlayerBase {
     }
 
     @Test
+    public void testIndominusRexAlphaGraveyardMovement() {
+
+        // When Rest in Peace enters the battlefield, exile all graveyards.
+        // If a card or token would be put into a graveyard from anywhere, exile it instead.
+        addCard(Zone.BATTLEFIELD, playerA, "Rest in Peace", 1);
+
+        // This test verifies that indominus can check cards that have moved to zones other than the graveyard after
+        // they've been discarded with her ability.
+
+        addCard(Zone.HAND, playerA, "Indominus Rex, Alpha", 1);
+        addCard(Zone.HAND, playerA, "Ornithopter", 1); // Flying
+        addCard(Zone.HAND, playerA, "Rograkh, Son of Rohgahh", 1); // First strike, menace, trample, Partner
+        addCard(Zone.HAND, playerA, "Adorned Pouncer", 1); // Double strike, Eternalize
+        addCard(Zone.HAND, playerA, "Ankle Biter", 1); // Deathtouch
+        addCard(Zone.HAND, playerA, "Gladecover Scout", 1); // Hexproof
+        addCard(Zone.HAND, playerA, "Banehound", 1); // Lifelink, haste
+        addCard(Zone.HAND, playerA, "Bontu the Glorified", 1); // Menace, indestructible
+        addCard(Zone.HAND, playerA, "Aerial Responder", 1); // Flying, vigilance, lifelink
+        addCard(Zone.HAND, playerA, "Stonecoil Serpent", 1); // Reach, trample, protection from multicolored
+        addCard(Zone.HAND, playerA, "Codespell Cleric", 1); // Vigilance
+
+        addCard(Zone.LIBRARY, playerA, "Swamp", 20);
+
+        addCard(Zone.BATTLEFIELD, playerA, "Swamp", 2);
+        addCard(Zone.BATTLEFIELD, playerA, "Forest", 3);
+
+        castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Indominus Rex, Alpha", true);
+
+        // Cards to discard
+        setChoice(playerA, "Ornithopter^Rograkh, Son of Rohgahh^Adorned Pouncer^Ankle Biter^Gladecover Scout" +
+                "^Banehound^Bontu the Glorified^Aerial Responder^Stonecoil Serpent^Codespell Cleric");
+
+        setStopAt(1, PhaseStep.END_TURN);
+        setStrictChooseMode(true);
+        execute();
+
+        assertCounterCount(playerA, "Indominus Rex, Alpha", CounterType.FLYING, 1);
+        assertCounterCount(playerA, "Indominus Rex, Alpha", CounterType.FIRST_STRIKE, 1);
+        assertCounterCount(playerA, "Indominus Rex, Alpha", CounterType.DOUBLE_STRIKE, 1);
+        assertCounterCount(playerA, "Indominus Rex, Alpha", CounterType.DEATHTOUCH, 1);
+        assertCounterCount(playerA, "Indominus Rex, Alpha", CounterType.HEXPROOF, 1);
+        assertCounterCount(playerA, "Indominus Rex, Alpha", CounterType.HASTE, 1);
+        assertCounterCount(playerA, "Indominus Rex, Alpha", CounterType.INDESTRUCTIBLE, 1);
+        assertCounterCount(playerA, "Indominus Rex, Alpha", CounterType.LIFELINK, 1);
+        assertCounterCount(playerA, "Indominus Rex, Alpha", CounterType.MENACE, 1);
+        assertCounterCount(playerA, "Indominus Rex, Alpha", CounterType.REACH, 1);
+        assertCounterCount(playerA, "Indominus Rex, Alpha", CounterType.TRAMPLE, 1);
+        assertCounterCount(playerA, "Indominus Rex, Alpha", CounterType.VIGILANCE, 1);
+
+        assertHandCount(playerA, 12);
+
+    }
+
+    @Test
     public void testIndominusRexAlphaSubset() {
 
         addCard(Zone.HAND, playerA, "Indominus Rex, Alpha", 1);
