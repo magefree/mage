8dfd7f1d7e3e8aa5d68380a6aa6d563652d5d681
Susucre <34709007+Susucre@users.noreply.github.com>
2024-05-07 13:36:49 +0200

    remove incorrect applyEffect. Not the right time to call it.

--- Mage/src/main/java/mage/game/GameState.java
+++ Mage/src/main/java/mage/game/GameState.java
@@ -701,7 +701,6 @@ public class GameState implements Serializable, Copyable<GameState> {
 
     public void removeTurnStartEffect(Game game) {
         delayed.removeStartOfNewTurn(game);
-        game.applyEffects();
     }
 
     public void addEffect(ContinuousEffect effect, Ability source) {
