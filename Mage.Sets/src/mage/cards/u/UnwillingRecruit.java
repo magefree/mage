2ee5ae7e94a000959df21d2062434187911bbab9
Cameron Merkel <44722506+cguy7777@users.noreply.github.com>
2024-05-20 02:21:24 -0500

    Make sure control gain is processed before untapping for 4 cards

--- Mage.Sets/src/mage/cards/u/UnwillingRecruit.java
+++ Mage.Sets/src/mage/cards/u/UnwillingRecruit.java
@@ -1,8 +1,9 @@
 package mage.cards.u;
 
 import java.util.UUID;
-import mage.abilities.Ability;
-import mage.abilities.effects.OneShotEffect;
+import mage.abilities.dynamicvalue.common.ManacostVariableValue;
+import mage.abilities.dynamicvalue.common.StaticValue;
+import mage.abilities.effects.common.UntapTargetEffect;
 import mage.abilities.effects.common.continuous.BoostTargetEffect;
 import mage.abilities.effects.common.continuous.GainAbilityTargetEffect;
 import mage.abilities.effects.common.continuous.GainControlTargetEffect;
@@ -11,11 +12,7 @@ import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.Duration;
-import mage.constants.Outcome;
-import mage.game.Game;
-import mage.game.permanent.Permanent;
 import mage.target.common.TargetCreaturePermanent;
-import mage.target.targetpointer.FixedTarget;
 
 /**
  *
@@ -27,7 +24,12 @@ public final class UnwillingRecruit extends CardImpl {
         super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{X}{R}{R}{R}");
 
         // Gain control of target creature until end of turn. Untap that creature. It gets +X/+0 and gains haste until end of turn.
-        this.getSpellAbility().addEffect(new UnwillingRecruitEffect());
+        this.getSpellAbility().addEffect(new GainControlTargetEffect(Duration.EndOfTurn));
+        this.getSpellAbility().addEffect(new UntapTargetEffect().setText("Untap that creature"));
+        this.getSpellAbility().addEffect(new BoostTargetEffect(ManacostVariableValue.REGULAR, StaticValue.get(0), Duration.EndOfTurn)
+                .setText("It gets +X/+0"));
+        this.getSpellAbility().addEffect(new GainAbilityTargetEffect(HasteAbility.getInstance(), Duration.EndOfTurn)
+                .setText("and gains haste until end of turn."));
         this.getSpellAbility().addTarget(new TargetCreaturePermanent());
 
     }
@@ -39,36 +41,5 @@ public final class UnwillingRecruit extends CardImpl {
     @Override
     public UnwillingRecruit copy() {
         return new UnwillingRecruit(this);
-    }
-}
-
-class UnwillingRecruitEffect extends OneShotEffect {
-
-    UnwillingRecruitEffect() {
-        super(Outcome.Benefit);
-        staticText = "Gain control of target creature until end of turn. Untap that creature. It gets +X/+0 and gains haste until end of turn";
-    }
-
-    private UnwillingRecruitEffect(final UnwillingRecruitEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public UnwillingRecruitEffect copy() {
-        return new UnwillingRecruitEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Permanent targetCreature = game.getPermanent(source.getFirstTarget());
-        if (targetCreature != null) {
-            source.getEffects().get(0).setTargetPointer(new FixedTarget(targetCreature.getId(), game));
-            game.addEffect(new GainControlTargetEffect(Duration.EndOfTurn), source);
-            targetCreature.untap(game);
-            game.addEffect(new BoostTargetEffect(source.getManaCostsToPay().getX(), 0, Duration.EndOfTurn), source);
-            game.addEffect(new GainAbilityTargetEffect(HasteAbility.getInstance(), Duration.EndOfTurn), source);
-            return true;
-        }
-        return false;
     }
 }
