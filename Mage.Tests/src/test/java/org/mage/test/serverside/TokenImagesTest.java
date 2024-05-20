4f1f28154d578b85b149442f5e3ecab4b99bfad9
Susucre <34709007+Susucre@users.noreply.github.com>
2024-05-20 19:13:53 +0200

    make random failure a little less likely for image tests
    15 was failing a little too often in my opinion

--- Mage.Tests/src/test/java/org/mage/test/serverside/TokenImagesTest.java
+++ Mage.Tests/src/test/java/org/mage/test/serverside/TokenImagesTest.java
@@ -733,7 +733,7 @@ public class TokenImagesTest extends CardTestPlayerBase {
     // TODO: implement mock or test command to setup "random" images in TokenImpl.generateTokenInfo
     //  (see setFlipCoinResult and setDieRollResult), so no needs in big amount
     public void test_FaceDown_CardWithMorph_MustGetDefaultImage() {
-        int faceDownAmount = 15;
+        int faceDownAmount = 20;
         addCard(Zone.HAND, playerA, "Ainok Tracker", faceDownAmount); // {5}{R}, Morph {4}{R}, face up {3}
         addCard(Zone.BATTLEFIELD, playerA, "Mountain", 5 * faceDownAmount);
 
@@ -752,7 +752,7 @@ public class TokenImagesTest extends CardTestPlayerBase {
 
     @Test // it's ok for fail in very rare random
     public void test_FaceDown_LandWithMorph_MustGetDefaultImage() {
-        int faceDownAmount = 15;
+        int faceDownAmount = 20;
         addCard(Zone.HAND, playerA, "Zoetic Cavern", faceDownAmount);
         addCard(Zone.BATTLEFIELD, playerA, "Mountain", 3 * faceDownAmount);
 
