934567959cab7aa39adf7926bb09a3a277ecd7dd
grimreap124 <19590931+grimreap124@users.noreply.github.com>
2024-05-28 09:59:14 +1000

    Update HideousTaskmaster.java

--- Mage.Sets/src/mage/cards/h/HideousTaskmaster.java
+++ Mage.Sets/src/mage/cards/h/HideousTaskmaster.java
@@ -16,11 +16,9 @@ import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.Duration;
-import mage.target.TargetPermanent;
 import mage.target.common.TargetCreaturePermanent;
 import mage.target.targetadjustment.EachOpponentPermanentTargetsAdjuster;
 import mage.target.targetpointer.EachTargetPointer;
-
 
 /**
  *
@@ -29,8 +27,8 @@ import mage.target.targetpointer.EachTargetPointer;
 public final class HideousTaskmaster extends CardImpl {
 
     public HideousTaskmaster(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{6}{R}");
-        
+        super(ownerId, setInfo, new CardType[] { CardType.CREATURE }, "{6}{R}");
+
         this.subtype.add(SubType.ELDRAZI);
         this.power = new MageInt(7);
         this.toughness = new MageInt(2);
@@ -40,13 +38,19 @@ public final class HideousTaskmaster extends CardImpl {
 
         // When you cast this spell, for each opponent, gain control of up to one target creature that player controls until end of turn.
         // Untap those creatures. They gain trample, haste, and annihilator 1 until end of turn.
-        Ability ability = new CastSourceTriggeredAbility(new GainControlTargetEffect(Duration.EndOfTurn).setTargetPointer(new EachTargetPointer()));
+        Ability ability = new CastSourceTriggeredAbility(
+                new GainControlTargetEffect(Duration.EndOfTurn).setTargetPointer(new EachTargetPointer()).setText(
+                        "for each opponent, gain control of up to one target creature that player controls until end of turn"));
         ability.setTargetAdjuster(new EachOpponentPermanentTargetsAdjuster());
-        ability.addTarget(new TargetCreaturePermanent(0,1));
-        ability.addEffect(new UntapTargetEffect().setTargetPointer(new EachTargetPointer()));
-        ability.addEffect(new GainAbilityTargetEffect(HasteAbility.getInstance(), Duration.EndOfTurn).setTargetPointer(new EachTargetPointer()));
-        ability.addEffect(new GainAbilityTargetEffect(TrampleAbility.getInstance(), Duration.EndOfTurn).setTargetPointer(new EachTargetPointer()));
-        ability.addEffect(new GainAbilityTargetEffect(new AnnihilatorAbility(1), Duration.EndOfTurn).setTargetPointer(new EachTargetPointer()));
+        ability.addTarget(new TargetCreaturePermanent(0, 1));
+        ability.addEffect(
+                new UntapTargetEffect().setTargetPointer(new EachTargetPointer()).setText("Untap those creatures"));
+        ability.addEffect(new GainAbilityTargetEffect(TrampleAbility.getInstance(), Duration.EndOfTurn)
+                .setTargetPointer(new EachTargetPointer()).setText("They gain trample"));
+        ability.addEffect(new GainAbilityTargetEffect(HasteAbility.getInstance(), Duration.EndOfTurn)
+                .setTargetPointer(new EachTargetPointer()).setText("haste"));
+        ability.addEffect(new GainAbilityTargetEffect(new AnnihilatorAbility(1), Duration.EndOfTurn)
+                .setTargetPointer(new EachTargetPointer()).setText("and annihilator 1 until end of turn"));
         this.addAbility(ability);
     }
 
