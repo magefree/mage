3e725fd400c3eb3785e6780316d7c4e4cbb2018a
theelk801 <theelk801@gmail.com>
2024-05-27 10:27:35 -0400

    [MH3] Implement Static Prison

---
+++ Mage.Sets/src/mage/cards/s/StaticPrison.java
@@ -0,0 +1,49 @@
+package mage.cards.s;
+
+import mage.abilities.Ability;
+import mage.abilities.common.BeginningOfPreCombatMainTriggeredAbility;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.costs.common.PayEnergyCost;
+import mage.abilities.effects.common.ExileUntilSourceLeavesEffect;
+import mage.abilities.effects.common.SacrificeSourceUnlessPaysEffect;
+import mage.abilities.effects.common.counter.GetEnergyCountersControllerEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.TargetController;
+import mage.filter.StaticFilters;
+import mage.target.TargetPermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class StaticPrison extends CardImpl {
+
+    public StaticPrison(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{W}");
+
+        // When Static Prison enters the battlefield, exile target nonland permanent an opponent controls until Static Prison leaves the battlefield. You get {E}{E}.
+        Ability ability = new EntersBattlefieldTriggeredAbility(new ExileUntilSourceLeavesEffect());
+        ability.addEffect(new GetEnergyCountersControllerEffect(2));
+        ability.addTarget(new TargetPermanent(
+                0, 1, StaticFilters.FILTER_OPPONENTS_PERMANENT_NON_LAND
+        ));
+        this.addAbility(ability);
+
+        // At the beginning of your precombat main phase, sacrifice Static Prison unless you pay {E}.
+        this.addAbility(new BeginningOfPreCombatMainTriggeredAbility(
+                new SacrificeSourceUnlessPaysEffect(new PayEnergyCost(1)), TargetController.YOU, false
+        ));
+    }
+
+    private StaticPrison(final StaticPrison card) {
+        super(card);
+    }
+
+    @Override
+    public StaticPrison copy() {
+        return new StaticPrison(this);
+    }
+}
--- Mage.Sets/src/mage/sets/ModernHorizons3.java
+++ Mage.Sets/src/mage/sets/ModernHorizons3.java
@@ -136,6 +136,7 @@ public final class ModernHorizons3 extends ExpansionSet {
         cards.add(new SetCardInfo("Sorin, Ravenous Neonate", 245, Rarity.MYTHIC, mage.cards.s.SorinRavenousNeonate.class));
         cards.add(new SetCardInfo("Sowing Mycospawn", 170, Rarity.RARE, mage.cards.s.SowingMycospawn.class));
         cards.add(new SetCardInfo("Spawn-Gang Commander", 140, Rarity.UNCOMMON, mage.cards.s.SpawnGangCommander.class));
+        cards.add(new SetCardInfo("Static Prison", 44, Rarity.UNCOMMON, mage.cards.s.StaticPrison.class));
         cards.add(new SetCardInfo("Strength of the Harvest", 258, Rarity.UNCOMMON, mage.cards.s.StrengthOfTheHarvest.class));
         cards.add(new SetCardInfo("Strix Serenade", 71, Rarity.RARE, mage.cards.s.StrixSerenade.class));
         cards.add(new SetCardInfo("Stump Stomp", 259, Rarity.UNCOMMON, mage.cards.s.StumpStomp.class));
