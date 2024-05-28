818ab5a1a64211e0a2aa32508d3a17e840f3eb15
grimreap124 <19590931+grimreap124@users.noreply.github.com>
2024-05-28 11:57:36 +1000

    Update CardPluginImpl.java

--- Mage.Client/src/main/java/org/mage/plugins/card/CardPluginImpl.java
+++ Mage.Client/src/main/java/org/mage/plugins/card/CardPluginImpl.java
@@ -141,7 +141,7 @@ public class CardPluginImpl implements CardPlugin {
             MagePermanent perm = (MagePermanent) card.getMainPanel(); // all cards must be MagePermanent on battlefield
 
             if (!rowType.isType(perm) || perm.getOriginalPermanent().isAttachedToPermanent()
-                    || (perm.isCreature() && rowType.equals(RowType.land))) {
+                    || (perm.isCreature() && !rowType.equals(RowType.creature))) {
                 continue;
             }
 
