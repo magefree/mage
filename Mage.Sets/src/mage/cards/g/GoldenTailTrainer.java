204999901f0a1312b3b555de361cfc8fdcfa09c4
grimreap124 <19590931+grimreap124@users.noreply.github.com>
2024-05-28 21:01:24 +1000

    Update GoldenTailTrainer.java

--- Mage.Sets/src/mage/cards/g/GoldenTailTrainer.java
+++ Mage.Sets/src/mage/cards/g/GoldenTailTrainer.java
@@ -2,14 +2,17 @@ package mage.cards.g;
 
 import java.util.UUID;
 import mage.MageInt;
+import mage.abilities.common.AttacksTriggeredAbility;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.dynamicvalue.common.SourcePermanentPowerCount;
-import mage.abilities.effects.common.cost.SpellsCostReductionAllOfChosenCardTypeEffect;
+import mage.abilities.effects.common.continuous.BoostControlledEffect;
 import mage.abilities.effects.common.cost.SpellsCostReductionControllerEffect;
+import mage.constants.Duration;
 import mage.constants.SubType;
-import mage.constants.Zone;
 import mage.filter.FilterCard;
+import mage.filter.common.FilterCreaturePermanent;
 import mage.filter.predicate.Predicates;
+import mage.filter.predicate.permanent.ModifiedPredicate;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
@@ -22,7 +25,11 @@ public final class GoldenTailTrainer extends CardImpl {
 
     private static final SourcePermanentPowerCount xValue = new SourcePermanentPowerCount(false);
     private static final FilterCard filter = new FilterCard();
+    private static final FilterCreaturePermanent modifiedFilter = new FilterCreaturePermanent(
+            "modified creatures you control");
+
     static {
+        modifiedFilter.add(ModifiedPredicate.instance);
         filter.add(Predicates.or(SubType.AURA.getPredicate(), SubType.EQUIPMENT.getPredicate()));
     }
 
@@ -36,8 +43,14 @@ public final class GoldenTailTrainer extends CardImpl {
 
         // Aura and Equipment spells you cast cost {X} less to cast, where X is Golden-Tail Trainer's power.
         this.addAbility(
-                new SimpleStaticAbility(new SpellsCostReductionControllerEffect(filter, xValue)));
+                new SimpleStaticAbility(new SpellsCostReductionControllerEffect(filter, xValue)
+                        .setText(
+                                "Aura and Equipment spells you cast cost {X} less to cast, where X is Golden-Tail Trainer's power")));
+
         // Whenever Golden-Tail Trainer attacks, other modified creatures you control get +X/+X until end of turn, where X is Golden-Tail Trainer's power.
+        this.addAbility(
+                new AttacksTriggeredAbility(
+                        new BoostControlledEffect(xValue, xValue, Duration.EndOfTurn, modifiedFilter, true)));
     }
 
     private GoldenTailTrainer(final GoldenTailTrainer card) {
