c760a23ff7246869572754b9e73b48b2e8487b97
tiera3 <87589219+tiera3@users.noreply.github.com>
2024-05-28 21:14:04 +1000

    DownloadPicturesService small class version

--- Mage.Client/src/main/java/org/mage/plugins/card/images/DownloadPicturesService.java
+++ Mage.Client/src/main/java/org/mage/plugins/card/images/DownloadPicturesService.java
@@ -76,8 +76,8 @@ public class DownloadPicturesService extends DefaultBoundedRangeModel implements
     enum DownloadSources {
         WIZARDS("1. wizards.com - low quality CARDS, multi-language, slow download", WizardCardsImageSource.instance),
         TOKENS("2. tokens.mtg.onl - high quality TOKENS", TokensMtgImageSource.instance),
-        SCRYFALL("3. scryfall.com - high quality CARDS and TOKENS, multi-language", ScryfallImageSource.instance),
-        SCRYFALL_SMALL("3a. scryfall.com small images - low quality CARDS and TOKENS, multi-language", ScryfallImageSourceSmall.instance),
+        SCRYFALL("3. scryfall.com - high quality CARDS and TOKENS, multi-language", ScryfallImageSource.getInstance()),
+        SCRYFALL_SMALL("3a. scryfall.com small images - low quality CARDS and TOKENS, multi-language", ScryfallImageSourceSmall.getInstance()),
         MAGIDEX("4. magidex.com - high quality CARDS", MagidexImageSource.instance),
         GRAB_BAG("5. GrabBag - STAR WARS cards and tokens", GrabbagImageSource.instance),
         MYTHICSPOILER("6. mythicspoiler.com", MythicspoilerComSource.instance),
@@ -165,7 +165,7 @@ public class DownloadPicturesService extends DefaultBoundedRangeModel implements
         // SOURCES - scryfall is default source
         uiDialog.getSourcesCombo().setModel(new DefaultComboBoxModel(DownloadSources.values()));
         uiDialog.getSourcesCombo().setSelectedItem(DownloadSources.SCRYFALL);
-        selectedSource = ScryfallImageSource.instance;
+        selectedSource = ScryfallImageSource.getInstance();
         uiDialog.getSourcesCombo().addItemListener((ItemEvent event) -> {
             if (event.getStateChange() == ItemEvent.SELECTED) {
                 comboboxSourceSelected(event);
