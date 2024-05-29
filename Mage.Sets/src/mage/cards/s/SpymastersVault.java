b8dd57ba795d14f167bd99306bfdbc406ca11eb4
grimreap124 <19590931+grimreap124@users.noreply.github.com>
2024-05-29 19:23:56 +1000

    Adding enter tapped and updated to use CreaturesDiedThisTurnCount

--- Mage.Sets/src/mage/cards/s/SpymastersVault.java
+++ Mage.Sets/src/mage/cards/s/SpymastersVault.java
@@ -1,30 +1,30 @@
 package mage.cards.s;
 
 import java.util.UUID;
-import mage.MageObject;
 import mage.abilities.Ability;
+import mage.abilities.common.EntersBattlefieldAbility;
 import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.condition.Condition;
+import mage.abilities.condition.common.PermanentsOnTheBattlefieldCondition;
 import mage.abilities.costs.common.TapSourceCost;
 import mage.abilities.costs.mana.ManaCostsImpl;
-import mage.abilities.dynamicvalue.DynamicValue;
-import mage.abilities.effects.Effect;
+import mage.abilities.decorator.ConditionalOneShotEffect;
+import mage.abilities.dynamicvalue.common.CreaturesDiedThisTurnCount;
 import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.TapSourceEffect;
 import mage.abilities.effects.keyword.ConniveSourceEffect;
-import mage.abilities.hint.Hint;
-import mage.abilities.hint.ValueHint;
 import mage.abilities.mana.BlackManaAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.constants.ComparisonType;
 import mage.constants.Outcome;
-import mage.constants.WatcherScope;
-import mage.constants.Zone;
+import mage.constants.SubType;
+import mage.filter.FilterPermanent;
+import mage.filter.common.FilterControlledPermanent;
 import mage.game.Game;
-import mage.game.events.GameEvent;
-import mage.game.events.ZoneChangeEvent;
 import mage.game.permanent.Permanent;
 import mage.target.common.TargetControlledCreaturePermanent;
-import mage.watchers.Watcher;
 
 /**
  *
@@ -32,17 +32,25 @@ import mage.watchers.Watcher;
  */
 public final class SpymastersVault extends CardImpl {
 
+    private static final FilterPermanent filter = new FilterControlledPermanent(SubType.SWAMP);
+    private static final Condition condition = new PermanentsOnTheBattlefieldCondition(filter, ComparisonType.EQUAL_TO,
+            0);
+
     public SpymastersVault(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[] { CardType.LAND }, "");
 
         // Spymaster's Vault enters the battlefield tapped unless you control a Swamp.
+        this.addAbility(new EntersBattlefieldAbility(new ConditionalOneShotEffect(
+                new TapSourceEffect(), condition), "tapped unless you control a Swamp"));
+
         // {T}: Add {B}.
         this.addAbility(new BlackManaAbility());
         // {B}, {T}: Target creature you control connives X, where X is the number of creatures that died this turn.
         Ability ability = new SimpleActivatedAbility(new SpymastersVaultEffect(), new ManaCostsImpl<>("{B}"));
         ability.addCost(new TapSourceCost());
         ability.addTarget(new TargetControlledCreaturePermanent());
-        this.addAbility(ability.addHint(SpymastersVaultCount.getHint()));
+        this.addAbility(ability.addHint(CreaturesDiedThisTurnCount.getHint()));
+
     }
 
     private SpymastersVault(final SpymastersVault card) {
@@ -73,7 +81,7 @@ class SpymastersVaultEffect extends OneShotEffect {
 
     @Override
     public boolean apply(Game game, Ability source) {
-        int deaths = SpymastersVaultCount.instance.calculate(game, source, this);
+        int deaths = CreaturesDiedThisTurnCount.instance.calculate(game, source, this);
 
         if (deaths < 1) {
             return false;
@@ -84,60 +92,4 @@ class SpymastersVaultEffect extends OneShotEffect {
         }
         return ConniveSourceEffect.connive(permanent, deaths, source, game);
     }
-}
-
-enum SpymastersVaultCount implements DynamicValue {
-    instance;
-
-    private static final Hint hint = new ValueHint("Number of creatures that died this turn", instance);
-
-    public static Hint getHint() {
-        return hint;
-    }
-
-    @Override
-    public int calculate(Game game, Ability sourceAbility, Effect effect) {
-        CreaturesDiedThisTurnWatcher watcher = game.getState().getWatcher(CreaturesDiedThisTurnWatcher.class);
-        return watcher == null ? 0 : watcher.getCreaturesDiedThisTurn();
-    }
-
-    @Override
-    public SpymastersVaultCount copy() {
-        return instance;
-    }
-
-    @Override
-    public String getMessage() {
-        return "";
-    }
-}
-
-class SpymastersVaultWatcher extends Watcher {
-
-    public int getCreaturesDiedThisTurn() {
-        return creaturesDiedThisTurn;
-    }
-
-    private int creaturesDiedThisTurn = 0;
-
-    public SpymastersVaultWatcher() {
-        super(WatcherScope.GAME);
-    }
-
-    @Override
-    public void watch(GameEvent event, Game game) {
-        if (event.getType() == GameEvent.EventType.ZONE_CHANGE && ((ZoneChangeEvent) event).isDiesEvent()) {
-            MageObject mageObject = game.getLastKnownInformation(event.getTargetId(), Zone.BATTLEFIELD);
-            if (mageObject != null && mageObject.isCreature(game)) {
-                creaturesDiedThisTurn++;
-            }
-        }
-    }
-
-    @Override
-    public void reset() {
-        super.reset();
-        creaturesDiedThisTurn = 0;
-    }
-
 }
