c8e65fa148f06be15b8d6a55eaed9aa4f5ee73cc
grimreap124 <19590931+grimreap124@users.noreply.github.com>
2024-05-30 19:38:57 +1000

    Update LethalThrowdown.java

--- Mage.Sets/src/mage/cards/l/LethalThrowdown.java
+++ Mage.Sets/src/mage/cards/l/LethalThrowdown.java
@@ -42,6 +42,7 @@ public final class LethalThrowdown extends CardImpl {
         // Destroy target creature or planeswalker. If the modified creature was sacrificed, draw a card.
         this.getSpellAbility().addEffect(new LethalThrowdownEffect());
         this.getSpellAbility().addTarget(new TargetCreatureOrPlaneswalker());
+        this.getSpellAbility().addEffect(new DestroyTargetEffect());
     }
 
     private LethalThrowdown(final LethalThrowdown card) {
@@ -70,15 +71,16 @@ class LethalThrowdownEffect extends OneShotEffect {
         for (Cost cost : source.getCosts()) {
             if (cost instanceof OrCost) {
                 OrCost orCost = (OrCost) cost;
+
+                // This is a bit of a hack to check if the modified creature was sacrificed
+                // if this comes up anywhere else we should think of a more elegant solution
                 if (orCost.getSelectedCost().getText().equals("sacrifice a modified creature")) {
-                    new DrawCardSourceControllerEffect(1).apply(game, source);
-                    break;
+                    return new DrawCardSourceControllerEffect(1).apply(game, source);
+                    
                 }
             }
         }
-
-        DestroyTargetEffect effect = new DestroyTargetEffect();
-        return effect.apply(game, source);
+        return false;
     }
 
     @Override
