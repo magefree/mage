f7b2e2860b704e9a43a8ba60442904fa05daad3c
grimreap124 <19590931+grimreap124@users.noreply.github.com>
2024-06-01 19:01:13 +1000

    Update Trenchpost.java

--- Mage.Sets/src/mage/cards/t/Trenchpost.java
+++ Mage.Sets/src/mage/cards/t/Trenchpost.java
@@ -4,13 +4,15 @@ import java.util.UUID;
 import mage.abilities.Ability;
 import mage.constants.SubType;
 import mage.constants.Zone;
-import mage.filter.FilterPermanent;
+import mage.filter.common.FilterControlledPermanent;
 import mage.target.TargetPlayer;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.common.TapSourceCost;
 import mage.abilities.costs.mana.GenericManaCost;
 import mage.abilities.dynamicvalue.common.PermanentsOnBattlefieldCount;
 import mage.abilities.effects.common.MillCardsTargetEffect;
+import mage.abilities.hint.Hint;
+import mage.abilities.hint.ValueHint;
 import mage.abilities.mana.ColorlessManaAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
@@ -22,7 +24,8 @@ import mage.constants.CardType;
  */
 public final class Trenchpost extends CardImpl {
 
-    private static final FilterPermanent filter = new FilterPermanent(SubType.LOCUS, "Locus you control");
+    private static final FilterControlledPermanent filter = new FilterControlledPermanent(SubType.LOCUS,
+            "Locus you control");
 
     public Trenchpost(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[] { CardType.LAND }, "");
@@ -32,10 +35,13 @@ public final class Trenchpost extends CardImpl {
         // {T}: Add {C}.
         this.addAbility(new ColorlessManaAbility());
         // {3}, {T}: Target player mills a card for each Locus you control.
+        PermanentsOnBattlefieldCount numLocus = new PermanentsOnBattlefieldCount(filter);
         Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD,
-                new MillCardsTargetEffect(new PermanentsOnBattlefieldCount(filter)), new GenericManaCost(3));
+                new MillCardsTargetEffect(numLocus), new GenericManaCost(3));
         ability.addCost(new TapSourceCost());
         ability.addTarget(new TargetPlayer());
+        Hint hint = new ValueHint("Locus you control", numLocus);
+        ability.addHint(hint);
         this.addAbility(ability);
     }
 
