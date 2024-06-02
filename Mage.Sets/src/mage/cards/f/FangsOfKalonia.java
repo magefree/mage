535f80d2abe26dfe59d53f63c781de10dc3eef3c
grimreap124 <19590931+grimreap124@users.noreply.github.com>
2024-06-02 19:14:30 +1000

    Update FangsOfKalonia.java

--- Mage.Sets/src/mage/cards/f/FangsOfKalonia.java
+++ Mage.Sets/src/mage/cards/f/FangsOfKalonia.java
@@ -1,9 +1,15 @@
 package mage.cards.f;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+
+import com.j256.ormlite.field.types.IntegerObjectType;
 
 import mage.abilities.Ability;
 import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.DoubleCountersTargetEffect;
 import mage.abilities.effects.common.counter.DoubleCounterOnEachPermanentEffect;
 import mage.abilities.effects.common.counter.AddCountersAllEffect;
@@ -12,8 +18,11 @@ import mage.abilities.keyword.OverloadAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.constants.Outcome;
 import mage.counters.CounterType;
 import mage.filter.StaticFilters;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
 import mage.target.common.TargetControlledCreaturePermanent;
 
 /**
@@ -26,17 +35,12 @@ public final class FangsOfKalonia extends CardImpl {
         super(ownerId, setInfo, new CardType[] { CardType.SORCERY }, "{1}{G}");
 
         // Put a +1/+1 counter on target creature you control, then double the number of +1/+1 counters on each creature that had a +1/+1 counter put on it this way.
-        this.getSpellAbility().addEffect(new AddCountersTargetEffect(CounterType.P1P1.createInstance()));
-        this.getSpellAbility().addEffect(new DoubleCountersTargetEffect(CounterType.P1P1).setText(
-                ", then double the number of +1/+1 counters on each creature that had a +1/+1 counter put on it this way"));
+        this.getSpellAbility().addEffect(new FangsOfKaloniaEffect());
         this.getSpellAbility().addTarget(new TargetControlledCreaturePermanent());
 
         // Overload {4}{G}{G}
-        Ability overloadAbility = new OverloadAbility(this, new AddCountersAllEffect(CounterType.P1P1.createInstance(1),
-                StaticFilters.FILTER_CONTROLLED_CREATURE), new ManaCostsImpl<>("{4}{G}{G}"));
-        overloadAbility.addEffect(new DoubleCounterOnEachPermanentEffect(CounterType.P1P1,
-                StaticFilters.FILTER_CONTROLLED_CREATURE).setText(
-                        ", then double the number of +1/+1 counters on each creature that had a +1/+1 counter put on it this way"));
+        Ability overloadAbility = new OverloadAbility(this, new FangsOfKaloniaOverloadEffect(),
+                new ManaCostsImpl<>("{4}{G}{G}"));
         this.addAbility(overloadAbility);
     }
 
@@ -47,5 +51,104 @@ public final class FangsOfKalonia extends CardImpl {
     @Override
     public FangsOfKalonia copy() {
         return new FangsOfKalonia(this);
+    }
+}
+
+class FangsOfKaloniaEffect extends OneShotEffect {
+
+    FangsOfKaloniaEffect() {
+        super(Outcome.Benefit);
+        staticText = "Put a +1/+1 counter on target creature you control, then double the number of +1/+1 counters on each creature that had a +1/+1 counter put on it this way";
+    }
+
+    private FangsOfKaloniaEffect(final FangsOfKaloniaEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public FangsOfKaloniaEffect copy() {
+        return new FangsOfKaloniaEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        UUID targetId = source.getTargets().getFirstTarget();
+
+        if (targetId == null) {
+            return false;
+        }
+        Permanent permanent = game.getPermanent(targetId);
+        if (permanent == null) {
+            return false;
+        }
+
+        int preP1P1Count = permanent.getCounters(game).getCount(CounterType.P1P1);
+        new AddCountersTargetEffect(CounterType.P1P1.createInstance()).apply(game, source);
+        int postP1P1Count = permanent.getCounters(game).getCount(CounterType.P1P1);
+        if (postP1P1Count > preP1P1Count) {
+            new DoubleCountersTargetEffect(CounterType.P1P1).apply(game, source);
+        }
+        return true;
+    }
+}
+
+class FangsOfKaloniaOverloadEffect extends OneShotEffect {
+
+    FangsOfKaloniaOverloadEffect() {
+        super(Outcome.Benefit);
+        staticText = "Put a +1/+1 counter on each creature you control, then double the number of +1/+1 counters on each creature that had a +1/+1 counter put on it this way";
+    }
+
+    private FangsOfKaloniaOverloadEffect(final FangsOfKaloniaOverloadEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public FangsOfKaloniaOverloadEffect copy() {
+        return new FangsOfKaloniaOverloadEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+
+        List<Permanent> controlledCreatures = game.getBattlefield().getActivePermanents(
+                StaticFilters.FILTER_CONTROLLED_CREATURE, source.getControllerId(), source, game);
+        Map<UUID, Integer> preP1P1Counts = new HashMap<>();
+
+        for (Permanent creature : controlledCreatures) {
+            preP1P1Counts.put(creature.getId(), creature.getCounters(game).getCount(CounterType.P1P1));
+            creature.addCounters(CounterType.P1P1.createInstance(1), source.getControllerId(), source,
+                    game);
+        }
+
+        for (Permanent creature : controlledCreatures) {
+            int postP1P1Count = creature.getCounters(game).getCount(CounterType.P1P1);
+            Integer preP1P1Count = preP1P1Counts.get(creature.getId());
+            if (preP1P1Count == null) {
+                continue;
+            }
+            if (postP1P1Count > preP1P1Count) {
+                creature.addCounters(CounterType.P1P1.createInstance(postP1P1Count), source.getControllerId(), source,
+                        game);
+            }
+        }
+
+        UUID targetId = source.getTargets().getFirstTarget();
+
+        if (targetId == null) {
+            return false;
+        }
+        Permanent permanent = game.getPermanent(targetId);
+        if (permanent == null) {
+            return false;
+        }
+
+        int preP1P1Count = permanent.getCounters(game).getCount(CounterType.P1P1);
+        new AddCountersTargetEffect(CounterType.P1P1.createInstance()).apply(game, source);
+        int postP1P1Count = permanent.getCounters(game).getCount(CounterType.P1P1);
+        if (postP1P1Count > preP1P1Count) {
+            new DoubleCountersTargetEffect(CounterType.P1P1).apply(game, source);
+        }
+        return true;
     }
 }
