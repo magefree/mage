8a05b47961ec4d4f247ae6bb0e4613bc0f94019a
xenohedron <xenohedron@users.noreply.github.com>
2024-06-02 03:12:42 -0400

    common class for "whenever ... deals damage to you, "

--- Mage.Sets/src/mage/cards/n/NoMercy.java
+++ Mage.Sets/src/mage/cards/n/NoMercy.java
@@ -1,17 +1,13 @@
-
 package mage.cards.n;
 
-import java.util.UUID;
-import mage.abilities.TriggeredAbilityImpl;
+import mage.abilities.common.DealsDamageToYouAllTriggeredAbility;
 import mage.abilities.effects.common.DestroyTargetEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.Zone;
-import mage.game.Game;
-import mage.game.events.GameEvent;
-import mage.game.permanent.Permanent;
-import mage.target.targetpointer.FixedTarget;
+import mage.filter.StaticFilters;
+
+import java.util.UUID;
 
 /**
  *
@@ -23,7 +19,8 @@ public final class NoMercy extends CardImpl {
         super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{2}{B}{B}");
 
         // Whenever a creature deals damage to you, destroy it.
-        this.addAbility(new NoMercyTriggeredAbility());
+        this.addAbility(new DealsDamageToYouAllTriggeredAbility(StaticFilters.FILTER_PERMANENT_CREATURE,
+                new DestroyTargetEffect().setText("destroy it")));
     }
 
     private NoMercy(final NoMercy card) {
@@ -35,42 +32,4 @@ public final class NoMercy extends CardImpl {
         return new NoMercy(this);
     }
 
-    public static class NoMercyTriggeredAbility extends TriggeredAbilityImpl {
-
-        public NoMercyTriggeredAbility() {
-            super(Zone.BATTLEFIELD, new DestroyTargetEffect());
-        }
-
-        private NoMercyTriggeredAbility(final NoMercyTriggeredAbility ability) {
-            super(ability);
-        }
-
-        @Override
-        public NoMercyTriggeredAbility copy() {
-            return new NoMercyTriggeredAbility(this);
-        }
-
-        @Override
-        public boolean checkEventType(GameEvent event, Game game) {
-            return event.getType() == GameEvent.EventType.DAMAGED_PLAYER;
-        }
-
-        @Override
-        public boolean checkTrigger(GameEvent event, Game game) {
-            if (event.getPlayerId().equals(this.getControllerId())) {
-                Permanent permanent = game.getPermanent(event.getSourceId());
-                if (permanent != null && permanent.isCreature(game)) {
-                    this.getEffects().setTargetPointer(new FixedTarget(permanent, game));
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        @Override
-        public String getRule() {
-            return "Whenever a creature deals damage to you, destroy it.";
-        }
-
-    }
 }
