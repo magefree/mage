cce9547efb7b2462caacda4d1f0236999ab5e21c
xenohedron <xenohedron@users.noreply.github.com>
2024-06-02 03:19:40 -0400

    fix more calls to respect range of influence

--- Mage.Sets/src/mage/cards/k/KumanoFacesKakkazan.java
+++ Mage.Sets/src/mage/cards/k/KumanoFacesKakkazan.java
@@ -1,32 +1,37 @@
 package mage.cards.k;
 
-import java.util.Set;
-import java.util.UUID;
-
 import mage.abilities.Ability;
 import mage.abilities.DelayedTriggeredAbility;
 import mage.abilities.common.SagaAbility;
-import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.ReplacementEffectImpl;
 import mage.abilities.effects.common.CreateDelayedTriggeredAbilityEffect;
+import mage.abilities.effects.common.DamageAllEffect;
+import mage.abilities.effects.common.DamagePlayersEffect;
 import mage.abilities.effects.common.ExileSagaAndReturnTransformedEffect;
 import mage.abilities.keyword.TransformAbility;
-import mage.constants.*;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
+import mage.constants.*;
 import mage.counters.CounterType;
+import mage.filter.common.FilterPlaneswalkerPermanent;
 import mage.game.Game;
 import mage.game.events.EntersTheBattlefieldEvent;
 import mage.game.events.GameEvent;
 import mage.game.permanent.Permanent;
 import mage.game.stack.Spell;
-import mage.players.Player;
+
+import java.util.UUID;
 
 /**
  *
  * @author weirddan455
  */
 public final class KumanoFacesKakkazan extends CardImpl {
+
+    private static final FilterPlaneswalkerPermanent filter = new FilterPlaneswalkerPermanent();
+    static {
+        filter.add(TargetController.OPPONENT.getControllerPredicate());
+    }
 
     public KumanoFacesKakkazan(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{R}");
@@ -38,7 +43,10 @@ public final class KumanoFacesKakkazan extends CardImpl {
         SagaAbility sagaAbility = new SagaAbility(this);
 
         // I — Kumano Faces Kakkazan deals 1 damage to each opponent and each planeswalker they control.
-        sagaAbility.addChapterEffect(this, SagaChapter.CHAPTER_I, new KumanoFacesKakkazanDamageEffect());
+        sagaAbility.addChapterEffect(this, SagaChapter.CHAPTER_I,
+                new DamagePlayersEffect(1, TargetController.OPPONENT),
+                new DamageAllEffect(1, filter).setText("and each planeswalker they control")
+        );
 
         // II — When you cast your next creature spell this turn, that creature enters the battlefield with an additional +1/+1 counter on it.
         sagaAbility.addChapterEffect(this, SagaChapter.CHAPTER_II, new CreateDelayedTriggeredAbilityEffect(new KumanoFacesKakkazanTriggeredAbility()));
@@ -57,43 +65,6 @@ public final class KumanoFacesKakkazan extends CardImpl {
     @Override
     public KumanoFacesKakkazan copy() {
         return new KumanoFacesKakkazan(this);
-    }
-}
-
-class KumanoFacesKakkazanDamageEffect extends OneShotEffect {
-
-    KumanoFacesKakkazanDamageEffect() {
-        super(Outcome.Damage);
-        this.staticText = "{this} deals 1 damage to each opponent and each planeswalker they control";
-    }
-
-    private KumanoFacesKakkazanDamageEffect(final KumanoFacesKakkazanDamageEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public KumanoFacesKakkazanDamageEffect copy() {
-        return new KumanoFacesKakkazanDamageEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Set<UUID> opponents = game.getOpponents(source.getControllerId());
-        if (opponents.isEmpty()) {
-            return false;
-        }
-        for (UUID opponentId : opponents) {
-            Player opponent = game.getPlayer(opponentId);
-            if (opponent != null) {
-                opponent.damage(1, source, game);
-            }
-        }
-        for (Permanent permanent : game.getBattlefield().getAllActivePermanents(CardType.PLANESWALKER, game)) {
-            if (opponents.contains(permanent.getControllerId())) {
-                permanent.damage(1, source, game);
-            }
-        }
-        return true;
     }
 }
 
