e51b94c74697e95ff4af4d25a8726268a0a1a525
grimreap124 <19590931+grimreap124@users.noreply.github.com>
2024-06-04 22:04:29 +1000

    Implement-M3C-Gluttonous-Hellkite

---
+++ Mage.Sets/src/mage/cards/g/GluttonousHellkite.java
@@ -0,0 +1,182 @@
+package mage.cards.g;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.SpellAbility;
+import mage.abilities.dynamicvalue.DynamicValue;
+import mage.abilities.dynamicvalue.common.GetXValue;
+import mage.abilities.effects.Effect;
+import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.ReplacementEffectImpl;
+import mage.abilities.effects.common.CastSourceTriggeredAbility;
+import mage.abilities.effects.common.EntersBattlefieldWithXCountersEffect;
+import mage.constants.*;
+import mage.abilities.keyword.FlyingAbility;
+import mage.abilities.keyword.TrampleAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.counters.CounterType;
+import mage.filter.StaticFilters;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.permanent.Permanent;
+import mage.players.Player;
+import mage.target.TargetImpl;
+import mage.target.common.TargetSacrifice;
+import mage.util.CardUtil;
+
+/**
+ *
+ * @author grimreap124
+ */
+public final class GluttonousHellkite extends CardImpl {
+
+    public GluttonousHellkite(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{X}{X}{B}{R}{G}");
+        
+        this.subtype.add(SubType.DRAGON);
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(3);
+
+        // When you cast this spell, each player sacrifices X creatures. Gluttonous Hellkite enters the battlefield with two +1/+1 counters on it for each creature sacrificed this way.
+        Ability ability = new CastSourceTriggeredAbility(new GluttonousHellkiteEffect());
+        ability.addEffect(new GluttonousHellkiteReplacementEffect());
+
+        this.addAbility(ability);
+
+        // Flying
+        this.addAbility(FlyingAbility.getInstance());
+
+        // Trample
+        this.addAbility(TrampleAbility.getInstance());
+
+    }
+
+    private GluttonousHellkite(final GluttonousHellkite card) {
+        super(card);
+    }
+
+    @Override
+    public GluttonousHellkite copy() {
+        return new GluttonousHellkite(this);
+    }
+}
+
+class GluttonousHellkiteEffect extends OneShotEffect {
+
+    DynamicValue xValue;
+
+    GluttonousHellkiteEffect() {
+        super(Outcome.Sacrifice);
+        this.staticText = "each player sacrifices X creatures";
+    }
+
+    private GluttonousHellkiteEffect(final GluttonousHellkiteEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public GluttonousHellkiteEffect copy() {
+        return new GluttonousHellkiteEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+
+        List<TargetSacrifice> sacTargets = new ArrayList<>();
+        Player controller = game.getPlayer(source.getControllerId());
+        Object obj = getValue(CastSourceTriggeredAbility.SOURCE_CAST_SPELL_ABILITY);
+        int sacrificeAmount = 0;
+        if (controller == null ) {
+            return false;
+        }
+        if (obj instanceof SpellAbility) {
+            sacrificeAmount = ((SpellAbility) obj).getManaCostsToPay().getX();
+        }
+
+        game.informPlayers("sacrificeAmount: " + sacrificeAmount);
+        if (sacrificeAmount < 1) {
+            return true;
+        }
+        for (UUID playerId : game.getState().getPlayersInRange(controller.getId(), game)) {
+
+            Player player = game.getPlayer(playerId);
+            if (player != null) {
+
+                TargetSacrifice target = new TargetSacrifice(sacrificeAmount, StaticFilters.FILTER_PERMANENT_CREATURE);
+                if (target.canChoose(player.getId(), source, game)) {
+                    player.choose(Outcome.Sacrifice, target, source, game);
+                    sacTargets.add(target);
+                }
+            }
+        }
+        for (TargetSacrifice sacTarget : sacTargets) {
+            for (UUID targetId : sacTarget.getTargets()) {
+                Permanent permanent = game.getPermanent(targetId);
+                if (permanent != null) {
+                    permanent.sacrifice(source, game);
+                }
+            }
+        }
+
+        int sacrificeCount = sacTargets.stream().map(TargetImpl::getTargets).map(List::size).reduce(0, Integer::sum);
+        for (Effect effect : source.getEffects()) {
+            if (effect instanceof GluttonousHellkiteReplacementEffect) {
+                effect.setValue("GluttonousHellkiteCounters", sacrificeCount);
+            }
+        }
+        return true;
+    }
+}
+
+class GluttonousHellkiteReplacementEffect extends ReplacementEffectImpl {
+
+    GluttonousHellkiteReplacementEffect() {
+        super(Duration.OneUse, Outcome.BoostCreature);
+        staticText = "{this} enters the battlefield with two +1/+1 counters on it for each creature sacrificed this way";
+    }
+
+    private GluttonousHellkiteReplacementEffect(final GluttonousHellkiteReplacementEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public boolean checksEventType(GameEvent event, Game game) {
+        return event.getType() == GameEvent.EventType.ENTERS_THE_BATTLEFIELD;
+    }
+
+    @Override
+    public boolean applies(GameEvent event, Ability source, Game game) {
+        return event.getTargetId().equals(source.getSourceId());
+    }
+
+    @Override
+    public boolean replaceEvent(GameEvent event, Ability source, Game game) {
+        Object object = this.getValue("GluttonousHellkiteCounters");
+
+        game.informPlayers("triggered ETB");
+        Permanent permanent = game.getPermanent(source.getSourceId());
+        if (permanent == null) {
+            permanent = game.getPermanentEntering(source.getSourceId());
+        }
+        if (permanent == null) {
+            game.informPlayers("no perm");
+        }
+        if (permanent != null && object instanceof Integer) {
+            int amount = ((Integer) object);
+            game.informPlayers("Counter: " + amount);
+            permanent.addCounters(CounterType.P1P1.createInstance(amount * 2), source.getControllerId(), source, game);
+        }
+        return false;
+    }
+
+    @Override
+    public GluttonousHellkiteReplacementEffect copy() {
+        return new GluttonousHellkiteReplacementEffect(this);
+    }
+
+}
