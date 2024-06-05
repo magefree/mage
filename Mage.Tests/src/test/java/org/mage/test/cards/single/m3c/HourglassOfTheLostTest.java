8e2276b10e4d3ea0cc113ccf68627b8d42994e04
grimreap124 <19590931+grimreap124@users.noreply.github.com>
2024-06-05 22:25:38 +1000

    Create HourglassOfTheLostTest.java

---
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/m3c/HourglassOfTheLostTest.java
@@ -0,0 +1,40 @@
+package org.mage.test.cards.single.m3c;
+
+import mage.constants.PhaseStep;
+import mage.constants.Zone;
+import mage.counters.CounterType;
+import org.junit.Test;
+import org.mage.test.player.TestPlayer;
+import org.mage.test.serverside.base.CardTestPlayerBase;
+
+/**
+ * @author Susucr
+ */
+public class HourglassOfTheLostTest extends CardTestPlayerBase {
+
+    /**
+     * {@link mage.cards.h.HourglassOfTheLost HourglassOfTheLost} {2}{U}
+     */
+    private static final String hourglass = "Hourglass Of The Lost";
+
+    @Test
+    public void test_Simple() {
+        setStrictChooseMode(true);
+        skipInitShuffling();
+
+        addCard(Zone.BATTLEFIELD, playerA, hourglass);
+        addCard(Zone.GRAVEYARD, playerA, "Birds of Paradise"); // Tribal Enchantment
+
+        activateManaAbility(1, PhaseStep.PRECOMBAT_MAIN, playerA, "{T}: Add {W}");
+        setStopAt(1, PhaseStep.POSTCOMBAT_MAIN);
+        execute();
+        assertCounterCount(playerA, hourglass, CounterType.TIME,1);
+        activateAbility(3, PhaseStep.PRECOMBAT_MAIN, playerA, "{T},");
+        setStopAt(3, PhaseStep.POSTCOMBAT_MAIN);
+        execute();
+        assertExileCount(playerA, 1);
+        assertGraveyardCount(playerA, 0);
+        assertPermanentCount(playerA, "Birds of Paradise", 1);
+
+    }
+}
