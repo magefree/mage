aa26437fe5dcdf35c48ed83e39861a586dc66068
Susucre <34709007+Susucre@users.noreply.github.com>
2024-06-07 00:20:46 +0200

    implement [MH3] Primal Prayers

--- Mage.Sets/src/mage/cards/p/PrimalPrayers.java
+++ Mage.Sets/src/mage/cards/p/PrimalPrayers.java
@@ -1,13 +1,16 @@
-package mage.cards.a;
+package mage.cards.p;
 
 import mage.MageIdentifier;
 import mage.abilities.Ability;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.condition.common.SourceIsSpellCondition;
 import mage.abilities.costs.AlternativeCostSourceAbility;
+import mage.abilities.costs.common.PayEnergyCost;
 import mage.abilities.effects.ContinuousEffectImpl;
 import mage.abilities.effects.Effect;
 import mage.abilities.effects.common.continuous.CastAsThoughItHadFlashAllEffect;
+import mage.abilities.effects.common.counter.GetEnergyCountersControllerEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.*;
@@ -19,19 +22,9 @@ import mage.players.Player;
 import java.util.UUID;
 
 /**
- * 10/4/2004 	The mana cost of the creatures being cast is still the stated cost on the card,
- * even though you did not pay the cost.
- * 10/4/2004 	Aluren checks the actual printed cost on the creature card, and is not affected
- * by things which allow you to cast the spell for less.
- * 10/4/2004 	You can't choose to cast a creature as though it had flash via Aluren and still pay the mana cost.
- * You either cast the creature normally, or via Aluren without paying the mana cost.
- * 10/4/2004 	You can't use Aluren when casting a creature using another alternate means,
- * such as the Morph ability.
- * 8/1/2008 	If creature with X in its cost is cast this way, X can only be 0.
- *
- * @author emerald000
+ * @author Susucr
  */
-public final class Aluren extends CardImpl {
+public final class PrimalPrayers extends CardImpl {
 
     private static final FilterCreatureCard filter = new FilterCreatureCard("creature cards with mana value 3 or less");
 
@@ -39,30 +32,33 @@ public final class Aluren extends CardImpl {
         filter.add(new ManaValuePredicate(ComparisonType.OR_LESS, 3));
     }
 
-    public Aluren(UUID ownerId, CardSetInfo setInfo) {
+    public PrimalPrayers(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{2}{G}{G}");
 
+        // When Primal Prayers enters the battlefield, you get {E}{E}.
+        this.addAbility(new EntersBattlefieldTriggeredAbility(new GetEnergyCountersControllerEffect(2)));
 
-        // Any player may play creature cards with converted mana cost 3 or less without paying their mana cost and as though they had flash.
-        Ability ability = new SimpleStaticAbility(new AlurenRuleEffect());
-        ability.setIdentifier(MageIdentifier.AlurenAlternateCast); // Is the link allowing the Flash part to only affect that Alternative Cast
-        Effect effect = new CastAsThoughItHadFlashAllEffect(Duration.WhileOnBattlefield, filter, true);
+        // You may cast creature spells with mana value 3 or less by paying {E} rather than paying their mana costs. If you cast a spell this way, you may cast it as though it had flash.
+        Ability ability = new SimpleStaticAbility(new PrimalPrayersCastEffect());
+        ability.setIdentifier(MageIdentifier.PrimalPrayersAlternateCast); // Is the link allowing the Flash part to only affect that Alternative Cast
+        Effect effect = new CastAsThoughItHadFlashAllEffect(Duration.WhileOnBattlefield, filter, false);
         effect.setText("and as though they had flash");
         ability.addEffect(effect);
         this.addAbility(ability);
     }
 
-    private Aluren(final Aluren card) {
+    private PrimalPrayers(final PrimalPrayers card) {
         super(card);
     }
 
     @Override
-    public Aluren copy() {
-        return new Aluren(this);
+    public PrimalPrayers copy() {
+        return new PrimalPrayers(this);
     }
 }
 
-class AlurenRuleEffect extends ContinuousEffectImpl {
+// Very close to Aluren
+class PrimalPrayersCastEffect extends ContinuousEffectImpl {
 
     private static final FilterCreatureCard filter = new FilterCreatureCard("creature cards with mana value 3 or less");
 
@@ -72,23 +68,23 @@ class AlurenRuleEffect extends ContinuousEffectImpl {
 
     private final AlternativeCostSourceAbility alternativeCastingCostAbility;
 
-    public AlurenRuleEffect() {
+    public PrimalPrayersCastEffect() {
         super(Duration.WhileOnBattlefield, Outcome.Detriment);
-        staticText = "Any player may cast creature cards with mana value 3 or less without paying their mana cost";
+        staticText = "you may cast creature cards with mana value 3 or less by paying {E} rather than paying their mana costs";
         alternativeCastingCostAbility = new AlternativeCostSourceAbility(
-                null, SourceIsSpellCondition.instance, null, filter, true
+                new PayEnergyCost(1), SourceIsSpellCondition.instance, null, filter, true
         );
-        alternativeCastingCostAbility.setIdentifier(MageIdentifier.AlurenAlternateCast);
+        alternativeCastingCostAbility.setIdentifier(MageIdentifier.PrimalPrayersAlternateCast);
     }
 
-    private AlurenRuleEffect(final AlurenRuleEffect effect) {
+    private PrimalPrayersCastEffect(final PrimalPrayersCastEffect effect) {
         super(effect);
         this.alternativeCastingCostAbility = effect.alternativeCastingCostAbility;
     }
 
     @Override
-    public AlurenRuleEffect copy() {
-        return new AlurenRuleEffect(this);
+    public PrimalPrayersCastEffect copy() {
+        return new PrimalPrayersCastEffect(this);
     }
 
     @Override
@@ -101,12 +97,7 @@ class AlurenRuleEffect extends ContinuousEffectImpl {
     public boolean apply(Layer layer, SubLayer sublayer, Ability source, Game game) {
         Player controller = game.getPlayer(source.getControllerId());
         if (controller != null) {
-            for (UUID playerId : game.getState().getPlayersInRange(controller.getId(), game)) {
-                Player player = game.getPlayer(playerId);
-                if (player != null) {
-                    player.getAlternativeSourceCosts().add(alternativeCastingCostAbility);
-                }
-            }
+            controller.getAlternativeSourceCosts().add(alternativeCastingCostAbility);
             return true;
         }
         return false;
