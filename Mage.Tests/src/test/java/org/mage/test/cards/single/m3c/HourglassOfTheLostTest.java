909f627f147a278399858075326d5b298f0012d8
grimreap124 <19590931+grimreap124@users.noreply.github.com>
2024-06-06 10:19:01 +1000

    Update HourglassOfTheLostTest.java

--- Mage.Tests/src/test/java/org/mage/test/cards/single/m3c/HourglassOfTheLostTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/m3c/HourglassOfTheLostTest.java
@@ -4,7 +4,6 @@ import mage.constants.PhaseStep;
 import mage.constants.Zone;
 import mage.counters.CounterType;
 import org.junit.Test;
-import org.mage.test.player.TestPlayer;
 import org.mage.test.serverside.base.CardTestPlayerBase;
 
 /**
@@ -15,7 +14,7 @@ public class HourglassOfTheLostTest extends CardTestPlayerBase {
     /**
      * {@link mage.cards.h.HourglassOfTheLost HourglassOfTheLost} {2}{U}
      */
-    private static final String hourglass = "Hourglass Of The Lost";
+    private static final String hourglass = "Hourglass of the Lost";
 
     @Test
     public void test_Simple() {
@@ -23,14 +22,18 @@ public class HourglassOfTheLostTest extends CardTestPlayerBase {
         skipInitShuffling();
 
         addCard(Zone.BATTLEFIELD, playerA, hourglass);
-        addCard(Zone.GRAVEYARD, playerA, "Birds of Paradise"); // Tribal Enchantment
-
+            addCard(Zone.GRAVEYARD, playerA, "Birds of Paradise"); // Tribal Enchantment
+        
         activateManaAbility(1, PhaseStep.PRECOMBAT_MAIN, playerA, "{T}: Add {W}");
         setStopAt(1, PhaseStep.POSTCOMBAT_MAIN);
         execute();
         assertCounterCount(playerA, hourglass, CounterType.TIME,1);
-        activateAbility(3, PhaseStep.PRECOMBAT_MAIN, playerA, "{T},");
+
+        showAvailableAbilities("Abilities: ", 3, PhaseStep.PRECOMBAT_MAIN, playerA);
+        activateAbility(3, PhaseStep.PRECOMBAT_MAIN, playerA, "{T}, Remove X");
+        setChoice(playerA, "X=1");
         setStopAt(3, PhaseStep.POSTCOMBAT_MAIN);
+
         execute();
         assertExileCount(playerA, 1);
         assertGraveyardCount(playerA, 0);
