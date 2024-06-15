f7b3bc8755f2b656517f384ffad6113adcfed6be
tiera3 <87589219+tiera3@users.noreply.github.com>
2024-06-15 14:53:29 +1000

    restore existing file, add not implemented comment

--- Mage.Sets/src/mage/cards/d/DealBroker.java
+++ Mage.Sets/src/mage/cards/d/DealBroker.java
@@ -3,8 +3,10 @@ package mage.cards.d;
 import java.util.UUID;
 import mage.MageInt;
 import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.costs.common.TapSourceCost;
 import mage.abilities.effects.common.DrawDiscardControllerEffect;
+import mage.abilities.effects.common.InfoEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
@@ -12,18 +14,27 @@ import mage.constants.SubType;
 import mage.constants.Zone;
 
 /**
- * @author tiera3 - based on MerfolkLooter
+ * 
+ * @author L_J
  * note - draftmatters ability not implemented
  */
 public final class DealBroker extends CardImpl {
 
     public DealBroker(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.CREATURE},"{3}");
+        super(ownerId,setInfo,new CardType[]{CardType.ARTIFACT, CardType.CREATURE},"{3}");
         this.subtype.add(SubType.CONSTRUCT);
-
         this.power = new MageInt(2);
         this.toughness = new MageInt(3);
-		// {t}: Draw a card, then discard a card.
+
+        // TODO: Draft specific abilities not implemented
+        // Draft Deal Broker face up.
+        this.addAbility(new SimpleStaticAbility(Zone.ALL, new InfoEffect("Draft Deal Broker face up.")));
+
+        // Immediately after the draft, you may reveal a card in your card pool. Each other player may offer you one card in their card pool in exchange. You may accept any one offer.
+        this.addAbility(new SimpleStaticAbility(Zone.ALL, new InfoEffect("Immediately after the draft, you may reveal a card in your card pool. "
+                + "Each other player may offer you one card in their card pool in exchange. You may accept any one offer - not implemented.")));
+
+        // {T}: Draw a card, then discard a card.
         this.addAbility(new SimpleActivatedAbility(Zone.BATTLEFIELD, new DrawDiscardControllerEffect(), new TapSourceCost()));
     }
 
