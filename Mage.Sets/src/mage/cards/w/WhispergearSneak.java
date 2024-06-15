02d303bcbdbcabf8eaa643c49be3d83dbfe127e7
tiera3 <87589219+tiera3@users.noreply.github.com>
2024-06-15 14:55:31 +1000

    Added SimpleStaticAbility for draftmatters effects

--- Mage.Sets/src/mage/cards/w/WhispergearSneak.java
+++ Mage.Sets/src/mage/cards/w/WhispergearSneak.java
@@ -6,10 +6,12 @@ import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.SubType;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.common.InfoEffect;
 
 /**
  *
- * @author tiera3 - based on PrizefighterConstruct
+ * @author tiera3 - based on PrizefighterConstruct and CanalDredger
  * note - draftmatters ability not implemented
  */
 public final class WhispergearSneak extends CardImpl {
@@ -20,6 +22,14 @@ public final class WhispergearSneak extends CardImpl {
         this.subtype.add(SubType.CONSTRUCT);
         this.power = new MageInt(1);
         this.toughness = new MageInt(1);
+
+        // TODO: Draft specific abilities not implemented
+        // Draft Whispergear Sneak face up.
+        this.addAbility(new SimpleStaticAbility(Zone.ALL, new InfoEffect("Draft Whispergear Sneak face up - not implemented.")));
+
+        // During the draft, you may turn Whispergear Sneak face down. If you do, look at any unopened booster pack in the draft or any booster pack not being looked at by another player.
+		this.addAbility(new SimpleStaticAbility(Zone.ALL, new InfoEffect("During the draft, you may turn Whispergear Sneak face down. If you do, "
+		        + "look at any unopened booster pack in the draft or any booster pack not being looked at by another player - not implemented.")));
     }
 
     private WhispergearSneak(final WhispergearSneak card) {
