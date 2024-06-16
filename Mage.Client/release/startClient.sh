91eb1057954101a4e31e15c53ff98bdff22f9bc3
Oleg Agafonov <jaydi85@gmail.com>
2024-06-17 00:39:12 +0400

    fixed mage client run script

--- Mage.Client/release/startClient.sh
+++ Mage.Client/release/startClient.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Xmx1024m -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true ./lib/mage-client-${project.version}.jar &
+java -Xmx1024m -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true .\lib\mage-client-${project.version}.jar &
