38acc47c679d68f406c7ee1504fb779d78bc12b3
grimreap124 <19590931+grimreap124@users.noreply.github.com>
2024-06-20 10:28:30 +1000

    Implement-ACR-The-Revelations-of-Ezio

---
+++ Mage.Sets/src/mage/cards/t/TheRevelationsOfEzio.java
@@ -0,0 +1,131 @@
+package mage.cards.t;
+
+import java.util.UUID;
+
+import mage.abilities.DelayedTriggeredAbility;
+import mage.abilities.common.AttacksTriggeredAbility;
+import mage.abilities.common.AttacksWithCreaturesTriggeredAbility;
+import mage.abilities.common.SagaAbility;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.effects.common.CreateDelayedTriggeredAbilityEffect;
+import mage.abilities.effects.common.DestroyTargetEffect;
+import mage.abilities.effects.common.FightTargetsEffect;
+import mage.abilities.effects.common.ReturnFromGraveyardToBattlefieldTargetEffect;
+import mage.abilities.effects.common.counter.AddCountersSourceEffect;
+import mage.abilities.effects.common.counter.AddCountersTargetEffect;
+import mage.constants.Duration;
+import mage.constants.SagaChapter;
+import mage.constants.SubType;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.counters.CounterType;
+import mage.filter.FilterCard;
+import mage.filter.FilterPermanent;
+import mage.filter.StaticFilters;
+import mage.filter.common.FilterCreatureCard;
+import mage.filter.common.FilterCreaturePermanent;
+import mage.filter.common.FilterOpponentsCreaturePermanent;
+import mage.filter.predicate.permanent.TappedPredicate;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.permanent.Permanent;
+import mage.target.TargetPermanent;
+import mage.target.common.TargetCardInYourGraveyard;
+import mage.target.common.TargetControlledCreaturePermanent;
+import mage.target.common.TargetCreaturePermanent;
+import mage.target.targetpointer.FixedTarget;
+
+/**
+ * @author grimreap124
+ */
+public final class TheRevelationsOfEzio extends CardImpl {
+
+    private static final FilterCreaturePermanent filter
+            = new FilterOpponentsCreaturePermanent("tapped creature an opponent controls");
+    private static final FilterCard assassinFilter
+            = new FilterCreatureCard("Assassin creature card");
+
+    static {
+        assassinFilter.add(SubType.ASSASSIN.getPredicate());
+        filter.add(TappedPredicate.TAPPED);
+    }
+
+    public TheRevelationsOfEzio(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{2}{B}");
+
+        this.subtype.add(SubType.SAGA);
+
+        // (As this Saga enters and after your draw step, add a lore counter. Sacrifice after III.)
+        SagaAbility sagaAbility = new SagaAbility(this);
+        // I -- Destroy target tapped creature an opponent controls.
+        sagaAbility.addChapterEffect(
+                this, SagaChapter.CHAPTER_I, SagaChapter.CHAPTER_I,
+                ability -> {
+                    ability.addEffect(new DestroyTargetEffect());
+                    ability.addTarget(new TargetCreaturePermanent(filter));
+                }
+        );
+        // II -- Whenever an Assassin you control attacks this turn, put a +1/+1 counter on it.
+        sagaAbility.addChapterEffect(this, SagaChapter.CHAPTER_II, new CreateDelayedTriggeredAbilityEffect(new TheRevelationsOfEzioTriggeredAbility()));
+        // III -- Return target Assassin creature card from your graveyard to the battlefield with an additional +1/+1 counter on it.
+        sagaAbility.addChapterEffect(
+                this, SagaChapter.CHAPTER_III, SagaChapter.CHAPTER_III,
+                ability -> {
+                    ability.addEffect(new ReturnFromGraveyardToBattlefieldTargetEffect());
+                    ability.addTarget(new TargetCardInYourGraveyard(assassinFilter).withTargetName(assassinFilter.getMessage() + " from your graveyard"));
+                    ability.addEffect(new AddCountersTargetEffect(CounterType.P1P1.createInstance()).setText("with an additional +1/+1 counter on it"));
+                }
+        );
+
+        this.addAbility(sagaAbility);
+    }
+
+    private TheRevelationsOfEzio(final TheRevelationsOfEzio card) {
+        super(card);
+    }
+
+    @Override
+    public TheRevelationsOfEzio copy() {
+        return new TheRevelationsOfEzio(this);
+    }
+}
+
+class TheRevelationsOfEzioTriggeredAbility extends DelayedTriggeredAbility {
+
+    TheRevelationsOfEzioTriggeredAbility() {
+        super(new AddCountersTargetEffect(CounterType.P1P1.createInstance()), Duration.EndOfTurn, false);
+
+    }
+
+    private TheRevelationsOfEzioTriggeredAbility(final TheRevelationsOfEzioTriggeredAbility ability) {
+        super(ability);
+    }
+
+    @Override
+    public TheRevelationsOfEzioTriggeredAbility copy() {
+        return new TheRevelationsOfEzioTriggeredAbility(this);
+    }
+
+    @Override
+    public boolean checkEventType(GameEvent event, Game game) {
+        return event.getType() == GameEvent.EventType.ATTACKER_DECLARED;
+    }
+
+    @Override
+    public boolean checkTrigger(GameEvent event, Game game) {
+        Permanent permanent = game.getPermanent(event.getSourceId());
+        if (permanent != null
+                && permanent.isControlledBy(getControllerId())
+                && permanent.hasSubtype(SubType.ASSASSIN, game)) {
+            getEffects().setTargetPointer(new FixedTarget(permanent, game));
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String getRule() {
+        return "Whenever an Assassin you control attacks this turn, put a +1/+1 counter on it.";
+    }
+}
