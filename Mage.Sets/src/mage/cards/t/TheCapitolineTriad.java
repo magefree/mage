72283d70673c7c54b5af1a9e9333b89376fa6f4d
grimreap124 <19590931+grimreap124@users.noreply.github.com>
2024-06-20 14:08:50 +1000

    Implement-ACR-The-Capitoline-Triad
    Just need to add Emblem to token DB

---
+++ Mage.Sets/src/mage/cards/t/TheCapitolineTriad.java
@@ -0,0 +1,140 @@
+package mage.cards.t;
+
+import java.awt.*;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import mage.MageInt;
+import mage.MageObject;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.costs.OrCost;
+import mage.abilities.costs.common.ExileManaValueFromGraveyardCost;
+import mage.abilities.dynamicvalue.DynamicValue;
+import mage.abilities.dynamicvalue.common.CardsInControllerGraveyardCount;
+import mage.abilities.effects.common.CreateTokenEffect;
+import mage.abilities.effects.common.GetEmblemEffect;
+import mage.abilities.effects.common.cost.SpellCostReductionForEachSourceEffect;
+import mage.abilities.hint.HintUtils;
+import mage.abilities.hint.ValueHint;
+import mage.constants.SubType;
+import mage.constants.SuperType;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Zone;
+import mage.filter.FilterCard;
+import mage.filter.StaticFilters;
+import mage.filter.predicate.mageobject.HistoricPredicate;
+import mage.game.Game;
+import mage.game.command.emblems.TheCapitolineTriadEmblem;
+import mage.game.permanent.token.CitizenToken;
+import mage.target.common.TargetCardInYourGraveyard;
+import mage.util.CardUtil;
+
+/**
+ * @author grimreap124
+ */
+public final class TheCapitolineTriad extends CardImpl {
+
+    private static final FilterCard filter = new FilterCard("historic card");
+
+    static {
+        filter.add(HistoricPredicate.instance);
+    }
+
+    public TheCapitolineTriad(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{10}");
+        
+        this.supertype.add(SuperType.LEGENDARY);
+        this.subtype.add(SubType.GOD);
+        this.subtype.add(SubType.ARTIFICER);
+        this.power = new MageInt(7);
+        this.toughness = new MageInt(7);
+
+        // Those Who Came Before - This spell costs 1 less to cast for each historic card in your graveyard.
+        DynamicValue xValue = new CardsInControllerGraveyardCount(filter);
+        Ability ability = new SimpleStaticAbility(Zone.ALL, new SpellCostReductionForEachSourceEffect(1, xValue));
+        ability.setRuleAtTheTop(true);
+        ability.addHint(new ValueHint("Historic cards in your graveyard", xValue));
+        ability.withFlavorWord("Those Who Came Before");
+        this.addAbility(ability);
+
+        // Exile any number of historic cards from your graveyard with total mana value 30 or greater: You get an emblem with "Creatures you control have base power and toughness 9/9."
+        ability = new SimpleActivatedAbility(new GetEmblemEffect(new TheCapitolineTriadEmblem()), new ExileManaValueFromGraveyardCost(new TheCapitolineTriadTarget(), 30));
+        this.addAbility(ability);
+    }
+
+    private TheCapitolineTriad(final TheCapitolineTriad card) {
+        super(card);
+    }
+
+    @Override
+    public TheCapitolineTriad copy() {
+        return new TheCapitolineTriad(this);
+    }
+}
+
+class TheCapitolineTriadTarget extends TargetCardInYourGraveyard {
+
+    private static final FilterCard filter = new FilterCard("historic cards");
+
+    static {
+        filter.add(HistoricPredicate.instance);
+    }
+
+    TheCapitolineTriadTarget() {
+        super(1, Integer.MAX_VALUE, filter, true);
+    }
+
+    private TheCapitolineTriadTarget(final TheCapitolineTriadTarget target) {
+        super(target);
+    }
+
+    @Override
+    public TheCapitolineTriadTarget copy() {
+        return new TheCapitolineTriadTarget(this);
+    }
+
+    @Override
+    public boolean isChosen(Game game) {
+        return super.isChosen(game) && metCondition(this.getTargets(), game);
+    }
+
+    @Override
+    public String getMessage(Game game) {
+        String text = "Select " + CardUtil.addArticle(targetName);
+        int manaValueOfSelection = manaValueOfSelection(this.getTargets(), game);
+        text += " (selected " + this.getTargets().size() + " cards; card types: ";
+        text += HintUtils.prepareText(
+                manaValueOfSelection + " of 30",
+                manaValueOfSelection >= 30 ? Color.GREEN : Color.RED
+        );
+        text +=")";
+        return text;
+    }
+
+    @Override
+    public boolean canChoose(UUID sourceControllerId, Ability source, Game game) {
+        if (!super.canChoose(sourceControllerId, source, game)) {
+            return false;
+        }
+        // Check that exiling all the possible cards would have >= 4 different card types
+        return metCondition(this.possibleTargets(sourceControllerId, source, game), game);
+    }
+
+    private static int manaValueOfSelection(Collection<UUID> cardsIds, Game game) {
+        return cardsIds
+                .stream()
+                .map(game::getCard)
+                .filter(Objects::nonNull).mapToInt(MageObject::getManaValue).sum();
+    }
+
+    private static boolean metCondition(Collection<UUID> cardsIds, Game game) {
+        return manaValueOfSelection(cardsIds, game) >= 30;
+    }
+}
--- Mage.Sets/src/mage/sets/AssassinsCreed.java
+++ Mage.Sets/src/mage/sets/AssassinsCreed.java
@@ -59,6 +59,7 @@ public final class AssassinsCreed extends ExpansionSet {
         cards.add(new SetCardInfo("Templar Knight", 10, Rarity.UNCOMMON, mage.cards.t.TemplarKnight.class));
         cards.add(new SetCardInfo("Temporal Trespass", 86, Rarity.MYTHIC, mage.cards.t.TemporalTrespass.class));
         cards.add(new SetCardInfo("The Animus", 69, Rarity.RARE, mage.cards.t.TheAnimus.class));
+        cards.add(new SetCardInfo("The Capitoline Triad", 1, Rarity.MYTHIC, mage.cards.t.TheCapitolineTriad.class));
         cards.add(new SetCardInfo("The Spear of Leonidas", 38, Rarity.RARE, mage.cards.t.TheSpearOfLeonidas.class));
         cards.add(new SetCardInfo("Waterlogged Grove", 116, Rarity.RARE, mage.cards.w.WaterloggedGrove.class));
     }
