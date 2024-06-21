f420c279dee03e9c011f2add726888cc552007eb
grimreap124 <19590931+grimreap124@users.noreply.github.com>
2024-06-21 10:10:56 +1000

    Update Jackdaw.java

--- Mage.Sets/src/mage/cards/j/Jackdaw.java
+++ Mage.Sets/src/mage/cards/j/Jackdaw.java
@@ -5,9 +5,11 @@ import mage.MageInt;
 import mage.abilities.common.DealsCombatDamageToAPlayerTriggeredAbility;
 import mage.abilities.costs.common.DiscardHandCost;
 import mage.abilities.dynamicvalue.DynamicValue;
+import mage.abilities.dynamicvalue.common.ArtifactYouControlCount;
 import mage.abilities.dynamicvalue.common.PermanentsOnBattlefieldCount;
 import mage.abilities.effects.common.DoIfCostPaid;
 import mage.abilities.effects.common.DrawCardSourceControllerEffect;
+import mage.abilities.hint.common.ArtifactYouControlHint;
 import mage.constants.SubType;
 import mage.constants.SuperType;
 import mage.abilities.keyword.CrewAbility;
@@ -22,12 +24,6 @@ import mage.filter.common.FilterControlledPermanent;
  */
 public final class Jackdaw extends CardImpl {
 
-    private static final FilterPermanent filter = new FilterControlledPermanent("artifact you control");
-
-    static {
-        filter.add(CardType.ARTIFACT.getPredicate());
-    }
-
     public Jackdaw(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT}, "{1}{U}{R}");
         
@@ -35,9 +31,9 @@ public final class Jackdaw extends CardImpl {
         this.subtype.add(SubType.VEHICLE);
         this.power = new MageInt(4);
         this.toughness = new MageInt(4);
-        DynamicValue xValue = new PermanentsOnBattlefieldCount(filter);
         // Whenever Jackdaw deals combat damage to a player, you may discard your hand. If you do, draw a card for each artifact you control.
-        this.addAbility(new DealsCombatDamageToAPlayerTriggeredAbility(new DoIfCostPaid(new DrawCardSourceControllerEffect(xValue), new DiscardHandCost()), false));
+        this.addAbility(new DealsCombatDamageToAPlayerTriggeredAbility(new DoIfCostPaid(new DrawCardSourceControllerEffect(ArtifactYouControlCount.instance), new DiscardHandCost()), false)
+                .addHint(ArtifactYouControlHint.instance));
         // Crew 3
         this.addAbility(new CrewAbility(3));
 
