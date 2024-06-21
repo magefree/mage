4f6d23393acadaeb1389c0fd3afd06d5f83103d4
grimreap124 <19590931+grimreap124@users.noreply.github.com>
2024-06-21 10:04:55 +1000

    Implement-ACR-Desmond-Miles

--- Mage.Sets/src/mage/cards/d/DesmondMiles.java
+++ Mage.Sets/src/mage/cards/d/DesmondMiles.java
@@ -5,11 +5,11 @@ import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.DealsCombatDamageToAPlayerTriggeredAbility;
 import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.dynamicvalue.AdditiveDynamicValue;
 import mage.abilities.dynamicvalue.DynamicValue;
-import mage.abilities.dynamicvalue.common.AssassinsControlledAndInGraveCount;
-import mage.abilities.dynamicvalue.common.SavedDamageValue;
-import mage.abilities.dynamicvalue.common.StaticValue;
-import mage.abilities.effects.OneShotEffect;
+import mage.abilities.dynamicvalue.common.*;
+
+import mage.abilities.effects.common.SurveilXEffect;
 import mage.abilities.effects.common.continuous.BoostSourceEffect;
 import mage.abilities.effects.keyword.ScryEffect;
 import mage.abilities.effects.keyword.SurveilEffect;
@@ -17,14 +17,26 @@ import mage.constants.*;
 import mage.abilities.keyword.MenaceAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
+import mage.filter.FilterCard;
+import mage.filter.common.FilterControlledPermanent;
+import mage.filter.predicate.mageobject.AnotherPredicate;
 import mage.game.Game;
 import mage.game.permanent.Permanent;
+import mage.players.Player;
 
 /**
  *
  * @author grimreap124
  */
 public final class DesmondMiles extends CardImpl {
+
+    private static final FilterCard filter = new FilterCard("Assassin card in your graveyard");
+    private static final FilterControlledPermanent filter2 = new FilterControlledPermanent(SubType.ASSASSIN);
+
+    static {
+        filter2.add(AnotherPredicate.instance);
+        filter.add(SubType.ASSASSIN.getPredicate());
+    }
 
     public DesmondMiles(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{1}{B}");
@@ -38,11 +50,15 @@ public final class DesmondMiles extends CardImpl {
         // Menace
         this.addAbility(new MenaceAbility());
 
+        AdditiveDynamicValue assassinsCount = new AdditiveDynamicValue(new CardsInControllerGraveyardCount(), new PermanentsOnBattlefieldCount(filter2));
         // Desmond Miles gets +1/+0 for each other Assassin you control and each Assassin card in your graveyard.
-        Ability ability = new SimpleStaticAbility(new BoostSourceEffect(AssassinsControlledAndInGraveCount.FOR_EACH, StaticValue.get(0), Duration.WhileOnBattlefield));
+        Ability ability = new SimpleStaticAbility(new BoostSourceEffect(assassinsCount, StaticValue.get(0), Duration.WhileOnBattlefield)
+                .setText("{this} gets +1/+0 for each other Assassin you control and each Assassin card in your graveyard"));
         this.addAbility(ability);
+
         // Whenever Desmond Miles deals combat damage to a player, surveil X, where X is the amount of damage it dealt to that player.
-        this.addAbility(new DealsCombatDamageToAPlayerTriggeredAbility(new DesmondMilesEffect(SavedDamageValue.MANY)));
+        this.addAbility(new DealsCombatDamageToAPlayerTriggeredAbility(new SurveilXEffect(SavedDamageValue.MANY,
+                "where X is the amount of damage it dealt to that player.")));
     }
 
     private DesmondMiles(final DesmondMiles card) {
@@ -55,29 +71,3 @@ public final class DesmondMiles extends CardImpl {
     }
 }
 
-class DesmondMilesEffect extends OneShotEffect {
-
-    private final DynamicValue amount;
-
-    DesmondMilesEffect(DynamicValue amount) {
-        super(Outcome.Benefit);
-        this.amount = amount;
-        staticText = "surveil X, where X is the amount of damage it dealt to that player";
-    }
-
-    private DesmondMilesEffect(final DesmondMilesEffect effect) {
-        super(effect);
-        this.amount = effect.amount;
-    }
-
-    @Override
-    public DesmondMilesEffect copy() {
-        return new DesmondMilesEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        int xValue = amount.calculate(game, source, this);
-        return new SurveilEffect(xValue).apply(game, source);
-    }
-}
