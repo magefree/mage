184387619d1695f5a9680fb28a7385a076e7fdaf
grimreap124 <19590931+grimreap124@users.noreply.github.com>
2024-06-23 10:15:27 +1000

    Implement-M3C-Wonderscape-Sage

--- Mage.Sets/src/mage/cards/w/WonderscapeSage.java
+++ Mage.Sets/src/mage/cards/w/WonderscapeSage.java
@@ -1,11 +1,10 @@
 package mage.cards.w;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 import java.util.UUID;
 
 import mage.MageInt;
-import mage.abilities.costs.CostImpl;
+import mage.abilities.costs.common.ReturnToHandChosenControlledPermanentCost;
 import mage.constants.SubType;
 import mage.constants.SuperType;
 import mage.constants.Zone;
@@ -22,12 +21,10 @@ import mage.abilities.costs.common.TapSourceCost;
 import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.DrawCardSourceControllerEffect;
 import mage.abilities.keyword.FlyingAbility;
-import mage.cards.Card;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.Outcome;
-import mage.util.CardUtil;
 
 /**
  * @author grimreap124
@@ -54,7 +51,7 @@ public final class WonderscapeSage extends CardImpl {
 
         // {T}, Return a land you control to its owner's hand: Draw a card. Then discard a card unless that land had a nonbasic land type.
         Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new WonderscapeSageEffect(), new TapSourceCost());
-        ability.addCost(new WonderscapeSageCost(new TargetControlledPermanent(filter)));
+        ability.addCost(new ReturnToHandChosenControlledPermanentCost(new TargetControlledPermanent(filter)));
         this.addAbility(ability);
     }
 
@@ -92,74 +89,21 @@ class WonderscapeSageEffect extends OneShotEffect {
             return false;
         }
         for (Cost cost :source.getCosts()) {
-            if (cost instanceof WonderscapeSageCost) {
-                if (((WonderscapeSageCost) cost).nonbasic) {
-                    return true;
+            if (cost instanceof ReturnToHandChosenControlledPermanentCost) {
+                List<Permanent> permanents = ((ReturnToHandChosenControlledPermanentCost) cost).getPermanents();
+                if (permanents.size() == 1) {
+                    Permanent permanent = permanents.get(0);
+                    if (permanent.getCardType().contains(CardType.LAND) && permanent.getSubtype().stream()
+                            .filter(subType -> !(subType == SubType.PLAINS || subType == SubType.ISLAND || subType == SubType.SWAMP
+                                    || subType == SubType.MOUNTAIN || subType == SubType.FOREST)).count() > 0) {
+                            return true;
+                    }
                 }
+            }
                 player.discardOne(false, false, source, game);
                 return true;
-            }
         }
+
         return true;
     }
-}
-
-class WonderscapeSageCost extends CostImpl {
-
-    protected boolean nonbasic = false;
-
-    public WonderscapeSageCost(TargetControlledPermanent target) {
-        target.withNotTarget(true);
-        this.addTarget(target);
-        if (target.getMaxNumberOfTargets() > 1 && target.getMaxNumberOfTargets() == target.getNumberOfTargets()) {
-            this.text = "return " + CardUtil.numberToText(target.getMaxNumberOfTargets()) + ' '
-                    + target.getTargetName()
-                    + (target.getTargetName().endsWith(" you control") ? "" : " you control")
-                    + " to their owner's hand";
-        } else {
-            this.text = "return " + CardUtil.addArticle(target.getTargetName())
-                    + (target.getTargetName().endsWith(" you control") ? "" : " you control")
-                    + " to its owner's hand";
-        }
-    }
-
-    private WonderscapeSageCost(final WonderscapeSageCost cost) {
-        super(cost);
-        this.nonbasic = cost.nonbasic;
-    }
-
-    @Override
-    public boolean pay(Ability ability, Game game, Ability source, UUID controllerId, boolean noMana, Cost costToPay) {
-        Player controller = game.getPlayer(controllerId);
-        if (controller != null) {
-            if (this.getTargets().choose(Outcome.ReturnToHand, controllerId, source.getSourceId(), source, game)) {
-                Set<Card> permanentsToReturn = new HashSet<>();
-                for (UUID targetId : this.getTargets().get(0).getTargets()) {
-                    Permanent permanent = game.getPermanent(targetId);
-                    if (permanent == null) {
-                        return false;
-                    }
-                    permanentsToReturn.add(permanent);
-                    if (permanent.getCardType().contains(CardType.LAND) && permanent.getSubtype().stream()
-                            .filter(subType -> !(subType == SubType.PLAINS || subType == SubType.ISLAND || subType == SubType.SWAMP || subType == SubType.MOUNTAIN || subType == SubType.FOREST)).count() > 0){
-                        nonbasic = true;
-                    }
-                }
-                controller.moveCards(permanentsToReturn, Zone.HAND, ability, game);
-                paid = true;
-            }
-        }
-        return paid;
-    }
-
-    @Override
-    public boolean canPay(Ability ability, Ability source, UUID controllerId, Game game) {
-        return this.getTargets().canChoose(controllerId, source, game);
-    }
-
-    @Override
-    public WonderscapeSageCost copy() {
-        return new WonderscapeSageCost(this);
-    }
-
 }
--- Mage/src/main/java/mage/abilities/costs/common/ReturnToHandChosenControlledPermanentCost.java
+++ Mage/src/main/java/mage/abilities/costs/common/ReturnToHandChosenControlledPermanentCost.java
@@ -13,14 +13,14 @@ import mage.players.Player;
 import mage.target.common.TargetControlledPermanent;
 import mage.util.CardUtil;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * @author BetaSteward_at_googlemail.com
  */
 public class ReturnToHandChosenControlledPermanentCost extends CostImpl {
+
+    private final List<Permanent> permanents = new ArrayList<>();
 
     public ReturnToHandChosenControlledPermanentCost(TargetControlledPermanent target) {
         target.withNotTarget(true);
@@ -39,6 +39,9 @@ public class ReturnToHandChosenControlledPermanentCost extends CostImpl {
 
     private ReturnToHandChosenControlledPermanentCost(final ReturnToHandChosenControlledPermanentCost cost) {
         super(cost);
+        for (Permanent permanent : cost.permanents) {
+            this.permanents.add(permanent.copy());
+        }
     }
 
     @Override
@@ -52,6 +55,7 @@ public class ReturnToHandChosenControlledPermanentCost extends CostImpl {
                     if (permanent == null) {
                         return false;
                     }
+                    addSacrificeTarget(game, permanent);
                     permanentsToReturn.add(permanent);
                 }
                 controller.moveCards(permanentsToReturn, Zone.HAND, ability, game);
@@ -59,6 +63,10 @@ public class ReturnToHandChosenControlledPermanentCost extends CostImpl {
             }
         }
         return paid;
+    }
+
+    protected void addSacrificeTarget(Game game, Permanent permanent) {
+        permanents.add(permanent.copy());
     }
 
     @Override
@@ -69,6 +77,10 @@ public class ReturnToHandChosenControlledPermanentCost extends CostImpl {
     @Override
     public ReturnToHandChosenControlledPermanentCost copy() {
         return new ReturnToHandChosenControlledPermanentCost(this);
+    }
+
+    public List<Permanent> getPermanents() {
+        return permanents;
     }
 
 }
