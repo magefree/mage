a47c2339b0f1816d7f4ce39fa1c646e708adbbe7
Steven Knipe <knipesteven@gmail.com>
2024-06-26 01:43:55 -0700

    Convert some cards

--- Mage.Sets/src/mage/cards/b/BlindZealot.java
+++ Mage.Sets/src/mage/cards/b/BlindZealot.java
@@ -2,8 +2,9 @@ package mage.cards.b;
 
 import mage.MageInt;
 import mage.abilities.Ability;
-import mage.abilities.TriggeredAbilityImpl;
+import mage.abilities.common.DealsCombatDamageToAPlayerTriggeredAbility;
 import mage.abilities.costs.common.SacrificeSourceCost;
+import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.DestroyTargetEffect;
 import mage.abilities.effects.common.DoIfCostPaid;
 import mage.abilities.keyword.IntimidateAbility;
@@ -11,22 +12,18 @@ import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.SubType;
-import mage.constants.Zone;
 import mage.filter.common.FilterCreaturePermanent;
-import mage.filter.predicate.permanent.ControllerIdPredicate;
-import mage.game.Game;
-import mage.game.events.DamagedEvent;
-import mage.game.events.GameEvent;
-import mage.game.events.GameEvent.EventType;
-import mage.players.Player;
-import mage.target.common.TargetCreaturePermanent;
+import mage.target.TargetPermanent;
+import mage.target.targetadjustment.DamagedPlayerControlsTargetAdjuster;
 
 import java.util.UUID;
 
 /**
- * @author North
+ * @author notgreat
  */
 public final class BlindZealot extends CardImpl {
+    private static final FilterCreaturePermanent filter
+            = new FilterCreaturePermanent("creature that player controls");
 
     public BlindZealot(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{1}{B}{B}");
@@ -39,7 +36,10 @@ public final class BlindZealot extends CardImpl {
         this.addAbility(IntimidateAbility.getInstance());
 
         // Whenever Blind Zealot deals combat damage to a player, you may sacrifice it. If you do, destroy target creature that player controls.
-        Ability ability = new BlindZealotTriggeredAbility();
+        OneShotEffect effect = new DoIfCostPaid(new DestroyTargetEffect(), new SacrificeSourceCost());
+        Ability ability = new DealsCombatDamageToAPlayerTriggeredAbility(effect, false, true);
+        ability.addTarget(new TargetPermanent(filter));
+        ability.setTargetAdjuster(new DamagedPlayerControlsTargetAdjuster());
         this.addAbility(ability);
     }
 
@@ -50,47 +50,5 @@ public final class BlindZealot extends CardImpl {
     @Override
     public BlindZealot copy() {
         return new BlindZealot(this);
-    }
-}
-
-class BlindZealotTriggeredAbility extends TriggeredAbilityImpl {
-
-    BlindZealotTriggeredAbility() {
-        super(Zone.BATTLEFIELD, new DoIfCostPaid(new DestroyTargetEffect(), new SacrificeSourceCost()), false);
-    }
-
-    private BlindZealotTriggeredAbility(final BlindZealotTriggeredAbility ability) {
-        super(ability);
-    }
-
-    @Override
-    public BlindZealotTriggeredAbility copy() {
-        return new BlindZealotTriggeredAbility(this);
-    }
-
-    @Override
-    public boolean checkEventType(GameEvent event, Game game) {
-        return event.getType() == GameEvent.EventType.DAMAGED_PLAYER;
-    }
-
-    @Override
-    public boolean checkTrigger(GameEvent event, Game game) {
-        Player opponent = game.getPlayer(event.getPlayerId());
-        if (opponent == null
-                || !event.getSourceId().equals(this.sourceId)
-                || !((DamagedEvent) event).isCombatDamage()) {
-            return false;
-        }
-        FilterCreaturePermanent filter = new FilterCreaturePermanent("creature " + opponent.getLogName() + " controls");
-        filter.add(new ControllerIdPredicate(opponent.getId()));
-        this.getTargets().clear();
-        this.addTarget(new TargetCreaturePermanent(filter));
-        return true;
-    }
-
-    @Override
-    public String getRule() {
-        return "Whenever {this} deals combat damage to a player, you may sacrifice it. "
-                + "If you do, destroy target creature that player controls.";
     }
 }
--- Mage.Sets/src/mage/cards/t/TimeReaper.java
+++ Mage.Sets/src/mage/cards/t/TimeReaper.java
@@ -49,7 +49,7 @@ public final class TimeReaper extends CardImpl {
 
         // Consume Anomaly -- Whenever Time Reaper deals combat damage to a player, put target face-up card they own in exile on the bottom of their library. If you do, you gain 3 life.
         Ability ability = new DealsCombatDamageToAPlayerTriggeredAbility(new PutOnLibraryTargetEffect(false));
-        ability.addEffect(new GainLifeEffect(3)); //I don't think the move can fail?
+        ability.addEffect(new GainLifeEffect(3)); //I don't think the move can fail? If there's no target then the trigger won't happen
         ability.addTarget(new TargetCardInExile(filter));
         ability.setTargetAdjuster(new DamagedPlayerControlsTargetAdjuster());
         ability.withFlavorWord("Consume Anomaly");
