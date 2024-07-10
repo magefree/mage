276e185e94d1cc9e01e35d0939778fadbeb46d48
theelk801 <theelk801@gmail.com>
2024-07-10 16:36:39 -0400

    [BLB] Implement Flowerfoot Swordmaster

---
+++ Mage.Sets/src/mage/cards/f/FlowerfootSwordmaster.java
@@ -0,0 +1,48 @@
+package mage.cards.f;
+
+import mage.MageInt;
+import mage.abilities.common.ValiantTriggeredAbility;
+import mage.abilities.effects.common.continuous.BoostControlledEffect;
+import mage.abilities.keyword.OffspringAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.SubType;
+import mage.filter.common.FilterCreaturePermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class FlowerfootSwordmaster extends CardImpl {
+
+    private static final FilterCreaturePermanent filter = new FilterCreaturePermanent(SubType.MOUSE, "Mice");
+
+    public FlowerfootSwordmaster(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{W}");
+
+        this.subtype.add(SubType.MOUSE);
+        this.subtype.add(SubType.SOLDIER);
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(2);
+
+        // Offspring {2}
+        this.addAbility(new OffspringAbility("{2}"));
+
+        // Valiant -- Whenever this creature becomes the target of a spell or ability you control for the first time each turn, Mice you control get +1/+0 until end of turn.
+        this.addAbility(new ValiantTriggeredAbility(new BoostControlledEffect(
+                1, 1, Duration.EndOfTurn, filter
+        )));
+    }
+
+    private FlowerfootSwordmaster(final FlowerfootSwordmaster card) {
+        super(card);
+    }
+
+    @Override
+    public FlowerfootSwordmaster copy() {
+        return new FlowerfootSwordmaster(this);
+    }
+}
