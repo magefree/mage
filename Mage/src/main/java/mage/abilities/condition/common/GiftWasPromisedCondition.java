af576b19c0337f5ec59a3ae3c32f274815b7cda3
theelk801 <theelk801@gmail.com>
2024-07-13 15:15:14 -0400

    [BLB] Implement Wear Down

---
+++ Mage/src/main/java/mage/abilities/condition/common/GiftWasPromisedCondition.java
@@ -0,0 +1,24 @@
+package mage.abilities.condition.common;
+
+import mage.abilities.Ability;
+import mage.abilities.condition.Condition;
+import mage.abilities.keyword.PromiseGiftAbility;
+import mage.game.Game;
+import mage.util.CardUtil;
+
+/**
+ * @author TheElk801
+ */
+public enum GiftWasPromisedCondition implements Condition {
+    instance;
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        return CardUtil.checkSourceCostsTagExists(game, source, PromiseGiftAbility.GIFT_ACTIVATION_VALUE_KEY);
+    }
+
+    @Override
+    public String toString() {
+        return "Gift was promised";
+    }
+}
---
+++ Mage/src/main/java/mage/constants/GiftType.java
@@ -0,0 +1,51 @@
+package mage.constants;
+
+import mage.abilities.Ability;
+import mage.game.Game;
+import mage.game.permanent.token.FishNoAbilityToken;
+import mage.game.permanent.token.FoodToken;
+import mage.players.Player;
+
+/**
+ * @author TheElk801
+ */
+public enum GiftType {
+    CARD(
+            "a card", "draw a card",
+            (p, g, s) -> p.drawCards(1, s, g) > 0
+    ),
+    FOOD(
+            "a Food", "create a Food token",
+            (p, g, s) -> new FoodToken().putOntoBattlefield(1, g, s, p.getId())
+    ),
+    TAPPED_FISH(
+            "a tapped Fish", "create a tapped 1/1 blue Fish creature token",
+            (p, g, s) -> new FishNoAbilityToken().putOntoBattlefield(1, g, s, p.getId(), true, false)
+    );
+
+    private interface GiftResolver {
+        boolean apply(Player player, Game game, Ability source);
+    }
+
+    private final String name;
+    private final String description;
+    private final GiftResolver giftResolver;
+
+    GiftType(String name, String description, GiftResolver giftResolver) {
+        this.name = name;
+        this.description = description;
+        this.giftResolver = giftResolver;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean applyGift(Player player, Game game, Ability source) {
+        return giftResolver.apply(player, game, source);
+    }
+}
