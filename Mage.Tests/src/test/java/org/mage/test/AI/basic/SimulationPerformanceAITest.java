feceddf50cef277a631da2be5f1a5160ab0d78f2
Frank Wang <broodstar_frank@hotmail.com>
2024-07-14 22:27:21 -0400

    setup burn deck matchup in test file

--- Mage.Tests/src/test/java/org/mage/test/AI/basic/SimulationPerformanceAITest.java
+++ Mage.Tests/src/test/java/org/mage/test/AI/basic/SimulationPerformanceAITest.java
@@ -61,4 +61,54 @@ public class SimulationPerformanceAITest extends CardTestPlayerBaseAI {
 
         Assert.assertTrue("One of player must won a game before turn " + maxTurn + ", but it ends on " + currentGame, currentGame.hasEnded());
     }
+
+    @Test
+    public void test_card_order() {
+        removeAllCardsFromLibrary(playerA);
+        removeAllCardsFromLibrary(playerB);
+        addCard(Zone.LIBRARY, playerA, "Monastery Swiftspear", 4);
+        addCard(Zone.LIBRARY, playerA, "Goblin Guide", 4);
+        addCard(Zone.LIBRARY, playerA, "Slickshot Show-Off", 4);
+        addCard(Zone.LIBRARY, playerA, "Lightning Bolt", 4);
+        addCard(Zone.LIBRARY, playerA, "Skullcrack", 2);
+        addCard(Zone.LIBRARY, playerA, "Boros Charm", 4);
+        addCard(Zone.LIBRARY, playerA, "Searing Blaze", 3);
+        addCard(Zone.LIBRARY, playerA, "Flare of Duplication", 4);
+        addCard(Zone.LIBRARY, playerA, "Lava Spike", 4);
+        addCard(Zone.LIBRARY, playerA, "Rift Bolt", 4);
+        addCard(Zone.LIBRARY, playerA, "Skewer the Critics", 4);
+        addCard(Zone.LIBRARY, playerA, "Underworld Breach", 4);
+        addCard(Zone.LIBRARY, playerA, "Mountain", 4);
+        addCard(Zone.LIBRARY, playerA, "Inspiring Vantage", 4);
+        addCard(Zone.LIBRARY, playerA, "Sacred Foundry", 2);
+        addCard(Zone.LIBRARY, playerA, "Bloodstained Mire", 2);
+        addCard(Zone.LIBRARY, playerA, "Wooded Foothills", 4);
+        addCard(Zone.LIBRARY, playerA, "Barbarian Ring", 1);
+        addCard(Zone.LIBRARY, playerA, "Sunbaked Canyon", 3);
+
+
+        addCard(Zone.LIBRARY, playerB, "Monastery Swiftspear", 4);
+        addCard(Zone.LIBRARY, playerB, "Goblin Guide", 4);
+        addCard(Zone.LIBRARY, playerB, "Slickshot Show-Off", 4);
+        addCard(Zone.LIBRARY, playerB, "Lightning Bolt", 4);
+        addCard(Zone.LIBRARY, playerB, "Skullcrack", 2);
+        addCard(Zone.LIBRARY, playerB, "Boros Charm", 4);
+        addCard(Zone.LIBRARY, playerB, "Searing Blaze", 3);
+        addCard(Zone.LIBRARY, playerB, "Flare of Duplication", 4);
+        addCard(Zone.LIBRARY, playerB, "Lava Spike", 4);
+        addCard(Zone.LIBRARY, playerB, "Rift Bolt", 4);
+        addCard(Zone.LIBRARY, playerB, "Skewer the Critics", 4);
+        addCard(Zone.LIBRARY, playerB, "Underworld Breach", 4);
+        addCard(Zone.LIBRARY, playerB, "Mountain", 4);
+        addCard(Zone.LIBRARY, playerB, "Inspiring Vantage", 4);
+        addCard(Zone.LIBRARY, playerB, "Sacred Foundry", 2);
+        addCard(Zone.LIBRARY, playerB, "Bloodstained Mire", 2);
+        addCard(Zone.LIBRARY, playerB, "Wooded Foothills", 4);
+        addCard(Zone.LIBRARY, playerB, "Barbarian Ring", 1);
+        addCard(Zone.LIBRARY, playerB, "Sunbaked Canyon", 3);
+
+        setStrictChooseMode(true);
+        setStopAt(20, PhaseStep.END_TURN);
+        execute();
+    }
 }
