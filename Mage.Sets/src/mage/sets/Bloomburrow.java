7ce877c04949796ffc2c84a822e5832a8f215311
theelk801 <theelk801@gmail.com>
2024-07-15 09:48:54 -0400

    [BLB] Implement Seasoned Warrenguard

--- Mage.Sets/src/mage/sets/Bloomburrow.java
+++ Mage.Sets/src/mage/sets/Bloomburrow.java
@@ -96,6 +96,7 @@ public final class Bloomburrow extends ExpansionSet {
         cards.add(new SetCardInfo("Rockface Village", 259, Rarity.UNCOMMON, mage.cards.r.RockfaceVillage.class));
         cards.add(new SetCardInfo("Run Away Together", 67, Rarity.COMMON, mage.cards.r.RunAwayTogether.class));
         cards.add(new SetCardInfo("Salvation Swan", 28, Rarity.RARE, mage.cards.s.SalvationSwan.class));
+        cards.add(new SetCardInfo("Seasoned Warrenguard", 30, Rarity.UNCOMMON, mage.cards.s.SeasonedWarrenguard.class));
         cards.add(new SetCardInfo("Seedglaive Mentor", 231, Rarity.UNCOMMON, mage.cards.s.SeedglaiveMentor.class));
         cards.add(new SetCardInfo("Seedpod Squire", 232, Rarity.COMMON, mage.cards.s.SeedpodSquire.class));
         cards.add(new SetCardInfo("Shoreline Looter", 70, Rarity.UNCOMMON, mage.cards.s.ShorelineLooter.class));
