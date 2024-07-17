f7eeb75545e223a38748b85f1536e2e26ccd3dc0
theelk801 <theelk801@gmail.com>
2024-07-17 10:19:13 -0400

    [BLB] Implement Star Charter

--- Mage.Sets/src/mage/sets/Bloomburrow.java
+++ Mage.Sets/src/mage/sets/Bloomburrow.java
@@ -140,6 +140,7 @@ public final class Bloomburrow extends ExpansionSet {
         cards.add(new SetCardInfo("Shrike Force", 31, Rarity.UNCOMMON, mage.cards.s.ShrikeForce.class));
         cards.add(new SetCardInfo("Sinister Monolith", 113, Rarity.UNCOMMON, mage.cards.s.SinisterMonolith.class));
         cards.add(new SetCardInfo("Splash Lasher", 73, Rarity.UNCOMMON, mage.cards.s.SplashLasher.class));
+        cards.add(new SetCardInfo("Star Charter", 33, Rarity.UNCOMMON, mage.cards.s.StarCharter.class));
         cards.add(new SetCardInfo("Stargaze", 114, Rarity.UNCOMMON, mage.cards.s.Stargaze.class));
         cards.add(new SetCardInfo("Starscape Cleric", 116, Rarity.UNCOMMON, mage.cards.s.StarscapeCleric.class));
         cards.add(new SetCardInfo("Starseer Mentor", 233, Rarity.UNCOMMON, mage.cards.s.StarseerMentor.class));
