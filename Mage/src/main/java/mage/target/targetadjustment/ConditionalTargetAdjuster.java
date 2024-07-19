ce8b325670e66d1c49a147f5a756ab5fa2ed1f56
Steven Knipe <knipesteven@gmail.com>
2024-07-19 00:22:38 -0700

    Take a Targets list

--- Mage/src/main/java/mage/target/targetadjustment/ConditionalTargetAdjuster.java
+++ Mage/src/main/java/mage/target/targetadjustment/ConditionalTargetAdjuster.java
@@ -4,30 +4,36 @@ import mage.abilities.Ability;
 import mage.abilities.condition.Condition;
 import mage.game.Game;
 import mage.target.Target;
+import mage.target.Targets;
 
 /**
  * @author notgreat
  */
 public class ConditionalTargetAdjuster implements TargetAdjuster {
     private final Condition condition;
-    private final Target replacementTarget;
+    private final Targets replacementTargets;
 
     /**
      * If the condition is true, replace the target
      *
      * @param condition   The condition to be checked
-     * @param replacementTarget The target to use if the condition is true
-     */
+     * @param replacementTarget The target to use if the condition is true. Can also take a Targets list
+     * */
     public ConditionalTargetAdjuster(Condition condition, Target replacementTarget) {
+        this(condition, new Targets(replacementTarget));
+    }
+    public ConditionalTargetAdjuster(Condition condition, Targets replacementTargets) {
         this.condition = condition;
-        this.replacementTarget = replacementTarget;
+        this.replacementTargets = replacementTargets;
     }
 
     @Override
     public void adjustTargets(Ability ability, Game game) {
         if (condition.apply(game, ability)){
             ability.getTargets().clear();
-            ability.addTarget(replacementTarget.copy());
+            for(Target target :replacementTargets) {
+                ability.addTarget(target.copy());
+            }
         }
     }
 }
