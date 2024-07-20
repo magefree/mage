30a4f32543bc65aaa6623aab392e23cde844c102
Steven Knipe <knipesteven@gmail.com>
2024-07-20 16:12:31 -0700

    Fix unset target tag, missing damage amount text

--- Mage.Sets/src/mage/cards/p/PhantomBlade.java
+++ Mage.Sets/src/mage/cards/p/PhantomBlade.java
@@ -34,13 +34,13 @@ public final class PhantomBlade extends CardImpl {
 
         // When Phantom Blade enters the battlefield, attach it to up to one target creature you control. Destroy up to one other target creature.
         Ability ability = new EntersBattlefieldTriggeredAbility(new AttachEffect(Outcome.BoostCreature).setText("attach it to up to one target creature you control"));
-        ability.addTarget(new TargetControlledCreaturePermanent(0, 1));
+        ability.addTarget(new TargetControlledCreaturePermanent(0, 1).setTargetTag(1));
 
         ability.addEffect(new DestroyTargetEffect().setTargetPointer(new SecondTargetPointer()));
         ability.addTarget(new TargetCreaturePermanent(0, 1, StaticFilters.FILTER_ANOTHER_CREATURE_TARGET_2, false).setTargetTag(2));
         this.addAbility(ability);
-        // Equipped creature gets +1/+1 and has menace.
 
+        // Equipped creature gets +1/+1 and has menace.
         Ability boostAbility = new SimpleStaticAbility(new BoostEquippedEffect(1, 1));
         boostAbility.addEffect(new GainAbilityAttachedEffect(new MenaceAbility(), AttachmentType.EQUIPMENT).setText("and has menace"));
         this.addAbility(boostAbility);
