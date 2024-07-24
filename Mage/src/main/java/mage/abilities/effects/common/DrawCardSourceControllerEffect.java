16bfc76fe1428e2771ee20d4812e23191af8e9aa
jimga150 <jimga150@users.noreply.github.com>
2024-07-24 01:17:54 -0400

    Update DrawCardSourceControllerEffect to use new DynamicValue pattern

--- Mage/src/main/java/mage/abilities/effects/common/DrawCardSourceControllerEffect.java
+++ Mage/src/main/java/mage/abilities/effects/common/DrawCardSourceControllerEffect.java
@@ -1,9 +1,8 @@
 package mage.abilities.effects.common;
 
 import mage.abilities.Ability;
-import mage.abilities.Mode;
 import mage.abilities.dynamicvalue.DynamicValue;
-import mage.abilities.dynamicvalue.common.PermanentsOnBattlefieldCount;
+import mage.abilities.dynamicvalue.MultipliedValue;
 import mage.abilities.dynamicvalue.common.StaticValue;
 import mage.abilities.effects.OneShotEffect;
 import mage.constants.Outcome;
@@ -30,10 +29,18 @@ public class DrawCardSourceControllerEffect extends OneShotEffect {
         this(amount, "");
     }
 
+    public DrawCardSourceControllerEffect(DynamicValue amount, boolean XPhrasing) {
+        this(amount, "", XPhrasing);
+    }
+
     public DrawCardSourceControllerEffect(DynamicValue amount, String whoDrawCard) {
+        this(amount, whoDrawCard, false);
+    }
+
+    public DrawCardSourceControllerEffect(DynamicValue amount, String whoDrawCard, boolean XPhrasing) {
         super(Outcome.DrawCard);
         this.amount = amount.copy();
-        createStaticText(whoDrawCard);
+        createStaticText(whoDrawCard, XPhrasing);
     }
 
     protected DrawCardSourceControllerEffect(final DrawCardSourceControllerEffect effect) {
@@ -57,16 +64,24 @@ public class DrawCardSourceControllerEffect extends OneShotEffect {
         return false;
     }
 
-    private void createStaticText(String whoDrawCard) {
+    private void createStaticText(String whoDrawCard, boolean useX) {
         StringBuilder sb = new StringBuilder(whoDrawCard);
         sb.append(whoDrawCard.isEmpty() ? "draw " : " draw ");
-        String value = amount.toString();
-        sb.append(CardUtil.numberToText(value, "a"));
-        sb.append(value.equals("1") ? " card" : " cards");
-        String message = amount.getMessage();
-        if (!message.isEmpty()) {
-            sb.append(value.equals("X") ? ", where X is " : " for each ");
-            sb.append(message);
+        String value = "a";
+        if (amount instanceof StaticValue) {
+            value = CardUtil.numberToText(((StaticValue)amount).getValue(), "a");
+        } else if (useX) {
+            value = "X";
+        } else if (amount instanceof MultipliedValue) {
+            value = ((MultipliedValue)amount).getMultiplierText();
+        }
+        sb.append(value).append(value.equals("a") ? " card" : " cards");
+        if (!(amount instanceof StaticValue)) {
+            if (useX){
+                sb.append(", where X is ").append(amount.getMessage(DynamicValue.Phrasing.NUMBER_OF));
+            } else {
+                sb.append(" for each ").append(amount.getMessage(DynamicValue.Phrasing.FOR_EACH));
+            }
         }
         staticText = sb.toString();
     }
