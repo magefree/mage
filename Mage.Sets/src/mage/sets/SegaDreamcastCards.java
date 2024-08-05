a66d292e064468b8a64e76680acc22ac2b6ff8e7
theelk801 <theelk801@gmail.com>
2024-08-05 11:41:56 -0400

    [PSDG] add set

---
+++ Mage.Sets/src/mage/sets/SegaDreamcastCards.java
@@ -0,0 +1,26 @@
+package mage.sets;
+
+import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
+import mage.constants.SetType;
+
+/**
+ *
+ * @author JayDi85
+ */
+public final class SegaDreamcastCards extends ExpansionSet {
+
+    private static final SegaDreamcastCards instance = new SegaDreamcastCards();
+
+    public static SegaDreamcastCards getInstance() {
+        return instance;
+    }
+
+    private SegaDreamcastCards() {
+        super("Sega Dreamcast Cards", "PSDG", ExpansionSet.buildDate(2001,6,28), SetType.SUPPLEMENTAL);
+        this.hasBoosters = false;
+        this.hasBasicLands = false;
+
+        cards.add(new SetCardInfo("Arden Angel", 1, Rarity.RARE, mage.cards.a.ArdenAngel.class));
+    }
+}
