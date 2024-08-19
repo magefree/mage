5d43904507e83ff200878aabd10234c13864773a
jimga150 <jimga150@users.noreply.github.com>
2024-08-19 13:45:41 -0400

    set cardsLeavingGraveyard value in CardsLeaveGraveyardTriggeredAbility

--- Mage/src/main/java/mage/abilities/common/CardsLeaveGraveyardTriggeredAbility.java
+++ Mage/src/main/java/mage/abilities/common/CardsLeaveGraveyardTriggeredAbility.java
@@ -11,6 +11,8 @@ import mage.game.events.GameEvent;
 import mage.game.events.ZoneChangeGroupEvent;
 
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author TheElk801
@@ -50,16 +52,18 @@ public class CardsLeaveGraveyardTriggeredAbility extends TriggeredAbilityImpl {
     @Override
     public boolean checkTrigger(GameEvent event, Game game) {
         ZoneChangeGroupEvent zEvent = (ZoneChangeGroupEvent) event;
-        return zEvent != null
-                && Zone.GRAVEYARD == zEvent.getFromZone()
-                && Zone.GRAVEYARD != zEvent.getToZone()
-                && zEvent.getCards() != null
-                && zEvent.getCards()
+        if (zEvent == null || Zone.GRAVEYARD != zEvent.getFromZone()
+                || Zone.GRAVEYARD == zEvent.getToZone() || zEvent.getCards() == null){
+            return false;
+        }
+        Set<Card> cards = zEvent.getCards()
                 .stream()
                 .filter(Objects::nonNull)
                 .filter(card -> filter.match(card, getControllerId(), this, game))
-                .map(Card::getOwnerId)
-                .anyMatch(this::isControlledBy);
+                .filter(card -> this.isControlledBy(card.getOwnerId()))
+                .collect(Collectors.toSet());
+        this.getAllEffects().setValue("cardsLeavingGraveyard", cards);
+        return !cards.isEmpty();
     }
 
     @Override
