0e0fa119f57767fbe5b187ba32ebb40c3aef35b0
xenohedron <xenohedron@users.noreply.github.com>
2024-08-21 21:49:09 -0400

    remove unused scoring system code

--- Mage/src/main/java/mage/actions/MageDrawAction.java
+++ Mage/src/main/java/mage/actions/MageDrawAction.java
@@ -2,7 +2,6 @@ package mage.actions;
 
 import mage.abilities.Ability;
 import mage.actions.impl.MageAction;
-import mage.actions.score.ArtificialScoringSystem;
 import mage.cards.Card;
 import mage.constants.Zone;
 import mage.game.Game;
@@ -13,10 +12,6 @@ import mage.game.events.GameEvent;
 import mage.players.Player;
 import mage.util.CardUtil;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
 /**
  * Action for drawing cards.
  *
@@ -24,10 +19,7 @@ import java.util.UUID;
  */
 public class MageDrawAction extends MageAction {
 
-    private static final int NEGATIVE_VALUE = -1000000;
-
     private final Player player;
-    private final List<Card> drawnCards;
     private final GameEvent originalDrawEvent; // for replace effects
 
     private int amount;
@@ -35,7 +27,6 @@ public class MageDrawAction extends MageAction {
     public MageDrawAction(Player player, int amount, GameEvent originalDrawEvent) {
         this.player = player;
         this.amount = amount;
-        this.drawnCards = new ArrayList<>();
         this.originalDrawEvent = originalDrawEvent;
     }
 
@@ -49,23 +40,20 @@ public class MageDrawAction extends MageAction {
     @Override
     public int doAction(Ability source, Game game) {
         int numDrawn = 0;
-        int score = 0;
         GameEvent event = new DrawCardsEvent(this.player.getId(), source, this.originalDrawEvent, this.amount);
         // TODO: This needs a better description of how it works. Why "amount < 2"?
         if (amount < 2 || !game.replaceEvent(event)) {
             amount = event.getAmount();
             for (int i = 0; i < amount; i++) {
-                int value = drawCard(source, this.originalDrawEvent, game);
-                if (value == NEGATIVE_VALUE) {
+                boolean value = drawCard(source, this.originalDrawEvent, game);
+                if (!value) {
                     continue;
                 }
                 numDrawn++;
-                score += value;
             }
             if (!player.isTopCardRevealed() && numDrawn > 0) {
                 game.fireInformEvent(player.getLogName() + " draws " + CardUtil.numberToText(numDrawn, "a") + " card" + (numDrawn > 1 ? "s" : ""));
             }
-            setScore(player, score);
         }
         return numDrawn;
     }
@@ -79,35 +67,21 @@ public class MageDrawAction extends MageAction {
      * @param game
      * @return
      */
-    protected int drawCard(Ability source, GameEvent originalDrawEvent, Game game) {
+    protected boolean drawCard(Ability source, GameEvent originalDrawEvent, Game game) {
         GameEvent event = new DrawCardEvent(this.player.getId(), source, originalDrawEvent);
         if (!game.replaceEvent(event)) {
             Card card = player.getLibrary().removeFromTop(game);
             if (card != null) {
-                drawnCards.add(card);
                 card.moveToZone(Zone.HAND, source, game, false); // if you want to use event.getSourceId() here then thinks x10 times
                 if (player.isTopCardRevealed()) {
                     game.fireInformEvent(player.getLogName() + " draws a revealed card  (" + card.getLogName() + ')');
                 }
 
                 game.fireEvent(new DrewCardEvent(card.getId(), player.getId(), source, originalDrawEvent));
-                return ArtificialScoringSystem.inst.getCardScore(card);
+                return true;
             }
         }
-        return NEGATIVE_VALUE;
+        return false;
     }
 
-    /**
-     * Return a card back to top.
-     *
-     * @param game Game context
-     */
-    @Override
-    public void undoAction(Game game) {
-        for (int index = drawnCards.size() - 1; index >= 0; index--) {
-            Card card = drawnCards.get(index);
-            player.getHand().remove(card);
-            player.getLibrary().putOnTop(card, game);
-        }
-    }
 }
