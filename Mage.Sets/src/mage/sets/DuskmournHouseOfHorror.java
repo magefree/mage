6e9389be02dc422b2caaa98ca15dddd6f64faa34
theelk801 <theelk801@gmail.com>
2024-09-09 12:11:32 -0400

    [DSK] Implement Most Valuable Slayer

--- Mage.Sets/src/mage/sets/DuskmournHouseOfHorror.java
+++ Mage.Sets/src/mage/sets/DuskmournHouseOfHorror.java
@@ -91,6 +91,7 @@ public final class DuskmournHouseOfHorror extends ExpansionSet {
         cards.add(new SetCardInfo("Leyline of Resonance", 143, Rarity.RARE, mage.cards.l.LeylineOfResonance.class));
         cards.add(new SetCardInfo("Leyline of the Void", 106, Rarity.RARE, mage.cards.l.LeylineOfTheVoid.class));
         cards.add(new SetCardInfo("Lionheart Glimmer", 19, Rarity.UNCOMMON, mage.cards.l.LionheartGlimmer.class));
+        cards.add(new SetCardInfo("Most Valuable Slayer", 144, Rarity.COMMON, mage.cards.m.MostValuableSlayer.class));
         cards.add(new SetCardInfo("Mountain", 275, Rarity.LAND, mage.cards.basiclands.Mountain.class, FULL_ART_BFZ_VARIOUS));
         cards.add(new SetCardInfo("Murder", 110, Rarity.COMMON, mage.cards.m.Murder.class));
         cards.add(new SetCardInfo("Murky Sewer", 263, Rarity.COMMON, mage.cards.m.MurkySewer.class));
