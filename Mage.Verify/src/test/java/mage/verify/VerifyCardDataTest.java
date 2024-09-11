3731674b614d9c29bab71389e9e1e340ead9fc70
theelk801 <theelk801@gmail.com>
2024-09-11 16:59:31 -0400

    fix verify failure

--- Mage.Verify/src/test/java/mage/verify/VerifyCardDataTest.java
+++ Mage.Verify/src/test/java/mage/verify/VerifyCardDataTest.java
@@ -142,16 +142,14 @@ public class VerifyCardDataTest {
         skipListAddName(SKIP_LIST_TYPE, "UNH", "Old Fogey"); // uses summon word as a joke card
         skipListAddName(SKIP_LIST_TYPE, "UND", "Old Fogey");
         skipListAddName(SKIP_LIST_TYPE, "UST", "capital offense"); // uses "instant" instead "Instant" as a joke card
-        skipListAddName(SKIP_LIST_TYPE, "DSK", "Balustrade Wurm"); // temporary
-        skipListAddName(SKIP_LIST_TYPE, "DSK", "Spineseeker Centipede"); // temporary
+        skipListAddName(SKIP_LIST_TYPE, "DSK", "Balemurk Leech"); // temporary
 
         // subtype
         // skipListAddName(SKIP_LIST_SUBTYPE, set, cardName);
         skipListAddName(SKIP_LIST_SUBTYPE, "UGL", "Miss Demeanor"); // uses multiple types as a joke card: Lady, of, Proper, Etiquette
         skipListAddName(SKIP_LIST_SUBTYPE, "UGL", "Elvish Impersonators"); // subtype is "Elves" pun
         skipListAddName(SKIP_LIST_SUBTYPE, "UND", "Elvish Impersonators");
-        skipListAddName(SKIP_LIST_SUBTYPE, "DSK", "Balustrade Wurm"); // temporary
-        skipListAddName(SKIP_LIST_SUBTYPE, "DSK", "Spineseeker Centipede"); // temporary
+        skipListAddName(SKIP_LIST_SUBTYPE, "DSK", "Balemurk Leech"); // temporary
 
         // number
         // skipListAddName(SKIP_LIST_NUMBER, set, cardName);
