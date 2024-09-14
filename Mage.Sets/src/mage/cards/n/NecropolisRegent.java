ccd656c22a8641863881d12a4952a7d2edfc4154
Grath <1895280+Grath@users.noreply.github.com>
2024-09-13 22:56:26 -0400

    Fix Aveline de Grandpre and Necropolis Regent to use SavedDamageValue.

--- Mage.Sets/src/mage/cards/n/NecropolisRegent.java
+++ Mage.Sets/src/mage/cards/n/NecropolisRegent.java
@@ -2,22 +2,17 @@ package mage.cards.n;
 
 import java.util.UUID;
 import mage.MageInt;
-import mage.abilities.TriggeredAbilityImpl;
-import mage.abilities.effects.Effect;
-import mage.abilities.effects.common.counter.AddCountersSourceEffect;
+import mage.abilities.common.DealsDamageToAPlayerAllTriggeredAbility;
+import mage.abilities.dynamicvalue.common.SavedDamageValue;
 import mage.abilities.effects.common.counter.AddCountersTargetEffect;
 import mage.abilities.keyword.FlyingAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.constants.SetTargetPointer;
 import mage.constants.SubType;
-import mage.constants.Zone;
 import mage.counters.CounterType;
-import mage.game.Game;
-import mage.game.events.DamagedPlayerEvent;
-import mage.game.events.GameEvent;
-import mage.game.permanent.Permanent;
-import mage.target.targetpointer.FixedTarget;
+import mage.filter.StaticFilters;
 
 /**
  *
@@ -36,7 +31,11 @@ public final class NecropolisRegent extends CardImpl {
         this.addAbility(FlyingAbility.getInstance());
 
         // Whenever a creature you control deals combat damage to a player, put that many +1/+1 counters on it.
-        this.addAbility(new NecropolisRegentTriggeredAbility());
+        this.addAbility(new DealsDamageToAPlayerAllTriggeredAbility(
+                new AddCountersTargetEffect(CounterType.P1P1.createInstance(), SavedDamageValue.MANY),
+                StaticFilters.FILTER_CONTROLLED_A_CREATURE,
+                false, SetTargetPointer.PERMANENT, true
+        ));
     }
 
     private NecropolisRegent(final NecropolisRegent card) {
@@ -46,46 +45,5 @@ public final class NecropolisRegent extends CardImpl {
     @Override
     public NecropolisRegent copy() {
         return new NecropolisRegent(this);
-    }
-}
-
-class NecropolisRegentTriggeredAbility extends TriggeredAbilityImpl {
-
-    public NecropolisRegentTriggeredAbility() {
-        super(Zone.BATTLEFIELD, new AddCountersSourceEffect(CounterType.QUEST.createInstance()), false);
-    }
-
-    private NecropolisRegentTriggeredAbility(final NecropolisRegentTriggeredAbility ability) {
-        super(ability);
-    }
-
-    @Override
-    public NecropolisRegentTriggeredAbility copy() {
-        return new NecropolisRegentTriggeredAbility(this);
-    }
-
-    @Override
-    public boolean checkEventType(GameEvent event, Game game) {
-        return event.getType() == GameEvent.EventType.DAMAGED_PLAYER;
-    }
-
-    @Override
-    public boolean checkTrigger(GameEvent event, Game game) {
-        if (((DamagedPlayerEvent) event).isCombatDamage()) {
-            Permanent creature = game.getPermanent(event.getSourceId());
-            if (creature != null && creature.isControlledBy(controllerId)) {
-                this.getEffects().clear();
-                Effect effect = new AddCountersTargetEffect(CounterType.P1P1.createInstance(event.getAmount()));
-                effect.setTargetPointer(new FixedTarget(creature.getId(), game));
-                this.addEffect(effect);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public String getRule() {
-        return "Whenever a creature you control deals combat damage to a player, put that many +1/+1 counters on it.";
     }
 }
