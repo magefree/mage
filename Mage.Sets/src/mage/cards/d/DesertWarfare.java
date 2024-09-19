70588cf3e52e4fb308cfa411914c82fa00470672
Sidorovich77 <nikolay.romanyuk3@gmail.com>
2024-09-19 22:16:55 +0300

    Fix for review.

--- Mage.Sets/src/mage/cards/d/DesertWarfare.java
+++ Mage.Sets/src/mage/cards/d/DesertWarfare.java
@@ -1,26 +1,27 @@
 package mage.cards.d;
 
 import mage.abilities.Ability;
-import mage.abilities.DelayedTriggeredAbility;
 import mage.abilities.TriggeredAbilityImpl;
 import mage.abilities.common.BeginningOfCombatTriggeredAbility;
-import mage.abilities.common.SacrificePermanentTriggeredAbility;
 import mage.abilities.common.delayed.AtTheBeginOfNextEndStepDelayedTriggeredAbility;
 import mage.abilities.condition.common.PermanentsOnTheBattlefieldCondition;
 import mage.abilities.decorator.ConditionalInterveningIfTriggeredAbility;
+import mage.abilities.dynamicvalue.DynamicValue;
+import mage.abilities.dynamicvalue.common.PermanentsOnBattlefieldCount;
 import mage.abilities.effects.Effect;
 import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.CreateDelayedTriggeredAbilityEffect;
 import mage.abilities.effects.common.continuous.GainAbilityTargetEffect;
-import mage.abilities.hint.common.DesertsYouControlHint;
+import mage.abilities.hint.Hint;
+import mage.abilities.hint.ValueHint;
 import mage.abilities.keyword.HasteAbility;
 import mage.cards.Card;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.*;
-import mage.filter.FilterCard;
 import mage.filter.FilterPermanent;
-import mage.filter.common.FilterLandPermanent;
+import mage.filter.common.FilterControlledPermanent;
+import mage.filter.common.FilterOwnedCard;
 import mage.game.Game;
 import mage.game.events.GameEvent;
 import mage.game.events.ZoneChangeEvent;
@@ -44,33 +45,24 @@ public final class DesertWarfare extends CardImpl {
         filter.add(SubType.DESERT.getPredicate());
     }
 
-    private static final FilterCard filter1 = new FilterCard();
-
-    static {
-        filter1.add(SubType.DESERT.getPredicate());
-    }
-
     public DesertWarfare(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{3}{G}");
 
-        // Whenever you sacrifice a Desert and whenever a Desert card is put into your
-        // graveyard from your hand or library, put that card onto the battlefield under your control at the beginning of your next end step.
-
+        // Whenever you sacrifice a Desert and whenever a Desert card is put into your graveyard from your hand or library, put that card onto the battlefield under your control at the beginning of your next end step.
         //Based on Seraph, Yuma, Proud Protector
-        Effect effect = new CreateDelayedTriggeredAbilityEffect((DelayedTriggeredAbility) new AtTheBeginOfNextEndStepDelayedTriggeredAbility(new DesertWarfareReturnEffect(), TargetController.YOU).setTriggerPhrase("Put that card onto the battlefield under your control at the beginning of your next end step.").setRuleVisible(false));
+        Effect effect = new CreateDelayedTriggeredAbilityEffect(new AtTheBeginOfNextEndStepDelayedTriggeredAbility(new DesertWarfareReturnEffect(), TargetController.YOU));
+        effect.setText("put that card onto the battlefield under your control at the beginning of your next end step");
 
-        this.addAbility(new SacrificePermanentTriggeredAbility(Zone.BATTLEFIELD, effect, filter, TargetController.YOU, SetTargetPointer.PERMANENT, false).setTriggerPhrase("Whenever you sacrifice a Desert and whenever a Desert card is put into your graveyard from your hand or library, put that card onto the battlefield under your control at the beginning of your next end step.").setRuleVisible(false));
-
-        this.addAbility(new PutCardIntoGraveFromHandLibraryAllTriggeredAbility(Zone.BATTLEFIELD, effect,
-                false, TargetController.YOU, SetTargetPointer.CARD).setTriggerPhrase("Whenever you sacrifice a Desert and whenever a Desert card is put into your graveyard from your hand or library, put that card onto the battlefield under your control at the beginning of your next end step."));
+        this.addAbility(new DesertWarfareTriggeredAbility(Zone.BATTLEFIELD, effect, false, TargetController.YOU, SetTargetPointer.CARD));
 
         //At the beginning of combat on your turn, if you control five or more Deserts, create that many 1/1 red, green, and white Sand Warrior creature tokens. They gain haste.
         //Based on Palani's Hatcher
         this.addAbility(new ConditionalInterveningIfTriggeredAbility(
                 new BeginningOfCombatTriggeredAbility(new DesertWarfareCreateTokensEffect(), TargetController.YOU, false),
                 new PermanentsOnTheBattlefieldCondition(filter, ComparisonType.OR_GREATER, 5, true),
-                "At the beginning of combat on your turn, if you control five or more Deserts, create that many 1/1 red, green, and white Sand Warrior creature tokens. They gain haste.").addHint(DesertsYouControlHint.instance));
-
+                "At the beginning of combat on your turn, if you control five or more Deserts, " +
+                        "create that many 1/1 red, green, and white Sand Warrior creature tokens. They gain haste.")
+                .addHint(DesertsYouControlCount.getHint()));
     }
 
     private DesertWarfare(final DesertWarfare card) {
@@ -85,9 +77,9 @@ public final class DesertWarfare extends CardImpl {
 }
 
 //Based on PutCardIntoGraveFromAnywhereAllTriggeredAbility
-class PutCardIntoGraveFromHandLibraryAllTriggeredAbility extends TriggeredAbilityImpl {
+class DesertWarfareTriggeredAbility extends TriggeredAbilityImpl {
 
-    private static FilterCard filterCard = new FilterCard();
+    private static FilterOwnedCard filterCard = new FilterOwnedCard();
 
     static {
         filterCard.add(SubType.DESERT.getPredicate());
@@ -95,48 +87,57 @@ class PutCardIntoGraveFromHandLibraryAllTriggeredAbility extends TriggeredAbilit
 
     private final SetTargetPointer setTargetPointer;
 
-    public PutCardIntoGraveFromHandLibraryAllTriggeredAbility(Zone zone, Effect effect, boolean optional, TargetController targetController, SetTargetPointer setTargetPointer) {
+    public DesertWarfareTriggeredAbility(Zone zone, Effect effect, boolean optional, TargetController targetController, SetTargetPointer setTargetPointer) {
         super(zone, effect, optional);
-        FilterCard filter = filterCard.copy();
+        FilterOwnedCard filter = filterCard.copy();
         this.setTargetPointer = setTargetPointer;
         filter.add(targetController.getOwnerPredicate());
+        setTriggerPhrase("Whenever you sacrifice a Desert and whenever a Desert card is put into your graveyard from your hand or library, ");
     }
 
-    protected PutCardIntoGraveFromHandLibraryAllTriggeredAbility(final PutCardIntoGraveFromHandLibraryAllTriggeredAbility ability) {
+    protected DesertWarfareTriggeredAbility(final DesertWarfareTriggeredAbility ability) {
         super(ability);
         filterCard = filterCard.copy();
         this.setTargetPointer = ability.setTargetPointer;
     }
 
     @Override
-    public PutCardIntoGraveFromHandLibraryAllTriggeredAbility copy() {
-        return new PutCardIntoGraveFromHandLibraryAllTriggeredAbility(this);
+    public DesertWarfareTriggeredAbility copy() {
+        return new DesertWarfareTriggeredAbility(this);
     }
 
     @Override
     public boolean checkEventType(GameEvent event, Game game) {
-        return event.getType() == GameEvent.EventType.ZONE_CHANGE;
+        return event.getType() == GameEvent.EventType.ZONE_CHANGE
+                || event.getType() == GameEvent.EventType.SACRIFICED_PERMANENT;
     }
 
     @Override
     public boolean checkTrigger(GameEvent event, Game game) {
         Card card = game.getCard(event.getTargetId());
-        if (((ZoneChangeEvent) event).getToZone() == Zone.GRAVEYARD
-                && zone.match(game.getState().getZone(getSourceId()))
-                && ((ZoneChangeEvent) event).getFromZone().match(Zone.LIBRARY)
-                && card != null
-                && filterCard.match(card, getControllerId(), this, game)) {
-            this.getEffects().setTargetPointer(new FixedTarget(card, game));
-            return true;
-        } else if (((ZoneChangeEvent) event).getToZone() == Zone.GRAVEYARD
-                && zone.match(game.getState().getZone(getSourceId()))
-                && ((ZoneChangeEvent) event).getFromZone().match(Zone.HAND)
-                && card != null
-                && filterCard.match(card, getControllerId(), this, game)) {
-            this.getEffects().setTargetPointer(new FixedTarget(card, game));
-            return true;
+        if (card == null) {
+            return false;
         }
-        return false;
+        switch (event.getType()) {
+            case SACRIFICED_PERMANENT:
+                Permanent permanent = game.getPermanentOrLKIBattlefield(event.getTargetId());
+                if (permanent == null || !permanent.hasSubtype(SubType.DESERT, game)) {
+                    return false;
+                }
+                break;
+            case ZONE_CHANGE:
+                ZoneChangeEvent zEvent = (ZoneChangeEvent) event;
+                if (zEvent.getToZone() != Zone.GRAVEYARD
+                        || (zEvent.getFromZone() != Zone.LIBRARY && zEvent.getFromZone() != Zone.HAND)
+                        || !filterCard.match(card, getControllerId(), this, game)) {
+                    return false;
+                }
+                break;
+            default:
+                return false;
+        }
+        this.getEffects().setTargetPointer(new FixedTarget(event.getTargetId(), game));
+        return true;
     }
 }
 
@@ -144,6 +145,7 @@ class DesertWarfareReturnEffect extends OneShotEffect {
 
     DesertWarfareReturnEffect() {
         super(Outcome.Benefit);
+        this.staticText = "put that card onto the battlefield under your control";
     }
 
     private DesertWarfareReturnEffect(final DesertWarfareReturnEffect effect) {
@@ -185,16 +187,39 @@ class DesertWarfareCreateTokensEffect extends OneShotEffect {
 
     @Override
     public boolean apply(Game game, Ability source) {
-        Player player = game.getPlayer(source.getControllerId());
-        Permanent permanent = source.getSourcePermanentIfItStillExists(game);
-        if (player == null || permanent == null) {
+        if (source.getControllerId() == null) {
             return false;
         }
-        int deserts = game.getBattlefield().countAll(new FilterLandPermanent(SubType.DESERT, "Deserts"), game.getControllerId(source.getSourceId()), game);
+        int deserts = DesertsYouControlCount.instance.calculate(game, source, this);
         Token token = new SandWarriorToken();
         token.putOntoBattlefield(deserts, game, source);
         game.addEffect(new GainAbilityTargetEffect(HasteAbility.getInstance(), Duration.Custom)
                 .setTargetPointer(new FixedTargets(token, game)), source);
         return true;
+    }
+}
+
+enum DesertsYouControlCount implements DynamicValue {
+    instance;
+
+    private static final Hint hint = new ValueHint("Deserts you control", instance);
+
+    @Override
+    public int calculate(Game game, Ability sourceAbility, Effect effect) {
+        return new PermanentsOnBattlefieldCount(new FilterControlledPermanent(SubType.DESERT)).calculate(game, sourceAbility, null);
+    }
+
+    @Override
+    public DynamicValue copy() {
+        return instance;
+    }
+
+    @Override
+    public String getMessage() {
+        return "";
+    }
+
+    public static Hint getHint() {
+        return hint;
     }
 }
