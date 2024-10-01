b1678036fa8878a080d00175a8f52b6034b41ae7
Grath <1895280+Grath@users.noreply.github.com>
2024-09-30 21:27:00 -0400

    Verify fix

--- Mage.Sets/src/mage/sets/DuskmournHouseOfHorrorCommander.java
+++ Mage.Sets/src/mage/sets/DuskmournHouseOfHorrorCommander.java
@@ -115,7 +115,7 @@ public final class DuskmournHouseOfHorrorCommander extends ExpansionSet {
         cards.add(new SetCardInfo("Giant Adephage", 179, Rarity.MYTHIC, mage.cards.g.GiantAdephage.class));
         cards.add(new SetCardInfo("Gleeful Arsonist", 27, Rarity.RARE, mage.cards.g.GleefulArsonist.class));
         cards.add(new SetCardInfo("Gnarlwood Dryad", 180, Rarity.UNCOMMON, mage.cards.g.GnarlwoodDryad.class));
-        cards.add(new SetCardInfo("Golgari Rot Farm", 279, Rarity.COMMON, mage.cards.g.GolgariRotFarm.class));
+        cards.add(new SetCardInfo("Golgari Rot Farm", 279, Rarity.UNCOMMON, mage.cards.g.GolgariRotFarm.class));
         cards.add(new SetCardInfo("Golgari Signet", 246, Rarity.COMMON, mage.cards.g.GolgariSignet.class));
         cards.add(new SetCardInfo("Goryo's Vengeance", 372, Rarity.MYTHIC, mage.cards.g.GoryosVengeance.class));
         cards.add(new SetCardInfo("Grapple with the Past", 82, Rarity.COMMON, mage.cards.g.GrappleWithThePast.class));
