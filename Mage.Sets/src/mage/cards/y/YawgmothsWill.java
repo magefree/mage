be745cb096c734b79c849980382ceffd2cbdeb99
ssk97 <knipesteven@gmail.com>
2024-10-12 11:07:52 -0700

    implement [BLB] Dragonhawk, Festival of Embers, Jackdaw Savior, Infamous Cruelclaw (#12906)
    * [BLB] Dragonhawk, Fate's Tempest
    
    * [BLB] Festival of Embers
    
    * [BLB] Jackdaw Savior + test
    
    * [BLB] The Infamous Cruelclaw
    
    * Dragonhawk changes
    
    * Create generic GraveyardFromAnywhereExileReplacementEffect
    
    * Hide Cruelclaw Menace reminder text
    
    * fix style errors

--- Mage.Sets/src/mage/cards/y/YawgmothsWill.java
+++ Mage.Sets/src/mage/cards/y/YawgmothsWill.java
@@ -1,26 +1,16 @@
 
 package mage.cards.y;
 
-import java.util.UUID;
 import mage.abilities.Ability;
 import mage.abilities.effects.ContinuousEffectImpl;
-import mage.abilities.effects.ReplacementEffectImpl;
-import mage.cards.Card;
+import mage.abilities.effects.common.replacement.GraveyardFromAnywhereExileReplacementEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.constants.CardType;
-import mage.constants.Duration;
-import mage.constants.Layer;
-import mage.constants.Outcome;
-import mage.constants.SubLayer;
-import mage.constants.Zone;
+import mage.constants.*;
 import mage.game.Game;
-import mage.game.events.GameEvent;
-import mage.game.events.GameEvent.EventType;
-import mage.game.events.ZoneChangeEvent;
-import mage.game.permanent.Permanent;
-import mage.game.permanent.PermanentToken;
 import mage.players.Player;
+
+import java.util.UUID;
 
 /**
  *
@@ -35,7 +25,7 @@ public final class YawgmothsWill extends CardImpl {
         this.getSpellAbility().addEffect(new CanPlayCardsFromGraveyardEffect());
 
         // If a card would be put into your graveyard from anywhere this turn, exile that card instead.
-        this.getSpellAbility().addEffect(new YawgmothsWillReplacementEffect());
+        this.getSpellAbility().addEffect(new GraveyardFromAnywhereExileReplacementEffect(Duration.EndOfTurn).concatBy("<br>"));
     }
 
     private YawgmothsWill(final YawgmothsWill card) {
@@ -74,49 +64,6 @@ class CanPlayCardsFromGraveyardEffect extends ContinuousEffectImpl {
         if (controller != null) {
             controller.setPlayCardsFromGraveyard(true);
             return true;
-        }
-        return false;
-    }
-
-}
-
-class YawgmothsWillReplacementEffect extends ReplacementEffectImpl {
-
-    YawgmothsWillReplacementEffect() {
-        super(Duration.EndOfTurn, Outcome.Detriment);
-        this.staticText = "If a card would be put into your graveyard from anywhere this turn, exile that card instead";
-    }
-
-    private YawgmothsWillReplacementEffect(final YawgmothsWillReplacementEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public YawgmothsWillReplacementEffect copy() {
-        return new YawgmothsWillReplacementEffect(this);
-    }
-
-    @Override
-    public boolean replaceEvent(GameEvent event, Ability source, Game game) {
-        ((ZoneChangeEvent) event).setToZone(Zone.EXILED);
-        return false;
-    }
-
-    @Override
-    public boolean checksEventType(GameEvent event, Game game) {
-        return event.getType() == GameEvent.EventType.ZONE_CHANGE;
-    }
-
-    @Override
-    public boolean applies(GameEvent event, Ability source, Game game) {
-        if (((ZoneChangeEvent) event).getToZone() == Zone.GRAVEYARD) {
-            Card card = game.getCard(event.getTargetId());
-            if (card != null && card.isOwnedBy(source.getControllerId())) {
-                Permanent permanent = ((ZoneChangeEvent) event).getTarget();
-                if (!(permanent instanceof PermanentToken)) {
-                    return true;
-                }
-            }
         }
         return false;
     }
