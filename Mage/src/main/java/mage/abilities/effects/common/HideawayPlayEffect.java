76a355db84cc34e65d7372fea3363d59157904b8
Grath <1895280+Grath@users.noreply.github.com>
2024-10-18 00:51:01 -0400

    Fix HideawayPlayEffect to allow you to attempt to play everything, in cases of multiple Hideaway triggers.

--- Mage/src/main/java/mage/abilities/effects/common/HideawayPlayEffect.java
+++ Mage/src/main/java/mage/abilities/effects/common/HideawayPlayEffect.java
@@ -10,6 +10,7 @@ import mage.game.Game;
 import mage.players.Player;
 import mage.util.CardUtil;
 
+import java.util.Set;
 import java.util.UUID;
 
 /**
@@ -38,33 +39,36 @@ public class HideawayPlayEffect extends OneShotEffect {
         if (controller == null || zone == null || zone.isEmpty()) {
             return true;
         }
-        Card card = zone.getRandom(game);
-        if (card == null) {
-            return false;
-        }
-        if (!controller.chooseUse(Outcome.PlayForFree, "Play " + card.getIdName() + " for free?", source, game)) {
-            return false;
-        }
-        card.setFaceDown(false, game);
-        int zcc = card.getZoneChangeCounter(game);
+        Set<Card> cards = zone.getCards(game);
 
-        /* 702.74. Hideaway, rulings:
-         * If the removed card is a land, you may play it as a result of the last ability only if it's your turn
-         * and you haven't already played a land that turn. This counts as your land play for the turn.
-         * TODO: this doesn't work correctly with the back half of MDFCs
-         */
-        if (card.isLand(game)) {
-            UUID playerId = controller.getId();
-            if (!game.isActivePlayer(playerId) || !game.getPlayer(playerId).canPlayLand()) {
-                return false;
+        boolean cardPlayed = false;
+        for (Card card : cards) {
+            /* 702.74. Hideaway, rulings:
+             * If the removed card is a land, you may play it as a result of the last ability only if it's your turn
+             * and you haven't already played a land that turn. This counts as your land play for the turn.
+             * TODO: this doesn't work correctly with the back half of MDFCs
+             */
+            if (card.isLand(game)) {
+                UUID playerId = controller.getId();
+                if (!game.isActivePlayer(playerId) || !game.getPlayer(playerId).canPlayLand()) {
+                    continue;
+                }
+            }
+
+            if (!controller.chooseUse(Outcome.PlayForFree, "Play " + card.getIdName() + " for free?", source, game)) {
+                continue;
+            }
+            card.setFaceDown(false, game);
+            int zcc = card.getZoneChangeCounter(game);
+
+            if (controller.playCard(card, game, true, new ApprovingObject(source, game))) {
+                cardPlayed = true;
+            } else {
+                if (card.getZoneChangeCounter(game) == zcc) {
+                    card.setFaceDown(true, game);
+                }
             }
         }
-
-        if (!controller.playCard(card, game, true, new ApprovingObject(source, game))) {
-            if (card.getZoneChangeCounter(game) == zcc) {
-                card.setFaceDown(true, game);
-            }
-        }
-        return true;
+        return cardPlayed;
     }
 }
