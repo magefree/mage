e22c3e1f1776069b51b55f3ecd11bc0caa769ea3
ciaccona007 <ciaccona007@users.noreply.github.com>
2024-11-03 09:40:38 -0500

    [FDN] Implement Fiendish Panda

--- Mage.Sets/src/mage/sets/Foundations.java
+++ Mage.Sets/src/mage/sets/Foundations.java
@@ -191,6 +191,7 @@ public final class Foundations extends ExpansionSet {
         cards.add(new SetCardInfo("Felidar Retreat", 574, Rarity.RARE, mage.cards.f.FelidarRetreat.class));
         cards.add(new SetCardInfo("Felidar Savior", 12, Rarity.COMMON, mage.cards.f.FelidarSavior.class));
         cards.add(new SetCardInfo("Felling Blow", 105, Rarity.UNCOMMON, mage.cards.f.FellingBlow.class));
+        cards.add(new SetCardInfo("Fiendish Panda", 120, Rarity.UNCOMMON, mage.cards.f.FiendishPanda.class));
         cards.add(new SetCardInfo("Fierce Empath", 636, Rarity.COMMON, mage.cards.f.FierceEmpath.class));
         cards.add(new SetCardInfo("Fiery Annihilation", 86, Rarity.UNCOMMON, mage.cards.f.FieryAnnihilation.class));
         cards.add(new SetCardInfo("Finale of Revelation", 589, Rarity.MYTHIC, mage.cards.f.FinaleOfRevelation.class));
