46d32011937bed097a266f6e28af496dfac9232b
theelk801 <theelk801@gmail.com>
2024-11-04 20:51:09 -0500

    remove verify skips

--- Mage.Verify/src/test/java/mage/verify/VerifyCardDataTest.java
+++ Mage.Verify/src/test/java/mage/verify/VerifyCardDataTest.java
@@ -143,16 +143,12 @@ public class VerifyCardDataTest {
         skipListAddName(SKIP_LIST_TYPE, "UNH", "Old Fogey"); // uses summon word as a joke card
         skipListAddName(SKIP_LIST_TYPE, "UND", "Old Fogey");
         skipListAddName(SKIP_LIST_TYPE, "UST", "capital offense"); // uses "instant" instead "Instant" as a joke card
-        skipListAddName(SKIP_LIST_TYPE, "FDN", "Billowing Shriekmass"); // temporary
-        skipListAddName(SKIP_LIST_TYPE, "FDN", "Icewind Elemental"); // temporary
 
         // subtype
         // skipListAddName(SKIP_LIST_SUBTYPE, set, cardName);
         skipListAddName(SKIP_LIST_SUBTYPE, "UGL", "Miss Demeanor"); // uses multiple types as a joke card: Lady, of, Proper, Etiquette
         skipListAddName(SKIP_LIST_SUBTYPE, "UGL", "Elvish Impersonators"); // subtype is "Elves" pun
         skipListAddName(SKIP_LIST_SUBTYPE, "UND", "Elvish Impersonators");
-        skipListAddName(SKIP_LIST_SUBTYPE, "FDN", "Billowing Shriekmass"); // temporary
-        skipListAddName(SKIP_LIST_SUBTYPE, "FDN", "Icewind Elemental"); // temporary
 
         // number
         // skipListAddName(SKIP_LIST_NUMBER, set, cardName);
@@ -160,10 +156,6 @@ public class VerifyCardDataTest {
         // rarity
         // skipListAddName(SKIP_LIST_RARITY, set, cardName);
         skipListAddName(SKIP_LIST_RARITY, "CMR", "The Prismatic Piper"); // Collation is not yet set up for CMR https://www.lethe.xyz/mtg/collation/cmr.html
-        skipListAddName(SKIP_LIST_RARITY, "DSC", "Suspicious Bookcase"); // temporary
-        skipListAddName(SKIP_LIST_RARITY, "FDN", "Maalfeld Twins"); // temporary
-        skipListAddName(SKIP_LIST_RARITY, "FDN", "Snakeskin Veil"); // temporary
-        skipListAddName(SKIP_LIST_RARITY, "FDN", "Lathril, Blade of the Elves"); // temporary
 
         // missing abilities
         // skipListAddName(SKIP_LIST_MISSING_ABILITIES, set, cardName);
