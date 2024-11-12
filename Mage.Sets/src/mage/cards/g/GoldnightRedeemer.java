f5468fe7ce9d5e4fe744b3a9e368c8685c49e0c4
xenohedron <xenohedron@users.noreply.github.com>
2024-11-11 23:47:34 -0500

    refactor: update to new static filter "other creature you control"

--- Mage.Sets/src/mage/cards/g/GoldnightRedeemer.java
+++ Mage.Sets/src/mage/cards/g/GoldnightRedeemer.java
@@ -1,7 +1,5 @@
-
 package mage.cards.g;
 
-import java.util.UUID;
 import mage.MageInt;
 import mage.abilities.common.EntersBattlefieldTriggeredAbility;
 import mage.abilities.dynamicvalue.common.PermanentsOnBattlefieldCount;
@@ -11,24 +9,17 @@ import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.SubType;
-import mage.filter.common.FilterControlledCreaturePermanent;
-import mage.filter.predicate.mageobject.AnotherPredicate;
+import mage.filter.StaticFilters;
+
+import java.util.UUID;
 
 /**
- *
  * @author noxx
-
  */
 public final class GoldnightRedeemer extends CardImpl {
 
-    private static final FilterControlledCreaturePermanent filter = new FilterControlledCreaturePermanent("other creature you control");
-
-    static {
-        filter.add(AnotherPredicate.instance);
-    }
-
     public GoldnightRedeemer(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.CREATURE},"{4}{W}{W}");
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{4}{W}{W}");
         this.subtype.add(SubType.ANGEL);
 
         this.power = new MageInt(4);
@@ -37,7 +28,7 @@ public final class GoldnightRedeemer extends CardImpl {
         this.addAbility(FlyingAbility.getInstance());
 
         // When Goldnight Redeemer enters the battlefield, you gain 2 life for each other creature you control.
-        this.addAbility(new EntersBattlefieldTriggeredAbility(new GainLifeEffect(new PermanentsOnBattlefieldCount(filter, 2)), false));
+        this.addAbility(new EntersBattlefieldTriggeredAbility(new GainLifeEffect(new PermanentsOnBattlefieldCount(StaticFilters.FILTER_OTHER_CONTROLLED_CREATURE, 2)), false));
     }
 
     private GoldnightRedeemer(final GoldnightRedeemer card) {
--- Mage.Sets/src/mage/cards/v/VictorysEnvoy.java
+++ Mage.Sets/src/mage/cards/v/VictorysEnvoy.java
@@ -1,16 +1,14 @@
 package mage.cards.v;
 
 import mage.MageInt;
-import mage.abilities.triggers.BeginningOfUpkeepTriggeredAbility;
 import mage.abilities.effects.common.counter.AddCountersAllEffect;
+import mage.abilities.triggers.BeginningOfUpkeepTriggeredAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.SubType;
 import mage.counters.CounterType;
-import mage.filter.FilterPermanent;
-import mage.filter.common.FilterControlledCreaturePermanent;
-import mage.filter.predicate.mageobject.AnotherPredicate;
+import mage.filter.StaticFilters;
 
 import java.util.UUID;
 
@@ -18,13 +16,6 @@ import java.util.UUID;
  * @author TheElk801
  */
 public final class VictorysEnvoy extends CardImpl {
-
-    private static final FilterPermanent filter
-            = new FilterControlledCreaturePermanent("other creature you control");
-
-    static {
-        filter.add(AnotherPredicate.instance);
-    }
 
     public VictorysEnvoy(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{3}{W}{W}");
@@ -36,7 +27,7 @@ public final class VictorysEnvoy extends CardImpl {
 
         // At the beginning of your upkeep, put a +1/1 counter on each other creature you control.
         this.addAbility(new BeginningOfUpkeepTriggeredAbility(
-                new AddCountersAllEffect(CounterType.P1P1.createInstance(), filter)
+                new AddCountersAllEffect(CounterType.P1P1.createInstance(), StaticFilters.FILTER_OTHER_CONTROLLED_CREATURE)
         ));
     }
 
