f3f2c3cf661f51dd0bfcb3b050e326b86ace27da
xenohedron <xenohedron@users.noreply.github.com>
2024-11-18 12:39:01 -0500

    fix Diresight

--- Mage.Sets/src/mage/cards/d/Diresight.java
+++ Mage.Sets/src/mage/cards/d/Diresight.java
@@ -19,7 +19,7 @@ public final class Diresight extends CardImpl {
 
         // Surveil 2, then draw two cards. You lose 2 life.
         this.getSpellAbility().addEffect(new SurveilEffect(2, false));
-        this.getSpellAbility().addEffect(new DrawCardSourceControllerEffect(1).concatBy(", then"));
+        this.getSpellAbility().addEffect(new DrawCardSourceControllerEffect(2).concatBy(", then"));
         this.getSpellAbility().addEffect(new LoseLifeSourceControllerEffect(2));
     }
 
