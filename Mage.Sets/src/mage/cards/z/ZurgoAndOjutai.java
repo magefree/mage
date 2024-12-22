magefree/mage

commit d06d5949347a0867eb1840d0f1c3c170e4577a61

rework batch events (#13066)
* add new framework for batch triggers

apply for tapped, untapped, sacrificed, milled

simplify Ob Nixilis, Captive Kingpin

* add a verify check

* fix mistakes

* add simple tests

* another test

* zone change - enters battlefield

* zone change: not battlefield

* zone change - leaves battlefield

* fix Kaya Spirit's Justice

* rename OneOrMoreCombatDamagePlayerTriggeredAbility

* refactor OneOrMoreDamagePlayerTriggeredAbility

* new YoureDealtDamageTriggeredAbility

* new OpponentDealtNoncombatDamageTriggeredAbility

* rework Risona, Asari Commander

* simplify War Elemental

* Add damage batch by source

rework some delayed triggered abilities

* fix Mindblade Render

* rework Initiative and a few others

* [temp] initiative test

* refactor: common style for DealsDamageSourceTriggeredAbility

* refactor cards to use common DealsDamageSourceTriggeredAbility

* update damage players batch triggers

* fix mistake in initiative

* new DealtDamageAnyTriggeredAbility

* new DealtCombatDamageToSourceTriggeredAbility

* update dealt damage to permanent batch triggered abilities

* refactor Hot Soup and param in DealtDamageAttachedTriggeredAbility

* a few more permanent batch triggered abilities

* fix mistake

* update some more damage batch triggers

* add test for Phyrexian Negator

* update Felix Five-Boots and enable test

update Wayta, Trainer Prodigy to align

* update damage batch by source triggers

* undo mistaken change

* fix verify

* cleanup unused methods

* Revert "[temp] initiative test"

This reverts commit 11ed19295fb4f54f5e0870acd4d3d515b54761f1.

* Revert "add a verify check"

This reverts commit e7de47a6562f13c127fdc4c29a7735a08f8da9ea.

* fixes from checking text discrepancies

* fix Shriekwood Devourer

* merge fix

---------

Co-authored-by: Susucre <34709007+Susucre@users.noreply.github.com>

Mage.Sets/src/mage/cards/z/ZurgoAndOjutai.java
@@ -2,6 +2,7 @@ package mage.cards.z;
 
 import mage.MageInt;
 import mage.abilities.Ability;
+import mage.abilities.BatchTriggeredAbility;
 import mage.abilities.TriggeredAbilityImpl;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.condition.common.SourceEnteredThisTurnCondition;
@@ -73,7 +74,7 @@ public final class ZurgoAndOjutai extends CardImpl {
     }
 }
 
-class ZurgoAndOjutaiTriggeredAbility extends TriggeredAbilityImpl {
+class ZurgoAndOjutaiTriggeredAbility extends TriggeredAbilityImpl implements BatchTriggeredAbility<DamagedEvent> {
 
     ZurgoAndOjutaiTriggeredAbility() {
         super(Zone.BATTLEFIELD, new LookLibraryAndPickControllerEffect(3, 1, PutCards.HAND, PutCards.BOTTOM_ANY));
@@ -96,23 +97,21 @@ class ZurgoAndOjutaiTriggeredAbility extends TriggeredAbilityImpl {
     }
 
     @Override
+    public boolean checkEvent(DamagedEvent event, Game game) {
+        Permanent permanent = game.getPermanent(event.getSourceId());
+        Permanent defender = game.getPermanent(event.getTargetId());
+        return permanent != null
+                && permanent.hasSubtype(SubType.DRAGON, game)
+                && permanent.isControlledBy(getControllerId())
+                && ((defender != null && defender.isBattle(game)) || game.getPlayer(event.getTargetId()) != null);
+    }
+
+    @Override
     public boolean checkTrigger(GameEvent event, Game game) {
-        List<Permanent> permanents = ((DamagedBatchAllEvent) event)
-                .getEvents()
+        List<Permanent> permanents = getFilteredEvents((DamagedBatchAllEvent) event, game)
                 .stream()
-                .filter(DamagedEvent::isCombatDamage)
-                .map(e -> {
-                    Permanent permanent = game.getPermanent(e.getSourceId());
-                    Permanent defender = game.getPermanent(e.getTargetId());
-                    if (permanent != null
-                            && permanent.hasSubtype(SubType.DRAGON, game)
-                            && permanent.isControlledBy(this.getControllerId())
-                            && ((defender != null && defender.isBattle(game))
-                            || game.getPlayer(e.getTargetId()) != null)) {
-                        return permanent;
-                    }
-                    return null;
-                })
+                .map(GameEvent::getSourceId)
+                .map(game::getPermanent)
                 .filter(Objects::nonNull)
                 .collect(Collectors.toList());
         if (permanents.isEmpty()) {

commit 89b2509fa562ade8e25537bfc5fff359ea55257d

fix #13177 (Zurgo and Ojutai)
trigger on combat damage only, regression from #13066

Mage.Sets/src/mage/cards/z/ZurgoAndOjutai.java
@@ -98,6 +98,9 @@ class ZurgoAndOjutaiTriggeredAbility extends TriggeredAbilityImpl implements Bat
 
     @Override
     public boolean checkEvent(DamagedEvent event, Game game) {
+        if (!event.isCombatDamage()) {
+            return false;
+        }
         Permanent permanent = game.getPermanent(event.getSourceId());
         Permanent defender = game.getPermanent(event.getTargetId());
         return permanent != null