5d6e6f6b28c9977925802c0b89325f47f11fa6a1
jam1garner <jam@jam1.re>
2024-12-21 23:24:22 -0500

    [PIP] Add Curie, Emergent Intelligence

---
+++ Mage.Sets/src/mage/cards/c/CurieEmergentIntelligence.java
@@ -0,0 +1,133 @@
+package mage.cards.c;
+
+import java.util.UUID;
+
+import mage.abilities.Ability;
+import mage.abilities.common.DealsCombatDamageToAPlayerTriggeredAbility;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.Cost;
+import mage.abilities.costs.common.ExileTargetCost;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.decorator.ConditionalOneShotEffect;
+import mage.abilities.dynamicvalue.common.SourcePermanentPowerValue;
+import mage.abilities.effects.common.DrawCardSourceControllerEffect;
+import mage.abilities.effects.OneShotEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.SubType;
+import mage.constants.SuperType;
+import mage.constants.Outcome;
+import mage.filter.common.FilterControlledPermanent;
+import mage.filter.predicate.mageobject.AnotherPredicate;
+import mage.filter.predicate.permanent.TokenPredicate;
+import mage.filter.predicate.Predicates;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.game.permanent.PermanentCard;
+import mage.MageInt;
+import mage.MageObject;
+import mage.target.common.TargetControlledPermanent;
+import mage.util.functions.CopyApplier;
+
+/**
+ *
+ * @author jam1garner
+ */
+public final class CurieEmergentIntelligence extends CardImpl {
+
+    private static final FilterControlledPermanent filter =
+            new FilterControlledPermanent("another nontoken artifact creature you control");
+
+    static {
+        filter.add(AnotherPredicate.instance);
+        filter.add(Predicates.and(
+            CardType.ARTIFACT.getPredicate(),
+            CardType.CREATURE.getPredicate()
+        ));
+        filter.add(TokenPredicate.FALSE);
+    }
+
+    public CurieEmergentIntelligence(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT, CardType.CREATURE}, "{1}{U}");
+        
+        this.supertype.add(SuperType.LEGENDARY);
+        this.subtype.add(SubType.ROBOT);
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(3);
+
+        // Whenever Curie, Emergent Intelligence deals combat damage to a player, draw cards equal to its base power.
+        this.addAbility(new DealsCombatDamageToAPlayerTriggeredAbility(
+            new DrawCardSourceControllerEffect(1).setText("draw cards equal to its base power"), false
+        ));
+
+        // {1}{U}, Exile another nontoken artifact creature you control: Curie becomes a copy of the exiled creature, except it has 
+        // "Whenever this creature deals combat damage to a player, draw cards equal to its base power."
+        Ability ability = new SimpleActivatedAbility(new CurieCopyEffect(), new ManaCostsImpl<>("{1}{U}"));
+        ability.addCost(new ExileTargetCost(new TargetControlledPermanent(filter)));
+        this.addAbility(ability);
+    }
+
+    private CurieEmergentIntelligence(final CurieEmergentIntelligence card) {
+        super(card);
+    }
+
+    @Override
+    public CurieEmergentIntelligence copy() {
+        return new CurieEmergentIntelligence(this);
+    }
+}
+
+class CurieCopyEffect extends OneShotEffect {
+
+    private static final CopyApplier applier = new CopyApplier() {
+        @Override
+        public boolean apply(Game game, MageObject blueprint, Ability source, UUID targetObjectId) {
+            // Needed to account for Spinal Parasite
+            int basePower = Math.max(0, blueprint.getPower().getValue());
+
+            blueprint.getAbilities().add(new DealsCombatDamageToAPlayerTriggeredAbility(
+                new DrawCardSourceControllerEffect(basePower).setText("draw cards equal to its base power"), false
+            ));
+            return true;
+        }
+    };
+
+    CurieCopyEffect() {
+        super(Outcome.Benefit);
+        this.setText("{this} becomes a copy of the exiled creature, except it has \"Whenever this creature deals combat damage to a player, draw cards equal to its base power.\"");
+    }
+
+    private CurieCopyEffect(final CurieCopyEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public CurieCopyEffect copy() {
+        return new CurieCopyEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        for (Cost c : source.getCosts()) {
+            if (c.isPaid() && c instanceof ExileTargetCost) {
+                for (Permanent exiled : ((ExileTargetCost) c).getPermanents()) {
+                    if (exiled != null) {
+                        game.copyPermanent(
+                            Duration.WhileOnBattlefield,
+                            exiled,
+                            source.getSourceId(), source, applier
+                        );
+
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+}
