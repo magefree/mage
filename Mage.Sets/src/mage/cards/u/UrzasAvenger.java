8772190c7be53d8d414cd9008def74e5f24be32e
xenohedron <xenohedron@users.noreply.github.com>
2025-01-09 00:35:59 -0500

    fix #13215 (Urza's Avenger, regression from #12619)

--- Mage.Sets/src/mage/cards/u/UrzasAvenger.java
+++ Mage.Sets/src/mage/cards/u/UrzasAvenger.java
@@ -1,4 +1,3 @@
-
 package mage.cards.u;
 
 import mage.MageInt;
@@ -6,7 +5,7 @@ import mage.abilities.Ability;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.mana.ManaCostsImpl;
 import mage.abilities.effects.common.GainsChoiceOfAbilitiesEffect;
-import mage.abilities.effects.common.continuous.BoostTargetEffect;
+import mage.abilities.effects.common.continuous.BoostSourceEffect;
 import mage.abilities.keyword.BandingAbility;
 import mage.abilities.keyword.FirstStrikeAbility;
 import mage.abilities.keyword.FlyingAbility;
@@ -14,12 +13,12 @@ import mage.abilities.keyword.TrampleAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.constants.Duration;
 import mage.constants.SubType;
 
 import java.util.UUID;
 
 /**
- *
  * @author Styxo & L_J
  */
 public final class UrzasAvenger extends CardImpl {
@@ -32,7 +31,7 @@ public final class UrzasAvenger extends CardImpl {
         this.toughness = new MageInt(4);
 
         // {0}: Urza's Avenger gets -1/-1 and gains your choice of banding, flying, first strike, or trample until end of turn.
-        Ability ability = new SimpleActivatedAbility(new BoostTargetEffect(-1, -1)
+        Ability ability = new SimpleActivatedAbility(new BoostSourceEffect(-1, -1, Duration.EndOfTurn)
                 .setText("{this} gets -1/-1"), new ManaCostsImpl<>("{0}"));
         ability.addEffect(new GainsChoiceOfAbilitiesEffect(GainsChoiceOfAbilitiesEffect.TargetType.Source, "", true,
                 BandingAbility.getInstance(), FlyingAbility.getInstance(), FirstStrikeAbility.getInstance(), TrampleAbility.getInstance())
