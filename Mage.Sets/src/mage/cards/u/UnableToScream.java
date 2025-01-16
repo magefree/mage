2f85026ca919f8f27233abcf1814ac6d1c5df2a5
Marco Romano <markort147@gmail.com>
2025-01-16 13:33:32 +0000

    Fixed Unable to Scream abilities

--- Mage.Sets/src/mage/cards/u/UnableToScream.java
+++ Mage.Sets/src/mage/cards/u/UnableToScream.java
@@ -3,16 +3,16 @@ package mage.cards.u;
 import mage.abilities.Ability;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.effects.ContinuousEffect;
-import mage.abilities.effects.ContinuousEffectImpl;
 import mage.abilities.effects.ContinuousRuleModifyingEffectImpl;
 import mage.abilities.effects.common.AttachEffect;
+import mage.abilities.effects.common.continuous.AddCardSubtypeAttachedEffect;
+import mage.abilities.effects.common.continuous.AddCardTypeAttachedEffect;
+import mage.abilities.effects.common.continuous.LoseAllAbilitiesAttachedEffect;
+import mage.abilities.effects.common.continuous.SetBasePowerToughnessEnchantedEffect;
 import mage.abilities.keyword.EnchantAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.constants.CardType;
-import mage.constants.Duration;
-import mage.constants.Outcome;
-import mage.constants.SubType;
+import mage.constants.*;
 import mage.game.Game;
 import mage.game.events.GameEvent;
 import mage.game.permanent.Permanent;
@@ -34,12 +34,16 @@ public final class UnableToScream extends CardImpl {
         // Enchant creature
         TargetPermanent auraTarget = new TargetCreaturePermanent();
         this.getSpellAbility().addTarget(auraTarget);
-        this.getSpellAbility().addEffect(new AttachEffect(Outcome.Benefit));
+        this.getSpellAbility().addEffect(new AttachEffect(Outcome.Detriment));
         this.addAbility(new EnchantAbility(auraTarget));
 
         // Enchanted creature loses all abilities and is a Toy artifact creature with base power and toughness 0/2
         // in addition to its other types.
-        this.addAbility(new SimpleStaticAbility(new UnableToScreamBecomesEffect()));
+        Ability ability = new SimpleStaticAbility(new LoseAllAbilitiesAttachedEffect(AttachmentType.AURA));
+        ability.addEffect(new AddCardSubtypeAttachedEffect(SubType.TOY, AttachmentType.AURA).setText(" and is a Toy"));
+        ability.addEffect(new AddCardTypeAttachedEffect(CardType.ARTIFACT, AttachmentType.AURA).setText(" artifact creature"));
+        ability.addEffect(new SetBasePowerToughnessEnchantedEffect(0, 2).setText(" with base power and toughness 0/2 in addition to its other types."));
+        this.addAbility(ability);
 
         // As long as enchanted creature is face down, it can't be turned face up.
         this.addAbility(new SimpleStaticAbility(new UnableToScreamPreventingEffect()));
@@ -55,48 +59,10 @@ public final class UnableToScream extends CardImpl {
     }
 }
 
-class UnableToScreamBecomesEffect extends ContinuousEffectImpl {
-
-    UnableToScreamBecomesEffect() {
-        super(Duration.WhileOnBattlefield, Outcome.Benefit);
-        setText("Enchanted creature loses all abilities and is " +
-                "a Toy artifact creature with base power and " +
-                "toughness 0/2 in addition to its other types.");
-    }
-
-    private UnableToScreamBecomesEffect(final UnableToScreamBecomesEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Permanent aura = game.getPermanent(source.getSourceId());
-        if (aura == null)
-            return false;
-
-        Permanent creature = game.getPermanent(aura.getAttachedTo());
-        if (creature == null)
-            return false;
-
-        creature.getPower().setModifiedBaseValue(0);
-        creature.getToughness().setModifiedBaseValue(2);
-        creature.removeAllAbilities(source.getSourceId(), game);
-        creature.getCardType().add(CardType.ARTIFACT);
-        creature.getSubtype().add(SubType.TOY);
-
-        return true;
-    }
-
-    @Override
-    public ContinuousEffect copy() {
-        return new UnableToScreamBecomesEffect(this);
-    }
-}
-
 class UnableToScreamPreventingEffect extends ContinuousRuleModifyingEffectImpl {
 
     UnableToScreamPreventingEffect() {
-        super(Duration.WhileOnBattlefield, Outcome.Benefit);
+        super(Duration.WhileOnBattlefield, Outcome.Detriment);
         setText("As long as enchanted creature is face down, it can't be turned face up.");
     }
 
