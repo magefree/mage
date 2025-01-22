812f0555e361471b7f44337f9fc38e6fa504f9d0
theelk801 <theelk801@gmail.com>
2025-01-22 11:55:39 -0500

    [DRC] Implement Temmet, Naktamun's Will

--- Mage.Sets/src/mage/sets/AetherdriftCommander.java
+++ Mage.Sets/src/mage/sets/AetherdriftCommander.java
@@ -1,6 +1,7 @@
 package mage.sets;
 
 import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
 import mage.constants.SetType;
 
 /**
@@ -17,5 +18,7 @@ public final class AetherdriftCommander extends ExpansionSet {
     private AetherdriftCommander() {
         super("Aetherdrift Commander", "DRC", ExpansionSet.buildDate(2025, 1, 21), SetType.SUPPLEMENTAL);
         this.hasBasicLands = false;
+
+        cards.add(new SetCardInfo("Temmet, Naktamun's Will", 4, Rarity.MYTHIC, mage.cards.t.TemmetNaktamunsWill.class));
     }
 }
