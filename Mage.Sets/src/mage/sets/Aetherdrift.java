86df1d198b48a23483f2dbdb50c1fb1784ffc74c
jackd149 <67557084+jackd149@users.noreply.github.com>
2025-01-24 12:49:59 -0500

    [DFT] Implement Aatchik, Emerald Radian (#13254)
    * [DSK] Implement Kaito, Bane of Monsters
    
    * add author to emblem
    
    * Fixes for review
    
    * Implement Aatchik
    
    * Remove extra Kaito line
    
    * back out changes
    
    * Changes for PR
    
    ---------
    
    Co-authored-by: Evan Kranzler <theelk801@gmail.com>

--- Mage.Sets/src/mage/sets/Aetherdrift.java
+++ Mage.Sets/src/mage/sets/Aetherdrift.java
@@ -25,6 +25,7 @@ public final class Aetherdrift extends ExpansionSet {
         this.hasBasicLands = true;
         this.hasBoosters = false; // temporary
 
+        cards.add(new SetCardInfo("Aatchik, Emerald Radian", 187, Rarity.RARE, mage.cards.a.AatchikEmeraldRadian.class));
         cards.add(new SetCardInfo("Aether Syphon", 38, Rarity.UNCOMMON, mage.cards.a.AetherSyphon.class));
         cards.add(new SetCardInfo("Air Response Unit", 1, Rarity.UNCOMMON, mage.cards.a.AirResponseUnit.class));
         cards.add(new SetCardInfo("Amonkhet Raceway", 248, Rarity.UNCOMMON, mage.cards.a.AmonkhetRaceway.class));
