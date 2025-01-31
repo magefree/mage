148a0bf8de5411cd4db27aaac86c86665e78a67b
theelk801 <theelk801@gmail.com>
2025-01-31 10:23:07 -0500

    [DFT] Implement Bounce Off

--- Mage.Sets/src/mage/sets/Aetherdrift.java
+++ Mage.Sets/src/mage/sets/Aetherdrift.java
@@ -44,6 +44,7 @@ public final class Aetherdrift extends ExpansionSet {
         cards.add(new SetCardInfo("Blossoming Sands", 252, Rarity.COMMON, mage.cards.b.BlossomingSands.class));
         cards.add(new SetCardInfo("Boom Scholar", 189, Rarity.UNCOMMON, mage.cards.b.BoomScholar.class));
         cards.add(new SetCardInfo("Boosted Sloop", 190, Rarity.UNCOMMON, mage.cards.b.BoostedSloop.class));
+        cards.add(new SetCardInfo("Bounce Off", 39, Rarity.COMMON, mage.cards.b.BounceOff.class));
         cards.add(new SetCardInfo("Brightfield Glider", 4, Rarity.COMMON, mage.cards.b.BrightfieldGlider.class));
         cards.add(new SetCardInfo("Brightfield Mustang", 5, Rarity.COMMON, mage.cards.b.BrightfieldMustang.class));
         cards.add(new SetCardInfo("Brightglass Gearhulk", 191, Rarity.MYTHIC, mage.cards.b.BrightglassGearhulk.class));
