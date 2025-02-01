a6041a371eff4ab9e56d816ac42c253145fc8576
theelk801 <theelk801@gmail.com>
2025-01-31 19:25:07 -0500

    [DFT] Implement Spin Out

--- Mage.Sets/src/mage/sets/Aetherdrift.java
+++ Mage.Sets/src/mage/sets/Aetherdrift.java
@@ -165,6 +165,7 @@ public final class Aetherdrift extends ExpansionSet {
         cards.add(new SetCardInfo("Skycrash", 146, Rarity.UNCOMMON, mage.cards.s.Skycrash.class));
         cards.add(new SetCardInfo("Skystreak Engineer", 61, Rarity.COMMON, mage.cards.s.SkystreakEngineer.class));
         cards.add(new SetCardInfo("Spell Pierce", 64, Rarity.UNCOMMON, mage.cards.s.SpellPierce.class));
+        cards.add(new SetCardInfo("Spin Out", 106, Rarity.COMMON, mage.cards.s.SpinOut.class));
         cards.add(new SetCardInfo("Spotcycle Scouter", 30, Rarity.COMMON, mage.cards.s.SpotcycleScouter.class));
         cards.add(new SetCardInfo("Starting Column", 244, Rarity.COMMON, mage.cards.s.StartingColumn.class));
         cards.add(new SetCardInfo("Stock Up", 67, Rarity.UNCOMMON, mage.cards.s.StockUp.class));
