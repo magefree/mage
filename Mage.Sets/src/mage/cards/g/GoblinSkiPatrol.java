9543b68364874e1010a7b975f371175d4fa63801
tiera3 <87589219+tiera3@users.noreply.github.com>
2025-02-02 23:02:53 +1000

    Add files via upload

--- Mage.Sets/src/mage/cards/g/GoblinSkiPatrol.java
+++ Mage.Sets/src/mage/cards/g/GoblinSkiPatrol.java
@@ -49,7 +49,7 @@ public final class GoblinSkiPatrol extends CardImpl {
         // {1}{R}: Goblin Ski Patrol gets +2/+0 and gains flying. Its controller sacrifices it at the beginning of the next end step. Activate only once and only if you control a snow Mountain.
         Effect effect = new BoostSourceEffect(2, 0, Duration.EndOfTurn);
         effect.setText("{this} gets +2/+0");
-        Ability ability = new ActivateOncePerGameActivatedAbility(Zone.BATTLEFIELD, effect, new ManaCostsImpl<>("{1}{R}"), 1, condition);
+        Ability ability = new ActivateOncePerGameActivatedAbility(Zone.BATTLEFIELD, effect, new ManaCostsImpl<>("{1}{R}"), condition);
         effect = new GainAbilitySourceEffect(FlyingAbility.getInstance(), Duration.EndOfTurn);
         effect.setText("and gains flying.");
         ability.addEffect(effect);
