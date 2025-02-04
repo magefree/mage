848c7524e5a02c3e0db9d97a122a4ef434c731e0
jackd149 <67557084+jackd149@users.noreply.github.com>
2025-02-04 10:07:38 -0500

    Update Boommobile.java

--- Mage.Sets/src/mage/cards/b/Boommobile.java
+++ Mage.Sets/src/mage/cards/b/Boommobile.java
@@ -89,7 +89,7 @@ class BoommobileManaCondition extends ManaCondition {
 
     @Override
     public boolean apply(Game game, Ability source) {
-        if (source != null && source.isActivated()) {
+        if (source != null && !source.isActivated()) {
             return source.isActivatedAbility();
         }
         return false;
