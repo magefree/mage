a41b8b9e8b497d01275ecebefd8870b7423bc269
theelk801 <theelk801@gmail.com>
2025-02-03 19:50:10 -0500

    [DFT] Implement Chitin Gravestalker

---
+++ Mage.Sets/src/mage/cards/c/ChitinGravestalker.java
@@ -0,0 +1,64 @@
+package mage.cards.c;
+
+import mage.MageInt;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.dynamicvalue.DynamicValue;
+import mage.abilities.dynamicvalue.common.CardsInControllerGraveyardCount;
+import mage.abilities.effects.common.cost.SpellCostReductionForEachSourceEffect;
+import mage.abilities.hint.Hint;
+import mage.abilities.hint.ValueHint;
+import mage.abilities.keyword.CyclingAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.constants.Zone;
+import mage.filter.FilterCard;
+import mage.filter.predicate.Predicates;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class ChitinGravestalker extends CardImpl {
+
+    private static final FilterCard filter = new FilterCard("artifact and/or creature card");
+
+    static {
+        filter.add(Predicates.or(
+                CardType.ARTIFACT.getPredicate(),
+                CardType.CREATURE.getPredicate()
+        ));
+    }
+
+    private static final DynamicValue xValue = new CardsInControllerGraveyardCount(filter);
+    private static final Hint hint = new ValueHint("Instant and sorcery card in your graveyard", xValue);
+
+    public ChitinGravestalker(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{5}{B}");
+
+        this.subtype.add(SubType.INSECT);
+        this.subtype.add(SubType.WARRIOR);
+        this.power = new MageInt(5);
+        this.toughness = new MageInt(4);
+
+        // This spell costs {1} less to cast for each artifact and/or creature card in your graveyard.
+        this.addAbility(new SimpleStaticAbility(
+                Zone.ALL, new SpellCostReductionForEachSourceEffect(1, xValue)
+        ).setRuleAtTheTop(true).addHint(hint));
+
+        // Cycling {2}
+        this.addAbility(new CyclingAbility(new ManaCostsImpl<>("{2}")));
+    }
+
+    private ChitinGravestalker(final ChitinGravestalker card) {
+        super(card);
+    }
+
+    @Override
+    public ChitinGravestalker copy() {
+        return new ChitinGravestalker(this);
+    }
+}
--- Mage.Sets/src/mage/sets/Aetherdrift.java
+++ Mage.Sets/src/mage/sets/Aetherdrift.java
@@ -57,6 +57,7 @@ public final class Aetherdrift extends ExpansionSet {
         cards.add(new SetCardInfo("Camera Launcher", 232, Rarity.COMMON, mage.cards.c.CameraLauncher.class));
         cards.add(new SetCardInfo("Caradora, Heart of Alacria", 195, Rarity.RARE, mage.cards.c.CaradoraHeartOfAlacria.class));
         cards.add(new SetCardInfo("Chandra, Spark Hunter", 116, Rarity.MYTHIC, mage.cards.c.ChandraSparkHunter.class));
+        cards.add(new SetCardInfo("Chitin Gravestalker", 79, Rarity.COMMON, mage.cards.c.ChitinGravestalker.class));
         cards.add(new SetCardInfo("Clamorous Ironclad", 117, Rarity.COMMON, mage.cards.c.ClamorousIronclad.class));
         cards.add(new SetCardInfo("Cloudspire Captain", 9, Rarity.UNCOMMON, mage.cards.c.CloudspireCaptain.class));
         cards.add(new SetCardInfo("Cloudspire Coordinator", 196, Rarity.UNCOMMON, mage.cards.c.CloudspireCoordinator.class));
