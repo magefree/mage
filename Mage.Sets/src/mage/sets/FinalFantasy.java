141ec011069c1c02cf90c01ede640e59c9a88e55
theelk801 <theelk801@gmail.com>
2025-02-18 14:37:16 -0500

    [FIN] Implement Sazh's Chocobo

--- Mage.Sets/src/mage/sets/FinalFantasy.java
+++ Mage.Sets/src/mage/sets/FinalFantasy.java
@@ -1,6 +1,7 @@
 package mage.sets;
 
 import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
 import mage.constants.SetType;
 
 /**
@@ -17,5 +18,7 @@ public final class FinalFantasy extends ExpansionSet {
     private FinalFantasy() {
         super("Final Fantasty", "FIN", ExpansionSet.buildDate(2025, 6, 13), SetType.EXPANSION);
         this.blockName = "Final Fantasty"; // for sorting in GUI
+
+        cards.add(new SetCardInfo("Sazh's Chocobo", 200, Rarity.UNCOMMON, mage.cards.s.SazhsChocobo.class));
     }
 }
