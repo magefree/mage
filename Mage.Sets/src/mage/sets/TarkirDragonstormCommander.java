9c29ff9ca4559e813dc2ce1d4ac118108c13a991
theelk801 <theelk801@gmail.com>
2025-02-22 12:42:24 -0500

    [TDC] Implement Teval, the Balanced Scale

--- Mage.Sets/src/mage/sets/TarkirDragonstormCommander.java
+++ Mage.Sets/src/mage/sets/TarkirDragonstormCommander.java
@@ -1,6 +1,7 @@
 package mage.sets;
 
 import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
 import mage.constants.SetType;
 
 /**
@@ -17,5 +18,7 @@ public final class TarkirDragonstormCommander extends ExpansionSet {
     private TarkirDragonstormCommander() {
         super("Tarkir: Dragonstorm Commander", "TDC", ExpansionSet.buildDate(2025, 4, 11), SetType.SUPPLEMENTAL);
         this.hasBasicLands = false;
+
+        cards.add(new SetCardInfo("Teval, the Balanced Scale", 8, Rarity.MYTHIC, mage.cards.t.TevalTheBalancedScale.class));
     }
 }
