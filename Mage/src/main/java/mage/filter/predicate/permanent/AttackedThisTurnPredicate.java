b27e3b31179697ef38c33c56b6808409cde96225
balazskristof <20043803+balazskristof@users.noreply.github.com>
2025-02-27 17:24:04 +0100

    [ACR] Implement Overpowering Attack

---
+++ Mage/src/main/java/mage/filter/predicate/permanent/AttackedThisTurnPredicate.java
@@ -0,0 +1,26 @@
+package mage.filter.predicate.permanent;
+
+import mage.filter.predicate.Predicate;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.watchers.common.AttackedThisTurnWatcher;
+
+/**
+ * Requires AttackedThisTurnWatcher to be added to the card
+ *
+ * @author balazskristof
+ */
+public enum AttackedThisTurnPredicate implements Predicate<Permanent> {
+    instance;
+
+    @Override
+    public boolean apply(Permanent input, Game game) {
+        AttackedThisTurnWatcher watcher = game.getState().getWatcher(AttackedThisTurnWatcher.class);
+        return watcher != null && watcher.checkIfAttacked(input, game);
+    }
+
+    @Override
+    public String toString() {
+        return "attacked this turn";
+    }
+}
