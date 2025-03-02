7459bfbb5a71c406b18b5635efaf15a45937ceaa
jmlundeen <jmlundee@yahoo.com>
2025-03-01 23:58:39 -0600

    fix Wickerfolk alternate cast from graveyard

--- Mage.Sets/src/mage/cards/w/WickerfolkIndomitable.java
+++ Mage.Sets/src/mage/cards/w/WickerfolkIndomitable.java
@@ -1,12 +1,18 @@
 package mage.cards.w;
 
 import java.util.UUID;
+
+import mage.MageIdentifier;
 import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.MayCastFromGraveyardSourceAbility;
 import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.costs.Cost;
+import mage.abilities.costs.Costs;
+import mage.abilities.costs.CostsImpl;
 import mage.abilities.costs.common.PayLifeCost;
 import mage.abilities.costs.common.SacrificeTargetCost;
+import mage.abilities.costs.mana.ManaCostsImpl;
 import mage.abilities.effects.AsThoughEffectImpl;
 import mage.abilities.effects.OneShotEffect;
 import mage.cards.Card;
@@ -15,6 +21,7 @@ import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.filter.StaticFilters;
 import mage.game.Game;
+import mage.players.Player;
 
 /**
  *
@@ -30,10 +37,8 @@ public final class WickerfolkIndomitable extends CardImpl {
         this.toughness = new MageInt(3);
 
         // You may cast this card from your graveyard by paying 2 life and sacrificing an artifact or creature in addition to paying its other costs.
-        Ability ability = new SimpleStaticAbility(Zone.GRAVEYARD, new WickerfolkIndomitableGraveyardEffect());
-        ability.addCost(new PayLifeCost(2).setText(""));
-        ability.addCost(new SacrificeTargetCost(StaticFilters.FILTER_CONTROLLED_PERMANENT_ARTIFACT_OR_CREATURE)
-                .setText(""));
+        Ability ability = new SimpleStaticAbility(Zone.GRAVEYARD, new WickerfolkIndomitableGraveyardEffect())
+                .setIdentifier(MageIdentifier.WickerfolkIndomitableAlternateCast);
 
         this.addAbility(ability);
     }
@@ -71,10 +76,19 @@ class WickerfolkIndomitableGraveyardEffect extends AsThoughEffectImpl {
 
     @Override
     public boolean applies(UUID objectId, Ability source, UUID affectedControllerId, Game game) {
-        if (!objectId.equals(source.getSourceId()) || !source.isControlledBy(affectedControllerId)) {
+        if (!objectId.equals(source.getSourceId()) || !source.isControlledBy(affectedControllerId)
+        || game.getState().getZone(source.getSourceId()) != Zone.GRAVEYARD) {
             return false;
         }
-        Card card = game.getCard(source.getSourceId());
-        return card != null && game.getState().getZone(source.getSourceId()) == Zone.GRAVEYARD;
+        Player controller = game.getPlayer(affectedControllerId);
+        if (controller != null) {
+            Costs<Cost> costs = new CostsImpl<>();
+            costs.add(new PayLifeCost(2));
+            costs.add(new SacrificeTargetCost(StaticFilters.FILTER_CONTROLLED_PERMANENT_ARTIFACT_OR_CREATURE));
+            controller.setCastSourceIdWithAlternateMana(objectId, new ManaCostsImpl<>("{3}{B}"), costs,
+                    MageIdentifier.WickerfolkIndomitableAlternateCast);
+            return true;
+        }
+        return false;
     }
 }
