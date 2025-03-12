ea6a1335d9baac1fd845f78700db9aeff0a7907e
balazskristof <20043803+balazskristof@users.noreply.github.com>
2025-03-12 02:13:16 +0100

    [FIC] Implement Cloud, Ex-SOLDIER

--- Mage.Sets/src/mage/sets/FinalFantasyCommander.java
+++ Mage.Sets/src/mage/sets/FinalFantasyCommander.java
@@ -19,6 +19,10 @@ public final class FinalFantasyCommander extends ExpansionSet {
         super("Final Fantasy Commander", "FIC", ExpansionSet.buildDate(2025, 6, 13), SetType.SUPPLEMENTAL);
         this.hasBasicLands = false;
 
+        cards.add(new SetCardInfo("Cloud, Ex-SOLDIER", 2, Rarity.MYTHIC, mage.cards.c.CloudExSOLDIER.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Cloud, Ex-SOLDIER", 168, Rarity.MYTHIC, mage.cards.c.CloudExSOLDIER.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Cloud, Ex-SOLDIER", 202, Rarity.MYTHIC, mage.cards.c.CloudExSOLDIER.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Cloud, Ex-SOLDIER", 221, Rarity.MYTHIC, mage.cards.c.CloudExSOLDIER.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Terra, Herald of Hope", 4, Rarity.MYTHIC, mage.cards.t.TerraHeraldOfHope.class));
     }
 }
