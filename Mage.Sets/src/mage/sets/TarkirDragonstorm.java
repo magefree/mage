162bccd8f13ac7c1f70e24c1f3cc0bab59a74862
balazskristof <20043803+balazskristof@users.noreply.github.com>
2025-03-18 21:55:18 +0100

    [TDM] Implement Voice of Victory

--- Mage.Sets/src/mage/sets/TarkirDragonstorm.java
+++ Mage.Sets/src/mage/sets/TarkirDragonstorm.java
@@ -33,5 +33,6 @@ public final class TarkirDragonstorm extends ExpansionSet {
         cards.add(new SetCardInfo("Skirmish Rhino", 224, Rarity.UNCOMMON, mage.cards.s.SkirmishRhino.class));
         cards.add(new SetCardInfo("Smile at Death", 24, Rarity.MYTHIC, mage.cards.s.SmileAtDeath.class));
         cards.add(new SetCardInfo("Stormscale Scion", 123, Rarity.MYTHIC, mage.cards.s.StormscaleScion.class));
+        cards.add(new SetCardInfo("Voice of Victory", 33, Rarity.RARE, mage.cards.v.VoiceOfVictory.class));
     }
 }
