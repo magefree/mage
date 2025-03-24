053986434b0fb8fcdbc924b6a6b1370449bc40ec
theelk801 <theelk801@gmail.com>
2025-03-24 14:50:14 -0400

    [TDM] Implement Salt Road Packbeast

---
+++ Mage.Sets/src/mage/cards/s/SaltRoadPackbeast.java
@@ -0,0 +1,47 @@
+package mage.cards.s;
+
+import mage.MageInt;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.dynamicvalue.common.CreaturesYouControlCount;
+import mage.abilities.effects.common.DrawCardSourceControllerEffect;
+import mage.abilities.effects.common.cost.SpellCostReductionForEachSourceEffect;
+import mage.abilities.hint.common.CreaturesYouControlHint;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.constants.Zone;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class SaltRoadPackbeast extends CardImpl {
+
+    public SaltRoadPackbeast(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{5}{W}");
+
+        this.subtype.add(SubType.BEAST);
+        this.power = new MageInt(4);
+        this.toughness = new MageInt(3);
+
+        // This spell costs {1} less to cast for each creature you control.
+        this.addAbility(new SimpleStaticAbility(
+                Zone.ALL, new SpellCostReductionForEachSourceEffect(1, CreaturesYouControlCount.instance)
+        ).addHint(CreaturesYouControlHint.instance));
+
+        // When this creature enters, draw a card.
+        this.addAbility(new EntersBattlefieldTriggeredAbility(new DrawCardSourceControllerEffect(1)));
+    }
+
+    private SaltRoadPackbeast(final SaltRoadPackbeast card) {
+        super(card);
+    }
+
+    @Override
+    public SaltRoadPackbeast copy() {
+        return new SaltRoadPackbeast(this);
+    }
+}
