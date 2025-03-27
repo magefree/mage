29269384bfb67eae93f7e802dab5839c4f074a73
theelk801 <theelk801@gmail.com>
2025-03-27 19:04:41 -0400

    fix error

--- Mage.Sets/src/mage/cards/d/DragonbroodsRelic.java
+++ Mage.Sets/src/mage/cards/d/DragonbroodsRelic.java
@@ -1,7 +1,5 @@
 package mage.cards.d;
 
-import java.util.UUID;
-
 import mage.abilities.Ability;
 import mage.abilities.common.ActivateAsSorceryActivatedAbility;
 import mage.abilities.costs.common.SacrificeSourceCost;
@@ -13,10 +11,12 @@ import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.filter.StaticFilters;
+import mage.game.permanent.token.ReliquaryDragonToken;
 import mage.target.common.TargetControlledPermanent;
 
+import java.util.UUID;
+
 /**
- *
  * @author TheElk801
  */
 public final class DragonbroodsRelic extends CardImpl {
@@ -25,10 +25,16 @@ public final class DragonbroodsRelic extends CardImpl {
         super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT}, "{1}{G}");
 
         // {T}, Tap an untapped creature you control: Add one mana of any color.
-        Ability ability=new AnyColorManaAbility();ability.addCost(new TapTargetCost(new TargetControlledPermanent(StaticFilters.FILTER_CONTROLLED_UNTAPPED_CREATURE)));this.addAbility(ability);
+        Ability ability = new AnyColorManaAbility();
+        ability.addCost(new TapTargetCost(new TargetControlledPermanent(StaticFilters.FILTER_CONTROLLED_UNTAPPED_CREATURE)));
+        this.addAbility(ability);
 
         // {3}{W}{U}{B}{R}{G}, Sacrifice this artifact: Create a 4/4 Dragon creature token named Reliquary Dragon that's all colors. It has flying, lifelink, and "When this token enters, it deals 3 damage to any target." Activate only as a sorcery.
-        ability=new ActivateAsSorceryActivatedAbility(new CreateTokenEffect(),new ManaCostsImpl<>("{3}{W}{U}{B}{R}{G}"));ability.addCost(new SacrificeSourceCost());this.addAbility(ability);
+        ability = new ActivateAsSorceryActivatedAbility(
+                new CreateTokenEffect(new ReliquaryDragonToken()), new ManaCostsImpl<>("{3}{W}{U}{B}{R}{G}")
+        );
+        ability.addCost(new SacrificeSourceCost());
+        this.addAbility(ability);
     }
 
     private DragonbroodsRelic(final DragonbroodsRelic card) {
