ff3174877a9ba1d5ac127dd378c07a9ba04c0e24
Evan Kranzler <theelk801@gmail.com>
2025-03-28 10:12:01 -0400

    Remove gold from ObjectColor (#13470)
    * remove gold
    
    * rework Sword of Dungeons and Dragons, add comments about color
    
    * rework Call to Arms, most common color condition

--- Mage/src/main/java/mage/game/permanent/token/DragonTokenGold.java
+++ Mage/src/main/java/mage/game/permanent/token/DragonTokenGold.java
@@ -6,6 +6,8 @@ import mage.constants.CardType;
 import mage.constants.SubType;
 
 /**
+ * This token is supposed to be "gold" but the game rules don't support gold as a color in black border
+ *
  * @author Saga
  */
 public final class DragonTokenGold extends TokenImpl {
@@ -13,7 +15,11 @@ public final class DragonTokenGold extends TokenImpl {
     public DragonTokenGold() {
         super("Dragon Token", "4/4 gold Dragon creature token with flying");
         cardType.add(CardType.CREATURE);
-        color.setGold(true);
+        color.setWhite(true);
+        color.setBlue(true);
+        color.setBlack(true);
+        color.setRed(true);
+        color.setGreen(true);
         subtype.add(SubType.DRAGON);
         power = new MageInt(4);
         toughness = new MageInt(4);
