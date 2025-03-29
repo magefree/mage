6780f1d004ee21de3ad75e8148f1d195bf403406
theelk801 <theelk801@gmail.com>
2025-03-28 22:15:33 -0400

    [TDM] Implement Adorned Crocodile

--- Mage.Sets/src/mage/sets/TarkirDragonstorm.java
+++ Mage.Sets/src/mage/sets/TarkirDragonstorm.java
@@ -26,6 +26,7 @@ public final class TarkirDragonstorm extends ExpansionSet {
 
         cards.add(new SetCardInfo("Abzan Devotee", 68, Rarity.COMMON, mage.cards.a.AbzanDevotee.class));
         cards.add(new SetCardInfo("Abzan Monument", 238, Rarity.UNCOMMON, mage.cards.a.AbzanMonument.class));
+        cards.add(new SetCardInfo("Adorned Crocodile", 69, Rarity.COMMON, mage.cards.a.AdornedCrocodile.class));
         cards.add(new SetCardInfo("Aegis Sculptor", 35, Rarity.UNCOMMON, mage.cards.a.AegisSculptor.class));
         cards.add(new SetCardInfo("Agent of Kotis", 36, Rarity.COMMON, mage.cards.a.AgentOfKotis.class));
         cards.add(new SetCardInfo("Alesha's Legacy", 72, Rarity.COMMON, mage.cards.a.AleshasLegacy.class));
