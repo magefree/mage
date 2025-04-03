fff7d908239c77b0aa5631ab3a3a72ea082adef3
jmlundeen <jmlundee@yahoo.com>
2025-04-03 12:56:02 -0500

    Abstract AdventureCard to SingleFaceSplitCard

---
+++ Mage/src/main/java/mage/cards/SingleFaceSplitCardSpell.java
@@ -0,0 +1,9 @@
+package mage.cards;
+
+public interface SingleFaceSplitCardSpell extends SubCard<SingleFaceSplitCard> {
+
+    @Override
+    SingleFaceSplitCardSpell copy();
+
+    void finalizeSpell();
+}
