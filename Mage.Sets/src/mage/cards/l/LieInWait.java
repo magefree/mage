bf952b5e2ad44450288308ef166182a19eaea1db
androosss <andrija.novakovic.1998@gmail.com>
2025-04-06 10:46:04 +0200

    improve ability

--- Mage.Sets/src/mage/cards/l/LieInWait.java
+++ Mage.Sets/src/mage/cards/l/LieInWait.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.UUID;
 
 import mage.abilities.Ability;
-import mage.abilities.dynamicvalue.common.StaticValue;
 import mage.abilities.effects.OneShotEffect;
 import mage.cards.Card;
 import mage.cards.CardImpl;
@@ -31,7 +30,7 @@ public final class LieInWait extends CardImpl {
         
 
         // Return target creature card from your graveyard to your hand. Lie in Wait deals damage equal to that card's power to target creature.
-        this.getSpellAbility().addEffect(new LieInWaitTargetEffect().setTargetPointer(new EachTargetPointer()));
+        this.getSpellAbility().addEffect(new LieInWaitTargetEffect());
         this.getSpellAbility().addTarget(new TargetCardInYourGraveyard(StaticFilters.FILTER_CARD_CREATURE));
         this.getSpellAbility().addTarget(new TargetPermanent(StaticFilters.FILTER_PERMANENT_CREATURE));
     }
@@ -53,6 +52,7 @@ class LieInWaitTargetEffect extends OneShotEffect {
         super(Outcome.Benefit);
         staticText = "Return target creature card from your graveyard to your hand. "
                 + "{this} deals damage equal to that card's power to target creature";
+        setTargetPointer(new EachTargetPointer());
     }
 
     private LieInWaitTargetEffect(final LieInWaitTargetEffect effect) {
@@ -66,20 +66,20 @@ class LieInWaitTargetEffect extends OneShotEffect {
 
     @Override
     public boolean apply(Game game, Ability source) {
-        Card card = game.getCard(getTargetPointer().getFirst(game, source));
+        List<UUID> targets = getTargetPointer().getTargets(game, source);
+        Card card = game.getCard(targets.get(0));
         if (card == null) {
             return false;
         }
-        List<UUID> targets = getTargetPointer().getTargets(game, source);
         Player controller = game.getPlayer(source.getControllerId());
         if (controller == null) {
             return false;
         }
-        boolean result = game.getState().getZone(targets.get(0)) == Zone.GRAVEYARD && card.moveToZone(Zone.HAND, source, game,false);
+        boolean result = card.moveToZone(Zone.HAND, source, game,false);
         if (result && targets.size() >= 2) {
             int power = card.getPower().getValue();
             Permanent permanent = game.getPermanent(targets.get(1));
-            permanent.damage(StaticValue.get(power).calculate(game, source, this), source, game);       
+            permanent.damage(power, source, game);       
         }
         return result;
     }
