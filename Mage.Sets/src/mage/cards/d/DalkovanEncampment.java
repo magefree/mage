0bd092cc8de3a78ec909e235f0809f316d70f3af
Mike Cunningham <cunningham.mjc@gmail.com>
2025-04-06 22:56:55 -0400

    Update DalkovanEncampment.java

--- Mage.Sets/src/mage/cards/d/DalkovanEncampment.java
+++ Mage.Sets/src/mage/cards/d/DalkovanEncampment.java
@@ -1,7 +1,12 @@
 package mage.cards.d;
 
-import mage.abilities.common.EntersBattlefieldTappedUnlessConditionAbility;
+import mage.abilities.common.EntersBattlefieldTappedUnlessAbility;
 import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.common.delayed.AtTheBeginOfNextEndStepDelayedTriggeredAbility;
+import mage.abilities.condition.common.YouControlPermanentCondition;
+import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.CreateDelayedTriggeredAbilityEffect;
 import mage.abilities.effects.common.CreateTokenEffect;
 import mage.abilities.effects.common.SacrificeEffect;
 import mage.abilities.mana.WhiteManaAbility;
@@ -9,16 +14,14 @@ import mage.abilities.mana.BlackManaAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.Duration;
-import mage.constants.Outcome;
 import mage.constants.SubType;
 import mage.constants.Zone;
 import mage.filter.common.FilterControlledLandPermanent;
-import mage.filter.predicate.mageobject.SubtypePredicate;
+import mage.filter.common.FilterControlledCreaturePermanent;
+import mage.filter.predicate.Predicates;
 import mage.game.permanent.token.WarriorToken;
-import mage.target.common.TargetControlledCreaturePermanent;
-import mage.costs.mana.ManaCostsImpl;
-import mage.costs.common.TapSourceCost;
+
+import mage.abilities.costs.common.TapSourceCost;
 
 import java.util.UUID;
 
@@ -31,15 +34,16 @@ public final class DalkovanEncampment extends CardImpl {
     private static final FilterControlledLandPermanent filter = new FilterControlledLandPermanent("a Swamp or a Mountain");
 
     static {
-        filter.add(new SubtypePredicate(SubType.SWAMP));
-        filter.add(new SubtypePredicate(SubType.MOUNTAIN));
+        filter.add(Predicates.or(SubType.SWAMP.getPredicate(), SubType.MOUNTAIN.getPredicate()));
     }
+
+    private static final YouControlPermanentCondition condition = new YouControlPermanentCondition(filter);
 
     public DalkovanEncampment(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.LAND}, "");
 
         // Dalkovan Encampment enters the battlefield tapped unless you control a Swamp or a Mountain.
-        this.addAbility(new EntersBattlefieldTappedUnlessConditionAbility(filter));
+        this.addAbility(new EntersBattlefieldTappedUnlessAbility(condition).addHint(condition.getHint()));
 
         // {T}: Add {W}.
         this.addAbility(new WhiteManaAbility());
@@ -49,12 +53,16 @@ public final class DalkovanEncampment extends CardImpl {
 
         // {2}{W}, {T}: Whenever you attack this turn, create two 1/1 red Warrior creature tokens that are tapped and attacking. Sacrifice them at the beginning of the next end step.
         SimpleActivatedAbility ability = new SimpleActivatedAbility(
-            Zone.BATTLEFIELD,
-            new CreateTokenEffect(new WarriorToken(), 2, true, true),
-            new ManaCostsImpl<>("{2}{W}")
+                Zone.BATTLEFIELD,
+                new CreateTokenEffect(new WarriorToken(), 2, true, true),
+                new ManaCostsImpl<>("{2}{W}")
         );
         ability.addCost(new TapSourceCost());
-        ability.addEffect(new SacrificeEffect(new FilterControlledCreaturePermanent("those tokens"), "", Duration.EndOfTurn));
+        ability.addEffect(new CreateDelayedTriggeredAbilityEffect(
+                new AtTheBeginOfNextEndStepDelayedTriggeredAbility(
+                        new SacrificeEffect(new FilterControlledCreaturePermanent("those tokens"), 2, "those tokens")
+                )
+        ));
         this.addAbility(ability);
     }
 
