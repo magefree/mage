a9553264794b178ba65e1a95dae7e2b12ded0be1
Mike Cunningham <cunningham.mjc@gmail.com>
2025-04-07 00:23:20 -0400

    Adding custom effect based on TargetPlayerShufflesTargetCardsEffect

--- Mage.Sets/src/mage/cards/r/RiteOfRenewal.java
+++ Mage.Sets/src/mage/cards/r/RiteOfRenewal.java
@@ -1,18 +1,23 @@
 package mage.cards.r;
 
-import java.util.UUID;
+import mage.abilities.Ability;
+import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.ExileSourceEffect;
 import mage.abilities.effects.common.ReturnFromGraveyardToHandTargetEffect;
-import mage.abilities.effects.common.ShuffleIntoLibraryTargetEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
+import mage.cards.Cards;
+import mage.cards.CardsImpl;
 import mage.constants.CardType;
+import mage.constants.Outcome;
+import mage.constants.Zone;
 import mage.filter.FilterCard;
+import mage.game.Game;
+import mage.players.Player;
 import mage.target.TargetPlayer;
 import mage.target.common.TargetCardInGraveyard;
 
-
-
+import java.util.UUID;
 
 /**
  * Rite of Renewal
@@ -28,9 +33,10 @@ public final class RiteOfRenewal extends CardImpl {
         this.getSpellAbility().addTarget(new TargetCardInGraveyard(0, 2, new FilterCard("permanent cards from your graveyard")));
 
         // Target player shuffles up to four target cards from their graveyard into their library.
-        this.getSpellAbility().addEffect(new ShuffleIntoLibraryTargetEffect());
+        this.getSpellAbility().addEffect(new RiteOfRenewalEffect());
         this.getSpellAbility().addTarget(new TargetPlayer());
         this.getSpellAbility().addTarget(new TargetCardInGraveyard(0, 4, new FilterCard("cards from their graveyard")));
+
         // Exile Rite of Renewal.
         this.getSpellAbility().addEffect(new ExileSourceEffect().setText("Exile {this}"));
     }
@@ -42,5 +48,42 @@ public final class RiteOfRenewal extends CardImpl {
     @Override
     public RiteOfRenewal copy() {
         return new RiteOfRenewal(this);
+    }
+}
+
+/**
+ * Custom effect for Rite of Renewal: Target player shuffles up to four target cards from their graveyard into their library.
+ */
+class RiteOfRenewalEffect extends OneShotEffect {
+
+    public RiteOfRenewalEffect() {
+        super(Outcome.Neutral);
+        this.staticText = "Target player shuffles up to four target cards from their graveyard into their library";
+    }
+
+    private RiteOfRenewalEffect(final RiteOfRenewalEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public RiteOfRenewalEffect copy() {
+        return new RiteOfRenewalEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        // Get the target player
+        Player targetPlayer = game.getPlayer(source.getTargets().get(0).getFirstTarget());
+        if (targetPlayer == null) {
+            return false;
+        }
+
+        // Get the cards chosen by the second target (cards in the graveyard)
+        Cards cards = new CardsImpl(source.getTargets().get(1).getTargets());
+        if (!cards.isEmpty()) {
+            targetPlayer.shuffleCardsToLibrary(cards, game, source);
+            return true;
+        }
+        return false;
     }
 }
