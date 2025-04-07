57f4ed26b9c1f8b8f7702a34683e3def5c5783b0
Mike Cunningham <cunningham.mjc@gmail.com>
2025-04-07 08:58:04 -0400

    Update debuff ability, adjust setText, update graveyard filter

--- Mage.Sets/src/mage/cards/w/WailOfWar.java
+++ Mage.Sets/src/mage/cards/w/WailOfWar.java
@@ -2,11 +2,12 @@ package mage.cards.w;
 
 import mage.abilities.Mode;
 import mage.abilities.effects.common.ReturnFromGraveyardToHandTargetEffect;
-import mage.abilities.effects.common.continuous.BoostControlledEffect;
+import mage.abilities.effects.common.continuous.BoostAllEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.Duration;
+import mage.constants.TargetController;
 import mage.filter.StaticFilters;
 import mage.filter.common.FilterCreaturePermanent;
 import mage.target.TargetPlayer;
@@ -20,24 +21,27 @@ import java.util.UUID;
  */
 public final class WailOfWar extends CardImpl {
 
+    private static final FilterCreaturePermanent filter = new FilterCreaturePermanent("creatures target player controls");
+
+    static {
+        filter.add(TargetController.SOURCE_TARGETS.getControllerPredicate());
+    }
+
     public WailOfWar(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{2}{B}");
 
-        // Mode 1: Debuff opponent's creatures
-        Mode debuffOpponentCreaturesMode = new Mode(new BoostControlledEffect(
-            -1, -1, Duration.EndOfTurn, 
-            new FilterCreaturePermanent("creatures target opponent controls")
+        // Mode 1: Creatures target player controls get -1/-1 until end of turn.
+        this.getSpellAbility().addEffect(new BoostAllEffect(
+            -1, -1, Duration.EndOfTurn, filter, false
+        ).setText("Creatures target opponent controls get -1/-1 until end of turn."
         ));
-        debuffOpponentCreaturesMode.addTarget(new TargetPlayer()); // Target a single opponent
-        debuffOpponentCreaturesMode.getEffects().get(0).setText("Creatures target opponent controls get -1/-1 until end of turn.");
-        this.getSpellAbility().addMode(debuffOpponentCreaturesMode);
+        this.getSpellAbility().addTarget(new TargetPlayer());
 
         // Mode 2: Return creatures from graveyard
         Mode returnCreaturesFromGraveyardMode = new Mode(new ReturnFromGraveyardToHandTargetEffect());
         returnCreaturesFromGraveyardMode.addTarget(new TargetCardInGraveyard(
-            0, 2, StaticFilters.FILTER_CARD_CREATURE_YOUR_GRAVEYARD
+            0, 2, StaticFilters.FILTER_CARD_CREATURES_YOUR_GRAVEYARD
         ));
-        returnCreaturesFromGraveyardMode.getEffects().get(0).setText("Return up to two target creatures from your graveyard to your hand.");
         this.getSpellAbility().addMode(returnCreaturesFromGraveyardMode);
     }
 
