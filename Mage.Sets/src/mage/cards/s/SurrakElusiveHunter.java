3b90fb428ad77ba7de64445f8805a64e9c993e76
jmlundeen <jmlundee@yahoo.com>
2025-04-11 09:27:25 -0500

    [TDM] fix Surrak, Elusive Hunter typo

--- Mage.Sets/src/mage/cards/s/SurrakElusiveHunter.java
+++ Mage.Sets/src/mage/cards/s/SurrakElusiveHunter.java
@@ -92,7 +92,7 @@ class SurrakElusiveHunterTriggeredAbility extends TriggeredAbilityImpl {
         }
         StackObject targetingObject = CardUtil.getTargetingStackObject(this.getId().toString(), event, game);
         return targetingObject != null
-                && !game.getOpponents(getControllerId()).contains(targetingObject.getControllerId())
+                && game.getOpponents(getControllerId()).contains(targetingObject.getControllerId())
                 && !CardUtil.checkTargetedEventAlreadyUsed(this.getId().toString(), targetingObject, event, game);
     }
 }
