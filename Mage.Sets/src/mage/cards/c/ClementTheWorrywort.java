c1e2110567827b72ee1db83deaa8b9581ba8d6d8
xenohedron <12538125+xenohedron@users.noreply.github.com>
2025-04-13 14:44:38 -0400

    fixes

--- Mage.Sets/src/mage/cards/c/ClementTheWorrywort.java
+++ Mage.Sets/src/mage/cards/c/ClementTheWorrywort.java
@@ -1,40 +1,32 @@
 package mage.cards.c;
 
-import java.util.UUID;
-
-import mage.abilities.Ability;
 import mage.ConditionalMana;
 import mage.MageInt;
 import mage.Mana;
-import mage.constants.SubType;
-import mage.constants.SuperType;
-import mage.constants.TargetController;
-import mage.constants.Zone;
-import mage.filter.Filter;
-import mage.filter.FilterPermanent;
-import mage.filter.common.FilterControlledCreaturePermanent;
-import mage.filter.common.FilterCreaturePermanent;
-import mage.game.Game;
-import mage.game.permanent.Permanent;
-import mage.players.Player;
-import mage.target.common.TargetCreaturePermanent;
-import mage.abilities.keyword.VigilanceAbility;
-import mage.abilities.mana.builder.ConditionalManaBuilder;
-import mage.abilities.mana.conditional.CreatureCastManaCondition;
-import mage.abilities.mana.ConditionalColoredManaAbility;
+import mage.abilities.Ability;
 import mage.abilities.common.EntersBattlefieldThisOrAnotherTriggeredAbility;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.costs.common.TapSourceCost;
-import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.ReturnToHandTargetEffect;
 import mage.abilities.effects.common.continuous.GainAbilityControlledEffect;
-import mage.cards.Card;
+import mage.abilities.keyword.VigilanceAbility;
+import mage.abilities.mana.ConditionalColoredManaAbility;
+import mage.abilities.mana.builder.ConditionalManaBuilder;
+import mage.abilities.mana.conditional.CreatureCastManaCondition;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
+import mage.constants.*;
+import mage.filter.Filter;
+import mage.filter.FilterPermanent;
+import mage.filter.StaticFilters;
+import mage.filter.common.FilterControlledCreaturePermanent;
 import mage.filter.predicate.mageobject.ManaValuePredicate;
-import mage.constants.CardType;
-import mage.constants.ComparisonType;
-import mage.constants.Duration;
-import mage.constants.Outcome;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.permanent.Permanent;
+import mage.target.TargetPermanent;
+
+import java.util.UUID;
 
 /**
  *
@@ -57,17 +49,20 @@ public final class ClementTheWorrywort extends CardImpl {
         this.addAbility(VigilanceAbility.getInstance());
 
         // Whenever Clement, the Worrywort or another creature you control enters, return up to one target creature you control with lesser mana value to its owner's hand.
-        this.addAbility(new EntersBattlefieldThisOrAnotherTriggeredAbility(new ClementWorrywortEffect(), new FilterCreaturePermanent(), false, true));
+        this.addAbility(new ClementTheWorrywortTriggeredAbility());
 
         // Frogs you control have "{T}: Add {G} or {U}. Spend this mana only to cast a creature spell."
-        Ability gMana = new ConditionalColoredManaAbility(new TapSourceCost(), Mana.GreenMana(1), new ClementWorrywortManaBuilder());
-        Ability bMana = new ConditionalColoredManaAbility(new TapSourceCost(), Mana.BlueMana(1), new ClementWorrywortManaBuilder());
-        this.addAbility(new SimpleStaticAbility(
-            new GainAbilityControlledEffect(gMana, Duration.WhileOnBattlefield, frogFilter, false)
-        ));
-        this.addAbility(new SimpleStaticAbility(
-            new GainAbilityControlledEffect(bMana, Duration.WhileOnBattlefield, frogFilter, false)
-        ));
+        Ability gMana = new ConditionalColoredManaAbility(new TapSourceCost(), Mana.GreenMana(1), new ClementTheWorrywortManaBuilder());
+        Ability bMana = new ConditionalColoredManaAbility(new TapSourceCost(), Mana.BlueMana(1), new ClementTheWorrywortManaBuilder());
+        Ability ability = new SimpleStaticAbility(
+                new GainAbilityControlledEffect(gMana, Duration.WhileOnBattlefield, frogFilter, false)
+                        .setText("Frogs you control have \"{T}: Add {G} or {U}.")
+        );
+        ability.addEffect(
+                new GainAbilityControlledEffect(bMana, Duration.WhileOnBattlefield, frogFilter, false)
+                        .setText("Spend this mana only to cast a creature spell.\"")
+        );
+        this.addAbility(ability);
     }
 
     private ClementTheWorrywort(final ClementTheWorrywort card) {
@@ -80,76 +75,46 @@ public final class ClementTheWorrywort extends CardImpl {
     }
 }
 
-class ClementWorrywortEffect extends OneShotEffect {
+class ClementTheWorrywortTriggeredAbility extends EntersBattlefieldThisOrAnotherTriggeredAbility {
 
-    ClementWorrywortEffect() {
-        super(Outcome.Benefit);
-        staticText = "return up to one target creature you control with lesser mana value to its owner's hand.";
-    }
-
-    private ClementWorrywortEffect(final ClementWorrywortEffect effect) {
-        super(effect);
+    ClementTheWorrywortTriggeredAbility() {
+        super(new ReturnToHandTargetEffect().setText("return up to one target creature you control with lesser mana value to its owner's hand"),
+                StaticFilters.FILTER_PERMANENT_CREATURE, false, true);
     }
 
     @Override
-    public boolean apply(Game game, Ability source) {
-
-        // Get the entering object.
-        Object obj = this.getValue("permanentEnteringBattlefield");
-        if (!(obj instanceof Permanent)) {
-            return false;
-        }
-
-        // Etb creature, it's always going to be a creature due to triggered ability filter.
-        Permanent permanent = (Permanent) obj;
-
-        FilterCreaturePermanent bounceCreature = new FilterCreaturePermanent(
-                "a creature you control with mana value " + permanent.getManaValue() + " or less"
-        );
-        // Can only bounce your creatures
-        bounceCreature.add(TargetController.YOU.getControllerPredicate());
-        // Less than etb card
-        bounceCreature.add(new ManaValuePredicate(ComparisonType.FEWER_THAN, permanent.getManaValue()));
-
-        Player player = game.getPlayer(source.getControllerId());
-        if (player == null) {
-            return false;
-        }
-
-        TargetCreaturePermanent target = new TargetCreaturePermanent(0, 1, bounceCreature, false);
-        if (target.canChoose(source.getControllerId(), source, game)) {
-            player.choose(outcome, target, source, game);
-            Card card = game.getCard(target.getFirstTarget());
-            if (card == null) {
+    public boolean checkTrigger(GameEvent event, Game game) {
+        if (super.checkTrigger(event, game)) {
+            Permanent permanent = game.getPermanent(event.getTargetId());
+            if (permanent == null) {
                 return false;
             }
-
-            player.moveCards(card, Zone.HAND, source, game);
+            int mv = permanent.getManaValue();
+            FilterControlledCreaturePermanent filter =
+                    new FilterControlledCreaturePermanent("creature you control with mana value " + (mv - 1) + " or less");
+            filter.add(new ManaValuePredicate(ComparisonType.FEWER_THAN, mv));
+            this.addTarget(new TargetPermanent(0,1, filter));
+            return true;
         }
-        return true;
-    }
-
-    @Override
-    public OneShotEffect copy() {
-        return new ClementWorrywortEffect(this);
+        return false;
     }
 
 }
 
-class ClementWorrywortConditionalMana extends ConditionalMana {
+class ClementTheWorrywortConditionalMana extends ConditionalMana {
 
-    ClementWorrywortConditionalMana(Mana mana) {
+    ClementTheWorrywortConditionalMana(Mana mana) {
         super(mana);
         setComparisonScope(Filter.ComparisonScope.Any);
         addCondition(new CreatureCastManaCondition());
     }
 }
 
-class ClementWorrywortManaBuilder extends ConditionalManaBuilder {
+class ClementTheWorrywortManaBuilder extends ConditionalManaBuilder {
 
     @Override
     public ConditionalMana build(Object... options) {
-        return new ClementWorrywortConditionalMana(this.mana);
+        return new ClementTheWorrywortConditionalMana(this.mana);
     }
 
     @Override
