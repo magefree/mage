48f2ccfe8f29cad067c663d38b1502a9afb4e64b
xenohedron <12538125+xenohedron@users.noreply.github.com>
2025-04-13 21:16:04 -0400

    updates

--- Mage.Sets/src/mage/cards/o/OsseousSticktwister.java
+++ Mage.Sets/src/mage/cards/o/OsseousSticktwister.java
@@ -1,6 +1,6 @@
 package mage.cards.o;
 
-import mage.abilities.triggers.BeginningOfEndStepTriggeredAbility;
+import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.condition.common.DeliriumCondition;
 import mage.abilities.costs.Cost;
@@ -9,32 +9,28 @@ import mage.abilities.costs.common.DiscardCardCost;
 import mage.abilities.costs.common.SacrificeTargetCost;
 import mage.abilities.dynamicvalue.common.CardTypesInGraveyardCount;
 import mage.abilities.effects.OneShotEffect;
-
-import java.util.HashMap;
-import java.util.Set;
-import java.util.UUID;
-import mage.MageInt;
-import mage.constants.SubType;
-import mage.constants.TargetController;
-import mage.filter.StaticFilters;
-import mage.game.Game;
-import mage.players.Player;
 import mage.abilities.keyword.LifelinkAbility;
+import mage.abilities.triggers.BeginningOfEndStepTriggeredAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.constants.AbilityWord;
-import mage.constants.CardType;
-import mage.constants.Outcome;
+import mage.constants.*;
+import mage.filter.StaticFilters;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.players.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 /**
- *
  * @author earchip94
  */
 public final class OsseousSticktwister extends CardImpl {
 
     public OsseousSticktwister(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT, CardType.CREATURE}, "{1}{B}");
-        
+
         this.subtype.add(SubType.SCARECROW);
         this.power = new MageInt(2);
         this.toughness = new MageInt(2);
@@ -63,8 +59,8 @@ class OsseousSticktwisterEffect extends OneShotEffect {
 
     OsseousSticktwisterEffect() {
         super(Outcome.Benefit);
-        this.staticText = "each opponent may sacrifice a nonland permanent or discard a card. Then {this} "
-                       + "deals damage equal to its power to each opponent who didn't sacrifice a permanent or discard a card in this way.";
+        this.staticText = "each opponent may sacrifice a nonland permanent of their choice or discard a card. Then {this} "
+                + "deals damage equal to its power to each opponent who didn't sacrifice a permanent or discard a card this way.";
     }
 
     OsseousSticktwisterEffect(final OsseousSticktwisterEffect effect) {
@@ -78,49 +74,36 @@ class OsseousSticktwisterEffect extends OneShotEffect {
 
     @Override
     public boolean apply(Game game, Ability source) {
-
-        Set<UUID> opponentsIds = game.getOpponents(source.getControllerId());
-
-        HashMap<UUID, MageInt> damageMap = new HashMap<UUID, MageInt>();
-        for (UUID id : game.getState().getPlayerList(source.getControllerId())) {
-            if (!opponentsIds.contains(id)) {
-                continue;
-            }
-
-            Player opponent = game.getPlayer(id);
+        Permanent permanent = source.getSourcePermanentOrLKI(game);
+        int dmgAmt = permanent == null ? 0 : permanent.getPower().getValue();
+        List<UUID> playersToDamage = new ArrayList<>();
+        String message = "Sacrifice a nonland permanent or discard a card to avoid " + dmgAmt + " damage?";
+        for (UUID uuid : game.getOpponents(source.getControllerId())) {
+            Player opponent = game.getPlayer(uuid);
             if (opponent == null) {
                 continue;
             }
-
             Cost cost = new OrCost(
-                "sacrifice a nonland permanent or discard a card",
-                new SacrificeTargetCost(StaticFilters.FILTER_PERMANENT_NON_LAND),
-                new DiscardCardCost()
+                    "sacrifice a nonland permanent or discard a card",
+                    new SacrificeTargetCost(StaticFilters.FILTER_PERMANENT_NON_LAND),
+                    new DiscardCardCost()
             );
-
-            // Damage equal to creature power.
-            MageInt dmg = game.getPermanent(source.getSourceId()).getPower();
-            boolean canPay = cost.canPay(source, source, id, game);
-
-            if (canPay) {
-                // Face the choice.
-                boolean choice = opponent.chooseUse(
-                    Outcome.Detriment, 
-                    "Sacrifice a nonland permanent or discard a card to avoid " + dmg.getValue() + " damage?",
-                    source, game);
-                
-                if (choice) { // Pay Cost
-                    cost.pay(source, game, source, id, false);
-                    continue; // Paid skip damage
-                }
+            if (!cost.canPay(source, source, opponent.getId(), game)
+                    || !opponent.chooseUse(Outcome.Detriment, message, source, game)
+                    || !cost.pay(source, game, source, opponent.getId(), false)) {
+                playersToDamage.add(opponent.getId());
             }
-            // They either can't discard or sacrifice or chose not to.
-            damageMap.put(id, dmg);
-        } // End for
-
-        // Do the damage to the opponents that chose not to pay the cost.
-        damageMap.forEach((id, dmg) -> game.getPlayer(id).damage(dmg.getValue(), source.getSourceId(), source, game));
-
+        }
+        if (dmgAmt <= 0) {
+            return true;
+        }
+        game.processAction();
+        for (UUID uuid : playersToDamage) {
+            Player opponent = game.getPlayer(uuid);
+            if (opponent != null) {
+                opponent.damage(dmgAmt, source, game);
+            }
+        }
         return true;
     }
 }
