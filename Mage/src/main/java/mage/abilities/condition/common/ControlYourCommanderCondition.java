d4954bf78465428bec9b78f68eb2d90e5bcf7129
theelk801 <theelk801@gmail.com>
2025-04-16 12:13:15 -0400

    rework effects and abilities which care about controlling your own commander (fixes #13542)

--- Mage/src/main/java/mage/abilities/condition/common/ControlYourCommanderCondition.java
+++ Mage/src/main/java/mage/abilities/condition/common/ControlYourCommanderCondition.java
@@ -3,10 +3,10 @@ package mage.abilities.condition.common;
 import mage.abilities.Ability;
 import mage.abilities.condition.Condition;
 import mage.constants.CommanderCardType;
+import mage.game.Controllable;
 import mage.game.Game;
-import mage.game.permanent.Permanent;
+import mage.players.Player;
 
-import java.util.Collection;
 import java.util.Objects;
 
 /**
@@ -19,21 +19,18 @@ public enum ControlYourCommanderCondition implements Condition {
 
     @Override
     public boolean apply(Game game, Ability source) {
-        return game.getPlayerList()
+        Player player = game.getPlayer(source.getControllerId());
+        return player != null && game
+                .getCommandersIds(player, CommanderCardType.COMMANDER_OR_OATHBREAKER, true)
                 .stream()
-                .map(game::getPlayer)
-                .filter(Objects::nonNull)
-                .map(player -> game.getCommandersIds(player, CommanderCardType.COMMANDER_OR_OATHBREAKER, true)) // must search all card parts (example: mdf commander on battlefield)
-                .flatMap(Collection::stream)
                 .map(game::getPermanent)
                 .filter(Objects::nonNull)
-                .filter(Permanent::isPhasedIn)
-                .map(Permanent::getOwnerId)
-                .anyMatch(source.getControllerId()::equals);
+                .map(Controllable::getControllerId)
+                .anyMatch(source::isControlledBy);
     }
 
     @Override
     public String toString() {
-        return "If you control your commander";
+        return "you control your commander";
     }
 }
