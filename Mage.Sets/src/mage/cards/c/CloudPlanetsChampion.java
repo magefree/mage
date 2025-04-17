378d6c6c223178e33e8ddff5b8566f0757e9352e
theelk801 <theelk801@gmail.com>
2025-04-17 10:02:30 -0400

    fix error

--- Mage.Sets/src/mage/cards/c/CloudPlanetsChampion.java
+++ Mage.Sets/src/mage/cards/c/CloudPlanetsChampion.java
@@ -1,6 +1,5 @@
 package mage.cards.c;
 
-import java.util.UUID;
 import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.SimpleStaticAbility;
@@ -13,21 +12,24 @@ import mage.abilities.effects.common.continuous.GainAbilitySourceEffect;
 import mage.abilities.effects.common.cost.ReduceCostEquipTargetSourceEffect;
 import mage.abilities.keyword.DoubleStrikeAbility;
 import mage.abilities.keyword.IndestructibleAbility;
-import mage.cards.f.CloudPlanetsChampion;
-import mage.constants.*;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.constants.SuperType;
+
+import java.util.UUID;
 
 /**
- *
  * @author TheElk801
  */
 public final class CloudPlanetsChampion extends CardImpl {
 
-    private static final Condition condition=new CompoundCondition(EquippedSourceCondition.instance, MyTurnCondition.instance);
+    private static final Condition condition = new CompoundCondition(EquippedSourceCondition.instance, MyTurnCondition.instance);
+
     public CloudPlanetsChampion(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{3}{R}{W}");
-        
+
         this.supertype.add(SuperType.LEGENDARY);
         this.subtype.add(SubType.HUMAN);
         this.subtype.add(SubType.SOLDIER);
@@ -36,7 +38,15 @@ public final class CloudPlanetsChampion extends CardImpl {
         this.toughness = new MageInt(4);
 
         // During your turn, as long as Cloud is equipped, it has double strike and indestructible.
-        Ability ability=new SimpleStaticAbility(new ConditionalContinuousEffect(new GainAbilitySourceEffect(DoubleStrikeAbility.getInstance()),condition,"during your turn, as long as {this} is equipped, it has double strike"));ability.addEffect(new ConditionalContinuousEffect(new GainAbilitySourceEffect(IndestructibleAbility.getInstance()),condition,"and indestructible"));this.addAbility(ability);
+        Ability ability = new SimpleStaticAbility(new ConditionalContinuousEffect(
+                new GainAbilitySourceEffect(DoubleStrikeAbility.getInstance()), condition,
+                "during your turn, as long as {this} is equipped, it has double strike"
+        ));
+        ability.addEffect(new ConditionalContinuousEffect(
+                new GainAbilitySourceEffect(IndestructibleAbility.getInstance()),
+                condition, "and indestructible"
+        ));
+        this.addAbility(ability);
 
         // Equip abilities you activate that target Cloud cost {2} less to activate.
         this.addAbility(new SimpleStaticAbility(new ReduceCostEquipTargetSourceEffect(2)));
