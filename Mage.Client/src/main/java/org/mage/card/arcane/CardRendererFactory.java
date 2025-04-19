0ba4253fc5e977013e8d74fff11453933acb0cef
jmlundeen <jmlundee@yahoo.com>
2025-04-19 09:39:11 -0500

    Add toggle for retro frames
    * Only render retro frames if user selected

--- Mage.Client/src/main/java/org/mage/card/arcane/CardRendererFactory.java
+++ Mage.Client/src/main/java/org/mage/card/arcane/CardRendererFactory.java
@@ -1,6 +1,7 @@
 package org.mage.card.arcane;
 
 import mage.cards.FrameStyle;
+import mage.client.dialog.PreferencesDialog;
 import mage.view.CardView;
 
 /**
@@ -14,7 +15,7 @@ public class CardRendererFactory {
     public CardRenderer create(CardView card) {
         if (card.isSplitCard()) {
             return new ModernSplitCardRenderer(card);
-        } else if (card.getFrameStyle().equals(FrameStyle.RETRO)) {
+        } else if (card.getFrameStyle().equals(FrameStyle.RETRO) && PreferencesDialog.getRenderRetroFrames()) {
             return new RetroCardRenderer(card);
         } else {
             return new ModernCardRenderer(card);
