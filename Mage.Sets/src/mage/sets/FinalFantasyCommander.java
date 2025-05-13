0f62f2451b25b274916cf260699f2dbda902946d
theelk801 <theelk801@gmail.com>
2025-05-13 17:58:00 -0400

    [FIC] Implement Yuna's Decision

--- Mage.Sets/src/mage/sets/FinalFantasyCommander.java
+++ Mage.Sets/src/mage/sets/FinalFantasyCommander.java
@@ -340,5 +340,6 @@ public final class FinalFantasyCommander extends ExpansionSet {
         cards.add(new SetCardInfo("Y'shtola, Night's Blessed", 215, Rarity.MYTHIC, mage.cards.y.YshtolaNightsBlessed.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Y'shtola, Night's Blessed", 226, Rarity.MYTHIC, mage.cards.y.YshtolaNightsBlessed.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Y'shtola, Night's Blessed", 7, Rarity.MYTHIC, mage.cards.y.YshtolaNightsBlessed.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Yuna's Decision", 125, Rarity.RARE, mage.cards.y.YunasDecision.class, NON_FULL_USE_VARIOUS));
     }
 }
