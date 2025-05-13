89b720d4b12d7d0a25c5c33150ca33c228ad40e2
theelk801 <theelk801@gmail.com>
2025-05-13 09:21:50 -0400

    fix test failure

--- Mage.Sets/src/mage/cards/s/SummoningMateria.java
+++ Mage.Sets/src/mage/cards/s/SummoningMateria.java
@@ -4,7 +4,7 @@ import mage.abilities.Ability;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.condition.Condition;
 import mage.abilities.condition.common.AttachedToMatchesFilterCondition;
-import mage.abilities.decorator.ConditionalContinuousEffect;
+import mage.abilities.decorator.ConditionalAsThoughEffect;
 import mage.abilities.effects.common.continuous.BoostEquippedEffect;
 import mage.abilities.effects.common.continuous.GainAbilityAttachedEffect;
 import mage.abilities.effects.common.continuous.LookAtTopCardOfLibraryAnyTimeEffect;
@@ -37,10 +37,9 @@ public final class SummoningMateria extends CardImpl {
         this.addAbility(new SimpleStaticAbility(new LookAtTopCardOfLibraryAnyTimeEffect()));
 
         // As long as this Equipment is attached to a creature, you may cast creature spells from the top of your library.
-        this.addAbility(new SimpleStaticAbility(new ConditionalContinuousEffect(
-                new PlayFromTopOfLibraryEffect(StaticFilters.FILTER_CARD_CREATURE), condition, "as long as " +
-                "{this} is attached to a creature, you may cast creature spells from the top of your library"
-        )));
+        this.addAbility(new SimpleStaticAbility(new ConditionalAsThoughEffect(
+                new PlayFromTopOfLibraryEffect(StaticFilters.FILTER_CARD_CREATURE), condition
+        ).setText("as long as {this} is attached to a creature, you may cast creature spells from the top of your library")));
 
         // Equipped creature gets +2/+2 and has vigilance and "{T}: Add {G}."
         Ability ability = new SimpleStaticAbility(new BoostEquippedEffect(2, 2));
