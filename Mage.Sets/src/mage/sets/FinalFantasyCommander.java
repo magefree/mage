7f8817a00afa71f7875baf547289bf830f6fbd6c
theelk801 <theelk801@gmail.com>
2025-05-14 16:00:48 -0400

    [FIC] add missing card

--- Mage.Sets/src/mage/sets/FinalFantasyCommander.java
+++ Mage.Sets/src/mage/sets/FinalFantasyCommander.java
@@ -365,5 +365,6 @@ public final class FinalFantasyCommander extends ExpansionSet {
         cards.add(new SetCardInfo("Y'shtola, Night's Blessed", 226, Rarity.MYTHIC, mage.cards.y.YshtolaNightsBlessed.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Y'shtola, Night's Blessed", 7, Rarity.MYTHIC, mage.cards.y.YshtolaNightsBlessed.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Yuna's Decision", 125, Rarity.RARE, mage.cards.y.YunasDecision.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Yuna's Decision", 74, Rarity.RARE, mage.cards.y.YunasDecision.class, NON_FULL_USE_VARIOUS));
     }
 }
