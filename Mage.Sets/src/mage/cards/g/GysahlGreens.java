ecec7e4476df5db857b42b3852afde6ec98a4140
theelk801 <theelk801@gmail.com>
2025-05-27 17:05:08 -0400

    [FIN] Implement Choco Comet

--- Mage.Sets/src/mage/cards/g/GysahlGreens.java
+++ Mage.Sets/src/mage/cards/g/GysahlGreens.java
@@ -7,7 +7,7 @@ import mage.abilities.keyword.FlashbackAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.game.permanent.token.BirdLandfallToken;
+import mage.game.permanent.token.ChocoboToken;
 
 /**
  *
@@ -19,7 +19,7 @@ public final class GysahlGreens extends CardImpl {
         super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{1}{G}");
 
         // Create a 2/2 green Bird creature token with "Whenever a land you control enters, this token gets +1/+0 until end of turn."
-        this.getSpellAbility().addEffect(new CreateTokenEffect(new BirdLandfallToken()));
+        this.getSpellAbility().addEffect(new CreateTokenEffect(new ChocoboToken()));
 
         // Flashback {6}{G}
         this.addAbility(new FlashbackAbility(this, new ManaCostsImpl<>("{6}{G}")));
