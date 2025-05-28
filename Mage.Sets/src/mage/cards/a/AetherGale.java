9e364e027eb34b7fb67441e8ffe41bfe7e627587
theelk801 <theelk801@gmail.com>
2025-05-28 15:54:00 -0400

    remove some more unnecessary filter arguments from target constructors

--- Mage.Sets/src/mage/cards/a/AetherGale.java
+++ Mage.Sets/src/mage/cards/a/AetherGale.java
@@ -1,26 +1,25 @@
 package mage.cards.a;
 
-import java.util.UUID;
 import mage.abilities.effects.common.ReturnToHandTargetEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.filter.StaticFilters;
-import mage.target.common.TargetNonlandPermanent;
+import mage.target.TargetPermanent;
+
+import java.util.UUID;
 
 /**
- *
  * @author LevelX2
  */
 public final class AetherGale extends CardImpl {
 
     public AetherGale(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.SORCERY},"{3}{U}{U}");
-
+        super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{3}{U}{U}");
 
         // Return six target nonland permanents to their owners' hands.
         this.getSpellAbility().addEffect(new ReturnToHandTargetEffect());
-        this.getSpellAbility().addTarget(new TargetNonlandPermanent(6,6, StaticFilters.FILTER_PERMANENTS_NON_LAND, false));
+        this.getSpellAbility().addTarget(new TargetPermanent(6, StaticFilters.FILTER_PERMANENTS_NON_LAND));
     }
 
     private AetherGale(final AetherGale card) {
--- Mage.Sets/src/mage/cards/q/QuicksilverGeyser.java
+++ Mage.Sets/src/mage/cards/q/QuicksilverGeyser.java
@@ -1,27 +1,25 @@
-
 package mage.cards.q;
 
-import java.util.UUID;
 import mage.abilities.effects.common.ReturnToHandTargetEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.filter.StaticFilters;
-import mage.target.common.TargetNonlandPermanent;
+import mage.target.TargetPermanent;
+
+import java.util.UUID;
 
 /**
- *
  * @author anonymous
  */
 public final class QuicksilverGeyser extends CardImpl {
 
     public QuicksilverGeyser(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.INSTANT},"{4}{U}");
-
+        super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{4}{U}");
 
         // Return up to two target nonland permanents to their owners' hands.
         this.getSpellAbility().addEffect(new ReturnToHandTargetEffect());
-        this.getSpellAbility().addTarget(new TargetNonlandPermanent(0, 2, StaticFilters.FILTER_PERMANENTS_NON_LAND, false));
+        this.getSpellAbility().addTarget(new TargetPermanent(0, 2, StaticFilters.FILTER_PERMANENTS_NON_LAND));
     }
 
     private QuicksilverGeyser(final QuicksilverGeyser card) {
