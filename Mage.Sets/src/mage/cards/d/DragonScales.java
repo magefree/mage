566839ceeb6d7b35c271087310a7678fa41b732d
xenohedron <12538125+xenohedron@users.noreply.github.com>
2025-05-31 15:00:52 -0400

    refactor cards to new common class
    "When a [] enters, you may return this card from your graveyard to the battlefield enchanting that creature"

--- Mage.Sets/src/mage/cards/d/DragonScales.java
+++ Mage.Sets/src/mage/cards/d/DragonScales.java
@@ -1,27 +1,22 @@
-
 package mage.cards.d;
 
-import java.util.UUID;
 import mage.abilities.Ability;
-import mage.abilities.common.EntersBattlefieldAllTriggeredAbility;
+import mage.abilities.common.EntersBattlefieldAnyReturnSourceFromGraveyardAttachedToItTriggeredAbility;
 import mage.abilities.common.SimpleStaticAbility;
-import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.AttachEffect;
 import mage.abilities.effects.common.continuous.BoostEnchantedEffect;
 import mage.abilities.effects.common.continuous.GainAbilityAttachedEffect;
 import mage.abilities.keyword.EnchantAbility;
 import mage.abilities.keyword.VigilanceAbility;
-import mage.cards.Card;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.*;
 import mage.filter.common.FilterCreaturePermanent;
 import mage.filter.predicate.mageobject.ManaValuePredicate;
-import mage.game.Game;
-import mage.game.permanent.Permanent;
-import mage.players.Player;
 import mage.target.TargetPermanent;
 import mage.target.common.TargetCreaturePermanent;
+
+import java.util.UUID;
 
 /**
  *
@@ -45,12 +40,14 @@ public final class DragonScales extends CardImpl {
         Ability ability = new EnchantAbility(auraTarget);
         this.addAbility(ability);
                 
-        // Enchanted creature gets +1/+2 and has vigilance.        
-        this.addAbility(new SimpleStaticAbility(new BoostEnchantedEffect(1, 2, Duration.WhileOnBattlefield)));
-        this.addAbility(new SimpleStaticAbility(new GainAbilityAttachedEffect(VigilanceAbility.getInstance(), AttachmentType.AURA)));
+        // Enchanted creature gets +1/+2 and has vigilance.
+        ability = new SimpleStaticAbility(new BoostEnchantedEffect(1, 2, Duration.WhileOnBattlefield));
+        ability.addEffect(new GainAbilityAttachedEffect(VigilanceAbility.getInstance(), AttachmentType.AURA)
+                .setText("and has vigilance"));
+        this.addAbility(ability);
         
         // When a creature with converted mana cost 6 or greater enters the battlefield, you may return Dragon Scales from your graveyard to the battlefield attached to that creature.
-        this.addAbility(new EntersBattlefieldAllTriggeredAbility(Zone.GRAVEYARD, new DragonScalesEffect(), filter, true, SetTargetPointer.PERMANENT));
+        this.addAbility(new EntersBattlefieldAnyReturnSourceFromGraveyardAttachedToItTriggeredAbility(filter));
     }
 
     private DragonScales(final DragonScales card) {
@@ -60,37 +57,5 @@ public final class DragonScales extends CardImpl {
     @Override
     public DragonScales copy() {
         return new DragonScales(this);
-    }
-}
-
-class DragonScalesEffect extends OneShotEffect {
-    
-    DragonScalesEffect() {
-        super(Outcome.Benefit);
-        this.staticText = "return {this} from your graveyard to the battlefield attached to that creature";
-    }
-    
-    private DragonScalesEffect(final DragonScalesEffect effect) {
-        super(effect);
-    }
-    
-    @Override
-    public DragonScalesEffect copy() {
-        return new DragonScalesEffect(this);
-    }
-    
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Card sourceCard = source.getSourceCardIfItStillExists(game);
-        Permanent permanent = game.getPermanent(this.getTargetPointer().getFirst(game, source));
-        Player controller = game.getPlayer(source.getControllerId());
-        if (sourceCard != null && permanent != null && controller != null) {
-            game.getState().setValue("attachTo:" + sourceCard.getId(), permanent);
-            if (controller.moveCards(sourceCard, Zone.BATTLEFIELD, source, game)) {
-                permanent.addAttachment(sourceCard.getId(), source, game);
-            }
-            return true;
-        }
-        return false;
     }
 }
