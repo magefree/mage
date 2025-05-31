6888ded026c2f69216137df4274783d6e7b15b4a
theelk801 <theelk801@gmail.com>
2025-05-31 18:46:04 -0400

    [FIC] a few changes to Blitzball Stadium

--- Mage.Sets/src/mage/cards/b/BlitzballStadium.java
+++ Mage.Sets/src/mage/cards/b/BlitzballStadium.java
@@ -1,7 +1,5 @@
 package mage.cards.b;
 
-import java.util.UUID;
-
 import mage.abilities.Ability;
 import mage.abilities.common.DealsCombatDamageToAPlayerTriggeredAbility;
 import mage.abilities.common.EntersBattlefieldTriggeredAbility;
@@ -14,7 +12,6 @@ import mage.abilities.effects.common.DrawCardSourceControllerEffect;
 import mage.abilities.effects.common.combat.CantBeBlockedTargetEffect;
 import mage.abilities.effects.common.continuous.GainAbilityTargetEffect;
 import mage.abilities.effects.common.counter.AddCountersTargetEffect;
-import mage.abilities.hint.ValueHint;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
@@ -24,35 +21,30 @@ import mage.game.permanent.Permanent;
 import mage.target.common.TargetCreaturePermanent;
 import mage.target.targetadjustment.XTargetsCountAdjuster;
 
+import java.util.UUID;
+
 /**
  * @author balazskristof
  */
 public final class BlitzballStadium extends CardImpl {
 
-    public static final ValueHint hint = new ValueHint("Kind of counters on it", BlitzballStadiumValue.instance);
-
     public BlitzballStadium(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT}, "{X}{U}");
 
         // When this artifact enters, support X.
-        Ability supportAbility = new EntersBattlefieldTriggeredAbility(new AddCountersTargetEffect(CounterType.P1P1.createInstance())
-                .setText("support X. <i>(Put a +1/+1 counter on each of up to X target creatures.)</i>")
+        Ability supportAbility = new EntersBattlefieldTriggeredAbility(
+                new AddCountersTargetEffect(CounterType.P1P1.createInstance())
+                        .setText("support X. <i>(Put a +1/+1 counter on each of up to X target creatures.)</i>")
         );
         supportAbility.addTarget(new TargetCreaturePermanent(0, 1));
         supportAbility.setTargetAdjuster(new XTargetsCountAdjuster());
         this.addAbility(supportAbility);
 
         // Go for the Goal! -- {3}, {T}: Until end of turn, target creature gains "Whenever this creature deals combat damage to a player, draw a card for each kind of counter on it" and it can't be blocked this turn.
-        Ability ability = new SimpleActivatedAbility(
-                new GainAbilityTargetEffect(
-                        new DealsCombatDamageToAPlayerTriggeredAbility(
-                                new DrawCardSourceControllerEffect(BlitzballStadiumValue.instance)
-                                    .setText("draw a card for each kind of counter on it"),
-                                false
-                        ).addHint(hint)
-                ).setText("Until end of turn, target creature gains \"Whenever this creature deals combat damage to a player, draw a card for each kind of counter on it\""),
-                new GenericManaCost(3)
-        );
+        Ability ability = new SimpleActivatedAbility(new GainAbilityTargetEffect(
+                new DealsCombatDamageToAPlayerTriggeredAbility(new DrawCardSourceControllerEffect(BlitzballStadiumValue.instance))
+        ).setText("Until end of turn, target creature gains \"Whenever this creature deals " +
+                "combat damage to a player, draw a card for each kind of counter on it\""), new GenericManaCost(3));
         ability.addCost(new TapSourceCost());
         ability.addEffect(new CantBeBlockedTargetEffect().setText("and it can't be blocked this turn"));
         ability.addTarget(new TargetCreaturePermanent());
@@ -88,6 +80,11 @@ enum BlitzballStadiumValue implements DynamicValue {
 
     @Override
     public String getMessage() {
-        return "";
+        return "kind of counter on it";
+    }
+
+    @Override
+    public String toString() {
+        return "1";
     }
 }
