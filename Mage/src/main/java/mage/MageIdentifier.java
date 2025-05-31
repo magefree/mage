a7dcd6988f2d83551e9144194e4a41e9bb26a768
Evan Kranzler <theelk801@gmail.com>
2025-05-31 13:42:39 -0400

    [FIC] Implement Locke, Treasure Hunter (#13695)

--- Mage/src/main/java/mage/MageIdentifier.java
+++ Mage/src/main/java/mage/MageIdentifier.java
@@ -37,6 +37,7 @@ public enum MageIdentifier {
     LaraCroftTombRaiderWatcher,
     CoramTheUndertakerWatcher,
     ThundermanDragonWatcher,
+    LockeTreasureHunterWatcher,
 
     // ----------------------------//
     //       alternate casts       //
