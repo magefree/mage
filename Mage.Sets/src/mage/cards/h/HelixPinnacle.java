0e179ccc1f4968af211a4c8b2e83b91e873b913c
Evan Kranzler <theelk801@gmail.com>
2025-06-07 21:03:21 -0400

    Refactor SourceHasCounterCondition to be an IntCompareCondition (#13724)
    * refactor SourceHasCounterCondition to be an IntCompareCondition
    
    * add comment for arguments

--- Mage.Sets/src/mage/cards/h/HelixPinnacle.java
+++ Mage.Sets/src/mage/cards/h/HelixPinnacle.java
@@ -1,29 +1,28 @@
 
 package mage.cards.h;
 
-import java.util.UUID;
-import mage.abilities.triggers.BeginningOfUpkeepTriggeredAbility;
 import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.condition.Condition;
 import mage.abilities.condition.common.SourceHasCounterCondition;
 import mage.abilities.costs.mana.ManaCostsImpl;
-import mage.abilities.decorator.ConditionalInterveningIfTriggeredAbility;
 import mage.abilities.dynamicvalue.common.GetXValue;
 import mage.abilities.effects.common.WinGameSourceControllerEffect;
 import mage.abilities.effects.common.counter.AddCountersSourceEffect;
 import mage.abilities.keyword.ShroudAbility;
+import mage.abilities.triggers.BeginningOfUpkeepTriggeredAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.Zone;
 import mage.counters.CounterType;
 
+import java.util.UUID;
+
 /**
- *
  * @author jeffwadsworth
  */
 public final class HelixPinnacle extends CardImpl {
 
-    static final String rule = "at the beginning of your upkeep, if there are 100 or more tower counters on Helix Pinnacle, you win the game";
+    private static final Condition condition = new SourceHasCounterCondition(CounterType.TOWER, 100);
 
     public HelixPinnacle(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ENCHANTMENT}, "{G}");
@@ -32,16 +31,12 @@ public final class HelixPinnacle extends CardImpl {
         this.addAbility(ShroudAbility.getInstance());
 
         // {X}: Put X tower counters on Helix Pinnacle.
-        this.addAbility(new SimpleActivatedAbility(
-                new AddCountersSourceEffect(CounterType.TOWER.createInstance(), GetXValue.instance, true),
-                new ManaCostsImpl<>("{X}")));
+        this.addAbility(new SimpleActivatedAbility(new AddCountersSourceEffect(
+                CounterType.TOWER.createInstance(), GetXValue.instance, true
+        ), new ManaCostsImpl<>("{X}")));
 
         // At the beginning of your upkeep, if there are 100 or more tower counters on Helix Pinnacle, you win the game.
-        this.addAbility(new ConditionalInterveningIfTriggeredAbility(
-                new BeginningOfUpkeepTriggeredAbility(new WinGameSourceControllerEffect()),
-                new SourceHasCounterCondition(CounterType.TOWER, 100, Integer.MAX_VALUE),
-                rule));
-
+        this.addAbility(new BeginningOfUpkeepTriggeredAbility(new WinGameSourceControllerEffect()).withInterveningIf(condition));
     }
 
     private HelixPinnacle(final HelixPinnacle card) {
--- Mage.Sets/src/mage/cards/r/RocHatchling.java
+++ Mage.Sets/src/mage/cards/r/RocHatchling.java
@@ -1,12 +1,10 @@
-
 package mage.cards.r;
 
-import java.util.UUID;
 import mage.MageInt;
 import mage.abilities.Ability;
-import mage.abilities.triggers.BeginningOfUpkeepTriggeredAbility;
 import mage.abilities.common.EntersBattlefieldAbility;
 import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.condition.Condition;
 import mage.abilities.condition.common.SourceHasCounterCondition;
 import mage.abilities.decorator.ConditionalContinuousEffect;
 import mage.abilities.effects.common.continuous.BoostSourceEffect;
@@ -14,37 +12,50 @@ import mage.abilities.effects.common.continuous.GainAbilitySourceEffect;
 import mage.abilities.effects.common.counter.AddCountersSourceEffect;
 import mage.abilities.effects.common.counter.RemoveCounterSourceEffect;
 import mage.abilities.keyword.FlyingAbility;
+import mage.abilities.triggers.BeginningOfUpkeepTriggeredAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.SubType;
+import mage.constants.ComparisonType;
 import mage.constants.Duration;
-import mage.constants.Zone;
+import mage.constants.SubType;
 import mage.counters.CounterType;
 
+import java.util.UUID;
+
 /**
- *
  * @author LoneFox
  */
 public final class RocHatchling extends CardImpl {
 
+    private static final Condition condition = new SourceHasCounterCondition(CounterType.SHELL, ComparisonType.EQUAL_TO, 0);
+
     public RocHatchling(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.CREATURE},"{R}");
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{R}");
         this.subtype.add(SubType.BIRD);
         this.power = new MageInt(0);
         this.toughness = new MageInt(1);
 
         // Roc Hatchling enters the battlefield with four shell counters on it.
-        this.addAbility(new EntersBattlefieldAbility(new AddCountersSourceEffect(CounterType.SHELL.createInstance(4)), "with four shell counters on it"));
+        this.addAbility(new EntersBattlefieldAbility(
+                new AddCountersSourceEffect(CounterType.SHELL.createInstance(4)),
+                "with four shell counters on it"
+        ));
+
         // At the beginning of your upkeep, remove a shell counter from Roc Hatchling.
-        this.addAbility(new BeginningOfUpkeepTriggeredAbility(new RemoveCounterSourceEffect(CounterType.SHELL.createInstance())));
+        this.addAbility(new BeginningOfUpkeepTriggeredAbility(
+                new RemoveCounterSourceEffect(CounterType.SHELL.createInstance())
+        ));
+
         // As long as Roc Hatchling has no shell counters on it, it gets +3/+2 and has flying.
         Ability ability = new SimpleStaticAbility(new ConditionalContinuousEffect(
-            new BoostSourceEffect(3, 2, Duration.WhileOnBattlefield),
-            new SourceHasCounterCondition(CounterType.SHELL, 0, 0),
-            "As long as {this} has no shell counters on it, it gets +3/+2"));
-        ability.addEffect(new ConditionalContinuousEffect(new GainAbilitySourceEffect(FlyingAbility.getInstance()),
-            new SourceHasCounterCondition(CounterType.SHELL, 0, 0), "and has flying"));
+                new BoostSourceEffect(3, 2, Duration.WhileOnBattlefield),
+                condition, "as long as {this} has no shell counters on it, it gets +3/+2"
+        ));
+        ability.addEffect(new ConditionalContinuousEffect(
+                new GainAbilitySourceEffect(FlyingAbility.getInstance()),
+                condition, "and has flying"
+        ));
         this.addAbility(ability);
     }
 
