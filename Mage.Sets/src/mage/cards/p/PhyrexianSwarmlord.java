6d3bd7a19f2f3b938ebb9573bce391aad24e57ab
theelk801 <theelk801@gmail.com>
2025-06-15 15:28:08 -0400

    refactor: remove OnEventTriggeredAbility

--- Mage.Sets/src/mage/cards/p/PhyrexianSwarmlord.java
+++ Mage.Sets/src/mage/cards/p/PhyrexianSwarmlord.java
@@ -1,27 +1,25 @@
-
 package mage.cards.p;
 
-import java.util.UUID;
 import mage.MageInt;
-import mage.abilities.common.OnEventTriggeredAbility;
 import mage.abilities.dynamicvalue.common.OpponentsPoisonCountersCount;
 import mage.abilities.effects.common.CreateTokenEffect;
 import mage.abilities.keyword.InfectAbility;
+import mage.abilities.triggers.BeginningOfUpkeepTriggeredAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.SubType;
-import mage.game.events.GameEvent.EventType;
 import mage.game.permanent.token.InsectInfectToken;
 
+import java.util.UUID;
+
 /**
- *
  * @author North
  */
 public final class PhyrexianSwarmlord extends CardImpl {
 
     public PhyrexianSwarmlord(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.CREATURE},"{4}{G}{G}");
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{4}{G}{G}");
         this.subtype.add(SubType.PHYREXIAN);
         this.subtype.add(SubType.INSECT);
         this.subtype.add(SubType.HORROR);
@@ -30,8 +28,9 @@ public final class PhyrexianSwarmlord extends CardImpl {
         this.toughness = new MageInt(4);
 
         this.addAbility(InfectAbility.getInstance());
-        this.addAbility(new OnEventTriggeredAbility(EventType.UPKEEP_STEP_PRE, "beginning of your upkeep",
-                new CreateTokenEffect(new InsectInfectToken(), OpponentsPoisonCountersCount.instance)));
+        this.addAbility(new BeginningOfUpkeepTriggeredAbility(
+                new CreateTokenEffect(new InsectInfectToken(), OpponentsPoisonCountersCount.instance)
+        ));
     }
 
     private PhyrexianSwarmlord(final PhyrexianSwarmlord card) {
@@ -43,4 +42,3 @@ public final class PhyrexianSwarmlord extends CardImpl {
         return new PhyrexianSwarmlord(this);
     }
 }
-
