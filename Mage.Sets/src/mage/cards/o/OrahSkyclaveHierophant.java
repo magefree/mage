ea40b3edc35b38f0f4ea38c3e70cf63d13fa8f9e
Steven Knipe <knipesteven@gmail.com>
2025-06-18 00:57:51 -0700

    OrahSkyclaveHierophant

--- Mage.Sets/src/mage/cards/o/OrahSkyclaveHierophant.java
+++ Mage.Sets/src/mage/cards/o/OrahSkyclaveHierophant.java
@@ -1,19 +1,24 @@
 package mage.cards.o;
 
 import mage.MageInt;
-import mage.MageObject;
-import mage.abilities.TriggeredAbilityImpl;
+import mage.abilities.Ability;
+import mage.abilities.common.DiesThisOrAnotherTriggeredAbility;
 import mage.abilities.effects.common.ReturnFromGraveyardToBattlefieldTargetEffect;
 import mage.abilities.keyword.LifelinkAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.constants.*;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.constants.SuperType;
 import mage.filter.FilterCard;
-import mage.filter.predicate.mageobject.ManaValuePredicate;
+import mage.filter.FilterPermanent;
+import mage.filter.common.FilterControlledPermanent;
+import mage.filter.predicate.ObjectSourcePlayer;
+import mage.filter.predicate.ObjectSourcePlayerPredicate;
 import mage.game.Game;
-import mage.game.events.GameEvent;
-import mage.game.events.ZoneChangeEvent;
-import mage.target.common.TargetCardInYourGraveyard;
+import mage.game.permanent.Permanent;
+import mage.target.common.TargetCardInGraveyard;
+import mage.util.CardUtil;
 
 import java.util.UUID;
 
@@ -21,6 +26,13 @@ import java.util.UUID;
  * @author TheElk801
  */
 public final class OrahSkyclaveHierophant extends CardImpl {
+    static FilterPermanent filterTrigger = new FilterControlledPermanent("Cleric you control");
+    static FilterCard filterTarget = new FilterCard("Cleric card with lesser converted mana cost");
+    static {
+        filterTrigger.add(SubType.CLERIC.getPredicate());
+        filterTarget.add(SubType.CLERIC.getPredicate());
+        filterTarget.add(OrahSkyclaveHierophantPredicate.instance);
+    }
 
     public OrahSkyclaveHierophant(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{2}{W}{B}");
@@ -35,7 +47,9 @@ public final class OrahSkyclaveHierophant extends CardImpl {
         this.addAbility(LifelinkAbility.getInstance());
 
         // Whenever Orah, Skyclave Hierophant or another Cleric you control dies, return target Cleric card with lesser converted mana cost from your graveyard to the battlefield.
-        this.addAbility(new OrahSkyclaveHierophantTriggeredAbility());
+        Ability ability = new DiesThisOrAnotherTriggeredAbility(new ReturnFromGraveyardToBattlefieldTargetEffect(), false, filterTrigger);
+        ability.addTarget(new TargetCardInGraveyard(filterTarget));
+        this.addAbility(ability);
     }
 
     private OrahSkyclaveHierophant(final OrahSkyclaveHierophant card) {
@@ -48,57 +62,15 @@ public final class OrahSkyclaveHierophant extends CardImpl {
     }
 }
 
-class OrahSkyclaveHierophantTriggeredAbility extends TriggeredAbilityImpl {
-
-    OrahSkyclaveHierophantTriggeredAbility() {
-        super(Zone.BATTLEFIELD, new ReturnFromGraveyardToBattlefieldTargetEffect());
-        setLeavesTheBattlefieldTrigger(true);
-    }
-
-    private OrahSkyclaveHierophantTriggeredAbility(final OrahSkyclaveHierophantTriggeredAbility ability) {
-        super(ability);
-    }
+enum OrahSkyclaveHierophantPredicate implements ObjectSourcePlayerPredicate<Permanent> {
+    instance;
 
     @Override
-    public boolean checkEventType(GameEvent event, Game game) {
-        return event.getType() == GameEvent.EventType.ZONE_CHANGE;
-    }
-
-    @Override
-    public boolean checkTrigger(GameEvent event, Game game) {
-        ZoneChangeEvent zEvent = (ZoneChangeEvent) event;
-        if (!zEvent.isDiesEvent()
-                ||  !zEvent.getTarget().isControlledBy(getControllerId())
-                || (!zEvent.getTarget().hasSubtype(SubType.CLERIC, game)
-                    && !zEvent.getTarget().getId().equals(getSourceId()))
-                ) {
-            return false;
-        }
-        FilterCard filterCard = new FilterCard(
-                "Cleric card with mana value less than " + (zEvent.getTarget().getManaValue())
-        );
-        filterCard.add(SubType.CLERIC.getPredicate());
-        filterCard.add(new ManaValuePredicate(
-                ComparisonType.FEWER_THAN, zEvent.getTarget().getManaValue()
-        ));
-        this.getTargets().clear();
-        this.addTarget(new TargetCardInYourGraveyard(filterCard));
-        return true;
-    }
-
-    @Override
-    public OrahSkyclaveHierophantTriggeredAbility copy() {
-        return new OrahSkyclaveHierophantTriggeredAbility(this);
-    }
-
-    @Override
-    public String getRule() {
-        return "Whenever {this} or another Cleric you control dies, return target Cleric card " +
-                "with lesser mana value from your graveyard to the battlefield.";
-    }
-
-    @Override
-    public boolean isInUseableZone(Game game, MageObject sourceObject, GameEvent event) {
-        return TriggeredAbilityImpl.isInUseableZoneDiesTrigger(this, sourceObject, event, game);
+    public boolean apply(ObjectSourcePlayer<Permanent> input, Game game) {
+        return CardUtil.getEffectValueFromAbility(
+                        input.getSource(), "creatureDied", Permanent.class
+                )
+                .filter(permanent -> input.getObject().getManaValue() < permanent.getManaValue())
+                .isPresent();
     }
 }
