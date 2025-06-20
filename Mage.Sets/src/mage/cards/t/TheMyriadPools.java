8511deb07c55ac0f6159b2983d7686f64e374262
Steven Knipe <knipesteven@gmail.com>
2025-06-19 20:24:53 -0700

    TheMyriadPools use CastSpellPaidBySourceTriggeredAbility, add test

--- Mage.Sets/src/mage/cards/t/TheMyriadPools.java
+++ Mage.Sets/src/mage/cards/t/TheMyriadPools.java
@@ -1,31 +1,38 @@
 package mage.cards.t;
 
 import mage.abilities.Ability;
-import mage.abilities.TriggeredAbilityImpl;
+import mage.abilities.common.CastSpellPaidBySourceTriggeredAbility;
 import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.CopyEffect;
 import mage.abilities.mana.BlueManaAbility;
-import mage.cards.Card;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.constants.*;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Outcome;
+import mage.constants.SuperType;
+import mage.filter.FilterSpell;
 import mage.filter.StaticFilters;
+import mage.filter.predicate.mageobject.PermanentPredicate;
 import mage.game.Game;
-import mage.game.events.GameEvent;
 import mage.game.permanent.Permanent;
 import mage.game.permanent.PermanentCard;
 import mage.game.stack.Spell;
 import mage.players.Player;
 import mage.target.TargetPermanent;
-import mage.watchers.Watcher;
 
 import java.util.UUID;
 
 /**
- *
  * @author jeffwadsworth
  */
 public class TheMyriadPools extends CardImpl {
+
+    private static final FilterSpell filter = new FilterSpell("a permanent spell");
+
+    static {
+        filter.add(PermanentPredicate.instance);
+    }
 
     public TheMyriadPools(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT, CardType.LAND}, null);
@@ -35,11 +42,12 @@ public class TheMyriadPools extends CardImpl {
         this.nightCard = true;
 
         // {T}: Add {U}.
-        Ability manaAbility = new BlueManaAbility();
-        // Whenever you cast a permanent spell using mana produced by The Myriad Pools, up to one other target permanent you control becomes a copy of that spell until end of turn.
-        this.addAbility(manaAbility, new TheMyriadPoolsWatcher(manaAbility.getOriginalId().toString()));
-        this.addAbility(new TheMyriadPoolsTriggeredAbility());
+        this.addAbility(new BlueManaAbility());
 
+        // Whenever you cast a permanent spell using mana produced by The Myriad Pools, up to one other target permanent you control becomes a copy of that spell until end of turn.
+        Ability ability = new CastSpellPaidBySourceTriggeredAbility(new TheMyriadPoolsCopyEffect(), filter, false);
+        ability.addTarget(new TargetPermanent(0, 1, StaticFilters.FILTER_CONTROLLED_ANOTHER_TARGET_PERMANENT));
+        this.addAbility(ability);
     }
 
     private TheMyriadPools(final TheMyriadPools card) {
@@ -52,85 +60,11 @@ public class TheMyriadPools extends CardImpl {
     }
 }
 
-class TheMyriadPoolsWatcher extends Watcher {
-
-    private UUID permanentId = UUID.randomUUID();
-    private final String originalId;
-
-    public TheMyriadPoolsWatcher(String originalId) {
-        super(WatcherScope.CARD);
-        this.originalId = originalId;
-    }
-
-    public boolean manaUsedToCastPermanentPart(UUID id) {
-        return permanentId.equals(id);
-    }
-
-    @Override
-    public void watch(GameEvent event, Game game) {
-        if (event.getType() == GameEvent.EventType.MANA_PAID) {
-            if (event.getData() != null && event.getData().equals(originalId)) {
-                Spell spell = game.getStack().getSpell(event.getTargetId());
-                if (spell != null && spell.isPermanent(game)) {
-                    Card card = spell.getCard();
-                    permanentId = card.getId();
-                }
-            }
-        }
-    }
-
-    @Override
-    public void reset() {
-        super.reset();
-    }
-}
-
-class TheMyriadPoolsTriggeredAbility extends TriggeredAbilityImpl {
-
-    public TheMyriadPoolsTriggeredAbility() {
-        super(Zone.BATTLEFIELD, new TheMyriadPoolsCopyEffect());
-        this.addTarget(new TargetPermanent(StaticFilters.FILTER_CONTROLLED_ANOTHER_PERMANENT));
-    }
-
-    private TheMyriadPoolsTriggeredAbility(final TheMyriadPoolsTriggeredAbility ability) {
-        super(ability);
-    }
-
-    @Override
-    public TheMyriadPoolsTriggeredAbility copy() {
-        return new TheMyriadPoolsTriggeredAbility(this);
-    }
-
-    @Override
-    public boolean checkEventType(GameEvent event, Game game) {
-        return event.getType() == GameEvent.EventType.SPELL_CAST;
-    }
-
-    @Override
-    public boolean checkTrigger(GameEvent event, Game game) {
-        TheMyriadPoolsWatcher watcher = game.getState().getWatcher(TheMyriadPoolsWatcher.class, this.getSourceId());
-        if (watcher != null && watcher.manaUsedToCastPermanentPart(event.getSourceId())) {
-            Spell spell = game.getSpell(event.getSourceId());
-            if (spell != null && spell.isControlledBy(getControllerId())) {
-                this.getEffects().setValue("SpellCastId", event.getSourceId());
-                return true;
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public String getRule() {
-        return "Whenever you cast a permanent spell using mana produced by {this}, up to one other target permanent you control becomes a copy of that spell until end of turn.";
-    }
-
-}
-
 class TheMyriadPoolsCopyEffect extends OneShotEffect {
 
     TheMyriadPoolsCopyEffect() {
         super(Outcome.Neutral);
-        this.staticText = "copy of card on stack";
+        this.staticText = "up to one other target permanent you control becomes a copy of that spell until end of turn";
     }
 
     private TheMyriadPoolsCopyEffect(final TheMyriadPoolsCopyEffect effect) {
@@ -150,9 +84,9 @@ class TheMyriadPoolsCopyEffect extends OneShotEffect {
         if (controller == null || targetPermanentToCopyTo == null || !(spellId instanceof UUID)) {
             return false;
         }
-        Card copyFromCardOnStack = game.getCard((UUID) spellId);
-        if (copyFromCardOnStack != null) {
-            Permanent newBluePrint = new PermanentCard(copyFromCardOnStack, source.getControllerId(), game);
+        Spell spell = game.getSpell((UUID) spellId);
+        if (spell != null) {
+            Permanent newBluePrint = new PermanentCard(spell.getCard(), source.getControllerId(), game);
             newBluePrint.assignNewId();
             CopyEffect copyEffect = new CopyEffect(Duration.EndOfTurn, newBluePrint, targetPermanentToCopyTo.getId());
             Ability newAbility = source.copy();
