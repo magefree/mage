3888547fdb75329deab91954ee0ca0e121170169
Steven Knipe <knipesteven@gmail.com>
2025-06-21 21:00:48 -0700

    Text fix for cards with "named {this}" to use actual card name

--- Mage.Sets/src/mage/cards/t/TakeInventory.java
+++ Mage.Sets/src/mage/cards/t/TakeInventory.java
@@ -1,7 +1,6 @@
 
 package mage.cards.t;
 
-import java.util.UUID;
 import mage.abilities.dynamicvalue.common.CardsInControllerGraveyardCount;
 import mage.abilities.effects.Effect;
 import mage.abilities.effects.common.DrawCardSourceControllerEffect;
@@ -13,8 +12,9 @@ import mage.constants.CardType;
 import mage.filter.FilterCard;
 import mage.filter.predicate.mageobject.NamePredicate;
 
+import java.util.UUID;
+
 /**
- *
  * @author fireshoes
  * modified tiera3 - added Hint
  */
@@ -25,17 +25,18 @@ public final class TakeInventory extends CardImpl {
     static {
         filter.add(new NamePredicate("Take Inventory"));
     }
+
     private static final Hint hint = new ValueHint(
             "Cards named Take Inventory in your graveyard", new CardsInControllerGraveyardCount(filter)
     );
 
     public TakeInventory(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.SORCERY},"{1}{U}");
+        super(ownerId, setInfo, new CardType[]{CardType.SORCERY}, "{1}{U}");
 
         // Draw a card, then draw cards equal to the number of cards named Take Inventory in your graveyard.
         this.getSpellAbility().addEffect(new DrawCardSourceControllerEffect(1));
         Effect effect = new DrawCardSourceControllerEffect(new CardsInControllerGraveyardCount(filter));
-        effect.setText(", then draw cards equal to the number of cards named {this} in your graveyard");
+        effect.setText(", then draw cards equal to the number of cards named Take Inventory in your graveyard");
         this.getSpellAbility().addEffect(effect);
         this.getSpellAbility().addHint(hint);
     }
