b4519a33615bc3937a7dfe77ee3cde8e09787670
Oleg Agafonov <jaydi85@gmail.com>
2025-06-29 18:13:27 +0400

    AI, tests: added testable use cases for getAmount and announceX dialogs (part of #13638, #13766)

--- Mage.Common/src/main/java/mage/utils/testers/AmountTestableResult.java
+++ Mage.Common/src/main/java/mage/utils/testers/AmountTestableResult.java
@@ -9,7 +9,11 @@ import java.util.List;
  */
 public class AmountTestableResult extends BaseTestableResult {
 
-    int amount = 0;
+    Integer amount = null;
+
+    boolean aiAssertEnabled = false;
+    int aiAssertMinAmount = 0;
+    int aiAssertMaxAmount = 0;
 
     public void onFinish(String resDebugSource, boolean status, List<String> info, int amount) {
         this.onFinish(resDebugSource, status, info);
@@ -18,12 +22,38 @@ public class AmountTestableResult extends BaseTestableResult {
 
     @Override
     public String getResAssert() {
-        return null; // TODO: implement
+        if (!this.aiAssertEnabled) {
+            return null;
+        }
+
+        // not finished
+        if (this.amount == null) {
+            return null;
+        }
+
+        if (!this.getResStatus()) {
+            return String.format("Wrong status: need %s, but get %s",
+                    true, // res must be true all the time
+                    this.getResStatus()
+            );
+        }
+
+        // wrong amount
+        if (this.amount < this.aiAssertMinAmount || this.amount > this.aiAssertMaxAmount) {
+            return String.format("Wrong amount: need [%d, %d], but get %d",
+                    this.aiAssertMinAmount,
+                    this.aiAssertMaxAmount,
+                    this.amount
+            );
+        }
+
+        // all fine
+        return "";
     }
 
     @Override
     public void onClear() {
         super.onClear();
-        this.amount = 0;
+        this.amount = null;
     }
 }
