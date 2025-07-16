2b34c1474f803f9030b3ff0186caf5056dd9061f
theelk801 <theelk801@gmail.com>
2025-07-16 17:37:55 -0400

    [EOE] Implement Atomic Microsizer

--- Mage.Sets/src/mage/sets/EdgeOfEternities.java
+++ Mage.Sets/src/mage/sets/EdgeOfEternities.java
@@ -43,6 +43,7 @@ public final class EdgeOfEternities extends ExpansionSet {
         cards.add(new SetCardInfo("Astelli Reclaimer", 288, Rarity.RARE, mage.cards.a.AstelliReclaimer.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Astelli Reclaimer", 4, Rarity.RARE, mage.cards.a.AstelliReclaimer.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Atmospheric Greenhouse", 171, Rarity.UNCOMMON, mage.cards.a.AtmosphericGreenhouse.class));
+        cards.add(new SetCardInfo("Atomic Microsizer", 47, Rarity.UNCOMMON, mage.cards.a.AtomicMicrosizer.class));
         cards.add(new SetCardInfo("Banishing Light", 6, Rarity.COMMON, mage.cards.b.BanishingLight.class));
         cards.add(new SetCardInfo("Beyond the Quiet", 303, Rarity.RARE, mage.cards.b.BeyondTheQuiet.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Beyond the Quiet", 7, Rarity.RARE, mage.cards.b.BeyondTheQuiet.class, NON_FULL_USE_VARIOUS));
