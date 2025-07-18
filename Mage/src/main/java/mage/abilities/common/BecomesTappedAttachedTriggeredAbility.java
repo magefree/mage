1a7d8ce3974dc4c8a8a18852edbfd252c677ceb3
Susucre <34709007+Susucre@users.noreply.github.com>
2025-07-18 18:43:21 +0200

    implement [EOE] Cryoshatter

--- Mage/src/main/java/mage/abilities/common/BecomesTappedAttachedTriggeredAbility.java
+++ Mage/src/main/java/mage/abilities/common/BecomesTappedAttachedTriggeredAbility.java
@@ -1,29 +1,39 @@
 
 package mage.abilities.common;
 
+import mage.constants.SetTargetPointer;
 import mage.constants.Zone;
 import mage.abilities.TriggeredAbilityImpl;
 import mage.abilities.effects.Effect;
 import mage.game.Game;
 import mage.game.events.GameEvent;
 import mage.game.permanent.Permanent;
+import mage.target.targetpointer.FixedTarget;
 
 /**
  * @author LoneFox
  */
 public class BecomesTappedAttachedTriggeredAbility extends TriggeredAbilityImpl {
 
+    private final SetTargetPointer setTargetPointer;
+
     public BecomesTappedAttachedTriggeredAbility(Effect effect, String description) {
         this(effect, description, false);
     }
 
     public BecomesTappedAttachedTriggeredAbility(Effect effect, String description, boolean isOptional) {
+        this(effect, description, isOptional, SetTargetPointer.NONE);
+    }
+
+    public BecomesTappedAttachedTriggeredAbility(Effect effect, String description, boolean isOptional, SetTargetPointer setTargetPointer) {
         super(Zone.BATTLEFIELD, effect, isOptional);
         setTriggerPhrase(getWhen() + description + " becomes tapped, ");
+        this.setTargetPointer = setTargetPointer;
     }
 
     protected BecomesTappedAttachedTriggeredAbility(final BecomesTappedAttachedTriggeredAbility ability) {
         super(ability);
+        this.setTargetPointer = ability.setTargetPointer;
     }
 
     @Override
@@ -43,6 +53,17 @@ public class BecomesTappedAttachedTriggeredAbility extends TriggeredAbilityImpl 
             return false;
         }
         Permanent enchanted = game.getPermanent(enchantment.getAttachedTo());
-        return enchanted != null && event.getTargetId().equals(enchanted.getId());
+        if (enchanted == null || !event.getTargetId().equals(enchanted.getId())) {
+            return false;
+        }
+        switch (setTargetPointer) {
+            case PERMANENT:
+                getEffects().setTargetPointer(new FixedTarget(enchanted, game));
+            case NONE:
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported SetTargetPointer in BecomesTappedAttachedTriggeredAbility");
+        }
+        return true;
     }
 }
