882737ffc9e7aa75e15584080b3b2325a92f3651
Susucre <34709007+Susucre@users.noreply.github.com>
2025-07-18 17:55:20 +0200

    implement [EOE] Rig for War

--- Mage.Sets/src/mage/sets/EdgeOfEternities.java
+++ Mage.Sets/src/mage/sets/EdgeOfEternities.java
@@ -228,6 +228,7 @@ public final class EdgeOfEternities extends ExpansionSet {
         cards.add(new SetCardInfo("Requiem Monolith", 333, Rarity.RARE, mage.cards.r.RequiemMonolith.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Reroute Systems", 31, Rarity.UNCOMMON, mage.cards.r.RerouteSystems.class));
         cards.add(new SetCardInfo("Rescue Skiff", 32, Rarity.UNCOMMON, mage.cards.r.RescueSkiff.class));
+        cards.add(new SetCardInfo("Rig for War", 156, Rarity.COMMON, mage.cards.r.RigForWar.class));
         cards.add(new SetCardInfo("Roving Actuator", 157, Rarity.UNCOMMON, mage.cards.r.RovingActuator.class));
         cards.add(new SetCardInfo("Ruinous Rampage", 158, Rarity.UNCOMMON, mage.cards.r.RuinousRampage.class));
         cards.add(new SetCardInfo("Rust Harvester", 159, Rarity.RARE, mage.cards.r.RustHarvester.class, NON_FULL_USE_VARIOUS));
