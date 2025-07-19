2c91ec65b0ba5948b416c840e0354d882068ad1d
Steven Knipe <knipesteven@gmail.com>
2025-07-19 00:40:50 -0700

    Didn't fix enough, failure was still possible (though incredibly unlikely)

--- Mage.Tests/src/test/java/org/mage/test/cards/single/blb/AlaniaDivergentStormTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/blb/AlaniaDivergentStormTest.java
@@ -161,8 +161,8 @@ public class AlaniaDivergentStormTest extends CardTestPlayerBase {
         // Test that the "first sorcery you've cast this turn" clause works
         // Also copies an adventure sorcery
 
-        addCard(Zone.BATTLEFIELD, playerA, "Mountain", 5);
-        addCard(Zone.BATTLEFIELD, playerA, "Island", 5);
+        addCard(Zone.BATTLEFIELD, playerA, "Mountain", 10);
+        addCard(Zone.BATTLEFIELD, playerA, "Island", 10);
         addCard(Zone.BATTLEFIELD, playerA, "Plains", 10);
         addCard(Zone.HAND, playerA, "Faerie Guidemother"); // adventure card
         addCard(Zone.HAND, playerA, "Maximize Velocity");
