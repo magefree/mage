a0c58c829ad032f6b51dced0f0d850255219699b
theelk801 <theelk801@gmail.com>
2025-07-23 16:33:20 -0400

    [SPM] add set

---
+++ Mage.Sets/src/mage/sets/MarvelsSpiderMan.java
@@ -0,0 +1,22 @@
+package mage.sets;
+
+import mage.cards.ExpansionSet;
+import mage.constants.SetType;
+
+/**
+ * @author TheElk801
+ */
+public final class MarvelsSpiderMan extends ExpansionSet {
+
+    private static final MarvelsSpiderMan instance = new MarvelsSpiderMan();
+
+    public static MarvelsSpiderMan getInstance() {
+        return instance;
+    }
+
+    private MarvelsSpiderMan() {
+        super("Marvel's Spider-Man", "SPM", ExpansionSet.buildDate(2025, 9, 26), SetType.EXPANSION);
+        this.blockName = "Marvel's Spider-Man"; // for sorting in GUI
+        this.hasBasicLands = false; // temporary
+    }
+}
