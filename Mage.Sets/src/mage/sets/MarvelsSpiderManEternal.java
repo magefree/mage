3b9300b1ff1410a00997d3357175bd6af4068ee2
theelk801 <theelk801@gmail.com>
2025-07-24 09:03:25 -0400

    [SPE] Implement Venom Blast

--- Mage.Sets/src/mage/sets/MarvelsSpiderManEternal.java
+++ Mage.Sets/src/mage/sets/MarvelsSpiderManEternal.java
@@ -36,6 +36,7 @@ public final class MarvelsSpiderManEternal extends ExpansionSet {
         cards.add(new SetCardInfo("Spider-Man, Peter Parker", 4, Rarity.MYTHIC, mage.cards.s.SpiderManPeterParker.class));
         cards.add(new SetCardInfo("Symbiote Spawn", 11, Rarity.COMMON, mage.cards.s.SymbioteSpawn.class));
         cards.add(new SetCardInfo("Twisted Spider-Clone", 19, Rarity.COMMON, mage.cards.t.TwistedSpiderClone.class));
+        cards.add(new SetCardInfo("Venom Blast", 20, Rarity.RARE, mage.cards.v.VenomBlast.class));
         cards.add(new SetCardInfo("Venom, Deadly Devourer", 22, Rarity.RARE, mage.cards.v.VenomDeadlyDevourer.class));
     }
 }
