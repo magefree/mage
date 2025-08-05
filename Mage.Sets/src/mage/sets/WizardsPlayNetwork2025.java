fc90416bdeb39c78dfa3da3d9acf6d560d94d5aa
ReSech <resech@chesssorrel.us>
2025-08-05 12:31:07 +0900

    Update WizardsPlayNetwork2025.java
    Fix Zidane Rarity and add Spectacular Spider-Man

--- Mage.Sets/src/mage/sets/WizardsPlayNetwork2025.java
+++ Mage.Sets/src/mage/sets/WizardsPlayNetwork2025.java
@@ -24,7 +24,9 @@ public class WizardsPlayNetwork2025 extends ExpansionSet {
         cards.add(new SetCardInfo("Despark", 2, Rarity.UNCOMMON, mage.cards.d.Despark.class));
         cards.add(new SetCardInfo("Dragon's Hoard", "1p", Rarity.RARE, mage.cards.d.DragonsHoard.class, RETRO_ART));
         cards.add(new SetCardInfo("Dragonspeaker Shaman", 3, Rarity.RARE, mage.cards.d.DragonspeakerShaman.class));
+        cards.add(new SetCardInfo("Palladium Myr", 6, Rarity.RARE, mage.cards.p.PalladiumMyr.class, RETRO_ART));
         cards.add(new SetCardInfo("Rishkar's Expertise", 1, Rarity.RARE, mage.cards.r.RishkarsExpertise.class, RETRO_ART));
-        cards.add(new SetCardInfo("Zidane, Tantalus Thief", 5, Rarity.MYTHIC, mage.cards.z.ZidaneTantalusThief.class));
+        cards.add(new SetCardInfo("Spectacular Spider-Man", 7, Rarity.RARE, mage.cards.s.SpectacularSpiderMan.class));
+        cards.add(new SetCardInfo("Zidane, Tantalus Thief", 5, Rarity.RARE, mage.cards.z.ZidaneTantalusThief.class));
     }
 }
