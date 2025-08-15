e8c04a58960452ba104dc3deffadc781a393b666
theelk801 <theelk801@gmail.com>
2025-08-15 08:23:53 -0400

    [TLA] Implement Pillar Launch

---
+++ Mage.Sets/src/mage/cards/p/PillarLaunch.java
@@ -0,0 +1,37 @@
+package mage.cards.p;
+
+import mage.abilities.effects.common.UntapTargetEffect;
+import mage.abilities.effects.common.continuous.BoostTargetEffect;
+import mage.abilities.effects.common.continuous.GainAbilityTargetEffect;
+import mage.abilities.keyword.ReachAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.target.common.TargetCreaturePermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class PillarLaunch extends CardImpl {
+
+    public PillarLaunch(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{G}");
+
+        // Target creature gets +2/+2 and gains reach until end of turn. Untap it.
+        this.getSpellAbility().addEffect(new BoostTargetEffect(2, 2).setText("target creature gets +2/+2"));
+        this.getSpellAbility().addEffect(new GainAbilityTargetEffect(ReachAbility.getInstance()).setText("and gain reach until end of turn"));
+        this.getSpellAbility().addEffect(new UntapTargetEffect("Untap it"));
+        this.getSpellAbility().addTarget(new TargetCreaturePermanent());
+    }
+
+    private PillarLaunch(final PillarLaunch card) {
+        super(card);
+    }
+
+    @Override
+    public PillarLaunch copy() {
+        return new PillarLaunch(this);
+    }
+}
