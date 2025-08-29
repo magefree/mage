71cd3ce3fcf911d79abd74d977beaf11038cc7dd
jmlundeen <jmlundee@yahoo.com>
2025-08-29 15:48:12 -0500

    update gen-list-unimplemented-cards-for-set
    * remove periods from camel case name

--- Utils/gen-list-unimplemented-cards-for-set.pl
+++ Utils/gen-list-unimplemented-cards-for-set.pl
@@ -82,7 +82,7 @@ sub cardSort {
 sub toCamelCase {
     my $string = $_[0];
     $string =~ s/\b([\w']+)\b/ucfirst($1)/ge;
-    $string =~ s/[-,\s\'!@#*\(\)]//g;
+    $string =~ s/[-,\s\'\.!@#*\(\)]//g;
     $string;
 }
 
