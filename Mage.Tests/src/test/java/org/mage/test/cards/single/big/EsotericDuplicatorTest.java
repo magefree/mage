d13a0e6e6b213f826d5272e9a16468de96c20cc8
jmlundeen <jmlundee@yahoo.com>
2025-08-29 15:54:31 -0500

    create token copy checks for copyfrom
    * Esoteric Duplicator test demonstrates fix. Sculpting steel token was not coming back as an Esoteric Duplicator

--- Mage.Tests/src/test/java/org/mage/test/cards/single/big/EsotericDuplicatorTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/big/EsotericDuplicatorTest.java
@@ -2,7 +2,6 @@ package org.mage.test.cards.single.big;
 
 import mage.constants.PhaseStep;
 import mage.constants.Zone;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mage.test.serverside.base.CardTestPlayerBase;
 
@@ -21,7 +20,6 @@ public class EsotericDuplicatorTest extends CardTestPlayerBase {
     private static final String esotericDuplicator = "Esoteric Duplicator";
 
     @Test
-    @Ignore("Sculpting steel doesn't retain copy on entering battlefield")
     public void testEsotericDuplicator() {
         setStrictChooseMode(true);
 
