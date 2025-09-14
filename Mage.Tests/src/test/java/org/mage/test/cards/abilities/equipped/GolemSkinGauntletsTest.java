ff5e7f2d16894b1f99a14dd3cfb3248b388b9d1c
Steven Knipe <knipesteven@gmail.com>
2025-09-13 17:59:29 -0700

    Use AttachedToAttachedPredicate from 49a3b11 for GolemSkinGauntlets and BrassKnuckles, improve GolemSkinGauntletsTest

--- Mage.Tests/src/test/java/org/mage/test/cards/abilities/equipped/GolemSkinGauntletsTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/abilities/equipped/GolemSkinGauntletsTest.java
@@ -2,8 +2,6 @@ package org.mage.test.cards.abilities.equipped;
 
 import mage.constants.PhaseStep;
 import mage.constants.Zone;
-import mage.game.permanent.Permanent;
-import org.junit.Assert;
 import org.junit.Test;
 import org.mage.test.serverside.base.CardTestPlayerBase;
 
@@ -19,27 +17,24 @@ public class GolemSkinGauntletsTest extends CardTestPlayerBase {
     @Test
     public void testBoostOnEquip() {
         addCard(Zone.BATTLEFIELD, playerA, "Mountain", 6);
-        // Equipped creature doesn't untap during its controller's untap step.
-        // Equipped creature has "{T}: This creature deals 2 damage to any target."
-        // Equip {4)        
-        addCard(Zone.BATTLEFIELD, playerA, "Heavy Arbalest");
-        addCard(Zone.BATTLEFIELD, playerA, "Golem-Skin Gauntlets");
-        addCard(Zone.BATTLEFIELD, playerA, "Elite Vanguard");
+        addCard(Zone.BATTLEFIELD, playerA, "Heavy Arbalest"); // Equip {4}
+        addCard(Zone.BATTLEFIELD, playerA, "Golem-Skin Gauntlets"); // +1/+0 per attached equipment, Equip {2}
+        addCard(Zone.BATTLEFIELD, playerA, "Shuko"); // +1/+0, Equip {0}
+        addCard(Zone.BATTLEFIELD, playerA, "Elite Vanguard"); // Creature 2/1
 
         activateAbility(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Equip {4}", "Elite Vanguard");
         waitStackResolved(1, PhaseStep.PRECOMBAT_MAIN);
         activateAbility(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Equip {2}", "Elite Vanguard");
+        waitStackResolved(1, PhaseStep.PRECOMBAT_MAIN);
+        checkPT("Gauntlets equipped", 1, PhaseStep.PRECOMBAT_MAIN, playerA, "Elite Vanguard", 4, 1);
+
+        activateAbility(1, PhaseStep.PRECOMBAT_MAIN, playerA, "Equip {0}", "Elite Vanguard");
 
         setStopAt(1, PhaseStep.BEGIN_COMBAT);
+        setStrictChooseMode(true);
         execute();
 
-        assertLife(playerA, 20);
-        assertLife(playerB, 20);
-
-        Permanent eliteVanguard = getPermanent("Elite Vanguard", playerA.getId());
-        Assert.assertTrue(eliteVanguard.getAttachments().size() == 2);
-        Assert.assertEquals(4, eliteVanguard.getPower().getValue());
-        Assert.assertEquals(1, eliteVanguard.getToughness().getValue());
+        assertPowerToughness(playerA, "Elite Vanguard", 6, 1);
     }
 
 }
