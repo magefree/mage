809cc25c3a98bc98bcd3206dc64211c81a15e80f
Oscar Scull <oscscull@gmail.com>
2025-10-14 20:49:59 +0900

    add comment

--- Mage/src/main/java/mage/cards/RoomCardHalfImpl.java
+++ Mage/src/main/java/mage/cards/RoomCardHalfImpl.java
@@ -47,6 +47,14 @@ public class RoomCardHalfImpl extends SplitCardHalfImpl implements RoomCardHalf 
         return super.cast(game, fromZone, ability, controllerId);
     }
 
+    /**
+     * A room half is used for the spell half on the stack, similar to a normal split card.
+     * On the stack, it has only one name, mana cost, etc.
+     * However, in the hand and on the battlefield, it is the full card, which is the parent of the half.
+     * This code helps to ensure that the parent, and not the halves, are the only part of the card active on the battlefield.
+     * This is important for example when that half has a triggered ability etc that otherwise might trigger twice (once for the parent, once for the half)
+     * - in the case that the half was an object on the battlefield. In all other cases, they should all move together.
+     */
     @Override
     public void setZone(Zone zone, Game game) {
         if (zone == Zone.BATTLEFIELD) {
