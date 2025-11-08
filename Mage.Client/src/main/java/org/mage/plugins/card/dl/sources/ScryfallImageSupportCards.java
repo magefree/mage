7e34363954dd3c4ef20e32c6cd103131e10a7f98
ReSech <resech@chesssorrel.us>
2025-11-09 06:58:25 +1100

    Move cards from PMEI to LMAR and add missing PURL Reprints (#14008)
    * Add new PURL reprints
    
    * Move LMAR cards to dedicated set, remove from PMEI

--- Mage.Client/src/main/java/org/mage/plugins/card/dl/sources/ScryfallImageSupportCards.java
+++ Mage.Client/src/main/java/org/mage/plugins/card/dl/sources/ScryfallImageSupportCards.java
@@ -611,6 +611,7 @@ public class ScryfallImageSupportCards {
             add("EOS"); // Edge of Eternities: Stellar Sights
             add("SPM"); // Marvel's Spider-Man
             add("SPE"); // Marvel's Spider-Man Eternal
+            add("LMAR"); // Marvel Legends Series Inserts
             add("MAR"); // Marvel Universe
             add("TLA"); // Avatar: The Last Airbender
             add("TLE"); // Avatar: The Last Airbender Eternal
