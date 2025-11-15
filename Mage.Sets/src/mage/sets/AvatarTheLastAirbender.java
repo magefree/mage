886dd1f0b2ae05549dbe6e1e0027a56f5943d56f
theelk801 <theelk801@gmail.com>
2025-11-15 10:24:28 -0500

    [TLA] Implement Lost Days

--- Mage.Sets/src/mage/sets/AvatarTheLastAirbender.java
+++ Mage.Sets/src/mage/sets/AvatarTheLastAirbender.java
@@ -230,6 +230,7 @@ public final class AvatarTheLastAirbender extends ExpansionSet {
         cards.add(new SetCardInfo("Lightning Strike", 146, Rarity.COMMON, mage.cards.l.LightningStrike.class));
         cards.add(new SetCardInfo("Lo and Li, Twin Tutors", 108, Rarity.UNCOMMON, mage.cards.l.LoAndLiTwinTutors.class));
         cards.add(new SetCardInfo("Long Feng, Grand Secretariat", 233, Rarity.UNCOMMON, mage.cards.l.LongFengGrandSecretariat.class));
+        cards.add(new SetCardInfo("Lost Days", 62, Rarity.COMMON, mage.cards.l.LostDays.class));
         cards.add(new SetCardInfo("Mai, Jaded Edge", 147, Rarity.UNCOMMON, mage.cards.m.MaiJadedEdge.class));
         cards.add(new SetCardInfo("Mai, Scornful Striker", 109, Rarity.RARE, mage.cards.m.MaiScornfulStriker.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Mai, Scornful Striker", 374, Rarity.RARE, mage.cards.m.MaiScornfulStriker.class, NON_FULL_USE_VARIOUS));
--- Mage/src/main/java/mage/abilities/effects/common/PutOnTopOrBottomLibraryTargetEffect.java
+++ Mage/src/main/java/mage/abilities/effects/common/PutOnTopOrBottomLibraryTargetEffect.java
@@ -6,21 +6,29 @@ import mage.abilities.effects.OneShotEffect;
 import mage.constants.Outcome;
 import mage.game.Game;
 import mage.players.Player;
+import mage.util.CardUtil;
 
 /**
  * @author TheElk801
  */
 public class PutOnTopOrBottomLibraryTargetEffect extends OneShotEffect {
 
+    private final int position;
     private final boolean textOwnerOf;
 
     public PutOnTopOrBottomLibraryTargetEffect(boolean textOwnerOf) {
+        this(1, textOwnerOf);
+    }
+
+    public PutOnTopOrBottomLibraryTargetEffect(int position, boolean textOwnerOf) {
         super(Outcome.ReturnToHand);
+        this.position = position;
         this.textOwnerOf = textOwnerOf;
     }
 
     private PutOnTopOrBottomLibraryTargetEffect(final PutOnTopOrBottomLibraryTargetEffect effect) {
         super(effect);
+        this.position = effect.position;
         this.textOwnerOf = effect.textOwnerOf;
     }
 
@@ -35,10 +43,14 @@ public class PutOnTopOrBottomLibraryTargetEffect extends OneShotEffect {
         if (player == null) {
             return false;
         }
+        String message = position > 1 ? "into your library " + CardUtil.numberToOrdinalText(position) + " from the top or on the" : "on the top or";
         boolean onTop = player.chooseUse(
-                Outcome.Detriment, "Put the targeted object on the top or bottom of your library?",
+                Outcome.Detriment, "Put the targeted object " + message + " bottom of your library?",
                 null, "Top", "Bottom", source, game
         );
+        if (onTop && position > 1) {
+            return new PutIntoLibraryNFromTopTargetEffect(position).apply(game, source);
+        }
         return new PutOnLibraryTargetEffect(onTop).apply(game, source);
     }
 
@@ -47,8 +59,23 @@ public class PutOnTopOrBottomLibraryTargetEffect extends OneShotEffect {
         if (staticText != null && !staticText.isEmpty()) {
             return staticText;
         }
+        StringBuilder sb = new StringBuilder();
         String targetText = getTargetPointer().describeTargets(mode.getTargets(), "that permanent");
-        return (textOwnerOf ? "the owner of " + targetText : targetText + "'s owner") +
-                " puts it on their choice of the top or bottom of their library";
+        if (textOwnerOf) {
+            sb.append("the owner of ");
+            sb.append(targetText);
+        } else {
+            sb.append(targetText);
+            sb.append("'s owner");
+        }
+        sb.append(" puts it");
+        if (position > 1) {
+            sb.append("into their library ");
+            sb.append(CardUtil.numberToOrdinalText(position));
+            sb.append(" from the top or on the bottom");
+        } else {
+            sb.append("on their choice of the top or bottom of their library");
+        }
+        return sb.toString();
     }
 }
