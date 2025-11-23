d744b347f2e012670da58a1f90e186487f9383dc
xenohedron <12538125+xenohedron@users.noreply.github.com>
2025-11-22 20:39:55 -0500

    fix Bitterbloom Bearer token

--- Mage/src/main/java/mage/game/permanent/token/FaerieBlueBlackToken.java
+++ Mage/src/main/java/mage/game/permanent/token/FaerieBlueBlackToken.java
@@ -6,17 +6,16 @@ import mage.constants.CardType;
 import mage.constants.SubType;
 
 /**
- * @author spjspj
+ * @author TheElk801
  */
 public final class FaerieBlueBlackToken extends TokenImpl {
 
     public FaerieBlueBlackToken() {
-        super("Faerie Rogue Token", "1/1 blue and black Faerie Rogue creature token with flying");
+        super("Faerie Token", "1/1 blue and black Faerie creature token with flying");
         cardType.add(CardType.CREATURE);
         color.setBlue(true);
         color.setBlack(true);
         subtype.add(SubType.FAERIE);
-        subtype.add(SubType.ROGUE);
         power = new MageInt(1);
         toughness = new MageInt(1);
         this.addAbility(FlyingAbility.getInstance());
