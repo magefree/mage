a4f061f9a9ab4b410e0645620629dd6c7185656a
Muz Ali <git@mustaqila.li>
2025-12-30 09:28:23 -0600

    Rename NoFlyingSpiritWhiteToken to NoFlyingSpiritWhite11Token in anticipation of adding missing TDM tokens

--- Mage/src/main/java/mage/game/permanent/token/NoFlyingSpiritWhite11Token.java
+++ Mage/src/main/java/mage/game/permanent/token/NoFlyingSpiritWhite11Token.java
@@ -7,9 +7,9 @@ import mage.constants.SubType;
 /**
  * @author PurpleCrowbar
  */
-public final class NoFlyingSpiritWhiteToken extends TokenImpl {
+public final class NoFlyingSpiritWhite11Token extends TokenImpl {
 
-    public NoFlyingSpiritWhiteToken() {
+    public NoFlyingSpiritWhite11Token() {
         super("Spirit Token", "1/1 white Spirit creature token");
         cardType.add(CardType.CREATURE);
         subtype.add(SubType.SPIRIT);
@@ -18,12 +18,12 @@ public final class NoFlyingSpiritWhiteToken extends TokenImpl {
         toughness = new MageInt(1);
     }
 
-    private NoFlyingSpiritWhiteToken(final NoFlyingSpiritWhiteToken token) {
+    private NoFlyingSpiritWhite11Token(final NoFlyingSpiritWhite11Token token) {
         super(token);
     }
 
     @Override
-    public NoFlyingSpiritWhiteToken copy() {
-        return new NoFlyingSpiritWhiteToken(this);
+    public NoFlyingSpiritWhite11Token copy() {
+        return new NoFlyingSpiritWhite11Token(this);
     }
 }
