1141e4c8e174a0f82c6edd317f469c03e24b409c
Muz Ali <git@mustaqila.li>
2026-01-05 21:55:41 -0600

    [ECL] Add Virulent Emissary

---
+++ Mage.Sets/src/mage/cards/v/VirulentEmissary.java
@@ -0,0 +1,50 @@
+
+package mage.cards.v;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.abilities.common.EntersBattlefieldControlledTriggeredAbility;
+import mage.abilities.effects.common.GainLifeEffect;
+import mage.abilities.keyword.DeathtouchAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.filter.common.FilterCreaturePermanent;
+import mage.filter.predicate.mageobject.AnotherPredicate;
+
+/**
+ *
+ * @author muz
+ */
+public final class VirulentEmissary extends CardImpl {
+
+    private static final FilterCreaturePermanent filter = new FilterCreaturePermanent("another creature");
+    static {
+        filter.add(AnotherPredicate.instance);
+    }
+
+    public VirulentEmissary(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId,setInfo,new CardType[]{CardType.CREATURE},"{G}");
+        this.subtype.add(SubType.ELF);
+        this.subtype.add(SubType.ASSASSIN);
+
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(1);
+
+        // Deathtouch
+        this.addAbility(DeathtouchAbility.getInstance());
+
+        // Whenever another creature you control enters, you gain 1 life.
+        this.addAbility(new EntersBattlefieldControlledTriggeredAbility(new GainLifeEffect(1), filter));
+    }
+
+    private VirulentEmissary(final VirulentEmissary card) {
+        super(card);
+    }
+
+    @Override
+    public VirulentEmissary copy() {
+        return new VirulentEmissary(this);
+    }
+}
