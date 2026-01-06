34c44fd7a60b131f6933090b6cb46a83770d0a90
theelk801 <theelk801@gmail.com>
2026-01-06 09:55:22 -0500

    [ECL] Implement Moonglove Extractor

---
+++ Mage.Sets/src/mage/cards/m/MoongloveExtractor.java
@@ -0,0 +1,42 @@
+package mage.cards.m;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.AttacksTriggeredAbility;
+import mage.abilities.effects.common.DrawCardSourceControllerEffect;
+import mage.abilities.effects.common.LoseLifeSourceControllerEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class MoongloveExtractor extends CardImpl {
+
+    public MoongloveExtractor(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{2}{B}");
+
+        this.subtype.add(SubType.ELF);
+        this.subtype.add(SubType.WARLOCK);
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(1);
+
+        // Whenever this creature attacks, you draw a card and lose 1 life.
+        Ability ability = new AttacksTriggeredAbility(new DrawCardSourceControllerEffect(1));
+        ability.addEffect(new LoseLifeSourceControllerEffect(1).setText("and lose 1 life"));
+        this.addAbility(ability);
+    }
+
+    private MoongloveExtractor(final MoongloveExtractor card) {
+        super(card);
+    }
+
+    @Override
+    public MoongloveExtractor copy() {
+        return new MoongloveExtractor(this);
+    }
+}
--- Mage.Sets/src/mage/sets/LorwynEclipsed.java
+++ Mage.Sets/src/mage/sets/LorwynEclipsed.java
@@ -76,6 +76,7 @@ public final class LorwynEclipsed extends ExpansionSet {
         cards.add(new SetCardInfo("Kirol, Attentive First-Year", 231, Rarity.RARE, mage.cards.k.KirolAttentiveFirstYear.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Kirol, Attentive First-Year", 374, Rarity.RARE, mage.cards.k.KirolAttentiveFirstYear.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Lys Alana Informant", 181, Rarity.COMMON, mage.cards.l.LysAlanaInformant.class));
+        cards.add(new SetCardInfo("Moonglove Extractor", 109, Rarity.COMMON, mage.cards.m.MoongloveExtractor.class));
         cards.add(new SetCardInfo("Morningtide's Light", 27, Rarity.MYTHIC, mage.cards.m.MorningtidesLight.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Morningtide's Light", 301, Rarity.MYTHIC, mage.cards.m.MorningtidesLight.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Mountain", 272, Rarity.LAND, mage.cards.basiclands.Mountain.class, NON_FULL_USE_VARIOUS));
