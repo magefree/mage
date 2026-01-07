2b07b6ab6f6138ddfb40b65f17589931396eb738
theelk801 <theelk801@gmail.com>
2026-01-07 09:19:17 -0500

    [ECL] Implement Heirloom Auntie

---
+++ Mage.Sets/src/mage/cards/h/HeirloomAuntie.java
@@ -0,0 +1,55 @@
+package mage.cards.h;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.DiesCreatureTriggeredAbility;
+import mage.abilities.common.EntersBattlefieldAbility;
+import mage.abilities.effects.common.counter.AddCountersSourceEffect;
+import mage.abilities.effects.common.counter.RemoveCounterSourceEffect;
+import mage.abilities.effects.keyword.SurveilEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.counters.CounterType;
+import mage.filter.StaticFilters;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class HeirloomAuntie extends CardImpl {
+
+    public HeirloomAuntie(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{2}{B}");
+
+        this.subtype.add(SubType.GOBLIN);
+        this.subtype.add(SubType.WARLOCK);
+        this.power = new MageInt(4);
+        this.toughness = new MageInt(4);
+
+        // This creature enters with two -1/-1 counters on it.
+        this.addAbility(new EntersBattlefieldAbility(
+                new AddCountersSourceEffect(CounterType.M1M1.createInstance(2)),
+                "with two -1/-1 counters on it"
+        ));
+
+        // Whenever another creature you control dies, surveil 1, then remove a -1/-1 counter from this creature.
+        Ability ability = new DiesCreatureTriggeredAbility(
+                new SurveilEffect(1), false,
+                StaticFilters.FILTER_ANOTHER_CREATURE_YOU_CONTROL
+        );
+        ability.addEffect(new RemoveCounterSourceEffect(CounterType.M1M1.createInstance()).concatBy(", then"));
+        this.addAbility(ability);
+    }
+
+    private HeirloomAuntie(final HeirloomAuntie card) {
+        super(card);
+    }
+
+    @Override
+    public HeirloomAuntie copy() {
+        return new HeirloomAuntie(this);
+    }
+}
