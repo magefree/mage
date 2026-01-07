8a95a91c19aec00316609dd0e0ae15c78413c988
Muz Ali <git@mustaqila.li>
2026-01-07 09:12:10 -0600

    [ECL] Implement Hovel Hurler

--- Mage.Sets/src/mage/cards/h/HovelHurler.java
+++ Mage.Sets/src/mage/cards/h/HovelHurler.java
@@ -1,4 +1,4 @@
-package mage.cards.b;
+package mage.cards.h;
 
 import java.util.UUID;
 import mage.MageInt;
@@ -7,12 +7,14 @@ import mage.abilities.common.ActivateAsSorceryActivatedAbility;
 import mage.abilities.common.EntersBattlefieldAbility;
 import mage.abilities.costs.common.RemoveCountersSourceCost;
 import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.continuous.BoostTargetEffect;
 import mage.abilities.effects.common.continuous.GainAbilityTargetEffect;
 import mage.abilities.effects.common.counter.AddCountersSourceEffect;
-import mage.abilities.keyword.IndestructibleAbility;
+import mage.abilities.keyword.FlyingAbility;
 import mage.constants.SubType;
 import mage.counters.CounterType;
-import mage.target.common.TargetCreaturePermanent;
+import mage.filter.StaticFilters;
+import mage.target.TargetPermanent;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
@@ -21,34 +23,35 @@ import mage.constants.CardType;
  *
  * @author muz
  */
-public final class BurdenedStoneback extends CardImpl {
+public final class HovelHurler extends CardImpl {
 
-    public BurdenedStoneback(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{1}{W}");
+    public HovelHurler(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{3}");
 
         this.subtype.add(SubType.GIANT);
         this.subtype.add(SubType.WARRIOR);
-        this.power = new MageInt(4);
-        this.toughness = new MageInt(4);
+        this.power = new MageInt(6);
+        this.toughness = new MageInt(7);
 
         // This creature enters with two -1/-1 counters on it.
         this.addAbility(new EntersBattlefieldAbility(new AddCountersSourceEffect(CounterType.M1M1.createInstance(2)),
             null, "This creature enters with two -1/-1 counters on it.", "")
         );
 
-        // {1}{W}, Remove a counter from this creature: Target creature gains indestructible until end of turn. Activate only as a sorcery.
-        Ability ability = new ActivateAsSorceryActivatedAbility(new GainAbilityTargetEffect(IndestructibleAbility.getInstance()), new ManaCostsImpl<>("{1}{W}"));
+        // {R/W}{R/W}, Remove a counter from this creature: Another target creature you control gets +1/+0 and gains flying until end of turn. Activate only as a sorcery.
+        Ability ability = new ActivateAsSorceryActivatedAbility(new GainAbilityTargetEffect(FlyingAbility.getInstance()), new ManaCostsImpl<>("{R/W}{R/W}"));
+        ability.addEffect(new BoostTargetEffect(1, 0));
         ability.addCost(new RemoveCountersSourceCost(1));
-        ability.addTarget(new TargetCreaturePermanent());
+        ability.addTarget(new TargetPermanent(StaticFilters.FILTER_ANOTHER_CREATURE_YOU_CONTROL));
         this.addAbility(ability);
     }
 
-    private BurdenedStoneback(final BurdenedStoneback card) {
+    private HovelHurler(final HovelHurler card) {
         super(card);
     }
 
     @Override
-    public BurdenedStoneback copy() {
-        return new BurdenedStoneback(this);
+    public HovelHurler copy() {
+        return new HovelHurler(this);
     }
 }
