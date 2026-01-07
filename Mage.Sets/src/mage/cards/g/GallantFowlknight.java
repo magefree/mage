929027d0c65911ba8e826931091565e342af1186
Muz <muz@users.noreply.github.com>
2026-01-07 08:37:44 -0600

    [ECL] Implement Gallant Fowlknight (#14181)
    * [ECL] Implement Gallant Fowlknight
    
    * Remove redundant setText on BoostControlledEffect
    
    * Move Kithkin creature filter to be static

---
+++ Mage.Sets/src/mage/cards/g/GallantFowlknight.java
@@ -0,0 +1,51 @@
+package mage.cards.g;
+
+import java.util.UUID;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.EntersBattlefieldTriggeredAbility;
+import mage.abilities.effects.common.continuous.BoostControlledEffect;
+import mage.abilities.effects.common.continuous.GainAbilityControlledEffect;
+import mage.abilities.keyword.FirstStrikeAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.SubType;
+import mage.filter.common.FilterCreaturePermanent;
+
+/**
+ *
+ * @author muz
+ */
+public final class GallantFowlknight extends CardImpl {
+
+    private static final FilterCreaturePermanent filter = new FilterCreaturePermanent(SubType.KITHKIN, "Kithkin creatures");
+
+    public GallantFowlknight(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{3}{W}");
+
+        this.subtype.add(SubType.KITHKIN);
+        this.subtype.add(SubType.KNIGHT);
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(4);
+
+        // When this creature enters, creatures you control get +1/+0 until end of turn. Kithkin creatures you control also gain first strike until end of turn.
+        Ability ability = new EntersBattlefieldTriggeredAbility(new BoostControlledEffect(1, 0, Duration.EndOfTurn));
+        ability.addEffect(new GainAbilityControlledEffect(
+            FirstStrikeAbility.getInstance(), Duration.EndOfTurn,
+            filter
+        ).setText("Kithkin creatures you control also gain first strike until end of turn"));
+        this.addAbility(ability);
+    }
+
+    private GallantFowlknight(final GallantFowlknight card) {
+        super(card);
+    }
+
+    @Override
+    public GallantFowlknight copy() {
+        return new GallantFowlknight(this);
+    }
+}
--- Mage.Sets/src/mage/sets/LorwynEclipsed.java
+++ Mage.Sets/src/mage/sets/LorwynEclipsed.java
@@ -80,6 +80,7 @@ public final class LorwynEclipsed extends ExpansionSet {
         cards.add(new SetCardInfo("Forest", 283, Rarity.LAND, mage.cards.basiclands.Forest.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Formidable Speaker", 176, Rarity.RARE, mage.cards.f.FormidableSpeaker.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Formidable Speaker", 366, Rarity.RARE, mage.cards.f.FormidableSpeaker.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Gallant Fowlknight", 17, Rarity.COMMON, mage.cards.g.GallantFowlknight.class));
         cards.add(new SetCardInfo("Gangly Stompling", 226, Rarity.COMMON, mage.cards.g.GanglyStompling.class));
         cards.add(new SetCardInfo("Goatnap", 142, Rarity.UNCOMMON, mage.cards.g.Goatnap.class));
         cards.add(new SetCardInfo("Great Forest Druid", 178, Rarity.COMMON, mage.cards.g.GreatForestDruid.class));
