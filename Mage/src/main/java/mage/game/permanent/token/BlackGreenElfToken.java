a2145e7e1de62ef0da36b1b8557cb190a1446e82
Muz Ali <git@mustaqila.li>
2026-01-08 17:05:02 -0600

    [ECL] Implement Unforgiving Aim

---
+++ Mage/src/main/java/mage/game/permanent/token/BlackGreenElfToken.java
@@ -0,0 +1,29 @@
+package mage.game.permanent.token;
+
+import mage.MageInt;
+import mage.constants.CardType;
+import mage.constants.SubType;
+
+/**
+ * @author muz
+ */
+public final class BlackGreenElfToken extends TokenImpl {
+
+    public BlackGreenElfToken() {
+        super("Elf Token", "2/2 black and green Elf creature token");
+        this.cardType.add(CardType.CREATURE);
+        this.color.setGreen(true);
+        this.subtype.add(SubType.ELF);
+
+        this.power = new MageInt(2);
+        this.toughness = new MageInt(2);
+    }
+
+    private BlackGreenElfToken(final BlackGreenElfToken token) {
+        super(token);
+    }
+
+    public BlackGreenElfToken copy() {
+        return new BlackGreenElfToken(this);
+    }
+}
