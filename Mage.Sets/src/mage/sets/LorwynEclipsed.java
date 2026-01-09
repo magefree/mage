c771736acc8456770860d729b9c6d95cf8d87720
theelk801 <theelk801@gmail.com>
2026-01-09 17:37:17 -0500

    [ECL] Implement Thirst for Identity

--- Mage.Sets/src/mage/sets/LorwynEclipsed.java
+++ Mage.Sets/src/mage/sets/LorwynEclipsed.java
@@ -274,6 +274,7 @@ public final class LorwynEclipsed extends ExpansionSet {
         cards.add(new SetCardInfo("Temple Garden", 351, Rarity.RARE, mage.cards.t.TempleGarden.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Temporal Cleansing", 78, Rarity.COMMON, mage.cards.t.TemporalCleansing.class));
         cards.add(new SetCardInfo("Tend the Sprigs", 197, Rarity.COMMON, mage.cards.t.TendTheSprigs.class));
+        cards.add(new SetCardInfo("Thirst for Identity", 79, Rarity.UNCOMMON, mage.cards.t.ThirstForIdentity.class));
         cards.add(new SetCardInfo("Thoughtweft Charge", 198, Rarity.UNCOMMON, mage.cards.t.ThoughtweftCharge.class));
         cards.add(new SetCardInfo("Thoughtweft Imbuer", 38, Rarity.UNCOMMON, mage.cards.t.ThoughtweftImbuer.class));
         cards.add(new SetCardInfo("Thoughtweft Lieutenant", 246, Rarity.UNCOMMON, mage.cards.t.ThoughtweftLieutenant.class, NON_FULL_USE_VARIOUS));
