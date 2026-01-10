d61dc0bb63ce5d1f3edb90daf5faad480394de5b
Muz <muz@users.noreply.github.com>
2026-01-10 16:14:43 -0600

    [ECL] Implement Rimefire Torque (#14228)

---
+++ Mage.Sets/src/mage/cards/r/RimefireTorque.java
@@ -0,0 +1,68 @@
+package mage.cards.r;
+
+import java.util.UUID;
+
+import mage.abilities.common.AsEntersBattlefieldAbility;
+import mage.abilities.common.EntersBattlefieldAllTriggeredAbility;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.common.delayed.CopyNextSpellDelayedTriggeredAbility;
+import mage.abilities.costs.common.RemoveCountersSourceCost;
+import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.effects.common.ChooseCreatureTypeEffect;
+import mage.abilities.effects.common.CreateDelayedTriggeredAbilityEffect;
+import mage.abilities.effects.common.counter.AddCountersSourceEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Outcome;
+import mage.constants.Zone;
+import mage.counters.CounterType;
+import mage.filter.common.FilterControlledPermanent;
+import mage.filter.predicate.mageobject.ChosenSubtypePredicate;
+
+/**
+ *
+ * @author muz
+ */
+public final class RimefireTorque extends CardImpl {
+
+    private static final FilterControlledPermanent filter = new FilterControlledPermanent("a permanent you control of the chosen type");
+
+    static {
+        filter.add(ChosenSubtypePredicate.TRUE);
+    }
+
+    public RimefireTorque(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT}, "{1}{U}");
+
+        // As this artifact enters, choose a creature type.
+        this.addAbility(new AsEntersBattlefieldAbility(new ChooseCreatureTypeEffect(Outcome.Benefit)));
+
+        // Whenever a permanent you control of the chosen type enters, put a charge counter on this artifact.
+        this.addAbility(
+            new EntersBattlefieldAllTriggeredAbility(
+                Zone.BATTLEFIELD,
+                new AddCountersSourceEffect(CounterType.CHARGE.createInstance()),
+                filter,
+                false
+            )
+        );
+
+        // {T}, Remove three charge counters from this artifact: When you next cast an instant or sorcery spell this turn, copy it. You may choose new targets for the copy.
+        SimpleActivatedAbility ability = new SimpleActivatedAbility(
+            new CreateDelayedTriggeredAbilityEffect(new CopyNextSpellDelayedTriggeredAbility()),
+            new RemoveCountersSourceCost(CounterType.CHARGE.createInstance(3))
+        );
+        ability.addCost(new TapSourceCost());
+        this.addAbility(ability);
+    }
+
+    private RimefireTorque(final RimefireTorque card) {
+        super(card);
+    }
+
+    @Override
+    public RimefireTorque copy() {
+        return new RimefireTorque(this);
+    }
+}
--- Mage.Sets/src/mage/sets/LorwynEclipsed.java
+++ Mage.Sets/src/mage/sets/LorwynEclipsed.java
@@ -255,6 +255,8 @@ public final class LorwynEclipsed extends ExpansionSet {
         cards.add(new SetCardInfo("Requiting Hex", 116, Rarity.UNCOMMON, mage.cards.r.RequitingHex.class));
         cards.add(new SetCardInfo("Rhys, the Evermore", 32, Rarity.RARE, mage.cards.r.RhysTheEvermore.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Rhys, the Evermore", 354, Rarity.RARE, mage.cards.r.RhysTheEvermore.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Rimefire Torque", 358, Rarity.RARE, mage.cards.r.RimefireTorque.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Rimefire Torque", 65, Rarity.RARE, mage.cards.r.RimefireTorque.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Rimekin Recluse", 66, Rarity.UNCOMMON, mage.cards.r.RimekinRecluse.class));
         cards.add(new SetCardInfo("Riverguard's Reflexes", 33, Rarity.COMMON, mage.cards.r.RiverguardsReflexes.class));
         cards.add(new SetCardInfo("Rooftop Percher", 2, Rarity.COMMON, mage.cards.r.RooftopPercher.class));
