3c51b0149934e82673ce65dcafdfeab613582521
theelk801 <theelk801@gmail.com>
2026-01-19 14:28:36 -0500

    fix build error

--- Mage.Sets/src/mage/cards/w/WeatherMaker.java
+++ Mage.Sets/src/mage/cards/w/WeatherMaker.java
@@ -1,17 +1,25 @@
 package mage.cards.w;
 
-import java.util.UUID;
-
+import mage.Mana;
+import mage.abilities.Ability;
 import mage.abilities.common.LandfallAbility;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.RemoveCountersSourceCost;
+import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.effects.common.DamageTargetEffect;
 import mage.abilities.effects.common.counter.AddCountersSourceEffect;
 import mage.abilities.mana.AnyColorManaAbility;
+import mage.abilities.mana.SimpleManaAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.constants.Zone;
 import mage.counters.CounterType;
+import mage.target.common.TargetAnyTarget;
+
+import java.util.UUID;
 
 /**
- *
  * @author muz
  */
 public final class WeatherMaker extends CardImpl {
