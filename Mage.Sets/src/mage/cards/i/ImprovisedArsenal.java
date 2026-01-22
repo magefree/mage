87b6e834a511c0055e0de8a0ad5c407a13ef4f4a
Muz Ali <git@mustaqila.li>
2026-01-22 16:31:41 -0600

    Use ArtifactYouControlCount over PermanentsOnBattlefieldCount

--- Mage.Sets/src/mage/cards/i/ImprovisedArsenal.java
+++ Mage.Sets/src/mage/cards/i/ImprovisedArsenal.java
@@ -2,12 +2,10 @@ package mage.cards.i;
 
 import java.util.UUID;
 import mage.constants.SubType;
-import mage.filter.common.FilterControlledArtifactPermanent;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.costs.mana.ManaCostsImpl;
-import mage.abilities.dynamicvalue.DynamicValue;
-import mage.abilities.dynamicvalue.common.PermanentsOnBattlefieldCount;
+import mage.abilities.dynamicvalue.common.ArtifactYouControlCount;
 import mage.abilities.dynamicvalue.common.StaticValue;
 import mage.abilities.effects.CreateTokenCopySourceEffect;
 import mage.abilities.effects.common.continuous.BoostEquippedEffect;
@@ -24,8 +22,6 @@ import mage.constants.Outcome;
  */
 public final class ImprovisedArsenal extends CardImpl {
 
-    private static final DynamicValue count = new PermanentsOnBattlefieldCount(new FilterControlledArtifactPermanent());
-
     public ImprovisedArsenal(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT}, "{1}{R}");
 
@@ -33,7 +29,7 @@ public final class ImprovisedArsenal extends CardImpl {
 
         // Equipped creature gets +1/+0 for each artifact you control.
         this.addAbility(new SimpleStaticAbility(
-            new BoostEquippedEffect(count, StaticValue.get(0))
+            new BoostEquippedEffect(ArtifactYouControlCount.instance, StaticValue.get(0))
         ).addHint(ArtifactYouControlHint.instance));
 
         // {4}{R}: Create a token that's a copy of this Equipment.
