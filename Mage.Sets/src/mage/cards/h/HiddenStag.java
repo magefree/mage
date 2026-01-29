28f9047b96ee30e7b5c77188bc0e1a571e1c9f59
Muz Ali <git@mustaqila.li>
2026-01-29 11:19:53 -0600

    Refactor to use CreatureToken: Hidden Stag

--- Mage.Sets/src/mage/cards/h/HiddenStag.java
+++ Mage.Sets/src/mage/cards/h/HiddenStag.java
@@ -1,6 +1,5 @@
 package mage.cards.h;
 
-import mage.MageInt;
 import mage.abilities.common.ControllerPlaysLandTriggeredAbility;
 import mage.abilities.common.OpponentPlaysLandTriggeredAbility;
 import mage.abilities.condition.Condition;
@@ -15,7 +14,6 @@ import mage.constants.Duration;
 import mage.constants.SubType;
 import mage.constants.Zone;
 import mage.filter.StaticFilters;
-import mage.game.permanent.token.TokenImpl;
 import mage.game.permanent.token.custom.CreatureToken;
 
 import java.util.UUID;
@@ -26,7 +24,7 @@ import java.util.UUID;
 public final class HiddenStag extends CardImpl {
 
     private static final Condition condition = new SourceMatchesFilterCondition(
-            "this permanent is a creature", StaticFilters.FILTER_PERMANENT_CREATURE
+        "this permanent is a creature", StaticFilters.FILTER_PERMANENT_CREATURE
     );
 
     public HiddenStag(UUID ownerId, CardSetInfo setInfo) {
@@ -34,14 +32,14 @@ public final class HiddenStag extends CardImpl {
 
         // Whenever an opponent plays a land, if Hidden Stag is an enchantment, Hidden Stag becomes a 3/2 Elk Beast creature.
         this.addAbility(new OpponentPlaysLandTriggeredAbility(new BecomesCreatureSourceEffect(
-                new CreatureToken(
-                        3, 2, "3/2 Elk Beast creature", SubType.ELK, SubType.BEAST
-                ), null, Duration.WhileOnBattlefield
+            new CreatureToken(
+                3, 2, "3/2 Elk Beast creature", SubType.ELK, SubType.BEAST
+            ), null, Duration.WhileOnBattlefield
         ), false).withInterveningIf(SourceIsEnchantmentCondition.instance).withRuleTextReplacement(true));
 
         // Whenever you play a land, if Hidden Stag is a creature, Hidden Stag becomes an enchantment.
         this.addAbility(new ControllerPlaysLandTriggeredAbility(
-                Zone.BATTLEFIELD, new BecomesEnchantmentSourceEffect(), false
+            Zone.BATTLEFIELD, new BecomesEnchantmentSourceEffect(), false
         ).withInterveningIf(condition).withRuleTextReplacement(true));
     }
 
@@ -52,25 +50,5 @@ public final class HiddenStag extends CardImpl {
     @Override
     public HiddenStag copy() {
         return new HiddenStag(this);
-    }
-}
-
-class ElkBeastToken extends TokenImpl {
-
-    public ElkBeastToken() {
-        super("Elk Beast", "3/2 Elk Beast creature");
-        cardType.add(CardType.CREATURE);
-        subtype.add(SubType.ELK);
-        subtype.add(SubType.BEAST);
-        power = new MageInt(3);
-        toughness = new MageInt(2);
-    }
-
-    private ElkBeastToken(final ElkBeastToken token) {
-        super(token);
-    }
-
-    public ElkBeastToken copy() {
-        return new ElkBeastToken(this);
     }
 }
