9d8890538bfe97d8a7ed7a0ece7e0376b7411c4f
Muz Ali <git@mustaqila.li>
2026-01-31 16:34:08 -0600

    Refactor private GruulKeyruneToken to CreatureToken

--- Mage.Sets/src/mage/cards/g/GruulKeyrune.java
+++ Mage.Sets/src/mage/cards/g/GruulKeyrune.java
@@ -2,7 +2,6 @@
 package mage.cards.g;
 
 import java.util.UUID;
-import mage.MageInt;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.mana.ManaCostsImpl;
 import mage.abilities.effects.common.continuous.BecomesCreatureSourceEffect;
@@ -14,8 +13,7 @@ import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.SubType;
 import mage.constants.Duration;
-import mage.constants.Zone;
-import mage.game.permanent.token.TokenImpl;
+import mage.game.permanent.token.custom.CreatureToken;
 
 /**
  *
@@ -31,7 +29,15 @@ public final class GruulKeyrune extends CardImpl {
         this.addAbility(new GreenManaAbility());
 
         // {R}{G}: Gruul Keyrune becomes a 3/2 red and green Beast artifact creature with trample until end of turn.
-        this.addAbility(new SimpleActivatedAbility(new BecomesCreatureSourceEffect(new GruulKeyruneToken(), CardType.ARTIFACT, Duration.EndOfTurn), new ManaCostsImpl<>("{R}{G}")));
+        this.addAbility(new SimpleActivatedAbility(
+            new BecomesCreatureSourceEffect(
+                new CreatureToken(3, 2, "3/2 red and green Beast artifact creature with trample", SubType.BEAST)
+                    .withColor("RG").withType(CardType.ARTIFACT).withAbility(TrampleAbility.getInstance()),
+                CardType.ARTIFACT,
+                Duration.EndOfTurn
+            ),
+            new ManaCostsImpl<>("{R}{G}")
+        ));
     }
 
     private GruulKeyrune(final GruulKeyrune card) {
@@ -41,27 +47,5 @@ public final class GruulKeyrune extends CardImpl {
     @Override
     public GruulKeyrune copy() {
         return new GruulKeyrune(this);
-    }
-
-    private static class GruulKeyruneToken extends TokenImpl {
-        GruulKeyruneToken() {
-            super("Beast", "3/2 red and green Beast artifact creature with trample");
-            cardType.add(CardType.ARTIFACT);
-            cardType.add(CardType.CREATURE);
-            color.setRed(true);
-            color.setGreen(true);
-            subtype.add(SubType.BEAST);
-            power = new MageInt(3);
-            toughness = new MageInt(2);
-            this.addAbility(TrampleAbility.getInstance());
-        }
-        private GruulKeyruneToken(final GruulKeyruneToken token) {
-            super(token);
-        }
-
-        public GruulKeyruneToken copy() {
-            return new GruulKeyruneToken(this);
-        }
-        
     }
 }
