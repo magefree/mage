bdf1ce786d2d632bb5ebcc0622d7b63e3f441ecf
Muz Ali <git@mustaqila.li>
2026-02-01 09:08:14 -0600

    Refactor private LavaclawReachesToken to CreatureToken

--- Mage.Sets/src/mage/cards/l/LavaclawReaches.java
+++ Mage.Sets/src/mage/cards/l/LavaclawReaches.java
@@ -1,7 +1,6 @@
 package mage.cards.l;
 
 import java.util.UUID;
-import mage.MageInt;
 import mage.abilities.common.EntersBattlefieldTappedAbility;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.mana.ManaCostsImpl;
@@ -16,8 +15,7 @@ import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.SubType;
 import mage.constants.Duration;
-import mage.constants.Zone;
-import mage.game.permanent.token.TokenImpl;
+import mage.game.permanent.token.custom.CreatureToken;
 
 /**
  *
@@ -36,7 +34,19 @@ public final class LavaclawReaches extends CardImpl {
         this.addAbility(new RedManaAbility());
 
         // {1}{B}{R}: Until end of turn, Lavaclaw Reaches becomes a 2/2 black and red Elemental creature with ": This creature gets +X/+0 until end of turn." It's still a land.
-        this.addAbility(new SimpleActivatedAbility(new BecomesCreatureSourceEffect(new LavaclawReachesToken(), CardType.LAND, Duration.EndOfTurn).withDurationRuleAtStart(true), new ManaCostsImpl<>("{1}{B}{R}")));
+        this.addAbility(new SimpleActivatedAbility(
+            new BecomesCreatureSourceEffect(
+                new CreatureToken(2, 2, "2/2 black and red Elemental creature with \"{X}: This creature gets +X/+0 until end of turn.\"", SubType.ELEMENTAL)
+                    .withColor("BR")
+                    .withAbility(
+                        new SimpleActivatedAbility(new BoostSourceEffect(GetXValue.instance, StaticValue.get(0), Duration.EndOfTurn),
+                        new ManaCostsImpl<>("{X}"))
+                    ),
+                CardType.LAND,
+                Duration.EndOfTurn
+            ).withDurationRuleAtStart(true),
+            new ManaCostsImpl<>("{1}{B}{R}")
+        ));
     }
 
     private LavaclawReaches(final LavaclawReaches card) {
@@ -46,27 +56,5 @@ public final class LavaclawReaches extends CardImpl {
     @Override
     public LavaclawReaches copy() {
         return new LavaclawReaches(this);
-    }
-
-}
-
-class LavaclawReachesToken extends TokenImpl {
-
-    public LavaclawReachesToken() {
-        super("", "2/2 black and red Elemental creature with \"{X}: This creature gets +X/+0 until end of turn.\"");
-        cardType.add(CardType.CREATURE);
-        subtype.add(SubType.ELEMENTAL);
-        color.setRed(true);
-        color.setBlack(true);
-        power = new MageInt(2);
-        toughness = new MageInt(2);
-        addAbility(new SimpleActivatedAbility(new BoostSourceEffect(GetXValue.instance, StaticValue.get(0), Duration.EndOfTurn), new ManaCostsImpl<>("{X}")));
-    }
-    private LavaclawReachesToken(final LavaclawReachesToken token) {
-        super(token);
-    }
-
-    public LavaclawReachesToken copy() {
-        return new LavaclawReachesToken(this);
     }
 }
