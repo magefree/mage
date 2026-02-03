7148d89e6eb8796c143d96992c68ca3b00194078
Muz Ali <git@mustaqila.li>
2026-02-02 19:43:57 -0600

    Refactor private OjutaiMonumentToken to CreatureToken

--- Mage.Sets/src/mage/cards/s/SilumgarMonument.java
+++ Mage.Sets/src/mage/cards/s/SilumgarMonument.java
@@ -2,7 +2,6 @@
 package mage.cards.s;
 
 import java.util.UUID;
-import mage.MageInt;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.mana.ManaCostsImpl;
 import mage.abilities.effects.common.continuous.BecomesCreatureSourceEffect;
@@ -14,8 +13,7 @@ import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.SubType;
 import mage.constants.Duration;
-import mage.constants.Zone;
-import mage.game.permanent.token.TokenImpl;
+import mage.game.permanent.token.custom.CreatureToken;
 
 /**
  *
@@ -29,10 +27,18 @@ public final class SilumgarMonument extends CardImpl {
         // {T}: Add {U} or {B}.
         this.addAbility(new BlueManaAbility());
         this.addAbility(new BlackManaAbility());
-        
+
         // {4}{U}{B}: Silumgar Monument becomes a 4/4 blue and black Dragon artifact creature with flying until end of turn.
-        this.addAbility(new SimpleActivatedAbility(new BecomesCreatureSourceEffect
-            (new OjutaiMonumentToken(), CardType.ARTIFACT, Duration.EndOfTurn), new ManaCostsImpl<>("{4}{U}{B}")));
+        this.addAbility(new SimpleActivatedAbility(
+            new BecomesCreatureSourceEffect(
+                new CreatureToken(
+                    4, 4, "4/4 blue and black Dragon artifact creature with flying", SubType.DRAGON
+                ).withColor("UB").withType(CardType.ARTIFACT).withAbility(FlyingAbility.getInstance()),
+                CardType.ARTIFACT,
+                Duration.EndOfTurn
+            ),
+            new ManaCostsImpl<>("{4}{U}{B}")
+        ));
     }
 
     private SilumgarMonument(final SilumgarMonument card) {
@@ -43,25 +49,4 @@ public final class SilumgarMonument extends CardImpl {
     public SilumgarMonument copy() {
         return new SilumgarMonument(this);
     }
-    
-    private static class OjutaiMonumentToken extends TokenImpl {
-        OjutaiMonumentToken() {
-            super("", "4/4 blue and black Dragon artifact creature with flying");
-            cardType.add(CardType.ARTIFACT);
-            cardType.add(CardType.CREATURE);
-            color.setBlue(true);
-            color.setBlack(true);
-            this.subtype.add(SubType.DRAGON);
-            power = new MageInt(4);
-            toughness = new MageInt(4);
-            this.addAbility(FlyingAbility.getInstance());
-        }
-        private OjutaiMonumentToken(final OjutaiMonumentToken token) {
-            super(token);
-        }
-    
-        public OjutaiMonumentToken copy() {
-            return new OjutaiMonumentToken(this);
-        }
-    }      
 }
