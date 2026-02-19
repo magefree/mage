7b1a05ac5e35f671aeb67ca65a000d13e9de08ee
Muz <muz@users.noreply.github.com>
2026-02-18 22:53:52 -0600

    Refactor private tokens to CreatureToken; T (#14377)
    * Refactor private TheCurseOfFenricHorrorToken to CreatureToken
    
    * Refactor private ThunderTotemToken to CreatureToken
    
    * Refactor private TreefolkToken to CreatureToken

--- Mage.Sets/src/mage/cards/t/ThunderTotem.java
+++ Mage.Sets/src/mage/cards/t/ThunderTotem.java
@@ -2,7 +2,6 @@
 package mage.cards.t;
 
 import java.util.UUID;
-import mage.MageInt;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.mana.ManaCostsImpl;
 import mage.abilities.effects.common.continuous.BecomesCreatureSourceEffect;
@@ -14,8 +13,7 @@ import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.SubType;
 import mage.constants.Duration;
-import mage.constants.Zone;
-import mage.game.permanent.token.TokenImpl;
+import mage.game.permanent.token.custom.CreatureToken;
 
 /**
  *
@@ -28,11 +26,20 @@ public final class ThunderTotem extends CardImpl {
 
         // {tap}: Add {W}.
         this.addAbility(new WhiteManaAbility());
-        
+
         // {1}{W}{W}: Thunder Totem becomes a 2/2 white Spirit artifact creature with flying and first strike until end of turn.
         this.addAbility(new SimpleActivatedAbility(
-                new BecomesCreatureSourceEffect(new ThunderTotemToken(), CardType.ARTIFACT, Duration.EndOfTurn),
-                new ManaCostsImpl<>("{1}{W}{W}")));
+            new BecomesCreatureSourceEffect(
+                new CreatureToken(2, 2, "2/2 white Spirit artifact creature with flying and first strike", SubType.SPIRIT)
+                    .withColor("W")
+                    .withType(CardType.ARTIFACT)
+                    .withAbility(FlyingAbility.getInstance())
+                    .withAbility(FirstStrikeAbility.getInstance()),
+                CardType.ARTIFACT,
+                Duration.EndOfTurn
+            ),
+            new ManaCostsImpl<>("{1}{W}{W}")
+        ));
     }
 
     private ThunderTotem(final ThunderTotem card) {
@@ -42,26 +49,5 @@ public final class ThunderTotem extends CardImpl {
     @Override
     public ThunderTotem copy() {
         return new ThunderTotem(this);
-    }
-
-    private static class ThunderTotemToken extends TokenImpl {
-        ThunderTotemToken() {
-            super("", "2/2 white Spirit artifact creature with flying and first strike");
-            cardType.add(CardType.ARTIFACT);
-            cardType.add(CardType.CREATURE);
-            color.setWhite(true);
-            this.subtype.add(SubType.SPIRIT);
-            power = new MageInt(2);
-            toughness = new MageInt(2);
-            this.addAbility(FlyingAbility.getInstance());
-            this.addAbility(FirstStrikeAbility.getInstance());
-        }
-        private ThunderTotemToken(final ThunderTotemToken token) {
-            super(token);
-        }
-    
-        public ThunderTotemToken copy() {
-            return new ThunderTotemToken(this);
-        }
     }
 }
