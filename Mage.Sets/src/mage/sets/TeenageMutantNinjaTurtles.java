a1a88e473a35898b11fe851d15e3fa95c5c7929b
Muz <muz@users.noreply.github.com>
2026-02-20 09:44:30 -0600

    [TMT] Implement Grounded for Life (#14469)

--- Mage.Sets/src/mage/sets/TeenageMutantNinjaTurtles.java
+++ Mage.Sets/src/mage/sets/TeenageMutantNinjaTurtles.java
@@ -79,6 +79,7 @@ public final class TeenageMutantNinjaTurtles extends ExpansionSet {
         cards.add(new SetCardInfo("Genghis Frog", 148, Rarity.UNCOMMON, mage.cards.g.GenghisFrog.class));
         cards.add(new SetCardInfo("Groundchuck & Dirtbag", 115, Rarity.RARE, mage.cards.g.GroundchuckAndDirtbag.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Groundchuck & Dirtbag", 238, Rarity.RARE, mage.cards.g.GroundchuckAndDirtbag.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Grounded for Life", 7, Rarity.COMMON, mage.cards.g.GroundedForLife.class));
         cards.add(new SetCardInfo("Hard-Won Jitte", 91, Rarity.UNCOMMON, mage.cards.h.HardWonJitte.class));
         cards.add(new SetCardInfo("Improvised Arsenal", 270, Rarity.RARE, mage.cards.i.ImprovisedArsenal.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Improvised Arsenal", 92, Rarity.RARE, mage.cards.i.ImprovisedArsenal.class, NON_FULL_USE_VARIOUS));
