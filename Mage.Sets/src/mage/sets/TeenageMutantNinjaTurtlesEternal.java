17a89cbe99a388928d7e8ed638757cf1e4d66c91
Muz Ali <git@mustaqila.li>
2026-02-19 22:51:03 -0600

    [TMC] Implement Super Combo

--- Mage.Sets/src/mage/sets/TeenageMutantNinjaTurtlesEternal.java
+++ Mage.Sets/src/mage/sets/TeenageMutantNinjaTurtlesEternal.java
@@ -77,6 +77,8 @@ public final class TeenageMutantNinjaTurtlesEternal extends ExpansionSet {
         cards.add(new SetCardInfo("Splinter, the Mentor", 89, Rarity.MYTHIC, mage.cards.s.SplinterTheMentor.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Steelbane Hydra", 52, Rarity.RARE, mage.cards.s.SteelbaneHydra.class));
         cards.add(new SetCardInfo("Sunken Hollow", 76, Rarity.RARE, mage.cards.s.SunkenHollow.class));
+        cards.add(new SetCardInfo("Super Combo", 34, Rarity.RARE, mage.cards.s.SuperCombo.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Super Combo", 95, Rarity.RARE, mage.cards.s.SuperCombo.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Supreme Verdict", 134, Rarity.MYTHIC, mage.cards.s.SupremeVerdict.class));
         cards.add(new SetCardInfo("Sword of Hearth and Home", 136, Rarity.MYTHIC, mage.cards.s.SwordOfHearthAndHome.class));
         cards.add(new SetCardInfo("Thriving Grove", 77, Rarity.COMMON, mage.cards.t.ThrivingGrove.class));
