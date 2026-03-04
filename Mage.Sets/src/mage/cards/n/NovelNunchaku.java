59e3f7888c16fd961b5d1f2a458d7f2a44be6501
Muz Ali <git@mustaqila.li>
2026-03-03 21:46:40 -0600

    Switch to reflexive ability

--- Mage.Sets/src/mage/cards/n/NovelNunchaku.java
+++ Mage.Sets/src/mage/cards/n/NovelNunchaku.java
@@ -1,21 +1,26 @@
 package mage.cards.n;
 
 import java.util.UUID;
-import mage.constants.SubType;
-import mage.filter.StaticFilters;
-import mage.target.TargetPermanent;
 import mage.abilities.Ability;
+import mage.abilities.common.delayed.ReflexiveTriggeredAbility;
 import mage.abilities.common.EntersBattlefieldAttachToTarget;
 import mage.abilities.common.SimpleStaticAbility;
-import mage.abilities.effects.common.FightTargetSourceEffect;
 import mage.abilities.effects.common.continuous.BoostEquippedEffect;
 import mage.abilities.effects.common.continuous.GainAbilityAttachedEffect;
+import mage.abilities.effects.OneShotEffect;
 import mage.abilities.keyword.EquipAbility;
 import mage.abilities.keyword.TrampleAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.AttachmentType;
 import mage.constants.CardType;
+import mage.constants.Outcome;
+import mage.constants.SubType;
+import mage.filter.StaticFilters;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
+import mage.target.TargetPermanent;
+import mage.target.targetpointer.FixedTarget;
 
 /**
  *
@@ -30,8 +35,7 @@ public final class NovelNunchaku extends CardImpl {
 
         // When this Equipment enters, attach it to target creature you control. When you do, equipped creature fights up to one target creature an opponent controls.
         Ability ability = new EntersBattlefieldAttachToTarget();
-        ability.addEffect(new FightTargetSourceEffect().setText("when you do, equipped creature fights up to one target creature an opponent controls"));
-        ability.addTarget(new TargetPermanent(0, 1, StaticFilters.FILTER_CREATURE_YOU_DONT_CONTROL));
+        ability.addEffect(new NovelNunchakuEffect());
         this.addAbility(ability);
 
         // Equipped creature gets +1/+1 and has trample.
@@ -50,5 +54,66 @@ public final class NovelNunchaku extends CardImpl {
     @Override
     public NovelNunchaku copy() {
         return new NovelNunchaku(this);
+    }
+}
+
+class NovelNunchakuEffect extends OneShotEffect {
+
+    NovelNunchakuEffect() {
+        super(Outcome.Benefit);
+        staticText = "When you do, equipped creature fights up to one target creature an opponent controls";
+    }
+
+    private NovelNunchakuEffect(final NovelNunchakuEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public NovelNunchakuEffect copy() {
+        return new NovelNunchakuEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Permanent creature = game.getPermanent(source.getFirstTarget());
+        if (creature == null) {
+            return false;
+        }
+        ReflexiveTriggeredAbility reflexive = new ReflexiveTriggeredAbility(
+                new NovelNunchakuFightEffect().setTargetPointer(new FixedTarget(creature.getId(), game)),
+                false, "equipped creature fights up to one target creature an opponent controls"
+        );
+        reflexive.addTarget(new TargetPermanent(0, 1, StaticFilters.FILTER_CREATURE_YOU_DONT_CONTROL));
+        game.fireReflexiveTriggeredAbility(reflexive, source);
+        return true;
+    }
+}
+
+class NovelNunchakuFightEffect extends OneShotEffect {
+
+    NovelNunchakuFightEffect() {
+        super(Outcome.Damage);
+    }
+
+    private NovelNunchakuFightEffect(final NovelNunchakuFightEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        Permanent equippedCreature = game.getPermanent(this.getTargetPointer().getFirst(game, source));
+        Permanent target = game.getPermanent(source.getFirstTarget());
+        if (equippedCreature != null
+                && target != null
+                && equippedCreature.isCreature(game)
+                && target.isCreature(game)) {
+            return equippedCreature.fight(target, source, game);
+        }
+        return false;
+    }
+
+    @Override
+    public NovelNunchakuFightEffect copy() {
+        return new NovelNunchakuFightEffect(this);
     }
 }
