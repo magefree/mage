d4cc3f2d95e561ea7440fe3dc60e8aa42330351f
Muz Ali <git@mustaqila.li>
2026-04-06 18:30:07 -0500

    Simplify filter usage

--- Mage.Sets/src/mage/cards/f/Flashback.java
+++ Mage.Sets/src/mage/cards/f/Flashback.java
@@ -13,8 +13,7 @@ import mage.constants.Duration;
 import mage.constants.Layer;
 import mage.constants.Outcome;
 import mage.constants.SubLayer;
-import mage.filter.FilterCard;
-import mage.filter.predicate.Predicates;
+import mage.filter.StaticFilters;
 import mage.game.Game;
 import mage.target.common.TargetCardInYourGraveyard;
 
@@ -24,21 +23,12 @@ import mage.target.common.TargetCardInYourGraveyard;
  */
 public final class Flashback extends CardImpl {
 
-    private static final FilterCard filter = new FilterCard("instant or sorcery card in your graveyard");
-
-    static {
-        filter.add(Predicates.or(
-                CardType.INSTANT.getPredicate(),
-                CardType.SORCERY.getPredicate()
-        ));
-    }
-
     public Flashback(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{R}");
 
         // Target instant or sorcery card in your graveyard gains flashback until end of turn. The flashback cost is equal to its mana cost.
         this.getSpellAbility().addEffect(new FlashbackEffect());
-        this.getSpellAbility().addTarget(new TargetCardInYourGraveyard(filter));
+        this.getSpellAbility().addTarget(new TargetCardInYourGraveyard(StaticFilters.FILTER_CARD_INSTANT_OR_SORCERY));
     }
 
     private Flashback(final Flashback card) {
