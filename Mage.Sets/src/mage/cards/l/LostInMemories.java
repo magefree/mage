754a49c2ca2e739de3047e887d0e0174f179446b
Muz Ali <git@mustaqila.li>
2026-04-06 19:05:06 -0500

    Refactor Lost in Memories to use shared GainFlashbackTargetEffect

--- Mage.Sets/src/mage/cards/l/LostInMemories.java
+++ Mage.Sets/src/mage/cards/l/LostInMemories.java
@@ -4,19 +4,16 @@ import mage.abilities.Ability;
 import mage.abilities.TriggeredAbility;
 import mage.abilities.common.DealsCombatDamageToAPlayerTriggeredAbility;
 import mage.abilities.common.SimpleStaticAbility;
-import mage.abilities.effects.ContinuousEffectImpl;
 import mage.abilities.effects.common.AttachEffect;
 import mage.abilities.effects.common.continuous.BoostEnchantedEffect;
 import mage.abilities.effects.common.continuous.GainAbilityAttachedEffect;
+import mage.abilities.effects.common.continuous.GainFlashbackTargetEffect;
 import mage.abilities.keyword.EnchantAbility;
 import mage.abilities.keyword.FlashAbility;
-import mage.abilities.keyword.FlashbackAbility;
-import mage.cards.Card;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.*;
 import mage.filter.StaticFilters;
-import mage.game.Game;
 import mage.target.TargetPermanent;
 import mage.target.common.TargetCardInYourGraveyard;
 import mage.target.common.TargetControlledCreaturePermanent;
@@ -43,7 +40,7 @@ public final class LostInMemories extends CardImpl {
         this.addAbility(new EnchantAbility(auraTarget));
 
         // Enchanted creature gets +1/+1 and has "Whenever this creature deals combat damage to a player, target instant or sorcery card in your graveyard gains flashback until end of turn. The flashback cost is equal to its mana cost."
-        TriggeredAbility triggeredAbility = new DealsCombatDamageToAPlayerTriggeredAbility(new LostInMemoriesEffect());
+        TriggeredAbility triggeredAbility = new DealsCombatDamageToAPlayerTriggeredAbility(new GainFlashbackTargetEffect());
         triggeredAbility.addTarget(new TargetCardInYourGraveyard(StaticFilters.FILTER_CARD_INSTANT_OR_SORCERY));
         Ability ability = new SimpleStaticAbility(new BoostEnchantedEffect(1, 1));
         ability.addEffect(new GainAbilityAttachedEffect(triggeredAbility, AttachmentType.AURA)
@@ -60,36 +57,5 @@ public final class LostInMemories extends CardImpl {
     @Override
     public LostInMemories copy() {
         return new LostInMemories(this);
-    }
-}
-
-class LostInMemoriesEffect extends ContinuousEffectImpl {
-
-    LostInMemoriesEffect() {
-        super(Duration.EndOfTurn, Layer.AbilityAddingRemovingEffects_6, SubLayer.NA, Outcome.AddAbility);
-        this.staticText = "target instant or sorcery card in your graveyard gains flashback until end of turn. " +
-                "The flashback cost is equal to its mana cost";
-    }
-
-    private LostInMemoriesEffect(final LostInMemoriesEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public LostInMemoriesEffect copy() {
-        return new LostInMemoriesEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Card card = game.getCard(getTargetPointer().getFirst(game, source));
-        if (card == null) {
-            return false;
-        }
-        FlashbackAbility ability = new FlashbackAbility(card, card.getManaCost());
-        ability.setSourceId(card.getId());
-        ability.setControllerId(card.getOwnerId());
-        game.getState().addOtherAbility(card, ability);
-        return true;
     }
 }
