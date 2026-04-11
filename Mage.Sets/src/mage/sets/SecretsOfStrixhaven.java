a96b1543fe3e3060bdac6dbf7a416181807681f2
Muz Ali <git@mustaqila.li>
2026-04-10 23:09:04 -0500

    [SOS] Implement Startled Relic Sloth

--- Mage.Sets/src/mage/sets/SecretsOfStrixhaven.java
+++ Mage.Sets/src/mage/sets/SecretsOfStrixhaven.java
@@ -225,6 +225,7 @@ public final class SecretsOfStrixhaven extends ExpansionSet {
         cards.add(new SetCardInfo("Splatter Technique", 356, Rarity.RARE, mage.cards.s.SplatterTechnique.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Stadium Tidalmage", 232, Rarity.COMMON, mage.cards.s.StadiumTidalmage.class));
         cards.add(new SetCardInfo("Stand Up for Yourself", 34, Rarity.UNCOMMON, mage.cards.s.StandUpForYourself.class));
+        cards.add(new SetCardInfo("Startled Relic Sloth", 233, Rarity.UNCOMMON, mage.cards.s.StartledRelicSloth.class));
         cards.add(new SetCardInfo("Stirring Honormancer", 234, Rarity.UNCOMMON, mage.cards.s.StirringHonormancer.class));
         cards.add(new SetCardInfo("Stirring Hopesinger", 314, Rarity.RARE, mage.cards.s.StirringHopesinger.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Stirring Hopesinger", 35, Rarity.RARE, mage.cards.s.StirringHopesinger.class, NON_FULL_USE_VARIOUS));
