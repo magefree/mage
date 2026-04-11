ca483d55fc9bc486c97951c5cd2fc76530051d09
Muz Ali <git@mustaqila.li>
2026-04-11 11:23:44 -0500

    Move damage effect to reflexive trigger

--- Mage.Sets/src/mage/cards/r/RubbleRouser.java
+++ Mage.Sets/src/mage/cards/r/RubbleRouser.java
@@ -6,19 +6,23 @@ import mage.Mana;
 import mage.abilities.Ability;
 import mage.abilities.common.EntersBattlefieldTriggeredAbility;
 import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.common.delayed.ReflexiveTriggeredAbility;
 import mage.abilities.costs.common.DiscardCardCost;
 import mage.abilities.costs.common.ExileFromGraveCost;
 import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.DamagePlayersEffect;
 import mage.abilities.effects.common.DoIfCostPaid;
 import mage.abilities.effects.common.DrawCardSourceControllerEffect;
 import mage.abilities.effects.mana.BasicManaEffect;
 import mage.constants.SubType;
 import mage.constants.TargetController;
+import mage.game.Game;
 import mage.target.common.TargetCardInYourGraveyard;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.constants.Outcome;
 
 /**
  *
@@ -42,7 +46,7 @@ public final class RubbleRouser extends CardImpl {
         // {T}, Exile a card from your graveyard: Add {R}. When you do, this creature deals 1 damage to each opponent.
         Ability ability = new SimpleActivatedAbility(new BasicManaEffect(Mana.RedMana(1)), new TapSourceCost());
         ability.addCost(new ExileFromGraveCost(new TargetCardInYourGraveyard()));
-        ability.addEffect(new DamagePlayersEffect(1, TargetController.OPPONENT));
+        ability.addEffect(new RubbleRouserDamageEffect());
         this.addAbility(ability);
     }
 
@@ -53,5 +57,29 @@ public final class RubbleRouser extends CardImpl {
     @Override
     public RubbleRouser copy() {
         return new RubbleRouser(this);
+    }
+}
+
+class RubbleRouserDamageEffect extends OneShotEffect {
+
+    RubbleRouserDamageEffect() {
+        super(Outcome.Damage);
+        staticText = "When you do, this creature deals 1 damage to each opponent";
+    }
+
+    private RubbleRouserDamageEffect(final RubbleRouserDamageEffect effect) {
+        super(effect);
+    }
+
+    @Override
+    public RubbleRouserDamageEffect copy() {
+        return new RubbleRouserDamageEffect(this);
+    }
+
+    @Override
+    public boolean apply(Game game, Ability source) {
+        ReflexiveTriggeredAbility ability = new ReflexiveTriggeredAbility(new DamagePlayersEffect(1, TargetController.OPPONENT), false, "this creature deals 1 damage to each opponent");
+        game.fireReflexiveTriggeredAbility(ability, source);
+        return true;
     }
 }
