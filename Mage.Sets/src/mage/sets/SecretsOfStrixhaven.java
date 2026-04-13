4ed558753779c01ad31726b55d0d7c1bc866b433
Muz Ali <git@mustaqila.li>
2026-04-12 22:39:46 -0500

    [SOS] Implement Nita, Forum Conciliator

--- Mage.Sets/src/mage/sets/SecretsOfStrixhaven.java
+++ Mage.Sets/src/mage/sets/SecretsOfStrixhaven.java
@@ -169,6 +169,8 @@ public final class SecretsOfStrixhaven extends ExpansionSet {
         cards.add(new SetCardInfo("Mountain", 279, Rarity.LAND, mage.cards.basiclands.Mountain.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Muse Seeker", 60, Rarity.UNCOMMON, mage.cards.m.MuseSeeker.class));
         cards.add(new SetCardInfo("Muse's Encouragement", 61, Rarity.COMMON, mage.cards.m.MusesEncouragement.class));
+        cards.add(new SetCardInfo("Nita, Forum Conciliator", 206, Rarity.RARE, mage.cards.n.NitaForumConciliator.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Nita, Forum Conciliator", 354, Rarity.RARE, mage.cards.n.NitaForumConciliator.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Noxious Newt", 155, Rarity.COMMON, mage.cards.n.NoxiousNewt.class));
         cards.add(new SetCardInfo("Old-Growth Educator", 207, Rarity.UNCOMMON, mage.cards.o.OldGrowthEducator.class));
         cards.add(new SetCardInfo("Oracle's Restoration", 156, Rarity.COMMON, mage.cards.o.OraclesRestoration.class));
