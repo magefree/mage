7ba2651bbd2f1ac7f87d6841389478a709619524
Muz Ali <git@mustaqila.li>
2026-04-21 21:25:36 -0500

    Reimplement part of the card on PR feedback

--- Mage.Sets/src/mage/cards/g/GeometersArthropod.java
+++ Mage.Sets/src/mage/cards/g/GeometersArthropod.java
@@ -3,17 +3,16 @@ package mage.cards.g;
 import java.util.UUID;
 import mage.MageInt;
 import mage.abilities.Ability;
-import mage.abilities.TriggeredAbilityImpl;
+import mage.abilities.common.SpellCastControllerTriggeredAbility;
 import mage.abilities.dynamicvalue.common.StaticValue;
 import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.LookLibraryAndPickControllerEffect;
 import mage.constants.PutCards;
 import mage.constants.SubType;
-import mage.constants.Zone;
+import mage.filter.FilterSpell;
+import mage.filter.predicate.mageobject.VariableManaCostPredicate;
 import mage.game.Game;
-import mage.game.events.GameEvent;
 import mage.game.stack.Spell;
-import mage.players.Player;
 import mage.util.CardUtil;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
@@ -26,7 +25,11 @@ import mage.constants.Outcome;
  */
 public final class GeometersArthropod extends CardImpl {
 
-    static final String needPrefix = "_GeometersArthropod_NeedSpell";
+    private static final FilterSpell filter = new FilterSpell("a spell with {X} in its mana cost");
+
+    static {
+        filter.add(VariableManaCostPredicate.instance);
+    }
 
     public GeometersArthropod(UUID ownerId, CardSetInfo setInfo) {
         super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{G}{U}");
@@ -37,7 +40,7 @@ public final class GeometersArthropod extends CardImpl {
         this.toughness = new MageInt(4);
 
         // Whenever you cast a spell with {X} in its mana cost, look at the top X cards of your library. Put one of them into your hand and the rest on the bottom of your library in a random order.
-        this.addAbility(new GeometersArthropodAbility());
+        this.addAbility(new SpellCastControllerTriggeredAbility(new GeometersArthropodEffect(), filter, false));
     }
 
     private GeometersArthropod(final GeometersArthropod card) {
@@ -50,45 +53,10 @@ public final class GeometersArthropod extends CardImpl {
     }
 }
 
-class GeometersArthropodAbility extends TriggeredAbilityImpl {
-
-    public GeometersArthropodAbility() {
-        super(Zone.BATTLEFIELD, new GeometersArthropodEffect(), false);
-        setTriggerPhrase("Whenever you cast a spell with {X} in its mana cost");
-    }
-
-    private GeometersArthropodAbility(final GeometersArthropodAbility ability) {
-        super(ability);
-    }
-
-    @Override
-    public boolean checkEventType(GameEvent event, Game game) {
-        return event.getType() == GameEvent.EventType.SPELL_CAST;
-    }
-
-    @Override
-    public boolean checkTrigger(GameEvent event, Game game) {
-        if (!event.getPlayerId().equals(getControllerId()) || event.getType() != GameEvent.EventType.SPELL_CAST) {
-            return false;
-        }
-        Spell spell = game.getStack().getSpell(event.getTargetId());
-        if (spell == null || !spell.getSpellAbility().getManaCostsToPay().containsX()) {
-            return false;
-        }
-        game.getState().setValue(this.getSourceId() + GeometersArthropod.needPrefix, spell);
-        return true;
-    }
-
-    @Override
-    public GeometersArthropodAbility copy() {
-        return new GeometersArthropodAbility(this);
-    }
-}
-
 class GeometersArthropodEffect extends OneShotEffect {
     GeometersArthropodEffect() {
         super(Outcome.Benefit);
-        this.staticText = ", look at the top X cards of your library. "
+        this.staticText = "look at the top X cards of your library. "
           + "Put one of them into your hand and the rest on the bottom "
           + "of your library in a random order.";
     }
@@ -104,17 +72,11 @@ class GeometersArthropodEffect extends OneShotEffect {
 
     @Override
     public boolean apply(Game game, Ability source) {
-        Player player = game.getPlayer(source.getControllerId());
-        if (player == null) {
+        Spell spell = (Spell) getValue("spellCast");
+        if (spell == null) {
             return false;
         }
 
-        Object needObject = game.getState().getValue(source.getSourceId() + GeometersArthropod.needPrefix);
-        if (!(needObject instanceof Spell)) {
-            return false;
-        }
-
-        Spell spell = (Spell) needObject;
         int xValue = CardUtil.getSourceCostsTag(game, spell.getSpellAbility(), "X", 0);
         if (xValue < 1) {
             return true;
