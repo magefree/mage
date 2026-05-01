c84e5b462752516421badbe3f3db7f1be15d76b7
Muz Ali <git@mustaqila.li>
2026-05-01 16:54:26 -0500

    WIP

---
+++ Mage.Sets/src/mage/sets/RealityFracture.java
@@ -0,0 +1,29 @@
+package mage.sets;
+
+import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
+import mage.constants.SetType;
+
+/**
+ * @author muz
+ */
+public final class RealityFracture extends ExpansionSet {
+
+    private static final RealityFracture instance = new RealityFracture();
+
+    public static RealityFracture getInstance() {
+        return instance;
+    }
+
+    private RealityFracture() {
+        super("Reality Fracture", "FRA", ExpansionSet.buildDate(2026, 10, 2), SetType.CUSTOM_SET);
+        // super("Reality Fracture", "FRA", ExpansionSet.buildDate(2026, 10, 2), SetType.EXPANSION); TODO: Once mtgjson/Scryfall have the set, change to EXPANSION
+        this.blockName = "Reality Fracture"; // for sorting in GUI
+        this.hasBasicLands = false; // TODO: Enable once basic collector numbers are known
+
+        // this.enablePlayBooster(305); TODO: Enable later
+
+
+
+    }
+}
