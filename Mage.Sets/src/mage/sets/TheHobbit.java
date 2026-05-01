1c377003e4be2b3f145bcd88d4a2457bdb2f1b9a
Muz Ali <git@mustaqila.li>
2026-05-01 16:28:01 -0500

    [HOB] Implement My Precious

--- Mage.Sets/src/mage/sets/TheHobbit.java
+++ Mage.Sets/src/mage/sets/TheHobbit.java
@@ -16,15 +16,16 @@ public final class TheHobbit extends ExpansionSet {
     }
 
     private TheHobbit() {
-        super("The Hobbit", "HOB", ExpansionSet.buildDate(2024, 8, 14), SetType.EXPANSION);
+        super("The Hobbit", "HOB", ExpansionSet.buildDate(2024, 8, 14), SetType.CUSTOM_SET);
+        // super("The Hobbit", "HOB", ExpansionSet.buildDate(2024, 8, 14), SetType.EXPANSION); TODO: Once mtgjson/Scryfall have the set, change to EXPANSION
         this.blockName = "The Hobbit"; // for sorting in GUI
         this.hasBasicLands = false; // TODO: Enable once basic collector numbers are known
 
         // this.enablePlayBooster(305); TODO: Enable later
 
         cards.add(new SetCardInfo("An Unexpected Party", 29, Rarity.RARE, mage.cards.a.AnUnexpectedParty.class));
-        //cards.add(new SetCardInfo("My Precious", 176, Rarity.RARE, mage.cards.m.MyPrecious.class));
-        //cards.add(new SetCardInfo("My Precious", 235, Rarity.RARE, mage.cards.m.MyPrecious.class));
+        cards.add(new SetCardInfo("My Precious", 176, Rarity.RARE, mage.cards.m.MyPrecious.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("My Precious", 235, Rarity.RARE, mage.cards.m.MyPrecious.class, NON_FULL_USE_VARIOUS));
         //cards.add(new SetCardInfo("Tom, Bert, and William", 169, Rarity.RARE, mage.cards.t.TomBertAndWilliam.class));
         //cards.add(new SetCardInfo("Tom, Bert, and William", 312, Rarity.RARE, mage.cards.t.TomBertAndWilliam.class));
     }
