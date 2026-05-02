71b1dcc787df3bae2ee319bfd4262c085180ad66
Muz Ali <git@mustaqila.li>
2026-05-01 22:28:59 -0500

    [HOB] Reprints, extra printings, and scryfall info is up

--- Mage.Sets/src/mage/sets/TheHobbit.java
+++ Mage.Sets/src/mage/sets/TheHobbit.java
@@ -16,18 +16,36 @@ public final class TheHobbit extends ExpansionSet {
     }
 
     private TheHobbit() {
-        super("The Hobbit", "HOB", ExpansionSet.buildDate(2026, 8, 14), SetType.CUSTOM_SET);
-        // super("The Hobbit", "HOB", ExpansionSet.buildDate(2026, 8, 14), SetType.EXPANSION); TODO: Once mtgjson/Scryfall have the set, change to EXPANSION
+        super("The Hobbit", "HOB", ExpansionSet.buildDate(2026, 8, 14), SetType.EXPANSION);
         this.blockName = "The Hobbit"; // for sorting in GUI
-        this.hasBasicLands = false; // TODO: Enable once basic collector numbers are known
+        this.hasBasicLands = true;
 
         // this.enablePlayBooster(305); TODO: Enable later
 
-        cards.add(new SetCardInfo("An Unexpected Party", 29, Rarity.RARE, mage.cards.a.AnUnexpectedParty.class));
+        cards.add(new SetCardInfo("An Unexpected Party", 29, Rarity.RARE, mage.cards.a.AnUnexpectedParty.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("An Unexpected Party", 289, Rarity.RARE, mage.cards.a.AnUnexpectedParty.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Forest", 198, Rarity.LAND, mage.cards.basiclands.Forest.class, FULL_ART));
+        cards.add(new SetCardInfo("Island", 195, Rarity.LAND, mage.cards.basiclands.Island.class, FULL_ART));
+        cards.add(new SetCardInfo("Mountain", 197, Rarity.LAND, mage.cards.basiclands.Mountain.class, FULL_ART));
         cards.add(new SetCardInfo("My Precious", 176, Rarity.RARE, mage.cards.m.MyPrecious.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("My Precious", 235, Rarity.RARE, mage.cards.m.MyPrecious.class, NON_FULL_USE_VARIOUS));
-        cards.add(new SetCardInfo("Smaug the Magnificent", 110, Rarity.MYTHIC, mage.cards.s.SmaugTheMagnificent.class));
+        cards.add(new SetCardInfo("My Precious", 271, Rarity.RARE, mage.cards.m.MyPrecious.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Plains", 194, Rarity.LAND, mage.cards.basiclands.Plains.class, FULL_ART_USE_VARIOUS));
+        cards.add(new SetCardInfo("Plains", 313, Rarity.LAND, mage.cards.basiclands.Plains.class, FULL_ART_USE_VARIOUS));
+        cards.add(new SetCardInfo("Plains", 314, Rarity.LAND, mage.cards.basiclands.Plains.class, FULL_ART_USE_VARIOUS));
+        cards.add(new SetCardInfo("Plains", 315, Rarity.LAND, mage.cards.basiclands.Plains.class, FULL_ART_USE_VARIOUS));
+        cards.add(new SetCardInfo("Plains", 316, Rarity.LAND, mage.cards.basiclands.Plains.class, FULL_ART_USE_VARIOUS));
+        cards.add(new SetCardInfo("Plains", 317, Rarity.LAND, mage.cards.basiclands.Plains.class, FULL_ART_USE_VARIOUS));
+        cards.add(new SetCardInfo("Plains", 318, Rarity.LAND, mage.cards.basiclands.Plains.class, FULL_ART_USE_VARIOUS));
+        cards.add(new SetCardInfo("Plains", 319, Rarity.LAND, mage.cards.basiclands.Plains.class, FULL_ART_USE_VARIOUS));
+        cards.add(new SetCardInfo("Plains", 320, Rarity.LAND, mage.cards.basiclands.Plains.class, FULL_ART_USE_VARIOUS));
+        cards.add(new SetCardInfo("Smaug the Magnificent", 110, Rarity.MYTHIC, mage.cards.s.SmaugTheMagnificent.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Smaug the Magnificent", 229, Rarity.MYTHIC, mage.cards.s.SmaugTheMagnificent.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Smaug the Magnificent", 249, Rarity.MYTHIC, mage.cards.s.SmaugTheMagnificent.class, FULL_ART_USE_VARIOUS));
+        cards.add(new SetCardInfo("Smaug the Magnificent", 265, Rarity.MYTHIC, mage.cards.s.SmaugTheMagnificent.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Swamp", 196, Rarity.LAND, mage.cards.basiclands.Swamp.class, FULL_ART));
         cards.add(new SetCardInfo("Tom, Bert, and William", 169, Rarity.RARE, mage.cards.t.TomBertAndWilliam.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Tom, Bert, and William", 312, Rarity.RARE, mage.cards.t.TomBertAndWilliam.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Wood Elves", 142, Rarity.COMMON, mage.cards.w.WoodElves.class));
     }
 }
