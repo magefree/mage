85b4940bd0f13c1c731755be847f1d29f806ce0a
xenohedron <12538125+xenohedron@users.noreply.github.com>
2026-05-10 00:06:10 -0400

    simplify Well of Knowledge test

--- Mage.Tests/src/test/java/org/mage/test/cards/single/wth/WellOfKnowledgeTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/wth/WellOfKnowledgeTest.java
@@ -2,7 +2,6 @@ package org.mage.test.cards.single.wth;
 
 import mage.constants.PhaseStep;
 import mage.constants.Zone;
-import org.junit.Assert;
 import org.junit.Test;
 import org.mage.test.serverside.base.CardTestPlayerBase;
 
@@ -16,10 +15,19 @@ public class WellOfKnowledgeTest extends CardTestPlayerBase {
     private static final String ability = "{2}: Draw";
 
     @Test
-    public void testController() {
+    public void testActivations() {
         addCard(Zone.BATTLEFIELD, playerA, well);
         addCard(Zone.BATTLEFIELD, playerA, "Island", 2);
         addCard(Zone.BATTLEFIELD, playerB, "Island", 2);
+
+        checkPlayableAbility("", 1, PhaseStep.PRECOMBAT_MAIN, playerA, ability, false);
+        checkPlayableAbility("", 1, PhaseStep.PRECOMBAT_MAIN, playerB, ability, false);
+
+        checkPlayableAbility("", 2, PhaseStep.DRAW, playerA, ability, false);
+        checkPlayableAbility("", 2, PhaseStep.DRAW, playerB, ability, true);
+
+        checkPlayableAbility("", 3, PhaseStep.DRAW, playerA, ability, true);
+        checkPlayableAbility("", 3, PhaseStep.DRAW, playerB, ability, false);
 
         activateAbility(3, PhaseStep.DRAW, playerA, ability);
 
@@ -30,88 +38,6 @@ public class WellOfKnowledgeTest extends CardTestPlayerBase {
         assertHandCount(playerA, 2); // draw step and well
         assertHandCount(playerB, 1); // draw step
 
-    }
-
-    @Test
-    public void testNotController() {
-        addCard(Zone.BATTLEFIELD, playerA, well);
-        addCard(Zone.BATTLEFIELD, playerA, "Island", 2);
-        addCard(Zone.BATTLEFIELD, playerB, "Island", 2);
-
-        activateAbility(2, PhaseStep.DRAW, playerB, ability);
-
-        setStrictChooseMode(true);
-        setStopAt(3, PhaseStep.PRECOMBAT_MAIN);
-        execute();
-
-        assertHandCount(playerA, 1); // draw step
-        assertHandCount(playerB, 2); // draw step and well
-    }
-
-    @Test
-    public void testControllerNotActive() {
-        addCard(Zone.BATTLEFIELD, playerA, well);
-        addCard(Zone.BATTLEFIELD, playerA, "Island", 2);
-        addCard(Zone.BATTLEFIELD, playerB, "Island", 2);
-
-        activateAbility(2, PhaseStep.DRAW, playerA, ability);
-
-        setStrictChooseMode(true);
-        setStopAt(3, PhaseStep.PRECOMBAT_MAIN);
-        try {
-            execute();
-        } catch (AssertionError e) {
-            Assert.assertEquals(
-                    "Correctly can't activate",
-                    "Can't find ability to activate command: " + ability, e.getMessage()
-            );
-            return;
-        }
-        Assert.fail("Was able to activate ability, but shouldn't");
-    }
-
-    @Test
-    public void testNotControllerNotActive() {
-        addCard(Zone.BATTLEFIELD, playerA, well);
-        addCard(Zone.BATTLEFIELD, playerA, "Island", 2);
-        addCard(Zone.BATTLEFIELD, playerB, "Island", 2);
-
-        activateAbility(3, PhaseStep.DRAW, playerB, ability);
-
-        setStrictChooseMode(true);
-        setStopAt(3, PhaseStep.PRECOMBAT_MAIN);
-        try {
-            execute();
-        } catch (AssertionError e) {
-            Assert.assertEquals(
-                    "Correctly can't activate",
-                    "Can't find ability to activate command: " + ability, e.getMessage()
-            );
-            return;
-        }
-        Assert.fail("Was able to activate ability, but shouldn't");
-    }
-
-    @Test
-    public void testControllerNotDrawStep() {
-        addCard(Zone.BATTLEFIELD, playerA, well);
-        addCard(Zone.BATTLEFIELD, playerA, "Island", 2);
-        addCard(Zone.BATTLEFIELD, playerB, "Island", 2);
-
-        activateAbility(3, PhaseStep.UPKEEP, playerA, ability);
-
-        setStrictChooseMode(true);
-        setStopAt(3, PhaseStep.PRECOMBAT_MAIN);
-        try {
-            execute();
-        } catch (AssertionError e) {
-            Assert.assertEquals(
-                    "Correctly can't activate",
-                    "Can't find ability to activate command: " + ability, e.getMessage()
-            );
-            return;
-        }
-        Assert.fail("Was able to activate ability, but shouldn't");
     }
 
 }
