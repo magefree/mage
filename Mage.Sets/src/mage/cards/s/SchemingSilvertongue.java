fe320bedb03f2149cd4d5d39e47b77c04dc0b23c
Stephan Walters <stephan.walters.92@gmail.com>
2026-05-16 15:17:37 +0200

    [SOS] Fix Scheming Silvertongue

--- Mage.Sets/src/mage/cards/s/SchemingSilvertongue.java
+++ Mage.Sets/src/mage/cards/s/SchemingSilvertongue.java
@@ -1,16 +1,13 @@
 package mage.cards.s;
 
-import java.util.UUID;
 import mage.MageInt;
-import mage.constants.SubType;
-import mage.target.TargetPlayer;
-import mage.watchers.common.PlayerGainedLifeWatcher;
+import mage.abilities.Ability;
 import mage.abilities.condition.Condition;
 import mage.abilities.condition.common.YouGainedLifeCondition;
+import mage.abilities.dynamicvalue.common.ControllerGainedLifeCount;
 import mage.abilities.effects.common.BecomePreparedSourceEffect;
 import mage.abilities.effects.common.DrawCardTargetEffect;
 import mage.abilities.effects.common.LoseLifeTargetEffect;
-import mage.abilities.hint.ConditionHint;
 import mage.abilities.hint.Hint;
 import mage.abilities.keyword.FlyingAbility;
 import mage.abilities.keyword.LifelinkAbility;
@@ -19,41 +16,44 @@ import mage.cards.CardSetInfo;
 import mage.cards.PrepareCard;
 import mage.constants.CardType;
 import mage.constants.ComparisonType;
+import mage.constants.SubType;
+import mage.target.TargetPlayer;
+import mage.watchers.common.PlayerGainedLifeWatcher;
+
+import java.util.UUID;
 
 /**
- *
- * @author muz
+ * @author Susucr
  */
 public final class SchemingSilvertongue extends PrepareCard {
 
     private static final Condition condition = new YouGainedLifeCondition(ComparisonType.OR_GREATER, 2);
-    private static final Hint hint = new ConditionHint(condition);
+    private static final Hint hint = ControllerGainedLifeCount.getHint();
 
     public SchemingSilvertongue(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{1}{B}", "Sign in Blood", new CardType[]{CardType.SORCERY}, "{B}{B}");
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{1}{B}", "Sign in Blood", CardType.SORCERY, "{B}{B}");
 
         this.subtype.add(SubType.VAMPIRE);
         this.subtype.add(SubType.WARLOCK);
         this.power = new MageInt(1);
         this.toughness = new MageInt(3);
 
-        // Flying
+        // Flying, lifelink
         this.addAbility(FlyingAbility.getInstance());
-
-        // Lifelink
         this.addAbility(LifelinkAbility.getInstance());
 
         // At the beginning of your second main phase, if you gained 2 or more life this turn, this creature becomes prepared.
-        this.addAbility(new BeginningOfSecondMainTriggeredAbility(
-            new BecomePreparedSourceEffect(), false
-        ).withInterveningIf(condition).addHint(hint), new PlayerGainedLifeWatcher());
+        Ability ability = new BeginningOfSecondMainTriggeredAbility(new BecomePreparedSourceEffect(), false)
+                .withInterveningIf(condition)
+                .addHint(hint);
+        this.addAbility(ability, new PlayerGainedLifeWatcher());
 
         // Sign in Blood
         // Sorcery {B}{B}
         // Target player draws two cards and loses 2 life.
+        this.getSpellCard().getSpellAbility().addTarget(new TargetPlayer());
         this.getSpellCard().getSpellAbility().addEffect(new DrawCardTargetEffect(2));
         this.getSpellCard().getSpellAbility().addEffect(new LoseLifeTargetEffect(2).withTargetDescription("and"));
-        this.getSpellCard().getSpellAbility().addTarget(new TargetPlayer());
     }
 
     private SchemingSilvertongue(final SchemingSilvertongue card) {
