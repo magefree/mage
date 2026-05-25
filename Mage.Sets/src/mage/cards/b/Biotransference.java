6a892655df5b9252aed198e58776c16a8d133a53
matoro <matoro@users.noreply.github.com>
2026-05-24 23:41:39 -0400

    Centralize all-zone attribute modification effects
    This centralizes into a pair of common effects the various cards with
    all-zone attribute modification effects e.g. Arcane Adaptation,
    Biotransference, Conspiracy, Leyline of Transformation, and Maskwood
    Nexus.  A few others will continue to work with no chnges.  Specifically
    I want to make sure that the code breaking objects into parts and
    applying individually to each part is always reused for any future
    implementations, so I've made the constructor fairly open-ended, taking
    an arbitrary modifier lambda.  All of these cards can now correctly take
    advantage of the shared code rather than having to implement it
    individually.
    
    Fixes https://github.com/magefree/mage/issues/14817

--- Mage.Sets/src/mage/cards/b/Biotransference.java
+++ Mage.Sets/src/mage/cards/b/Biotransference.java
@@ -3,25 +3,17 @@ package mage.cards.b;
 import mage.abilities.Ability;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.common.SpellCastControllerTriggeredAbility;
-import mage.abilities.effects.ContinuousEffectImpl;
 import mage.abilities.effects.common.CreateTokenEffect;
 import mage.abilities.effects.common.LoseLifeSourceControllerEffect;
-import mage.cards.Card;
+import mage.abilities.effects.common.continuous.ModifyObjectAllMultiZoneEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.*;
 import mage.filter.StaticFilters;
-import mage.filter.common.FilterControlledCreaturePermanent;
-import mage.game.Game;
-import mage.game.command.CommandObject;
-import mage.game.command.Commander;
-import mage.game.permanent.Permanent;
+import mage.filter.common.FilterControlledCreatureSpell;
+import mage.filter.common.FilterOwnedCreatureCard;
 import mage.game.permanent.token.NecronWarriorToken;
-import mage.game.stack.Spell;
-import mage.game.stack.StackObject;
-import mage.players.Player;
 
-import java.util.List;
 import java.util.UUID;
 
 public final class Biotransference extends CardImpl {
@@ -48,12 +40,16 @@ public final class Biotransference extends CardImpl {
     }
 }
 
-class BiotransferenceEffect extends ContinuousEffectImpl {
+class BiotransferenceEffect extends ModifyObjectAllMultiZoneEffect {
 
     BiotransferenceEffect() {
-        super(Duration.WhileOnBattlefield, Layer.TypeChangingEffects_4, SubLayer.NA, Outcome.Benefit);
-        staticText = "Creatures you control are artifacts in addition to their other types. " +
-                "The same is true for creature spells you control and creature cards you own that aren't on the battlefield";
+        super(
+            StaticFilters.FILTER_CONTROLLED_CREATURES,
+            new FilterControlledCreatureSpell("creature spells you control"),
+            new FilterOwnedCreatureCard("creature cards you own"),
+            (obj, source, game) -> { game.getState().getCreateMageObjectAttribute(obj, game).getCardType().add(CardType.ARTIFACT); },
+            CardType.ARTIFACT.getPluralName() + " in addition to their other types"
+        );
         this.dependencyTypes.add(DependencyType.ArtifactAddingRemoving); // March of the Machines
     }
 
@@ -66,69 +62,4 @@ class BiotransferenceEffect extends ContinuousEffectImpl {
         return new BiotransferenceEffect(this);
     }
 
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Player controller = game.getPlayer(source.getControllerId());
-        if (controller == null) {
-            return false;
-        }
-        // Creature cards you own that aren't on the battlefield
-        // in graveyard
-        for (UUID cardId : controller.getGraveyard()) {
-            Card card = game.getCard(cardId);
-            if (card != null && card.isCreature(game) && !card.isArtifact(game)) {
-                card.addCardType(game, CardType.ARTIFACT);
-            }
-        }
-        // on Hand
-        for (UUID cardId : controller.getHand()) {
-            Card card = game.getCard(cardId);
-            if (card != null && card.isCreature(game) && !card.isArtifact(game)) {
-                card.addCardType(game, CardType.ARTIFACT);
-            }
-        }
-        // in Exile
-        for (Card card : game.getState().getExile().getCardsOwned(game, source.getControllerId())) {
-            if (card.isCreature(game) && !card.isArtifact(game)) {
-                card.addCardType(game, CardType.ARTIFACT);
-            }
-        }
-        // in Library (e.g. for Mystical Teachings)
-        for (Card card : controller.getLibrary().getCards(game)) {
-            if (card.isOwnedBy(controller.getId()) && card.isCreature(game) && !card.isArtifact(game)) {
-                card.addCardType(game, CardType.ARTIFACT);
-            }
-        }
-        // commander in command zone
-        for (CommandObject commandObject : game.getState().getCommand()) {
-            if (commandObject instanceof Commander) {
-                Card card = game.getCard((commandObject).getId());
-                if (card != null && card.isOwnedBy(controller.getId())
-                        && card.isCreature(game) && !card.isArtifact(game)) {
-                    card.addCardType(game, CardType.ARTIFACT);
-                }
-            }
-        }
-
-        // creature spells you control
-        for (StackObject stackObject : game.getStack()) {
-            if (stackObject instanceof Spell
-                    && stackObject.isControlledBy(source.getControllerId())
-                    && stackObject.isCreature(game)
-                    && !stackObject.isArtifact(game)) {
-                Card card = ((Spell) stackObject).getCard();
-                card.addCardType(game, CardType.ARTIFACT);
-            }
-        }
-        // creatures you control
-        List<Permanent> creatures = game.getBattlefield().getAllActivePermanents(
-                new FilterControlledCreaturePermanent(), source.getControllerId(), game);
-        for (Permanent creature : creatures) {
-            if (creature != null) {
-                creature.addCardType(game, CardType.ARTIFACT);
-            }
-        }
-        return true;
-
-    }
 }
