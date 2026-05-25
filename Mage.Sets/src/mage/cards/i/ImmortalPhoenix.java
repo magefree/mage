981b53eae899ff173cd2fd2d0035519d86a2edb3
xenohedron <12538125+xenohedron@users.noreply.github.com>
2026-05-25 02:09:06 -0400

    update dies return to hand

--- Mage.Sets/src/mage/cards/i/ImmortalPhoenix.java
+++ Mage.Sets/src/mage/cards/i/ImmortalPhoenix.java
@@ -2,11 +2,12 @@ package mage.cards.i;
 
 import mage.MageInt;
 import mage.abilities.common.DiesSourceTriggeredAbility;
-import mage.abilities.effects.common.ReturnToHandSourceEffect;
+import mage.abilities.effects.common.ReturnToHandTargetEffect;
 import mage.abilities.keyword.FlyingAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.constants.SetTargetPointer;
 import mage.constants.SubType;
 
 import java.util.UUID;
@@ -26,8 +27,8 @@ public final class ImmortalPhoenix extends CardImpl {
         this.addAbility(FlyingAbility.getInstance());
 
         // When Immortal Phoenix dies, return it to its owner’s hand.
-        this.addAbility(new DiesSourceTriggeredAbility(new ReturnToHandSourceEffect()));
-    }
+        this.addAbility(new DiesSourceTriggeredAbility(new ReturnToHandTargetEffect()
+                .setText("return it to its owner's hand"), false, SetTargetPointer.CARD));    }
 
     private ImmortalPhoenix(final ImmortalPhoenix card) {
         super(card);
