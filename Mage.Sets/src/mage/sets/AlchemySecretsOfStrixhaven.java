601f2e6b2e3183fd3c601dbb2e43f33ea5616672
Muz Ali <git@mustaqila.li>
2026-05-27 22:42:46 -0500

    [YSOS] Add set and implement Grave Studies

---
+++ Mage.Sets/src/mage/sets/AlchemySecretsOfStrixhaven.java
@@ -0,0 +1,26 @@
+package mage.sets;
+
+import mage.cards.ExpansionSet;
+import mage.constants.Rarity;
+import mage.constants.SetType;
+
+/**
+ * @author muz
+ */
+public final class AlchemySecretsOfStrixhaven extends ExpansionSet {
+
+    private static final AlchemySecretsOfStrixhaven instance = new AlchemySecretsOfStrixhaven();
+
+    public static AlchemySecretsOfStrixhaven getInstance() {
+        return instance;
+    }
+
+    private AlchemySecretsOfStrixhaven() {
+        super("Alchemy: Secrets of Strixhaven", "YSOS", ExpansionSet.buildDate(2026, 5, 19), SetType.MAGIC_ARENA);
+        this.blockName = "Alchemy";
+        this.hasBoosters = false;
+        this.hasBasicLands = false;
+
+        cards.add(new SetCardInfo("Grave Studies", 20, Rarity.UNCOMMON, mage.cards.g.GraveStudies.class));
+    }
+}
