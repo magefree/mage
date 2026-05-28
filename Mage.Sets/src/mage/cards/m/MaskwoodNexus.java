706a7e4fe0326a1ee6cd793e92c03d2f40e84374
matoro <12038583+matoro@users.noreply.github.com>
2026-05-28 00:21:37 -0400

    Centralize all-zone attribute modification effects (#14925)
    This centralizes into a pair of common effects the various cards with
    all-zone attribute modification effects e.g. Arcane Adaptation,
    Biotransference, Conspiracy, Leyline of Transformation, and Maskwood
    Nexus.  A few others will continue to work with no chnges.  Specifically
    I want to make sure that the code breaking objects into parts and
    applying individually to each part is always reused for any future
    implementations, so I've made the constructor fairly open-ended, taking
    an arbitrary modifier lambda.  All of these cards can now correctly take
    advantage of the shared code rather than having to implement it
    individually.
    
    Fixes https://github.com/magefree/mage/issues/14817
    
    Co-authored-by: matoro <matoro@users.noreply.github.com>

--- Mage.Sets/src/mage/cards/m/MaskwoodNexus.java
+++ Mage.Sets/src/mage/cards/m/MaskwoodNexus.java
@@ -1,33 +1,21 @@
 package mage.cards.m;
 
-import mage.MageItem;
-import mage.MageObject;
 import mage.abilities.Ability;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.costs.common.TapSourceCost;
 import mage.abilities.costs.mana.GenericManaCost;
-import mage.abilities.effects.ContinuousEffectImpl;
 import mage.abilities.effects.common.CreateTokenEffect;
-import mage.cards.Card;
+import mage.abilities.effects.common.continuous.ModifyObjectAllMultiZoneEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.*;
 import mage.filter.StaticFilters;
-import mage.game.Game;
-import mage.game.command.Commander;
-import mage.game.permanent.Permanent;
+import mage.filter.common.FilterControlledCreatureSpell;
+import mage.filter.common.FilterOwnedCreatureCard;
 import mage.game.permanent.token.ShapeshifterBlueToken;
-import mage.game.stack.Spell;
-import mage.game.stack.StackObject;
-import mage.players.Player;
-import mage.util.CardUtil;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 /**
  * @author TheElk801
@@ -56,16 +44,19 @@ public final class MaskwoodNexus extends CardImpl {
     public MaskwoodNexus copy() {
         return new MaskwoodNexus(this);
     }
+
 }
 
-class MaskwoodNexusEffect extends ContinuousEffectImpl {
+class MaskwoodNexusEffect extends ModifyObjectAllMultiZoneEffect {
 
     MaskwoodNexusEffect() {
-        super(Duration.WhileOnBattlefield, Layer.TypeChangingEffects_4, SubLayer.NA, Outcome.Benefit);
-        staticText = "Creatures you control are every creature type. "
-                + "The same is true for creature spells you control "
-                + "and creature cards you own that aren't on the battlefield.";
-        this.dependendToTypes.add(DependencyType.BecomeCreature);
+        super(
+            StaticFilters.FILTER_CONTROLLED_CREATURES,
+            new FilterControlledCreatureSpell("creature spells you control"),
+            new FilterOwnedCreatureCard("creature cards you own"),
+            (obj, source, game) -> { game.getState().getCreateMageObjectAttribute(obj, game).getSubtype().setIsAllCreatureTypes(true); },
+            "every creature type"
+        );
     }
 
     private MaskwoodNexusEffect(final MaskwoodNexusEffect effect) {
@@ -77,79 +68,4 @@ class MaskwoodNexusEffect extends ContinuousEffectImpl {
         return new MaskwoodNexusEffect(this);
     }
 
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Player controller = game.getPlayer(source.getControllerId());
-        if (controller == null) {
-            return false;
-        }
-        // Creature cards you own that aren't on the battlefield
-        // in graveyard
-        Set<Card> affectedCards = controller.getGraveyard().stream()
-                .map(game::getCard)
-                .filter(Objects::nonNull)
-                .collect(Collectors.toSet());
-        // on Hand
-        controller.getHand().stream()
-                .map(game::getCard)
-                .filter(Objects::nonNull)
-                .forEach(affectedCards::add);
-
-        // in Exile
-        game.getState().getExile().getCardsOwned(game, controller.getId()).stream()
-                .filter(card -> card.isOwnedBy(controller.getId()))
-                .forEach(affectedCards::add);
-
-        // in Library (e.g. for Mystical Teachings)
-        affectedCards.addAll(controller.getLibrary().getCards(game));
-
-        // commander in command zone
-        game.getState().getCommand().stream()
-                .filter(Commander.class::isInstance)
-                .map(MageItem::getId)
-                .map(game::getCard)
-                .filter(Objects::nonNull)
-                .filter(c -> c.isOwnedBy(controller.getId()))
-                .forEach(affectedCards::add);
-
-        // Apply to all affected cards by object parts
-        affectedCards.stream()
-                .map(card -> game.getObject(card.getId()))
-                .filter(Objects::nonNull)
-                .forEach(mageObject -> {
-                    if (mageObject.isCreature(game)) {
-                        game.getState().getCreateMageObjectAttribute(mageObject, game).getSubtype().setIsAllCreatureTypes(true);
-                    }
-                    CardUtil.getObjectParts(mageObject).stream()
-                            .filter(Objects::nonNull)
-                            .forEach(objectId -> {
-                                MageObject partObject = game.getObject(objectId);
-                                if (partObject != null && partObject.isCreature(game)) {
-                                    game.getState().getCreateMageObjectAttribute(partObject, game).getSubtype().setIsAllCreatureTypes(true);
-                                }
-                            });
-                });
-
-        // creature spells you control
-        for (StackObject stackObject : game.getStack()) {
-            if (stackObject instanceof Spell
-                    && stackObject.isControlledBy(source.getControllerId())
-                    && stackObject.isCreature(game)) {
-                Card card = ((Spell) stackObject).getCard();
-                game.getState().getCreateMageObjectAttribute(card, game).getSubtype().setIsAllCreatureTypes(true);
-            }
-        }
-
-        // creatures you control
-        List<Permanent> creatures = game.getBattlefield().getAllActivePermanents(
-                StaticFilters.FILTER_CONTROLLED_CREATURE, source.getControllerId(), game);
-        for (Permanent creature : creatures) {
-            if (creature != null) {
-                creature.setIsAllCreatureTypes(game, true);
-            }
-        }
-
-        return true;
-
-    }
 }
