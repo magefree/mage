f2cba7739f20798b6d623372c9cd850fb6562547
xenohedron <12538125+xenohedron@users.noreply.github.com>
2026-05-30 20:22:00 -0400

    fix Quintessential Katana (related to #14975)

--- Mage.Sets/src/mage/cards/q/QuintessentialKatana.java
+++ Mage.Sets/src/mage/cards/q/QuintessentialKatana.java
@@ -1,25 +1,21 @@
 package mage.cards.q;
 
-import java.util.UUID;
-import mage.constants.SubType;
-import mage.constants.Zone;
-import mage.filter.FilterPermanent;
 import mage.abilities.Ability;
 import mage.abilities.common.DealsCombatDamageTriggeredAbility;
 import mage.abilities.common.EntersBattlefieldControlledTriggeredAbility;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.effects.common.AttachEffect;
 import mage.abilities.effects.common.GainLifeEffect;
-import mage.abilities.effects.common.UntapAttachedEffect;
+import mage.abilities.effects.common.UntapSourceEffect;
 import mage.abilities.effects.common.continuous.BoostEquippedEffect;
 import mage.abilities.effects.common.continuous.GainAbilityAttachedEffect;
 import mage.abilities.keyword.EquipAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.constants.AttachmentType;
-import mage.constants.CardType;
-import mage.constants.Outcome;
-import mage.constants.SetTargetPointer;
+import mage.constants.*;
+import mage.filter.FilterPermanent;
+
+import java.util.UUID;
 
 /**
  *
@@ -37,7 +33,7 @@ public final class QuintessentialKatana extends CardImpl {
         // Equipped creature gets +1/+1 and has "Whenever this creature deals combat damage, untap it and you gain 2 life."
         Ability ability = new SimpleStaticAbility(new BoostEquippedEffect(1, 1));
         Ability gainedAbility = new DealsCombatDamageTriggeredAbility(
-            new UntapAttachedEffect().setText("untap it "), false
+            new UntapSourceEffect().setText("untap it "), false
         ).setTriggerPhrase("Whenever this creature deals combat damage, ");
         gainedAbility.addEffect(new GainLifeEffect(2).concatBy("and"));
         ability.addEffect(new GainAbilityAttachedEffect(gainedAbility, AttachmentType.EQUIPMENT
