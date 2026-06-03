5a33b87a8400b76982bb89831b219b893d2559da
Muz Ali <git@mustaqila.li>
2026-06-02 20:40:42 -0500

    [MSH] Implement The Scarlet Witch

--- Mage.Sets/src/mage/sets/MarvelSuperHeroes.java
+++ Mage.Sets/src/mage/sets/MarvelSuperHeroes.java
@@ -41,6 +41,9 @@ public final class MarvelSuperHeroes extends ExpansionSet {
         cards.add(new SetCardInfo("The Mind Stone", 21, Rarity.MYTHIC, mage.cards.t.TheMindStone.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("The Mind Stone", 385, Rarity.MYTHIC, mage.cards.t.TheMindStone.class, FULL_ART_USE_VARIOUS));
         cards.add(new SetCardInfo("The Mind Stone", 386, Rarity.MYTHIC, mage.cards.t.TheMindStone.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("The Scarlet Witch", 151, Rarity.RARE, mage.cards.t.TheScarletWitch.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("The Scarlet Witch", 368, Rarity.RARE, mage.cards.t.TheScarletWitch.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("The Scarlet Witch", 431, Rarity.RARE, mage.cards.t.TheScarletWitch.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("The Sentry, Golden Guardian", 35, Rarity.RARE, mage.cards.t.TheSentryGoldenGuardian.class));
         cards.add(new SetCardInfo("The Vision", 255, Rarity.RARE, mage.cards.t.TheVision.class));
         cards.add(new SetCardInfo("Thor Odinson", 234, Rarity.UNCOMMON, mage.cards.t.ThorOdinson.class));
