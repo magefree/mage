b57beef267a4cc9db0f6db76c8cd31525132f8e3
Muz Ali <git@mustaqila.li>
2026-06-06 15:50:31 -0500

    [MSH] Implement Epic Fight

--- Mage.Sets/src/mage/sets/MarvelSuperHeroes.java
+++ Mage.Sets/src/mage/sets/MarvelSuperHeroes.java
@@ -56,6 +56,8 @@ public final class MarvelSuperHeroes extends ExpansionSet {
         cards.add(new SetCardInfo("Elektra, Daughter of the Hand", 366, Rarity.RARE, mage.cards.e.ElektraDaughterOfTheHand.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Elektra, Daughter of the Hand", 395, Rarity.RARE, mage.cards.e.ElektraDaughterOfTheHand.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Elektra, Daughter of the Hand", 97, Rarity.RARE, mage.cards.e.ElektraDaughterOfTheHand.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Epic Fight", 166, Rarity.RARE, mage.cards.e.EpicFight.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Epic Fight", 351, Rarity.RARE, mage.cards.e.EpicFight.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Forest", 285, Rarity.LAND, mage.cards.basiclands.Forest.class, FULL_ART_BFZ_VARIOUS));
         cards.add(new SetCardInfo("Forest", 286, Rarity.LAND, mage.cards.basiclands.Forest.class, FULL_ART_BFZ_VARIOUS));
         cards.add(new SetCardInfo("Forest", 295, Rarity.LAND, mage.cards.basiclands.Forest.class, NON_FULL_USE_VARIOUS));
