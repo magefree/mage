7a924dfe6ef3365e99f3ab7a05493ba79067d199
Muz <muz@users.noreply.github.com>
2026-06-08 17:24:22 -0500

    [MSH] Implement Agents of HYDRA (#15173)

--- Mage.Sets/src/mage/sets/MarvelSuperHeroes.java
+++ Mage.Sets/src/mage/sets/MarvelSuperHeroes.java
@@ -29,6 +29,7 @@ public final class MarvelSuperHeroes extends ExpansionSet {
         cards.add(new SetCardInfo("Agent Phil Coulson", 4, Rarity.RARE, mage.cards.a.AgentPhilCoulson.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Agent Phil Coulson", 402, Rarity.RARE, mage.cards.a.AgentPhilCoulson.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Agent of Atlas", 3, Rarity.COMMON, mage.cards.a.AgentOfAtlas.class));
+        cards.add(new SetCardInfo("Agents of HYDRA", 85, Rarity.COMMON, mage.cards.a.AgentsOfHYDRA.class));
         cards.add(new SetCardInfo("Arc Reactor", 243, Rarity.RARE, mage.cards.a.ArcReactor.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Arc Reactor", 310, Rarity.RARE, mage.cards.a.ArcReactor.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Armor Wars", 203, Rarity.RARE, mage.cards.a.ArmorWars.class, NON_FULL_USE_VARIOUS));
