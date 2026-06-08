d3d78a5e7a9f36629feec3b2cc8b8c738486b716
Muz <muz@users.noreply.github.com>
2026-06-08 18:27:47 -0500

    [MSH] Implement Vision of Love (#15115)

---
+++ Mage.Sets/src/mage/cards/v/VisionOfLove.java
@@ -0,0 +1,45 @@
+
+
+package mage.cards.v;
+
+import java.util.UUID;
+
+import mage.abilities.costs.OrCost;
+import mage.abilities.costs.common.DiscardCardCost;
+import mage.abilities.costs.common.SacrificeTargetCost;
+import mage.abilities.effects.common.DoIfCostPaid;
+import mage.abilities.effects.common.DrawCardSourceControllerEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.filter.StaticFilters;
+
+/**
+ *
+ * @author muz
+ */
+public final class VisionOfLove extends CardImpl {
+
+    public VisionOfLove(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId,setInfo,new CardType[]{CardType.SORCERY},"{1}{R}");
+
+        // You may sacrifice an artifact or discard a card. If you do, draw two cards.
+        this.getSpellAbility().addEffect(new DoIfCostPaid(
+                new DrawCardSourceControllerEffect(2),
+                new OrCost(
+                    "sacrifice an artifact or discard a card",
+                    new SacrificeTargetCost(StaticFilters.FILTER_PERMANENT_ARTIFACT),
+                    new DiscardCardCost()
+                )
+        ));
+    }
+
+    private VisionOfLove(final VisionOfLove card) {
+        super(card);
+    }
+
+    @Override
+    public VisionOfLove copy() {
+        return new VisionOfLove(this);
+    }
+}
