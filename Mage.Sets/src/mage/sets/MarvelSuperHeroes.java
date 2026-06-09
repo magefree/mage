d2bc4e99987c799973f4d3ecf7abeae16a1308f9
Muz <muz@users.noreply.github.com>
2026-06-09 08:54:45 -0500

    [MSH] Implement Monica Rambeau (#15062)

--- Mage.Sets/src/mage/sets/MarvelSuperHeroes.java
+++ Mage.Sets/src/mage/sets/MarvelSuperHeroes.java
@@ -131,6 +131,9 @@ public final class MarvelSuperHeroes extends ExpansionSet {
         cards.add(new SetCardInfo("M.O.D.O.K.", 106, Rarity.MYTHIC, mage.cards.m.MODOK.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("M.O.D.O.K.", 408, Rarity.MYTHIC, mage.cards.m.MODOK.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Mockingbird, Ace Agent", 22, Rarity.UNCOMMON, mage.cards.m.MockingbirdAceAgent.class));
+        cards.add(new SetCardInfo("Monica Rambeau", 23, Rarity.MYTHIC, mage.cards.m.MonicaRambeau.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Monica Rambeau", 345, Rarity.MYTHIC, mage.cards.m.MonicaRambeau.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Monica Rambeau", 356, Rarity.MYTHIC, mage.cards.m.MonicaRambeau.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Moon Girl and Devil Dinosaur", 223, Rarity.RARE, mage.cards.m.MoonGirlAndDevilDinosaur.class));
         cards.add(new SetCardInfo("Mountain", 440, Rarity.LAND, mage.cards.basiclands.Mountain.class, FULL_ART_BFZ_VARIOUS));
         cards.add(new SetCardInfo("Mountain", 439, Rarity.LAND, mage.cards.basiclands.Mountain.class, FULL_ART_BFZ_VARIOUS));
