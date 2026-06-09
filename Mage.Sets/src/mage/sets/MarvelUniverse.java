b6e184d45e2521f4617ac7761ba21e43a3c8fd56
Muz Ali <git@mustaqila.li>
2026-06-08 23:35:18 -0500

    [MAR] Add Archangel of Thune reprint

--- Mage.Sets/src/mage/sets/MarvelUniverse.java
+++ Mage.Sets/src/mage/sets/MarvelUniverse.java
@@ -26,6 +26,7 @@ public class MarvelUniverse extends ExpansionSet {
         cards.add(new SetCardInfo("Anthem of Champions", 85, Rarity.MYTHIC, mage.cards.a.AnthemOfChampions.class));
         cards.add(new SetCardInfo("Arachnogenesis", 31, Rarity.MYTHIC, mage.cards.a.Arachnogenesis.class));
         cards.add(new SetCardInfo("Arasta of the Endless Web", 32, Rarity.MYTHIC, mage.cards.a.ArastaOfTheEndlessWeb.class));
+        cards.add(new SetCardInfo("Archangel of Thune", 41, Rarity.MYTHIC, mage.cards.a.ArchangelOfThune.class));
         cards.add(new SetCardInfo("Aurelia the Warleader", 86, Rarity.MYTHIC, mage.cards.a.AureliaTheWarleader.class));
         cards.add(new SetCardInfo("Beast Within", 33, Rarity.MYTHIC, mage.cards.b.BeastWithin.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Beast Within", 75, Rarity.MYTHIC, mage.cards.b.BeastWithin.class, NON_FULL_USE_VARIOUS));
