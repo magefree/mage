34e9b7680345ed0130c13e3c669b1dc529539d9a
Muz Ali <git@mustaqila.li>
2026-06-11 16:30:39 -0500

    [MSC] Implement Amazing Spider-Girl

--- Mage.Sets/src/mage/sets/MarvelSuperHeroesCommander.java
+++ Mage.Sets/src/mage/sets/MarvelSuperHeroesCommander.java
@@ -23,6 +23,7 @@ public final class MarvelSuperHeroesCommander extends ExpansionSet {
         cards.add(new SetCardInfo("A.I.M. Bot", 529, Rarity.COMMON, mage.cards.a.AIMBot.class));
         cards.add(new SetCardInfo("Abomination, World Ravager", 47, Rarity.RARE, mage.cards.a.AbominationWorldRavager.class));
         cards.add(new SetCardInfo("Alien Symbiosis", 791, Rarity.UNCOMMON, mage.cards.a.AlienSymbiosis.class));
+        cards.add(new SetCardInfo("Amazing Spider-Girl", 583, Rarity.RARE, mage.cards.a.AmazingSpiderGirl.class));
         cards.add(new SetCardInfo("Annie Joins Up", 281, Rarity.RARE, mage.cards.a.AnnieJoinsUp.class));
         cards.add(new SetCardInfo("Ant-Man, Scott Lang", 834, Rarity.COMMON, mage.cards.a.AntManScottLang.class));
         cards.add(new SetCardInfo("Arcane Denial", 147, Rarity.COMMON, mage.cards.a.ArcaneDenial.class));
