d5f1a2efd4613b7baa78b56ac5af86593e1230ad
Muz <muz@users.noreply.github.com>
2026-06-11 19:23:18 -0500

    [MSH] Implement Luke Cage, Power Man (#15051)

--- Mage.Sets/src/mage/sets/MarvelSuperHeroes.java
+++ Mage.Sets/src/mage/sets/MarvelSuperHeroes.java
@@ -147,6 +147,7 @@ public final class MarvelSuperHeroes extends ExpansionSet {
         cards.add(new SetCardInfo("Kree Sentinel", 141, Rarity.COMMON, mage.cards.k.KreeSentinel.class));
         cards.add(new SetCardInfo("Lightning Strike", 142, Rarity.COMMON, mage.cards.l.LightningStrike.class));
         cards.add(new SetCardInfo("Los Diablos Missile Base", 270, Rarity.COMMON, mage.cards.l.LosDiablosMissileBase.class));
+        cards.add(new SetCardInfo("Luke Cage, Power Man", 20, Rarity.UNCOMMON, mage.cards.l.LukeCagePowerMan.class));
         cards.add(new SetCardInfo("Machinesmith Automaton", 144, Rarity.COMMON, mage.cards.m.MachinesmithAutomaton.class));
         cards.add(new SetCardInfo("Madame Hydra", 221, Rarity.UNCOMMON, mage.cards.m.MadameHydra.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Madame Hydra", 318, Rarity.UNCOMMON, mage.cards.m.MadameHydra.class, NON_FULL_USE_VARIOUS));
