92523fef34852c53ae50911d66377f38e39c284e
Muz <muz@users.noreply.github.com>
2026-06-11 21:40:07 -0500

    [MSC] Implement X-23, Deadly Weapon (#15218)

--- Mage.Sets/src/mage/sets/MarvelSuperHeroesCommander.java
+++ Mage.Sets/src/mage/sets/MarvelSuperHeroesCommander.java
@@ -254,6 +254,7 @@ public final class MarvelSuperHeroesCommander extends ExpansionSet {
         cards.add(new SetCardInfo("Wild Pack Squad", 781, Rarity.COMMON, mage.cards.w.WildPackSquad.class));
         cards.add(new SetCardInfo("Winter Soldier, Bucky Barnes", 845, Rarity.COMMON, mage.cards.w.WinterSoldierBuckyBarnes.class));
         cards.add(new SetCardInfo("Wisecrack", 813, Rarity.UNCOMMON, mage.cards.w.Wisecrack.class));
+        cards.add(new SetCardInfo("X-23, Deadly Weapon", 738, Rarity.UNCOMMON, mage.cards.x.X23DeadlyWeapon.class));
         cards.add(new SetCardInfo("Zarda, the Power Princess", 615, Rarity.UNCOMMON, mage.cards.z.ZardaThePowerPrincess.class));
         cards.add(new SetCardInfo("Zuri, Warrior of Wakanda", 72, Rarity.RARE, mage.cards.z.ZuriWarriorOfWakanda.class));
 
