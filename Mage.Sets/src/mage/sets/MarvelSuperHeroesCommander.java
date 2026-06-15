daf7128deab507736e0c66be5e11d9406738227d
Muz <muz@users.noreply.github.com>
2026-06-15 18:24:18 -0500

    [MSC] Implement Infinity Formula (#15328)

--- Mage.Sets/src/mage/sets/MarvelSuperHeroesCommander.java
+++ Mage.Sets/src/mage/sets/MarvelSuperHeroesCommander.java
@@ -236,6 +236,7 @@ public final class MarvelSuperHeroesCommander extends ExpansionSet {
         cards.add(new SetCardInfo("Iceman and Firestar", 742, Rarity.UNCOMMON, mage.cards.i.IcemanAndFirestar.class));
         cards.add(new SetCardInfo("Imperial Cosmographer", 624, Rarity.UNCOMMON, mage.cards.i.ImperialCosmographer.class));
         cards.add(new SetCardInfo("Infernal Rebirth", 537, Rarity.COMMON, mage.cards.i.InfernalRebirth.class));
+        cards.add(new SetCardInfo("Infinity Formula", 601, Rarity.COMMON, mage.cards.i.InfinityFormula.class));
         cards.add(new SetCardInfo("Ingenious Smith", 135, Rarity.UNCOMMON, mage.cards.i.IngeniousSmith.class));
         cards.add(new SetCardInfo("Inner Demons Gangsters", 797, Rarity.COMMON, mage.cards.i.InnerDemonsGangsters.class));
         cards.add(new SetCardInfo("Inspired Fire", 690, Rarity.COMMON, mage.cards.i.InspiredFire.class));
