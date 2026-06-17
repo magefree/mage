11768e5948783e44c97201204fad7f6534327541
Muz <muz@users.noreply.github.com>
2026-06-17 17:04:11 -0500

    [MSC] Implement The Wasp, Winsome Avenger (#15386)

--- Mage.Sets/src/mage/sets/MarvelSuperHeroesCommander.java
+++ Mage.Sets/src/mage/sets/MarvelSuperHeroesCommander.java
@@ -565,6 +565,8 @@ public final class MarvelSuperHeroesCommander extends ExpansionSet {
         cards.add(new SetCardInfo("The Squadron Sinister", 96, Rarity.RARE, mage.cards.t.TheSquadronSinister.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("The Thing", 4, Rarity.MYTHIC, mage.cards.t.TheThing.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("The Thing", 878, Rarity.MYTHIC, mage.cards.t.TheThing.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("The Wasp, Winsome Avenger", 343, Rarity.RARE, mage.cards.t.TheWaspWinsomeAvenger.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("The Wasp, Winsome Avenger", 39, Rarity.RARE, mage.cards.t.TheWaspWinsomeAvenger.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("The Whizzer, Classic Speedster", 710, Rarity.UNCOMMON, mage.cards.t.TheWhizzerClassicSpeedster.class));
         cards.add(new SetCardInfo("Think Twice", 787, Rarity.COMMON, mage.cards.t.ThinkTwice.class));
         cards.add(new SetCardInfo("Thor, Guardian of Midgard", 503, Rarity.RARE, mage.cards.t.ThorGuardianOfMidgard.class));
