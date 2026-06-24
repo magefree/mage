ddcc5f6b44fc1fd6efa4c51fd9ecdf8c61aa2b53
Muz Ali <git@mustaqila.li>
2026-06-23 21:39:59 -0500

    [MSH] Implement Swordsman, Sharp Scoundrel

--- Mage.Sets/src/mage/sets/MarvelSuperHeroes.java
+++ Mage.Sets/src/mage/sets/MarvelSuperHeroes.java
@@ -256,6 +256,7 @@ public final class MarvelSuperHeroes extends ExpansionSet {
         cards.add(new SetCardInfo("Super Villain Lockup", 37, Rarity.UNCOMMON, mage.cards.s.SuperVillainLockup.class));
         cards.add(new SetCardInfo("Super-Skrull", 115, Rarity.RARE, mage.cards.s.SuperSkrull.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Super-Skrull", 409, Rarity.RARE, mage.cards.s.SuperSkrull.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Swordsman, Sharp Scoundrel", 116, Rarity.UNCOMMON, mage.cards.s.SwordsmanSharpScoundrel.class));
         cards.add(new SetCardInfo("Surveillance Room", 274, Rarity.COMMON, mage.cards.s.SurveillanceRoom.class));
         cards.add(new SetCardInfo("Swamp", 438, Rarity.LAND, mage.cards.basiclands.Swamp.class, FULL_ART_BFZ_VARIOUS));
         cards.add(new SetCardInfo("Swamp", 437, Rarity.LAND, mage.cards.basiclands.Swamp.class, FULL_ART_BFZ_VARIOUS));
