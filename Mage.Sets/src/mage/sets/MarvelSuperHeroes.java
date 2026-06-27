9dc1b0a41d5a7f7fe7606346c4483c17aecd9e60
Muz <muz@users.noreply.github.com>
2026-06-26 22:38:00 -0500

    [MSH] Implement Jennifer Walters (#15077)

--- Mage.Sets/src/mage/sets/MarvelSuperHeroes.java
+++ Mage.Sets/src/mage/sets/MarvelSuperHeroes.java
@@ -170,6 +170,10 @@ public final class MarvelSuperHeroes extends ExpansionSet {
         cards.add(new SetCardInfo("Island", 279, Rarity.LAND, mage.cards.basiclands.Island.class, FULL_ART_BFZ_VARIOUS));
         cards.add(new SetCardInfo("Island", 289, Rarity.LAND, mage.cards.basiclands.Island.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Island", 290, Rarity.LAND, mage.cards.basiclands.Island.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Jennifer Walters", 18, Rarity.MYTHIC, mage.cards.j.JenniferWalters.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Jennifer Walters", 328, Rarity.MYTHIC, mage.cards.j.JenniferWalters.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Jennifer Walters", 355, Rarity.MYTHIC, mage.cards.j.JenniferWalters.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Jennifer Walters", 388, Rarity.MYTHIC, mage.cards.j.JenniferWalters.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("K'un-Lun Warrior", 140, Rarity.COMMON, mage.cards.k.KunLunWarrior.class));
         cards.add(new SetCardInfo("Ka-Zar of the Savage Land", 174, Rarity.UNCOMMON, mage.cards.k.KaZarOfTheSavageLand.class));
         cards.add(new SetCardInfo("Kang, Temporal Tyrant", 217, Rarity.UNCOMMON, mage.cards.k.KangTemporalTyrant.class, NON_FULL_USE_VARIOUS));
