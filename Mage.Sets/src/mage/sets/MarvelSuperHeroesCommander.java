ae52855320795167e0b51d21a346a6956fae2912
Muz Ali <git@mustaqila.li>
2026-07-05 21:35:59 -0500

    [MSC] Implement T'Chaka, Venerable King

--- Mage.Sets/src/mage/sets/MarvelSuperHeroesCommander.java
+++ Mage.Sets/src/mage/sets/MarvelSuperHeroesCommander.java
@@ -543,6 +543,8 @@ public final class MarvelSuperHeroesCommander extends ExpansionSet {
         cards.add(new SetCardInfo("Sword of the Animist", 452, Rarity.RARE, mage.cards.s.SwordOfTheAnimist.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Swords to Plowshares", 143, Rarity.UNCOMMON, mage.cards.s.SwordsToPlowshares.class));
         cards.add(new SetCardInfo("Syphon Mind", 159, Rarity.COMMON, mage.cards.s.SyphonMind.class));
+        cards.add(new SetCardInfo("T'Chaka, Venerable King", 419, Rarity.RARE, mage.cards.t.TChakaVenerableKing.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("T'Chaka, Venerable King", 98, Rarity.RARE, mage.cards.t.TChakaVenerableKing.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("T'Challa, the Black Panther", 7, Rarity.MYTHIC, mage.cards.t.TChallaTheBlackPanther.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("T'Challa, the Black Panther", 881, Rarity.MYTHIC, mage.cards.t.TChallaTheBlackPanther.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("TVA Bureaucrat", 761, Rarity.COMMON, mage.cards.t.TVABureaucrat.class));
