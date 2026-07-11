ca75faa3dd517ffa4e532c5e736fa9a024013b1e
Oleg Agafonov <jaydi85@gmail.com>
2026-07-11 19:52:07 +0000

    Baron Helmut Zemo - fixed that it was able to cast cards from previous boast (#15240)

--- Mage.Tests/src/test/java/org/mage/test/cards/single/msh/BaronHelmutZemoTest.java
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/msh/BaronHelmutZemoTest.java
@@ -4,6 +4,9 @@ import mage.constants.PhaseStep;
 import mage.constants.Zone;
 import mage.counters.CounterType;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.mage.test.player.TestPlayer;
 import org.mage.test.serverside.base.CardTestPlayerBase;
@@ -43,7 +46,7 @@ public class BaronHelmutZemoTest extends CardTestPlayerBase {
     }
 
     @Test
-    public void test_Boast() {
+    public void test_Boast_FirstTime() {
         skipInitShuffling();
 
         // Whenever you cast a black spell from your hand, Baron Helmut Zemo connives.
@@ -84,5 +87,51 @@ public class BaronHelmutZemoTest extends CardTestPlayerBase {
 
         assertLife(playerA, 20 + 3);
         assertLife(playerB, 20 - 3 - 3); // 3 from attack, 3 from x3 free casts
+    }
+
+    @Test
+    public void test_Boast_SecondTime() {
+        skipInitShuffling();
+
+        // Whenever you cast a black spell from your hand, Baron Helmut Zemo connives.
+        //
+        // Boast — Exile any number of black cards from your graveyard with fifteen or more black mana symbols 
+        // among their mana costs: Copy those exiled cards. You may cast up to three of the copies without paying 
+        // their mana costs. (Activate only if this creature attacked this turn and only once each turn.)
+        addCard(Zone.BATTLEFIELD, playerA, "Baron Helmut Zemo");
+        addCard(Zone.GRAVEYARD, playerA, "Abomination", 10); // for 1 turn, {3}{B}{B}
+        addCard(Zone.GRAVEYARD, playerA, "Alpharael, Stonechosen", 10); // for 3 turn {3}{B}{B}
+
+        // turn 1 - boast but don't use free case
+        attack(1, playerA, "Baron Helmut Zemo");
+        activateAbility(1, PhaseStep.POSTCOMBAT_MAIN, playerA, "Boast");
+        setChoice(playerA, "Abomination", 10);
+        setChoice(playerA, TestPlayer.CHOICE_SKIP); // stop to cards choose
+        setChoice(playerA, TestPlayer.CHOICE_SKIP); // do not use free cast
+        setChoice(playerA, false); // stop to free cast
+
+        // turn 3 - make you have only new free cast
+        attack(3, playerA, "Baron Helmut Zemo");
+        activateAbility(3, PhaseStep.POSTCOMBAT_MAIN, playerA, "Boast");
+        setChoice(playerA, "Alpharael, Stonechosen", 10);
+        // cast free 1
+        setChoice(playerA, "Alpharael, Stonechosen"); // cast free - choose card
+        setChoice(playerA, true); // cast free - confirm
+        // cast free 3
+        setChoice(playerA, "Abomination"); // cast free - choose card
+
+        // TODO: implement possible targets check/assert
+        setStrictChooseMode(true);
+        try {
+            setStopAt(3, PhaseStep.END_TURN);
+            execute();
+        } catch (AssertionError e) {
+            Assert.assertTrue(
+                    "catch wrong exception: " + e.getMessage(),
+                    e.getMessage().contains("Found wrong choice command") && e.getMessage().contains("Abomination")
+            );
+            return;
+        }
+        fail("Was able to pick [[Abomination]] but it must be unavailable on second boast");
     }
 }
