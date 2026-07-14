25f127d1379ca975a32c840772fb4cd60a6a36cf
Muz Ali <git@mustaqila.li>
2026-07-14 17:48:43 -0500

    [TRC] Implement Tenacious Tosk

--- Mage.Sets/src/mage/sets/StarTrekCommander.java
+++ Mage.Sets/src/mage/sets/StarTrekCommander.java
@@ -21,6 +21,7 @@ public final class StarTrekCommander extends ExpansionSet {
 
         cards.add(new SetCardInfo("Benjamin Sisko, Besieged", 200, Rarity.MYTHIC, mage.cards.b.BenjaminSiskoBesieged.class));
         cards.add(new SetCardInfo("Spock, Logical Choice", 194, Rarity.MYTHIC, mage.cards.s.SpockLogicalChoice.class));
+        cards.add(new SetCardInfo("Tenacious Tosk", 201, Rarity.RARE, mage.cards.t.TenaciousTosk.class));
         cards.add(new SetCardInfo("Trelane, Squire of Gothos", 195, Rarity.RARE, mage.cards.t.TrelaneSquireOfGothos.class));
     }
 }
