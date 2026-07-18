4cc531e128f6e1922787b4b44b9df176b1a105ba
Muz Ali <git@mustaqila.li>
2026-07-18 16:54:36 -0500

    Make the Enduring Story hint conditionally say if you have it, or how far along you are towards earning it

--- Mage/src/main/java/mage/abilities/hint/common/EnduringStoryHint.java
+++ Mage/src/main/java/mage/abilities/hint/common/EnduringStoryHint.java
@@ -2,9 +2,16 @@ package mage.abilities.hint.common;
 
 import mage.abilities.Ability;
 import mage.abilities.condition.common.EnduringStoryCondition;
-import mage.abilities.hint.ConditionHint;
 import mage.abilities.hint.Hint;
+import mage.constants.CardType;
+import mage.constants.SuperType;
+import mage.constants.SubType;
+import mage.filter.common.FilterControlledPermanent;
+import mage.filter.predicate.Predicates;
 import mage.game.Game;
+import mage.players.Player;
+
+import java.util.Arrays;
 
 /**
  * @author muz
@@ -12,11 +19,25 @@ import mage.game.Game;
 public enum EnduringStoryHint implements Hint {
 
     instance;
-    private static final ConditionHint hint = new ConditionHint(EnduringStoryCondition.instance, "You have an enduring story");
+    private static final FilterControlledPermanent filter = new FilterControlledPermanent("artifacts, legendaries, and/or Sagas");
+
+    static {
+        filter.add(Predicates.or(Arrays.asList(
+            CardType.ARTIFACT.getPredicate(),
+            SuperType.LEGENDARY.getPredicate(),
+            SubType.SAGA.getPredicate()
+        )));
+    }
 
     @Override
     public String getText(Game game, Ability ability) {
-        return hint.getText(game, ability);
+        Player controller = game.getPlayer(ability.getControllerId());
+        if (EnduringStoryCondition.instance.apply(game, ability)) {
+            return "You have an enduring story";
+        }
+
+        int count = game.getBattlefield().countAll(filter, controller.getId(), game);
+        return "You don't have an enduring story (controlled artifacts/sagas/legendaries: " + count + " of 3)";
     }
 
     @Override
