e644bb61fdaa2b354866b65babb60331637914cc
Muz Ali <git@mustaqila.li>
2026-07-19 08:25:33 -0500

    Include color icons for Enduring Story designation hints

--- Mage/src/main/java/mage/abilities/hint/common/EnduringStoryHint.java
+++ Mage/src/main/java/mage/abilities/hint/common/EnduringStoryHint.java
@@ -3,6 +3,7 @@ package mage.abilities.hint.common;
 import mage.abilities.Ability;
 import mage.abilities.condition.common.EnduringStoryCondition;
 import mage.abilities.hint.Hint;
+import mage.abilities.hint.HintUtils;
 import mage.constants.CardType;
 import mage.constants.SuperType;
 import mage.constants.SubType;
@@ -32,12 +33,16 @@ public enum EnduringStoryHint implements Hint {
     @Override
     public String getText(Game game, Ability ability) {
         Player controller = game.getPlayer(ability.getControllerId());
-        if (EnduringStoryCondition.instance.apply(game, ability)) {
-            return "You have an enduring story";
+        boolean hasEnduringStory = EnduringStoryCondition.instance.apply(game, ability);
+        if (hasEnduringStory) {
+            return HintUtils.prepareText("You have an enduring story", null, HintUtils.HINT_ICON_GOOD);
         }
 
-        int count = game.getBattlefield().countAll(filter, controller.getId(), game);
-        return "You don't have an enduring story (controlled artifacts/sagas/legendaries: " + count + " of 3)";
+        int count = controller == null ? 0 : game.getBattlefield().countAll(filter, controller.getId(), game);
+        return HintUtils.prepareText(
+                "You don't have an enduring story (controlled artifacts/legendaries/sagas: " + count + " of 3)",
+                null, HintUtils.HINT_ICON_BAD
+        );
     }
 
     @Override
