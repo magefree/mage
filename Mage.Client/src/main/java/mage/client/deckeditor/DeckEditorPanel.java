0f8de6d127685cb3973c918db1c7413194895763
Oleg Agafonov <jaydi85@gmail.com>
2026-07-23 06:05:00 +0000

    GUI, deck editor: fixed broken/empty deck on too fast sideboarding e.g. instant loose (close #11877)

--- Mage.Client/src/main/java/mage/client/deckeditor/DeckEditorPanel.java
+++ Mage.Client/src/main/java/mage/client/deckeditor/DeckEditorPanel.java
@@ -24,7 +24,6 @@ import mage.client.util.Listener;
 import mage.client.util.audio.AudioManager;
 import mage.components.CardInfoPane;
 import mage.game.GameException;
-import mage.remote.Session;
 import mage.util.DeckUtil;
 import mage.util.ThreadUtils;
 import mage.util.XmageThreadFactory;
@@ -33,6 +32,7 @@ import mage.view.SimpleCardView;
 import org.apache.log4j.Logger;
 
 import javax.swing.*;
+import javax.swing.Timer;
 import javax.swing.border.Border;
 import javax.swing.filechooser.FileFilter;
 import java.awt.*;
@@ -64,7 +64,7 @@ public class DeckEditorPanel extends javax.swing.JPanel {
     private DeckEditorMode mode;
     private int timeout;
     private javax.swing.Timer countdown;
-    private UpdateDeckTask updateDeckTask;
+    private javax.swing.Timer updateDeckTimer;
     private int timeToSubmit = -1;
 
     public DeckEditorPanel() {
@@ -88,8 +88,8 @@ public class DeckEditorPanel extends javax.swing.JPanel {
                     if (--timeout > 0) {
                         setTimeout(timeout);
                     } else {
-                        if (updateDeckTask != null) {
-                            updateDeckTask.cancel(true);
+                        if (updateDeckTimer != null && updateDeckTimer.isRunning()) {
+                            updateDeckTimer.stop();
                         }
                         setTimeout(0);
                         countdown.stop();
@@ -148,9 +148,17 @@ public class DeckEditorPanel extends javax.swing.JPanel {
      */
     public void cleanUp() {
         saveDividerLocationsAndDeckAreaSettings();
-        if (updateDeckTask != null) {
-            updateDeckTask.cancel(true);
+
+        if (updateDeckTimer != null) {
+            if (updateDeckTimer.isRunning()) {
+                updateDeckTimer.stop();
+            }
+            for (ActionListener al : updateDeckTimer.getActionListeners()) {
+                updateDeckTimer.removeActionListener(al);
+            }
+            updateDeckTimer = null;
         }
+
         if (countdown != null) {
             if (countdown.isRunning()) {
                 countdown.stop();
@@ -158,7 +166,9 @@ public class DeckEditorPanel extends javax.swing.JPanel {
             for (ActionListener al : countdown.getActionListeners()) {
                 countdown.removeActionListener(al);
             }
+            countdown = null;
         }
+
         this.cardSelector.cleanUp();
         this.deckArea.cleanUp();
 
@@ -241,10 +251,19 @@ public class DeckEditorPanel extends javax.swing.JPanel {
                 setTimeout(timeout);
                 if (timeout != 0) {
                     countdown.start();
-                    if (updateDeckTask == null || updateDeckTask.isDone()) {
-                        updateDeckTask = new UpdateDeckTask(SessionHandler.getSession(), currentTableId, deck);
-                        updateDeckTask.execute();
+                    if (updateDeckTimer != null && updateDeckTimer.isRunning()) {
+                        updateDeckTimer.stop();
                     }
+                    updateDeckTimer = new Timer(1000, action -> {
+                        try {
+                            // must run in gui thread only
+                            SessionHandler.updateDeck(currentTableId, deck.prepareCardsOnlyDeck());
+                        } catch (Throwable e) {
+                            logger.error("Can't send current deck to server: " + e);
+                        }
+                    });
+                    updateDeckTimer.setRepeats(true);
+                    updateDeckTimer.start();
                 }
                 break;
             case FREE_BUILDING:
@@ -1519,8 +1538,8 @@ public class DeckEditorPanel extends javax.swing.JPanel {
     }//GEN-LAST:event_btnGenDeckActionPerformed
 
     private void btnSubmitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSubmitActionPerformed
-        if (updateDeckTask != null) {
-            updateDeckTask.cancel(true);
+        if (updateDeckTimer != null && updateDeckTimer.isRunning()) {
+            updateDeckTimer.stop();
         }
 
         if (SessionHandler.submitDeck(mode, currentTableId, deck.prepareCardsOnlyDeck())) {
@@ -1538,8 +1557,8 @@ public class DeckEditorPanel extends javax.swing.JPanel {
 
         // TODO: need code and feature review. It sends deck every minute -- is it useless? There is another feature with auto-save
         executorService.schedule(() -> {
-            if (updateDeckTask != null) {
-                updateDeckTask.cancel(true);
+            if (updateDeckTimer != null && updateDeckTimer.isRunning()) {
+                updateDeckTimer.stop();
             }
 
             if (SessionHandler.submitDeck(mode, currentTableId, deck.prepareCardsOnlyDeck())) {
@@ -1635,38 +1654,5 @@ class ImportFilter extends FileFilter {
     @Override
     public String getDescription() {
         return "All formats (*.dec; *.mwDeck; *.txt; *.dek; *.cod; *.o8d; *.json; *.draft; *.mtga;)";
-    }
-}
-
-class UpdateDeckTask extends SwingWorker<Void, Void> {
-
-    private static final Logger logger = Logger.getLogger(UpdateDeckTask.class);
-    private final Session session;
-    private final UUID tableId;
-    private final Deck deck;
-
-    UpdateDeckTask(Session session, UUID tableId, Deck deck) {
-        this.session = session;
-        this.tableId = tableId;
-        this.deck = deck;
-    }
-
-    @Override
-    protected Void doInBackground() throws Exception {
-        while (!isCancelled()) {
-            SessionHandler.updateDeck(tableId, deck.prepareCardsOnlyDeck());
-            TimeUnit.SECONDS.sleep(5);
-        }
-        return null;
-    }
-
-    @Override
-    protected void done() {
-        try {
-            get();
-        } catch (InterruptedException | ExecutionException ex) {
-            logger.fatal("Update Matches Task error", ex);
-        } catch (CancellationException ex) {
-        }
     }
 }
