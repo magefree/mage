833b2426ab4cff3b24b9c10ca751c61c61e2b4f1
Muz Ali <git@mustaqila.li>
2026-07-30 22:47:52 -0500

    [HOB] Implement Fili the Pathfinder

--- Mage.Sets/src/mage/sets/TheHobbit.java
+++ Mage.Sets/src/mage/sets/TheHobbit.java
@@ -37,6 +37,7 @@ public final class TheHobbit extends ExpansionSet {
         cards.add(new SetCardInfo("Desolation of Smaug", 262, Rarity.RARE, mage.cards.d.DesolationOfSmaug.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Desolation of Smaug", 93, Rarity.RARE, mage.cards.d.DesolationOfSmaug.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Fearsome Goblin Pair", 156, Rarity.UNCOMMON, mage.cards.f.FearsomeGoblinPair.class));
+        cards.add(new SetCardInfo("Fili the Pathfinder", 14, Rarity.RARE, mage.cards.f.FiliThePathfinder.class));
         cards.add(new SetCardInfo("Forest", 198, Rarity.LAND, mage.cards.basiclands.Forest.class, FULL_ART));
         cards.add(new SetCardInfo("Gathering of Darkness", 68, Rarity.UNCOMMON, mage.cards.g.GatheringOfDarkness.class));
         cards.add(new SetCardInfo("Gigantic Big Bear", 126, Rarity.RARE, mage.cards.g.GiganticBigBear.class, NON_FULL_USE_VARIOUS));
