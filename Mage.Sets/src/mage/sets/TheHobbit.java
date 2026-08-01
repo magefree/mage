ba96188b940fa69bc848d4c63bc3f4c2337910a4
Muz <muz@users.noreply.github.com>
2026-08-01 10:06:25 -0400

    [HOB] Implement Down in the Valley (#15776)

--- Mage.Sets/src/mage/sets/TheHobbit.java
+++ Mage.Sets/src/mage/sets/TheHobbit.java
@@ -48,6 +48,7 @@ public final class TheHobbit extends ExpansionSet {
         cards.add(new SetCardInfo("Desolation of Smaug", 262, Rarity.RARE, mage.cards.d.DesolationOfSmaug.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Desolation of Smaug", 93, Rarity.RARE, mage.cards.d.DesolationOfSmaug.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Dori, Bearer of Friends", 94, Rarity.COMMON, mage.cards.d.DoriBearerOfFriends.class));
+        cards.add(new SetCardInfo("Down in the Valley", 124, Rarity.RARE, mage.cards.d.DownInTheValley.class));
         cards.add(new SetCardInfo("Duskwatch Hunter", 153, Rarity.COMMON, mage.cards.d.DuskwatchHunter.class));
         cards.add(new SetCardInfo("Dwalin, Weaponmaster", 154, Rarity.RARE, mage.cards.d.DwalinWeaponmaster.class));
         cards.add(new SetCardInfo("Dwarven Mauler", 95, Rarity.UNCOMMON, mage.cards.d.DwarvenMauler.class));
