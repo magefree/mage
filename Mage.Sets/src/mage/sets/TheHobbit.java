9e91d9c680ed3fc07fa3cd7d04568f71a12fde7f
Muz <muz@users.noreply.github.com>
2026-08-01 10:02:05 -0400

    [HOB] Implement Bolg's Company (#15769)

--- Mage.Sets/src/mage/sets/TheHobbit.java
+++ Mage.Sets/src/mage/sets/TheHobbit.java
@@ -35,6 +35,8 @@ public final class TheHobbit extends ExpansionSet {
         cards.add(new SetCardInfo("Bilbo, Thief in the Night", 219, Rarity.MYTHIC, mage.cards.b.BilboThiefInTheNight.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Bilbo, Thief in the Night", 255, Rarity.MYTHIC, mage.cards.b.BilboThiefInTheNight.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Bilbo, Thief in the Night", 33, Rarity.MYTHIC, mage.cards.b.BilboThiefInTheNight.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Bolg's Company", 149, Rarity.RARE, mage.cards.b.BolgsCompany.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Bolg's Company", 211, Rarity.RARE, mage.cards.b.BolgsCompany.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Bothersome Noisemaker", 89, Rarity.UNCOMMON, mage.cards.b.BothersomeNoisemaker.class));
         cards.add(new SetCardInfo("Burn, Burn, Tree and Fern", 90, Rarity.UNCOMMON, mage.cards.b.BurnBurnTreeAndFern.class));
         cards.add(new SetCardInfo("Crude Bent Blade", 63, Rarity.COMMON, mage.cards.c.CrudeBentBlade.class));
