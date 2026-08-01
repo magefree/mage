e24dc15bf5ff5fbdf9f416b02af9b7aa12f5449b
Muz Ali <git@mustaqila.li>
2026-08-01 11:22:41 -0500

    Extend ExileThenReturnTargetEffect to allow setting that the object re-enters with a counter on it

--- Mage.Sets/src/mage/cards/d/Daydream.java
+++ Mage.Sets/src/mage/cards/d/Daydream.java
@@ -1,24 +1,15 @@
 package mage.cards.d;
 
-import mage.abilities.Ability;
-import java.util.UUID;
-
+import mage.abilities.effects.common.ExileThenReturnTargetEffect;
 import mage.abilities.costs.mana.ManaCostsImpl;
-import mage.abilities.effects.OneShotEffect;
 import mage.abilities.keyword.FlashbackAbility;
-import mage.cards.Card;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.Outcome;
-import mage.constants.Zone;
 import mage.counters.CounterType;
-import mage.counters.Counters;
-import mage.game.Game;
-import mage.game.permanent.Permanent;
-import mage.players.Player;
 import mage.target.common.TargetControlledCreaturePermanent;
-import mage.util.CardUtil;
+
+import java.util.UUID;
 
 /**
  *
@@ -31,7 +22,9 @@ public final class Daydream extends CardImpl {
 
         // Exile target creature you control, then return that card to the battlefield under its owner's control with a +1/+1 counter on it.
         this.getSpellAbility().addTarget(new TargetControlledCreaturePermanent());
-        this.getSpellAbility().addEffect(new DaydreamEffect());
+        this.getSpellAbility().addEffect(
+            new ExileThenReturnTargetEffect(false, false).withEnterWithCounters(CounterType.P1P1.createInstance())
+        );
 
         // Flashback {2}{W}
         this.addAbility(new FlashbackAbility(this, new ManaCostsImpl<>("{2}{W}")));
@@ -44,43 +37,5 @@ public final class Daydream extends CardImpl {
     @Override
     public Daydream copy() {
         return new Daydream(this);
-    }
-}
-
-class DaydreamEffect extends OneShotEffect {
-
-    DaydreamEffect() {
-        super(Outcome.Benefit);
-        staticText = "Exile target creature you control, then return that card to the battlefield under its owner's control with a +1/+1 counter on it";
-    }
-
-    private DaydreamEffect(final DaydreamEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public DaydreamEffect copy() {
-        return new DaydreamEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Permanent permanent = game.getPermanent(source.getFirstTarget());
-        Player controller = game.getPlayer(source.getControllerId());
-        if (permanent != null && controller != null) {
-            UUID exileId = CardUtil.getExileZoneId("DaydreamEffectExile" + source.toString(), game);
-            if (controller.moveCardsToExile(permanent, source, game, true, exileId, "")) {
-                if (game.getExile().getExileZone(exileId) != null) {
-                    Card exiledCard = game.getExile().getExileZone(exileId).get(permanent.getId(), game);
-                    if (exiledCard != null) {
-                        Counters countersToAdd = new Counters();
-                        countersToAdd.addCounter(CounterType.P1P1.createInstance());
-                        game.setEnterWithCounters(exiledCard.getId(), countersToAdd);
-                        return controller.moveCards(exiledCard, Zone.BATTLEFIELD, source, game, false, false, true, null);
-                    }
-                }
-            }
-        }
-        return false;
     }
 }
--- Mage.Sets/src/mage/cards/s/Spaceshift.java
+++ Mage.Sets/src/mage/cards/s/Spaceshift.java
@@ -2,22 +2,13 @@ package mage.cards.s;
 
 import java.util.UUID;
 
-import mage.abilities.Ability;
-import mage.abilities.effects.OneShotEffect;
-import mage.cards.Card;
+import mage.abilities.effects.common.ExileThenReturnTargetEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.Outcome;
-import mage.constants.Zone;
 import mage.counters.CounterType;
-import mage.counters.Counters;
 import mage.filter.StaticFilters;
-import mage.game.Game;
-import mage.game.permanent.Permanent;
-import mage.players.Player;
 import mage.target.TargetPermanent;
-import mage.util.CardUtil;
 
 /**
  *
@@ -30,7 +21,9 @@ public final class Spaceshift extends CardImpl {
 
         // Exile target artifact or creature, then return that card to the battlefield under its owner's control with a +1/+1 counter on it.
         this.getSpellAbility().addTarget(new TargetPermanent(StaticFilters.FILTER_PERMANENT_ARTIFACT_OR_CREATURE));
-        this.getSpellAbility().addEffect(new SpaceshiftEffect());
+        this.getSpellAbility().addEffect(
+            new ExileThenReturnTargetEffect(false, false).withEnterWithCounters(CounterType.P1P1.createInstance())
+        );
     }
 
     private Spaceshift(final Spaceshift card) {
@@ -40,43 +33,5 @@ public final class Spaceshift extends CardImpl {
     @Override
     public Spaceshift copy() {
         return new Spaceshift(this);
-    }
-}
-
-class SpaceshiftEffect extends OneShotEffect {
-
-    SpaceshiftEffect() {
-        super(Outcome.Benefit);
-        staticText = "Exile target artifact or creature, then return that card to the battlefield under its owner's control with a +1/+1 counter on it";
-    }
-
-    private SpaceshiftEffect(final SpaceshiftEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public SpaceshiftEffect copy() {
-        return new SpaceshiftEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Permanent permanent = game.getPermanent(source.getFirstTarget());
-        Player controller = game.getPlayer(source.getControllerId());
-        if (permanent != null && controller != null) {
-            UUID exileId = CardUtil.getExileZoneId("SpaceshiftEffectExile" + source.toString(), game);
-            if (controller.moveCardsToExile(permanent, source, game, true, exileId, "")) {
-                if (game.getExile().getExileZone(exileId) != null) {
-                    Card exiledCard = game.getExile().getExileZone(exileId).get(permanent.getId(), game);
-                    if (exiledCard != null) {
-                        Counters countersToAdd = new Counters();
-                        countersToAdd.addCounter(CounterType.P1P1.createInstance());
-                        game.setEnterWithCounters(exiledCard.getId(), countersToAdd);
-                        return controller.moveCards(exiledCard, Zone.BATTLEFIELD, source, game, false, false, true, null);
-                    }
-                }
-            }
-        }
-        return false;
     }
 }
