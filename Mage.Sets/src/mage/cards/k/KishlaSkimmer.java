85ff1b51c88a37e6e31bbc7c91ac753225a74d14
Muz Ali <git@mustaqila.li>
2026-08-01 12:24:01 -0500

    [fix] Correct Kishla Skimmer to trigger on individual cards and not the batch event

--- Mage.Sets/src/mage/cards/k/KishlaSkimmer.java
+++ Mage.Sets/src/mage/cards/k/KishlaSkimmer.java
@@ -1,14 +1,18 @@
 package mage.cards.k;
 
 import mage.MageInt;
-import mage.abilities.common.CardsLeaveGraveyardTriggeredAbility;
+import mage.abilities.TriggeredAbilityImpl;
 import mage.abilities.effects.common.DrawCardSourceControllerEffect;
 import mage.abilities.keyword.FlyingAbility;
+import mage.cards.Card;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.SubType;
-import mage.filter.StaticFilters;
+import mage.constants.Zone;
+import mage.game.Game;
+import mage.game.events.GameEvent;
+import mage.game.events.ZoneChangeEvent;
 
 import java.util.UUID;
 
@@ -29,9 +33,7 @@ public final class KishlaSkimmer extends CardImpl {
         this.addAbility(FlyingAbility.getInstance());
 
         // Whenever a card leaves your graveyard during your turn, draw a card. This ability triggers only once each turn.
-        this.addAbility(new CardsLeaveGraveyardTriggeredAbility(
-                new DrawCardSourceControllerEffect(1), StaticFilters.FILTER_CARD, true
-        ).setTriggerPhrase("Whenever a card leaves your graveyard during your turn, ").setTriggersLimitEachTurn(1));
+        this.addAbility(new KishlaSkimmerTriggeredAbility().setTriggersLimitEachTurn(1));
     }
 
     private KishlaSkimmer(final KishlaSkimmer card) {
@@ -41,5 +43,40 @@ public final class KishlaSkimmer extends CardImpl {
     @Override
     public KishlaSkimmer copy() {
         return new KishlaSkimmer(this);
+    }
+}
+
+class KishlaSkimmerTriggeredAbility extends TriggeredAbilityImpl {
+
+    public KishlaSkimmerTriggeredAbility() {
+        super(Zone.BATTLEFIELD, new DrawCardSourceControllerEffect(1));
+        setTriggerPhrase("Whenever a card leaves your graveyard during your turn, ");
+    }
+
+    private KishlaSkimmerTriggeredAbility(final KishlaSkimmerTriggeredAbility ability) {
+        super(ability);
+    }
+
+    @Override
+    public KishlaSkimmerTriggeredAbility copy() {
+        return new KishlaSkimmerTriggeredAbility(this);
+    }
+
+    @Override
+    public boolean checkEventType(GameEvent event, Game game) {
+        return event.getType() == GameEvent.EventType.ZONE_CHANGE;
+    }
+
+    @Override
+    public boolean checkTrigger(GameEvent event, Game game) {
+        if (!game.isActivePlayer(getControllerId())) {
+            return false;
+        }
+        ZoneChangeEvent zEvent = (ZoneChangeEvent) event;
+        if (zEvent.getFromZone() != Zone.GRAVEYARD) {
+            return false;
+        }
+        Card card = game.getCard(zEvent.getTargetId());
+        return card != null && card.getOwnerId().equals(getControllerId());
     }
 }
