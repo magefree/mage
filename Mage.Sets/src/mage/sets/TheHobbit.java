fd7ec8da4987d64641da919a77c48f56b7d3826c
Muz Ali <git@mustaqila.li>
2026-07-31 21:33:00 -0500

    [HOB] Implement Dwarven Provisioner

--- Mage.Sets/src/mage/sets/TheHobbit.java
+++ Mage.Sets/src/mage/sets/TheHobbit.java
@@ -47,6 +47,7 @@ public final class TheHobbit extends ExpansionSet {
         cards.add(new SetCardInfo("Dwalin, Weaponmaster", 154, Rarity.RARE, mage.cards.d.DwalinWeaponmaster.class));
         cards.add(new SetCardInfo("Dwarven Mauler", 95, Rarity.UNCOMMON, mage.cards.d.DwarvenMauler.class));
         cards.add(new SetCardInfo("Dwarven Mattock", 172, Rarity.UNCOMMON, mage.cards.d.DwarvenMattock.class));
+        cards.add(new SetCardInfo("Dwarven Provisioner", 9, Rarity.COMMON, mage.cards.d.DwarvenProvisioner.class));
         cards.add(new SetCardInfo("Elvenking's Halls", 182, Rarity.COMMON, mage.cards.e.ElvenkingsHalls.class));
         cards.add(new SetCardInfo("Fateful Discovery", 220, Rarity.MYTHIC, mage.cards.f.FatefulDiscovery.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Fateful Discovery", 256, Rarity.MYTHIC, mage.cards.f.FatefulDiscovery.class, NON_FULL_USE_VARIOUS));
