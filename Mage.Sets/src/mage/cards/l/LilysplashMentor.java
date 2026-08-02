6340933e4ee044875f253e7868ee60d32a8e9a04
Muz <muz@users.noreply.github.com>
2026-08-02 10:32:16 -0400

    Refactor Daydream/Spaceshift to align with Lilysplash Mentor (#15713)
    * Refactor Daydream and Spaceshift to follow Lilyspash Mentor's implementation
    
    * Extend ExileThenReturnTargetEffect to allow setting that the object re-enters with a counter on it
    
    * Update Planar Incision to use this shared Effect and option

--- Mage.Sets/src/mage/cards/l/LilysplashMentor.java
+++ Mage.Sets/src/mage/cards/l/LilysplashMentor.java
@@ -3,24 +3,16 @@ package mage.cards.l;
 import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.ActivateAsSorceryActivatedAbility;
+import mage.abilities.effects.common.ExileThenReturnTargetEffect;
 import mage.abilities.costs.mana.ManaCostsImpl;
-import mage.abilities.effects.OneShotEffect;
 import mage.abilities.keyword.ReachAbility;
-import mage.cards.Card;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.Outcome;
 import mage.constants.SubType;
-import mage.constants.Zone;
 import mage.counters.CounterType;
-import mage.counters.Counters;
 import mage.filter.StaticFilters;
-import mage.game.Game;
-import mage.game.permanent.Permanent;
-import mage.players.Player;
 import mage.target.TargetPermanent;
-import mage.util.CardUtil;
 
 import java.util.UUID;
 
@@ -41,7 +33,9 @@ public final class LilysplashMentor extends CardImpl {
         this.addAbility(ReachAbility.getInstance());
 
         // {1}{G}{U}: Exile another target creature you control, then return it to the battlefield under its owner's control with a +1/+1 counter on it. Activate only as a sorcery.
-        Ability ability = new ActivateAsSorceryActivatedAbility(new LilysplashMentorEffect(), new ManaCostsImpl<>("{1}{G}{U}"));
+        Ability ability = new ActivateAsSorceryActivatedAbility(
+            new ExileThenReturnTargetEffect(false, false).withEnterWithCounters(CounterType.P1P1.createInstance()),
+            new ManaCostsImpl<>("{1}{G}{U}"));
         ability.addTarget(new TargetPermanent(StaticFilters.FILTER_ANOTHER_TARGET_CREATURE_YOU_CONTROL));
         this.addAbility(ability);
     }
@@ -53,45 +47,5 @@ public final class LilysplashMentor extends CardImpl {
     @Override
     public LilysplashMentor copy() {
         return new LilysplashMentor(this);
-    }
-}
-
-//Copied from Planar Incision
-class LilysplashMentorEffect extends OneShotEffect {
-
-    LilysplashMentorEffect() {
-        super(Outcome.Benefit);
-        staticText = "Exile another target creature you control, then return it to the battlefield under its owner's control with a +1/+1 counter on it";
-    }
-
-    private LilysplashMentorEffect(final LilysplashMentorEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public LilysplashMentorEffect copy() {
-        return new LilysplashMentorEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Permanent permanent = game.getPermanent(source.getFirstTarget());
-        Player controller = game.getPlayer(source.getControllerId());
-        if (permanent != null
-                && controller != null) {
-            UUID exileId = CardUtil.getExileZoneId("LilysplashMentorEffectExile" + source.toString(), game);
-            if (controller.moveCardsToExile(permanent, source, game, true, exileId, "")) {
-                if (game.getExile().getExileZone(exileId) != null) {
-                    Card exiledCard = game.getExile().getExileZone(exileId).get(permanent.getId(), game);
-                    if (exiledCard != null) {
-                        Counters countersToAdd = new Counters();
-                        countersToAdd.addCounter(CounterType.P1P1.createInstance());
-                        game.setEnterWithCounters(exiledCard.getId(), countersToAdd);
-                        return controller.moveCards(exiledCard, Zone.BATTLEFIELD, source, game, false, false, true, null);
-                    }
-                }
-            }
-        }
-        return false;
     }
 }
--- Mage.Sets/src/mage/cards/p/PlanarIncision.java
+++ Mage.Sets/src/mage/cards/p/PlanarIncision.java
@@ -1,22 +1,13 @@
 package mage.cards.p;
 
 import java.util.UUID;
-import mage.abilities.Ability;
-import mage.abilities.effects.OneShotEffect;
-import mage.cards.Card;
+import mage.abilities.effects.common.ExileThenReturnTargetEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.Outcome;
-import mage.constants.Zone;
 import mage.counters.CounterType;
-import mage.counters.Counters;
 import mage.filter.StaticFilters;
-import mage.game.Game;
-import mage.game.permanent.Permanent;
-import mage.players.Player;
 import mage.target.TargetPermanent;
-import mage.util.CardUtil;
 
 /**
  * @author jeffwadsworth
@@ -27,7 +18,9 @@ public final class PlanarIncision extends CardImpl {
         super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{1}{U}");
 
         // Exile target artifact or creature, then return it to the battlefield under its owner’s control with a +1/+1 counter on it.
-        this.getSpellAbility().addEffect(new PlanarIncisionEffect());
+        this.getSpellAbility().addEffect(
+            new ExileThenReturnTargetEffect(false, false).withEnterWithCounters(CounterType.P1P1.createInstance())
+        );
         this.getSpellAbility().addTarget(new TargetPermanent(StaticFilters.FILTER_PERMANENT_ARTIFACT_OR_CREATURE));
     }
 
@@ -38,44 +31,5 @@ public final class PlanarIncision extends CardImpl {
     @Override
     public PlanarIncision copy() {
         return new PlanarIncision(this);
-    }
-}
-
-class PlanarIncisionEffect extends OneShotEffect {
-
-    PlanarIncisionEffect() {
-        super(Outcome.Benefit);
-        staticText = "Exile target artifact or creature, then return it to the battlefield under its owner's control with a +1/+1 counter on it";
-    }
-
-    private PlanarIncisionEffect(final PlanarIncisionEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public PlanarIncisionEffect copy() {
-        return new PlanarIncisionEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Permanent permanent = game.getPermanent(source.getFirstTarget());
-        Player controller = game.getPlayer(source.getControllerId());
-        if (permanent != null
-                && controller != null) {
-            UUID exileId = CardUtil.getExileZoneId("planarIncisionExile" + source.toString(), game);
-            if (controller.moveCardsToExile(permanent, source, game, true, exileId, "")) {
-                if (game.getExile().getExileZone(exileId) != null) {
-                    Card exiledCard = game.getExile().getExileZone(exileId).get(permanent.getId(), game);
-                    if (exiledCard != null) {
-                        Counters countersToAdd = new Counters();
-                        countersToAdd.addCounter(CounterType.P1P1.createInstance());
-                        game.setEnterWithCounters(exiledCard.getId(), countersToAdd);
-                        return controller.moveCards(exiledCard, Zone.BATTLEFIELD, source, game, false, false, true, null);
-                    }
-                }
-            }
-        }
-        return false;
     }
 }
