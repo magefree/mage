4dec0d9c9ce05b22dd3b0253b3f99e31cf25ac19
Muz <muz@users.noreply.github.com>
2026-08-02 15:57:22 -0400

    [HOB] Implement Esgaroth Garrison (#15824)

--- Mage.Sets/src/mage/sets/TheHobbit.java
+++ Mage.Sets/src/mage/sets/TheHobbit.java
@@ -69,6 +69,7 @@ public final class TheHobbit extends ExpansionSet {
         cards.add(new SetCardInfo("Dwarven Shortsword", 10, Rarity.COMMON, mage.cards.d.DwarvenShortsword.class));
         cards.add(new SetCardInfo("Elvenking's Halls", 182, Rarity.COMMON, mage.cards.e.ElvenkingsHalls.class));
         cards.add(new SetCardInfo("Elvenking's Harper", 38, Rarity.COMMON, mage.cards.e.ElvenkingsHarper.class));
+        cards.add(new SetCardInfo("Esgaroth Garrison", 13, Rarity.COMMON, mage.cards.e.EsgarothGarrison.class));
         cards.add(new SetCardInfo("Fateful Discovery", 220, Rarity.MYTHIC, mage.cards.f.FatefulDiscovery.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Fateful Discovery", 256, Rarity.MYTHIC, mage.cards.f.FatefulDiscovery.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Fateful Discovery", 40, Rarity.MYTHIC, mage.cards.f.FatefulDiscovery.class, NON_FULL_USE_VARIOUS));
