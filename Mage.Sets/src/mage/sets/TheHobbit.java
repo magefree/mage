f09cd55697c481407d2719e5bda8238c61bfb947
Muz <muz@users.noreply.github.com>
2026-08-01 21:13:10 -0400

    [HOB] Implement Gandalf, Spark Starter (#15800)

--- Mage.Sets/src/mage/sets/TheHobbit.java
+++ Mage.Sets/src/mage/sets/TheHobbit.java
@@ -73,6 +73,8 @@ public final class TheHobbit extends ExpansionSet {
         cards.add(new SetCardInfo("Forest", 193, Rarity.LAND, mage.cards.basiclands.Forest.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Forest", 198, Rarity.LAND, mage.cards.basiclands.Forest.class, FULL_ART_USE_VARIOUS));
         cards.add(new SetCardInfo("Front Porch Sentries", 67, Rarity.COMMON, mage.cards.f.FrontPorchSentries.class));
+        cards.add(new SetCardInfo("Gandalf, Spark Starter", 203, Rarity.UNCOMMON, mage.cards.g.GandalfSparkStarter.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Gandalf, Spark Starter", 97, Rarity.UNCOMMON, mage.cards.g.GandalfSparkStarter.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Gathering of Darkness", 68, Rarity.UNCOMMON, mage.cards.g.GatheringOfDarkness.class));
         cards.add(new SetCardInfo("Giant's Boulder", 173, Rarity.COMMON, mage.cards.g.GiantsBoulder.class));
         cards.add(new SetCardInfo("Gleaming Splendor", 275, Rarity.MYTHIC, mage.cards.g.GleamingSplendor.class));
