f4ccb05fc6de62f7d9ea60f92e210b5ec3ceee7f
PurpleCrowbar <26198472+PurpleCrowbar@users.noreply.github.com>
2026-08-02 14:29:33 +0100

    [HOB] Implement Smaug, Wicked Worm

--- Mage.Sets/src/mage/sets/TheHobbit.java
+++ Mage.Sets/src/mage/sets/TheHobbit.java
@@ -140,12 +140,14 @@ public final class TheHobbit extends ExpansionSet {
         cards.add(new SetCardInfo("Ravenhill Flock", 52, Rarity.UNCOMMON, mage.cards.r.RavenhillFlock.class));
         cards.add(new SetCardInfo("Riddles in the Dark", 292, Rarity.RARE, mage.cards.r.RiddlesInTheDark.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Riddles in the Dark", 53, Rarity.RARE, mage.cards.r.RiddlesInTheDark.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Smaug's Fury", 111, Rarity.COMMON, mage.cards.s.SmaugsFury.class));
+        cards.add(new SetCardInfo("Smaug, the Great Calamity", 109, Rarity.COMMON, mage.cards.s.SmaugTheGreatCalamity.class));
         cards.add(new SetCardInfo("Smaug the Magnificent", 110, Rarity.MYTHIC, mage.cards.s.SmaugTheMagnificent.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Smaug the Magnificent", 229, Rarity.MYTHIC, mage.cards.s.SmaugTheMagnificent.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Smaug the Magnificent", 249, Rarity.MYTHIC, mage.cards.s.SmaugTheMagnificent.class, FULL_ART_USE_VARIOUS));
         cards.add(new SetCardInfo("Smaug the Magnificent", 265, Rarity.MYTHIC, mage.cards.s.SmaugTheMagnificent.class, NON_FULL_USE_VARIOUS));
-        cards.add(new SetCardInfo("Smaug's Fury", 111, Rarity.COMMON, mage.cards.s.SmaugsFury.class));
-        cards.add(new SetCardInfo("Smaug, the Great Calamity", 109, Rarity.COMMON, mage.cards.s.SmaugTheGreatCalamity.class));
+        cards.add(new SetCardInfo("Smaug, Wicked Worm", 164, Rarity.RARE, mage.cards.s.SmaugWickedWorm.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Smaug, Wicked Worm", 245, Rarity.RARE, mage.cards.s.SmaugWickedWorm.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Sting, Bilbo's Sword", 178, Rarity.RARE, mage.cards.s.StingBilbosSword.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Sting, Bilbo's Sword", 237, Rarity.RARE, mage.cards.s.StingBilbosSword.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Sting, Bilbo's Sword", 273, Rarity.RARE, mage.cards.s.StingBilbosSword.class, NON_FULL_USE_VARIOUS));
