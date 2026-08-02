a25062277a2679889b87dedc08874819bc9094ee
Muz Ali <git@mustaqila.li>
2026-08-02 11:19:53 -0500

    [HOB] Implement Thorin's Last Stand

--- Mage.Sets/src/mage/sets/TheHobbit.java
+++ Mage.Sets/src/mage/sets/TheHobbit.java
@@ -174,6 +174,8 @@ public final class TheHobbit extends ExpansionSet {
         cards.add(new SetCardInfo("The Queen of Dale", 253, Rarity.MYTHIC, mage.cards.t.TheQueenOfDale.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Thorin Oakenshield", 165, Rarity.UNCOMMON, mage.cards.t.ThorinOakenshield.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Thorin Oakenshield", 202, Rarity.UNCOMMON, mage.cards.t.ThorinOakenshield.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Thorin's Last Stand", 209, Rarity.COMMON, mage.cards.t.ThorinsLastStand.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Thorin's Last Stand", 28, Rarity.COMMON, mage.cards.t.ThorinsLastStand.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Thorin, Mountain-king", 114, Rarity.MYTHIC, mage.cards.t.ThorinMountainKing.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Thorin, Mountain-king", 243, Rarity.MYTHIC, mage.cards.t.ThorinMountainKing.class, FULL_ART_USE_VARIOUS));
         cards.add(new SetCardInfo("Thorin, Mountain-king", 279, Rarity.MYTHIC, mage.cards.t.ThorinMountainKing.class, FULL_ART_USE_VARIOUS));
