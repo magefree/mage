59ac16d0998a3622cf3c5fde3467203c0927ad92
Muz <muz@users.noreply.github.com>
2026-08-02 15:54:15 -0400

    [HOB] Implement The Mountain-king's Return (#15818)

--- Mage.Sets/src/mage/sets/TheHobbit.java
+++ Mage.Sets/src/mage/sets/TheHobbit.java
@@ -175,6 +175,7 @@ public final class TheHobbit extends ExpansionSet {
         cards.add(new SetCardInfo("The Lonely Mountain", 284, Rarity.RARE, mage.cards.t.TheLonelyMountain.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("The Misty Mountains Cold", 104, Rarity.RARE, mage.cards.t.TheMistyMountainsCold.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("The Misty Mountains Cold", 321, Rarity.RARE, mage.cards.t.TheMistyMountainsCold.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("The Mountain-king's Return", 22, Rarity.UNCOMMON, mage.cards.t.TheMountainKingsReturn.class));
         cards.add(new SetCardInfo("The Queen of Dale", 217, Rarity.MYTHIC, mage.cards.t.TheQueenOfDale.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("The Queen of Dale", 24, Rarity.MYTHIC, mage.cards.t.TheQueenOfDale.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("The Queen of Dale", 253, Rarity.MYTHIC, mage.cards.t.TheQueenOfDale.class, NON_FULL_USE_VARIOUS));
