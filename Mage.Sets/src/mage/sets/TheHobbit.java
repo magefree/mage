eb9f23d92a79f8f81d0e9cc31c50962df5aa3a9b
Muz Ali <git@mustaqila.li>
2026-08-02 15:30:45 -0500

    [HOB] Implement Through the Forest Gate

--- Mage.Sets/src/mage/sets/TheHobbit.java
+++ Mage.Sets/src/mage/sets/TheHobbit.java
@@ -219,6 +219,8 @@ public final class TheHobbit extends ExpansionSet {
         cards.add(new SetCardInfo("Thranduil, Sindarin Liege", 233, Rarity.UNCOMMON, mage.cards.t.ThranduilSindarinLiege.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Thranduil, Sindarin Liege", 269, Rarity.UNCOMMON, mage.cards.t.ThranduilSindarinLiege.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Thror's Map", 179, Rarity.UNCOMMON, mage.cards.t.ThrorsMap.class));
+        cards.add(new SetCardInfo("Through the Forest Gate", 137, Rarity.RARE, mage.cards.t.ThroughTheForestGate.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Through the Forest Gate", 310, Rarity.RARE, mage.cards.t.ThroughTheForestGate.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Tidings of War", 115, Rarity.COMMON, mage.cards.t.TidingsOfWar.class));
         cards.add(new SetCardInfo("Tom, Bert, and William", 169, Rarity.RARE, mage.cards.t.TomBertAndWilliam.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Tom, Bert, and William", 312, Rarity.RARE, mage.cards.t.TomBertAndWilliam.class, NON_FULL_USE_VARIOUS));
