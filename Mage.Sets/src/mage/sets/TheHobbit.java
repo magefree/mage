e4547d5157a76cb3c613ff3662830e0da924793b
Muz <muz@users.noreply.github.com>
2026-08-02 16:13:26 -0400

    [HOB] Implement Eagle of the Great Shelf (#15827)

--- Mage.Sets/src/mage/sets/TheHobbit.java
+++ Mage.Sets/src/mage/sets/TheHobbit.java
@@ -70,6 +70,7 @@ public final class TheHobbit extends ExpansionSet {
         cards.add(new SetCardInfo("Dwarven Mattock", 172, Rarity.UNCOMMON, mage.cards.d.DwarvenMattock.class));
         cards.add(new SetCardInfo("Dwarven Provisioner", 9, Rarity.COMMON, mage.cards.d.DwarvenProvisioner.class));
         cards.add(new SetCardInfo("Dwarven Shortsword", 10, Rarity.COMMON, mage.cards.d.DwarvenShortsword.class));
+        cards.add(new SetCardInfo("Eagle of the Great Shelf", 11, Rarity.UNCOMMON, mage.cards.e.EagleOfTheGreatShelf.class));
         cards.add(new SetCardInfo("Elven Raft-Steerer", 37, Rarity.UNCOMMON, mage.cards.e.ElvenRaftSteerer.class));
         cards.add(new SetCardInfo("Elvenking's Halls", 182, Rarity.COMMON, mage.cards.e.ElvenkingsHalls.class));
         cards.add(new SetCardInfo("Elvenking's Harper", 38, Rarity.COMMON, mage.cards.e.ElvenkingsHarper.class));
