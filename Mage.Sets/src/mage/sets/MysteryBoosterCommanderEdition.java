bfa30fd1c6359570eaf0e828b8f948eebb8ab100
Muz Ali <git@mustaqila.li>
2026-08-02 23:24:07 -0500

    [MBC] Implement Boss Uramon, Shadow's Reach

--- Mage.Sets/src/mage/sets/MysteryBoosterCommanderEdition.java
+++ Mage.Sets/src/mage/sets/MysteryBoosterCommanderEdition.java
@@ -24,6 +24,7 @@ public final class MysteryBoosterCommanderEdition extends ExpansionSet {
         // this.enableSetBooster(Integer.MAX_VALUE);
 
         cards.add(new SetCardInfo("Blor the Impervious", 28, Rarity.RARE, mage.cards.b.BlorTheImpervious.class));
+        cards.add(new SetCardInfo("Boss Uramon, Shadow's Reach", 13, Rarity.RARE, mage.cards.b.BossUramonShadowsReach.class));
         cards.add(new SetCardInfo("Chaos Warp", 72, Rarity.RARE, mage.cards.c.ChaosWarp.class));
         cards.add(new SetCardInfo("Chromatic Lantern", 73, Rarity.RARE, mage.cards.c.ChromaticLantern.class));
         cards.add(new SetCardInfo("Dust to Dust", 71, Rarity.UNCOMMON, mage.cards.d.DustToDust.class));
