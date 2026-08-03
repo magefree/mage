3a99073e432bde14f0898f76213c5a1e5c4d0908
Muz Ali <git@mustaqila.li>
2026-08-03 07:55:12 -0500

    [MBC] Implement Blor the Impervious

--- Mage.Sets/src/mage/sets/MysteryBoosterCommanderEdition.java
+++ Mage.Sets/src/mage/sets/MysteryBoosterCommanderEdition.java
@@ -23,6 +23,7 @@ public final class MysteryBoosterCommanderEdition extends ExpansionSet {
         this.hasBasicLands = false; // TODO: Confirm once more information is released about the set
         // this.enableSetBooster(Integer.MAX_VALUE);
 
+        cards.add(new SetCardInfo("Blor the Impervious", 28, Rarity.RARE, mage.cards.b.BlorTheImpervious.class));
         cards.add(new SetCardInfo("Joven and Chandler", 24, Rarity.RARE, mage.cards.j.JovenAndChandler.class));
         // cards.add(new SetCardInfo("Tolabow, Loch Rascal", 12, Rarity.RARE, mage.cards.t.TolabowLochRascal.class));
     }
