magefree/mage

commit c7a485b7284b1b6a8fd0fad693433f5b0f383876

reworked AI, targeting and targets logic: - refactor: simplified target implementation from a dozen canTarget, canChoose and possibleTargets methods to canTarget/possibleTargets only (part of #13638, #13766); - refactor: fixed wrong target implementations in many cards (example: TargetCardInHand for opponent's hand, close #6210); - AI: now human, AI and test players -- all use possibleTargets logic in most use cases instead filters or custom validation; - AI: improved AI sims support for multiple targets abilities; - AI: improved AI stability, freezes and targets errors in some use cases;

Mage/src/main/java/mage/target/Target.java
@@ -1,11 +1,13 @@
 package mage.target;
 
+import mage.MageObject;
 import mage.abilities.Ability;
 import mage.cards.Cards;
 import mage.constants.Outcome;
 import mage.constants.Zone;
 import mage.filter.Filter;
 import mage.game.Game;
+import mage.game.permanent.Permanent;
 import mage.players.Player;
 import mage.util.Copyable;
 
@@ -27,10 +29,24 @@ public interface Target extends Copyable<Target>, Serializable {
      * Warning, for "up to" targets it will return true all the time, so make sure your dialog
      * use do-while logic and call "choose" one time min or use isChoiceCompleted
      */
-    @Deprecated // TODO: replace with UUID abilityControllerId, Ability source, Game game
+    @Deprecated
+    // TODO: replace with UUID abilityControllerId, Ability source, Game game
     boolean isChosen(Game game);
 
-    boolean isChoiceCompleted(UUID abilityControllerId, Ability source, Game game);
+    /**
+     * Checking target complete and nothing to choose (X=0, all selected, all possible selected, etc)
+     *
+     * @param fromCards can be null for non cards selection
+     */
+    boolean isChoiceCompleted(UUID abilityControllerId, Ability source, Game game, Cards fromCards);
+
+    /**
+     * Temporary status to work with "up to" targets (mark target that it was skip on selection)
+     * TODO: remove after target.chooseXXX remove
+     */
+    boolean isSkipChoice();
+
+    void setSkipChoice(boolean isSkipChoice);
 
     void clearChosen();
 
@@ -51,16 +67,50 @@ public interface Target extends Copyable<Target>, Serializable {
      */
     Target withNotTarget(boolean notTarget);
 
-    // methods for targets
+    /**
+     * Checks if there are enough targets the player can choose from among them
+     * or if they are autochosen since there are fewer than the minimum number.
+     * <p>
+     * Implement as return canChooseFromPossibleTargets(sourceControllerId, source, game);
+     * TODO: remove after all canChoose replaced with default
+     *
+     * @param sourceControllerId - controller of the target event source
+     * @param source             - can be null
+     * @param game
+     * @return - true if enough valid choices exist
+     */
     boolean canChoose(UUID sourceControllerId, Ability source, Game game);
 
     /**
+     * Make sure target can be fully selected or already selected, e.g. by AI sims
+     * <p>
+     * Do not override
+     */
+    default boolean canChooseOrAlreadyChosen(UUID sourceControllerId, Ability source, Game game) {
+        return this.isChosen(game) || this.canChoose(sourceControllerId, source, game);
+    }
+
+    default boolean canChooseFromPossibleTargets(UUID sourceControllerId, Ability source, Game game) {
+        // TODO: replace all canChoose override methods by that code call
+        if (getMinNumberOfTargets() == 0) {
+            return true;
+        }
+
+        int selectedCount = getSize();
+        int moreSelectCount = possibleTargets(sourceControllerId, source, game).size();
+
+        if (selectedCount >= getMaxNumberOfTargets()) {
+            return false;
+        }
+
+        return moreSelectCount > 0 && selectedCount + moreSelectCount >= getMinNumberOfTargets();
+    }
+
+    /**
      * Returns a set of all possible targets that match the criteria of the implemented Target class.
+     * WARNING, it must filter already selected targets by keepValidPossibleTargets call at the end
      *
-     * @param sourceControllerId UUID of the ability's controller
-     * @param source             Ability which requires the targets
-     * @param game               Current game
-     * @return Set of the UUIDs of possible targets
+     * @param source - can be null
      */
     Set<UUID> possibleTargets(UUID sourceControllerId, Ability source, Game game);
 
@@ -68,6 +118,42 @@ public interface Target extends Copyable<Target>, Serializable {
         // do not override
         return possibleTargets(sourceControllerId, source, game).stream()
                 .filter(id -> cards == null || cards.contains(id))
+                .collect(Collectors.toSet());
+    }
+
+    /**
+     * Keep only valid and not selected targets - must be used inside any possibleTargets implementation
+     */
+    default Set<UUID> keepValidPossibleTargets(Set<UUID> possibleTargets, UUID sourceControllerId, Ability source, Game game) {
+        // TODO: check target amount in human dialogs - is it allow to select it again
+        // do not override
+        // keep only valid and not selected targets list
+        return possibleTargets.stream()
+                .filter(this::notContains)
+                .filter(targetId -> {
+                    // non-target allow any
+                    if (source == null || source.getSourceId() == null || isNotTarget()) {
+                        return true;
+                    }
+                    MageObject sourceObject = game.getObject(source);
+                    if (sourceObject == null) {
+                        return true;
+                    }
+
+                    // target allow non-protected
+                    Player targetPlayer = game.getPlayer(targetId);
+                    if (targetPlayer != null) {
+                        return !targetPlayer.hasLeft()
+                                && canTarget(sourceControllerId, targetId, source, game)
+                                && targetPlayer.canBeTargetedBy(sourceObject, sourceControllerId, source, game);
+                    }
+                    Permanent targetPermanent = game.getPermanent(targetId);
+                    if (targetPermanent != null) {
+                        return canTarget(sourceControllerId, targetId, source, game)
+                                && targetPermanent.canBeTargetedBy(sourceObject, sourceControllerId, source, game);
+                    }
+                    return true;
+                })
                 .collect(Collectors.toSet());
     }
 
@@ -86,8 +172,6 @@ public interface Target extends Copyable<Target>, Serializable {
     void addTarget(UUID id, Ability source, Game game, boolean skipEvent);
 
     void addTarget(UUID id, int amount, Ability source, Game game, boolean skipEvent);
-
-    boolean canTarget(UUID id, Game game);
 
     /**
      * @param id
@@ -108,11 +192,12 @@ public interface Target extends Copyable<Target>, Serializable {
      */
     List<? extends Target> getTargetOptions(Ability source, Game game);
 
-    boolean canChoose(UUID sourceControllerId, Game game);
+    default boolean canChoose(UUID sourceControllerId, Game game) {
+        return canChoose(sourceControllerId, null, game);
+    }
 
-    Set<UUID> possibleTargets(UUID sourceControllerId, Game game);
-
-    @Deprecated // TODO: need replace to source only version?
+    @Deprecated
+        // TODO: need replace to source only version?
     boolean choose(Outcome outcome, UUID playerId, UUID sourceId, Ability source, Game game);
 
     /**
@@ -235,6 +320,11 @@ public interface Target extends Copyable<Target>, Serializable {
     int getSize();
 
     boolean contains(UUID targetId);
+
+    default boolean notContains(UUID targetId) {
+        // for better usage in streams
+        return !contains(targetId);
+    }
 
     /**
      * This function tries to auto-choose the next target.

commit a4a0129a4ed24506ca3e34c9b93e187bde58692b

Consider rule modifying effects for valid targeting
This unfortunately requires a series of changes to makework.  As the
reported issue says, in its current state rule-modifying effects which
affect target legality aren't checked when considering whether to show
an object as targetable, rather they reject the attempt to target when
the user tries to.  This indeed causes a game lockup.

The first thing I did was try to take this into account in
`TargetObject.canTarget()`.  That worked, but only for permanents, it
didn't work for the stated test case where a *card* was an illegal
target.  Following that, I saw that we don't check card target legality
generally at all.  I applied this in `TargetCard` to call up to the
parent, and had to do the same for most of its subclasses.

This fixed the issue interactively (when there are no valid legal
targets you are allowed to click the cancel button) but I also had to
make some changes to the test framework in order to get `TestPlayer` to
allow this possible state.  Now if there are no legal targets it allows
omitting the `addTarget()` call.

This *might* be a regression in c7a485b as it removed a log of calls to
`game.replaceEvent`, but not sure.

Fixes https://github.com/magefree/mage/issues/15756

Mage/src/main/java/mage/target/Target.java
@@ -2,6 +2,7 @@ package mage.target;
 
 import mage.MageObject;
 import mage.abilities.Ability;
+import mage.cards.Card;
 import mage.cards.Cards;
 import mage.constants.Outcome;
 import mage.constants.Zone;
@@ -151,6 +152,10 @@ public interface Target extends Copyable<Target>, Serializable {
                     if (targetPermanent != null) {
                         return canTarget(sourceControllerId, targetId, source, game)
                                 && targetPermanent.canBeTargetedBy(sourceObject, sourceControllerId, source, game);
+                    }
+                    Card targetCard = game.getCard(targetId);
+                    if (targetCard != null) {
+                        return canTarget(sourceControllerId, targetId, source, game);
                     }
                     return true;
                 })

Mage/src/main/java/mage/target/TargetCard.java
@@ -194,7 +194,7 @@ public class TargetCard extends TargetObject {
     public boolean canTarget(UUID id, Ability source, Game game) {
         // copy-pasted from super but with card instead object
         Card card = game.getCard(id);
-        return card != null
+        return super.canTarget(id, source, game) && card != null
                 && zone != null && zone.match(game.getState().getZone(id))
                 && getFilter() != null && getFilter().match(card, game);
     }
@@ -202,7 +202,7 @@ public class TargetCard extends TargetObject {
     @Override
     public boolean canTarget(UUID playerId, UUID id, Ability source, Game game) {
         Card card = game.getCard(id);
-        return card != null
+        return super.canTarget(id, source, game) && card != null
                 && zone != null && zone.match(game.getState().getZone(id))
                 && getFilter() != null && getFilter().match(card, playerId, source, game);
     }

Mage/src/main/java/mage/target/TargetObject.java
@@ -4,6 +4,7 @@ import mage.MageObject;
 import mage.abilities.Ability;
 import mage.constants.Zone;
 import mage.game.Game;
+import mage.game.events.TargetEvent;
 
 import java.util.UUID;
 
@@ -56,7 +57,8 @@ public abstract class TargetObject extends TargetImpl {
         MageObject object = game.getObject(id);
         return object != null
                 && zone != null && zone.match(game.getState().getZone(id))
-                && getFilter() != null && getFilter().match(object, game);
+                && getFilter() != null && getFilter().match(object, game)
+                && !game.getContinuousEffects().preventedByRuleModification(new TargetEvent(id, source), source, game, true);
     }
 
     @Override

Mage/src/main/java/mage/target/common/TargetCardInCommandZone.java
@@ -39,7 +39,8 @@ public class TargetCardInCommandZone extends TargetCard {
     @Override
     public boolean canTarget(UUID playerId, UUID id, Ability source, Game game) {
         Card card = game.getCard(id);
-        return game.getState().getZone(id) == Zone.COMMAND
+        return super.canTarget(playerId, id, source, game)
+                && game.getState().getZone(id) == Zone.COMMAND
                 && game.getState().getPlayersInRange(getTargetController() == null ? playerId : getTargetController(), game).contains(game.getOwnerId(id))
                 && filter.match(card, game);
     }

Mage/src/main/java/mage/target/common/TargetCardInGraveyard.java
@@ -49,7 +49,7 @@ public class TargetCardInGraveyard extends TargetCard {
     @Override
     public boolean canTarget(UUID id, Ability source, Game game) {
         Card card = game.getCard(id);
-        return card != null && game.getState().getZone(card.getId()) == Zone.GRAVEYARD && filter.match(card, game);
+        return super.canTarget(id, source, game) && card != null && game.getState().getZone(card.getId()) == Zone.GRAVEYARD && filter.match(card, game);
     }
 
     @Override

Mage/src/main/java/mage/target/common/TargetCardInHand.java
@@ -48,7 +48,8 @@ public class TargetCardInHand extends TargetCard {
         // Has to be a card in the hand of a player in range. We don't know here, from which player's hand so we have to check all possible players
         // And because a card in hand is never targeted we can omitt specific targeting related checks
         Card card = game.getCard(id);
-        return game.getState().getZone(id) == Zone.HAND
+        return super.canTarget(playerId, id, source, game)
+                && game.getState().getZone(id) == Zone.HAND
                 && game.getState().getPlayersInRange(getTargetController() == null ? playerId : getTargetController(), game).contains(game.getOwnerId(id))
                 && filter.match(card, game);
     }

Mage/src/main/java/mage/target/common/TargetCardInLibrary.java
@@ -116,7 +116,7 @@ public class TargetCardInLibrary extends TargetCard {
     @Override
     public boolean canTarget(UUID id, Ability source, Game game) {
         Card card = game.getPlayer(source.getControllerId()).getLibrary().getCard(id, game);
-        return filter.match(card, source.getControllerId(), source, game);
+        return super.canTarget(id, source, game) && filter.match(card, source.getControllerId(), source, game);
     }
 
     @Override

Mage/src/main/java/mage/target/common/TargetCardInOpponentsGraveyard.java
@@ -38,6 +38,9 @@ public class TargetCardInOpponentsGraveyard extends TargetCard {
 
     @Override
     public boolean canTarget(UUID playerId, UUID id, Ability source, Game game) {
+        if (!super.canTarget(playerId, id, source, game)) {
+            return false;
+        }
         Card card = game.getCard(id);
         if (card != null && zone.match(game.getState().getZone(id))) {
             if (game.getPlayer(source.getControllerId()).hasOpponent(card.getOwnerId(), game)) {
@@ -55,6 +58,9 @@ public class TargetCardInOpponentsGraveyard extends TargetCard {
 
     @Override
     public boolean canTarget(UUID id, Ability source, Game game) {
+        if (!super.canTarget(id, source, game)) {
+            return false;
+        }
         Card card = game.getCard(id);
         if (card != null && game.getState().getZone(card.getId()) == Zone.GRAVEYARD) {
             if (game.getPlayer(source.getControllerId()).hasOpponent(card.getOwnerId(), game)) {

Mage/src/main/java/mage/target/common/TargetCardInYourGraveyard.java
@@ -55,6 +55,9 @@ public class TargetCardInYourGraveyard extends TargetCard {
 
     @Override
     public boolean canTarget(UUID id, Ability source, Game game) {
+        if (!super.canTarget(id, source, game)) {
+            return false;
+        }
         Card card = game.getCard(id);
         if (card != null && game.getState().getZone(card.getId()) == Zone.GRAVEYARD) {
             if (game.getPlayer(source.getControllerId()).getGraveyard().contains(id)) {
@@ -66,6 +69,9 @@ public class TargetCardInYourGraveyard extends TargetCard {
 
     @Override
     public boolean canTarget(UUID playerId, UUID id, Ability ability, Game game) {
+        if (!super.canTarget(playerId, id, ability, game)) {
+            return false;
+        }
         Card card = game.getCard(id);
         if (card != null && game.getState().getZone(card.getId()) == Zone.GRAVEYARD) {
             if (game.getPlayer(playerId).getGraveyard().contains(id)) {

Mage/src/main/java/mage/target/common/TargetCardInYourGraveyardOrExile.java
@@ -4,6 +4,7 @@ import mage.abilities.Ability;
 import mage.cards.Card;
 import mage.constants.Zone;
 import mage.filter.FilterCard;
+import mage.filter.StaticFilters;
 import mage.game.Game;
 import mage.players.Player;
 import mage.target.TargetCard;
@@ -21,7 +22,7 @@ public class TargetCardInYourGraveyardOrExile extends TargetCard {
     private final FilterCard filterExile;
 
     public TargetCardInYourGraveyardOrExile(FilterCard filterGraveyard, FilterCard filterExile) {
-        super(1, 1, Zone.GRAVEYARD, filterGraveyard); // we do not actually use those, as all is overwritten
+        super(1, 1, Zone.ALL, StaticFilters.FILTER_CARD); // we do not actually use those, as all is overwritten
         this.filterGraveyard = filterGraveyard;
         this.filterExile = filterExile;
         this.targetName = filterGraveyard.getMessage() + " or " + filterExile.getMessage();
@@ -57,6 +58,9 @@ public class TargetCardInYourGraveyardOrExile extends TargetCard {
 
     @Override
     public boolean canTarget(UUID playerId, UUID id, Ability source, Game game) {
+        if (!super.canTarget(playerId, id, source, game)) {
+            return false;
+        }
         Card card = game.getCard(id);
         if (card == null) {
             return false;

Mage.Tests/src/test/java/org/mage/test/cards/targets/TargetRestrictionsTest.java
@@ -3,6 +3,7 @@ package org.mage.test.cards.targets;
 import mage.constants.PhaseStep;
 import mage.constants.Zone;
 import org.junit.Test;
+import org.mage.test.player.TestPlayer;
 import org.mage.test.serverside.base.CardTestPlayerBase;
 
 /**
@@ -76,5 +77,22 @@ public class TargetRestrictionsTest extends CardTestPlayerBase {
         
         assertLife(playerA, 20);
         assertLife(playerB, 20);
+    }
+
+    // https://github.com/magefree/mage/issues/15756
+    @Test
+    public void testCantTargetCardsInGraveyards() {
+        addCard(Zone.BATTLEFIELD, playerA, "Dennick, Pious Apprentice");
+        addCard(Zone.GRAVEYARD, playerA, "Balduvian Bears");
+        addCard(Zone.BATTLEFIELD, playerA, "Scrabbling Claws");
+
+        activateAbility(1, PhaseStep.PRECOMBAT_MAIN, playerA, "{T}: Target player exiles a card from their graveyard");
+        addTarget(playerA, playerA);
+
+        setStrictChooseMode(true);
+        setStopAt(1, PhaseStep.POSTCOMBAT_MAIN);
+        execute();
+
+        assertGraveyardCount(playerA, "Balduvian Bears", 1);
     }
 }

Mage.Tests/src/test/java/org/mage/test/player/TestPlayer.java
@@ -2621,9 +2621,9 @@ public class TestPlayer implements Player {
 
         // wrong target settings by addTarget
         // how to fix: implement target class processing above (if it a permanent target then check "filter instanceof" code too)
+        Set<UUID> possibleTargets = target.possibleTargets(abilityControllerId, source, game);
         if (!targets.isEmpty()) {
             String message;
-            Set<UUID> possibleTargets = target.possibleTargets(abilityControllerId, source, game);
 
             if (source != null) {
                 message = this.getName() + " - Targets list was setup by addTarget with " + targets + ", but not used"
@@ -2639,6 +2639,10 @@ public class TestPlayer implements Player {
                         + "\nYou must implement target class support in TestPlayer, \"filter instanceof\", or setup good targets";
             }
             Assert.fail(message);
+        }
+
+        if (possibleTargets.isEmpty()) {
+            return false;
         }
 
         this.chooseStrictModeFailed("target", game, getInfo(source, game) + "\n" + getInfo(target, source, game, null));