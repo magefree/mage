a00402c5ed78ca11058051b9388c62685edc36f9
Muz Ali <git@mustaqila.li>
2026-08-04 22:34:35 -0500

    [MBC] Implement Dyfed, the Guiding Hand

--- Mage.Sets/src/mage/sets/MysteryBoosterCommanderEdition.java
+++ Mage.Sets/src/mage/sets/MysteryBoosterCommanderEdition.java
@@ -27,6 +27,7 @@ public final class MysteryBoosterCommanderEdition extends ExpansionSet {
         cards.add(new SetCardInfo("Chaos Warp", 72, Rarity.RARE, mage.cards.c.ChaosWarp.class));
         cards.add(new SetCardInfo("Chromatic Lantern", 73, Rarity.RARE, mage.cards.c.ChromaticLantern.class));
         cards.add(new SetCardInfo("Dust to Dust", 71, Rarity.UNCOMMON, mage.cards.d.DustToDust.class));
+        cards.add(new SetCardInfo("Dyfed, the Guiding Hand", 7, Rarity.RARE, mage.cards.d.DyfedTheGuidingHand.class));
         cards.add(new SetCardInfo("Exotic Orchard", 79, Rarity.RARE, mage.cards.e.ExoticOrchard.class));
         cards.add(new SetCardInfo("Fellwar Stone", 74, Rarity.UNCOMMON, mage.cards.f.FellwarStone.class));
         cards.add(new SetCardInfo("Jeweled Amulet", 75, Rarity.UNCOMMON, mage.cards.j.JeweledAmulet.class));
