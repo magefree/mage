24048f1f53ebf810dd9d8c8c96cde0be80ccb84b
Muz <muz@users.noreply.github.com>
2026-08-06 17:40:14 -0500

    [MBC] Implement Emerald Collector (#15877)

--- Mage.Sets/src/mage/sets/MysteryBoosterCommanderEdition.java
+++ Mage.Sets/src/mage/sets/MysteryBoosterCommanderEdition.java
@@ -29,6 +29,7 @@ public final class MysteryBoosterCommanderEdition extends ExpansionSet {
         cards.add(new SetCardInfo("Chromatic Lantern", 73, Rarity.RARE, mage.cards.c.ChromaticLantern.class));
         cards.add(new SetCardInfo("Davvol, Evincar of Rath", 36, Rarity.RARE, mage.cards.d.DavvolEvincarOfRath.class));
         cards.add(new SetCardInfo("Dust to Dust", 71, Rarity.UNCOMMON, mage.cards.d.DustToDust.class));
+        cards.add(new SetCardInfo("Emerald Collector", 63, Rarity.UNCOMMON, mage.cards.e.EmeraldCollector.class));
         cards.add(new SetCardInfo("Exotic Orchard", 79, Rarity.RARE, mage.cards.e.ExoticOrchard.class));
         cards.add(new SetCardInfo("Fellwar Stone", 74, Rarity.UNCOMMON, mage.cards.f.FellwarStone.class));
         cards.add(new SetCardInfo("Istvan, Butcher of Eln", 14, Rarity.RARE, mage.cards.i.IstvanButcherOfEln.class));
