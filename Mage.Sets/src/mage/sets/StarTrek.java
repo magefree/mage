a8ca0f3a05a77436c7d810f122acc1eac029e943
Muz Ali <git@mustaqila.li>
2026-08-09 16:05:31 -0500

    [TRK] Implement Bat'leth

--- Mage.Sets/src/mage/sets/StarTrek.java
+++ Mage.Sets/src/mage/sets/StarTrek.java
@@ -24,6 +24,7 @@ public final class StarTrek extends ExpansionSet {
 
         cards.add(new SetCardInfo("Amok Time", 183, Rarity.UNCOMMON, mage.cards.a.AmokTime.class));
         cards.add(new SetCardInfo("Assault Drone", 92, Rarity.COMMON, mage.cards.a.AssaultDrone.class));
+        cards.add(new SetCardInfo("Bat'leth", 94, Rarity.UNCOMMON, mage.cards.b.Batleth.class));
         cards.add(new SetCardInfo("Beckett Mariner, Impetuous Ensign", 138, Rarity.UNCOMMON, mage.cards.b.BeckettMarinerImpetuousEnsign.class));
         cards.add(new SetCardInfo("Blood Crypt", 278, Rarity.RARE, mage.cards.b.BloodCrypt.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Blood Crypt", 394, Rarity.RARE, mage.cards.b.BloodCrypt.class, NON_FULL_USE_VARIOUS));
