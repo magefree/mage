3a21ff17071252b571e7fb08c8975c01de4e60ee
Muz Ali <git@mustaqila.li>
2026-08-17 11:57:28 -0500

    [MBC] Implement Chief Magistrate of Mercadia

--- Mage.Sets/src/mage/sets/MysteryBoosterCommanderEdition.java
+++ Mage.Sets/src/mage/sets/MysteryBoosterCommanderEdition.java
@@ -28,6 +28,7 @@ public final class MysteryBoosterCommanderEdition extends ExpansionSet {
         cards.add(new SetCardInfo("Blor the Impervious", 28, Rarity.RARE, mage.cards.b.BlorTheImpervious.class));
         cards.add(new SetCardInfo("Boss Uramon, Shadow's Reach", 13, Rarity.RARE, mage.cards.b.BossUramonShadowsReach.class));
         cards.add(new SetCardInfo("Chaos Warp", 72, Rarity.RARE, mage.cards.c.ChaosWarp.class));
+        cards.add(new SetCardInfo("Chief Magistrate of Mercadia", 35, Rarity.RARE, mage.cards.c.ChiefMagistrateOfMercadia.class));
         cards.add(new SetCardInfo("Chromatic Lantern", 73, Rarity.RARE, mage.cards.c.ChromaticLantern.class));
         cards.add(new SetCardInfo("Davvol, Evincar of Rath", 36, Rarity.RARE, mage.cards.d.DavvolEvincarOfRath.class));
         cards.add(new SetCardInfo("Dust to Dust", 71, Rarity.UNCOMMON, mage.cards.d.DustToDust.class));
