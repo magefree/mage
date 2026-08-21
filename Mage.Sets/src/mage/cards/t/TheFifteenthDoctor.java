aca09c0ec2b7cb08dc712cb48440a882ab2dc56c
Muz Ali <git@mustaqila.li>
2026-08-20 20:17:58 -0500

    Address PR comments

--- Mage.Sets/src/mage/cards/t/TheFifteenthDoctor.java
+++ Mage.Sets/src/mage/cards/t/TheFifteenthDoctor.java
@@ -48,15 +48,15 @@ public class TheFifteenthDoctor extends CardImpl {
         this.toughness = new MageInt(3);
 
         // Whenever The Fifteenth Doctor enters or attacks, mill three cards. You may put an artifact card with mana value 2 or 3 from among them into your hand.
-        this.addAbility(new EntersBattlefieldOrAttacksSourceTriggeredAbility(new TheFifteenthDoctorEffect(), false));
+        this.addAbility(new EntersBattlefieldOrAttacksSourceTriggeredAbility(new TheFifteenthDoctorEffect()));
 
         // The first nonartifact spell you cast each turn has improvise. (Your artifacts can help cast that spell. Each artifact you tap after you’re done activating mana abilities pays for {1}.)
         this.addAbility(new SimpleStaticAbility(
-                new GainAbilityControlledSpellsEffect(new ImproviseAbility(), filter)
-                    .setText("The first nonartifact spell you cast each turn has improvise. " +
-                            "<i>(Your artifacts can help cast that spell. Each artifact you " +
-                            "tap after you’re done activating mana abilities pays for {1}.)")),
-                new TheFifteenthDoctorSpellWatcher()
+            new GainAbilityControlledSpellsEffect(new ImproviseAbility(), filter)
+                .setText("The first nonartifact spell you cast each turn has improvise. " +
+                        "<i>(Your artifacts can help cast that spell. Each artifact you " +
+                        "tap after you're done activating mana abilities pays for {1}.)")),
+            new TheFifteenthDoctorSpellWatcher()
         );
     }
 
@@ -76,21 +76,25 @@ class TheFifteenthDoctorEffect extends OneShotEffect {
 
     static {
         filter.add(Predicates.or(
-                new ManaValuePredicate(ComparisonType.EQUAL_TO, 2),
-                new ManaValuePredicate(ComparisonType.EQUAL_TO, 3)
+            new ManaValuePredicate(ComparisonType.EQUAL_TO, 2),
+            new ManaValuePredicate(ComparisonType.EQUAL_TO, 3)
         ));
     }
 
     TheFifteenthDoctorEffect() {
         super(Outcome.Benefit);
         staticText = "mill three cards. You may put an artifact card " +
-                "with mana value 2 or 3 from among them into your hand.";
+            "with mana value 2 or 3 from among them into your hand.";
     }
 
-    private TheFifteenthDoctorEffect(final TheFifteenthDoctorEffect effect) { super(effect); }
+    private TheFifteenthDoctorEffect(final TheFifteenthDoctorEffect effect) {
+        super(effect);
+    }
 
     @Override
-    public TheFifteenthDoctorEffect copy() { return new TheFifteenthDoctorEffect(this); }
+    public TheFifteenthDoctorEffect copy() {
+        return new TheFifteenthDoctorEffect(this);
+    }
 
     @Override
     public boolean apply(Game game, Ability source) {
@@ -99,20 +103,14 @@ class TheFifteenthDoctorEffect extends OneShotEffect {
             return false;
         }
         Cards cards = controller.millCards(3, source, game);
-        String message = "Put " + filter.getMessage() +
-                " from among the milled cards into your hand?";
-        if (cards.isEmpty()
-                || !controller.chooseUse(outcome, message, source, game)) {
-            return true;
-        }
-        TargetCard target = new TargetCard(Zone.GRAVEYARD, filter);
+        TargetCard target = new TargetCard(0, 1, Zone.GRAVEYARD, filter);
         if (controller.choose(outcome, cards, target, source, game)) {
             Card card = game.getCard(target.getFirstTarget());
             if (card != null) {
-                return controller.moveCards(card, Zone.HAND, source, game);
+                controller.moveCards(card, Zone.HAND, source, game);
             }
         }
-        return false;
+        return true;
     }
 }
 
@@ -134,7 +132,9 @@ class TheFifteenthDoctorSpellWatcher extends Watcher {
         }
     }
 
-    public int nonartifactSpellsCastThisTurn(UUID playerId) { return nonartifactSpells.getOrDefault(playerId, 0); }
+    public int nonartifactSpellsCastThisTurn(UUID playerId) {
+        return nonartifactSpells.getOrDefault(playerId, 0);
+    }
 
     @Override
     public void reset() {
@@ -148,7 +148,7 @@ enum TheFifteenthDoctorCastSpellPredicate implements ObjectSourcePlayerPredicate
 
     @Override
     public boolean apply(ObjectSourcePlayer<Card> input, Game game) {
-        if (input.getObject() != null && !input.getObject().isArtifact(game)) {
+        if (!input.getObject().isArtifact(game)) {
             TheFifteenthDoctorSpellWatcher watcher = game.getState().getWatcher(TheFifteenthDoctorSpellWatcher.class);
             return watcher != null && watcher.nonartifactSpellsCastThisTurn(input.getPlayerId()) == 0;
         }
@@ -156,5 +156,7 @@ enum TheFifteenthDoctorCastSpellPredicate implements ObjectSourcePlayerPredicate
     }
 
     @Override
-    public String toString() { return "The first nonartifact spell you cast each turn"; }
+    public String toString() {
+        return "The first nonartifact spell you cast each turn";
+    }
 }
