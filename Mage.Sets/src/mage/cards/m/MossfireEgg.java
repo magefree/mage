8d958c91f5b9e7b75be55aa61fb93ae6cbb92ec8
matoro <matoro@users.noreply.github.com>
2026-08-28 17:00:36 -0400

    Implement mana ability rules update from CR 20260807
    Affected cards:
    
    * Charmed Pendant
    * Chromatic Sphere
    * Darkwater Egg
    * Deranged Assistant
    * Millikin
    * Mossfire Egg
    * Selvala, Explorer Returned
    * Shadowblood Egg
    * Skycloud Egg
    * Sungrass Egg
    
    I used Arcum's Astrolabe as a replacement card for tests covering mana
    filtering.
    
    Fixes https://github.com/magefree/mage/issues/15876

--- Mage.Sets/src/mage/cards/m/MossfireEgg.java
+++ Mage.Sets/src/mage/cards/m/MossfireEgg.java
@@ -3,16 +3,15 @@ package mage.cards.m;
 
 import java.util.UUID;
 import mage.Mana;
+import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.common.SacrificeSourceCost;
 import mage.abilities.costs.common.TapSourceCost;
 import mage.abilities.costs.mana.ManaCostsImpl;
 import mage.abilities.effects.common.DrawCardSourceControllerEffect;
-import mage.abilities.mana.ActivatedManaAbilityImpl;
-import mage.abilities.mana.SimpleManaAbility;
+import mage.abilities.effects.mana.BasicManaEffect;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.Zone;
 
 /**
  *
@@ -24,11 +23,10 @@ public final class MossfireEgg extends CardImpl {
         super(ownerId,setInfo,new CardType[]{CardType.ARTIFACT},"{1}");
 
         // {2}, {tap}, Sacrifice Mossfire Egg: Add {R}{G}. Draw a card.
-        ActivatedManaAbilityImpl ability = new SimpleManaAbility(Zone.BATTLEFIELD, new Mana(0, 0, 0, 1, 1, 0, 0, 0), new ManaCostsImpl<>("{2}"));
+        final SimpleActivatedAbility ability = new SimpleActivatedAbility(new BasicManaEffect(new Mana(0, 0, 0, 1, 1, 0, 0, 0)), new ManaCostsImpl<>("{2}"));
         ability.addCost(new TapSourceCost());
         ability.addCost(new SacrificeSourceCost());
         ability.addEffect(new DrawCardSourceControllerEffect(1));
-        ability.setUndoPossible(false);
         this.addAbility(ability);
     }
 
