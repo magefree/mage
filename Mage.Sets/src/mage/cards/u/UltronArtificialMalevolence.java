0999330d5a7e3d490c34960b9320098b25abd8a8
Riley Jones <riajones88@gmail.com>
2026-09-07 14:06:19 -0700

    attempt to fix ultron

--- Mage.Sets/src/mage/cards/u/UltronArtificialMalevolence.java
+++ Mage.Sets/src/mage/cards/u/UltronArtificialMalevolence.java
@@ -96,12 +96,12 @@ class UltronArtificialMaleveolenceEffect extends OneShotEffect {
                 effect.setPower(2);
                 effect.setToughness(2);
 
-                effect.setTargetPointer(this.getTargetPointer());
+                effect.setSavedPermanent(permanent);
 
                 return effect.apply(game, source);
             } else {
                 CreateTokenCopyTargetEffect effect = new CreateTokenCopyTargetEffect(source.getControllerId());
-                effect.setTargetPointer(this.getTargetPointer());
+                effect.setSavedPermanent(permanent);
                 return effect.apply(game, source);
             }
         }
