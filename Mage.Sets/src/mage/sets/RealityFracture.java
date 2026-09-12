18012470b7426917c7829626efcb00864a3aa180
Muz <git@mustaqila.li>
2026-09-12 10:00:41 -0500

    [FRA] Implement Vraska, the Cutting Glare

--- Mage.Sets/src/mage/sets/RealityFracture.java
+++ Mage.Sets/src/mage/sets/RealityFracture.java
@@ -185,6 +185,8 @@ public final class RealityFracture extends ExpansionSet {
         cards.add(new SetCardInfo("Titanbones, Towering Heart", 266, Rarity.UNCOMMON, mage.cards.t.TitanbonesToweringHeart.class));
         cards.add(new SetCardInfo("Vigorbloom Vanguard", 161, Rarity.UNCOMMON, mage.cards.v.VigorbloomVanguard.class));
         cards.add(new SetCardInfo("Vraska, Soul of Stone", 277, Rarity.RARE, mage.cards.v.VraskaSoulOfStone.class));
+        cards.add(new SetCardInfo("Vraska, the Cutting Glare", 278, Rarity.RARE, mage.cards.v.VraskaTheCuttingGlare.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Vraska, the Cutting Glare", 319, Rarity.RARE, mage.cards.v.VraskaTheCuttingGlare.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Warrior's Blades", 163, Rarity.UNCOMMON, mage.cards.w.WarriorsBlades.class));
         cards.add(new SetCardInfo("Way of the Cryomancer", 223, Rarity.UNCOMMON, mage.cards.w.WayOfTheCryomancer.class));
         cards.add(new SetCardInfo("Way of the Deathbringer", 238, Rarity.UNCOMMON, mage.cards.w.WayOfTheDeathbringer.class));
