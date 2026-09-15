93c5e82766ecdbdcdc9f78a8524f063d90f0cd69
Muz Ali <git@mustaqila.li>
2026-09-14 21:09:16 -0500

    [FRA] Implement Identity Echo

--- Mage.Sets/src/mage/sets/RealityFracture.java
+++ Mage.Sets/src/mage/sets/RealityFracture.java
@@ -94,6 +94,8 @@ public final class RealityFracture extends ExpansionSet {
         cards.add(new SetCardInfo("Hexhaven Invigorator", 330, Rarity.MYTHIC, mage.cards.h.HexhavenInvigorator.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Hexhaven Invigorator", 458, Rarity.MYTHIC, mage.cards.h.HexhavenInvigorator.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Icy Reception", 30, Rarity.COMMON, mage.cards.i.IcyReception.class));
+        cards.add(new SetCardInfo("Identity Echo", 371, Rarity.RARE, mage.cards.i.IdentityEcho.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Identity Echo", 87, Rarity.RARE, mage.cards.i.IdentityEcho.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Ingris Stingerquill", 137, Rarity.MYTHIC, mage.cards.i.IngrisStingerquill.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Ingris Stingerquill", 351, Rarity.MYTHIC, mage.cards.i.IngrisStingerquill.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Ingris Stingerquill", 409, Rarity.MYTHIC, mage.cards.i.IngrisStingerquill.class, NON_FULL_USE_VARIOUS));
