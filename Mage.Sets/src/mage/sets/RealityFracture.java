4fe9a4a5d001a1819c432db0eafb5cc52f954849
Muz Ali <git@mustaqila.li>
2026-09-15 20:24:00 -0500

    [FRA] Implement Frostbite Pyromental

--- Mage.Sets/src/mage/sets/RealityFracture.java
+++ Mage.Sets/src/mage/sets/RealityFracture.java
@@ -76,6 +76,9 @@ public final class RealityFracture extends ExpansionSet {
         cards.add(new SetCardInfo("Forest", 394, Rarity.LAND, mage.cards.basiclands.Forest.class, FULL_ART_BFZ_VARIOUS));
         cards.add(new SetCardInfo("Forest", 395, Rarity.LAND, mage.cards.basiclands.Forest.class, FULL_ART_BFZ_VARIOUS));
         cards.add(new SetCardInfo("Forest", 396, Rarity.LAND, mage.cards.basiclands.Forest.class, FULL_ART_BFZ_VARIOUS));
+        cards.add(new SetCardInfo("Frostbite Pyromental", 135, Rarity.RARE, mage.cards.f.FrostbitePyromental.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Frostbite Pyromental", 333, Rarity.RARE, mage.cards.f.FrostbitePyromental.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Frostbite Pyromental", 461, Rarity.RARE, mage.cards.f.FrostbitePyromental.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Gallia, Tragic Host", 228, Rarity.UNCOMMON, mage.cards.g.GalliaTragicHost.class));
         cards.add(new SetCardInfo("Garruk, Curse Breaker", 259, Rarity.MYTHIC, mage.cards.g.GarrukCurseBreaker.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Garruk, Curse Breaker", 314, Rarity.MYTHIC, mage.cards.g.GarrukCurseBreaker.class, NON_FULL_USE_VARIOUS));
