7c46c1f04eeec6120a7d1f01894d2741cea2cea5
Muz Ali <git@mustaqila.li>
2026-09-16 22:38:48 -0500

    [FRC] Implement Nissa, Leyline Tamer

--- Mage.Sets/src/mage/sets/RealityFractureCommander.java
+++ Mage.Sets/src/mage/sets/RealityFractureCommander.java
@@ -56,6 +56,7 @@ public final class RealityFractureCommander extends ExpansionSet {
         cards.add(new SetCardInfo("Martial Coup", 28, Rarity.RARE, mage.cards.m.MartialCoup.class));
         cards.add(new SetCardInfo("Mass Polymorph", 42, Rarity.RARE, mage.cards.m.MassPolymorph.class));
         cards.add(new SetCardInfo("Mystic Gate", 76, Rarity.RARE, mage.cards.m.MysticGate.class));
+        cards.add(new SetCardInfo("Nissa, Leyline Tamer", 2, Rarity.MYTHIC, mage.cards.n.NissaLeylineTamer.class));
         cards.add(new SetCardInfo("Occult Epiphany", 43, Rarity.RARE, mage.cards.o.OccultEpiphany.class));
         cards.add(new SetCardInfo("Overlord of the Mistmoors", 29, Rarity.MYTHIC, mage.cards.o.OverlordOfTheMistmoors.class));
         cards.add(new SetCardInfo("Path of Ancestry", 77, Rarity.COMMON, mage.cards.p.PathOfAncestry.class));
