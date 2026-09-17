fb326b8cb295d8d48ffd1eaa2145dba6f8dac4cb
Muz Ali <git@mustaqila.li>
2026-09-16 21:08:32 -0500

    [FRC] Add reprints

--- Mage.Sets/src/mage/sets/RealityFractureCommander.java
+++ Mage.Sets/src/mage/sets/RealityFractureCommander.java
@@ -19,7 +19,12 @@ public final class RealityFractureCommander extends ExpansionSet {
         super("Reality Fracture Commander", "FRC", ExpansionSet.buildDate(2026, 10, 2), SetType.SUPPLEMENTAL);
         this.hasBasicLands = false;
 
+        cards.add(new SetCardInfo("Akroma, Angel of Fury", 19, Rarity.MYTHIC, mage.cards.a.AkromaAngelOfFury.class));
+        cards.add(new SetCardInfo("Arcane Signet", 20, Rarity.UNCOMMON, mage.cards.a.ArcaneSignet.class));
+        cards.add(new SetCardInfo("Command Tower", 22, Rarity.COMMON, mage.cards.c.CommandTower.class));
         cards.add(new SetCardInfo("Darksteel Angel", 98, Rarity.RARE, mage.cards.d.DarksteelAngel.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Darksteel Angel", 13, Rarity.RARE, mage.cards.d.DarksteelAngel.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Reflecting Pool", 23, Rarity.RARE, mage.cards.r.ReflectingPool.class));
+        cards.add(new SetCardInfo("Sol Ring", 21, Rarity.UNCOMMON, mage.cards.s.SolRing.class));
     }
 }
