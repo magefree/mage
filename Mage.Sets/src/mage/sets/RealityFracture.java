9d2430a7bcfc949ba5a042b3d082654134a48afc
Muz <muz@users.noreply.github.com>
2026-09-17 21:24:36 -0500

    [FRA] Implement Terminal Criticism (#16250)

--- Mage.Sets/src/mage/sets/RealityFracture.java
+++ Mage.Sets/src/mage/sets/RealityFracture.java
@@ -225,6 +225,8 @@ public final class RealityFracture extends ExpansionSet {
         cards.add(new SetCardInfo("Tam, the Possibility", 276, Rarity.RARE, mage.cards.t.TamThePossibility.class));
         cards.add(new SetCardInfo("Tarmogoyf", 116, Rarity.MYTHIC, mage.cards.t.Tarmogoyf.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Tarmogoyf", 374, Rarity.MYTHIC, mage.cards.t.Tarmogoyf.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Terminal Criticism", 366, Rarity.UNCOMMON, mage.cards.t.TerminalCriticism.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Terminal Criticism", 68, Rarity.UNCOMMON, mage.cards.t.TerminalCriticism.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Tether Technician", 94, Rarity.COMMON, mage.cards.t.TetherTechnician.class));
         cards.add(new SetCardInfo("Tethermage's Advantage", 117, Rarity.COMMON, mage.cards.t.TethermagesAdvantage.class));
         cards.add(new SetCardInfo("Tetsuko Umezawa, Fugitive", 221, Rarity.UNCOMMON, mage.cards.t.TetsukoUmezawaFugitive.class));
