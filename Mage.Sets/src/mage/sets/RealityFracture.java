934dbbfed4a59e9943a28967d97e29705c7cc49c
Muz <muz@users.noreply.github.com>
2026-09-17 21:42:29 -0500

    [FRA] Implement Koth, the Geomancer (#16253)

--- Mage.Sets/src/mage/sets/RealityFracture.java
+++ Mage.Sets/src/mage/sets/RealityFracture.java
@@ -139,6 +139,7 @@ public final class RealityFracture extends ExpansionSet {
         cards.add(new SetCardInfo("Konstrari Charm", 138, Rarity.UNCOMMON, mage.cards.k.KonstrariCharm.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Konstrari Charm", 433, Rarity.UNCOMMON, mage.cards.k.KonstrariCharm.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Konstrari Improviser", 139, Rarity.COMMON, mage.cards.k.KonstrariImproviser.class));
+        cards.add(new SetCardInfo("Koth, the Geomancer", 248, Rarity.UNCOMMON, mage.cards.k.KothTheGeomancer.class));
         cards.add(new SetCardInfo("Koth of the Homestead", 199, Rarity.UNCOMMON, mage.cards.k.KothOfTheHomestead.class));
         cards.add(new SetCardInfo("Kwia Vigorbloom", 140, Rarity.MYTHIC, mage.cards.k.KwiaVigorbloom.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Kwia Vigorbloom", 352, Rarity.MYTHIC, mage.cards.k.KwiaVigorbloom.class, NON_FULL_USE_VARIOUS));
