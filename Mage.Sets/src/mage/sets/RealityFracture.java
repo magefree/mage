9ca2ee39650bb3c3927e41f8873e28e517c5b352
Muz <muz@users.noreply.github.com>
2026-09-17 21:43:34 -0500

    [FRA] Implement Ajani's Anguish (#16251)

--- Mage.Sets/src/mage/sets/RealityFracture.java
+++ Mage.Sets/src/mage/sets/RealityFracture.java
@@ -28,6 +28,8 @@ public final class RealityFracture extends ExpansionSet {
         cards.add(new SetCardInfo("Aerid Konstrari", 407, Rarity.MYTHIC, mage.cards.a.AeridKonstrari.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Aerid Konstrari", 417, Rarity.MYTHIC, mage.cards.a.AeridKonstrari.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Afterthought Sentry", 166, Rarity.COMMON, mage.cards.a.AfterthoughtSentry.class));
+        cards.add(new SetCardInfo("Ajani's Anguish", 367, Rarity.RARE, mage.cards.a.AjanisAnguish.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Ajani's Anguish", 72, Rarity.RARE, mage.cards.a.AjanisAnguish.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Ajani Resolute", 195, Rarity.MYTHIC, mage.cards.a.AjaniResolute.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Ajani Resolute", 291, Rarity.MYTHIC, mage.cards.a.AjaniResolute.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Ajani Unrelenting", 242, Rarity.MYTHIC, mage.cards.a.AjaniUnrelenting.class, NON_FULL_USE_VARIOUS));
