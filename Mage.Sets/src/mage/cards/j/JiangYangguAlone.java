3b7b76585fb82845fa9f5871e549c7c65901a855
Muz Ali <git@mustaqila.li>
2026-09-19 19:14:01 -0500

    Move triggered ability to reusable class

--- Mage.Sets/src/mage/cards/j/JiangYangguAlone.java
+++ Mage.Sets/src/mage/cards/j/JiangYangguAlone.java
@@ -2,7 +2,7 @@ package mage.cards.j;
 
 import mage.MageInt;
 import mage.abilities.Ability;
-import mage.abilities.TriggeredAbilityImpl;
+import mage.abilities.common.AttacksPlayerAloneControlledTriggeredAbility;
 import mage.abilities.dynamicvalue.DynamicValue;
 import mage.abilities.effects.Effect;
 import mage.abilities.effects.common.DrawCardSourceControllerEffect;
@@ -16,12 +16,8 @@ import mage.cards.CardSetInfo;
 import mage.constants.CardType;
 import mage.constants.SubType;
 import mage.constants.SuperType;
-import mage.constants.Zone;
 import mage.counters.CounterType;
 import mage.game.Game;
-import mage.game.events.GameEvent;
-import mage.game.permanent.Permanent;
-import mage.target.targetpointer.FixedTarget;
 import mage.watchers.common.DiscardedCardWatcher;
 
 import java.util.UUID;
@@ -45,7 +41,10 @@ public final class JiangYangguAlone extends CardImpl {
         this.addAbility(new MenaceAbility());
 
         // Whenever a creature you control attacks a player alone, discard a card, then draw a card. Then put a +1/+1 counter on that creature for each card you've discarded this turn.
-        Ability ability = new JiangYangguAloneTriggeredAbility();
+        Ability ability = new AttacksPlayerAloneControlledTriggeredAbility(new DiscardControllerEffect(1));
+        ability.addEffect(new DrawCardSourceControllerEffect(JiangYangguAloneValue.instance).concatBy("then"));
+        ability.addEffect(new AddCountersTargetEffect(CounterType.P1P1.createInstance(), JiangYangguAloneValue.instance)
+            .setText("then put a +1/+1 counter on that creature for each card you've discarded this turn"));
         this.addAbility(ability.addHint(JiangYangguAloneValue.getHint()), new DiscardedCardWatcher());
     }
 
@@ -56,51 +55,6 @@ public final class JiangYangguAlone extends CardImpl {
     @Override
     public JiangYangguAlone copy() {
         return new JiangYangguAlone(this);
-    }
-}
-
-class JiangYangguAloneTriggeredAbility extends TriggeredAbilityImpl {
-
-    JiangYangguAloneTriggeredAbility() {
-        super(Zone.BATTLEFIELD, new DiscardControllerEffect(1));
-        this.addEffect(new DrawCardSourceControllerEffect(JiangYangguAloneValue.instance).concatBy("then"));
-        this.addEffect(new AddCountersTargetEffect(CounterType.P1P1.createInstance(), JiangYangguAloneValue.instance)
-                .setText("then put a +1/+1 counter on that creature for each card you've discarded this turn"));
-        this.setTriggerPhrase("Whenever a creature you control attacks a player alone, ");
-    }
-
-    private JiangYangguAloneTriggeredAbility(final JiangYangguAloneTriggeredAbility ability) {
-        super(ability);
-    }
-
-    @Override
-    public JiangYangguAloneTriggeredAbility copy() {
-        return new JiangYangguAloneTriggeredAbility(this);
-    }
-
-    @Override
-    public boolean checkEventType(GameEvent event, Game game) {
-        return event.getType() == GameEvent.EventType.ATTACKER_DECLARED;
-    }
-
-    @Override
-    public boolean checkTrigger(GameEvent event, Game game) {
-        Permanent permanent = game.getPermanent(event.getSourceId());
-        if (permanent == null || !permanent.isControlledBy(getControllerId())) {
-            return false;
-        }
-        UUID defenderId = game.getCombat().getDefenderId(permanent.getId());
-        if (defenderId == null || game.getPlayer(defenderId) == null) {
-            return false;
-        }
-        if (game.getCombat().getAttackers().stream()
-                .map(game.getCombat()::getDefenderId)
-                .filter(defenderId::equals)
-                .count() != 1) {
-            return false;
-        }
-        this.getAllEffects().setTargetPointer(new FixedTarget(permanent, game));
-        return true;
     }
 }
 
