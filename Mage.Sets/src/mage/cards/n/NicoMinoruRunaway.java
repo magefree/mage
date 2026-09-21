e60789620c607d557c8112c128b314b0f6dffeeb
Muz <muz@users.noreply.github.com>
2026-09-20 20:53:08 -0500

    Change Nico Minoru to using modern CardUtil.castSpellWithAttributesForFree

--- Mage.Sets/src/mage/cards/n/NicoMinoruRunaway.java
+++ Mage.Sets/src/mage/cards/n/NicoMinoruRunaway.java
@@ -1,23 +1,15 @@
 package mage.cards.n;
 
-import mage.ApprovingObject;
 import mage.MageInt;
-import mage.MageObject;
 import mage.abilities.Ability;
-import mage.abilities.SpellAbility;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.common.SpellCastControllerTriggeredAbility;
-import mage.abilities.costs.Cost;
-import mage.abilities.costs.Costs;
-import mage.abilities.costs.CostsImpl;
 import mage.abilities.costs.common.DiscardTargetCost;
 import mage.abilities.costs.common.TapSourceCost;
 import mage.abilities.costs.mana.ManaCostsImpl;
 import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.DamagePlayersEffect;
-import mage.cards.Card;
-import mage.cards.CardImpl;
-import mage.cards.CardSetInfo;
+import mage.cards.*;
 import mage.constants.*;
 import mage.filter.FilterSpell;
 import mage.filter.predicate.Predicates;
@@ -27,9 +19,7 @@ import mage.players.Player;
 import mage.target.common.TargetCardInHand;
 import mage.util.CardUtil;
 
-import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 /**
  * @author notshauna
@@ -83,7 +73,7 @@ public final class NicoMinoruRunaway extends CardImpl {
         NicoMinoruRunawayEffect() {
             super(Outcome.PlayForFree);
             staticText = "Exile cards from the top of your library until you exile a nonland card." +
-                    "You may cast that card without paying its mana cost.";
+                    " You may cast that card without paying its mana cost.";
         }
 
         private NicoMinoruRunawayEffect(final NicoMinoruRunawayEffect effect) {
@@ -95,44 +85,30 @@ public final class NicoMinoruRunaway extends CardImpl {
             return new NicoMinoruRunawayEffect(this);
         }
 
-        @Override
-        public boolean apply(Game game, Ability source) {
-            Player controller = game.getPlayer(source.getControllerId());
-            if (controller == null || !controller.getLibrary().hasCards()) {
-                return false;
-            }
-            for (Card card : controller.getLibrary().getCards(game)) {
-                controller.moveCards(card, Zone.EXILED, source, game);
+        private Card getCard(Player player, Cards cards, Game game, Ability source) {
+            for (Card card : player.getLibrary().getCards(game)) {
+                cards.add(card);
+                player.moveCards(card, Zone.EXILED, source, game);
+                game.processAction();
                 if (!card.isLand(game)) {
-                    List<Card> castableComponents = CardUtil.getCastableComponents(card, null, source, controller, game, null, false);
-                    if (castableComponents.isEmpty()) {
-                        break;
-                    }
-                    String partsInfo = castableComponents
-                            .stream()
-                            .map(MageObject::getLogName)
-                            .collect(Collectors.joining(" or "));
-                    if (!controller.chooseUse(Outcome.PlayForFree, "You may cast that card without paying its mana cost.(" + partsInfo + ")?", source, game)) {
-                        break;
-                    }
-                    castableComponents.forEach(partCard -> game.getState().setValue("PlayFromNotOwnHandZone" + partCard.getId(), Boolean.TRUE));
-                    SpellAbility chosenAbility = controller.chooseAbilityForCast(card, game, true);
-                    if (chosenAbility != null) {
-                        Card faceCard = game.getCard(chosenAbility.getSourceId());
-                        if (faceCard != null) {
-                            Costs<Cost> newCosts = new CostsImpl<>();
-                            newCosts.addAll(chosenAbility.getCosts());
-                            controller.setCastSourceIdWithAlternateMana(faceCard.getId(), null, newCosts);
-                            controller.cast(
-                                    chosenAbility, game, true,
-                                    new ApprovingObject(source, game)
-                            );
-                        }
-                    }
-                    castableComponents.forEach(partCard -> game.getState().setValue("PlayFromNotOwnHandZone" + partCard.getId(), null));
-                    break;
+                    return card;
                 }
             }
+            return null;
+        }
+
+        @Override
+        public boolean apply(Game game, Ability source) {
+            Player player = game.getPlayer(source.getControllerId());
+            if (player == null) {
+                return false;
+            }
+            Cards cards = new CardsImpl();
+            Card card = getCard(player, cards, game, source);
+            if (card != null) {
+                CardUtil.castSpellWithAttributesForFree(player, source, game, card);
+            }
+            cards.retainZone(Zone.EXILED, game);
             return true;
         }
     }
